8adad3753261fd0aa7a82023e78d49ce5611ba23
jangsumin <jsm0726@naver.com>
2022-11-09 22:29:19 +0900

    chore: skip MissionUtils.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 
 const PROMPT_MONEY = '구입금액을 입력해 주세요.';
 const LOTTO_PRICE = 1000;
@@ -13,20 +13,20 @@ class App {
   }
 
   userInput(prompt) {
-    MissionUtils.Console.readLine(`${prompt}\n`, (input) => {
+    Console.readLine(`${prompt}\n`, (input) => {
       this.countLotto(input);
     });
   }
 
   countLotto(money) {
     const amountLotto = money / LOTTO_PRICE;
-    MissionUtils.Console.print(`\n${amountLotto}개를 구매했습니다.`);
+    Console.print(`\n${amountLotto}개를 구매했습니다.`);
     this.createLotto(amountLotto);
   }
 
   createLotto(amount) {
     for (let i = 0; i < amount; i++) {
-      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
       this.arrayLotto.push(numbers);
     }
   }
