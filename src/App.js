5dc471ebee535c26e0c4fc1c6c835a7a1d43259e
이종현 <ijonghyeon@ijonghyeon-ui-MacBookAir.local>
2022-11-09 22:09:05 +0900

    feat : before statistics

--- src/App.js
+++ src/App.js
@@ -10,6 +10,41 @@ const Check = require('./Check')
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  // 보너스번호
+  bonusNumber(){
+    let BONUS = 0
+    MissionUtils.Console.readLine('',(bonus) => {
+      MissionUtils.Console.print('보너스 번호를 입력해 주세요')
+      BONUS = bonus
+    })
+    console.log(BONUS)
+    return BONUS
+  }
+  
+  // 당첨번호
+  winNumber(){
+    let WIN_NUMBER = 0
+    MissionUtils.Console.readLine('',(winNum) => {
+      MissionUtils.Console.print(`당첨 번호를 입력해 주세요.`)
+      console.log(winNum)
+      WIN_NUMBER = winNum.split(",")
+      // 유효성 검사
+      const LottoCheck = new Lotto(WIN_NUMBER)
+      LottoCheck.setCheck()
+    })
+    return WIN_NUMBER
+  }
+  
+  // 로또 번호 출력
+  lottoNumbers(times){
+    let lottoNumbersArray = []
+    for (let i = 0; i < times; i ++){
+      let LOTTO_NUM = this.createLottoNumber()
+      console.log(LOTTO_NUM)
+      lottoNumbersArray.push(LOTTO_NUM)
+    }
+    return lottoNumbersArray
+  }
 
   // 로또 번호 생성
   createLottoNumber(){
@@ -21,56 +56,51 @@ class App {
     return lottoNum
   }
 
-  // 당첨번호
-  winNumber(){
-    let WIN_NUMBER = 0
-    MissionUtils.Console.readLine('',(winNum) => {
-      WIN_NUMBER = winNum.split(",")
-      // 유효성 검사
-      const LottoCheck = new Lotto(WIN_NUMBER)
-      LottoCheck.setCheck()
-    })
-    return WIN_NUMBER
-  }
-
-
-  // 로또 번호 출력
-  lottoNumbers(times){
-    // 당첨 번호 출력
-    const WIN_NUMBER = this.winNumber()
-    for (let i = 0; i < times; i ++){
-      let LOTTO_NUM = this.createLottoNumber()
-      console.log(LOTTO_NUM)
-      console.log(WIN_NUMBER)
-    }
-  }
 
   // 로또 구입
   buyLotto(money) {
     let LOTTO_AMOUNT = 0
+    let LOTTO_ARRAY = []
     const checking = new Check()
     LOTTO_AMOUNT = checking.buyLotto(money)
     MissionUtils.Console.print(`${LOTTO_AMOUNT}개를 구매했습니다.`)
-    this.lottoNumbers(LOTTO_AMOUNT)
+    LOTTO_ARRAY = this.lottoNumbers(LOTTO_AMOUNT)
+    return LOTTO_ARRAY
   }
 
   // 로또 구입 금액
   money() {
     let MONEY = 0
+    let LOTTO_ARRAY = []
     MissionUtils.Console.readLine('', (number) => {
       const checking = new Check()
       checking.checkNumbers(number)
       console.log(`${number}`)
       MONEY = number
     })
-    this.buyLotto(MONEY)
+    LOTTO_ARRAY = this.buyLotto(MONEY)
+    return LOTTO_ARRAY
   }
 
   // Play
   play() {
+    let LOTTO_ARRAY  = []
+    let WIN_NUMBER = []
+    let BONUS = 0
     // Start
     MissionUtils.Console.print("구입금액을 입력해 주세요.")
-    this.money()
+    LOTTO_ARRAY = this.money()
+    WIN_NUMBER = this.winNumber()
+    BONUS = this.bonusNumber()
+
+    MissionUtils.Console.print("당첨 통계")
+    MissionUtils.Console.print("---")
+    this.statistics(LOTTO_ARRAY,WIN_NUMBER,BONUS)
+  }
+
+  // statistics
+  statistics(lottoArray,winNumber,bonus){
+    
   }
 }
 
