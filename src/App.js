170f0f10cf14b9209f5f1c4fbe4257957953ae1e
chiman <rlaclghks123@naver.com>
2022-11-09 18:01:08 +0900

    feat: 구입금액을 통한 로또를 구매하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ class App {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
       if (money % 1000 !== 0 || money === "0")
         throw new Error("[ERROR] 구입 금액은 1000원 단위 입니다.");
+      const lottoTickets = this.getLottoTickets(money);
+
+      console.log(`\n${lottoTickets}개를 구매했습니다.`);
     });
+  }
+
+  getLottoTickets(money) {
+    return money / 1000;
   }
 }
 
