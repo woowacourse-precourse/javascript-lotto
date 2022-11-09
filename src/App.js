e91f98b396d196c8005b7ce368cb633d6eaadc7a
Suyeon <msjgr2801@gmail.com>
2022-11-10 00:31:55 +0900

    Feat: [App.js, Lotto.js] 구매번호, 당첨번호 비교 후 수익률 출력 기능 구현
    [App.js]
    1. 당첨 번호와 가지고 있는 로또 번호 비교, Lotto.js 에서 로또 숫자 별 당첨 순위를 받아옴
    2. 가지고 있는 등수의 금액만큼 합산
    3. 원금과 얻은 금액으로 수익률 계산
    4. 가지고 있는 로또 번호 등수와 수익률 출력
    
    [Lotto.js]
    1. App.js 로 부터 당첨 번호를 받아서 로또 번호와 비교하여 일치하는 숫자를 셈
    2. 일치하는 숫자를 통해서 로또 당첨 순위를 App.js 로 반환

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 const Lotto = require("./Lotto");
 
+const FIRST_LOTTO = 200000000;
+const SECOND_LOTTO = 30000000;
+const THIRD_LOTTO = 1500000;
+const FOURTH_LOTTO = 50000;
+const FIFTH_LOTTO = 5000;
+
 class App {
   play() {
-    // 클래스를 분리 해서 시작
-    const countOfLotto = this.calCountOfLotto(); // 수정
-    const myLotto = this.generateLottoNumber(countOfLotto); // generateLotto
-    const winningLotto = this.getWinningLotto();
+    const countOfLotto = this.buyLotto();
+    const myLotto = this.generateLottoNumber(countOfLotto);
+    const winningNums = this.getWinningNums();
+    this.calLottoProfit(myLotto, winningNums);
   }
 
-  calCountOfLotto() {
+  buyLotto() {
     let money = 0;
 
     MissionUtils.Console.readLine("구매금액을 입력해주세요. \n >", (answer) => {
@@ -23,7 +29,7 @@ class App {
   }
 
   isValidMoney(money) {
-    if (Number.isNaN(number)) {
+    if (Number.isNaN(money)) {
       throw new Error(`[ERROR] 입력 금액이 숫자형태가 아닙니다.`);
     }
 
@@ -43,51 +49,125 @@ class App {
   generateLottoNumber(count) {
     const myLotto = [];
 
-    while (myLotto.length !== count) {
-      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-      const lotto = new Lotto(numbers, false);
+    while (myLotto.length !== count) { // greater than 으로 변경
+      const numbersOfLotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      const lotto = new Lotto(numbersOfLotto, false);
       myLotto.push(lotto);
     }
-    this.printMyLotto(count, myLotto);
+    this.showMyLotto(myLotto);
 
     return myLotto;
   }
 
-  printMyLotto(count, myLotto) {
-    MissionUtils.Console.print(`${count}개를 구매했습니다.`);
+  showMyLotto(myLotto) {
+    MissionUtils.Console.print(`${myLotto.length}개를 구매했습니다.`);
     myLotto.forEach((lotto) => {
       lotto.printLottoNumbers();
     });
   }
 
-  getWinningLotto() {
+  getWinningNums() {
     const winningLotto = [];
 
     MissionUtils.Console.readLine(
       "당첨 번호를 입력해주세요. \n >",
       (answer) => {
-        answer.split("").forEach((element) => {
+        answer.split(",").forEach((element) => {
           winningLotto.push(Number(element));
         });
       }
     );
     MissionUtils.Console.close();
 
-    MissionUtils.Console.reaLine(
+    MissionUtils.Console.readLine(
       "보너스 번호를 입력해주세요. \n >",
       (answer) => {
         winningLotto.push(Number(answer));
       }
     );
     MissionUtils.Console.close();
-    new Lotto(winningLotto, true);
+    this.validWinningLotto(winningLotto);
 
     return winningLotto;
   }
 
-  isValidLotto(lotto, bonus) {
-    
+  validWinningLotto(numbers) {
+    const numSet = new Set(numbers);
+    if (numbers.length !== 7) {
+      throw new Error(`[ERROR] 로또 번호는 7개여야 합니다.`);
+    }
+
+    if (numSet.size !== 7) {
+      throw new Error(`[ERROR] 로또 번호가 중복됩니다.`);
+    }
+
+    numbers.forEach((num) => {
+      if (Number.isNaN(num)) {
+        throw new Error(`[ERROR] 숫자가 아닌 값이 있습니다.`);
+      }
+    });
   }
+
+  calLottoProfit(myLotto, winningNums) {
+    const winningInfo = {};
+    const principal = myLotto.length * 1000;
+    let totalMoney = 0;
+
+    for (let rank = 0; rank <= 5; rank++) {
+      winningInfo[rank] = 0;
+    }
+
+    myLotto.forEach((lotto) => {
+      const rank = lotto.calRank([...winningNums]);
+      winningInfo[rank] += 1;
+      totalMoney += this.moneyAccordingToRank(rank);
+    });
+
+    this.showProfitResult(winningInfo, totalMoney, principal);
+  }
+
+  moneyAccordingToRank(rank) {
+    let money = 0;
+
+    switch(rank) {
+      case 1:
+        money += FIRST_LOTTO;
+        break;
+
+      case 2:
+        money += SECOND_LOTTO;
+        break;
+
+      case 3:
+        money += THIRD_LOTTO;
+        break;
+
+      case 4:
+        money += FOURTH_LOTTO;
+        break;
+
+      case 5:
+        money += FIFTH_LOTTO;
+        break;
+
+      default:
+        break;
+    }
+
+    return money;
+  }
+
+  showProfitResult(winningInfo, profit, principal) {
+    const totalProfit = profit / principal;
+
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${winningInfo[5]}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${winningInfo[4]}개`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${winningInfo[3]}개`);
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningInfo[2]}개`);
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${winningInfo[1]}개`);
+    MissionUtils.Console.print(`총 수익률은 ${Math.floor(totalProfit * 1000) / 10}%입니다.`)
+  }
+
 }
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -4,41 +4,80 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers, isWinning);
+    this.validate(numbers);
     this.#numbers = numbers;
   }
 
   // 유효한 값 검사
-  validate(numbers, isWinning) {
+  validate(numbers) {
     const numSet = new Set(numbers);
 
-    if (isWinning) {
-      if (numbers.length !== 7) {
-        throw new Error("[ERROR] 당첨 번호는 총 7개여야 합니다.")
-      }
-
-      if (numSet.size !== 7) {
-        throw new Error("[ERROR] 로또 번호가 중복됩니다.");
-      }
-    } else {
-      if (numbers.length !== 6) {
-        throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-      }
-
-      if (numSet.size !== 6) {
-        throw new Error(`[ERROR] 로또 번호가 중복됩니다.`)
-      }
+    if (numbers.length !== 6) {
+      throw `[ERROR] 로또 번호는 6개여야 합니다.`;
     }
-    
-    numbers.forEach(num => {
+
+    if (numSet.size !== 6) {
+      throw `[ERROR] 로또 번호가 중복됩니다.`;
+    }
+
+    numbers.forEach((num) => {
       if (Number.isNaN(num)) {
-        throw new Error(`[ERROR] 숫자가 아닌 값이 있습니다.`)
+        throw `[ERROR] 숫자가 아닌 값이 있습니다.`;
       }
-    })
+    });
   }
 
   printLottoNumbers() {
-    MissionUtils.Console.print(this.#numbers);
+    MissionUtils.Console.print(`[${this.#numbers.join(", ")}]`);
+  }
+
+  calRank(winningNums) {
+    const bonusNumber = winningNums.pop();
+    const winningNumSet = new Set(winningNums);
+    let countOfCorrectNum = 0;
+    let bonusFlag = false;
+
+    for (let num of this.#numbers) {
+      if (winningNumSet.has(num)) {
+        countOfCorrectNum++;
+      }
+      if (num === bonusNumber){
+        bonusFlag = true;
+      }
+    }
+
+    return this.standardOfRank(countOfCorrectNum, bonusFlag);
+  }
+
+  standardOfRank(count, flag) {
+    let rank = 0;
+
+    switch (count) {
+      case 3:
+        rank = 5;
+        break;
+
+      case 4:
+        rank = 4;
+        break;
+
+      case 5:
+        if (flag === true) {
+          rank = 2;
+          break;
+        } 
+        rank = 3;
+        break;
+
+      case 6:
+        rank = 1;
+        break;
+      
+      default:
+        break;
+    }
+
+    return rank;
   }
 }
 
