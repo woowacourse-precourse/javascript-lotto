4ec0d0645f6fe37e7b66ac9ae0fbbfe781e6ce5b
Suyoung <jsy8492@gmail.com>
2022-11-09 17:04:49 +0900

    refactor(app): 로또 구입 금액 유효성 검사를 LottoManager 클래스에서 처리하도록 수정

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App {
   play() {
     Console.readLine('구입금액을 입력해주세요.\n', purchaseAmount => {
       try {
-        this.validate(purchaseAmount);
+        this.lottoManager.validatePurchaseAmount(purchaseAmount);
         this.lottoManager.issueLottos(purchaseAmount);
         this.printLottos(this.lottoManager.lottos);
       } catch (err) {
@@ -20,31 +20,6 @@ class App {
         throw err;
       }
     });
-  }
-
-  validate(purchaseAmount) {
-    if (this.isInvalidPurchaseAmount(purchaseAmount)) {
-      throw new Error(
-        '[ERROR] 구입 금액은 1,000으로 나누어 떨어지는 숫자여야 합니다.',
-      );
-    }
-  }
-
-  isInvalidPurchaseAmount(purchaseAmount) {
-    return (
-      !/^\d+$/g.test(purchaseAmount) ||
-      parseInt(purchaseAmount, 10) % 1000 !== 0
-    );
-  }
-
-  issueLottos(purchaseAmount) {
-    const lottoCount = purchaseAmount / 1000;
-    /**
-     * TODO: 랜덤 번호 6개 생성하여 lottoCount 만큼의 로또 발행
-     */
-    for (let i = 0; i < lottoCount; i++) {
-      this.lottos.push(new Lotto([1, 2, 3, 4, 5, 6]));
-    }
   }
 
   printLottos(lottos) {
--- src/LottoManager.js
+++ src/LottoManager.js
@@ -7,6 +7,21 @@ class LottoManager {
   #winningNumbers;
   #earningsRate = 0;
 
+  validatePurchaseAmount(purchaseAmount) {
+    if (this.isInvalidPurchaseAmount(purchaseAmount)) {
+      throw new Error(
+        '[ERROR] 구입 금액은 1,000으로 나누어 떨어지는 숫자여야 합니다.',
+      );
+    }
+  }
+
+  isInvalidPurchaseAmount(purchaseAmount) {
+    return (
+      !/^\d+$/g.test(purchaseAmount) ||
+      parseInt(purchaseAmount, 10) % 1000 !== 0
+    );
+  }
+
   issueLottos(purchaseAmount) {
     const lottoCount = purchaseAmount / 1000;
 
