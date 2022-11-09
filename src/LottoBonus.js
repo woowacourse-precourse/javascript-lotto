91cbf22f50451ea5209348e39c3c48ce5dcdcac5
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-09 19:35:21 +0900

    Feat : make LottoBonus Class for bonus number

---
+++ src/LottoBonus.js
@@ -0,0 +1,27 @@
+class LottoBonus {
+  #number;
+
+  constructor(number) {
+    this.validate(number);
+    this.#number = number;
+  }
+
+  validate(number) {
+    this.validateIsNumber(number);
+    this.validateIsProperNumber(number);
+  }
+  validateIsNumber(number) {
+    const nums = number.split("");
+    if (nums.some((num) => !/[0-9]/.test(num))) {
+      throw new Error("[ERROR] 숫자가 아닌 값이 입력되었습니다.");
+    }
+  }
+
+  validateIsProperNumber(number) {
+    if (0 < number || 45 > number) {
+      throw new Error("[ERROR] 1~45 사이의 숫자를 입력하여야 합니다.");
+    }
+  }
+}
+
+module.exports = LottoBonus;
