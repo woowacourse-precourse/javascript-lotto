90ae08ccbd5bd44b074d7297f22aeb7b85a508a6
loco9939 <kls9939@naver.com>
2022-11-09 22:13:33 +0900

    feat: create getPurchasedPrice Func & Error Handling
    - 금액을 입력 받는 함수 생성
    - 입력 받은 금액 에러 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.purchasedPrice = 0;
+  }
+
+  play() {
+    // 구입 금액 입력
+    this.getPurchasedPrice();
+    // 구입 금액만큼 로또 번호 출력
+    // 당첨 번호 입력
+    // 보너스 번호 입력
+    // 당첨 결과 출력
+  }
+
+  getPurchasedPrice() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (answers) => {
+      this.checkPrice();
+      this.purchasedPrice = answers;
+    });
+  }
+
+  checkPrice() {
+    if (typeof answers !== "number")
+      throw new TypeError("[ERROR] 숫자를 입력해주세요.");
+    if (answers % 1000)
+      throw new RangeError("[ERROR] 1,000 단위로만 입력가능합니다.");
+  }
 }
 
 module.exports = App;
