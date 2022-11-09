ea96b4088fffb33cc1402659465a4f7d9990525e
chiman <rlaclghks123@naver.com>
2022-11-09 17:59:26 +0900

    feat: 구입 금액을 입력 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.inputMoney();
+  }
+
+  inputMoney() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (money) => {});
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
