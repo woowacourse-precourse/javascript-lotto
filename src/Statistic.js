afacc798e453aa7c963025dc72ab548c1bd51a83
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-09 23:02:11 +0900

    feat(judgeRank): 로또의 일치하는 숫자에 따라 등수를 정한다

--- src/Statistic.js
+++ src/Statistic.js
@@ -17,6 +17,15 @@ class Statistic {
       isBonusNumberSame: false,
     });
   }
+
+  judgeRank({ numberOfSame, isBonusNumberSame }) {
+    if (numberOfSame === 6) return '1ST';
+    if (numberOfSame === 5 && isBonusNumberSame) return '2ND';
+    if (numberOfSame === 5) return '3RD';
+    if (numberOfSame === 4) return '4TH';
+    if (numberOfSame === 3) return '5TH';
+    return 'NOPRIZE';
+  }
 }
 
 module.exports = Statistic;
