b4718ecf8f86869e73f947314978c9c8a876333c
jaewoogwak <jaewoo010207@gmail.com>
2022-11-10 00:22:31 +0900

    feat: 로또 구입 금액 입력 받기 및 유효성 검사 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { MESSAGES, ERROR_MESSAGES } = require('./constant/messages');
+// const Lotto = require('./Lotto');
+
 class App {
-  play() {}
+  play() {
+    this.inputMoney();
+  }
+
+  inputMoney() {
+    Console.readLine(MESSAGES.INPUT, (money) => {
+      this.validateInputMoney(money);
+      console.log(money);
+    });
+  }
+
+  validateInputMoney(money) {
+    const check = /^[0-9]+$/;
+    if (!check.test(money))
+      throw Error(ERROR_MESSAGES.INPUT_TYPE_MUST_BE_NUBMER);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
