3972d9c8cbe80754e865ea8d7f056593ad40fa48
jangsumin <jsm0726@naver.com>
2022-11-10 01:09:24 +0900

    feat: calculate and print profit percentage

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ const PROMPT_MONEY = '구입금액을 입력해 주세요.';
 const PROMPT_LOTTO = '당첨 번호를 입력해 주세요.';
 const PROMPT_BONUS = '보너스 번호를 입력해 주세요.';
 const RESULT_STATISTICS = '\n당첨 통계\n---';
-const THREE_SAME = '3개 일치 (5,000원)';
-const FOUR_SAME = '4개 일치 (50,000원)';
-const FIVE_SAME = '5개 일치 (1,500,000원)';
-const FIVE_BONUS_SAME = '5개 일치, 보너스 볼 일치 (30,000,000원)';
-const SIX_SAME = '6개 일치 (2,000,000,000원)';
+const THREE_SAME_MONEY = '5000';
+const FOUR_SAME_MONEY = '50000';
+const FIVE_SAME_MONEY = '1500000';
+const FIVE_BONUS_SAME_MONEY = '30000000';
+const SIX_SAME_MONEY = '2000000000';
 const LOTTO_PRICE = 1000;
 
 class App {
   constructor() {
+    this.money = 0;
     this.arrayLotto = [];
     this.arrayWinLotto = 0;
     this.numberBonus = 0;
@@ -25,6 +26,7 @@ class App {
 
   moneyInput(prompt) {
     Console.readLine(`${prompt}\n`, (input) => {
+      this.money = input;
       this.countLotto(input);
     });
   }
@@ -75,28 +77,39 @@ class App {
   }
 
   initializeResult() {
-    this.result[THREE_SAME] = 0;
-    this.result[FOUR_SAME] = 0;
-    this.result[FIVE_SAME] = 0;
-    this.result[FIVE_BONUS_SAME] = 0;
-    this.result[SIX_SAME] = 0;
+    this.result[THREE_SAME_MONEY] = 0;
+    this.result[FOUR_SAME_MONEY] = 0;
+    this.result[FIVE_SAME_MONEY] = 0;
+    this.result[FIVE_BONUS_SAME_MONEY] = 0;
+    this.result[SIX_SAME_MONEY] = 0;
   }
 
   separateWin(count, bonus) {
-    if (count === 3) this.result[THREE_SAME] += 1;
-    if (count === 4) this.result[FOUR_SAME] += 1;
+    if (count === 3) this.result[THREE_SAME_MONEY] += 1;
+    if (count === 4) this.result[FOUR_SAME_MONEY] += 1;
     if (count === 5) {
-      if (bonus === false) this.result[FIVE_SAME] += 1;
-      if (bonus === true) this.result[FIVE_BONUS_SAME] += 1;
+      if (bonus === false) this.result[FIVE_SAME_MONEY] += 1;
+      if (bonus === true) this.result[FIVE_BONUS_SAME_MONEY] += 1;
     }
-    if (count === 6) this.result[SIX_SAME] += 1;
+    if (count === 6) this.result[SIX_SAME_MONEY] += 1;
   }
 
   printResult() {
     Console.print(RESULT_STATISTICS);
+    Console.print(`3개 일치 (5,000원) - ${this.result[THREE_SAME_MONEY]}개`);
+    Console.print(`4개 일치 (50,000원) - ${this.result[FOUR_SAME_MONEY]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${this.result[FIVE_SAME_MONEY]}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.result[FIVE_BONUS_SAME_MONEY]}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${this.result[SIX_SAME_MONEY]}개`);
+    this.calculateProfitPercentage();
+  }
+
+  calculateProfitPercentage() {
+    let profit = 0;
     for (let key in this.result) {
-      Console.print(`${key} - ${this.result[key]}개`);
+      profit += parseInt(key) * this.result[key];
     }
+    Console.print(`총 수익률은 ${(profit / this.money * 100).toFixed(1)}%입니다.`);
   }
 }
 
