29a046a42e4980f1d81bbb5d9c066a23898bb370
jongwonp <jw032876@gmail.com>
2022-11-10 00:13:26 +0900

    feat: 구입 금액 입력받는 기능 완료

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ const { Console } = require('@woowacourse/mission-utils');
 
 class App {
   play() {
-    Console.readLine('구입금액을 입력해주세요.', (num) => {
-      if (isDivisibleBy1000(num)) {
-        Console.print('올바른 금액입니다.');
-      } else {
-        Console.print('올바르지 않은 금액입니다.');
-      }
-    });
+    getPurchaseAmount();
   }
 }
+
+const getPurchaseAmount = () => {
+  Console.readLine('구입금액을 입력해주세요.', (num) => {
+    if (isDivisibleBy1000(num)) {
+      Console.print('올바른 금액입니다.');
+    } else {
+      errorMessage();
+    }
+  });
+};
 
 const isDivisibleBy1000 = (num) => {
   if (num % 1000 === 0) {
     return true;
   }
   return false;
+};
+
+const errorMessage = () => {
+  throw new Error('[ERROR] 임시 에러메세지');
 };
 
 module.exports = App;
