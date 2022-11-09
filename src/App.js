fa95c26f69909b21b9b070f40b8091d0f3b724be
eunsung <esk114477@gmail.com>
2022-11-09 17:42:17 +0900

    feat : 랜덤 로또 번호 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const setLottoNumber = () => {
+  const lottoNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  return lottoNumber;
+}
+
+
 class App {
   play() {}
 }
+const app = new App();
+app.play()
 
 module.exports = App;
