e498975d37da4980fc2ebb33cd52a5da1234d2df
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-09 19:03:22 +0900

    Fix: sort+비교함수로 숫자 오름차순 정렬

--- src/model/Random.js
+++ src/model/Random.js
@@ -3,7 +3,7 @@ const {DEFAULTS} = require('../utils/Constants');
 
 function randomNum(){
   const numbers = MissionUtils.Random.pickUniqueNumbersInRange(DEFAULTS.START_RANGE_NUM, DEFAULTS.END_RANGE_NUM, DEFAULTS.CAN_CNT_RANGE);
-  return numbers.sort();
+  return numbers.sort((a,b) => a - b);
 }
 
 module.exports = randomNum;
