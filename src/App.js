9405ed538787f63a3363ff363851d882427b4079
jangsumin <jsm0726@naver.com>
2022-11-09 21:51:43 +0900

    refactor: move functions into the App class

--- src/App.js
+++ src/App.js
@@ -4,24 +4,32 @@ const PROMPT_MONEY = '구입금액을 입력해 주세요.';
 const LOTTO_PRICE = 1000;
 
 class App {
+  constructor() {
+    this.inputMoney = 0;
+    this.amountLotto = 0;
+  }
+
   play() {
-    userInput(PROMPT_MONEY, countLotto);
+    this.userInput(PROMPT_MONEY);
+  }
+
+  userInput(prompt) {
+    MissionUtils.Console.readLine(`${prompt}\n`, (input) => {
+      this.inputMoney = input;
+      this.countLotto(this.inputMoney);
+    });
+  }
+
+  printSentence(sentence) {
+    MissionUtils.Console.print(sentence);
+  }
+
+  countLotto(money) {
+    const amountLotto = money / LOTTO_PRICE;
+    this.amountLotto = amountLotto;
+    this.printSentence(`\n${this.amountLotto}개를 구매했습니다.`);
   }
 }
-
-const userInput = (prompt, callback) => {
-  MissionUtils.Console.readLine(`${prompt}\n`, (input) => {
-    callback(input);
-  });
-};
-
-const printSentence = (sentence) => {
-  MissionUtils.Console.print(sentence);
-};
-
-const countLotto = (money) => {
-  const amountLotto = money / LOTTO_PRICE;
-};
 
 let app = new App();
 app.play();
