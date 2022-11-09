d8bae9e268eeb9a5b544d90e8296cbc854da1b44
shackstack <kms1061@naver.com>
2022-11-09 23:20:21 +0900

    feat: 로또 구매 금액 입력하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.inputPurchaseAmount();
+  }
+  inputPurchaseAmount() {
+    MissionUtils.Console.readLine(
+      "구입금액을 입력해 주세요.\n",
+      (answer) => {}
+    );
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
