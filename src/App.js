ebeeaf21ac41dfa50714253217a1b99a8d8ca15a
AnJaeSeong <dktmzh6@gmail.com>
2022-11-10 01:29:38 +0900

    feat: App interface
    App 메서드 목록 선언

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
 class App {
   play() {}
+
+  inputPurchase() {
+    // 로또 구입 금액 입력
+  }
+
+  inputWinNum() {
+    // 당첨 번호 입력
+  }
+
+  inputBonusNum() {
+    // 보너스 번호 입력
+  }
+
+  checkMatchLottoNum() {
+    // 로또 발행 번호와 당첨 번호 일치 조회 
+  }
+
+  getWinHistory() {
+    // 발행된 모든 로또의 당첨 내역 조회
+  }
+
+  calRate() {
+    // 수익률 계산 
+  }
+
+  printMyLotto() {
+    // 구매한 로또 내역 출력
+  }
+
+  printWinHistory() {
+    // 로또 당첨 내역 출력
+  }
+
+  printRate() {
+    // 수익률 출력
+  }
 }
 
 module.exports = App;
