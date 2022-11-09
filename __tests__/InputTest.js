77b1ba589d8775650d4480844f42c026e2ed2ee5
Adult96 <djxor007@gmail.com>
2022-11-09 22:58:26 +0900

    test(InputTest): Add amount numeric validation test case

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,4 +1,7 @@
-const { checkAmountUnit } = require('../src/validation/inputValidation');
+const {
+  checkAmountUnit,
+  checkAmountStirng,
+} = require('../src/validation/inputValidation');
 
 describe('사용자 입력 테스트', () => {
   test('입력한 금액이 1000원 단위가 아닐경우 예외가 발생한다.', () => {
@@ -6,6 +9,14 @@ describe('사용자 입력 테스트', () => {
 
     expect(() => {
       checkAmountUnit(input);
+    }).toThrow('[ERROR]');
+  });
+
+  test('입력한 금액이 숫자가 아닐경우 예외가 발생한다.', () => {
+    const input = '1000a';
+
+    expect(() => {
+      checkAmountStirng(input);
     }).toThrow('[ERROR]');
   });
 });
