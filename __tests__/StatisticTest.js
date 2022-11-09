389d34a6dcaae4c4ddfbd31613b3e21675de3abd
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-09 22:41:33 +0900

    test(StatisticTest): 로또통계를 만드는 Statistic 클래스 테스트

---
+++ __tests__/StatisticTest.js
@@ -0,0 +1,35 @@
+const Statistic = require('../src/Statistic');
+
+describe('통계 클래스 테스트', () => {
+  const statistic = new Statistic();
+
+  test('로또가 주어지면 일치하는 숫자를 확인한다.', () => {
+    const publishedLotto = [1, 2, 3, 4, 5, 7];
+    const winningLotto = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 7;
+    const judgeResult = statistic.judgeLotto(winningLotto, bonusNumber, publishedLotto);
+
+    expect(judgeResult).toEqual({
+      numberOfSame: 5,
+      isBonusNumberSame: true,
+    });
+  });
+
+  test('일치하는 숫자의 수에 따라 등수를 정한다. ', () => {
+    const judgeResult = {
+      numberOfSame: 5,
+      isBonusNumberSame: true,
+    };
+    const rankResult = statistic.judgeRank(judgeResult);
+
+    expect(rankResult).toEqual('2ND');
+  });
+
+  test('해당 등수의 count를 1 증가시킨다.', () => {
+    const rank = '2ND';
+    statistic.increaseRankCount(rank);
+    const count = statistic.getCount();
+
+    expect(count[rank]).toEqual(1);
+  });
+});
