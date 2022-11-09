f988d3b58406935c5b8293f79b6192c1445f851d
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-09 19:15:34 +0900

    Feat : add getLottoNumber & lottoNumberInputCb

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const { Console } = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+
+const LOTTO_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+
 class App {
-  play() {}
+  $Lotto;
+  $LottoBonus;
+
+  constructor() {}
+
+  lottoNumberInputCb = (input) => {
+    const numbers = input.split(",");
+    this.$Lotto = new Lotto(numbers);
+    Console.Print(this.$Lotto.#numbers.join(""));
+    Console.close();
+  };
+
+  getLottoNumber = () => {
+    Console.readLine(LOTTO_NUMBER_INPUT_MESSAGE, this.lottoNumberInputCb);
+  };
+  play() {
+    this.getLottoNumber();
+  }
 }
 
 module.exports = App;
