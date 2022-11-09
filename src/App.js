baa55c30242b253f1d55720200a98628271fa294
Suyeon <msjgr2801@gmail.com>
2022-11-09 20:13:43 +0900

    Feat: [App.js, Lotto.js] 당첨 번호 입력 받는 기능 구현
    [App.js]
    1. 당첨 번호 입력 받기
    2. 보넛 번호 입력 받기
    3. 로또 객체 생성해서 유효한 값이 아니면 throw 발생
    
    [Lotto.js]
    1. isWinning: 당첨 번호와 일반 로또 발생 번호를 구분하는 플래그

--- src/App.js
+++ src/App.js
@@ -5,19 +5,18 @@ const Lotto = require("./Lotto");
 class App {
   play() {
     // 클래스를 분리 해서 시작
-    const countOfLotto = this.calCountOfLotto();
-    const myLotto = this.makeLotto(countOfLotto);
+    const countOfLotto = this.calCountOfLotto(); // 수정
+    const myLotto = this.generateLottoNumber(countOfLotto); // generateLotto
+    const winningLotto = this.getWinningLotto();
   }
 
-  calCountOfLotto() { 
+  calCountOfLotto() {
     let money = 0;
 
-    MissionUtils.Console.readLine(
-      "구매금액을 입력해주세요. \n >",
-      (answer) => {
-        money = Number(answer);
-      }
-    )
+    MissionUtils.Console.readLine("구매금액을 입력해주세요. \n >", (answer) => {
+      money = Number(answer);
+    });
+    MissionUtils.Console.close();
     this.isValidMoney(money);
 
     return parseInt(money / 1000);
@@ -25,24 +24,28 @@ class App {
 
   isValidMoney(money) {
     if (Number.isNaN(number)) {
-      throw new Error(`[Error] 입력 금액이 숫자형태가 아닙니다.`);
+      throw new Error(`[ERROR] 입력 금액이 숫자형태가 아닙니다.`);
     }
 
     if (money < 1000) {
-      throw new Error(`[Error] 로또 한장의 가격은 1000원입니다. 입력한 금액: ${money}`);
+      throw new Error(
+        `[ERROR] 로또 한장의 가격은 1000원입니다. 입력한 금액: ${money}`
+      );
     }
 
-    if (money > 100000) {
-      throw new Error(`[Error] 한 번에 최대로 구입할 수 있는 금액은 10만원 입니다. 입력한 금액: ${money}`);
+    if (money > 1000000) {
+      throw new Error(
+        `[ERROR] 한 번에 최대로 구입할 수 있는 금액은 100만원 입니다. 입력한 금액: ${money}`
+      );
     }
   }
 
-  makeLotto(count) {
+  generateLottoNumber(count) {
     const myLotto = [];
 
     while (myLotto.length !== count) {
       const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-      const lotto = new Lotto(numbers);
+      const lotto = new Lotto(numbers, false);
       myLotto.push(lotto);
     }
     this.printMyLotto(count, myLotto);
@@ -54,7 +57,36 @@ class App {
     MissionUtils.Console.print(`${count}개를 구매했습니다.`);
     myLotto.forEach((lotto) => {
       lotto.printLottoNumbers();
-    })
+    });
+  }
+
+  getWinningLotto() {
+    const winningLotto = [];
+
+    MissionUtils.Console.readLine(
+      "당첨 번호를 입력해주세요. \n >",
+      (answer) => {
+        answer.split("").forEach((element) => {
+          winningLotto.push(Number(element));
+        });
+      }
+    );
+    MissionUtils.Console.close();
+
+    MissionUtils.Console.reaLine(
+      "보너스 번호를 입력해주세요. \n >",
+      (answer) => {
+        winningLotto.push(Number(answer));
+      }
+    );
+    MissionUtils.Console.close();
+    new Lotto(winningLotto, true);
+
+    return winningLotto;
+  }
+
+  isValidLotto(lotto, bonus) {
+    
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -4,21 +4,37 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.validate(numbers, isWinning);
     this.#numbers = numbers;
   }
 
   // 유효한 값 검사
-  validate(numbers) {
+  validate(numbers, isWinning) {
     const numSet = new Set(numbers);
 
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    if (isWinning) {
+      if (numbers.length !== 7) {
+        throw new Error("[ERROR] 당첨 번호는 총 7개여야 합니다.")
+      }
 
-    if (numSet.size !== 6) {
-      throw new Error("[Error] 로또 번호가 중복됩니다.")
+      if (numSet.size !== 7) {
+        throw new Error("[ERROR] 로또 번호가 중복됩니다.");
+      }
+    } else {
+      if (numbers.length !== 6) {
+        throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      }
+
+      if (numSet.size !== 6) {
+        throw new Error(`[ERROR] 로또 번호가 중복됩니다.`)
+      }
     }
+    
+    numbers.forEach(num => {
+      if (Number.isNaN(num)) {
+        throw new Error(`[ERROR] 숫자가 아닌 값이 있습니다.`)
+      }
+    })
   }
 
   printLottoNumbers() {
