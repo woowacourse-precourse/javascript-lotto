4670856b72b3e0ac3d7faa7393e50b80236a70fc
Rki0 <pky00823@gmail.com>
2022-11-09 21:21:08 +0900

    feat(getPurchaseAmount): add function to get user's purchase amount
    Add a function to get the amount for the user to decide how many lottos to buy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const Console = require("@woowacourse/mission-utils").Console;
+
 class App {
-  play() {}
+  play() {
+    this.getPurchaseAmount();
+  }
+
+  getPurchaseAmount() {
+    Console.readLine("구입금액을 입력해 주세요.\n", (amount) => {
+      if (amount % 1000 !== 0) {
+        throw new Error("[ERROR] 1,000원 단위로만 구매 가능합니다.");
+      }
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
