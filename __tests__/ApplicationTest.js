1c91b4068924f0bdf77d1b991e52ebea1b5b93c4
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-09 20:20:57 +0900

    Test : add test for validateIsNotNumber

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -23,8 +23,8 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-describe.skip("로또 테스트", () => {
-  test("기능 테스트", () => {
+describe("로또 테스트", () => {
+  test.skip("기능 테스트", () => {
     mockRandoms([
       [8, 21, 23, 41, 42, 43],
       [3, 5, 11, 16, 32, 38],
@@ -63,6 +63,14 @@ describe.skip("로또 테스트", () => {
 
   test("예외 테스트", () => {
     mockQuestions(["1000j"]);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR]");
+  });
+
+  test("숫자 여러개 예외 테스트", () => {
+    mockQuestions(["1,2,3,4,5,i"]);
     expect(() => {
       const app = new App();
       app.play();
