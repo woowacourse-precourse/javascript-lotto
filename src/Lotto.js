e80c743e3c4e8a4d0e73c452a7603b8adec4b2ec
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-09 18:47:06 +0900

    Feat: validate 함수 errorCheck와 연결

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,19 @@
+const ExceptionCheck = require('./utils/ExceptionCheck');
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
+    this.errorCheck = new ExceptionCheck();
     this.validate(numbers);
     this.#numbers = numbers;
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    this.errorCheck.answerNumCheck(numbers);
   }
 
-  // TODO: 추가 기능 구현
+  
 }
 
 module.exports = Lotto;
