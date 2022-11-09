2506671385970d32e926936d45a3f9ccde74852d
bassyu <yrkdgus@gmail.com>
2022-11-09 20:33:15 +0900

    feat(Lotto): 로또의 순위를 계산하는 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,21 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  rank(winNumbers, bonusNumber) {
+    let count = 0;
+    winNumbers.forEach((winNumber) => {
+      count += this.#numbers.includes(winNumber);
+    });
+
+    if (count === 6) return 1;
+    if (count === 5 && this.#numbers.includes(bonusNumber)) return 2;
+    if (count >= 3) return 8 - count;
+    return 0;
+  }
 }
 
 module.exports = Lotto;
