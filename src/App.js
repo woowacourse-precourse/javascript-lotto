d75c04b063aaa646016e6cae10e37759bf1e7a7c
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-10 00:39:14 +0900

    feat($app): implement publishing lotto function

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const { printError, isPositiveInteger } = require('./Utils');
 
 const LOTTO_PRICE = 1000;
+const LOTTO_START = 1;
+const LOTTO_END = 45;
 class App {
   #lottoCount;
+  #lottos;
 
   constructor() {
     this.#lottoCount = 0;
+    this.#lottos = [];
   }
 
   #validate(money) {
@@ -20,10 +24,25 @@ class App {
     return true;
   }
 
+  #publishLotto() {
+    for (let count = 0; count < this.#lottoCount; count += 1) {
+      let lotto = MissionUtils.Random.pickUniqueNumbersInRange(
+        LOTTO_START,
+        LOTTO_END,
+        6,
+      );
+      lotto = lotto.sort(function (a, b) {
+        return a - b;
+      });
+      this.#lottos.push(lotto);
+    }
+  }
+
   #getUserMoney() {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
       if (this.#validate(money)) {
         this.#lottoCount = parseInt(money / LOTTO_PRICE, 10);
+        this.#publishLotto();
       }
     });
   }
@@ -32,5 +51,8 @@ class App {
     this.#getUserMoney();
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
