8f9835f889fb7f952b23bbb1dc34314c153ba073
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-09 19:07:37 +0900

    Feat: 로또 구입 갯수 출력및 자동 로또 생성 배열들 출력

--- src/Controller.js
+++ src/Controller.js
@@ -3,11 +3,13 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const {DEFAULTS, MONEY, CONSOLELINE, RESULTLINE} = require('./utils/Constants');
 const ExceptionCheck = require('./utils/ExceptionCheck');
 const Lotto = require("./Lotto");
+const randomNum = require('./model/Random');
 
 class Controller{
   constructor(){
     this.errorCheck = new ExceptionCheck();
-    this.lotto;
+    this.lottos = [];
+    this.lottoGame;
   }
 
   startGame(){
@@ -17,12 +19,22 @@ class Controller{
   inputForMoney(){
     MissionUtils.Console.readLine(CONSOLELINE.PURCHASE_MONEY_INPUT+'\n', (input) => {
       this.errorCheck.purchaseMoneyErrorCheck(input);
-      this.printPurchaseNums(input);
+      this.printPurchaseNums(input/1000);
     });
   }
 
-  printPurchaseNums(input){
-    MissionUtils.Console.close();
+  printPurchaseNums(lotto_cnt){
+    MissionUtils.Console.print(RESULTLINE.PURCHASE_CHECK(lotto_cnt));
+    this.printLottos(lotto_cnt);
+  }
+
+  printLottos(lotto_cnt){
+    let i = 0;
+    while(i < lotto_cnt){
+      this.lottos.push(randomNum());
+      MissionUtils.Console.print(randomNum());
+      i++;
+    }
   }
 
   inputForAnswerNum(){
@@ -35,7 +47,7 @@ class Controller{
   inputForAnswerNumCheck(answerStr){
     const answerSplit = answerStr.split(',');
     this.errorCheck.noSeperatorErrorCheck(answerSplit);
-    this.lotto = new Lotto(answerSplit);
+    this.lottoGame = new Lotto(answerSplit);
   }
 
 
@@ -44,4 +56,4 @@ class Controller{
 module.exports = Controller;
 
 const c = new Controller();
-c.inputForAnswerNum();
+c.startGame();
