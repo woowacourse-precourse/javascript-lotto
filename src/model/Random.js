ce1e86ef6de6e3c555f391d5865681e02803f737
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-09 18:51:45 +0900

    Feat: Random함수 구현

---
+++ src/model/Random.js
@@ -0,0 +1,9 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const {DEFAULTS} = require('../utils/Constants');
+
+function randomNum(){
+  const numbers = MissionUtils.Random.pickUniqueNumbersInRange(DEFAULTS.START_RANGE_NUM, DEFAULTS.END_RANGE_NUM, DEFAULTS.CAN_CNT_RANGE);
+  return numbers.sort();
+}
+
+module.exports = randomNum;
