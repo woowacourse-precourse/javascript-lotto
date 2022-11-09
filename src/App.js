e49348a81d03eb31b7e37847b07b5ce2231cd34c
Rki0 <pky00823@gmail.com>
2022-11-09 21:23:59 +0900

    refactor(getPurchaseAmount): modularize the constraint validation code
    Modularize constraint validation code for clean code.
    Move function to 'checkPurchaseAmount()'.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,16 @@ class App {
 
   getPurchaseAmount() {
     Console.readLine("구입금액을 입력해 주세요.\n", (amount) => {
-      if (amount % 1000 !== 0) {
-        throw new Error("[ERROR] 1,000원 단위로만 구매 가능합니다.");
-      }
+      this.checkPurchaseAmount(amount);
     });
+  }
+
+  checkPurchaseAmount(amount) {
+    if (amount % 1000 !== 0) {
+      throw new Error("[ERROR] 1,000원 단위로만 구매 가능합니다.");
+    }
+
+    return true;
   }
 }
 
