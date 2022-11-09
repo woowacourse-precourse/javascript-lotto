c599965d900c567d1bf77af2578b0c35edfd578a
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-09 18:49:50 +0900

    Feat : add validateIsDuplicated & validateSizeIsSix fnc

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
 
@@ -7,12 +9,24 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    this.validateSizeIsSix(numbers);
+    this.validateIsDuplicated(numbers);
+  }
+  // TODO: 추가 기능 구현
+  validateIsDuplicated(numbers) {
+    const numbersSet = new Set(numbers);
+    if (numbersSet.size !== numbers.length) {
+      Console.close();
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 포함되어 있습니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  validateSizeIsSix(numbers) {
+    if (numbers.length !== 6) {
+      Console.close();
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  }
 }
 
 module.exports = Lotto;
