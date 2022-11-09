10fec81cefcf31345e4fed8992c0fba490cb7738
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-09 19:31:30 +0900

    feat: 입력값 타당성 검사 (로또를 구매할 돈)

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -2,7 +2,19 @@ const { Console, Random } = require("@woowacourse/mission-utils");
 
 class LottoGame {
   getUserMoney() {
-    Console.readLine("구매금액을 입력해 주세요", (money) => {});
+    Console.readLine("구매금액을 입력해 주세요", (money) => {
+      this.userMoneyCheck(money);
+    });
+  }
+
+  userMoneyCheck(money) {
+    if (money % 1000 !== 0) {
+      throw new Error(`[ERROR] 돈은 1000원 단위로 입력이 가능합니다.`);
+    }
+    if (money <= 0) {
+      throw new Error(`[ERROR] 돈은 1000원부터 입력이 가능합니다.`);
+    }
   }
 }
+
 module.exports = LottoGame;
