1f77c52c03f3bda0ff839275a6cca10726dd579d
Adult96 <djxor007@gmail.com>
2022-11-09 21:24:23 +0900

    feat(App): Add readLine user input method
    readLine 사용자 입력 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
+  constructor() {}
+
   play() {}
+
+  readLine(message, callback) {
+    return MissionUtils.Console.readLine(message, callback);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
