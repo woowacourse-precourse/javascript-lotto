23e6bc8e9eebbd87e4764e5d3943a4cfc47a4456
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-09 20:09:14 +0900

    feat: 입력값에 따라 구매 개수 계산 및 출력

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,19 +1,35 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
 
 class LottoGame {
+  lineBreak() {
+    Console.print(``);
+  }
+
   getUserMoney() {
     Console.readLine("구매금액을 입력해 주세요", (money) => {
-      this.userMoneyCheck(money);
+      this.checkUserMoney(money);
+
+      this.showlottoNumOfBuying(money);
     });
   }
 
-  userMoneyCheck(money) {
+  checkUserMoney(money) {
     if (money % 1000 !== 0) {
       throw new Error(`[ERROR] 돈은 1000원 단위로 입력이 가능합니다.`);
     }
     if (money <= 0) {
       throw new Error(`[ERROR] 돈은 1000원부터 입력이 가능합니다.`);
     }
+  }
+
+  lottoNumOfBuying(money) {
+    return money % 1000;
+  }
+
+  showlottoNumOfBuying(money) {
+    Console.print(`${this.lottoNumOfBuying(money)}개를 구매했습니다.`);
+
+    this.lineBreak();
   }
 }
 
