8f1d3d868eda6aff189ea4f5d4a641f21523757e
Suyeon <msjgr2801@gmail.com>
2022-11-09 17:23:46 +0900

    Feat: [Lotto.js] 로또 번호 입력 시 예외 처리 기능 구현
    1. 개수가 6개인지 검사 & 에러 구문 수정
    2. 로또 번호가 중복되는지 검사

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,12 +10,16 @@ class Lotto {
 
   // 유효한 값 검사
   validate(numbers) {
+    const numSet = new Set(numbers);
+
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-  }
 
-  
+    if (numSet.size !== 6) {
+      throw new Error("[Error] 로또 번호가 중복됩니다.")
+    }
+  }
 }
 
 module.exports = Lotto;
