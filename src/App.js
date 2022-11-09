a1554a673fc84d8051c2d0e35d3767ab48c9f348
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-11-10 03:43:00 +0900

    feat: 구입한 수량만큼 중복하지 않는, 랜덤 한 6자리 숫자를 뽑아주는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
-  play() {}
+  play() {
+    this.calculateMoney();
+  }
 
   calculateMoney() {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (userInput) => {
       const LOTTO_PAPER = userInput / 1000;
 
       if (userInput % 1000 !== 0) {
-        throw new Error('[ERROR] 1000원 단위로 구매해 주세요.');
+        throw new Error('[ERROR] 1000원 단위로 구입해 주세요.');
       }
 
-      return LOTTO_PAPER;
+      return this.UserLottoNumber(LOTTO_PAPER);
     });
   }
+
+  UserLottoNumber(amount) {
+    let LOTTO_PAPER_NUMBER = amount;
+    let number = 0;
+
+    MissionUtils.Console.print(`\n${LOTTO_PAPER_NUMBER}개를 구매했습니다.`);
+
+    while (number < LOTTO_PAPER_NUMBER) {
+      const NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      MissionUtils.Console.print(NUMBERS.sort((a, b) => a - b));
+      number++;
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
