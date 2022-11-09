d68f5cc84156032d577d9a207a72d4e3236bafc8
eunsung <esk114477@gmail.com>
2022-11-09 18:29:24 +0900

    feat : 로또 구입 금액 기능 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const { Random, Console } = require("@woowacourse/mission-utils");
+
+const buyLotto = () => {
+  let countLotto;
+  let throwInput;
+  let input;
+  Console.readLine('구입금액을 입력해 주세요.\n', (num) => {
+    countLotto = Number(num) / 1000;
+    throwInput = Number(num) % 1000;
+    input = num.toString().split('').map((str) => Number(str));
+    });
+}
 
 const setLottoNumber = () => {
   const lottoNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
