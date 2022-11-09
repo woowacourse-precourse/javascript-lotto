aae8e55b86e23afdb58e34cdaeeea36533536871
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-11-09 21:26:48 +0900

    App.js/feat : 구입금액 입력 바라는 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print("구입금액을 입력해 주세요.");
+  }
 }
 
 module.exports = App;
