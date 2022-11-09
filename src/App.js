b339f427881bd411b743fea47950416271d4092c
Suyoung <jsy8492@gmail.com>
2022-11-09 15:49:53 +0900

    feat(app): 로또 구입 금액을 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    Console.readLine('구입금액을 입력해주세요.\n', purchaseAmount => {
+      try {
+        this.validate(purchaseAmount);
+      } catch (err) {
+        throw err;
+      }
+    });
+  }
 }
 
 module.exports = App;
