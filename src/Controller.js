4d35900ff8c85a0e11e3ece5770be601774fe7c7
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-09 18:48:03 +0900

    Feat: 당첨번호 입력 기능 구현, 예외throw 연결

--- src/Controller.js
+++ src/Controller.js
@@ -1,10 +1,13 @@
 
 const MissionUtils = require('@woowacourse/mission-utils');
 const {DEFAULTS, MONEY, CONSOLELINE, RESULTLINE} = require('./utils/Constants');
+const ExceptionCheck = require('./utils/ExceptionCheck');
+const Lotto = require("./Lotto");
 
 class Controller{
   constructor(){
-
+    this.errorCheck = new ExceptionCheck();
+    this.lotto;
   }
 
   startGame(){
@@ -13,8 +16,8 @@ class Controller{
 
   inputForMoney(){
     MissionUtils.Console.readLine(CONSOLELINE.PURCHASE_MONEY_INPUT+'\n', (input) => {
+      this.errorCheck.purchaseMoneyErrorCheck(input);
       this.printPurchaseNums(input);
-      
     });
   }
 
@@ -22,6 +25,23 @@ class Controller{
     MissionUtils.Console.close();
   }
 
+  inputForAnswerNum(){
+    MissionUtils.Console.readLine(CONSOLELINE.ANSWERNUM_INPUT+'\n', (answerStr) => {
+      this.inputForAnswerNumCheck(answerStr);
+      this.inputForBonusNum();
+    });
+  }
+
+  inputForAnswerNumCheck(answerStr){
+    const answerSplit = answerStr.split(',');
+    this.errorCheck.noSeperatorErrorCheck(answerSplit);
+    this.lotto = new Lotto(answerSplit);
+  }
+
+
 }
 
 module.exports = Controller;
+
+const c = new Controller();
+c.inputForAnswerNum();
