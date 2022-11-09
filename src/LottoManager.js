6e31d4b95295fb5689cb6b55b2022e51a1adff69
Suyoung <jsy8492@gmail.com>
2022-11-09 16:37:25 +0900

    feat(lottoManager): LottoManager 클래스에 getter 추가
    - private 필드의 값을 받아올 수 있도록 getter 추가

--- src/LottoManager.js
+++ src/LottoManager.js
@@ -2,6 +2,18 @@ class LottoManager {
   #lottos;
   #winningNumbers;
   #earningsRate;
+
+  get lottos() {
+    return this.#lottos;
+  }
+
+  get winningNumbers() {
+    return this.#winningNumbers;
+  }
+
+  get earningsRate() {
+    return this.#earningsRate;
+  }
 }
 
 module.exports = LottoManager;
