8c9ccc8174056ed33b0a90ce6f180798d186442a
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-09 16:55:54 +0900

    Feat: 구입금액 입력 처리

---
+++ src/Controller.js
@@ -0,0 +1,27 @@
+
+const MissionUtils = require('@woowacourse/mission-utils');
+const {DEFAULTS, MONEY, CONSOLELINE, RESULTLINE} = require('./Constants');
+
+class Controller{
+  constructor(){
+
+  }
+
+  startGame(){
+    this.inputForMoney();
+  }
+
+  inputForMoney(){
+    MissionUtils.Console.readLine(CONSOLELINE.PURCHASE_MONEY_INPUT+'\n', (input) => {
+      this.printPurchaseNums(input);
+      
+    });
+  }
+
+  printPurchaseNums(input){
+    MissionUtils.Console.close();
+  }
+
+}
+
+module.exports = Controller;
