55d345ce1a92a855cd58cf70f6439625e3efbc6d
ksone02 <eksovus@gmail.com>
2022-11-09 20:14:24 +0900

    test(Winning): 당첨번호 입력 테스트

---
+++ __tests__/WinningTest.js
@@ -0,0 +1,21 @@
+const Winning = require('../src/components/Winning');
+const { ERROR } = require('../src/data/constants');
+
+describe('로또 당첨번호 입력 테스트', () => {
+  test('잘못된 입력 형식 테스트', () => {
+    expect(() => {
+      const win = new Winning("1, 2. 3' 4. 5[ 6.");
+      console.log(win.winningNumber);
+    }).toThrow(ERROR.NUMBER_WAY);
+  });
+  test('숫자 중복 테스트', () => {
+    expect(() => {
+      new Winning('1,2,3,4,4,5');
+    }).toThrow(ERROR.NOT_UNIQUE);
+  });
+  test('숫자 범위 테스트', () => {
+    expect(() => {
+      new Winning('1,2,3,4,0,25');
+    }).toThrow(ERROR.RANGE);
+  });
+});
