2a16a08d4aa42e80c7f908f36e3525b9682f164b
“somm12” <sominan8744@gmail.com>
2022-11-10 07:32:02 +0900

    feat: 서로 다른 랜덤 숫자 6개를 가진 배열 만들기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 const Lotto = require("./Lotto");
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
+  boughtLottos = [];
+
   play() {
     this.inputAmountOfMoneyToBuy();
   }
 
   inputAmountOfMoneyToBuy() {
     const WORD_TO_PRINT = "구입금액을 입력해 주세요.\n";
-
+    let numberOfLotto;
     MissionUtils.Console.readLine(WORD_TO_PRINT, (money) => {
       money = Number(money);
+      numberOfLotto = money / 1000;
       this.checkAmountOfMoneyToBuy(money);
-      this.makeRandomLottoNumber(money);
+      this.printBoughtNumberOfLotto(numberOfLotto);
+      this.makeRandomLottoNumber(numberOfLotto);
     });
   }
 
@@ -28,6 +32,19 @@ class App {
         const WRONG_INPUT = "[ERROR] 1000원 단위 금액이 아닙니다.";
         throw new Error(WRONG_INPUT);
     }
+  }
+
+  printBoughtNumberOfLotto(NUMBER_OF_LOTTO) {
+    const WORD_TO_PRINT = `${NUMBER_OF_LOTTO}개를 구매했습니다.\n`;
+    MissionUtils.Console.print(WORD_TO_PRINT);
+  }
+
+  makeRandomLottoNumber(NUMBER_OF_LOTTO) {
+    for (let i = 0; i < NUMBER_OF_LOTTO; i++) {
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      this.boughtLottos.push(numbers);
+    }
+    MissionUtils.Console.print(this.boughtLottos);
   }
 }
 
