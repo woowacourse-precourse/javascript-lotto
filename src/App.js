efeb1659a21b49cf45ef9058698531379f21aa05
Suyoung <jsy8492@gmail.com>
2022-11-09 15:51:55 +0900

    feat(app): 사용자가 잘못된 구입 금액을 입력할 경우 예외 처리 기능 구현
    - 1,000으로 나누어 떨어지는 숫자가 아닌 경우 예외 처리하도록 구현

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ class App {
       }
     });
   }
+
+  validate(purchaseAmount) {
+    if (this.isInvalidPurchaseAmount(purchaseAmount)) {
+      throw new Error(
+        '[ERROR] 구입 금액은 1,000으로 나누어 떨어지는 숫자여야 합니다.',
+      );
+    }
+  }
+
+  isInvalidPurchaseAmount(purchaseAmount) {
+    return (
+      !/^\d+$/g.test(purchaseAmount) ||
+      parseInt(purchaseAmount, 10) % 1000 !== 0
+    );
+  }
 }
 
 module.exports = App;
