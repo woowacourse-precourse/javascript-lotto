9b04979d7343a86d2b8115fe48e8c6b85bfac183
Dahye <06robin11@gmail.com>
2022-11-09 21:13:03 +0900

    feat: money validate method, test  implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+const { INPUT_MESSAGE, ERROR_MESSAGE } = require("./constant/constant");
+
 class App {
   play() {}
+
+  getMoney() {
+    Console.readLine(INPUT_MESSAGE.MONEY, (money) => {
+      this.validateMoney(money);
+    });
+  }
+
+  validateMoney(money) {
+    if (+money % 1000 !== 0) {
+      throw new Error(ERROR_MESSAGE.WRONG_MONEY);
+    }
+  }
 }
 
 module.exports = App;
--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,19 +1,22 @@
 const ERROR = "[ERROR]";
 
-export const ERROR_MESSAGE = {
+const ERROR_MESSAGE = {
   HAS_REPEAT: `${ERROR} 로또 번호는 중복되는 수가 들어갈 수 없습니다.`,
   WRONG_QUANTITY: `${ERROR} 로또 번호는 6개여야 합니다.`,
   NOT_IN_RANGE: `${ERROR} 로또 번호는 1부터 45사이의 수여야 합니다.`,
+  WRONG_MONEY: `${ERROR}1000원 단위로 입력해야 합니다.`,
 };
 
-export const INPUT_MESSAGE = {
+const INPUT_MESSAGE = {
   MONEY: "구입금액을 입력해 주세요.",
   WINNING_NUMBER: "당첨 번호를 입력해 주세요.",
   BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
 };
 
-export const RESULT_MEESAGE = {
+const RESULT_MEESAGE = {
   PURCHASE: "N개를 구매했습니다.",
   LOTTERY_RESULT: "당첨 통계",
   PROFIT: "총 수익률은 N%입니다.",
 };
+
+module.exports = { ERROR_MESSAGE, INPUT_MESSAGE, RESULT_MEESAGE };
