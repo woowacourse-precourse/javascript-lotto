4e8aeac7189042e0a5078352a00a40893c29eb96
moonelysian <dandyblack@naver.com>
2022-11-09 17:32:29 +0900

    feat: out of lotto length 에러 추가
    - 에러 메세지 상수화
    - 에러 메세지에 맞춰 LottoTest.js에서 해당하는 테스트 변경
    - Lotto class validation에서 상수화할 수 있는 부분 상수화

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,10 +1,11 @@
-const Lotto = require("../src/Lotto");
+const Lotto = require("../src/Lotto.js");
+const { ERROR } = require("../src/constructor.js");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR.OUT_OF_LOTTO_LENGTH);
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
--- src/constructor.js
+++ src/constructor.js
@@ -5,6 +5,11 @@ const LOTTO = {
   BONUS_COUNT: 1,
 }
 
+const ERROR = {
+  OUT_OF_LOTTO_LENGTH: "[ERROR] 로또 번호는 6개여야 합니다.",
+}
+
 module.exports = {
   LOTTO,
+  ERROR,
 }
