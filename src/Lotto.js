78f52854f131ebcbc2d18ef010de4b98dec9cb1d
JH <vvaa277@gmail.com>
2022-11-10 00:09:03 +0900

    Feat : 구매 수량 출력 기능
    구입 금액을 1000으로 나눈 몫을 수량으로 출력한다

--- src/Lotto.js
+++ src/Lotto.js
@@ -6,31 +6,34 @@ class Lotto {
   constructor(numbers) {
     // this.validate(numbers);
     this.#numbers = numbers;
+    this.START_MESSAGE = "구입금액을 입력해 주세요.";
   }
 
-  buyLotto(){
-    MissionUtils.Console.print('구입금액을 입력해 주세요.');
+  buyLotto() {
+    MissionUtils.Console.print(this.START_MESSAGE);
+    this.pay();
   }
-  pay(){
-    MissionUtils.Console.readLine('', (pay) => {
-      MissionUtils.Console.print(pay);
-      return pay
+  pay() {
+    MissionUtils.Console.readLine("", (payNumber) => {
+      MissionUtils.Console.print(""); // 공백
+      this.countMyLottoSheets(payNumber);
     });
   }
-  
-
+  countMyLottoSheets(payNumber) {
+    const countedSheets = payNumber / 1000;
+    MissionUtils.Console.print(`${countedSheets}개를 구매했습니다.`);
+    // return this.
+  }
 
   // validate(numbers) {
-    
+
   //   if (numbers.length !== 6) {
   //     throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
   //   }
   // }
-
-  
 }
 
-// const lotto = new Lotto();
-// lotto.play();
+const lotto = new Lotto();
+lotto.buyLotto();
 
 module.exports = Lotto;
