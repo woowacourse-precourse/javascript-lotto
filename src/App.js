6e584ad3c6e852f09f1483c72b764346a7f125b6
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-11-10 04:27:51 +0900

    App.js/feat : 금액 입력 받기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     MissionUtils.Console.print("구입금액을 입력해 주세요.");
+    this.inputMoney();
+  }
+
+  inputMoney() {
+    MissionUtils.Console.readLine("", (answer) => {});
   }
 }
 
