677083c223fa72f3cc7da4a7445d8397e0001a3c
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-09 17:11:42 +0900

    Feat: 예외 throw용 문구 추가

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -22,12 +22,20 @@ const CONSOLELINE = Object.freeze({
 
 const RESULTLINE = {
   PURCHASE_CHECK : num => `${String(num)}개를 구매했습니다.`,
-  THREE : num => `3개 일치 (5,000원) - ${String(num)}개`,
-  FOUR : num => `4개 일치 (50,000원) - ${String(num)}개`,
-  FIVE : num => `5개 일치 (1,500,000원) - ${String(num)}개`,
-  FIVE_BONUS : num => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${String(num)}개`,
-  SIX : num => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${String(num)}개`,
+  FIFTH : num => `3개 일치 (5,000원) - ${String(num)}개`,
+  FOURTH : num => `4개 일치 (50,000원) - ${String(num)}개`,
+  THIRD : num => `5개 일치 (1,500,000원) - ${String(num)}개`,
+  SECOND : num => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${String(num)}개`,
+  FIRST : num => `6개 일치 (2,000,000,000원) - ${String(num)}개`,
   TOTAL_PER : percent => `총 수익률은 ${String(percent)}%입니다.`
 };
 
-module.exports = {DEFAULTS, MONEY, CONSOLELINE, RESULTLINE};
+const ERRORLINE = Object.freeze({
+  INPUT_UNIT_CHECK : '[ERROR] 구매 금액은 1,000원 단위로 입력해주세요.',
+  SEPARATOR_CHECK : '[ERROR] 당첨번호는 쉼표를 사용하여 구분해주세요.',
+  NUMRANGE_CHECK : '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+  INPUT_CNT_CHECK : '[ERROR] 숫자는 6개만 입력해주세요.',
+  BONUS_CHECK : '[ERROR] 1개의 숫자만 입력해주세요.',
+})
+
+module.exports = {DEFAULTS, MONEY, CONSOLELINE, RESULTLINE, ERRORLINE};
