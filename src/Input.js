064065c688164d86779376ec35e9051fbf58984b
hyunjin <leehj0110@kakao.com>
2022-11-10 04:02:48 +0900

    feat: getPurchaseAmount

---
+++ src/Input.js
@@ -0,0 +1,13 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+class Input {
+  static PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.\n";
+
+  static getPurchaseAmount(callback) {
+    Console.readLine(this.PURCHASE_AMOUNT_MESSAGE, callback);
+  }
+
+  // static checkPurchaseAmount(purchaseAmount) {}
+}
+
+module.exports = Input;
