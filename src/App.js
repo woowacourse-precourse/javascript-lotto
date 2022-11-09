dbf943c0a4a6177f6297780125bf4aa0c70945af
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-09 20:08:56 +0900

    Feat : make validateIsNotNumber at App

--- src/App.js
+++ src/App.js
@@ -12,9 +12,17 @@ class App {
 
   lottoNumberInputCb = (input) => {
     const numbers = input.split(",");
+    this.validateIsNotNumber(numbers);
     this.$Lotto = new Lotto(numbers);
     Console.close();
   };
+
+  validateIsNotNumber(...numbers) {
+    if (numbers.some((number) => !/[0-9]/.test(number))) {
+      Console.close();
+      throw new Error("[ERROR] 숫자만 입력해야 합니다.");
+    }
+  }
 
   getLottoNumber = () => {
     Console.readLine(LOTTO_NUMBER_INPUT_MESSAGE, this.lottoNumberInputCb);
