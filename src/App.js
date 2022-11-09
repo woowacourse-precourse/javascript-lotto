32c574c7ade13150562bad07f2c306fa9d21d3bf
jongwonp <jw032876@gmail.com>
2022-11-09 23:54:20 +0900

    test: 구입금액 입력받기 테스트 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    Console.readLine('구입금액을 입력해주세요.', (num) => {
+      if (isDivisibleBy1000(num)) {
+        Console.print('올바른 금액입니다.');
+      } else {
+        Console.print('올바르지 않은 금액입니다.');
+      }
+    });
+  }
 }
+
+const isDivisibleBy1000 = (num) => {
+  if (num % 1000 === 0) {
+    return true;
+  }
+  return false;
+};
 
 module.exports = App;
