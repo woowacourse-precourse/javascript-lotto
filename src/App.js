f7b435a5ad78420568db3fa5154905a6259cd668
wang-yurin <king_1994@naver.com>
2022-11-10 00:22:10 +0900

    feat: 로또 번호 입력 받는 함수 구현 중

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+    inputMoney();
+  }
 }
+
+function inputMoney() {
+  MissionUtils.Console.readLine(`구입금액을 입력해 주세요.\n`, (answer) => {
+    MissionUtils.Console.print(answer);
+  });
+}
+
+const app = new App();
+app.play();
 
 module.exports = App;
