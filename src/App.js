eba85467df12687d3d3ddb9cfd8907c4be5ced51
jangsumin <jsm0726@naver.com>
2022-11-09 22:46:25 +0900

    feat: input lotto win numbers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
 
 const PROMPT_MONEY = '구입금액을 입력해 주세요.';
+const PROMPT_LOTTO = '당첨 번호를 입력해 주세요.';
 const LOTTO_PRICE = 1000;
 
 class App {
@@ -9,10 +10,10 @@ class App {
   }
 
   play() {
-    this.userInput(PROMPT_MONEY);
+    this.moneyInput(PROMPT_MONEY);
   }
 
-  userInput(prompt) {
+  moneyInput(prompt) {
     Console.readLine(`${prompt}\n`, (input) => {
       this.countLotto(input);
     });
@@ -30,6 +31,13 @@ class App {
       Console.print(numbers);
       this.arrayLotto.push(numbers);
     }
+    this.lottoInput(PROMPT_LOTTO);
+  }
+
+  lottoInput(prompt) {
+    Console.readLine(`\n${prompt}\n`, (input) => {
+      
+    });
   }
 }
 
