82393d4e631ce749bc87c17b90df3bdcbf61055d
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-10 00:35:18 +0900

    feat: 구입금액 입력 요청 기능 구현

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,5 +1,10 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { REQUEST_MESSAGE } = require("./constants/message.js");
+
 class LottoGame {
-  play() {}
+  play() {
+    Console.readLine(REQUEST_MESSAGE.PURCHASE_MONEY, (purchaseMoney) => {});
+  }
 }
 
 module.exports = LottoGame;
