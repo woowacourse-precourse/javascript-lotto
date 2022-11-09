b11dd214885a330e3dce313cd9b8fc33e0972c62
jangsumin <jsm0726@naver.com>
2022-11-09 22:33:32 +0900

    feat: print lotto numbers once or many times

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App {
   createLotto(amount) {
     for (let i = 0; i < amount; i++) {
       const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      Console.print(numbers);
       this.arrayLotto.push(numbers);
     }
   }
