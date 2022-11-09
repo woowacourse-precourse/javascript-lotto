5ba5e7af157cbef945146573d94fbd885a48a5de
이종현 <ijonghyeon@ijonghyeon-ui-MacBookAir.local>
2022-11-09 19:12:39 +0900

    feat : get money info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const Lotto = require('./Lotto')
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  // 로또 구입
+  buyLotto(money) {
+    console.log(`${money} 만큼 구입`)
+  }
+
+  // 로또 구입 금액
+  money() {
+    let MONEY = 0
+    MissionUtils.Console.readLine('', (number) => {
+      console.log(`${number}`)
+      MONEY = number
+    })
+    this.buyLotto(MONEY)
+  }
+  play() {
+    // Start
+    MissionUtils.Console.print("구입금액을 입력해 주세요.")
+    this.money()
+
+  }
 }
 
 module.exports = App;
