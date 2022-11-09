a5b8ad24b7fcf08f3221cfeab9ab10d60b8692c2
Suyoung <jsy8492@gmail.com>
2022-11-09 16:49:38 +0900

    refactor(lottoManager): 무작위로 생성한 로또 번호를 기반으로 로또를 발행하도록 수정

--- src/LottoManager.js
+++ src/LottoManager.js
@@ -19,11 +19,9 @@ class LottoManager {
 
   issueLottos(purchaseAmount) {
     const lottoCount = purchaseAmount / 1000;
-    /**
-     * TODO: 랜덤 번호 6개 생성하여 lottoCount 만큼의 로또 발행
-     */
+
     for (let i = 0; i < lottoCount; i++) {
-      this.lottos.push(new Lotto([1, 2, 3, 4, 5, 6]));
+      this.lottos.push(new Lotto(this.createLottoNumbers()));
     }
   }
 
