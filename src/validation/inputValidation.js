70371e70bf31abafd40d0170b171ab8d22bf8a4c
Adult96 <djxor007@gmail.com>
2022-11-09 22:28:17 +0900

    feat(inputValidation): Add function to check the amount unit
    사용자 금액 단위 확인 기능 추가

--- src/validation/inputValidation.js
+++ src/validation/inputValidation.js
@@ -1,12 +1,23 @@
 function amountValidation(input) {
-  checkAmountNumber(input);
+  checkAmountUnit(input);
+  checkAmountStirng(input);
 
   return true;
 }
 
-function checkAmountUnits(input) {}
+function checkAmountUnit(input) {
+  if (input[input.length - 1] !== '0') {
+    throw new Error('[ERROR] 일의 자리 금액은 입력할수 없습니다.');
+  }
+  if (input[input.length - 2] !== '0') {
+    throw new Error('[ERROR] 십의 자리 금액은 입력할수 없습니다.');
+  }
+  if (input[input.length - 3] !== '0') {
+    throw new Error('[ERROR] 백의 자리 금액은 입력할수 없습니다.');
+  }
+}
 
 module.exports = {
   amountValidation,
-  checkAmountUnits,
+  checkAmountUnit,
 };
