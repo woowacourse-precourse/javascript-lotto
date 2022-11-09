3ce3ba81a63c4046691b2881266e5456943ff31e
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-11-09 20:59:12 +0900

    feat: 구입금액을 입력받고 계산하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
   play() {}
+
+  calculateMoney() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (userInput) => {
+      const LOTTO_PAPER = userInput / 1000;
+
+      if (userInput % 1000 !== 0) {
+        throw new Error('[ERROR] 1000원 단위로 구매해 주세요.');
+      }
+
+      return LOTTO_PAPER;
+    });
+  }
 }
 
 module.exports = App;
