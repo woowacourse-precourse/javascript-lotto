ad61b7f8bc5e156daba17418608432ad907d68af
olo90632951 <olo90632951@gmail.com>
2022-11-10 01:11:19 +0900

    feat: 보너스 번호를 입력 받는 기능 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,11 +2,22 @@ const { Console } = require('@woowacourse/mission-utils');
 
 class InputView {
   getWinningNumbers() {
-    this.triggerWinningNumbersConsole((numbers) => console.log(numbers));
+    this.triggerWinningNumbersConsole((numbers) => {
+      console.log(numbers);
+      this.getBonusNumber();
+    });
+  }
+
+  getBonusNumber() {
+    this.triggerBonusNumberConsole((number) => console.log(number));
   }
 
   triggerWinningNumbersConsole(callback) {
     Console.readLine('당첨 번호를 입력해 주세요.\n', callback);
+  }
+
+  triggerBonusNumberConsole(callback) {
+    Console.readLine('보너스 번호를 입력해 주세요.\n', callback);
   }
 }
 
