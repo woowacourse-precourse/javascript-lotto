35a89d16640912856f0ff5adf7cee3c5540d3f3d
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-09 21:36:06 +0900

    Feat: 수익률계산 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,12 +1,12 @@
 const ExceptionCheck = require('./utils/ExceptionCheck');
 const Calculate = require('./model/Calculation');
+const {MONEY} = require('./utils/Constants');
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.errorCheck = new ExceptionCheck();
-    this.validate(numbers);
     this.#numbers = numbers;
     this.calculate = new Calculate();
   }
@@ -15,10 +15,21 @@ class Lotto {
     this.errorCheck.answerNumCheck(numbers);
   }
 
-  printOfResultFromCalc(lottos, answers, bonusNum){
-    this.calculate.returns(lottos, answers, bonusNum);
+  printOfResultFromCalc(lottos, answers, bonusNum, baseMoney){
+    const answerinNum = ans => ans.map(Number);
+    const lotteryArr = (this.calculate.testAllLottos(lottos, answerinNum(answers), bonusNum));
+    const yieldCheck = this.yieldCalculation(lotteryArr, baseMoney);
+    console.log(yieldCheck)
   }
   
+  yieldCalculation(arr, baseMoney){
+    const money = arr.map((value,idx) => {
+      return value * MONEY.MONEY_SUCCESS[idx];
+    })
+    const sum = money.reduce((a,b) => (a+b));
+    return (sum*100/baseMoney).toFixed(1);
+  }
+
 }
 
 module.exports = Lotto;
