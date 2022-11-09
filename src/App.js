6057d5a4579d9e81cd7be289e436ebb505ca7fc1
“somm12” <sominan8744@gmail.com>
2022-11-10 07:10:06 +0900

    feat: 로또 구입 금액 exception 처리

--- src/App.js
+++ src/App.js
@@ -10,8 +10,26 @@ class App {
 
     MissionUtils.Console.readLine(WORD_TO_PRINT, (money) => {
       money = Number(money);
+      this.checkAmountOfMoneyToBuy(money);
     });
+  }
+
+  checkAmountOfMoneyToBuy(money) {
+    const CHECK_RESULT = money % 1000;
+
+    switch (CHECK_RESULT || isNaN(CHECK_RESULT)) {
+      case 0:
+        break;
+      case true:
+        const WRONG_NUMBER = "[ERROR] 숫자가 아닙니다.";
+        throw new Error(WRONG_NUMBER);
+      default:
+        const WRONG_INPUT = "[ERROR] 1000원 단위 금액이 아닙니다.";
+        throw new Error(WRONG_INPUT);
+    }
   }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
