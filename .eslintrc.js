8afc9cd412c59b5fdf2b4772bdccdef899de6519
Dean <yjj052800@gmail.com>
2022-11-09 20:51:05 +0900

    style: eslint, prettier 적용

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+      },
+    ],
+    semi: ['error', 'always'],
+  },
+  plugins: ['prettier'],
+};
