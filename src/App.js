0d469cd017f02d9ac3f77e9271b6608f49a7bcb4
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-10 19:47:33 +0900

    Fix : validationIsNumber fnc

--- src/App.js
+++ src/App.js
@@ -46,12 +46,12 @@ class App {
     this.printNewLine();
     this.$LottoMachine = new LottoMachine(this.$Budget.budget);
     this.printNewLine();
-    Console.close();
     this.getInput(LOTTO_NUMBER_INPUT_MESSAGE, this.lottoNumberInputCb);
   };
 
   validateIsNotNumber(...numbers) {
-    if (numbers.some((number) => !/[0-9]/.test(number))) {
+    console.log("numbers", numbers);
+    if (numbers.some((number) => /\D/.test(number))) {
       Console.close();
       throw new Error("[ERROR] 숫자만 입력해야 합니다.");
     }
@@ -61,28 +61,7 @@ class App {
     Console.readLine(message, cb);
   };
 
-  codeForPassingTest() {
-    const logs = [
-      "8개를 구매했습니다.",
-      "[8, 21, 23, 41, 42, 43]",
-      "[3, 5, 11, 16, 32, 38]",
-      "[7, 11, 16, 35, 36, 44]",
-      "[1, 8, 11, 31, 41, 42]",
-      "[13, 14, 16, 38, 42, 45]",
-      "[7, 11, 30, 40, 42, 43]",
-      "[2, 13, 22, 32, 38, 45]",
-      "[1, 3, 5, 14, 22, 45]",
-      "3개 일치 (5,000원) - 1개",
-      "4개 일치 (50,000원) - 0개",
-      "5개 일치 (1,500,000원) - 0개",
-      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-      "6개 일치 (2,000,000,000원) - 0개",
-      "총 수익률은 62.5%입니다.",
-    ];
-    logs.forEach((log) => Console.print(log));
-  }
   play() {
-    // this.codeForPassingTest();
     this.getInput(BUDGET_INPUT_MESSAGE, this.budgetInputCb);
   }
 }
