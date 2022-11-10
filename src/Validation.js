278f3e6fdb67a57a7e113674d2adf70e90f08a7b
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-10 17:03:39 +0900

    fix: hasOnlyNumber메서드가 빈 입력을 숫자로 취급한 오류 수정

--- src/Validation.js
+++ src/Validation.js
@@ -2,10 +2,14 @@ const { LOTTO_PRICE } = require("./constants/condition.js");
 
 class Validation {
   static hasOnlyNumber(input, separator = "") {
-    return input
-      .split(separator)
-      .map((eachLetter) => parseInt(eachLetter, 10))
-      .every((number) => !isNaN(number));
+    if (input) {
+      return input
+        .split(separator)
+        .map((eachLetter) => parseInt(eachLetter, 10))
+        .every((number) => !isNaN(number));
+    }
+
+    return false;
   }
   static isDivisibleByLottoPrice(purchaseMoney) {
     const remainder = purchaseMoney % LOTTO_PRICE;
