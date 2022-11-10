b2ea92b71bdd44a12a0c4c04f38da31b1c6d11c9
Rki0 <pky00823@gmail.com>
2022-11-10 10:14:13 +0900

    feat(Lotto): add function to check the range of the input
    Since only natural numbers between 1 and 45 are possible for input values,
    an Error is thrown when a number outside the range is input.

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,6 +3,7 @@ class Lotto {
 
   constructor(numbers) {
     this.validate(numbers);
+    this.checkNumberRanges(numbers);
     this.#numbers = numbers;
   }
 
@@ -12,7 +13,15 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  checkNumberRanges(numbers) {
+    numbers.forEach((item) => {
+      if (item < 1 || item > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    });
+
+    return true;
+  }
 }
 
 module.exports = Lotto;
