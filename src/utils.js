65784cce361c402e520e9df1c0d92fa48032f39f
moonelysian <dandyblack@naver.com>
2022-11-10 14:37:24 +0900

    fix: 세미콜론 빼먹은거 수정

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ const { LOTTO } = require('./constructor.js');
 const { Random, Console } = require("@woowacourse/mission-utils");
 
 const createRandomLottoNumbers = () => {
-  const randoms = Random.pickUniqueNumbersInRange(LOTTO.MIN, LOTTO.MAX, LOTTO.LENGTH)
+  const randoms = Random.pickUniqueNumbersInRange(LOTTO.MIN, LOTTO.MAX, LOTTO.LENGTH);
   return randoms.sort((num1, num2) => num1 - num2);
 }
 
