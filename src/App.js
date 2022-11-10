66b5473924bb7447c54943b0a55508ce0f7ab02f
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-10 16:10:17 +0900

    기능목록별 기능 완성, 리팩토링 시작점

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+const LottoGame = require("./LottoGame");
+
 class App {
-  play() {}
+  play() {
+    const lottoGame = new LottoGame(); //입력값을 여기서
+    lottoGame.getUserMoney();
+  }
 }
 
 module.exports = App;
