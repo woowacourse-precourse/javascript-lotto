d24fc058f1e2ef4aac2c6930f584e21171fce291
Tory <88531407+1two13@users.noreply.github.com>
2022-11-10 15:56:29 +0900

    refactor(WinningAndBonusNumbers, Validation): 사용자가 중복되지 않는 로또 번호를 입력했는지 확인

--- src/Validation.js
+++ src/Validation.js
@@ -1,7 +1,14 @@
 class Validation {
+  #notDuplicateMessage = '[ERROR] 중복되지 않는 숫자를 입력해주세요.';
+
   purchaseAmount(money) {
     const MESSAGE = '[ERROR] 1,000원으로 나누어 떨어지는 금액을 입력해주세요.';
     if (money % 1000 !== 0) throw new Error(MESSAGE);
+  }
+
+  winningNumber(numbers) {
+    // 중복된 숫자를 입력한 경우
+    if (new Set(numbers).size !== numbers.length) throw new Error(this.#notDuplicateMessage);
   }
 }
 
--- src/WinningAndBonusNumbers.js
+++ src/WinningAndBonusNumbers.js
@@ -1,13 +1,16 @@
 const Lotto = require('./Lotto');
+const Validation = require('./Validation');
 
 class WinningAndBonusNumbers {
   constructor() {
     this.lotto = new Lotto();
+    this.validation = new Validation();
   }
 
   sixNumbersInRange(numbers) {
     numbers = numbers.split(',');
     this.lotto.validate(numbers);
+    this.validation.winningNumber(numbers);
   }
 }
 
