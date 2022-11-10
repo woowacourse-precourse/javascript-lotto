f101e89455fb98b6db33a3017fb04037c2d5a842
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-10 15:22:17 +0900

    feat: 구입금액 입력이 모두 숫자인지 검사하는 메서드 구현

--- src/Validation.js
+++ src/Validation.js
@@ -1,3 +1,10 @@
-class Validation {}
+class Validation {
+  static hasOnlyNumber(input, separator = "") {
+    return input
+      .split(separator)
+      .map((eachLetter) => parseInt(eachLetter, 10))
+      .every((number) => !isNaN(number));
+  }
+}
 
 module.exports = Validation;
