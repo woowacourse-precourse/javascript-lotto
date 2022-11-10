ede9e126dc223396ce8b32dfddd6cf226d6b06ec
Jiwoo Park <jiwoo9446@naver.com>
2022-11-10 23:08:45 +0900

    feat: 로또 구입 금액 입력받기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {}
+  play() {
+    this.getInput("구입금액을 입력해 주세요.\n", this.getSeedMoney);
+  }
+
+  getInput = (message, inputFunction) => {
+    Console.readLine(message, inputFunction);
+  };
+
+  getSeedMoney = (input) => {
+    const seedMoney = input / 1000;
+  };
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
