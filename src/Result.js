3556d194027e801613e687dc93b850ec651d57ce
Rki0 <pky00823@gmail.com>
2022-11-10 23:49:25 +0900

    refactor(Result): change the way rankings are measured
    Previously, the ranking was assigned as a variable one by one,
    but it is considered to be very inefficient.
    
    So, change the way to store the rank in one array.
    
    BREAKING CHANGE: Use array for calculate ranking.
    
    Before:
    
    this.fifth = 0;
    this.fourth = 0;
    // ... //
    this.first = 0;
    
    After:
    
    this.rank = [0,0,0,0,0];

--- src/Result.js
+++ src/Result.js
@@ -1,14 +1,12 @@
+const Console = require("@woowacourse/mission-utils").Console;
+
 class Result {
   constructor(lottoArray, userLotto, bonusNumber) {
     this.lottoArray = lottoArray;
     this.userLotto = userLotto;
     this.bonusNumber = bonusNumber;
 
-    this.fifth = 0;
-    this.fourth = 0;
-    this.third = 0;
-    this.second = 0;
-    this.first = 0;
+    this.rank = [0, 0, 0, 0, 0];
 
     this.calculateEachLotto();
   }
@@ -50,41 +48,43 @@ class Result {
 
   getFifthRank() {
     if (this.score === 3) {
-      this.fifth += 1;
+      this.rank[4] += 1;
     }
   }
 
   getFourthRank() {
     if (this.score === 4) {
-      this.fourth += 1;
+      this.rank[3] += 1;
     }
   }
 
   getThirdRankOrSecondRank() {
     if (this.score === 5) {
       if (this.matchBonus) {
-        this.second += 1;
+        this.rank[2] += 1;
 
         return;
       }
 
-      this.third += 1;
+      this.rank[1] += 1;
     }
   }
 
   getFirstRank() {
     if (this.score === 6) {
-      this.first += 1;
+      this.rank[0] += 1;
     }
   }
 
   showResult() {
     Console.print("\n당첨 통계\n---");
-    Console.print(`3개 일치 (5,000원) - ${this.fifth}개`);
-    Console.print(`4개 일치 (50,000원) - ${this.fourth}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${this.third}개`);
-    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.second}개`);
-    Console.print(`6개 일치 (2,000,000,000원) - ${this.first}개`);
+    Console.print(`3개 일치 (5,000원) - ${this.rank[4]}개`);
+    Console.print(`4개 일치 (50,000원) - ${this.rank[3]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${this.rank[2]}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.rank[1]}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${this.rank[0]}개`);
   }
 }
 
