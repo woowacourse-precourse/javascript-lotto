04aec1841aa4c63d6c62d478dcd82650b34b9bf9
An Sol <apdlvmf486@gmail.com>
2022-11-10 15:15:53 +0900

    refactor: 로또 당첨 확인 기능 분리
    resultCheck.js가 당첨 확인 기능을 담당하도록 변경

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,44 +13,6 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
-
-  makeLottoWinningNumber() {
-    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 7);
-  }
-
-  winningCheck(lottonumbers, winningNumbers, bonusNumber) {
-    //상수화 필요
-    const COUNT_OF_CORRECT_NUMBERS = lottonumbers.filter((number) => winningNumbers.includes(number)).length;
-    if (COUNT_OF_CORRECT_NUMBERS === 3) return 5;
-    if (COUNT_OF_CORRECT_NUMBERS === 4) return 4;
-    if (COUNT_OF_CORRECT_NUMBERS === 5) return 3;
-    if (COUNT_OF_CORRECT_NUMBERS === 5 && lottonumbers.includes(bonusNumber)) return 2;
-    if (COUNT_OF_CORRECT_NUMBERS === 6) return 1;
-  }
-
-  lottoRankingsCount(rankingsArray, ranking) {
-    //상수화 필요.
-    if (ranking === 1) rankingsArray[0]++;
-    if (ranking === 2) rankingsArray[1]++;
-    if (ranking === 3) rankingsArray[2]++;
-    if (ranking === 4) rankingsArray[3]++;
-    if (ranking === 5) rankingsArray[4]++;
-  }
-
-  getTotalWinningMoney(rankingsArray) {
-    const PRIZE_LIST = [2000000000, 30000000, 1500000, 50000, 5000];
-    const INITIAL_VALUE = 0;
-    return rankingsArray.reduce(
-      (accumulator, currentValue, currentIndex) => accumulator + currentValue * PRIZE_LIST[currentIndex],
-      INITIAL_VALUE
-    );
-  }
-
-  getEarningsRate(totalWinningMoney, userMoney) {
-    //%로 맞추기 위한 100도 상수화 해야하나?
-    const earningsRate = ((totalWinningMoney / userMoney) * 100).toFixed(2);
-    MissionUtils.Console.print(earningsRate);
-  }
 }
 
 module.exports = Lotto;
