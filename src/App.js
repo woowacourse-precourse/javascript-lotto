05b85d75e2d0dc63da0a3f538011054b0c0eecf6
jaewoogwak <jaewoo010207@gmail.com>
2022-11-10 17:35:37 +0900

    feat: Validator 추가로 인한 유효성 검사 함수 분리

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
-const { MESSAGES, ERROR_MESSAGES } = require('./constant/messages');
+const { MESSAGES } = require('./constant/messages');
 const Lotto = require('./Lotto');
-// const Lotto = require('./Lotto');
+const Validator = require('./validator');
 
 class App {
+  constructor() {
+    this.validator = new Validator();
+  }
+
   play() {
     this.inputMoney();
   }
 
   inputMoney() {
     Console.readLine(MESSAGES.INPUT, (money) => {
-      this.validateInput(money);
+      this.validator.validateInput(money);
       this.processLottoPurchase(money);
     });
-  }
-
-  validateInput(money) {
-    this.validateInputMoney(money);
-    this.validateInputMoneyUnit(money);
-  }
-
-  validateInputMoney(money) {
-    const check = /^[0-9]+$/;
-    if (!check.test(money))
-      throw Error(ERROR_MESSAGES.INPUT_TYPE_MUST_BE_NUBMER);
-  }
-
-  validateInputMoneyUnit(money) {
-    if (parseInt(money, 10) % 1000 !== 0)
-      throw Error(ERROR_MESSAGES.MONEY_MUST_BE_DIVIDED_INTO_1000);
   }
 
   pickRandomNumbers() {
