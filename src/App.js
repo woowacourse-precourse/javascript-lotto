177d04e3128288cff2ed049d781b2a3e3b871d5b
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-10 14:32:46 +0900

    feat: take Lotto purchase amount and print out the number of purchases

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
+
 class App {
-  play() {}
+  constructor() {
+    let purchaseAmount = 0;
+  }
+
+  play() {
+    this.buyLotto();
+  }
+  buyLotto() {
+    Console.readLine('구입금액을 입력해 주세요.', (amount) => {
+      this.purchaseAmount = amount;
+      this.validatePurchaseAmount();
+      Console.print(`${this.purchaseAmount / 1000}개를 구매했습니다.`)
+    })
+  }
+
+  validatePurchaseAmount() {
+    if (this.purchaseAmount % 1000 !== 0) {
+      throw "[ERROR] 로또 구입 금액을 1,000원 단위로 입력 하세요."
+    }
+  }
 }
 
 module.exports = App;
