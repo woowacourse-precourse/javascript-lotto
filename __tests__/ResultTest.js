6576558938a8d941a646441b37df2182d55d1b3f
Rki0 <pky00823@gmail.com>
2022-11-10 22:55:32 +0900

    test(Result): add test to check the change of rank variable
    Add test to check the change of rank variable based on input value.

--- __tests__/ResultTest.js
+++ __tests__/ResultTest.js
@@ -9,4 +9,14 @@ describe("결과 테스트", () => {
     expect(result.score).toBe(0);
     expect(result.matchBonus).toBe(true);
   });
+
+  test("맞춘 숫자 개수에 따라 등수 산정", () => {
+    const result = new Result([[7, 13, 19, 22, 31, 45]], [1, 2, 3, 4, 5, 6], 7);
+
+    result.score = 3;
+
+    result.getRanking();
+
+    expect(result.fifth).toBe(1);
+  });
 });
