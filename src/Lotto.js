5f65b683d428c0166e29d33844832986f011ca5e
박정규 <srk99279@naver.com>
2022-11-10 17:22:20 +0900

    feat : 번호 일치 카운트 및 분류 메서드 생성
    당첨 개수에 의한 분류 메서드 및 그에 따른 private Map 생성. 보너스 번호에 대해서는 일치하는 카운트에 2를 더해주어 2등인 경우 총점 7점을 얻어 6개가 일치하는 경우와 구별하였음.

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,6 +3,8 @@ const { Console } = require("@woowacourse/mission-utils");
 class Lotto {
   #numbers;
   #lotteries;
+  #bonusNumber;
+  #winResult = new Map();
 
   constructor(numbers, lotteries) {
     this.validate(numbers);
@@ -11,18 +13,46 @@ class Lotto {
     this.inputBonusNumber();
   }
 
-  inputBonusNumber() {
-    console.log(this.#lotteries);
-    Console.readLine("보너스 번호를 입력해 주세요,\n", (number) => {});
-  }
-
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  inputBonusNumber() {
+    Console.readLine("\n보너스 번호를 입력해 주세요,\n", (number) => {
+      this.#bonusNumber = number;
+      this.iterateLotteries();
+      console.log(this.#winResult);
+    });
+  }
+
+  iterateLotteries() {
+    this.#lotteries.forEach((lottery) => {
+      this.checkLottery(lottery);
+    });
+  }
+
+  checkLottery(lottery) {
+    let matchCount = 0;
+    this.#numbers.forEach((number) => {
+      if (lottery.includes(number)) {
+        matchCount += 1;
+      }
+    });
+    if (matchCount == 5) this.checkBonus(lottery) ? (matchCount += 2) : "";
+    this.makeWinResult(matchCount);
+  }
+
+  checkBonus(lottery) {
+    return lottery.includes(+this.#bonusNumber);
+  }
+
+  makeWinResult(result) {
+    this.#winResult.get(result)
+      ? this.#winResult.set(result, this.#winResult.get(result) + 1)
+      : this.#winResult.set(result, 1);
+  }
 }
 
 module.exports = Lotto;
