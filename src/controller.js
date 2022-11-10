b9eaef89766e75e92aa9e3946a8ff78dffdb4548
Ryong-E <think8867@gmail.com>
2022-11-10 21:00:06 +0900

    feat(controller): 1000원 단위로 나눈 몫만큼 로또 생성 기능 구현

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,20 @@
 const { Random } = require('@woowacourse/mission-utils');
+const View = require('./view');
 
 class Controller {
-  constructor() {}
+  constructor() {
+    this.lottos = [];
+    this.view = new View(this);
+  }
+
+  generateLotto(answer) {
+    const quantity = answer / 1000;
+    for (let count = 0; count < quantity; count += 1) {
+      this.lottos.push(new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6)));
+    }
+
+    this.view.printLottos(this.lottos);
+  }
 }
 
 module.exports = Controller;
