b7f2227b73683f967de8bcb501484d626fd94118
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-10 16:57:03 +0900

    feat(Machine): 사용자에게 구입 금액을 입력받음

--- src/Machine.js
+++ src/Machine.js
@@ -1,11 +1,23 @@
 const User = require('./User');
 
+const { Console } = require('@woowacourse/mission-utils');
+
 class Machine {
   #money;
 
   constructor() {
     this.user = new User();
     this.#money = 0;
+  }
+
+  #getMoney() {
+    Console.readLine('구입금액을 입력해주세요.\n', (answer) => {
+      this.#money = Number(answer);
+    })
+  }
+
+  sell() {
+    this.#getMoney();
   }
 }
 
