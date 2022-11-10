256c65a1a585316b0efa69c2458a8da0173d5995
yuu <22yuu@naver.com>
2022-11-11 00:28:10 +0900

    feat: 입출력 및 로또 번호 생성 관련 Utils 기능 구현

---
+++ src/Utils.js
@@ -0,0 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+class Utils {
+
+    print(msg) {
+        MissionUtils.Console.print(msg);
+    }
+
+    readLine(query, callback) {
+        MissionUtils.Console.readLine(query, callback);
+    }
+
+    close() {
+        MissionUtils.Console.close();
+    }
+
+    getLottoNumbers() {
+        return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}
