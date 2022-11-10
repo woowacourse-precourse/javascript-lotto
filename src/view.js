cb34528f5a067caa34b2c67ca19980a68ab54afe
Ryong-E <think8867@gmail.com>
2022-11-10 20:52:30 +0900

    feat(view): 구입금액 입력 구현,constructor 수정
    - constructor controller를 받아 view클래스에 적용

--- src/view.js
+++ src/view.js
@@ -1,6 +1,14 @@
 const { Console } = require('@woowacourse/mission-utils');
 class View {
-  constructor() {}
+  constructor(controller) {
+    this.controller = controller;
+  }
+
+  getAmountInput() {
+    Console.readLine('구입금액을 입력해 주세요', (answer) => {
+      this.controller.generateLotto(answer);
+    });
+  }
 }
 
 module.exports = View;
