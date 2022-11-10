8806caaa4f25d3ee9c2e0c44cc50277f89aeab86
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-10 16:24:05 +0900

    test: Lotto.test.js compareTo 테스트 케이스 추가

--- src/Components/Lotto/Lotto.test.js
+++ src/Components/Lotto/Lotto.test.js
@@ -1,29 +1,13 @@
 const Lotto = require('./Lotto');
 
-describe('로또 클래스 테스트', () => {
-  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
-    expect(() => new Lotto([1, 2, 3, 4, 5, 6, 7])).toThrow(
-      new Error('[ERROR] 로또 번호는 6개여야 합니다.')
-    );
-  });
+describe('Lotto 클래스 테스트', () => {
+  test('로또 번호와 당첨 번호, 보너스 번호를 비교 후 결과를 반환한다.', () => {
+    const lottoNumbers = [1, 3, 5, 14, 22, 45];
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const BONUS_NUMBER = 7;
+    const lottoResult = new Lotto(lottoNumbers).compareTo(winningNumbers, BONUS_NUMBER);
 
-  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
-    expect(() => new Lotto([1, 2, 3, 4, 5, 5])).toThrow(
-      new Error('[ERROR] 로또 번호는 중복이 없어야 합니다.')
-    );
+    expect(lottoResult.getSameNumberCount()).toBe(3);
+    expect(lottoResult.isBonusNumberMatched()).toBe(false);
   });
-
-  test('로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.', () => {
-    expect(() => new Lotto([0, 2, 3, 4, 5, 6])).toThrow(
-      new Error('[ERROR] 로또 번호는 1부터 45까지의 수만 가능합니다.')
-    );
-  });
-
-  test('로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.', () => {
-    expect(() => new Lotto([1, 2, 3, 4, 5, 46])).toThrow(
-      new Error('[ERROR] 로또 번호는 1부터 45까지의 수만 가능합니다.')
-    );
-  });
-
-  test('');
 });
