9d5a9cb1313f70ada4b2000d20b7ff34fe6feff4
woo-jk <201702042@o.cnu.ac.kr>
2022-11-11 02:10:21 +0900

    refactor: LottoMachine 입력 검증 테스트 에러 메시지 구체화

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -4,13 +4,12 @@ describe("로또 머신 클래스 테스트", () => {
   test("금액이 숫자가 아닐 경우 예외가 발생한다.", () => {
     expect(() => {
       new LottoMachine("천원");
-    }).toThrow("[ERROR]");
+    }).toThrow("[ERROR] 금액은 숫자만 입력해야 합니다.");
   });
 
   test("금액이 1,000원 단위로 입력되지 않을 경우 예외가 발생한다.", () => {
     expect(() => {
       new LottoMachine("1500");
-    }).toThrow("[ERROR]");
+    }).toThrow("[ERROR] 금액은 1,000원 단위만 입력 가능합니다.");
   });
-
 });
