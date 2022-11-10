253dff336e33d04a5d2719319f241c5e41b9951e
moonelysian <dandyblack@naver.com>
2022-11-10 14:44:23 +0900

    refac: createRandomLottoNumbers 위치 변경
    - utils -> App class 내부로 변경
    - App 내부에서만 사용될 것 같아 내부로 옮김

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+const { LOTTO } = require('./constructor.js');
+const { Random } = require("@woowacourse/mission-utils");
+
 class App {
+  createRandomLottoNumbers = () => {
+    const randoms = Random.pickUniqueNumbersInRange(LOTTO.MIN, LOTTO.MAX, LOTTO.LENGTH);
+    return randoms.sort((num1, num2) => num1 - num2);
+  }
+
   play() {}
 }
 
