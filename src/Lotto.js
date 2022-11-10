2c034ce2513f989b25bed9dfb5b2b6619225fad3
Jiwoo Park <jiwoo9446@naver.com>
2022-11-11 03:16:34 +0900

    feat: 로또 번호가 중복되는 경우 예외처리 메서드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,10 +8,16 @@ class Lotto {
 
   validate(numbers) {
     this.isSixNumber(numbers);
+    this.isUniqueNumber(numbers);
   }
 
   isSixNumber(numbers) {
     if (numbers.length !== 6) throw new Error("[ERROR] 입력 숫자가 6개가 아님");
+  }
+
+  isUniqueNumber(numbers) {
+    if (new Set(numbers).size !== 6)
+      throw new Error("[ERROR] 겹치는 숫자 존재");
   }
   // TODO: 추가 기능 구현
 }
