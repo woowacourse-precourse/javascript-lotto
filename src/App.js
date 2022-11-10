c9af6ce98532d856b86c9ce275a07cc5ad2269f2
kwh97 <zzzang0512@naver.com>
2022-11-10 16:57:36 +0900

    feat: 사용자에게 로또 구입금액을 입력받는 기능 구현  - getPurchaseAmount() 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    this.getPurchaseAmount();
+  }
+
+  getPurchaseAmount() {
+    Console.print('구매금액을 입력해 주세요.');
+    Console.readLine('', (userInput) => {
+      console.log(userInput);
+    })
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
