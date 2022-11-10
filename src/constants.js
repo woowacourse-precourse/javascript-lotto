e42af0f4dad683c8b4e55fd04387c2d75f380a88
An Sol <apdlvmf486@gmail.com>
2022-11-10 13:50:35 +0900

    feat: 상수화를 위한 상수 목록 생성

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+const SMALLEST_LOTTO_NUMBER = 1;
+const BIGGEST_LOTTO_NUMBER = 45;
+const TOTAL_LOTTO_NUMBERS = 6;
+const MONEY_FOR_ONE_GAME = 1000;
+
+const FIRST_GRADE = 1;
+const SECOND_GRADE = 2;
+const THIRD_GRADE = 3;
+const FOURTH_GRADE = 4;
+const FIFTH_GRADE = 5;
+
+module.exports = { aaa, bbb };
