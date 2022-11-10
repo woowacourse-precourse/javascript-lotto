2f24bf9ee2f137593055f7ceec5ed588d18670e0
Buyoung Chung <sunarol@naver.com>
2022-11-10 20:49:01 +0900

    feat: Add class Message
    input message constant

---
+++ src/Message.js
@@ -0,0 +1,9 @@
+class Message {
+  static ASK_BUDGET = "구입금액을 입력해 주세요.";
+
+  static ASK_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+
+  static ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+}
+
+module.exports = Message;
