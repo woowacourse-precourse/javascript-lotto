342ef0e07c9f5e72aaa37165efc8b2b513870df1
h2002y <h2002y@naver.com>
2022-11-10 18:18:41 +0900

    feat: Add checkLottoValue for 1~45 number exception in src/Lotto.js

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,6 @@
 const ERROR_LOTTO_SIZE_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
 const ERROR_LOTTO_OVERLAP_MESSAGE = "[ERROR] 로또 번호는 중복되는 숫자가 없어야 합니다.";
+const ERROR_LOTTO_VALUE_MEESAGE = "[ERROR] 로또 번호는 1 ~ 45 사이의 숫자입니다."
 
 class Lotto {
   #numbers;
@@ -12,6 +13,7 @@ class Lotto {
   validate(numbers) {
     this.checkLottoSize(numbers);
     this.checkLottoOverlap(numbers);
+    this.checkLottoValue(numbers);
   }
 
   checkLottoSize(numbers) {
@@ -28,6 +30,14 @@ class Lotto {
       }
       else {
         throw new Error(ERROR_LOTTO_OVERLAP_MESSAGE);
+      }
+    }
+  }
+
+  checkLottoValue(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+      if(numbers[i] < 1 || 45 < numbers[i]) {
+        throw new Error(ERROR_LOTTO_VALUE_MESSAGE);
       }
     }
   }
