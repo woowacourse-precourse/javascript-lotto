b80e31949531fa95738eea00fbbcaa3f63ba112e
moonelysian <dandyblack@naver.com>
2022-11-10 17:02:23 +0900

    test: 보너스 번호관련 테스트 추가
    - 로또 번호 범위에 맞지 않는 수일때
    - 보너스 번호가 숫자가 아닐때
    - 당첨 번호와 겹칠 때

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,3 +1,4 @@
+const Bonus = require("../src/Bonus.js");
 const Lotto = require("../src/Lotto.js");
 
 describe("로또 클래스 테스트", () => {
@@ -24,6 +25,24 @@ describe("로또 클래스 테스트", () => {
   test("로또 번호에 범위에 맞지 않는 숫자가 있다면 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 90]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 로또 번호에 범위에 맞지 않는 숫자가 있다면 발생한다.", () => {
+    expect(() => {
+      new Bonus(90, [1,2,3,4,5,6]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 숫자가 아니면 발생한다.", () => {
+    expect(() => {
+      new Bonus('!', [1,2,3,4,5,6]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 당첨 번호와 겹치면 발생한다.", () => {
+    expect(() => {
+      new Bonus(1, [1,2,3,4,5,6]);
     }).toThrow("[ERROR]");
   });
 });
