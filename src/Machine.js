0d11e55a7609ef73d91f2f3c66ac292dcc088b6a
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-10 17:06:09 +0900

    feat(Machine): 사용자에게 입력받은 구매 금액이 1000원 단위인지 확인

--- src/Machine.js
+++ src/Machine.js
@@ -10,10 +10,18 @@ class Machine {
     this.#money = 0;
   }
 
+  #checkAmount() {
+    if (this.#money % 1000 !== 0) {
+      throw Error('[ERROR] 로또는 1000원 단위로 구매가 가능합니다.');
+    }
+    return true;
+  }
+
   #getMoney() {
     Console.readLine('구입금액을 입력해주세요.\n', (answer) => {
       this.#money = Number(answer);
-    })
+      this.#checkAmount();
+    });
   }
 
   sell() {
