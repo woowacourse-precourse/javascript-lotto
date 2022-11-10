02bbff58dac36e9fd20f0f1e830599b9fe296e70
jinseo <ghkdwlstj99@gmail.com>
2022-11-11 00:31:29 +0900

    feat: 발행한 로또 숫자가 1이상 45이하인 경우 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,6 +13,15 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+
+  checkLottoNumber(numbers){
+    for(let i = 0; i < numbers.length; i++){
+      if(numbers[i] > 1 || numbers[i] < 45){
+        throw new Error("[ERROR] 로또 번호는 1이상 45이하여야 합니다.");
+      }
+    }
+  }
 }
+
 
 module.exports = Lotto;
