6df7e4ad3ad4b081c3c8b1231aa48a085e17b95d
dudgp128 <20181048@sungshin.ac.kr>
2022-11-11 02:10:20 +0900

    FEAT(isValidInput) : 입력된 금액의 오류/정상 판별하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const { Console, Random } = require("@woowacourse/mission-utils/");
 class App {
-  play() {}
+  play() {
+    this.getInputMoney();
+  }
+
+  getInputMoney() {
+    Console.readline(question, (input) => {
+      if (!this.isValidInput(input)) {
+        throw new Error("유효하지 않은 값");
+      }
+    });
+  }
+
+  isValidInput(input) {
+    if (input % 1000 != 0) return false;
+    if (input === "") return false;
+    if (/[\D]/.test(input)) return false;
+    else return true;
+  }
 }
 
 module.exports = App;
