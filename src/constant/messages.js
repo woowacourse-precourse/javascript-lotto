f71ef519496b24888aad5db68b148f54c3b52858
jaewoogwak <jaewoo010207@gmail.com>
2022-11-10 17:35:47 +0900

    feat: 메시지 상수화

---
+++ src/constant/messages.js
@@ -0,0 +1,12 @@
+const MESSAGES = {
+  INPUT: '구입 금액 입력 ',
+  BUY_LOTTO: '개를 구매했습니다.',
+};
+
+const ERROR_MESSAGES = {
+  NUMBER_LENGTH_MUST_BE_SIX: '[ERROR] 로또 번호는 6개여야 합니다',
+  INPUT_TYPE_MUST_BE_NUBMER: '[ERROR] 입력 금액은 숫자여야 합니다',
+  MONEY_MUST_BE_DIVIDED_INTO_1000: '[ERROR] 금액은 1,000원 단위여야 합니다',
+};
+
+module.exports = { MESSAGES, ERROR_MESSAGES };
