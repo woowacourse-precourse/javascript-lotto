413c1bd7e13662d9ec3e3700604d44cee6fb04f8
jinseo <ghkdwlstj99@gmail.com>
2022-11-10 18:52:14 +0900

    feat: 보너스 번호에 대한 입력 구현

--- src/WinninNumberInput.js
+++ src/WinninNumberInput.js
@@ -2,9 +2,10 @@ const {Console} = require("@woowacourse/mission-utils");
 const INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
 
 class WinningNumberInput{
-    
+
     constructor(){
         this.winningNum = [];
+        this.bonusNum = [];
     }
 
     winningNumber(){
@@ -12,6 +13,13 @@ class WinningNumberInput{
             return this.winningNum.push(num);
         })
     }
+
+    bonusNumber(){
+        Console.readLine(INPUT_BONUS_NUMBER, (num) => {
+            return this.bonusNum.push(num);
+        })
+    }
+    
 }
 
 module.exports = WinningNumberInput;
