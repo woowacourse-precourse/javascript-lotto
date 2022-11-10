f8f7eb711bae5ecef38aee48c8808adf03426943
haesa <xdhaesa@gmail.com>
2022-11-10 18:35:34 +0900

    feat(Lotto.js): 로또 당첨 번호에 대한 예외 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,7 +8,23 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+
+    if (numbers.some((number) => /[^0-9]/.test(number))) {
+      throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+    }
+
+    if (
+      numbers.some(
+        (number) => numbers.indexOf(number) !== numbers.lastIndexOf(number)
+      )
+    ) {
+      throw new Error('[ERROR] 로또 번호는 중복을 허용하지 않습니다.');
+    }
+
+    if (numbers.some((number) => !(number >= 1 && number <= 45))) {
+      throw new Error('[ERROR] 로또 번호는 1과 45 사이의 숫자여야 합니다.');
     }
   }
 
