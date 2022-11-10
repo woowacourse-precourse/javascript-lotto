865c6b5470568b4c03429cf09df81d19e6f4acf2
woo-jk <201702042@o.cnu.ac.kr>
2022-11-11 03:17:22 +0900

    test: LottoMachine 로또 생성 개수 확인 테스트 코드 작성

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -1,4 +1,13 @@
 const LottoMachine = require("../src/LottoMachine");
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("../src/Lotto");
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
 
 describe("로또 머신 클래스 테스트", () => {
   test("금액이 숫자가 아닐 경우 예외가 발생한다.", () => {
@@ -12,4 +21,10 @@ describe("로또 머신 클래스 테스트", () => {
       new LottoMachine("1500");
     }).toThrow("[ERROR] 금액은 1,000원 단위만 입력 가능합니다.");
   });
+
+  test("로또 머신 클래스를 생성할 때 입력한 금액만큼 로또를 생성한다.", ()=> {
+    const lottoMachine = new LottoMachine("8000");
+    const lottos = lottoMachine.generateLottos();
+    expect(lottos).toHaveLength(8);
+  })
 });
