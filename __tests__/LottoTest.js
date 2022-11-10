f734048a43e91bae306ac67f71fcbe0c0a271ad3
myung-j-p <jmpack97@gmail.com>
2022-11-11 02:53:05 +0900

    Test: 로또 구매 개수 계산 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,8 @@
 const Lotto = require("../src/Lotto");
 
 describe("로또 클래스 테스트", () => {
+  const lotto = new Lotto();
+
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
@@ -14,5 +16,7 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('지불된 만큼 로또 구매 했는지 테스트', () => {
+    expect(lotto.checkHowManyLottos(10000)).toEqual(10);
+  })
 });
