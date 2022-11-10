3247f0ae94368d92de482b7dd0214f5260fc054e
Woong <jiyko7@kyonggi.ac.kr>
2022-11-10 19:28:11 +0900

    feat: 구입금액 입력 받기
    feat: 구입금액 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 class App {
-  play() {}
+  play() {
+    const MISSIONUTILS = require("@woowacourse/mission-utils");
+    MISSIONUTILS.Console.readLine("구입금액을 입력해 주세요.\n", function(input) {
+      var temp = input;
+      MISSIONUTILS.Console.print(temp);
+    });
+  }
 }
+
+var a = new App;
+a.play();
 
 module.exports = App;
