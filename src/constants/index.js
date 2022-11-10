490f11c0e3f1b8d605f61a56899aa53faef5ac6f
박준서 <oponize@naver.com>
2022-11-10 18:30:43 +0900

    feat: 매직넘버 제거를 위한 상수 추가

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+const LOTTO_AMOUNT = Object.freeze({
+  VALID_UNIT: 1000,
+});
+
+const LOTTO_NUMBER = Object.freeze({
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 45,
+  VALID_NUMBER_LENGTH: 6,
+  VALID_WIN_NUMBER_LENGTH: 11,
+});
+
+module.exports = {
+  LOTTO_AMOUNT,
+  LOTTO_NUMBER,
+};
