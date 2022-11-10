62681e535e5790756215e03b74d53d573c879b40
MAGHC <windowsapple95@gmail.com>
2022-11-10 18:08:16 +0900

    feat: Lotto Validate 중복 검사 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,21 @@
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
+  constructor(numbersArray) {
+    this.validate(numbersArray);
+    this.#numbers = numbersArray;
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  static validate(numbersArray) {
+    if (numbersArray.length !== 6) throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    if (new Set([...numbersArray]).size !== 6) throw new Error('[ERROR] 번호는 중복되지 않아야 합니다');
   }
 
   // TODO: 추가 기능 구현
 }
+
+const lotto = new Lotto([1, 1, 2, 3, 4, 5]);
+
+console.log(lotto.validate());
 
 module.exports = Lotto;
