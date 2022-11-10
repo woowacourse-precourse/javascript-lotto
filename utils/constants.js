2c7e12434c334a53c8f3eae8296a1ef69366cc82
turtle601 <turtle601@naver.com>
2022-11-10 22:47:04 +0900

    refactor(constant) LottoStore 관련 enum 객체 추가
    Add:
    1) VARIBLE_LOTTO.priceUnit
    2) VARIBLE_LOTTO.priceRegex
    3) LOTTO_ERROR_MESSAGE.priceLimit

--- utils/constants.js
+++ utils/constants.js
@@ -3,12 +3,16 @@ const VARIABLE_LOTTO = Object.freeze({
   start: 1,
   end: 45,
   len: 6,
+
+  priceUnit: 1000,
+  priceRegex: /^[1-9]+0{3,}$/,
 });
 
 const LOTTO_ERROR_MESSAGE = Object.freeze({
   range: '[ERROR] 보너스 번호는 1 ~ 45사이 숫자여야 합니다.',
   len: '[ERROR] 로또 번호는 6개여야 합니다.',
   overlap: '[ERROR] 로또 번호는 중복이 되어서는 안됩니다.',
+  priceLimit: '[ERROR] 금액 단위는 천 단위여야 합니다.',
 });
 
 module.exports = {
