f624dc653a21647f695886402e4f807114dafabd
Ryong-E <think8867@gmail.com>
2022-11-11 01:57:46 +0900

    fix(validation): 당첨번호 예외처리 로직 수정
    - 문자열로 들어오는 값을 바로 forEach하는 오류가 있어 split(,)을 추가 하였습니다

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -19,7 +19,7 @@ class Validation {
     if (input.split(',').length !== 6) {
       throw new Error('[ERROR] 1~45 서로다른 6개 수를 입력해주세요');
     }
-    input.forEach((number) => {
+    input.split(',').forEach((number) => {
       if (Number(number) >= 1 && Number(number) <= 45) {
         return;
       }
