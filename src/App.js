e428ea22e5e5a5f11c75696f6628d9edb8a3cee0
evencoding <evencoding@gmail.com>
2022-11-10 17:22:47 +0900

    feat(Validator): 입력 받은 금액에 대한 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const { Console } = require('@woowacourse/mission-utils');
+const Validator = require('./Validator');
+
 class App {
-  play() {}
+  constructor() {
+    this.lottoCount = 0;
+    this.userLottoNumbers = [];
+  }
+
+  play() {
+    this.buyLotto();
+  }
+
+  buyLotto() {
+    Console.print('구입금액을 입력해 주세요.');
+    Console.readLine('', (money) => {
+      Validator.throwErrorIfInValidMoney(money);
+      this.lottoCount = money / 1000;
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
