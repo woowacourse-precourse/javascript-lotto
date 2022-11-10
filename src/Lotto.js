be19b917a73b650a9d87bb9a3c22f8f1a26d570f
chee9835 <huisu121@gmail.com>
2022-11-11 02:11:22 +0900

    refactor: 재사용가능 메서드 utils로 분리

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,19 +1,21 @@
 const { LOTTO_LENGTH, ERROR } = require("./constant/lotto");
+const Utils = require("./utils/utils");
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
+    this.utils = new Utils();
     this.validate(numbers);
     this.#numbers = numbers;
   }
 
   validate(numbers) {
     if (this.isNotVaildLength(numbers)) {
-      this.throwError(ERROR.NOT_SIX_NUMBER);
+      this.utils.throwError(ERROR.NOT_SIX_NUMBER);
     }
     if (this.isNotDiffNumbers(numbers)) {
-      this.throwError(ERROR.DUPLICATE_NUMBERS);
+      this.utils.throwError(ERROR.DUPLICATE_NUMBERS);
     }
   }
 
@@ -24,10 +26,6 @@ class Lotto {
   isNotDiffNumbers(numbers) {
     const numberSet = new Set(numbers);
     return numberSet.size !== LOTTO_LENGTH;
-  }
-
-  throwError(messege) {
-    throw new Error(`[ERROR] ${messege}`);
   }
 }
 
