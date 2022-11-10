fe0879197e8f2676f27f5aaedce8c5d6d82fa945
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-10 18:37:53 +0900

    feat(Deposit): 로또 구입 금액이 1000원 단위가 아니면 예외발생

--- src/Deposit.js
+++ src/Deposit.js
@@ -4,7 +4,14 @@ class Deposit {
 
   constructor(amount) {
     this.#priceOfLotto = 1000;
+    this.validate(amount);
     this.#amount = amount;
+  }
+
+  validate(amount) {
+    if (amount % this.#priceOfLotto) {
+      throw new Error('[ERROR] 로또 구입 금액은 1000원 단위만 가능합니다.');
+    }
   }
 }
 
