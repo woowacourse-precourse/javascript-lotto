54940dad308e7a7fcfc67f201234805b2a61299f
gunyongbok <qhrrjsdyd123@gmail.com>
2022-11-10 14:28:22 +0900

    Feat: 구입 금액 검증 함수 구현
    입력받은 구입 금액이 1000의 배수가 아닌경우와 0인 경우 예외처리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const ONE_LOTTO_PRICE = 1000;
 
 class App {
     play() {
@@ -8,9 +9,15 @@ class App {
         MissionUtils.Console.readLine(
             '구입금액을 입력해 주세요.\n',
             (price) => {
+                this.validatePurchasePrice(price);
                 console.log(price);
             }
         );
+    }
+    validatePurchasePrice(price) {
+        if (price % ONE_LOTTO_PRICE !== 0 || price === String(0)) {
+            throw '[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.';
+        }
     }
 }
 
