f2f01a0b1cf550e2a07488e048aa8e96487da52c
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2022-11-11 01:25:46 +0900

    gitignore tracking 오류 수정

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -5,4 +5,8 @@ const isNumber = (input) => {
   throw new Error("[ERROR] 숫자가 아닌 값은 입력할 수 없습니다.");
 };
 
+const isNotZero = (input) => {
+  if (input !== "0") return;
+};
+
 module.exports = { isNumber };
