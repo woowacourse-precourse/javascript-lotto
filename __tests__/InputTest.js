7aef870524c5c64cc82a3380e90f191518154799
Adult96 <djxor007@gmail.com>
2022-11-10 17:14:07 +0900

    feat(winningValidation): Add function to check the blank number of winning numbers
    당첨번호 공백 확인 기능 추가

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -4,6 +4,8 @@ const {
   checkAmountEmpty,
 } = require('../src/validation/amountValidation');
 
+const { checkWinningEmpty } = require('../src/validation/winningValidation');
+
 describe('사용자 입력 테스트', () => {
   test('입력한 금액이 1000원 단위가 아닐경우 예외가 발생한다.', () => {
     const input = '1001';
--- src/validation/winningValidation.js
+++ src/validation/winningValidation.js
@@ -2,7 +2,11 @@ function winningValidation(string) {
   checkWinningEmpty(string);
 }
 
-function checkWinningEmpty(string) {}
+function checkWinningEmpty(string) {
+  if (string === '') {
+    throw new Error('[ERROR] 공백은 입력할수 없습니다.');
+  }
+}
 
 function checkWinningString(string) {}
 
