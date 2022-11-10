eddbd5cb8a8d65bf4397c98666210276d043fdc6
khj0426 <706shin1728@naver.com>
2022-11-11 01:07:49 +0900

    :feat:중복되지 않는 6개의 로또를 뽑는 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,6 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const MESSAGE = require('./MESSAGE');
+
 class Lotto {
   #numbers;
 
@@ -8,11 +11,23 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  pubishLotto() {
+    const randomArray = MissionUtils.Random.pickUniqueNumbersInRange(
+      MESSAGE.FIRST_RANGE,
+      MESSAGE.LAST_RANGE,
+      MESSAGE.LOTTO_LENGTH
+    );
+    randomArray.sort((a, b) => {
+      return a - b;
+    });
+
+    return randomArray;
+  }
 }
 
 module.exports = Lotto;
