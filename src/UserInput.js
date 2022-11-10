9e83b9bbe90007f96127e9526a5776d01b96aaa3
khj0426 <706shin1728@naver.com>
2022-11-10 16:55:37 +0900

    feat:로또 몇 장 구매가능한지 알려주는 기능 추가

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,24 +1,40 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-const Message = require('./MESSAGE');
+const MESSAGE = require('./MESSAGE');
 
 const UserInput = {
   userInput: '',
+  errorOutput: MESSAGE.INPUT_ERROR_MESSAGE,
   getInput() {
-    MissionUtils.Console.readLine(Message.USERBUYMESSAGE, (userinputvalue) => {
-      this.userInput = userinputvalue;
-    });
+    MissionUtils.Console.readLine(
+      MESSAGE.USER_BUY_MESSAGE,
+      (userinputvalue) => {
+        this.userInput = userinputvalue;
+      }
+    );
   },
   validate(inputStr) {
-    if (!Message.CHECKVALIDINPUT.test(inputStr)) {
+    if (!MESSAGE.CHECK_VALID_INPUT.test(inputStr)) {
       return false;
     }
     return true;
   },
   DivideByThousand(inputStr) {
-    if (Number(inputStr) % Message.THOUSAND === 0) {
+    if (Number(inputStr) % MESSAGE.THOUSAND === 0) {
       return true;
     }
     return false;
+  },
+  isValidInput() {
+    if (this.validate(this.userInput) === false) {
+      return this.errorOutput;
+    }
+    if (this.DivideByThousand(this.userInput) === false) {
+      return this.errorOutput;
+    }
+    return true;
+  },
+  numofLotts(inputStr) {
+    return Number(inputStr) / MESSAGE.THOUSAND;
   },
 };
 
