503856ac52480f0b7000f026a4dbe46a5d2f2086
Leejin-Yang <ejin1996@gmail.com>
2022-11-10 18:22:11 +0900

    feat: 구입 금액만큼 로또 발행 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { GAME_MESSAGE } = require('./constants/lotto');
+
 const LottoBuyer = require('./LottoBuyer');
+const LottoSales = require('./LottoSales');
 
 class App {
   #buyer;
@@ -16,7 +18,8 @@ class App {
   #readMoney() {
     Console.readLine(GAME_MESSAGE.MONEY_INPUT, (money) => {
       this.#buyer = new LottoBuyer(money);
-      Console.print(this.#buyer.money);
+      const lottos = LottoSales.issueLottos(this.#buyer.money);
+      Console.print(lottos);
       Console.close();
     });
   }
--- src/LottoSales.js
+++ src/LottoSales.js
@@ -6,6 +6,13 @@ class LottoSales {
     const numbers = Random.pickUniqueNumbersInRange(RULE.RANGE_START, RULE.RANGE_END, RULE.LENGTH);
     return numbers.sort((prev, cur) => prev - cur);
   }
+
+  static issueLottos(money) {
+    const number = money / 1_000;
+    const lottos = new Array(number).fill(0).map(() => LottoSales.issueLotto());
+
+    return lottos;
+  }
 }
 
 module.exports = LottoSales;
