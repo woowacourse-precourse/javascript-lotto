61975ed4c58b6c617e70ca262339d73f44595486
MAGHC <windowsapple95@gmail.com>
2022-11-10 20:02:01 +0900

    feat: 공통 validate 음수,실수 검증

--- src/Validate.js
+++ src/Validate.js
@@ -1,8 +1,9 @@
 const validate = {
   validateCommon(input) {
     if (typeof +input !== 'number' || Number.isNaN(input)) return false;
+    if (Math.sign(+input) === -1 || !Number.isInteger(+input)) return false;
     return true;
   },
 };
 
-console.log(validate.validateCommon(NaN));
+console.log(validate.validateCommon(12.23));
