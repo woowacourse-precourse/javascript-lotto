9397db1bf1f551ce6896cbfa86eea7028c3e6105
jaewoogwak <jaewoo010207@gmail.com>
2022-11-10 17:28:00 +0900

    feat: 로또 구매 동작 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
 const { MESSAGES, ERROR_MESSAGES } = require('./constant/messages');
+const Lotto = require('./Lotto');
 // const Lotto = require('./Lotto');
 
 class App {
@@ -10,6 +11,7 @@ class App {
   inputMoney() {
     Console.readLine(MESSAGES.INPUT, (money) => {
       this.validateInput(money);
+      this.processLottoPurchase(money);
     });
   }
 
@@ -31,6 +33,34 @@ class App {
 
   pickRandomNumbers() {
     return Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
+
+  // 구매할 로또의 개수를 받으면 해당 개수만큼 로또를 배열로 리턴함
+  buyLotto(number) {
+    const tokens = this.createToken(number);
+    const lottos = tokens.map((token) => new Lotto(token));
+    return lottos;
+  }
+
+  showLotto(lottos) {
+    Console.print(`${lottos.length}${MESSAGES.BUY_LOTTO}`);
+    lottos.forEach((lotto) => Console.print(lotto.getNumbers()));
+  }
+
+  processLottoPurchase(number) {
+    const lottos = this.buyLotto(number / 1000);
+    this.showLotto(lottos);
+  }
+
+  // 정수값을 받으면 해당 수만큼의 토큰을 리턴함
+  createToken(number) {
+    let count = 0;
+    const tokens = [];
+    while (count < number) {
+      tokens.push(this.pickRandomNumbers());
+      count += 1;
+    }
+    return tokens;
   }
 }
 
