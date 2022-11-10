009196b8a06946715c982775c475b12bc952803a
Bori-github <qhflrnfl4324@gmail.com>
2022-11-10 21:47:34 +0900

    feat(App): print game start message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    this.startGame();
+  }
+
+  printStartMessage() {
+    Console.print('구입금액을 입력해 주세요.');
+  }
+
+  startGame() {
+    this.printStartMessage();
+  }
 }
 
 module.exports = App;
