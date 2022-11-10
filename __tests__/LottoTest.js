22dfbe148737815121a996c8ba412adf0f15f9b0
Buyoung Chung <sunarol@naver.com>
2022-11-10 22:05:55 +0900

    test: Add AutoLotto makeRandomLottoArray test

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
 const Budget = require("../src/Budget");
+const AutoLotto = require("../src/AutoLotto");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -18,5 +19,12 @@ describe("로또 클래스 테스트", () => {
     const budget = new Budget();
     budget.divideBudget(8000);
     expect(budget.returnCount()).toBe(8);
+  });
+
+  test("중복이 없는 자동 로또를 생성한다.", () => {
+    const autoLotto = new AutoLotto();
+    autoLotto.makeRandomLottoArray(1);
+    const testSet = new Set(...autoLotto.randomLottoArray);
+    expect([...testSet].length).toBe(6);
   });
 });
