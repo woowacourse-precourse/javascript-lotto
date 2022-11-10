8a746c06b2c01d6967b6e385573a3120156ab026
kyw0716 <kyw0716@naver.com>
2022-11-11 01:21:42 +0900

    test: 입력된 보너스 번호 유효성 테스트 코드 추가

--- __tests__/ValidCheckTest.js
+++ __tests__/ValidCheckTest.js
@@ -51,4 +51,37 @@ describe("입력값 유효성 테스트", () => {
       });
     });
   });
+
+  test("보너스 번호가 당첨 번호와 중복되면 에러를 발생시킨다.", () => {
+    const invalidInput = ["1", "32", "27"];
+    const winningNum = [
+      ["1", "2", "3", "4", "5", "6"],
+      ["1", "2", "32", "4", "5", "6"],
+      ["1", "27", "3", "4", "5", "6"],
+    ];
+
+    invalidInput.forEach((input, i) => {
+      expect(() => {
+        ValidCheckUtils.checkBonusNumber(input, winningNum[i]);
+      }).toThrow("[ERROR]");
+    });
+  });
+
+  test("보너스 번호가 1부터 45사이의 값이 아니라면 오류를 발생시킨다.", () => {
+    const invalidInput = ["0", "46", "123", "-5"];
+    invalidInput.forEach((input) => {
+      expect(() => {
+        ValidCheckUtils.checkBonusNumber(input, ["1", "2", "3", "4", "5", "6"]);
+      }).toThrow("[ERROR]");
+    });
+  });
+
+  test("보너스 번호가 숫자가 아니면 오류를 발생시킨다.", () => {
+    const invalidInput = ["j", " ", ""];
+    invalidInput.forEach((input) => {
+      expect(() => {
+        ValidCheckUtils.checkBonusNumber(input, ["1", "2", "3", "4", "5", "6"]);
+      }).toThrow("[ERROR]");
+    });
+  });
 });
