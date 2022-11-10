2c198090eb261d7614896ebc621b35bd76395904
pakxe <pigkill40@naver.com>
2022-11-11 05:38:15 +0900

    fix(shop): invalidMonetaryUnit 메서드의 틀린 부분 수정

--- src/Shop.js
+++ src/Shop.js
@@ -9,13 +9,16 @@ class Shop {
 
   validate(money) {
     if (isNaN(money)) throw new Error(`${ERROR.PREFIX} ${ERROR.NUMBER_ONLY}`);
-    if (this.#invalidMonetaryUnit())
+    if (this.#invalidMonetaryUnit(money))
       throw new Error(`${ERROR.PREFIX} ${ERROR.MONETARY_UNIT}`);
+    return true;
   }
 
   #invalidMonetaryUnit(money) {
-    return !(money % 1000);
+    return money % 1000;
   }
 }
+
+const shop = new Shop(1500);
 
 module.exports = Shop;
