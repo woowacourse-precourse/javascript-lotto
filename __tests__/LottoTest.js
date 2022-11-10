bd946788f58fe4e9ccc2d4debe59ce4c5e88ae3f
banma70 <banma1234@gmail.com>
2022-11-10 16:41:11 +0900

    test: 추가된 예외처리 함수에 대한 test 진행및 검토

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,38 +1,43 @@
 const Lotto = require("../src/Lotto");
+const MissionUtils = require("@woowacourse/mission-utils");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+      new Lotto("1, 2, 3, 4, 5, 6, 7");
     }).toThrow("[ERROR]");
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
+      new Lotto("1, 2, 3, 4, 5, 5");
     }).toThrow("[ERROR]");
   });
+
+  // 아래에 추가 테스트 작성 가능
 
   test("로또 번호의 범위가 1 ~ 45가 아니면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 77, 5, 6]);
+      new Lotto("1, 2, 3, 77, 5, 6");
     }).toThrow("[ERROR]");
   });
-
+  
   test("보너스 숫자의 범위가 1 ~ 45가 아니면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6], 77, 1000);
+      new Lotto("1, 2, 3, 4, 5, 6", "77", "1000");
     }).toThrow("[ERROR]")
   })
 
   test("금액을 1000원 단위로 입력하지 않으면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6], 10, 7777);
+      new Lotto("1, 2, 3, 4, 5, 6", "10", "7777");
     }).toThrow("[ERROR]")
   })
 
-  
-
-  // 아래에 추가 테스트 작성 가능
+  test("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto("1, 2, 3, 4, 5, 6", "1", "5000");
+    }).toThrow("[ERROR]")
+  })
 });
