53cf61190ef4f0dfef592242994687f237aebec2
moonelysian <dandyblack@naver.com>
2022-11-10 22:35:06 +0900

    feat: 구매액 입력받고 구매한 장수 출력
    - purchase class를 생성자를 통해 입력값을 검증
    - getPurchaseAmount로 검증된 입력값 가져옴
    - getPushlushCount로 구매한 장수 가져옴

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-const { LOTTO } = require('./constructor.js');
-const { Random } = require("@woowacourse/mission-utils");
+const { Random } = require('@woowacourse/mission-utils');
+const { LOTTO, COMMAND, ERROR, MONEY, MESSAGE } = require('./constructor.js');
+const { printMessage, userInput, throwErrorMessage } = require('./utils.js');
+const Lotto = require('./Lotto.js');
+const Bonus = require('./Bonus.js');
+const Purchase = require('./Purchase.js');
 
 class App {
-  createRandomLottoNumbers = () => {
+  constructor() {
+    this.purchaseAmount = 0;
+  }
+
+  createRandomLottoNumbers() {
     const randoms = Random.pickUniqueNumbersInRange(LOTTO.MIN, LOTTO.MAX, LOTTO.LENGTH);
     return randoms.sort((num1, num2) => num1 - num2);
   }
 
-  play() {}
+  play() {
+    userInput(COMMAND.INPUT_PURCHASE_AMOUNT, (amount) => {
+      const purchase = new Purchase(amount);
+      this.purchaseAmount = purchase.getPurchaseAmount();
+      
+      const publishCount = purchase.getPublishCount();
+      printMessage(`${publishCount}개 구매했습니다.`);
+    });
+  }
 }
 
 module.exports = App;
