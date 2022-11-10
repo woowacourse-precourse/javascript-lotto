17e22e3de667700e434e17d5d08f344d85f06375
Woong <jiyko7@kyonggi.ac.kr>
2022-11-10 20:32:58 +0900

    fix: 당첨 번호 에러 추가 처리
    fix: 당첨 번호 에러 추가 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,6 +13,8 @@ class Lotto {
 
     var checkArray = [];
     for (var n of numbers){
+      checkNum(n);
+      checkNumLotto(n);
       checkArr(checkArray,n);
     }
   }
@@ -32,6 +34,18 @@ class Lotto {
     if (this.#numbers.includes(bonus)) {
       throw new Error("[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.");
     }
+  }
+}
+
+function checkNum(n){
+  if (isNaN(n)){
+    throw new Error("[ERROR] 로또 번호는 숫자만 가능합니다");
+  }
+}
+
+function checkNumLotto(n){
+  if(n<1 || n>45){
+    throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
   }
 }
 
