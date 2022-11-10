42f1d85642a95386d5d098a3fe78f62084904094
Rki0 <pky00823@gmail.com>
2022-11-10 13:50:52 +0900

    test(BonusNumber): add test to check the range of the input
    Throw an Error if it is not between 1 and 45.

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -6,4 +6,10 @@ describe("보너스 번호 클래스 테스트", () => {
       new BonusNumber("1 2");
     }).toThrow("[ERROR] 숫자만이 입력 가능합니다.");
   });
+
+  test("보너스 번호는 1에서 45 사이의 값만 입력 가능하다.", () => {
+    expect(() => {
+      new BonusNumber("451");
+    }).toThrow("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+  });
 });
