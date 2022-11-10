56cb453a6b719e64abd5a177f1cafbabbc412030
somin <smhs12@naver.com>
2022-11-10 16:54:15 +0900

    feat: 금액 입력 요구 메세지 출력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print("구입금액을 입력해 주세요.");
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
