738ca4cbfdccf124e41dbd3266ff0a610064a42b
khj0426 <706shin1728@naver.com>
2022-11-11 02:20:14 +0900

    feat:1-45의 1과 45 상수화

--- src/MESSAGE.js
+++ src/MESSAGE.js
@@ -4,7 +4,12 @@ const MESSAGE = {
   THOUSAND: 1000,
   INPUT_ERROR_MESSAGE: '[ERROR]:1000으로 나누어 떨어지는 숫자를 입력해주세요',
   USER_INIT_INPUT: '',
-  USER_NUMBER_LOTTOS: '개를 구매했습니다',
+  USER_NUMBER_LOTTOS: '개를 구매했습니다.',
+  FIRST_RANGE: 1,
+  LAST_RANGE: 45,
+  LOTTO_LENGTH: 6,
+  LOTTO_INIT_STR: '######',
+  SEPERATOR: ', ',
 };
 
 module.exports = MESSAGE;
