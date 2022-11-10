b5341fce6dfdad6878ef60bd611713b788138bbb
nalsae <nalsae0207@naver.com>
2022-11-10 20:37:08 +0900

    feat: implement a method to start game
    게임을 시작하기 위한 메서드 구현 완료
    - 가격을 사용자의 입력 값으로 전달 받음
    - 전달 받은 가격을 인수로 전달하여 에러 판별 메서드 호출
    - 정상적인 가격이라면 가격을 출력
    - 가격만큼 로또를 발행하는 메서드 호출

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 const Lotto = require("./Lotto");
 const { Console, Random } = require("@woowacourse/mission-utils");
 
-class App extends Lotto {
+class App {
+  startGame() {
+    Console.readLine("구입 금액을 입력해주세요.\n", (price) => {
+      this.findPriceError(price);
+
+      Console.print(price);
+      this.issueLotto(price);
+    });
+  }
+
   play() {}
 }
 
