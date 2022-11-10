cb1bcb10c6eae8d9b8b2ad4b0e1ed10ba8fb629f
SWARVY <swarvy0826@naver.com>
2022-11-11 00:05:17 +0900

    refactor : 사용 예정인 상수 추가

--- src/Constants.js
+++ src/Constants.js
@@ -10,4 +10,40 @@ const PICK_TYPE = {
   bonus: 1,
 };
 
-module.exports = { INGAME_MESSAGE, PICK_TYPE };
+const PICK_LENGTH = {
+  main: 6,
+  bonus: 1,
+};
+
+const RANK_INDEX = {
+  first: 4,
+  second: 3,
+  third: 2,
+  fourth: 1,
+  fifth: 0,
+};
+
+const RANK_PRIZE = {
+  fitrst: 2000000000,
+  second: 30000000,
+  third: 15000000,
+  fourth: 50000,
+  fifth: 5000,
+};
+
+const RANK_PRINT = {
+  first: '6개 일치 (2,000,000,000원) - ',
+  second: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  third: '5개 일치 (1,500,000원) - ',
+  fourth: '4개 일치 (50,000원) - ',
+  fifth: '3개 일치 (5,000원) - ',
+};
+
+module.exports = {
+  INGAME_MESSAGE,
+  PICK_TYPE,
+  PICK_LENGTH,
+  RANK_INDEX,
+  RANK_PRIZE,
+  RANK_PRINT,
+};
