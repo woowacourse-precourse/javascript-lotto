7cbbe94298c118e1492180b8f50310b04e03969b
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-10 19:01:03 +0900

    test(Purchase): 사용자가 입력한 금액이 1000원 단위가 아니라면 에러를 던지는 테스트

---
+++ __tests__/PurchaseTest.js
@@ -0,0 +1,13 @@
+const Machine = require('../src/Machine');
+const { getLogSpy, mockQuestions } = require('./ApplicationTest');
+const App = require('../src/App');
+
+describe('사용자의 금액 투입 테스트', () => {
+  test('사용자가 입력한 금액이 1000원 단위가 아니라면 에러를 발생시킨다.', () => {
+    mockQuestions(['5555']);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow('[ERROR]');
+  });
+});
