221aaa2d9105a98ebc64ba66fe8ba9990cddddad
khj0426 <706shin1728@naver.com>
2022-11-10 14:09:29 +0900

    feat:예외처리 메시지 상수화

--- src/MESSAGE.js
+++ src/MESSAGE.js
@@ -2,6 +2,8 @@ const MESSAGE = {
   USER_BUY_MESSAGE: '구입 금액을 입력해주세요',
   CHECK_VALID_INPUT: /^[0-9]+$/,
   THOUSAND: 1000,
+  INPUT_ERROR_MESSAGE: '숫자를 입력해주세요',
+  INPUT_DIVIDE_MESSAGE: '1000으로 나누어 떨어지는 숫자를 입력해주세요',
 };
 
 module.exports = MESSAGE;
