1f6352c35d66c595e2937855939540a57b978eab
eunsung <esk114477@gmail.com>
2022-11-10 10:40:26 +0900

    refactor : 랜덤 로또 번호 생성 시 오름차순 출력으로 변경

--- src/App.js
+++ src/App.js
@@ -8,19 +8,35 @@ const buyLotto = () => {
   Console.readLine('구입금액을 입력해 주세요.\n', (num) => {
     countLotto = Number(num) / 1000;
     throwInput = Number(num) % 1000;
-    input = num.toString().split('').map((str) => Number(str));
+    showLottoNumber(countLotto,throwInput);
     });
 }
 
 const setLottoNumber = () => {
   const lottoNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-  return lottoNumber;
+  lottoNumber.sort(function(a,b) {
+    if(a > b) return 1;
+    if(a === b) return 0;
+    if(a < b) return -1;
+  });
+  Console.print(lottoNumber);
+}
+
+const showLottoNumber = (countLotto, throwInput) => {
+  if(throwInput > 0) throw '금액은 1,000원 단위로 입력해주세요';
+  Console.print(`${countLotto}개를 구매했습니다.`);
+  for(let i = 0; i<countLotto; i++){
+    setLottoNumber()
+  }
 }
 
 
 class App {
-  play() {}
+  play() {
+    buyLotto();
+  }
 }
+
 const app = new App();
 app.play()
 
