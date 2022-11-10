a5973f9da506341d36a9709c7fe357a2f086183e
SWARVY <swarvy0826@naver.com>
2022-11-10 14:37:02 +0900

    refactor : MESSAGE 개행 문자 추가

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,7 @@
 const INGAME_MESSAGE = {
-  buy: '구입금액을 입력해 주세요.',
-  pick: '당첨 번호를 입력해 주세요.',
-  bonuspick: '보너스 번호를 입력해 주세요.',
+  buy: '구입금액을 입력해 주세요.\n',
+  pick: '당첨 번호를 입력해 주세요.\n',
+  bonuspick: '보너스 번호를 입력해 주세요.\n',
   statistic: '당첨 통계\n ---',
 };
 
