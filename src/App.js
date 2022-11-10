1a64aa8cde8d19e2871cd0693a672498655ecfec
GunHoi <lgh509@kyonggi.ac.kr>
2022-11-10 23:08:47 +0900

    feat - 2 : 로또 번호 구매매수 만큼 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   insertedMoney;
-  play() {}
+  userLottoNumberLists = [];
+  async play() {
+    await this.insertMoney();
+    const LottoCount = this.insertedMoney / 1000;
+    for(let count = 0; count <LottoCount; count++){
+      this.userLottoNumberLists.push(generateUserLottoNumber());
+    }
+  }
   insertMoney(){
     MissionUtils.Console.readLine('구입금액을 입력해주세요 (1000원 단위)', (insertMoney) => {
       insertMoneyValidCheck(insertMoney);
@@ -11,6 +18,7 @@ class App {
   insertMoneyValidCheck(insertMoney) {
     isUnit1000(insertMoney);
     isNegativeNumber(insertMoney);
+    isNumber(insertMoney);
   }
   isUnit1000(insertMoney){
     if (insertMoney % 1000 !== 0) {
@@ -27,6 +35,10 @@ class App {
     if(IS_NOT_NUMBER.test(inputNumber)){
       throw new Error("[ERROR] 숫자만 입력해주세요.");
     }
+  }
+  generateUserLottoNumber(){
+    const generatedNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    return generatedNumbers;
   }
 }
 
