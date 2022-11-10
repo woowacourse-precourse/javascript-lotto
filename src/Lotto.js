7eead328a37fdd4e1e597bbcab0139079115270f
jangsumin <jsm0726@naver.com>
2022-11-10 09:46:27 +0900

    feat(Lotto): exclude the string form of number input

--- src/Lotto.js
+++ src/Lotto.js
@@ -16,11 +16,14 @@ class Lotto {
     }
 
     numbers.map((number) => {
-      if (!(number >= 1 && number <= 45)) throw new Error("[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다.");
+      if (!(parseInt(number) >= 1 && parseInt(number) <= 45)) throw new Error("[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다.");
     });
   }
 
   // TODO: 추가 기능 구현
 }
+
+let lotto = new Lotto([1, 2, 3, 4, 5, '47']);
+lotto.validate;
 
 module.exports = Lotto;
