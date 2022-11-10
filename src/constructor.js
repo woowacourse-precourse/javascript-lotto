4afdd1d29067ae86c6cdd5b0dcde6b023c880048
moonelysian <dandyblack@naver.com>
2022-11-10 15:15:02 +0900

    feat: 필요한 에러메세지 추가

--- src/constructor.js
+++ src/constructor.js
@@ -7,8 +7,10 @@ const LOTTO = {
 
 const ERROR = {
   PREFIX: "[ERROR]",
-  NO_DUPLICATE: "중복된 숫자가 있습니다.",
-  OUT_OF_LOTTO_LENGTH: "로또 번호는 6개여야 합니다.",
+  DUPLICATE_NUMBERS: "중복된 숫자가 있습니다.",
+  OUT_OF_LOTTO_LENGTH: `로또 번호는 ${LOTTO.LENGTH}개여야 합니다.`,
+  ONLY_NUMBER: "숫자만 입력해야합니다.",
+  OUT_OF_NUMEBR_RANGE: `${LOTTO.MIN}~${LOTTO.MAX}사이 숫자만 가능합니다.`,
 }
 
 module.exports = {
