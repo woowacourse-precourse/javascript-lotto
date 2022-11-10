b8f4ee6c0fc5dac4bfb2f61238eb9906173501ab
jiho1996 <jhp9270@naver.com>
2022-11-10 22:05:48 +0900

    feat : 숫자가 아니라면 throw 예외처리 추가

--- src/LottoNumberGenerator.js
+++ src/LottoNumberGenerator.js
@@ -5,17 +5,20 @@ class LottoNumberGenerator {
   inputNumberFromUser() {
     readLine("구입 금액을 입력해주세요.", (input) => {
       this.#inputNumber = input;
-
-      this.isInputNumbersValid(this.#inputNumber);
+      this.#isInputNumbersValid(this.#inputNumber);
     });
   }
 
-  isInputNumbersValid(numbers) {
-    if (this.#isNotDividedThousand(numbers))
-      throw Error("[ERROR] 천원 단위 입력을 해주세요.");
+  #isInputNumbersValid(numbers) {
+    if (this.#isNotConsistOnlyNumber(numbers)) throw Error("[ERROR] 숫자만 입력 가능합니다.");
+    if (this.#isNotDividedThousand(numbers)) throw Error("[ERROR] 천원 단위 입력을 해주세요.");
   }
 
-  #isNotDividedThousand(numbers) {
-    return numbers === "0" || numbers % 1000 !== 0;
+  #isNotDividedThousand(input) {
+    return input === "0" || input % 1000 !== 0;
+  }
+
+  #isNotConsistOnlyNumber(input) {
+    return /[^0-9]/g.test(input);
   }
 }
