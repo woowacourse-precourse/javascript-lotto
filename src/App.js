6386cda7f423cb71523e29c4206ae53979cf220b
hailey-hy <lillyine.hy@gmail.com>
2022-11-10 14:31:06 +0900

    feat(App): 로또 구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+
+  purchaseAmount() {
+    MissionUtils.Console.readLine((answer) => {
+      return answer
+    })
+  }
 }
 
+const app = new App;
+app.play();
 module.exports = App;
