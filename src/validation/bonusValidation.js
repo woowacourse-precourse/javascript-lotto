1c1c2b050e6262e837f71ed490f5f385e856c6c2
Adult96 <djxor007@gmail.com>
2022-11-10 19:00:19 +0900

    feat(bonusValidation): Add bonus number and winning number duplicate check function
    보너스 번호 및 당첨 번호 중복 확인 기능 추가

--- src/validation/bonusValidation.js
+++ src/validation/bonusValidation.js
@@ -2,7 +2,13 @@ function bonusValidation(input, WinningNumber) {
   return true;
 }
 
-function DoubleCheckBonus(input, WinningNumber) {}
+function DoubleCheckBonus(input, WinningNumber) {
+  const includes = WinningNumber.split(',').includes(input);
+
+  if (includes) {
+    throw new Error('[ERROR] 중복되지 않는 숫자만 입력할수 있습니다.');
+  }
+}
 
 function checkBonusOnlyNumber(input) {}
 
