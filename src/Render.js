6969239304ab9aa9ef456303d2edd29380f234c4
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-11 00:34:31 +0900

    refactor(View): 로또 결과에 따라 사용자에게 결과를 출력하는 함수 분리

--- src/Render.js
+++ src/Render.js
@@ -11,5 +11,39 @@ class Render {
       Console.print(madeLotto[i]);
     }
   }
+  lineBreak() {
+    Console.print(``);
+  }
+
+  renderResult(result) {
+    this.lineBreak();
+    Console.print(`---`);
+    this.lineBreak();
+    let benefit = 0;
+    for (let i = 0; i < 5; i++) {
+      switch (this.result[i][0]) {
+        case "5등":
+          benefit = benefit + 5000 * this.result[i][1];
+          Console.print(`3개 일치 (5,000원) - ${this.result[i][1]}개`);
+          break;
+        case "4등":
+          benefit = benefit + 50000 * this.result[i][1];
+          Console.print(`4개 일치 (50,000원) - ${this.result[i][1]}개`);
+          break;
+        case "3등":
+          benefit = benefit + 1500000 * this.result[i][1];
+          Console.print(`5개 일치 (1,500,000원) - ${this.result[i][1]}개`);
+          break;
+        case "2등":
+          benefit = benefit + 30000000 * this.result[i][1];
+          Console.print(`5개 일치 (30,000,000원) - ${this.result[i][1]}개`);
+          break;
+        case "1등":
+          benefit = benefit + 200000000 * this.result[i][1];
+          Console.print(`6개 일치 (2,000,000,000원) - ${this.result[i][1]}개`);
+          break;
+      }
+    }
+  }
 }
 module.exports = Render;
