36a682ddc51568a37b3eb168f832c2d7ad1c1e7e
hyunjin <leehj0110@kakao.com>
2022-11-10 13:57:47 +0900

    refactor: input을 받는 함수 네이밍 개선

--- src/Console.js
+++ src/Console.js
@@ -3,8 +3,8 @@ const { Console } = require("@woowacourse/mission-utils");
 class Console {
   static PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.\n";
 
-  static getPurchaseAmount(callback) {
-    Console.readLine(this.PURCHASE_AMOUNT_MESSAGE, callback);
+  static askAndGetUserInput(question, callback) {
+    Console.readLine(question, callback);
   }
 
   // static checkPurchaseAmount(purchaseAmount) {}
