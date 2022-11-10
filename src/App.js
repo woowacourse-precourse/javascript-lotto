26b8cc1a84ad6d24c42ba121d2a957e7580f0831
gunyongbok <qhrrjsdyd123@gmail.com>
2022-11-10 18:18:19 +0900

    Feat: 구입 금액으로 살 수 있는 로또의 개수와 개수만큼의 로또 출력
    로또를 보여주는 함수를 메서드별로 나누고 문자열 형태로 출력

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
+
 const ONE_LOTTO_PRICE = 1000;
 
 class App {
     play() {
         this.purchasePriceInput();
     }
+
+    // 구입 금액 입력 받는 함수
     purchasePriceInput() {
-        MissionUtils.Console.readLine(
-            '구입금액을 입력해 주세요.\n',
-            (price) => {
-                this.validatePurchasePrice(price);
-                console.log(price);
-            }
+        MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (price) =>
+            this.callBackOfPurchase(price)
         );
     }
+
+    // 구입 금액 입력 받는 함수의 callback
+    callBackOfPurchase(price) {
+        const numberOfLotto = price / ONE_LOTTO_PRICE;
+        let lotto = new Lotto();
+        return (
+            MissionUtils.Console.print(`\n${numberOfLotto}개를 구매했습니다.`),
+            this.validatePurchasePrice(price),
+            lotto.showLotto(numberOfLotto)
+        );
+    }
+
+    // 구입 금액 검증 함수
     validatePurchasePrice(price) {
         if (price % ONE_LOTTO_PRICE !== 0 || price === String(0)) {
             throw '[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.';
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class Lotto {
     #numbers;
 
@@ -5,7 +7,30 @@ class Lotto {
         this.#numbers = numbers;
     }
 
-    // TODO: 추가 기능 구현
+    // 구입금액에 해당하는 만큼의 로또를 보여주는 함수
+    showLotto(numbers) {
+        for (let i = 0; i < numbers; i++) {
+            this.printLotto();
+        }
+    }
+
+    // 로또들을 출력하는 함수
+    printLotto() {
+        MissionUtils.Console.print(this.pickLotto());
+    }
+
+    // 로또들의 숫자를 랜덤하게 뽑는 함수
+    pickLotto() {
+        const pickLottoArray = MissionUtils.Random.pickUniqueNumbersInRange(1,45,6).sort(function (a, b) {
+            if (a > b) return 1;
+            if (a === b) return 0;
+            if (a < b) return -1;
+        });
+        const pickLottoString = pickLottoArray.join(', ');
+        return `[${pickLottoString}]`;
+    }
 }
+
+let lotto = new Lotto();
 
 module.exports = Lotto;
