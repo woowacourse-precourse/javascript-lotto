eba4acc312ad92dc973ec4bfa6d1b756112dfcb1
Suyoung <jsy8492@gmail.com>
2022-11-10 19:37:50 +0900

    refactor(lotto): 로또 번호 유효성 검사를 위한 여러 개의 if문을 switch문으로 리팩토링

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,16 +7,13 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (this.isNotLottoLength(numbers)) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-    }
-
-    if (this.hasDuplicate(numbers)) {
-      throw new Error('[ERROR] 로또 번호는 중복이 없어야 합니다.');
-    }
-
-    if (this.hasOutOfBoundNumber(numbers)) {
-      throw new Error('[ERROR] 로또 번호는 1 이상 45 이하여야 합니다.');
+    switch (true) {
+      case this.isNotLottoLength(numbers):
+        throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+      case this.hasDuplicate(numbers):
+        throw new Error('[ERROR] 로또 번호는 중복이 없어야 합니다.');
+      case this.hasOutOfBoundNumber(numbers):
+        throw new Error('[ERROR] 로또 번호는 1 이상 45 이하여야 합니다.');
     }
   }
 
