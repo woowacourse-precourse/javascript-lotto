b17150be02093b86d0cb932a0a109205d27a5c0a
h2002y <h2002y@naver.com>
2022-11-10 17:57:32 +0900

    refactor: Rename setLottoOverlap func =>  checkLottoOverlap func, delete unneccesary code in src/Lotto.js

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,31 +2,26 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.lottoOverlap = new Map();
-    this.setLottoOverlap(numbers, this.lottoOverlap);
-    this.validate(numbers, this.lottoOverlap);
+    this.checkLottoOverlap(numbers);
+    this.validate(numbers);
     this.#numbers = numbers;
   }
 
-  setLottoOverlap(numbers, lottoOverlap) {
+  checkLottoOverlap(numbers) {
+    const lottoOverlap = new Map();
     for (let i = 0; i < numbers.length; i++) {
       if (!lottoOverlap.has(numbers[i])) {
         lottoOverlap.set(numbers[i], 1);
       }
       else {
-        lottoOverlap.set(numbers[i], lottoOverlap.get(numbers[1]) + 1);
+        throw new Error("[ERROR] 로또 번호는 중복되는 숫자가 없어야 합니다.");
       }
     }
   }
 
-  validate(numbers, lottoOverlap) {
+  validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-    for(let i = 0; i < numbers.length; i++) {
-      if (lottoOverlap.get(numbers[i]) > 1) {
-        throw new Error("[ERROR] 로또 번호는 중복되는 숫자가 없어야 합니다.")
-      }
     }
   }
 
