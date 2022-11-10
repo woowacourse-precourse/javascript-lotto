90e466e2c245d22c15c54c927e98197d434725d0
SeungWoo Kim <osabero@naver.com>
2022-11-10 18:59:22 +0900

    로또번호 생성기능입니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+
 class App {
-  play() {}
+  play() {
+    lottoNumber()
+  }
+}
+
+function lottoNumber () {
+  let numbers = MissionUtils.Console.pickUniqueNumbersInRange(1, 45, 7);
+  const LOTTO_NUMBER = numbers.slice(0,5)
+  const BONUS_NUMBER = numbers.slice(5,)
+  console.log(LOTTO_NUMBER)
+  console.log(BONUS_NUMBER)
 }
 
 module.exports = App;
