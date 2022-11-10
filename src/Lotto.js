227a1a30ffd5f1e8ef1fc9245385cd2554668126
Steadystudy <sgmin206@naver.com>
2022-11-10 15:29:37 +0900

    feat: Lotto 숫자 오름차순으로 리턴 및 예외처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,19 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (!Array.isArray(numbers)) {
+      throw new Error('[ERROR] 배열이 아닙니다.');
+    }
+    if ([...new Set(numbers)].length !== 6) {
+      throw new Error('[ERROR] 로또 번호가 중복되면 안됩니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  show() {
+    return this.#numbers.sort((a, b) => a - b);
+  }
 }
 
 module.exports = Lotto;
