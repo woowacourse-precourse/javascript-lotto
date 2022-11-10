c964d070c5d1d7ec6902b1fe73defc7421b6a881
Woong <jiyko7@kyonggi.ac.kr>
2022-11-10 19:30:04 +0900

    feat: 구입금액 에러 처리
    feat: 구입금액 에러 처리

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ class App {
   play() {
     const MISSIONUTILS = require("@woowacourse/mission-utils");
     MISSIONUTILS.Console.readLine("구입금액을 입력해 주세요.\n", function(input) {
-      var temp = input;
-      MISSIONUTILS.Console.print(temp);
+      var temp = input/1000;
+      purchaseCheck(temp);
     });
   }
 }
+
+function purchaseCheck(input) {
+  if (!Number.isInteger(input)){
+    throw new Error("[ERROR] 1000원 단위로 입력하세요");
+  }
+}
+
 
 var a = new App;
 a.play();
