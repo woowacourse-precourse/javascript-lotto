8c8a6a1c5a9ec63e96a2f18e4bf6c9b3990d1061
myung-j-p <jmpack97@gmail.com>
2022-11-11 02:15:24 +0900

    Feat: 사용자 구입금액 입력 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,34 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    // this.validate(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  // validate(numbers) {
+  //   if (numbers.length !== 6) {
+  //     throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  //   }
+  // }
+
+  getUserPayment() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (userInput) => {
+      MissionUtils.Console.close();
+      return userInput
+    })
   }
 
   // TODO: 추가 기능 구현
 }
+
+
+//>>>>>>>>>>>>UI test<<<<<<<<<<<<<<
+const lotto = new Lotto();
+lotto.getUserPayment();
+
+
 
 module.exports = Lotto;
