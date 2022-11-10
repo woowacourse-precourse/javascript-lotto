10145137ead7e0895028b634f7932d0e7a3fd1dc
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-10 14:38:53 +0900

    refactor: change variable name

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
-const Lotto = require('./Lotto');
 
 class App {
   constructor() {
-    let purchaseAmount = 0;
+    let purchaseNum = 0;
   }
 
   play() {
     this.buyLotto();
   }
+
   buyLotto() {
     Console.readLine('구입금액을 입력해 주세요.', (amount) => {
-      this.purchaseAmount = amount;
-      this.validatePurchaseAmount();
-      Console.print(`${this.purchaseAmount / 1000}개를 구매했습니다.`)
+      this.validatePurchaseAmount(amount);
+      this.purchaseNum = amount / 1000;
+      Console.print(`${this.purchaseNum}개를 구매했습니다.`)
     })
   }
 
-  validatePurchaseAmount() {
-    if (this.purchaseAmount % 1000 !== 0) {
+  validatePurchaseAmount(amount) {
+    if (amount % 1000 !== 0) {
       throw "[ERROR] 로또 구입 금액을 1,000원 단위로 입력 하세요."
     }
   }
