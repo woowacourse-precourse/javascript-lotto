6c8f44d003b4c491b9e28cbc722b5d3a65ac3cf7
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-10 16:21:03 +0900

    feat: 구입금액 입력이 로또가격으로 나누어 떨어지는지 검사하는 메서드 구현

--- src/Validation.js
+++ src/Validation.js
@@ -1,9 +1,16 @@
+const { LOTTO_PRICE } = require("./constants/condition.js");
+
 class Validation {
   static hasOnlyNumber(input, separator = "") {
     return input
       .split(separator)
       .map((eachLetter) => parseInt(eachLetter, 10))
       .every((number) => !isNaN(number));
+  }
+  static isDivisibleByLottoPrice(purchaseMoney) {
+    const remainder = purchaseMoney % LOTTO_PRICE;
+
+    return remainder === 0;
   }
 }
 
