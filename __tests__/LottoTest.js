1d78aa163b1cfb65b476fca1299f89d4440ef24f
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-11-11 00:22:46 +0900

    feat(Lotto): 로또 일치 갯수 계산하기 구현

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,11 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require("../src/Lotto");
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -15,4 +22,65 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test('매개변수로 n을 받고 콘솔에 "n개를 구매했습니다." 출력한다.', () => {
+    const logSpy = getLogSpy();
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const N = 5;
+    const ALERT_MESSAGE = `${N}개를 구매했습니다.`;
+    lotto.alertHowManyBought(N);
+
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(ALERT_MESSAGE));
+  });
+
+  test("구입한 로또 번호의 일치하는 숫자를 반환한다", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    const purchasedNumbers = [
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 7, 8, 9],
+      [4, 6, 34, 12, 23, 44],
+    ];
+
+    const expectResult = [
+      { count: 6, bonus: false },
+      { count: 3, bonus: false },
+      { count: 2, bonus: false },
+    ];
+
+    const matchedCount = purchasedNumbers.map((purchasedNumber) =>
+      lotto.getMatchedCount(purchasedNumber)
+    );
+
+    matchedCount.forEach((matchNumber, index) =>
+      expect(matchNumber).toEqual(expectResult[index])
+    );
+  });
+
+  test("구입한 로또 번호들의 일치하는 숫자를 반환한다", () => {
+    const purchasedAmount = 100000;
+
+    const purchasedNumbers = [
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 7, 8, 9],
+      [6, 5, 4, 3, 10, 11],
+    ];
+
+    const lotto = new Lotto(
+      [1, 2, 3, 4, 5, 6],
+      purchasedAmount,
+      purchasedNumbers
+    );
+
+    const statistics = {
+      match3: 1,
+      match4: 1,
+      match5: 0,
+      match5withBonus: 0,
+      match6: 1,
+    };
+
+    const winStatistics = lotto.getWinStatistics(purchasedNumbers);
+
+    expect(winStatistics).toEqual(statistics);
+  });
 });
--- src/Lotto.js
+++ src/Lotto.js
@@ -2,10 +2,16 @@ const { Console } = require("@woowacourse/mission-utils");
 
 class Lotto {
   #numbers;
+  #bonus;
+  #purchasedAmount;
+  #purchasedNumbers;
 
-  constructor(numbers) {
+  constructor(numbers, purchasedAmount, purchasedNumbers = [], bonus) {
     this.validate(numbers);
     this.#numbers = numbers;
+    this.#purchasedAmount = purchasedAmount;
+    this.#purchasedNumbers = purchasedNumbers;
+    this.#bonus = bonus;
   }
 
   validate(numbers) {
@@ -29,6 +35,49 @@ class Lotto {
   alertPurchaseResult(count, pickedNumbers) {
     this.alertHowManyBought(count);
     this.alertNumberOfPurchases(pickedNumbers);
+  }
+
+  getMatchedCount(userNumbers) {
+    const match = [];
+
+    this.#numbers.forEach((number) => {
+      const existNumber = userNumbers.find(
+        (pickedNumber) => pickedNumber === number
+      );
+      if (existNumber) match.push(existNumber);
+    });
+
+    const bonus = !!userNumbers.find(
+      (userNumber) => userNumber === this.#bonus
+    );
+
+    return { count: match.length, bonus };
+  }
+
+  getWinStatistics() {
+    const statistics = {
+      match3: 0,
+      match4: 0,
+      match5: 0,
+      match5withBonus: 0,
+      match6: 0,
+    };
+
+    const matchedCounts = this.#purchasedNumbers.map((userNumbers) =>
+      this.getMatchedCount(userNumbers)
+    );
+
+    matchedCounts.forEach(({ count, bonus }) => {
+      if (count < 3) return;
+      if (bonus && count === 5) {
+        statistics.match5withBonus = statistics.match5withBonus + 1;
+        return;
+      }
+      const key = `match${count}`;
+      statistics[key] = statistics[key] + 1;
+    });
+
+    return statistics;
   }
 }
 
