ce46e013e155c1dd6609613c89870bea0f1f2fc4
Ryong-E <think8867@gmail.com>
2022-11-10 22:33:47 +0900

    fix(view): 발행된 로또 출력시 배열안에 배열로 출력되는 부분 수정
    - 발권된 로또를 돌면서 값을 가져오고 배열로 반환하는 로직이 맞지않아 forEach로 돌면서 값을 출력하는 것으로 변경

--- src/view.js
+++ src/view.js
@@ -12,12 +12,12 @@ class View {
 
   printLottos(lottos) {
     Console.print(`${lottos.length}개를 구매했습니다.`);
-    Console.print(lottos.map((lotto) => lotto.getNumber()));
+    lottos.forEach((lotto) => Console.print(lotto.getNumber()));
   }
 
   winningNumber() {
     Console.readLine('당첨 번호를 입력해 주세요.', (answer) => {
-      this.controller.setWinningNumber(answer);
+      this.controller.enterWinningNumber(answer);
     });
   }
 }
