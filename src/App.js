0a4aa46554e61eddc3a5ba2e252fd7d36f26cde1
Steadystudy <sgmin206@naver.com>
2022-11-10 21:36:19 +0900

    feat: 로또 번호와 당첨 번호 비교해 당첨 결과 출력하기

--- src/App.js
+++ src/App.js
@@ -42,15 +42,25 @@ class App {
   }
 
   getWinningLotteryNumbers() {
-    MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.', (numbers) => {
-      this.winningLotteryNumbers = numbers;
-      this.getBonusNumber();
-    });
+    MissionUtils.Console.readLine(
+      '당첨 번호를 입력해 주세요.',
+      (inputNumbers) => {
+        const numbers = inputNumbers.toString().split(',').map(Number);
+        if (numbers.length !== 6) {
+          throw new Error('[ERROR] 당첨번호는 6개여야 합니다.');
+        }
+
+        this.winningLotteryNumbers = numbers;
+        this.getBonusNumber();
+      },
+    );
   }
 
   getBonusNumber() {
     MissionUtils.Console.readLine('보너스 번호를 입력해 주세요.', (number) => {
       this.bonusNumber = number;
+      this.compareNumbers();
+      this.computeProfit();
       this.printResult();
     });
   }
@@ -58,7 +68,7 @@ class App {
   printPurchaseList() {
     MissionUtils.Console.print(`${this.lottoAmount}개를 구매했습니다.`);
     this.lottos.forEach((lottoNumbers) => {
-      const a = lottoNumbers.join(',');
+      const a = lottoNumbers.join(', ');
       MissionUtils.Console.print(`[${a}]`);
     });
   }
@@ -71,7 +81,8 @@ class App {
       30000000 * this.result.second +
       2000000000 * this.result.first;
     const purchaseAmount = this.lottoAmount * 1000;
-    this.profit = Math.round((totalWinningPrice / purchaseAmount) * 100) / 100;
+    const profitPercent = (totalWinningPrice / purchaseAmount) * 100;
+    this.profit = Math.round(profitPercent * 100) / 100;
   }
 
   makeLottoNumbers(amount) {
@@ -82,21 +93,53 @@ class App {
     }
   }
 
+  compareNumbers() {
+    this.lottos.forEach((lotto) => {
+      const numbersOfMatch = this.winningLotteryNumbers.filter((num) =>
+        lotto.includes(num),
+      ).length;
+      switch (numbersOfMatch) {
+        case 3:
+          this.result.fifth += 1;
+          break;
+        case 4:
+          this.result.fourth += 1;
+          break;
+        case 5:
+          if (!lotto.includes(this.bonusNumber)) {
+            this.result.third += 1;
+          } else {
+            this.result.second += 1;
+          }
+        case 6:
+          this.result.first += 1;
+          console.log(lotto);
+        default:
+          return;
+      }
+    });
+  }
+
   printResult() {
+    MissionUtils.Console.print('당첨 통계');
+    MissionUtils.Console.print('---');
     MissionUtils.Console.print(`3개 일치 (5,000원) - ${this.result.fifth}개`);
     MissionUtils.Console.print(`4개 일치 (50,000원) - ${this.result.fourth}개`);
     MissionUtils.Console.print(
       `5개 일치 (1,500,000원) - ${this.result.third}개`,
     );
     MissionUtils.Console.print(
-      `5개 일치, 보너스 불 일치 (1,500,000원) - ${this.result.second}개`,
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.result.second}개`,
     );
     MissionUtils.Console.print(
       `6개 일치 (2,000,000,000원) - ${this.result.first}개`,
     );
-    MissionUtils.Console.print(`총 수익률은 ${this.profit}입니다.`);
+    MissionUtils.Console.print(`총 수익률은 ${this.profit}%입니다.`);
     MissionUtils.Console.close();
   }
 }
 
 module.exports = App;
+
+const a = new App();
+a.play();
