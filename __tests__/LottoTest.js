ac9d35c43992ddfd5ae6efedfeb544a6b086591e
Buyoung Chung <sunarol@naver.com>
2022-11-10 14:34:13 +0900

    test: Add divideBudget test

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,5 +1,7 @@
 const Lotto = require("../src/Lotto");
 
+const lotto = new Lotto();
+
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
@@ -12,5 +14,9 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
+  });
+
+  test("구매 금액을 1000으로 나눈다", () => {
+    expect(lotto.divideBudget(8000)).toBe(8);
   });
 });
