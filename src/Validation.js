a9ef20e013687f968a5bbf4422be61792347e010
Tory <88531407+1two13@users.noreply.github.com>
2022-11-10 15:57:19 +0900

    refactor(Validation): MESSAGE => ZERO_REST_MESSAGE로 변수명 변경

--- src/Validation.js
+++ src/Validation.js
@@ -2,8 +2,8 @@ class Validation {
   #notDuplicateMessage = '[ERROR] 중복되지 않는 숫자를 입력해주세요.';
 
   purchaseAmount(money) {
-    const MESSAGE = '[ERROR] 1,000원으로 나누어 떨어지는 금액을 입력해주세요.';
-    if (money % 1000 !== 0) throw new Error(MESSAGE);
+    const ZERO_REST_MESSAGE = '[ERROR] 1,000원으로 나누어 떨어지는 금액을 입력해주세요.';
+    if (money % 1000 !== 0) throw new Error(ZERO_REST_MESSAGE);
   }
 
   winningNumber(numbers) {
