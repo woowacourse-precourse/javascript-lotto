c0a0b948262603c36b641315afa8e9855a7c6d2e
khj0426 <706shin1728@naver.com>
2022-11-10 16:32:23 +0900

    refactor:입력 에러 메시지 수정

--- src/MESSAGE.js
+++ src/MESSAGE.js
@@ -2,8 +2,8 @@ const MESSAGE = {
   USER_BUY_MESSAGE: '구입 금액을 입력해주세요',
   CHECK_VALID_INPUT: /^[0-9]+$/,
   THOUSAND: 1000,
-  INPUT_ERROR_MESSAGE: '숫자를 입력해주세요',
-  INPUT_DIVIDE_MESSAGE: '1000으로 나누어 떨어지는 숫자를 입력해주세요',
+  INPUT_ERROR_MESSAGE: '[ERROR]:1000으로 나누어 떨어지는 숫자를 입력해주세요',
+  USER_INIT_INPUT: '',
 };
 
 module.exports = MESSAGE;
