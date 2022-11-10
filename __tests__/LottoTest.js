14dc842c39a4c0690a1bd4a8ce190d2d9036b1c9
moonelysian <dandyblack@naver.com>
2022-11-10 15:14:26 +0900

    test: 로또 6자리 번호관련 예외처리 테스트 추가
    - 중복 처리
    - 숫자 범위 오류
    - 숫자가 아닌게 섞여있는 오류

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,11 +1,10 @@
 const Lotto = require("../src/Lotto.js");
-const { ERROR } = require("../src/constructor.js");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow(ERROR.OUT_OF_LOTTO_LENGTH);
+    }).toThrow("[ERROR]");
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
@@ -16,4 +15,15 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또 번호에 숫자가 아닌게 있다면 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 'a', 5]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 범위에 맞지 않는 숫자가 있다면 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 90]);
+    }).toThrow("[ERROR]");
+  });
 });
