206f961b97fa1f83d6fca82bf914f9ddf0b40592
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-10 19:12:46 +0900

    Feat : make Print lotto winning result

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
+const { PLACE_MESSAGE, PLACE_ARR, WINNING_PRICE } = require("./constants");
 
 class Lotto {
   #numbers;
@@ -12,7 +13,6 @@ class Lotto {
     this.validateSizeIsSix(numbers);
     this.validateIsDuplicated(numbers);
   }
-  // TODO: 추가 기능 구현
   validateIsDuplicated(numbers) {
     const numbersSet = new Set(numbers);
     if (numbersSet.size !== numbers.length) {
@@ -27,6 +27,63 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
+
+  getPlace(number, isBonus) {
+    if (number === 6) return "first";
+    if (number === 5 && isBonus) return "second";
+    if (number === 5) return "third";
+    if (number === 4) return "forth";
+    if (number === 3) return "fifth";
+    return "boom";
+  }
+
+  compareLottoNumber = (ticket, lottoBonusNumber) => {
+    const includeNumbers = ticket.reduce((acc, cur) => {
+      return acc + (this.#numbers.includes(cur) ? 1 : 0);
+    }, 0);
+    const isBonus = this.#numbers.includes(lottoBonusNumber);
+    return this.getPlace(includeNumbers, isBonus);
+  };
+
+  printEachPlace(place, number) {
+    Console.print(`${PLACE_MESSAGE[place]}${number}개`);
+  }
+
+  checkTicketsNumber = (tickets, lottoBonusNumber, winningTickets) => {
+    tickets.forEach((ticket) => {
+      const place = this.compareLottoNumber(ticket, lottoBonusNumber);
+      winningTickets[place] += 1;
+    });
+  };
+
+  calculateYield(winningTickets) {
+    return PLACE_ARR.reduce((acc, place) => {
+      return acc + winningTickets[place] * WINNING_PRICE[place];
+    }, 0);
+  }
+
+  calculateYieldRate(winningTickets, budget) {
+    const lottoYield = this.calculateYield(winningTickets);
+    const yieldRate = Math.round((lottoYield / budget) * 1000) / 10;
+    return yieldRate;
+  }
+
+  printWinningStat = (tickets, lottoBonusNumber, budget) => {
+    console.log("당첨 통계");
+    console.log("---");
+    const winningTickets = {
+      first: 0,
+      second: 0,
+      third: 0,
+      forth: 0,
+      fifth: 0,
+      boom: 0,
+    };
+    this.checkTicketsNumber(tickets, lottoBonusNumber, winningTickets);
+    PLACE_ARR.map((place) => this.printEachPlace(place, winningTickets[place]));
+    const yieldRate = this.calculateYieldRate(winningTickets, budget);
+    Console.print(`총 수익률은 ${yieldRate}%입니다.`);
+  };
 }
 
 module.exports = Lotto;
