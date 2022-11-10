9ff16809d72a9da980ca1abc7345e871c26e2602
jiho1996 <jhp9270@naver.com>
2022-11-10 22:07:56 +0900

    feat : 금액 입력 예외처리 입력이 없다면 throw

--- src/LottoNumberGenerator.js
+++ src/LottoNumberGenerator.js
@@ -10,6 +10,7 @@ class LottoNumberGenerator {
   }
 
   #isInputNumbersValid(numbers) {
+    if (this.#isInputless(numbers)) throw Error("[ERROR] 입력이 없습니다.");
     if (this.#isNegativeNumber(numbers)) throw Error("[ERROR] 양수만 입력해주세요.");
     if (this.#isNotConsistOnlyNumber(numbers)) throw Error("[ERROR] 숫자만 입력 가능합니다.");
     if (this.#isNotDividedThousand(numbers)) throw Error("[ERROR] 천원 단위 입력을 해주세요.");
@@ -17,6 +18,10 @@ class LottoNumberGenerator {
 
   #isNotDividedThousand(input) {
     return input === "0" || input % 1000 !== 0;
+  }
+
+  #isInputless(input) {
+    return !input;
   }
 
   #isNotConsistOnlyNumber(input) {
