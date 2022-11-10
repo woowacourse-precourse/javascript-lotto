57a46baa0395b82c9345cbc2ca4240ca28a37c76
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-10 17:08:56 +0900

    test: 사용자 입력 유효성 검사 테스트 케이스 추가
    hasOnlyNumber 메서드
    case 3: 입력이 없는 경우

--- __tests__/UserInputValidationTest.js
+++ __tests__/UserInputValidationTest.js
@@ -19,6 +19,15 @@ describe("Validation.hasOnlyNumber", () => {
     expect(result).toBe(false);
   });
 
+  test("input이 없는 경우, false를 반환해야 한다.", () => {
+    // given
+    const input = "";
+    // when
+    const result = Validation.hasOnlyNumber(input);
+    // then
+    expect(result).toBe(false);
+  });
+
   test("input이 모두 숫자일 경우, true를 반환해야 한다.", () => {
     // given
     const input = "8000";
