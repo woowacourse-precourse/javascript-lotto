0981fcdf948233f3a9ca48b9033f5b5ba8753ae6
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-10 11:52:45 +0900

    Refactor: rename, 코드 구조 정리

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,16 +1,16 @@
 const ExceptionCheck = require('./utils/ExceptionCheck');
 const Calculate = require('./model/Calculation');
 const {MONEY} = require('./utils/Constants');
-const printResult = require('./model/Print');
+const printResult = require('./utils/Print');
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.errorCheck = new ExceptionCheck();
+    this.calculate = new Calculate();
     this.validate(numbers);
     this.#numbers = numbers;
-    this.calculate = new Calculate();
   }
 
   validate(numbers) {
@@ -19,9 +19,9 @@ class Lotto {
 
   printOfResultFromCalc(lottos, answers, bonusNum, baseMoney){
     const answerinNum = ans => ans.map(Number);
-    const lotteryArr = (this.calculate.testAllLottos(lottos, answerinNum(answers), bonusNum));
-    const yieldCheck = this.yieldCalculation(lotteryArr, baseMoney);
-    printResult(lotteryArr, yieldCheck);
+    const winLotteryAfterCalc = (this.calculate.testAllLottos(lottos, answerinNum(answers), bonusNum));
+    const rateOfReturn = this.yieldCalculation(winLotteryAfterCalc, baseMoney);
+    printResult(winLotteryAfterCalc, rateOfReturn);
   }
   
   yieldCalculation(arr, baseMoney){
