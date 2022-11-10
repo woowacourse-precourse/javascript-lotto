512eaa764c94753fd2f4c0460157159b1e821ac0
An Sol <apdlvmf486@gmail.com>
2022-11-10 14:58:02 +0900

    feat: 구입 금액만큼 로또를 생성하는 기능 추가

--- src/issueLotto.js
+++ src/issueLotto.js
@@ -9,6 +9,10 @@ class LottoGenerator {
     //문자열 상수화 필요
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (userInput) => {
       const TOTAL_LOTTO_GAMES = parseInt(userInput / 1000);
+      MissionUtils.Console.print(`${TOTAL_LOTTO_GAMES}개를 구매했습니다.`);
+      for (let gameCount = 0; gameCount < TOTAL_LOTTO_GAMES; gameCount++) {
+        MissionUtils.Console.print(this.makeLottoNumber());
+      }
     });
   }
 }
