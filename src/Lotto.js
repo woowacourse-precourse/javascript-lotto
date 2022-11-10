0b9867ffe88f0fb769b2f18092c2e092f0baff51
c0c0pang <coc1789@naver.com>
2022-11-10 18:30:44 +0900

    refactor: 중복 체크 기능 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,24 +1,19 @@
 class Lotto {
   #numbers;
-
   constructor(numbers) {
     this.validate(numbers);
-    this.overlap(numbers);
     this.#numbers = numbers;
   }
 
   validate(numbers) {
+    const overlapNumbers = new Set(numbers);
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-  }
-  overlap(number) {
-    const overlapNumver = new Set(number);
-    if (overlapNumver.size !== number.length){
+    if (overlapNumbers.size !== numbers.length) {
       throw new Error("[ERROR] 로또 번호는 중복이 없습니다.");
     }
   }
-  
   // TODO: 추가 기능 구현
 }
 
