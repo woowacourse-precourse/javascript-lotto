736827a37f817a1f0bad94a6031e2a2db8b255c8
turtle601 <turtle601@naver.com>
2022-11-10 17:40:41 +0900

    refactor(Bonus) Bonus 객체에 enum객체 적용

--- src/Bonus.js
+++ src/Bonus.js
@@ -1,3 +1,5 @@
+const { LOTTO_ERROR_MESSAGE, VARIABLE_LOTTO } = require('../utils/constants');
+
 class Bonus {
   #number;
 
@@ -15,8 +17,8 @@ class Bonus {
   }
 
   #validateLottoRange(number) {
-    if (!/^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$/.test(number)) {
-      throw new Error('[ERROR] 보너스 번호는 1 ~ 45사이 숫자여야 합니다.');
+    if (!VARIABLE_LOTTO.regex.test(number)) {
+      throw new Error(LOTTO_ERROR_MESSAGE.range);
     }
 
     return this;
