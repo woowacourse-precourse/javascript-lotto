46fdf44e1398ba02374b742693f28831ebcbdd49
Buyoung Chung <sunarol@naver.com>
2022-11-10 21:11:42 +0900

    feat: Add function returnHowManyLotto

--- src/Message.js
+++ src/Message.js
@@ -1,9 +1,13 @@
 class Message {
-  static ASK_BUDGET = "구입금액을 입력해 주세요.";
+  static ASK_BUDGET = "구입금액을 입력해 주세요.\n";
 
-  static ASK_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+  static ASK_WINNING_NUMBER = "당첨 번호를 입력해 주세요.\n";
 
-  static ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+  static ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.\n";
+
+  static returnHowManyLotto(count) {
+    return `${count}개를 구매했습니다.`;
+  }
 }
 
 module.exports = Message;
