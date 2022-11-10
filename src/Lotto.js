28be0db4a650dd15e0316d20ff092f71a3ff890e
turtle601 <turtle601@naver.com>
2022-11-10 14:46:00 +0900

    feat(Lotto) lotto 입력 시 중복 숫자 에러 처리 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,20 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    this.#validateOverlap(numbers);
   }
 
   // TODO: 추가 기능 구현
+  #validateOverlap(numbers) {
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error('[ERROR] 로또 번호는 중복이 되어서는 안됩니다.');
+    }
+
+    return this;
+  }
 }
 
 module.exports = Lotto;
