f38799730c35955ee55e3a7f455c6a8b8d30fbbe
Suyoung <jsy8492@gmail.com>
2022-11-10 19:59:10 +0900

    test(lottoManagerTest):  로또 구입 금액에 숫자가 아닌 문자가 존재하는 경우 예외 처리 테스트 작성

--- __tests__/LottoManagerTest.js
+++ __tests__/LottoManagerTest.js
@@ -7,4 +7,11 @@ describe('로또 관리자 클래스 테스트', () => {
       lottoManager.initLottos('1001');
     }).toThrow('[ERROR]');
   });
+
+  test('로또 구입 금액에 숫자가 아닌 문자가 존재하면 예외가 발생한다.', () => {
+    expect(() => {
+      const lottoManager = new LottoManager();
+      lottoManager.initLottos('1000j');
+    }).toThrow('[ERROR]');
+  });
 });
