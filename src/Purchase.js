d2ed12adc07342e6ce93bb64607ec29f9031a8d7
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-10 14:43:51 +0900

    feat: 구매 금액에 대해서 유효성 검사
    - 1000원으로 떨어지지 않으면 에러 처리
    - 양수 값이 아니면 에러 처리

---
+++ src/Purchase.js
@@ -0,0 +1,24 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+class Purchase {
+  #cash;
+
+  #lottoCount;
+
+  constructor(cash) {
+    this.#lottoCount = 0;
+    this.#cash = cash;
+  }
+
+  validateCashInput() {
+    if (this.#cash % 1000 !== 0) {
+      throw new Error('[ERROR] 1,000으로 나누어떨어지는 금액을 입력해주세요.');
+    }
+    if (this.#cash <= 0) {
+      throw new Error('[ERROR] 양수 값을 입력해주세요.');
+    }
+    return this;
+  }
+}
+
+module.exports = Purchase;
