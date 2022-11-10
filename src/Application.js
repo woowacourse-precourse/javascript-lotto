25d41c8e5c50c84ba869741ff206ac46974d24bc
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-10 23:49:29 +0900

    feat(Application): 숫자 예외 검사 함수

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,10 @@
 class Application {
+  static validateNumber(target) {
+    if (Number.isNaN(target)) {
+      throw new TypeError('[ERROR] 전달된 인수는 숫자로 변환이 가능해야 합니다.');
+    }
+  }
+
   static convertNumber(target) {
     const result = parseInt(target, 10);
 
