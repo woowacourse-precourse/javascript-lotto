c2ff0a6dc04236e06c9622973f632703c89ed27d
turtle601 <turtle601@naver.com>
2022-11-10 15:16:35 +0900

    feat(Lotto) 로토 번호가 1 ~ 45 사이가 아닐 시 에러 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,13 +11,25 @@ class Lotto {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
 
-    this.#validateOverlap(numbers);
+    this.#validateOverlap(numbers).#validateLottoRange(numbers);
   }
 
   // TODO: 추가 기능 구현
   #validateOverlap(numbers) {
     if (numbers.length !== new Set(numbers).size) {
       throw new Error('[ERROR] 로또 번호는 중복이 되어서는 안됩니다.');
+    }
+
+    return this;
+  }
+
+  #isLottoRange(number) {
+    return 1 <= number && number <= 45;
+  }
+
+  #validateLottoRange(numbers) {
+    if (numbers.filter(this.#isLottoRange).length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 1 ~ 45사이 숫자여야 합니다.');
     }
 
     return this;
