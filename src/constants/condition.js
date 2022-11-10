bae2caf829adcdb7d70186df3839cfc0afba1cf4
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-11 00:33:29 +0900

    feat: 게임 조건 상수 추가
    1. 로또 번호 최소 범위
    2. 로또 번호 최대 범위
    3. 로또 자릿수

--- src/constants/condition.js
+++ src/constants/condition.js
@@ -1,3 +1,11 @@
 const LOTTO_PRICE = 1000;
+const LOTTO_NUM_MIN_RANGE = 1;
+const LOTTO_NUM_MAX_RANGE = 45;
+const LOTTO_DIGITS = 6;
 
-module.exports = { LOTTO_PRICE };
+module.exports = {
+  LOTTO_PRICE,
+  LOTTO_NUM_MIN_RANGE,
+  LOTTO_NUM_MAX_RANGE,
+  LOTTO_DIGITS,
+};
