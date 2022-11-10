4c4fb294613417ebdcf651cbe3f2ab84c31b1267
myung-j-p <jmpack97@gmail.com>
2022-11-11 04:29:28 +0900

    Style: 에러 출력 메시지에 [ERROR] 문구 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,10 +9,11 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    if (numbers.length !== 6) {throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");}
+    
+
   }
+
 
 
 
@@ -24,8 +25,8 @@ class LottoMaker {
 
   getUserPayment() {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (userInput) => {
-      if (isNaN(userInput) == true) throw new Error('숫자를 입력 해주세요.');
-      if (userInput % 1000 !== 0) throw new Error('1,000원 단위로 입력 해주세요.');
+      if (isNaN(userInput) == true) throw new Error('[ERROR] 숫자를 입력 해주세요.');
+      if (userInput % 1000 !== 0) throw new Error('[ERROR] 1,000원 단위로 입력 해주세요.');
       MissionUtils.Console.close();
       return this.makeLottos(userInput)
     })
