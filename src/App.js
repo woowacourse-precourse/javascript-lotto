9ba006a6d42b52602320ea10d23ae0b486beeaca
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-11-10 19:53:32 +0900

    feat: 당첨번호를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
 
 class App {
   play() {
@@ -6,14 +7,15 @@ class App {
   }
 
   calculateMoney() {
-    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (userInput) => {
+    MissionUtils.Console.print('구입금액을 입력해 주세요.');
+    MissionUtils.Console.readLine('', (userInput) => {
       const LOTTO_PAPER = userInput / 1000;
 
       if (userInput % 1000 !== 0) {
         throw new Error('[ERROR] 1000원 단위로 구입해 주세요.');
       }
 
-      return this.UserLottoNumber(LOTTO_PAPER);
+      this.UserLottoNumber(LOTTO_PAPER);
     });
   }
 
@@ -28,6 +30,21 @@ class App {
       MissionUtils.Console.print(NUMBERS.sort((a, b) => a - b));
       number++;
     }
+    this.winNumber();
+  }
+
+  winNumber() {
+    let winNumberArray;
+
+    MissionUtils.Console.print('\n당첨 번호를 쉼표로 구분하여 입력해 주세요.');
+    MissionUtils.Console.readLine('', (userInput) => {
+      winNumberArray = userInput.split(',').map((item) => +item);
+      const WIN_NUMBER = new Lotto(winNumberArray);
+      WIN_NUMBER.duplicate();
+      WIN_NUMBER.checkNumber();
+    });
+
+    return winNumberArray;
   }
 }
 
