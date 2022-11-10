505b34c2573f43334768e30e3e683d656df1c578
somin <smhs12@naver.com>
2022-11-10 16:59:12 +0900

    feat: 사용자로부터 금액 입력 받는 기능

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     MissionUtils.Console.print("구입금액을 입력해 주세요.");
+    MissionUtils.Console.readLine("", (amount) => {
+      createLottos(amount);
+    });
   }
 }
 
