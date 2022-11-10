8384b811d895615a46873582dab1a97d8dece1dc
jungsmins1 <jungsmins00@gmail.com>
2022-11-10 22:37:19 +0900

    feat: 당첨 번호에 중복된 번호 입력 시 예외 처리 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,8 +1,11 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
+    this.duplicate(numbers);
     this.#numbers = numbers;
   }
 
@@ -13,6 +16,11 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  duplicate(numbers) {
+    if ([...new Set(numbers)] !== 6) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
+  }
 }
 
 module.exports = Lotto;
