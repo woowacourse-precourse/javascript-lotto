15dcae0b2cc36b67c66b26df66b5d97ffcdb6c42
Buyoung Chung <sunarol@naver.com>
2022-11-10 16:27:54 +0900

    feat: Add function inputBonusNumber

--- src/View.js
+++ src/View.js
@@ -1,10 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 const inputBudget = () => {
   MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (budget) => {});
 };
 
-const inputWinningNumbers = () => {
+const inputWinningNumbers = (splitWinningNumbers) => {
   MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.", (numbers) => {});
 };
 
+const inputBounsNumber = () => {
+  MissionUtils.Console.readLine("보너스 번호를 입력해 주세요.", (numbers) => {});
+};
 exports.inputBudget = inputBudget;
+
 exports.inputWinningNumbers = inputWinningNumbers;
