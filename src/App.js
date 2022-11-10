0ebf39e076cb18bf1125cc2592dbfad68572b32f
wontae chang <cwt629@naver.com>
2022-11-10 23:36:59 +0900

    feat: validate the input cost

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+
+  }
+
+  checkCost(cost) {
+    if (isNaN(cost)) {
+      MissionUtils.Console.close();
+      throw new Error("[ERROR] 로또 구입 금액은 숫자여야 합니다.");
+    }
+
+    const COST_IN_NUMBER = parseInt(cost);
+
+    if (COST_IN_NUMBER % 1000 !== 0) {
+      MissionUtils.Console.close();
+      throw new Error("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
+    }
+  }
 }
 
 module.exports = App;
