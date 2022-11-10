df3a15665b4c262b571ec452d310d21237f026ed
MAGHC <windowsapple95@gmail.com>
2022-11-10 20:30:44 +0900

    refactor: Lotto 에러메세지 상수 명 의미 중복이라 생각하여 축약

--- src/Constant.js
+++ src/Constant.js
@@ -6,8 +6,8 @@ const GAME_PROGRESS_MESSAGES = Object.freeze({
 });
 
 const LOTTO_ERROR_MESSAGES = Object.freeze({
-  LOTTO_NUMBER_LENGTH_ONLY_SIX: '[ERROR] 로또 번호는 6개여야 합니다.',
-  LOTTO_NUMBER_ONLY_UNIQUE: '[ERROR] 번호는 중복되지 않아야 합니다',
+  LENGTH_ONLY_SIX: '[ERROR] 로또 번호는 6개여야 합니다.',
+  UNIQUE: '[ERROR] 번호는 중복되지 않아야 합니다',
 });
 
 const UNIT_INVALID_ERROR_MESSAGES = '[ERROR] 1000원 단위로 입력해주세요';
