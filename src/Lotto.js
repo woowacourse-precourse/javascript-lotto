b455d4006cb3dec4603b51f805e5b9fa0cd8b98e
SWARVY <swarvy0826@naver.com>
2022-11-10 16:37:23 +0900

    feat : Lotto 컴포넌트에 테스트용 코드 및 맞춘 개수를 확인하는 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,19 +1,35 @@
+const { PICK_TYPE } = require('./Constants');
+
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
-    this.validate(numbers);
+  constructor(numbers, type) {
+    // this.validate(numbers, type);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
-    // if (numbers.length !== 6) {
-    //   throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-    // }
-    return numbers;
+  validate(numbers, type) {
+    if (numbers.length !== 6 && type === PICK_TYPE.main) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (numbers.length !== 6 && type === PICK_TYPE.bonus) {
+      throw new Error('[ERROR] 보너스 번호는 1개여야 합니다.');
+    }
   }
 
   // TODO: 추가 기능 구현
+  printLotto() {
+    console.log(this.#numbers);
+  }
+
+  checkWin(lotto) {
+    const splitNumber = this.#numbers.split(',');
+    let count = 0;
+    lotto.forEach((item, index) => {
+      if (item === +splitNumber[index]) count += 1;
+    });
+    return count;
+  }
 }
 
 module.exports = Lotto;
