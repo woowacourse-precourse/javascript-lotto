4db5bfc5dc5d240feba02273a2d2a25a8e468f87
wontae chang <cwt629@naver.com>
2022-11-10 23:45:03 +0900

    test: validate the input cost

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,39 @@
 const Lotto = require("../src/Lotto");
+const App = require("../src/App");
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe("입력 예외 발생 테스트", () => {
+  const APP = new App();
+
+  test("입력한 로또 구매 비용이 숫자가 아니면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+      const INPUT = "abc1000";
+      APP.checkCost(INPUT)
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test("입력한 로또 구매 비용이 천 단위가 아니면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
-  });
+      const INPUT = "20500";
+      APP.checkCost(INPUT)
+    }).toThrow("[ERROR]")
+  })
+})
 
-  // 아래에 추가 테스트 작성 가능
-});
+// describe("로또 클래스 테스트", () => {
+//   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+//     expect(() => {
+//       new Lotto([1, 2, 3, 4, 5, 6, 7]);
+//     }).toThrow("[ERROR]");
+//   });
+
+//   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+//   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+//     expect(() => {
+//       new Lotto([1, 2, 3, 4, 5, 5]);
+//     }).toThrow("[ERROR]");
+//   });
+
+//   // 아래에 추가 테스트 작성 가능
+// });
+
+
