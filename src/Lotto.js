b197277bf6f485e2e8fcb71eed2ed68142a7d4d7
kyw0716 <kyw0716@naver.com>
2022-11-11 00:35:33 +0900

    feat: 로또 번호 유효성 검사 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,6 +12,14 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복을 포함할 수 없습니다.");
+    }
+
+    if (numbers.join("").replace(/[0-9]/g, "").length > 0) {
+      throw new Error("[ERROR] 로또 번호는 숫자만 사용할 수 있습니다.");
+    }
   }
 
   get objectName() {
