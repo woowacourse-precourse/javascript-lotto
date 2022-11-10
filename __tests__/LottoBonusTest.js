f3a948844f3b58fe2b7de34083b6eddde8a3cde6
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-10 19:11:30 +0900

    Recpactor : LottoBonusTest - delete check is number fnc

--- __tests__/LottoBonusTest.js
+++ __tests__/LottoBonusTest.js
@@ -1,12 +1,6 @@
 const LottoBonus = require("../src/LottoBonus");
 
 describe("로또 클래스 테스트", () => {
-  test("숫자 말고 다른 값이 들어가면 에러가 발생한다.", () => {
-    expect(() => {
-      new LottoBonus("a1");
-    }).toThrow("[ERROR]");
-  });
-
   test("1~45 사이의 숫자가 나오지 않으면 에러가 발생한다.", () => {
     expect(() => {
       new LottoBonus("46");
