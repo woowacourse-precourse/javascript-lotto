2047bd04579f3646f3274bcb4ec379da62e7e949
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-11-11 03:14:30 +0900

    docs: 예외 처리 추가

--- __tests__/TicketsTest.js
+++ __tests__/TicketsTest.js
@@ -3,12 +3,24 @@ const Tickets = require('../src/Tickets');
 describe('Tickets 클래스 테스트', () => {
   const ticket = Tickets.publish(1);
 
+  test('로또의 Boolean값은 true값이다.', () => {
+    expect(Boolean(ticket)).toBeTruthy();
+  });
+
+  test('로또 번호의 Boolean값은 true값이다.', () => {
+    expect(ticket[0].every(v => Boolean(v))).toBeTruthy();
+  });
+
   test('로또는 숫자로 이루어져 있다.', () => {
-    expect(() => ticket[0].every(v => typeof v === 'number')).toBeTruthy();
+    expect(ticket[0].every(v => typeof v === 'number')).toBeTruthy();
   });
 
   test('로또는 중복되지 않은 숫자로 이루어져 있다.', () => {
-    expect(() => ticket[0].every((v, i, arr) => arr.indexOf(v) === i)).toBeTruthy();
+    expect(ticket[0].every((v, i, arr) => arr.indexOf(v) === i)).toBeTruthy();
+  });
+
+  test('로또 번호는 1 ~ 45의 값이다.', () => {
+    expect(ticket[0].every(v => v >= 1 && v <= 45)).toBeTruthy();
   });
 
   test('로또는 6개의 숫자로 이루어져 있다.', () => {
