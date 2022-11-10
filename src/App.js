f19289fd671a91e77a1ed237f4d6a9087a1194e8
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-10 16:59:43 +0900

    refactor(Controller): 입력을 받은 후 타당성검사 실행이 가능하도록 코드 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+const Lotto = require("./Lotto");
 const LottoGame = require("./LottoGame");
 
 class App {
   play() {}
 
   getUserMoney() {
-    Console.readLine("구매금액을 입력해 주세요.", (money) => {});
+    Console.readLine("구매금액을 입력해 주세요.", (money) => {
+      this.userInputMoney = money;
+      const lotto = new Lotto({ userInputMoney: this.userInputMoney });
+    });
   }
 }
 
