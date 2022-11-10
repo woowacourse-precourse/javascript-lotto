c219e457120c2f5d81cbc9c5a5c6aba990d66a4e
yuu <22yuu@naver.com>
2022-11-11 01:36:43 +0900

    feat: 금액 입력 받기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+
+  constructor() {
+    MissionUtils.Console.print('로또 게임을 시작합니다~!');
+  }
+  
+
+  play() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (input) => {
+      this.onValidation(Number(input));
+    });
+  }
+
+  onValidation(input) {
+    
+    if(typeof input !== 'number' || Number.isNaN(input)) throw new Error('[ERROR] 금액은 1000원 단위로 숫자만 입력해주세요.');
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
