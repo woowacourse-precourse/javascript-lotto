499a4249e07bbe780477a4dada3d3576075e59c1
gunyongbok <qhrrjsdyd123@gmail.com>
2022-11-10 14:25:58 +0900

    Feat: 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-    play() {}
+    play() {
+        this.purchasePriceInput();
+    }
+    purchasePriceInput() {
+        MissionUtils.Console.readLine(
+            '구입금액을 입력해 주세요.\n',
+            (price) => {
+                console.log(price);
+            }
+        );
+    }
 }
 
 let app = new App();
