08d0c64c9cd1c14667cdcafc069fe75ea63d77a2
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-10 19:31:07 +0900

    feat(BonusNumber): 보너스 번호가 1~45 사이의 숫자가 아닐 때 예외처리

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -3,13 +3,13 @@ const BonusNumber = require('../src/BonusNumber');
 describe('보너스 번호 테스트', () => {
   test('보너스 번호가 하나의 숫자가 아니라면 예외가 발생한다.', () => {
     expect(() => {
-      new BonusNumber('');
+      new BonusNumber(Number(''));
     }).toThrow('[ERROR]');
   });
 
   test('보너스 번호가 하나의 숫자가 아니라면 예외가 발생한다.', () => {
     expect(() => {
-      new BonusNumber('헐');
+      new BonusNumber(Number('헐'));
     }).toThrow('[ERROR]');
   });
 
---
+++ src/BonusNumber.js
@@ -0,0 +1,27 @@
+class BonusNumber {
+  #value;
+
+  constructor(value) {
+    this.validate(value);
+    this.#value = value;
+  }
+
+  validate(value) {
+    this.validateIsNumber(value);
+    this.validateNumberRange(1, 45, value);
+  }
+
+  validateIsNumber(value) {
+    if (Number.isNaN(value)) {
+      throw new Error('[ERROR] 로또 번호는 1이상 45이하 입니다.');
+    }
+  }
+
+  validateNumberRange(start, end, number) {
+    if (number < start || number > end) {
+      throw new Error('[ERROR] 로또 번호는 1이상 45이하 입니다.');
+    }
+  }
+}
+
+module.exports = BonusNumber;
