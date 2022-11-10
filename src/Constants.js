e52666d3b22eef92876c95e61e636d049f3370f7
SWARVY <swarvy0826@naver.com>
2022-11-10 16:34:55 +0900

    refactor : Constants 파일의 INGAME_MESSAGE 값 변경

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,13 @@
 const INGAME_MESSAGE = {
   buy: '구입금액을 입력해 주세요.\n',
-  pick: '당첨 번호를 입력해 주세요.\n',
-  bonuspick: '보너스 번호를 입력해 주세요.\n',
+  pickMain: '당첨 번호를 입력해 주세요.\n',
+  pickBonus: '보너스 번호를 입력해 주세요.\n',
   statistic: '당첨 통계\n ---',
 };
 
-module.exports = { INGAME_MESSAGE };
+const PICK_TYPE = {
+  main: 0,
+  bonus: 1,
+};
+
+module.exports = { INGAME_MESSAGE, PICK_TYPE };
