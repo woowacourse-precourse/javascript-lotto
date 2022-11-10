55c8387518332c116b989f9b40fc3de4ec0697d8
Jiwoo Park <jiwoo9446@naver.com>
2022-11-11 03:14:19 +0900

    feat: 로또 번호가 6자리가 아닐경우 예외처리 메서드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,10 +7,12 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6)
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    this.isSixNumber(numbers);
   }
 
+  isSixNumber(numbers) {
+    if (numbers.length !== 6) throw new Error("[ERROR] 입력 숫자가 6개가 아님");
+  }
   // TODO: 추가 기능 구현
 }
 
