7b986f855e066136bdac56adb2ab060d5fa77219
Tory <88531407+1two13@users.noreply.github.com>
2022-11-10 14:23:10 +0900

    refactor(Messages, App): '당첨 번호를 입력해 주세요.' 출력

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 const { Console } = require('@woowacourse/mission-utils');
 const Messages = require('./Messages');
+const LottoSeller = require('./LottoSeller');
 
 class App {
+  constructor() {
+    this.lottoSeller = new LottoSeller();
+  }
+
   play() {
     Console.readLine(Messages.INPUT_MONEY, (money) => {
-      money;
+      this.lottoSeller.resultsForCountAndNumbers(money);
+      this.enterNumbersInRange();
+    });
+  }
+
+  enterNumbersInRange() {
+    Console.readLine(Messages.INPUT_WINNER_NUMBER, (numbers) => {
+      numbers;
     });
   }
 }
