dac072f5546f543ba8903caa530393141c217a8e
GunHoi <lgh509@kyonggi.ac.kr>
2022-11-10 13:27:54 +0900

    feat - 1 : 로또 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
+  insertedMoney;
   play() {}
+  insertMoney(){
+    MissionUtils.Console.readLine('구입금액을 입력해주세요 (1000원 단위)', (insertMoney) => {
+      this.insertedMoney = insertMoney;
+    });
+  }
 }
 
 module.exports = App;
