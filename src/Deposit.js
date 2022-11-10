641adaa9a83de635d6ce922971f5c9e427963add
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-10 20:15:15 +0900

    style(Deposit): 메소드 순서변경

--- src/Deposit.js
+++ src/Deposit.js
@@ -8,18 +8,18 @@ class Deposit {
     this.#amount = amount;
   }
 
-  validate(amount) {
-    if (amount % PRICE_OF_LOTTO) {
-      throw new Error('[ERROR] 로또 구입 금액은 1000원 단위만 가능합니다.');
-    }
-  }
-
   get amount() {
     return this.#amount;
   }
 
   get quantity() {
     return this.#amount / PRICE_OF_LOTTO;
+  }
+
+  validate(amount) {
+    if (amount % PRICE_OF_LOTTO) {
+      throw new Error('[ERROR] 로또 구입 금액은 1000원 단위만 가능합니다.');
+    }
   }
 }
 
