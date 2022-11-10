db67308981aab2eb6aa7d1a31b03c4ee448b73dc
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-10 18:00:31 +0900

    refactor(Model): 유저에게 입력받은 당첨 번호 타당성 검사 함수 분리

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,20 +1,75 @@
 class Lotto {
   #numbers;
 
-  constructor({ userInputMoney }) {
-    this.validate(numbers);
-    this.#numbers = userInputMoney; //Lotto.number를 써야한다는말!
+  constructor(numbers) {
+    this.#numbers = numbers; //Lotto.number를 써야한다는말!
   }
 
-  validate({ userInputMoney }) {
-    if (userInputMoney % 1000 !== 0) {
+  validateMoney(numbers) {
+    if (numbers % 1000 !== 0) {
       throw new Error(`[ERROR] 돈은 1000원 단위로 입력이 가능합니다.`);
     }
-    if (userInputMoney <= 0) {
+    if (numbers <= 0) {
       throw new Error(`[ERROR] 돈은 1000원부터 입력이 가능합니다.`);
     }
   }
-  // TODO: 추가 기능 구현
-}
 
+  validateWinningNum(winningNum) {
+    this.#numbers = winningNum;
+    if (this.checkRange(winningNum) === false) {
+      throw new Error(`[ERROR] 0은 입력이 불가능합니다.`);
+    }
+
+    if (this.checkComma(winningNum) === false) {
+      throw new Error(`[ERROR] ,이 연속으로 입력되었습니다.`);
+    }
+
+    if (this.checkNumLength(winningNum) === false) {
+      throw new Error(`[ERROR] 당첨 번호는 1~45 사이에 있습니다.`);
+    }
+    if (this.checkCount(winningNum) === false) {
+      throw new Error(`[ERROR] 당첨 번호는 6개만 입력이 가능합니다.`);
+    }
+    if (this.checkDuplication(winningNum) === false) {
+      throw new Error(`[ERROR] 당첨 번호에 중복이 있을 수 없습니다.`);
+    }
+  }
+
+  checkRange(arr) {
+    if (/^[0-9]*$/g.test(arr.join("")) === false) {
+      return false;
+    }
+  }
+  checkComma(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === ``) {
+        return false;
+      }
+    }
+  }
+
+  checkNumLength(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > 45 || arr[i] < 1) {
+        return false;
+      }
+    }
+  }
+
+  checkCount(arr) {
+    if (arr.length !== 6) {
+      return false;
+    }
+  }
+
+  checkDuplication(arr) {
+    let checkarr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (checkarr.includes(arr[i])) {
+        return false;
+      }
+      checkarr.push(arr[i]);
+    }
+  }
+}
 module.exports = Lotto;
