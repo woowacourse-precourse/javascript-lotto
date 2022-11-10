dce938253db288d602226175cd53b1a825e237f6
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-11-10 19:51:53 +0900

    feat: 당첨번호를 입력받고 조건에따라 에러를 발생시켜주는 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,25 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  duplicate() {
+    const NOT_DUPLICATE = new Set(this.#numbers);
+    if (NOT_DUPLICATE.size !== 6) {
+      throw new Error('[ERROR] 중복되지 않는 숫자를 입력하여야 합니다.');
+    }
+  }
+
+  checkNumber() {
+    const NUMBER = this.#numbers;
+    for (let index = 0; index < NUMBER.length; index++) {
+      if (Number.isNaN(NUMBER[index]) || NUMBER[index] === 0) {
+        throw new Error('[ERROR] 숫자를 입력하셔야 합니다.');
+      }
+    }
+  }
 }
 
 module.exports = Lotto;
