99b06bf2574f4d36c719c303ca169b2a0b5732dc
Hchan2 <seongchan_@naver.com>
2022-11-10 20:49:48 +0900

    당첨 번호 입력 에러 처리 수정

--- src/input/winningNumbers.js
+++ src/input/winningNumbers.js
@@ -2,21 +2,30 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const { WINNING_NUMBERS } = require('../constant/constant');
 
 const validationNumbers = (numbers) => {
-    if (numbers.length !== 6) {
-        throw new Error("[ERROR] 6자리를 입력해주세요.");
+    if (!numbers.includes(',')) {
+        throw new Error("[ERROR] (,)로 구분하여 6자리 숫자를 입력해주세요.");
     }
 
-    const numberSet = new Set(numbers);
-    if (numberSet.size !== numbers.length) {
-        throw new Error("[ERROR] 중복되지 않은 수를 입력해주세요.");
+    const numberSplit = numbers.split(',');
+    const numberSet = new Set(numberSplit);
+    if (numberSet.size !== 6) {
+        throw new Error("[ERROR] (,)로 구분하여 중복되지 않는 숫자를 입력해주세요.");
+    }
+
+    const commaLength = numberSplit.length - 1;
+    if (commaLength !== 5) {
+        throw new Error("[ERROR] (,)로 구분하여 6자리 숫자를 입력해주세요. (예. 1,2,3,4,5,6)");
     }
 }
 
 const winningNumbers = () => {
+    let userNumberInput = '';
     MissionUtils.Console.readLine(WINNING_NUMBERS, (input) => {
         validationNumbers(input);
-        userBuyAmount = input;
+        userNumberInput = input;
     });
+
+    return userNumberInput;
 }
 
 module.exports = winningNumbers;
