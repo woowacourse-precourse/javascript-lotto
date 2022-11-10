d3696c96b8983346e8baed525da049d597277d6d
jiho1996 <jhp9270@naver.com>
2022-11-10 21:31:13 +0900

    feat : 입력 예외처리 천원 단위가 아니라면 throw

--- src/LottoNumberGenerator.js
+++ src/LottoNumberGenerator.js
@@ -5,6 +5,17 @@ class LottoNumberGenerator {
   inputNumberFromUser() {
     readLine("구입 금액을 입력해주세요.", (input) => {
       this.#inputNumber = input;
+
+      this.isInputNumbersValid(this.#inputNumber);
     });
+  }
+
+  isInputNumbersValid(numbers) {
+    if (this.#isNotDividedThousand(numbers))
+      throw Error("[ERROR] 천원 단위 입력을 해주세요.");
+  }
+
+  #isNotDividedThousand(numbers) {
+    return numbers === "0" || numbers % 1000 !== 0;
   }
 }
