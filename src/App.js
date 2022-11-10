6938d64a9f2a073340ea73c6fb56fcfa98463284
Jiwoo Park <jiwoo9446@naver.com>
2022-11-11 03:09:43 +0900

    feat: 당첨번호 입력 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
 const Seed = require("../src/Seed");
 const LottoGenerator = require("../src/LottoGenerator");
+const Lotto = require("../src/Lotto");
 class App {
   seed;
+  lottoTicket;
+  lotto;
   constructor() {}
   play() {
     this.getInput("구입금액을 입력해 주세요.\n", this.getSeedMoney);
@@ -14,8 +17,13 @@ class App {
 
   getSeedMoney = (input) => {
     this.seed = new Seed(Number(input));
-    this.Lottoticket = new LottoGenerator(this.seed.ticketAmount);
+    this.lottoTicket = new LottoGenerator(this.seed.ticketAmount);
+    this.getInput("\n당첨 번호를 입력해 주세요.\n", this.getWinnerNumber);
   };
+
+  getWinnerNumber(input) {
+    new Lotto(input.split(","));
+  }
 }
 
 const app = new App();
