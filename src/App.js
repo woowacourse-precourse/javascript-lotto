72f2dc316be52b73dabc2fd4a885d417f5abb93d
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-10 20:14:45 +0900

    feat: pick lotto number randomly as many as bought, validate lotto number

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
 
 class App {
   constructor() {
@@ -14,12 +15,25 @@ class App {
       this.validatePurchaseAmount(amount);
       this.purchaseNum = amount / 1000;
       Console.print(`${this.purchaseNum}개를 구매했습니다.`)
+
+      this.pickLottoNumber();
     })
   }
 
   validatePurchaseAmount(amount) {
     if (amount % 1000 !== 0) {
       throw "[ERROR] 로또 구입 금액을 1,000원 단위로 입력 하세요."
+    }
+  }
+
+  pickLottoNumber() {
+    for (let i = 0; i < this.purchaseNum; i++) {
+      let lottoNums = Random.pickUniqueNumbersInRange(1, 45, 6);
+
+      lottoNums = lottoNums.sort(function (a, b) { return a - b });
+
+      let buyLotto = new Lotto(lottoNums);
+      Console.print(buyLotto.getNumbers());
     }
   }
 }
--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,17 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    for(let i=0; i<numbers.length; i++){
+      if(numbers[i] === numbers[i+1]){
+        throw new Error("[ERROR] 로또 번호는 중복되면 안됩니다.")
+      }
+    }
   }
 
   // TODO: 추가 기능 구현
-}
+  getNumbers() {
+    return this.#numbers
+  }
+};
 
 module.exports = Lotto;
