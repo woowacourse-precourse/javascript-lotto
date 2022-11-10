eb085d0fc9702a56c70de5e09da5ccc026803ef9
js43o <js43o@naver.com>
2022-11-10 21:54:03 +0900

    feat(Lotto): add validateNumbers() with checkers

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,17 +2,37 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
-  }
-
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (numbers) {
+      this.validateNumbers(numbers);
+      this.#numbers = numbers;
     }
   }
 
-  // TODO: 추가 기능 구현
+  checkDistinct(numbers) {
+    let set = new Set(numbers);
+    return numbers.length === set.size;
+  }
+
+  checkInRange(number) {
+    const BEGIN = 1;
+    const END = 45;
+
+    return number >= BEGIN && number <= END;
+  }
+
+  validateNumbers(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (!this.checkDistinct(numbers)) {
+      throw new Error('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
+    }
+    numbers.forEach((number) => {
+      if (!/^[0-9]{1,2}$/.test(number) || !this.checkInRange(Number(number))) {
+        throw new Error('[ERROR] 로또 번호는 1부터 45까지의 정수여야 합니다.');
+      }
+    });
+  }
 }
 
 module.exports = Lotto;
