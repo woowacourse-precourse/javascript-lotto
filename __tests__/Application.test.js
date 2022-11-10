5e7f6b070c3425d6989298589a4629980e64cee6
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-10 15:43:10 +0900

    test(Application): 숫자 변환 함수 테스트
    전달받은 인수는 숫자로 변환이 불가능하면 예외를 발생시킨다.

--- __tests__/Application.test.js
+++ __tests__/Application.test.js
@@ -15,8 +15,19 @@ describe('숫자 변환 함수 테스트', () => {
 
   test('전달받은 인수를 숫자로 변환시킨다.', () => {
     expect(Application.convertNumber('111')).toEqual(111);
+
     expect(Application.convertNumber('222')).toEqual(222);
-    expect(Application.convertNumber('333')).toEqual(333);
-    expect(Application.convertNumber('444')).toEqual(444);
+  });
+
+  test('전달받은 인수는 숫자로 변환이 불가능하면 예외를 발생시킨다.', () => {
+    const ERROR_TEXT = '[ERROR] 전달된 인수는 숫자로 변환이 가능해야 합니다.';
+
+    expect(() => {
+      Application.convertNumber('a1a1');
+    }).toThrow(ERROR_TEXT);
+
+    expect(() => {
+      Application.convertNumber('b1b1');
+    }).toThrow(ERROR_TEXT);
   });
 });
