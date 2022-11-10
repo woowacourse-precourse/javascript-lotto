22d0bad460686bd6184935fe0d0d94ff2ff889ff
Woong <jiyko7@kyonggi.ac.kr>
2022-11-10 19:34:43 +0900

    feat: 구매개수 표기
    feat: 구매개수 표기

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ class App {
     MISSIONUTILS.Console.readLine("구입금액을 입력해 주세요.\n", function(input) {
       var temp = input/1000;
       purchaseCheck(temp);
+
+      var arr = new Array(temp);
+      purchaseShow(MISSIONUTILS, temp);
     });
   }
 }
@@ -12,6 +15,11 @@ function purchaseCheck(input) {
   if (!Number.isInteger(input)){
     throw new Error("[ERROR] 1000원 단위로 입력하세요");
   }
+}
+
+function purchaseShow(MISSIONUTILS, input) {
+  MISSIONUTILS.Console.print("");
+  MISSIONUTILS.Console.print(input + "개를 구매했습니다.");
 }
 
 
