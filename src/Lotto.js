eab9f94bdb67e6589e8d20acad74677e692466d7
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-10 16:58:59 +0900

    refactor(Model): 입력값 타당성 검사 (로또를 구매할 돈) 함수를 Lotto Class에 저장

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,17 +1,19 @@
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
+  constructor({ userInputMoney }) {
     this.validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = userInputMoney; //Lotto.number를 써야한다는말!
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  validate({ userInputMoney }) {
+    if (userInputMoney % 1000 !== 0) {
+      throw new Error(`[ERROR] 돈은 1000원 단위로 입력이 가능합니다.`);
+    }
+    if (userInputMoney <= 0) {
+      throw new Error(`[ERROR] 돈은 1000원부터 입력이 가능합니다.`);
     }
   }
-
   // TODO: 추가 기능 구현
 }
 
