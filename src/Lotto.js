c576da47bc1ef02b858839445ca9dc76bcdb7cd1
yuu <22yuu@naver.com>
2022-11-11 01:39:26 +0900

    test: 구입 금액 유효성 검사, 중복, 자리수 초과에 대한 예외처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,23 @@ class Lotto {
 
   constructor(numbers) {
     this.validate(numbers);
-    this.#numbers = numbers;
+    // this.#numbers = numbers;
   }
 
   validate(numbers) {
+  
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    } 
+    
+    if(numbers.length !== new Set(numbers).size) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
+    
   }
 
   // TODO: 추가 기능 구현
 }
+
 
 module.exports = Lotto;
