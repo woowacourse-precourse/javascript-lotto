1ee875af342d4f8d4ffbf6c0d99fef024f4d8090
Rki0 <pky00823@gmail.com>
2022-11-10 13:43:33 +0900

    feat(BonusNumber): add function to check the range of the input
    Throw an Error if it is not between 1 and 45.

--- src/BonusNumber.js
+++ src/BonusNumber.js
@@ -1,6 +1,7 @@
 class BonusNumber {
   constructor(bonus) {
     this.checkOnlyNumber(bonus);
+    this.checkNumberRanges(bonus);
   }
 
   checkOnlyNumber() {
@@ -13,6 +14,14 @@ class BonusNumber {
         throw new Error("[ERROR] 숫자만이 입력 가능합니다.");
       }
     });
+  }
+
+  checkNumberRanges(bonus) {
+    const bonusNumber = Number(bonus);
+
+    if (bonusNumber < 1 || bonusNumber > 45) {
+      throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
   }
 }
 
