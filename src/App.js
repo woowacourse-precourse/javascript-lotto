1f89ea8c180a8bacd4c14f1124a1836855fa99cb
Dahye <06robin11@gmail.com>
2022-11-10 10:26:36 +0900

    feat: generate lotteries according to money

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
-const { INPUT_MESSAGE, ERROR_MESSAGE } = require("./constant/constant");
+const { INPUT_MESSAGE, ERROR_MESSAGE, UNIT } = require("./constant/constant");
+const Lotto = require("./Lotto");
 
 class App {
-  play() {}
+  play() {
+    this.getMoney();
+  }
 
   getMoney() {
     Console.readLine(INPUT_MESSAGE.MONEY, (money) => {
       this.validateMoney(money);
+      this.lotteryQuantity = +money / UNIT.MONEY;
+      const lotteries = this.exchangeLottery(this.lotteryQuantity);
     });
   }
 
@@ -15,6 +20,18 @@ class App {
       throw new Error(ERROR_MESSAGE.WRONG_MONEY);
     }
   }
-}
 
+  exchangeLottery(quantity) {
+    const lotteries = [];
+    for (let i = 0; i < quantity; i++) {
+      lotteries.push(new Lotto(this.generateRandomNumbers()));
+    }
+    return lotteries;
+  }
+
+  generateRandomNumbers() {
+    return Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
+}
+console.log(new App().generateRandomNumbers());
 module.exports = App;
