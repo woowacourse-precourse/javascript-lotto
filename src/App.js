8edcc82910ddd1a9ed67bda841a4a59c5e4a5f77
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-11-10 16:01:59 +0900

    feat : 금액을 적절하게 입력했는지 확인

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ const { MESSAGE } = require('./constants/constant');
 class App {
   play() {
     Console.readLine(MESSAGE.INPUT_MONEY, (inputMoney) => {
-      this.money = inputMoney;
+      this.isValidInputMoney(inputMoney);
     });
+  }
+  isValidInputMoney(input) {
+    this.isNumber(input);
+    this.isDisvisible(input);
+  }
+  isNumber(input) {
+    if (Number.isNaN(parseInt(input, 10))) {
+      throw new Error('숫자를 입력하세요.');
+    }
+  }
+  isDisvisible(input) {
+    if (input % 1000 !== 0) {
+      throw new Error('1000으로 나누어 떨어지도록 입력하세요.');
+    }
   }
 }
 let app = new App();
