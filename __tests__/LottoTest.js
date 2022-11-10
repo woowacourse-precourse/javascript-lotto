ecf17d0454ed75e755d9c4946586c578722fd0df
Steadystudy <sgmin206@naver.com>
2022-11-10 15:31:11 +0900

    test: Lotto 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,29 @@
-const Lotto = require("../src/Lotto");
+const Lotto = require('../src/Lotto');
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR] 로또 번호는 6개여야 합니다.');
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR] 로또 번호가 중복되면 안됩니다.');
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또 번호는 배열로 받아야합니다.', () => {
+    expect(() => {
+      new Lotto(1, 2, 3, 4, 5, 6);
+    }).toThrow('[ERROR] 배열이 아닙니다.');
+  });
+
+  test('로또 번호', () => {
+    const lotto = new Lotto([1, 3, 2, 5, 6, 4]);
+    const result = lotto.show();
+    expect(result).toContainEqual('1', '2', '3', '4', '5', '6');
+  });
+
+  test();
 });
