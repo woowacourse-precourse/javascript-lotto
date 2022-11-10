31cea75e37065b5cec952df1ccf1b220b95ca454
Rki0 <pky00823@gmail.com>
2022-11-10 10:20:21 +0900

    refactor(ApplicationTest): migrate test code for proper unit testing
    App's unit test are written in Lotto's unit test files,
    move them to the appropriate file(ApplicationTest.js).
    
    BREAKING CHANGE: Migrate test code.
    
    Before:
    
    LottoTest.js - unit tests of the App are written
    
    After:
    
    ApplicationTest.js - unit test for App is migrated in ApplicationTest.js

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -23,7 +23,80 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-describe("로또 테스트", () => {
+describe.skip("로또 테스트", () => {
+  test("로또 구매를 1000원 단위로 했을 경우", () => {
+    expect(() => {
+      const app = new App();
+      app.checkPurchaseAmount(8000);
+    }).toBeTruthy();
+  });
+
+  test("로또 구매를 1000원 단위로 하지 않았을 경우", () => {
+    expect(() => {
+      const app = new App();
+      app.checkPurchaseAmount(8500);
+    }).toThrow("[ERROR] 1,000원 단위로만 구매 가능합니다.");
+  });
+
+  test("금액에 따른 로또 생성 횟수 테스트", () => {
+    const app = new App();
+    app.createRandomLotto(8);
+
+    expect(app.lottoArray.length).toBe(8);
+  });
+
+  test("생성된 로또 번호 오름차순 정렬 테스트", () => {
+    const app = new App();
+
+    expect(app.sortLottoNumber([10, 35, 42, 31, 26, 1])).toStrictEqual([
+      1, 10, 26, 31, 35, 42,
+    ]);
+  });
+
+  test("string 타입 입력값이 배열로 변환되는지 테스트", () => {
+    const app = new App();
+
+    expect(app.getArrayedUserInput("1,2,3,4,5,6")).toStrictEqual([
+      "1",
+      ",",
+      "2",
+      ",",
+      "3",
+      ",",
+      "4",
+      ",",
+      "5",
+      ",",
+      "6",
+    ]);
+  });
+
+  test("숫자와 , 기호 이 외의 문자는 에러 처리", () => {
+    expect(() => {
+      const app = new App();
+      app.checkUesrInputHaveOnlyNumberAndComma([
+        "1",
+        ",",
+        "2",
+        ",",
+        "3",
+        ",",
+        "4",
+        ",",
+        "5",
+        ",",
+        " ",
+        "6",
+      ]);
+    }).toThrow("[ERROR] 숫자와 ,(쉼표) 기호만을 입력해주세요.");
+  });
+
+  test("배열 원소가 number 타입으로 변환되어야한다.", () => {
+    const app = new App();
+
+    expect(app.getUserLotto("1,2,3,4,5,6")).toStrictEqual([1, 2, 3, 4, 5, 6]);
+  });
+
   test("기능 테스트", () => {
     mockRandoms([
       [8, 21, 23, 41, 42, 43],
