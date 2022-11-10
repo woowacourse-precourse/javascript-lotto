40851779402d01a8ea861e4e3ccfa3b8dde8df79
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-11-10 23:33:49 +0900

    feat: 로또 구입금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const { readLine } = require('./ui');
+
 class App {
-  play() {}
+  constructor() {
+    this.purchaseAmount = 0;
+  }
+
+  play() {
+    this.inputPurchaseAmount();
+  }
+
+  inputPurchaseAmount() {
+    readLine('구입금액을 입력해 주세요.\n', (amount) => {
+      this.purchaseAmount = amount;
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
