1afaec9e41401ae9156b362466e5c348ee94e5cf
SWARVY <swarvy0826@naver.com>
2022-11-10 14:29:35 +0900

    feat : 구매자의 로또 정보를 관리하는 Consumer 파일 생성

---
+++ src/Consumer.js
@@ -0,0 +1,31 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+
+class Consumer {
+  #LotteryList = [];
+
+  constructor(money) {
+    // this.validate(money);
+    this.createConsumer(money);
+  }
+
+  validate(money) {}
+
+  createConsumer(money) {
+    // 천원 당 1장
+    const amount = money / 1000;
+
+    // 추후 반복문 수정 할 것
+    for (let count = 1; count < amount; count += 1) {
+      const number = Random.pickUniqueNumbersInRange(1, 45, 6);
+      this.#LotteryList.push(number);
+    }
+  }
+
+  printConsumer() {
+    this.#LotteryList.forEach((lotto) => {
+      Console.print(lotto);
+    });
+  }
+}
+
+exports.module = Consumer;
