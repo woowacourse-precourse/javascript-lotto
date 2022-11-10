5d534760bcf3d23da130dd8010784c6cc765f74c
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2022-11-11 00:28:01 +0900

    feat: 유저로부터 구입금액을 입력받는다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.buyLotteryTicket();
+  }
+
+  buyLotteryTicket() {
+    Console.readLine("구입금액을 입력해 주세요.", (money) => {
+      Console.print(money);
+      Console.close();
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
