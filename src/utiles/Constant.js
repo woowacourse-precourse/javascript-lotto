99400f36127ded66cc8255d825284fe26b15d690
pakxe <pigkill40@naver.com>
2022-11-11 04:09:18 +0900

    feat(constant): ERROR.COUNT 내용 수정

--- src/utiles/Constant.js
+++ src/utiles/Constant.js
@@ -25,7 +25,7 @@ const ERROR = Object.freeze({
   PREFIX: '[ERROR] ',
   RANGE: '로또 번호는 min부터 max 사이의 숫자여야 합니다.',
   MONETARY_UNIT: ' 로또는 1,000원 단위로만 구매할 수 있습니다.',
-  COUNTS: `당첨 번호는 ${LOTTO.COUNT}개로 입력해주세요.`,
+  COUNTS: `로또 번호는 ${LOTTO.COUNT}개여야 합니다.`,
   NUMBER_ONLY: '숫자만 입력해주세요.',
   COMMA: '쉼표로 구분해 입력해주세요.',
   DUPLICATION: '중복된 번호는 입력할 수 없습니다.',
