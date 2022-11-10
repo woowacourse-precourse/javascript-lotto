d5e8d924ff643788c3817bf6457d560338982fd7
Buyoung Chung <sunarol@naver.com>
2022-11-10 14:37:32 +0900

    feat: Add function inputWinningNumbers

--- src/View.js
+++ src/View.js
@@ -2,4 +2,8 @@ const inputBudget = () => {
   MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (budget) => {});
 };
 
+const inputWinningNumbers = () => {
+  MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.", (numbers) => {});
+};
+
 exports.inputBudget = inputBudget;
