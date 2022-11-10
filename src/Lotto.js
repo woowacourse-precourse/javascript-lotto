cbcbbc29fef13fb5ac50fc41e6e58b05e790801a
jongwonp <jw032876@gmail.com>
2022-11-10 16:19:20 +0900

    test: 당첨번호로 숫자만 입력받기 테스트 완료

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,13 +9,21 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+      throw new Error(
+        '[ERROR] 로또 번호는 6개여야 합니다. 쉼표(,)로 구분해주세요.'
+      );
     }
 
     if (isDuplicated(numbers)) {
       throw new Error('[ERROR] 중복된 번호가 없어야 합니다.');
     }
     Console.print('중복되지 않은 6개의 번호 입니다.');
+
+    numbers.forEach((number) => {
+      if (isNaN(Number(number))) {
+        throw new Error('[ERROR] 다른 형식이 아닌 숫자만 입력해 주세요.');
+      }
+    });
   }
 }
 
