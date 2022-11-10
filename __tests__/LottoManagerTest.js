61591310d4499e551d29174151a235f4b8034a3c
Suyoung <jsy8492@gmail.com>
2022-11-10 19:58:36 +0900

    test(lottoManagerTest): 로또 구입 금액이 1,000으로 나누어 떨어지지 않는 경우 예외 처리 테스트 작성

--- __tests__/LottoManagerTest.js
+++ __tests__/LottoManagerTest.js
@@ -1,3 +1,10 @@
 const LottoManager = require('../src/LottoManager');
 
-describe('로또 관리자 클래스 테스트', () => {});
+describe('로또 관리자 클래스 테스트', () => {
+  test('로또 구입 금액이 1,000으로 나누어 떨어지지 않으면 예외가 발생한다', () => {
+    expect(() => {
+      const lottoManager = new LottoManager();
+      lottoManager.initLottos('1001');
+    }).toThrow('[ERROR]');
+  });
+});
