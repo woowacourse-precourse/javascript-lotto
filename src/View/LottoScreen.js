c40f594d4940039775a02a4f3017679d30df3c4c
powercording <adoim@naver.com>
2022-11-10 21:43:36 +0900

    Feat: 생성된 모든 로또 번호 출력

--- src/View/LottoScreen.js
+++ src/View/LottoScreen.js
@@ -2,20 +2,27 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const MESSAGE = require("./Message");
 const Validate = require("../Validate");
 const LottoApp = require("../Domain/LottoApp");
+const CONSTANT = require("../constant");
 
 class LottoScreen {
-  lottApp;
+  lottoApp;
 
   static insertMoney() {
     MissionUtils.Console.readLine(MESSAGE.INSERT_MONEY, (money) => {
       Validate.money(money);
       LottoScreen.lottoApp = new LottoApp(money);
-      // console.log(lottoApp.getLottos()[0].getNumbers());
+      LottoScreen.printBuyingAmount(money / CONSTANT.LOTTO_PRICE);
     });
   }
 
-  static printBuyAmount(amount) {
+  static printBuyingAmount(amount) {
     MissionUtils.Console.print(amount + MESSAGE.BUY_AMOUNT);
+    LottoScreen.printLottoNumbers();
+  }
+
+  static printLottoNumbers() {
+    const lottos = LottoScreen.lottoApp.getLottos();
+    lottos.forEach((lotto) => MissionUtils.Console.print(lotto.getNumbers()));
   }
 }
 
