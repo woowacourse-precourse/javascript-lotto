3c58caadc191cf530a43f78f73fda90ae6542267
Sijun <sionwer4@naver.com>
2022-11-10 17:58:44 +0900

    feat: Lotto exception check function add

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,6 +3,9 @@ class Lotto {
 
   constructor(numbers) {
     this.validate(numbers);
+    this.overlap(numbers);
+    this.character(numbers);
+    this.range(numbers);
     this.#numbers = numbers;
   }
 
@@ -13,6 +16,28 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  overlap(numbers) {
+    const set = new Set(numbers);
+    if(numbers.length !== set.size){
+      throw new Error("[ERROR] 로또 번호는 서로 다른 숫자여야 합니다.");
+    }
+  }
+
+  character(numbers) {
+    for(let i = 0; i < numbers.length; i++){
+      if(typeof(numbers[i]) != 'number'){
+        throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+      }
+    }
+  }
+
+  range(numbers){
+    for(let i = 0; i < numbers.length; i++){
+      if(numbers[i] > 45 || numbers[i] < 1){
+        throw new Error("[ERROR] 로또 번호는 1에서 45사이의 숫자여야 합니다.");
+      }
+    }
+  }
 }
 
 module.exports = Lotto;
