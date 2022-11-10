12d22ed6db66ccc0b1dda8ef3c667b7615cb9ff7
Rki0 <pky00823@gmail.com>
2022-11-10 10:52:34 +0900

    feat(Lotto): add function to check for duplicate values
    Add function that attempts to remove duplicate values and throws an Error if the length of the array changes.

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,6 +4,7 @@ class Lotto {
   constructor(numbers) {
     this.validate(numbers);
     this.checkNumberRanges(numbers);
+    this.checkNoSameNumber(numbers);
     this.#numbers = numbers;
   }
 
@@ -19,6 +20,16 @@ class Lotto {
         throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
       }
     });
+
+    return true;
+  }
+
+  checkNoSameNumber(numbers) {
+    const arrayRemovedDuplicate = new Set(numbers);
+
+    if (arrayRemovedDuplicate.size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 모두 다른 숫자여야 합니다.");
+    }
 
     return true;
   }
