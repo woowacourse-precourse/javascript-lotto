7dff66870ffff8c54be0e0f2b6f3373324252303
Sallyvely <tpdus4159@naver.com>
2022-11-10 18:25:33 +0900

    feat : 당첨 내역을 계산하는 기능 구현

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -15,5 +15,26 @@ const calWinning= ()=> {
     for(let count=0;count<lottoArray.size();count++){
         result.push(JSON.stringify(lottoArray)===JSON.stringify(inputLottoNumber));
     }
+    return result;
 }
+
+const winningScore=(calWinning)=>{
+    let count=[];
+    for(let index=0;index<createLotto().size();index++){
+        if(calWinning().result[i]==6){
+            count[6]+=1;
+        }
+        if(calWinning().result[i]==5){
+            count[5]+=1;
+        }
+        if(calWinning().result[i]==4){
+            count[4]+=1;
+        }
+        if(calWinning().result[i]==3){
+            count[3]+=1;
+        }
+    }
+    }
+
+
 module.exports=createLotto;
