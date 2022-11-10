1cc28ffdbf48e2deeb85490d048e8e50db8c749a
Leejin-Yang <ejin1996@gmail.com>
2022-11-10 14:31:21 +0900

    feat: 로또 구입 금액 입력 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { GAME_MESSAGE } = require('./constants/lotto');
+
 class App {
-  play() {}
+  play() {
+    this.readMoney();
+  }
+
+  readMoney() {
+    Console.readLine(GAME_MESSAGE.MONEY_INPUT, (money) => {
+      App.#validateMoney(money);
+      Console.print(money);
+      Console.close();
+    });
+  }
+
+  static #validateMoney(money) {
+    if (money === '') {
+      throw new Error('[ERROR] 빈 값을 입력하였습니다.');
+    }
+
+    if (money.includes(' ')) {
+      throw new Error('[ERROR] 공백을 포함해 입력하였습니다.');
+    }
+
+    if (isNaN(money)) {
+      throw new Error('[ERROR] 구입금액은 숫자여야 합니다.');
+    }
+
+    if (Number(money) % 1000 !== 0) {
+      throw new Error('[ERROR] 구입금액은 1,000의 배수여야 합니다.');
+    }
+  }
 }
 
 module.exports = App;
