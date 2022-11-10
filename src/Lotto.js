41d8292739da90844abc6a75aef1c725b4fe86b9
wontae chang <cwt629@naver.com>
2022-11-10 23:54:07 +0900

    feat: check if the input lotto number is in valid range 1-45

--- src/Lotto.js
+++ src/Lotto.js
@@ -17,6 +17,18 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
+
+  checkEachElement(numbers) {
+    for (let element of numbers) {
+      if (isNaN(element)) {
+        throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+      }
+      const ELEMENT_IN_NUMBER = parseInt(element);
+      if (ELEMENT_IN_NUMBER < 1 || ELEMENT_IN_NUMBER > 45) {
+        throw new Error("[ERROR] 로또 번호는 1-45 사이 숫자여야 합니다.");
+      }
+    }
+  }
 }
 
 module.exports = Lotto;
