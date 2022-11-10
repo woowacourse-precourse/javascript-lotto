be671a674e417c4fa03c04ec844787135740dcc9
woo-jk <201702042@o.cnu.ac.kr>
2022-11-11 04:51:38 +0900

    feat: LottoMachine 클래스 출력 함수 구현

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -5,18 +5,18 @@ class LottoMachine {
   #money;
   #lottos;
 
-  constructor(money){
+  constructor(money) {
     this.validate(money);
     this.#money = Number(money);
     this.#lottos = this.generateLottos();
   }
 
   validate(money) {
-    if(!Number(money)) {
+    if (!Number(money)) {
       throw new Error("[ERROR] 금액은 숫자만 입력해야 합니다.");
     }
-    if(Number(money) % 1000 !== 0 ){
-      throw new Error("[ERROR] 금액은 1,000원 단위만 입력 가능합니다.")
+    if (Number(money) % 1000 !== 0) {
+      throw new Error("[ERROR] 금액은 1,000원 단위만 입력 가능합니다.");
     }
   }
 
@@ -30,9 +30,13 @@ class LottoMachine {
 
   generateLottos() {
     const lottosCount = this.#money / 1000;
-    return Array.from({length:lottosCount}, ()=>this.generateLotto());
+    return Array.from({ length: lottosCount }, () => this.generateLotto());
   }
-  
+
+  print() {
+    MissionUtils.Console.print(`${this.#lottos.length}개를 구매했습니다.`);
+    this.#lottos.forEach((lotto) => lotto.print());
+  }
 }
 
 module.exports = LottoMachine;
