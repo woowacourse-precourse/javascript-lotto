b91c2d200db1f9dddcc27d97a41def5de52b935a
pakxe <pigkill40@naver.com>
2022-11-11 05:22:11 +0900

    test(shopTest): invalidLegnth 메서드 테스트 코드 작성

---
+++ __tests__/ShopTest.js
@@ -0,0 +1,10 @@
+const Shop = require('../src/Shop');
+
+describe('로또판매점 클래스 테스트', () => {
+  test('1000원 단위로 입력하지 않으면 예외 발생', () => {
+    expect(() => new Shop(1500)).toThrow('[ERROR]');
+  });
+  test('올바르게 1000원 단위로 입력', () => {
+    expect(() => new Shop(12000)).toBeTruthy();
+  });
+});
