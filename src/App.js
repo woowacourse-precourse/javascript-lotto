6c77a7031c23e8095b70b49fc2fb9d4a94f21729
“somm12” <sominan8744@gmail.com>
2022-11-11 06:45:55 +0900

    feat: 당첨번호 6개 입력받기 기능 구현완료

--- src/App.js
+++ src/App.js
@@ -45,7 +45,26 @@ class App {
       this.boughtLottos.push(numbers);
     }
     MissionUtils.Console.print(this.boughtLottos);
+    this.inputWinningNumbers();
   }
+  // 당첨 번호 입력 받기
+  // 이때 new Lotto 생성하자.
+
+  inputWinningNumbers() {
+    const WORD_TO_PRINT = "당첨 번호를 입력해주세요.\n";
+    let winngNumbers;
+    MissionUtils.Console.readLine(WORD_TO_PRINT, (numbers) => {
+      winngNumbers = numbers.split(",").map(Number);
+    });
+  }
+
+  // 보너스 번호 입력 받기, playfh return: lotto에서 정의.
+
+  // 비교하는 거 lotto에서 정의(play에서 보너스 번호랑, 로또 2차원배열 전달)
+  // 그리고 개수 반환받아서 통계 배열에 push
+
+  // play가 통계 배열을 lotto로 전달
+  // lotto에서 통계 출력하는 함수 정의
 }
 
 const app = new App();
