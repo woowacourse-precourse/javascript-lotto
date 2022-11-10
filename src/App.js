0aa4a648ac094912a3124b02fd8e220ed26ed59f
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-11-10 15:49:56 +0900

    feat : 로또 구입금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+const { MESSAGE } = require('./constants/constant');
 class App {
-  play() {}
+  play() {
+    Console.readLine(MESSAGE.INPUT_MONEY, (inputMoney) => {
+      this.money = inputMoney;
+    });
+  }
 }
-
+let app = new App();
+app.play();
 module.exports = App;
