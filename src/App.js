f51218e92bd1153eaa70c897d5246d751fb19c41
TaeeunKim <xodms0309@naver.com>
2022-11-10 17:53:30 +0900

    feat: 사용자가 구매한 개수에 따라 로또 번호 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-const { Console } = require('@woowacourse/mission-utils');
 const Store = require('./Store');
+const { Console } = require('@woowacourse/mission-utils');
+const { createLottoNumbers } = require('./utils/lottoUtils');
 class App {
   play() {
     this.buyLotto();
@@ -9,7 +10,18 @@ class App {
     Console.readLine('구입금액을 입력해 주세요.', (money) => {
       console.log(money);
       const store = new Store(money);
+      const lottoBundle = this.getLotto(money);
     });
+  }
+
+  getLotto(money) {
+    let count = money / 1000;
+    const lottoBundle = [];
+    while (count--) {
+      const lotto = createLottoNumbers();
+      lottoBundle.push(lotto);
+    }
+    return lottoBundle;
   }
 }
 
