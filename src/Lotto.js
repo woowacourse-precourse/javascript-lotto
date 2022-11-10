651dd2584b864582dd853ff667ffce16faa2eb21
Leejin-Yang <ejin1996@gmail.com>
2022-11-10 20:59:32 +0900

    feat: 로또 당첨 번호 예외 처리 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,14 +1,28 @@
+const { RULE } = require('./constants/lotto');
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    Lotto.#validate(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
+  static #validate(numbers) {
+    if (numbers.some((number) => Number.isNaN(number))) {
+      throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+    }
+
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+
+    if (numbers.some((number) => number < RULE.RANGE_START || number > RULE.RANGE_END)) {
+      throw new Error('[ERROR] 로또 번호는 1-45 사이의 수여야 합니다.');
+    }
+
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error('[ERROR] 중복된 수를 입력하였습니다.');
     }
   }
 
