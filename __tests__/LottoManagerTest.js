fd5c5e8fb1d99114ffe4d5b23d05567815c3d811
Suyoung <jsy8492@gmail.com>
2022-11-10 20:06:42 +0900

    test(lottoManagerTest): 유효한 로또 구입 금액인 경우 구입 금액에 해당하는 만큼 로또 발행 테스트 작성
    - 무작위 로또 번호 생성은 Mock Function을 사용하여 테스트하기 용이하도록 개선

--- __tests__/LottoManagerTest.js
+++ __tests__/LottoManagerTest.js
@@ -1,5 +1,7 @@
 const LottoManager = require('../src/LottoManager');
 
+const mockCreateLottoNumbers = () => [1, 2, 3, 4, 5, 6];
+
 describe('로또 관리자 클래스 테스트', () => {
   test('로또 구입 금액이 1,000으로 나누어 떨어지지 않으면 예외가 발생한다', () => {
     expect(() => {
@@ -13,5 +15,13 @@ describe('로또 관리자 클래스 테스트', () => {
       const lottoManager = new LottoManager();
       lottoManager.initLottos('1000j');
     }).toThrow('[ERROR]');
+  });
+
+  test('유효한 로또 구입 금액인 경우 구입 금액에 해당하는 만큼의 로또를 발행한다.', () => {
+    const lottoManager = new LottoManager();
+    lottoManager.createLottoNumbers = jest.fn(mockCreateLottoNumbers);
+    lottoManager.initLottos('8000');
+
+    expect(lottoManager.lottos).toHaveLength(8000 / 1000);
   });
 });
