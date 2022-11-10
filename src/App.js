d39dd5e981f8df55ce643ce723999da6e3bbfbc4
GunHoi <lgh509@kyonggi.ac.kr>
2022-11-10 13:35:02 +0900

    feat - 1 : 로또 구입 금액 예외처리(1000원 단위가 아닌 경우)

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ class App {
   play() {}
   insertMoney(){
     MissionUtils.Console.readLine('구입금액을 입력해주세요 (1000원 단위)', (insertMoney) => {
+      insertMoneyValidCheck(insertMoney);
       this.insertedMoney = insertMoney;
     });
+  }
+  insertMoneyValidCheck(insertMoney) {
+    if (insertMoney % 1000 !== 0) {
+      throw new Error("[ERROR] 1000원 단위로 금액을 투입해주세요.");
+    }
   }
 }
 
