dfeaeb730675a4eff65f399d36b5c4145049488f
moonelysian <dandyblack@naver.com>
2022-11-10 21:09:22 +0900

    feat: Lotto class 내 checkRank 함수 추가
    - 당첨번호와 몇개가 동일한지 count에 저장
    - 보너스번호 일치하는지 확인
    - 당첨 기준과 비교해 몇등인지 반환

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,4 @@
-const { LOTTO, ERROR } = require('./constructor.js');
+const { LOTTO, ERROR, WINNING_CRITERIA } = require('./constructor.js');
 const { throwErrorMessage } = require('./utils.js');
 
 class Lotto {
@@ -34,6 +34,29 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
+
+  isWin(criteria, count, bonusCount) {
+    return criteria.equals === count && criteria.bonus === bonusCount;
+  }
+
+  checkRank(winnigNumbers, bonusNumber) {
+    let count = 0;
+    let bonus = this.#numbers.includes(bonusNumber) ? 1 : 0;
+    
+    for (const number of winnigNumbers) {
+      if (this.#numbers.includes(number)) count += 1;
+    }
+
+    for (let key in WINNING_CRITERIA) {
+      const criteria = WINNING_CRITERIA[key];
+      if (this.isWin(criteria, count, bonus)) return criteria.rank;
+    }
+
+    return 0;
+  }
 }
 
 module.exports = Lotto;
