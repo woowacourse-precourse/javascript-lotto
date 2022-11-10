37757be7a7891de684185332ba8877be99e9e5bf
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-10 13:14:54 +0900

    refactor: LottoNumberValidator.test.js 클래스명 NumberValidator를 LottoNumberValidator로 수정

--- src/Components/LottoNumberValidator/LottoNumberValidator.test.js
+++ src/Components/LottoNumberValidator/LottoNumberValidator.test.js
@@ -1,14 +1,14 @@
-const NumberValidator = require('./NumberValidator');
+const LottoNumberValidator = require('./LottoNumberValidator');
 
-describe('NumberValidator 테스트', () => {
+describe('LottoNumberValidator 테스트', () => {
   test('로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.', () => {
-    expect(() => NumberValidator.execute(0)).toThrow(
+    expect(() => LottoNumberValidator.execute(0)).toThrow(
       new Error('[ERROR] 로또 번호는 1부터 45까지의 수만 가능합니다.')
     );
   });
 
   test('로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.', () => {
-    expect(() => NumberValidator.execute(46)).toThrow(
+    expect(() => LottoNumberValidator.execute(46)).toThrow(
       new Error('[ERROR] 로또 번호는 1부터 45까지의 수만 가능합니다.')
     );
   });
