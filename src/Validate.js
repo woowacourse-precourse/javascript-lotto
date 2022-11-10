4cc4f93f2a6d1d4c4048ace44155b942c78738a5
MAGHC <windowsapple95@gmail.com>
2022-11-10 20:39:42 +0900

    refactor: 기존 validate 함수들 thorow 에러 처리

--- src/Validate.js
+++ src/Validate.js
@@ -1,16 +1,28 @@
+const {
+  UNIT_INVALID_ERROR_MESSAGES,
+  LOTTO_NUMBER_RANGE,
+  COMMON_INVALID_ERROR_MESSAGES,
+} = require('./Constant');
+
 const validate = {
   validateCommon(input) {
-    if (typeof +input !== 'number' || Number.isNaN(input)) return false;
-    if (Math.sign(+input) === -1 || !Number.isInteger(+input)) return false;
-    if (input === '') return false;
-    if (+input > 45 || +input === 0) return false;
+    if (typeof +input !== 'number' || Number.isNaN(input)) {
+      throw new Error(COMMON_INVALID_ERROR_MESSAGES.ONLY_NUMBER);
+    }
+    if (Math.sign(+input) === -1 || !Number.isInteger(+input)) {
+      throw new Error(COMMON_INVALID_ERROR_MESSAGES.NOT_DECIMAL_AND_MINUS);
+    }
+    if (input === '') throw new Error(COMMON_INVALID_ERROR_MESSAGES.NOT_EMPTY);
+    if (+input > LOTTO_NUMBER_RANGE.MAX_RANGE || +input === 0) {
+      throw new Error(COMMON_INVALID_ERROR_MESSAGES.ONLY_NUMBER_BETWEEN_1_45);
+    }
     return true;
   },
 
   validateUnit(input) {
-    if (input % 1000 !== 0) return false;
+    if (input % 1000 !== 0) throw new Error(UNIT_INVALID_ERROR_MESSAGES);
     return true;
   },
 };
 
-console.log(validate.validateCommon(46));
+console.log(validate.validateCommon(''));
