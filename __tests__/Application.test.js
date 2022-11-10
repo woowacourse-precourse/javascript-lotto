73e6260975f26aee307ec45dd3f0ea301454d7de
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-10 15:36:26 +0900

    test(Application): 숫자 변환 함수 테스트
    전달받은 인수를 숫자로 변환시킨다.

--- __tests__/Application.test.js
+++ __tests__/Application.test.js
@@ -12,4 +12,11 @@ describe('숫자 변환 함수 테스트', () => {
 
     expect(Application.convertNumber.name).toEqual(METHOD_NAME);
   });
+
+  test('전달받은 인수를 숫자로 변환시킨다.', () => {
+    expect(Application.convertNumber('111')).toEqual(111);
+    expect(Application.convertNumber('222')).toEqual(222);
+    expect(Application.convertNumber('333')).toEqual(333);
+    expect(Application.convertNumber('444')).toEqual(444);
+  });
 });
