5d21e14953d0dc324cf4fdb3143241c06751f8ab
myung-j-p <jmpack97@gmail.com>
2022-11-11 04:22:10 +0900

    Style: LottoMaker 클래스 분리
    금액을 입력받고 로또번호 생성하는 파트를 Lotto에서 LottoMaker 클래스로 분리

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,16 +4,23 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    // this.validate(numbers);
+    this.validate(numbers);
     this.#numbers = numbers;
   }
 
-  // validate(numbers) {
-  //   if (numbers.length !== 6) {
-  //     throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-  //   }
-  // }
+  validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  }
 
+
+
+  // TODO: 추가 기능 구현
+}
+
+class LottoMaker {
+  constructor() {}
 
   getUserPayment() {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (userInput) => {
@@ -51,14 +58,15 @@ class Lotto {
       MissionUtils.Console.print(lotto);
     });
   }
-  
-  // TODO: 추가 기능 구현
 }
 
 
 //>>>>>>>>>>>>test<<<<<<<<<<<<<<
 const lotto = new Lotto();
-lotto.getUserPayment();
+const lottoMaker = new LottoMaker();
+
+
+lottoMaker.getUserPayment();
 // lotto.makePaidLottoNumbers();
 
 
