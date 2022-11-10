4bb82289d875b0cfad4548873bbf09a528966887
AnJaeSeong <dktmzh6@gmail.com>
2022-11-10 23:15:49 +0900

    feat: create test files

---
+++ __tests__/AppTest.js
@@ -0,0 +1,6 @@
+const App = require("../src/App");
+
+describe("앱 클래스 테스트", () => {
+   
+});
+  
---
+++ __tests__/MyLottoTest.js
@@ -0,0 +1,5 @@
+const MyLotto = require("../src/MyLotto");
+
+describe("마이로또 클래스 테스트", () => {
+   
+});
