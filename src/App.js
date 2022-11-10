2dfc04604852c811e58bb5a5c64ba393fc74108e
JaeSung <dktmzh6@gmail.com>
2022-11-10 20:56:18 +0900

    feat: App.contructor()
    myLotto, lotto 인스턴스를 위한 클래스 변수 초기화

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import * as MissionUtils from "@woowacourse/mission-utils";
-import Lotto from "./Lotto";
-import MyLotto from "./MyLotto";
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require('./Lotto.js');
+const MyLotto = require("./MyLotto.js");
 
 class App {
-  #myLotto
-  #lotto
+  constructor() {
+    this.myLotto = null;
+    this.lotto = null;
+  }
 
   play() {
     // 어플리케이션 시작
@@ -13,29 +15,30 @@ class App {
 
   inputPurchase() {
     // 로또 구입 금액 입력
-    MissionUtils.readLine('구입금액을 입력해 주세요.\n', (input) => {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
       this.#myLotto = new MyLotto(parseInt(input));
       this.inputWinNum();
-    })
+    });
   }
 
   inputWinNum() {
     // 당첨 번호 입력
-    MissionUtils.readLine('당첨 번호를 입력해 주세요.\n', (input) => {
+    MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.\n', (input) => {
       this.#lotto = new Lotto(input.split(',').map(num => parseInt(num)));
       this.inputBonusNum();
-    })
+    });
   }
 
   inputBonusNum() {
     // 보너스 번호 입력
-    MissionUtils.readLine('보너스 번호를 입력해 주세요.\n', (input) => {
+    MissionUtils.Console.readLine('보너스 번호를 입력해 주세요.\n', (input) => {
       this.#lotto.setBonusNum(parseInt(input));
-    })
+    });
   }
 
   checkMatchLottoNum() {
     // 로또 발행 번호와 당첨 번호 일치 조회 
+    
   }
 
   getWinHistory() {
@@ -58,5 +61,8 @@ class App {
     // 수익률 출력
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
