388c44fe5a7410073ecdc16555a8051d07307c41
Tory <88531407+1two13@users.noreply.github.com>
2022-11-10 14:44:15 +0900

    refactor(Lotto): 사용자가 6개의 로또 번호를 입력했는지 확인

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class Lotto {
   #numbers;
 
@@ -7,8 +9,8 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (numbers !== undefined && numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
