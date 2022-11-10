1fb46fd6537ca265c9433ffdcf1965023bcb7b77
h2002y <h2002y@naver.com>
2022-11-10 17:37:37 +0900

    feat: Add lotto number check overlap func for input exception in Lotto.js

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,13 +2,31 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.lottoOverlap = new Map();
+    this.setLottoOverlap(numbers, this.lottoOverlap);
+    this.validate(numbers, this.lottoOverlap);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
+  setLottoOverlap(numbers, lottoOverlap) {
+    for (let i = 0; i < numbers.length; i++) {
+      if (!lottoOverlap.has(numbers[i])) {
+        lottoOverlap.set(numbers[i], 1);
+      }
+      else {
+        lottoOverlap.set(numbers[i], lottoOverlap.get(numbers[1]) + 1);
+      }
+    }
+  }
+
+  validate(numbers, lottoOverlap) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    for(let i = 0; i < numbers.length; i++) {
+      if (lottoOverlap.get(numbers[i]) > 1) {
+        throw new Error("[ERROR] 로또 번호는 중복되는 숫자가 없어야 합니다.")
+      }
     }
   }
 
