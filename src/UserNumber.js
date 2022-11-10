cb3cb5f89b26a7e35bdf23ca0efb3b769dd72f75
jinseo <ghkdwlstj99@gmail.com>
2022-11-10 17:37:04 +0900

    rafactor: 발행한 로또 수랑에 대한 임의의 로또 번호 출력 기능 수정

--- src/UserNumber.js
+++ src/UserNumber.js
@@ -1,16 +1,24 @@
 const {Console} = require("@woowacourse/mission-utils");
 const CreateRandomLotto = require("./CreateRandomLotto")
-const createRandomLotto = new CreateRandomLotto();
 const INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요."
 
 class UserInputNumber {
+    constructor(){
+        this.createRandomLotto = new CreateRandomLotto();
+    }
     
     userLottoPaymentAmount(){
         Console.readLine( INPUT_MONEY_MESSAGE , (input) => {
             let issuedLotto = input/1000;
             Console.print(issuedLotto + "개를 구매했습니다.");
+            this.userIssuedNum(issuedLotto);
         })
     }
+
+    userIssuedNum(input){
+        this.createRandomLotto.issuedRandomNumber(input);
+    }
+
 }
 
 module.exports = UserInputNumber;
