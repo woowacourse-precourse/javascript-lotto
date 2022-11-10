d095b148b6a1657243ae7b40977d483f7cfc98b4
somin <smhs12@naver.com>
2022-11-10 17:29:32 +0900

    feat: 사용자 입력 값 유효성 검사 기능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const UserLotto = require("./UserLotto");
+const { isValidateUserInput } = require("./utils/validator");
 class App {
+  constructor() {
+    this.userLotto = new UserLotto(0);
+  }
   play() {
     MissionUtils.Console.print("구입금액을 입력해 주세요.");
     MissionUtils.Console.readLine("", (amount) => {
-      createLottos(amount);
+      if (isValidateUserInput(amount)) {
+        this.userLotto.createUserLotto(amount);
+      }
     });
   }
 }
