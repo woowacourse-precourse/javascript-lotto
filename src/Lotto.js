cd6d9b0f59fd98f85a264d7a7619de02dabbf222
woo-jk <201702042@o.cnu.ac.kr>
2022-11-11 03:51:43 +0900

    feat: Lotto 클래스 로또 번호 중복 검사 로직, 범위 검사 로직 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
 
@@ -6,13 +8,24 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  isIncorrectRange(numbers) {
+    const filteredNumbers = numbers.filter((number) => (1 <= number && number <= 45));
+    return filteredNumbers.length !== numbers.length; 
+  }
+  isDuplicated(numbers) {
+    return numbers.length !== new Set(numbers).size;
+  }
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if(this.isIncorrectRange(numbers)) {
+      throw new Error("[ERROR] 로또 번호의 범위는 1~45 사이여야 합니다.")
+    }
+    if(this.isDuplicated(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 서로 중복 되지 않아야 합니다.");
+    }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
