5ba20ca971ffd381445222fe4447c56f8cf6be6d
jun <ggprgrkjh@naver.com>
2022-11-11 01:41:26 +0900

    feat: 구입한 로또 갯수만큼 로또를 발행한다.

--- src/App.js
+++ src/App.js
@@ -3,31 +3,51 @@ const Lotto = require("./Lotto");
 
 class App {
   constructor() {
-    this.buyPirce;
-    this.lottoNumbers = [];
+    this.buyPrice;
+    this.buyAmount;
+    this.lottoArray = [];
+  }
+  play() {
+    this.gameStart();
   }
 
-  play() {
+  gameStart() {
     this.getBuyPrice();
   }
 
   getBuyPrice() {
     MissionUtils.Console.readLine("구입금액읍 입력해 주세요. \n", (price) => {
-      this.buyPirce = price;
+      this.buyPrice = price;
       this.priceValidate();
       this.createLottoNumber();
+
       MissionUtils.Console.close();
     });
   }
 
   priceValidate() {
-    if (this.buyPirce % 1000 !== 0) {
+    if (this.buyPrice % 1000 !== 0) {
       throw new Error("[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.");
     }
   }
 
+  buyAmountCalculate() {
+    this.buyAmount = this.buyPrice / 1000;
+    MissionUtils.Console.print("\n" + this.buyAmount + "개를 구매했습니다.");
+  }
+
   createLottoNumber() {
-    console.log("creating lottonumbers...");
+    this.buyAmountCalculate();
+
+    for (let index = 0; index < this.buyAmount; index++) {
+      this.lottoArray.push(
+        MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
+      );
+    }
+
+    this.lottoArray.map((data) => {
+      console.log(data);
+    });
   }
 }
 
