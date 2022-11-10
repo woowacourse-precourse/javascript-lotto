21b1575b45a8743ca85d8b5ced6fa7ff784c7753
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-10 17:50:19 +0900

    feat:  사용자로부터 입력받은 당첨번호에 대한 유효성 검사 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,7 +8,17 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    const numberSet = new Set(numbers);
+    if (numberSet.size !== numbers.length) {
+      throw new Error('[ERROR] 로또 번호는 서로 다른 숫자로 이루어져야 합니다.');
+    }
+    let isNumberInRange = numbers.every(number => {
+      return number && number >= 1 && number <= 45;
+    });
+    if (!isNumberInRange) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 수들로 이루어져야 합니다.');
     }
   }
 
