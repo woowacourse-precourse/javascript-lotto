e9eff6b7fb96363dd57a4dcc75fa561f1f531c85
olo90632951 <olo90632951@gmail.com>
2022-11-11 01:14:43 +0900

    test: 입력 받은 당첨 번호의 형식이 잘못되었을 경우 예외 발생 테스트 코드 추가가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,32 @@
 /* eslint-disable max-lines-per-function */
 const Lotto = require('../src/Lotto');
 
+const throwException = (numbers) => {
+  expect(() => {
+    new Lotto(numbers);
+  }).toThrow('[ERROR]');
+};
+
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow('[ERROR]');
+    throwException([1, 2, 3, 4, 5, 6, 7]);
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow('[ERROR]');
+    throwException([1, 2, 3, 4, 5, 5]);
+  });
+
+  test('로또 번호가 숫자가 아니면 예외가 발생한다.', () => {
+    throwException([1, 2, 3, 4, 'a', 6]);
+  });
+
+  test('로또 번호가 0이하의 숫자일 때 예외가 발생한다.', () => {
+    throwException([0, 1, 2, 3, 4, 5]);
+  });
+
+  test('로또 번호가 46이상의 숫자일 때 예외가 발생한다.', () => {
+    throwException([1, 2, 3, 4, 5, 46]);
   });
 
   // 아래에 추가 테스트 작성 가능
