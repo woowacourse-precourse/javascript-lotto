e5d184f430fb48ad27c02eaf9d40b706507c44ff
woo-jk <201702042@o.cnu.ac.kr>
2022-11-11 00:19:26 +0900

    feat: LottoMachine 잘못된 금액을 입력 할 경우  예외 처리 구현

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -2,7 +2,17 @@ class LottoMachine {
   #money;
 
   constructor(money){
+    this.validate(money);
     this.#money = money;
+  }
+
+  validate(money) {
+    if(isNaN(money)) {
+      throw new Error("[ERROR] 금액은 숫자만 입력해야 합니다.");
+    }
+    if(money % 1000 !== 0 ){
+      throw new Error("[ERROR] 금액은 1,000원 단위만 입력 가능합니다.")
+    }
   }
   
 }
