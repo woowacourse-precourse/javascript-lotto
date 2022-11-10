a50d9df564024734b28bedd8ecfd3e262da447d4
hyunjin <leehj0110@kakao.com>
2022-11-10 19:40:33 +0900

    chore: add TypeError validation in Lotto class

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,8 +7,16 @@ class Lotto {
   }
 
   validate(numbers) {
+    if (typeof numbers !== "object") {
+      throw new TypeError("[ERROR] numbers의 타입은 object 타입이어야 합니다.");
+    }
+
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
     }
   }
 
