6cb73f069a4de73cccf2ac018a8d6a9c73b3d227
moonelysian <dandyblack@naver.com>
2022-11-10 15:11:12 +0900

    feat: lotto class validation 추가
    - 중복된 수 없는지 점검
    - 숫자가 아닌 값이 섞여있는지 점검
    - 범위에 맞지 않는 수가 있는지 점검

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,16 +1,35 @@
 const { LOTTO, ERROR } = require('./constructor.js');
+const { throwErrorMessage } = require('./utils.js');
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.map(number => parseInt(number));
   }
 
   validate(numbers) {
     if (numbers.length !== LOTTO.LENGTH) {
-      throw new Error(ERROR.OUT_OF_LOTTO_LENGTH);
+      throwErrorMessage(ERROR.OUT_OF_LOTTO_LENGTH);
+    }
+
+    const unduplicatedNums = new Set(numbers);
+    if (unduplicatedNums.size != numbers.length) {
+      throwErrorMessage(ERROR.DUPLICATE_NUMBERS);
+    }
+
+    const notNumberInputs = numbers.filter(number => isNaN(number));
+    if (notNumberInputs.length != 0) {
+      throwErrorMessage(ERROR.ONLY_NUMBER);
+    }
+
+    const outOfRangeNumbers = numbers.filter(value => {
+      const number = parseInt(value);
+      return number < LOTTO.MIN || number > LOTTO.MAX;
+    })
+    if (outOfRangeNumbers.length != 0) {
+      throwErrorMessage(ERROR.OUT_OF_NUMEBR_RANGE);
     }
   }
 
