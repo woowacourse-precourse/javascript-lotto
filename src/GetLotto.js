15a53ee875d8b90fd9bc8f20c0339f6a142323df
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-10 21:26:50 +0900

    feat(GetLotto): 로또 갯수를 받아 발행하는 함수 구현

--- src/GetLotto.js
+++ src/GetLotto.js
@@ -5,9 +5,25 @@ class GetLotto {
         this.lottoList = [];
     }
 
+    lottoNumberPackage(money) {
+        const lottoCount = this.howManyLotto(money);
+        for (let i = 0; i < lottoCount; i++) {
+            let lottoNumber = this.sortLottoNumber(Random.pickUniqueNumbersInRange(1, 45, 6));
+            Console.print(lottoNumber);
+            this.lottoList.push(lottoNumber);
+        }
+    }
+
     howManyLotto(money) {
         return money / 1000;
     }
+
+    sortLottoNumber(Array) {
+        return Array.sort((a,b) => a - b);
+    }
 }
+
+let a = new GetLotto();
+a.lottoNumberPackage(5000);
 
 module.exports = GetLotto;
