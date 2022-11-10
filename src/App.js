abd103bc8755b6fe9824e4ae312493a85f057ec8
shackstack <kms1061@naver.com>
2022-11-10 15:37:50 +0900

    feat: 수익률 제외 당첨 통계출력 기능구현

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App {
 
     for (
       let sequence = 1;
-      sequence < this.getPurchaseQuantity(money);
+      sequence <= this.getPurchaseQuantity(money);
       sequence++
     ) {
       let LottoArray = this.getLottoNumber();
@@ -65,7 +65,7 @@ class App {
       (WinningNumbers) => {
         let splitWinningNumbers = WinningNumbers.split(",");
 
-        this.winningNumbers = splitWinningNumbers;
+        this.winningNumbers = splitWinningNumbers.map(Number);
         this.inputBonusNumber();
       }
     );
@@ -75,9 +75,71 @@ class App {
     MissionUtils.Console.readLine(
       OUTPUT_PHRASE.LINE_UP + READLINE_PHRASE.INPUT_BONUS_NUMBER,
       (bonusNumber) => {
-        this.bonusNumber = bonusNumber;
+        this.bonusNumber = Number(bonusNumber);
+
+        this.printWinningStastics();
       }
     );
+  }
+
+  printWinningStastics() {
+    MissionUtils.Console.print(
+      OUTPUT_PHRASE.LINE_UP + OUTPUT_PHRASE.WINNING_STATISTICS.INTRO
+    );
+
+    let coincide = {
+      threeMatches: 0,
+      fourMatches: 0,
+      fiveMatches: 0,
+      fiveAndBonusMatches: 0,
+      allMatches: 0,
+    };
+
+    this.lottoArrays.forEach((item) => {
+      let matchCount = this.getMatchCount(item, this.winningNumbers);
+      let bonusMatch = this.getBonusMatch(item, this.bonusNumber);
+      if (matchCount == 3) {
+        coincide.threeMatches += 1;
+      } else if (matchCount == 4) {
+        coincide.fourMatches += 1;
+      } else if (matchCount == 5 && bonusMatch == true) {
+        coincide.fiveAndBonusMatches += 1;
+      } else if (matchCount == 5) {
+        coincide.fiveMatches += 1;
+      } else if (matchCount == 6) {
+        coincide.allMatches += 1;
+      }
+    });
+
+    MissionUtils.Console.print(
+      OUTPUT_PHRASE.WINNING_STATISTICS.THREE_MATCHES +
+        `${coincide.threeMatches}개`
+    );
+    MissionUtils.Console.print(
+      OUTPUT_PHRASE.WINNING_STATISTICS.FOUR_MATCHES +
+        `${coincide.fourMatches}개`
+    );
+    MissionUtils.Console.print(
+      OUTPUT_PHRASE.WINNING_STATISTICS.FIVE_MATCHES +
+        `${coincide.fiveMatches}개`
+    );
+    MissionUtils.Console.print(
+      OUTPUT_PHRASE.WINNING_STATISTICS.FIVE_AND_BONUS_MATCHES +
+        `${coincide.fiveAndBonusMatches}개`
+    );
+    MissionUtils.Console.print(
+      OUTPUT_PHRASE.WINNING_STATISTICS.ALL_MATCHES + `${coincide.allMatches}개`
+    );
+  }
+
+  getMatchCount(A, B) {
+    let arr = new Set(A.concat(B));
+    arr = [...arr];
+
+    return A.length + B.length - arr.length;
+  }
+  getBonusMatch(A, B) {
+    return A.includes(B);
   }
 }
 
--- src/Constant.js
+++ src/Constant.js
@@ -8,7 +8,7 @@ const OUTPUT_PHRASE = {
   LINE_UP: "\n",
   PURCHASE_QUANTITY: "개를 구매했습니다.",
   WINNING_STATISTICS: {
-    INTRO: "당첨 통계\n" + "---\n",
+    INTRO: "당첨 통계\n" + "---",
     THREE_MATCHES: "3개 일치 (5,000원) - ",
     FOUR_MATCHES: "4개 일치 (50,000원) - ",
     FIVE_MATCHES: "5개 일치 (1,500,000원) - ",
@@ -20,7 +20,7 @@ const OUTPUT_PHRASE = {
 const LOTTO_RANGE = {
   START_NUMBER: 1,
   END_NUMBER: 45,
-  LENGTH: 9,
+  LENGTH: 6,
 };
 
 module.exports = { READLINE_PHRASE, OUTPUT_PHRASE, LOTTO_RANGE };
