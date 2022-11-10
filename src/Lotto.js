b6a00eb8e4107e9996579203351ba12f7ef2de3e
박준서 <oponize@naver.com>
2022-11-10 18:57:54 +0900

    refactor: 검증 클래스로 검증 로직 분리

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const Validator = require("./utils/Validator");
+
 class Lotto {
   #numbers;
 
@@ -9,19 +11,9 @@ class Lotto {
   validateLotto(numbers) {
     if (numbers.length !== 6) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     if (new Set(numbers).size !== 6) throw new Error("[ERROR] 중복된 번호가 존재합니다.");
-    if (!numbers.every(this.isNumber)) throw new Error("[ERROR] 숫자가 아닌 번호가 존재합니다.");
-    if (!numbers.every(this.isLottoNumberFrom1to45)) throw new Error("[ERROR] 로또 번호가 1~45 사이가 아닙니다.");
+    if (!numbers.every(Validator.isNumber)) throw new Error("[ERROR] 숫자가 아닌 번호가 존재합니다.");
+    if (!numbers.every(Validator.isLottoNumberFrom1to45)) throw new Error("[ERROR] 로또 번호가 1~45 사이가 아닙니다.");
     return true;
-  }
-
-  isNumber(number) {
-    if (number.toString().match(/\d/)) return true;
-    return false;
-  }
-
-  isLottoNumberFrom1to45(number) {
-    if (number >= 1 && number <= 45) return true;
-    return false;
   }
 
   get lotto() {
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,10 +1,22 @@
-const { LOTTO_AMOUNT } = require("../constants/index");
+const { LOTTO_AMOUNT, LOTTO_NUMBER } = require("../constants/index");
 
-const isValidAmount = (amount) => {
-  if (!amount.match(/\d/)) throw new Error("[ERROR] 숫자를 입력해주세요.");
-  if (Number(amount) % LOTTO_AMOUNT.VALID_UNIT) throw new Error("[ERROR] 1,000원 단위로 입력해주세요.");
-  if (Number(amount) < LOTTO_AMOUNT.VALID_UNIT) throw new Error("[ERROR] 1,000원 이상으로 입력해주세요.");
-  return true;
+const Validator = class {
+  static isValidAmount = (amount) => {
+    if (!amount.match(/\d/)) throw new Error("[ERROR] 숫자를 입력해주세요.");
+    if (Number(amount) % LOTTO_AMOUNT.VALID_UNIT) throw new Error("[ERROR] 1,000원 단위로 입력해주세요.");
+    if (Number(amount) < LOTTO_AMOUNT.VALID_UNIT) throw new Error("[ERROR] 1,000원 이상으로 입력해주세요.");
+    return true;
+  };
+
+  static isNumber = (number) => {
+    if (number.toString().match(/\d/)) return true;
+    return false;
+  };
+
+  static isLottoNumberFrom1to45 = (number) => {
+    if (number >= LOTTO_NUMBER.MIN_NUMBER && number <= LOTTO_NUMBER.MAX_NUMBER) return true;
+    return false;
+  };
 };
 
-exports.isValidAmount = isValidAmount;
+module.exports = Validator;
