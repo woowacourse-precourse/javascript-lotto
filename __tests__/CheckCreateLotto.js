98eb1cbb02e10473b876ad7c531d68795ba3dde5
khj0426 <706shin1728@naver.com>
2022-11-11 20:52:59 +0900

    test:랜덤으로 발행한 로또가 올바른지 테스트

--- __tests__/CheckCreateLotto.js
+++ __tests__/CheckCreateLotto.js
@@ -3,8 +3,12 @@ const Lotto = require('../src/Lotto');
 
 const testlotto = new Lotto('123456');
 test('랜덤 로또 발행 테스트', () => {
-  const newlotto = testlotto.pubishLotto(1);
-  const newlottolist = newlotto[0];
+  let newlotto = testlotto.pubishLotto(1);
+  let newlottolist = newlotto[0];
+  expect(testlotto.validate(newlottolist)).toBe(true);
+  expect(testlotto.validate_indepence(newlottolist)).toBe(true);
+  newlotto = testlotto.pubishLotto(1);
+  newlottolist = newlotto[0];
   expect(testlotto.validate(newlottolist)).toBe(true);
   expect(testlotto.validate_indepence(newlottolist)).toBe(true);
 });
