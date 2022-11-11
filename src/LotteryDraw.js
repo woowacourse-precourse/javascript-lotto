efd781b37dd72f1c786a33a5959d2b5c5ce3ae8a
c0c0pang <coc1789@naver.com>
2022-11-11 20:05:37 +0900

    feat: Struct를 상속 받게 기능 구현

---
+++ src/LotteryDraw.js
@@ -0,0 +1,10 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Struct = require('./Struct');
+const MoneyExceptionHandling = require('./MoneyExceptionHandling');
+const WinningExceptionHandling = require('./WinningExceptionHandling');
+const BonusExceptionHandling = require('./BonusExceptionHandling');
+class LotteryDraw extends Struct{ 
+
+};
+
+module.exports = LotteryDraw;
