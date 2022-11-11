5bb32518f2091317762c943fd0f2975a28101323
c0c0pang <coc1789@naver.com>
2022-11-11 19:48:29 +0900

    feat: 보너스 번호와 당첨 번호가 중복 되는지 확인하는 예외처리 기능 추가

--- src/BonusExceptionHandling.js
+++ src/BonusExceptionHandling.js
@@ -1,10 +1,16 @@
 class BonusExceptionHandling {
     constructor(bonus,winning){
         this.rangeCheck(bonus);
+        this.overlapCheck(bonus,winning);
     };
     rangeCheck(bonus){
         if(!(bonus>=1 && bonus<=45)){
             throw new Error("[ERROR] 보너스 숫자 범위는 1~45 입니다.");
+        };
+    };
+    overlapCheck(bonus,winning){
+        if(winning.indexOf(bonus)>=0){
+            throw new Error("[ERROR] 보너스 숫자가 당첨번호와 중복 됩니다.");
         };
     };
 };
