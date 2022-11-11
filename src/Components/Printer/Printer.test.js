6c041df1bb376382830cd06ab738f76e8923b08a
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-11 19:35:21 +0900

    test:

--- src/Components/Printer/Printer.test.js
+++ src/Components/Printer/Printer.test.js
@@ -1,14 +1,14 @@
-const Printer = require("./Printer");
-const { getLogSpy } = require("../../testFunction");
+const Printer = require('./Printer');
+const { getLogSpy } = require('../../testFunction');
 
-describe("Printer 클래스 테스트", () => {
-  test("일치하는 번호 개수, 상금, 당첨 개수를 출력한다.", () => {
+describe('Printer 클래스 테스트', () => {
+  test('일치하는 번호 개수, 상금, 당첨 개수를 출력한다.', () => {
     const logSpy = getLogSpy();
     const printString = `6개 일치 (2,000,000,000원) - 0개`;
     new Printer()
-      .setMatchedNumberCount("6")
-      .setWinningMoney("2,000,000,000")
-      .setMatchedLottoCount("0")
+      .setLottoNumberCount('6')
+      .setWinningMoney('2,000,000,000')
+      .setMatchedLottoCount('0')
       .print();
 
     expect(logSpy).toHaveBeenCalledWith(printString);
--- src/Components/PrinterWithBonusBall/PrinterWithBonusBall.test.js
+++ src/Components/PrinterWithBonusBall/PrinterWithBonusBall.test.js
@@ -1,14 +1,14 @@
-const PrinterWithBonusBall = require("./PrinterWithBonusBall");
-const { getLogSpy } = require("../../testFunction");
+const PrinterWithBonusBall = require('./PrinterWithBonusBall');
+const { getLogSpy } = require('../../testFunction');
 
-describe("PrinPrinterWithBonusBallter 클래스 테스트", () => {
-  test("일치하는 번호 개수, 보너스 볼 일치 여부, 상금, 당첨 개수를 출력한다.", () => {
+describe('PrinPrinterWithBonusBallter 클래스 테스트', () => {
+  test('일치하는 번호 개수, 보너스 볼 일치 여부, 상금, 당첨 개수를 출력한다.', () => {
     const logSpy = getLogSpy();
     const printString = `5개 일치, 보너스 볼 일치 (30,000,000원) - 0개`;
     new PrinterWithBonusBall()
-      .setMatchedNumberCount("5")
-      .setWinningMoney("30,000,000")
-      .setMatchedLottoCount("0")
+      .setLottoNumberCount('5')
+      .setWinningMoney('30,000,000')
+      .setMatchedLottoCount('0')
       .print();
 
     expect(logSpy).toHaveBeenCalledWith(printString);
