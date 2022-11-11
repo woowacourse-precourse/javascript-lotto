263f708a02c35d83e350b9c962ff2a349a85c940
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-11 23:22:08 +0900

    feat: 정답 수를 통한 등수 확인 기능 추가

---
+++ src/utils/returnMyRank.js
@@ -0,0 +1,10 @@
+const { CORRECT_NUMBER } = require('../constants/constants');
+
+const returnMyRank = (countResult, bonusResult) => {
+  let result = countResult + bonusResult;
+
+  if (result === 6) if (bonusResult === 0) result += 1;
+  return CORRECT_NUMBER[result];
+};
+
+module.exports = returnMyRank;
