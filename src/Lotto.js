e9f994e3b582452068a14a1a9412f5d2b3a65cf2
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-11 14:25:26 +0900

    refactor(Lotto): 배열 길이 예외 메소드

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,15 +1,17 @@
+const Application = require('./Application');
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    Lotto.validate(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  static validate(numbers) {
+    const CRITERION = 6;
+
+    Application.validateArrayLength(numbers, CRITERION);
   }
 
   // TODO: 추가 기능 구현
