461eb2369d8523c3618a03bf6c2a4399121d12ca
wontae chang <cwt629@naver.com>
2022-11-11 23:12:04 +0900

    fix: switch the message for 2nd place and 3rd place

--- src/LottoInfo.js
+++ src/LottoInfo.js
@@ -2,7 +2,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class PrizeInformation {
     constructor() {
-        this.message = ["", "6개 일치", "5개 일치", "5개 일치, 보너스 볼 일치", "4개 일치", "3개 일치"];
+        this.message = ["", "6개 일치", "5개 일치, 보너스 볼 일치", "5개 일치", "4개 일치", "3개 일치"];
         this.prize = [0, 2000000000, 30000000, 1500000, 50000, 5000];
         this.quantity = [0, 0, 0, 0, 0, 0];
     }
