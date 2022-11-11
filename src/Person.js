3abf76b75c17de625a4e214604e7d10e53652e71
Narcoker <jykim9868@gmail.com>
2022-11-12 01:34:24 +0900

    Feat: 당첨 내역, 수익율 출력 기능 추가

--- src/Person.js
+++ src/Person.js
@@ -2,14 +2,14 @@ const { MissionUtils, SYSTEM } = require('./System');
 const { MESSAGE } = require('./Constants')
 class Person {
     constructor() {
-        this.lotto = null;
+        this.lottos = null;
     }
 
     buy() {
         MissionUtils.Console.readLine(MESSAGE.ENTER_CASH, (cache) => {
             SYSTEM.isCorrectCache(cache);
-            this.lotto = SYSTEM.publishLotto(cache);
-            SYSTEM.getResult(this.lotto);
+            this.lottos = SYSTEM.publishLotto(cache);
+            SYSTEM.getResult(this.lottos, cache);
         });
     }
 }
--- src/System.js
+++ src/System.js
@@ -31,8 +31,9 @@ const SYSTEM = Object.freeze({
     },
 
     printLottos(lottos, maxCount) {
-        this.print(`\n${maxCount}${MESSAGE.BUY_LOTTOS_COUNT}`);
-        lottos.forEach(lotto => { SYSTEM.print(lotto.getNumber()); });
+        this.print(`${maxCount}${MESSAGE.BUY_LOTTOS_COUNT}`);
+        // lottos.forEach(lotto => { SYSTEM.print(lotto.getNumber()); });
+        lottos.forEach(lotto => { SYSTEM.print(`[${lotto.getNumber().join(', ')}]`); });
     },
 
     publishLotto(cache) {
@@ -41,14 +42,14 @@ const SYSTEM = Object.freeze({
     },
 
 
-    getResult(lotto) {
-        this.makeWinningLotto(lotto);
+    getResult(lottos, cache) {
+        this.makeWinningLotto(lottos, cache);
     },
 
-    makeWinningLotto(lotto) {
+    makeWinningLotto(lottos, cache) {
         MissionUtils.Console.readLine(MESSAGE.ENTER_WINNING_LOTTO, (winningLotto) => {
             winningLotto = new Lotto(winningLotto.split(",").map(Number));
-            this.makeBonusNumber(lotto, winningLotto);
+            this.makeBonusNumber(lottos, winningLotto.getNumber(), cache);
         });
     },
 
@@ -67,12 +68,46 @@ const SYSTEM = Object.freeze({
 
     },
 
-    makeBonusNumber(lotto, winningLotto) {
+    makeBonusNumber(lottos, winningLotto, cache) {
         MissionUtils.Console.readLine(MESSAGE.ENTER_BOUNS_NUMBER, (bonusNumber) => {
             bonusNumber = Number(bonusNumber);
-            this.isCorrectBonusNumber(bonusNumber, winningLotto.getNumber());
-            console.log(winningLotto.getNumber(), bonusNumber);
+            this.isCorrectBonusNumber(bonusNumber, winningLotto);
+            this.printResult(lottos, winningLotto, bonusNumber, cache);
         });
+    },
+
+    compare(lotto, winningLotto, bonusNumber) {
+        let rank = 8;
+        for (let number of lotto) {
+            if (winningLotto.has(number)) rank--;
+            if (rank === 3 && lotto.includes(bonusNumber)) rank--;
+        }
+        return rank - 1;
+    },
+
+    calulateRate(cache, result) {
+        let winnings = [2_000_000_000, 30_000_000, 1_500_000, 50_000, 5_000];
+        let revenue = winnings.reduce((sum, money, rank) => sum + (result[rank] * money), 0);
+        return Math.round(revenue / cache * 100 * 10) / 10;
+
+    },
+
+    printResult(lottos, winningLotto, bonusNumber, cache) {
+        let result = new Array(5).fill(0);
+
+        for (let lotto of lottos) {
+            let rankIndex = this.compare(lotto.getNumber(), new Set(winningLotto), bonusNumber);
+            result[rankIndex]++;
+        }
+
+        for (let rank = 5; rank >= 1; rank--) {
+            let rankIndex = rank - 1;
+            SYSTEM.print(`${MESSAGE.RANK_TEXT[rankIndex]} - ${result[rankIndex]}개`)
+        }
+        let rate = this.calulateRate(cache, result);
+        SYSTEM.print(`총 수익률은 ${rate}%입니다.`);
+        this.exit();
+
     },
 
     exit() {
