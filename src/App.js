4aa961affd3b5d0bbe87ed300550fd955369d32e
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-12 03:34:29 +0900

    feat: 당첨 내역과 수익률을 출력하는 기능 구현
      - 수익률은 소수점 둘째 자리에서 반올림

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
 
 class App {
+  earning = 0;
+  statistics = {
+    '3개 일치 (5,000원)': 0,
+    '4개 일치 (50,000원)': 0,
+    '5개 일치 (1,500,000원)': 0,
+    '5개 일치, 보너스 볼 일치 (30,000,000원)': 0,
+    '6개 일치 (2,000,000,000원)': 0,
+  };
+
   getCount() {
     Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
+      this.amount = input;
       this.lottoCount = input / 1000;
     });
   }
@@ -23,8 +34,8 @@ class App {
     const { lottoCount, lottos } = this;
 
     Console.print(`${lottoCount}개를 구매했습니다.`);
-    lottos.forEach((lotto) => {
-      Console.print(lotto);
+    lottos.forEach((lottoNumbers) => {
+      Console.print(lottoNumbers);
     });
   }
 
@@ -38,6 +49,39 @@ class App {
     Console.readLine('보너스 번호를 입력해 주세요.\n', (input) => {
       this.bonusNumber = Number(input);
     });
+  }
+
+  getStatistics() {
+    const { lottos, winningNumbers, bonusNumber } = this;
+
+    lottos.forEach((lottoNumbers) => {
+      const lotto = new Lotto(lottoNumbers);
+
+      lotto.matchNumbers(winningNumbers, bonusNumber);
+      lotto.getMatchInformation();
+      if (lotto.matchCount >= 3) {
+        this.earning += lotto.prize;
+        this.statistics[lotto.matchInfo] += 1;
+      }
+    });
+    this.getEarningRate(this.earning);
+  }
+
+  getEarningRate() {
+    const { amount, earning } = this;
+    const earningRate = (earning / amount) * 100;
+
+    this.earningRate = +(Math.round(earningRate + 'e+1') + 'e-1');
+  }
+
+  printStatistics() {
+    const { statistics, earningRate } = this;
+
+    Console.print('당첨 통계\n---');
+    for (const key in statistics) {
+      Console.print(`${key} - ${statistics[key]}개`);
+    }
+    Console.print(`총 수익률은 ${earningRate}%입니다.`);
   }
 
   play() {}
