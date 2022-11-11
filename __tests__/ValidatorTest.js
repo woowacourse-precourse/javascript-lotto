0d16a4da53adc0db4a96ce673c8ebc5858d40842
Cobocho <rlaalsrb1111@naver.com>
2022-11-11 23:10:15 +0900

    test: 잘못된 변수명 변경 및 에러 로직 분리에 따른 예측값 변경

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,5 +1,5 @@
 const Validator = require("../src/Utils/Validator");
-const MESSAGES = require("../src/constants");
+const { MESSAGES } = require("../src/constants");
 
 describe("amountValidCheck 테스트", () => {
   test("공백 여부 판별", () => {
@@ -9,7 +9,7 @@ describe("amountValidCheck 테스트", () => {
       const result = Validator.isBlank(subject);
       expect(result).toBeUndefined();
     });
-    expect(Validator.isBlank(blank)).toBe(MESSAGES.ERROR.isBlank);
+    expect(() => Validator.isBlank(blank)).toThrowError(MESSAGES.ERROR.isBlank);
   });
   test("공백 유무 판별", () => {
     const normalNumbers = ['1341', '23134', '1243124141', '1'];
@@ -19,8 +19,7 @@ describe("amountValidCheck 테스트", () => {
       expect(result).toBeUndefined();
     });
     haveBlankNumbers.forEach(subject => {      
-      const result = Validator.hasBlank(subject);
-      expect(result).toBe(MESSAGES.ERROR.hasBlank);
+      expect(() => Validator.hasBlank(subject)).toThrowError(MESSAGES.ERROR.hasBlank);
     });
   });
   test("숫자 여부 판별", () => {
@@ -30,21 +29,19 @@ describe("amountValidCheck 테스트", () => {
       const result = Validator.isNotNumber(subject);
       expect(result).toBeUndefined();
     });
-    notNumbers.forEach(subject => {      
-      const result = Validator.isNotNumber(subject);
-      expect(result).toBe(MESSAGES.ERROR.isNotNumber);
+    notNumbers.forEach(subject => {    
+      expect(() => Validator.isNotNumber(subject)).toThrowError(MESSAGES.ERROR.isNotNumber);
     });
   });
   test("천원 단위 판별", () => {
-    const normalNumbers = [1000, 10000, 231000];
-    const haveBlankNumbers = [1001, 500, 1230, 999, 111001];
-    normalNumbers.forEach(subject => {      
+    const kilos = [1000, 10000, 231000];
+    const notKilos = [1001, 500, 1230, 999, 111001];
+    kilos.forEach(subject => {      
       const result = Validator.isNotKilo(subject);
       expect(result).toBeUndefined();
     });
-    haveBlankNumbers.forEach(subject => {      
-      const result = Validator.isNotKilo(subject);
-      expect(result).toBe(MESSAGES.ERROR.isNotKilo);
+    notKilos.forEach(subject => {      
+      expect(() => Validator.isNotKilo(subject)).toThrowError(MESSAGES.ERROR.isNotKilo);
     });
   });
 });
