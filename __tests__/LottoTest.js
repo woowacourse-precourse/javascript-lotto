4f0dacce38f23a34f3c7c33813bb4d981d6fdc3a
MyungAe <ladiescheong@naver.com>
2022-11-11 19:38:13 +0900

    test(LottoTest): mockQuestion의 로직 변경, 반환 값 그대로

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,10 +1,8 @@
 const Lotto = require("../src/Lotto");
-const Customer = require("../src/Customer");
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const mockQuestions = (answer) => {
-  MissionUtils.Console.readLine = jest.fn();
-  MissionUtils.Console.readLine.mockReturnValue(answer);
+  MissionUtils.Console.readLine = jest.fn().mockReturnValue(answer);
 };
 
 describe("로또 클래스 테스트", () => {
@@ -25,7 +23,7 @@ describe("로또 클래스 테스트", () => {
 describe("Feat 1. Customer.buyLotto", () => {
   test("입력된 금액이 천원으로 나누어 떨어진다.", () => {
     mockQuestions("8000");
-    const customer = new Customer();
+    const app = new App();
     expect(customer.buyLotto()).toBe(8);
   });
 
