b84d8d4d4d8361bd5558b90c057fc8d5ea413ae0
chiman <rlaclghks123@naver.com>
2022-11-11 14:55:17 +0900

    feat: 일치하는 번호의 개수를 랭킹순으로 맞춰주는 기능
    1. 5개가 일치할경우 보너스와 비교해서 2등과 3등을 나눠준다.
    2. 일치하는 개수가 2개이상 4개 이하일경우 -1을 해줌으로서 랭킹순으로 나타낸다.
    3. 일치하는 개수가 6개일경우 6개로 나타낸다.

--- src/Statistics.js
+++ src/Statistics.js
@@ -5,6 +5,15 @@ class Statistics {
     this.bonusNumber = bonusNumber;
   }
 
+  getNumberOfMatchingNumbersRanked(numbers) {
+    const numberOfMatchingWinnerNumbers = this.getNumberOfMatchingWinnerNumbers(numbers);
+    if (numberOfMatchingWinnerNumbers === 5) return this.compareWithBonusNumber(numbers);
+    if (numberOfMatchingWinnerNumbers > 1 && numberOfMatchingWinnerNumbers <= 4) {
+      return numberOfMatchingWinnerNumbers - 1;
+    }
+    return numberOfMatchingWinnerNumbers;
+  }
+
   getNumberOfMatchingWinnerNumbers(numbers) {
     const numberOfWinnerNumber = numbers.map((number) => {
       if (number.toString() === this.winnerNumber[index]) {
@@ -12,6 +21,13 @@ class Statistics {
       }
     });
     return numberOfWinnerNumber.filter((number) => number !== undefined).length;
+  }
+
+  compareWithBonusNumber(numbers) {
+    if (numbers.includes(Number(this.bonusNumber))) {
+      return 5;
+    }
+    return 4;
   }
 }
 module.exports = Statistics;
