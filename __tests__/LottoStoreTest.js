5cb0024da01f66e2d08a15964b0eae9091cb2ac1
turtle601 <turtle601@naver.com>
2022-11-11 21:40:00 +0900

    refactor(LottoStoreTest) 에러 체크 테스트 코드 수정
    Changed:
    - 생성자만 생성해도 에러 체크 가능하게 수정

--- __tests__/LottoStoreTest.js
+++ __tests__/LottoStoreTest.js
@@ -3,57 +3,45 @@ const LottoStore = require('../src/LottoStore');
 
 describe('로또 가게 클래스 테스트', () => {
   test('a - 금액이 숫자 + 뒷글자가 000이 아니면 에러 처리', () => {
-    const lottoStore = new LottoStore('a');
-
     expect(() => {
-      lottoStore.getBuyAt();
+      new LottoStore('a');
     }).toThrow('[ERROR]');
   });
 
   test('a1000 - 금액이 숫자 + 뒷글자가 000이 아니면 에러 처리', () => {
-    const lottoStore = new LottoStore('a1000');
-
     expect(() => {
-      lottoStore.getBuyAt();
+      new LottoStore('a1000');
     }).toThrow('[ERROR]');
   });
 
   test('1a000 - 금액이 숫자 + 뒷글자가 000이 아니면 에러 처리', () => {
-    const lottoStore = new LottoStore('1a000');
-
     expect(() => {
-      lottoStore.getBuyAt();
+      new LottoStore('1a000');
     }).toThrow('[ERROR]');
   });
 
   test('1200 - 금액이 숫자 + 뒷글자가 000이 아니면 에러 처리', () => {
-    const lottoStore = new LottoStore('1200');
-
     expect(() => {
-      lottoStore.getBuyAt();
+      new LottoStore('1200');
     }).toThrow('[ERROR]');
   });
 
   test('(공백)12000 - 금액이 숫자 + 뒷글자가 000이 아니면 에러 처리', () => {
-    const lottoStore = new LottoStore(' 12000');
-
     expect(() => {
-      lottoStore.getBuyAt();
+      new LottoStore(' 12000');
     }).toThrow('[ERROR]');
   });
 
   test('12000(공백) - 금액이 숫자 + 뒷글자가 000이 아니면 에러 처리', () => {
-    const lottoStore = new LottoStore('12000 ');
-
     expect(() => {
-      lottoStore.getBuyAt();
+      new LottoStore('12000 ');
     }).toThrow('[ERROR]');
   });
 
   test('옳은 입력값 - 주어진 돈만큼 로또가 나왔는지 확인', () => {
     const lottoStore = new LottoStore('12000');
 
-    const lottoCount = lottoStore.getBuyAt().length;
+    const lottoCount = lottoStore.getLottos().length;
 
     expect(lottoCount).toEqual(12);
 
