df0f75b63b0df176f27de14ea40531bdeb49f6d9
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-11 19:28:31 +0900

    test(Application): 로또 구매 메소드
    1,000원으로 나누어 떨어지지 않는 경우 예외 처리한다.

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -3,6 +3,8 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 const Lotto = require('../src/Lotto');
 
+const ERROR_TEXT = '[ERROR]';
+
 afterAll(() => {
   MissionUtils.Console.close();
 });
@@ -11,20 +13,20 @@ describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow('[ERROR]');
+    }).toThrow(ERROR_TEXT);
   });
 
   test('배열이 아니면 예외를 발생한다.', () => {
     expect(() => {
       new Lotto(1, 2, 3, 4, 5, 6);
-    }).toThrow('[ERROR]');
+    }).toThrow(ERROR_TEXT);
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow('[ERROR]');
+    }).toThrow(ERROR_TEXT);
   });
 
   describe('로또 발행 메소드 테스트', () => {
@@ -54,6 +56,16 @@ describe('로또 클래스 테스트', () => {
 
     test('요소의 길이는 6이다.', () => {
       expect(Lotto.buyLotto('8000')[0]).toHaveLength(6);
+    });
+
+    test('1,000원으로 나누어 떨어지지 않는 경우 예외 처리한다.', () => {
+      expect(() => {
+        Lotto.buyLotto('8800');
+      }).toThrow(ERROR_TEXT);
+
+      expect(() => {
+        Lotto.buyLotto('8000');
+      }).not.toThrow(ERROR_TEXT);
     });
   });
 });
