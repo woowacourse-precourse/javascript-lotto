1531530aeba820ab0a34a909007a349d8576f3f0
d0422 <rlfehd2013@naver.com>
2022-11-11 14:39:04 +0900

    Feat:입력 기능 추가 body: 돈입력, 당첨 번호입력, 보너스 번호 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const input = require('./Input');
 class App {
-  play() {}
+  lottos = [];
+  winLotto;
+  bonusNumber;
+
+  play() {
+    input.call(
+      this,
+      this.getLottosByMoney,
+      this.setWinLotto,
+      this.setBonusNumber
+    );
+  }
+
+  getLottosByMoney(money) {}
+  setWinLotto(inputNumbers) {}
+  setBonusNumber(inputNumber) {}
 }
 
+new App().play();
 module.exports = App;
