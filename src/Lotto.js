96614851cd9d6e3e3918a2a0716dd3dc164c6860
evencoding <evencoding@gmail.com>
2022-11-11 23:33:08 +0900

    refactor(Lotto): InValid의 네이밍 형식을 Invalid로 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,7 +10,7 @@ class Lotto {
   }
 
   validate(numbers) {
-    Validator.throwErrorIfInValidWinningNumbers(numbers);
+    Validator.throwErrorIfInvalidWinningNumbers(numbers);
   }
 
   get winningNumbers() {
