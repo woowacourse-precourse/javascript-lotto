c29b806acdbc4698479fc28d774d8db29c62637b
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-11 14:19:44 +0900

    feat: LottoResult 클래스 hasEqualNumberCount, hasBonusNumber 메서드 추가

--- src/Components/LottoResult/LottoResult.js
+++ src/Components/LottoResult/LottoResult.js
@@ -1,18 +1,26 @@
 class LottoResult {
-  #sameNumberCount;
+  #matchedNumberCount;
   #isBonusNumberMatched;
 
-  constructor(sameNumberCount, isBonusNumberMatched) {
-    this.#sameNumberCount = sameNumberCount;
+  constructor(matchedNumberCount, isBonusNumberMatched) {
+    this.#matchedNumberCount = matchedNumberCount;
     this.#isBonusNumberMatched = isBonusNumberMatched;
   }
 
-  getSameNumberCount() {
-    return this.#sameNumberCount;
+  getMatchedNumberCount() {
+    return this.#matchedNumberCount;
   }
 
   isBonusNumberMatched() {
     return this.#isBonusNumberMatched;
+  }
+
+  hasEqualNumberCount(lottoResult) {
+    return this.#matchedNumberCount === lottoResult.getMatchedNumberCount();
+  }
+
+  hasBonusNumber(lottoResult) {
+    return this.#isBonusNumberMatched === lottoResult.isBonusNumberMatched();
   }
 }
 
