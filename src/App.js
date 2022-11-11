636e272386e3c9c8f1559157bd112d5d8fb44fda
Hwang Seong Taek <hst007@naver.com>
2022-11-12 00:11:30 +0900

    feat: 로또 구매를 위한 금액 입력 예외 처리 기능

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 const { Console } = require('@woowacourse/mission-utils');
-const { LOTTO_USER_INPUT } = require('./constants');
+const { LOTTO_USER_INPUT, LOTTO_ERROR_MESSAGE } = require('./constants');
 
 class App {
   play() {
     Console.readLine(LOTTO_USER_INPUT.PURCHASE_LOTTO, (purchaseLotto) => {
-      Console.print(purchaseLotto);
+      if (this.validateUserInput(purchaseLotto)) Console.print(purchaseLotto/1000);
     });
+  }
+
+  validateUserInput(purchaseLotto) {
+    if (isNaN(purchaseLotto)) throw new Error(LOTTO_ERROR_MESSAGE.NOT_NUMBER);
+    if (purchaseLotto % 1000 !== 0) throw new Error(LOTTO_ERROR_MESSAGE.NOT_DIVIDE);
+
+    return true;
   }
 }
 
