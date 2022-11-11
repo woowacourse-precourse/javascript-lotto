22ed722a884fe8fb73f57a97e75db621ce4e82a9
turtle601 <turtle601@naver.com>
2022-11-12 00:54:15 +0900

    refactor(LottoTest) Lotto 내부에서 1~45 확인하는 테스트 코드 제거

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,17 +15,6 @@ describe('로또 클래스 테스트', () => {
   });
 
   // 아래에 추가 테스트 작성 가능
-
-  test('로또 번호가 1 ~ 45사이가 아니면 예외가 발생한다.', () => {
-    expect(() => {
-      new Lotto([0, 1, 2, 3, 4, 5]);
-    }).toThrow('[ERROR]');
-
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 46]);
-    }).toThrow('[ERROR]');
-  });
-
   test('로토 번호가 정상적으로 입력되는지 테스트', () => {
     const input = [1, 2, 3, 4, 5, 6];
     const lotto = new Lotto(input);
