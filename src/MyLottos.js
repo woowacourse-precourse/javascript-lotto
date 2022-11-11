df0dc5986b56b3f9acd2f1fde950d0d684045525
leeuihyun <meta212121@naver.com>
2022-11-11 16:13:18 +0900

    feat : 오름차순으로 정렬된 랜덤 로또 배열 메서드

--- src/MyLottos.js
+++ src/MyLottos.js
@@ -2,7 +2,7 @@ const { Console, Random } = require('@woowacourse/mission-utils');
 const { MESSAGE, VALUE } = require('./lib/constants');
 
 class MyLottos {
-  #myLottos;
+  #myLottos = [];
 
   constructor(purchaseAccount) {
     this.validate(purchaseAccount);
@@ -18,6 +18,31 @@ class MyLottos {
 
   ConsolepurchaseAccount(purchaseAccount) {
     Console.print(MESSAGE.OUTPUT_PURCHASE_ACCOUNT(purchaseAccount));
+  }
+
+  makeRandomLotto() {
+    return Random.pickUniqueNumbersInRange(
+      VALUE.LOTTO_MIN_NUMBER,
+      VALUE.LOTTO_MAX_NUMBER,
+      VALUE.LOTTO_NUMBER_COUNT
+    );
+  }
+
+  sortRandomLotto(lottoArray) {
+    lottoArray.sort(function (a, b) {
+      return a - b;
+    });
+    return lottoArray;
+  }
+
+  makeMyLottos(purchaseAccount) {
+    while (purchaseAccount--) {
+      this.#myLottos.push(this.sortRandomLotto(this.makeRandomLotto()));
+    }
+  }
+
+  getMyLottos() {
+    return this.#myLottos;
   }
 }
 
