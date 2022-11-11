8a5b92bd407d84609be5f10aa6ab8eae602c6f6b
박정규 <srk99279@naver.com>
2022-11-11 10:57:50 +0900

    feat : 상수 추가
    상금 계산을 위한 금액 추가 및 출력 문구 수정

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+const MATCH_MSG = [
+  "당첨 통계\n---\n3개 일치 (5,000원) - ",
+  "4개 일치 (50,000원) - ",
+  "5개 일치 (1,500,000원) - ",
+  "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+  "6개 일치 (2,000,000,000원) - ",
+];
+
+const PAYOUT = [5000, 50000, 1500000, 30000000, 2000000000];
+
+const MIN_MATCH = 3;
+const MAX_MATCH = 7;
+
+module.exports = {
+  MATCH_MSG,
+  PAYOUT,
+  MIN_MATCH,
+  MAX_MATCH,
+};
