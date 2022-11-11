5a3906f33eb44d7b0ebebc0ca5cc8cecbeecfb1f
SNUPI <joohaem@daum.net>
2022-11-11 21:00:04 +0900

    feat: 구입금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const { Console } = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+
 class App {
-  play() {}
+  play() {
+    Console.readLine("구입금액을 입력해 주세요.\n", (price) => {}).bind(this);
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
