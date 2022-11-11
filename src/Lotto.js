3efc08b949eac056407d0d3f6e7cf2e745e25c82
yong <tkddbs1644@gmail.com>
2022-11-12 00:45:53 +0900

    feat(Lotto): 당첨 번호를 생성하는 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,15 @@
+const LOTTO = Object.freeze({
+  RANGE_MIN: 1,
+  RANGE_MAX: 45,
+  LENGTH: 6,
+});
+
+const ERRORS = Object.freeze({
+  LENGTH: "[ERROR] 로또 번호는 6개여야 합니다.",
+  DUPLICATION: "[ERROR] 로또 번호는 중복되지 않아야 합니다.",
+  RANGE: "[ERROR] 로또 번호는 1부터 45까지여야 합니다.",
+});
+
 class Lotto {
   #numbers;
 
@@ -7,12 +19,31 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    this.checkLength(numbers);
+    this.checkDuplication(numbers);
+    this.checkRange(numbers);
+  }
+
+  checkLength(numbers) {
+    if (numbers.length !== LOTTO.LENGTH) {
+      throw new Error(ERRORS.LENGTH);
     }
   }
 
-  // TODO: 추가 기능 구현
+  checkDuplication(numbers) {
+    const numberSet = new Set(numbers);
+    if ([...numberSet].length !== LOTTO.COUNT) {
+      throw new Error(ERRORS.DUPLICATION);
+    }
+  }
+
+  checkRange(numbers) {
+    numbers.forEach((number) => {
+      if (number < LOTTO.RANGE_MIN || number > LOTTO.RANGE_MAX) {
+        throw new Error(ERRORS.DUPLICATION);
+      }
+    });
+  }
 }
 
 module.exports = Lotto;
