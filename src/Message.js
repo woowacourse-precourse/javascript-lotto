853a645ab9761766316de1d146a2201317108434
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-11 21:19:12 +0900

    feat: 게임 진행 관련 메세지

---
+++ src/Message.js
@@ -0,0 +1,12 @@
+const MESSAGE = Object.freeze({
+    INPUT: {
+        PURCHASE_AMOUNT: '구입금액을 입력해 주세요.',
+        WINNING_NUMBER: '당첨 번호를 입력해 주세요.',
+        BONUS: '보너스 번호를 입력해 주세요.',
+    },
+    OUTPUT: {
+        PURCHASE_COUNT: '개를 구매했습니다.',
+    }
+});
+
+module.exports = MESSAGE;
