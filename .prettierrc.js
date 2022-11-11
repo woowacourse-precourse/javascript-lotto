8d01479b3f5a3436c2df8415ace89589a99c8903
Cobocho <rlaalsrb1111@naver.com>
2022-11-11 22:01:45 +0900

    chore: `prettierrc.js` tabWidth 변경

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,7 @@ module.exports = {
   // 문자열은 singleQuote로 ("" -> '')
   semi: true,
   //코드 마지막에 세미콜론이 있게 formatting
-  tabWidth: 4,
+  tabWidth: 2,
   // 들여쓰기 너비는 4칸
   trailingComma: 'all',
   // 배열 키:값 뒤에 항상 콤마를 붙히도록 formatting
