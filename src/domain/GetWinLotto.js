f98360c9d1f9f9dee0cd24ce7ce12ec35ea135d0
lee-il-woo <empty001430@gmail.com>
2022-11-12 04:21:47 +0900

    Style:코드 포맷팅

--- src/domain/GetWinLotto.js
+++ src/domain/GetWinLotto.js
@@ -1,36 +1,44 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Exceptions = require("./Exception");
 let exception = new Exceptions();
-class GetWinLotto{
-    constructor(){
-        this.lottoNumbers = [];
-        this.bonusNumber = 0;
-    }
+class GetWinLotto {
+  constructor() {
+    this.lottoNumbers = [];
+    this.bonusNumber = 0;
+  }
 
-    getLottoNumbers(){
-        MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.', (lottoNumbers) => {
-            this.lottoNumbers = lottoNumbers.split(',').map((index) => Number(index))
-            exception.checkInputCount(this.lottoNumbers)
-            this.lottoNumbers.map((index)=>{
-                exception.checkInputNotNumber(index)
-            })
-            exception.checkInputDuplicate(this.lottoNumbers)
-            this.lottoNumbers.map((index)=>{
-                exception.checkInputRange(index)
-            })
-            return this.getBonusNumbers();
-          });
-    }
+  getLottoNumbers() {
+    MissionUtils.Console.readLine(
+      "당첨 번호를 입력해 주세요.",
+      (lottoNumbers) => {
+        this.lottoNumbers = lottoNumbers
+          .split(",")
+          .map((index) => Number(index));
+        exception.checkInputCount(this.lottoNumbers);
+        this.lottoNumbers.map((index) => {
+          exception.checkInputNotNumber(index);
+        });
+        exception.checkInputDuplicate(this.lottoNumbers);
+        this.lottoNumbers.map((index) => {
+          exception.checkInputRange(index);
+        });
+        return this.getBonusNumbers();
+      }
+    );
+  }
 
-    getBonusNumbers(){
-        MissionUtils.Console.readLine('보너스 번호를 입력해 주세요.', (bonusNumber) => {
-            this.bonusNumber = Number(bonusNumber);
-            exception.checkInputNotNumber(this.bonusNumber);
-            exception.checkInputRange(this.bonusNumber);
-            exception.checkBonusInLotto(this.bonusNumber,this.lottoNumbers)
-          });
-    }
+  getBonusNumbers() {
+    MissionUtils.Console.readLine(
+      "보너스 번호를 입력해 주세요.",
+      (bonusNumber) => {
+        this.bonusNumber = Number(bonusNumber);
+        exception.checkInputNotNumber(this.bonusNumber);
+        exception.checkInputRange(this.bonusNumber);
+        exception.checkBonusInLotto(this.bonusNumber, this.lottoNumbers);
+      }
+    );
+  }
 }
 
-let getWinLotto = new GetWinLotto()
-console.log(getWinLotto.getLottoNumbers())
+let getWinLotto = new GetWinLotto();
+console.log(getWinLotto.getLottoNumbers());
