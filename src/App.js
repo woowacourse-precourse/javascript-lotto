6b796ce2ab3a2d4e119ec31e52c98f873d0ede54
2wndrhs <2wndrhs@gmail.com>
2022-11-10 23:11:06 -0800

    feat(lotto): 로또 구입 금액을 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { PURCHASE } = require('./constants');
+
 class App {
-  play() {}
+  #purchaseAmout = 0;
+
+  play() {
+    this.askPurchaseAmount();
+  }
+
+  askPurchaseAmount() {
+    Console.readLine(PURCHASE.INPUT, answer => {
+      this.#purchaseAmout = answer;
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const PURCHASE = Object.freeze({
+  INPUT: '구입금액을 입력해 주세요.\n',
+});
+
+module.exports = {
+  PURCHASE,
+};
