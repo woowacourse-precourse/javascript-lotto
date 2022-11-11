25d12686f36aed41a76a88f1aa81313c68408406
yeopto <gunyeop530@gmail.com>
2022-11-11 22:07:53 +0900

    feat: 생성한 로또번호에 중복된 수가 있으면 예외 처리한다

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,17 +2,23 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.lottoLengthValidate(numbers);
+    this.lottoOverlapNumValidate(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
+  lottoLengthValidate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  lottoOverlapNumValidate(numbers) {
+    const set = new Set(numbers);
+    if (set.size < numbers.length) {
+      throw new Error("[ERROR] 로또 번호에 중복되는 숫자가 있습니다.");
+    }
+  }
 }
 
 module.exports = Lotto;
