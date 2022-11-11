2f71ebde2ea7aa164be7e8186480e211538c35e7
yan9vvoojin <yan9vvoojin@naver.com>
2022-11-11 13:21:22 +0900

    feat(App): 구입 금액이 유효한지 확인하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -10,7 +10,17 @@ class App {
   requestMoneyInput() {
     Console.readLine('구입 금액을 입력해 주세요.\n', (input) => {
       const money = Number(input);
+      this.validateMoney(money);
     });
+  }
+
+  validateMoney(money) {
+    if (Number.isNaN(money)) {
+      throw new Error('[ERROR] 구입 금액은 숫자만 입력해 주세요.');
+    }
+    if (!Number.isInteger(money / 1000)) {
+      throw new Error('[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요.');
+    }
   }
 }
 
