4ef04638d2ffeb4f61f5d1f65b645ea23dbdfb07
yeongi <cbg5255@gmail.com>
2022-11-11 13:11:09 +0900

    delete: LottoTest 로또 출력 테스트 삭제

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,12 +1,6 @@
 const Lotto = require("../src/Lotto");
 const MissionUtils = require("@woowacourse/mission-utils");
 
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
-  logSpy.mockClear();
-  return logSpy;
-};
-
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
@@ -24,25 +18,5 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new Lotto([101, 1, 2, 3, 4, 5]);
     }).toThrow("[ERROR]");
-  });
-
-  test("로또 번호 출력 하기", () => {
-    const logSpy = getLogSpy();
-    const lottos = [
-      [1, 2, 3, 4, 5, 6],
-      [2, 3, 4, 5, 6, 7],
-      [6, 11, 17, 27, 35, 42],
-    ];
-    const logs = [
-      "[1, 2, 3, 4, 5, 6]",
-      "[2, 3, 4, 5, 6, 7]",
-      "[6, 11, 17, 27, 35, 42]",
-    ];
-
-    lottos.forEach((numbers) => new Lotto(numbers).printNumbers());
-
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
   });
 });
