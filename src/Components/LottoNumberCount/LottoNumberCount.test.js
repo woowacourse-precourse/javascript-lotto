71cf790565638f58cb46d27a8bb8880665e4d9f2
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-11 19:33:05 +0900

    test: LottoNumberCount 클래스 isSame 메서드 테스트 추가

---
+++ src/Components/LottoNumberCount/LottoNumberCount.test.js
@@ -0,0 +1,19 @@
+const LottoNumberCount = require('./LottoNumberCount');
+
+describe('LottoNumberCount 클래스 테스트', () => {
+  test('로또 번호와 일치하는 당첨 번호의 개수와 당첨 조건 개수를 비교한다.', () => {
+    const lottoNumberCount = new LottoNumberCount(6);
+    const winningLottoNumberCount = new LottoNumberCount(6);
+    const isSame = lottoNumberCount.isSame(winningLottoNumberCount);
+
+    expect(isSame).toBe(true);
+  });
+
+  test('로또 번호와 일치하는 당첨 번호의 개수와 당첨 조건 개수를 비교한다.', () => {
+    const lottoNumberCount = new LottoNumberCount(6);
+    const winningLottoNumberCount = new LottoNumberCount(5);
+    const isSame = lottoNumberCount.isSame(winningLottoNumberCount);
+
+    expect(isSame).toBe(false);
+  });
+});
