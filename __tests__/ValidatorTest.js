3baf563603c524d4dd8a345068ebccbe9ce6c9ae
Cobocho <rlaalsrb1111@naver.com>
2022-11-11 20:57:21 +0900

    test: 숫자 여부 판별 테스트 작성 및 에러 출력 상세화

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -6,9 +6,23 @@ describe("amountValidCheck 테스트", () => {
     const nonBlankInput = ['1341', '23134', 'dsads ass'];
     const blank = '';
     nonBlankInput.forEach(subject => {      
-      const result = Validator.amountValidCheck(subject);
-      expect(result).toBeFalsy();
+      const result = Validator.isBlank(subject);
+      expect(result).toBeUndefined();
     });
-    expect(() => Validator.amountValidCheck(blank)).toThrowError();
+    expect(() => Validator.isBlank(blank)).toThrowError(MESSAGES.ERROR.isBlank);
+  });
+  test("숫자여부 판별", () => {
+    const numbers = ['1341', '23134', '1243124141', '1'];
+    const strings = ['@#!$!', 'asasda', '한글', 'dda>?', '123'];
+    numbers.forEach(subject => {      
+      const result = Validator.isNotNumber(subject);
+      expect(result).toBeUndefined();
+    });
+    const string = 'oneTwo';
+    const specials = '@!#!@$>';
+    const mixed = '123abc';
+    expect(() => Validator.isNotNumber(string)).toThrowError(MESSAGES.ERROR.isNotNumber);
+    expect(() => Validator.isNotNumber(specials)).toThrowError(MESSAGES.ERROR.isNotNumber);
+    expect(() => Validator.isNotNumber(mixed)).toThrowError(MESSAGES.ERROR.isNotNumber);
   });
 });
