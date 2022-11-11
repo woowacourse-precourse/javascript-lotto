545742f2b510447f8fc1b9c89b067178085435fd
somin <smhs12@naver.com>
2022-11-11 20:03:32 +0900

    feat: 보너스 번호 입력 값에 대한 테스트

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,4 +1,7 @@
-const { isValidateUserInput } = require("../src/utils/validator");
+const {
+  isValidateUserInput,
+  isValidateBonusNumber,
+} = require("../src/utils/validator");
 
 describe("isValidateUserInput 함수 테스트", () => {
   test("공백 입력 테스트", () => {
@@ -20,5 +23,35 @@ describe("isValidateUserInput 함수 테스트", () => {
     expect(() => {
       isValidateUserInput(amount);
     }).toThrow();
+  });
+});
+
+describe("isValidateBonusNumber 함수 테스트", () => {
+  test("보너스 번호 입력 값이 숫자가 아닌지 테스트", () => {
+    let bonusNumber = "2,7";
+    expect(() => {
+      isValidateBonusNumber(bonusNumber);
+    }).toThrow("[ERROR] 숫자가 아닙니다.");
+  });
+
+  test("보너스 번호 입력 값이 1-45 사이인지 테스트", () => {
+    let bonusNumber = "-1";
+    expect(() => {
+      isValidateBonusNumber(bonusNumber);
+    }).toThrow("[ERROR] 1-45 사이의 숫자가 아닙니다.");
+  });
+
+  test("공백 입력할 경우 테스트", () => {
+    let bonusNumber = " ";
+    expect(() => {
+      isValidateBonusNumber(bonusNumber);
+    }).toThrow("[ERROR] 아무 것도 입력하지 않았습니다.");
+  });
+
+  test("보너스 번호 입력 값이 숫자인지 테스트", () => {
+    let bonusNumber = "1";
+    expect(() => {
+      isValidateBonusNumber(bonusNumber);
+    }).toBeTruthy;
   });
 });
