d10da2186b2e2b340e67bce35bb29249bf9df8f7
dev-redo <mis05041@naver.com>
2022-11-12 00:18:27 +0900

    Feat: game view 인터페이스 정의

---
+++ src/view/IGameView.js
@@ -0,0 +1,15 @@
+const IGameView = class {
+  static input() {
+    throw Error('메서드구현필요');
+  }
+
+  static output() {
+    throw Error('메서드구현필요');
+  }
+
+  static close() {
+    throw Error('메서드구현필요');
+  }
+};
+
+export default IGameView;
