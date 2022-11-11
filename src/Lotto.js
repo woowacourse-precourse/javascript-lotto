4038348626baafb2f33e5bc6b06b42e59244063e
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-11 16:02:11 +0900

    refactor(Model): 당첨 번호 타당겅 감사 함수를 클래스로부터 분리

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,40 @@
+function checkRange(arr) {
+  if (/^[0-9]*$/g.test(arr.join("")) === false) {
+    return false;
+  }
+}
+function checkComma(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === ``) {
+      return false;
+    }
+  }
+}
+
+function checkNumLength(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 45 || arr[i] < 1) {
+      return false;
+    }
+  }
+}
+
+function checkCount(arr) {
+  if (arr.length !== 6) {
+    return false;
+  }
+}
+
+function checkDuplication(arr) {
+  let checkarr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (checkarr.includes(arr[i])) {
+      return false;
+    }
+    checkarr.push(arr[i]);
+  }
+}
+
 class Lotto {
   #numbers;
 
@@ -6,86 +43,50 @@ class Lotto {
   }
 
   validateMoney(numbers) {
-    if (numbers % 1000 !== 0) {
+    this.#numbers = numbers;
+    if (this.#numbers % 1000 !== 0) {
       throw new Error(`[ERROR] 돈은 1000원 단위로 입력이 가능합니다.`);
     }
-    if (numbers <= 0) {
+    if (this.#numbers <= 0) {
       throw new Error(`[ERROR] 돈은 1000원부터 입력이 가능합니다.`);
     }
   }
 
   validateWinningNum(winningNum) {
     this.#numbers = winningNum;
-    if (this.checkRange(winningNum) === false) {
+    if (checkRange(this.#numbers) === false) {
       throw new Error(`[ERROR] 0은 입력이 불가능합니다.`);
     }
 
-    if (this.checkComma(winningNum) === false) {
+    if (checkComma(this.#numbers) === false) {
       throw new Error(`[ERROR] ,이 연속으로 입력되었습니다.`);
     }
 
-    if (this.checkNumLength(winningNum) === false) {
+    if (checkNumLength(this.#numbers) === false) {
       throw new Error(`[ERROR] 당첨 번호는 1~45 사이에 있습니다.`);
     }
-    if (this.checkCount(winningNum) === false) {
+    if (checkCount(this.#numbers) === false) {
       throw new Error(`[ERROR] 당첨 번호는 6개만 입력이 가능합니다.`);
     }
-    if (this.checkDuplication(winningNum) === false) {
+    if (checkDuplication(this.#numbers) === false) {
       throw new Error(`[ERROR] 당첨 번호에 중복이 있을 수 없습니다.`);
     }
   }
 
-  validateBonusNum(bonuseNum) {
+  validateBonusNum(bonuseNum, winningNum) {
     this.#numbers = bonuseNum;
-    if (bonuseNum > 45 || bonuseNum < 1) {
+    if (this.#numbers > 45 || this.#numbers < 1) {
       throw new Error(`[ERROR] 1~45사이의 번호를 입력해주세요`);
     }
-    if (bonuseNum) {
-      if (/^[0-9]*$/g.test(bonuseNum) === false) {
+    if (this.#numbers) {
+      if (/^[0-9]*$/g.test(this.#numbers) === false) {
         throw new Error(`[ERROR] 숫자만을 입력해주세요`);
       }
     }
-    if (this.winningNum.includes(bonuseNum) === true) {
+    if (winningNum.includes(this.#numbers) === true) {
       throw new Error(
         `[ERROR] 당첨번호에 입력한 숫자를 보너스 번호에 입력할 수 없습니다.`
       );
-    }
-  }
-
-  checkRange(arr) {
-    if (/^[0-9]*$/g.test(arr.join("")) === false) {
-      return false;
-    }
-  }
-  checkComma(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === ``) {
-        return false;
-      }
-    }
-  }
-
-  checkNumLength(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > 45 || arr[i] < 1) {
-        return false;
-      }
-    }
-  }
-
-  checkCount(arr) {
-    if (arr.length !== 6) {
-      return false;
-    }
-  }
-
-  checkDuplication(arr) {
-    let checkarr = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (checkarr.includes(arr[i])) {
-        return false;
-      }
-      checkarr.push(arr[i]);
     }
   }
 }
