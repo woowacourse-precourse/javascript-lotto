b3d1ea0064c8909475201e9dcf1eeb58f76938d0
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-12 01:07:03 +0900

    refactor($app): implement validate functions for input values validation functions of money, winningNumber, bonusNumber

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const Lotto = require('./Lotto');
-const { printError, isPositiveInteger, isDuplicated } = require('./Utils');
+const {
+  printError,
+  isPositiveInteger,
+  isDuplicated,
+  isIncludeInRange,
+} = require('./Utils');
 const {
   LOTTO_END,
   LOTTO_PRICE,
@@ -34,15 +39,47 @@ class App {
     this.#revenue = 0;
   }
 
-  #validate(money) {
-    if (!isPositiveInteger(money)) {
+  #validateMoney() {
+    if (!isPositiveInteger(this.#money)) {
       printError('금액을 숫자로 입력해주세요.');
     }
-    if (money % LOTTO_PRICE !== 0) {
+    if (this.#money % LOTTO_PRICE !== 0) {
       printError('1000원 단위로 입력해주세요.');
     }
 
     return true;
+  }
+
+  // FIXME: 15줄 아래로 줄일 것
+  #validateWinningNumbers() {
+    if (this.#winningNumber.length !== LOTTO_NUMBER_COUNT) {
+      printError('당첨 번호 6개를 입력해 주세요.');
+    }
+    if (isDuplicated(this.#winningNumber))
+      printError('당첨 번호가 중복되었습니다.');
+
+    this.#winningNumber.forEach((number) => {
+      if (
+        !isPositiveInteger(number) ||
+        !isIncludeInRange(number, LOTTO_START, LOTTO_END)
+      ) {
+        printError('당첨 번호는 숫자만 입력해주세요.');
+      }
+    });
+
+    return true;
+  }
+
+  #validateBonusNumber() {
+    if (
+      !isPositiveInteger(this.#bonusNumber) ||
+      !isIncludeInRange(this.#bonusNumber, LOTTO_START, LOTTO_END)
+    ) {
+      printError('1부터 45 사이의 숫자를 입력해주세요.');
+    }
+    if (isDuplicated([...this.#winningNumber, this.#bonusNumber])) {
+      printError('당첨 번호와 보너스 번호가 중복되었습니다.');
+    }
   }
 
   #publishLotto() {
@@ -100,23 +137,19 @@ class App {
 
   #getBonusNumber() {
     ui.input('보너스 번호를 입력해 주세요.\n', (bonusNumber) => {
-      if (!isPositiveInteger(bonusNumber)) {
-        printError('양의 정수만 입력해주세요.');
-      }
       this.#bonusNumber = Number(bonusNumber);
-      this.#matchLotto();
-      this.#printResult();
-      ui.end();
+      if (this.#validateBonusNumber(bonusNumber)) {
+        this.#matchLotto();
+        this.#printResult();
+        ui.end();
+      }
     });
   }
 
   #getWinningNumber() {
     ui.input('당첨 번호를 입력해 주세요.\n', (winningNumber) => {
       this.#winningNumber = winningNumber.split(',').map(Number);
-      if (isDuplicated(this.#winningNumber)) {
-        printError('당첨 번호가 중복되었습니다.');
-      }
-      this.#getBonusNumber();
+      if (this.#validateWinningNumbers()) this.#getBonusNumber();
     });
   }
 
@@ -127,8 +160,8 @@ class App {
 
   #getMoney() {
     ui.input('구입금액을 입력해 주세요.\n', (money) => {
-      if (this.#validate(money)) {
-        this.#money = money;
+      this.#money = money;
+      if (this.#validateMoney()) {
         this.#startLotto();
       }
     });
