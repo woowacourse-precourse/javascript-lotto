64dd5b90aca40eb4ef4bb53d92216f3414ce22b0
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-11 22:12:12 +0900

    feat($ui): add UI class for methods related to UI

---
+++ src/UI.js
@@ -0,0 +1,17 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+class UI {
+  print(message) {
+    MissionUtils.Console.print(message);
+  }
+
+  input(query, callback) {
+    MissionUtils.Console.readLine(query, callback);
+  }
+
+  printError(message) {
+    throw Error(`[ERROR] ${message}`);
+  }
+}
+
+module.exports = UI;
