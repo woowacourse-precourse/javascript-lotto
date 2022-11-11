536da4870af30d3cef13078ac1e328faaaba0c23
yeongi <cbg5255@gmail.com>
2022-11-11 19:00:56 +0900

    feature : LottoSetting 로또 번호 확인 기능 구현

--- src/LottoSetting.js
+++ src/LottoSetting.js
@@ -1,8 +1,26 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { INPUT } = require("./lib/library");
+const Lotto = require("./Lotto");
 
 class LottoSetting {
-  inputWinLottoNum() {}
-  exportWinLottoArray() {}
+  inputWinLottoNum() {
+    Console.readLine(INPUT.WIN_NUMBER);
+  }
+
+  isLottoNum(array) {
+    let lotto;
+    try {
+      lotto = new Lotto(array);
+    } catch (e) {
+      throw e;
+    }
+    return lotto;
+  }
+
+  exportWinLottoArray(string) {
+    return string.split(",");
+  }
   inputBonusNum() {}
 }
+
+module.exports = LottoSetting;
