db046efd16094e7216e3e22d21631b02d81f5f0c
wang-yurin <king_1994@naver.com>
2022-11-11 19:37:24 +0900

    feat: 천원 단위 확인 함수 구현

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ class App {
 }
 
 function inputMoney() {
-  MissionUtils.Console.readLine(`구입금액을 입력해 주세요.\n`, (answer) => {
-    MissionUtils.Console.print(answer);
+  MissionUtils.Console.readLine(`구입금액을 입력해 주세요.\n`, (money) => {
+    thousandValidate(money);
+    MissionUtils.Console.print(money);
   });
+}
+
+function thousandValidate(money) {
+  if (money % 1000 !== 0) {
+    throw new Error("[ERROR] 1000원 단위의 금액을 입력해주세요.");
+  }
 }
 
 const app = new App();
