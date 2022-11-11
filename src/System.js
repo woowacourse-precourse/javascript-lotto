0941fa982c92b630fa977650b66d5c11cdfdcdb2
Narcoker <jykim9868@gmail.com>
2022-11-11 19:45:48 +0900

    Feat: 로또 구입 가격 유효성 검사 기능 추가

--- src/System.js
+++ src/System.js
@@ -1,11 +1,18 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-
+const { ERROR } = require('./Constants')
 const SYSTEM = Object.freeze({
     print(message) {
         MissionUtils.Console.print(message);
     },
+
+    isCorrectCache(cache) {
+        if (cache % 1000) {
+            throw new Error(ERROR.INVAID_CACHE);
+        }
+    },
+
     exit() {
         MissionUtils.Console.close();
     }
 })
-module.exports = { SYSTEM };
+module.exports = { MissionUtils, SYSTEM };
