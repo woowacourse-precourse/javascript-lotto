d09917fa2cf20cfcf03ddc4fe1cf87f54d28406f
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-11 18:31:09 +0900

    refactor(BonusNumber): NUMBER_RANGE 상수화

--- src/model/BonusNumber.js
+++ src/model/BonusNumber.js
@@ -1,3 +1,5 @@
+const { NUMBER_RANGE } = require('../utils/constants');
+
 class BonusNumber {
   #value;
 
@@ -12,18 +14,18 @@ class BonusNumber {
 
   validate(value) {
     this.validateIsNumber(value);
-    this.validateNumberRange(1, 45, value);
+    this.validateNumberRange(value);
   }
 
   validateIsNumber(value) {
     if (Number.isNaN(value)) {
-      throw new Error('[ERROR] 로또 번호는 1이상 45이하 입니다.');
+      throw new Error('[ERROR] 로또 번호는 숫자만 입력 가능합니다.');
     }
   }
 
-  validateNumberRange(start, end, number) {
-    if (number < start || number > end) {
-      throw new Error('[ERROR] 로또 번호는 1이상 45이하 입니다.');
+  validateNumberRange(number) {
+    if (number < NUMBER_RANGE.START || number > NUMBER_RANGE.END) {
+      throw new Error(`[ERROR] 로또 번호는 ${NUMBER_RANGE.START}이상 ${NUMBER_RANGE.END}이하 입니다.`);
     }
   }
 }
