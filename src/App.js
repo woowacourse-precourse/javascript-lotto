f80824497ef10e40404d904cbcf212758af02e3e
haesa <xdhaesa@gmail.com>
2022-11-11 21:21:48 +0900

    feat(App.js): 로또 구입 금액 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 class App {
+  constructor() {}
+
+  validate(purchaseAmount) {
+    if (/[^0-9]/.test(purchaseAmount)) {
+      throw new Error('[ERROR] 구입 금액은 숫자여야 합니다.');
+    }
+
+    if (Number(purchaseAmount) % 1000 !== 0) {
+      throw new Error('[ERROR] 구입 금액은 1000단위여야 합니다.');
+    }
+  }
+
   play() {}
 }
 
