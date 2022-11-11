7bacf3f45cda48387d7fa40d09f1ba68e348ac95
Jeong Hun Nam <njh3421@gmail.com>
2022-11-11 16:17:44 +0900

    Implement (Lotto.js):implement code expect err

--- src/Lotto.js
+++ src/Lotto.js
@@ -16,18 +16,16 @@ class Lotto {
 
   // TODO: 추가 기능 구현
   setComputerRandomNumber() {
-    const computerNumbers = MissionUtils.Random.pickUniqueNumbersInRange(
-      1,
-      45,
-      6
-    );
+    const computerNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1,45,6);
     return computerNumbers;
   }
+
   getUserInputMoney() {
     MissionUtils.Console.readLine("구입금액을 입력해주세요.\n", (money) => {
       this.createLottoNumArrays(money);
     });
   }
+
   getUserLottoNumber(computerNumberArray) {
     MissionUtils.Console.readLine(
       "당첨 번호를 입력해 주세요.\n",
@@ -36,6 +34,7 @@ class Lotto {
       }
     );
   }
+
   getUserBonusNumber(computerNumberArray, userLottoNumber) {
     MissionUtils.Console.readLine(
       "보너스 번호를 입력해 주세요.\n",
@@ -57,21 +56,25 @@ class Lotto {
     this.showAmountOfMoney(amountOfMoney);
     this.showLottoArrays(computerNumberArray);
   }
+
   showAmountOfMoney(amount) {
     MissionUtils.Console.print(`${amount}개를 구매했습니다.`);
   }
+  
   divideMoney(money) {
     // 테스트 케이스 작성
     // 1000보다 작을경우, 나누어떨어지지 않을경우, 숫자 앞자리가 0일경우, 문자가 있을경우,음수일경우
     let quotient = money / 1000;
     return quotient;
   }
+
   showLottoArrays(computerNumberArray) {
     computerNumberArray.forEach((e) => {
       MissionUtils.Console.print(e);
     });
     this.getUserLottoNumber(computerNumberArray);
   }
+
   calculatePrizeLottery(computerNumberArray, userLottoNumber, bonusMoney) {
     let userLottoArray = userLottoNumber.split(",").map((e) => Number(e));
     let countArray = [];
@@ -80,7 +83,7 @@ class Lotto {
       counted = this.checkLottery(eachNumberArray, userLottoArray, bonusMoney);
       countArray.push(counted);
     });
-    this.showPrizeLottery(countArray);
+    this.checkPrizeAmount(countArray);
   }
   checkLottery(eachNumberArray, userLottoArray, bonusMoney) {
     let count = 0;
@@ -96,33 +99,26 @@ class Lotto {
     }
     return count + checkBonus;
   }
-  
+
   checkLotteryHelper(eachNumberArray, bonusMoney) {
     if (eachNumberArray.includes(bonusMoney)) {
       return 2;
     }
   }
-  showPrizeLottery(countArray) {
-    switch (countArray) {
-      case 3:
-        MissionUtils.Console.print(`3개 일치 (5000원) - ${countArray}개`)
-        break;
-      case 4:
-        MissionUtils.Console.print(`4개 일치 (50,000원) - ${countArray}개`)
-        break;
-      case 5:
-        MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${countArray}개`)
-        break;
-      case 6:
-        MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${countArray}개`)
-        break;
-        case 7:
-        MissionUtils.Console.print(`5개 일치,보너스 볼 일치 (3,000,000원) - ${countArray}개`)
-        break;
-      default:
-        MissionUtils.Console.print('당첨된게 없습니다.')
-        MissionUtils.Console.close()
+  checkPrizeAmount(countArray){
+    let prizeObj={}
+    for(const val of countArray){
+      prizeObj[val]=(prizeObj[val]||0)+1
     }
+    this.showPrizeLottery(prizeObj)
+  }
+  showPrizeLottery(prizeObj) {
+    MissionUtils.Console.print(`3개 일치 (5000원) - ${prizeObj['3'] ? prizeObj['3'] : 0}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${prizeObj['4'] ? prizeObj['4'] : 0}개`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${prizeObj['5'] ? prizeObj['5'] : 0}개`);
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${prizeObj['6'] ? prizeObj['6'] : 0}개`);
+    MissionUtils.Console.print(`5개 일치,보너스 볼 일치 (3,000,000원) - ${prizeObj['7'] ? prizeObj['7'] : 0}개`);
+    MissionUtils.Console.close()
   }
 }
 
