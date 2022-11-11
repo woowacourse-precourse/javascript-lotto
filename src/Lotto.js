1a9857abfd9774f85b5505c489ad50fceed02e4d
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-12 03:38:54 +0900

    fix: `Lotto` 클래스의 버그 수정
    - `matchNumbers()` 메서드에서 `this.#numbers`가 아닌 `this.number`에 접근하던 문제 수정
    - `this.prizes` 객체의 value 값을 `String` 타입에서 `Number` 타입으로 수정
    - `Number.prototype.toLocaleString()` 메서드 이용하여 금액 포맷팅

--- src/Lotto.js
+++ src/Lotto.js
@@ -5,12 +5,14 @@ class Lotto {
     this.validate(numbers);
     this.#numbers = numbers;
     this.prizes = Object.freeze({
-      꽝: '0',
-      '3개 일치': '5,000',
-      '4개 일치': '50,000',
-      '5개 일치': '1,500,000',
-      '5개 일치, 보너스 볼 일치': '30,000,000',
-      '6개 일치': '2,000,000,000',
+      '0개 일치': 0,
+      '1개 일치': 0,
+      '2개 일치': 0,
+      '3개 일치': 5000,
+      '4개 일치': 50000,
+      '5개 일치': 1500000,
+      '5개 일치, 보너스 볼 일치': 30000000,
+      '6개 일치': 2000000000,
     });
   }
 
@@ -21,7 +23,7 @@ class Lotto {
   }
 
   matchNumbers(winningNumbers, bonusNumber) {
-    const { numbers } = this;
+    const numbers = this.#numbers;
     const intersection = numbers.filter((num) => winningNumbers.has(num));
 
     this.matchCount = intersection.length;
@@ -29,14 +31,15 @@ class Lotto {
   }
 
   getMatchInformation() {
-    const { matchCount, hasBonus } = this;
+    const { matchCount, hasBonus, prizes } = this;
     let matchInfo = `${matchCount}개 일치`;
 
     if (matchCount === 5 && hasBonus) {
       matchInfo += ', 보너스 볼 일치';
     }
     this.prize = prizes[matchInfo];
-    this.matchInfo = `${matchInfo} (${this.prize}원)`;
+    this.formatPrize = this.prize.toLocaleString('ko-KR');
+    this.matchInfo = `${matchInfo} (${this.formatPrize}원)`;
   }
 }
 
