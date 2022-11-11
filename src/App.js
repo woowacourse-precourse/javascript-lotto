1b45d4af30655202353e7178c22dece3c4110656
nsyeon <lovepny@gmail.com>
2022-11-11 21:51:05 +0900

    feat(App): 함수 등록

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const USER = require("../src/User");
+
 class App {
-  play() {}
+  play() {
+    const User = new USER();
+    
+    let price = 0;
+    let number = 0;
+    let lottoNumber = [];
+
+    price = User.enterLottoBuyPrice();
+    number = User.lottoNumberOfPapers(price);
+    lottoNumber = User.createLottoNumber(number);
+  }
 }
 
 module.exports = App;
