2e66cc5d4c5f770641708fd79f5fead67caf5975
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-11-12 00:43:19 +0900

    Refactor: seperate inputWinNum function

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ class App {
   play() {
     MissionUtils.Console.readLine(LOTTO_MESSAGE.INPUT_MONEY_MSG, (money) => {
       this.buyLotto(money);
-      this.inputWinNum();
+      MissionUtils.Console.readLine(
+        LOTTO_MESSAGE.INPUT_WIN_NUM_MSG,
+        (winNums) => {
+          this.inputWinNum(winNums);
+        }
+      );
     });
   }
 
@@ -18,17 +23,10 @@ class App {
     Lotto.printLottoNumArr(this.lottoNumArr);
   }
 
-  inputWinNum() {
-    MissionUtils.Console.readLine(
-      LOTTO_MESSAGE.INPUT_WIN_NUM_MSG,
-      (numbers) => {
-        const winNum = numbers.split(",").map((number) => Number(number));
-        const lotto = new Lotto(winNum);
-      }
-    );
+  inputWinNum(winNums) {
+    const numbers = winNums.split(",").map((number) => Number(number));
+    const lotto = new Lotto(numbers);
   }
 }
-
-new App().play();
 
 module.exports = App;
