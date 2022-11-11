367d63b85bfd6609171ca63a87f315ee081ce303
Suyoung <jsy8492@gmail.com>
2022-11-11 17:17:23 +0900

    fix(error): 각 에러 메시지의 타이틀을 TITLE_INFO 객체에서 받아올 수 있도록 수정

--- src/lib/constants/error.js
+++ src/lib/constants/error.js
@@ -2,31 +2,34 @@ const ERROR_TAG = '[ERROR]';
 const NOT_NUMBER_TEMPLATE = `에 숫자가 아닌 문자가 존재합니다.`;
 const OUT_OF_BOUND_TEMPLATE = `는 1부터 45까지의 숫자여야 합니다.`;
 
+const TITLE_INFO = {
+  PURCHASE_AMOUNT: `구입 금액`,
+  WINNING_NUMBERS: `당첨 번호`,
+  BONUS_NUMBER: `보너스 번호`,
+  LOTTO: `로또 번호`,
+};
+
 const ERROR = {
   PURCHASE_AMOUNT: {
-    TITLE: `구입 금액`,
-    NOT_NUMBER: `${ERROR_TAG} ${TITLE}${NOT_NUMBER_TEMPLATE}`,
-    SMALLER: `${ERROR_TAG} ${TITLE}은 1,000원 이상이어야 합니다.`,
-    CANNOT_BE_DIVIDED: `${ERROR_TAG} ${TITLE}은 1,000으로 나누어 떨어지는 숫자여야 합니다.`,
+    NOT_NUMBER: `${ERROR_TAG} ${TITLE_INFO.PURCHASE_AMOUNT}${NOT_NUMBER_TEMPLATE}`,
+    SMALLER: `${ERROR_TAG} ${TITLE_INFO.PURCHASE_AMOUNT}은 1,000원 이상이어야 합니다.`,
+    CANNOT_BE_DIVIDED: `${ERROR_TAG} ${TITLE_INFO.PURCHASE_AMOUNT}은 1,000으로 나누어 떨어지는 숫자여야 합니다.`,
   },
   WINNING_NUMBERS: {
-    TITLE: `당첨 번호`,
-    NOT_NUMBER: `${ERROR_TAG} ${TITLE}${NOT_NUMBER_TEMPLATE}`,
-    NOT_LOTTO_LENGTH: `${ERROR_TAG} ${TITLE}의 개수는 로또 번호의 개수와 같아야 합니다.`,
-    OUT_OF_BOUND: `${ERROR_TAG} ${TITLE}${OUT_OF_BOUND_TEMPLATE}`,
-    DUPLICATE: `${ERROR_TAG} ${TITLE}에 중복된 숫자가 없어야 합니다.`,
+    NOT_NUMBER: `${ERROR_TAG} ${TITLE_INFO.WINNING_NUMBERS}${NOT_NUMBER_TEMPLATE}`,
+    NOT_LOTTO_LENGTH: `${ERROR_TAG} ${TITLE_INFO.WINNING_NUMBERS}의 개수는 로또 번호의 개수와 같아야 합니다.`,
+    OUT_OF_BOUND: `${ERROR_TAG} ${TITLE_INFO.WINNING_NUMBERS}${OUT_OF_BOUND_TEMPLATE}`,
+    DUPLICATE: `${ERROR_TAG} ${TITLE_INFO.WINNING_NUMBERS}에 중복된 숫자가 없어야 합니다.`,
   },
   BONUS_NUMBER: {
-    TITLE: `보너스 번호`,
-    NOT_NUMBER: `${ERROR_TAG} ${TITLE}${NOT_NUMBER_TEMPLATE}`,
-    OUT_OF_BOUND: `${ERROR_TAG} ${TITLE}${OUT_OF_BOUND_TEMPLATE}`,
-    DUPLICATE: `${ERROR_TAG} ${TITLE}는 이전에 입력한 당첨 번호에 없는 숫자여야 합니다.`,
+    NOT_NUMBER: `${ERROR_TAG} ${TITLE_INFO.BONUS_NUMBER}${NOT_NUMBER_TEMPLATE}`,
+    OUT_OF_BOUND: `${ERROR_TAG} ${TITLE_INFO.BONUS_NUMBER}${OUT_OF_BOUND_TEMPLATE}`,
+    DUPLICATE: `${ERROR_TAG} ${TITLE_INFO.BONUS_NUMBER}는 이전에 입력한 당첨 번호에 없는 숫자여야 합니다.`,
   },
   LOTTO: {
-    TITLE: `로또 번호`,
-    NOT_LOTTO_LENGTH: `${ERROR_TAG} ${TITLE}는 6개여야 합니다.`,
-    OUT_OF_BOUND: `${ERROR_TAG} ${TITLE}${OUT_OF_BOUND_TEMPLATE}`,
-    DUPLICATE: `${ERROR_TAG} ${TITLE}는 중복이 없어야 합니다.`,
+    NOT_LOTTO_LENGTH: `${ERROR_TAG} ${TITLE_INFO.LOTTO}는 6개여야 합니다.`,
+    OUT_OF_BOUND: `${ERROR_TAG} ${TITLE_INFO.LOTTO}${OUT_OF_BOUND_TEMPLATE}`,
+    DUPLICATE: `${ERROR_TAG} ${TITLE_INFO.LOTTO}는 중복이 없어야 합니다.`,
   },
 };
 
