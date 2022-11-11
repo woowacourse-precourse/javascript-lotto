7c294bb3b12c59fcc33dd52cd02f0831dad24447
2yunseong <dbsdltjd3701@naver.com>
2022-11-11 21:31:34 +0900

    feat(vaildator): 양의 정수 인지 확인하는 기능

--- src/Vaildator.js
+++ src/Vaildator.js
@@ -8,6 +8,13 @@ class Vaildator {
   static isLottoLength(target) {
     return target === util.LOTTO_MAX_LENGTH;
   }
+
+  static isPositiveNumber(target) {
+    if (isNaN(target)) {
+      return false;
+    }
+    return Number.isInteger(+target) && +target > 0;
+  }
 }
 
 module.exports = Vaildator;
