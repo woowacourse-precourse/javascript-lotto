cb91e9e49894d0a88376914211110f313e3fc6a7
sunub <bsc5672@gmail.com>
2022-11-12 03:19:19 +0900

    feat : 도메인 로젝1에서 2로 연결 시키는 기능 추가
    도메인 1에서 수행한 결과를 도메인 2로 가져와서 수행할 수 있게끔 기능을
    추가하였다.

--- src/DomainLogic1.js
+++ src/DomainLogic1.js
@@ -1,11 +1,13 @@
 const Generator = require("./Generator.js");
 const Checker = require("./Checker.js");
+const DomainLogic2 = require("./DomainLogic2.js");
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class DomainLogic1 {
   constructor() {
     this.generator = new Generator();
     this.checker = new Checker();
+    this.domainLogic2 = new DomainLogic2();
   }
 
   getMoneyToSpend() {
@@ -16,7 +18,7 @@ class DomainLogic1 {
 
       const lottoTicket = this.generator.generateLottoNumbers(money);
 
-      this.getWinningNumber(lottoTicket, money);
+      this.domainLogic2.getWinningNumber(lottoTicket, money);
     });
   }
 }
