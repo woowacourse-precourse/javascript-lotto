d39ddee28b81215901f734edf0023bd750084446
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-11 16:24:16 +0900

    feat(App): 로또 구입 갯수 만큼 당첨 번호 출력, 배열 저장

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ const Lotto = require("./Lotto.js")
 class App {
   constructor(){
     this.Lotto = new Lotto()
+    this.Lottobuynumber = [];
   }
   play() {
     MissionUtils.Console.readLine(Notice.INPUT_MONEY,(money) => {
       this.inputMoneyValidate(money)
+      this.makeRandomnumber(money)
     });
   }
   inputMoneyValidate(money){
     if (money % 1000 != 0) {
       throw Error.UNIT_ERROR;
+    }
+  }
+  makeRandomnumber(money){
+    const LottoCount = money / 1000;
+    MissionUtils.Console.print(LottoCount + Notice.BUY_LOTTO)
+    for (let i = 0; i<LottoCount; i++){
+      let RandomNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+      MissionUtils.Console.print(`[${RandomNumber.join(', ')}]`);
+      this.Lotto.validate(RandomNumber);
+      this.Lottobuynumber.push(`${RandomNumber}`)
     }
   }
 }
--- src/NoticeMessage.js
+++ src/NoticeMessage.js
@@ -1,5 +1,6 @@
 const NOTICE_MESSAGE = {
     INPUT_MONEY : "구입금액을 입력해 주세요.",
+    BUY_LOTTO : "개를 구매했습니다."
 };
 
 module.exports = NOTICE_MESSAGE;
