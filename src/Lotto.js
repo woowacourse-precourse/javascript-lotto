ad6ca6688bb842863317937bb3cc599fec0bf199
unsnruu <unsnruu@gmail.com>
2022-11-12 03:44:42 +0900

    feat : setNumber 메서드를 삭제
    겹치지 않는 임의의 6자리의 번호를 외부에서 생성하여 인스턴스 생성시에
    전달하기로 전략을 수정함에 따라서 삭제하였습니다. 또한 Lotto 클래스의 내부적으로
    크게 로직을 작성하지 않을 것으로 예상되어 parameter에 대한 주석도 삭제하였습니다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,12 +1,6 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-
-const { Random, Console } = MissionUtils;
-
 class Lotto {
   #numbers;
-  /**
-   * @param {array} numbers: 6자리의 숫자 배열
-   */
+
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
@@ -16,9 +10,6 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-  }
-  setNumbers() {
-    this.#numbers = pickUniqueNumbersInRange(1, 45, 6);
   }
 }
 
