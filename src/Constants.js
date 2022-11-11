915c0bd297d7c650c6213b5db3f68bdaa8825a51
hae-on <solbi2004@naver.com>
2022-11-12 00:41:47 +0900

    refactor: 변수에 하나의 값만 할당

--- src/Constants.js
+++ src/Constants.js
@@ -3,10 +3,8 @@ const LOTTO_NUMBER_MIN = 1;
 const LOTTO_NUMBER_MAX = 45;
 const LOTTO_LENGTH = 6;
 const PURCHASE_MESSAGE = '구입금액을 입력해 주세요.';
-const PURCHASE_COUNT = {
-  PURCHASE_COUNT_MESSAGE: (purchaseCount) =>
-    `${purchaseCount}개를 구매했습니다.`,
-};
+const PURCHASE_COUNT_MESSAGE = (purchaseCount) =>
+  `${purchaseCount}개를 구매했습니다.`;
 const WINNING_NUMBER_MESSAGE = '당첨 번호를 입력해 주세요.';
 const BONUS_NUMBER_MESSAGE = '보너스 번호를 입력해 주세요.';
 const WINNING_STATISTICS_MESSAGE = '당첨 통계';
@@ -28,7 +26,7 @@ module.exports = {
   LOTTO_NUMBER_MAX,
   LOTTO_LENGTH,
   PURCHASE_MESSAGE,
-  PURCHASE_COUNT,
+  PURCHASE_COUNT_MESSAGE,
   WINNING_NUMBER_MESSAGE,
   BONUS_NUMBER_MESSAGE,
   WINNING_STATISTICS_MESSAGE,
