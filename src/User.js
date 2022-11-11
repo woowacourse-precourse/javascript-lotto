6141eaeb981cc073a6801eee5d1928664e661e21
nsyeon <lovepny@gmail.com>
2022-11-11 19:29:39 +0900

    feat(User): 로또 구입 금액 입력 및 예외처리

---
+++ src/User.js
@@ -0,0 +1,15 @@
+const MISSION = require("@woowacourse/mission-utils");
+const LOTTO_PRICE = 1000;
+const SPECIAL_CHARACTERS = /[~!@#$%^&*()_+|<>?:{}]/;
+
+class User {
+
+  enterLottoBuyPrice() {
+    let price = MISSION.Console.readLine();
+    if (price % LOTTO_PRICE != 0 || price <= 0 || price === String || SPECIAL_CHARACTERS.test(price)) {
+      throw new Error("[ERROR]");
+    }
+    return price;
+  }
+
+}
