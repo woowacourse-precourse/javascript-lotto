81fe8866e0e7425da03033f53cf09af869dde133
hwanfront <kim015jh@gmail.com>
2022-11-11 12:52:57 +0900

    test(LottoAmount): 로또 구입금액에서 로또 발행 수량을 생성하는 함수 테스트 추가

--- __tests__/LottoAmountTest.js
+++ __tests__/LottoAmountTest.js
@@ -29,13 +29,19 @@ describe("로또 클래스 테스트", () => {
       new LottoAmount(999);
     }).toThrow(LottoAmountLessException);
   });
-});
 
-test("로또 금액이 로또 가격으로 나누어지지 떨어지지 않는 경우", () => {
-  expect(() => {
-    new LottoAmount(1001);
-  }).toThrow(LottoAmountDivideException);
-  expect(() => {
-    new LottoAmount(12800);
-  }).toThrow(LottoAmountDivideException);
+  test("로또 금액이 로또 가격으로 나누어지지 떨어지지 않는 경우", () => {
+    expect(() => {
+      new LottoAmount(1001);
+    }).toThrow(LottoAmountDivideException);
+    expect(() => {
+      new LottoAmount(12800);
+    }).toThrow(LottoAmountDivideException);
+  });
+
+  test("로또 발행 수량 출력", () => {
+    expect(LottoAmount.from(1000).getLottoCount()).toEqual(1);
+    expect(LottoAmount.from(8000).getLottoCount()).toEqual(8);
+    expect(LottoAmount.from(12000).getLottoCount()).toEqual(12);
+  });
 });
