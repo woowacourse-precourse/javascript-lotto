ceda9bd2319a463882542691a4766a942a6f029e
kaori-killer <sksbthwjd@daum.net>
2022-11-11 17:18:40 +0900

    feat: 입력 메시지 출력 및 로또 구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+
+const TEXTS = Object.freeze({
+  amountInput: '구입금액을 입력해 주세요. \n',
+  lottoCount: '개를 구매했습니다.',
+  numberInput : '당첨 번호를 입력해 주세요.',
+  bonusInput: '보너스 번호를 입력해 주세요.',
+  resultInput: '당첨 통계 \n\n --'
+});
+
 class App {
-  play() {}
+  play() {
+    this.printMessage(TEXTS.amountInput, this.inputAmount);
+  }
+
+  printMessage(text, callback) {
+    Console.readLine(text, callback.bind(this));
+  }
+
+  inputAmount(input){
+    Console.print(input)
+  }
+
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
