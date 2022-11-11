922d6e9384ca4cf2436bc083fc2277e07f17948f
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-11 23:05:57 +0900

    test(bonusNumber): 보너스 번호가 로또 숫자의 범위가 아니라면 에러를 발생시키는 테스트

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -9,4 +9,12 @@ describe('입력받은 보너스 번호가 유효한지 확인하는 테스트',
       app.play();
     }).toThrow('[ERROR]');
   });
+
+  test('사용자가 입력한 보너스 번호가 로또 숫자의 범위가 아닌 상황이라면 에러를 발생시킨다.', () => {
+    mockQuestions(['1000', '1,2,3,4,5,6', '46']);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow('[ERROR]');
+  });
 });
