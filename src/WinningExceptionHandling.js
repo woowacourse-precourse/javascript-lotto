a7c9691c65cfa05395ad54d028e6b823f52cd829
c0c0pang <coc1789@naver.com>
2022-11-11 19:44:11 +0900

    feat: 당첨 번호에 중복이 있는지 확인하는 예외처리 기능 구현

--- src/WinningExceptionHandling.js
+++ src/WinningExceptionHandling.js
@@ -1,10 +1,17 @@
 class WinningExceptionHandling {
     constructor(winning) {
         this.lengthCheck(winning);
+        this.overlapCheck(winning);
     };
     lengthCheck(winning) {
         if (winning.length !== 6) {
             throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        };
+    };
+    overlapCheck(winning) {
+        const setWinning = new Set(winning);
+        if (setWinning.size !== winning.length) {
+            throw new Error("[ERROR] 로또 번호는 중복이 없습니다.");
         };
     };
 };
