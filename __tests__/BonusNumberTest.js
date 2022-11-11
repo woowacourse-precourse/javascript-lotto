b5b44ec08fa68c292a6fafa29afa00cb6f59242a
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-11 23:06:38 +0900

    test(bonusNumber): 보너스 번호가 당첨 번호와 중복된다면 에러를 발생시키는 테스트

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -17,4 +17,12 @@ describe('입력받은 보너스 번호가 유효한지 확인하는 테스트',
       app.play();
     }).toThrow('[ERROR]');
   });
+
+  test('사용자가 입력한 보너스 번호가 당첨 번호와 중복된 상황이라면 에러를 발생시킨다.', () => {
+    mockQuestions(['1000', '1,2,3,4,5,6', '1']);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow('[ERROR]');
+  });
 });
