0df16529a148ed9a86666dd58ada2ac53a1a7924
leeuihyun <meta212121@naver.com>
2022-11-11 15:03:39 +0900

    feat : 구입 금액을 검사 및 반환하는 클래스

---
+++ src/Purchase.js
@@ -0,0 +1,32 @@
+const { ERROR } = require('./lib/constants');
+
+class Purchase {
+  #money;
+
+  constructor(money) {
+    this.validate(money);
+    this.#money = Number(money);
+  }
+
+  validate(money) {
+    const numberMoney = Number(money);
+
+    if (isNaN(numberMoney)) {
+      throw new Error(ERROR.PURCHASE_MONEY_NAN);
+    }
+
+    if (numberMoney % 1000 !== 0) {
+      throw new Error(ERROR.PURCHASE_MONEY_NOT_DIVISIBLE);
+    }
+
+    if (numberMoney < 1000) {
+      throw new Error(ERROR.PURCHASE_MONEY_LESS_STANDARD);
+    }
+  }
+
+  getNumberTypeMoney() {
+    return this.#money;
+  }
+}
+
+module.exports = Purchase;
