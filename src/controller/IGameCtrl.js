838f44b21acf072cfd5c0fe82a8aede392b1915f
dev-redo <mis05041@naver.com>
2022-11-12 00:17:57 +0900

    Feat: game controller 인터페이스 정의

---
+++ src/controller/IGameCtrl.js
@@ -0,0 +1,23 @@
+const IGameCtrl = class {
+  start() {
+    throw Error('메서드구현필요');
+  }
+
+  game() {
+    throw Error('메서드구현필요');
+  }
+
+  play() {
+    throw Error('메서드구현필요');
+  }
+
+  gameProcess() {
+    throw Error('메서드구현필요');
+  }
+
+  renderGameResult() {
+    throw Error('메서드구현필요');
+  }
+};
+
+export default IGameCtrl;
