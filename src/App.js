45540de10a77718c131889ef47fdaf40e9d7c45a
gilpop8663 <wolfye@naver.com>
2022-11-11 14:13:49 +0900

    feat: 라이브러리를 이용하여 랜덤 로또 숫자 만드는 기능 구현
    6개의 중복되지 않는 숫자들을 만들어야 한다
    
    로또 번호는 오름차순으로 정렬하여 보여준다

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-const { Console } = require('@woowacourse/mission-utils');
-const { QUESTION_MESSAGE, ERROR_MESSAGE } = require('./libs/const');
+const { Console, Random } = require('@woowacourse/mission-utils');
+const {
+  QUESTION_MESSAGE,
+  ERROR_MESSAGE,
+  BUY_MESSAGE,
+} = require('./libs/const');
 
 class App {
+  #lottoArr = [];
+
   play() {
     this.start();
   }
@@ -9,7 +15,16 @@ class App {
   start() {
     Console.readLine(QUESTION_MESSAGE.buy, money => {
       if (money % 1000 !== 0) throw new Error(ERROR_MESSAGE.purchase);
+      this.purchase(money);
     });
+  }
+
+  purchase(money) {}
+
+  setLotto() {
+    const randomArr = Random.pickUniqueNumbersInRange(1, 45, 6);
+    const lottoArr = randomArr.sort((a, b) => a - b);
+    return lottoArr;
   }
 }
 
