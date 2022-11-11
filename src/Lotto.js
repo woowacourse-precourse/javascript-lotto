73730123d8ac8ccdf6eb89adfe45a4f45d182f1e
yan9vvoojin <yan9vvoojin@naver.com>
2022-11-11 14:22:24 +0900

    feat(Lotto): 로또를 생성하는 기능에 예외 사항 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,7 +8,15 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    const isOutOfRange = numbers.some((number) => number < 1 || number > 45);
+    if (isOutOfRange) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+    }
+    const numberSet = new Set(numbers);
+    if (numbers.length !== numberSet.size) {
+      throw new Error('[ERROR] 로또 번호는 서로 중복되지 않아야 합니다.');
     }
   }
 
