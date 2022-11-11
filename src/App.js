ba73bba62e618cc285988f752301f68a183ea992
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-11 18:17:24 +0900

    feat: add revenue constant

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ const { printError, isPositiveInteger } = require('./Utils');
 const LOTTO_PRICE = 1000;
 const LOTTO_START = 1;
 const LOTTO_END = 45;
+const REVENUE = {
+  3: {
+    message: '3개 일치 (5,000원)',
+    revenue: 5000,
+  },
+  4: {
+    message: '4개 일치 (50,000원)',
+    revenue: 50000,
+  },
+  5: {
+    message: '5개 일치 (1,500,000원)',
+    revenue: 1500000
+  },
+  '5B': {
+    message: '5개 일치, 보너스 볼 일치(30,000,000원)',
+    revenue: 30000000
+  }
+  6: {
+    message: '6개 일치 (2,000,000,000원)',
+    revenue: 2000000000
+  }
+},
 class App {
   #money;
   #lottos;
