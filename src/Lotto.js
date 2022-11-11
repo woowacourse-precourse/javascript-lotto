899aea5ac53f112b021724698fb272e6d379e1cd
sunub <bsc5672@gmail.com>
2022-11-12 03:27:04 +0900

    feat : 로또 당첨번호를 입력 받아 저장하고 결과를 판단하는 기능 추가
    도메인 로직 2를 통하여 입력 받은 값은 프라이빗 필드로 저장하여 외부에서
    접글 할 수 없게끔 값을 저장하고 그 값을 토대로 올바른 값을 입력하였는지
    판단하고 또한 당첨 번호와 복권 번호가 얼마나 일치하고 등수가 어떻게 되는
    지 판단하는 기능을 총괄하는 기능을 수행한다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,32 @@
+const Checker = require("./Checker.js");
+const Generator = require("./Generator.js");
+
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
+  constructor(numbers, ticket) {
     this.validate(numbers);
     this.#numbers = numbers;
+    this.generateLottoResult(numbers, ticket);
   }
 
   validate(numbers) {
+    const checker = new Checker();
+
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    checker.isOverlapping(numbers);
+    checker.isRightRangeNumber(numbers);
   }
 
   // TODO: 추가 기능 구현
+
+  generateLottoResult(lottoNumber, ticke) {
+    const generator = new Generator();
+    return generator.generateLottoResult(lottoNumber, ticke);
+  }
 }
 
 module.exports = Lotto;
