af36a830bfabf0a0a04aa0180ff694abe7389ce9
guridaek <iue16@naver.com>
2022-11-11 14:57:21 +0900

    feat: 구입금액 입력받기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto.js');
+
 class App {
-  play() {}
+  #money = 0;
+
+  #lottos = [];
+
+  #winningNumber;
+
+  #bounsNumber;
+
+  inputMoney() {
+    Console.readLine('구입금액을 입력해 주세요.', answer => {
+      this.validateMoney(answer);
+      this.money = Number(answer);
+    });
+  }
+
+  validateMoney(money) {
+    if (!Number.isNaN(money)) {
+      throw new Error('[ERROR] 올바르지 않은 금액입니다.');
+    }
+    if (Number(money) % 1000 !== 0) {
+      throw new Error('[ERROR] 1000원 단위로 입력해야 합니다.');
+    }
+  }
+
+  play() {
+    this.inputMoney();
+    Console.close();
+  }
 }
 
 module.exports = App;
