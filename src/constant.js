cdef3e7b32e07f32e547972494659c9be465dfd3
vinivin153 <vinivin153@naver.com>
2022-11-11 21:48:21 +0900

    refactor: 상수명 변경 및 추가

--- src/constant.js
+++ src/constant.js
@@ -2,6 +2,15 @@ const ERROR = Object.freeze({
   LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
   RANGE: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
   DUPLICATE: '[ERROR] 로또 번호는 중복되지 않아야 합니다.',
+  PURCHASABLE: '[ERROR] 로또는 1000원 이상부터 구매 가능합니다',
+  DIVISIBLE: '[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.',
 });
 
-module.exports = ERROR;
+const CONDITION = Object.freeze({
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 45,
+  LOTTO_LENGTH: 6,
+  LOTTO_PRICE: 1000,
+});
+
+module.exports = { ERROR, CONDITION };
