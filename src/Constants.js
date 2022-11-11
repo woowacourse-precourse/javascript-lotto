89daf29b1dfe049396e133b456a298c7ce9d176f
Narcoker <jykim9868@gmail.com>
2022-11-11 21:40:25 +0900

    Feat:  ENTER_WINNING_LOTTO 메세지 문구 추가

--- src/Constants.js
+++ src/Constants.js
@@ -1,11 +1,12 @@
 const MESSAGE = Object.freeze({
     ENTER_CASH: "구입금액을 입력해 주세요.\n",
+    ENTER_WINNING_LOTTO: "당첨 번호를 입력해 주세요.\n",
     BUY_LOTTOS_COUNT: "개를 구매했습니다."
-})
+});
 
 const ERROR = Object.freeze({
     INVAID_CACHE: "[ERROR] 구입 금액은 1000원 단위여야합니다.\n",
     CACHE_IS_NOT_NUMBER: "[ERROR] 입력값은 숫자여야 합니다.\n"
 });
 
-module.exports = { MESSAGE, ERROR };
+module.exports = { MESSAGE, ERROR }; 
