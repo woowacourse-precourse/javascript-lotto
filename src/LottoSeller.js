9b767f56596f2692d596303abaf35a7e9aefa5c6
Tory <88531407+1two13@users.noreply.github.com>
2022-11-11 11:37:08 +0900

    refactor(LottoSeller): COUNT_LOTTO => NUMBER, lottoNumbersList => lottoArray, randomNumberInRange => uniqueNumberInRange로 변수명 변경

--- src/LottoSeller.js
+++ src/LottoSeller.js
@@ -4,22 +4,22 @@ const Lotto = require('./Lotto');
 class LottoSeller {
   constructor() {
     this.lotto = new Lotto();
-    this.lottoNumbersList = [];
+    this.lottoArray = [];
   }
 
   countBuying(money) {
-    const COUNT_LOTTO = money / 1000;
-    Console.print(`\n${COUNT_LOTTO}개를 구매했습니다.`);
+    const NUMBER = money / 1000;
+    Console.print(`\n${NUMBER}개를 구매했습니다.`);
   }
 
   printLottos(money) {
-    const COUNT_LOTTO = money / 1000;
+    const NUMBER = money / 1000;
 
-    for (let count = 1; count <= COUNT_LOTTO; count++) {
-      let randomNumberInRange = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
-      Console.print(`[${randomNumberInRange.join(', ')}]`);
-      this.lotto.validate(randomNumberInRange);
-      this.lottoNumbersList.push(randomNumberInRange);
+    for (let count = 1; count <= NUMBER; count++) {
+      let uniqueNumberInRange = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+      Console.print(`[${uniqueNumberInRange.join(', ')}]`);
+      this.lotto.validate(uniqueNumberInRange);
+      this.lottoArray.push(uniqueNumberInRange);
     }
   }
 }
