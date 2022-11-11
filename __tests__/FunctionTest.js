f874d81d16e3660921a341bda2a0a90536920080
yeonsuBeak <soothbugger@gmail.com>
2022-11-11 18:14:46 +0900

    test(lotto): 발행 로또 개수 출력 테스트 완료

---
+++ __tests__/FunctionTest.js
@@ -0,0 +1,37 @@
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("기능 테스트", () => {
+  test("발행 로또 개수 출력", () => {
+    const logSpy = getLogSpy();
+    const log = "10개를 구매했습니다.";
+    const money = 10000;
+
+    const app = new App();
+    app.printQuantity(money);
+
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+  });
+});
