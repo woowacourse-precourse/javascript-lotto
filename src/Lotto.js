027fd4f5b0391a34218d507eba3f9e892e4633f3
AnJaeSeong <dktmzh6@gmail.com>
2022-11-11 17:59:28 +0900

    fix: Lotto.validate()
    당첨번호 입력시 에러 체크 추가
    - 각 번호 1~45 범위내 체크
    - 당첨번호 중복 체크

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,6 +12,12 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (numbers.some(number => number <= 0 || number > 45)){
+      throw new Error("[ERROR] 로또 번호는 1~45 범위 내에 존재해야 합니다.")
+    }
+    if (numbers.length !== new Set(numbers).size){
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.")
+    }
   }
 
   setBonusNum(bonus) {
