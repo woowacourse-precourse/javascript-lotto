012387263b7112fb294a2f5a3037525ff6f53cc7
Leejin-Yang <ejin1996@gmail.com>
2022-11-11 19:19:00 +0900

    test: 보너스 번호 예외 테스트 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -18,9 +18,23 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  test('1-45 범위 밖 숫자가 있다면 예외가 발생한다.', () => {
+  test('로또 번호에 1-45 범위 밖 숫자가 있다면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('보너스 번호가 1-45 범위 밖 숫자라면 예외가 발생한다.', () => {
+    expect(() => {
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      lotto.setBonusNumber(46);
+    }).toThrow('[ERROR]');
+  });
+
+  test('보너스 번호가 로또 번호에 포함되어 있다면 예외가 발생한다.', () => {
+    expect(() => {
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      lotto.setBonusNumber(6);
     }).toThrow('[ERROR]');
   });
 });
