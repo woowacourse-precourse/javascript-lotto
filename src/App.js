62653a35608194b405b4ac7b35f0a33c5f0c8cd3
yeonsuBeak <soothbugger@gmail.com>
2022-11-11 17:48:26 +0900

    refactor: 입력 받은 구입 금액을 constructor에 저장

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ const { Console } = require("@woowacourse/mission-utils");
 const Money = require("./Money");
 
 class App {
+  constructor() {
+    this.money = 0;
+  }
+
   play() {
     Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
       new Money(money);
-      Console.print(money);
+      this.money = money;
     });
   }
 }
