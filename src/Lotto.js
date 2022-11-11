4cc10f91233d65084b732a29593a5f6cc8ad98b3
jinlee <vito9270@naver.com>
2022-11-11 23:20:38 +0900

    feat: 로또 구입금액 입력 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const MissionUtils = require("@woowacourse/mission-utils")
+
 class Lotto {
   #numbers;
 
@@ -6,13 +8,18 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  inputPurchaseAmount() {
+    Console.readLine('구입금액을 입력해 주세요.', (purchaseAmount) => {
+      is1000Multiple(purchaseAmount)
+    });
+  }
+
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
