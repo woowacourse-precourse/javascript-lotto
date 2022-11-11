d55a1af69b2156be2ecff2e60c7fbc4bd4c5f5c3
Sijun <sionwer4@naver.com>
2022-11-11 15:15:31 +0900

    feat: Bonus class exception check function add

--- src/Bonus.js
+++ src/Bonus.js
@@ -2,7 +2,21 @@ class Bonus {
   #number;
 
   constructor(number) {
+    this.range(number);
+    this.character(number);
     this.#number = number;
+  }
+
+  range(number) {
+    if (number > 45 || number < 1) {
+      throw new Error("[ERROR] 보너스 번호는 1에서 45사이의 숫자여야 합니다.");
+    }
+  }
+
+  character(number) {
+    if (typeof number != "number") {
+      throw new Error("[ERROR] 보너스 번호는 숫자여야 합니다.");
+    }
   }
 }
 
