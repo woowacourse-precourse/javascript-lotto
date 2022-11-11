dde5874c8b5dd0aa4d9c90f22a94dbf4321d4421
jongwonp <jw032876@gmail.com>
2022-11-11 15:40:41 +0900

    feat: 보너스 번호받는 기능 완료 (당첨번호와 다른 번호로 받기 테스트 완료)

--- src/Lotto.js
+++ src/Lotto.js
@@ -41,7 +41,18 @@ class Lotto {
       if (!is1To45(bonusNumber)) {
         throw new Error('[ERROR] 1부터 45 사이의 숫자만 입력해 주세요.');
       }
+
+      if (this.isIncluded(bonusNumber)) {
+        throw new Error('[ERROR] 로또 번호 6개와 다른 수를 입력해주세요');
+      }
     });
+  }
+
+  isIncluded(num) {
+    if (this.#numbers.includes(num)) {
+      return true;
+    }
+    return false;
   }
 }
 
