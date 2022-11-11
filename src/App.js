78e9ba134fbdc70d9d3f1df91d5c1c2f4217323f
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-11 20:04:45 +0900

    feat: make a lotto game process

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 const { Console } = require('@woowacourse/mission-utils');
+const BonusLotto = require('./BonusLotto');
 const Lotto = require('./Lotto');
+const PurChase = require('./Purchase');
 
 class App {
   constructor() {
-    // this.lotto = new Lotto();
+    this.purChaseLottoAmount;
+    this.fullLottoNumber;
   }
 
   play() {
-    Console.readLine('당첨 번호를 입력해 주세요.\n', (inputNum) => {
-      new Lotto(inputNum);
-      // lotto();
+    this.purChaseLotto();
+  }
+
+  purChaseLotto() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (amount) => {
+      this.purChaseLottoAmount = new PurChase(amount) ? Number(amount) / 1000 : '';
+      this.inputLottoNumber();
     });
+  }
+
+  inputLottoNumber() {
+    Console.readLine('당첨 번호를 입력해 주세요.\n', (lottoInput) => {
+      new Lotto(lottoInput);
+      Console.readLine('보너스 번호를 입력해 주세요.\n', (bonusInput) => {
+        this.fullLottoNumber = new BonusLotto([lottoInput.split(','), bonusInput])
+          ? [lottoInput.split(','), bonusInput]
+          : '';
+        this.showStats();
+      });
+    });
+  }
+
+  showStats() {
+    console.log(this.purChaseLottoAmount, this.fullLottoNumber);
   }
 }
 
