45965c6413b58b5c17e1ae2509a3bc601720d2e5
sungjihyun <jhsung23@gmail.com>
2022-11-11 17:24:59 +0900

    feat(App): 구입 금액 입력 받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+const { MESSAGE } = require("../src/utils/constants");
+
 class App {
   play() {}
+
+  //구매 금액 입력 요청하기
+  getPurchaseAmount() {
+    Console.readLine(MESSAGE.ENTER_PURCHASE_AMOUNT, (amount) => {
+      console.log(amount);
+    });
+  }
 }
 
 module.exports = App;
