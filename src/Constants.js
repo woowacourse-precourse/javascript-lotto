75700a4288d790a1a0d072acdb284d1d15b04411
Narcoker <jykim9868@gmail.com>
2022-11-11 20:45:32 +0900

    Feat: CACHE_IS_NOT_NUMBER 에러 문구 추가

--- src/Constants.js
+++ src/Constants.js
@@ -3,7 +3,8 @@ const MESSAGE = Object.freeze({
 })
 
 const ERROR = Object.freeze({
-    INVAID_CACHE: "[ERROR] 구입 금액은 1000원 단위여야합니다.\n"
+    INVAID_CACHE: "[ERROR] 구입 금액은 1000원 단위여야합니다.\n",
+    CACHE_IS_NOT_NUMBER: "[ERROR] 입력값은 숫자여야 합니다.\n"
 });
 
 module.exports = { MESSAGE, ERROR };
