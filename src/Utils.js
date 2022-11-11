4d63e348081695bd49a8389b854db98ee75ac1d9
Sukyeong Park <zztnrudzz13@naver.com>
2022-11-11 20:23:52 +0900

    feat($utils): implement validation functions in utils add isDuplicated function, isIncludeInRange function

--- src/Utils.js
+++ src/Utils.js
@@ -3,11 +3,22 @@ function isPositiveInteger(input) {
   return regex.test(input);
 }
 
+// FIXME: UI 로직 분리
 function printError(message) {
   throw Error(`[ERROR] ${message}`);
+}
+
+function isDuplicated(numbers) {
+  return numbers.length !== new Set(numbers).size;
+}
+
+function isIncludeInRange(number, startRange, endRange) {
+  return number >= startRange && number <= endRange;
 }
 
 module.exports = {
   isPositiveInteger,
   printError,
+  isDuplicated,
+  isIncludeInRange,
 };
