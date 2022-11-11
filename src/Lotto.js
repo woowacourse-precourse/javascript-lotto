96ad794acfaeb54c45bc91ebfa772b4f4a8b2e3b
MAGHC <windowsapple95@gmail.com>
2022-11-11 18:31:11 +0900

    feat: 당첨로또번호 정렬 기능 생성

--- src/Lotto.js
+++ src/Lotto.js
@@ -15,13 +15,17 @@ class Lotto {
     if (new Set([...numbersArray]).size !== LOTTO_NUMBER_RANGE.LENGTH) {
       throw new Error(LOTTO_ERROR_MESSAGES.UNIQUE);
     }
+    return true;
   }
 
   // TODO: 추가 기능 구현
+  ascendingSortLottoArray() {
+    this.#numbers.sort((a, b) => a - b);
+  }
 }
 
-const lotto = new Lotto([1, 1, 2, 3, 4, 5]);
+const lotto = new Lotto([1, 3, 2, 7, 4, 5]);
 
-console.log(lotto.validate());
+console.log(lotto.ascendingSortArray());
 
 module.exports = Lotto;
