5b0e1ab7a713b4378a0eb635d27f27d54a55be4c
ShimSungBo <shimdb96@gmail.com>
2022-11-11 18:02:53 +0900

    feat:(Lotto) 랜덤 로또번호 생성

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const { Random } = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
 
@@ -10,9 +12,17 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR 로또 번호는 중복되지 않아야 합니다.]");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  static generateRandomLottoNumber(lottoCount) {
+    return Array.from({ length: lottoCount }, () =>
+      Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b)
+    );
+  }
 }
 
 module.exports = Lotto;
