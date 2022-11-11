13e5541258c5069cce07382c746c46578a80a0de
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-11-11 15:01:03 +0900

    feat: 로또번호 입력 예외처리 메시지 추가

--- src/constants.js
+++ src/constants.js
@@ -13,7 +13,11 @@ const ERROR = '[ERROR]';
 const ERROR_MESSAGE = {
   PURCHASE_AMOUNT_UNIT: `${ERROR} 구입 금액은 1,000원 단위여야 합니다.`,
   LOTTO_LENGTH: `${ERROR} 로또 번호는 6개여야 합니다.`,
+  NUMBER_COMMA: `${ERROR} 로또 번호는 숫자와 쉼표(,)만을 사용하여 입력해야 합니다.`,
+  OUT_OF_RANGE: `${ERROR} 로또 번호는 1부터 45 사이의 숫자여야 합니다.`,
+  DUPLICATE_NUMBER: `${ERROR} 로또 번호는 중복되지 않는 숫자여야 합니다.`,
 };
+
 module.exports = {
   INPUT_MESSAGE,
   PRINT_MESSAGE,
