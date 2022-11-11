f662afd05cb3df7fd17b710826514629f7451b75
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-11 22:45:37 +0900

    test: 구매 로또의 보너스 번호 정답 여부 확인 함수 테스트

--- __tests__/UtilTest.js
+++ __tests__/UtilTest.js
@@ -1,5 +1,6 @@
 const countCorrectNumber = require('../src/utils/countCorrectNumber');
 const getLottoQuantity = require('../src/utils/getLottoQuantity');
+const isBonusNumberCorrect = require('../src/utils/isBonusNumberCorrect');
 
 describe('유틸 함수 동작 테스트', () => {
   test('구매 로또의 정답 개수를 리턴하는 함수', () => {
@@ -12,5 +13,11 @@ describe('유틸 함수 동작 테스트', () => {
       countCorrectNumber([19, 22, 42, 14, 35, 27], [13, 42, 23, 44, 35, 27])
     ).toBe(3);
   });
-  // 아래에 추가 테스트 작성 가능
+
+  test('구매 로또의 보너스 번호 정답 여부 확인', () => {
+    expect(isBonusNumberCorrect(1, [2, 3, 4, 5, 6, 7])).toBe(0);
+    expect(isBonusNumberCorrect(4, [2, 3, 4, 5, 6, 7])).toBe(1);
+    expect(isBonusNumberCorrect(14, [12, 31, 42, 15, 26, 37])).toBe(0);
+    expect(isBonusNumberCorrect(15, [12, 31, 42, 15, 26, 37])).toBe(1);
+  });
 });
