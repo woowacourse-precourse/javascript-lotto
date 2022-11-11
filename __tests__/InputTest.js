8de4b857656b10dada5d581301fb4fad18d99998
Chanwoong1 <jcw7468@gmail.com>
2022-11-11 20:55:42 +0900

    chore: 클래스 분할에 따른 코드 변경

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,4 +1,5 @@
 const LottoProgram = require("../src/LottoProgram");
+const ValidateInput = require("../src/ValidateInput");
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const getLogSpy = () => {
@@ -21,118 +22,118 @@ afterAll(() => {
 });
 
 describe('validateInputMoney(inputMoney)', () => {
-  const lottoProgram = new LottoProgram();
+  const validateInput = new ValidateInput();
 
   test("적절한 입력값이 들어오는 경우, true를 반환해야 합니다.", () => {
-    expect(lottoProgram.validateInputMoney('8000')).toBeTruthy();
+    expect(validateInput.validateInputMoney('8000')).toBeTruthy();
   });
 
   test('적절하지 않은 입력값이 들어오는 경우, 에러를 발생해야 합니다.', () => {
     expect(() => {
-      lottoProgram.validateInputMoney('8500');
+      validateInput.validateInputMoney('8500');
     }).toThrow();
   });
 });
 
 describe('validateWinningNumbers(winningNumbers)', () => {
-  const lottoProgram = new LottoProgram();
+  const validateInput = new ValidateInput();
 
   test("적절한 입력값이 들어오는 경우, true를 반환해야 합니다.", () => {
-    expect(lottoProgram.validateWinningNumbers('1,2,3,4,5,6')).toBeTruthy();
+    expect(validateInput.validateWinningNumbers('1,2,3,4,5,6')).toBeTruthy();
   });
 
   test("공백을 포함하는 입력값도 적합한 입력값이라 판단하였습니다. 따라서 true를 반환해야 합니다.", () => {
-    expect(lottoProgram.validateWinningNumbers('1   0 , 2   1 ,  3  3,   4, 5,      6')).toBeTruthy();
+    expect(validateInput.validateWinningNumbers('1   0 , 2   1 ,  3  3,   4, 5,      6')).toBeTruthy();
   });
 
   test('적절하지 않은 입력값이 들어오는 경우, 에러를 발생해야 합니다. - 숫자가 아닌 경우', () => {
     expect(() => {
-      lottoProgram.validateWinningNumbers('1,2,3,a,b,6');
+      validateInput.validateWinningNumbers('1,2,3,a,b,6');
     }).toThrow();
   });
 
   test('적절하지 않은 입력값이 들어오는 경우, 에러를 발생해야 합니다. - 입력받은 수가 6개가 아닌 경우', () => {
     expect(() => {
-      lottoProgram.validateWinningNumbers('1,2,3,4,5');
+      validateInput.validateWinningNumbers('1,2,3,4,5');
     }).toThrow();
 
     expect(() => {
-      lottoProgram.validateWinningNumbers('1,2,3,4,5,6,7');
+      validateInput.validateWinningNumbers('1,2,3,4,5,6,7');
     }).toThrow();
   });
 
   test('적절하지 않은 입력값이 들어오는 경우, 에러를 발생해야 합니다. - 로또 범위 내의 수가 아닌 경우', () => {
     expect(() => {
-      lottoProgram.validateWinningNumbers('1,2,3,46,5,6');
+      validateInput.validateWinningNumbers('1,2,3,46,5,6');
     }).toThrow();
 
     expect(() => {
-      lottoProgram.validateWinningNumbers('1,2,3,0,5,6');
+      validateInput.validateWinningNumbers('1,2,3,0,5,6');
     }).toThrow();
     expect(() => {
-      lottoProgram.validateWinningNumbers('-1,2,3,4,5,6');
+      validateInput.validateWinningNumbers('-1,2,3,4,5,6');
     }).toThrow();
   });
 
   test('적절하지 않은 입력값이 들어오는 경우, 에러를 발생해야 합니다. - 자연수가 아닌 경우', () => {
     expect(() => {
-      lottoProgram.validateWinningNumbers('1,2,3,4.7,5,6');
+      validateInput.validateWinningNumbers('1,2,3,4.7,5,6');
     }).toThrow();
   });
 
   test('적절하지 않은 입력값이 들어오는 경우, 에러를 발생해야 합니다. - 중복된 값이 존재하는 경우', () => {
     expect(() => {
-      lottoProgram.validateWinningNumbers('1,2,3,3,5,6');
+      validateInput.validateWinningNumbers('1,2,3,3,5,6');
     }).toThrow();
   });
 });
 
 describe('validateBonusNumber(winningNumbers)', () => {
-  const lottoProgram = new LottoProgram();
+  const validateInput = new ValidateInput();
 
   test("적절한 입력값이 들어오는 경우, true를 반환해야 합니다.", () => {
-    expect(lottoProgram.validateBonusNumber('7')).toBeTruthy();
+    expect(validateInput.validateBonusNumber('7')).toBeTruthy();
   });
 
   test('적절하지 않은 입력값이 들어오는 경우, 에러를 발생해야 합니다. - 숫자가 아닌 경우', () => {
     expect(() => {
-      lottoProgram.validateBonusNumber('a');
+      validateInput.validateBonusNumber('a');
     }).toThrow();
   });
 
   test('적절하지 않은 입력값이 들어오는 경우, 에러를 발생해야 합니다. - 입력받은 수가 1개가 아닌 경우', () => {
     expect(() => {
-      lottoProgram.validateBonusNumber('1,2');
+      validateInput.validateBonusNumber('1,2');
     }).toThrow();
 
     expect(() => {
-      lottoProgram.validateBonusNumber('');
+      validateInput.validateBonusNumber('');
     }).toThrow();
   });
 
   test('적절하지 않은 입력값이 들어오는 경우, 에러를 발생해야 합니다. - 로또 범위 내의 수가 아닌 경우', () => {
     expect(() => {
-      lottoProgram.validateBonusNumber('46');
+      validateInput.validateBonusNumber('46');
     }).toThrow();
 
     expect(() => {
-      lottoProgram.validateBonusNumber('0');
+      validateInput.validateBonusNumber('0');
     }).toThrow();
     expect(() => {
-      lottoProgram.validateBonusNumber('-12');
+      validateInput.validateBonusNumber('-12');
     }).toThrow();
   });
 
   test('적절하지 않은 입력값이 들어오는 경우, 에러를 발생해야 합니다. - 자연수가 아닌 경우', () => {
     expect(() => {
-      lottoProgram.validateBonusNumber('4.7');
+      validateInput.validateBonusNumber('4.7');
     }).toThrow();
   });
 
   test('적절하지 않은 입력값이 들어오는 경우, 에러를 발생해야 합니다. - 중복된 값이 존재하는 경우', () => {
     expect(() => {
       mockQuestions(["8000", "1,2,3,4,5,6", "3"]);
-      lottoProgram.start();
+      validateInput.start();
     }).toThrow();
   });
 });
