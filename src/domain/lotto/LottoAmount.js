799f00e05befa249a7cea62a10b3b02c0b317c92
hwanfront <kim015jh@gmail.com>
2022-11-11 12:37:14 +0900

    feat(LottoAmount): add LottoAmount validate method

--- src/domain/lotto/LottoAmount.js
+++ src/domain/lotto/LottoAmount.js
@@ -1,22 +1,39 @@
+const LottoAmountDivideException = require('../../exception/lotto/LottoAmountDivideException');
+const LottoAmountLessException = require('../../exception/lotto/LottoAmountLessException');
+const LottoAmountNotNumberException = require('../../exception/lotto/LottoAmountNotNumberException');
+
 class LottoAmount {
-  static LOTTO_PRICE = 1000;
+  static #LOTTO_PRICE = 1000;
 
   #value;
 
   constructor(value) {
+    LottoAmount.validate(value);
     this.#value = value;
-  }
-
-  static validate(value) {
-
   }
 
   static from(value) {
     return new LottoAmount(value);
   }
 
-  getLottoCount() {
+  static validate(value) {
+    if (Number.isNaN(parseInt(value, 10)) || typeof value !== 'number') {
+      throw new LottoAmountNotNumberException(value);
+    }
+    if (value < LottoAmount.#LOTTO_PRICE) {
+      throw new LottoAmountLessException(LottoAmount.#LOTTO_PRICE);
+    }
+    if (value % 1000 !== 0) {
+      throw new LottoAmountDivideException(LottoAmount.#LOTTO_PRICE);
+    }
+  }
 
+  getValue() {
+    return this.#value;
+  }
+
+  getLottoCount() {
+    
   }
 }
 
