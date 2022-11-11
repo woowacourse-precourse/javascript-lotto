26222dd18826838cd707630665f22e779df92baa
unknown <chn0202@gmail.com>
2022-11-11 16:18:00 +0900

    fix(Lotto): validata오류 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,7 +10,7 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    if (!this.checkDuplicateNumber(numbers)) {
+    if (!this.duplicateNumber(numbers)) {
       throw new Error("[ERROR] 로또 번호는 중복이 없어야 합니다.");
     }
   }
