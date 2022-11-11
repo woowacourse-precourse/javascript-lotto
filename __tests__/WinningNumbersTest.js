31db2551bb672b623f50c3ce5a4d937a0e6aa229
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-11 22:27:33 +0900

    test(winningNumbers): 입력받은 당첨 번호가 쉼표로 구분받지 않은 상황이라면 에러를 반환하는 테스트

--- __tests__/WinningNumbersTest.js
+++ __tests__/WinningNumbersTest.js
@@ -1,9 +1,17 @@
 const { mockQuestions } = require('./ApplicationTest');
 const App = require('../src/App');
 
-describe('입력받은 당첨 번호가 6자리인지 확인하는 테스트', () => {
+describe('입력받은 당첨 번호가 유효한지 확인하는 테스트', () => {
   test('사용자가 입력한 당첨 번호가 6자리가 아니라면 에러를 발생시킨다.', () => {
     mockQuestions(['1000', '1,2,3,4,5']);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow('[ERROR]');
+  });
+
+  test('사용자가 입력한 당첨 번호가 쉼표로 구분받지 않은 상황이라면 에러를 발생시킨다.', () => {
+    mockQuestions(['1000', '123456']);
     expect(() => {
       const app = new App();
       app.play();
