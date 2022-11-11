2bbe24e3617577a550a3c857617ec14eef5afe9e
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-11 19:28:27 +0900

    Update Lotto.test.js

--- src/Components/Lotto/Lotto.test.js
+++ src/Components/Lotto/Lotto.test.js
@@ -1,28 +1,26 @@
-const Lotto = require("./Lotto");
+const LottoNumberCount = require('../LottoNumberCount/LottoNumberCount');
+const Lotto = require('./Lotto');
 
-describe("Lotto 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('Lotto 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  test("로또 번호와 당첨 번호, 보너스 번호를 비교 후 결과를 반환한다.", () => {
+  test('로또 번호와 당첨 번호, 보너스 번호를 비교 후 결과를 반환한다.', () => {
     const lottoNumbers = [1, 3, 5, 14, 22, 45];
     const winningNumbers = [1, 2, 3, 4, 5, 6];
     const BONUS_NUMBER = 7;
-    const lottoResult = new Lotto(lottoNumbers).compareTo(
-      winningNumbers,
-      BONUS_NUMBER
-    );
+    const lottoResult = new Lotto(lottoNumbers).compareTo(winningNumbers, BONUS_NUMBER);
+    const lottoNumberCount = new LottoNumberCount(3);
 
-    expect(lottoResult.getMatchedNumberCount()).toBe(3);
-    expect(lottoResult.isBonusNumberMatched()).toBe(false);
+    expect(lottoResult.isWinning(lottoNumberCount)).toBe(true);
   });
 });
