d0bf600952c657e70ab71af3fe81b6512b7000d7
yan9vvoojin <yan9vvoojin@naver.com>
2022-11-11 14:35:14 +0900

    feat(App): 구입 금액만큼 로또를 발행하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-const { Console } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
 
 class App {
-  constructor() {}
+  constructor() {
+    this.myMoney;
+    this.myLottos = [];
+  }
 
   play() {
     this.requestMoneyInput();
@@ -11,6 +15,8 @@ class App {
     Console.readLine('구입 금액을 입력해 주세요.\n', (input) => {
       const money = Number(input);
       this.validateMoney(money);
+      this.myMoney = money;
+      this.createLotto(this.myMoney);
     });
   }
 
@@ -20,6 +26,18 @@ class App {
     }
     if (!Number.isInteger(money / 1000)) {
       throw new Error('[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요.');
+    }
+  }
+
+  createLotto(money) {
+    const numberToCreate = money / 1000;
+    Console.print(`\n${numberToCreate}개를 구매했습니다.`);
+    while (this.myLottos.length < numberToCreate) {
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+      const lotto = new Lotto(numbers);
+      const lottoNumbers = lotto.getNumbers();
+      this.myLottos.push(lottoNumbers);
+      Console.print(`[${lottoNumbers.join(', ')}]`);
     }
   }
 }
