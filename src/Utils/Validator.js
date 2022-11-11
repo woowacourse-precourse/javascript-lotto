6416c4eba7ae1d1aa03e51bba74d863d063b46a8
Cobocho <rlaalsrb1111@naver.com>
2022-11-11 21:18:21 +0900

    feat: 천원 단위 판별 메서드 구현

--- src/Utils/Validator.js
+++ src/Utils/Validator.js
@@ -4,18 +4,21 @@ class Validator {
   static amountValidCheck(amount) {
     Validator.isBlank(amount);
     Validator.hasBlank(amount);
+    amount = Number(amount);
     Validator.isNotNumber(amount);
   }
   static isBlank(value) {
     if(value === '') return MESSAGES.ERROR.isBlank;
   }
-  static isNotNumber(value) {
-    value = Number(value);
-    if(isNaN(value)) return MESSAGES.ERROR.isNotNumber;
-  }
   static hasBlank(value) {
     const regex = /\s/g;    
     if(value.match(regex)) return MESSAGES.ERROR.hasBlank;
+  }
+  static isNotNumber(value) {
+    if(isNaN(value)) return MESSAGES.ERROR.isNotNumber;
+  }
+  static isNotKilo(value) {
+    if(value % 1000 !== 0) return MESSAGES.ERROR.isNotKilo;
   }
 }
 
--- src/constants.js
+++ src/constants.js
@@ -6,6 +6,7 @@ const MESSAGES = {
     isBlank: '공백이 아닌 값을 입력해주세요!',
     isNotNumber: '숫자를 입력해주세요!',
     hasBlank: '공백이 없는 숫자를 입력해주세요!',
+    isNotKilo: '1,000원 단위의 금액을 입력해주세요!',
   },
 }
 
