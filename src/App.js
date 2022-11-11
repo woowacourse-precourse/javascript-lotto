d896c29e5e94eaacecee551999dcda761b3c5c92
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-11 18:33:14 +0900

    feat(App): play 메소드 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+const Controller = require('./controller/Controller');
+
 class App {
-  play() {}
+  play() {
+    const controller = new Controller();
+    controller.start();
+  }
 }
 
 module.exports = App;
