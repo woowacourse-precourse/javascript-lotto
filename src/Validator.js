6a27c1a98abfc7a8886e5dbe48df938a9a7a88c5
evencoding <evencoding@gmail.com>
2022-11-12 06:45:47 +0900

    refactor:(Validator): 공통으로 체크하는 항목을 따로 메소드로 구현, 변수명 변경

--- src/Validator.js
+++ src/Validator.js
@@ -2,50 +2,64 @@ const { ERROR_MESSAGE, LOTTO, REGEX } = require('./constants');
 
 class Validator {
   static throwErrorIfInvalidMoney(money) {
+    this.throwErrorIfhasBlack(money);
+    this.throwErrorIfstartsWithZero(money);
     if (!REGEX.money.test(money)) {
-      throw Error(ERROR_MESSAGE.MONEY_FORM);
+      throw Error(ERROR_MESSAGE.INT_FORM);
     }
     if (money % LOTTO.PRICE) {
-      throw Error(ERROR_MESSAGE.MONEY_MULTIPLE);
-    }
-    if (money[0] === '0') {
-      throw Error(ERROR_MESSAGE.INT_FORM);
+      throw Error(ERROR_MESSAGE.HAS_MOD);
     }
   }
 
   static throwErrorIfInvalidWinningNumbers(inputValue) {
+    this.throwErrorIfhasBlack(inputValue);
     const winningNumbers = inputValue.split(',');
     if (!REGEX.winningNumber.test(inputValue)) {
       throw Error(ERROR_MESSAGE.WINNING_NUM_FORM);
     }
     winningNumbers.forEach((number) => {
-      if (number.startsWith('0')) {
-        throw Error(ERROR_MESSAGE.INT_FORM);
-      }
+      this.throwErrorIfstartsWithZero(number);
     });
     if (winningNumbers.length !== LOTTO.LENGTH) {
-      throw Error(ERROR_MESSAGE.LOTTO_NUM_LENGTH);
+      throw Error(ERROR_MESSAGE.LENGTH);
     }
     winningNumbers.forEach((number) => {
-      if (number > LOTTO.MAX_NUMBER || number < LOTTO.MIN_NUMBER) {
-        throw Error(ERROR_MESSAGE.LOTTO_NUM_RANGE);
-      }
+      this.throwErrorIfoutOfRange(number);
     });
     if (new Set(winningNumbers).size < LOTTO.LENGTH) {
-      throw Error(ERROR_MESSAGE.WINNING_NUM_DUPLICATION);
+      throw Error(ERROR_MESSAGE.DUPLICATION);
     }
   }
 
   static throwErrorIfInvalidBonusNumber(winningNumbers, bonusNumber) {
-    if (
-      !REGEX.bonusNumber.test(bonusNumber) ||
-      bonusNumber > LOTTO.MAX_NUMBER ||
-      bonusNumber < LOTTO.MIN_NUMBER
-    ) {
-      throw Error(ERROR_MESSAGE.LOTTO_NUM_RANGE);
+    this.throwErrorIfhasBlack(bonusNumber);
+    this.outOfRange(bonusNumber);
+    this.throwErrorIfstartsWithZero(bonusNumber);
+    this.throwErrorIfoutOfRange(bonusNumber);
+    if (!REGEX.bonusNumber.test(bonusNumber)) {
+      throw Error(ERROR_MESSAGE.INT_FORM);
     }
     if (winningNumbers.split(',').includes(bonusNumber)) {
-      throw Error(ERROR_MESSAGE.INCLUDED_WINNING_NUM);
+      throw Error(ERROR_MESSAGE.WINNING_HAS);
+    }
+  }
+
+  static throwErrorIfhasBlack(string) {
+    if (string.includes(' ')) {
+      throw Error(ERROR_MESSAGE.HAS_BLACK);
+    }
+  }
+
+  static throwErrorIfstartsWithZero(string) {
+    if (string.startsWith('0')) {
+      throw Error(ERROR_MESSAGE.START_WITH_ZERO);
+    }
+  }
+
+  static throwErrorIfoutOfRange(number) {
+    if (number < 1 || number > 45) {
+      throw Error(ERROR_MESSAGE.OUT_OF_RANGE);
     }
   }
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -7,15 +7,15 @@ const LOTTO = Object.freeze({
 });
 
 const ERROR_MESSAGE = Object.freeze({
-  MONEY_FORM: '[ERROR] 음이아닌 정수를 입력해야 합니다.',
-  MONEY_MULTIPLE: '[ERROR] 1000의 배수를 입력해야 합니다.',
-  INT_FORM: '[ERROR] 정수 앞에 0이 올 수 없습니다.',
+  HAS_BLACK: '[ERROR] 입력에 공백을 포함시킬 수 없습니다.',
+  START_WITH_ZERO: '[ERROR] 정수의 앞에 0이 올 수 없습니다.',
+  OUT_OF_RANGE: '[ERROR] 1에서 45까지의 숫자를 입력해야 합니다.',
+  INT_FORM: '[ERROR] 음이아닌 정수를 입력해야 합니다.',
+  HAS_MOD: '[ERROR] 1000의 배수를 입력해야 합니다.',
   WINNING_NUM_FORM: '[ERROR] 숫자를 ,로 구분해 입력해야 합니다.',
-  LOTTO_NUM_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
-  LOTTO_NUM_RANGE: '[ERROR] 1에서 45까지의 숫자를 입력해야 합니다.',
-  WINNING_NUM_DUPLICATION: '[ERROR] 중복된 숫자를 입력할 수 없습니다.',
-  INCLUDED_WINNING_NUM:
-    '[ERROR] 이미 당첨 번호에 포함된 번호를 입력할 수 없습니다.',
+  LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
+  DUPLICATION: '[ERROR] 중복된 숫자를 입력할 수 없습니다.',
+  WINNING_HAS: '[ERROR] 이미 당첨 번호에 포함된 번호를 입력할 수 없습니다.',
 });
 
 const MESSAGE = Object.freeze({
