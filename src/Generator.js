9f5b270e068b655eca45cf9e2fcaaac45a244a97
sunub <bsc5672@gmail.com>
2022-11-12 03:24:01 +0900

    feat : 당첨번호와 복권 번호를 비교하여 결과를 생성하는 기능 추가
    도메인 로직 2에서 입력 받은 당첨 번호와 복권 번호를 비교하여 몇 개의
    숫자가 일치하는지 판단하고 구분하여 1등 ~ 5등까지 구분하는 기능을
    수행한다.

--- src/Generator.js
+++ src/Generator.js
@@ -14,6 +14,39 @@ class Generator {
     }
     return lottoTicket;
   }
+
+  generateLottoResult(lottoNumbers, lottoTicket) {
+    const atLeastWinNumber = 3;
+    const needToCheckBonus = 5;
+    const lottoResult = {
+      6: 0,
+      5: 0,
+      4: 0,
+      3: 0,
+    };
+
+    lottoTicket.forEach((ticketNumber) => {
+      const matchingNumber = this.countMatchNumber(lottoNumbers, ticketNumber);
+      if (needToCheckBonus === matchingNumber) {
+        lottoResult[matchingNumber] = ticketNumber;
+      }
+      if (atLeastWinNumber <= matchingNumber && needToCheckBonus !== matchingNumber) {
+        lottoResult[matchingNumber] += 1;
+      }
+    });
+
+    return lottoResult;
+  }
+
+  countMatchNumber(lottoNumber, ticketNumber) {
+    let matchingCount = 0;
+    for (let i = 0; i < lottoNumber.length; i++) {
+      if (ticketNumber.includes(lottoNumber[i])) {
+        matchingCount++;
+      }
+    }
+    return matchingCount;
+  }
 }
 
 module.exports = Generator;
