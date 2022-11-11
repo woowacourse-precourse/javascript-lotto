7682b678b033934a8662ff3b552844c89dfbd183
yan9vvoojin <yan9vvoojin@naver.com>
2022-11-11 13:09:00 +0900

    feat(App): 사용자에게 구입 금액을 입력받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  constructor() {}
+
+  play() {
+    this.requestMoneyInput();
+  }
+
+  requestMoneyInput() {
+    Console.readLine('구입 금액을 입력해 주세요.\n', (input) => {
+      const money = Number(input);
+    });
+  }
 }
 
 module.exports = App;
