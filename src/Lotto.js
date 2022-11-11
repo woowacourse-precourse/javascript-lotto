1272434fb06ead78485f56e32b9ee22c3b3d2028
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-11-11 19:11:42 +0900

    refactor : 당첨 번호 + 보너스 번호와 발행 받은 복권의 번호와 비교 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -5,8 +5,8 @@ class Lotto {
   constructor(numbers) {
     this.messages = {
       inputMoney: "구입금액을 입력해 주세요.",
-      inputWinNumber: "당첨 번호를 입력해 주세요.",
-      inputBonusNumber: "보너스 번호를 입력해 주세요.",
+      inputWinNumberPrint: "당첨 번호를 입력해 주세요.",
+      inputBonusNumberPrint: "보너스 번호를 입력해 주세요.",
     };
     this.myLotto = [];
     this.useMoney = 0;
@@ -14,6 +14,13 @@ class Lotto {
     this.bonusNumber = 0;
     this.validate(numbers);
     this.#numbers = numbers;
+    this.winLotto = {
+      first: 0,
+      second: 0,
+      third: 0,
+      fourth: 0,
+      fifth: 0,
+    };
   }
 
   validate(numbers) {
@@ -32,8 +39,11 @@ class Lotto {
   process() {
     this.inputMoney();
     this.lotto();
-    this.inputNumberAndBonusNumber();
-    this.lottoResult();
+    this.inputWinNumber();
+    this.inputBonusNumber();
+    this.myLottoAndWinNumber();
+    this.lottoResultPrint();
+    this.rateOfReturnPrint();
   }
 
   inputMoney() {
@@ -76,19 +86,18 @@ class Lotto {
     MissionUtils.Console.print(`[${this.myLotto[number].join(", ")}]`);
   }
 
-  inputNumberAndBonusNumber() {
-    MissionUtils.Console.print(`${this.messages.inputWinNumber}`);
+  inputWinNumber() {
+    MissionUtils.Console.print(`${this.messages.inputWinNumberPrint}`);
     MissionUtils.Console.readLine("", (answer) => {
       this.winNumber = answer.split(",");
     });
-    MissionUtils.Console.print(`${this.messages.inputBonusNumber}`);
+  }
+
+  inputBonusNumber() {
+    MissionUtils.Console.print(`${this.messages.inputBonusNumberPrint}`);
     MissionUtils.Console.readLine("", (answer) => {
       this.bonusNumber = parseInt(answer);
     });
-  }
-
-  lottoResult() {
-    this.myLottoAndWinNumber();
   }
 
   myLottoAndWinNumber() {
@@ -96,56 +105,49 @@ class Lotto {
       const matchNumber = this.winNumber.filter((item) =>
         this.myLotto[i].includes(parseInt(item))
       );
-      if (matchNumber.length >= 3) this.winLottoCount(matchNumber, i);
+      if (matchNumber.length >= 3) {
+        this.winLottoCountIf(matchNumber, i);
+      }
     }
   }
 
-  winLottoCount(matchNumber, i) {
-    let winLotto = {
-      first: 0,
-      second: 0,
-      third: 0,
-      fourth: 0,
-      fifth: 0,
-    };
-    this.winLottoCountIf(matchNumber, i, winLotto);
-    this.lottoResultPrint(winLotto);
-    this.rateOfReturnPrint(winLotto);
-  }
-
-  winLottoCountIf(matchNumber, i, winLotto) {
-    if (matchNumber.length == 3) winLotto.fifth++;
-    if (matchNumber.length == 4) winLotto.fourth++;
+  winLottoCountIf(matchNumber, i) {
+    if (matchNumber.length == 3) this.winLotto.fifth++;
+    if (matchNumber.length == 4) this.winLotto.fourth++;
     if (
       matchNumber.length == 5 &&
       this.myLotto[i].includes(this.bonusNumber) === false
     )
-      winLotto.third++;
-    if (matchNumber.length == 5 && myLotto[i].includes(this.bonusNumber))
-      winLotto.second++;
-    if (matchNumber.length == 6) winLotto.first++;
+      this.winLotto.third++;
+    if (matchNumber.length == 5 && this.myLotto[i].includes(this.bonusNumber))
+      this.winLotto.second++;
+    if (matchNumber.length == 6) this.winLotto.first++;
   }
 
-  lottoResultPrint(winLotto) {
+  lottoResultPrint() {
     MissionUtils.Console.print("당첨통계\n---");
-    MissionUtils.Console.print(`3개 일치 (5,000원) - ${winLotto.fifth}개`);
-    MissionUtils.Console.print(`4개 일치 (50,000원) - ${winLotto.fourth}개`);
-    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${winLotto.third}개`);
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${this.winLotto.fifth}개`);
     MissionUtils.Console.print(
-      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winLotto.second}개`
+      `4개 일치 (50,000원) - ${this.winLotto.fourth}개`
     );
     MissionUtils.Console.print(
-      `6개 일치 (2,000,000,000원) - ${winLotto.first}개`
+      `5개 일치 (1,500,000원) - ${this.winLotto.third}개`
+    );
+    MissionUtils.Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.winLotto.second}개`
+    );
+    MissionUtils.Console.print(
+      `6개 일치 (2,000,000,000원) - ${this.winLotto.first}개`
     );
   }
 
-  rateOfReturnPrint(winLotto) {
+  rateOfReturnPrint() {
     const rateOfReturn =
-      winLotto.first * 2000000000 +
-      winLotto.second * 30000000 +
-      winLotto.third * 1500000 +
-      winLotto.fourth * 50000 +
-      winLotto.fifth * 5000;
+      this.winLotto.first * 2000000000 +
+      this.winLotto.second * 30000000 +
+      this.winLotto.third * 1500000 +
+      this.winLotto.fourth * 50000 +
+      this.winLotto.fifth * 5000;
     MissionUtils.Console.print(
       `총 수익률은 ${((rateOfReturn / this.useMoney) * 100).toFixed(1)}%입니다.`
     );
