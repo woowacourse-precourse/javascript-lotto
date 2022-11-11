05317d376042abf123765a13b81763dc9fafe866
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-11-12 00:17:23 +0900

    Test(feature No.3): add code to check the correct winNumber

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,20 +1,24 @@
 const Lotto = require("../src/Lotto");
 
 describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+  test.only("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR] 로또 번호는 6개여야 합니다.");
+    }).toThrow("[ERROR] 당첨번호는 6개여야 합니다.");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test.only("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow("[ERROR] 당첨번호는 중복되지 않은 6개 숫자여야합니다");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test.only("로또 번호에 범위가 벗어난 값이 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([0, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR] 당첨번호는 1~45사의 숫자이어야합니다");
+  });
+
   test("금액이 숫자이면서 1000의 배수가 아닌 경우 예외가 발생한다.", () => {
     expect(() => {
       Lotto.checkMoney("1001");
@@ -27,7 +31,7 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR] 금액은 1000의 배수인 숫자이여야 합니다.");
   });
 
-  test.only("입력받은 금액/로또가격 만큼의 배열이 반환된다", () => {
+  test("입력받은 금액/로또가격 만큼의 배열이 반환된다", () => {
     const money = 3000;
     const price = 1000;
     expect(() => Lotto.genLottoNumArr(money).length.toEqual(money / price));
