71f829348db6269f294fa1ff679f74ef2ebb6804
yeonsuBeak <soothbugger@gmail.com>
2022-11-11 18:11:13 +0900

    feat(lotto): 발행한 로또 수량 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ const Money = require("./Money");
 class App {
   constructor() {
     this.money = 0;
+    this.quantity = 0;
   }
 
   play() {
     Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
       new Money(money);
       this.money = money;
+      this.printQuantity(this.money);
     });
+  }
+
+  printQuantity(money) {
+    const UNIT = 1000;
+    this.quantity = parseInt(money, 10) / UNIT;
+    Console.print(`\n${this.quantity}개를 구매했습니다.`);
   }
 }
 
