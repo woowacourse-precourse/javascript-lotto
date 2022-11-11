68bc478320a57d70756802b8551078fef247d65b
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-12 00:26:19 +0900

    feat: 로또 번호를 발행할 로또 개수만큼 생성하는 기능 구현
      - 로또 번호는 오름차순으로 정렬

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-const { Console } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 
 class App {
   getCount() {
     Console.readLine('구입금액을 입력해 주세요.\n', (amount) => {
       this.lottoCount = amount / 1000;
     });
+  }
+
+  generateLottos() {
+    const { lottoCount } = this;
+    let lottos = [];
+
+    while (lottos.length < lottoCount) {
+      let numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+
+      lottos.push(numbers.sort((num1, num2) => num1 - num2));
+    }
+    this.lottos = lottos;
   }
 
   play() {}
