30e89f69d33d05c07edd715a4cc8554334c6d6e9
Hchan2 <seongchan_@naver.com>
2022-11-11 22:15:25 +0900

    로또 번호 입력 에러 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,12 +9,38 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    this.notComma(numbers);
+    this.overlapCheck(numbers);
+    this.lengthCheck(numbers);
+    this.notNumberCheck(numbers);
+  }
+
+  notComma(numbers) {
+    if (!numbers.includes(',')) {
+      throw new Error("[ERROR] (,)로 구분하여 숫자를 입력해주세요.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  overlapCheck(numbers) {
+    const numberSplit = numbers.split(',');
+    const numberSet = new Set(numberSplit);
+    if (numberSet.size !== 6) {
+        throw new Error("[ERROR] (,)로 구분하여 중복되지 않는 숫자를 입력해주세요.");
+    }
+  }
+
+  lengthCheck(numbers) {
+    const commaLength = numbers.split(',').length - 1;
+    if (commaLength !== 5) {
+        throw new Error("[ERROR] (,)로 구분하여 6자리 숫자를 입력해주세요. (예. 1,2,3,4,5,6)");
+    }
+  }
+
+  notNumberCheck(numbers) {
+    if (isNaN(numbers)) {
+        throw new Error('[ERROR] 숫자만 입력해주세요.');
+    }
+  }
 }
 
 module.exports = Lotto;
