c686f59bf7b30c0c59c434fe4b248df8802a3cb4
Cobocho <rlaalsrb1111@naver.com>
2022-11-11 23:18:41 +0900

    chore: [ERROR] 말머리 상수화 후 적용

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,16 @@
+const HEADER = {
+  error: '[ERROR]',
+}
+
 const MESSAGES = {
   GAME: {
     requirePurchaseAmount: '구입금액을 입력해 주세요.',
   },
   ERROR: {
-    isBlank: '공백이 아닌 값을 입력해주세요!',
-    isNotNumber: '숫자를 입력해주세요!',
-    hasBlank: '공백이 없는 숫자를 입력해주세요!',
-    isNotKilo: '1,000원 단위의 금액을 입력해주세요!',
+    isBlank: `${HEADER.error} 공백이 아닌 값을 입력해주세요!`,
+    isNotNumber: `${HEADER.error} 숫자를 입력해주세요!`,
+    hasBlank: `${HEADER.error} 공백이 없는 숫자를 입력해주세요!`,
+    isNotKilo: `${HEADER.error} 1,000원 단위의 금액을 입력해주세요!`,
   },
 }
 
