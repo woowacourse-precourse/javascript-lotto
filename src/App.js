5684164e485216f6ed7506fe85c0297560f716aa
sungjihyun <jhsung23@gmail.com>
2022-11-11 18:25:30 +0900

    feat(App): 입력받은 구입 금액의 유효성 검사 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const { Console } = require("@woowacourse/mission-utils");
 
-const { MESSAGE } = require("../src/utils/constants");
+const { MESSAGE, AMOUNT_UNIT, ERROR } = require("../src/utils/constants");
 
 class App {
   play() {}
@@ -10,6 +10,23 @@ class App {
     Console.readLine(MESSAGE.ENTER_PURCHASE_AMOUNT, (amount) => {
       console.log(amount);
     });
+  }
+
+  //구매 금액 유효성 검사하기
+  validateInput(input) {
+    const trimmedInput = input.trim();
+
+    //문자가 섞여있지 않은지 검사
+    if (trimmedInput.match(RegExp(/([^0-9])/g)) !== null) {
+      throw new Error(ERROR.INVALID_INPUT);
+    }
+
+    //금액이 1000으로 나눠떨어지는지 검사
+    if (Number(trimmedInput) % AMOUNT_UNIT !== 0) {
+      throw new Error(ERROR.INDIVISIBLE);
+    }
+
+    return true;
   }
 }
 
