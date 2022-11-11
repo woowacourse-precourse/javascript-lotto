b671d00cceee0887e5739138253a410e89c0b66a
ReturnReason <reasonz@daum.net>
2022-11-12 01:11:37 +0900

    feat: 사용자가 로또를 몇 개 구매했는지 출력하는 기능 추가

--- src/LottoView.js
+++ src/LottoView.js
@@ -1,10 +1,14 @@
 const { Console } = require('./utils/missionUtil');
-const { INPUT_MESSAGES } = require('./common/messages');
+const { INPUT_MESSAGES, OUTPUT_MESSAGES } = require('./common/messages');
+const { LOTTO_INFO } = require('./common/constants');
+const Validator = require('./Validator');
 
 class LottoView {
   static getPurchaseAmount() {
-    Console.readLine(`${INPUT_MESSAGES.AMOUNT}\n`, (answer) => {
-      console.log(answer);
+    Console.readLine(`${INPUT_MESSAGES.AMOUNT}\n`, (money) => {
+      Validator.checkValidInput(money);
+      const lottoCount = money / LOTTO_INFO.PRICE;
+      Console.print(`\n${OUTPUT_MESSAGES.BUY(lottoCount)}`);
     });
   }
 }
