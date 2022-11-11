277325fecd579fa954f87f1a7f995ae92531e0f8
Hoon <si932174@gmail.com>
2022-11-11 14:14:24 +0900

    feat : 금액입력기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  static priceInput() {
+    Console.readLine('구입금액을 입력해 주세요.\n', answer => {
+      Console.print(answer);
+    });
+  }
+
+  play() {
+    App.priceInput();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
