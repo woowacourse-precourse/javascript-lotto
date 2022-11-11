2df2423962e1f7a09f609c47a44c72e4fcd22bed
gilpop8663 <wolfye@naver.com>
2022-11-11 13:49:46 +0900

    feat: 로또 구입 금액을 입력 받는 기능 구현
    "구입금액을 입력해 주세요."를 출력한다
    
    구입 금액은 1,000원 단위로 입력 받으며 1,000원으로 나누어 떨어지지 않는 경우 예외 처리한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { QUESTION_MESSAGE, ERROR_MESSAGE } = require('./libs/const');
+
 class App {
-  play() {}
+  play() {
+    this.start();
+  }
+
+  start() {
+    Console.readLine(QUESTION_MESSAGE.buy, money => {
+      if (money % 1000 !== 0) throw new Error(ERROR_MESSAGE.purchase);
+    });
+  }
 }
+
+const app = new App();
+
+app.play();
 
 module.exports = App;
