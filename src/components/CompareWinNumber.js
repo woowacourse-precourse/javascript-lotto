fffe98ed0ea6eff92f8a1ce1dedaf3e07eacd20a
semnil5202 <semnil5202@gmail.com>
2022-11-12 05:41:01 +0900

    feat: 당첨 번호 및 보너스 번호와 발행한 로또를 비교하는 기능

---
+++ src/components/CompareWinNumber.js
@@ -0,0 +1,31 @@
+class compareWinNumber {
+  winAndBonus(lotteries, winNumber, bonusNumber) {
+    const results = [];
+    lotteries.forEach((lotteries) => {
+      results.push(this.countWinNumber({ lotteries, winNumber, bonusNumber }));
+    });
+    return results;
+  }
+
+  countWinNumber({ lotteries, winNumber, bonusNumber }) {
+    let winCount = 0;
+    let isBonus = false;
+    winNumber.forEach((number) => {
+      if (lotteries.includes(number)) winCount += 1;
+    });
+    if (winCount === 5) {
+      isBonus = this.checkBonus(lotteries, bonusNumber);
+    }
+    return [winCount, isBonus];
+  }
+
+  checkBonus(lotteries, bonusNumber) {
+    let isBonus = false;
+    lotteries.forEach((number) => {
+      if (number === bonusNumber) isBonus = true;
+    });
+    return isBonus;
+  }
+}
+
+module.exports = compareWinNumber;
