cc034521adcc9ebfc9cd4aa44ad2173bad19f4d8
장경은 <jang_kyung_eun@gyeong-eun-ui-MacBookAir.local>
2022-11-12 04:32:59 +0900

    feat: 로또 번호 6개를 뽑는 기능 구현 (정렬 X)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Console = MissionUtils.Console;
 const Random = MissionUtils.Random;
+let numOfLotto = 0;
+let lottoNumObj = {};
 
 class App {
+  lottoMoney() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+      numOfLotto = money/1000;
+      Console.print(`\n${numOfLotto}개를 구매했습니다.`);
+      this.createNumber();
+    });
+  }
+
+  createNumber() {
+    for (let i = 1; i <= numOfLotto; i++) {
+      lottoNumObj[`lotto${i}`] = Random.pickUniqueNumbersInRange(1, 45, 6);
+    }
+    for (let i = 1; i <= numOfLotto; i++) {
+      Console.print(lottoNumObj[`lotto${i}`]);
+    }
+  }
+
   play() {
     this.lottoMoney();
   }
-
-  lottoMoney() {
-    Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
-      Console.print(`\n${money/1000}개를 구매했습니다.`);
-    });
-  }
 }
-
-//const app = new App();
-//app.play();
 
 module.exports = App;
