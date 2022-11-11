0d6c75fd82af3c58a988c1db2e9262da52526d98
이건우 <lifeflavor@igeon-uui-MacBookPro.local>
2022-11-11 15:06:25 +0900

    feat(Lotto)- 몇개의 숫자가 같은지 알 수 있다.

---
+++ src/modules/CorrectLotto.js
@@ -0,0 +1,25 @@
+class CorrectLotto {
+  #lottoArr = [];
+  #answer;
+  #bonus;
+  #count = [];
+  constructor(lottoArr = [], answer, bonus) {
+    this.#lottoArr = lottoArr;
+    this.#answer = answer;
+    this.#bonus = bonus;
+  }
+  //몇개의 숫자가 같은지 알 수 있다.
+  haveCorrect() {
+    this.#lottoArr.forEach((arr) => {
+      this.#count.push(
+        arr.reduce(
+          (count, element) =>
+            this.#answer.includes(element) ? (count += 1) : count,
+          0
+        )
+      );
+    });
+    return this.#count; // ex [0, 2, 0, 1, 0, 0, 1, 3]
+  }
+}
+module.exports = CorrectLotto;
