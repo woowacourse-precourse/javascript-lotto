1d3ad364f9295f1d5651cec7c4b11a15ffbb8581
hwanfront <kim015jh@gmail.com>
2022-11-11 16:13:51 +0900

    feat(LottoNumber): 로또 번호에 대한 예외처리 추가

--- src/domain/lotto/LottoNumber.js
+++ src/domain/lotto/LottoNumber.js
@@ -1,3 +1,7 @@
+const LottoNumberRangeException = require('../../exception/lotto/LottoNumberRangeException');
+const NotNumberException = require('../../exception/NotNumberException');
+const { checkNotNumber } = require('../../util/validate');
+
 class LottoNumber {
   static RANGE = Object.freeze({
     min: 1,
@@ -7,15 +11,30 @@ class LottoNumber {
   #number;
 
   constructor(number) {
+    LottoNumber.validate(number);
     this.#number = number;
   }
 
-  static validate(number) {
+  static validate(value) {
+    if (checkNotNumber(value)) {
+      throw new NotNumberException(value);
+    }
 
+    if (value < LottoNumber.RANGE.min) {
+      throw new LottoNumberRangeException(LottoNumber.RANGE);
+    }
+
+    if (value > LottoNumber.RANGE.max) {
+      throw new LottoNumberRangeException(LottoNumber.RANGE);
+    }
   }
 
   static of(number) {
     return new LottoNumber(number);
+  }
+
+  getNumber() {
+    return this.#number;
   }
 }
 
