7bd0ba5cf79066e9c7658721d3800d8a2902d1be
osdoonhyun <skw1996@naver.com>
2022-11-11 17:42:40 +0900

    feat(App): 기능목록[1] 로또 구입 금액 1000원 단위로 입력 받기 기능 & 예외 처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+const Exception = require("./Exception");
+const { MESSAGES } = require("./Constants");
+
 class App {
+  constructor() {
+    this.exception = new Exception();
+  }
+
+  purchaseLotto() {
+    Console.readLine(MESSAGES.ENTER_PURCHASE_AMOUNT, (amount) => {
+      this.exception.checkAmountExceptions(amount);
+    });
+  }
+
   play() {}
 }
 
