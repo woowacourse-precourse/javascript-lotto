ccbb2a6faf7aae5b0b90cd4b0bc3fd9019bca6f0
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-11 15:37:14 +0900

    test: Printer 클래스 print 인터페이스 테스트

---
+++ src/Components/Printer/Printer.test.js
@@ -0,0 +1,16 @@
+const Printer = require("./Printer");
+const { getLogSpy } = require("../../testFunction");
+
+describe("Printer 클래스 테스트", () => {
+  test("일치하는 번호 개수, 상금, 당첨 개수를 출력한다.", () => {
+    const logSpy = getLogSpy();
+    const printString = `6개 일치 (2,000,000,000원) - 0개`;
+    new Printer()
+      .setMatchedNumberCount("6")
+      .setWinningMoney("2,000,000,000")
+      .setMatchedLottoCount("0")
+      .print();
+
+    expect(logSpy).toHaveBeenCalledWith(printString);
+  });
+});
