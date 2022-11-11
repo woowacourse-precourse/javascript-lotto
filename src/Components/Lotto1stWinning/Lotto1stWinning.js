34483a03dc5e28f14d313e29279cc2f532f2b081
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-11 19:29:45 +0900

    refactor: Lotto1stWinning 클래스 matchedNumberCount 필드명, isSatisfied 메서드 수정

--- src/Components/Lotto1stWinning/Lotto1stWinning.js
+++ src/Components/Lotto1stWinning/Lotto1stWinning.js
@@ -1,14 +1,15 @@
 const AbstractLottoWinning = require('../AbstractLottoWinning/AbstractLottoWinning');
+const LottoNumberCount = require('../LottoNumberCount/LottoNumberCount');
 const Money = require('../Money/Money');
 const Printer = require('../Printer/Printer');
 
 class Lotto1stWinning extends AbstractLottoWinning {
-  matchedNumberCount = 6;
+  lottoNumberCount = new LottoNumberCount(6);
   winningMoney = new Money(2_000_000_000);
   printer = new Printer();
 
   isSatisfied(lottoResult) {
-    return this.matchedNumberCount === lottoResult.getMatchedNumberCount();
+    return lottoResult.isWinning(this.lottoNumberCount);
   }
 }
 
