7b1820e6c3c2a921a56b3ad1b88d7025cfa1a076
leeuihyun <meta212121@naver.com>
2022-11-11 23:23:17 +0900

    feat : 본인의 각각의 로또가 당첨번호와 몇개 일치하는지 세는 메서드

--- src/Result.js
+++ src/Result.js
@@ -11,6 +11,23 @@ class Result {
     this.#sameResultArray = new Array(5).fill(0);
     this.checkEachUserLottos();
   }
+
+  checkEachUserLottos() {
+    this.#userLottos.forEach((array) => {
+      this.checkWinningNumber(array);
+    });
+  }
+
+  checkWinningNumber(array) {
+    const sum = array.reduce((acc, value) => {
+      if (this.#winningNumber.includes(value)) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+
+    this.rankCount(sum);
+  }
 }
 
 module.exports = Result;
