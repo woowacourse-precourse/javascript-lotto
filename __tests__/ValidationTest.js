00593fcefd1f8e44ea83003e7bbccdf0a35f270d
2wndrhs <2wndrhs@gmail.com>
2022-11-11 07:34:50 -0800

    test(lotto): 로또 번호가 유효한 로또 번호인지 확인하는 테스트 작성

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,8 @@
-const { validate, isPurchaseInput } = require('../src/Validator');
+const {
+  validate,
+  isPurchaseInput,
+  isLottoNumber,
+} = require('../src/Validator');
 
 describe('Validator 클래스 테스트', () => {
   test('로또 구입 금액이 1000원 단위의 숫자가 아니면 예외 발생', () => {
@@ -6,6 +10,19 @@ describe('Validator 클래스 테스트', () => {
     inputs.forEach(input => {
       expect(() => {
         validate(input, isPurchaseInput);
+      }).toThrow('[ERROR]');
+    });
+  });
+
+  test('로또 번호가 1~45 범위의 중복되지 않는 6개의 숫자가 아니면 예외 발생', () => {
+    const inputs = [
+      [1, 2, 3, 4, 5, 6, 7],
+      [0, 1, 2, 3, 4, 46],
+      [1, 1, 2, 3, 4, 5],
+    ];
+    inputs.forEach(input => {
+      expect(() => {
+        validate(input, isLottoNumber);
       }).toThrow('[ERROR]');
     });
   });
