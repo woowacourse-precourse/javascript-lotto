b4065ea4765b750ca2d9d933c0850458a21844f3
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-11 14:20:14 +0900

    test: LottoResult 클래스 hasEqualNumberCount, hasBonusNumber 메서드 테스트 추가

--- src/Components/LottoResult/LottoResult.test.js
+++ src/Components/LottoResult/LottoResult.test.js
@@ -1,19 +1,41 @@
-const LottoResult = require('./LottoResult');
+const LottoResult = require("./LottoResult");
 
-describe('LottoResult 클래스 테스트', () => {
-  test('로또 번호와 일치하는 당첨 번호의 개수를 관리한다.', () => {
+describe("LottoResult 클래스 테스트", () => {
+  test("로또 번호와 일치하는 당첨 번호의 개수를 관리한다.", () => {
     const SAME_NUMBER_COUNT = 7;
     const IS_BONUS_NUMBER_MATCHED = true;
-    const lottoResult = new LottoResult(SAME_NUMBER_COUNT, IS_BONUS_NUMBER_MATCHED);
+    const lottoResult = new LottoResult(
+      SAME_NUMBER_COUNT,
+      IS_BONUS_NUMBER_MATCHED
+    );
 
-    expect(lottoResult.getSameNumberCount()).toEqual(SAME_NUMBER_COUNT);
+    expect(lottoResult.getMatchedNumberCount()).toEqual(SAME_NUMBER_COUNT);
   });
 
-  test('로또 번호와 보너스 번호 일치 여부를 관리한다.', () => {
+  test("로또 번호와 보너스 번호 일치 여부를 관리한다.", () => {
     const SAME_NUMBER_COUNT = 7;
     const IS_BONUS_NUMBER_MATCHED = true;
-    const lottoResult = new LottoResult(SAME_NUMBER_COUNT, IS_BONUS_NUMBER_MATCHED);
+    const lottoResult = new LottoResult(
+      SAME_NUMBER_COUNT,
+      IS_BONUS_NUMBER_MATCHED
+    );
 
     expect(lottoResult.isBonusNumberMatched()).toEqual(IS_BONUS_NUMBER_MATCHED);
+  });
+
+  test("당첨 번호의 개수가 같은지 비교한다.", () => {
+    const SAME_NUMBER_COUNT = 7;
+    const lottoResult = new LottoResult(SAME_NUMBER_COUNT, true);
+    const winningCondition = new LottoResult(SAME_NUMBER_COUNT, false);
+
+    expect(winningCondition.hasEqualNumberCount(lottoResult)).toBe(true);
+  });
+
+  test("보너스 번호 일치 여부를 비교한다.", () => {
+    const IS_BONUS_NUMBER_MATCHED = true;
+    const lottoResult = new LottoResult(1, IS_BONUS_NUMBER_MATCHED);
+    const winningCondition = new LottoResult(2, IS_BONUS_NUMBER_MATCHED);
+
+    expect(winningCondition.hasBonusNumber(lottoResult)).toBe(true);
   });
 });
