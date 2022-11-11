f7419c04b94ba9198102fc87972ff6f862a539b1
HWI <dlwognl99@naver.com>
2022-11-12 01:40:00 +0900

    feat: 상수 파일 추가

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const PURCHASEAMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+const ERROR_MESSAGE_UNDIVIDED =
+  "[ERROR] 구입 금액이 로또 가격으로 나누어 떨어지지 않습니다.";
+
+const LOTTO_PRICE = 1000;
+module.exports = {
+  PURCHASEAMOUNT_INPUT_MESSAGE,
+  ERROR_MESSAGE_UNDIVIDED,
+  LOTTO_PRICE,
+};
