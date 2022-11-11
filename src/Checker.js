53671f851da716406893d0d8c3fc784ef08724af
sunub <bsc5672@gmail.com>
2022-11-12 03:22:09 +0900

    feat : 도메인2의 입력 값이 적절한지 판단하는 기능 추가
    도메인 2에서 입력하게 되는 복권 당첨 번호 값들이 중복되지 않는 지, 1 ~
    45 사이의 숫자를 입력하였는지, 총 6자리의 숫자를 입력하였는지를 파악하여
       옳지 않은 경우 예외를 발생 시키는 기능을 추가 하였다.

--- src/Checker.js
+++ src/Checker.js
@@ -9,6 +9,25 @@ class Checker {
       throw "[ERROR] 입력값이 숫자가 아닙니다.";
     }
   }
+
+  isOverlapping(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+      const copy = numbers.concat();
+      const indicator = copy.splice(i, 1);
+
+      if (copy.includes(indicator[0])) {
+        throw "[ERROR] 중복되는 숫자를 입력 하셨습니다.";
+      }
+    }
+  }
+
+  isRightRangeNumber(numbers) {
+    numbers.forEach((eachNumber) => {
+      if (eachNumber < 1 || eachNumber > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    });
+  }
 }
 
 module.exports = Checker;
