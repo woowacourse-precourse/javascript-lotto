ee11733b484e6acd78f2172e845243333ba037bd
leeuihyun <meta212121@naver.com>
2022-11-11 17:47:05 +0900

    feat : 로또 당첨 번호 예외처리 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,8 +8,18 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다.');
+    }
+
+    numbers.forEach((item) => {
+      if (!/^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$/.test(item)) {
+        throw new Error('[ERROR] 1-45 범위의 숫자를 입력해주세요.');
+      }
+    });
   }
 
   // TODO: 추가 기능 구현
