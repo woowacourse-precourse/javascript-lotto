ebffd8611a6a6a453124ce4e56b868db84e1d62f
wang-yurin <king_1994@naver.com>
2022-11-11 19:51:42 +0900

    refactor: inputMoney, thousandValidate 함수 class App 필드로 이동

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
-    inputMoney();
+    this.inputMoney();
   }
-}
 
-function inputMoney() {
-  MissionUtils.Console.readLine(`구입금액을 입력해 주세요.\n`, (money) => {
-    thousandValidate(money);
-    MissionUtils.Console.print(money);
-  });
-}
+  inputMoney() {
+    MissionUtils.Console.readLine(`구입금액을 입력해 주세요.\n`, (money) => {
+      this.thousandValidate(money);
+      MissionUtils.Console.print(money);
+    });
+  }
 
-function thousandValidate(money) {
-  if (money % 1000 !== 0) {
-    throw new Error("[ERROR] 1000원 단위의 금액을 입력해주세요.");
+  thousandValidate(money) {
+    if (money % 1000 !== 0) {
+      throw new Error("[ERROR] 1000원 단위의 금액을 입력해주세요.");
+    }
   }
 }
 
