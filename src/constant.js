9363cb7282b1d340d2e08ea3e2070120fbaae545
vinivin153 <vinivin153@naver.com>
2022-11-11 16:23:43 +0900

    feat: 에러 메시지 상수 추가

---
+++ src/constant.js
@@ -0,0 +1,7 @@
+const ERROR = Object.freeze({
+  LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
+  RANGE: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+  DUPLICATE: '[ERROR] 로또 번호는 중복되지 않아야 합니다.',
+});
+
+module.exports = ERROR;
