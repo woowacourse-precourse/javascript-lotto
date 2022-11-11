3f727df440923f3067bf9c45ec3f0b52728802e3
unknown <chn0202@gmail.com>
2022-11-11 18:45:19 +0900

    feat(App): 구입 성공 메세지 출력

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App {
     this.inputOutputAmount();
     this.buyLotto();
   }
+
   inputOutputAmount() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (amount) => {
       MissionUtils.Console.print("구입금액을 입력해 주세요.");
@@ -19,7 +20,17 @@ class App {
     buyLottery.checkAmout(this.amount);
     const quantity = buyLottery.numberOfpurchases(this.amount);
     const lottos = buyLottery.createRendomLotto(quantity);
+    this.quantity = quantity;
     this.lottos = lottos;
+    this.outPurchaseSuccess();
+  }
+  outPurchaseSuccess() {
+    MissionUtils.Console.print(`${this.quantity}개를 구매했습니다.`);
+    let idx = 0;
+    while (idx !== this.quantity) {
+      MissionUtils.Console.print(`${this.lottos[idx]}`);
+      idx++;
+    }
   }
 }
 
