b880626706f50d42947709b1bee2245095f9aca9
sohyun <park.sohyun0430@gmail.com>
2022-11-11 23:03:50 +0900

    feat: isValidMoney 함수 테스트 코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,42 @@
-const Lotto = require("../src/Lotto");
+/* eslint-disable no-new */
+const Lotto = require('../src/Lotto');
+const App = require('../src/App');
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
+      // eslint-disable-next-line no-new
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // 아래에 추가 테스트 작성 가능
+});
+
+describe('로또 구입 금액 유효성 검사', () => {
+  test('로또 구입 금액은 숫자여야 하고, 1000원 미만일 수 없다.', () => {
+    const app = new App();
+    const testInput = ['0', '-1000', '990', 'abc', 'a98bc'];
+    testInput.forEach((testElement) => {
+      const result = app.isValidMoney(testElement);
+      expect(result).toEqual(false);
+    });
+  });
+
+  test('로또 구입 금액은 1000원 단위여야 한다.', () => {
+    const app = new App();
+    const testInput = ['9900', '9000', '9856', '14000', '1450001', '185000'];
+    const testOutput = [false, true, false, true, false, true];
+    testInput.forEach((testElement, idx) => {
+      const result = app.isValidMoney(testElement);
+      expect(result).toEqual(testOutput[idx]);
+    });
+  });
 });
