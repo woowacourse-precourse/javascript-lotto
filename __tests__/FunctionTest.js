22538b51ac78b67f59ac3f1c0befd1116b041166
yeonsuBeak <soothbugger@gmail.com>
2022-11-11 18:28:21 +0900

    test(lotto): 발행한 로또 번호 출력 테스트 완료

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -34,4 +34,34 @@ describe("기능 테스트", () => {
 
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
   });
+
+  test("발행 로또 번호 출력", () => {
+    const logSpy = getLogSpy();
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+    ]);
+    const logs = [
+      "[8, 21, 23, 41, 42, 43]",
+      "[3, 5, 11, 16, 32, 38]",
+      "[7, 11, 16, 35, 36, 44]",
+      "[1, 8, 11, 31, 41, 42]",
+      "[13, 14, 16, 38, 42, 45]",
+      "[7, 11, 30, 40, 42, 43]",
+      "[2, 13, 22, 32, 38, 45]",
+    ];
+    const quantity = 7;
+
+    const app = new App();
+    app.publishLotto(quantity);
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
 });
