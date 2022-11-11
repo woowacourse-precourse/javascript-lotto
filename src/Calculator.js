72cb333f074fd8d3ea1accee485fb6e4abe77b86
MAGHC <windowsapple95@gmail.com>
2022-11-11 20:27:58 +0900

    feat: 총당첨금액 계산기능 구현

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,18 @@
+const { WINNING_AMOUNT_UNITS } = require('./Constant');
+
 class Calculator {
+  static totalWinningAmount(winningArray) {
+    let totalAmount = 0;
+    winningArray.forEach((winning) => {
+      if (winning === 1) totalAmount += WINNING_AMOUNT_UNITS.FIFTH_PLACE;
+      if (winning === 2) totalAmount += WINNING_AMOUNT_UNITS.SECOND_PLACEs;
+      if (winning === 3) totalAmount += WINNING_AMOUNT_UNITS.THIRD_PLACE;
+      if (winning === 4) totalAmount += WINNING_AMOUNT_UNITS.FOURTH_PLACE;
+      if (winning === 5) totalAmount += WINNING_AMOUNT_UNITS.FIFTH_PLACE;
+    });
+    return totalAmount;
+  }
+
   static statistics(totalWinning, purchageAmount) {
     return (totalWinning / purchageAmount) * 100;
   }
@@ -10,4 +24,5 @@ class Calculator {
 
 module.exports = Calculator;
 
-console.log(Calculator.statistics(5000, 8000));
+console.log(Calculator.totalWinningAmoun([5, 5, 5, 5, 5]));
+t;
