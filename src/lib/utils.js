dfd09fcf7fa28fbbb48cdc52f1d16092613efd5d
leeuihyun <meta212121@naver.com>
2022-11-11 23:39:03 +0900

    feat : 상수화 시킨 결과메시지를 출력하는 기능

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,20 @@
+const { MESSAGE } = require('./constants');
+const { Console } = require('@woowacourse/mission-utils');
+
 const convertAnswerIntoArray = (answer) => {
   return answer.split(',').map((item) => Number(item));
 };
 
-module.exports = { convertAnswerIntoArray };
+const printWinningResult = (winningArray) => {
+  Console.print(
+    MESSAGE.RESULT(
+      winningArray[0],
+      winningArray[1],
+      winningArray[2],
+      winningArray[3],
+      winningArray[4]
+    )
+  );
+};
+
+module.exports = { convertAnswerIntoArray, printWinningResult };
