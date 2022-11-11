15064eb84df7ac0e7d754a7e93579c7be7a78542
Jinu <jinu200131@gmail.com>
2022-11-11 17:44:06 +0900

    feat (lotto) : Lotto 번호 검사 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,8 +8,21 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error("[ERROR] 로또 번호는 6개 입니다.");
     }
+
+    for(let elem of numbers) {
+      if(isNaN(elem) && !Number.isInteger(elem)) {
+        throw new Error("[ERROR] 로또 번호는 정수형 입니다.");
+      }
+    }
+    
+    for(let elem of numbers) {
+      if(1 >= elem || elem >= 45) {
+        throw new Error("[ERROR] 로또 번호의 범위는 1~45 입니다.");
+      }
+    }
+
   }
 
   // TODO: 추가 기능 구현
