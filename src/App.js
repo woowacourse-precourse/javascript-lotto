655c3fcaa9e51e3da3a90016f668be7ae3566603
oio337a <oio337a@gmail.com>
2022-11-11 20:11:55 +0900

    feat : get purchase amount input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.purchaseAmount();
+  }
+
+  purchaseAmount() {
+    Console.readLine("구매금액을 입력해 주세요.\n", (amount) => {
+      Console.close();
+      Console.print(amount);
+    });
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
