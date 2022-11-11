12b620bc9ceaf49ec1827f6d5eacaf829fa0dd79
hyunjin <leehj0110@kakao.com>
2022-11-11 12:42:26 +0900

    feat: 당첨 내역 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -23,16 +23,54 @@ class App {
           Console.print(lotto);
         });
 
-      // Console.print("\n" + Console.ASK_WINNING_NUMBER);
-
       Console.askAndGetUserInput("\n" + Console.ASK_WINNING_NUMBER, (winningNumbers) => {
-        this.winningNumbers = winningNumbers.split(",").map(Number);
+        this.winningNumbers = winningNumbers
+          .split(",")
+          .map(Number)
+          .sort((a, b) => (a > b ? 1 : -1));
 
         Console.askAndGetUserInput("\n" + Console.ASK_BONUS_NUMBER, (bonusNumber) => {
           this.bonusNumber = Number(bonusNumber);
+
+          Console.print(Console.STATISTICS);
+
+          Object.entries(
+            this.lottos.reduce(
+              (acc, lotto) => {
+                const sameNumberCount = this.countSameNumberWithWinningNumber(lotto);
+
+                switch (sameNumberCount) {
+                  case 3:
+                    acc["3개 일치"] += 1;
+                    break;
+                  case 4:
+                    acc["4개 일치"] += 1;
+                    break;
+                  case 5:
+                    acc["5개 일치"] += 1;
+                    break;
+                  case 6:
+                    acc["6개 일치"] += 1;
+                    break;
+                }
+
+                if (this.bonusNumber === lotto.includes(this.bonusNumber) && sameNumberCount === 5) {
+                  acc["5개 일치"] -= 1;
+                  acc["5개 일치, 보너스 볼 일치"] += 1;
+                }
+
+                return acc;
+              },
+              { "3개 일치": 0, "4개 일치": 0, "5개 일치": 0, "5개 일치, 보너스 볼 일치": 0, "6개 일치": 0 }
+            )
+          ).forEach(([a, b]) => console.log(a, b));
         });
       });
     });
+  }
+
+  countSameNumberWithWinningNumber(lotto) {
+    return lotto.filter((number) => this.winningNumbers.includes(number)).length;
   }
 }
 
