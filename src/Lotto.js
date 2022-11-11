cce39d39afc9248650af1f9c49e45d1ad52dcc80
yeongi <cbg5255@gmail.com>
2022-11-11 11:43:46 +0900

    feature : Lotto 로또 출력 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
 
@@ -10,9 +12,13 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    if (new Set(...numbers).size !== 6) {
+    if (new Set(numbers).size !== 6) {
       throw new Error("[ERROR] 로또 번호는 중복이 없어야 합니다.");
     }
+  }
+
+  printMyNum() {
+    Console.print(this.#numbers);
   }
 }
 
