c55a86ba16fe657f4b62e393353a4c54ca52e87e
jjo-mi <whdal2636@naver.com>
2022-11-11 22:34:48 +0900

    faet: 2. 구입금액 입력 및 금액대비 생성할 로또 수 계산

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ const { Console, Random } = require("@woowacourse/mission-utils");
 class App {
   play() {
     this.printStart();
+    this.orderAmount();
   }
 
-  
+  /** 1. 게임시작 안내 문구 출력*/
   printStart() {
     Console.print('구입금액을 입력해 주세요.');
+  }
+
+  /**2. 구입금액 입력 및 금액대비 생성할 로또 수 계산 */
+  orderAmount() {
+    Console.readLine('', (amount) => {
+      if(isNaN(amount) || amount % 1000 !== 0) {
+        throw new Error('입력값이 잘못되어, 게임을 종료합니다.');
+      } 
+      var lottoCount = amount / 1000
+    });
   }
 }
 
