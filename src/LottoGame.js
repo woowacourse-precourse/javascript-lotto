44cc5b0ea749f37608df93e53f0322e93c01acfa
2wndrhs <2wndrhs@gmail.com>
2022-11-11 07:13:50 -0800

    feat(lotto): 로또 구입 금액에 해당하는 만큼 로또를 발행하는 기능 구현

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,16 +1,28 @@
-const { Console } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 const { PURCHASE } = require('./constants');
 const { validate, isPurchaseInput } = require('./Validator');
+const Lotto = require('./Lotto');
 
 class LottoGame {
   #purchaseAmout = 0;
+
+  #lotteries = [];
 
   start() {
     Console.readLine(PURCHASE.INPUT, answer => {
       validate(answer, isPurchaseInput);
 
       this.#purchaseAmout = answer;
+      this.publishLotteries();
     });
+  }
+
+  publishLotteries() {
+    const count = this.#purchaseAmout / PURCHASE.UNIT;
+    const lotteries = new Array(count).fill(
+      new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6)),
+    );
+    this.#lotteries = lotteries;
   }
 }
 
