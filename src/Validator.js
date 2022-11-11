b04ee2780ef15f94ad7e52f67a0217360dcece03
BB-choi <choibobae0809@gmail.com>
2022-11-11 16:47:40 +0900

    feat(Validator): 빈 입력 및 공백 입력 검증

--- src/Validator.js
+++ src/Validator.js
@@ -1,5 +1,7 @@
 const LOTTO_NUMBERS_COUNT = 6;
 
+// TODO: 6, 1, 45 같은 상수 어떻게 처리할것인지?
+// TODO: [ERROR] 부분도 상수 처리
 class Validator {
   constructor(lottoNumbersCount = LOTTO_NUMBERS_COUNT) {
     this.lottoNumbersCount = lottoNumbersCount;
@@ -9,6 +11,19 @@ class Validator {
     if (lottoNumbers.length !== this.lottoNumbersCount) {
       throw new Error(`[ERROR] 로또 번호는 ${this.lottoNumbersCount}개여야 합니다.`);
     }
+  }
+
+  static isValidInput(input) {
+    // 빈입력인지
+    if (!input.length) {
+      throw new Error('[ERROR] 아무것도 입력하지 않았습니다.');
+    }
+
+    if (input !== input.trim()) {
+      throw new Error('[ERROR] 입력에 공백이 포함되어 있습니다.');
+    }
+
+    return true;
   }
 }
 
