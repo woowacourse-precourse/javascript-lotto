82bf32fdc437ff417b9a49b794015ea90588698c
woo-jk <201702042@o.cnu.ac.kr>
2022-11-11 18:53:36 +0900

    feat: WinningLotto 클래스 생성

---
+++ src/WinningLotto.js
@@ -0,0 +1,16 @@
+const Lotto = require("./Lotto");
+
+class WinningLotto extends Lotto {
+  #bonusNumber;
+
+  constructor(numbers, bonusNumber) {
+    super(numbers);
+    this.validateBonus(bonusNumber);
+    this.#bonusNumber = bonusNumber;
+  }
+  validateBonus(bonusNumber) {
+    if (bonusNumber < 1 || bonusNumber > 45) {
+      throw new Error("[ERROR] 보너스 번호의 범위는 1~45 사이여야 합니다.");
+    }
+  }
+}
