5b64382c37dcc2442d8aa05ab5fab38eae77939e
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-11-11 17:40:27 +0900

    Feat: 1000원 이하일때 예외처리 추가

--- src/utils/errorHandling.js
+++ src/utils/errorHandling.js
@@ -1,15 +1,18 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { TYPE, MESSAGE } = require("./constant/errorMessage");
+const { TYPE, MESSAGE } = require("../constant/errorMessage");
 
 class UserInputValidationError {
   static showErrorMessage(type) {
     switch (type) {
       case TYPE.NOT_NUMBER:
         Console.print(MESSAGE.NOT_INPUT_NUMBER);
-        throw Error(TYPE.NOT_NUMBER);
+        throw new Error(TYPE.NOT_NUMBER);
       case TYPE.NOT_THOUSAND:
         Console.print(MESSAGE.ONLY_INPUT_THOUSAND);
-        throw Error(TYPE.NOT_THOUSAND);
+        throw new Error(TYPE.NOT_THOUSAND);
+      case TYPE.NOT_BUY_LOTTO:
+        Console.print(MESSAGE.BELOW_THOUSAND);
+        throw new Error(TYPE.NOT_BUY_LOTTO);
     }
   }
 }
--- src/validateNumber.js
+++ src/validateNumber.js
@@ -1,10 +1,13 @@
 const { TYPE } = require("./constant/errorMessage");
 const { THOUSAND } = require("./constant/inputMessage");
-const { UserInputValidationError } = require("./errorHandling");
+const { UserInputValidationError } = require("./utils/errorHandling");
 
 const insideNotNumber = (userInput) =>
   [...userInput].every((v) => parseInt(v) || v === "0");
+
 const onlyInputInThousand = (userInput) => +userInput % THOUSAND !== 0;
+
+const checkBelowThousand = (userInput) => +userInput <= 900;
 
 function validateInputNumber(userInput) {
   if (!insideNotNumber(userInput)) {
@@ -14,6 +17,12 @@ function validateInputNumber(userInput) {
   if (onlyInputInThousand(userInput)) {
     UserInputValidationError.showErrorMessage(TYPE.NOT_THOUSAND);
   }
+
+  if (checkBelowThousand(userInput)) {
+    UserInputValidationError.showErrorMessage(TYPE.NOT_BUY_LOTTO);
+  }
+
+  return true;
 }
 
 module.exports = {
