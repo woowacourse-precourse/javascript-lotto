953d29999f4aa0a376aab1ddc0b9915c3b438511
YeonsuBaek <soothbugger@gmail.com>
2022-11-11 21:57:06 +0900

    feat(lotto): 보너스 번호가 범위를 벗어난 경우 예외 처리 기능 구현

--- src/Bonus.js
+++ src/Bonus.js
@@ -8,11 +8,21 @@ class Bonus {
 
   validate(number) {
     this.checkNumber(number);
+    this.checkRange(number);
   }
 
   checkNumber(number) {
     if (isNaN(number)) {
       throw new Error("[ERROR] 보너스 번호는 숫자여야 합니다.");
+    }
+  }
+
+  checkRange(number) {
+    const MINIMUN_NUMBER = 1;
+    const MAXIMUN_NUMBER = 45;
+
+    if (number < MINIMUN_NUMBER || number > MAXIMUN_NUMBER) {
+      throw new Error("[ERROR] 보너스 번호는 1과 45 사이의 숫자여야 합니다");
     }
   }
 }
