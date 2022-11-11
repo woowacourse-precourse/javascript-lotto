c9967157ce2443dc37073fbece85cf790582ba59
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-11 20:54:24 +0900

    feat: 결과 출력시 공백라인 만들어주는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
 const UserBudget = require('./UserBudget');
-const INPUT_BUDGET = '구입금액을 입력해 주세요.';
+const INPUT_BUDGET = '구입금액을 입력해 주세요.\n';
 
 class App {
   budget;
@@ -12,11 +12,16 @@ class App {
   getUserBudget(INPUT_BUDGET) {
     Console.readLine(INPUT_BUDGET, (input) => {
       this.validateUserBudget(input);
+      this.printSpaceLine();
     });
   }
 
   validateUserBudget(budget) {
     this.budget = new UserBudget(Number(budget));
+  }
+
+  printSpaceLine() {
+    Console.print('');
   }
 }
 
