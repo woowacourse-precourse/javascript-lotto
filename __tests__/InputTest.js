2154b63789949a320f5d94d16ffb20566bb69e7b
yuu <22yuu@naver.com>
2022-11-11 17:39:30 +0900

    test: 당첨번호 입력에 대한 예외처리

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,18 +1,35 @@
 const App = require("../src/App");
 
 
-describe("사용자 입력 테스트", () => {
+describe('사용자 입력 예외 테스트', () => {
 
     let app;
     beforeEach(() => {
         app = new App();
     });
 
-    test("금액이 1000원 단위가 아니면 예외가 발생한다.", () => {
-        expect(app.countLottos("1234")).toThrow("[ERROR]");
-    })
+    test('금액이 1000원 단위가 아니면 예외가 발생한다.', () => {
+        expect(() => {
+            app.countLottos('1234A');
+        }).toThrow();
+    });
 
-    test("금액은 숫자가 아니면 예외가 발생한다.", () => {
-        expect(app.countLottos("14,000")).toThrow("[ERROR]");
-    })
+    test('금액 입력 시 콤마(,)는 제외해야 하므로 포함될 경우 예외가 발생한다.', () => {
+        expect(() =>{
+            app.countLottos('14,000');
+        }).toThrow('[ERROR]');
+    });
+
+    test('당첨 번호 입력 시 콤마(,)를 사용해 구분해야 한다.', () => {
+        expect(() => {
+            app.setTargetNumbers('123456');
+        }).toThrow("[ERROR]");
+    });
+
+    test('당첨 번호 입력 시 1~45 사이의 수를 입력해야한다.', () => {
+        expect(() => {
+            app.setTargetNumbers('0,1,2,3,4,5');
+        }).toThrow();
+    });
+
 })
