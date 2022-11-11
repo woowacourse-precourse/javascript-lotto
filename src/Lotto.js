f1d3ed03f7e37f07567f17e0f237341ebac0c4ae
GunHoi <lgh509@kyonggi.ac.kr>
2022-11-11 17:42:32 +0900

    feat - 4 : 당첨번호 예외처리(번호가 1~45 범위 밖 인  경우)

--- src/Lotto.js
+++ src/Lotto.js
@@ -17,8 +17,15 @@ class Lotto {
   isDuplication(numbers){
     const deDuplicationNumbers = new Set(numbers);
     if(deDuplicationNumbers.size !== 6){
-      throw new Error("[ERROR] 로또 번호는 중복되지 않는 6개의 숫자여야 합니다.")
+      throw new Error("[ERROR] 로또 번호는 중복되지 않는 6개의 숫자여야 합니다.");
     }
+  }
+  isInRange(numbers){
+    numbers.forEach((number)=>{
+      if(number<1 || number>45){
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    })
   }
   // TODO: 추가 기능 구현
 }
