11f52d4e95a4ed5a3b16ab604b35ceec9a72f249
HWI <dlwognl99@naver.com>
2022-11-12 01:54:33 +0900

    feat: 구입 금액 입력 에러 메세지 추가

--- src/constants.js
+++ src/constants.js
@@ -3,9 +3,13 @@ const PURCHASEAMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
 const ERROR_MESSAGE_UNDIVIDED =
   "[ERROR] 구입 금액이 로또 가격으로 나누어 떨어지지 않습니다.";
 
+const ERROR_MESSAGE_NOTONLY_NUMBER =
+  "[ERROR] 구입 금액은 숫자만 입력 가능합니다.";
+
 const LOTTO_PRICE = 1000;
 module.exports = {
   PURCHASEAMOUNT_INPUT_MESSAGE,
   ERROR_MESSAGE_UNDIVIDED,
+  ERROR_MESSAGE_NOTONLY_NUMBER,
   LOTTO_PRICE,
 };
