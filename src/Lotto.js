e3cdcdcfdf6f0d337b507f0b046e1251d2b67da7
js43o <js43o@naver.com>
2022-11-11 20:47:08 +0900

    refactor(Lotto): separate constants & rename checkers

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,11 @@
+const LOTTO_RANGE_BEGIN = 1;
+const LOTTO_RANGE_END = 45;
+
+const ERROR_HEADER = '[ERROR]';
+const ERROR_WRONG_LENGTH = `${ERROR_HEADER} 로또 번호는 6개여야 합니다.`;
+const ERROR_DUPLICATED = `${ERROR_HEADER} 로또 번호는 중복되지 않아야 합니다.`;
+const ERROR_WRONG_RANGE = `${ERROR_HEADER} 로또 번호는 1부터 45까지의 정수여야 합니다.'`;
+
 class Lotto {
   #numbers;
 
@@ -8,28 +16,25 @@ class Lotto {
     }
   }
 
-  checkDistinct(numbers) {
+  isDistinct(numbers) {
     let set = new Set(numbers);
     return numbers.length === set.size;
   }
 
-  checkInRange(number) {
-    const BEGIN = 1;
-    const END = 45;
-
-    return number >= BEGIN && number <= END;
+  isInRange(number) {
+    return number >= LOTTO_RANGE_BEGIN && number <= LOTTO_RANGE_END;
   }
 
   validateNumbers(numbers) {
     if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+      throw new Error(ERROR_WRONG_LENGTH);
     }
-    if (!this.checkDistinct(numbers)) {
-      throw new Error('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
+    if (!this.isDistinct(numbers)) {
+      throw new Error(ERROR_DUPLICATED);
     }
     numbers.forEach((number) => {
-      if (!/^[0-9]{1,2}$/.test(number) || !this.checkInRange(Number(number))) {
-        throw new Error('[ERROR] 로또 번호는 1부터 45까지의 정수여야 합니다.');
+      if (!/^[0-9]{1,2}$/.test(number) || !this.isInRange(Number(number))) {
+        throw new Error(ERROR_WRONG_RANGE);
       }
     });
   }
