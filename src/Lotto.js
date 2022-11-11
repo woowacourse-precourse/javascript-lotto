8425285127e7a03105bcac92909a9c150488f03b
AnJaeSeong <dktmzh6@gmail.com>
2022-11-11 18:19:02 +0900

    fix: Lotto numbers, bonus validate
    당첨번호와 보너스번호 유효성 검사 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,18 +11,30 @@ class Lotto {
     // 로또 번호 유효 검사
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-    if (numbers.some(number => number <= 0 || number > 45)){
-      throw new Error("[ERROR] 로또 번호는 1~45 범위 내에 존재해야 합니다.")
-    }
-    if (numbers.length !== new Set(numbers).size){
-      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.")
+    } else if (numbers.some(number => isNaN(number))) {
+      throw new Error("[ERROR] 로또 번호는 숫자로 입력해야 합니다.");
+    } else if (numbers.some(number => number <= 0 || number > 45)){
+      throw new Error("[ERROR] 로또 번호는 1~45 범위 내에 존재해야 합니다.");
+    } else if (numbers.length !== new Set(numbers).size){
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
     }
   }
 
   setBonusNum(bonus) {
     // 보너스 번호 설정
+    this.bonusValidate(bonus);
     this.#bonus = bonus;
+  }
+
+  bonusValidate(bonus) {
+    // 보너스 번호 유효 검사
+    if (isNaN(bonus)){
+      throw new Error("[ERROR] 보너스 번호는 숫자로 입력해야 합니다.");
+    } else if (bonus <=0 || bonus > 45){
+      throw new Error("[ERROR] 보너스 번호는 1~45 범위 내에 존재해야 합니다.");
+    } else if (this.#numbers.includes(bonus)){
+      throw new Error("[ERROR] 보너스 번호는 당첨번호와 중복되지 않아야 합니다.");
+    }
   }
 
   getNumbers() {
