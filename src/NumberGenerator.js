f6f909fac4b079b3c66e489517cdd1f47f681b58
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-11 15:34:57 +0900

    fix: 불필요한 생성자 및 메서드 제거

--- src/NumberGenerator.js
+++ src/NumberGenerator.js
@@ -1,16 +1,8 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class NumberGenerator {
-  constructor() {
-    this.numbers = this.createRandomSixNumbers();
-  }
-
   createRandomSixNumbers() {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-  }
-
-  getNumbers() {
-    return this.numbers;
   }
 }
 
