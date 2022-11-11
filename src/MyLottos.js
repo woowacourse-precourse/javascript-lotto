526844ee7b7623d7eee86a531f492b227f7ad02d
leeuihyun <meta212121@naver.com>
2022-11-11 16:11:04 +0900

    refactor : Purchase 클래스의 반환 메서드 추가로 인한 생성자 변경

--- src/MyLottos.js
+++ src/MyLottos.js
@@ -1,14 +1,19 @@
-const { Console } = require('@woowacourse/mission-utils');
-const { MESSAGE } = require('./lib/constants');
+const { Console, Random } = require('@woowacourse/mission-utils');
+const { MESSAGE, VALUE } = require('./lib/constants');
 
 class MyLottos {
-  #purchaseAccount;
   #myLottos;
 
-  constructor(purchaseMoney) {
-    this.ConsolepurchaseAccount(purchaseMoney / 1000);
-    this.#purchaseAccount = purchaseMoney / 1000;
-    this.#myLottos = [];
+  constructor(purchaseAccount) {
+    this.validate(purchaseAccount);
+    this.ConsolepurchaseAccount(purchaseAccount);
+    this.makeMyLottos(purchaseAccount);
+  }
+
+  validate(purchaseAccount) {
+    if (typeof purchaseAccount !== 'number') {
+      throw new Error('[ERROR] 로또의 개수가 숫자가 아닙니다.');
+    }
   }
 
   ConsolepurchaseAccount(purchaseAccount) {
