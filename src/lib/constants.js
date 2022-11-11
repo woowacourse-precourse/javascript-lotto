d886756585809dd2e375c9aa0ea1f7d46a2630f6
leeuihyun <meta212121@naver.com>
2022-11-11 23:37:58 +0900

    feat : 결과를 출력 상수와 수익률 출력 상수 생성

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -5,6 +5,9 @@ const MESSAGE = {
   INPUT_WINNING_NUMBER: '\n당첨 번호를 입력해주세요.\n',
   INPUT_BONUS_NUMBER: '\n보너스 번호를 입력해주세요.\n',
   OUTPUT_WINNING_STATISTICS: '당첨 통계\n---',
+  RESULT: (fifthPlace, fourthPlace, thirdPlace, secondPlace, firstPlace) =>
+    `3개 일치 (5,000원) - ${fifthPlace}개\n4개 일치 (50,000원) - ${fourthPlace}개\n5개 일치 (1,500,000원) - ${thirdPlace}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${secondPlace}개\n6개 일치 (2,000,000,000원) - ${firstPlace}개`,
+  YIELD: (percentage) => `\n총 수익률은 ${percentage}%입니다.`,
 };
 
 const VALUE = {
