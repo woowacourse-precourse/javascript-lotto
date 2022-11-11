75df03872578153e098dab730ea43d60524d3e4b
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-11-11 10:42:08 +0900

    fix: 로또 구매 시 구매한 로또 전체를 기록하도록 수정

--- src/BuyLotto.js
+++ src/BuyLotto.js
@@ -2,31 +2,36 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const { LOTTO, MESSAGE, ERROR } = require('./Constants');
 
 class BuyLotto {
+  lottoList = [];
+
   getPurchaseAmount() {
     MissionUtils.Console.readLine(MESSAGE.PURCHASE_AMOUNT, (amount) => {
       const AMOUNT = Number(amount);
       if (AMOUNT % LOTTO.PRICE === 0) {
-        this.printUserLotto(AMOUNT / LOTTO.PRICE);
+        this.setLottoNumber(AMOUNT / LOTTO.PRICE);
       } else {
         throw new Error(ERROR.AMOUNT);
       }
     });
   }
 
-  setLottoNumber() {
-    const LOTTO_NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(
-      LOTTO.NUMBER_START,
-      LOTTO.NUMBER_END,
-      LOTTO.NUMBER_SELECT
-    );
-    LOTTO_NUMBER.sort((x, y) => x - y);
-    return LOTTO_NUMBER;
+  setLottoNumber(amount) {
+    for (let i = 0; i < amount; i++) {
+      const LOTTO_NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(
+        LOTTO.NUMBER_START,
+        LOTTO.NUMBER_END,
+        LOTTO.NUMBER_SELECT
+      );
+      LOTTO_NUMBER.sort((x, y) => x - y);
+      this.lottoList.push(LOTTO_NUMBER);
+    }
+    this.printUserLotto(this.lottoList);
   }
 
-  printUserLotto(amount) {
-    MissionUtils.Console.print(`${amount}개를 구매했습니다.`);
-    for (let print = 0; print < amount; print++) {
-      MissionUtils.Console.print(this.setLottoNumber());
+  printUserLotto(lottoLitst) {
+    MissionUtils.Console.print(`${lottoLitst.length}개를 구매했습니다.`);
+    for (let i = 0; i < lottoLitst.length; i++) {
+      MissionUtils.Console.print(lottoLitst[i]);
     }
   }
 }
