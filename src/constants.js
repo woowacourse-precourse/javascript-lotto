016ce6c14a1fd2d8afa94943078ce0fb3357684e
박정규 <srk99279@naver.com>
2022-11-11 15:05:48 +0900

    feat : 당첨 번호 범위 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -11,9 +11,14 @@ const PAYOUT = [5000, 50000, 1500000, 30000000, 2000000000];
 const MIN_MATCH = 3;
 const MAX_MATCH = 7;
 
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 45;
+
 module.exports = {
   MATCH_MSG,
   PAYOUT,
   MIN_MATCH,
   MAX_MATCH,
+  MIN_NUMBER,
+  MAX_NUMBER,
 };
