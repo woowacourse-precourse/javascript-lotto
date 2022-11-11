d0480ac4a200d61504948bc4e901f24f70bca564
sungjihyun <jhsung23@gmail.com>
2022-11-12 02:32:48 +0900

    feat(Lotto): 당첨 번호가 범위 내에 있는지 검사하는 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const { MIN_NUMBER, MAX_NUMBER, ERROR } = require("../src/utils/constants");
+
 class Lotto {
   #numbers;
 
@@ -10,6 +12,16 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if (this.isOutOfRange(numbers)) {
+      throw new Error(ERROR.OUT_OF_RANGE);
+    }
+  }
+
+  isOutOfRange(numberArray) {
+    return numberArray.some(
+      (number) => number < MIN_NUMBER || number > MAX_NUMBER
+    );
   }
 
   // TODO: 추가 기능 구현
