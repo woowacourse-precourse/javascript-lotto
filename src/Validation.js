dd92b25f255c3a90d422fc17b88545ed502c3164
HWI <dlwognl99@naver.com>
2022-11-12 02:16:49 +0900

    refactor: 예외 문구 호출 메소드 조건문 순서 변경

--- src/Validation.js
+++ src/Validation.js
@@ -6,21 +6,19 @@ const {
 
 class Validation {
   static checkPurchaseAmount(purchaseAmount) {
-    if (!this.isDivided(purchaseAmount)) {
-      return ERROR_MESSAGE_UNDIVIDED;
-    }
     if (this.isOnlyNumber(purchaseAmount)) {
-      return ERROR_MESSAGE_NOTONLY_NUMBER;
+      throw new Error(ERROR_MESSAGE_NOTONLY_NUMBER);
+    }
+    if (!this.isDivided(purchaseAmount)) {
+      throw new Error(ERROR_MESSAGE_UNDIVIDED);
     }
   }
-
+  static isOnlyNumber(purchaseAmount) {
+    return isNaN(purchaseAmount);
+  }
   static isDivided(purchaseAmount) {
     const change = purchaseAmount % LOTTO_PRICE;
     return change === 0;
-  }
-
-  static isOnlyNumber(purchaseAmount) {
-    return isNaN(purchaseAmount);
   }
 }
 
