fd6378a4de5707451efead3426d5fb7f368491b3
Narcoker <jykim9868@gmail.com>
2022-11-11 19:43:26 +0900

    Feat: 구입 금액 입력 요구 문자열, 유효하지 않은 구입금액 에러 문자열 추가

--- src/Constants.js
+++ src/Constants.js
@@ -1,9 +1,9 @@
 const MESSAGE = Object.freeze({
-
+    ENTER_CASH: "구입금액을 입력해 주세요.",
 })
 
 const ERROR = Object.freeze({
-
+    INVAID_CACHE: "[ERROR] 구입 금액은 1000원 단위여야합니다."
 });
 
 module.exports = { MESSAGE, ERROR };
