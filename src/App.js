a04332c65b0fd9d7d93c5d31f64159b0e02e94bc
Hwang Seong Taek <hst007@naver.com>
2022-11-11 23:40:29 +0900

    feat: 사용자에게 로또 구입 금액을 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { LOTTO_USER_INPUT } = require('./constants');
+
 class App {
-  play() {}
+  play() {
+    Console.readLine(LOTTO_USER_INPUT.PURCHASE_LOTTO, (purchaseLotto) => {
+      Console.print(purchaseLotto);
+    });
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
