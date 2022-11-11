4d6e3088401aa5e9424f3215e416b62738762d41
Jiwoo Park <jiwoo9446@naver.com>
2022-11-11 15:32:46 +0900

    feat: 특정 개수만큼 일치하는 로또 개수 출력 메서드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
 
@@ -42,6 +44,7 @@ class Lotto {
   calculate(ticket, lottoList, bonus) {
     const statistic = this.calculateStatistics(lottoList, bonus);
     const rank = this.calculateRank(statistic);
+    this.showMatchResult(rank);
   }
 
   calculateStatistics(lottoList, bonusNumber) {
@@ -67,6 +70,15 @@ class Lotto {
       }
     });
     return Object.values(rank);
+  }
+
+  showMatchResult(result) {
+    Console.print("당첨 통계\n---\n");
+    Console.print(`3개 일치 (5,000원) - ${result[0]}개`);
+    Console.print(`4개 일치 (50,000원) - ${result[1]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${result[2]}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[4]}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${result[3]}개`);
   }
 }
 
