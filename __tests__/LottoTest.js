1fa6ee6b76982a5b90d7389cd2b86f4625c8c576
2yunseong <dbsdltjd3701@naver.com>
2022-11-11 20:50:02 +0900

    test(vaildator): 1부터 45까지 범위 내에 있는 숫자인지 확인하는 기능 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,28 @@
-const Lotto = require("../src/Lotto");
+const Lotto = require('../src/Lotto');
+const Vaildator = require('../src/Vaildator');
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // 아래에 추가 테스트 작성 가능
+});
+
+describe('Validator 클래스 테스트', () => {
+  test('isRangeIn : 범위 내의 숫자인지 테스트한다.', () => {
+    expect(Vaildator.isRangeIn(1, 45, 1)).toEqual(true);
+    expect(Vaildator.isRangeIn(1, 45, 45)).toEqual(true);
+    expect(Vaildator.isRangeIn(1, 45, 46)).toEqual(true);
+    expect(Vaildator.isRangeIn(1, 45, 0)).toEqual(true);
+  });
 });
