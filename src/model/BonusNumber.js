6ea56fb48dbece2977fae3f68416cefd969337b8
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-11 18:55:38 +0900

    refactor(BonusNumber): 예외처리 문구 변경

--- src/model/BonusNumber.js
+++ src/model/BonusNumber.js
@@ -19,13 +19,13 @@ class BonusNumber {
 
   validateIsNumber(value) {
     if (Number.isNaN(value)) {
-      throw new Error('[ERROR] 로또 번호는 숫자만 입력 가능합니다.');
+      throw new Error('[ERROR] 보너스 번호는 숫자만 입력 가능합니다.');
     }
   }
 
   validateNumberRange(number) {
     if (number < NUMBER_RANGE.START || number > NUMBER_RANGE.END) {
-      throw new Error(`[ERROR] 로또 번호는 ${NUMBER_RANGE.START}이상 ${NUMBER_RANGE.END}이하 입니다.`);
+      throw new Error(`[ERROR] 보너스 번호는 ${NUMBER_RANGE.START}이상 ${NUMBER_RANGE.END}이하 입니다.`);
     }
   }
 }
