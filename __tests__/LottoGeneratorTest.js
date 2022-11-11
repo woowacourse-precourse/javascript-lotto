19536044534023ec73ae16a29848402f88dcdfa9
oseunghyun <ohsh1477@gmail.com>
2022-11-11 17:26:54 +0900

    test(LottoGeneratorTest): implement error exception test

---
+++ __tests__/LottoGeneratorTest.js
@@ -0,0 +1,27 @@
+const LottoGenerator = require('../src/domain/LottoGenerator');
+
+describe('로또 생성기 클래스 - 금액 입력 테스트', () => {
+  test('로또 구매 금액을 입력하지 않으면 예외가 발생한다.', () => {
+    expect(() => {
+      new LottoGenerator();
+    }).toThrow('[ERROR] 구입 금액을 입력해야 합니다.');
+  });
+
+  test('로또 구매 금액에 숫자가 아닌 값을 입력하면 예외가 발생한다.', () => {
+    expect(() => {
+      new LottoGenerator('외상 되나요?');
+    }).toThrow('[ERROR] 숫자만 입력할 수 있습니다.');
+  });
+
+  test('로또 구매 금액에 0 이하의 값을 입력하면 예외가 발생한다.', () => {
+    expect(() => {
+      new LottoGenerator(-1000);
+    }).toThrow('[ERROR] 금액은 0보다 커야 합니다.');
+  });
+
+  test('로또 구매 금액을 1000원 단위로 입력하지 않으면 예외가 발생한다.', () => {
+    expect(() => {
+      new LottoGenerator(34280);
+    }).toThrow('[ERROR] 천 원 단위로 입력해야 합니다.');
+  });
+});
