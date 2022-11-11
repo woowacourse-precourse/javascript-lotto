876341065838b99d20ce8f2004451d7bcf8463f7
Ryong-E <think8867@gmail.com>
2022-11-11 19:59:18 +0900

    feat(view): 총 당첨 로또 갯수와 수익률 출력 기능

--- src/view.js
+++ src/view.js
@@ -6,7 +6,7 @@ class View {
   }
 
   getAmountInput() {
-    Console.readLine('구입금액을 입력해 주세요', (answer) => {
+    Console.readLine('구입금액을 입력해 주세요.\n', (answer) => {
       Validation.amountInputValidate(answer);
       this.controller.generateLotto(answer);
     });
@@ -18,17 +18,33 @@ class View {
   }
 
   winningNumber() {
-    Console.readLine('당첨 번호를 입력해 주세요.', (answer) => {
+    Console.readLine('당첨 번호를 입력해 주세요.\n', (answer) => {
       Validation.winningNumberValidate(answer);
       this.controller.enterWinningNumber(answer.split(','));
     });
   }
 
   bonusNumberInput() {
-    Console.readLine('보너스 번호를 입력해 주세요.', (answer) => {
+    Console.readLine('보너스 번호를 입력해 주세요.\n', (answer) => {
       Validation.bonusNumberValidate(answer);
       this.controller.enterBonusNumber(answer);
     });
+  }
+
+  printWinningResult(result) {
+    const [fifthRanking, forthRanking, thirdRanking, secondRanking, firstRanking] = result;
+    let grossEarnings = 0;
+    const prizeMoney = [5000, 50000, 1500000, 30000000, 2000000000];
+    Console.print('당첨 통계\n---');
+    Console.print(
+      `3개 일치 (5000원) - ${fifthRanking}개\n4개 일치 (50,000원) - ${forthRanking}개\n5개 일치 (1,500,000원) - ${thirdRanking}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${secondRanking}개\n6개 일치 (2,000,000,000원) - ${firstRanking}개`,
+    );
+    result.forEach((result, index) => {
+      grossEarnings += result * prizeMoney[index];
+    });
+    Console.print(
+      `총 수익률은 ${((grossEarnings / this.controller.totalAmount) * 100).toFixed(1)}%입니다.`,
+    );
   }
 }
 
