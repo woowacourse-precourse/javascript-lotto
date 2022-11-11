3c94eb8e784e9c685e9105d9592b760c7d9e74f5
nsyeon <lovepny@gmail.com>
2022-11-11 23:04:53 +0900

    feat(App): 함수 등록

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 const USER = require("../src/User");
+const LOTTOWINNINGNUMBER = require("../src/LottoWinningNumber");
 
 class App {
   play() {
     const User = new USER();
+    const LottoWinningNumber = new LOTTOWINNINGNUMBER();
     
     let price = 0;
     let number = 0;
     let lottoNumber = [];
+    let winningNumber = [];
 
     price = User.enterLottoBuyPrice();
     number = User.lottoNumberOfPapers(price);
     lottoNumber = User.createLottoNumber(number);
+    winningNumber = LottoWinningNumber.lottoWinningNumber();
+    winningNumber = LottoWinningNumber.lottoWinningNumberSort(winningNumber);
   }
 }
 
