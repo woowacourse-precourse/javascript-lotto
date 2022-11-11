f5b43a237170b8bb1a694879930c257ddb84c813
sungjihyun <jhsung23@gmail.com>
2022-11-12 01:19:55 +0900

    feat(App): 입력받은 당첨 번호의 유효성 검사 후 숫자 배열로 변경하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -25,11 +25,13 @@ class App {
     const trimmedInput = input.trim();
 
     //문자가 섞여있지 않은지 검사
+    //TODO match -> test 변경
     if (trimmedInput.match(RegExp(/([^0-9])/g)) !== null) {
       throw new Error(ERROR.ONLY_NUMBER);
     }
 
     //금액이 1000으로 나눠떨어지는지 검사
+    //TODO user로 넘겨도 될듯?
     if (Number(trimmedInput) % AMOUNT_UNIT !== 0) {
       throw new Error(ERROR.INDIVISIBLE);
     }
@@ -61,6 +63,47 @@ class App {
     Console.readLine(MESSAGE.ENTER_WINNING_NUMBERS, (numbers) => {
       console.log(numbers);
     });
+  }
+
+  //입력 받은 당첨 번호의 유효성 검사 후 유효하다면 숫자 배열로 변경하기
+  parseInput(input) {
+    const trimmedInput = input.trim();
+
+    //문자가 없는지 검사
+    if (hasChar(string)) {
+      throw new Error(ERROR.ONLY_NUMBER_AND_COMMA);
+    }
+
+    //문자열을 쉼표로 구분해 배열로 만들기
+    const array = makeSplit(trimmedInput);
+
+    //문자 요소를 숫자로 변환하기
+    const numberArray = makeNumberArray(array);
+
+    return numberArray;
+  }
+
+  static hasChar(string) {
+    if (RegExp(/([^?!,0-9 ])/g).test(string)) {
+      return true;
+    }
+    return false;
+  }
+
+  static makeSplit(string) {
+    const array = string.split(",");
+    if (array.includes("") || array.includes(" ")) {
+      throw new Error(ERROR.MISUSE_COMMA);
+    }
+    return array;
+  }
+
+  static makeNumberArray(array) {
+    const numberArray = array.map(Number);
+    if (numberArray.includes(NaN)) {
+      throw new Error(ERROR.IS_NAN);
+    }
+    return numberArray;
   }
 }
 
