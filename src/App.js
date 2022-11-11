0da756f33c9339cf7409e085237df6ccfa4d544d
hailey-hy <lillyine.hy@gmail.com>
2022-11-11 18:45:11 +0900

    fix(App, Constants): 로또 구매 금액 예외 처리 오류 수정

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const INPUT_ERROR = require('./Constants');
 
 class App {
-  play() {}
+  play() {
+    this.purchaseAmount();
+  }
 
   purchaseAmount() {
-    MissionUtils.Console.readLine((answer) => {
-      return answer
-    })
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (answer) => {
+      this.isPurchaseAmountValid(answer);
+    });
   }
 
   isPurchaseAmountValid(money) {
     if(Number(money) % 1000 != 0){
       throw INPUT_ERROR.NOT_DIVIDED;
     }
-    return true;
+    return money;
   }
 }
 
--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,5 @@
 const INPUT_ERROR = {
-    NOT_DIVIDED: '[ERROR] 로또를 살 수 없는 금액입니다.'
-}
+    NOT_DIVIDED: "[ERROR] 로또를 살 수 없는 금액입니다."
+};
+
+module.exports = INPUT_ERROR;
