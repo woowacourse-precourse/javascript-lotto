0c245ca265c0b23d6ea9483dfc6f08e2ab649eb2
yeonsuBeak <soothbugger@gmail.com>
2022-11-11 18:25:45 +0900

    feat(lotto): 발행한 로또 번호 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 const Money = require("./Money");
 
 class App {
   constructor() {
     this.money = 0;
     this.quantity = 0;
+    this.publishList = [];
   }
 
   play() {
@@ -19,6 +20,22 @@ class App {
     const UNIT = 1000;
     this.quantity = parseInt(money, 10) / UNIT;
     Console.print(`\n${this.quantity}개를 구매했습니다.`);
+    this.publishLotto(this.quantity);
+  }
+
+  publishLotto(quantity) {
+    for (let turn = 0; turn < quantity; turn++) {
+      const MINIMUN_NUMBER = 1;
+      const MAXIMUN_NUMBER = 45;
+      const NUMBER_LENGTH = 6;
+      const publishPiece = Random.pickUniqueNumbersInRange(
+        MINIMUN_NUMBER,
+        MAXIMUN_NUMBER,
+        NUMBER_LENGTH
+      );
+      this.publishList.push(publishPiece);
+      Console.print(JSON.stringify(publishPiece).replace(/,/g, ", "));
+    }
   }
 }
 
