6bc9d42f8a3732990e8b5fc9bc4a3215ed1ae742
myung-j-p <jmpack97@gmail.com>
2022-11-12 02:10:41 +0900

    Test: Lotto 테스트 케이스 추가
    - 로또 번호의 범위가 1~45 넘어가면 예외 발생

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -5,24 +5,22 @@ describe("로또 클래스 테스트", () => {
 
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+      lotto.validate([1, 2, 3, 4, 5, 6, 7],6);
     }).toThrow("[ERROR]");
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test("로또 번호의 범위가 1~45를 넘어가면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
+      lotto.validate([1, 2, 3, 4, 5, 90]);
     }).toThrow("[ERROR]");
   });
 
-  test('지불한 액수를 1000으로 나눠서 출력한다.', () => {
-    expect(lotto.checkHowManyLottos(10000)).toEqual(10);
-  })
-
-  test('구매 개수만큼 로또번호가 생성된다.', () => {
-    expect(lotto.makePaidLottoNumbers(8).length).toEqual(8);
-  })
+  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      lotto.validate([1, 2, 3, 4, 5, 5]);
+    }).toThrow("[ERROR]");
+  });
 
 
 });
