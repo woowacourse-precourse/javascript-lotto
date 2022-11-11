42b3daef2aa011a9eb0939705368fb5901c88895
Cobocho <rlaalsrb1111@naver.com>
2022-11-11 19:55:19 +0900

    feat: 게임 시작 문구 출력 구현 및 상수화 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const MESSAGES = require('./constants');
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.readLine(MESSAGES.GAME.requirePurchaseAmount, (amount) => {
+      console.log(amount);
+      MissionUtils.Console.close();
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const MESSAGES = {
+  GAME: {
+    requirePurchaseAmount: '구입금액을 입력해 주세요.',
+  }
+}
+
+module.exports = MESSAGES;
