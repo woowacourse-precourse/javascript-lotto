072bb60d0eda0ae7e181bcf878cf90c7ec5bd0b2
bassyu <yrkdgus@gmail.com>
2022-11-11 17:13:33 +0900

    feat(Calculator): 수익 계산 기능 추가
    - #prizeMap으로 순위에 해당하는 수익 정의
    - #totalPrize에 누적하여 저장

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,16 +1,29 @@
 class Calculator {
   #pay;
-  #income;
+  #prizeMap;
+  #totalPrize;
 
   constructor(pay) {
     this.validate(pay);
     this.#pay = pay;
+    this.#prizeMap = {
+      1: 2000000000,
+      2: 30000000,
+      3: 1500000,
+      4: 50000,
+      5: 5000,
+    };
+    this.#totalPrize = 0;
   }
 
   validate(pay) {}
 
   calcBuyCount() {
     return Math.ceil(this.#pay / 1000);
+  }
+
+  addPrize(ranking) {
+    this.#totalPrize += this.#prizeMap[ranking];
   }
 }
 
