11bd901e4609acaf59e51dbed48e53bb2f104c58
daal2quf <daal2quf@gmail.com>
2022-11-11 20:39:41 +0900

    feat(lotto): 중복된 숫자 입력시 예외 처리 추가
    add:
    validate() 메서드 안에 if 조건문 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,10 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (numbers.length !== [...new Set(numbers)].length) {
+      throw new Error("[ERROR] 중복된 숫자가 없어야 합니다.");
+    }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
