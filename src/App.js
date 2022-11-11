2aac38f7820a693194a4dc0b2691c64cf379512e
Dean <yjj052800@gmail.com>
2022-11-11 15:40:50 +0900

    feat: 당첨 내역 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -65,6 +65,16 @@ class App {
     this.lotto.compare(this.publishResult);
     this.printWinningResult();
   }
+
+  printWinningResult() {
+    Console.print(`3개 일치 (5,000원) - ${this.lotto.fifthCount}개`);
+    Console.print(`4개 일치 (50,000원) - ${this.lotto.fourthCount}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${this.lotto.thirdCount}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.lotto.secondCount}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${this.lotto.firstCount}개`);
+  }
 }
 
 const app = new App();
