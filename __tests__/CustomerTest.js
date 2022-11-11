16da74f07eed89c44de3a4058698e0967ba2c0bd
MyungAe <ladiescheong@naver.com>
2022-11-11 21:30:27 +0900

    test(Customer, LottoStore): LottoStore 클래스의 generateLotto 메소드 관련 테스트를 새 파일로 분리

--- __tests__/CustomerTest.js
+++ __tests__/CustomerTest.js
@@ -1,15 +1,8 @@
 const Customer = require("../src/Customer");
-const LottoStore = require("../src/LottoStore");
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const mockQuestions = (answer) => {
   MissionUtils.Console.readLine = jest.fn().mockReturnValue(answer);
-};
-
-const mockRandoms = (numbers) => {
-  MissionUtils.Random.pickUniqueNumbersInRange = jest
-    .fn()
-    .mockReturnValue(numbers);
 };
 
 // dummy test codes
@@ -30,19 +23,6 @@ describe("Feat 1. Customer.buyLotto", () => {
   test("입력된 금액이 천원으로 나누어 떨어지지 않으면 예외가 발생한다.", () => {
     mockQuestions("8001");
     expect(customer.payMoney).toThrow("[ERROR]");
-  });
-});
-
-describe("Feat 2. LottoStore.generateLotto", () => {
-  const lottoStore = new LottoStore();
-
-  test("지불된 돈으로 살 수 있는 로또의 개수를 반환한다.", () => {
-    expect(lottoStore.calculateLottoCount(8000)).toBe(8);
-  });
-
-  test("살 수 있는 로또의 개수만큼 만든 로또 번호를 반환한다.", () => {
-    mockRandoms([1, 2, 3, 4, 5, 6]);
-    expect(lottoStore.generateLottoNumber(1).length).toBe(1);
   });
 });
 
---
+++ __tests__/LottoStoreTest.js
@@ -0,0 +1,28 @@
+const LottoStore = require("../src/LottoStore");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+// dummy test codes
+// describe("", () => {
+//   test("", () => {
+//     expect()
+//   });
+// });
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest
+    .fn()
+    .mockReturnValue(numbers);
+};
+
+describe("Feat 2. LottoStore.generateLotto", () => {
+  const lottoStore = new LottoStore();
+
+  test("지불된 돈으로 살 수 있는 로또의 개수를 반환한다.", () => {
+    expect(lottoStore.calculateLottoCount(8000)).toBe(8);
+  });
+
+  test("살 수 있는 로또의 개수만큼 만든 로또 번호를 반환한다.", () => {
+    mockRandoms([1, 2, 3, 4, 5, 6]);
+    expect(lottoStore.generateLottoNumber(1).length).toBe(1);
+  });
+});
