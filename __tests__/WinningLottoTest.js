137b4bfa028d3cf694f4125c37dc7b058ed19120
woo-jk <201702042@o.cnu.ac.kr>
2022-11-11 19:47:35 +0900

    test: WinningLotto 클래스 보너스 번호 범위 테스트 코드 작성

---
+++ __tests__/WinningLottoTest.js
@@ -0,0 +1,9 @@
+const WinningLotto = require("../src/WinningLotto");
+
+describe("당첨 로또 클래스 테스트", () => {
+  test("보너스 번호의 범위가 1~45 사이가 아닐 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new WinningLotto([1, 2, 3, 4, 5, 6], 50);
+    }).toThrow("[ERROR] 보너스 번호의 범위는 1~45 사이여야 합니다.");
+  });
+});
