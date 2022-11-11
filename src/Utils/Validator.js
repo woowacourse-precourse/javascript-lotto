78b03e88d926a3d692ff4629abba8cba88a42499
Cobocho <rlaalsrb1111@naver.com>
2022-11-11 22:29:11 +0900

    refactor: error 출력 ui 로직에서 제거

--- src/Utils/Validator.js
+++ src/Utils/Validator.js
@@ -6,19 +6,21 @@ class Validator {
     Validator.hasBlank(amount);
     amount = Number(amount);
     Validator.isNotNumber(amount);
+    Validator.isNotKilo(amount);
+    return amount;
   }
   static isBlank(value) {
-    if(value === '') return MESSAGES.ERROR.isBlank;
+    if(value === '') throw new Error(MESSAGES.ERROR.isBlank);
   }
   static hasBlank(value) {
     const regex = /\s/g;    
-    if(value.match(regex)) return MESSAGES.ERROR.hasBlank;
+    if(value.match(regex)) throw new Error(MESSAGES.ERROR.hasBlank);
   }
   static isNotNumber(value) {
-    if(isNaN(value)) return MESSAGES.ERROR.isNotNumber;
+    if(isNaN(value)) throw new Error(MESSAGES.ERROR.isNotNumber);
   }
   static isNotKilo(value) {
-    if(value % 1000 !== 0) return MESSAGES.ERROR.isNotKilo;
+    if(value % 1000 !== 0) throw new Error(MESSAGES.ERROR.isNotKilo);
   }
 }
 
---
+++ src/gamePrint.js
@@ -0,0 +1,9 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const MESSAGES = require('./constants');
+
+class gamePrint {
+  static gameStart() {
+  }
+}
+
+module.exports = gamePrint;
