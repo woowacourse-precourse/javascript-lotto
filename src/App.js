8aefde5e17418e675bddbbf2267ff845d2a8bc05
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-11 12:46:09 +0900

    feat(Lotto): 로또 구입 금액 입력 받기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Notice = require("./NoticeMessage.js")
+const Lotto = require("./Lotto.js")
 class App {
-  play() {}
+  constructor(){
+    this.Lotto = new Lotto()
+  }
+  play() {
+    MissionUtils.Console.readLine(Notice.INPUT_MONEY,(money) => {
+      console.log(money)
+    });
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
---
+++ src/NoticeMessage.js
@@ -0,0 +1,5 @@
+const NOTICE_MESSAGE = {
+    INPUT_MONEY : "구입금액을 입력해 주세요.",
+};
+
+module.exports = NOTICE_MESSAGE;
