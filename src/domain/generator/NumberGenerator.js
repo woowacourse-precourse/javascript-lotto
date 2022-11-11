e18186253633f8365ed34a33b7e1fde2f8ea2740
hwanfront <kim015jh@gmail.com>
2022-11-11 13:37:19 +0900

    feat(NumberGenerator): 1부터 45사이 중복되지 않은 6개 숫자 생성 기능 추가

--- src/domain/generator/NumberGenerator.js
+++ src/domain/generator/NumberGenerator.js
@@ -1,6 +1,14 @@
+const { Random } = require('@woowacourse/mission-utils');
+const Lotto = require('../lotto/Lotto');
+const LottoNumber = require('../lotto/LottoNumber');
+
+const sortByNumber = (a, b) => a - b;
+
 class NumberGenerator {
   static createRandomNumbers() {
-
+    const { min, max } = LottoNumber.RANGE;
+    const size = Lotto.SIZE;
+    return Random.pickUniqueNumbersInRange(min, max, size).sort(sortByNumber);
   }
 }
 
