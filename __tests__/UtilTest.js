98a30dfdec743584f8b05c189d2e6395fcfa6ac5
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-11 23:19:58 +0900

    test: 정답 수를 통한 등수 확인 함수 테스트

--- __tests__/UtilTest.js
+++ __tests__/UtilTest.js
@@ -1,6 +1,7 @@
 const countCorrectNumber = require('../src/utils/countCorrectNumber');
 const getLottoQuantity = require('../src/utils/getLottoQuantity');
 const isBonusNumberCorrect = require('../src/utils/isBonusNumberCorrect');
+const returnMyRank = require('../src/utils/returnMyRank');
 
 describe('유틸 함수 동작 테스트', () => {
   test('구매 로또의 정답 개수를 리턴하는 함수', () => {
@@ -19,5 +20,16 @@ describe('유틸 함수 동작 테스트', () => {
     expect(isBonusNumberCorrect(4, [2, 3, 4, 5, 6, 7])).toBe(1);
     expect(isBonusNumberCorrect(14, [12, 31, 42, 15, 26, 37])).toBe(0);
     expect(isBonusNumberCorrect(15, [12, 31, 42, 15, 26, 37])).toBe(1);
+  });
+
+  test('정답 수를 통한 등수 확인', () => {
+    expect(returnMyRank(1, 1)).toBe('FALSE');
+    expect(returnMyRank(2, 1)).toBe('THREE');
+    expect(returnMyRank(0, 1)).toBe('FALSE');
+    expect(returnMyRank(3, 1)).toBe('FOUR');
+    expect(returnMyRank(4, 1)).toBe('FIVE');
+    expect(returnMyRank(5, 0)).toBe('FIVE');
+    expect(returnMyRank(5, 1)).toBe('FIVE_AND_BONUS');
+    expect(returnMyRank(6, 0)).toBe('SIX');
   });
 });
