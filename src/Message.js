ee7e4b36a2b999c9f6f7831cfc238e2a09a7dc7f
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-12 00:18:31 +0900

    feat: 개행 문자 (\n) 추가

--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,11 @@
 const MESSAGE = Object.freeze({
     INPUT: {
-        PURCHASE_AMOUNT: '구입금액을 입력해 주세요.',
-        WINNING_NUMBER: '당첨 번호를 입력해 주세요.',
-        BONUS: '보너스 번호를 입력해 주세요.',
+        PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
+        WINNING_NUMBER: '당첨 번호를 입력해 주세요.\n',
+        BONUS: '보너스 번호를 입력해 주세요.\n',
     },
     OUTPUT: {
-        PURCHASE_COUNT: '개를 구매했습니다.',
+        PURCHASE_COUNT: '개를 구매했습니다.\n',
     }
 });
 
