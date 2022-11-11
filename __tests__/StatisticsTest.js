d44c0c40d71c485d61f3336c05eaeff4f3331f88
chiman <rlaclghks123@naver.com>
2022-11-11 21:14:48 +0900

    test: 순위에 맞춘 당첨번호 일치 개수 테스트
    getNumberOfMatchingNumbersRanked의 개수 테스트

--- __tests__/StatisticsTest.js
+++ __tests__/StatisticsTest.js
@@ -5,22 +5,30 @@ const totalLottoNumber = [
   [1, 2, 3, 4, 5, 6],
 ];
 
-const winnerNumber = [1, 5, 6, 10, 11, 12];
+const winnerNumber = ["1", "5", "6", "10", "11", "12"];
 
 const numbers = [1, 5, 6, 10, 11, 13];
 
 describe("compareBonus Number Test", () => {
   test("bonus number가 포함되어 있을경우 결과값이 4인경우 Test", () => {
-    const bonusNumber = [12];
+    const bonusNumber = "12";
     const statistics = new Statistics(totalLottoNumber, winnerNumber, bonusNumber);
     const result = statistics.compareWithBonusNumber(numbers);
     expect(result).toBe(4);
   });
 
   test("bonus number가 포함되어 있을경우 결과값이 5인경우 Test", () => {
-    const bonusNumber = [13];
+    const bonusNumber = "13";
     const statistics = new Statistics(totalLottoNumber, winnerNumber, bonusNumber);
     const result = statistics.compareWithBonusNumber(numbers);
+    expect(result).toBe(5);
+  });
+
+  test("getNumberOfMatchingNumbersRanked 테스트", () => {
+    const bonusNumber = "13";
+    const statistics = new Statistics(totalLottoNumber, winnerNumber, bonusNumber);
+    const result = statistics.getNumberOfMatchingNumbersRanked(numbers);
+
     expect(result).toBe(5);
   });
 });
