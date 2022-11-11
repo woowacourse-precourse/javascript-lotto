3038b90efd1e32df56cc09f3bf6058a770d03d6b
c0c0pang <coc1789@naver.com>
2022-11-11 20:07:28 +0900

    feat: 금액을 입력 받고 예외처리를 거쳐 구입할 수 있는 개수를 계산하는 기능 구현

--- src/LotteryDraw.js
+++ src/LotteryDraw.js
@@ -4,7 +4,13 @@ const MoneyExceptionHandling = require('./MoneyExceptionHandling');
 const WinningExceptionHandling = require('./WinningExceptionHandling');
 const BonusExceptionHandling = require('./BonusExceptionHandling');
 class LotteryDraw extends Struct{ 
-
+    moneyInput() {
+        MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+            money = Number(money);
+            new MoneyExceptionHandling(money);
+            this.money = money / 1000;
+        });
+    };
 };
 
 module.exports = LotteryDraw;
