60ba1abf831002380a1f19a101a4a05e2a987d01
guridaek <iue16@naver.com>
2022-11-11 13:58:33 +0900

    feat: 로또 클래스 예외사항 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,15 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복된 숫자가 없어야 합니다.');
+    }
+    if (numbers.map(Number).includes(NaN) || numbers.find(number => number < 1 || number > 45)) {
+      throw new Error('[ERROR] 로또 번호는 1 ~ 45 사이의 숫자여야 합니다.');
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
