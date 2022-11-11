20bf25ae503f1dcf7a9edbdbf1b9eda9ac2c75da
ashleysyheo <ashleysyheo@gmail.com>
2022-11-11 21:00:08 +0900

    feat: validate purchase amount input

--- src/Exception.js
+++ src/Exception.js
@@ -1,6 +1,14 @@
 const { ERROR_MESSAGE, PARAMETERS } = require('./utils/constants');
 
 class Exception {
+  validatevalidatePurchaseAmount(input) {
+    this.checkIsDigit(input);
+
+    if (input % PARAMETERS.purchaseAmountUnit !== 0) {
+      throw new Error(ERROR_MESSAGE.inValidUnitOfMoney);
+    }
+  }
+
   checkIsDigit(number) {
     if (!/^\d+$/.test(number)) {
       throw new Error(ERROR_MESSAGE.nonDigitInput);
--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,8 +1,10 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const Exception = require('./Exception');
 const { CONSOLE_MESSAGE, PARAMETERS } = require('./utils/constants');
 
 class LottoGame {
   constructor() {
+    this.exception = new Exception();
     this.purchaseCount = 0;
   }
 
@@ -10,6 +12,7 @@ class LottoGame {
 
   getPurchaseAmount() {
     MissionUtils.Console.readLine(`${CONSOLE_MESSAGE.purchaseAmount}`, (input) => {
+      this.exception.validatePurchaseAmount(input);
       this.purchaseCount = input / PARAMETERS.purchaseAmountUnit;
     });
   }
