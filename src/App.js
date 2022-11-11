ff0ce7bfa44f4eabaef29de1d65a7509122b73db
jjo-mi <whdal2636@naver.com>
2022-11-11 21:34:24 +0900

    feat: 1. 게임시작 안내문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.printStart();
+  }
+
+  
+  printStart() {
+    Console.print('구입금액을 입력해 주세요.');
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
