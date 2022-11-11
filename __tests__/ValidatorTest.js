d6ecd20a2e5f401bde7e61431d66e32795c12dce
Cobocho <rlaalsrb1111@naver.com>
2022-11-11 21:07:34 +0900

    test: 공백 유무 테스트 구현

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -2,27 +2,37 @@ const Validator = require("../src/Utils/Validator");
 const MESSAGES = require("../src/constants");
 
 describe("amountValidCheck 테스트", () => {
-  test("공백유무 판별", () => {
+  test("공백 여부 판별", () => {
     const nonBlankInput = ['1341', '23134', 'dsads ass'];
     const blank = '';
     nonBlankInput.forEach(subject => {      
       const result = Validator.isBlank(subject);
       expect(result).toBeUndefined();
     });
-    expect(() => Validator.isBlank(blank)).toThrowError(MESSAGES.ERROR.isBlank);
+    expect(Validator.isBlank(blank)).toBe(MESSAGES.ERROR.isBlank);
   });
-  test("숫자여부 판별", () => {
+  test("숫자 여부 판별", () => {
     const numbers = ['1341', '23134', '1243124141', '1'];
-    const strings = ['@#!$!', 'asasda', '한글', 'dda>?', '123'];
+    const notNumbers = ['oneTwo', '@!#!@$>', '123abc'];
     numbers.forEach(subject => {      
       const result = Validator.isNotNumber(subject);
       expect(result).toBeUndefined();
     });
-    const string = 'oneTwo';
-    const specials = '@!#!@$>';
-    const mixed = '123abc';
-    expect(() => Validator.isNotNumber(string)).toThrowError(MESSAGES.ERROR.isNotNumber);
-    expect(() => Validator.isNotNumber(specials)).toThrowError(MESSAGES.ERROR.isNotNumber);
-    expect(() => Validator.isNotNumber(mixed)).toThrowError(MESSAGES.ERROR.isNotNumber);
+    notNumbers.forEach(subject => {      
+      const result = Validator.isNotNumber(subject);
+      expect(result).toBe(MESSAGES.ERROR.isNotNumber);
+    });
+  });
+  test("공백 유무 판별", () => {
+    const normalNumbers = ['1341', '23134', '1243124141', '1'];
+    const haveBlankNumbers = ['1  21 3', '   123', '123   '];
+    normalNumbers.forEach(subject => {      
+      const result = Validator.hasBlank(subject);
+      expect(result).toBeUndefined();
+    });
+    haveBlankNumbers.forEach(subject => {      
+      const result = Validator.hasBlank(subject);
+      expect(result).toBe(MESSAGES.ERROR.hasBlank);
+    });
   });
 });
