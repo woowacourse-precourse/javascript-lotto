c6f71e64e517af483d3ed1a5e0c273f58dcf6500
Narcoker <jykim9868@gmail.com>
2022-11-11 19:22:05 +0900

    Feat(SYSTEM): 종료 기능 추가

--- src/System.js
+++ src/System.js
@@ -3,6 +3,9 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const SYSTEM = Object.freeze({
     print(message) {
         MissionUtils.Console.print(message);
+    },
+    exit() {
+        MissionUtils.Console.close();
     }
 })
 module.exports = { SYSTEM };
