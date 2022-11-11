1e837747c02eaee25f11c0978f044422911e9763
Sijun <sionwer4@naver.com>
2022-11-11 15:53:46 +0900

    Money class exception check fuction add

--- src/Money.js
+++ src/Money.js
@@ -2,7 +2,21 @@ class Money {
   #number;
 
   constructor(number) {
+    this.character(number);
+    this.divide(number);
     this.#number = number;
+  }
+
+  character(number) {
+    if (typeof number != "number") {
+      throw new Error("[ERROR] 구입 금액은 숫자여야 합니다.");
+    }
+  }
+
+  divide(number){
+    if(number % 1000 !== 0){
+      throw new Error("[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.")
+    }
   }
 }
 
