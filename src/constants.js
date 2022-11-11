80aae74c5c80c88074fd35a35874210896eb5d32
Hwang Seong Taek <hst007@naver.com>
2022-11-11 23:50:10 +0900

    refactor: constants.js 예외 메세지 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -23,6 +23,7 @@ const LOTTO_ERROR_MESSAGE = {
   DUPLICATE: '[ERROR] 로또 번호는 중복될 수 없습니다.',
   BONUS_DUPLICATE: '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.',
   NOT_DIVIDE: '[ERROR] 1,000원 단위의 금액을 입력해야 합니다.',
+  NOT_NUMBER: '[ERROR] 로또 구입은 숫자를 입력해야 합니다.',
   OUT_OF_RANGE: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
   BONUS_OUT_OF_RANGE: '[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.',
   NOT_REST: '[ERROR] 당첨 번호는 쉼표로 구분되어야 합니다.',
