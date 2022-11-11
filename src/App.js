f280905d08935c18593743866ec4c1e6e05de0b9
sohyun <park.sohyun0430@gmail.com>
2022-11-11 23:02:54 +0900

    feat: isValidMoney 함수 구현
    사용자가 입력한 로또 구입 가격이 유효한지 검사한다.
    유효하지 않은 경우 false를 return, 유효한 경우 true를 return한다.
    유효하지 않은 경우)
    1. 구입가격이 1000미만인 경우
    2. 구입가격이 숫자가 아닌 경우
    3. 구입가격이 1000원 단위가 아닌 경우

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+/* eslint-disable no-empty-function */
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const USER_MONEY_INPUT_REQUEST = '구입금액을 입력해 주세요.';
+
 class App {
-  play() {}
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {
+    MissionUtils.Console.print(USER_MONEY_INPUT_REQUEST);
+  }
+
+  play() {
+    MissionUtils.Console.readLine('', (userMoneyInput) => {
+      const isValid = this.isValidMoney(userMoneyInput);
+      MissionUtils.Console.print(isValid);
+      this.play();
+    });
+  }
+
+  isValidMoney(userMoneyInput) {
+    if (
+      userMoneyInput < 1000 ||
+      !Number(userMoneyInput) ||
+      userMoneyInput % 1000 !== 0
+    ) {
+      return false;
+    }
+    return true;
+  }
+
+  // issueLotteries(userMoneyInput) {
+  //   return lotteryQuantity;
+  // }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
