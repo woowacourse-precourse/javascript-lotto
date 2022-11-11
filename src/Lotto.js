564b4cf73cfdbae4ed91a2e782ada15c73268c8d
Vo9Ming <kimminj2017@gmail.com>
2022-11-11 13:52:31 +0900

    사용자에게 입력받은 금액에 대한 예외 처리 함수인 checkAmount() 함수를 생성한다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,8 +11,15 @@ class Lotto {
   inputAmount(numbers) {
     MissionUtils.Console.readLine("금액을 입력해주세요.", (answer) => {
       console.log(`입력한 금액: ${answer}`);
+      this.checkAmount(answer);
       MissionUtils.Console.close();
     });
+  }
+
+  checkAmount(amount) {
+    if (amount % 1000 !== 0) {
+      throw new Error("[ERROR] 금액을 1,000 단위로 입력해주세요.");
+    }
   }
 }
 
