e38a6cd9e2e089ee21996026e88168d7b4c2d183
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-11 11:37:44 +0900

    feat: Buyer 클래스 삭제 및 구입금액 입력받는 메서드 App.js 로 이동

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const LottoCounter = require('./LottoCounter');
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  constructor() {
+    this.lottoCounter;
+  }
+
+  play() {
+    this.inputCash();
+  }
+
+  inputCash() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
+      this.lottoCounter = new LottoCounter(input);
+      console.log(this.lottoCounter.countLotto());
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
