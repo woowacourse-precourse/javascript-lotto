facc8b21534a6cc06ea22dcdc5e32549dfe4dc23
HWI <dlwognl99@naver.com>
2022-11-12 02:14:21 +0900

    test: 입력에 대한 유효성 테스트 코드 추가 Validation.isOnlyNumber 1. 구입 가격인 input이 모두 숫자인 경우 2. 구입 가격인 input에 문자가 포함되어 있는 경우

--- __tests__/InputValidationTest.js
+++ __tests__/InputValidationTest.js
@@ -1,3 +1,7 @@
+const {
+  ERROR_MESSAGE_NOTONLY_NUMBER,
+  ERROR_MESSAGE_UNDIVIDED,
+} = require("../src/constants.js");
 const Validation = require("../src/Validation.js");
 
 describe("Validation.isDivided", () => {
@@ -17,5 +21,24 @@ describe("Validation.isDivided", () => {
     const result = Validation.isDivided(input);
     // then
     expect(result).toBe(false);
+  });
+});
+
+describe("Validation.isOnlyNumber", () => {
+  test("구입 가격인 input이 모두 숫자라면 true를 반환해야 한다.", () => {
+    // given
+    const input = "8000";
+    // when
+    const result = Validation.isOnlyNumber(input);
+    // then
+    expect(result).toBe(false);
+  });
+  test("구입 가격인 input에 문자가 포함되어 있다면 false를 반환해야 한다.", () => {
+    // given
+    const input = "8000j";
+    // when
+    const result = Validation.isOnlyNumber(input);
+    // then
+    expect(result).toBe(true);
   });
 });
