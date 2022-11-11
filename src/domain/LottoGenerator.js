ab66dcc6ba83869cb33ce7071adc40482d50c2c5
oseunghyun <ohsh1477@gmail.com>
2022-11-11 12:23:47 +0900

    feat(LottoGenerator): implement generateLottoNum method

--- src/domain/LottoGenerator.js
+++ src/domain/LottoGenerator.js
@@ -2,10 +2,19 @@ const { Random } = require('@woowacourse/mission-utils');
 
 const MIN_LOTTO_NUM = 1;
 const MAX_LOTTO_NUM = 45;
+const NUM_OF_LOTTO = 6;
 
 class LottoGenerator {
   constructor() {
     this.lotto;
+  }
+
+  generateLottoNum() {
+    return Random.pickUniqueNumbersInRange(
+      MIN_LOTTO_NUM,
+      MAX_LOTTO_NUM,
+      NUM_OF_LOTTO
+    );
   }
 
   generateRandomNum() {
