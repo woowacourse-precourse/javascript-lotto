35b2d86fa45df64458f2294b8dcd6e6648ad6101
turtle601 <turtle601@naver.com>
2022-11-11 23:44:50 +0900

    feat(app) 입력 기능 구현(최종 기능 구현 완료)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,61 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+const LottoAdjustment = require('./LottoAdjustment');
+const Lotto = require('./Lotto');
+const LottoStore = require('./LottoStore');
+const LottoDrawFactory = require('./LottoDrawFactory');
+const Bonus = require('./Bonus');
+
 class App {
-  play() {}
+  #lottoStore;
+
+  #lotto;
+
+  #bonus;
+
+  constructor() {
+    this.#lottoStore = null;
+    this.#lotto = null;
+    this.#bonus = null;
+  }
+
+  #buyLotto() {
+    Console.readLine('구입금액을 입력해주세요.\n', input => {
+      this.#lottoStore = new LottoStore(input);
+
+      this.#drawLotto();
+    });
+  }
+
+  #drawLotto() {
+    Console.readLine('당첨 번호를 입력해 주세요.\n', input => {
+      const inputNumArr = input.split(',').map(value => Number(value));
+
+      this.#lotto = new Lotto(inputNumArr);
+
+      this.#drawBonus();
+    });
+  }
+
+  #drawBonus() {
+    Console.readLine('보너스 번호를 입력해 주세요.\n', input => {
+      this.#bonus = new Bonus(input);
+
+      const lottoPayment = new LottoAdjustment({
+        draw: new LottoDrawFactory({ lotto: this.#lotto, bonus: this.#bonus }),
+        payment: this.#lottoStore,
+      });
+
+      lottoPayment.print();
+    });
+  }
+
+  play() {
+    this.#buyLotto();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
