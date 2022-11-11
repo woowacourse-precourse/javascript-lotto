5cd1dbee480713da31c38bbcc2fdbd675247882a
Subin Jo <sbjo1202@naver.com>
2022-11-12 02:21:32 +0900

    feat: 구입 금액 입력값 확인하기 구현

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -11,7 +11,15 @@ class LottoMachine {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
       this.#money = Number(money);
       console.log(this.#money);
+      
+      this.checkInputMoney();
     });
+  }
+
+  checkInputMoney() {
+    if (this.#money % 1000 !== 0) {
+      throw Error("[ERROR] 1,000원 단위의 금액을 투입해주세요.");
+    }
   }
 
 }
