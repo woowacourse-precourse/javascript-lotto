2ad0dd5653442730183f09f4d63830e87040408d
d0422 <rlfehd2013@naver.com>
2022-11-12 03:35:48 +0900

    정답번호와 보너스 번호가 중복되는 경우 예외처리하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -38,12 +38,19 @@ class App {
   }
 
   setBonusNumber(inputNumber) {
+    this.checkBonusNumber(inputNumber);
     this.bonusNumber = Number(inputNumber);
   }
 
   checkMoney(money) {
     if (money % 1000 != 0) {
       throw new Error('[ERROR] 돈은 1000원단위여야 합니다!');
+    }
+  }
+
+  checkBonusNumber(inputNumber) {
+    if (this.winLotto.lottoNumbers.includes(Number(inputNumber))) {
+      throw new Error('[ERROR] 보너스 숫자와 정답 로또 숫자가 중복되었습니다.');
     }
   }
 
