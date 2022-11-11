831e8658cfd48a30b2b94ae6b75dd284d1c7a57f
Cobocho <rlaalsrb1111@naver.com>
2022-11-11 20:52:19 +0900

    feat: 숫자여부 판별 메서드 작성 및 테스트를 위한 private 필드 해제

--- src/Utils/Validator.js
+++ src/Utils/Validator.js
@@ -2,11 +2,15 @@ const MESSAGES = require('../constants');
 
 class Validator {
   static amountValidCheck(amount) {
-    Validator.#isBlank(amount);
+    Validator.isBlank(amount);
+    Validator.isNotNumber(amount);
   }
-  static #isBlank(value) {
+  static isBlank(value) {
     if(value === '') throw new Error(MESSAGES.ERROR.isBlank);
-    return false;
+  }
+  static isNotNumber(value) {
+    value = Number(value);
+    if(isNaN(value)) throw new Error(MESSAGES.ERROR.isNotNumber);
   }
 }
 
--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,7 @@ const MESSAGES = {
   },
   ERROR: {
     isBlank: '공백이 아닌 값을 입력해주세요!',
+    isNotNumber: '숫자를 입력해주세요!',
   },
 }
 
