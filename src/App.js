0103b65602932175336ffb93f214c1f6739f4176
d0422 <rlfehd2013@naver.com>
2022-11-11 15:31:00 +0900

    Feat:돈의 액수에 따라 오름차순으로 로또를 발행하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-const input = require('./Input');
+const MissionUtils = require('@woowacourse/mission-utils');
+const input = require('./UI/Input');
+const Lotto = require('./Lotto');
+
 class App {
   lottos = [];
   winLotto;
@@ -13,9 +16,29 @@ class App {
     );
   }
 
-  getLottosByMoney(money) {}
+  getLottosByMoney(money) {
+    this.checkMoney(money);
+    const lottoNums = money / 1000;
+    for (let x = 0; x < lottoNums; x++) {
+      const lottoNumbers = this.getRandomNumber();
+      this.lottos.push(new Lotto(lottoNumbers));
+    }
+  }
+
   setWinLotto(inputNumbers) {}
   setBonusNumber(inputNumber) {}
+
+  checkMoney(money) {
+    if (money % 1000 != 0) {
+      throw new Error('돈은 1000원단위여야 합니다!');
+    }
+  }
+
+  getRandomNumber() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+      (a, b) => a - b
+    );
+  }
 }
 
 new App().play();
