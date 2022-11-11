fefa0369cabf292f7577b2c50e1280336ef61a87
bassyu <yrkdgus@gmail.com>
2022-11-12 01:49:36 +0900

    refactor(Calculator): 상수 활용

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,29 +1,23 @@
+const { LOTTO_PRICE, WINNIG_PRIZE } = require('./constants/price');
+
 class Calculator {
   #pay;
   #totalPrize;
-  #prizeMap;
 
   constructor(pay) {
     this.validate(pay);
     this.#pay = pay;
     this.#totalPrize = 0;
-    this.#prizeMap = {
-      1: 2000000000,
-      2: 30000000,
-      3: 1500000,
-      4: 50000,
-      5: 5000,
-    };
   }
 
   validate(pay) {}
 
   calcBuyCount() {
-    return Math.ceil(this.#pay / 1000);
+    return Math.ceil(this.#pay / LOTTO_PRICE);
   }
 
   addPrize(ranking) {
-    this.#totalPrize += this.#prizeMap[ranking];
+    this.#totalPrize += WINNIG_PRIZE[ranking];
   }
 
   calcProfitRate() {
