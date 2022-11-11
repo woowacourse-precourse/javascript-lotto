23ecc8e9ff17d47aff16b3fc6da9c496e57f3bba
BaeDongWoo <ehddn5410@naver.com>
2022-11-11 23:42:04 +0900

    로또 구매자의 구입금액을 입력.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.cost();
+  }
+  cost(){
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (money)=>{
+    });
+  }
 }
 
 module.exports = App;
