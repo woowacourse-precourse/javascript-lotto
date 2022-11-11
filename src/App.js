b5b4c0b5851364316a5bdca5fdf6266913754db4
yuu <22yuu@naver.com>
2022-11-11 17:40:02 +0900

    feat: 당첨번호 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-const MissionUtils = require("@woowacourse/mission-utils");
 const Utils = require("./Utils");
 const Lotto = require("./Lotto");
 
 class App {
 
   constructor() {
-    Utils.print('로또 게임을 시작합니다~!');
-    this.cost = undefined;
+    // Utils.print('로또 게임을 시작합니다~!');
   }
   
-
   play() {
     Utils.readLine('구입금액을 입력해 주세요.\n', (input) => {
       const cost = this.onValidation(Number(input));
@@ -17,6 +14,11 @@ class App {
       
       console.log(this.issueLottery(nLottos));
 
+      Utils.readLine('당첨 번호를 입력해 주세요.\n', (input) => {
+        this.setTargetNumbers(input);
+      });
+
+      // Utils.close();
     });
   }
 
@@ -32,6 +34,7 @@ class App {
 
     if(input % 1000 == 0) count = input / 1000;
     else throw new Error('[ERROR] 금액은 1000원 단위로 숫자만 입력해주세요.');
+
     return count;
   }
 
@@ -42,6 +45,25 @@ class App {
 
     return array;
   }
+
+  setTargetNumbers(input) {
+    if(!input.includes(',')) throw new Error('[ERROR] 당첨 번호는 콤마(,)를 사용해서 구분해주세요.');
+
+    let countComma = input.match(/,/g).length ? input.match(/,/g).length : 0;
+    console.log(countComma);
+
+    if(countComma > 5) throw new Error('[ERROR] 당첨 번호는 콤마(,)를 사용해서 구분해주세요.(5개)');
+    
+    let array = input.split(',').map(num => Number(num));
+
+    let max = Math.max(...array);
+
+    if(max > 45 || array.includes(0)) throw new Error('[ERROR] 당첨 번호는 1~45 사이의 수를 입력해주세요.');
+
+    return array;
+  }
+
+ 
 
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,10 +1,18 @@
+/**
+ * 로또 클래스
+ * 1. 사용자로 부터 입력 받은 당첨 번호의 유효성 검사를 수행
+ * 2. 발행한 로또들과 당첨 번호 확인
+ * 3. 결과 출력
+ * 4. 
+ * 
+ */
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
-    // this.#numbers = numbers;
-    console.log(numbers);
+    this.#numbers = numbers;
   }
 
   validate(numbers) {
@@ -20,6 +28,7 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+
 }
 
 module.exports = Lotto;
