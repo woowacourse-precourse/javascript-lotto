966dfa1c7714e545b947953ec7c328a194bfd532
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-12 00:50:09 +0900

    feat: 발행한 로또 수량 및 번호를 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ const { Console, Random } = require('@woowacourse/mission-utils');
 
 class App {
   getCount() {
-    Console.readLine('구입금액을 입력해 주세요.\n', (amount) => {
-      this.lottoCount = amount / 1000;
+    Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
+      this.lottoCount = input / 1000;
     });
   }
 
@@ -17,6 +17,15 @@ class App {
       lottos.push(numbers.sort((num1, num2) => num1 - num2));
     }
     this.lottos = lottos;
+  }
+
+  printLottos() {
+    const { lottoCount, lottos } = this;
+
+    Console.print(`${lottoCount}개를 구매했습니다.`);
+    lottos.forEach((lotto) => {
+      Console.print(lotto);
+    });
   }
 
   play() {}
