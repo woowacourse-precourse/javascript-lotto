288487e013f8b6b4e52d293b1c233e8f05383486
jinseo <ghkdwlstj99@gmail.com>
2022-11-11 22:10:52 +0900

    refactor: class 로직 수정

--- src/InputWinninNumber.js
+++ src/InputWinninNumber.js
@@ -1,16 +1,19 @@
 const {Console} = require("@woowacourse/mission-utils");
-const INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
-const INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요." 
+const Lotto = require("./Lotto")
+const INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+const INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
 class InputWinningNumber{
 
     constructor(){
+        this.lotto = new Lotto();
         this.winningNum = [];
         this.bonusNum = [];
     }
 
     winningNumber(){
         Console.readLine(INPUT_WINNING_NUMBER, (num) => {
+            this.lotto.validate(num);
             return this.winningNum.push(num);
         })
     }
