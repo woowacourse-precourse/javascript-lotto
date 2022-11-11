0b8c46beb7aa69a1006f38470393635db98f00f2
oio337a <oio337a@gmail.com>
2022-11-11 20:30:57 +0900

    feat : add invalidate case

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ class App {
 
   purchaseAmount() {
     Console.readLine("구매금액을 입력해 주세요.\n", (amount) => {
+      this.validatePurchaseAmount(amount);
       Console.close();
       Console.print(amount);
     });
+  }
+
+  validatePurchaseAmount(amount) {
+    if (amount < 1000)
+      throw new Error("[ERROR] 구매금액은 1000원 이상이어야 합니다.");
+    const regex = /[^0-9]/g;
+    if (amount.match(regex).length !== 0)
+      throw new Error("[ERROR] 숫자만 입력해 주세요.");
   }
 }
 
