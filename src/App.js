670781524090750c4422a4f4908104f61e5229a9
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-12 00:25:17 +0900

    feat: 로또 구입 금액을 입력 받아 발행할 로또 개수를 구하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
+  getCount() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (amount) => {
+      this.lottoCount = amount / 1000;
+    });
+  }
+
   play() {}
 }
 
