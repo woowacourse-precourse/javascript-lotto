3a1700c7eb5a12f5aeb765e64c255cc39418ac00
yeongi <cbg5255@gmail.com>
2022-11-11 18:55:42 +0900

    docs : README.md LottoSetting 클래스 기능 추가

---
+++ src/LottoSetting.js
@@ -0,0 +1,8 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { INPUT } = require("./lib/library");
+
+class LottoSetting {
+  inputWinLottoNum() {}
+  exportWinLottoArray() {}
+  inputBonusNum() {}
+}
