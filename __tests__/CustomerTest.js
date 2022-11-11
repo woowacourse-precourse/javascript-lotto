1b7892a189dc609dc372deff188f2e97527840f2
MyungAe <ladiescheong@naver.com>
2022-11-11 20:26:28 +0900

    test(Customer): 기능 2. LottoStore.generateLottoNumber 메소드에 대한 테스트 코드 추가
    - 주요 기능
    1. 지불된 돈을 받아 살 수 있는 복권의 개수를 반환하는 테스트
    2. 살 수 있는 복권의 개수를 받아 만든 복권을 반환하는 테스트

--- __tests__/CustomerTest.js
+++ __tests__/CustomerTest.js
@@ -13,23 +13,24 @@ const mockQuestions = (answer) => {
 // });
 
 describe("Feat 1. Customer.buyLotto", () => {
+  const customer = new Customer();
   test("입력된 금액이 천원으로 나누어 떨어진다.", () => {
     mockQuestions("8000");
-    const customer = new Customer();
     expect(customer.payMoney()).toBe(8000);
   });
 
   test("입력된 금액이 천원으로 나누어 떨어지지 않으면 예외가 발생한다.", () => {
     mockQuestions("8001");
-    const customer = new Customer();
     expect(customer.payMoney).toThrow("[ERROR]");
   });
 });
 
 describe("Feat 2. LottoStore.generateLottoNumber", () => {
-  test("입력된 금액이 천원으로 나누어 떨어진다.", () => {
-    mockQuestions("8000");
-    const customer = new Customer();
-    expect(customer.payMoney()).toBe(8000);
+  const lottoStore = new LottoStore();
+  test("지불된 돈으로 살 수 있는 복권의 개수를 반환한다.", () => {
+    expect(lottoStore.calculateLottoCount(8000)).toBe(8);
+  });
+  test("살 수 있는 복권의 개수만큼 만든 복권 번호를 반환한다.", () => {
+    expect(lottoStore.generateLotto(8).length).toBe(8);
   });
 });
