0d7f9cb3ab2445baf2411ecc380fe2c6822502c5
BaeDongWoo <ehddn5410@naver.com>
2022-11-12 00:01:30 +0900

    입력받은 구입금액을 확인

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ class App {
   }
   cost(){
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (money)=>{
+      this.costValidate(money);
     });
+  }
+  costValidate(money){
+    if(money<1000){
+      throw new Error("[ERROR] 최소 구입금액은 1000원 이상 이여야합니다");
+    }
+    if(money%1000!=0){
+      throw new Error("[ERROR] 구입금액은 1000원 단위 이여야합니다");
+    }
   }
 }
 
