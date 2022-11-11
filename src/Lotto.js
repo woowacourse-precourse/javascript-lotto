3365271f3b88bd10d767c43016991c9c2c60772c
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-12 07:29:02 +0900

    feat: add lotto validation - overlap

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,14 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+    this.validate_overlap(numbers);
+  }
+
+  validate_overlap(numbers) {
+    const NUMBERS = new Set(numbers);
+    if (NUMBERS.size != numbers.length) {
+      throw new Error('[ERROR] 로또 번호는 중복이 없어야 합니다.');
+    }
   }
 
   order_lotto() {
