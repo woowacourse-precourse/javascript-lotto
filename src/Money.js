825be048622339c8a1d84ee5a5f1f329b2594602
yeonsuBeak <soothbugger@gmail.com>
2022-11-11 17:27:36 +0900

    feat(lotto): 구매 금액이 천원 단위가 아닌 경우 예외 처리 기능 구현

--- src/Money.js
+++ src/Money.js
@@ -8,11 +8,19 @@ class Money {
 
   validate(money) {
     this.checkNumber(money);
+    this.checkUnit(money);
   }
 
   checkNumber(money) {
     if (isNaN(money)) {
       throw new Error("[ERROR] 구매 금액은 숫자여야 합니다.");
+    }
+  }
+
+  checkUnit(money) {
+    const UNIT = 1000;
+    if (parseInt(money, 10) % UNIT !== 0) {
+      throw new Error("[ERROR] 구매 금액은 천원 단위여야 합니다.");
     }
   }
 }
