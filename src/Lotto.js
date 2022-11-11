13f2ee4ade329608936426c49c26e28590cc164b
Tory <88531407+1two13@users.noreply.github.com>
2022-11-11 16:19:59 +0900

    refactor(Lotto, Messages): 출력한 로또 번호 예외 사항 체크

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
+const Messages = require('./Messages');
 
 class Lotto {
   #numbers;
@@ -9,12 +10,16 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers !== undefined && numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    if (numbers !== undefined) {
+      const NUMBERS_IN_RANGE = numbers.filter((el) => Number(el) >= 1 && Number(el) <= 45);
+
+      if (numbers.length !== 6) throw new Error(Messages.SIX_NUMBERS_MESSAGES);
+      if (numbers.length !== new Set(numbers).size) throw new Error(Messages.NOT_DUPLICATE_MESSAGE);
+      if (numbers.length !== NUMBERS_IN_RANGE.length) {
+        throw new Error(Messages.NUMBERS_IN_RANGE_MESSAGE);
+      }
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
--- src/Messages.js
+++ src/Messages.js
@@ -2,7 +2,11 @@ class Messages {
   static ENTER_MONEY = '구입금액을 입력해주세요.\n';
   static ENTER_WINNER_NUMBER = '\n당첨 번호를 입력해 주세요.\n';
   static ENTER_BONUS_NUMBER = '\n보너스 번호를 입력해 주세요.\n';
-  static WINNING_STATICS = '당첨 통계\n---';
+  static WINNING_STATICS = '\n당첨 통계\n---';
+
+  static SIX_NUMBERS_MESSAGES = '[ERROR] 로또 번호는 6개여야 합니다.';
+  static NOT_DUPLICATE_MESSAGE = '[ERROR] 중복되지 않는 숫자를 입력해주세요.';
+  static NUMBERS_IN_RANGE_MESSAGE = '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.';
 }
 
 module.exports = Object.freeze(Messages);
