c9aceb75d0d005172d66cc7e7cbe2dc21de9cf8a
이석준 <sukjune96@gmail.com>
2022-11-11 21:29:33 +0900

    feat: 구입금액 입력 예외처리 메서드 구현

--- src/LottoGenerator.js
+++ src/LottoGenerator.js
@@ -6,7 +6,9 @@ class LottoGenerator {
   }
   validatePayment(input) {
     const payment = parseInt(input);
-    if (payment < 1000) {
+    if (isNaN(input)) {
+      throw new Error("[ERROR] 숫자를 입력해 주세요.");
+    } else if (payment < 1000) {
       throw new Error("[ERROR] 돈이 부족합니다.");
     } else if (payment % this.#PRICE !== 0) {
       throw new Error("[ERROR] 1000으로 나누어 떨어지지 않습니다.");
