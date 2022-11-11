5e7c21ba1a74df0679391076cb54036c9801fc10
김세환 <sehwan@gimsehwan-ui-MacBookPro.local>
2022-11-11 19:00:20 +0900

    feat(lotto): 당첨번호 입력시 발생할 수 있는 예외사항 함수 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,10 +7,14 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    if (numbers.length !== 6) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (numbers.length !== new Set(numbers).size) throw new Error("[ERROR] 중복되는 숫자가 존재합니다.");
+    
+    const validNumber = numbers.filter(number => number > 0 && number < 46);
+    if (numbers !== validNumber) throw new Error("[ERROR] 1과 45사이의 숫자를 입력해주세요.")
   }
+
+
 
   
 
