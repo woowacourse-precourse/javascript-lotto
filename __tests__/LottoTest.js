bbb6e69a99780d9bed90ee972f594fa0ce12e3e0
hwanfront <kim015jh@gmail.com>
2022-11-11 16:58:29 +0900

    test(Lotto): 로또 예외처리에 대한 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,6 @@
-const Lotto = require("../src/Lotto");
+const Lotto = require("../src/domain/lotto/Lotto");
+const LottoHasDuplicatedException = require("../src/exception/lotto/LottoHasDuplicatedException");
+const LottoLengthException = require("../src/exception/lotto/LottoLengthException");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -15,4 +17,24 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또 번호의 개수가 6이 아닌 경우", () => {
+    expect(() => {
+      new Lotto([]);
+    }).toThrow(LottoLengthException);
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow(LottoLengthException);
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+    }).toThrow(LottoLengthException);
+  })
+  
+  test("로또 번호에 중복이 존재하는 경우", () => {
+    expect(() => {
+      new Lotto([1, 1, 1, 1, 1, 1]);
+    }).toThrow(LottoHasDuplicatedException);
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 5]);
+    }).toThrow(LottoHasDuplicatedException);
+  })
 });
