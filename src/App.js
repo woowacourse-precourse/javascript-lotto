6d857f0ac2772db9e5383cfe55130f29af903f62
AnJaeSeong <dktmzh6@gmail.com>
2022-11-11 12:06:32 +0900

    feat: App flow function
    inputPurchase, inputWinNum, inputBonusNum, getResult

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const { calRate } = require("./AppUtils");
+const AppUtils = require('./AppUtils');
 const Lotto = require('./Lotto.js');
 const MyLotto = require("./MyLotto.js");
 
 class App {
   constructor() {
     this.myLotto = null;
-    this.lotto = null;
+    this.winlotto = null;
   }
 
   play() {
@@ -16,7 +18,11 @@ class App {
   inputPurchase() {
     // 로또 구입 금액 입력
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
+      MissionUtils.Console.print('');
       this.myLotto = new MyLotto(parseInt(input));
+      AppUtils.printCountLotto(this.myLotto);
+      AppUtils.printMyLotto(this.myLotto);
+      MissionUtils.Console.print('');
       this.inputWinNum();
     });
   }
@@ -24,7 +30,8 @@ class App {
   inputWinNum() {
     // 당첨 번호 입력
     MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.\n', (input) => {
-      this.lotto = new Lotto(input.split(',').map(num => parseInt(num)));
+      MissionUtils.Console.print('');
+      this.winlotto = new Lotto(input.split(',').map(num => parseInt(num)));
       this.inputBonusNum();
     });
   }
@@ -32,9 +39,20 @@ class App {
   inputBonusNum() {
     // 보너스 번호 입력
     MissionUtils.Console.readLine('보너스 번호를 입력해 주세요.\n', (input) => {
-      this.lotto.setBonusNum(parseInt(input));
+      MissionUtils.Console.print('');
+      this.winlotto.setBonusNum(parseInt(input));
+      this.getResult();
     });
   }
+
+  getResult() {
+    // 결과 출력 및 종료
+    AppUtils.printWinStat(this.myLotto, this.winlotto)
+    MissionUtils.Console.close();
+  }
 }
+
+// const app = new App();
+// app.play();
 
 module.exports = App;
