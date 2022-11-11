f35867324ad92a95f15c5ad96b8255b3dbb10daa
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-11-11 11:02:36 +0900

    feat: 당첨 번호와 비교 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,9 +3,10 @@ const { LOTTO, ERROR } = require('./Constants');
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
+  constructor(numbers, winningNumber, bonusNumber) {
     this.validate(numbers);
     this.#numbers = numbers;
+    this.checkLotto(numbers, winningNumber, bonusNumber);
   }
 
   validate(numbers) {
@@ -14,7 +15,37 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  checkLotto(numbers, winningNumber, bonusNumber) {
+    let winning = 0;
+    let bonus = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      if (winningNumber.includes(numbers[i])) {
+        winning += 1;
+      }
+      if (numbers[i] === bonusNumber) {
+        bonus += 1;
+      }
+    }
+    this.lottoResult(winning, bonus);
+  }
+
+  lottoResult(winning, bonus) {
+    if (winning === 6) {
+      return 'Ranking1';
+    }
+    if (winning === 5 && bonus === 1) {
+      return 'Ranking2';
+    }
+    if (winning === 5 && bonus === 0) {
+      return 'Ranking3';
+    }
+    if (winning === 4) {
+      return 'Ranking4';
+    }
+    if (winning === 3) {
+      return 'Ranking5';
+    }
+  }
 }
 
 module.exports = Lotto;
