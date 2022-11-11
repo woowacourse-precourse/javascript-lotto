b88fd5c88a76a04fff82588e7a93a68be643f135
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-12 02:47:45 +0900

    feat: 당첨 기준에 따른 당첨 내역 및 금액을 구하는 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,6 +4,14 @@ class Lotto {
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
+    this.prizes = Object.freeze({
+      꽝: '0',
+      '3개 일치': '5,000',
+      '4개 일치': '50,000',
+      '5개 일치': '1,500,000',
+      '5개 일치, 보너스 볼 일치': '30,000,000',
+      '6개 일치': '2,000,000,000',
+    });
   }
 
   validate(numbers) {
@@ -17,7 +25,18 @@ class Lotto {
     const intersection = numbers.filter((num) => winningNumbers.has(num));
 
     this.matchCount = intersection.length;
-    this.bonus = numbers.includes(bonusNumber);
+    this.hasBonus = numbers.includes(bonusNumber);
+  }
+
+  getMatchInformation() {
+    const { matchCount, hasBonus } = this;
+    let matchInfo = `${matchCount}개 일치`;
+
+    if (matchCount === 5 && hasBonus) {
+      matchInfo += ', 보너스 볼 일치';
+    }
+    this.prize = prizes[matchInfo];
+    this.matchInfo = `${matchInfo} (${this.prize}원)`;
   }
 }
 
