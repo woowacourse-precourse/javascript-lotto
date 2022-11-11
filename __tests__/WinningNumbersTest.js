52ff604aa5342b3bdcc237d7be915ce18d21da2c
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-11 22:29:27 +0900

    test(winningNumbers): 입력받은 당첨 번호에 중복 숫자가 있다면 에러를 반환하는 테스트

--- __tests__/WinningNumbersTest.js
+++ __tests__/WinningNumbersTest.js
@@ -25,4 +25,12 @@ describe('입력받은 당첨 번호가 유효한지 확인하는 테스트', ()
       app.play();
     }).toThrow('[ERROR]');
   });
+
+  test('사용자가 입력한 당첨 번호에 중복된 숫자가 포함되어 있다면 에러를 발생시킨다.', () => {
+    mockQuestions(['1000', '1,2,3,4,5,5']);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow('[ERROR]');
+  });
 });
