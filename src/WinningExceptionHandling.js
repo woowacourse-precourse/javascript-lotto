f22193c6debcd301429bbe00f4e710fd2f4f7234
c0c0pang <coc1789@naver.com>
2022-11-11 19:45:24 +0900

    feat: 당첨번호의 숫자가 1~45 범위에 속하는지 확인하는 예외처리 기능 구현

--- src/WinningExceptionHandling.js
+++ src/WinningExceptionHandling.js
@@ -2,6 +2,7 @@ class WinningExceptionHandling {
     constructor(winning) {
         this.lengthCheck(winning);
         this.overlapCheck(winning);
+        this.rangeCheck(winning);
     };
     lengthCheck(winning) {
         if (winning.length !== 6) {
@@ -14,6 +15,13 @@ class WinningExceptionHandling {
             throw new Error("[ERROR] 로또 번호는 중복이 없습니다.");
         };
     };
+    rangeCheck(winning) {
+        for(let index = 0 ;index<winning.length;index++){
+          if(!(winning[index]>=1 && winning[index]<=45)){
+            throw new Error("[ERROR] 로또 번호는 숫자는 1~45 까지 입니다.");
+          };
+        };
+      };
 };
 
 module.exports = WinningExceptionHandling;
