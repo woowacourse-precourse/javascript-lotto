61eb12580d7b47cc2fb43b9a0ccec9eb458aae74
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-11 14:11:15 +0900

    feat: validate lotto number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { Console } = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
+
 class App {
-  play() {}
+  constructor() {
+    // this.lotto = new Lotto();
+  }
+
+  play() {
+    Console.readLine('당첨 번호를 입력해 주세요.\n', (inputNum) => {
+      new Lotto(inputNum);
+      // lotto();
+    });
+  }
 }
 
-module.exports = App;
+new App().play();
+
+// module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,51 @@
+const ThrowError = require('./components/ThrowError');
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
     this.#numbers = numbers;
+    this.showValidateResult(this.#numbers);
+  }
+
+  showValidateResult(lottoNumber) {
+    const resultMessage = this.validate(lottoNumber);
+    return resultMessage ? ThrowError(resultMessage) : true;
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    const numbersList = numbers.split(',');
+    return (
+      this.isNumber(numbersList) ||
+      this.isSplit(numbers) ||
+      this.isNumberCorrect(numbersList) ||
+      this.isNumberRepeated(numbersList) ||
+      this.isNumberRange(numbersList)
+    );
   }
 
-  // TODO: 추가 기능 구현
+  isNumber(numbersList) {
+    return /^\d+$/.test(numbersList.join('')) === false ? 'NUMBER' : false;
+  }
+
+  isSplit(numbers) {
+    return !numbers.includes(',') ? 'SPLIT' : false;
+  }
+
+  isNumberCorrect(numbersList) {
+    return numbersList.length !== 6 ? 'LENGTH' : false;
+  }
+
+  isNumberRepeated(numbersList) {
+    return new Set(numbersList).size !== 6 ? 'REPEAT' : false;
+  }
+
+  isNumberRange(numbersList) {
+    return numbersList.filter((eachNumber) => Number(eachNumber) >= 1 && Number(eachNumber) <= 45)
+      .length !== 6
+      ? 'RANGE'
+      : false;
+  }
 }
 
 module.exports = Lotto;
