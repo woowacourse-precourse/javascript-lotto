95c97eb3a1a25a259ad831ffca681f0b7c6aff85
turtle601 <turtle601@naver.com>
2022-11-11 10:29:58 +0900

    feat(LottoDrawFactory) 보너스 + 로또 중복 에러 처리 기능 구현

--- src/LottoDrawFactory.js
+++ src/LottoDrawFactory.js
@@ -1,7 +1,11 @@
+const { VARIABLE_LOTTO } = require('../utils/constants');
+
 class LottoDrawFactory {
-  constructor({ lotto, bunus }) {
+  constructor({ lotto, bonus }) {
     this.lotto = lotto;
-    this.bunus = bunus;
+    this.bonus = bonus;
+
+    this.#validate();
   }
 
   getNumber(type) {
@@ -9,10 +13,25 @@ class LottoDrawFactory {
       case 'lotto':
         return this.lotto.getNumber();
       case 'bonus':
-        return this.bunus.getNumber();
+        return this.bonus.getNumber();
       default:
         throw new Error('type을 명시해야합나디.');
     }
+  }
+
+  #validate() {
+    if (this.#validateOverlap()) {
+      throw new Error('[ERROR] 보너스 번호와 로또 번호가 중복입니다!!');
+    }
+
+    return this;
+  }
+
+  #validateOverlap() {
+    return (
+      new Set([...this.lotto.getNumber(), this.bonus.getNumber()]).size ===
+      VARIABLE_LOTTO.len
+    );
   }
 }
 
