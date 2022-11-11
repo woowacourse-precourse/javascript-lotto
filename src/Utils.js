4a050a220dfda5bd44baa3b2eabe77c64d5a316a
MAGHC <windowsapple95@gmail.com>
2022-11-11 20:32:36 +0900

    refactor: utils class로 변경

--- src/Utils.js
+++ src/Utils.js
@@ -1,18 +1,21 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
-const utils = {
-  getUserInput(query, callback) {
+class Utils {
+  static getUserInput(query, callback) {
     MissionUtils.Console.readLine(query, callback);
-  },
-  printMessage(message) {
-    MissionUtils.Console.print(message);
-  },
-  close() {
-    MissionUtils.Console.close();
-  },
-  generateRandomNumberArray(minRange, maxRange, length) {
-    return MissionUtils.Random.pickNumberInRange(minRange, maxRange, length);
-  },
-};
+  }
 
-module.exports = utils;
+  static printMessage(message) {
+    MissionUtils.Console.print(message);
+  }
+
+  static close() {
+    MissionUtils.Console.close();
+  }
+
+  static generateRandomNumberArray(minRange, maxRange, length) {
+    return MissionUtils.Random.pickNumberInRange(minRange, maxRange, length);
+  }
+}
+
+module.exports = Utils;
