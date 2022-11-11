2d64081a85ab7ea6fd3f318e3a869e129eebac6e
Narcoker <jykim9868@gmail.com>
2022-11-11 19:46:55 +0900

    Feat: 각 메세지별 개행 추가

--- src/Constants.js
+++ src/Constants.js
@@ -1,9 +1,9 @@
 const MESSAGE = Object.freeze({
-    ENTER_CASH: "구입금액을 입력해 주세요.",
+    ENTER_CASH: "구입금액을 입력해 주세요.\n",
 })
 
 const ERROR = Object.freeze({
-    INVAID_CACHE: "[ERROR] 구입 금액은 1000원 단위여야합니다."
+    INVAID_CACHE: "[ERROR] 구입 금액은 1000원 단위여야합니다.\n"
 });
 
 module.exports = { MESSAGE, ERROR };
