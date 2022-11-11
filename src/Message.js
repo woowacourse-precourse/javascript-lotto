a0894b70d5514a6fad1394684bac21e6e9ea5db1
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-12 00:35:52 +0900

    style: 공백 기준 4칸->2칸 수정

--- src/Message.js
+++ src/Message.js
@@ -1,12 +1,12 @@
 const MESSAGE = Object.freeze({
-    INPUT: {
-        PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
-        WINNING_NUMBER: '당첨 번호를 입력해 주세요.\n',
-        BONUS: '보너스 번호를 입력해 주세요.\n',
-    },
-    OUTPUT: {
-        PURCHASE_COUNT: '개를 구매했습니다.\n',
-    }
+  INPUT: {
+    PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
+    WINNING_NUMBER: '당첨 번호를 입력해 주세요.\n',
+    BONUS: '보너스 번호를 입력해 주세요.\n',
+  },
+  OUTPUT: {
+    PURCHASE_COUNT: '개를 구매했습니다.\n',
+  }
 });
 
 module.exports = MESSAGE;
