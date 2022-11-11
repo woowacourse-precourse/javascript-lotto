15554717a1e517d647af0f2cea3d1335736c242f
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-12 05:31:55 +0900

    feat: issue one lotto

---
+++ src/Lottos.js
@@ -0,0 +1,18 @@
+const Lotto = require('./Lotto');
+const MissionUtils = require('@woowacourse/mission-utils');
+
+class Lottos {
+  constructor(number) {
+    this.number = number;
+    this.lottos = [];
+  }
+
+  issue_one_lotto() {
+    let NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    const LOTTO = new Lotto(NUMBERS);
+    NUMBERS = LOTTO.order_lotto();
+    return NUMBERS;
+  }
+}
+
+module.exports = Lottos;
