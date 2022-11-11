bce376687d1b696f2b076f51a2ae3b5ad54cecb9
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-11 19:11:52 +0900

    feat: 기능 구현 예정 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 class App {
+  #winNumber;
+  #bonusNumber;
+  #myLottoList;
+
   play() {}
+
+  // 구입 금액 입력 메서드
+  buy() {
+    // 구입 금액 입력받기
+    // 입력받은 금액으로 getLottoQuantity호출
+    // 구매 수량에 맞게 로또 생성 메서드 호출
+  }
+
+  // 로또 수량만큼 로또 생성하는 메서드 (new Lotto())
+  makeLottery() {
+    // makeRandomLottoNumber() 호출
+  }
+
+  // 로또 결과 출력하기
+  myLotteryResult() {
+    // Lotto.checkMyLotteryRank() 사용
+  }
+
+  // 로또 당첨번호 생성
+  makeWinNumber() {
+    // 당첨번호 입력
+    // 보너스 넘버 입력
+  }
+
+  // 수익률 출력하기
+  printBenefitRate() {}
 }
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,32 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
+    // 문자열
     this.validate(numbers);
     this.#numbers = numbers;
+    this.printMyLottery();
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  printMyLottery() {
+    Console.print(this.#numbers);
+  }
+
+  // 내 로또번호와 당첨번호를 통한 등수확인 메서드 (안에는 당첨번호 들어가도록)
+  checkMyLotteryRank(answer) {
+    // 정답 번호 개수 리턴 유틸함수 호출
+    // 정답 번호 개수에 따른 등수 리턴
+    // 문자열로 반환.
+    return '등수';
+  }
 }
 
 module.exports = Lotto;
