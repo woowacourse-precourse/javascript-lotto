673f732fff3d857b84420a8d1a2988b80c6072b9
hwanfront <kim015jh@gmail.com>
2022-11-11 12:23:07 +0900

    test(LottoAmount): add LottoAmount validate test

---
+++ __tests__/LottoAmountTest.js
@@ -0,0 +1,41 @@
+const LottoAmount = require("../src/domain/lotto/LottoAmount");
+const LottoAmountDivideException = require("../src/exception/lotto/LottoAmountDivideException");
+const LottoAmountLessException = require("../src/exception/lotto/LottoAmountLessException");
+const LottoAmountNotNumberException = require("../src/exception/lotto/LottoAmountNotNumberException");
+
+describe("로또 클래스 테스트", () => {
+  test("로또 금액이 조건에 만족하는 경우", () => {
+    const amount = LottoAmount.from(1000).getValue();
+    expect(amount).toEqual(1000);
+  });
+
+  test("로또 금액이 숫자가 아닌 경우", () => {
+    expect(() => {
+      new LottoAmount('a');
+    }).toThrow(LottoAmountNotNumberException);
+    expect(() => {
+      new LottoAmount([1, 2, 3, 4, 5, 5]);
+    }).toThrow(LottoAmountNotNumberException);
+    expect(() => {
+      new LottoAmount();
+    }).toThrow(LottoAmountNotNumberException);
+  });
+  
+  test("로또 금액이 로또 가격보다 낮은 경우", () => {
+    expect(() => {
+      new LottoAmount(0);
+    }).toThrow(LottoAmountLessException);
+    expect(() => {
+      new LottoAmount(999);
+    }).toThrow(LottoAmountLessException);
+  });
+});
+
+test("로또 금액이 로또 가격으로 나누어지지 떨어지지 않는 경우", () => {
+  expect(() => {
+    new LottoAmount(1001);
+  }).toThrow(LottoAmountDivideException);
+  expect(() => {
+    new LottoAmount(12800);
+  }).toThrow(LottoAmountDivideException);
+});
