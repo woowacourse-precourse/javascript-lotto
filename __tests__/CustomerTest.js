d12f488d782217f4dbc615c468a6deedb76e809c
MyungAe <ladiescheong@naver.com>
2022-11-11 21:05:31 +0900

    test(Customer): pickUniqueNumbersInRange 함수를 대체할 mock 함수 생성 및 세팅

--- __tests__/CustomerTest.js
+++ __tests__/CustomerTest.js
@@ -1,8 +1,15 @@
 const Customer = require("../src/Customer");
+const LottoStore = require("../src/LottoStore");
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const mockQuestions = (answer) => {
   MissionUtils.Console.readLine = jest.fn().mockReturnValue(answer);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest
+    .fn()
+    .mockReturnValue(numbers);
 };
 
 // dummy test codes
@@ -14,6 +21,7 @@ const mockQuestions = (answer) => {
 
 describe("Feat 1. Customer.buyLotto", () => {
   const customer = new Customer();
+
   test("입력된 금액이 천원으로 나누어 떨어진다.", () => {
     mockQuestions("8000");
     expect(customer.payMoney()).toBe(8000);
@@ -25,12 +33,15 @@ describe("Feat 1. Customer.buyLotto", () => {
   });
 });
 
-describe("Feat 2. LottoStore.generateLottoNumber", () => {
+describe("Feat 2. LottoStore.generateLotto", () => {
   const lottoStore = new LottoStore();
-  test("지불된 돈으로 살 수 있는 복권의 개수를 반환한다.", () => {
+
+  test("지불된 돈으로 살 수 있는 로또의 개수를 반환한다.", () => {
     expect(lottoStore.calculateLottoCount(8000)).toBe(8);
   });
-  test("살 수 있는 복권의 개수만큼 만든 복권 번호를 반환한다.", () => {
-    expect(lottoStore.generateLotto(8).length).toBe(8);
+
+  test("살 수 있는 로또의 개수만큼 만든 로또 번호를 반환한다.", () => {
+    mockRandoms([1, 2, 3, 4, 5, 6]);
+    expect(lottoStore.generateLottoNumber(1).length).toBe(1);
   });
 });
