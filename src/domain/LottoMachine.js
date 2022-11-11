bd185e727e9ec7d7abeb8c0d535ac23c4a7964ec
yong <tkddbs1644@gmail.com>
2022-11-11 22:45:38 +0900

    feat(LottoMachine): 로또 구매 기능 구현

--- src/domain/LottoMachine.js
+++ src/domain/LottoMachine.js
@@ -1,3 +1,36 @@
-class LottoMachine {}
+const LottoGenerator = require("./LottoGenerator");
+
+const MONEY_UNIT = 1000;
+
+const ERRORS = Object.freeze({
+  RANGE: "[ERROR] 1000원 이상 입력해주세요.",
+  NOT_DIVIDE: "[ERROR] 1000원 단위로 입력해주세요.",
+});
+
+class LottoMachine {
+  buy(money) {
+    this.validate(money);
+    const amount = Math.floor(money / MONEY_UNIT);
+    return Array.from({ length: amount }, LottoGenerator.generate);
+  }
+
+  validate(money) {
+    this.isValidRange(money);
+    this.isDiv(money);
+  }
+
+  isValidRange(money) {
+    if (money < MONEY_UNIT) {
+      throw new Error(ERRORS.RANGE);
+    }
+  }
+
+  isDiv(money) {
+    const remainder = money % MONEY_UNIT;
+    if (remainder !== 0) {
+      throw new Error(ERRORS.NOT_DIVIDE);
+    }
+  }
+}
 
 module.exports = LottoMachine;
