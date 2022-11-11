36cc62263f7eb637761aeb1eed7d224cb9c2c5b4
hwanfront <kim015jh@gmail.com>
2022-11-11 17:19:13 +0900

    feat(Lotto): 로또에 대한 예외처리 추가

--- src/domain/lotto/Lotto.js
+++ src/domain/lotto/Lotto.js
@@ -1,4 +1,5 @@
-const LottoNumberRangeException = require('../../exception/lotto/LottoNumberRangeException');
+const LottoHasDuplicatedException = require('../../exception/lotto/LottoHasDuplicatedException');
+const LottoLengthException = require('../../exception/lotto/LottoLengthException');
 
 class Lotto {
   static SIZE = 6;
@@ -6,13 +7,16 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    Lotto.validate(numbers);
     this.#numbers = numbers;
   }
 
   static validate(numbers) {
     if (numbers.length !== 6) {
-      throw new LottoNumberRangeException(numbers.length);
+      throw new LottoLengthException(numbers.length);
+    }
+    if (new Set(numbers).size !== 6) {
+      throw new LottoHasDuplicatedException();
     }
   }
   // TODO: 추가 기능 구현
@@ -26,7 +30,7 @@ class Lotto {
   }
 
   includes(number) {
-    
+
   }
 }
 
