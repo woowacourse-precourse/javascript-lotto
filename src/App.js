c9a7f9a2abb78a7bac98da7dcea1d07889f8e9a4
이건우 <lifeflavor@igeon-uui-MacBookPro.local>
2022-11-11 22:36:54 +0900

    feat(Lotto)- 당첨 내역 및 수익률을 출력

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ const AnswerLotto = require("./modules/AnswerLotto");
 const CorrectLotto = require("./modules/CorrectLotto");
 const Award = require("./modules/Award");
 const Print = require("./modules/Print");
+const Margin = require("./modules/Margin");
 
 class App {
   play() {
     //로또 구입 금액을 입력하면 해당하는 로또를 발행한다.
     let nTime = 0;
+    let nTenWon = 0;
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (price) => {
+      nTenWon = price;
       const BUYLOTTO = new BuyLotto(price);
       nTime = BUYLOTTO.nTimes();
       MissionUtils.Console.print(`${nTime}개를 구매했습니다.`);
     });
+
     //로또 번호 1~45까지의 서로 다른 임의의 수 6자리를 뽑는다.
     const USERLOTTO = new UserLotto();
     const lottoArr = USERLOTTO.haveLotto(nTime);
@@ -25,18 +29,26 @@ class App {
     const ANSWERLOTTO = new AnswerLotto();
     const answer = ANSWERLOTTO.haveAnswer();
     const bonus = ANSWERLOTTO.bonusAnswer();
+
     // 로또 번호와 당첨 번호를 비교한다.
     const CORRECT = new CorrectLotto(lottoArr, answer, bonus);
     const correctArr = CORRECT.haveCorrect();
+
     // 몇개 일치하는 지에 따라 상금을 정한다.
     const AWARD = new Award(correctArr, CORRECT.haveBonus());
-    const price = AWARD.haveAward();
+    const PROFIT = AWARD.haveAward();
+
     // 상금 개수와 일치 개수에 따라 출력한다.
     const PRINT = new Print();
-    const object = PRINT.haveObject(correctArr);
+    PRINT.haveObject(correctArr);
     PRINT.doPrint();
-    // 로또 게임을 종료한다.
-    MissionUtils.Console.close();
+
+    //수익률 계산
+    const MARGIN = new Margin();
+    MARGIN.haveMargin(nTenWon, PROFIT);
+
+    //종료
+    return MissionUtils.Console.close();
   }
 }
 
