c1771ec94a0fc76f724bad11d347711f8ad9fda4
2yunseong <dbsdltjd3701@naver.com>
2022-11-11 21:05:37 +0900

    feat(vaildator): 로또의 길이(개수)가 올바른지 확인하는 기능

--- src/Vaildator.js
+++ src/Vaildator.js
@@ -1,5 +1,11 @@
+const util = require('./util/util');
+
 class Vaildator {
   static isRangeIn(start, end, target) {
     return start <= target && target <= end;
+  }
+
+  static isLottoLength(target) {
+    return target === util.LOTTO_MAX_LENGTH;
   }
 }
