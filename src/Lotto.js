60a44bacd2d625013e98487168f75acf2d4d25c5
yeonsuBeak <soothbugger@gmail.com>
2022-11-11 19:12:28 +0900

    feat(lotto): 당첨 번호가 숫자가 아닌 경우 예외 처리 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,7 +7,18 @@ class Lotto {
   }
 
   validate(numbers) {
+    this.checkNumber(numbers);
     this.checkLength(numbers);
+  }
+
+  checkNumber(numbers) {
+    const invalidList = numbers.filter((number) => {
+      return Number.isNaN(number);
+      // Error: 숫자+숫자아닌값 조합인 경우 예외 처리가 되지 않음.
+    });
+    if (invalidList.length > 0) {
+      throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+    }
   }
 
   checkLength(numbers) {
