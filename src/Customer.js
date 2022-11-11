9a9431f340e2344ab7700da3d101f2b204e4c753
MyungAe <ladiescheong@naver.com>
2022-11-11 20:12:53 +0900

    feat(Customer): get, set 함수 추가 및 메소드 명 변경

--- src/Customer.js
+++ src/Customer.js
@@ -1,16 +1,31 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class Customer {
-  buyLotto() {
+  #lottoInstance;
+
+  constructor(lottoInstance) {
+    this.lottoInstance = lottoInstance;
+  }
+
+  set buyLotto(lottos) {
+    this.lottoInstance = lottos;
+  }
+
+  get showLotto() {
+    return this.lottoInstance;
+  }
+
+  payMoney() {
     const input = MissionUtils.Console.readLine(
       "구입금액을 입력해 주세요.\n",
-      (value) => {
-        return value;
+      (money) => {
+        return money;
       }
     );
     const inputInt = parseInt(input);
-    if (inputInt % 1000 !== 0) throw new Error("[ERROR]");
-    return inputInt / 1000;
+    if (inputInt % 1000 !== 0)
+      throw new Error("[ERROR] 구입 금액이 천원 단위가 아닙니다");
+    return inputInt;
   }
 }
 
