1fb4d8a257a795f0a01d0db88b70357be47f42f4
박정규 <srk99279@naver.com>
2022-11-11 23:37:36 +0900

    refactor : Lotto클래스에서 Bonus클래스 분리
    단위 테스트를 위해 Lotto클래스에서 Bonus클래스 분리.

---
+++ src/Bonus.js
@@ -0,0 +1,60 @@
+const Statistic = require("./Statistic");
+const ValidateBonus = require("./ValidateBonus");
+const { MIN_MATCH, MAX_MATCH } = require("./constants");
+
+class Bonus {
+  #numbers;
+  #bonusNumber;
+  #winResult;
+  #lotteries;
+
+  constructor(numbers, bonusNumber, lotteries) {
+    ValidateBonus(numbers, bonusNumber);
+    this.#numbers = numbers;
+    this.#bonusNumber = bonusNumber;
+    this.#winResult = this.setWinResult();
+    this.#lotteries = lotteries;
+  }
+
+  setWinResult() {
+    let winResult = new Map();
+    for (let matchCnt = MIN_MATCH; matchCnt <= MAX_MATCH; matchCnt++) {
+      winResult.set(matchCnt, 0);
+    }
+    return winResult;
+  }
+
+  iterateLotteries() {
+    this.#lotteries.forEach((lottery) => {
+      this.checkLottery(lottery);
+    });
+    const statistic = new Statistic(this.#winResult, this.#lotteries.length);
+    statistic.printStatistic();
+  }
+
+  checkLottery(lottery) {
+    let matchCount = 0;
+    this.#numbers.forEach((number) => {
+      if (lottery.includes(number)) {
+        matchCount += 1;
+      }
+    });
+    this.calcMatchCount(matchCount);
+  }
+
+  calcMatchCount(matchCount) {
+    if (matchCount == 6) matchCount += 1;
+    if (matchCount == 5) this.checkBonus(lottery) ? (matchCount += 1) : "";
+    if (matchCount > 2) this.makeWinResult(matchCount);
+  }
+
+  checkBonus(lottery) {
+    return lottery.includes(+this.#bonusNumber);
+  }
+
+  makeWinResult(result) {
+    this.#winResult.set(result, this.#winResult.get(result) + 1);
+  }
+}
+
+module.exports = Bonus;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,8 +1,6 @@
 const { Console } = require("@woowacourse/mission-utils");
-const ValidateBonus = require("./ValidateBonus");
-const Statistic = require("./Statistic");
 const ValidateLotto = require("./ValidateLotto");
-const { MIN_MATCH, MAX_MATCH } = require("./constants");
+const Bonus = require("./Bonus");
 
 class Lotto {
   #numbers;
@@ -10,60 +8,21 @@ class Lotto {
   constructor(numbers, lotteries) {
     this.validate(numbers);
     this.#numbers = numbers;
-    let winResult = this.setWinResult();
-    this.inputBonusNumber(winResult, lotteries);
+    this.inputBonusNumber(lotteries);
   }
 
   validate(numbers) {
     ValidateLotto(numbers);
   }
 
-  setWinResult() {
-    let winResult = new Map();
-    for (let matchCnt = MIN_MATCH; matchCnt <= MAX_MATCH; matchCnt++) {
-      winResult.set(matchCnt, 0);
-    }
-    return winResult;
-  }
-
-  inputBonusNumber(winResult, lotteries) {
+  inputBonusNumber(lotteries) {
     let bonusNumber;
     Console.readLine("보너스 번호를 입력해 주세요,\n", (number) => {
       bonusNumber = number;
 
-      ValidateBonus(this.#numbers, bonusNumber);
-      this.iterateLotteries(lotteries, winResult, bonusNumber);
-
-      const statistic = new Statistic(winResult, lotteries.length);
-      statistic.printStatistic();
+      const bonus = new Bonus(this.#numbers, bonusNumber, lotteries);
+      bonus.iterateLotteries();
     });
-  }
-
-  iterateLotteries(lotteries, winResult, bonusNumber) {
-    lotteries.forEach((lottery) => {
-      this.checkLottery(lottery, winResult, bonusNumber);
-    });
-  }
-
-  checkLottery(lottery, winResult, bonusNumber) {
-    let matchCount = 0;
-    this.#numbers.forEach((number) => {
-      if (lottery.includes(number)) {
-        matchCount += 1;
-      }
-    });
-    if (matchCount == 6) matchCount += 1;
-    if (matchCount == 5)
-      this.checkBonus(lottery, bonusNumber) ? (matchCount += 1) : "";
-    if (matchCount > 2) this.makeWinResult(matchCount, winResult);
-  }
-
-  checkBonus(lottery, bonusNumber) {
-    return lottery.includes(+bonusNumber);
-  }
-
-  makeWinResult(result, winResult) {
-    winResult.set(result, winResult.get(result) + 1);
   }
 }
 
