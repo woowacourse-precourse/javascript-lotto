67ab0cc9672f0e9f0ee018ec63c05df930639f15
An Sol <apdlvmf486@gmail.com>
2022-11-11 09:28:01 +0900

    refactor: 로또 번호 유효성 검증과 사용자 입력 유효성 검증을 분리

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,29 +3,16 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
+    this.validate(numbers);
     this.#numbers = numbers;
   }
 
-  isMoneyValid(money) {
-    if (isNaN(money)) throw new Error("[ERROR] 숫자를 입력해 주세요.");
-    if (!/[0-9]{4,}/.test(money)) throw new Error("[ERROR] 4자리 이상의 숫자를 입력해 주세요.");
-    if (parseInt(money) % 1000 !== 0) throw new Error("[ERROR] 1000원 단위의 금액을 입력해 주세요.");
-    return true;
-  }
-
-  isLottoNumberInputValid() {
-    const NUMBERS_FROM_INPUT = this.#numbers.split(",");
-    if (/[^\d,]/g.test(this.#numbers)) throw new Error("[ERROR] 숫자+',' 형태로 입력해 주세요. ex) 1,2,3,4,5,6");
-    if (NUMBERS_FROM_INPUT.length !== 6) throw new Error("[ERROR] 중복되는 숫자가 있습니다.");
-    if (NUMBERS_FROM_INPUT.find((arrayElement) => parseInt(arrayElement) < 1 || parseInt(arrayElement) > 45))
+  validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    if (numbers.find((arrayElement) => parseInt(arrayElement) < 1 || parseInt(arrayElement) > 45))
       throw new Error("[ERROR] 숫자는 1-45 사이의 수여야 합니다.");
-    return true;
-  }
-
-  isBonusNumberValid(bonusNumber) {
-    if (+bonusNumber < 1 || +bonusNumber > 45) throw new Error("[ERROR] 보너스 숫자는 1-45 사이의 수여야 합니다.");
-    if (numbers.includes(bonusNumber)) throw new Error("[ERROR] 이전에 입력한 번호가 보너스 숫자와 중복됩니다.");
-    return true;
   }
 }
 
---
+++ src/inputCheck.js
@@ -0,0 +1,18 @@
+class ValidationCheck {
+  isMoneyValid(money) {
+    if (isNaN(money)) throw new Error("[ERROR] 숫자를 입력해 주세요.");
+    if (!/[0-9]{4,}/.test(money)) throw new Error("[ERROR] 4자리 이상의 숫자를 입력해 주세요.");
+    if (parseInt(money) % 1000 !== 0) throw new Error("[ERROR] 1000원 단위의 금액을 입력해 주세요.");
+    return true;
+  }
+
+  isBonusNumberValid(bonusNumber) {
+    if (+bonusNumber < 1 || +bonusNumber > 45) throw new Error("[ERROR] 보너스 숫자는 1-45 사이의 수여야 합니다.");
+    if (numbers.includes(bonusNumber)) throw new Error("[ERROR] 이전에 입력한 번호가 보너스 숫자와 중복됩니다.");
+    return true;
+  }
+
+  isWinningNumberValid(winningNumber) {
+    if (/[^\d,]/g.test(this.#numbers)) throw new Error("[ERROR] 숫자+',' 형태로 입력해 주세요. ex) 1,2,3,4,5,6");
+  }
+}
