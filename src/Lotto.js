a575031989272d401a100672324ddd23ee3b274e
c0c0pang <coc1789@naver.com>
2022-11-11 22:12:24 +0900

    refactor: Lotto class 함수 위치 변경 및 필드 위치 변경

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,39 +1,42 @@
 class Lotto {
   #numbers;
-  constructor(numbers) {
-    this.validate(numbers);
-    this.overlap(numbers);
-    this.rangeCheck(numbers);
-    this.letterCheck(numbers);
-    this.#numbers = numbers;
-  };
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
+  constructor(numbers) {
+    this.#numbers = numbers;
+    this.validate(numbers);
+  };
+  validate(winning) {
+    this.lengthCheck(winning);
+    this.overlapCheck(winning);
+    this.rangeCheck(winning);
+    this.letterCheck(winning);
+  };
+  lengthCheck(winning) {
+    if (winning.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     };
   };
-
-  overlap(numbers) {
-    this.#numbers = new Set(numbers);
-    if (this.#numbers.size !== numbers.length) {
+  overlapCheck(winning) {
+    const setWinning = new Set(this.#numbers);
+    if (setWinning.size !== winning.length) {
       throw new Error("[ERROR] 로또 번호는 중복이 없습니다.");
     };
   };
-
-  rangeCheck(numbers) {
-    for(let index = 0 ;index<numbers.length;index++){
-      if(!(numbers[index]>=1 && numbers[index]<=45)){
+  rangeCheck(winning) {
+    for (let index = 0; index < winning.length; index++) {
+      if (!(winning[index] >= 1 && winning[index] <= 45)) {
         throw new Error("[ERROR] 로또 번호는 숫자는 1~45 까지 입니다.");
       };
     };
   };
-  letterCheck(numbers) {
-    this.#numbers = String(numbers).replace(/[^0-9]/g, '');
-    if (this.#numbers.length !== 6) {
+  letterCheck(winning) {
+    const regex = /[^0-9]/g;
+    const result = String(this.#numbers).replace(regex, '');
+    if (result.length !== winning.join("").length) {
       throw new Error("[ERROR] 로또 번호는 숫자만 입력해야 합니다.");
     };
   };
+
   // TODO: 추가 기능 구현
 };
 
