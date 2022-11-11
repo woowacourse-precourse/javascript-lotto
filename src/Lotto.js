3e3d3289fee8ed79c281983aa1d8b77a31a01585
Vo9Ming <kimminj2017@gmail.com>
2022-11-11 13:42:00 +0900

    [기능 구현 목록 내용추가] 사용자에게 로또 구입금액을 입력받는 inputAmount()함수를  생성한다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,29 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.inputAmount();
+    //this.validate(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
+  //validate(numbers) {
+  //  if (numbers.length !== 6) {
+  //    throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  //  }
+  //}
 
   // TODO: 추가 기능 구현
+  inputAmount(numbers) {
+    MissionUtils.Console.readLine("금액을 입력해주세요.", (answer) => {
+      console.log(`입력한 금액: ${answer}`);
+      MissionUtils.Console.close();
+    });
+  }
 }
 
+const lotto = new Lotto();
+lotto.inputAmount();
 module.exports = Lotto;
