1443ee804833d0fb88e6876c8d3c87d676ff187d
박정규 <srk99279@naver.com>
2022-11-11 10:56:27 +0900

    refactor : 필드 삭제
    제한 조건 : Lotto 클래스 내 필드는 numbers 하나.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,58 +1,74 @@
 const { Console } = require("@woowacourse/mission-utils");
 const Statistic = require("./Statistic");
+const { MIN_MATCH, MAX_MATCH } = require("./constants");
 
 class Lotto {
   #numbers;
-  #lotteries;
-  #bonusNumber;
-  #winResult = new Map();
 
   constructor(numbers, lotteries) {
     this.validate(numbers);
     this.#numbers = numbers;
-    this.#lotteries = lotteries;
-    this.inputBonusNumber();
+    let winResult = this.setWinResult();
+    this.inputBonusNumber(winResult, lotteries);
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    let idx = 0;
+    while (idx < 6) {
+      if (numbers.indexOf(numbers[idx]) != numbers.lastIndexOf(numbers[idx])) {
+        throw new Error("[ERROR] 로또 번호는 중복되어서는 안 됩니다.");
+      }
+      idx += 1;
+    }
   }
 
-  inputBonusNumber() {
-    Console.readLine("\n보너스 번호를 입력해 주세요,\n", (number) => {
-      this.#bonusNumber = number;
-      this.iterateLotteries();
-      const statistic = new Statistic(this.#winResult);
+  setWinResult() {
+    let winResult = new Map();
+    for (let matchCnt = MIN_MATCH; matchCnt <= MAX_MATCH; matchCnt++) {
+      winResult.set(matchCnt, 0);
+    }
+    return winResult;
+  }
+
+  inputBonusNumber(winResult, lotteries) {
+    let bonusNumber;
+    Console.readLine("보너스 번호를 입력해 주세요,\n", (number) => {
+      bonusNumber = number;
+      this.iterateLotteries(lotteries, winResult, bonusNumber);
+
+      const statistic = new Statistic(winResult, lotteries.length);
+      statistic.printStatistic();
     });
   }
 
-  iterateLotteries() {
-    this.#lotteries.forEach((lottery) => {
-      this.checkLottery(lottery);
+  iterateLotteries(lotteries, winResult, bonusNumber) {
+    lotteries.forEach((lottery) => {
+      this.checkLottery(lottery, winResult, bonusNumber);
     });
   }
 
-  checkLottery(lottery) {
+  checkLottery(lottery, winResult, bonusNumber) {
     let matchCount = 0;
     this.#numbers.forEach((number) => {
       if (lottery.includes(number)) {
         matchCount += 1;
       }
     });
-    if (matchCount == 5) this.checkBonus(lottery) ? (matchCount += 2) : "";
-    this.makeWinResult(matchCount);
+    if (matchCount == 6) matchCount += 1;
+    if (matchCount == 5)
+      this.checkBonus(lottery, bonusNumber) ? (matchCount += 1) : "";
+    if (matchCount > 2) this.makeWinResult(matchCount, winResult);
   }
 
-  checkBonus(lottery) {
-    return lottery.includes(+this.#bonusNumber);
+  checkBonus(lottery, bonusNumber) {
+    return lottery.includes(+bonusNumber);
   }
 
-  makeWinResult(result) {
-    this.#winResult.get(result)
-      ? this.#winResult.set(result, this.#winResult.get(result) + 1)
-      : this.#winResult.set(result, 1);
+  makeWinResult(result, winResult) {
+    winResult.set(result, winResult.get(result) + 1);
   }
 }
 
