4392c4d156c27b696901a159241f5274624ae43c
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-11 22:30:33 +0900

    test(winningNumbers): 입력받은 당첨 번호에 로또 숫자의 범위를 벗어나는 번호가 있다면 에러를 반환하는 테스트

--- __tests__/WinningNumbersTest.js
+++ __tests__/WinningNumbersTest.js
@@ -33,4 +33,12 @@ describe('입력받은 당첨 번호가 유효한지 확인하는 테스트', ()
       app.play();
     }).toThrow('[ERROR]');
   });
+
+  test('사용자가 입력한 당첨 번호가 로또 숫자의 범위가 아닌 상황이라면 에러를 발생시킨다.', () => {
+    mockQuestions(['1000', '1,2,3,4,5,46']);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow('[ERROR]');
+  });
 });
