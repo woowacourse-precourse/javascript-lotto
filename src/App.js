de1d5fd319ded481ca064572e117f80acbbe0917
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-11 19:59:58 +0900

    feat: 로또 구입 금액 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const { Console } = require('@woowacourse/mission-utils');
+const UserBudget = require('./UserBudget');
+const INPUT_BUDGET = '구입금액을 입력해 주세요.';
+
 class App {
-  play() {}
+  budget;
+
+  play() {
+    this.getUserBudget(INPUT_BUDGET, this.validateUserBudget);
+  }
+
+  getUserBudget(INPUT_BUDGET, validateUserBudget) {
+    Console.readLine(INPUT_BUDGET, validateUserBudget);
+  }
+
+  validateUserBudget(input) {
+    this.budget = new UserBudget(Number(input));
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
