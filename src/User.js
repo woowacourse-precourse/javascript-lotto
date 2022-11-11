31dc7ce797bdc242a0a878a54a9ec81d830cd291
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-12 03:40:36 +0900

    refactor: delete unnecessary lines

--- src/User.js
+++ src/User.js
@@ -20,16 +20,13 @@ class User {
 
   input_amount() {
     MissionUtils.Console.readLine('구입 금액을 입력해 주세요\n', amount => {
-      //입력값 확인
       const VALIDATION = this.check_validation(amount);
-      //예외처리
       if (!VALIDATION) {
         throw new Error('[ERROR] 구입 금액은 1000의 단위 숫자여야 합니다.');
       }
       const COUNT = amount / 1000;
       this.setAmount(amount);
       return MissionUtils.Console.print(`\n${COUNT}개를 구매했습니다.`);
-      //예외가 아닐 경우 -> 몇개 구매한건지 계산
     });
   }
 
@@ -49,7 +46,6 @@ class User {
           throw new Error('[Error] 롯또 번호는 1~45 사이의 숫자입니다.');
         }
         this.setWinningNumber(winning_number);
-        console.log(this.winning_number);
       },
     );
   }
@@ -89,11 +85,3 @@ class User {
 module.exports = User;
 const user = new User();
 user.input_winning_number();
-
-// User 클래스
-//  로또 구입 금액 입력 (O)
-//  예외처리 (1) 구입 금액 입력값이 1000으로 나누어 떨어지지 않으면 예외처리 (O)
-//  당첨번호 입력(O)
-//  보너스번호 입력(O)
-//  예외처리 (2) 당첨/보너스 번호 입력값이 1 ~ 45 사이의 숫자 6개가 아닌 경우 예외 처리(O)
-//  예외처리 시, [ERROR]로 시작하는 에러 메시지 출력(O)
