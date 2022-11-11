e90125f8443cd6529313167685f60383ef548a99
ksg2388 <ksg2388@naver.com>
2022-11-11 16:25:10 +0900

    feat(Lotto): 당첨 번호 입력시 유효성 검증

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,8 +7,16 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    const winningNumber = numbers.split(",").map((num) => Number(num));
+
+    if (winningNumber.length !== 6) {
+      throw new Error("[ERROR] 잘못된 입력입니다.");
+    }
+    if (winningNumber.filter((num) => num >= 1 && num <= 45).length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+    if (new Set(winningNumber).size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
   }
 
