d36adcc84817675e80fefc50a9e16b040df2b7bc
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-11-11 21:02:33 +0900

    Test(feature No.2): add code to cehck the correct number of arrays
    cehck the correct number of arrays to match the price

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,15 +15,21 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
-  test.only("금액이 숫자이면서 1000의 배수가 아닌 경우 예외가 발생한다.", () => {
+  test("금액이 숫자이면서 1000의 배수가 아닌 경우 예외가 발생한다.", () => {
     expect(() => {
       Lotto.checkMoney("1001");
     }).toThrow("[ERROR] 금액은 1000의 배수인 숫자이여야 합니다.");
   });
 
-  test.only("금액이 숫자이면서 1000의 배수가 아닌 경우 예외가 발생한다.", () => {
+  test("금액이 숫자이면서 1000의 배수가 아닌 경우 예외가 발생한다.", () => {
     expect(() => {
       Lotto.checkMoney("문자열");
     }).toThrow("[ERROR] 금액은 1000의 배수인 숫자이여야 합니다.");
+  });
+
+  test.only("입력받은 금액/로또가격 만큼의 배열이 반환된다", () => {
+    const money = 3000;
+    const price = 1000;
+    expect(() => Lotto.genLottoNumArr(money).length.toEqual(money / price));
   });
 });
