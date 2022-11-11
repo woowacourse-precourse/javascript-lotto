99e99ca29a26ede0a028d78f7b3a41f3ff9aaeea
Buyoung Chung <sunarol@naver.com>
2022-11-11 19:47:05 +0900

    feat: winning number and bonus number input and save

--- src/Lotto.js
+++ src/Lotto.js
@@ -14,11 +14,19 @@ class Lotto {
     this.autoLotto = new AutoLotto();
   }
 
-  lottoPurchase() {
-    View.input(Message.ASK_BUDGET, this.calculateHowManyLottoandPrint.bind(this));
+  // Entry
+  lottoPlay() {
+    View.input(Message.ASK_BUDGET, this.lottoPlayCallback.bind(this));
   }
 
-  calculateHowManyLottoandPrint(input) {
+  // Entry Callback
+  lottoPlayCallback(input) {
+    this.lottoPurchase(input);
+    this.winningNumberandBonusNumber();
+  }
+
+  // 로또 구매
+  lottoPurchase(input) {
     this.saveBudget(input);
     this.printHowManyLotto(Message.returnHowManyLotto(this.budget.returnCount()));
     this.makeAutoLottoandPrint(this.budget.count);
@@ -45,6 +53,28 @@ class Lotto {
     this.autoLotto.randomLottoArray.forEach((array) => {
       View.output(array);
     });
+  }
+
+  // 당첨 번호, 보너스 번호
+  winningNumberandBonusNumber() {
+    this.inputWinningNumbers();
+  }
+
+  inputWinningNumbers() {
+    View.input(Message.ASK_WINNING_NUMBER, this.saveWinningNumbers.bind(this));
+  }
+
+  saveWinningNumbers(input) {
+    this.numbers = input.split(",").map((number) => parseInt(number));
+    this.inputBonusNumber();
+  }
+
+  inputBonusNumber() {
+    View.input(Message.ASK_BONUS_NUMBER, this.saveBonusNumber.bind(this));
+  }
+
+  saveBonusNumber(input) {
+    this.bonusNumber = parseInt(input);
   }
 }
 
