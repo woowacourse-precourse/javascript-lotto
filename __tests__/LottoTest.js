7be5f57e6a88ae92aa83526749e3c9cd0a8d2e35
daal2quf <daal2quf@gmail.com>
2022-11-11 21:21:04 +0900

    test(lotto): makeSixNumbers() 기능 테스트
    add:
    test1 : makeSixNumbers()로 만들어진 배열의 길이가 6 인지 확인
    test2 : Set을 이용해 중복값이 있을때에 size가 달라짐을 비교해 중복이 없는지 확인

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,24 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
+});
 
-  // 아래에 추가 테스트 작성 가능
+describe("Lotto 클래스의 makeSixNumbers() 기능 테스트", () => {
+  test("6자리로 만들어지는지 확인", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const numbersLength = lotto.makeSixNumbers().length;
+    expect(numbersLength).toBe(6);
+  });
+
+  test("6자리 숫자에 중복값은 없는지 확인", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const sixNumbers = lotto.makeSixNumbers();
+    const noDuplicateNumber = [...new Set(sixNumbers)];
+    expect(sixNumbers.length).toEqual(noDuplicateNumber.length);
+  });
 });
