3af08c6bd48a6c4ad90973ce77c287f3c607a2fa
d0422 <rlfehd2013@naver.com>
2022-11-12 02:07:21 +0900

    Feat: 로또번호가 중복되는 경우 error를 발생시키는 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,8 +11,23 @@ class Lotto {
   }
 
   validate(numbers) {
+    this.checkLength(numbers);
+    this.checkDuplicate(numbers);
+  }
+
+  checkLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+  }
+
+  checkDuplicate(numbers) {
+    const set = new Set();
+    numbers.forEach((number) => {
+      set.add(number);
+    });
+    if (set.size != 6) {
+      throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다. ');
     }
   }
 
