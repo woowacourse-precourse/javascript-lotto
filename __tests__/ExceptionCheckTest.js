2e207578e9d587d952000aae3160995215261d43
daal2quf <daal2quf@gmail.com>
2022-11-13 05:05:47 +0900

    fix typo: 오타수정
    change:
    ExeptionCheck, exeptionCheck을 ExceptionCheck, exceptionCheck으로 변경

--- __tests__/ExceptionCheckTest.js
+++ __tests__/ExceptionCheckTest.js
@@ -1,5 +1,5 @@
 const App = require("../src/App");
-const ExeptionCheck = require("../src/ExeptionCheck");
+const ExceptionCheck = require("../src/ExceptionCheck");
 const { Console } = require("@woowacourse/mission-utils");
 
 afterEach(() => {
@@ -8,14 +8,14 @@ afterEach(() => {
 
 describe("App 클래스의 예외 처리 테스트", () => {
   test("유저 입력값이 1,000단위가 아닐때 예외를 throw 하는지 확인", () => {
-    const exeptionCheck = new ExeptionCheck();
+    const exceptionCheck = new ExceptionCheck();
     const moneyValue = 1100;
-    expect(() => { exeptionCheck.userInputMoneyValue(moneyValue); }).toThrow();
+    expect(() => { exceptionCheck.userInputMoneyValue(moneyValue); }).toThrow();
   });
 
   test("예외 처리 발생시에 [ERROR]문구가 메세지의 맨앞에 있는지 확인", () => {
-    const exeptionCheck = new ExeptionCheck();
+    const exceptionCheck = new ExceptionCheck();
     const moneyValue = 1100;
-    expect(() => { exeptionCheck.userInputMoneyValue(moneyValue); }).toThrowError(/^\[ERROR\]/);
+    expect(() => { exceptionCheck.userInputMoneyValue(moneyValue); }).toThrowError(/^\[ERROR\]/);
   });
 });
