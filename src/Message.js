bec56810fc8285f59a0b5a1bca815f545bfb7884
Buyoung Chung <sunarol@naver.com>
2022-11-12 16:58:04 +0900

    feat: Add newline to Messages

--- src/Message.js
+++ src/Message.js
@@ -1,12 +1,14 @@
 class Message {
   static ASK_BUDGET = "구입금액을 입력해 주세요.\n";
 
-  static ASK_WINNING_NUMBER = "당첨 번호를 입력해 주세요.\n";
+  static ASK_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.\n";
 
-  static ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.\n";
+  static ASK_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.\n";
+
+  static WINNING_STATS = "\n당첨통계\n---";
 
   static returnHowManyLotto(count) {
-    return `${count}개를 구매했습니다.`;
+    return `\n${count}개를 구매했습니다.`;
   }
 }
 
