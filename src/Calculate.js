334f90d65d9f2a4e3a6d11e4a7b3a16b8a65eadf
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-12 16:20:31 +0900

    feat: calculate winning number, check bonus number in every lotto

--- src/Calculate.js
+++ src/Calculate.js
@@ -27,6 +27,17 @@ class Calculate {
     if (lotto.includes(bonus)) count++;
     return count;
   }
+
+  //계산해서 [[winning갯수, 보너스 일치 여부],[winning갯수, 보너스 일치 여부],[winning갯수, 보너스 일치 여부] ... ] 형식으로 return
+  calculate_lottos(lottos, winning, bonus) {
+    const CALCULATE = [];
+    lottos.map(lotto => {
+      const WIN_NUM = this.count_number_of_wins(winning, lotto);
+      const BONUS_CHECK = this.check_bonus(bonus, lotto);
+      CALCULATE.push([WIN_NUM, BONUS_CHECK]);
+    });
+    return CALCULATE;
+  }
 }
 
 module.exports = Calculate;
