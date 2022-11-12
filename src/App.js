af61caf51de7db856bee2d3568da08e71a0e2035
chee9835 <huisu121@gmail.com>
2022-11-12 21:35:03 +0900

    feat: 로또 구입 금액 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSEGE, ERROR } = require("./constant/lotto");
+const Utils = require("./utils/utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.utils = new Utils();
+  }
+
+  play() {
+    this.getPurchaseAmout();
+  }
+
+  getPurchaseAmout() {
+    Console.readLine(MESSEGE.INPUT_PURCHASE_AMOUNT, (input) => {
+      if (this.isNotDividedByThousand(input))
+        this.utils.throwError(ERROR.NOT_DIVIDED_BY_THOUSAND);
+    });
+  }
+
+  isNotDividedByThousand(input) {
+    return input % 1000 !== 0;
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/constant/lotto.js
+++ src/constant/lotto.js
@@ -3,9 +3,15 @@ const LOTTO_LENGTH = 6;
 const ERROR = {
   NOT_SIX_NUMBERS: "로또 번호는 6개여야 합니다.",
   DUPLICATE_NUMBERS: "중복된 숫자가 있으면 안 됩니다.",
+  NOT_DIVIDED_BY_THOUSAND: "1000원 단위로 입력해야합니다.",
+};
+
+const MESSEGE = {
+  INPUT_PURCHASE_AMOUNT: "구입금액을 입력해 주세요.",
 };
 
 module.exports = {
   LOTTO_LENGTH,
   ERROR,
+  MESSEGE,
 };
