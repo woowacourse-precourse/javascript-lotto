a6d38fe55f7bb39d6814e4021f5b6f6b5fca152f
Hwang Seong Taek <hst007@naver.com>
2022-11-12 20:28:35 +0900

    test: 1,000원 미만의 입력시 예외 발생 테스트

--- __tests__/PurchaseLottoTest.js
+++ __tests__/PurchaseLottoTest.js
@@ -15,4 +15,11 @@ describe('로또 구입을 위한 사용자 입력 테스트', () => {
       app.validateUserInput('1,000');
     }).toThrow(LOTTO_ERROR_MESSAGE.NOT_NUMBER);
   });
+
+  test('입력이 1,000원 미만일 경우 예외 발생', () => {
+    expect(() => {
+      const app = new App();
+      app.validateUserInput('100');
+    }).toThrow(LOTTO_ERROR_MESSAGE.UNDER_MONEY);
+  });
 });
