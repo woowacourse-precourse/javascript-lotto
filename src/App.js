8f0f54da1d0d85d37813c7d16638ac0380168414
kinoasm <kinoasm@gmail.com>
2022-11-12 22:33:58 +0900

    feat: 당첨 번호 입력 받기
    입력 오류 처리까지 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
     LOTTO_LIST = [];
+    CHECK_LIST = [0] * 46;
+
     play() {
         this.start();
     }
@@ -41,7 +43,32 @@ class App {
         for (let i in arr) arr.print();
         this.getWinNumbers();
     }
-    getWinNumbers() {}
+    getWinNumbers() {
+        MissionUtils.Console.readLine(
+            "\n당첨 번호를 입력해 주세요.\n",
+            (answer) => {
+                this.checkWinNumbers(answer);
+            }
+        );
+    }
+    checkWinNumbers(str) {
+        let arr = str.split(",").map(Number);
+        if (arr.length !== 6)
+            throw new Error("[ERROR] 당첩 번호는 6개여야 합니다.");
+        for (let i in arr) {
+            this.checkWinNumber(arr[i]);
+        }
+    }
+    checkWinNumber(int) {
+        if (isNaN(int)) throw new Error("[ERROR] 입력 금액은 숫자여야 합니다.");
+        if (int < 1 || int > 45)
+            throw new Error(
+                "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+            );
+        if (this.CHECK_LIST[int] > 0)
+            throw new Error("[ERROR] 중복되지 않는 수를 입력해야 합니다.");
+        this.CHECK_LIST[int] += 2;
+    }
 }
 const app = new App();
 console.log(app.makeLotto(5));
