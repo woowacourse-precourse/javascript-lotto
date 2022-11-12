6ad2b2c99968424a772c4682072607deff387a81
ReturnReason <reasonz@daum.net>
2022-11-12 13:01:26 +0900

    feat: 로또 번호 개수가 6개가 아닌 경우 예외가 발생하는 메서드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,7 +1,10 @@
+const Validator = require('./Validator');
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
+    Validator.checkValidLottoLength(numbers);
     this.#numbers = numbers;
   }
 
--- src/Validator.js
+++ src/Validator.js
@@ -1,10 +1,17 @@
 const { rest } = require('./utils/calculator');
 const { ERROR_MESSAGES } = require('./common/messages');
+const { LOTTO_INFO } = require('./common/constants');
 
 class Validator {
   static checkValidMoney(money) {
     if (rest(money) > 0) {
       throw new Error(`${ERROR_MESSAGES.INVALID_MONEY}`);
+    }
+  }
+
+  static checkValidLottoLength(lotto) {
+    if (lotto.length > `${LOTTO_INFO.COUNT}`) {
+      throw new Error(`${ERROR_MESSAGES.INVALID_LOTTO_COUNT}`);
     }
   }
 }
