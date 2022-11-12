75060ff114e7ecb32717e8a61182dedd02237e13
millifail <hwangbird99@gmail.com>
2022-11-13 08:37:59 +0900

    feat(lotto-generator):validate lotto numbers
    [로또 발행] 1~45의 랜덤한 숫자, 중복되지 않은 6개의 숫자 체크하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+const LOTTO = require('./Lotto');
+
 class App {
   play() {}
 }
--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,31 @@ class Lotto {
 
   constructor(numbers) {
     this.validate(numbers);
+    this.overlap_check(numbers);
+    this.number_check(numbers);
     this.#numbers = numbers;
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  overlap_check(numbers) {
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다.');
+    }
+  }
+
+  number_check(numbers) {
+    numbers.forEach((e) => {
+      if (e < 1 || e > 45) {
+        throw new Error('[ERROR] 로또 번호의 범위는 1~45까지입니다.');
+      }
+    });
+  }
 }
 
 module.exports = Lotto;
