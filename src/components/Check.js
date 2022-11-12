cb9337fcd5e21da12fb1ecb0603c268a58a98abe
Sallyvely <tpdus4159@naver.com>
2022-11-12 19:19:59 +0900

    feat : 당첨 번호가 유효한지 확인하는 기능 구현

--- src/components/Check.js
+++ src/components/Check.js
@@ -11,6 +11,23 @@ class Check{
         
     }
 
+    checkDivideMoney(money){
+        if(!(money%1000==0)){
+            throw new Error("[ERROR]");
+        }
+        return true;
+    }
+
+    checkWinNumVaildation(winningstr){; 
+        let winningArr=winningstr.split(",").map(Number);
+        let lotto=new Lotto(winningArr);
+        if(!this.checkNumberRange(winningArr)){
+            throw new Error("[ERROR]");
+        }
+        lotto.validate(winningArr);
+        lotto.checkUniqueRange(winningArr);
+        return true;
+    }
 
 
 }
