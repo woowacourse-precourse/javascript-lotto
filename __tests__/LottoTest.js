46757f82323653491797dd517a2317b282518f6f
moseung <tmdah900@naver.com>
2022-11-12 21:38:15 +0900

    ✅ test($기능1) : 기능목록 1 관련 테스트 추가 -기능 1중 사용자에게 입력받은 값이 1000으로 나눠떨어지지 않으면 throw error

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,6 @@
+const BuyLotto = require("../src/BuyLotto");
 const Lotto = require("../src/Lotto");
+const { BUY_LOTTO } = require("../src/constant/constant");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -12,6 +14,12 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
+  });
+
+  test("1000의 배수가 아닌 값을 받으면 예외가 발생한다.", () => {
+    expect(() => {
+      new BuyLotto(13250);
+    }).toThrow(BUY_LOTTO.ERROR);
   });
 
   // 아래에 추가 테스트 작성 가능
