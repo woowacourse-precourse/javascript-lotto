003f7417c749ad70e3f687c5888be710a55ed4bd
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-13 02:56:12 +0900

    feat: 로또 구입 금액 관련 검증 구현
    사용자의 구입 금액 검증을 validateMoney() 메소드로 관리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
   play() {
+    this.inputMoney();
+  }
+
+  inputMoney() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (answer) => {
+      MissionUtils.Console.print(answer);
+      if (this.validateMoney(answer)) {
+        MissionUtils.Console.print(`${this.countTickets(answer)}개를 구매했습니다.`);
+      }
+    });
+  }
+
+  validateMoney(answer) {
+    const money = Number(answer);
+    if (Number.isNaN(money)) {
+      throw Error('[ERROR] 숫자여야 합니다.');
+    }
+    if (money % 1000 !== 0) {
+      throw Error('[ERROR] 1,000원 단위로 입력하세요');
+    }
+    return true;
+  }
+
+  countTickets(answer) {
+    return answer / 1000;
   }
 }
 
