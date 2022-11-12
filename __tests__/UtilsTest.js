340c35faf56f15e76a720c744d7ab2f903ef2528
yong <tkddbs1644@gmail.com>
2022-11-12 22:06:48 +0900

    test(Utils): 문자열을 배열로 변환하는 메서드 테스트

--- __tests__/UtilsTest.js
+++ __tests__/UtilsTest.js
@@ -12,4 +12,20 @@ describe("유틸 클래스 테스트", () => {
       expect(Utils.transformArrayToString(array)).toEqual(result);
     });
   });
+
+  test("transformStringToNumberArray: 문자열을 배열로 변환한다.", () => {
+    const TEST_CASE = [
+      {
+        string: "1, 2, 3, 4, 5, 6",
+        result: [1, 2, 3, 4, 5, 6],
+      },
+      {
+        string: "1,2,3,4,5,6",
+        result: [1, 2, 3, 4, 5, 6],
+      },
+    ];
+    TEST_CASE.forEach(({ string, result }) => {
+      expect(Utils.transformStringToNumberArray(string)).toEqual(result);
+    });
+  });
 });
