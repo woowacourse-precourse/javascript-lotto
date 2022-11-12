9c53037f56209f807579a03cc5d47ebe4a0dda6d
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-13 08:23:29 +0900

    refactor : 상수 게임 메시지 나누기

--- src/lib/Constants.js
+++ src/lib/Constants.js
@@ -2,11 +2,22 @@ const GAME_MESSAGE = {
   input_price: '구입금액을 입력해 주세요.\n',
   input_win_number: '당첨 번호를 입력해 주세요.\n',
   input_bonus_number: '보너스 번호를 입력해 주세요.\n',
+};
+
+const LOTTO_RESULT_MESSAGE = {
   result_three_match: '3개 일치 (5,000원) - ',
   result_four_match: '4개 일치 (50,000원) - ',
   result_five_match: '5개 일치 (1,500,000원) - ',
   result_five_bonus_match: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
   result_six_match: '6개 일치 (2,000,000,000원) - ',
+};
+
+const LOTTO_RESULT_PRICE = {
+  result_three_number: 5000,
+  result_four_number: 50000,
+  result_five_number: 1500000,
+  result_five_bonus_number: 30000000,
+  result_six_number: 2000000000,
 };
 
 const PRICE_ERROR_MESSAGE = {
@@ -32,6 +43,8 @@ const PRICE_MEASURE = 1000;
 
 module.exports = {
   GAME_MESSAGE,
+  LOTTO_RESULT_MESSAGE,
+  LOTTO_RESULT_PRICE,
   PRICE_ERROR_MESSAGE,
   WIN_NUMBER_ERROR_MESSAGE,
   BONUS_NUMBER_ERROR_MESSAGE,
