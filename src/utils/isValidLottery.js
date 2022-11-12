29ae1b23c4b255bfb40f6a5301b6274985ce5bec
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-12 17:41:22 +0900

    fix: 로또 검증 함수 오타 수정

--- src/utils/isValidLottery.js
+++ src/utils/isValidLottery.js
@@ -1,7 +1,7 @@
 const isValidLottery = (lottery) => {
   const checkLotteryLength = new Set(lottery).size;
   if (checkLotteryLength !== 6) return false;
-  for (let lotteryIndex = 0; i < lottery.length; lotteryIndex += 1) {
+  for (let lotteryIndex = 0; lotteryIndex < lottery.length; lotteryIndex += 1) {
     if (isNaN(lottery[lotteryIndex])) return false;
   }
   return true;
