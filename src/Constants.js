3cdff5f376dae9d402bf41d2bb849eaaf82673e9
osdoonhyun <skw1996@naver.com>
2022-11-12 22:36:04 +0900

    feat(Constants): ERROR MESSAGES, NUMBERS에 MAX,MIN RANGE 추가

--- src/Constants.js
+++ src/Constants.js
@@ -1,14 +1,17 @@
 const MESSAGES = {
   ENTER_PURCHASE_AMOUNT: "구입 금액을 입력해 주세요.\n",
-
-  ERROR_AMOUNT_UNIT: "구입 금액은 1000원 단위여야 합니다.",
+  INPUT_WINNING_NUMBERS: "당첨 번호를 입력해 주세요.\n",
+  ERROR_AMOUNT_UNIT: "[ERROR] 구입 금액은 1000원 단위여야 합니다.",
+  ERROR_INVALID_NUMBER: "[ERROR] 로또 번호는 6개여야 합니다.",
+  ERROR_INVALID_RANGE: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+  ERROR_DUPLICATION_NUMBERS: "[ERROR] 로또 번호는 중복이 없어야 합니다.",
 };
 
 const NUMBERS = {
   LOTTO: 6,
   BONUS: 1,
-  FIRST: 1,
-  LAST: 45,
+  MIN_RANGE: 1,
+  MAX_RANGE: 45,
   AMOUNT: 1000,
 };
 
