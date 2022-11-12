520142496a20db1ece7238000cd9df54e5ce0cee
yong <tkddbs1644@gmail.com>
2022-11-12 22:00:05 +0900

    feat(Lotto): 원소가 숫자가 아닐 때의 예외처리 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,7 +3,7 @@ const { LOTTO } = require("./constants");
 const ERRORS = Object.freeze({
   LENGTH: "[ERROR] 로또 번호는 6개여야 합니다.",
   DUPLICATION: "[ERROR] 로또 번호는 중복되지 않아야 합니다.",
-  RANGE: "[ERROR] 로또 번호는 1부터 45까지여야 합니다.",
+  RANGE: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
 });
 
 class Lotto {
@@ -35,8 +35,12 @@ class Lotto {
 
   checkRange(numbers) {
     numbers.forEach((number) => {
-      if (number < LOTTO.RANGE_MIN || number > LOTTO.RANGE_MAX) {
-        throw new Error(ERRORS.DUPLICATION);
+      if (
+        Number.isNaN(Number(number)) ||
+        number < LOTTO.RANGE_MIN ||
+        number > LOTTO.RANGE_MAX
+      ) {
+        throw new Error(ERRORS.RANGE);
       }
     });
   }
