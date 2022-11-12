51a92d6cc7de6ca30d2da5998261fe8e35bdc78d
minjaeKim <smallkdb@gmail.com>
2022-11-13 00:23:01 +0900

    feat(Payment): 입력값이 숫자인지 확인하는 유효성 검사 추가
    Payment 클래스의 validate 메서드에 입력값이 숫자인지 확인하는 유효성 검사를 추가하였습니다.

--- src/Payment.js
+++ src/Payment.js
@@ -7,6 +7,10 @@ class Payment {
   }
 
   static validate(money) {
+    const regExp = /^[0-9]/g;
+    if (!regExp.test(money)) {
+      throw new Error('[ERROR] 입력값이 숫자가 아닙니다.');
+    }
     if (money % 1000 !== 0) {
       throw new Error('[ERROR] 구입금액의 단위는 1000원입니다.');
     }
