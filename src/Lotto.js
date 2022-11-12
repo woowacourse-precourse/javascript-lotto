02fbba486012590d602adefa69035f6f9db7821d
xilucks <siun0331@gmail.com>
2022-11-12 21:44:35 +0900

    feat: validate 재사용을 위한 함수 분리 및 보너스 번호 validate 생성

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,20 +10,26 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    this.rangeValidate(numbers);
+    numbers.map((number) => {
+      this.rangeValidate(number);
+    });
   }
 
-  // TODO: 추가 기능 구현
-  getNumbers(){
+  bonusValidate(winningNumbers, bonusNumber) {
+    if (winningNumbers.includes(bonusNumber)) {
+      throw new Error("[ERROR] 보너스 번호가 당첨번호와 동일합니다.");
+    }
+    this.rangeValidate(bonusNumber);
+  }
+
+  getNumbers() {
     return this.#numbers;
   }
 
-  rangeValidate(numbers){
-    numbers.map((number) => {
-      if(number >= 1 && number <= 45){
-        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-      }
-    });
+  rangeValidate(number) {
+    if (number < 1 || number > 45) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
   }
 }
 
