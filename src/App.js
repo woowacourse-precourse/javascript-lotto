f55c8b9be58cdac12e7ab2696ad5b85aae0cbf41
GC-Park <kuyt3122@naver.com>
2022-11-12 10:18:09 +0900

    feat:발행한 로또 수량에 따라 번호를 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   money=0;
+  userLottoNumbers=[];
+
+  constructor(){
+
+  }
 
   play() {
     this.insertMoney();
@@ -10,7 +15,17 @@ class App {
   insertMoney(){
     MissionUtils.Console.readLine('구입금액을 입력해 주세요. ', (answer) => {
       this.money = answer/1000;
+      this.makeLottoNumber();
   })
+  }
+
+  makeLottoNumber(){
+    for(let i=0; i<this.money; i++){
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      this.userLottoNumbers.push(numbers);
+    }
+    MissionUtils.Console.print(this.userLottoNumbers)
+    MissionUtils.Console.close();
   }
 
 }
