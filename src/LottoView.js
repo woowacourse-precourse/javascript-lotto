c968c91b99f7194648412328d970f8e725f3b801
ReturnReason <reasonz@daum.net>
2022-11-12 19:55:07 +0900

    feat: 일치하는 로또 개수를 출력하는 메서드 추가

--- src/LottoView.js
+++ src/LottoView.js
@@ -1,5 +1,5 @@
 const { Console } = require('./utils/missionUtil');
-const { OUTPUT_MESSAGES } = require('./common/messages');
+const { OUTPUT_MESSAGES, WINNING_AMOUNT } = require('./common/messages');
 
 class LottoView {
   static getUserInput(query, callback) {
@@ -18,6 +18,14 @@ class LottoView {
 
   static printStatsMessage() {
     Console.print(`\n${OUTPUT_MESSAGES.WIN_STATS}`);
+  }
+
+  static printMatchNumbers() {
+    Console.print(`${OUTPUT_MESSAGES.TOTAL(3, `${WINNING_AMOUNT[5]}`, 0)}`); // 카운트 추가 및 하드코딩 수정하기
+    Console.print(`${OUTPUT_MESSAGES.TOTAL(4, `${WINNING_AMOUNT[4]}`, 0)}`); // 카운트 추가 및 하드코딩 수정하기
+    Console.print(`${OUTPUT_MESSAGES.TOTAL(5, `${WINNING_AMOUNT[3]}`, 0)}`); // 카운트 추가 및 하드코딩 수정하기
+    Console.print(`${OUTPUT_MESSAGES.TOTAL_BONUS(`${WINNING_AMOUNT[2]}`, 0)}`); // 카운트 추가 및 하드코딩 수정하기
+    Console.print(`${OUTPUT_MESSAGES.TOTAL(6, `${WINNING_AMOUNT[1]}`, 0)}`); // 카운트 추가 및 하드코딩 수정하기
   }
 }
 
