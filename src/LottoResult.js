e9d55b4138cda5f967274b824bbd398f243b279f
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-12 18:08:36 +0900

    ♻️refactor: 코드 리펙토링

--- src/LottoResult.js
+++ src/LottoResult.js
@@ -27,8 +27,7 @@ class LottoResult {
   }
 
   #checkBonus(lotto) {
-    if (lotto.includes(this.#bonusNumber)) return true;
-    return false;
+    return lotto.includes(this.#bonusNumber);
   }
 
   #checkLottos(lottos) {
@@ -37,7 +36,7 @@ class LottoResult {
       if (sameNumbers == 3) this.#numberOfRanks[RANK.FIFTH_INDEX]++;
       if (sameNumbers == 4) this.#numberOfRanks[RANK.FOURTH_INDEX]++;
       if (sameNumbers == 5) {
-        this.#checkBonus()
+        this.#checkBonus(lotto)
           ? this.#numberOfRanks[RANK.SECOND_INDEX]++
           : this.#numberOfRanks[RANK.THIRD_INDEX]++;
       }
@@ -49,7 +48,7 @@ class LottoResult {
     let totalReward = 0;
 
     this.#numberOfRanks.forEach((numberOfRank, index) => {
-      totalReward += numberOfRank * REWARD.MONEY_ARRAY[index];
+      totalReward += numberOfRank * RANK.MONEY_ARRAY[index];
     });
     return totalReward;
   }
@@ -61,21 +60,10 @@ class LottoResult {
 
   printLottoResult() {
     MissionUtils.Console.print(COMMENT.WINTITLE);
-    MissionUtils.Console.print(
-      COMMENT.FIFTH + this.#numberOfRanks[RANK.FIFTH_INDEX] + "개"
-    );
-    MissionUtils.Console.print(
-      COMMENT.FOURTH + this.#numberOfRanks[RANK.FOURTH_INDEX] + "개"
-    );
-    MissionUtils.Console.print(
-      COMMENT.THIRD + this.#numberOfRanks[RANK.THIRD_INDEX] + "개"
-    );
-    MissionUtils.Console.print(
-      COMMENT.SECOND + this.#numberOfRanks[RANK.SECOND_INDEX] + "개"
-    );
-    MissionUtils.Console.print(
-      COMMENT.FIRST + this.#numberOfRanks[RANK.FIRST_INDEX] + "개"
-    );
+
+    this.#numberOfRanks.forEach((number, index) => {
+      MissionUtils.Console.print(COMMENT.RESULT_ARRAY[index] + number + "개");
+    });
     MissionUtils.Console.print("총 수익률은 " + this.#yield + "%입니다.");
     MissionUtils.Console.close();
   }
--- src/constant/comment.js
+++ src/constant/comment.js
@@ -1,21 +1,19 @@
 const PURCHASE = "구입금액을 입력해 주세요.\n";
 const WINNING = "\n당첨 번호를 입력해 주세요.\n";
 const BONUS = "\n보너스 번호를 입력해 주세요.\n";
-const FIFTH = "3개 일치 (5,000원) - ";
-const FOURTH = "4개 일치 (50,000원) - ";
-const THIRD = "5개 일치 (1,500,000원) - ";
-const SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-const FIRST = "6개 일치 (2,000,000,000원) - ";
+const RESULT_ARRAY = [
+  "3개 일치 (5,000원) - ",
+  "4개 일치 (50,000원) - ",
+  "5개 일치 (1,500,000원) - ",
+  "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+  "6개 일치 (2,000,000,000원) - ",
+];
 const WINTITLE = "\n당첨 통계\n---";
 
 module.exports = {
   PURCHASE,
   WINNING,
   BONUS,
-  FIFTH,
-  FOURTH,
-  THIRD,
-  SECOND,
-  FIRST,
+  RESULT_ARRAY,
   WINTITLE,
 };
