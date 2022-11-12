8cd002bf0e0bd2301aac1b12a160f27f102f8f28
yuu <22yuu@naver.com>
2022-11-13 01:09:00 +0900

    test: 보너스 번호 입력시 당첨 번호와의 중복 여부 테스트 추가

--- __tests__/InputTest.test.js
+++ __tests__/InputTest.test.js
@@ -1,35 +1,37 @@
 const App = require("../src/App");
-
+const Validation = require('../src/Validation');
 
 describe('사용자 입력 예외 테스트', () => {
 
     let app;
+    let val;
+
     beforeEach(() => {
         app = new App();
     });
 
     test('구입 금액이 최소 금액 미만이면 예외가 발생한다.', () => {
         expect(() => {
-            app.countLottos('700');
-        }).toThrow();
+            Validation.validate('700');
+        }).toThrow('[ERROR]');
     })
 
     test('금액이 1000원 단위가 아니면 예외가 발생한다.', () => {
         expect(() => {
-            app.countLottos('1234A');
+            Validation.validate('1234A');
         }).toThrow();
     });
 
     test('금액 입력 시 콤마(,)는 제외해야 하므로 포함될 경우 예외가 발생한다.', () => {
         expect(() =>{
-            app.countLottos('14,000');
+            Validation.validate('14,000');
         }).toThrow('[ERROR]');
     });
 
     test('당첨 번호 입력 시 콤마(,)를 사용해 구분해야 한다.', () => {
         expect(() => {
             app.setTargetNumbers('123456');
-        }).toThrow("[ERROR]");
+        }).toThrow('[ERROR]');
     });
 
     test('당첨 번호는 1~45 사이의 6자리를 입력해야한다.', () => {
@@ -39,19 +41,26 @@ describe('사용자 입력 예외 테스트', () => {
 
         expect(() => {
             app.setTargetNumbers('0,1,2,3,4');
-        }).toThrow();
+        }).toThrow('[ERROR]');
     });
 
-    test('보너스 번호는 1~45 사이의 수를 입력해야 한다.', () => {
+    test('보너스 번호 입력는 1~45 사이의 숫자여야만 한다.', () => {
+        let bonus;
         expect(() => {
-            app.setBonusNumber('46')
+            bonus = Number('46');
+            Validation.validate(bonus, []);
         }).toThrow();
+
+        expect(() => {
+            bonus = Number('1A');
+            Validation.validate(bonus, []);
+        }).toThrow('[ERROR]');
     });
 
-    test('보너스 번호 입력는 1~45 사의 숫자여야만 한다.', () => {
+    test('보너스 번호는 당첨 번호와 중복이 되어서는 안된다.', () => {
         expect(() => {
-            app.setBonusNumber('1A');
-        }).toThrow();
-    });
+            Validation.validate(7, [1,2,3,4,5,7]);
+        }).toThrow('[ERROR]');
+    })
 
 })
