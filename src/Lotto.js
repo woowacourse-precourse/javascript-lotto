674fe8a44bbd27da58c4bd7243729f5dd9e96bf4
wonkyum-kim <dodo12120@naver.com>
2022-11-12 21:02:07 +0900

    feat(Lotto): check invalidate winning numbers
    - In Lotto class, validate() can handle invalidate numbers
    - The length of numbers must be 6.
    - Numbers must be btween 1 and 45.
    - Duplicated numbers are not allowed.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,34 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.map((num) => parseInt(num, 10));
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    numbers.forEach(this.checkNumberValidity);
+    const UNIQUE_NUMBERS = new Set(numbers);
+
+    if (UNIQUE_NUMBERS.size !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복이 없어야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  checkNumberValidity(num) {
+    const VALID_NUMBER_REGEX = /^[\d]+$/;
+    const LOWER_BOUND = 1;
+    const UPPER_BOUND = 45;
+    const VALID_BOUND = ((+num) >= LOWER_BOUND && (+num) <= UPPER_BOUND);
+    if (!VALID_NUMBER_REGEX.test(num) || !VALID_BOUND) {
+      throw new Error('[ERROR] 로또 번호는 1 ~ 45 사이의 숫자여야 합니다.');
+    }
+  }
 }
 
 module.exports = Lotto;
