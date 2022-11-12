256d7fca803f14001656f2248e2631ab6fa16350
jongwonp <jw032876@gmail.com>
2022-11-12 17:21:55 +0900

    test: 구입한 로또 맞힌 개수 출력하는 기능 테스트 완료

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,11 +1,12 @@
 const { Console } = require('@woowacourse/mission-utils');
+
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
+  constructor(numbers, myTickets) {
     this.validate(numbers);
     this.#numbers = numbers;
-    this.getBonusNumber();
+    this.getBonusNumber(myTickets);
   }
 
   validate(numbers) {
@@ -20,21 +21,20 @@ class Lotto {
     }
 
     numbers.forEach((number) => {
-      if (isNaN(Number(number))) {
+      if (isNaN(number)) {
         throw new Error('[ERROR] 다른 형식이 아닌 숫자만 입력해 주세요.');
       }
 
-      if (!is1To45(Number(number))) {
+      if (!is1To45(number)) {
         throw new Error('[ERROR] 1부터 45 사이의 숫자만 입력해 주세요.');
       }
     });
   }
 
-  getBonusNumber() {
+  getBonusNumber(myTickets) {
     Console.readLine('보너스 번호를 입력해 주세요.', (num) => {
-      const bonusNumber = num;
-
-      if (isNaN(Number(bonusNumber))) {
+      const bonusNumber = Number(num);
+      if (isNaN(bonusNumber)) {
         throw new Error('[ERROR] 다른 형식이 아닌 숫자만 입력해 주세요.');
       }
 
@@ -45,7 +45,38 @@ class Lotto {
       if (this.isIncluded(bonusNumber)) {
         throw new Error('[ERROR] 로또 번호 6개와 다른 수를 입력해주세요');
       }
+
+      const numberOfWonTicket = this.getNumberOfWonTicket(
+        myTickets,
+        this.#numbers,
+        bonusNumber
+      );
+
+      Console.print(numberOfWonTicket);
     });
+  }
+
+  getNumberOfWonTicket(tickets, winningNums, bonusNumber) {
+    const numberOfWonTicket = [];
+
+    tickets.forEach((ticket) => {
+      numberOfWonTicket.push(
+        this.compareLotto(ticket, winningNums, bonusNumber)
+      );
+    });
+    return numberOfWonTicket;
+  }
+
+  compareLotto(ticket, winningNums, bonusNumber) {
+    const matchedNumbers = ticket.filter((myNumber) =>
+      winningNums.includes(myNumber)
+    );
+
+    // 5개 맞히고 보너스 번호 맞혔다면(2등 당첨이면) 10을 더해서 반환
+    if (matchedNumbers.length === 5 && ticket.includes(bonusNumber)) {
+      return matchedNumbers.length + 10;
+    }
+    return matchedNumbers.length;
   }
 
   isIncluded(num) {
