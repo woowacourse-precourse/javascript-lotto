4ad8ba5575eacff21a9089bc3215529aa2feeaff
ReturnReason <reasonz@daum.net>
2022-11-12 10:49:42 +0900

    test: 사용자가 1,000으로 나누어 떨어지지 않는 금액을 입력하면 발생하는 예외 테스트

---
+++ __tests__/ValidatorTest.js
@@ -0,0 +1,12 @@
+const Validator = require('../src/Validator');
+const { ERROR_MESSAGES } = require('../src/common/messages');
+
+describe('VAlidator 클래스 테스트', () => {
+  test('🖐 사용자가 1,000으로 나누어 떨어지지 않는 금액을 입력하면 에러가 발생한다.', () => {
+    const invalidMoney = '1234';
+
+    expect(() => {
+      Validator.checkValidMoney(invalidMoney);
+    }).toThrowError(`${ERROR_MESSAGES.INVALID_MONEY}`);
+  });
+});
