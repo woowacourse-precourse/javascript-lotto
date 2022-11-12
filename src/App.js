a9bb2ac5cd44680291ef772516562503aae46171
jiheon788 <jiheon788@ajou.ac.kr>
2022-11-12 20:12:45 +0900

    feat: 구입 금액 만큼 로또 발행 기능

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
-const Shop = require('./Shop')
+const Lotto = require('./Lotto')
 
 class App {
-  shop = new Shop();
+  numberOfLotto;
 
   play() {
-    this.shop.pay();
+    this.pay();
+  }
 
+  pay() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
+      this.numberOfLotto = Number(input) / 1000;
+      this.publish();
+    });
+  }
+
+  publish() {
+    Console.print(`${this.numberOfLotto}개를 구매했습니다.`);
+    for(let i = 0; i < this.numberOfLotto; i++) {
+      const lotto = new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6))
+    }
   }
 
   
