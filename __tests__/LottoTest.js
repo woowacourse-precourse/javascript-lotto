65238663e92e59007977c5fa831fed12a891a9b8
pakxe <pigkill40@naver.com>
2022-11-12 16:14:46 +0900

    test(lottoTest): 정확하게 예외의 원인을 출력하는지 확인하도록 테스트 코드 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,27 +1,28 @@
 const Lotto = require('../src/Lotto');
+const { ERROR } = require('../src/utiles/Constant');
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow('[ERROR]');
+    }).toThrow(`${ERROR.PREFIX} ${ERROR.COUNT}`);
   });
 
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow('[ERROR]');
+    }).toThrow(`${ERROR.PREFIX} ${ERROR.DUPLICATION}`);
   });
 
   test('로또 번호에 숫자 외의 요소가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 'a']);
-    }).toThrow('[ERROR]');
+    }).toThrow(`${ERROR.PREFIX} ${ERROR.NUMBER_ONLY}`);
   });
 
   test('로또 번호의 범위가 주어진 범위를 벗어난다면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 46]);
-    }).toThrow('[ERROR]');
+    }).toThrow(`${ERROR.PREFIX} ${ERROR.RANGE}`);
   });
 });
