e711b509d51e631300cf8c01e863dbab44cd095e
Buyoung Chung <sunarol@naver.com>
2022-11-12 13:11:20 +0900

    feat: Add winning number redundancy exception

--- src/ExceptionHandler.js
+++ src/ExceptionHandler.js
@@ -2,6 +2,10 @@ class ExceptionHandler {
   budgetUnit(budget) {
     if (budget % 1000 !== 0) throw new Error("[ERROR] 단위를 정확히 입력해주세요.");
   }
+  winningNumberRedundancy(winningNumber) {
+    const checkSet = new Set(winningNumber.split(","));
+    if ([...checkSet].length < 6) throw new Error("[ERROR] 당첨 번호를 중복없이 입력해주세요.");
+  }
 }
 
 exports.module = ExceptionHandler;
