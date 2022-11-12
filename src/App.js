c342526e22efcc891af3cad85153b770051291a4
pp449 <mma7710@naver.com>
2022-11-13 02:04:51 +0900

    refactor: Lotto 객체에 무작위 숫자를 저장하도록 변경

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 const { Console } = require('@woowacourse/mission-utils');
 
+const Lotto = require('./Lotto');
 const MESSAGE = require('./utils/constants');
 const RandomNumbers = require('./RandomNumbers');
 
 class App {
+  constructor() {
+    this.lottoList = [];
+  }
+
   play() {
     Console.readLine(MESSAGE.PURCHASE_AMOUT, (amount) => {
       this.amount = amount;
-      // this.print(`총 구매 갯수: ${parseInt(this.amount / 1000)}`);
-      this.print('랜덤넘버 : ' + RandomNumbers.generate());
+      this.lotteryPurchase(amount);
     });
+  }
+
+  lotteryPurchase() {
+    for (let i = 0; i < parseInt(this.amount / 1000); i++) {
+      const lotto = new Lotto(RandomNumbers.generate());
+      this.lottoList.push(lotto);
+    }
+    console.log(this.lottoList[0].showNumber());
   }
 
   print(message) {
     Console.print(message);
-  }
-
-  input(message) {
-    Console.readLine(message, (input_val) => {
-      return input_val;
-    });
   }
 }
 
