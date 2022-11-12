43bc2e8f03404487b3e76f1d5b4a67c9ea05f228
pp449 <mma7710@naver.com>
2022-11-12 17:08:19 +0900

    feat: 게임 시작 시 구입금액 입력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+const MESSAGE = require('./utils/constants');
+
 class App {
-  play() {}
+  play() {
+    Console.readLine(MESSAGE.PURCHASE_AMOUT, (amount) => {
+      this.print(`총 구매 : ${amount}`);
+    });
+  }
+
+  print(message) {
+    Console.print(message);
+  }
+
+  input(message) {
+    Console.readLine(message, (input_val) => {
+      return input_val;
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
