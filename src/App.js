a427fa4d68e27750bec6eefdcda3d2f685515929
kinoasm <kinoasm@gmail.com>
2022-11-12 21:58:24 +0900

    feat: 구입금액 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-class App {
-  play() {}
-}
+const Lotto = require("./Lotto");
+const MissionUtils = require("@woowacourse/mission-utils");
 
+class App {
+    play() {
+        this.start();
+    }
+    start() {
+        MissionUtils.Console.readLine(
+            "구입금액을 입력해 주세요.\n",
+            (answer) => {
+                this.getMoney(answer);
+            }
+        );
+    }
+    getMoney(int) {}
+}
 module.exports = App;
