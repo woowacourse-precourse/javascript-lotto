6b4e9b471471f8cffbe07628cda3df8a6f27cead
khj0426 <706shin1728@naver.com>
2022-11-12 11:35:36 +0900

    feat:내가 산 로또와 당첨번호가 몇 개 일치하는지 관리기능 추가

---
+++ src/NumberEqaulCount.js
@@ -0,0 +1,12 @@
+const MESSAGE = require('./MESSAGE');
+
+const [Three, Four, Five, FiveBonus, Six] = MESSAGE;
+
+const numbereqaulObj = {
+  Three: 0,
+  Four: 0,
+  Five: 0,
+  FiveBonus: 0,
+  Six: 0,
+};
+module.exports = numbereqaulObj;
