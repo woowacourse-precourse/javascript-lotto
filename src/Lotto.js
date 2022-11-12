8f1bd5d9b63b55589239aee4a80b76a5f0d3a5ba
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-11-12 19:02:06 +0900

    fe값 예외(오류) 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,15 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (
+      numbers.filter((number) => number >= 1 && number <= 45) !== numbers.length
+    ) {
+      throw new Error("[ERROR] 로또 번호는 1~45범위 내의 숫자여야 합니다.");
+    }
+    const overlap = new Set(numbers);
+    if (overlap.size !== 0) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
