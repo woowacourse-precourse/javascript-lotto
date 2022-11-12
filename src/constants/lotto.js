b2cfe2c21aeaa0e1799fb2724be0613d0703fe85
dev-redo <mis05041@naver.com>
2022-11-13 08:38:37 +0900

    style: 파일 위치 이동

---
+++ src/constants/lotto.js
@@ -0,0 +1,9 @@
+const LOTTO_ERROR_MESSAGE = Object.freeze({
+  LENGTH: '로또 번호는 6개여야 합니다.',
+  DUPLICATED: '로또 번호에 중복되는 숫자가 없어야 합니다.',
+  RANGE: '로또 번호의 숫자 범위는 1~45까지 입니다.',
+});
+
+module.exports = {
+  LOTTO_ERROR_MESSAGE,
+};
