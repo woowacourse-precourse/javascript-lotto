83c9b18f6f77f00d40a35e4ef585b8dbabad1a64
somin <smhs12@naver.com>
2022-11-12 21:00:25 +0900

    feat: 발행 로또 출력 함수 출력 형태 변경

--- src/UserLotto.js
+++ src/UserLotto.js
@@ -1,21 +1,23 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class UserLotto {
-  #lottoCount;
   constructor() {
     this.userLottoList = [];
+    this.lottoCount;
   }
   createUserLotto(amount) {
-    this.#lottoCount = amount / 1000;
-    for (let make = 0; make < this.#lottoCount; make++) {
+    this.lottoCount = amount / 1000;
+    for (let make = 0; make < this.lottoCount; make++) {
       const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
       this.userLottoList.push(numbers);
     }
   }
 
   printUserLottoInformation() {
-    MissionUtils.Console.print(`${this.#lottoCount}개를 구매했습니다.`);
-    this.userLottoList.map((lotto) => MissionUtils.Console.print(lotto));
+    MissionUtils.Console.print(`\n${this.lottoCount}개를 구매했습니다.`);
+    this.userLottoList.map((lotto) => {
+      MissionUtils.Console.print(`[${lotto.join(", ")}]`);
+    });
   }
 }
 
