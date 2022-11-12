0080df1ba6e448e067f2a10a36f7f6ef84da444f
Buyoung Chung <sunarol@naver.com>
2022-11-12 15:49:40 +0900

    feat: Add constant FIFTH ~ FIRST

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -2,6 +2,14 @@ class Constant {
   static THOUSAND = 1000;
 
   static COMMA = ",";
+
+  static FIFTH = 3;
+
+  static FOURTH = 4;
+
+  static THIRD = 5;
+
+  static FIRST = 6;
 }
 
 module.exports = Constant;
