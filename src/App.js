5a99e591f37cb1fa7ef760fde2a86f8deb7895dd
hafnium1923 <hafnium1923@gmail.com>
2022-11-13 01:12:24 +0900

    feat buyLotto 구현
    buyLotto 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MU = require("@woowacourse/mission-utils");
 class App {
   play() {}
+  buyLotto(){
+    MU.Console.readLine('구입금액을 입력해 주세요.',(money) => {
+      if(isNaN(money)){
+        throw new Error("[ERROR] 구입금액은 숫자여야 합니다.");
+      }
+      let purchaseNum = parseInt(money/1000);
+      this.makeLottoNum(purchaseNum);
+
+      MU.Console.close();
+    });
+  }
+  makeLottoNum(purchaseNum){
+    let Lottos = [purchaseNum][6];
+  }
 }
 
 module.exports = App;
