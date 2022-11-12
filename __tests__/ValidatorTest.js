cd7c3468b46aae2914c63444290dc92f37baa304
ReturnReason <reasonz@daum.net>
2022-11-12 18:02:24 +0900

    test: 사용자가 당첨 번호에 숫자가 아닌 값을 입력하면 예외 처리 테스트

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,12 +1,24 @@
 const Validator = require('../src/Validator');
 const { ERROR_MESSAGES } = require('../src/common/messages');
 
-describe('Validator 클래스 테스트', () => {
+describe('Validator 클래스 : 구입 금액 테스트', () => {
   test('🖐 사용자가 1,000으로 나누어 떨어지지 않는 금액을 입력하면 에러가 발생한다.', () => {
     const invalidMoney = '1234';
 
     expect(() => {
       Validator.checkValidMoney(invalidMoney);
     }).toThrowError(`${ERROR_MESSAGES.INVALID_MONEY}`);
+  });
+});
+
+describe('Validator 클래스 : 당첨 번호 테스트', () => {
+  test('🖐 사용자가 숫자가 아닌 번호를 입력하면 에러가 발생한다', () => {
+    const invalidInputs = ['우테코 조아요', '', ' ', 'Reason', '1a', '1e4'];
+
+    expect(() => {
+      invalidInputs.forEach((input) => {
+        Validator.checkWinNumbers(input);
+      });
+    }).toThrowError(`${ERROR_MESSAGES.INVALID_INPUT}`);
   });
 });
