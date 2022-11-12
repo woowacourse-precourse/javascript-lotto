ff78a3c0a141ef7650c71c4b20ba26fcf575445a
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-12 23:09:50 +0900

    add : 상수 파일 생성 및 게임 메시지 상수 선언

---
+++ src/lib/Constants.js
@@ -0,0 +1,9 @@
+const GAME_MESSAGE = {
+  input_price: '구입금액을 입력해 주세요.\n',
+  input_win_number: '당첨 번호를 입력해 주세요.\n',
+  input_bonus_number: '보너스 번호를 입력해 주세요.\n',
+};
+
+module.exports = {
+  GAME_MESSAGE,
+};
