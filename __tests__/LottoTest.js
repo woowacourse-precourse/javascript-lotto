cc2a3dfc6b9c366737f118b60ef23a2d4e405a21
moseung <tmdah900@naver.com>
2022-11-13 00:47:07 +0900

    ✅ test($기능2) : 기능2 테스트 구현 -기능 2의 입력값으로 숫자가 아닌수를 받을시 에러를 throw하는 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,6 @@
 const BuyLotto = require("../src/BuyLotto");
 const Lotto = require("../src/Lotto");
-const { BUY_LOTTO } = require("../src/constant/constant");
+const { BUY_LOTTO, ERROR } = require("../src/constant/constant");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -20,6 +20,12 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new BuyLotto(13250);
     }).toThrow(BUY_LOTTO.ERROR);
+  });
+
+  test("숫자가 아닌 값을 받으면 예외가 발생한다", () => {
+    expect(() => {
+      new BuyLotto("dadd");
+    }).toThrow(ERROR.NAN);
   });
 
   // 아래에 추가 테스트 작성 가능
