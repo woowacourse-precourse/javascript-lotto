f00e57de529c9221dc3ed2160437ada7d71fcf48
violetwhenisawu <anashape710@gmail.com>
2022-11-12 22:31:35 +0900

    test: 입력 값이 1000원 이하가 아닌지 확인하는 기능 테스트코드 작성

--- __tests__/InputCheckTest.js
+++ __tests__/InputCheckTest.js
@@ -1,15 +1,27 @@
 const InputCheck = require('../src/model/InputCheck');
+const INPUT_CHECK = new InputCheck();
 
 describe('사용자 입력 값 유효성 테스트', () => {
   test('입력 값이 숫자인지 확인', () => {
     const [input1, input2] = ['12000a', '1000'];
 
-    const INPUT_CHECK = new InputCheck();
-
     const [CHECKED1, CHECKED2] = [
       INPUT_CHECK.checkIsNum(input1),
       INPUT_CHECK.checkIsNum(input2),
     ];
+
+    expect(CHECKED1).toBeFalsy();
+    expect(CHECKED2).toBeTruthy();
+  });
+
+  test('입력 값이 1,000원 이하가 아닌지 확인', () => {
+    const [input1, input2] = ['980', '1000'];
+
+    const [CHECKED1, CHECKED2] = [
+      INPUT_CHECK.checkThousand(input1),
+      INPUT_CHECK.checkThousand(input2),
+    ];
+
     expect(CHECKED1).toBeFalsy();
     expect(CHECKED2).toBeTruthy();
   });
