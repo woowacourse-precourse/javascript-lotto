4f2d9b027361d3e46b99bbc15cd86d776503aaf8
bkdragon0228 <rlaqjarb2067@naver.com>
2022-11-12 22:10:47 +0900

    feat : 총 수익률 계산 및 출력

--- src/App.js
+++ src/App.js
@@ -35,6 +35,11 @@ class App {
         );
 
         const finalResult = this.showResult(compareResult);
+        const profitRate = this.calProfitRate(finalResult);
+
+        console.log(`총 수익률은 ${profitRate}%입니다.`);
+
+        Console.close();
     }
 
     inputMoney() {
@@ -98,20 +103,28 @@ class App {
     }
 
     showResult(compareResult) {
+        let sum = 0;
         const result = {
-            '3': ['3개 일치 (5,000원) - ', 0],
-            '4': ['4개 일치 (50,000원) - ', 0],
-            '5': ['5개 일치 (1,500,000원) - ', 0],
-            '6': ['5개 일치, 보너스 볼 일치 (30,000,000원) - ', 0],
-            '7': ['6개 일치 (2,000,000,000원) - ', 0],
+            '3': ['3개 일치 (5,000원) - ', 0, 5000],
+            '4': ['4개 일치 (50,000원) - ', 0, 50000],
+            '5': ['5개 일치 (1,500,000원) - ', 0, 1500000],
+            '6': ['5개 일치, 보너스 볼 일치 (30,000,000원) - ', 0, 30000000],
+            '7': ['6개 일치 (2,000,000,000원) - ', 0, 200000000],
         };
         compareResult.forEach((number) => {
             if (+number >= 3) result[number.toString()][1]++;
         });
 
         for (let key in result) {
+            if (result[key][1] > 0) sum += result[key][1];
             console.log(`${result[key][0]} + ${result[key][1]}개`);
         }
+
+        return sum;
+    }
+
+    calProfitRate(sum) {
+        return (((sum - +this.money) / +this.money) * 100).toFixed(2);
     }
 }
 
