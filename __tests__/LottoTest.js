4288e95bfe5dac14d46723cb13ba7570dfe52cb1
somin <smhs12@naver.com>
2022-11-13 00:18:16 +0900

    test: Lotto 클래스 compareNumbers 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -27,3 +27,30 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR] 숫자가 아닌 값을 입력했습니다.");
   });
 });
+
+describe.only("compareNumbers함수 테스트", () => {
+  test("일치하는 숫자가 3개인 경우 테스트", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const userLotto = [1, 2, 3, 10, 11, 12];
+    const bonusNumber = 10;
+    const winningNumber = lotto.getNumbers();
+    const answer = lotto.compareNumbers(userLotto, winningNumber, bonusNumber);
+    expect(answer).toStrictEqual([3]);
+  });
+  test("5개 일치하고 보너스 번호 일치하지 않는 경우 테스트", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const userLotto = [1, 2, 3, 4, 5, 7];
+    const bonusNumber = 10;
+    const winningNumber = lotto.getNumbers();
+    const answer = lotto.compareNumbers(userLotto, winningNumber, bonusNumber);
+    expect(answer).toStrictEqual([5]);
+  });
+  test("5개 일치하고 보너스 번호 일치하는 경우 테스트", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const userLotto = [1, 2, 3, 4, 5, 7];
+    const bonusNumber = 7;
+    const winningNumber = lotto.getNumbers();
+    const answer = lotto.compareNumbers(userLotto, winningNumber, bonusNumber);
+    expect(answer).toStrictEqual([5, 7]);
+  });
+});
