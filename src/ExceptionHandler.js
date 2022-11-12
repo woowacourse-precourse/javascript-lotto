c6e657bd3f656b4052b02901dd485a9f7d5123e4
Buyoung Chung <sunarol@naver.com>
2022-11-12 13:14:25 +0900

    feat: Add winning number length exception

--- src/ExceptionHandler.js
+++ src/ExceptionHandler.js
@@ -1,9 +1,16 @@
+const Constant = require("../src/constant/Constant");
+
 class ExceptionHandler {
   static budgetUnit(budget) {
     if (budget % 1000 !== 0) throw new Error("[ERROR] 단위를 정확히 입력해주세요.");
   }
+
+  static winningNumberLength(winningNumber) {
+    if (winningNumber.split(Constant.COMMA).length > 6) throw new Error("[ERROR] 숫자를 6개 입력해주세요.");
+  }
+
   static winningNumberRedundancy(winningNumber) {
-    const checkSet = new Set(winningNumber.split(","));
+    const checkSet = new Set(winningNumber.split(Constant.COMMA));
     if ([...checkSet].length < 6) throw new Error("[ERROR] 당첨 번호를 중복없이 입력해주세요.");
   }
 }
