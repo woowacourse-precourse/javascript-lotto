a6150d977ffa11deac11a1e93f45c37bb48700e8
2pandi <coder.2pandi@gmail.com>
2022-11-12 13:58:20 +0900

    feat: add function to ask player for inputAmount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  inputAmount;
+  play() {
+    this.askForAmount();
+  }
+
+  askForAmount() {
+    Console.readLine("구입금액을 입력해 주세요.", (input) => {
+      this.inputAmount = Number(input);
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
