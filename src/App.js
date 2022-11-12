88fa300272ca80f3cd3e77383da97285b70c9518
WaiNaat <qoli0703@gmail.com>
2022-11-12 20:37:36 +0900

    feat(App): add user money input feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const { LottoConfig, AppConfig, Message } = require('./Config');
+const Lotto = require('./Lotto');
+
 class App {
+  #money = 0;
+
+  static #validateMoney(money) {
+    if (money % LottoConfig.PRICE !== 0) {
+      throw new Error(Message.ERROR_MONEY);
+    }
+  }
+
   play() {}
+
+  #getMoney() {
+    let money;
+    MissionUtils.Console.readLine(
+      Message.ENTER_MONEY_MESSAGE,
+      (answer) => { money = Number(answer); },
+    );
+
+    App.#validateMoney(money);
+    this.#money += money;
+  }
 }
 
 module.exports = App;
