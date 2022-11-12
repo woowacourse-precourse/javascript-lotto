272e069c72ead5ce4d6a941e0fab66bc4af93dd4
Ryong-E <think8867@gmail.com>
2022-11-12 15:55:52 +0900

    refactor(validation): 예외 처리 추가,예외 처리로인한 로직 변경
    - this를 사용하지않는 매서드는 정적으로 변경
    - 보너스 번호 예외처리시 당첨번호를 얻어야하기때문에 controller에서 값을 가져와 비교
    - 출력 문 상수로 변경

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,38 +1,55 @@
+const { ERROR_MASSAGE, NUMBER_LIMIT } = require('./constant');
+
 class Validation {
+  constructor(controller) {
+    this.controller = controller;
+  }
+
   static amountInputValidate(input) {
-    if (Number(input) > 100000) {
-      throw new Error('[ERROR] 일주일 최대 구매 금액은 10만원입니다');
+    if (Number(input) > NUMBER_LIMIT.MAX_PURCHASES) {
+      throw new Error(ERROR_MASSAGE.MAX_PURCHASES);
     }
 
     if (input[0] === '0') {
-      throw new Error('[ERROR] 금액을 잘 못 입력하셨습니다');
+      throw new Error(ERROR_MASSAGE.INCORRECT_INPUT);
     }
+
     input.split('').forEach((number) => {
       if (Number(number) >= 0 && Number(number) <= 9) {
         return;
       }
-      throw new Error('[ERROR] 1~9 숫자만 입력가능합니다');
+
+      throw new Error(ERROR_MASSAGE.POSSIBLE_AMOUNT_NUMBER);
     });
   }
 
   static winningNumberValidate(input) {
-    if (new Set(input.split(',')).size !== 6) {
-      throw new Error('[ERROR] 1~45 서로다른 6개 수를 입력해주세요');
+    if (new Set(input).size !== NUMBER_LIMIT.QUANTITY) {
+      throw new Error(ERROR_MASSAGE.LOTTO_NUMBER_LENGTH);
     }
-    input.split(',').forEach((number) => {
-      if (Number(number) >= 1 && Number(number) <= 45) {
+
+    input.forEach((number) => {
+      if (Number(number) >= NUMBER_LIMIT.MIN_NUMBER && Number(number) <= NUMBER_LIMIT.MAX_NUMBER) {
         return;
       }
-      throw new Error('[ERROR] 1~45의 숫자만 입력가능합니다');
+
+      throw new Error(ERROR_MASSAGE.POSSIBLE_LOTTO_NUMBER);
     });
   }
 
-  static bonusNumberValidate(input) {
-    if (input.length > 2) {
-      throw new Error('[ERROR] 1~45 숫자만 입력가능합니다');
+  bonusNumberValidate(input) {
+    const winningNumber = this.controller.winningNumber.getWinningNumber();
+
+    if (winningNumber.includes(input)) {
+      throw new Error(ERROR_MASSAGE.DUPLICATE_NUMBER);
     }
-    if (!(Number(input) >= 1 && Number(input) <= 45)) {
-      throw new Error('[ERROR] 1~45 숫자만 입력가능합니다');
+
+    if (input.length > 2) {
+      throw new Error(ERROR_MASSAGE.POSSIBLE_LOTTO_NUMBER);
+    }
+
+    if (!(Number(input) >= NUMBER_LIMIT.MIN_NUMBER && Number(input) <= NUMBER_LIMIT.MAX_NUMBER)) {
+      throw new Error(ERROR_MASSAGE.POSSIBLE_LOTTO_NUMBER);
     }
   }
 }
