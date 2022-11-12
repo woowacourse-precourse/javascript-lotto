a610d237094daa74486e49ef84aa292d154ec052
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-11-12 22:56:33 +0900

    Feat: 로또 번호에서 중복되는 번호 검사 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,6 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { removeOverlap } = require("./utils/removeOverlap");
+const { LOTTO_MESSAGE } = require("./constant/errorMessage");
+const { removeDuplication } = require("./utils/removeDuplication");
 
 class Lotto {
   #numbers;
@@ -11,11 +12,11 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(LOTTO_MESSAGE.NOT_SIX_NUMBERS);
     }
 
-    if (removeOverlap(numbers).length !== numbers.length) {
-      throw new Error("[ERROR] 중복된 번호가 존재합니다.");
+    if (removeDuplication(numbers).length !== numbers.length) {
+      throw new Error(LOTTO_MESSAGE.CHECK_DUPLICATION);
     }
   }
 
