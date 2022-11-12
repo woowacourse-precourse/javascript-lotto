9c3c3849283779283cf55e7a5320f4a8de701660
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-12 23:49:29 +0900

    feat : 로또 당첨번호 관련 오류 메시지 상수 선언

--- src/lib/Constants.js
+++ src/lib/Constants.js
@@ -10,7 +10,18 @@ const PRICE_ERROR_MESSAGE = {
   not_valid_answer: '[ERROR] 입력값이 없습니다.',
 };
 
+const WIN_NUMBER_ERROR_MESSAGE = {
+  not_valid_answer: '[ERROR] 입력값이 없습니다.',
+  not_valid_division: '[ERROR] 구분은 쉼표(,)를 사용해주세요.',
+  not_valid_range_number: '[ERROR] 1부터 45까지의 숫자를 입력해주세요',
+  not_valid_overlap_number: '[ERROR] 중복되는 숫자는 입력할 수 없습니다.',
+};
+
+const PRICE_MEASURE = 1000;
+
 module.exports = {
   GAME_MESSAGE,
   PRICE_ERROR_MESSAGE,
+  WIN_NUMBER_ERROR_MESSAGE,
+  PRICE_MEASURE,
 };
