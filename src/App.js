6a63d7256a9c36ae3124786730f37316d101613c
woo-jk <201702042@o.cnu.ac.kr>
2022-11-12 18:34:22 +0900

    feat: App 클래스 금액 입력받는 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const LottoMachine = require("./LottoMachine");
+const WinningCalculator = require("./WinningCalculator");
+const WinningLotto = require("./WinningLotto");
+
 class App {
-  play() {}
+  play() {
+    this.getMoneyInput();
+  }
+
+  getMoneyInput() {
+    MissionUtils.Console.readLine("구입 금액을 입력해 주세요.", (input) => {
+      this.lottoMachine = new LottoMachine(input);
+      this.lottoMachine.print();
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
