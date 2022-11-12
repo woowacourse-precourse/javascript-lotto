34f9e83ef65aeeb28eac8c349f72393791ba269f
sohyun <park.sohyun0430@gmail.com>
2022-11-13 03:40:04 +0900

    feat: validate 함수 구현
    로또 번호 배열이 들어왔을 때 예외사항인 경우 throw문 발생
    1. 로또 번호가 6개가 아닐 때
    2. 로또 번호에 중복이 있을 때
    3. 로또 번호가 1~45 사이가 아닐 때
    4. 로또 번호가 숫자가 아닐 때

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const { MissionUtils } = require('@woowacourse/mission-utils');
+
 class Lotto {
   #numbers;
 
@@ -7,12 +9,24 @@ class Lotto {
   }
 
   validate(numbers) {
+    const numberSet = new Set(numbers);
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
-  }
 
-  // TODO: 추가 기능 구현
+    if (numbers.length !== numberSet.size) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
+    }
+
+    [...numbers].forEach((number) => {
+      if (number <= 0 || number > 45) {
+        throw new Error('[ERROR] 로또 번호는 1~45번 사이여야 합니다.');
+      }
+      if (!Number(number)) {
+        throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+      }
+    });
+  }
 }
 
 module.exports = Lotto;
