1b312d14baa14b8601cfdcac6813f5b8d127951f
osdoonhyun <skw1996@naver.com>
2022-11-12 22:43:39 +0900

    refactor(Validation): 사용자가 입력한 당첨 번호 유효성 검사 메서드 예외 처리 메세지 Constants  모듈 사용해서 변경

--- src/Validation.js
+++ src/Validation.js
@@ -6,7 +6,7 @@ class Validation {
    * @param {string} playerInput - 플레이어의 입력
    */
   checkAmountExceptions(playerInput) {
-    if (Number(playerInput) % 1000 !== 0) {
+    if (Number(playerInput) % NUMBERS !== 0) {
       throw new Error(MESSAGES.ERROR_AMOUNT_UNIT);
     }
   }
@@ -17,13 +17,13 @@ class Validation {
    */
   static validateWinningNumbers(winningNumbers) {
     if (!this.isValidNumberofNumbers(winningNumbers, NUMBERS.LOTTO)) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(MESSAGES.ERROR_INVALID_NUMBER);
     }
     if (!this.isValidRangeOfNumber(winningNumbers)) {
-      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      throw new Error(MESSAGES.ERROR_INVALID_RANGE);
     }
     if (!this.hasUniqueLottoNumber(winningNumbers)) {
-      throw new Error("[ERROR] 로또 번호는 중복이 없어야 합니다.");
+      throw new Error(MESSAGES.ERROR_DUPLICATION_NUMBERS);
     }
   }
 
