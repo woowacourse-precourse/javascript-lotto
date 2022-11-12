f5138fd7507ddacd274012f55cc1700fbbbff264
Buyoung Chung <sunarol@naver.com>
2022-11-12 13:09:10 +0900

    feat: Add budget unit exception

--- src/ExceptionHandler.js
+++ src/ExceptionHandler.js
@@ -1,1 +1,7 @@
-class ExceptionHandler {}
+class ExceptionHandler {
+  budgetUnit(budget) {
+    if (budget % 1000 !== 0) throw new Error("[ERROR] 단위를 정확히 입력해주세요.");
+  }
+}
+
+exports.module = ExceptionHandler;
