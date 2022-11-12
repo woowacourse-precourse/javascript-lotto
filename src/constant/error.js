a2c4b3d78e8e54f534b116b59b7263d86ccb0f71
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-12 16:44:20 +0900

    📌feat: 로또 게임에서 사용되는 error 상수 생성

---
+++ src/constant/error.js
@@ -0,0 +1,17 @@
+const LOTTO_1 = "[ERROR] 로또 번호의 개수는 6개 이하입니다.";
+const LOTTO_2 = "[ERROR] 로또 번호는 중복될 수 없습니다.";
+const MONEY_1 = "[ERROR] 1000원 단위의 금액을 입력해 주세요.";
+const MONEY_2 = "[ERROR] 금액이 부족합니다.";
+const WINNING_1 = "[ERROR] 당첨 번호는 6개여야 합니다.";
+const WINNING_2 = "[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.";
+const WINNING_3 = "[ERROR] 당첨 번호는 중복될 수 없습니다.";
+
+module.exports = {
+  LOTTO_1,
+  LOTTO_2,
+  MONEY_1,
+  MONEY_2,
+  WINNING_1,
+  WINNING_2,
+  WINNING_3,
+};
