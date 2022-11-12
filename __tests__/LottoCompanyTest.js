240a5d05887babcef766c5c72674094d60421f5e
하지훈 <jnh03336@gmail.com>
2022-11-12 19:09:25 +0900

    feat(lottoCompany): 오름차순인 두 배열을 비교하여 같은 번호의 개수를 반환

--- __tests__/LottoCompanyTest.js
+++ __tests__/LottoCompanyTest.js
@@ -2,9 +2,18 @@ const LottoCompany = require("../src/LottoCompany");
 
 describe("로또 발행 회사 테스트", () => {
   test("금액에 맞는 개수만큼 로또를 발행한다.", () => {
-    const lottos = LottoCompany.publishLottos(2000);
-    const lottos2 = LottoCompany.publishLottos(5000);
+    const lottoCompany = new LottoCompany(1000);
+    const lottos = lottoCompany.publishLottos(2000);
+    const lottos2 = lottoCompany.publishLottos(5000);
     expect(lottos.length).toBe(2);
     expect(lottos2.length).toBe(5);
+  });
+
+  test("오름차순으로 정렬된 두 개의 배열이 주어질 떄, 같은 수의 개수를 찾는다.", () => {
+    const array1 = [1, 4, 9, 10, 16, 29];
+    const array2 = [4, 5, 9, 11, 29, 31];
+    expect(LottoCompany.countSameNumbersOfAscSortedArrays(array1, array2)).toBe(
+      3
+    );
   });
 });
--- src/LottoCompany.js
+++ src/LottoCompany.js
@@ -4,6 +4,10 @@ const Lotto = require("./Lotto");
 class LottoCompany {
   #lottoPrice;
 
+  #winningNumbers;
+
+  #bonusNumber;
+
   constructor(lottoPrice) {
     this.#lottoPrice = lottoPrice;
   }
@@ -12,14 +16,34 @@ class LottoCompany {
     const lottoCount = money / this.#lottoPrice;
     const lottos = Array(lottoCount);
     for (let i = 0; i < lottoCount; i += 1) {
-      lottos[i] = new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6));
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      numbers.sort((a, b) => a - b);
+      lottos[i] = new Lotto(numbers);
     }
     return lottos;
   }
 
   makeWinningNumbers() {}
 
+  makeBonusNumber() {}
+
   notifyLottoResult() {}
+
+  static countSameNumbersOfAscSortedArrays(ascSortedNums1, ascSortedNums2) {
+    let i = 0;
+    let j = 0;
+    let count = 0;
+    while (i < ascSortedNums1.length && j < ascSortedNums2.length) {
+      if (ascSortedNums1 === ascSortedNums2[j]) {
+        count += 1;
+        i += 1;
+        j += 1;
+      }
+      if (ascSortedNums1 < ascSortedNums2[j]) i += 1;
+      if (ascSortedNums1 > ascSortedNums2[j]) j += 1;
+    }
+    return count;
+  }
 }
 
 module.exports = LottoCompany;
