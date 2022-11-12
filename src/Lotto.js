45471cd1b61c56cf97cc74ec00b3c1b8cb0e40f1
kinoasm <kinoasm@gmail.com>
2022-11-12 23:28:07 +0900

    feat: Lotto 입력 오류 처리
    테스트 입력대로 중복케이스 오류 처리 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,6 +12,12 @@ class Lotto {
         if (numbers.length !== 6) {
             throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        let filtered = numbers.filter((_, i, arr) => arr.indexOf(_) === i);
+        if (filtered.length !== 6) {
+            throw new Error(
+                "[ERROR] 로또 번호에 중복된 숫자가 있으면 안됩니다."
+            );
+        }
     }
 
     // TODO: 추가 기능 구현
