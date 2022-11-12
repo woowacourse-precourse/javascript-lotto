a5dd9fb4087f9f1bb6d1e8977df465c4fbe92305
js43o <js43o@naver.com>
2022-11-12 17:43:01 +0900

    feat(App): add winning/bonus number setters.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
+
+const console = MissionUtils.Console;
+
 class App {
-  play() {}
+  #winningNumbers;
+  #bonusNumber;
+
+  setWinningNumbers(numbers) {
+    Lotto.validateNumbers(numbers);
+    this.#winningNumbers = numbers;
+  }
+
+  setBonusNumber(number) {
+    this.#bonusNumber = number;
+  }
+
+  play() {
+    console.print('구입 금액을 입력해주세요.');
+    console.close();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
