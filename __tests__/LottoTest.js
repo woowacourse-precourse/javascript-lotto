cca5d9493f037a6a30c54176a36723d1a9134471
myung-j-p <jmpack97@gmail.com>
2022-11-12 17:33:20 +0900

    Test: Lotto 테스트 케이스 추가
    - 보너스 번호 입력시 당첨번호와 중복되면 예외 발생

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -22,5 +22,10 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
+  test("보너스 번호에 당첨번호와 중복된 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      lotto.validateBonus([1, 2, 3, 4, 5, 6],[1]);
+    }).toThrow("[ERROR]");
+  });
 
 });
