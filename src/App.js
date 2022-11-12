880411a4b8538e42290032fede92268d1864c9ac
devMingu <rooster100@naver.com>
2022-11-12 13:38:49 +0900

    feat: 구입금액을 입력받는 메소드

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const Lotto = require('./Lotto');
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor () { 
+    // const lotto = new Lotto();
+  }
+
+  async play() {}
+
+  userInput() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요. \n', async (userMoney) => {
+      this.numberOfAvailablePurchase(userMoney);
+      resolve();
+    });     
+  }
+
+  
 }
 
 module.exports = App;
