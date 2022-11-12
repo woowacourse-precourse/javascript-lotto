221fe27db07187f1f85e7e7e9082beb365b84885
daal2quf <daal2quf@gmail.com>
2022-11-13 04:44:15 +0900

    fix: 예외처리 조건 수정 및 fix typo
    add:
    userInputMoneyValue() 의 조건중 moneyValue > 0 을 moneyValue === 0 으로 수정
    오타 exeption을 exception으로 수정

--- src/ExeptionCheck.js
+++ src/ExeptionCheck.js
@@ -1,13 +1,13 @@
 const App = require("./App");
+const { Console } = require("@woowacourse/mission-utils");
 
-
-class ExeptionCheck {
+class ExceptionCheck {
 
   userInputMoneyValue(moneyValue) {
-    if (moneyValue % 1000 !== 0 || moneyValue > 0) {
+    if (moneyValue % 1000 !== 0 || moneyValue === 0) {
       throw new Error("[ERROR] 구매 금액은 1,000 단위로 입력해주세요");
     }
   }
 }
 
-module.exports = ExeptionCheck;
+module.exports = ExceptionCheck;
