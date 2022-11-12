a6d49bf72743bf92c85a01f3a6cb616366ae2c4f
devMingu <rooster100@naver.com>
2022-11-12 14:08:53 +0900

    feat: 중복된 예외사항 처리 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,6 +3,7 @@ class Lotto {
 
   constructor(numbers) {
     this.validate(numbers);
+    this.overlay(numbers);
     this.#numbers = numbers;
   }
 
@@ -11,6 +12,17 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
+
+  overlay(numbers) {
+    const set = new Set(numbers);
+    numbers = [...set];
+    
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 중복이 포함되서는 안됩니다.");
+    }
+  }
+
+  
 
   // TODO: 추가 기능 구현
 }
