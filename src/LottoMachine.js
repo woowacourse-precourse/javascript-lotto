e011a0ec496911cbaa26f3750c494845526b3795
jsh3418 <albert5428@gmail.com>
2022-11-12 19:25:38 +0900

    feat(apply): implement exception handling when user input not a number

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -6,8 +6,18 @@ class LottoMachine {
 
   getPayment() {
     Console.readLine(MESSAGE.REQUEST.PAYMENT, (payment) => {
+      this.validatePayment(payment);
+
       this.#payment = payment;
     });
+  }
+
+  validatePayment(payment) {
+    if (this.isNotANumber(payment)) throw new Error(MESSAGE.ERROR.PAYMENT_MUST_BE_NUMBER);
+  }
+
+  isNotANumber(number) {
+    return isNaN(Number(number));
   }
 }
 
--- src/constants/message.js
+++ src/constants/message.js
@@ -2,6 +2,10 @@ const MESSAGE = {
   REQUEST: {
     PAYMENT: "구입금액을 입력해 주세요.\n",
   },
+
+  ERROR: {
+    PAYMENT_MUST_BE_NUMBER: "[ERROR] 구입금액을 입력할 때 숫자를 입력해주세요!",
+  },
 };
 
 module.exports = MESSAGE;
