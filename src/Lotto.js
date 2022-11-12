26e07fe36143a02297a44443f857b5ac17fe4d36
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-12 17:28:53 +0900

    ♻️refactor: 코드 리펙토링

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,4 @@
-const { COMMENT, ERROR } = require("./constant");
+const { COMMENT } = require("./constant");
 const MissionUtils = require("@woowacourse/mission-utils");
 const WinningNumber = require("./WinningNumber");
 
@@ -6,24 +6,24 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.validateLotto(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
+  validateLotto(numbers) {
     if (numbers.length !== 6) {
-      throw new Error(ERROR.LOTTO_1);
+      throw new Error("[ERROR] 로또 번호의 개수는 6개 이하입니다.");
     }
 
     const uniqueNumbers = new Set(numbers);
     if ([...uniqueNumbers].length !== 6) {
-      throw new Error(ERROR.LOTTO_2);
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
   }
 
   getMoneyOfLottos() {
     MissionUtils.Console.readLine(COMMENT.PURCHASE, (money) => {
-      if (this.#checkValidMoney(money)) {
+      if (this.#validateMoney(money)) {
         const numberOfLottos = money / 1000;
         const lottos = this.#createLottos(numberOfLottos);
 
@@ -33,12 +33,12 @@ class Lotto {
     });
   }
 
-  #checkValidMoney(money) {
+  #validateMoney(money) {
     if (money < 1000) {
-      throw new Error(ERROR.MONEY_2);
+      throw new Error("[ERROR] 금액이 부족합니다.");
     }
     if (money % 1000 != 0) {
-      throw new Error(ERROR.MONEY_1);
+      throw new Error("[ERROR] 1000원 단위의 금액을 입력해 주세요.");
     }
     return true;
   }
---
+++ src/constant/index.js
@@ -0,0 +1,5 @@
+const COMMENT = require("./comment");
+const REGEX = require("./regex");
+const REWARD = require("./reward");
+
+module.exports = { COMMENT, REGEX, REWARD };
