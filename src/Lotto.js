8373f7ba797ba82dc0c1b9bb47fc20656621c299
kangju2000 <kangju2000@naver.com>
2022-11-12 16:31:52 +0900

    feat: Lotto 클래스의 validate 메서드 구현 - 입력된 로또 번호가 유효한 값인지 확인함

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const ERROR = require('./constants/error');
+
 class Lotto {
   #numbers;
 
@@ -7,12 +9,20 @@ class Lotto {
   }
 
   validate(numbers) {
+    const uniqueNumbers = [...new Set(numbers)];
+
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR.SIX_NUMBERS);
+    }
+
+    if (uniqueNumbers.length !== 6) {
+      throw new Error(ERROR.UNIQUE_NUMBERS);
+    }
+
+    if (uniqueNumbers.some((number) => number < 1 && number > 45)) {
+      throw new Error(ERROR.FROM1TO45_NUMBERS);
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
