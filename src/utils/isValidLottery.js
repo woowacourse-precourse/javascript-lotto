c516a952af9f4e5b6c2cffe7e14548f8e845fcda
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-12 18:58:59 +0900

    refactor: isValidLottery 함수 반환값 문자열로 변경

--- src/utils/isValidLottery.js
+++ src/utils/isValidLottery.js
@@ -1,8 +1,10 @@
+const { EXCEPTION_REASON } = require('../constants/constants');
+
 const isValidLottery = (lottery) => {
   const checkLotteryLength = new Set(lottery).size;
-  if (checkLotteryLength !== 6) return false;
+  if (checkLotteryLength !== 6) return EXCEPTION_REASON.INPUT_LENGTH_ERROR;
   for (let lotteryIndex = 0; lotteryIndex < lottery.length; lotteryIndex += 1) {
-    if (isNaN(lottery[lotteryIndex])) return false;
+    if (isNaN(lottery[lotteryIndex])) return EXCEPTION_REASON.INPUT_OVERLAPPED;
   }
   return true;
 };
