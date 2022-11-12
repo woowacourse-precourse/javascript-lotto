c8ca4e3b4c959db9f53218d6c323f40910fac137
confidential-nt <youyoy689@gmail.com>
2022-11-13 05:35:06 +0900

    feat(요구사항): 요구사항1. 사용자로부터 구입금액 입력받기
    Console.readLine을 이용하여 사용자로부터 구입금액을 입력받는 기능을 구현하였다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  // money;
+
+  play() {
+    this.makePayment();
+  }
+
+  readLine(message, callback) {
+    Console.readLine(message, callback);
+  }
+
+  print(message) {
+    Console.print(message);
+  }
+
+  makePayment() {
+    this.readLine("구임금액을 입력해주세요.", (value) => {
+      const money = value;
+    });
+  }
 }
 
+new App().play();
 module.exports = App;
