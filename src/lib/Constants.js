84ce0366ac19acd5a1b88bd5f7533085ce9d4cd3
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-13 00:30:08 +0900

    feat : 게임 메시지 상수에 로또 당첨 기준 추가

--- src/lib/Constants.js
+++ src/lib/Constants.js
@@ -2,6 +2,11 @@ const GAME_MESSAGE = {
   input_price: '구입금액을 입력해 주세요.\n',
   input_win_number: '당첨 번호를 입력해 주세요.\n',
   input_bonus_number: '보너스 번호를 입력해 주세요.\n',
+  result_three_match: '3개 일치 (5,000원) - ',
+  result_four_match: '4개 일치 (50,000원) - ',
+  result_five_match: '5개 일치 (1,500,000원) - ',
+  result_five_bonus_match: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  result_six_match: '6개 일치 (2,000,000,000원) - ',
 };
 
 const PRICE_ERROR_MESSAGE = {
