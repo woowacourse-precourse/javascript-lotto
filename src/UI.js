207b6ae9ce88512cbf5fefab17cb726749c1a51b
Sijun <sionwer4@naver.com>
2022-11-12 16:14:52 +0900

    fix: output print random number fix with another making sentence function

--- src/UI.js
+++ src/UI.js
@@ -26,21 +26,38 @@ const bonusNumber = () => {
 
 const printBuying = (count) => {
   MissionUtils.Console.print(`${count}개를 구매했습니다.`);
-}
+};
 
 const printGameNumber = (games) => {
-  for(game of games){
-    MissionUtils.Console.print(game);
+  for (const game of games) {
+    const str = makingSentence(game);
+    MissionUtils.Console.print(str);
   }
+};
+
+const makingSentence = (arr) => {
+  str = "[";
+  arr.forEach((element) => {
+    const seperator = ", ";
+    if (str.length == 1) {
+      str = str + element;
+    } else {
+      str = str + seperator + element;
+    }
+  });
+  str = str + "]";
+  return str;
 };
 
 const printWinning = (rank) => {
   MissionUtils.Console.print(`3개 일치 (5,000원) - ${rank[0]}개`);
   MissionUtils.Console.print(`4개 일치 (50,000원) - ${rank[1]}개`);
   MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${rank[2]}개`);
-  MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank[3]}개`);
+  MissionUtils.Console.print(
+    `5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank[3]}개`
+  );
   MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${rank[4]}개`);
-}
+};
 
 module.exports = {
   lottoBuy,
@@ -48,5 +65,5 @@ module.exports = {
   bonusNumber,
   printBuying,
   printGameNumber,
-  printWinning
+  printWinning,
 };
