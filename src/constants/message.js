df988eb7fc78e41842bcc1eb4904bb0e95959dec
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-12 19:22:16 +0900

    feat: 보너스번호 입력 요청 메시지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -4,10 +4,13 @@ const PURCHASE_AMOUNT = `구입금액을 입력해 주세요.
 `;
 const WINNING_NUMBERS = `당첨 번호를 입력해 주세요.
 `;
+const BONUS_NUMBER = `보너스 번호를 입력해 주세요.
+`;
 
 const REQUEST_MESSAGE = {
   PURCHASE_AMOUNT,
   WINNING_NUMBERS,
+  BONUS_NUMBER,
 };
 
 const INVALID_INPUT_TYPE = "[ERROR] 숫자 외의 문자를 입력할 수 없습니다. 게임 종료!";
