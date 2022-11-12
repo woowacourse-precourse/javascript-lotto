6c08732b10cd51a07daa28ff1edc63eaba32ac66
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-12 23:18:41 +0900

    feat :  로또 금액 입력값 유효성 검사 - empty 확인

--- src/Validation/PriceValidation.js
+++ src/Validation/PriceValidation.js
@@ -1,4 +1,5 @@
 const Validation = require('./index');
+const { PRICE_ERROR_MESSAGE } = require('../lib/Constants');
 
 class PriceValidation extends Validation {
   constructor(answer) {
@@ -6,7 +7,16 @@ class PriceValidation extends Validation {
     this.answer = answer;
   }
 
-  validate() {}
+  validate() {
+    this.checkIsEmpty();
+  }
+
+  checkIsEmpty() {
+    if (this.answer === null || this.answer === undefined || this.answer === '') {
+      throw new Error(PRICE_ERROR_MESSAGE.not_valid_answer);
+    }
+    return true;
+  }
 }
 
 module.exports = PriceValidation;
