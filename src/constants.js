4a3c5bb01cd5e8d9d4c67ff2afedc612438ec8e4
xilucks <siun0331@gmail.com>
2022-11-12 17:55:53 +0900

    feat: 상수 메세지 설정

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+  MESSAGE_INPUT_PURCHASE_AMOUNT: `구입금액을 입력해 주세요.`,
+  MESSAGE_INPUT_WINNING_NUMBERS: `당첨 번호를 입력해 주세요.`,
+  MESSAGE_INPUT_BONUS_NUMBER: `보너스 번호를 입력해 주세요.`,
+  MESSAGE_OUTPUT_WINNING_STATISTICS: `당첨 통계 \n---`,
+});
