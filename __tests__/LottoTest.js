b80653db99eb10d609b5e2a61c5dfc12de7511c6
nsyeon <lovepny@gmail.com>
2022-11-12 20:33:17 +0900

    test(LottoTest): 사용자가 구매한 로또 번호와 당첨 번호를 비교

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
 const Lotto = require("../src/Lotto");
+const Comparison = require("../src/ComparisonOfWinningNumbers");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -15,4 +16,8 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("사용자가 구매한 로또 번호와 당첨 번호를 비교", () => {
+    const comparison = new Comparison([2, 4, 6, 8, 10, 12], [1, 2, 3, 4, 5, 6]);
+    expect(comparison.Comparison()).toEqual([2, 4, 6]);
+  });
 });
