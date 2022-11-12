5e386eaa097a315ade13a90057b3abfe5a155036
JSH99 <tjgus9966@naver.com>
2022-11-12 14:42:14 +0900

    feat(Lotto): 당첨 번호 유효성 검사 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,47 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if (isDuplicated(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않는 숫자 6개여야 합니다.");
+    }
+
+    for(let i = 0; i < numbers.length; i++) {
+      const number = Number(numbers[i]);
+      
+      if (isNaN(number)) {
+        throw new Error("[ERROR] 숫자만 입력해야 합니다.");
+      }
+      
+      if (!Number.isInteger(number)) {
+        throw new Error("[ERROR] 1부터 45 사이의 정수여야 합니다.");
+      }
+      
+      if (!isInRange(number)) {
+        throw new Error("[ERROR] 1부터 45 사이의 숫자여야 합니다.");
+      }
+    }
+
   }
 
-  // TODO: 추가 기능 구현
+  isDuplicated(numbers) {
+    const set = new Set(numbers);
+    
+    if(set.size !== 6) {
+      return true;
+    }
+    return false;
+  }
+
+  isInRange(number) {
+    if(number >=1 && number <= 45) {
+        return true;
+    }
+    return false;
+  }
+
+
+  
 }
 
 module.exports = Lotto;
