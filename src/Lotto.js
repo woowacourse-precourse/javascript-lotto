886f12c4dce100b8c5edeac74a8b0a9cbdca375e
kangju2000 <kangju2000@naver.com>
2022-11-12 23:39:28 +0900

    feat: Lotto 클래스의 getRanking 메서드 구현 - Lotto 클래스의 constructor에서 numbers를 문자에서 숫자로 바꿔주는 로직 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,3 @@
-const ERROR = require('./constants/error');
 const Validator = require('./Validator');
 
 class Lotto {
@@ -6,11 +5,24 @@ class Lotto {
 
   constructor(numbers) {
     Validator.validateLottoNumbers(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.map(Number);
   }
 
   getLottoNumbers() {
     return this.#numbers;
+  }
+
+  getRanking(winningLotto) {
+    const matchCount = winningLotto.get('당첨 번호').reduce((acc, cur) => {
+      if (this.#numbers.includes(cur)) return acc + 1;
+      return acc;
+    }, 0);
+
+    if (matchCount === 6) return 1;
+    if (matchCount === 5 && this.#numbers.includes(winningLotto.get('보너스 번호'))) return 2;
+    if (matchCount === 5) return 3;
+    if (matchCount === 4) return 4;
+    if (matchCount === 3) return 5;
   }
 }
 
