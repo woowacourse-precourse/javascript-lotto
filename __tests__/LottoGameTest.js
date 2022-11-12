0fd38d4ca2f4d60c0077bbc9be842c19bc60c1f9
Jin1won <wlsdnjs16521@gmail.com>
2022-11-12 21:42:39 +0900

    test: 로또 구입 입력값 1000단위 확인 테스트 코드 작성

--- __tests__/LottoGameTest.js
+++ __tests__/LottoGameTest.js
@@ -1,9 +1,15 @@
 const LottoGame = require("../src/LottoGame");
 
-describe("LottoGame 클래스 - isPurchaseAmountValid", () => {
-  test("문자 입력 예외처리", () => {
+describe("LottoGame 클래스 isPurchaseAmountValid 함수 테스트", () => {
+  test("문자를 입력하면 예외가 발생한다.", () => {
     expect(() => {
       new LottoGame().isPurchaseAmountValid("abc");
     }).toThrow("[ERROR] 숫자를 입력해주세요.");
+  });
+
+  test("1,000으로 나누어 떨어지지 않는 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new LottoGame().isPurchaseAmountValid(8750);
+    }).toThrow("[ERROR] 1,000으로 나누어 떨어지는 금액을 입력해주세요.");
   });
 });
