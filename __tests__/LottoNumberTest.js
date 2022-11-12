dbe399867b31c33b6445ec01f777bc84c154cf03
hwanfront <kim015jh@gmail.com>
2022-11-12 21:57:15 +0900

    test(LottoNumber): LottoNumber에 대한 예외처리 방식의 변경에 맞게 테스트 수정

--- __tests__/LottoNumberTest.js
+++ __tests__/LottoNumberTest.js
@@ -3,27 +3,21 @@ const LottoNumberRangeException = require('../src/exception/lotto/LottoNumberRan
 const NotNumberException = require('../src/exception/NotNumberException');
 
 describe("LottoNumber 클래스 테스트", () => {
-  test("로또 숫자가 조건에 만족하는 경우", () => {
-    expect(LottoNumber.of(1).getNumber()).toEqual(1);
-    expect(LottoNumber.of(23).getNumber()).toEqual(23);
-    expect(LottoNumber.of(45).getNumber()).toEqual(45);
-  });
-
   test("로또 숫자가 숫자가 아닌 경우", () => {
     expect(() => {
-      new LottoNumber('a');
+      LottoNumber.validate('a');
     }).toThrow(NotNumberException);
     expect(() => {
-      new LottoNumber([]);
+      LottoNumber.validate([]);
     }).toThrow(NotNumberException);
   });
 
   test("로또 숫자가 범위를 벗어난 경우", () => {
     expect(() => {
-      new LottoNumber(0);
+      LottoNumber.validate(0);
     }).toThrow(LottoNumberRangeException);
     expect(() => {
-      new LottoNumber(46);
+      LottoNumber.validate(46);
     }).toThrow(LottoNumberRangeException);
   });
 });
