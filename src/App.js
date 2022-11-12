50353e8884c3a98fd15d46ef776a495c3b56ffca
osdoonhyun <skw1996@naver.com>
2022-11-12 23:25:43 +0900

    feat(App,Validation): 기능목록[4] 보너스 입력 받는 기능 & 사용자 잘못된 보너스 번호 입력 시 예외처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGES } = require("./Constants");
 const Game = require("./Game");
 const Lotto = require("./Lotto");
-const Exception = require("./Exception");
-const { MESSAGES } = require("./Constants");
+const Validation = require("./Validation");
 
 class App {
   constructor() {
     this.game = new Game();
     this.lotto = new Lotto();
-    this.exception = new Exception();
   }
 
   purchaseLotto() {
     Console.readLine(MESSAGES.ENTER_PURCHASE_AMOUNT, (amount) => {
-      this.exception.checkAmountExceptions(amount);
+      Validation.checkAmountExceptions(amount);
       const lottoQuantity = this.game.countLottoTickets(amount);
 
       this.game.issueLotto(lottoQuantity);
     });
   }
 
+  inputWinningNumbers() {
+    Console.readLine(MESSAGES.INPUT_WINNING_NUMBERS, (input) => {
+      //로또 번호 유효성 검사 Lotto에서 에러시 throw
+      const winningNumbers = input.split(",");
+      console.log("winningNumbers", winningNumbers);
+      Validation.validateWinningNumbers(winningNumbers);
+
+      return winningNumbers;
+    });
+  }
+
+  inputBonusNumber() {
+    Console.readLine(MESSAGES.INPUT_BONUS_NUMBER, (bonusNumber) => {
+      Validation.validateBonusNumber(bonusNumber);
+
+      return bonusNumber;
+    });
+  }
+
   play() {
     this.purchaseLotto();
+    this.inputWinningNumbers();
+    this.inputBonusNumber();
   }
 }
 
--- src/Validation.js
+++ src/Validation.js
@@ -6,14 +6,15 @@ class Validation {
    * @param {string} playerInput - 플레이어의 입력
    */
   checkAmountExceptions(playerInput) {
-    if (Number(playerInput) % NUMBERS !== 0) {
+    if (Number(playerInput) % 1000 !== 0) {
       throw new Error(MESSAGES.ERROR_AMOUNT_UNIT);
     }
   }
 
   /**
-   * 플레이어가 게임 중 당첨 번호가 형식에 어긋날 경우 예외를 발생시킵니다.
+   * 플레이어가 게임 중 당첨 번호, 보너스 번호가 형식에 어긋날 경우 예외를 발생시킵니다.
    * @param {array} winningNumbers - 플레이어가 입력한 당첨 번호
+   * @param {string} bonusNumber - 플레이어가 입력한 보너스 번호
    */
   static validateWinningNumbers(winningNumbers) {
     if (!this.isValidNumberofNumbers(winningNumbers, NUMBERS.LOTTO)) {
@@ -27,13 +28,23 @@ class Validation {
     }
   }
 
+  static validateBonusNumber(bonusNumber) {
+    if (!this.isValidNumberofNumbers(bonusNumber, NUMBERS.BONUS)) {
+      throw new Error(MESSAGES.ERROR_INVALID_NUMBER);
+    }
+    if (!this.isValidRangeOfNumber(bonusNumber)) {
+      throw new Error(MESSAGES.ERROR_INVALID_RANGE);
+    }
+  }
+
   /**
    * 플레이어가 게임 중 입력한 당첨 번호가 형식에 어긋날 경우 예외를 발생시킵니다.
    * @param {array} inputNumbers - 당첨 번호
    * @param {number} number - 당첨 번호의 갯수
    */
   static isValidNumberofNumbers(inputNumbers, number) {
-    return inputNumbers.length === number;
+    console.log("inputNumbers,number", inputNumbers, number);
+    if (inputNumbers) return inputNumbers.length === number;
   }
 
   static isValidRangeOfNumber(inputNumbers) {
