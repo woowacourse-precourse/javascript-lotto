8a14fd8d504d252f3ef461cc62a951f95b99f0bd
LEEJW1953 <jiwonlee1953@naver.com>
2022-11-12 23:22:41 +0900

    feat: 1부터 45까지의 숫자 중 6개를 고르고 오름차순으로 정렬하는 메서드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,9 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
+  }
+
+  lottoGame() { // 1부터 45까지의 숫자 중 6개를 고르고 오름차순으로 정렬하는 메서드
+    const LOTTO_RESULT = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    LOTTO_RESULT.sort((a, b) => {
+      return a - b;
+    });
+    return LOTTO_RESULT;
   }
 
   validate(numbers) {
