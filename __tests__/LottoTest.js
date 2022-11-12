b5d4327a1515a603a8bfb2f9584b6b12ebc02b85
minjaeKim <smallkdb@gmail.com>
2022-11-13 03:11:47 +0900

    feat(test): Lotto 클래스에 대한 테스트 코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -14,5 +14,15 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또 번호에 1~45범위가 아닌 숫자가 있으면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 55]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호를 가지고 오는 getNumbers 매서드가 정상적으로 동작한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const result = lotto.getNumbers();
+    expect(() => result.toEqual([1, 2, 3, 4, 5, 6]));
+  });
 });
