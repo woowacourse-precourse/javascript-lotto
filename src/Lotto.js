80ecd1ba0de393ea42788cb02ebaf646c77fddad
s0ojin <onepst@hanyang.ac.kr>
2022-11-12 22:43:02 +0900

    feat(Lotto.js) : 입력값이 숫자인지 확인하는 메서드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,13 +2,20 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.isSix(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
+  isSix(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+  }
+
+  isNumber(numbers) {
+    const regex = /^[0-9]+$/;
+    if (!regex.test(numbers)) {
+      throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
     }
   }
 
