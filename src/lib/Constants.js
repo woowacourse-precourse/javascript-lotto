f93b53f9ea26cfd4b268e529170b20cedce2b6db
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-12 23:15:47 +0900

    feat : 로또 입력 금액 관련 오류 메시지 상수 선언

--- src/lib/Constants.js
+++ src/lib/Constants.js
@@ -4,6 +4,13 @@ const GAME_MESSAGE = {
   input_bonus_number: '보너스 번호를 입력해 주세요.\n',
 };
 
+const PRICE_ERROR_MESSAGE = {
+  not_valid_number: '[ERROR] 숫자를 입력해주세요.',
+  not_valid_unit_number: '[ERROR] 1000원 단위로 입력해주세요.',
+  not_valid_answer: '[ERROR] 입력값이 없습니다.',
+};
+
 module.exports = {
   GAME_MESSAGE,
+  PRICE_ERROR_MESSAGE,
 };
