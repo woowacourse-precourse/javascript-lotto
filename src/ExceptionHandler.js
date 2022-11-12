133a89246545ae2432e0dcc5d4c9eaa167e858b3
Buyoung Chung <sunarol@naver.com>
2022-11-12 13:12:01 +0900

    refactor: change methods to static methods

--- src/ExceptionHandler.js
+++ src/ExceptionHandler.js
@@ -1,8 +1,8 @@
 class ExceptionHandler {
-  budgetUnit(budget) {
+  static budgetUnit(budget) {
     if (budget % 1000 !== 0) throw new Error("[ERROR] 단위를 정확히 입력해주세요.");
   }
-  winningNumberRedundancy(winningNumber) {
+  static winningNumberRedundancy(winningNumber) {
     const checkSet = new Set(winningNumber.split(","));
     if ([...checkSet].length < 6) throw new Error("[ERROR] 당첨 번호를 중복없이 입력해주세요.");
   }
