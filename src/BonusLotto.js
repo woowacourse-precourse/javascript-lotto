54e7155238ee29ede7b3deda7526c68e481ff018
minjaeKim <smallkdb@gmail.com>
2022-11-13 00:21:41 +0900

    feat(src): 로또 숫자 범위에 대한 유효성 검사 추가
    Lotto와 BonusLotto 클래스의 validate 메서드에 유효성 검사를 추가하였습니다.
    - 입력값이 1~45까지의 숫자인지 확인합니다.

--- src/BonusLotto.js
+++ src/BonusLotto.js
@@ -7,6 +7,10 @@ class BonusLotto {
   }
 
   static validate(number, winningNumbers) {
+    const regExp = /^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$/;
+    if (!regExp.test(number)) {
+      throw new Error('[ERROR] 로또 번호는 1~45범위의 숫자여야 합니다.');
+    }
     if (winningNumbers.includes(number)) {
       throw new Error('[ERROR] 보너스 번호가 당첨 번호에 중복되는 값입니다.');
     }
--- src/Lotto.js
+++ src/Lotto.js
@@ -7,6 +7,12 @@ class Lotto {
   }
 
   static validate(numbers) {
+    const regExp = /^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$/;
+    numbers.forEach((number) => {
+      if (!regExp.test(number)) {
+        throw new Error('[ERROR] 로또 번호는 1~45범위의 숫자여야 합니다.');
+      }
+    });
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
