f5174661925f8d81617fac443f082dff444773a7
kimeuns <dmstn2006@gmail.com>
2022-11-12 15:00:36 +0900

    당첨 번호 입력

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ const Buyer = require('./Buyer.js')
 const {Console, Random} = require("@woowacourse/mission-utils");
 
 const ASK_LOTTO_PRICE = "구입금액을 입력해 주세요.";
-const ASK_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
+const ASK_WINNING_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
 const ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
 class App {
   lottoCount;
+  winningLottoNumber;
 
   buyLotto(price){
     const buyer = new Buyer(price);
     this.lottoCount = buyer.lottoCount;
   }
 
-  printbuyLotto(lottoCount){
-    Console.print(`\n${lottoCount}를 구매했습니다`)
+  printLottoCount(lottoCount){
+    Console.print(`\n${lottoCount}개를 구매했습니다`)
   }
 
   issuedLottoNumber() {
@@ -27,16 +28,28 @@ class App {
   }
 
   printLottoNumber(lottoNumber) {
-    return Console.print(`[${lottoNumber.join(',')}]`)
+    return Console.print(`[${lottoNumber.join(', ')}]`)
+  }
+
+  printIssuendLotto(lottoCount){
+    for(let i =0;i<lottoCount;i++){
+      let lottoNumber = this.sortLottoNumberInAscendignOrder(this.issuedLottoNumber())
+      this.printLottoNumber(lottoNumber)
+    }
+  }
+
+  getWinningLottoNumber(){
+    Console.print(ASK_WINNING_LOTTO_NUMBER);
+    Console.readline("", number=> this.winningLottoNumber=number.split(","));
   }
 
   play() {
     Console.print(ASK_LOTTO_PRICE);
-    Console.readLine('',price => this.buyLotto(price))
+    // Console.readLine('',price => this.buyLotto(price))
   }
 }
 
-const app = new App();
-app.play();
+// const app = new App();
+// app.play();
 
-// module.exports = App;
+module.exports = App;
