c3262645f708df1571c054ab5b77023b2a939e66
wonkyung <mwk0725@gmail.com>
2022-11-12 21:15:10 +0900

    feat: 구입 금액 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const Console = MissionUtils.Console;
+
 class App {
-  play() {}
+  constructor() {
+    this.LOTTO_TICKET_PRICE = 1000;
+    this.money = 0;
+  }
+  play() {
+    this.injectMoney();
+  }
+
+  injectMoney() {
+    Console.readLine(`구입금액을 입력해 주세요.\n`, money => {
+      this.money += Number(money);
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
