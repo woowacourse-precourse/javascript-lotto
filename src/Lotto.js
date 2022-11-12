9a7afc36a54fb6fb8e77e66940ef052f578a270a
Sallyvely <tpdus4159@naver.com>
2022-11-12 19:17:38 +0900

    feat : 당첨번호 중복을 확인하는 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,15 +4,23 @@ class Lotto {
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
+    this.checkUniqueRange(numbers);
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    
   }
-
   // TODO: 추가 기능 구현
+  checkUniqueRange(winningArr){
+    const setWinningArr=new Set(winningArr);
+    if(setWinningArr.size==6){
+        return true;
+    }
+    throw new Error("[ERROR] 로또 번호는 중복되면 안됩니다.");
+}
 }
 
 module.exports = Lotto;
