171920efa9d07b080270e9e6b8b130e5f7a303f2
chex <chex417@naver.com>
2022-11-12 23:54:06 +0900

    fix: 로또번호 유효성 검사 순서 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -19,14 +19,14 @@ class Lotto {
   }
 
   validate(numbers) {
+    if (!Util.hasNElements(numbers, LOTTO_LENGTH)) {
+      throw new Error(ERROR_MESSAGE.INVALID_LENGTH_LOTTO);
+    }
     if (!Util.isBetween(numbers, LOTTO_START, LOTTO_END)) {
       throw new Error(ERROR_MESSAGE.OUT_OF_RANGE_LOTTO);
     }
     if (Util.hasDuplicateElements(numbers)) {
       throw new Error(ERROR_MESSAGE.HAS_DUPLICATE_NUMBERS);
-    }
-    if (!Util.hasNElements(numbers, LOTTO_LENGTH)) {
-      throw new Error(ERROR_MESSAGE.INVALID_LENGTH_LOTTO);
     }
   }
 
