fe9a0e7fb5f60445154ea7605a52f23ebb16bce6
jsh3418 <albert5428@gmail.com>
2022-11-12 20:07:44 +0900

    test(apply): add exception handling test when user input not a number

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,6 @@
+const MESSAGE = require("../src/constants/message");
 const Lotto = require("../src/Lotto");
+const LottoMachine = require("../src/LottoMachine");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -15,4 +17,10 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("사용자가 로또 구입 금액이 숫자가 아닌 값을 입력하는 경우 예외 처리한다.", () => {
+    expect(() => {
+      const lottoMachine = new LottoMachine();
+      if (lottoMachine.isNotANumber("abc!")) throw new Error(MESSAGE.ERROR.PAYMENT_MUST_BE_NUMBER);
+    }).toThrow("[ERROR]");
+  });
 });
