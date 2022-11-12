d38ff6617f68fa61053ebcae807e939264eda9d4
pakxe <pigkill40@naver.com>
2022-11-12 16:10:18 +0900

    feat(lotto): 로또 번호에 중복된 숫자를 감지하는 메서드 작성

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,7 @@
 const { ERROR, LOTTO } = require('./utiles/Constant');
 
+// 로또회사
+
 class Lotto {
   #numbers;
 
@@ -9,14 +11,20 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (this.#invalidLength(numbers)) {
+    if (this.#invalidLength(numbers))
       throw new Error(`${ERROR.PREFIX} ${ERROR.COUNT}`);
-    }
-    return true;
+    if (this.#duplication(numbers))
+      throw new Error(`${ERROR.PREFIX} ${ERROR.DUPLICATION}`);
+    if (isNaN(numbers.join('')))
+      throw new Error(`${ERROR.PREFIX} ${ERROR.NUMBER_ONLY}`);
   }
 
   #invalidLength(numbers) {
     return numbers.length !== LOTTO.COUNT;
+  }
+
+  #duplication(numbers) {
+    return new Set(numbers).size !== numbers.length;
   }
 }
 
