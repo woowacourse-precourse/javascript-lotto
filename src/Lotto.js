dffbf30879d063dc685e4b779e2fb16263cb172a
osdoonhyun <skw1996@naver.com>
2022-11-12 22:21:47 +0900

    refactor(Lotto): 로또 당첨 번호 유효성 검사 후 번호 리턴

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,40 +1,23 @@
-const { Random } = require("@woowacourse/mission-utils");
-const { NUMBERS } = require("./Constants");
+// const { Random } = require("@woowacourse/mission-utils");
+// const { NUMBERS } = require("./Constants");
+const Validation = require("./Validation");
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    // this.validate(numbers);
+    Validation.validateWinningNumbers(numbers);
     this.#numbers = numbers;
   }
 
+  getNumbers() {
+    return this.#numbers;
+  }
   /**
    * 플레이어가 구매한 로또 갯수만큼 로또 번호를 생성합니다.
-   * @param {number} lottoQuantity - 플레이어의 구매금액에 해당하는 로또 갯수
+   * @param {number} loqttoQuantity - 플레이어의 구매금액에 해당하는 로또 갯수
    * @returns {array} - 로또 번호가 담긴 배열(로또 번호 오름차순 정렬)
    */
-  createLottoNumbers(lottoQuantity) {
-    const lottoNumbersArray = [];
-    for (let i = 0; i < lottoQuantity; i++) {
-      const lottoNumbers = Random.pickUniqueNumbersInRange(
-        NUMBERS.FIRST,
-        NUMBERS.LAST,
-        NUMBERS.LOTTO
-      );
-      lottoNumbersArray.push(lottoNumbers.sort());
-    }
-    return lottoNumbersArray;
-  }
-
-  // validate(numbers) {
-  //   if (numbers.length !== 6) {
-  //     throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-  //   }
-  // }
-
-  // TODO: 추가 기능 구현
-  // 로또 번호의 갯수가 6개 이상
 }
 
 module.exports = Lotto;
