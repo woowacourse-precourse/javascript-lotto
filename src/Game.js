e183a005d1ad8a95f4d0f046183b74e4bc914242
lsy20140 <lsy20140@naver.com>
2022-11-13 01:32:18 +0900

    feat: 구입 금액에 해당하는 수량과 번호를 출력

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,9 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 
 class Game {
-
   gameStart() {
     this.getPriceInput();
+    this.showNumbers(this.price);
   }
 
   constructor() {
@@ -23,6 +23,22 @@ class Game {
     if(inputPrice % 1000 !== 0) throw "[ERROR] 로또 1장의 가격은 1,000원입니다. 1,000원 단위로 입력해 주세요.";
     if(inputPrice < 1000) throw "[ERROR] 로또 1장의 가격은 1,000원입니다. 1,000원 이상을 입력해 주세요.";
   }
+
+  generateNumbers() {
+    const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    return numbers;
+  }
+
+  showNumbers(price){
+    let count = price / 1000;
+    
+    Console.print(`${count}개를 구매했습니다.`)
+    while(count--){
+      Console.print(this.generateNumbers());
+    }
+  }
+
+  
 
 
 
