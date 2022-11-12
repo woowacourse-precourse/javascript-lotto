42419eedfc2cb39b46afc4504abee6b48d54fb95
loco9939 <kls9939@naver.com>
2022-11-12 18:43:36 +0900

    feat: create getWinNum Func
    - 당첨번호를 받는 함수를 생성하고 당첨번호를 받아 변수에 저장해두었다.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-// import Lotto from "./Lotto";
 const Lotto = require("./Lotto");
 
 class App {
   constructor() {
     this.LottoCount = 0;
     this.Lotto = [];
+    this.winNum = [];
   }
 
   play() {
     // 구입 금액 입력
-    this.getLottoCount();
+    // this.getLottoCount();
     // 구입 금액만큼 로또 번호 출력
-    this.buyLotto();
+    // this.buyLotto();
     // 당첨 번호 입력
+    this.getWinNum();
     // 보너스 번호 입력
     // 당첨 결과 출력
   }
@@ -36,6 +37,26 @@ class App {
     for (let i = 0; i < this.LottoCount; i++) {
       this.Lotto = [...this.Lotto, new Lotto()];
     }
+  }
+
+  getWinNum() {
+    MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.", (answers) => {
+      this.checkWinNum(answers);
+      this.winNum = answers.split(",").map(Number);
+    });
+  }
+
+  checkWinNum(answers) {
+    // "1,2,3,4,5,6"
+    const winNum = answers.split(",");
+    if (winNum.length !== 6)
+      throw new RangeError(
+        "[ERROR] 숫자 6개를 쉽표(,)로 구분하여 입력해주세요."
+      );
+    if (winNum.every((num) => typeof num === "number"))
+      throw new TypeError(
+        "[ERROR] 숫자 6개를 쉽표(,)로 구분하여 입력해주세요."
+      );
   }
 }
 
