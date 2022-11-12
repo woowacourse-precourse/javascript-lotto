11904f084fe15069eff100b4f0f3da4a12521a72
yeong bin <2976622@naver.com>
2022-11-12 16:40:39 +0900

    feat(model): Lotto.js validate조건 추가

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -10,12 +10,12 @@ class Lotto {
   }
 
   static validate(numbers) {
-    if (numbers.length !== 6) {
+    if (numbers.length !== 6)
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-    if (numbers.length !== [...new Set(numbers)].length) {
-      throw new Error("[ERROR] 중복된 값이 입력되면 안됩니다.");
-    }
+    else if (numbers.length !== [...new Set(numbers)].length)
+      throw new Error("[ERROR] 로또 번호는 중복되면 안됩니다.");
+    else if (numbers.some((number) => number <= 0 || number > 45))
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
   }
 
   /**
