b9e532eeed8809f56722ae13a4efed6576123404
moonelysian <dandyblack@naver.com>
2022-11-12 09:54:50 +0900

    feat: setWinningNumbers 추가
    - 당첨번호를 입력받고 받은 번호를 검증
    - this.winningNumbers에 당첨 번호를 배열로 저장

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const { Random } = require('@woowacourse/mission-utils');
-const { LOTTO, COMMAND, MESSAGE } = require('./constructor.js');
-const { printMessage, userInput } = require('./utils.js');
+const { LOTTO, COMMAND, MONEY, MESSAGE } = require('./constructor.js');
+const { printMessage, userInput, printRankCountMessage, printRatioMessage } = require('./utils.js');
 const Lotto = require('./Lotto.js');
 const Bonus = require('./Bonus.js');
 const Purchase = require('./Purchase.js');
@@ -9,6 +9,7 @@ class App {
   constructor() {
     this.purchaseAmount = 0;
     this.lottos = [];
+    this.winningNumbers = [];
   }
 
   createRandomLottoNumbers() {
@@ -21,6 +22,17 @@ class App {
       const randoms = this.createRandomLottoNumbers();
       this.lottos.push(new Lotto(randoms));
     }
+  }
+
+  setWinnigNumbers(numbers) {
+    const lotto = new Lotto(numbers);
+    this.winningNumbers = lotto.getNumbers();
+  }
+
+  getResult() {
+    userInput(COMMAND.INPUT_WINNING_NUMBERS, (numbers) => {
+      this.setWinnigNumbers(numbers.split(','));
+    });
   }
 
   play() {
