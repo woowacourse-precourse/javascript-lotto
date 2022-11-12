6d3b6375b47c031b9f0b542386f441fccefd92e6
jinseo <ghkdwlstj99@gmail.com>
2022-11-12 14:13:07 +0900

    feat: 4등 로또의 개수를 출력해주는 메서드 구현

--- src/WinningHistory.js
+++ src/WinningHistory.js
@@ -15,4 +15,8 @@ class WinningHistory{
     fifthPlace(){
         Console.print(`3개 일치 (5,000원) - ${this.lottoRanking[0]}개`)
     }
+
+    fourthPlace(){
+        Console.print(`4개 일치 (50,000원) - ${this.lottoRanking[1]}개`)
+    }
 }
