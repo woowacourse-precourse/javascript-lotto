dd496b30d7de4dda69689ee34bb728682997ffb5
TeusLee <xodn1311@gmail.com>
2022-11-12 14:41:31 +0900

    Add method calculate_cnt in Lotto Classes, -> counting matching count Add method calculate_expectation in Lotto Claases. -> calculate money expectation about try and add function test about Lotto.calculate

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,7 +3,7 @@ class Lotto {
 
   constructor(numbers) {
     this.validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers;    
   }
 
   validate(numbers) {
@@ -13,6 +13,35 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  calculate_cnt(solution, bouns){
+    solution = solution.split(",").map((e)=>parseInt(e))
+    bouns = [bouns]
+    let match_cnt = 0
+    let bouns_cnt = 0
+    for (let i = 0; i < this.#numbers.length; i++) {
+      if (solution.includes(this.#numbers[i])){
+        match_cnt += 1
+      }
+      if (bouns.includes(this.#numbers[i])){
+        bouns_cnt += 1
+      }
+    }
+    return this.calculate_expectation(match_cnt, bouns_cnt)
+  }
+
+  calculate_expectation(match_cnt, bouns_cnt){
+    let money_result = 0
+    if (match_cnt === 6){
+      money_result = 2000000000
+    } else if (match_cnt === 5 && bouns_cnt === 1){
+      money_result = 30000000
+    } else if (match_cnt === 4){
+      money_result = 50000
+    } else if (match_cnt === 3){
+      money_result = 5000
+    }
+    return money_result
+  }
 }
 
 module.exports = Lotto;
