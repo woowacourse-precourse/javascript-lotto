924fb4415554bf90e6ffc2e99a5fe36c5311f95c
Ryong-E <think8867@gmail.com>
2022-11-12 20:01:05 +0900

    fix(lotto): validate매서드 this를 사용하지않아 eslint에러 정적메서드로 변경하여 해결

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,14 +2,15 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    Lotto.validate(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
+  static validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
     if (new Set(numbers).size !== 6) {
       throw new Error('[ERROR] 서로 다른 6개 번호여야 합니다');
     }
