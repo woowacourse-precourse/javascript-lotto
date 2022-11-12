d448b73938c1edf7b3bee1340b2a2ad5c2f777c2
seongminn <berry9901@gmail.com>
2022-11-12 23:56:33 +0900

    refactor(constant): Error 메세지 상수를 생성하여 관리

--- src/constant.js
+++ src/constant.js
@@ -4,4 +4,14 @@ const INPUT_MESSAGE = {
   BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
 };
 
-module.exports = { INPUT_MESSAGE };
+const PREFIX = '[ERROR]';
+
+const ERROR_MESSAGE = {
+  TYPE: `${PREFIX} 숫자를 입력해 주세요.`,
+  LENGTH: `${PREFIX} 숫자의 자릿수를 확인해 주세요.`,
+  RANGE: `${PREFIX} 1부터 45 사이의 숫자를 입력해 주세요.`,
+  DUPLICATION: `${PREFIX} 중복되지 않은 수를 입력해 주세요.`,
+  CURRENCY_UNIT: `${PREFIX} 1,000원 단위의 금액을 입력해 주세요.`,
+};
+
+module.exports = { INPUT_MESSAGE, ERROR_MESSAGE };
--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,6 +1,8 @@
+const { ERROR_MESSAGE } = require('../constant');
+
 class NumberValidator {
   static isValidNumber(input) {
-    if (isNaN(input)) throw new Error('[ERROR] 숫자를 입력해 주세요.');
+    if (isNaN(input)) throw new Error(ERROR_MESSAGE.TYPE);
   }
 }
 
@@ -15,8 +17,7 @@ class MoneyValidator extends NumberValidator {
   }
 
   static isValidMoney(input) {
-    if (input % 1000)
-      throw new Error('[ERROR] 1,000원 단위의 금액을 입력해 주세요.');
+    if (input % 1000) throw new Error(ERROR_MESSAGE.CURRENCY_UNIT);
   }
 }
 
