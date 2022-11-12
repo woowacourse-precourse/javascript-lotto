69b8031ed361e439cd7b33bb833887bccfe046d7
woo-jk <201702042@o.cnu.ac.kr>
2022-11-13 02:22:07 +0900

    refactor: LottoMachine 외부에서 사용하지 않는 메소드 private으로 변경, validate 함수 분리

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -2,28 +2,36 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 
 class LottoMachine {
-  validate(money) {
+  #validateType(money) {
     if (!Number(money)) {
       throw new Error("[ERROR] 금액은 숫자만 입력해야 합니다.");
     }
+  }
+
+  #validateDivideThousand(money) {
     if (Number(money) % 1000 !== 0) {
       throw new Error("[ERROR] 금액은 1,000원 단위만 입력 가능합니다.");
     }
   }
 
-  generateSixNumber() {
+  #validate(money) {
+    this.#validateType(money);
+    this.#validateDivideThousand(money);
+  }
+
+  #generateSixNumber() {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
   }
 
-  generateLotto() {
-    return new Lotto(this.generateSixNumber());
+  #generateLotto() {
+    return new Lotto(this.#generateSixNumber());
   }
 
   generateLottos(money) {
-    this.validate(money);
+    this.#validate(money);
     const lottosCount = money / 1000;
 
-    return Array.from({ length: lottosCount }, () => this.generateLotto());
+    return Array.from({ length: lottosCount }, () => this.#generateLotto());
   }
 
   print(lottos) {
