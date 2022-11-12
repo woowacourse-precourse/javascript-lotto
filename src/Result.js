516a7d514d436b0664c36523de0377ad5fb34fda
YeonsuBaek <soothbugger@gmail.com>
2022-11-12 23:57:48 +0900

    refactor(lotto): 당첨 내역 출력하기 전 숫자 체크 생략

--- src/Result.js
+++ src/Result.js
@@ -5,58 +5,22 @@ class Result {
   #number;
   #reward;
 
-  constructor([number, quantity]) {
+  constructor(quantity, number, reward) {
     this.#quantity = quantity;
-    this.#number = 0;
-    this.#reward = 0;
+    this.#number = number;
+    this.#reward = reward;
     this.totalReward = 0;
-    this.check(number);
+    this.print();
   }
 
-  check(number) {
-    if (number === "three") {
-      this.#number = 3;
-      this.#reward = 5000;
-    }
-    if (number === "four") {
-      this.#number = 4;
-      this.#reward = 50000;
-    }
-    if (number === "five") {
-      this.#number = 5;
-      this.#reward = 1500000;
-    }
-    if (number === "bonus") {
-      this.#number = 5;
-      this.#reward = 30000000;
-
-      this.printBonus(this.#number, this.#reward, this.#quantity);
-      this.countTotalReward(this.#reward, this.#quantity);
-      return;
-    }
-    if (number === "six") {
-      this.#number = 6;
-      this.#reward = 2000000000;
-    }
-
-    this.print(this.#number, this.#reward, this.#quantity);
-    this.countTotalReward(this.#reward, this.#quantity);
+  print() {
+    const reward = this.#reward.toLocaleString();
+    Console.print(`${this.#number} (${reward}원) - ${this.#quantity}개`);
+    this.countTotalReward();
   }
 
-  print(number, reward, quantity) {
-    reward = reward.toLocaleString();
-    Console.print(`${number}개 일치 (${reward}원) - ${quantity}개`);
-  }
-
-  printBonus(number, reward, quantity) {
-    reward = reward.toLocaleString();
-    Console.print(
-      `${number}개 일치, 보너스 볼 일치 (${reward}원) - ${quantity}개`
-    );
-  }
-
-  countTotalReward(reward, quantity) {
-    this.totalReward += reward * quantity;
+  countTotalReward() {
+    this.totalReward += this.#reward * this.#quantity;
   }
 }
 
