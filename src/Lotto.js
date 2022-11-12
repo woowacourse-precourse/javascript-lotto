d074aa80269e3b4790142ecd4dd798e3dfe06e7a
kyw0716 <kyw0716@naver.com>
2022-11-12 20:32:21 +0900

    refactor: 에러 메시지만 따로 묶어 관리

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
+const ErrorMsg = require("../src/message/Error");
 
 class Lotto {
   #numbers;
@@ -9,17 +10,13 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    if (numbers.length !== 6) throw new Error(ErrorMsg.INVALID_LOTTO_COUNT);
 
-    if (new Set(numbers).size !== numbers.length) {
-      throw new Error("[ERROR] 로또 번호는 중복을 포함할 수 없습니다.");
-    }
+    if (new Set(numbers).size !== numbers.length)
+      throw new Error(ErrorMsg.INVALID_LOTTO_DUPLICATE);
 
-    if (numbers.join("").replace(/[0-9]/g, "").length > 0) {
-      throw new Error("[ERROR] 로또 번호는 숫자만 사용할 수 있습니다.");
-    }
+    if (numbers.join("").replace(/[0-9]/g, "").length > 0)
+      throw new Error(ErrorMsg.INVALID_LOTTO_NOT_NUM);
   }
 
   get objectName() {
--- src/utils/ValidCheckUtils.js
+++ src/utils/ValidCheckUtils.js
@@ -1,46 +1,43 @@
+const ErrorMsg = require("../message/Error");
+
 class ValidCheckUtils {
   constructor() {}
 
   static checkPay(pay) {
     if (pay.replace(/[0-9]/g, "").length > 0)
-      throw new Error("[ERROR] 구입금액은 숫자만 입력할 수 있습니다.");
+      throw new Error(ErrorMsg.INVALID_PAY_NOT_NUM);
 
-    if (Number(pay) % 1000 > 0)
-      throw new Error("[ERROR] 구입 금액은 1000원 단위로 입력해야합니다.");
+    if (Number(pay) % 1000 > 0) throw new Error(ErrorMsg.INVALID_PAY_UNIT);
   }
 
   static checkWinningNumber(winningNum) {
     const winningNumberArray = String(winningNum).split(",");
 
     if (new Set(winningNumberArray).size !== winningNumberArray.length)
-      throw new Error("[ERROR] 당첨 번호는 중복을 포함할 수 없습니다.");
+      throw new Error(ErrorMsg.INVALID_WINNING_DUPLICATE);
 
     if (
       winningNumberArray.filter((num) => Number(num) < 1 || Number(num) > 45)
         .length > 0
     )
-      throw new Error(
-        "[ERROR] 당첨 번호는 1에서 45 사이의 번호만 입력 가능합니다."
-      );
+      throw new Error(ErrorMsg.INVALID_WINNING_RANGE);
 
     if (winningNumberArray.length !== 6)
-      throw new Error("[ERROR] 당첨 번호는 총 6개입니다.");
+      throw new Error(ErrorMsg.INVALID_WINNING_COUNT);
 
     if (winningNum.replace(/[0-9]|\,/g, "").length > 0)
-      throw new Error("[ERROR] 당첨 번호는 숫자만 입력 가능합니다.");
+      throw new Error(ErrorMsg.INVALID_WINNING_NOT_NUM);
   }
 
   static checkBonusNumber(bonusNum, winningNumberArray) {
     if (winningNumberArray.includes(bonusNum))
-      throw new Error("[ERROR] 당첨번호와 보너스 번호는 중복될 수 없습니다.");
+      throw new Error(ErrorMsg.INVALID_BONUS_DUPLICATE);
 
     if (Number(bonusNum) < 1 || Number(bonusNum) > 45)
-      throw new Error(
-        "[ERROR] 보너스 번호는 1부터 45사이의 번호만 입력 가능합니다."
-      );
+      throw new Error(ErrorMsg.INVALID_BONUS_RANGE);
 
     if (String(bonusNum).replace(/[0-9]/g, "").length > 0 || bonusNum === "")
-      throw new Error("[ERROR] 보너스 번호는 숫자만 입력 가능합니다.");
+      throw new Error(ErrorMsg.INVALID_BONUS_NOT_NUM);
   }
 }
 
