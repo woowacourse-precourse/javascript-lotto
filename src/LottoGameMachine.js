5fd0b6c49332a06ff59347ee450641399e971935
kangju2000 <kangju2000@naver.com>
2022-11-12 19:04:13 +0900

    feat: LottoGameMachine 클래스의 printLottoNumbers 메서드 구현 - totalLottosCount 필드 추가

--- src/LottoGameMachine.js
+++ src/LottoGameMachine.js
@@ -1,6 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
 const Lotto = require('./Lotto');
-const ERROR = require('./constants/error');
 const MESSAGE = require('./constants/message');
 const generateLottoNumbers = require('./utils/generateRandomLottoNumbers');
 const Validator = require('./Validator');
@@ -8,6 +7,7 @@ const Validator = require('./Validator');
 class LottoGameMachine {
   constructor() {
     this.totalPurchaseAmount = 0;
+    this.totalLottosCount = 0;
     this.Lottos = new Map();
   }
 
@@ -15,17 +15,25 @@ class LottoGameMachine {
     Console.readLine(MESSAGE.INPUT.TOTAL_PURCHASE_AMOUNT, (totalPurchaseAmount) => {
       Validator.validateTotalPurchaseAmount(totalPurchaseAmount);
       this.totalPurchaseAmount = totalPurchaseAmount;
+      this.totalLottosCount = this.totalPurchaseAmount / 1000;
       this.setLottos();
+      this.printLottoNumbers();
     });
   }
 
   setLottos() {
-    const totalCount = this.totalPurchaseAmount / 1000;
     let count = 0;
 
-    while (count < totalCount) {
+    while (count < this.totalLottosCount) {
       count += 1;
       this.Lottos.set(`로또${count}`, new Lotto(generateLottoNumbers()));
+    }
+  }
+
+  printLottoNumbers() {
+    Console.print(MESSAGE.OUTPUT.TOTAL_PURCHASE_AMOUNT(this.totalLottosCount));
+    for (const lottoNumbers of this.Lottos.values()) {
+      Console.print(lottoNumbers.getLottoNumbers());
     }
   }
 }
