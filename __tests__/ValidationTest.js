1e6a13762946d8e340df9f1620d65546e8651fb9
2wndrhs <2wndrhs@gmail.com>
2022-11-12 02:41:13 -0800

    test(lotto): 당첨 번호가 쉼표로 구분되는 로또 번호인지 확인하는 테스트 작성

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -2,6 +2,7 @@ const {
   validate,
   isPurchaseInput,
   isLottoNumber,
+  isWinningNumber,
 } = require('../src/Validator');
 
 describe('Validator 클래스 테스트', () => {
@@ -23,6 +24,15 @@ describe('Validator 클래스 테스트', () => {
     inputs.forEach(input => {
       expect(() => {
         validate(input, isLottoNumber);
+      }).toThrow('[ERROR]');
+    });
+  });
+
+  test('당첨 번호가 쉼표로 구분되는 로또 번호가 아니면 예외 발생', () => {
+    const inputs = ['123456', '1,2,3,4,5,6,7', '0,1,2,3,4,46', '1,1,2,3,4,5'];
+    inputs.forEach(input => {
+      expect(() => {
+        validate(input, isWinningNumber);
       }).toThrow('[ERROR]');
     });
   });
