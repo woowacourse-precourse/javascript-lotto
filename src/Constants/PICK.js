324503554a8203b13dbfe38c88b0afafead4c736
SWARVY <swarvy0826@naver.com>
2022-11-12 13:18:02 +0900

    refactor : PICK 상수 파일의 필요없는 내용 정리

--- src/Constants/PICK.js
+++ src/Constants/PICK.js
@@ -1,14 +1,8 @@
-const PICK_TYPE = {
-  main: 0,
-  bonus: 1,
-};
-
-const PICK_LENGTH = {
-  main: 6,
-  bonus: 1,
+const PICK_INDEX = {
+  mainStart: 0,
+  bonusStart: 6,
 };
 
 module.exports = {
-  PICK_TYPE,
-  PICK_LENGTH,
+  PICK_INDEX,
 };
