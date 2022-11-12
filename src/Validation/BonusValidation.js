803ebd70790ecd9d5e34f3c92e616f99b1cb868e
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-13 08:20:56 +0900

    fix : BonusNumberValidation -> BonusValidation 클래스 이름 변경

---
+++ src/Validation/BonusValidation.js
@@ -0,0 +1,42 @@
+const Validation = require('./index');
+const { BONUS_NUMBER_ERROR_MESSAGE } = require('../lib/Constants');
+
+class BonusValidation extends Validation {
+  winNumberList = [];
+
+  constructor(answer, winNumberList) {
+    super(answer);
+    this.winNumberList = winNumberList;
+  }
+
+  validate() {
+    this.checkIsEmpty();
+    this.checkIsRangeNumber();
+    this.checkIsOverlapNumber();
+  }
+
+  checkIsEmpty() {
+    if (this.answer === null || this.answer === undefined || this.answer === '') {
+      throw new Error(BONUS_NUMBER_ERROR_MESSAGE.not_valid_answer);
+    }
+    return true;
+  }
+
+  checkIsRangeNumber() {
+    const regExp = /^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$/;
+    if (!regExp.test(this.answer)) {
+      throw new Error(BONUS_NUMBER_ERROR_MESSAGE.not_valid_range_number);
+    }
+    return true;
+  }
+
+  checkIsOverlapNumber() {
+    const answerNumber = Number(this.answer);
+    if (this.winNumberList.includes(answerNumber)) {
+      throw new Error(BONUS_NUMBER_ERROR_MESSAGE.not_valid_overlap_number);
+    }
+    return true;
+  }
+}
+
+module.exports = BonusValidation;
