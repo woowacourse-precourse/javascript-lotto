8a98269b07edc8d79e023fb7096240bb0035e1c8
Hchan2 <seongchan_@naver.com>
2022-11-12 21:55:01 +0900

    1000원 상수 등 추가

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,11 +1,13 @@
 const BUY_LOTTO = '구입금액을 입력해 주세요.';
-const BUY_MESSAGE = (buyAmount) => {return `${buyAmount}개를 구매했습니다.`};
+const BUY_MESSAGE = (buyAmount) => { `${buyAmount}개를 구매했습니다.` };
 const WINNING_NUMBERS = '당첨 번호를 입력해주세요.';
 const BONUS_NUMBER = '보너스 번호를 입력해 주세요.';
+const THOUSAND = 1000;
 
 module.exports = {
     BUY_LOTTO,
     BUY_MESSAGE,
     WINNING_NUMBERS,
-    BONUS_NUMBER
+    BONUS_NUMBER,
+    THOUSAND
 }
