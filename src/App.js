295b37728ab50ebc90a02d9d38c8279e40192ba3
imb96 <kimminje7267@naver.com>
2022-11-12 16:23:25 +0900

    feat : 로또를 구입할 금액을 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
 class App {
-  play() {}
-}
+  constructor() {
+    this.myMoney = 0;
+  }
+  play() {
+    this.start();
+  }
+  start() {
+    let piece = 0;
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요. ", (answer) => {
+      if (+answer % 1000 > 0) {
+        throw new Error("[ERROR] 천원 단위로만 구매 가능합니다.");
+      }
 
+      this.myMoney += +answer;
+      piece = +answer / 1000;
+      if (isNaN(piece)) {
+        throw new Error("[ERROR] 숫자만 입력 가능합니다.");
+      }
+      MissionUtils.Console.print(`${piece}개를 구매했습니다.`);
+    });
+  }
+}
+const app = new App();
+app.play();
 module.exports = App;
