fb465ae0fb905d862c4b970049de35d31be1d72f
hwanfront <kim015jh@gmail.com>
2022-11-12 18:23:49 +0900

    test(Lotto): 로또 메서드 및 예외처리에 대한 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,6 @@
 const Lotto = require("../src/domain/lotto/Lotto");
+const LottoNumber = require("../src/domain/lotto/LottoNumber");
+const InstanceException = require("../src/exception/InstanceException");
 const LottoHasDuplicatedException = require("../src/exception/lotto/LottoHasDuplicatedException");
 const LottoLengthException = require("../src/exception/lotto/LottoLengthException");
 const LottoNumberRangeException = require("../src/exception/lotto/LottoNumberRangeException");
@@ -19,6 +21,38 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("숫자가 로또에 포함되는지 확인하는 함수", () => {
+    expect(Lotto.of([1, 2, 3, 4, 5, 6]).includes(8)).toEqual(false);
+    expect(Lotto.of([1, 2, 3, 4, 5, 6]).includes(6)).toEqual(true);
+  })
+
+  test("로또 번호에 다른 로또번호가 몇개나 포함되는지 갯수를 세는 함수", () => {
+    expect(
+      Lotto.of([1, 2, 3, 4, 5, 6]).countSameNumber(new Lotto([40, 41, 42, 43, 44, 45]))
+    ).toEqual(0);
+    expect(
+      Lotto.of([1, 2, 3, 4, 5, 6]).countSameNumber(new Lotto([1, 2, 3, 4, 5, 6]))
+    ).toEqual(6);
+    expect(
+      Lotto.of([1, 2, 3, 4, 5, 6]).countSameNumber(new Lotto([1, 2, 3, 4, 8, 9]))
+    ).toEqual(4);
+    expect(
+      Lotto.of([1, 2, 3, 4, 5, 6]).countSameNumber(new Lotto([1, 2, 3, 4, 5, 6]))
+    ).toEqual(6);
+  })
+
+  test("인수로 로또를 사용하는 함수의 인수가 로또가 아닌 경우", () => {
+    expect(() => {
+      Lotto.of([1, 2, 3, 4, 5, 6]).countSameNumber();
+    }).toThrow(InstanceException);
+    expect(() => {
+      Lotto.of([1, 2, 3, 4, 5, 6]).countSameNumber(1);
+    }).toThrow(InstanceException);
+    expect(() => {
+      Lotto.of([1, 2, 3, 4, 5, 6]).countSameNumber([1, 2, 3, 4, 5, 6, 7]);
+    }).toThrow(InstanceException);
+  })
+
   test("로또 번호의 개수가 6이 아닌 경우", () => {
     expect(() => {
       new Lotto([]);
