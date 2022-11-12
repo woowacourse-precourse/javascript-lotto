d15625de9d57af2da97c1d3188bed33e5da95d69
sungjihyun <jhsung23@gmail.com>
2022-11-12 21:08:08 +0900

    refactor(User): 생성자를 통해 필드를 갖도록 변경
    구매 금액을 받아 구매 수량과 로또 번호를 갖도록 함

--- src/User.js
+++ src/User.js
@@ -1,22 +1,50 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Random, Console } = require("@woowacourse/mission-utils");
 
-const { isDivisible } = require("./utils/utils");
-const { ERROR } = require("./utils/constants");
+const { isDivisible, ascendingSort } = require("./utils/utils");
+const {
+  MIN_NUMBER,
+  MAX_NUMBER,
+  NUMBER_COUNT,
+  AMOUNT_UNIT,
+  ERROR,
+} = require("./utils/constants");
+const Lotto = require("./Lotto");
 
 class User {
   purchaseAmount;
+  quantity;
   lottos;
-  constructor() {
-    this.purchaseAmount;
-    this.lottos;
+
+  constructor(purchaseAmount) {
+    this.validate(purchaseAmount);
+    this.purchaseAmount = purchaseAmount;
+    this.quantity = User.countAvailableQuantity(purchaseAmount);
+    this.lottos = User.generateLottoNumbers(this.quantity);
   }
 
-  // app으로부터 유효한 입력값 받으면 divisible 검사 후 필드 할당
-  set setPurchaseAmount(amount) {
-    if (!isDivisible(amount)) {
+  validate(input) {
+    if (!isDivisible(input)) {
       throw new Error(ERROR.INDIVISIBLE);
     }
-    this.purchaseAmount = amount;
+  }
+
+  //구매 금액으로 로또 몇장 살 수 있는지 계산하기
+  static countAvailableQuantity(amount) {
+    return Number(amount) / AMOUNT_UNIT;
+  }
+
+  //랜덤 번호 뽑아주기
+  static generateLottoNumbers(quantity) {
+    const lottos = [];
+    for (let i = 0; i < quantity; i++) {
+      const lotto = Random.pickUniqueNumbersInRange(
+        MIN_NUMBER,
+        MAX_NUMBER,
+        NUMBER_COUNT
+      );
+      lottos.push(new Lotto(ascendingSort(lotto)));
+    }
+    return lottos;
   }
 }
 
