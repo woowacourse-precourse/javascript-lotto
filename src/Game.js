619f9949f85fd91c0e306aa00cc0df2930d9aea0
lsy20140 <lsy20140@naver.com>
2022-11-13 01:08:39 +0900

    feat: 입력 받은 구입 금액에 대한 예외처리

--- src/Game.js
+++ src/Game.js
@@ -12,9 +12,16 @@ class Game {
 
   getPriceInput() {
     return Console.readLine('구입금액을 입력해 주세요.', (inputPrice) => {
+      this.checkPriceInput(inputPrice);
       this.price = inputPrice;
       console.log(this.price);
     });
+  }
+
+  checkPriceInput(inputPrice) {
+    if(isNaN(inputPrice) === true) throw "[ERROR] 숫자를 입력해 주세요.";
+    if(inputPrice % 1000 !== 0) throw "[ERROR] 로또 1장의 가격은 1,000원입니다. 1,000원 단위로 입력해 주세요.";
+    if(inputPrice < 1000) throw "[ERROR] 로또 1장의 가격은 1,000원입니다. 1,000원 이상을 입력해 주세요.";
   }
 
 
