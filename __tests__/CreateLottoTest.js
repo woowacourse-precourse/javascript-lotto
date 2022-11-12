91f6b1dbb0802cb47f2e1db144d8233c6c209fda
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-13 00:24:43 +0900

    test: 로또 생성 기능 테스트 케이스 수정
    인스턴스 생성 로직을 test케이스 내부로 이동하였음.

--- __tests__/CreateLottoTest.js
+++ __tests__/CreateLottoTest.js
@@ -2,14 +2,13 @@ const LottoGame = require("../src/LottoGame.js");
 const { Console } = require("@woowacourse/mission-utils");
 
 describe("LottoGame.createLottos", () => {
-  const lottoGame = new LottoGame();
-
   afterEach(() => Console.close());
 
   test("input 숫자로 생성된 로또의 개수는 input과 같아야 한다.", () => {
     // given
     const input = 8;
     // when
+    const lottoGame = new LottoGame();
     lottoGame.createLottos(input);
     const result = lottoGame.lottos;
     // then
