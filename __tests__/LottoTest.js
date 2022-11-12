be0f3122927da7d33ca8e4d56fc4e9c4e3841385
WaiNaat <qoli0703@gmail.com>
2022-11-12 14:18:50 +0900

    test: add success case for random/player picked Lotto generation

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,13 @@
+const MissionUtils = require('@woowacourse/mission-utils');
 const Lotto = require('../src/Lotto');
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickUniqueNumbersInRange,
+  );
+};
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -10,5 +19,23 @@ describe('로또 클래스 테스트', () => {
     expect(() => new Lotto([1, 2, 3, 4, 5, 5])).toThrow('[ERROR]');
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('자동 로또 번호 생성 성공', () => {
+    const randomLotto = [16, 26, 29, 31, 36, 8];
+    const correctOutput = randomLotto.sort((a, b) => a - b).join(', ');
+
+    mockRandoms([randomLotto]);
+
+    let lotto = new Lotto();
+    lotto = lotto.toString(', ');
+    expect(lotto).toEqual(correctOutput);
+  });
+
+  test('수동 로또 번호 입력 성공', () => {
+    const myLotto = [16, 26, 29, 31, 36, 8];
+    const correctOutput = myLotto.sort((a, b) => a - b).join(', ');
+
+    let lotto = new Lotto(myLotto);
+    lotto = lotto.toString(', ');
+    expect(lotto).toEqual(correctOutput);
+  });
 });
