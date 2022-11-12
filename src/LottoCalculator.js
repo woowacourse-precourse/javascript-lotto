edc92a8a0b9aa7a88e93579bad9d70f2c4a6c056
yeongi <cbg5255@gmail.com>
2022-11-12 11:46:13 +0900

    feat: LottoCalculator 로또 번호를 비교하는 기능 구현

--- src/LottoCalculator.js
+++ src/LottoCalculator.js
@@ -1,21 +1,47 @@
 const { Console } = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 
-// - [ ] 로또번호를 비교하는 기능 - compareLotto
-// - [ ] 당첨 내역을 출력하는 기능 - printWinResult
-// - [ ] 수익률을 계산하는 기능 - printGainPercent
 class LottoCalculate {
   #winLotto;
   #boughtLottos;
+  #bonusNum;
 
-  constructor(win, lottos) {
+  constructor(win, lottos, bonus) {
     this.#winLotto = win;
     this.#boughtLottos = lottos;
+    this.#bonusNum = bonus;
   }
 
-  compareLotto(my, win) {}
+  resultCaculator(lottos, win) {}
+
+  compareLotto(my, win, bonus) {
+    const result = my.filter((num) => win.includes(num));
+    switch (result.length) {
+      case 6:
+        return 1;
+      case 5:
+        if (my.includes(bonus)) return 2;
+        return 3;
+      case 4:
+        return 4;
+      case 3:
+        return 5;
+    }
+  }
+
   printWinResult() {}
   printGainPercent() {}
 }
+
+const a = new LottoCalculate();
+console.log("1등", a.compareLotto([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 8));
+
+console.log("2등", a.compareLotto([1, 2, 3, 4, 5, 8], [1, 2, 3, 4, 5, 6], 8));
+
+console.log("3등", a.compareLotto([1, 2, 3, 4, 5, 7], [1, 2, 3, 4, 5, 6], 8));
+
+console.log("4등", a.compareLotto([1, 2, 3, 4, 9, 10], [1, 2, 3, 4, 5, 6], 8));
+
+console.log("5등", a.compareLotto([1, 2, 3, 9, 10, 12], [1, 2, 3, 4, 5, 6], 8));
 
 module.exports = LottoCalculate;
