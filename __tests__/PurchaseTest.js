9604722f1e5a60a857057852109b5b79421a0e85
금가경 <gageum0@gmail.com>
2022-11-12 23:36:28 +0900

    test(lotto) : 로또 구입 금액 유효성 테스트

---
+++ __tests__/PurchaseTest.js
@@ -0,0 +1,22 @@
+const Purchase = require('../src/Purchase');
+const Messages = '../src/Constants/Messages';
+
+describe('구매 클래스 테스트', () => {
+  test('구매 값은 숫자만으로 이루어져 있어야 합니다.', () => {
+    expect(() => {
+      new Purchase('1234j');
+    }).toThrow(Messages.NOT_NUMBER);
+  });
+
+  test('ERROR] 0보다 큰 수를 입력해 주세요.', () => {
+    expect(() => {
+      new Purchase('-3000');
+    }).toThrow(Messages.MINUS);
+  });
+
+  test('구매 값은 1000원 단위입니다', () => {
+    expect(() => {
+      new Purchase('3800');
+    }).toThrow(Messages.NOT_DIVIDED_BY_THOUSAND);
+  });
+});
---
+++ src/Constants/Messages.js
@@ -0,0 +1,7 @@
+const error = {
+  NOT_NUMBER: '[ERROR] 구입 금액은 숫자여야 합니다.',
+  MINUS: '[ERROR] 0보다 큰 수를 입력해 주세요.',
+  NOT_DIVIDED_BY_THOUSAND: '[ERROR] 구입 금액은 1000원 단위로 입력해 주세요.',
+};
+
+module.exports = error;
