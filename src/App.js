4fa9af67847b6f1d4f2a71d692c0252a0316ee3c
kaori-killer <sksbthwjd@daum.net>
2022-11-12 13:26:51 +0900

    feat: [예외처리] 1,000원 단위로 입력 받는다.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
-
-const TEXTS = Object.freeze({
-  amountInput: '구입금액을 입력해 주세요. \n',
-  lottoCount: '개를 구매했습니다.',
-  numberInput : '당첨 번호를 입력해 주세요.',
-  bonusInput: '보너스 번호를 입력해 주세요.',
-  resultInput: '당첨 통계 \n\n --'
-});
+const Purchase = require('./Purchase');
+const Lotto = require('./Lotto');
 
 class App {
   play() {
-    this.printMessage(TEXTS.amountInput, this.inputAmount);
+    this.inputMessage('구입금액을 입력해 주세요.', (amount) => {
+      new Purchase(Number(amount));
+    });
   }
-
-  printMessage(text, callback) {
+  inputMessage(text, callback) {
     Console.readLine(text, callback.bind(this));
   }
-
-  inputAmount(input){
-    Console.print(input)
-  }
-
 }
 
 const app = new App();
