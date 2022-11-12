d51959ef5d088dcb1d5bca2524ee23e4ea6a0a35
wonkyum-kim <dodo12120@naver.com>
2022-11-12 14:54:22 +0900

    test(ApplicationTest): add unit test codes
    - Test various exception cases for entering money.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,30 +1,29 @@
-const App = require("../src/App");
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils');
+const App = require('../src/App');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
-  answers.reduce((acc, input) => {
-    return acc.mockImplementationOnce((question, callback) => {
-      callback(input);
-    });
-  }, MissionUtils.Console.readLine);
+  answers.reduce((acc, input) => acc.mockImplementationOnce((question, callback) => {
+    callback(input);
+  }), MissionUtils.Console.readLine);
 };
 
 const mockRandoms = (numbers) => {
   MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number);
-  }, MissionUtils.Random.pickUniqueNumbersInRange);
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickUniqueNumbersInRange,
+  );
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
-describe("로또 테스트", () => {
-  test("기능 테스트", () => {
+describe('로또 테스트', () => {
+  test('기능 테스트', () => {
     mockRandoms([
       [8, 21, 23, 41, 42, 43],
       [3, 5, 11, 16, 32, 38],
@@ -35,23 +34,23 @@ describe("로또 테스트", () => {
       [2, 13, 22, 32, 38, 45],
       [1, 3, 5, 14, 22, 45],
     ]);
-    mockQuestions(["8000", "1,2,3,4,5,6", "7"]);
+    mockQuestions(['8000', '1,2,3,4,5,6', '7']);
     const logs = [
-      "8개를 구매했습니다.",
-      "[8, 21, 23, 41, 42, 43]",
-      "[3, 5, 11, 16, 32, 38]",
-      "[7, 11, 16, 35, 36, 44]",
-      "[1, 8, 11, 31, 41, 42]",
-      "[13, 14, 16, 38, 42, 45]",
-      "[7, 11, 30, 40, 42, 43]",
-      "[2, 13, 22, 32, 38, 45]",
-      "[1, 3, 5, 14, 22, 45]",
-      "3개 일치 (5,000원) - 1개",
-      "4개 일치 (50,000원) - 0개",
-      "5개 일치 (1,500,000원) - 0개",
-      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-      "6개 일치 (2,000,000,000원) - 0개",
-      "총 수익률은 62.5%입니다.",
+      '8개를 구매했습니다.',
+      '[8, 21, 23, 41, 42, 43]',
+      '[3, 5, 11, 16, 32, 38]',
+      '[7, 11, 16, 35, 36, 44]',
+      '[1, 8, 11, 31, 41, 42]',
+      '[13, 14, 16, 38, 42, 45]',
+      '[7, 11, 30, 40, 42, 43]',
+      '[2, 13, 22, 32, 38, 45]',
+      '[1, 3, 5, 14, 22, 45]',
+      '3개 일치 (5,000원) - 1개',
+      '4개 일치 (50,000원) - 0개',
+      '5개 일치 (1,500,000원) - 0개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
+      '6개 일치 (2,000,000,000원) - 0개',
+      '총 수익률은 62.5%입니다.',
     ];
     const logSpy = getLogSpy();
     const app = new App();
@@ -61,11 +60,35 @@ describe("로또 테스트", () => {
     });
   });
 
-  test("예외 테스트", () => {
-    mockQuestions(["1000j"]);
+  test('로또 구입 예외 처리 - 숫자 이외의 문자가 입력되는 경우', () => {
+    mockQuestions(['1000j']);
     expect(() => {
       const app = new App();
       app.play();
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR] 올바른 입력이 아닙니다.');
+  });
+
+  test('로또 구입 예외 처리 - 1000원 단위로 나누어 떨어지지 않는 경우', () => {
+    mockQuestions(['8001']);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow('[ERROR] 1000원 단위의 금액을 입력해야 합니다.');
+  });
+
+  test('로또 구입 예외 처리 - 1000원 미만의 금액이 들어오는 경우', () => {
+    mockQuestions(['600']);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow('[ERROR] 1000원 단위의 금액을 입력해야 합니다.');
+  });
+
+  test('로또 구입 예외 처리 - 0으로 시작하는 경우', () => {
+    mockQuestions(['01000']);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow('[ERROR] 올바른 입력이 아닙니다.');
   });
 });
