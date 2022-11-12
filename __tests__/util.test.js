de75505859c8dcddef438894ac25816b7832a523
sungjihyun <jhsung23@gmail.com>
2022-11-12 14:32:44 +0900

    refactor(util.test): 메서드 위치 변경에 따른 호출 방법 수정

--- __tests__/util.test.js
+++ __tests__/util.test.js
@@ -1,67 +1,30 @@
 const App = require("../src/App");
+const utils = require("../src/utils/utils");
 const { ERROR } = require("../src/utils/constants");
 
 describe("입력한 구매 금액이 유효한 값인지 검사한다.", () => {
-  test("금액에 숫자 이외의 값이 있다면 예외가 발생한다.", () => {
-    const app = new App();
-    expect(() => {
-      app.validateInput("1oo0");
-    }).toThrow(ERROR.ONLY_NUMBER);
-    expect(() => {
-      app.validateInput("1dollar");
-    }).toThrow(ERROR.ONLY_NUMBER);
-    expect(() => {
-      app.validateInput("삼만사천원");
-    }).toThrow(ERROR.ONLY_NUMBER);
-    expect(() => {
-      app.validateInput("5천원");
-    }).toThrow(ERROR.ONLY_NUMBER);
-    expect(() => {
-      app.validateInput("만원");
-    }).toThrow(ERROR.ONLY_NUMBER);
-    expect(() => {
-      app.validateInput("4000_");
-    }).toThrow(ERROR.ONLY_NUMBER);
-    expect(() => {
-      app.validateInput("1000원");
-    }).toThrow(ERROR.ONLY_NUMBER);
-    expect(() => {
-      app.validateInput("1000won");
-    }).toThrow(ERROR.ONLY_NUMBER);
-    expect(() => {
-      app.validateInput("anything");
-    }).toThrow(ERROR.ONLY_NUMBER);
+  test("금액에 숫자 이외의 값이 있다면 true를 반환한다.", () => {
+    expect(utils.hasChar("1oo0")).toEqual(true);
+    expect(utils.hasChar("1dollar")).toEqual(true);
+    expect(utils.hasChar("삼만사천원")).toEqual(true);
+    expect(utils.hasChar("5천원")).toEqual(true);
+    expect(utils.hasChar("만원")).toEqual(true);
+    expect(utils.hasChar("4000_")).toEqual(true);
+    expect(utils.hasChar("1000원")).toEqual(true);
+    expect(utils.hasChar("1000won")).toEqual(true);
+    expect(utils.hasChar("anything")).toEqual(true);
   });
 
-  test("금액이 1000원으로 나눠떨어지지 않으면 예외가 발생한다.", () => {
-    const app = new App();
-    expect(() => {
-      app.validateInput("1500");
-    }).toThrow(ERROR.INDIVISIBLE);
-    expect(() => {
-      app.validateInput("2100");
-    }).toThrow(ERROR.INDIVISIBLE);
-    expect(() => {
-      app.validateInput("800");
-    }).toThrow(ERROR.INDIVISIBLE);
-    expect(() => {
-      app.validateInput("100");
-    }).toThrow(ERROR.INDIVISIBLE);
-    expect(() => {
-      app.validateInput("10");
-    }).toThrow(ERROR.INDIVISIBLE);
-    expect(() => {
-      app.validateInput("50500");
-    }).toThrow(ERROR.INDIVISIBLE);
-    expect(() => {
-      app.validateInput("3333");
-    }).toThrow(ERROR.INDIVISIBLE);
-    expect(() => {
-      app.validateInput("37400");
-    }).toThrow(ERROR.INDIVISIBLE);
-    expect(() => {
-      app.validateInput("10000001");
-    }).toThrow(ERROR.INDIVISIBLE);
+  test("금액이 1000원으로 나눠떨어지지 않으면 false를 반환한다.", () => {
+    expect(utils.isDivisible("1500")).toEqual(false);
+    expect(utils.isDivisible("2100")).toEqual(false);
+    expect(utils.isDivisible("800")).toEqual(false);
+    expect(utils.isDivisible("100")).toEqual(false);
+    expect(utils.isDivisible("10")).toEqual(false);
+    expect(utils.isDivisible("50500")).toEqual(false);
+    expect(utils.isDivisible("3333")).toEqual(false);
+    expect(utils.isDivisible("37400")).toEqual(false);
+    expect(utils.isDivisible("10000001")).toEqual(false);
   });
 });
 
@@ -93,109 +56,111 @@ describe("구매 금액으로 로또를 구매할 수 있는 수량을 검사한
 
 describe("입력받은 당첨 번호가 유효한 입력인지 검사한다.", () => {
   test("문자열에 쉼표와 숫자 이외의 문자가 있다면 true를 반환한다.", () => {
-    expect(App.hasCharExceptComma("1,2,3,4w,5")).toEqual(true);
-    expect(App.hasCharExceptComma("1,2,ww,wer,3,45")).toEqual(true);
-    expect(App.hasCharExceptComma("a,b,c,d,e,f")).toEqual(true);
-    expect(App.hasCharExceptComma("one,2,3,4,5,six")).toEqual(true);
-    expect(App.hasCharExceptComma("one,two,three,four,five,six")).toEqual(true);
-    expect(App.hasCharExceptComma("1,2,#,4,5,6")).toEqual(true);
-    expect(App.hasCharExceptComma("1,2,',4,5,6")).toEqual(true);
-    expect(App.hasCharExceptComma('1,2,3,",5,6')).toEqual(true);
+    expect(utils.hasCharExceptComma("1,2,3,4w,5")).toEqual(true);
+    expect(utils.hasCharExceptComma("1,2,ww,wer,3,45")).toEqual(true);
+    expect(utils.hasCharExceptComma("a,b,c,d,e,f")).toEqual(true);
+    expect(utils.hasCharExceptComma("one,2,3,4,5,six")).toEqual(true);
+    expect(utils.hasCharExceptComma("one,two,three,four,five,six")).toEqual(
+      true
+    );
+    expect(utils.hasCharExceptComma("1,2,#,4,5,6")).toEqual(true);
+    expect(utils.hasCharExceptComma("1,2,',4,5,6")).toEqual(true);
+    expect(utils.hasCharExceptComma('1,2,3,",5,6')).toEqual(true);
   });
 
   test("문자열에 쉼표가 연속적으로 존재한다면 예외가 발생한다.", () => {
     expect(() => {
-      App.makeSplit("1,,2,3,4,5");
+      utils.makeSplit("1,,2,3,4,5");
     }).toThrow(ERROR.MISUSE_COMMA);
     expect(() => {
-      App.makeSplit("1,2,,3,4,5");
+      utils.makeSplit("1,2,,3,4,5");
     }).toThrow(ERROR.MISUSE_COMMA);
     expect(() => {
-      App.makeSplit("1,2,3,,4,5");
+      utils.makeSplit("1,2,3,,4,5");
     }).toThrow(ERROR.MISUSE_COMMA);
     expect(() => {
-      App.makeSplit("1,2,3,4,,5");
+      utils.makeSplit("1,2,3,4,,5");
     }).toThrow(ERROR.MISUSE_COMMA);
     expect(() => {
-      App.makeSplit("1,2,3,4,5,");
+      utils.makeSplit("1,2,3,4,5,");
     }).toThrow(ERROR.MISUSE_COMMA);
   });
 
   test("문자열이 쉼표로 시작하거나 쉼표로 끝나면 예외가 발생한다.", () => {
     expect(() => {
-      App.makeSplit(",1,2,3,4,5,6");
+      utils.makeSplit(",1,2,3,4,5,6");
     }).toThrow(ERROR.MISUSE_COMMA);
     expect(() => {
-      App.makeSplit("1,2,3,4,5,6,");
+      utils.makeSplit("1,2,3,4,5,6,");
     }).toThrow(ERROR.MISUSE_COMMA);
     expect(() => {
-      App.makeSplit(",1,2,3,4,5,6,");
+      utils.makeSplit(",1,2,3,4,5,6,");
     }).toThrow(ERROR.MISUSE_COMMA);
     expect(() => {
-      App.makeSplit(",,1,2,3,4,5,6");
+      utils.makeSplit(",,1,2,3,4,5,6");
     }).toThrow(ERROR.MISUSE_COMMA);
     expect(() => {
-      App.makeSplit("1,2,3,4,5,6,,");
+      utils.makeSplit("1,2,3,4,5,6,,");
     }).toThrow(ERROR.MISUSE_COMMA);
     expect(() => {
-      App.makeSplit(",,1,2,3,4,5,6,,");
+      utils.makeSplit(",,1,2,3,4,5,6,,");
     }).toThrow(ERROR.MISUSE_COMMA);
   });
 
   test("문자열에 두 쉼표가 공백을 사이에 두고 있다면 예외가 발생한다.", () => {
     expect(() => {
-      App.makeSplit("1, ,2,3,4,5");
+      utils.makeSplit("1, ,2,3,4,5");
     }).toThrow(ERROR.MISUSE_COMMA);
     expect(() => {
-      App.makeSplit("1,2, ,3,4,5");
+      utils.makeSplit("1,2, ,3,4,5");
     }).toThrow(ERROR.MISUSE_COMMA);
     expect(() => {
-      App.makeSplit("1,2,3, ,4,5");
+      utils.makeSplit("1,2,3, ,4,5");
     }).toThrow(ERROR.MISUSE_COMMA);
     expect(() => {
-      App.makeSplit("1,2,3,4, ,5");
+      utils.makeSplit("1,2,3,4, ,5");
     }).toThrow(ERROR.MISUSE_COMMA);
   });
 
   test("숫자와 숫자 사이에 공백이 있다면 예외가 발생한다.", () => {
     expect(() => {
-      App.makeNumberArray(["1", "2 3", "3", "4", "5"]);
+      utils.makeNumberArray(["1", "2 3", "3", "4", "5"]);
     }).toThrow(ERROR.IS_NAN);
     expect(() => {
-      App.makeNumberArray(["1", "2  3", "3", "4", "5"]);
+      utils.makeNumberArray(["1", "2  3", "3", "4", "5"]);
     }).toThrow(ERROR.IS_NAN);
     expect(() => {
-      App.makeNumberArray(["1", "23", "3   5", "4", "5"]);
+      utils.makeNumberArray(["1", "23", "3   5", "4", "5"]);
     }).toThrow(ERROR.IS_NAN);
     expect(() => {
-      App.makeNumberArray(["1", "23", "3", "4 4", "5"]);
+      utils.makeNumberArray(["1", "23", "3", "4 4", "5"]);
     }).toThrow(ERROR.IS_NAN);
     expect(() => {
-      App.makeNumberArray(["1", "3", "22", "4 4", "5"]);
+      utils.makeNumberArray(["1", "3", "22", "4 4", "5"]);
     }).toThrow(ERROR.IS_NAN);
     expect(() => {
-      App.makeNumberArray(["1", "3", "2 2", "4 4", "5"]);
+      utils.makeNumberArray(["1", "3", "2 2", "4 4", "5"]);
     }).toThrow(ERROR.IS_NAN);
     expect(() => {
-      App.makeNumberArray(["1 0", "3 0", "2 2", "4 4", "5 8"]);
+      utils.makeNumberArray(["1 0", "3 0", "2 2", "4 4", "5 8"]);
     }).toThrow(ERROR.IS_NAN);
   });
 
   test("배열 요소 중 연속된 숫자의 앞뒤에 있는 공백은 제거한 후 숫자 배열을 반환한다.", () => {
-    expect(App.makeNumberArray([" 1", "2", "3  ", "4 ", " 5  "])).toEqual([
+    expect(utils.makeNumberArray([" 1", "2", "3  ", "4 ", " 5  "])).toEqual([
       1, 2, 3, 4, 5,
     ]);
-    expect(App.makeNumberArray(["  1", "  2", "  3", "  4", "  5"])).toEqual([
+    expect(utils.makeNumberArray(["  1", "  2", "  3", "  4", "  5"])).toEqual([
       1, 2, 3, 4, 5,
     ]);
-    expect(App.makeNumberArray(["1  ", "2  ", "3  ", "4  ", "5  "])).toEqual([
+    expect(utils.makeNumberArray(["1  ", "2  ", "3  ", "4  ", "5  "])).toEqual([
       1, 2, 3, 4, 5,
     ]);
-    expect(App.makeNumberArray([" 1 ", " 2 ", " 3 ", " 4 ", " 5 "])).toEqual([
+    expect(utils.makeNumberArray([" 1 ", " 2 ", " 3 ", " 4 ", " 5 "])).toEqual([
       1, 2, 3, 4, 5,
     ]);
     expect(
-      App.makeNumberArray([" 12 ", " 23 ", " 32 ", " 41 ", " 45 "])
+      utils.makeNumberArray([" 12 ", " 23 ", " 32 ", " 41 ", " 45 "])
     ).toEqual([12, 23, 32, 41, 45]);
   });
 });
