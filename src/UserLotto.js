3ead48760370877198b130e4da2c900c81ea6c2a
Sim <zh025700@naver.com>
2022-11-13 00:56:16 +0900

    fix(src/UserLotto): 랜덤 로또 생성 함수 수정 PickNumberInRage를 PickUniqueNumbersInRange로 변경

--- src/UserLotto.js
+++ src/UserLotto.js
@@ -8,24 +8,11 @@ class UserLotto {
   }
 
   createNumber = () => {
-    let i = 0;
-    const answer = [];
-    while (i < STATIC.LOTTOVALUE.LENGTH) {
-      const randVal = MissionUtils.Random.pickNumberInRange(
-        STATIC.LOTTOVALUE.MIN,
-        STATIC.LOTTOVALUE.MAX
-      );
-      if (this.duplicateCheck(answer, randVal)) {
-        answer.push(randVal);
-        i++;
-      }
-    }
-
-    return answer;
-  };
-
-  duplicateCheck = (answer, val) => {
-    return answer.every((e) => val !== e);
+    return MissionUtils.Random.pickUniqueNumbersInRange(
+      STATIC.LOTTOVALUE.MIN,
+      STATIC.LOTTOVALUE.MAX,
+      STATIC.LOTTOVALUE.LENGTH
+    );
   };
 }
 
