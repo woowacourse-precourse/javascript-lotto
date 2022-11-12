69349bf1cb02602970751a5a4ef61e90dca92bdf
kimeuns <dmstn2006@gmail.com>
2022-11-12 20:20:54 +0900

    feat: 보너스 번호 입력

--- src/App.js
+++ src/App.js
@@ -8,66 +8,68 @@ const ASK_BONUS_LOTTO_NUMBER = "보너스 번호를 입력해 주세요.";
 
 class App {
   lottoCount;
-  issuedLotto=[];
+  issuedLotto = [];
   winningLottoNumber;
   BonusLottoNumber;
 
-  buyLotto(price){
+  buyLotto(price) {
     const buyer = new Buyer(price);
     this.lottoCount = buyer.lottoCount;
   }
 
-  printLottoCount(lottoCount){
-    Console.print(`\n${lottoCount}개를 구매했습니다`)
+  printLottoCount(lottoCount) {
+    Console.print(`\n${lottoCount}개를 구매했습니다`);
   }
 
   makeLottoNumber() {
     return Random.pickUniqueNumbersInRange(1, 45, 6);
   }
 
-  sortLottoNumberInAscendignOrder(lottoNumber){
+  sortLottoNumberInAscendignOrder(lottoNumber) {
     return lottoNumber.sort((a, b) => a - b);
   }
 
   printLottoNumber(lottoNumber) {
-    return Console.print(`[${lottoNumber.join(', ')}]`)
+    return Console.print(`[${lottoNumber.join(", ")}]`);
   }
 
-  makeissuedLotto(lottoCount){
-    for(let i =0;i<lottoCount;i++){
+  makeissuedLotto(lottoCount) {
+    for (let i = 0; i < lottoCount; i++) {
       let lottoNumber = this.sortLottoNumberInAscendignOrder(
         this.makeLottoNumber()
       );
-      this.issuedLotto.push(lottoNumber)
+      this.issuedLotto.push(lottoNumber);
     }
   }
 
-  printIssuendLotto(issuedLotto){
-    issuedLotto.forEach((value)=>{
+  printIssuendLotto(issuedLotto) {
+    issuedLotto.forEach((value) => {
       this.printLottoNumber(value);
-    })
-  }
-
-  getWinningLottoNumber(){
-    Console.print(`\n${ASK_WINNING_LOTTO_NUMBER}`);
-    Console.readline("", (input) => {
-      this.isValidinput(input);
     });
   }
 
-  isValidinput(input){
-    const inputValue = input.split(",")
-    if ([...new Set(inputValue)].length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 중복되지 않는 숫자여야 합니다.");
-    }
+  getWinningLottoNumber() {
+    Console.print(`\n${ASK_WINNING_LOTTO_NUMBER}`);
+    Console.readline("", (input) => {
+      const inputNumber = input.split(",").map(Number)
+      this.isValidinput(inputNumber);
+      this.winningLottoNumber = inputNumber;
+    });
+  }
+
+  isValidinput(input) {
+    const inputValue = input.split(",").map(Number);
     inputValue.forEach((value) => {
       if (isNaN(value)) {
         throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
       }
-      if (Number(value) <1 || Number(value) >45){
-        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+      if (value < 1 || value > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
       }
     });
+    if ([...new Set(inputValue)].length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않는 숫자여야 합니다.");
+    }
   }
 
   play() {
