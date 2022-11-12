cefc912c89b2345856e3db42d7e6d228981d349f
Buyoung Chung <sunarol@naver.com>
2022-11-12 15:51:29 +0900

    feat: Add constant FIFTH_PRIZE ~ FIRST_PRIZE

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -10,6 +10,16 @@ class Constant {
   static THIRD = 5;
 
   static FIRST = 6;
+
+  static FIFTH_PRIZE = 5000;
+
+  static FOURTH_PRIZE = 50000;
+
+  static THIRD_PRIZE = 1500000;
+
+  static SECOND_PRIZE = 30000000;
+
+  static FIRST_PRIZE = 2000000000;
 }
 
 module.exports = Constant;
