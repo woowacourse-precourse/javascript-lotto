79a1ae81f7b69beed6ef45c3d2d7d5b36556b706
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-13 08:24:06 +0900

    refactor : 기능별로 구분한 객체를 사용하여 프로그램 흐름 작성

--- src/LottoController.js
+++ src/LottoController.js
@@ -1,9 +1,17 @@
 const { GAME_MESSAGE } = require('./lib/Constants');
 const { readLine } = require('./lib/Utils');
 const Price = require('./Price');
+const Lottery = require('./Lottery');
+const Lotto = require('./Lotto');
+const Bonus = require('./Bonus');
+const CalculationLotto = require('./CalculationLotto');
 
 class LottoController {
   price;
+
+  lottery;
+
+  bonus;
 
   start() {
     this.inputPrice();
@@ -15,6 +23,47 @@ class LottoController {
 
   getPrice(answer) {
     this.price = new Price(answer);
+    return this.publishLotto();
+  }
+
+  publishLotto() {
+    const lottoCount = this.price.getLottoCount();
+    this.lottery = new Lottery(lottoCount);
+    this.lottery.publishLottoList().printLottoList();
+
+    return this.inputLotto();
+  }
+
+  inputLotto() {
+    readLine(GAME_MESSAGE.input_win_number, (answer) => this.getWinNumbers(answer));
+  }
+
+  getWinNumbers(answer) {
+    this.lotto = new Lotto(answer);
+    return this.inputBonus();
+  }
+
+  inputBonus() {
+    readLine(GAME_MESSAGE.input_bonus_number, (answer) => this.getBonusNumber(answer));
+  }
+
+  getBonusNumber(answer) {
+    this.bonus = new Bonus(answer, this.lotto.getNumbers());
+    return this.calculationMyLottoResult();
+  }
+
+  calculationMyLottoResult() {
+    const lottoList = this.lottery.getLottoList();
+    const winNumberList = this.lotto.getNumbers();
+    const bonusNumber = this.bonus.getBonusNumber();
+    const lottoPrice = this.price.getLottoPrice();
+
+    const calculationLotto = new CalculationLotto();
+    calculationLotto
+      .calculationLottoResult(lottoList, winNumberList, bonusNumber, lottoPrice)
+      .matchResult()
+      .calculationLottoRate()
+      .printResult();
   }
 }
 
