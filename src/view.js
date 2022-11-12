f5e5d5dc765dba81c579d0844997f2bac75cb0c1
Ryong-E <think8867@gmail.com>
2022-11-12 18:50:45 +0900

    refactor(view): 커밋 컨벤션,상수 처리,매서드분리,사용하지않는 함수 삭제
    - view라는 클래스에 맞에 출력하는 구문만 있도록 validation부분 controller로 이동
    - printLottos매서드 출력 부분 로직 수정
    - this를 사용하지 않는 메서드는 컨벤션에 맞게 정적 메서드로 수정
    - Console 인스턴스 종료하는 매서드 생성

--- src/view.js
+++ src/view.js
@@ -1,50 +1,49 @@
 const { Console } = require('@woowacourse/mission-utils');
-const Validation = require('./utils/validation');
+const { OUTPUT_MESSAGE } = require('./utils/constant');
+
 class View {
   constructor(controller) {
     this.controller = controller;
   }
 
+  static endgame() {
+    Console.close();
+  }
+
   getAmountInput() {
-    Console.readLine('구입금액을 입력해 주세요.\n', (answer) => {
-      Validation.amountInputValidate(answer);
+    Console.readLine(OUTPUT_MESSAGE.ENTER_AMOUNT, (answer) => {
       this.controller.generateLotto(answer);
     });
   }
 
-  printLottos(lottos) {
+  static printLottos(lottos) {
     Console.print(`${lottos.length}개를 구매했습니다.`);
-    lottos.forEach((lotto) => Console.print(lotto.getNumber()));
+
+    lottos.forEach((lotto) => Console.print(`[${String(lotto.getNumber()).replace(/,/g, ', ')}]`));
   }
 
   winningNumber() {
-    Console.readLine('당첨 번호를 입력해 주세요.\n', (answer) => {
-      Validation.winningNumberValidate(answer);
+    Console.readLine(OUTPUT_MESSAGE.ENTER_WINNING_NUMBER, (answer) => {
       this.controller.enterWinningNumber(answer.split(','));
     });
   }
 
   bonusNumberInput() {
-    Console.readLine('보너스 번호를 입력해 주세요.\n', (answer) => {
-      Validation.bonusNumberValidate(answer);
+    Console.readLine(OUTPUT_MESSAGE.ENTER_BONUS_NUMBER, (answer) => {
       this.controller.enterBonusNumber(answer);
     });
   }
 
-  printWinningResult(result) {
+  static printWinningResult(result, earningRate) {
     const [fifthRanking, forthRanking, thirdRanking, secondRanking, firstRanking] = result;
-    let grossEarnings = 0;
-    const prizeMoney = [5000, 50000, 1500000, 30000000, 2000000000];
-    Console.print('당첨 통계\n---');
+
+    Console.print(OUTPUT_MESSAGE.WINNING_STATISTICS);
+
     Console.print(
-      `3개 일치 (5000원) - ${fifthRanking}개\n4개 일치 (50,000원) - ${forthRanking}개\n5개 일치 (1,500,000원) - ${thirdRanking}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${secondRanking}개\n6개 일치 (2,000,000,000원) - ${firstRanking}개`,
+      `3개 일치 (5,000원) - ${fifthRanking}개\n4개 일치 (50,000원) - ${forthRanking}개\n5개 일치 (1,500,000원) - ${thirdRanking}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${secondRanking}개\n6개 일치 (2,000,000,000원) - ${firstRanking}개`,
     );
-    result.forEach((result, index) => {
-      grossEarnings += result * prizeMoney[index];
-    });
-    Console.print(
-      `총 수익률은 ${((grossEarnings / this.controller.totalAmount) * 100).toFixed(1)}%입니다.`,
-    );
+
+    Console.print(`총 수익률은 ${earningRate}%입니다.`);
   }
 }
 
