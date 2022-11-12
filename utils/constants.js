e6cf676304f84be7abc966a6c5cfc0508af61bb9
turtle601 <turtle601@naver.com>
2022-11-12 14:48:20 +0900

    refactor(constant) VARIABLE_LOTTO enum 데이터 추가
    Add:
    1) lotto: 'lotto'
    2) bonus: 'bonus'

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,12 @@
 const VARIABLE_LOTTO = Object.freeze({
+  lotto: 'lotto',
+  bonus: 'bonus',
+
   regex: /^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$/,
   start: 1,
   end: 45,
   len: 6,
+
   priceUnit: 1000,
   priceRegex: /^[1-9]+0{3,}$/,
 });
@@ -12,6 +16,7 @@ const LOTTO_ERROR_MESSAGE = Object.freeze({
   len: '[ERROR] 로또 번호는 6개여야 합니다.',
   overlap: '[ERROR] 로또 번호는 중복이 되어서는 안됩니다.',
   priceLimit: '[ERROR] 금액 단위는 천 단위여야 합니다.',
+  factoryTypeError: '[ERROR] 로또 타입을 명시해야 합니다.',
 });
 
 const LOTTO_AMOUNT = Object.freeze([
