df39999ba56c9d6ebbd5bfb33722cc532200a7c0
Rki0 <pky00823@gmail.com>
2022-11-12 14:15:29 +0900

    test(Result): change variables and add tests
    As the variable in the class changes, the variable used in the test is modified.
    
    Add tests for modularized functions.

--- __tests__/ResultTest.js
+++ __tests__/ResultTest.js
@@ -9,9 +9,36 @@ describe("결과 테스트", () => {
       7
     );
 
-    result.calculateOneLotto(result.lottoArray[0]);
+    result.calculateOneLotto(result.bundleOfLotto[0]);
 
     expect(result.score).toBe(0);
+    expect(result.matchBonus).toBe(true);
+  });
+
+  test("로또와 당첨 번호가 하나 일치하면 점수가 1 오른다.", () => {
+    const result = new Result(
+      1000,
+      [[1, 9, 12, 22, 31, 45]],
+      [1, 2, 3, 4, 5, 6],
+      7
+    );
+
+    result.score = 0;
+    result.calculateScore(1);
+
+    expect(result.score).toBe(1);
+  });
+
+  test("보너스 번호 당첨 여부는 참, 거짓으로 판단한다.", () => {
+    const result = new Result(
+      1000,
+      [[1, 7, 12, 22, 31, 45]],
+      [1, 2, 3, 4, 5, 6],
+      7
+    );
+
+    result.checkBonusNumber(7);
+
     expect(result.matchBonus).toBe(true);
   });
 
@@ -38,6 +65,8 @@ describe("결과 테스트", () => {
       7
     );
 
+    result.rank = [0, 1, 0, 0, 0];
+
     const finalPrize = result.getPrize();
 
     expect(finalPrize).toBe(30000000);
@@ -57,6 +86,7 @@ describe("결과 테스트", () => {
 
     const result = new Result("8000", lottoArray, [1, 2, 3, 4, 5, 6], 7);
 
+    result.rank = [0, 0, 0, 0, 1];
     const earningsRate = result.getEarningsRate();
 
     expect(earningsRate).toBe("62.5");
