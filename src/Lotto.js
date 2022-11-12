699fc8048ec933158e3146d9fcfba2914734a2b8
Hwang Seong Taek <hst007@naver.com>
2022-11-12 20:16:49 +0900

    feat: 1부터 45사이의 당첨 번호가 아닐 경우 예외 처리 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,4 @@
-const { LOTTO_ERROR_MESSAGE } = require("./constants");
+const { LOTTO_ERROR_MESSAGE } = require('./constants');
 
 class Lotto {
   #numbers;
@@ -9,15 +9,13 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error(LOTTO_ERROR_MESSAGE.LENGTH);
-    }
-    if (new Set(numbers).size !== 6) {
-      throw new Error(LOTTO_ERROR_MESSAGE.DUPLICATE);
-    }
+    if (numbers.length !== 6) throw new Error(LOTTO_ERROR_MESSAGE.LENGTH);
+    if (new Set(numbers).size !== 6) throw new Error(LOTTO_ERROR_MESSAGE.DUPLICATE);
+    const inRangeNumber = numbers.filter(x => x >= 1 && x <= 45).length;
+    if (inRangeNumber !== 6) throw new Error(LOTTO_ERROR_MESSAGE.OUT_OF_RANGE);
   }
-
   // TODO: 추가 기능 구현
 }
+
 
 module.exports = Lotto;
