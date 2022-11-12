ca1382c212f896b24cc93f2859c6c85187fc4017
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-11-12 22:56:05 +0900

    Refactor: 일부 변수명과 범위 상수 추가

--- src/constant/errorMessage.js
+++ src/constant/errorMessage.js
@@ -1,16 +1,32 @@
-const MESSAGE = {
+const PRICE_MESSAGE = {
   NOT_INPUT_NUMBER: `[ERROR] 구입 금액은 숫자만 입력해야 합니다.`,
   ONLY_INPUT_THOUSAND: `[ERROR] 입력할 로또 구입 금액은 1,000원 단위여야 합니다.`,
   BELOW_THOUSAND: `[ERROR] 로또 구입 금액은 최소 1,000원입니다.`,
 };
 
-const TYPE = {
+const LOTTO_MESSAGE = {
+  NOT_SIX_NUMBERS: `[ERROR] 로또 번호는 6개여야 합니다.`,
+  CHECK_RANGE: `[ERROR] 당첨 번호는 1 ~ 45 사이의 숫자만 입력해야 합니다.`,
+  CHECK_DUPLICATION: `[ERROR] 중복된 번호가 존재합니다.`,
+  CHECK_VALIDATION_INPUT: `[ERROR] 숫자 이외의 값을 입력하셨습니다.`,
+};
+
+const PRICE_TYPE = {
   NOT_NUMBER: `Not a Number in inside`,
   NOT_THOUSAND: `Not a Thousand in inside`,
   NOT_BUY_LOTTO: `Can not buy a lotto`,
 };
 
+const LOTTO_TYPE = {
+  NOT_SIX_NUMBERS: `Not 6 Numbers`,
+  NOT_RANGE: `Only numbers for the Range`,
+  IS_DUPLICATION: `Exists Duplication`,
+  IS_VALIDATION: `Exists other than a Number`,
+};
+
 module.exports = {
-  MESSAGE,
-  TYPE,
+  PRICE_MESSAGE,
+  LOTTO_MESSAGE,
+  PRICE_TYPE,
+  LOTTO_TYPE,
 };
--- src/constant/inputMessage.js
+++ src/constant/inputMessage.js
@@ -1,6 +1,8 @@
 const THOUSAND = 1000;
 
 const GAME_START_MESSAGE = "구입금액을 입력해 주세요.\n";
+const WIN_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.\n";
+const BONUS_NUMBERS_MESSAGE = "보너스 번호를 입력해 주세요.\n";
 const BUY_LOTTOS_MESSAGE = (count) => `${count}개를 구매했습니다.`;
 
 const RANGE = {
@@ -12,6 +14,8 @@ const RANGE = {
 module.exports = {
   GAME_START_MESSAGE,
   BUY_LOTTOS_MESSAGE,
+  WIN_NUMBERS_MESSAGE,
+  BONUS_NUMBERS_MESSAGE,
   THOUSAND,
   RANGE,
 };
