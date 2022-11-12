bcdcee9c3e5c236f38b4f3e39f443db46859b7de
xilucks <siun0331@gmail.com>
2022-11-12 20:30:53 +0900

    feat: 로또 판매점에 필요한 프로세스 생성

---
+++ src/LottoMarket.js
@@ -0,0 +1,42 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require('./Lotto');
+const { Console, Random } = MissionUtils;
+const constants = require("./constants");
+
+class LottoMarket {
+  purchaseLotto(money) {
+    let userLottos = [];
+    while (userLottos.length < parseInt(money / 1000)) {
+      userLottos.push(new Lotto(this.drawLottery()));
+    }
+    this.showLottoes(userLottos);
+    return userLottos;
+  }
+
+  showLottoes(buyingLottoes) {
+    let Lottoes = buyingLottoes.reduce((acc, cur) => {
+      Console.print(cur);
+    }, []);
+  }
+
+  drawLottery() {
+    let lottoNums = [];
+    while (lottoNums.length < 6) {
+      lottoNums.push(this.createNumber(lottoNums));
+    }
+    lottoNums.sort((a, b) => a - b);
+    return lottoNums;
+  }
+
+  createNumber(winningNumbers) {
+    let newNumber = Random.pickNumberInRange(1, 45);
+
+    return this.validate(winningNumbers, newNumber)
+      ? this.createNumber(winningNumbers)
+      : parseInt(newNumber);
+  }
+
+  validate(winningNumbers, newNumber) {
+    return winningNumbers.includes(newNumber);
+  }
+}
