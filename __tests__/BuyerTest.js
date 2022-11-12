c5be0316c11f7f8971a96d022e7009aaf8d7b1ed
GuHyeonWoo <ghw9174@gmail.com>
2022-11-12 18:32:06 +0900

    feat (Buyer.js): 입력 금액이 정수가 아닌 경우 예외처리
    README.md: 2.1.의 정수가 아닐 경우 예외처리를 해결으로 수정
    Buyer.js: stringException 함수를 통해 입력 값이 정수인지 확인
    BuyerTest.js: 클래스 생성 시 문자열을 넣었을 때 예상한 에러 메세지가 나오는지 확인
    
    docs/README.md #2.1. 정수가 아닐 경우 예외처리

--- __tests__/BuyerTest.js
+++ __tests__/BuyerTest.js
@@ -5,4 +5,10 @@ describe("구매자 클래스 테스트", () => {
     const buyer = new Buyer(5000);
     expect(buyer.getMoney()).toBe(5000);
   });
+
+  test("입력된 금액이 정수가 아닐경우 예외처리한다.", () => {
+    expect(() => {
+      new Buyer('string');
+    }).toThrowError("[ERROR] 금액은 정수로 입력해야 합니다.");
+  });
 });
--- src/Buyer.js
+++ src/Buyer.js
@@ -4,11 +4,18 @@ class Buyer {
     #money;
 
     constructor(money) {
+        this.stringException(money);
         this.#money = money;
     }
 
     getMoney() {
         return this.#money;
+    }
+
+    stringException(money) {
+        if (!Number(money)) {
+            throw new Error("[ERROR] 금액은 정수로 입력해야 합니다.");
+        }
     }
 }
 
