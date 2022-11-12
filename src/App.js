cef1dc1aa0cae1da3d23bfc67d9f836f47890e09
loopy-lim <loopy@jnu.ac.kr>
2022-11-12 21:34:57 +0900

    refector(valiator, console): validator을 console에서 검사한다

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ const { InputConsole, OutputConsole } = require('./Console');
 const {
   LottoPurchaseDto,
   LottoInputDto,
-  LottoValidator,
 } = require('./LottoDto');
 const { LOTTO_COST } = require('./Resource');
 
 class App {
   #LottoPurchases = [];
+  #LottoInputDto;
 
   async play() {
     const money = await InputConsole.getMoney();
-    LottoValidator.checkMoney(money);
 
     this.#LottoPurchases = Array.from(
       { length: money / LOTTO_COST },
       () => new LottoPurchaseDto(),
     );
+
     const lottoNumbers = await InputConsole.getLotto();
+    this.#LottoInputDto = new LottoInputDto(lottoNumbers, 0);
   }
 }
 
--- src/Console/Input.console.js
+++ src/Console/Input.console.js
@@ -1,9 +1,18 @@
 const Console = require('./Console');
 const { INPUT } = require('../Resource/String');
+const { LottoValidator } = require('../LottoDto');
 
 class InputConsole {
-  static getMoney = async () => Console.input(INPUT.GET_MONEY);
-  static getLotto = async () => Console.input(INPUT.GET_LOTTO);
+  static getMoney = async () => {
+    const money = Console.input(INPUT.GET_MONEY);
+    LottoValidator.checkMoney(money);
+    return money;
+  };
+  static getLotto = async () => {
+    const lottoNumbersString = await Console.input(INPUT.GET_LOTTO);
+    const lottoNumbers = lottoNumbersString.split(',');
+    return lottoNumbers
+  };
 }
 
 module.exports = InputConsole;
