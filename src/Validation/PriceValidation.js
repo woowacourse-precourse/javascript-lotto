b190453d2457a9d9465752f57b3b46037679ecfc
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-12 23:20:18 +0900

    feat :  로또 금액 입력값 유효성 검사 - 1000원 단위인지 확인

--- src/Validation/PriceValidation.js
+++ src/Validation/PriceValidation.js
@@ -10,6 +10,7 @@ class PriceValidation extends Validation {
   validate() {
     this.checkIsEmpty();
     this.checkIsNumber();
+    this.checkIsUnitNumber();
   }
 
   checkIsEmpty() {
@@ -23,6 +24,14 @@ class PriceValidation extends Validation {
     const regExp = /^[0-9]+$/;
     if (!regExp.test(this.answer)) {
       throw new Error(PRICE_ERROR_MESSAGE.not_valid_number);
+    }
+    return true;
+  }
+
+  checkIsUnitNumber() {
+    const price = Number(this.answer);
+    if (price % 1000 !== 0) {
+      throw new Error(PRICE_ERROR_MESSAGE.not_valid_unit_number);
     }
     return true;
   }
