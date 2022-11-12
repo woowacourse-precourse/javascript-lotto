de3d8168c24a47b3c25bc990e2c3ec45470ec4fb
yong <tkddbs1644@gmail.com>
2022-11-12 21:25:03 +0900

    test(Utils): 배열 자료형을 문자열로 변환하는 테스트 코드

---
+++ __tests__/UtilsTest.js
@@ -0,0 +1,15 @@
+const Utils = require("../src/Utils");
+
+describe("유틸 클래스 테스트", () => {
+  test("transformArrayToString: 배열 자료형을 문자열 형태로 변환한다.", () => {
+    const TEST_CASE = [
+      {
+        array: [1, 2, 3, 4, 5, 6],
+        result: "[1, 2, 3, 4, 5, 6]",
+      },
+    ];
+    TEST_CASE.forEach(({ array, result }) => {
+      expect(Utils.transformArrayToString(array)).toEqual(result);
+    });
+  });
+});
