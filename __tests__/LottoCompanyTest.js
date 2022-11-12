bf075e26ea01ef7842bbea576b9e0e75c9e73f82
MyungAe <ladiescheong@naver.com>
2022-11-13 01:04:29 +0900

    test(LottoCompany): 기능 3에 대한 테스트 코드 수정
    주요 변경사항
    - 입력값을 받는 메소드와 변환하는 메소드를 분리할 수 있도록 테스트 로직 변경
    - arrow 함수를 이용해 입력값을 주고도 에러가 날 수 있도록 테스트 수정

--- __tests__/LottoCompanyTest.js
+++ __tests__/LottoCompanyTest.js
@@ -8,26 +8,18 @@ const MissionUtils = require("@woowacourse/mission-utils");
 //   });
 // });
 
-const mockQuestions = (answer) => {
-  MissionUtils.Console.readLine = jest.fn().mockReturnValue(answer);
-};
-
 describe("Feat 4. LottoCompany.drawLottoNumber", () => {
   const lottoCompany = new LottoCompany();
 
-  test("로또 당첨 번호와 보너스 번호를 입력받아 당첨 번호 전체를 반환한다.", () => {
-    mockQuestions("1,2,3,4,5,6");
-    mockQuestions("7");
-    expect(lottoCompany.drawLottoNumber()).toEqual([1, 2, 3, 4, 5, 6, 7]);
+  test("로또 당첨 번호를 받아 당첨 번호 전체를 반환한다.", () => {
+    expect(lottoCompany.transferIntArray("1,2,3,4,5,6,7")).toEqual([
+      1, 2, 3, 4, 5, 6, 7,
+    ]);
   });
 
   test("입력된 로또 당첨 번호가 숫자가 아닌 경우 예외를 반환한다.", () => {
-    mockRandoms("1,2,3,4,5,A");
-    expect(lottoCompany.drawLottoNumber).toThrow("[ERROR]");
-  });
-
-  test("입력된 보너스 번호가 숫자가 아닌 경우 예외를 반환한다.", () => {
-    mockRandoms("A");
-    expect(lottoCompany.drawLottoNumber).toThrow("[ERROR]");
+    expect(() => {
+      lottoCompany.transferIntArray("a,2,3,4,5,6,7");
+    }).toThrow("[ERROR]");
   });
 });
