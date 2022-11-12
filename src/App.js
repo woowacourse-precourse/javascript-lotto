c2def23bfdd02229c7332e41a4df081b3d7c6d5d
xilucks <siun0331@gmail.com>
2022-11-12 20:56:52 +0900

    feat: 각 객체를 생성하고 유저가 로또를 구입하는 과정까지 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { Console, Random } = MissionUtils;
+const User = require('./User');
+const LottoMarket = require('./LottoMarket');
+const {MESSAGE_INPUT_PURCHASE_AMOUNT} =require('./constants');
+
 class App {
   play() {
-
+    const USER = new User();
+    const LOTTO_MARKET = new LottoMarket();
+    Console.readLine(MESSAGE_INPUT_PURCHASE_AMOUNT, (purchaseAmount) => {
+      USER.setUserMoney(purchaseAmount);
+      USER.buyLotto(LOTTO_MARKET);
+    })
   }
 }
+
+const ap = new App();
+ap.play();
 
 module.exports = App;
