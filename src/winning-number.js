5c6eda7f62b6e8dee75f270ad1298e2c000d5f74
Ryong-E <think8867@gmail.com>
2022-11-12 22:42:18 +0900

    refactor(winning-number): set할때 validate를 하도록 매서드 생성
    - validation이라는 클래스를 사용하지않고 내부에서 바로 예외처리할 수 있어 불필요한 클래스를 줄일수있음

--- src/winning-number.js
+++ src/winning-number.js
@@ -1,9 +1,17 @@
+const { ERROR_MASSAGE, NUMBER_LIMIT } = require('./utils/constant');
+
+const { LOTTO_NUMBER_LENGTH_MESSAGE, POSSIBLE_LOTTO_NUMBER_MESSAGE, DUPLICATE_NUMBER_MESSAGE } =
+  ERROR_MASSAGE;
+const { MAX_NUMBER, MIN_NUMBER, QUANTITY } = NUMBER_LIMIT;
+
 class WinningNumber {
   setWinningNumber(numbers) {
+    WinningNumber.#winningNumberValidate(numbers);
     this.winningNumber = numbers;
   }
 
   setBonusNumber(number) {
+    this.#bonusNumberValidate(number);
     this.bonusNumber = number;
   }
 
@@ -13,6 +21,34 @@ class WinningNumber {
 
   getBonusNumber() {
     return this.bonusNumber;
+  }
+
+  static #winningNumberValidate(input) {
+    if (new Set(input).size !== QUANTITY) {
+      throw new Error(LOTTO_NUMBER_LENGTH_MESSAGE);
+    }
+
+    input.forEach((number) => {
+      if (Number(number) >= MIN_NUMBER && Number(number) <= MAX_NUMBER) {
+        return;
+      }
+
+      throw new Error(POSSIBLE_LOTTO_NUMBER_MESSAGE);
+    });
+  }
+
+  #bonusNumberValidate(input) {
+    if (this.winningNumber.includes(input)) {
+      throw new Error(DUPLICATE_NUMBER_MESSAGE);
+    }
+
+    if (input.length > 2) {
+      throw new Error(POSSIBLE_LOTTO_NUMBER_MESSAGE);
+    }
+
+    if (!(Number(input) >= MIN_NUMBER && Number(input) <= MAX_NUMBER)) {
+      throw new Error(POSSIBLE_LOTTO_NUMBER_MESSAGE);
+    }
   }
 }
 
