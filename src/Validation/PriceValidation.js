9591d19f27b1ea3a1f634e54da625ffdb2223d86
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-12 23:19:13 +0900

    feat :  로또 금액 입력값 유효성 검사 - 숫자인지 확인

--- src/Validation/PriceValidation.js
+++ src/Validation/PriceValidation.js
@@ -9,11 +9,20 @@ class PriceValidation extends Validation {
 
   validate() {
     this.checkIsEmpty();
+    this.checkIsNumber();
   }
 
   checkIsEmpty() {
     if (this.answer === null || this.answer === undefined || this.answer === '') {
       throw new Error(PRICE_ERROR_MESSAGE.not_valid_answer);
+    }
+    return true;
+  }
+
+  checkIsNumber() {
+    const regExp = /^[0-9]+$/;
+    if (!regExp.test(this.answer)) {
+      throw new Error(PRICE_ERROR_MESSAGE.not_valid_number);
     }
     return true;
   }
