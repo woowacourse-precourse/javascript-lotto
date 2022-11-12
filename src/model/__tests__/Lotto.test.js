37dd514315450fab926ac7aa0e1921f4f89f7e4c
yeong bin <2976622@naver.com>
2022-11-12 17:25:28 +0900

    test(model): Lotto모델 테스트 코드 추가

--- src/model/__tests__/Lotto.test.js
+++ src/model/__tests__/Lotto.test.js
@@ -1,6 +1,6 @@
 const Lotto = require("../Lotto");
 
-describe("로또 생성 테스트", () => {
+describe("Lotto 유효성 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
@@ -17,5 +17,19 @@ describe("로또 생성 테스트", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 99]);
     }).toThrow("[ERROR]");
+  });
+});
+
+describe("Lotto 메서드 테스트", () => {
+	const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+	test("getNumbers()", () => {
+    expect(lotto.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+	test("getBonus()", () => {
+		lotto.setBonus(7);
+    expect(lotto.getBonus()).toEqual(7);
+  });
+	test("setBonus() 에러 - numbers에 있는 값 추가", () => {
+    expect(lotto.setBonus(6)).toThrow("[ERROR]");
   });
 });
