eb161fdc130eeaefdaa6b6c8296c923c381e4864
minjaeKim <smallkdb@gmail.com>
2022-11-12 18:50:18 +0900

    feat(LottoGame): 로또 구입 갯수를 출력하는 purchase 함수 추가
    로또를 구입한 갯수를 출력하는 purchase 함수를 추가하였습니다.
    - 로또 구매 가격을 저장하기 위해 생성자에 money를 추가하였습니다.

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,11 +1,22 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 class LottoGame {
+  constructor() {
+    this.money = 0;
+  }
+
   run() {
     Console.readLine('구입금액을 입력해 주세요.\n', (input) => this.inputMoney(input));
   }
 
-  inputMoney(input) {}
+  inputMoney(input) {
+    this.money = Number(input);
+    this.purchase(this.money / 1000);
+  }
+
+  purchase(number) {
+    Console.print(`\n${number}개를 구매했습니다.`);
+  }
 }
 
 module.exports = LottoGame;
