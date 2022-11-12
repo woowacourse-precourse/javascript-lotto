c57af89e45deaced6c4853ecf21c1a5d004c4e8a
GuHyeonWoo <ghw9174@gmail.com>
2022-11-12 20:27:23 +0900

    feat (Lotto.js): 로또 번호 중복 될 시 예외처리 구현
    README.md:
    - 2.3. 중복되는 번호가 있을 경우 예외처리 해결 표시
    Lotto.js:
    - duplicateCheck(): 리스트 중복 확인 후 중복된 수가 있을 시 throw 처리
    LottoTest.js
    - 로또 번호에 중복된 숫자가 있으면 예외가 발생한다. 처리
    
    docs/README.md #2.3. 중복되는 번호가 있을 경우 예외처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,6 +3,7 @@ class Lotto {
 
   constructor(numbers) {
     this.validate(numbers);
+    this.duplicateCheck(numbers);
     this.#numbers = numbers;
   }
 
@@ -12,7 +13,14 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  duplicateCheck(numbers) {
+    let tempNumbers = new Set(numbers);
+    tempNumbers = [...tempNumbers];
+
+    if(tempNumbers.length < 6){
+      throw new Error("[ERROR] 로또 번호는 중복되면 안됩니다.");
+    }
+  }
 }
 
 module.exports = Lotto;
