19fcc7f2b10beb997c962347cb3ad9c637a8c429
d0422 <rlfehd2013@naver.com>
2022-11-13 01:14:51 +0900

    Test:로또 범위가 1보다작거나, 45보다 큰 경우의 테스트코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,4 @@
-const Lotto = require('../src/Lotto');
+const Lotto = require('../src/Model/Lotto');
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -17,6 +17,18 @@ describe('로또 클래스 테스트', () => {
   test('로또 번호에 숫자가 아닌 문자가 오면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto(['가', 2, 3, 4, 6, 7]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호 범위 테스트', () => {
+    expect(() => {
+      new Lotto([65, 75, 85, 95, 1000, 12]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호 범위 테스트', () => {
+    expect(() => {
+      new Lotto([0, 1, -5, 95, 1000, 12]);
     }).toThrow('[ERROR]');
   });
 });
