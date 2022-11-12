f3e3e49fe058ce5108801fa7d747e8b146e5cfec
moonelysian <dandyblack@naver.com>
2022-11-12 10:07:02 +0900

    feat: printRankCountMessage 추가
    - 각 등수가 몇개 맞았는지 부분 출력
    - 상수로 만들기 어려워서 함수화 시킴

--- src/utils.js
+++ src/utils.js
@@ -29,10 +29,29 @@ const checkIsOutOfRange = (number) => {
   }
 }
 
+const printRankCountMessage = (result) => {
+  let message = ''
+  const reversedKeys = Object.keys(result).sort((key1, key2) => key2 - key1);
+  
+  for (let rank of reversedKeys) {
+    const { count, prize } = result[rank];
+    rank = parseInt(rank);
+    if (rank === 1) message += '6개 일치';
+    if (rank === 2) message += '5개 일치, 보너스 볼 일치';
+    if (rank === 3) message += '5개 일치';
+    if (rank === 4) message += '4개 일치';
+    if (rank === 5) message += '3개 일치';
+    message += ` (${prize.toLocaleString()}원) - ${count}개\n`
+  }
+
+  printMessage(message);
+}
+
 module.exports = {
   printMessage,
   userInput,
   throwErrorMessage,
   checkIsNumber,
   checkIsOutOfRange,
+  printRankCountMessage,
 }
