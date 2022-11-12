8b2706be3970a8d01b0d16de75bd46bb1ab183ac
xilucks <siun0331@gmail.com>
2022-11-12 21:18:54 +0900

    feat: 결과 출력을 위한 로또번호 유효성 검사 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,11 +10,20 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    this.rangeValidate(numbers);
   }
 
   // TODO: 추가 기능 구현
   getNumbers(){
     return this.#numbers;
+  }
+
+  rangeValidate(numbers){
+    numbers.map((number) => {
+      if(number >= 1 && number <= 45){
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    });
   }
 }
 
