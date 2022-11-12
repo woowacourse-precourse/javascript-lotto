81656bf0972213d0b73878746e98287c64114978
userHWSEO <gmldnjseocnd@gmail.com>
2022-11-12 11:38:53 +0900

    feat: 유저 로또 생성 기능 구현
    - 유저의 로또 구입 금액을 입력 받음
    - 금액 만큼 로또를 생성함
    - 생성된 로또는 오름차순으로 정렬
    - 정렬 된 로또 Console.print 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+  userLottoNumber() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요. \n", (money) => {
+      const lottoSheets = money / 1000;
+      const userLottos = [];
+      MissionUtils.Console.print(`${lottoSheets}개를 구매했습니다. \n`);
+      for (let i = 0; i < lottoSheets; i++) {
+        const userLotto = MissionUtils.Random.pickUniqueNumbersInRange(
+          1,
+          45,
+          6
+        ).sort((a, b) => {
+          return a - b;
+        });
+        MissionUtils.Console.print(userLotto);
+        userLottos.push(userLotto);
+      }
+    });
+  }
 }
+
+const app = new App();
+app.userLottoNumber();
 
 module.exports = App;
