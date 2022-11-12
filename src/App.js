3bffa1cb99f9bfd0433af9a095254e027b2a4684
LEEJW1953 <jiwonlee1953@naver.com>
2022-11-12 22:12:12 +0900

    feat: 로또 구입 금액을 입력받고 유효성을 확인하는 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+
+  purchasePrice() { // 구입 금액을 입력받는 메서드
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (price) => {
+      if (checkPrice(price)) { }
+    })
+  }
+
+  checkPrice(price) { // 구입 금액이 조건에 맞게 입력했는지 확인하는 메서드
+    for (let i = 0; i < price.length; i++) {
+      if (isNaN(parseInt(price[i]))) {
+        throw new Error("[ERROR] 구입 금액은 정수여야 합니다.");
+      }
+    }
+    if ((price === '0') || (price % 1000 !== 0)) {
+      throw new Error("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
+    }
+    return;
+  }
+  play() { }
 }
 
 module.exports = App;
+
+// const app = new App();
+// app.play();
