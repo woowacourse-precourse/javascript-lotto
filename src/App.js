bef1a0a48af067fb813b73344c86492c80f2e1a1
kinoasm <kinoasm@gmail.com>
2022-11-12 22:03:48 +0900

    feat: 입력 오류 처리

--- src/App.js
+++ src/App.js
@@ -13,6 +13,15 @@ class App {
             }
         );
     }
-    getMoney(int) {}
+    getMoney(input) {
+        let now = this.checkMoney(input);
+    }
+    checkMoney(input) {
+        if (isNaN(input))
+            throw new Error("[ERROR] 입력 금액은 숫자여야 합니다.");
+        if (Number(input) % 1000 !== 0)
+            throw new Error("[ERROR] 1000원 단위로 입력해 주세요.");
+        return Number(input) / 1000;
+    }
 }
 module.exports = App;
