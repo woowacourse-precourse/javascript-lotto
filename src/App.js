9ead6097797561cd630f203fbdfac84695931016
jiho1996 <jhp9270@naver.com>
2022-11-12 17:43:07 +0900

    refactor : 입력에따른 함수 분리

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
+const Calculator = require("./Calculator");
+const MyNumberGenerator = require("./Generator");
 const Lotto = require("./Lotto");
 const { readLine } = require("./Missionutils");
 
 class App {
+  #winningNumbers;
+  #numberGenerator = new MyNumberGenerator();
+
   play() {
-    this.getWinningNumbersFromUser();
+    this.inputMoneyFromUser();
   }
-  getWinningNumbersFromUser() {
-    readLine("당첨 번호를 입력해 주세요.", (winningNumbers) => {
-      this.getBonusWinningNumberFromUser(winningNumbers);
+
+  inputMoneyFromUser() {
+    readLine("구입 금액을 입력해주세요.", (moneyInput) => {
+      const myLottoNumbers = this.#numberGenerator.generateMyLottoNumber(moneyInput);
+      this.getWinningNumbersFromUser(myLottoNumbers);
     });
   }
-  getBonusWinningNumberFromUser(winningNumber) {
+
+  getWinningNumbersFromUser(myLottoNumbers) {
+    readLine("당첨 번호를 입력해 주세요.", (winningNumber) => {
+      this.getBonusWinningNumberFromUser(myLottoNumbers, winningNumber);
+    });
+  }
+
+  getBonusWinningNumberFromUser(myLottoNumbers, winningNumber) {
     readLine("보너스 번호를 입력해 주세요.", (bonusNumber) => {
-      new Lotto({ winningNumber, bonus: +bonusNumber });
+      this.#winningNumbers = new Lotto({ winningNumber, bonus: +bonusNumber });
+      this.getResult(myLottoNumbers);
     });
+  }
+  getResult(myLottoNumbers) {
+    this.calculateWinningCount(myLottoNumbers);
+  }
+  calculateWinningCount(myLottoNumbers) {
+    new Calculator(myLottoNumbers, this.#winningNumbers);
   }
 }
 const a = new App();
