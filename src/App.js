3fe2619a04137fd39714a28fd8f761f1ab1241f0
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-12 23:35:47 +0900

    feat : 로또 발행 및 저장 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 const { GAME_MESSAGE, PRICE_MEASURE } = require('./lib/Constants');
-const { print, readLine } = require('./lib/Utils');
+const { print, readLine, pickUniqueNumbersInRange } = require('./lib/Utils');
 const PriceValidation = require('./Validation/PriceValidation');
 
 class App {
   lottoPrice = 0;
 
   lottoCount = 0;
+
+  lottoList = [];
 
   play() {
     readLine(GAME_MESSAGE.input_price, (answer) => this.getLottoPrice(answer));
@@ -30,6 +32,17 @@ class App {
 
   printLottoCount() {
     print(`\n${this.lottoCount}개를 구매했습니다.`);
+    return this.publishLottos();
+  }
+
+  publishLottos() {
+    const lottos = [];
+    for (let index = 0; index < this.lottoCount; index += 1) {
+      const lotto = pickUniqueNumbersInRange(1, 45, 6);
+      lottos.push(lotto.sort((a, b) => a - b));
+    }
+
+    this.lottoList = lottos;
   }
 }
 
