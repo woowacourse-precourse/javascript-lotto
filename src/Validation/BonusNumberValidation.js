d0c85191fbbc49be1e77e42d02b8d23d726f7ba9
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-13 00:10:56 +0900

    feat : 로또 보너스 번호 유효성 검사 - 각 숫자가 1~45 범위의 숫자인지 확인

--- src/Validation/BonusNumberValidation.js
+++ src/Validation/BonusNumberValidation.js
@@ -11,11 +11,20 @@ class BonusNumberValidation extends Validation {
 
   validate() {
     this.checkIsEmpty();
+    this.checkIsRangeNumber();
   }
 
   checkIsEmpty() {
     if (this.answer === null || this.answer === undefined || this.answer === '') {
       throw new Error(BONUS_NUMBER_ERROR_MESSAGE.not_valid_answer);
+    }
+    return true;
+  }
+
+  checkIsRangeNumber() {
+    const regExp = /^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$/;
+    if (!regExp.test(this.answer)) {
+      throw new Error(BONUS_NUMBER_ERROR_MESSAGE.not_valid_range_number);
     }
     return true;
   }
