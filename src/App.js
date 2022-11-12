d1c9b603f6dd7195a1e1d76af69aee689262e41f
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-12 23:10:48 +0900

    feat : 로또 금액 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { GAME_MESSAGE } = require('./lib/Constants');
+const { readLine } = require('./lib/Utils');
+
 class App {
-  play() {}
+  play() {
+    readLine(GAME_MESSAGE.input_price, (answer) => this.getLottoPrice(answer));
+  }
+
+  getLottoPrice(answer) {
+    // TODO
+    // 금액 관련 유효성 검사
+    // 맴버변수에 금액 저장
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
