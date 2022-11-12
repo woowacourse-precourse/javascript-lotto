7b499c38abd16a91d52d62ce170b21212af146f1
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-12 16:18:36 +0900

    feat: calculate winning number in one lotto

---
+++ src/Calculate.js
@@ -0,0 +1,28 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const { result, money } = require('./Result');
+
+class Calculate {
+  constructor(amount, winning, bonus, lottos) {
+    this.amount = amount;
+    this.winning = winning;
+    this.bonus = bonus;
+    this.lottos = lottos;
+  }
+
+  //   lottos에 있는 lotto 번호 하나씩 보기
+  // lotto 번호의 요소가 winning에 있다면 count 증가
+  //하나의 로또에 대한 winning 당첨 갯수
+  count_number_of_wins(winning, lotto) {
+    const COUNT = lotto.reduce((count, number) => {
+      if (!winning.includes(number)) {
+        return count;
+      }
+      return (count += 1);
+    }, 0);
+    return COUNT;
+  }
+}
+
+module.exports = Calculate;
+
+const calculate = new Calculate();
