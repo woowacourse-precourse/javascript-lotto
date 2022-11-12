15ce982965404b527a906aa2b9784468225a3a00
2yunseong <dbsdltjd3701@naver.com>
2022-11-12 21:55:36 +0900

    feat(app): 입/출력 UI 객체 선언

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+const OutputUI = require('./OutputUI');
+const InputUI = require('./InputUI');
+
 class App {
+  constructor() {
+    this.input = new InputUI();
+    this.output = new OutputUI();
+  }
   play() {}
 }
 
