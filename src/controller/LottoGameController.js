829f39ad3c5adb7f34c5bb4fabecf657dcca9832
seongminn <berry9901@gmail.com>
2022-11-12 22:12:23 +0900

    refactor(LottoGameController): 문자열을 상수로 관리

--- src/controller/LottoGameController.js
+++ src/controller/LottoGameController.js
@@ -1,10 +1,11 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { INPUT_MESSAGE } = require('../constant');
 
 class LottoGameController {
   constructor() {}
 
   start() {
-    Console.readLine('구입금액을 입력해 주세요.', (input) => {});
+    Console.readLine(INPUT_MESSAGE.MONEY, (input) => {});
   }
 }
 
