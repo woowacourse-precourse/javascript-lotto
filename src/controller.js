1593af9371eb22f801a32817b27581c4d6c7adf8
Ryong-E <think8867@gmail.com>
2022-11-12 20:09:26 +0900

    refactor(controller): 클래스 분리로 로직 수정
    - validation 매서드 입력이 일어나는 메서드에 적용 view와 분리함으로써 view라는 클래스에 맞는 행동만 가능
    - 각 메서드 이름처럼 작동이 되도록 분리
    - 보너스 번호 예외처리를 위해 구입총액을 totalAmount이름으로 저장

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,8 @@
 const { Random } = require('@woowacourse/mission-utils');
+const CheckLotto = require('./check-lotto');
 const Lotto = require('./Lotto');
+const Calculate = require('./utils/calculate');
+const Validation = require('./utils/validation');
 const View = require('./view');
 const WinningNumber = require('./winning-number');
 
@@ -8,25 +11,50 @@ class Controller {
     this.lottos = [];
     this.view = new View(this);
     this.winningNumber = new WinningNumber();
+    this.checklotto = new CheckLotto(this);
+    this.validation = new Validation(this);
+    this.calculate = new Calculate(this);
   }
 
   generateLotto(answer) {
     const quantity = answer / 1000;
+    this.totalAmount = quantity * 1000;
+    Validation.amountInputValidate(answer);
+
     for (let count = 0; count < quantity; count += 1) {
       this.lottos.push(new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6)));
     }
 
-    this.view.printLottos(this.lottos);
+    View.printLottos(this.lottos);
     this.view.winningNumber();
   }
 
   enterWinningNumber(answer) {
+    Validation.winningNumberValidate(answer);
     this.winningNumber.setWinningNumber(answer);
     this.view.bonusNumberInput();
   }
 
   enterBonusNumber(answer) {
+    this.validation.bonusNumberValidate(answer);
     this.winningNumber.setBonusNumber(answer);
+    this.checkingLotto();
+  }
+
+  checkingLotto() {
+    this.checklotto.checkLotto();
+    this.endgame();
+  }
+
+  endgame() {
+    const winningResult = this.checklotto.getWinningresult();
+    const earningRate = this.calculate.getEarningRate(winningResult);
+    View.printWinningResult(winningResult, earningRate);
+    View.endgame();
+  }
+
+  init() {
+    this.view.getAmountInput();
   }
 }
 
