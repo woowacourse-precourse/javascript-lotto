1c28b801e96606b2b9b3a87c0b606f8247e132e9
kaori-killer <sksbthwjd@daum.net>
2022-11-12 15:57:41 +0900

    feat: N번 중복되지 않는 1~45의 숫자를 6개 발행한다.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-const { Console } = require('@woowacourse/mission-utils');
+const Utils = require('./Utils');
 const Lotto = require('./Lotto');
 
 class App {
   constructor() {
+    this.userLottos = [];
   }
 
   play() {
-    Console.readLine('구입금액을 입력해 주세요.', (input) => {
+    Utils.readLine('구입금액을 입력해 주세요.', (input) => {
       const amount = this.validateAmount(Number(input));
       const quantity = this.countLottoQuantity(amount);
-      Console.print(`${quantity}개를 구매했습니다.`);
+      Utils.print(`${quantity}개를 구매했습니다.`);
+      this.userLottos = this.createLotto(quantity);
+      console.log(this.userLottos)
     });
+
   }
 
-  validateAmount(amount){
-    if (amount === 0 || amount % 1000 !== 0) {
-      throw new Error("[ERROR] 구입금액은 1,000원 단위로 입력해야 합니다.");
+  validateAmount(input){
+    console.log(input);
+
+    if (input === 0 || input % 1000 !== 0) {
+      throw new Error("[ERROR] 구입금액은 1,000원 단위로 숫자만 입력해야 합니다.");
     }
-    return amount;
+    return input;
   }
 
   countLottoQuantity(amount){
+    let quantity = 0;
+    quantity = amount / 1000;
+
+    return quantity
   }
 
+  createLotto(quantity){
+    let lottos = [];
+    for(let count = 0; count < quantity; count++){
+      lottos.push(Utils.pickNumberInLotto());
+    }
+    return lottos;
+  } 
 
 }
 
