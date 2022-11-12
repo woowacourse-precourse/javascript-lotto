38788fdb9b9521267a1482445e24d8c6501f4ece
xilucks <siun0331@gmail.com>
2022-11-12 21:46:09 +0900

    feat: 초기 시작시, 유저, 마켓, 결과를 표기하는 객체를 생성하는 함수 분리

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const { Console, Random } = MissionUtils;
 const User = require('./User');
 const LottoMarket = require('./LottoMarket');
+const Result = require('./Result');
 const {MESSAGE_INPUT_PURCHASE_AMOUNT} =require('./constants');
 
 class App {
   play() {
-    const USER = new User();
-    const LOTTO_MARKET = new LottoMarket();
     Console.readLine(MESSAGE_INPUT_PURCHASE_AMOUNT, (purchaseAmount) => {
+      const [USER, LOTTO_MARKET, RESULT] = this.init()
       USER.setUserMoney(purchaseAmount);
       USER.buyLotto(LOTTO_MARKET);
+      RESULT.setWinningNumbers();
     })
+  }
+
+  init(){
+    const USER = new User();
+    const LOTTO_MARKET = new LottoMarket();
+    const RESULT = new Result();
+    return [USER, LOTTO_MARKET, RESULT];
   }
 }
 
