7f087c436bb8682404a770e53e8d62a3e601ab5a
jinlee <vito9270@naver.com>
2022-11-13 00:39:23 +0900

    feat: 숫자 rangeValidate 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,9 +9,16 @@ class Lotto {
   }
 
   validate(numbers) {
-    const deduplicationNumbers = new Set(numbers)
+    const deduplicationNumbers = new Set(numbers);
     if (deduplicationNumbers.size !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    this.rangeValidate(numbers.toString());
+  }
+
+  rangeValidate(numbers) {
+    if(!numbers.match(/[1-45]{6}/)){
+      throw new Error("[ERROR] 로또 번호는 1 ~ 45 사이의 숫자입니다.");
     }
   }
 }
