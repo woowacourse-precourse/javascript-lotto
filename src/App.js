4ed2a53c8e7a0f0f0df9b33a81389dd4a3c2b102
su2333066 <su2333066@naver.com>
2022-11-13 03:09:10 +0900

    2번 기능 구현

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ class App {
   constructor() {
     this.buyCost = 0;
     this.myLotto = [];
+    this.winLotto = undefined;
+    this.bonusNum = 0;
   }
 
   costValidation(cost) {
@@ -35,13 +37,66 @@ class App {
     }
   }
 
+  winNumValidation(winNum) {
+    const set = new Set(winNum);
+
+    for (let number of winNum) {
+      if (number < 1 || 45 < number) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    }
+
+    if (set.size !== winNum.length) {
+      throw new Error("[ERROR] 중복되지 않는 번호로 입력하세요.");
+    }
+  }
+
+  commaValidation(number) {
+    if (number.split(",").length - 1 !== 5) {
+      throw new Error("[ERROR] 콤마의 개수를 5개로 맞춰주세요.");
+    }
+  }
+
+  getWinLotto() {
+    MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.\n", (number) => {
+      this.commaValidation(number);
+      let winNum = number.split(",", 6).map(function (item) {
+        return parseInt(item, 10);
+      });
+      this.winNumValidation(winNum);
+      this.winLotto = new Lotto(winNum);
+      // this.getBonusNum();
+    });
+  }
+
+  // bonusValidation(bonusNum) {
+  //   if (this.winLotto.getWinLotto().includes(bonusNum)) {
+  //     throw new Error("[ERROR] 당첨 번호와 겹치지 않는 번호로 입력하세요.");
+  //   }
+  //   if (bonusNum < 1 || 45 < bonusNum) {
+  //     throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+  //   }
+  // }
+
+  // getBonusNum() {
+  //   MissionUtils.Console.readLine(
+  //     "보너스 번호를 입력해 주세요.\n",
+  //     (number) => {
+  //       const bonusNum = parseInt(number);
+  //       this.bonusValidation(bonusNum);
+  //     }
+  //   );
+  // }
+
   play() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (cost) => {
-      this.buyCost = this.costValidation(Number(cost));
+      this.buyCost = this.costValidation(parseInt(cost));
       const lottoCount = this.getLottoTicketCount(this.buyCost);
 
       MissionUtils.Console.print(`${lottoCount}개를 구매했습니다.`);
       this.getLottoNum(lottoCount);
+
+      this.getWinLotto();
     });
   }
 }
