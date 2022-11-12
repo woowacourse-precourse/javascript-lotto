c7a125cb99f824393e1e9a338e423a1c3f1dcfcd
jiho1996 <jhp9270@naver.com>
2022-11-12 12:46:31 +0900

    feat : 당첨 번호 입력 받은 숫자가 전부 숫자로 구성되어있는지 체크 예외처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,10 +7,27 @@ class Lotto {
 
   validate(numbers) {
     if (this.#isLengthNotEqualsSix(numbers)) throw Error("6자리가 아님ㅋ");
+    if (this.#isIncludeNotNumber(numbers)) throw Error("숫자가 아님ㅋ");
   }
 
   #isLengthNotEqualsSix(numbers) {
     return numbers.length !== 6;
+  }
+
+  #getConvertedWinningLottoNumber(numbers) {
+    this.#getSplittedNumber(numbers);
+  }
+
+  #getSplittedNumber(numbers) {
+    this.#winningNumbers = Array.from(numbers.split(","), this.#convertArgsStringToInt);
+  }
+
+  #convertArgsStringToInt(input) {
+    return +input;
+  }
+
+  #isIncludeNotNumber(input) {
+    return input.includes(NaN);
   }
 }
 
