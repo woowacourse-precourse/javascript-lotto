0d3e4469e529f526ac58910c1258cf9269e05967
jongwonp <jw032876@gmail.com>
2022-11-12 21:00:24 +0900

    refactor: App.js 파일 중간 정리

--- src/App.js
+++ src/App.js
@@ -2,55 +2,52 @@ const { Console, Random } = require('@woowacourse/mission-utils');
 const Lotto = require('./Lotto');
 
 class App {
+  myTickets = [];
+
   play() {
-    getPurchaseAmount();
+    Console.readLine('구입금액을 입력해주세요.', (num) => {
+      if (isDivisibleBy1000(num)) {
+        this.getHowManyLottos(num);
+      } else {
+        errorMessage();
+      }
+    });
+  }
+
+  getHowManyLottos(money) {
+    const numberOfTicket = money / 1000;
+
+    Console.print(`${numberOfTicket}개를 구매했습니다.`);
+
+    this.publishLottos(numberOfTicket);
+    this.printLottos();
+    this.getWinningNumbers();
+  }
+
+  publishLottos(num) {
+    while (this.myTickets.length < num) {
+      this.myTickets.push(
+        Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b)
+      );
+    }
+  }
+
+  getWinningNumbers() {
+    Console.readLine('당첨 번호를 입력해 주세요.', (numbers) => {
+      new Lotto(makeSixNumbersArr(numbers), this.myTickets);
+    });
+  }
+
+  printLottos() {
+    this.myTickets.forEach((ticket) => {
+      Console.print(ticket);
+    });
   }
 }
 
-const getHowManyLottos = (money) => {
-  const numberOfTicket = money / 1000;
-
-  Console.print(`${numberOfTicket}개를 구매했습니다.`);
-  printLottos(publishLottos(numberOfTicket));
-};
-
-const publishLottos = (num) => {
-  const lottos = [];
-
-  while (lottos.length < num) {
-    lottos.push(
-      Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b)
-    );
-  }
-  return lottos;
-};
-
-const printLottos = (lottos) => {
-  lottos.forEach((lotto) => {
-    Console.print(lotto);
-  });
-};
-
-// 아래는 구입금액, 당첨번호 입력받기 관련 함수들
-
-const getWinningNumbers = () => {
-  Console.readLine('당첨 번호를 입력해 주세요.', (numbers) => {
-    const lotto = new Lotto(makeSixNumbersArr(numbers));
-  });
-};
-
 const makeSixNumbersArr = (str) => {
-  return str.split(',');
-};
-
-const getPurchaseAmount = () => {
-  Console.readLine('구입금액을 입력해주세요.', (num) => {
-    if (isDivisibleBy1000(num)) {
-      getHowManyLottos(num);
-    } else {
-      errorMessage();
-    }
-  });
+  const arr = str.split(',');
+  return arr.map((value) => Number(value));
 };
 
 const isDivisibleBy1000 = (num) => {
@@ -61,7 +58,10 @@ const isDivisibleBy1000 = (num) => {
 };
 
 const errorMessage = () => {
-  throw new Error('[ERROR] 임시 에러메세지');
+  throw new Error('[ERROR] 1000원 단위로 금액을 입력해주세요.');
 };
+
+const app = new App();
+app.play();
 
 module.exports = App;
