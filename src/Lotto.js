b7af412820948b164cd70b30dc3661ef725d6de9
박준서 <oponize@naver.com>
2022-11-12 17:08:26 +0900

    refactor: Validator 유틸 클래스 this 체이닝

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,28 +10,28 @@ class Lotto {
   }
 
   validateLotto(numbers) {
-    Validator.isLength(numbers, LOTTO_NUMBER.VALID_NUMBER_LENGTH);
-    Validator.isDuplicated(numbers);
-    Validator.isEvery({
-      target: numbers,
-      callback: Validator.isNumber,
-      message: "[ERROR] 숫자가 아닌 번호가 존재합니다.",
-    });
-    Validator.isEvery({
-      target: numbers,
-      callback: (number) =>
-        Validator.isRange({
-          target: number,
-          start: LOTTO_NUMBER.MIN_NUMBER,
-          end: LOTTO_NUMBER.MAX_NUMBER,
-        }),
-      message: "[ERROR] 로또 번호가 1~45 사이가 아닙니다.",
-    });
+    Validator.isLength(numbers, LOTTO_NUMBER.VALID_NUMBER_LENGTH)
+      .isDuplicated(numbers)
+      .isEvery({
+        target: numbers,
+        callback: Validator.isNumber,
+        message: "[ERROR] 숫자가 아닌 번호가 존재합니다.",
+      })
+      .isEvery({
+        target: numbers,
+        callback: (number) =>
+          Validator.isRange({
+            target: number,
+            start: LOTTO_NUMBER.MIN_NUMBER,
+            end: LOTTO_NUMBER.MAX_NUMBER,
+          }),
+        message: "[ERROR] 로또 번호가 1~45 사이가 아닙니다.",
+      });
     return true;
   }
 
-  get lotto() {
-    return this.#numbers;
+  getSortedLotto() {
+    return this.#numbers.sort((a, b) => a - b);
   }
 }
 
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,37 +1,37 @@
 const Validator = class {
   static isLength = (target, length) => {
     if (target.length !== length) throw new Error(`[ERROR] 번호는 ${length}개여야 합니다.`);
-    return true;
+    return this;
   };
 
   static isNumber = (number) => {
     if (!number.toString().match(/\d/)) throw new Error(`[ERROR] 숫자를 입력해주세요.`);
-    return true;
+    return this;
   };
 
   static isRange = ({ target, start, end }) => {
-    if (target >= start && target <= end) return true;
+    if (target >= start && target <= end) return this;
     throw new Error(`[ERROR] ${start}~${end}사이의 값을 입력해주세요.`);
   };
 
   static isDuplicated = (origin) => {
     if (new Set(origin).size !== origin.length) throw new Error("[ERROR] 중복이 존재합니다.");
-    return true;
+    return this;
   };
 
   static isDivisible = (target, unit) => {
     if (Number(target) % unit) throw new Error(`[ERROR] ${unit} 단위로 입력해주세요.`);
-    return true;
+    return this;
   };
 
   static isGreaterOrEqual = (target, unit) => {
     if (target < unit) throw new Error("[ERROR] 1,000원 이상으로 입력해주세요.");
-    return true;
+    return this;
   };
 
   static isEvery({ target, callback, message }) {
     if (!target.every(callback)) throw new Error(message);
-    return true;
+    return this;
   }
 };
 
