a659b397dcd0de6105998f891cf610c303ee2f8d
ReturnReason <reasonz@daum.net>
2022-11-12 11:00:11 +0900

    feat: 로또 번호를 생성하는 메서드 추가

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,9 +1,19 @@
+const { LOTTO_INFO } = require('./common/constants');
 const LottoView = require('./LottoView');
+const { Random } = require('./utils/missionUtil');
 
 class LottoGame {
   constructor() {
     const lottoView = new LottoView();
     lottoView.getPurchaseAmount();
+  }
+
+  generateLottoNumbers() {
+    return Random.pickUniqueNumbersInRange(
+      LOTTO_INFO.BEGIN_NUMBER,
+      LOTTO_INFO.END_NUMBER,
+      LOTTO_INFO.COUNT
+    );
   }
 }
 
