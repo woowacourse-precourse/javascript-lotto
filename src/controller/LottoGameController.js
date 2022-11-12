d41df75ae84673a2e7413281c46cf7a271348de5
seongminn <berry9901@gmail.com>
2022-11-12 23:51:51 +0900

    fix(Validator): 클래스 상속을 통해 MoneyValidator 기능 확장
    숫자인지 검사하는 NumberValidator를 생성하여 MoneyValidator가 상속 받도록 하였음
    추후 로또 번호의 유효성 검사 클래스도 상속할 수 있기 때문에 해당 방법 채택

--- src/controller/LottoGameController.js
+++ src/controller/LottoGameController.js
@@ -1,13 +1,13 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { INPUT_MESSAGE } = require('../constant');
-const Validator = require('../utils/Validator');
+const { MoneyValidator } = require('../utils/Validator');
 
 class LottoGameController {
   constructor() {}
 
   start() {
     Console.readLine(INPUT_MESSAGE.MONEY, (input) => {
-      Validator.isValidMoney(input);
+      MoneyValidator.validate(input);
     });
   }
 }
--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,8 +1,23 @@
-class Validator {
+class NumberValidator {
+  static isValidNumber(input) {
+    if (isNaN(input)) throw new Error('[ERROR] 숫자를 입력해 주세요.');
+  }
+}
+
+class MoneyValidator extends NumberValidator {
+  constructor(args) {
+    super(...args);
+  }
+
+  static validate(input) {
+    this.isValidMoney(input);
+    this.isValidNumber(input);
+  }
+
   static isValidMoney(input) {
     if (input % 1000)
       throw new Error('[ERROR] 1,000원 단위의 금액을 입력해 주세요.');
   }
 }
 
-module.exports = Validator;
+module.exports = { NumberValidator, MoneyValidator };
