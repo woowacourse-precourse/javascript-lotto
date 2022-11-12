3810ed2786114e25194dcb95c9cd9699cc934f74
jinlee <vito9270@naver.com>
2022-11-12 15:44:37 +0900

    docs: 기능 목록 업데이트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("../src/Lotto.js");
+
 class App {
-  play() {}
+  play() {
+    inputPurchaseAmount()
+
+    const lotto = new Lotto();
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
+
+function inputPurchaseAmount() {
+  console.log(1)
+  MissionUtils.Console.readLine(
+    "구입금액을 입력해 주세요.\n",
+    (purchaseAmount) => {
+      is1000Multiple(purchaseAmount);
+    }
+  );
+}
+
+function is1000Multiple(purchaseAmount) {
+  console.log(2)
+  if (purchaseAmount % 1000 !== 0) {
+    throw new Error("[ERROR] 로또 구입 금액은 1000의 배수여야 합니다.");
+  }
+  inputWinningNumbers();
+}
+
+function inputWinningNumbers() {
+  console.log(3)
+  MissionUtils.Console.readLine(
+    "당첨 번호를 입력해 주세요.\n",
+    (winningNumbers) => {
+      return winningNumbers;
+    }
+  );
+}
--- src/Lotto.js
+++ src/Lotto.js
@@ -8,18 +8,6 @@ class Lotto {
     this.#numbers = numbers;
   }
 
-  inputPurchaseAmount() {
-    Console.readLine("구입금액을 입력해 주세요.", (purchaseAmount) => {
-      is1000Multiple(purchaseAmount);
-    });
-  }
-
-  is1000Multiple(purchaseAmount) {
-    if(purchaseAmount % 1000 !== 0){
-      throw new Error("[ERROR] 로또 구입 금액은 1000의 배수여야 합니다.")
-    }
-  }
-
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
