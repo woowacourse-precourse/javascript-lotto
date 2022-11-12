15b513ae0fef3bb8deab4eb62e6804da80645633
Cobocho <rlaalsrb1111@naver.com>
2022-11-13 01:27:39 +0900

    test: `userLotto` 클래스에서의 분리에 따른 테스트 코드 변경사항 적용

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,25 +1,25 @@
-const Lotto = require("../src/Utils/Lotto");
+const { Lotto } = require("../src/Utils/Lotto");
 const { MESSAGES } = require("../src/constants");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow(MESSAGES.ERROR.isDiffrentLength);
+    }).toThrow(MESSAGES.ERROR.isDiffrentLottoLength);
   });
 
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 6]);
-    }).toThrow(MESSAGES.ERROR.isDiffrentLength);
+      new Lotto([1, 2, 3, 4, 6, 6]);
+    }).toThrow(MESSAGES.ERROR.isDuplicated);
   });
 
   test("로또 번호에 1~45가 아닌 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 0, 6]);
     }).toThrow(MESSAGES.ERROR.isNotRange);
-  });
     expect(() => {
       new Lotto([1, 2, 3, 4, 0, 45]);
     }).toThrow(MESSAGES.ERROR.isNotRange);
+  });
 });
