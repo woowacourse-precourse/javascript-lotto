070d18003eef14d558b0bfa27f49743a6a287844
GC-Park <kuyt3122@naver.com>
2022-11-12 10:02:38 +0900

    feat:로또 구입 금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-class App {
-  play() {}
-}
+const MissionUtils = require("@woowacourse/mission-utils");
 
+class App {
+  money=0;
+
+  play() {
+    this.insertMoney();
+  }
+
+  insertMoney(){
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요. ', (answer) => {
+      this.money = answer/1000;
+  })
+  }
+
+}
+const app = new App();
+app.play();
 module.exports = App;
