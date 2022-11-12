0f4900f86938e74e8e0eb65905edaa4f88c57fa2
evencoding <evencoding@gmail.com>
2022-11-12 17:00:39 +0900

    refactor(stateOfWinning): 연속되는 if문을 else if문으로 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -19,10 +19,10 @@ class Lotto {
       const matchingCount = GameTools.getMatchingNumCount(lotto, this.#numbers);
       const matchesBonusNumber = lotto.includes(Number(bonusNumber));
       if (matchingCount === 6) state.first += 1;
-      if (matchingCount === 5 && matchesBonusNumber) state.second += 1;
-      if (matchingCount === 5 && !matchesBonusNumber) state.third += 1;
-      if (matchingCount === 4) state.fourth += 1;
-      if (matchingCount === 3) state.fifth += 1;
+      else if (matchingCount === 5 && matchesBonusNumber) state.second += 1;
+      else if (matchingCount === 5 && !matchesBonusNumber) state.third += 1;
+      else if (matchingCount === 4) state.fourth += 1;
+      else if (matchingCount === 3) state.fifth += 1;
 
       return state;
     }, stateOfPrize);
