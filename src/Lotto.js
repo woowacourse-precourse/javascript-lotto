0bcbf62c8b0a639a5f83b690bb5d3fdf73433723
wang-yurin <king_1994@naver.com>
2022-11-12 16:15:37 +0900

    feat: 입력 받은 로또 번호 1~45 이내 숫자인지 판별하는 함수 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,22 +4,23 @@ class Lotto {
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
-    this.overlapValidate(numbers);
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-  }
 
-  // TODO: 추가 기능 구현
-  // 입력 받은 수 중복된 숫자 제거
-  overlapValidate(numbers) {
     const setNum = new Set(numbers);
     if (setNum.length !== 6) {
       throw new Error("[ERROR] 중복된 숫자가 있습니다.");
     }
+
+    numbers.map((number) => {
+      if (!(parseInt(number) >= 1 && parseInt(number) >= 45)) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    });
   }
 }
 
