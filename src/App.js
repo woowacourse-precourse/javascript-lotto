551ee6fffeb85932efc688b3ede337bc04b3d149
wonkyung <mwk0725@gmail.com>
2022-11-12 23:37:46 +0900

    feat: 발행한 로또 수량 구하기

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App {
   constructor() {
     this.LOTTO_TICKET_PRICE = 1000;
     this.money = 0;
+    this.lottoCount = 0;
   }
   play() {
     this.injectMoney();
@@ -15,6 +16,7 @@ class App {
       if (money % 1000 !== 0) {
         throw new Error('[ERROR] 1000원 단위로 입력해주세요.');
       }
+      this.lottoCount += Number(money) / this.LOTTO_TICKET_PRICE;
       this.money += Number(money);
     });
   }
