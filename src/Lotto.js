57f006c1c0f8f3f36b1907cdbfc973a0786f5cd6
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-12 13:57:50 +0900

    feat(Lotto): 개수 일치 메소드
    1. 3개 일치
    2. 4개 일치
    3. 5개 일치
    4. 5개 일치 + 보너스 볼 일치
    5. 6개 일치

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,7 +4,7 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    Lotto.validate(numbers);
+    Application.validateArray(numbers);
     this.#numbers = numbers;
   }
 
@@ -32,6 +32,60 @@ class Lotto {
     const lottoCount = Application.purchaseCount(purchaseAmount, UNIT_AMOUNT);
 
     return Array.from({ length: lottoCount }, this.createLotto);
+  }
+
+  static isThreeMatche(count, array) {
+    const DATUM_POINT = 3;
+    const newArray = [...array];
+
+    if (Application.isMatcheCount(count, DATUM_POINT)) {
+      newArray[0] += 1;
+    }
+
+    return newArray;
+  }
+
+  static isFourMatche(count, array) {
+    const DATUM_POINT = 4;
+    const newArray = [...array];
+
+    if (Application.isMatcheCount(count, DATUM_POINT)) {
+      newArray[1] += 1;
+    }
+
+    return newArray;
+  }
+
+  static isFiveMatche(count, array, target = [], bonus = 0) {
+    const DATUM_POINT = 5;
+    const newArray = [...array];
+
+    if (Application.isMatcheCount(count, DATUM_POINT) && target.includes(bonus)) {
+      return Lotto.isBonusMatche(newArray);
+    }
+
+    newArray[2] += 1;
+
+    return newArray;
+  }
+
+  static isBonusMatche(array) {
+    const newArray = [...array];
+
+    newArray[3] += 1;
+
+    return newArray;
+  }
+
+  static isSixMatche(count, array) {
+    const DATUM_POINT = 6;
+    const newArray = [...array];
+
+    if (Application.isMatcheCount(count, DATUM_POINT)) {
+      newArray[4] += 1;
+    }
+
+    return newArray;
   }
 }
 
