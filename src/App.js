260ffe7d173739b4e635a9ff49833a90fb2d4909
confidential-nt <youyoy689@gmail.com>
2022-11-13 06:54:57 +0900

    feat(요구사항): 요구사항5. 사용자가 당첨번호 및 보너스 번호 입력할 수 있도록 하기
    사용자가 당첨번호와 보너스 번호를 입력할 수 있게 하는 기능을 구현하였다.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ const { Console, Random } = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 
 class App {
-  // userLottos;
+  LENGTH_OF_LOTTO_NUMBER = 6;
 
   play() {
-    this.makePayment();
-    console.log(this.userLottos);
+    this.readLine("구입금액을 입력해주세요.", this.runLotto.bind(this));
   }
 
   readLine(message, callback) {
@@ -17,21 +16,42 @@ class App {
     Console.print(message);
   }
 
-  makePayment() {
-    this.readLine("구입금액을 입력해주세요.", (value) => {
-      const money = value;
+  runLotto(value) {
+    const numberOfLotto = this.makePayment(value);
+    const lottos = this.issueLotto(numberOfLotto);
+    this.getWinner(lottos);
+  }
 
-      if (money % 1000 !== 0) {
-        this.print("1000원 단위로 입력해주세요.");
-        this.makePayment();
-        return;
-      }
+  getWinner(lottos) {
+    this.readLine("당첨번호를 입력해주세요.", this.getWinnerNumber.bind(this));
+  }
 
-      const numberOfLotto = money / 1000;
+  getWinnerNumber(value) {
+    const winnerNumber = value.split(",");
+    this.readLine(
+      "보너스 번호를 입력해주세요.",
+      this.getBonusNumber.bind(this)
+    );
+  }
 
-      this.print(`${numberOfLotto}개를 구매했습니다.`);
-      this.issueLotto(numberOfLotto);
-    });
+  getBonusNumber(value) {
+    const bonusNumber = value;
+  }
+
+  makePayment(value) {
+    const money = value;
+
+    if (money % 1000 !== 0) {
+      this.print("1000원 단위로 입력해주세요.");
+      this.makePayment();
+      return;
+    }
+
+    const numberOfLotto = money / 1000;
+
+    this.print(`${numberOfLotto}개를 구매했습니다.`);
+
+    return numberOfLotto;
   }
 
   issueLotto(numberOfLotto) {
@@ -42,18 +62,22 @@ class App {
       lottos.push(new Lotto(lottoNumbers));
     }
 
-    this.printLotto(lottos);
+    this.printLottos(lottos);
+
+    return lottos;
   }
 
   generateLottoNumber() {
-    const count = 6;
-
-    const numbers = Random.pickUniqueNumbersInRange(1, 45, count);
+    const numbers = Random.pickUniqueNumbersInRange(
+      1,
+      45,
+      this.LENGTH_OF_LOTTO_NUMBER
+    );
 
     return numbers.sort();
   }
 
-  printLotto(lottos) {
+  printLottos(lottos) {
     for (let i = 0; i < lottos.length; i++) {
       lottos[i].printNumbers();
     }
