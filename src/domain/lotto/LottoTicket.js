b7b1be4a2de0c4b4e75755396f2b2d728df11c81
hwanfront <kim015jh@gmail.com>
2022-11-12 20:59:51 +0900

    feat(LottoTicket): 인수에 대한 예외처리 추가
    추가 변경 사항:
    - getter 추가

--- src/domain/lotto/LottoTicket.js
+++ src/domain/lotto/LottoTicket.js
@@ -1,13 +1,30 @@
+const InstanceException = require('../../exception/InstanceException');
+const Lotto = require('./Lotto');
+
 class LottoTicket {
   #lottos;
 
   constructor(lottos) {
+    LottoTicket.validate(lottos);
     this.#lottos = lottos;
   }
 
   static of(lottos) {
     return new LottoTicket(lottos);
   }
-}
 
+  static validate(lottos) {
+    lottos.forEach(LottoTicket.validateIsTicket);
+  }
+
+  static validateIsTicket(lotto) {
+    if (!(lotto instanceof Lotto)) {
+      throw new InstanceException('Lotto');
+    }
+  }
+
+  getLottos() {
+    return this.#lottos;
+  }
+}
 module.exports = LottoTicket;
