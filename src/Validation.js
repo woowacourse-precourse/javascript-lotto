0826c479e3025b9b469ab361840b72c65404f891
imhynni <omelas329@gmail.com>
2022-11-12 14:21:21 +0900

    feat: 1000으로 나누어 떨어지지 않는 경우 예외 발생

--- src/Validation.js
+++ src/Validation.js
@@ -1,11 +1,16 @@
-const { NAN_ERROR } = require("./Constants");
+const { NAN_ERROR, REMAINDER_ERROR } = require("./Constants");
 
 const checkNaN = (amount) => {
   if (isNaN(Number(amount))) return true;
 };
 
+const checkRemainder = (amount) => {
+  if (parseInt(amount) % 1000 !== 0) return true;
+};
+
 const checkPurchaseAmount = (amount) => {
   if (checkNaN(amount)) throw NAN_ERROR;
+  if (checkRemainder(amount)) throw REMAINDER_ERROR;
 };
 
 module.exports = {
