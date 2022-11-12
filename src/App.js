592d5c5a87e0f95f6e1e45964a88c6d30403192b
userHWSEO <gmldnjseocnd@gmail.com>
2022-11-12 13:25:22 +0900

    feat: 당첨번호와 보너스 번호 입력 기능 구현
    - 당첨번호 6자리를 입력 받는 기능 구현
    - 보너스 번호를 입력 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  play() {}
-  userLottoNumber() {
+  makeUserLottoNumber() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요. \n", (money) => {
       const lottoSheets = money / 1000;
       const userLottos = [];
-      MissionUtils.Console.print(`${lottoSheets}개를 구매했습니다. \n`);
+      MissionUtils.Console.print(`\n${lottoSheets}개를 구매했습니다.`);
       for (let i = 0; i < lottoSheets; i++) {
         const userLotto = MissionUtils.Random.pickUniqueNumbersInRange(
           1,
@@ -18,11 +17,29 @@ class App {
         MissionUtils.Console.print(userLotto);
         userLottos.push(userLotto);
       }
+      this.winningAndBonusNumber(userLottos);
     });
+  }
+  winningAndBonusNumber(userLottos) {
+    MissionUtils.Console.readLine(
+      "\n당첨 번호를 입력해 주세요. \n",
+      (winningNum) => {
+        const winningLotto = winningNum.split(",");
+        MissionUtils.Console.readLine(
+          "\n보너스 번호를 입력해 주세요. \n",
+          (bonusNum) => {
+            this.statistics(winningLotto, bonusNum);
+          }
+        );
+      }
+    );
+  }
+  play() {
+    this.makeUserLottoNumber();
   }
 }
 
 const app = new App();
-app.userLottoNumber();
+app.play();
 
 module.exports = App;
