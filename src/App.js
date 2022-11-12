a5df1d4b91eaec87782a3421650106af4ac5e4f3
js43o <js43o@naver.com>
2022-11-12 17:55:24 +0900

    refactor: move validators from Lotto.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const Lotto = require('./Lotto');
-
 const console = MissionUtils.Console;
+
+const LOTTO_NUMBER_LENGTH = 6;
+const LOTTO_RANGE_BEGIN = 1;
+const LOTTO_RANGE_END = 45;
+
+const ERROR_HEADER = '[ERROR]';
+const ERROR_WRONG_LENGTH = `${ERROR_HEADER} 로또 번호는 6개여야 합니다.`;
+const ERROR_DUPLICATED = `${ERROR_HEADER} 로또 번호는 중복되지 않아야 합니다.`;
+const ERROR_WRONG_RANGE = `${ERROR_HEADER} 로또 번호는 1부터 45까지의 정수여야 합니다.'`;
 
 class App {
   #winningNumbers;
   #bonusNumber;
 
+  isDistinct(numbers) {
+    let set = new Set(numbers);
+    return numbers.length === set.size;
+  }
+
+  isInRange(number) {
+    return (
+      /^[0-9]+$/.test(number) &&
+      number >= LOTTO_RANGE_BEGIN &&
+      number <= LOTTO_RANGE_END
+    );
+  }
+
+  validateNumbers(numbers) {
+    if (numbers.length !== LOTTO_NUMBER_LENGTH) {
+      throw new Error(ERROR_WRONG_LENGTH);
+    }
+    if (!this.isDistinct(numbers)) {
+      throw new Error(ERROR_DUPLICATED);
+    }
+    numbers.forEach((number) => {
+      if (!this.isInRange(Number(number))) {
+        throw new Error(ERROR_WRONG_RANGE);
+      }
+    });
+  }
+
   setWinningNumbers(numbers) {
-    Lotto.validateNumbers(numbers);
+    this.validateNumbers(numbers);
     this.#winningNumbers = numbers;
   }
 
   setBonusNumber(number) {
+    this.isInRange(number);
     this.#bonusNumber = number;
   }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,46 +1,8 @@
-const LOTTO_RANGE_BEGIN = 1;
-const LOTTO_RANGE_END = 45;
-
-const ERROR_HEADER = '[ERROR]';
-const ERROR_WRONG_LENGTH = `${ERROR_HEADER} 로또 번호는 6개여야 합니다.`;
-const ERROR_DUPLICATED = `${ERROR_HEADER} 로또 번호는 중복되지 않아야 합니다.`;
-const ERROR_WRONG_RANGE = `${ERROR_HEADER} 로또 번호는 1부터 45까지의 정수여야 합니다.'`;
-
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    if (numbers) {
-      this.validateNumbers(numbers);
-      this.#numbers = numbers;
-    }
-  }
-
-  isDistinct(numbers) {
-    let set = new Set(numbers);
-    return numbers.length === set.size;
-  }
-
-  isInRange(number) {
-    return (
-      /^[0-9]+$/.test(number) &&
-      number >= LOTTO_RANGE_BEGIN &&
-      number <= LOTTO_RANGE_END
-    );
-  }
-
-  validateNumbers(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error(ERROR_WRONG_LENGTH);
-    }
-    if (!this.isDistinct(numbers)) {
-      throw new Error(ERROR_DUPLICATED);
-    }
-    numbers.forEach((number) => {
-      if (!this.isInRange(Number(number))) {
-        throw new Error(ERROR_WRONG_RANGE);
-      }
-    });
+    this.#numbers = numbers;
   }
 
   getNumberOfMatch(winningNumbers) {
