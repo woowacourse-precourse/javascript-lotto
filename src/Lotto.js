3b6cbf5c843b93d8b68e0c85cf5f7b89dd49ea6f
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-13 08:19:36 +0900

    refactor : 로또 당첨번호 관리를 위한 Lotto 클래스 생성 및 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,30 @@
+const { print } = require('./lib/Utils');
+const WinNumbersValidation = require('./Validation/WinNumbersValidation');
+
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
+  constructor(answer) {
+    const stringAnswer = typeof answer === 'string' ? answer : JSON.stringify(answer);
+    Lotto.winNumberValidate(stringAnswer);
+    this.saveWinNumbers(stringAnswer);
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  static winNumberValidate(answer) {
+    const winNumberValidate = new WinNumbersValidation(answer);
+    return winNumberValidate.validate();
   }
 
-  // TODO: 추가 기능 구현
+  saveWinNumbers(answer) {
+    const winNumberArray = answer.split(',');
+    this.#numbers = winNumberArray.map((number) => Number(number));
+
+    return print('\n');
+  }
+
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 module.exports = Lotto;
