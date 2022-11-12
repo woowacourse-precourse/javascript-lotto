8e1120cdc49174e6a6548e2a4b282c05d04e4fc4
Ryong-E <think8867@gmail.com>
2022-11-12 16:19:58 +0900

    refactor(check-lotto): 로또 번호 비교 로직 분리, 상수 값으로 변경
    - reduce 부분 분리
    - 블록 앞뒤로 공백 추가
    - winningNumber를 변수에 저장해서 reduce하지않고 바로 호출하여 reduce하도록 변경

--- src/check-lotto.js
+++ src/check-lotto.js
@@ -1,3 +1,5 @@
+const { RANGKING_COUNT } = require('./utils/constant');
+
 class CheckLotto {
   #controller;
   #result;
@@ -9,34 +11,44 @@ class CheckLotto {
   getWinningresult() {
     const sortedResult = Object.entries(this.#result).sort(([a], [b]) => (a < b ? -1 : 1));
     const winningResult = [];
+
     sortedResult.forEach((result) => {
       winningResult.push(result[1]);
     });
+
     return winningResult;
   }
 
   checkLotto() {
-    this.#result = this.#controller.lottos
-      .map((lotto) => lotto.getNumber().map((number) => String(number)))
-      .reduce(
-        (total, lotto) => {
-          let count = 0;
-          const winningNumber = this.#controller.winningNumber.getWinningNumber();
-          winningNumber.forEach((number) => {
-            if (lotto.includes(number)) {
-              count += 1;
-            }
-          });
-          if (count === 5 && this.#isSecond(lotto)) count += 0.5;
-          if (count >= 3) total[count] += 1;
-          return total;
-        },
-        { 3: 0, 4: 0, 5: 0, 5.5: 0, 6: 0 },
-      );
+    const calledlottoNumbers = this.#controller.lottos.map((lotto) =>
+      lotto.getNumber().map((number) => String(number)),
+    );
+
+    this.#result = this.#countingWinLottos(calledlottoNumbers);
+  }
+
+  #countingWinLottos(lottos) {
+    return lottos.reduce(
+      (total, lotto) => {
+        let count = 0;
+
+        this.#controller.winningNumber.getWinningNumber().forEach((number) => {
+          if (lotto.includes(number)) count += 1;
+        });
+
+        if (count === RANGKING_COUNT.THIRD_RANKING_COUNT && this.#isSecond(lotto)) count += 0.5;
+
+        if (count >= RANGKING_COUNT.FIFTH_RANKING_COUNT) total[count] += 1;
+
+        return total;
+      },
+      { 3: 0, 4: 0, 5: 0, 5.5: 0, 6: 0 },
+    );
   }
 
   #isSecond(lotto) {
     const bonusNumber = this.#controller.winningNumber.getBonusNumber();
+
     if (lotto.includes(bonusNumber)) {
       return true;
     }
