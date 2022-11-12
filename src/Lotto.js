77c09857f85c9ab5f8ece0c6bdfaf80c8a65bb3e
“somm12” <sominan8744@gmail.com>
2022-11-12 21:12:07 +0900

    fix: 로또 당첨 번호 입력 기능  예외처리 문구 순서 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,9 +11,6 @@ class Lotto {
   validate(numbers) {
     const LENGTH = new Set(numbers).size;
 
-    if (LENGTH !== 6) {
-      throw new Error("[ERROR] 로또 번호는 서로 다른 6개여야 합니다!");
-    }
     for (let i = 0; i < LENGTH; i++) {
       if (isNaN(numbers[i])) {
         throw new Error("[ERROR] 숫자가 아닙니다!");
@@ -21,6 +18,9 @@ class Lotto {
       if (numbers[i] <= 0 || numbers[i] > 45) {
         throw new Error("[ERROR] 1 ~ 45 숫자가 아닙니다!");
       }
+    }
+    if (LENGTH !== 6) {
+      throw new Error("[ERROR] 로또 번호는 서로 다른 6개여야 합니다!");
     }
   }
 
