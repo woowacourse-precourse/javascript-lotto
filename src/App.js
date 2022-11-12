3b1eadea954c59c9bfaa56d5afec8ee1a805e536
2pandi <coder.2pandi@gmail.com>
2022-11-12 14:48:08 +0900

    feat: add function to validate inputAmount

--- src/App.js
+++ src/App.js
@@ -9,8 +9,22 @@ class App {
   askForAmount() {
     Console.readLine("구입금액을 입력해 주세요.", (input) => {
       this.inputAmount = Number(input);
+      this.validateInputAmount();
     });
   }
+
+  validateInputAmount() {
+    const inputAmount = this.inputAmount;
+    const error = new Error("[ERROR] 구입금액은 천 원 단위여야 합니다.");
+    if (
+      inputAmount < 1000 ||
+      inputAmount / 1000 !== Math.floor(inputAmount / 1000)
+    )
+      throw error;
+    this.generateLotto();
+  }
+
+  generateLotto() {}
 }
 
 const app = new App();
