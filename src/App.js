7bfffcedad7782395c584fd77c8f8e617e6f2c5b
GuHyeonWoo <ghw9174@gmail.com>
2022-11-12 20:17:45 +0900

    feat (App.js): 로또 번호 입력 및 정수 개수 예외처리
    README.md: 2.3. 당첨 번호를 입력 목록 상태를 진행 중으로 변경 및 정수가 6개가 아닐 경우 예외처리 완료 처리
    App.js:
    - enterAmount(): play()에 있던 enterLottoNumbers() 함수 참조를 이동 -> Console.readme 메서드가 실행 안되는 오류 때문
    - enterLottoNumbers(): 당첨 번호를 입력받아 Lotto 클래스를 생성하는 로직 구현
    - splitCharactre(): 입력받은 문자열을 정수로 된 리스트로 변환하는 로직 구현
    LottoTest.js:
    - 로또 번호의 개수가 6개가 넘어가면 예외가 발생한다. 테스트 처리
    
    docs/README.md #2.3. 당첨 번호를 입력

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Console = MissionUtils.Console;
 
 const Buyer = require("../src/Buyer");
+const Lotto = require("../src/Lotto");
 
 class App {
   #Buyer;
+  #Lotto;
 
   play() {
     this.enterAmount();
@@ -14,12 +16,25 @@ class App {
     Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
       this.#Buyer = new Buyer(money);
       this.showPurchaseLottos();
+
+      this.enterLottoNumbers();
     });
   }
 
   showPurchaseLottos() {
     this.#Buyer.countLotto();
     this.#Buyer.createLottos();
+  }
+
+  enterLottoNumbers() {
+    Console.readLine('\n당첨 번호를 입력해 주세요.\n', (numbers) => {
+      const tempNumbers = this.splitCharacter(numbers);
+      this.#Lotto = new Lotto(tempNumbers);
+    });
+  }
+
+  splitCharacter(numbers) {
+    return numbers.split(',').map(Number);
   }
 }
 
