7b52bee9091c66c735f256f3eb55393ec04cb1f8
pangkyu <baesense@naver.com>
2022-11-12 14:39:25 +0900

    Feat : constant 상수 설정

---
+++ src/constant/constant.js
@@ -0,0 +1,9 @@
+const MESSAGE = {
+  BUYING_AMOUNT: "구입금액을 입력해 주세요.",
+  BUYING_RESULT: "개를 구매했습니다.",
+  INPUT_PRIZE: "당첨 번호를 입력해 주세요.",
+  INPUT_BONUS: "보너스 번호를 입력해 주세요.",
+  PRIZE_RESULT: "당첨 통계",
+};
+
+module.exports = { MESSAGE };
