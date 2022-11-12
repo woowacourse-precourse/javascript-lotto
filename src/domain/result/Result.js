263105e74b4fa2e7b9d5537c91106a05f403263d
hwanfront <kim015jh@gmail.com>
2022-11-12 23:42:25 +0900

    feat(Result): 당첨 결과 개수를 구하는 기능 추가
    추가 사항:
    - Map 초기화 하는 #initializePrizes 메서드 추가

--- src/domain/result/Result.js
+++ src/domain/result/Result.js
@@ -1,16 +1,57 @@
+const LottoTicket = require('../lotto/LottoTicket');
+const WinningLotto = require('../lotto/WinningLotto');
 const Prize = require('./Prize');
+const InstanceException = require('../../exception/InstanceException');
 
 class Result {
   #prizes = new Map();
 
   constructor(lottoTicket, winningLotto) {
-    this.prize = new Map();
+    Result.validate();
+    this.#prizes = new Map();
     this.lottoTicket = lottoTicket;
     this.winningLotto = winningLotto;
+    this.#initializePrizes();
+    this.#countPrize();
+  }
+
+  static from(lottoTicket, winningLotto) {
+    return new Result(lottoTicket, winningLotto);
+  }
+
+  static validate(lottoTicket, winningLotto) {
+    if (!(lottoTicket instanceof LottoTicket)) {
+      throw new InstanceException('LottoTicket');
+    }
+
+    if (!(winningLotto instanceof WinningLotto)) {
+      throw new InstanceException('WinningLotto');
+    }
+  }
+
+  getPrizes() {
+    return this.#prizes;
   }
 
   getProfit() {
 
+  }
+
+  #initializePrizes() {
+    const { values } = Object;
+    values(Prize).forEach((prize) => {
+      this.#prizes.set(prize, 0);
+    });
+  }
+
+  #countPrize() {
+    this.lottoTicket.getLottos().forEach((lotto) => {
+      const matchCount = this.winningLotto.countSameNumber(lotto);
+      const isBonus = lotto.includes(this.winningLotto.getBonusNumber());
+      const prize = Prize.getPrize(matchCount, isBonus);
+      const newValue = this.#prizes.get(prize) + 1;
+      this.#prizes.set(prize, newValue);
+    });
   }
 }
 
