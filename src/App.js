7ac503a5a6f9689d08fd7154b13ef6389aa0c549
pangkyu <baesense@naver.com>
2022-11-12 16:45:01 +0900

    Feat : 1000원 단위 예외처리
    1000으로 나누었을때 나머지가 남아있다면 1000원단위 입력이 아니기 때문에 throw 처리를 한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const Lotto = require("./Lotto");
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.lotto = new Lotto();
+  }
+  play() {
+    this.lotto.start();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,45 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+const BuyLotto = require("./utils/BuyLotto");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    // this.validate(numbers);
     this.#numbers = numbers;
+    this.buyLotto = new BuyLotto();
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  // validate(numbers) {
+  //   if (numbers.length !== 6) {
+  //     throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  //   }
+  // }
+
+  start() {
+    const buyLotto = this.buyLotto.howMuch();
+    this.numbers = buyLotto;
+
+    // const buyLotto = this.BuyLotto.making();
   }
 
-  // TODO: 추가 기능 구현
+  // userInputNumber() {
+  //   Console.readLine("숫자를 입력해주세요 : ", (input) => {
+  //     this.numbers = Array.from(input);
+  //     for (let i = 0; i < this.numbers.length; i++) {
+  //       this.numbers[i] = parseInt(this.numbers[i]);
+  //     }
+  //   });
+  //   return this.#numbers;
+  // }
+
+  // making() {
+  //   const makeNumbers = [];
+  //   const number = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  //   makeNumbers.push(number);
+  //   console.log(makeNumbers);
+  //   this.number = number;
+  // }
 }
 
 module.exports = Lotto;
