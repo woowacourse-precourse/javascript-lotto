e1b355ae96d2ba5ae9980fb72dcc07b1ef65fb56
yeopto <gunyeop530@gmail.com>
2022-11-12 19:36:13 +0900

    feat: 당첨 번호 입력 시, 수가 중복되는 경우 예외 처리한다

--- src/WinNums.js
+++ src/WinNums.js
@@ -1,6 +1,7 @@
 class WinNum {
   constructor(winNums) {
     this.isWinNumInRange(winNums);
+    this.isWinNumOverlap(winNums);
   }
 
   isWinNumInRange(winNums) {
@@ -9,7 +10,15 @@ class WinNum {
       if (winNum < 1 || winNum > 45) {
         throw new Error('[ERROR] 1에서 45사이의 수를 입력해주세요.');
       } 
-    })
+    });
+  }
+
+  isWinNumOverlap(winNums) {
+    const winNumsArr = winNums.split(',');
+    const set = new Set(winNumsArr);
+    if (set.size < winNumsArr.length) {
+      throw new Error('[ERROR] 당첨 번호에 중복되는 숫자가 있습니다.');
+    }
   }
 }
 
