9408e444c4271586306387ba86786175054063ca
ReturnReason <reasonz@daum.net>
2022-11-13 08:03:31 +0900

    feat: 로또 당첨 금액 관련 상수 추가

--- src/common/constants.js
+++ src/common/constants.js
@@ -13,7 +13,16 @@ const LOTTO_MATCH = {
   SIX: 6,
 };
 
+const PRIZE_MONEY = {
+  three: 5000,
+  four: 50000,
+  five: 1500000,
+  bonus: 30000000,
+  six: 2000000000,
+};
+
 module.exports = {
   LOTTO_INFO,
   LOTTO_MATCH,
+  PRIZE_MONEY,
 };
