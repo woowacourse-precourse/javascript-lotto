91f99a23df59b160e95fc6b01a6907cbaba2baed
GC-Park <kuyt3122@naver.com>
2022-11-12 14:24:05 +0900

    feat:각각의 당첨 개수 배열에 넣기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,79 +1,99 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils')
 
 class App {
-  lottoCount=0;
-  userLottoNumbers=[];
-  winningNumbers=[];
-  bonusNumber;
+    lottoCount = 0
+    userLottoNumbers = []
+    winningNumbers = []
+    bonusNumber
+    rank = [0, 0, 0, 0, 0]
 
-  constructor(){
+    constructor() {}
 
-  }
-
-  play() {
-    this.insertMoney();
-  }
-
-  insertMoney(){
-    MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (answer) => {
-      this.lottoCount = this.lottoPurchaseCount(answer);
-      this.lottoCountPrint(this.lottoCount);
-      this.makeLottoNumber();
-    })
-  }
-
-  lottoPurchaseCount(money){
-    return money/1000;
-  }
-
-  lottoCountPrint(number){
-    MissionUtils.Console.print(number+"개를 구매했습니다.");
-  }
-
-  makeLottoNumber(){
-    for(let i=0; i<this.lottoCount; i++){
-      const numbers = this.lottoNumberSort(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6));
-      this.lottoNumberPrint(numbers);
-      this.userLottoNumbers.push(numbers);
-    }
-    this.winningNumberInput();
-  }
-
-  lottoNumberPrint(numbers){
-    MissionUtils.Console.print("["+numbers+"]");
-  }
-
-  lottoNumberSort(numbers){
-    function compareNumbers(a, b) {
-      return a - b;
+    play() {
+        this.insertMoney()
     }
 
-    return numbers.sort(compareNumbers)
-  }
-
-  winningNumberInput(){
-    MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.', (answer) => {
-      this.winningNumbers=answer.split(',');
-      this.BonusNumberInput();
-    })
-  }
-
-  BonusNumberInput(){
-    MissionUtils.Console.readLine('보너스 번호를 입력해 주세요.', (answer) => {
-      this.bonusNumber=Number(answer);
-      this.winningCount()
-    })
-  }
-
-  winningCount(){
-    for(let i=0; i<this.userLottoNumbers.length; i++){
-      this.userLottoNumbers[i].filter(lotto => this.winningNumbers.includes(String(lotto))).length;
+    insertMoney() {
+        MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (answer) => {
+            this.lottoCount = this.lottoPurchaseCount(answer)
+            this.lottoCountPrint(this.lottoCount)
+            this.makeLottoNumber()
+        })
     }
-  }
 
-  
+    lottoPurchaseCount(money) {
+        return money / 1000
+    }
 
+    lottoCountPrint(number) {
+        MissionUtils.Console.print(number + '개를 구매했습니다.')
+    }
+
+    makeLottoNumber() {
+        for (let i = 0; i < this.lottoCount; i++) {
+            const numbers = this.lottoNumberSort(
+                MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
+            )
+            this.lottoNumberPrint(numbers)
+            this.userLottoNumbers.push(numbers)
+        }
+        this.winningNumberInput()
+    }
+
+    lottoNumberPrint(numbers) {
+        MissionUtils.Console.print('[' + numbers + ']')
+    }
+
+    lottoNumberSort(numbers) {
+        function compareNumbers(a, b) {
+            return a - b
+        }
+
+        return numbers.sort(compareNumbers)
+    }
+
+    winningNumberInput() {
+        MissionUtils.Console.readLine(
+            '당첨 번호를 입력해 주세요.',
+            (answer) => {
+                this.winningNumbers = answer.split(',')
+                this.BonusNumberInput()
+            }
+        )
+    }
+
+    BonusNumberInput() {
+        MissionUtils.Console.readLine(
+            '보너스 번호를 입력해 주세요.',
+            (answer) => {
+                this.bonusNumber = Number(answer)
+                this.winningCount()
+            }
+        )
+    }
+
+    winningCount(order) {
+        this.userLottoNumbers[order].filter((lotto) =>
+            this.winningNumbers.includes(String(lotto))
+        ).length
+    }
+
+    putWinNumToArray() {
+        for (let i = 0; i < this.userLottoNumbers.length; i++) {
+            if (
+                this.winningCount(i) == 5 &&
+                this.winningNumbers.includes(this.bonusNumber)
+            ) {
+                rank[4] += 1
+            }
+            if (this.winningCount(i) >= 3)
+                this.rank[this.winningCount(i) - 6] += 1
+        }
+    }
+
+    
 }
-const app = new App();
-app.play();
-module.exports = App;
+
+const app = new App()
+app.play()
+module.exports = App
