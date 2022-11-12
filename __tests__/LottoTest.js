a131e666e99e2dce57e63646f602bea008441a4a
daal2quf <daal2quf@gmail.com>
2022-11-12 22:16:39 +0900

    test(lotto): makeBonusNumber() 기능 테스트 및 afterEach() 함수 추가
    add:
    test1 : makeBonusNumber()로 만들어진 보너스 번호가 1~45 범위의 숫자인지 확인
    afterEach() 함수 내부에 Console.close() 실행함으로써 각각의 테스트 종료후에 콘솔종료

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,9 @@
+const { Console } = require("@woowacourse/mission-utils");
 const Lotto = require("../src/Lotto");
+
+afterEach(() => {
+  Console.close();
+});
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -26,5 +31,16 @@ describe("Lotto 클래스의 makeSixNumbers() 기능 테스트", () => {
     const sixNumbers = lotto.makeSixNumbers();
     const noDuplicateNumber = [...new Set(sixNumbers)];
     expect(sixNumbers.length).toEqual(noDuplicateNumber.length);
+  });
+});
+
+describe("Lotto 클래스의 makeBonusNumber() 기능 테스트", () => {
+  test("보너스 숫자가 1~45 내의 숫자인지 테스트", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const numberRangeCheck = (number) => {
+      return (number >= 1) && (number <= 45);
+    };
+    const bonusNumber = lotto.makeBonusNumber();
+    expect(numberRangeCheck(bonusNumber)).toBeTruthy();
   });
 });
