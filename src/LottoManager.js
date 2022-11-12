0e036e33c64afb25f98698981cce6af69964bfac
Subin Jo <sbjo1202@naver.com>
2022-11-12 19:47:30 +0900

    feat: 당첨 번호 확인하기 구현
    1부터 45사이의 값을 걸러주는 필터 함수 filterRange()도 함께 구현

--- src/LottoManager.js
+++ src/LottoManager.js
@@ -18,7 +18,34 @@ class LottoManager {
       winningNumbers = winningNumbers.split(',');
       this.#winningNumbers = winningNumbers.map((idx) => Number(idx));
       console.log(this.#winningNumbers);
+
+      this.checkInputWinningNumbers();
     });
+  }
+
+  checkInputWinningNumbers() {
+    const winningNumbers = new Set(this.#winningNumbers);
+    const filteredWinningNumbers = this.filterRange(this.#winningNumbers, 1, 45)
+  
+    if (this.#winningNumbers.includes(NaN) === true) {
+      throw Error("[ERROR] 숫자를 입력해주세요.");
+    }
+
+    if (this.#winningNumbers.length !== 6) {
+      throw Error("[ERROR] 6개의 당첨 번호를 입력해주세요.");
+    }
+
+    if (winningNumbers.size !== 6) {
+      throw Error("[ERROR] 당첨 번호를 중복 없이 입력해주세요.");
+    }
+
+    if (filteredWinningNumbers.length !== 6) {
+      throw Error("[ERROR] 1부터 45사이의 숫자를 입력해주세요.");
+    }
+  }
+
+  filterRange(arr, a, b) {
+    return arr.filter(arr => (arr >= a && arr <= b));
   }
 }
 
