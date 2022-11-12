089337c862460c4283fdd267f0b73d07b7bc10ff
하지훈 <jnh03336@gmail.com>
2022-11-12 20:05:33 +0900

    feat(lottoManger): 로또 구매 금액을 입력받는다.

--- src/LottoManager.js
+++ src/LottoManager.js
@@ -1,21 +1,36 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class LottoManager {
   #lottos;
+
   #winningResults;
 
-  buyLottos(money, lottoCompany) {
-    this.#lottos = lottoCompany.publishLottos(money);
-  }
+  #money;
 
-  printLottosStatus() {}
+  buyLottos(lottoCompany) {
+    Console.print("구매금액을 입력해 주세요");
+    Console.readLine((), (money)=>{
+        this.#money = Number(money);
+        this.#lottos = lottoCompany.publishLottos(money);
+    });
+  }
 
   checkResults(lottoCompany) {
     this.#winningResults = [];
     this.#lottos.forEach((lotto) => {
-      this.#winningResults.push(lottoCompany.checkResult(lotto));
+      const result = lottoCompany.checkResult(lotto);
+      if (result === -1) return;
+      this.#winningResults.push(result);
     });
   }
 
-  calculateYield() {}
+  calculateYield(lottoCompany) {
+    let totalMoney = 0;
+    this.#winningResults.forEach((winningRank) => {
+      totalMoney += lottoCompany.winningMoney(winningRank);
+    });
+    return `${Math.round((totalMoney / this.#money) * 1000) / 10}%`;
+  }
 }
 
 module.exports = LottoManager;
