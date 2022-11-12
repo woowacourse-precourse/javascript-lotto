be505a90361ac44acdbf5625527a8b1c582582f3
devMingu <rooster100@naver.com>
2022-11-12 18:52:22 +0900

    feat: 당첨에 대한 결과를 보여주는 메소드

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   constructor () { 
+    this.lottoPrize = {
+      3: [0, 5000, '3개 일치 (5,000원) - '],
+      4: [0, 50000, '4개 일치 (50,000원) - '],
+      5: [0, 1500000, '5개 일치 (1,500,000원) - '],
+      6: [0, 30000000, '5개 일치, 보너스 볼 일치 (30,000,000원) - '],
+      7: [0, 2000000000, '6개 일치 (2,000,000,000원) - '],
+    }
+    this.purchaseMoney = 0;
     // const lotto = new Lotto();
   }
 
@@ -10,12 +18,15 @@ class App {
     const user = await this.userInput();
     const prize = await this.prizeInput()
     const bonus = await this.bonusInput();
-    this.isBonusInPrize(bonus, prize);
+    this.caculateResult(user, prize, bonus);
+    this.showResult();
+    MissionUtils.Console.close();
   }
 
   userInput() {
     return new Promise((resolve, reject) => {
       MissionUtils.Console.readLine('구입금액을 입력해 주세요. \n', (userMoney) => {
+        this.purchaseMoney = userMoney;
         resolve(this.lottoPurchase(userMoney));
       })
     })    
@@ -45,7 +56,7 @@ class App {
   }
 
   lottoPurchase(userMoney) {
-    this.numberOfAvailablePurchase(userMoney);
+    return this.numberOfAvailablePurchase(userMoney);
   }
 
   numberOfAvailablePurchase(userMoney) {
@@ -78,10 +89,53 @@ class App {
     });
   }
 
-  isBonusInPrize(bonus, prize) {
+  isBonusInPrize(prize, bonus) {
     if (prize.includes(bonus)) {
       throw new Error("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.")
     }
+  }
+
+  caculateResult(user, prize, bonus) {
+    this.isBonusInPrize(prize, bonus);
+
+    user.forEach(element => {
+      let cnt = 0;
+      for (let i=0 ; i<prize.length ; i++){
+        const n = parseInt(prize[i]);
+        if (element.includes(n)) {
+          cnt++;
+        }
+      }
+
+      if (element.includes(bonus) || cnt >= 5) {
+        this.lottoPrize[cnt+1][0] += 1;
+      }
+      else if (cnt >= 3){
+        this.lottoPrize[cnt][0] += 1;
+      }
+    });
+  }
+
+  showResult() {
+    MissionUtils.Console.print('\n당첨 통계');
+    MissionUtils.Console.print('---');
+
+    for (let i=3 ; i<=7 ; i++){
+      MissionUtils.Console.print(`${this.lottoPrize[i][2]} ${this.lottoPrize[i][0]}개`);
+    }
+    let sum = this.totalRevenue();
+    let revenue = (sum / this.purchaseMoney)*100;
+    let total = parseFloat(revenue.toFixed(2));
+    
+    MissionUtils.Console.print(`총 수익률은 ${total.toLocaleString('ro-RO')}%입니다.`);
+  }
+
+  totalRevenue() {
+    let sum = 0;
+    for (let i=3 ; i<=7 ; i++){
+      sum += (this.lottoPrize[i][0] * this.lottoPrize[i][1])
+    }
+    return sum;
   }
 }
 
