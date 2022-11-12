349102b2724e37da7d117e3952ebd0ce79c0a541
Narcoker <jykim9868@gmail.com>
2022-11-12 17:17:59 +0900

    Feat: 현금값 유효성 검사 코드 이동 Sytem -> Person

--- src/Person.js
+++ src/Person.js
@@ -1,13 +1,22 @@
 const { MissionUtils, SYSTEM } = require('./System');
-const { MESSAGE } = require('./Constants')
+const { MESSAGE, ERROR } = require('./Constants')
 class Person {
     constructor() {
         this.lottos = null;
     }
 
+    isCorrectCash(cash) {
+        if (isNaN(cash)) {
+            throw new Error(ERROR.CASH_IS_NOT_NUMBER);
+        }
+        if (cash % 1000) {
+            throw new Error(ERROR.INVAID_CASH);
+        }
+    }
+
     buy() {
         MissionUtils.Console.readLine(MESSAGE.ENTER_CASH, (cash) => {
-            SYSTEM.isCorrectCash(cash);
+            this.isCorrectCash(cash);
             this.lottos = SYSTEM.publishLotto(cash);
             SYSTEM.getResult(this.lottos, cash);
         });
