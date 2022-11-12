3e489ef1285454662defcb29a5e29b80a22aeb00
bkdragon0228 <rlaqjarb2067@naver.com>
2022-11-13 01:49:56 +0900

    refactor : lotto 로직 변경
    Input class에서 입력받은 당첨번호를 저장하고 검증한다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,47 +1,19 @@
-const { Random, Console } = require('@woowacourse/mission-utils');
+const { LottoException } = require('./Exception');
 
 class Lotto {
     #numbers;
 
-    constructor(numbers = [1, 2, 3, 4, 5, 6]) {
-        this.validate(numbers);
+    constructor(numbers) {
+        this.validate(this.#numbers);
         this.#numbers = numbers;
     }
 
     validate(numbers) {
-        let newSet = new Set(numbers);
-        if (newSet.size !== 6) {
-            throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-        }
+        const exception = new LottoException();
+        exception.checkLottoException(numbers);
     }
 
     // TODO: 추가 기능 구현
-
-    // 자동 복권 (로또 한장)
-    makeRandomLotto() {
-        const lotto = Random.pickUniqueNumbersInRange(1, 45, 6);
-        lotto.sort((a, b) => a - b);
-        return lotto;
-    }
-
-    showLottoNumbers(lotto) {
-        Console.print(
-            `[${lotto[0]}, ${lotto[1]}, ${lotto[2]}, ${lotto[3]}, ${lotto[4]}, ${lotto[5]}]`
-        );
-    }
-
-    // 자동 복권 (로또 여러장)
-    makeRandomLottos(number) {
-        const lottos = [];
-
-        for (let index = 1; index <= number; index++) {
-            const currentNumbers = this.makeRandomLotto();
-            this.showLottoNumbers(currentNumbers);
-            lottos.push(currentNumbers);
-        }
-
-        return lottos;
-    }
 }
 
 module.exports = Lotto;
