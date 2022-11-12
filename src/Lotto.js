d7b163356ed79db42de6fbe9d6ebd0d40b9e4b74
banma70 <banma1234@gmail.com>
2022-11-12 17:22:11 +0900

    feature: 로또번호 생성 함수 작성

--- src/Lotto.js
+++ src/Lotto.js
@@ -5,16 +5,27 @@ class Lotto {
 
   constructor(numbers, BONUS_NUMBER, MONEY) {
     this.validate(numbers);
-    this.errorHandler(BONUS_NUMBER, MONEY);
+    this.errorHandler(numbers, BONUS_NUMBER, MONEY);
+    // this.makeRandomValue(MONEY);
+    // this.winnigPrize() = winnigPrize();
 
     this.#numbers = numbers;
+    // this.numbersArray = this.sliceNumber(numbers)
+    // this.makeRandomValue() = makeRandomValue();
     this.BONUS_NUMBER = BONUS_NUMBER;
     this.MONEY = MONEY;
   }
 
+  sliceNumber(numbers) {
+    return numbers.split(',').map((item) => {
+      return parseInt(item);
+    })
+  }
+
   validate(numbers) {
-    const lottoSet = new Set(numbers);
-    if (lottoSet.size != numbers.length) {
+    let numberArray = this.sliceNumber(numbers);
+    const lottoSet = new Set(numberArray);
+    if (lottoSet.size != numberArray.length) {
       throw new Error("[ERROR] 중복된 숫자를 입력할 수 없습니다.");
     }
 
@@ -29,7 +40,7 @@ class Lotto {
     }
   }
   
-  errorHandler(BONUS_NUMBER, MONEY) {
+  errorHandler(numbers, BONUS_NUMBER, MONEY) {
     if (BONUS_NUMBER < 1 | BONUS_NUMBER > 45) {
       throw new Error("[ERROR] 1 ~ 45까지의 숫자만 입력할 수 있습니다.");
     }
@@ -38,10 +49,23 @@ class Lotto {
       throw new Error("[ERROR] 복권의 1장당 가격은 1000원입니다.");
     }
 
-    if (this.#numbers.includes(BONUS_NUMBER)) {
+    if (numbers.includes(BONUS_NUMBER)) {
       throw new Error("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
     }
   }
-}
 
+  makeRandomValue(MONEY) {
+    let lotteryTicket = [];
+
+    for(let i=0; i<MONEY/1000; i++) {
+      let temp = [];
+      temp = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      lotteryTicket.push(temp.sort((a, b) => {
+        return a-b;
+      }));
+    }
+
+    return lotteryTicket
+  };
+}
 module.exports = Lotto;
