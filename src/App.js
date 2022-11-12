6ca59e06c4f6a6dce1cbbe7b73e74caaaa31ec0b
eunsung <esk114477@gmail.com>
2022-11-13 00:09:21 +0900

    feat : 수익률 기능 추가 및 당첨 숫자 중복 입력 예외 처리 추가

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const buyLotto = () => {
   Console.readLine('구입금액을 입력해 주세요.\n', (num) => {
     countLotto = Number(num) / 1000;
     throwInput = Number(num) % 1000;
+    Console.print(`${countLotto}개를 구매했습니다.`);
     showLottoNumber(countLotto, throwInput);
   });
 }
@@ -26,7 +27,6 @@ const setLottoNumbers = () => {
 
 const showLottoNumber = (countLotto, throwInput) => {
   if (throwInput > 0) throw '[ERROR] 금액은 1,000원 단위로 입력해주세요';
-  Console.print(`${countLotto}개를 구매했습니다.`);
   for (let i = 0; i < countLotto; i++) {
     lottos[i] = setLottoNumbers()
     Console.print(lottos[i]);
@@ -39,6 +39,10 @@ const inputLottoNumber = (lottos) => {
   Console.readLine('당첨 번호를 입력해 주세요.\n', (num) => {
     inputSixNumber = num.toString().split(",").map((str) => Number(str));
     console.log(inputSixNumber);
+    const lengthThrow = new Set(inputSixNumber);
+    const uniqueNumber = [...lengthThrow];
+    if(uniqueNumber.length != inputSixNumber.length) throw '[ERROR] 중복된 로또 번호가 있습니다.';
+    console.log(uniqueNumber);
     inputBonusLotto(inputSixNumber, lottos);
   })
 }
@@ -63,7 +67,7 @@ const compareLotto = (inputSixNumber, lottos) => {
   countScore(checkLength,lottos,last);
 }
 
-const countScore = (checkLength,lottos, last) => {
+const countScore = (checkLength,lottos,last) => {
   let second = 0;
   for(let i = 0; i < lottos.length; i++){
     if (lottos[i].includes(last) && checkLength[i] === 4) {
@@ -74,16 +78,29 @@ const countScore = (checkLength,lottos, last) => {
   let third = checkLength.filter(num => 5 === num).length;
   let fourth = checkLength.filter(num => 4 === num).length;
   let fiveth = checkLength.filter(num => 3 === num).length;
-  showCount(first,second,third,fourth,fiveth);
+  showCount(first,second,third,fourth,fiveth,lottos);
 }
 
-const showCount = (first, second, third, fourth, fiveth) => {
+const showCount = (first, second, third, fourth, fiveth,lottos) => {
   Console.print("당첨 통계\n---");
   Console.print(`3개 일치 (5,000원) - ${fiveth}개`);
   Console.print(`4개 일치 (50,000원) - ${fourth}개`);
   Console.print(`5개 일치 (1,500,000원) - ${third}개`);
   Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${second}개`);
   Console.print(`6개 일치 (2,000,000,000원) - ${first}개`);
+  let fivethMoney = fiveth * 5000;
+  let fourthMoney = fourth * 50000;
+  let thirdMoney = third * 1500000;
+  let secondMoney = second * 30000000;
+  let firstMoney = first * 2000000000;
+  plusRate(fivethMoney,fourthMoney,thirdMoney,secondMoney,firstMoney,lottos);
+}
+
+const plusRate = (first, second, third, fourth, fiveth,lottos) => {
+  let allPlus = first + second + third + fourth + fiveth;
+  let lottoLength = lottos.length;
+  let returnRate = (allPlus/lottoLength)/100;
+  Console.print(`총 수익률은 ${returnRate.toFixed(1)}%입니다.`);
 }
 
 class App {
