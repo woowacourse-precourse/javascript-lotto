99dc2d960a0402d952e808425c236e9f27bc2d0a
jinlee <vito9270@naver.com>
2022-11-12 16:23:04 +0900

    fix: 기능 목록 변경에 따른 함수 변경

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ const Lotto = require("../src/Lotto.js");
 
 class App {
   play() {
-    inputPurchaseAmount()
+    inputPurchaseAmount();
 
-    const lotto = new Lotto();
+    // const lotto = new Lotto();
   }
 }
 
@@ -15,7 +15,6 @@ const app = new App();
 app.play();
 
 function inputPurchaseAmount() {
-  console.log(1)
   MissionUtils.Console.readLine(
     "구입금액을 입력해 주세요.\n",
     (purchaseAmount) => {
@@ -25,19 +24,38 @@ function inputPurchaseAmount() {
 }
 
 function is1000Multiple(purchaseAmount) {
-  console.log(2)
   if (purchaseAmount % 1000 !== 0) {
     throw new Error("[ERROR] 로또 구입 금액은 1000의 배수여야 합니다.");
   }
-  inputWinningNumbers();
+  makeCandidateNumberSets(parseInt(purchaseAmount / 1000));
 }
 
-function inputWinningNumbers() {
-  console.log(3)
-  MissionUtils.Console.readLine(
-    "당첨 번호를 입력해 주세요.\n",
-    (winningNumbers) => {
-      return winningNumbers;
-    }
-  );
+function makeCandidateNumberSets(purchaseNumber) {
+  MissionUtils.Console.print(`${purchaseNumber}개를 구매했습니다.`);
+  let candidateNumberSets = [];
+  for (let i = 0; i < purchaseNumber; i++) {
+    candidateNumberSets.push(makeCandidateNumber());
+  }
+  printCandidateNumberSets(candidateNumberSets);
+}
+
+function makeCandidateNumber() {
+  const candidateNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
+  return candidateNumbers.sort(sortNumbers)
+}
+
+function printCandidateNumberSets(candidateNumberSets) {
+  candidateNumberSets.forEach((eachSets) => {
+    MissionUtils.Console.print(eachSets);
+  });
+}
+
+function sortNumbers(a, b) {
+  if (a > b) {
+    return 1;
+  } else if (b > a) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
