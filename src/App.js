66cd7ea1e2112e26da1273df6366deb96eec7243
wonkyum-kim <dodo12120@naver.com>
2022-11-12 20:14:29 +0900

    feat: prefare to buy lotto tickets
    - purchase amount can get by purchaseAmountInputHandler()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    this.purchaseAmountInputHandler();
+  }
+
+  purchaseAmountInputHandler = () => {
+    const REQUIRE_PURCHASE_AMOUNT = '구입 금액을 입력해 주세요.\n';
+    MissionUtils.Console.readLine(REQUIRE_PURCHASE_AMOUNT, this.userMoneyInputHandler);
+  };
+
+  userMoneyInputHandler = (userMoney) => {
+
+  };
 }
 
 module.exports = App;
