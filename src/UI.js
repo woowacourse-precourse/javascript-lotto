7766749512b593d4ab96f3e30a8780af42c1ca7e
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-13 02:16:29 +0900

    feat(method): add getCash
    Add to UI class
    - getCash method:
       - Try to get cash input from getCashInput function and return cash
       - Catch error if it fails
    - getCashInput function:
       - Get cash input from user
       - Return Promise object of cash input

--- src/UI.js
+++ src/UI.js
@@ -1,3 +1,29 @@
-class UI {}
+const MissionUtils = require("@woowacourse/mission-utils");
+const ErrorCase = require("./ErrorCase");
+
+const CASH_INPUT_MESSAGE = "구입금액을 입력해 주세요.\n";
+const CASH_INPUT_ERROR_MESSAGE = "잘못된 금액입니다.";
+
+const getCashInput = () => {
+  return new Promise((resolve, reject) =>
+    MissionUtils.Console.readLine(CASH_INPUT_MESSAGE, (cashInput) => {
+      const isWrongInput = ErrorCase.isWrongCashInput(cashInput);
+      if (isWrongInput) reject();
+
+      resolve(cashInput);
+    })
+  );
+};
+
+class UI {
+  static async getCash() {
+    try {
+      const cash = await getCashInput();
+      return cash;
+    } catch {
+      throw new Error(CASH_INPUT_ERROR_MESSAGE);
+    }
+  }
+}
 
 module.exports = UI;
