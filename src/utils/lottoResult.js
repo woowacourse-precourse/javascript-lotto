99c75e4f1ca8aa999ab16b7615af92bc76c38b89
olo90632951 <olo90632951@gmail.com>
2022-11-12 13:44:34 +0900

    test: 당첨 번호와 사용자 번호를 비교하여 개수를 구하는 테스트 코드 작성

---
+++ src/utils/lottoResult.js
@@ -0,0 +1,11 @@
+lottoResult = {};
+
+lottoResult.getAllMatchingNumbersResult = (userAllLottoNumbers, winningNumbers, bonusNumber) => {
+  return 1;
+};
+
+lottoResult.getMatchingNumbersResult = (userLottoNumbers, winningNumbers, bonusNumber) => {
+  return 1;
+};
+
+module.exports = lottoResult;
