2438a0b48c381860294204964e13fd74dca30d14
chex <chex417@naver.com>
2022-11-12 14:39:55 +0900

    feat: 0으로 시작하는 입력인지 확인하는 기능 추가

--- src/Util.js
+++ src/Util.js
@@ -3,6 +3,11 @@ class Util {
     const NUMBER_REGEXP = /^[0-9]+$/;
     return NUMBER_REGEXP.test(str);
   }
+
+  static isZeroStartInput(str) {
+    const ZERO_START_REGEXP = /^[0]+/;
+    return ZERO_START_REGEXP.test(str);
+  }
 }
 
 module.exports = Util;
