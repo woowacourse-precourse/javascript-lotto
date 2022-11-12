8883bf15c17786d754fdbc7cccddeca6d5107d9e
unknown <chn0202@gmail.com>
2022-11-13 03:03:55 +0900

    feat(BuyLottery): 로또구입 도메인분리

--- src/BuyLottery.js
+++ src/BuyLottery.js
@@ -1,15 +1,24 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class BuyLottery {
+  buy(money) {
+    this.checkAmout(money);
+    const quentity = this.numberOfpurchases(money);
+
+    const lottos = this.createRendomLotto(quentity);
+    return { quentity, lottos };
+  }
   checkAmout(money) {
     if (money % 1000 !== 0)
       throw new Error("[ERROR] 금액은 1000원 단위로 나누어 떨어져야 합니다.");
   }
   numberOfpurchases(money) {
     let numberOfPurchases = money / 1000;
+
     return numberOfPurchases;
   }
   createRendomLotto(lotteryNumber) {
     let totalLottoNumbers = [];
-    while (lotteryNumber !== arr.length) {
+    while (lotteryNumber !== totalLottoNumbers.length) {
       const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
       totalLottoNumbers.push(numbers);
     }
