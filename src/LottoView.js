d8b5792c68d3212932d5628a116efaba65c92b63
ReturnReason <reasonz@daum.net>
2022-11-12 10:07:42 +0900

    refactor: 계산 모듈 적용 및 코드 리팩토링

--- src/LottoView.js
+++ src/LottoView.js
@@ -1,19 +1,19 @@
 const { Console } = require('./utils/missionUtil');
+const { lottoCount } = require('./utils/calculator');
 const { INPUT_MESSAGES, OUTPUT_MESSAGES } = require('./common/messages');
-const { LOTTO_INFO } = require('./common/constants');
 const Validator = require('./Validator');
 
 class LottoView {
-  static getPurchaseAmount() {
+  getPurchaseAmount() {
     Console.readLine(`${INPUT_MESSAGES.AMOUNT}\n`, (money) => {
-      Validator.checkValidInput(money);
+      Validator.checkValidMoney(money);
       this.printLottoCount(money);
     });
   }
 
-  static printLottoCount(money) {
-    const lottoCount = money / LOTTO_INFO.PRICE;
-    Console.print(`\n${OUTPUT_MESSAGES.BUY(lottoCount)}`);
+  printLottoCount(money) {
+    const count = lottoCount(money);
+    Console.print(`\n${OUTPUT_MESSAGES.BUY(count)}`);
   }
 }
 
