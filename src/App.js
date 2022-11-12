95886e98d8a6b24a76056ea0e1508feac477fe79
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-11-12 19:47:53 +0900

    feat : 로또 구입 금액 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  constructor() {}
+
   play() {}
+
+  // 금액 입력
+  lottoStart(answer) {
+    Console.readLine("구입금액을 입력해 주세요.", (answer) => {
+      if (isNaN(answer)) {
+        throw new Error("[ERROR] 숫자를 입력하지 않았습니다.");
+      } else {
+        checkMoney(answer);
+      }
+    });
+  }
 }
 
 module.exports = App;
