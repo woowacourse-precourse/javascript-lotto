89252462d9a589c7950bbea58320a005c35d661b
h2002y <h2002y@naver.com>
2022-11-13 23:02:11 +0900

    feat: Delete checkLottoSeparateSize and convertLottoStrToInt func in Lotto.js

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,4 @@
 const ERROR_LOTTO_SIZE_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
-const ERROR_LOTTO_SEPARATE_SIZE_MESSAGE = "[ERROR] 십진수가 아닌 숫자입니다.";
 const ERROR_LOTTO_IS_INT_MESSAGE = "[ERROR] 정수가 아닌 숫자입니다.";
 const ERROR_LOTTO_OVERLAP_MESSAGE = "[ERROR] 로또 번호는 중복되는 숫자가 없어야 합니다.";
 const ERROR_LOTTO_VALUE_MESSAGE = "[ERROR] 로또 번호는 1 ~ 45 사이의 숫자입니다.";
@@ -9,34 +8,18 @@ class Lotto {
 
   constructor(numbers) {
     this.validate(numbers);
-    this.convertWinLottoStrToINT(numbers);
     this.#numbers = numbers;
   }
 
   validate(numbers) {
     this.checkLottoSize(numbers);
-    this.checkLottoSeparateSize(numbers);
     this.checkLottoOverlap(numbers);
     this.checkLottoValue(numbers);
-  }
-
-  convertWinLottoStrToINT(numbers) {
-    for(let i = 0; i < numbers.length; i++) {
-      numbers[i] = Number(numbers[i]);
-    }
   }
 
   checkLottoSize(numbers) {
     if (numbers.length !== 6) {
       throw new Error(ERROR_LOTTO_SIZE_MESSAGE);
-    }
-  }
-
-  checkLottoSeparateSize(numbers) {
-    for(let i = 0; i < numbers.length; i++) {
-      if(numbers[i].length !== 1) {
-        throw new Error(ERROR_LOTTO_SEPARATE_SIZE_MESSAGE);
-      }
     }
   }
 
@@ -62,7 +45,7 @@ class Lotto {
 
   checkLottoValue(numbers) {
     for (let i = 0; i < numbers.length; i++) {
-      if((Number(numbers[i]) < 1) || (45 < Number(numbers[i]))) {
+      if(numbers[i] < 1 || (45 < numbers[i])) {
         throw new Error(ERROR_LOTTO_VALUE_MESSAGE);
       }
     }
