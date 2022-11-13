63a607777cfff6085477feeb10bb7e14a084898d
nerdchanii <nerdchanii@gmail.com>
2022-11-13 17:07:43 +0900

    feat: 로또 발권시 에러처리 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,14 +1,23 @@
+const { LOTTO } = require('./constants');
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    Lotto.validate(numbers);
     this.#numbers = numbers;
+    return this;
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  static validate(numbers) {
+    if (numbers.length !== LOTTO.NUMBER_COUNT) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (!numbers.every((number) => number >= LOTTO.MIN_NUMBER && number <= LOTTO.MAX_NUMBER)) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45까지입니다.');
+    }
+    if (new Set(numbers).size !== LOTTO.NUMBER_COUNT) {
+      throw new Error('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
     }
   }
 
