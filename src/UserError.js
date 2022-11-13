b6fc2c4e657eaca58f0e01e489931460213e194b
khkh0109 <ist200120@gmail.com>
2022-11-13 22:16:23 +0900

    feat(UserError): 당첨번호가 6개가 아닌 경우 예외 처리 하는 기능 추가

--- src/UserError.js
+++ src/UserError.js
@@ -10,6 +10,12 @@ class UserError {
       throw new Error("[ERROR] 1,000원 단위로 입력해 주세요.");
     }
   }
+
+  validateWinningNumbers(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  }
 }
 
 module.exports = UserError;
