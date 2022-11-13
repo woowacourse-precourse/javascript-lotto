726a4880623955b57a5c9efccdb4754d80ba36ce
강인영 <kiy@gang-in-yeong-ui-MacBookAir.local>
2022-11-13 16:18:01 +0900

    Enter lotto answer

--- src/App.js
+++ src/App.js
@@ -13,7 +13,26 @@ class App {
     }
     return NEW_ERROR;
   }
+/*
+  enterBonus(){
+    Console.readLine('보너스 번호를 입력해 주세요.\n',(bonus) =>{
 
+    })  
+    return;
+  }
+*/
+  enterAnswerNums(){
+    Console.readLine('\n당첨 번호를 입력해 주세요.\n',(answer) =>{
+      const answerList = answer.split(',');
+      console.log(answerList);
+    })
+    return;
+  }
+/*
+  enterLottoAnswer(){
+    
+  }
+*/
   makeLottoNums(amount){
     let allLines = []; //
     for(var i=0; i<amount; i++){
@@ -21,6 +40,8 @@ class App {
       allLines.push(oneLine);
       Console.print(oneLine);
     }
+
+    this.enterAnswerNums();
     //console.log(allLines);
   }
 
@@ -30,22 +51,22 @@ class App {
     Console.print(`\n${this.amount}개를 구매했습니다.`);
     
     this.makeLottoNums(this.amount);
+    //return this.amount;
   }
 
   //1. 로또 구입 금액 입력받기
   payLottoMoney() {  
     //let lottoAmount;
-    Console.readLine('구입금액을 입력해 주세요.\n', (answer) => {
-      //console.log(`구입금액: ${answer}`);
+    Console.readLine('구입금액을 입력해 주세요.\n', (total) => {
+      //console.log(`구입금액: ${total}`);
 
       //1-1. 구입 금액의 단위에 대한 예외 처리
-      if(parseInt(answer) % 1000 != 0){      
+      if(parseInt(total) % 1000 != 0){      
         Console.print(this.getErrorMessage('unitError'));
         throw '1000원 단위로 입력되지 않음'; //1-2. 예외 출력
       } 
-      this.showLottoAmount(answer);
+      return this.showLottoAmount(total);
       //console.log('로또 수량은...',this.amount)
-      return;
     });
   }
 
@@ -54,7 +75,7 @@ class App {
   }
 
   play() {
-    this.money = this.payLottoMoney(); //1)로또 구입 금액 입력
+    this.amount = this.payLottoMoney(); //1)로또 구입 금액 입력
     //console.log('구매 금액은 ...',this.money);
     //this.amount = this.showLottoAmount(this.money); //2)발행한 로또 수량 및 번호 출력
     //console.log('로또 수량은...',this.amount);
