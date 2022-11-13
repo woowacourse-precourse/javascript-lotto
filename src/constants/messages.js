5974bf35132ea08ab6ddc426addace695eb561be
sh981013s <sh981013s@gmail.com>
2022-11-13 14:17:32 +0900

    feat(messages): 유저 구입 예외 메세지 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,5 +1,7 @@
 const messages = {
   PURCHASING_AMOUNT_MESSAGE: "구입금액을 입력해 주세요.\n",
+  PURCHASING_AMOUNT_ERROR_MESSAGE:
+    "[ERROR] 구입 금액은 1,000원 단위여야 하며, 1,000원으로 나누어 떨어져야 합니다.",
 };
 
 module.exports = messages;
