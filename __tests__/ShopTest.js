bda28e2eb97a28c5bc53c1ae0b8d4adc2c0b75e6
pakxe <pigkill40@naver.com>
2022-11-14 00:15:46 +0900

    chore(shopTest): Shop 클래스 생성자 제거에 의해 테스트 코드 수정

--- __tests__/ShopTest.js
+++ __tests__/ShopTest.js
@@ -3,25 +3,35 @@ const { ERROR } = require('../src/utiles/Constant');
 
 describe('로또판매점 클래스 테스트', () => {
   test('1000원 단위로 입력하지 않으면 예외 발생', () => {
-    expect(() => new Shop(1500)).toThrow(ERROR.PREFIX);
+    const shop = new Shop();
+    expect(() => shop.buyFor(1500)).toThrow(ERROR.PREFIX);
   });
 
   test('올바르게 1000원 단위로 입력', () => {
-    expect(() => new Shop(12000)).toBeTruthy();
+    const shop = new Shop();
+
+    expect(() => shop.buyFor(12000)).toBeTruthy();
   });
 
   test('숫자가 아닌 값을 입력하면 예외 발생', () => {
-    expect(() => new Shop('a')).toThrow(`${ERROR.PREFIX} ${ERROR.NUMBER_ONLY}`);
+    const shop = new Shop();
+
+    expect(() => shop.buyFor('a')).toThrow(
+      `${ERROR.PREFIX} ${ERROR.NUMBER_ONLY}`
+    );
   });
 
   test('개수만큼의 로또 번호들을 반환하는지 테스트', () => {
-    const shop = new Shop(3000);
-    expect(shop.getLottoNumbers().length).toBe(shop.getLottoCount());
+    const shop = new Shop();
+    shop.buyFor(12000);
+    expect(shop.getAllLottoNumbers().length).toBe(shop.getLottoCount());
   });
 
   test('로또번호가 오름차순인지 테스트', () => {
-    const shop = new Shop(2000);
-    const lottoNumbers = shop.getLottoNumbers();
+    const shop = new Shop();
+    shop.buyFor(5000);
+
+    const lottoNumbers = shop.getAllLottoNumbers();
     expect(
       lottoNumbers.filter(
         (lottoNumber, i) =>
