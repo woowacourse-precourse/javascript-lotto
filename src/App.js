20a8d3c5b8ce96f2a221b53580e2821b6d9ca99f
hyunjin <leehj0110@kakao.com>
2022-11-14 04:22:44 +0900

    feat: calculateWinningHistory

--- src/App.js
+++ src/App.js
@@ -63,42 +63,60 @@ class App {
 
   askBonusNumber() {
     Console.askAndGetUserInput("\n" + Console.ASK_BONUS_NUMBER, (bonusNumber) => {
+      Validation.isValidBonusNumber(bonusNumber);
       this.bonusNumber = Number(bonusNumber);
 
-      Console.print(Console.STATISTICS);
+      Console.print("\n" + Console.STATISTICS);
 
-      console.log(
-        this.lottos.reduce((acc, lotto) => {
-          const sameNumberCount = this.countSameNumberWithWinningNumber(lotto, this.winningNumbers);
+      this.calculateWinningHistory(this.lottos, this.winningNumbers, this.bonusNumber, this.winningHistory);
+      this.printWinningHistory(this.winningHistory);
 
-          switch (sameNumberCount) {
-            case 3:
-              acc[THREE] += 1;
-              break;
-            case 4:
-              acc[FOUR] += 1;
-              break;
-            case 5:
-              acc[FIVE] += 1;
-              break;
-            case 6:
-              acc[SIX] += 1;
-              break;
-            default:
-              break;
-          }
+      const yeild = this.calculateYeild(this.lottos, this.winningHistory);
 
-          if (this.bonusNumber === lotto.includes(this.bonusNumber) && sameNumberCount === 5) {
-            acc[FIVE] -= 1;
-            acc[FIVE_WITH_BONUS] += 1;
-          }
-
-          return acc;
-        }, this.winningHistory)
-      );
-
+      Console.print(`총 수익률은 ${yeild}입니다.`);
       Console.close();
     });
+  }
+
+  calculateWinningHistory(lottos, winningNumbers, bonusNumber, winningHistory) {
+    return lottos.reduce((acc, lotto) => {
+      const sameNumberCount = this.countSameNumberWithWinningNumber(lotto, winningNumbers);
+
+      if (sameNumberCount === 3) acc[THREE] += 1;
+      if (sameNumberCount === 4) acc[FOUR] += 1;
+      if (sameNumberCount === 5 && !lotto.includes(bonusNumber)) acc[FIVE] += 1;
+      if (sameNumberCount === 5) acc[FIVE_WITH_BONUS] += 1;
+      if (sameNumberCount === 6) acc[SIX] += 1;
+
+      if (this.bonusNumber === lotto.includes(bonusNumber) && sameNumberCount === 5) {
+        acc[FIVE] -= 1;
+        acc[FIVE_WITH_BONUS] += 1;
+      }
+
+      return acc;
+    }, winningHistory);
+  }
+
+  printWinningHistory(winningHistory) {
+    Console.print(
+      Object.entries(winningHistory)
+        .map(([key, value]) => {
+          return `${key}${value}개`;
+        })
+        .join("\n")
+    );
+  }
+
+  calculateYeild(lottos, winningHistory) {
+    const totalPrize =
+      winningHistory[THREE] * 5000 +
+      winningHistory[FOUR] * 50000 +
+      winningHistory[FIVE] * 1500000 +
+      winningHistory[FIVE_WITH_BONUS] * 30000000 +
+      winningHistory[SIX] * 2000000000;
+    const totalPurchaseAmount = lottos.length * 1000;
+
+    return Math.round((totalPrize / totalPurchaseAmount) * 1000) / 10 + "%";
   }
 }
 
