e9c5791b3fafbbf133adb69af481f80b75b06cc4
HWI <dlwognl99@naver.com>
2022-11-13 14:19:13 +0900

    feat: 입력된 당첨 번호가 로또 사이즈인지 검사하는 메서드 구현 feat: 입력된 당첨 번호의 범위가 1이상 45이하인지 검사하는 메서드 구현 - 테스트를 하기 위해 split을 추가했다

--- src/Validation.js
+++ src/Validation.js
@@ -17,17 +17,28 @@ class Validation {
     return change === 0;
   }
   static checkInputWinnerNumber(winnerNumber) {
-    const winnerNumberArr = winnerNumber.split(",");
+    if (!this.isLottoSize(winnerNumber)) {
+      throw new Error(ERROR_MESSAGE.ERROR_MESSAGE_SIZE_INVALID);
+    }
     if (this.isOnlyNumber(winnerNumber)) {
       throw new Error(ERROR_MESSAGE.ERROR_MESSAGE_NOTONLY_NUMBER);
     }
-
     if (this.isUniqueNumber(winnerNumber)) {
       throw new Error(ERROR_MESSAGE.ERROR_MESSAGE_NOTUNIQUE_NUMBER);
     }
+    if (!this.isNumberInRange(winnerNumber)) {
+      throw new Error(ERROR_MESSAGE.ERROR_MESSAGE_NOTINRANGE);
+    }
+  }
+  static isLottoSize(winnerNumber) {
+    return winnerNumber.length === LOTTO_SIZE;
   }
   static isUniqueNumber(winnerNumber) {
     return new Set(winnerNumber).size === LOTTO_SIZE;
+  }
+  static isNumberInRange(winnerNumber) {
+    const winnerNumberArr = winnerNumber.split(",");
+    return winnerNumberArr.every((number) => number >= 1 && number <= 45);
   }
 }
 
