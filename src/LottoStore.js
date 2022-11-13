83dca8e8bdc2d8f7517d3525a356bb5e0e3f0f1d
thewronghand <penfreak77@gmail.com>
2022-11-14 03:17:52 +0900

    feat: askBuyLottoCount 메서드 생성
    금액을 입력받고 로또 구매 매수를 반환하는 메서드를 생성한다.

--- src/LottoStore.js
+++ src/LottoStore.js
@@ -1,6 +1,11 @@
 const { isValidMoneyNumberAmount } = require("../../backup/src/new/util/utils");
 
 class LottoStore {
+  askBuyLottoCount(money) {
+    this.validateMoney(money);
+    return money / 1000;
+  }
+
   validateMoney(input) {
     const money = Number(input);
     isValidMoneyNumberAmount(money);
