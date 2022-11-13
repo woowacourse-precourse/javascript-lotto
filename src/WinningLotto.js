1959e2b015ee36d7b62eb256a1b6c7955a3bc753
2je0 <bulljy@naver.com>
2022-11-13 20:48:02 +0900

    🐛 fix: winningLotto 에서 validate 상속받아 겹치던 문제 해결

--- src/WinningLotto.js
+++ src/WinningLotto.js
@@ -8,7 +8,7 @@ class WinningLotto extends Lotto {
    */
   constructor(numbers, bonus) {
     super(numbers);
-    this.validate(numbers, bonus);
+    this.validateBonus(numbers, bonus);
     this.bonus = bonus;
   }
 
@@ -17,7 +17,7 @@ class WinningLotto extends Lotto {
    * @param {Array<number>} numbers - 당첨 로또 번호
    * @param {number} bonus - bonus 번호
    */
-  validate(numbers, bonus) {
+  validateBonus(numbers, bonus) {
     if (numbers.includes(bonus)) throw new Error(MESSAGE.ERROR_BONUS_IN_NUMS);
     if (!Number.isInteger(bonus)) throw new Error(MESSAGE.ERROR_NO_INTEGER);
     if (bonus < 1 || bonus > 45) throw new Error(MESSAGE.ERROR_OUT_OF_RANGE);
