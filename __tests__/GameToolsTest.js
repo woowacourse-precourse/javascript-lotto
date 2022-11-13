4687884e17db65779dbcd0e34bb4f67eafd755ea
evencoding <evencoding@gmail.com>
2022-11-13 17:04:53 +0900

    test: 입력받은 당첨 번호를 ,로 구분된 숫자 배열로 반환하는 기능 테스트

--- __tests__/GameToolsTest.js
+++ __tests__/GameToolsTest.js
@@ -1,10 +1,21 @@
+const { stringToSortedNumberArray } = require('../src/GameTools');
 const GameTools = require('../src/GameTools');
 
 describe('로또 발행 테스트', () => {});
 
-describe('문자열을 ,로 구분된 숫자 배열로 반환', () => {});
+test.only('입력받은 당첨 번호를 ,로 구분된 숫자 배열로 반환', () => {
+  const winningString = ['2,3,4,5,6,1', '31,1,24,45,3,4'];
+  const answers = [
+    [1, 2, 3, 4, 5, 6],
+    [1, 3, 4, 24, 31, 45],
+  ];
 
-test.only('두 배열이 모두 가진 숫자 요소의 개수를 반환', () => {
+  winningString.forEach((string, i) => {
+    expect(stringToSortedNumberArray(string)).toEqual(answers[i]);
+  });
+});
+
+test('두 배열이 모두 가진 숫자 요소의 개수를 반환', () => {
   const lottoNumbers = [
     [1, 2, 3, 4, 5, 6],
     [1, 5, 8, 31, 24, 45],
