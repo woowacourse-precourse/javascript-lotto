f22b96048562d81218a34625b11651650ff01ff7
millifail <hwangbird99@gmail.com>
2022-11-13 09:04:23 +0900

    feat(get-number):get number of lotto
    - [사용자 입출력] 사용자가 입력한 가격에 대해 로또 개수 출력 및 에러 처리 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 const LOTTO = require('./Lotto');
+const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
-  play() {}
+  play() {
+    const number_of_lottos = this.get_number_of_lottos();
+    MissionUtils.Console.print(number_of_lottos + '개를 구매했습니다.');
+  }
+
+  get_number_of_lottos() {
+    let user_price;
+    MissionUtils.Console.readLine('구입 금액을 입력하세요.', (price) => {
+      user_price = Number(price);
+    });
+    if (user_price % 1000 !== 0) {
+      throw new Error('[ERROR] 구입 금액은 1000원단위입니다.');
+    }
+    return user_price / 1000;
+  }
 }
 
 module.exports = App;
