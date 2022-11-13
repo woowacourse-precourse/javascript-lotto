580d6f1143cc0921afc0888d073da698153177a4
Cobocho <rlaalsrb1111@naver.com>
2022-11-14 00:42:18 +0900

    test: `GameUtils` 캡슐화에 따른 단위 테스트 재구현

--- __tests__/GameUtilsTest.js
+++ __tests__/GameUtilsTest.js
@@ -1,38 +1,72 @@
 const GameUtils = require('../src/Utils/GameUtils');
 
 describe("GameUtils 테스트", () => {
-  test("공백제거 확인", () => {
+  test("금액 표기 표준 제거 테스트", () => {
     const input = [
-      '1  2  3 4   5   6 7 8',
-      '1 23  4  567 8   ',
-      '   1  2 3  4  5678 ',
-      '1 2 3  45   67  8 ',
-      '12345678'
+      '10,000,000원',
+      '8,000원',
+      '3,000,000원',
     ];
-    input.forEach(subject => {      
-      const result = GameUtils.removeBlank(subject);
-      expect(result).toBe('12345678');
-    })
+    const output = [
+      '10000000',
+      '8000',
+      '3000000',
+    ];
+    input.forEach((subject, idx) => {      
+      const result = GameUtils.removeMarkingStandardMoney(subject);
+      expect(result).toBe(output[idx]);
+    });
   });
-  test("원 및 콤마 제거 확인", () => {
+
+  test("입력값 배열화 테스트", () => {
     const input = [
-      '10,000원',
-      '19,999원',
-      '원원원원,,,,,',
+      '1,2,3,4,5,6',
+      '10,20,30,40,50,60',
+      '1,2,3,4,5,6,7,8,9,10',
     ];
-    input.forEach(subject => {      
-      let result = true;
-      subject = GameUtils.removeMarkingStandardMoney(subject);
-      if(subject.includes(',') || subject.includes('원')) result = false;
-      expect(result).toBeTruthy();
-    })
+    const output = [
+      [1, 2, 3, 4, 5, 6],
+      [10, 20, 30, 40, 50, 60],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    ];
+    input.forEach((subject, idx) => {      
+      const result = GameUtils.toArray(subject);
+      expect(result).toEqual(output[idx]);
+    });
   });
-  test("로또 개수 산출 확인", () => {
-    const input = [1000, 5000, 10000, 15000];
-    const result = [1, 5, 10, 15];
-    input.forEach((subject, idx) => {  
-      const sheets = GameUtils.getSheets(subject);
-      expect(sheets).toBe(result[idx]);
-    })
+  
+  test("구매 장수 계산 테스트", () => {
+    const input = [1000, 2000, 3000, 5000, 30000];
+    const output = [1, 2, 3, 5, 30];
+    input.forEach((subject, idx) => {      
+      const result = GameUtils.getSheets(subject);
+      expect(result).toBe(output[idx]);
+    });
+  });
+
+  test("수익률 계산 테스트", () => {
+    const input = [
+      [10000, 100000],
+      [20000, 200000],
+      [8000, 5000],
+      [500, 8000],
+      [10000, 1000],
+      [5000, 100],
+      [4360, 700]
+    ];
+    const output = ['1000.0', '1000.0', '62.5', '1600.0', '10.0', '2.0', '16.1'];
+    input.forEach((subject, idx) => {      
+      const result = GameUtils.getRevenueRate(subject[0], subject[1]);
+      expect(result).toBe(output[idx]);
+    });
+  });
+
+  test("콤마 삽입 테스트", () => {
+    const input = [1000, 10000, 9999999, 213111, 100, 10, 1000000000];
+    const output = ['1,000', '10,000', '9,999,999', '213,111', '100', '10', '1,000,000,000'];
+    input.forEach((subject, idx) => {      
+      const result = GameUtils.addComma(subject);
+      expect(result).toBe(output[idx]);
+    });
   });
 });
