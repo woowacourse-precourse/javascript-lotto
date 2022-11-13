ef757967dbc04cdafc51c41b54277d095529ffd1
ywoo12321 <ywoo12321@gmail.com>
2022-11-13 18:02:01 +0900

    feat: App.js에서 Lotto.js 불러오기 구현 및 App.js 실행 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+const { Console } = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
 class App {
-  play() {}
+  play() {
+    new Lotto();
+  }
 }
-
+const app = new App();
+app.play();
 module.exports = App;
