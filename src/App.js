17bcb525c4fc244d49b1c49f493eff5297db7c94
bae-sh <hyeon1293@gmail.com>
2022-11-13 22:06:20 +0900

    feat : 보너스 숫자 입력

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ const Lotto = require('./Lotto');
 const { print, readLine, makeLottoNumber } = require('./util');
 class App {
   #lotto;
+  #bonusNumber;
   #myLottos;
 
   printBoughtLottos({ lottoCount }) {
     print(`${lottoCount}개를 구매했습니다.`);
     this.#myLottos.forEach(lotto => print(lotto));
   }
-
+  inputBonusNumber() {
+    readLine('보너스 번호를 입력해 주세요.\n', number => (this.#bonusNumber = number));
+  }
   inputLottoNumber() {
     readLine('당첨 번호를 입력해 주세요.\n', numbers => {
       const lottoNumbers = numbers.split(',');
 
       this.#lotto = new Lotto(lottoNumbers);
+      inputBonusNumber();
     });
   }
+
   play() {
     readLine('구입금액을 입력해 주세요.\n', money => {
       const lottoCount = Math.floor(Number(money) / 1000);
