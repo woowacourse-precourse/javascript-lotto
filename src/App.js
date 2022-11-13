b34f97e763a78c092ebb882b629c76d63ebdc5d4
Siby1lA <psb4644@gmail.com>
2022-11-13 13:05:57 +0900

    feat: 로또 생성 로직

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Purchase = require("./Purchase");
-const Issuance = require("./Issuance");
+const Lotto = require("./Lotto");
 class App {
   lottoIssuance = (count) => {
     MissionUtils.Console.print(`${count}개를 구매했습니다.`);
-    const lottoNumber = Array.from({ length: count }, () =>
-      MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
-    );
-    MissionUtils.Console.print(lottoNumber);
+    const lottoNumber = Array.from({ length: count }, () => {
+      const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(
+        1,
+        45,
+        6
+      );
+      const ascending = randomNumber.sort((a, b) => a - b);
+      return new Lotto(ascending);
+    });
+    let data = [];
+    lottoNumber.forEach((num) => {
+      data.push(num.getNumbers());
+    });
+    MissionUtils.Console.print(data);
   };
   lottoPurchase = () => {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (purchase) => {
-      new Purchase(purchase);
+      // new Purchase(purchase);
       this.lottoIssuance(parseInt(purchase, 10) / 1000);
     });
   };
--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,12 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    if (numbers.map((num) => isNaN(num))) {
+    if (new Set(numbers).size !== 6) {
       throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
     }
+  }
+  getNumbers() {
+    return this.#numbers;
   }
 }
 
