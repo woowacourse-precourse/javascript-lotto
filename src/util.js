b501fa8e00729d0630739331209ccbdf46ee7b26
bae-sh <hyeon1293@gmail.com>
2022-11-13 21:12:58 +0900

    feat :로또 숫자 만들기 함수 구현

--- src/util.js
+++ src/util.js
@@ -7,3 +7,14 @@ const print = content => {
 const readLine = (content, func) => {
   MissionUtils.Console.readLine(content, func);
 };
+
+const makeRandomLottoNumber = count => {
+  const lottoNumbers = [];
+
+  for (let i = 0; i < count; i++) {
+    const lottoNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+
+    lottoNumbers.push(lottoNumber);
+  }
+  return lottoNumbers;
+};
