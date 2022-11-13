ce33df31cb0c25ef9b21b06c787e7dd8da4e84b4
DongWooKim97 <nanaukim@naver.com>
2022-11-13 20:30:38 +0900

    refactor: 상수 저장 클래스 활용에 따른 변수명 변경 및 로또 생성 클래스 연결

--- src/play/Clerk.js
+++ src/play/Clerk.js
@@ -1,11 +1,18 @@
 const Mission = require("@woowacourse/mission-utils");
+const { GAME_MESSAGES } = require("../utils/Constants");
+const QuickPick = require("../input/QuickPick");
 
 class Clerk {
   #payment;
+  #myLottoArray;
+  #lottoAmount;
 
   inputLottoAmount() {
-    Mission.Console.readLine("구입금액을 입력해 주세요.", (payment) => {
+    Mission.Console.readLine(GAME_MESSAGES.ASK_TO_PAY, (payment) => {
+      const quickPick = new QuickPick(payment);
       this.#payment = payment;
+      this.#myLottoArray = quickPick.arrayOutput();
+      this.#lottoAmount = quickPick.amountOutput();
     });
     return;
   }
