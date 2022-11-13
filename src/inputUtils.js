4cdc4e0ac3201b325da267110b086fbb7c805ce2
pinkishincoloragain <smb1103@gmail.com>
2022-11-14 03:10:47 +0900

    refactor: inputUtils ticket -> lotto 문구 변경

--- src/inputUtils.js
+++ src/inputUtils.js
@@ -1,22 +1,31 @@
-const { Console } = require("@woowacourse/mission-utils");
-const { USER_INPUT_PHRASE } = require('./config');
+const { Console } = require('@woowacourse/mission-utils');
+const { USER_INPUT_PHRASE, LOTTO_PRICE, GAME_INPUT_ERRORS } = require('./config');
 
-
-const getTicketBudget = (budgetAction) => {
-    Console.readLine(USER_INPUT_PHRASE.REQUEST_PURCHASE_AMOUNT,
-        (budget) => budgetAction(budget));
+const getLottoBudget = (budgetAction) => {
+  Console.readLine(USER_INPUT_PHRASE.REQUEST_PURCHASE_AMOUNT, (budget) => {
+    budgetAction(budget);
+  });
 };
 
 const getTargetNumber = (targetAction) => {
-    Console.readLine(USER_INPUT_PHRASE.REQUEST_TARGET_NUMBER, (targetInput) => {
-        targetAction(targetInput.split(USER_INPUT_PHRASE.REQUEST_TARGET_DELIMITER));
-    });
+  Console.readLine(USER_INPUT_PHRASE.REQUEST_TARGET_NUMBER, (targetInput) => {
+    targetAction(targetInput.split(USER_INPUT_PHRASE.REQUEST_TARGET_DELIMITER));
+  });
 };
 
 const getBonusNumber = (bonusAction) => {
-    Console.readLine(USER_INPUT_PHRASE.REQUEST_BONUS_NUMBER, (bonusInput) => {
-        bonusAction(bonusInput);
-    });
+  Console.readLine(USER_INPUT_PHRASE.REQUEST_BONUS_NUMBER, (bonusInput) => {
+    bonusAction(bonusInput);
+  });
 };
 
-module.exports = { getTicketBudget, getTargetNumber, getBonusNumber };
+const validateLottoBudget = (budget) => {
+  const isNumber = !isNaN(budget);
+  const isPositive = budget > 0;
+  const isMultiple = budget % LOTTO_PRICE === 0;
+
+  if (!isNumber || !isPositive || !isMultiple) {
+    throw Error(GAME_INPUT_ERRORS.INVALID_BUDGET_RANGE);
+  }
+};
+module.exports = { getLottoBudget, getTargetNumber, getBonusNumber };
