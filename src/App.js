0be6c44e8353642b8ff77c292a2f7e29b1bf4758
unsnruu <unsnruu@gmail.com>
2022-11-13 15:13:43 +0900

    feat : 사용자가 요구 사항에 맞는 금액을 입력했는지 확인하는 메서드 구현

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const { Console, Random } = MissionUtils;
 
 class App {
+  validateMoney(money) {
+    const UNIT = 1000;
+    const remains = money % UNIT;
+    if (remains !== 0) {
+      throw new Error(`[ERROR] 1000원 단위로 금액을 입력해주세요.`);
+    }
+  }
   buyLottos() {
-    Console.readLine("구입금액을 입력해 주세요.", (inputs) => {});
+    Console.readLine("구입금액을 입력해 주세요.", (money) => {
+      this.validateMoney(money);
+    });
   }
   play() {}
 }
