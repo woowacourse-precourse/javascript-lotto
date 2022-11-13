3481b46f17c124a970d948a57a94eb2aa49fac7a
HWI <dlwognl99@naver.com>
2022-11-13 12:50:45 +0900

    feat: 상수 추가 1. 숫자만 입력 에러메세지 수정 2. 중복된 숫자 에러 메세지 추가 3. 로또 사이즈 에러 메세지 추가 4. 로또 사이즈 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -4,10 +4,12 @@ const WINNERNUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
 const ERROR_MESSAGE_UNDIVIDED =
   "[ERROR] 구입 금액이 로또 가격으로 나누어 떨어지지 않습니다.";
 
-const ERROR_MESSAGE_NOTONLY_NUMBER =
-  "[ERROR] 구입 금액은 숫자만 입력 가능합니다.";
+const ERROR_MESSAGE_NOTONLY_NUMBER = "[ERROR] 숫자만 입력하실 수 있습니다.";
+const ERROR_MESSAGE_NOTUNIQUE_NUMBER = "[ERROR] 중복된 숫자가 있습니다.";
+const ERROR_MESSAGE_SIZE_INVALID = "[ERROR] 로또의 크기는 6 이어야 합니다.";
 
 const LOTTO_PRICE = 1000;
+const LOTTO_SIZE = 6;
 
 const GUIDE_MESSAGE = {
   PURCHASEAMOUNT_INPUT_MESSAGE,
@@ -16,10 +18,12 @@ const GUIDE_MESSAGE = {
 const ERROR_MESSAGE = {
   ERROR_MESSAGE_UNDIVIDED,
   ERROR_MESSAGE_NOTONLY_NUMBER,
+  ERROR_MESSAGE_NOTUNIQUE_NUMBER,
 };
 
 module.exports = {
   GUIDE_MESSAGE,
   ERROR_MESSAGE,
   LOTTO_PRICE,
+  LOTTO_SIZE,
 };
