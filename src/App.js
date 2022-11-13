147dfcdd4ca34dfa31197e2747d7604b94df0e16
h2002y <h2002y@naver.com>
2022-11-13 12:46:53 +0900

    feat: Add getMoney func for user Input money in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+
 class App {
+  #money;
+
+  constructor() {
+  }
+
   play() {}
+
+  getMoney() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (userInput) => {
+      this.validateMoney(userInput);
+      this.#money = userInput;
+    });
+  }
 }
 
 module.exports = App;
