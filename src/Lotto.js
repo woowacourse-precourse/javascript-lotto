00bfd828ebef7ccf09f95952139c5fde2f4a9cea
hxezin <leehxejin@gmail.com>
2022-11-14 02:17:52 +0900

    feat: 로또 번호 유효성 검사

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,9 +7,18 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
+    const test = new Set(numbers);
+    if (test.size !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    test.forEach((num) => {
+      if (isNaN(num)) {
+        throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+      }
+      if (num < 1 || num > 45) {
+        throw new Error("[ERROR] 로또 번호는 1에서 45사이의 숫자여야 합니다.");
+      }
+    });
   }
 
   // TODO: 추가 기능 구현
