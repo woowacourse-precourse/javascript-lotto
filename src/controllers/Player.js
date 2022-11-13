e85d838b48b51dbfc6c40e77152a7dcb9963ae49
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-11-13 22:58:55 +0900

    feat: 당첨 통계를 출력하는 기능 추가

--- src/controllers/Player.js
+++ src/controllers/Player.js
@@ -21,16 +21,24 @@ class Player {
   }
 
   calculateProfit(result) {
-    const prizeMoney = [2000000000, 30000000, 1500000, 50000, 5000, 0];
+    const totalAmount = Object.values(result).reduce(this.#calculateTotalAmount, 0);
+    const profit = this.#convertIntegerToFloatWithFirstDecimalPlace(totalAmount);
 
-    const totalAmount = Object.values(result).reduce((accumulator, currentValue, currentIndex) => {
-      return accumulator + currentValue * prizeMoney[currentIndex];
-    }, 0);
+    return this.#convertProfitToProfitWithComma(profit);
+  }
 
-    const profit = (totalAmount / this.#data.purchaseAmount) * 100;
-    profit.toFixed(1);
+  #calculateTotalAmount(accumulator, currentValue, currentIndex) {
+    const prizeMoney = [2000000000, 30000000, 1500000, 50000, 5000];
 
-    return profit;
+    return accumulator + currentValue * prizeMoney[currentIndex];
+  }
+
+  #convertIntegerToFloatWithFirstDecimalPlace(totalAmount) {
+    return parseFloat(Math.round((totalAmount / this.#data.purchaseAmount) * 1000) / 10).toFixed(1);
+  }
+
+  #convertProfitToProfitWithComma(profit) {
+    return profit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   }
 }
 
