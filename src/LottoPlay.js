78d7f753b9da9c7af9f32eeb475f43b1ddaeef75
JangHoonSeok <orirori512@naver.com>
2022-11-14 00:11:14 +0900

    ✨ feat: 로또 번호 랜덤 생성 기능 구현

--- src/LottoPlay.js
+++ src/LottoPlay.js
@@ -1,16 +1,24 @@
 const UserInterface = require("./UserInterface.js");
 const CheckError = require("./CheckError.js");
+const Lotto = require("./Lotto.js");
+const { Random } = require("@woowacourse/mission-utils");
 class LottoPlay {
   // Lotto 게임 기능 구현하는 클래스.
   constructor() {}
   play() {
     const purchaseAmount = UserInterface.purchaseRequest(); // 구입 금액 입력.
     CheckError.checkPurchaseAmount(purchaseAmount); //
-    UserInterface.printLottoNumber(this.purchaseLotto(purchaseAmount));
+    const lottoNumber = this.purchaseLotto(purchaseAmount);
+    UserInterface.printLottoNumber(lottoNumber);
   }
 
   purchaseLotto(purchaseAmount) {
     return purchaseAmount / 1000;
+  }
+
+  createOneLotto() {
+    // 한 개의 로또 번호를 생성하는 함수.
+    return Random.pickUniqueNumbersInRange(1, 45, 6);
   }
 }
 
