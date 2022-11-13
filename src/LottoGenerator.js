471248c4d26e62ffc82829ed128d2ee5b55307ce
2yunseong <dbsdltjd3701@naver.com>
2022-11-13 18:06:47 +0900

    refactor(LottoGenerator): 함수 분리 및 상수 변수화
    - sort에 전달하는 콜백 함수 분리
    - 라이브러리 호출에 사용되는 상수 변수화

--- src/LottoGenerator.js
+++ src/LottoGenerator.js
@@ -1,13 +1,25 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const utils = require('./util/util');
 const Lotto = require('./Lotto');
+const {
+  LOTTO_RANGE_MIN,
+  LOTTO_RANGE_MAX,
+  LOTTO_MAX_LENGTH,
+} = require('./util/util');
 
 class LottoGenerator {
   static generate() {
     return new Lotto(
-      MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
-        (a, b) => a - b
-      )
+      MissionUtils.Random.pickUniqueNumbersInRange(
+        LOTTO_RANGE_MIN,
+        LOTTO_RANGE_MAX,
+        LOTTO_MAX_LENGTH
+      ).sort(this.sortLotto)
     );
+  }
+
+  static sortLotto(a, b) {
+    return a - b;
   }
 }
 
