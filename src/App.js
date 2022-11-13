84b49acee76008b4803065cfd4555b8ccc6bf5a6
daal2quf <daal2quf@gmail.com>
2022-11-13 22:21:28 +0900

    feat(app): 로또 구매 개수 계산 기능 구현 및 로또 개수 출력 메서드 추가
    add:
    calculatorLottoCount(): 입력받은 구매 금액에 1000으로 나눠 구매 개수추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 const { Console } = require("@woowacourse/mission-utils");
 const ExceptionCheck = require("./ExceptionCheck");
 const Lotto = require("./Lotto");
+const Printer = require("./Printer");
 
 class App {
   constructor() {
     this.exeptionCheck = new ExceptionCheck;
+    this.print = new Printer();
   }
 
   play() {
@@ -14,8 +16,17 @@ class App {
   requestForLotto() {
     Console.readLine('구매 금액을 1,000단위로 입력해주세요.\n', moneyValue => {
       this.exeptionCheck.userInputMoneyValue(moneyValue);
+      const purchaseLottoCount = this.calculatorLottoCount(moneyValue);
+      this.print.theNumberOfLotto(purchaseLottoCount);
     });
   }
+
+  calculatorLottoCount(moneyValue) {
+    return moneyValue / 1000;
+  }
+
+
+
 }
 
 const app = new App();
