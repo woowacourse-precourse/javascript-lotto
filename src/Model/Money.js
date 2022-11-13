4b423ebf4987837acf2fe6a1a50e87fa6e6e2978
d0422 <rlfehd2013@naver.com>
2022-11-13 17:08:38 +0900

    Feat:돈이 음수인 경우 예외를 발생시키는 기능 추가

--- src/Model/Money.js
+++ src/Model/Money.js
@@ -6,10 +6,16 @@ class Money {
   }
   validate(money) {
     this.checkMoneyDivide(money);
+    this.checkMoneyMinus(money);
   }
   checkMoneyDivide(money) {
     if (money % 1000 != 0) {
       throw new Error('[ERROR] 돈은 1000원단위여야 합니다!');
+    }
+  }
+  checkMoneyMinus(money) {
+    if (money < 0) {
+      throw new Error('[ERROR] 돈은 자연수여야 합니다');
     }
   }
 }
