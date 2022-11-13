d5faf63af3fa5439aeb60a79eee3f2a0abda3a98
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-13 20:59:22 +0900

    test: 구입 입력 금액 예외 테스트 메소드명 변경

--- __tests__/LottoPurchaseAmountTest.js
+++ __tests__/LottoPurchaseAmountTest.js
@@ -6,61 +6,61 @@ describe("로또 구입 금액 입력 테스트", () => {
   test("구입 금액이 숫자가 아니면 예외가 발생한다.", () => {
     const ERROR_MESSAGE = "[ERROR] 숫자만 입력해주세요.";
     expect(() => {
-      testLottoGame.validate(Number('3h5'));
+      testLottoGame.validatePurchaseAmount(Number('3h5'));
     }).toThrow(ERROR_MESSAGE);
 
     expect(() => {
-      testLottoGame.validate(Number('zzz'));
+      testLottoGame.validatePurchaseAmount(Number('zzz'));
     }).toThrow(ERROR_MESSAGE);
 
     expect(() => {
-      testLottoGame.validate(Number('12@'));
+      testLottoGame.validatePurchaseAmount(Number('12@'));
     }).toThrow(ERROR_MESSAGE);
   });
 
   test(`구입 금액이 ${LOTTERY_PRICE}원 미만이면 예외가 발생한다.`, () => {
     const ERROR_MESSAGE = `[ERROR] 최소 입력 금액은 ${LOTTERY_PRICE}원입니다.`;
     expect(() => {
-      testLottoGame.validate(250.7);
+      testLottoGame.validatePurchaseAmount(250.7);
     }).toThrow(ERROR_MESSAGE);
 
     expect(() => {
-      testLottoGame.validate(900);
+      testLottoGame.validatePurchaseAmount(900);
     }).toThrow(ERROR_MESSAGE);
 
     expect(() => {
-      testLottoGame.validate(-1000);
+      testLottoGame.validatePurchaseAmount(-1000);
     }).toThrow(ERROR_MESSAGE);
 
     expect(() => {
-      testLottoGame.validate(0);
+      testLottoGame.validatePurchaseAmount(0);
     }).toThrow(ERROR_MESSAGE);    
   });
 
   test(`구입 금액이 ${LOTTERY_PRICE}로 나누어 떨어지지 않는 경우 예외가 발생한다.`, () => {
     const ERROR_MESSAGE = "[ERROR] 1,000원 단위로 입력해주세요.";
     expect(() => {
-      testLottoGame.validate(9950);
+      testLottoGame.validatePurchaseAmount(9950);
     }).toThrow(ERROR_MESSAGE);
 
     expect(() => {
-      testLottoGame.validate(1200);
+      testLottoGame.validatePurchaseAmount(1200);
     }).toThrow(ERROR_MESSAGE);
 
     expect(() => {
-      testLottoGame.validate(100000009);
+      testLottoGame.validatePurchaseAmount(100000009);
     }).toThrow(ERROR_MESSAGE);
 
     expect(() => {
-      testLottoGame.validate(1000.7);
+      testLottoGame.validatePurchaseAmount(1000.7);
     }).toThrow(ERROR_MESSAGE);
 
     expect(() => {
-      testLottoGame.validate(500000.123);
+      testLottoGame.validatePurchaseAmount(500000.123);
     }).toThrow(ERROR_MESSAGE);
 
     expect(() => {
-      testLottoGame.validate(500000.000123);
+      testLottoGame.validatePurchaseAmount(500000.000123);
     }).toThrow(ERROR_MESSAGE);
   });
 });
