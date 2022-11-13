0dc3a84bf451bdae8b42d6d5d21077a7e1518c07
wlthdud <thdudzi@naver.com>
2022-11-14 03:05:40 +0900

    feat: 당첨번호 입력받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App {
   constructor(){ //변수선언
     this.lottoMoney; //로또 구입금액
     this.lottoAmount; //로또 구입수량
+    this.winArr = []; //당첨번호(배열에 넣음)
   }
 
   play() { //게임시작
@@ -20,6 +21,13 @@ class App {
   getLottoAmount(){ //로또수량 계산하기 
     this.lottoAmount = parseInt(this.lottoMoney/1000);
     MissionUtils.Console.print(this.lottoAmount + '개를 구매했습니다.\n');
+  }
+
+  getWinNumber(){ //당첨번호 입력받기
+    MissionUtils.Console.readline('당첨 번호를 입력해 주세요.\n', (winNum) => {
+      const winStr = winNum.toString(); //숫자->문자열
+      this.winArr = winStr.split(","); //문자열->배열
+    });
   }
 }
 
