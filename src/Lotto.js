58fed3dd52bb2d5f160e10e50995b91e1d981f3e
Tory <88531407+1two13@users.noreply.github.com>
2022-11-13 15:41:09 +0900

    refactor(Lotto): validate 메소드 코드 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -5,20 +5,16 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.#validate(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
-    if (numbers !== undefined) {
-      const NUMBERS_IN_RANGE = numbers.filter((el) => Number(el) >= 1 && Number(el) <= 45);
+  #validate(numbers) {
+    const numbersInRange = numbers.filter((el) => Number(el) >= 1 && Number(el) <= 45);
 
-      if (numbers.length !== 6) throw new Error(Messages.SIX_NUMBERS);
-      if (numbers.length !== new Set(numbers).size) throw new Error(Messages.NOT_DUPLICATE);
-      if (numbers.length !== NUMBERS_IN_RANGE.length) {
-        throw new Error(Messages.NUMBERS_IN_RANGE);
-      }
-    }
+    if (numbers.length !== 6) throw new Error(Messages.SIX_NUMBERS);
+    if (numbers.length !== new Set(numbers).size) throw new Error(Messages.NOT_DUPLICATE);
+    if (numbers.length !== numbersInRange.length) throw new Error(Messages.NUMBERS_IN_RANGE);
   }
 
   toString() {
