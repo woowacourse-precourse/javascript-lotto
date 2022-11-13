9c858218c527e93e0b8404fe10070537185f4bb2
sungjihyun <jhsung23@gmail.com>
2022-11-13 16:47:55 +0900

    feat(Lotto): 로또 번호들을 문자열로 바꾸는 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,6 +9,10 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  get numbers() {
+    return this.#numbers;
+  }
+
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
@@ -23,10 +27,9 @@ class Lotto {
     }
   }
 
-  get numbers() {
-    return this.#numbers;
+  toString() {
+    return `[${this.#numbers.join(", ")}]`;
   }
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
