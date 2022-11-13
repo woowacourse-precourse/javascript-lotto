e10685be8545b204710512e0cafe8c100b76f3e4
dev-redo <mis05041@naver.com>
2022-11-14 01:38:34 +0900

    chore(constants): 매직넘버 추가

--- src/constants/lotto.js
+++ src/constants/lotto.js
@@ -2,8 +2,16 @@ const LOTTO_ERROR_MESSAGE = Object.freeze({
   LENGTH: '로또 번호는 6개여야 합니다.',
   DUPLICATED: '로또 번호에 중복되는 숫자가 없어야 합니다.',
   RANGE: '로또 번호의 숫자 범위는 1~45까지 입니다.',
+  DEFAULT: '로또 번호의 유효성을 검사하는데 예기치 못한 에러가 발생했습니다,',
+});
+
+const BONUS_ERROR_MESSAGE = Object.freeze({
+  DUPLICATED: '보너스 번호는 로또 번호와 중복되는 숫자가 없어야 합니다.',
+  RANGE: '로또 번호의 숫자 범위는 1~45까지 입니다.',
+  DEFAULT: '보너스 번호의 유효성을 검사하는데 예기치 못한 에러가 발생했습니다,',
 });
 
 module.exports = {
   LOTTO_ERROR_MESSAGE,
+  BONUS_ERROR_MESSAGE,
 };
