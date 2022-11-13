b81edb7d5eb45938fa87078e03edb0b4fb048ba3
yesolyo <ahnyelim77@gmail.com>
2022-11-13 20:10:38 +0900

    feat: 구입 금액 입력 기능

---
+++ src/numbers/AmountNumber.js
@@ -0,0 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+class AmountNumber {
+  constructor(purchaseAmout, purchaseNumber) {
+    this.purchaseAmout = purchaseAmout;
+    this.purchaseNumber = purchaseNumber;
+  }
+
+  amount() {
+    MissionUtils.Console.readLine(
+      "구입금액을 입력해 주세요.\n",
+      (amountInput) => {
+        this.purchaseAmout = amountInput;
+      }
+    );
+  }
+}
