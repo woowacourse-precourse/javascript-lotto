d84a419272c3e59577f57f1b1c603feb5d664751
freemoon99 <soloist132@gmail.com>
2022-11-13 23:52:59 +0900

    feat: 생성된 로또 번호를 출력하는 기능

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App {
       const lottoNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
       this.checkSort(lottoNum);
     }
+    this.printLotto(this.lottoArr);
   }
 
   checkSort(lottoNum) {
@@ -40,6 +41,20 @@ class App {
     for (let i = 0; i < tempArr.length; i += 6) {
       this.lottoArr.push(tempArr.slice(i, i + 6));
     }
+  }
+
+  printLotto(lottoArr) {
+    lottoArr.map((arr) => MissionUtils.Console.print(this.toStr(JSON.stringify(arr.flat()))));
+  }
+
+  toStr(arr) {
+    let outPut = '';
+    for (let i = 0; i < arr.length; i += 1) {
+      outPut += arr[i];
+      if (arr[i] === ',') outPut += ' ';
+    }
+    this.outPut = outPut;
+    return this.outPut;
   }
 }
 
