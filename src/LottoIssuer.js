46d5cd594846bc6993deaf7d60425e1a1edad19c
Yebin Kim <kyb4312@gmail.com>
2022-11-14 02:27:59 +0900

    feat(LottoIssuer): implement function to generate lottoes

--- src/LottoIssuer.js
+++ src/LottoIssuer.js
@@ -5,6 +5,16 @@ class LottoIssuer {
     return money / 1000;
   }
 
+  generateLottoes(numberOfLottoes) {
+    const lottoes = [];
+
+    for (let i = 0; i < numberOfLottoes; i++) {
+      lottoes.push(this.generateLottoNumbers());
+    }
+
+    return lottoes;
+  }
+
   generateLottoNumbers() {
     return Random.pickUniqueNumbersInRange(1, 45, 6).sort(this.compareNumbers);
   }
