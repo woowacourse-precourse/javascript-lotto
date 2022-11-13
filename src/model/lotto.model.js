3735c8d69a9d707b436277a2046eded5b9b97467
SeungMiL <tmskqj@naver.com>
2022-11-13 22:33:51 +0900

    feat: 기능 2구입 금액시 예외 처리 완료

--- src/model/lotto.model.js
+++ src/model/lotto.model.js
@@ -1,7 +1,19 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class LottoModel {
-    
+  inputValidCheck(input) {
+    if (/[\d]/g.test(input)) {
+      MissionUtils.Console.close();
+      throw new Error("[ERROR] 로또 번호에 문자가 포함 되있습니다.");
+    }
+
+    if (input % 1000 !== 0) {
+      MissionUtils.Console.close();
+      throw new Error("[ERROR] 1,000원 단위가 아닙니다");
+    }
+
+    return input;
+  }
 }
 
 module.exports = LottoModel;
--- src/view/lotto.view.js
+++ src/view/lotto.view.js
@@ -1,9 +1,14 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const Model = require("../model/lotto.model");
 
 class LottoView {
+  constructor() {
+    this.model = new Model();
+  }
+
   getPayInput() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (input) => {
-      MissionUtils.Console.print(input);
+      MissionUtils.Console.print(this.model.inputValidCheck(input));
     });
   }
 }
