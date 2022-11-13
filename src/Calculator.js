1ad2af72c79f5b8950e117024289d6b990cd1492
wonkyung <mwk0725@gmail.com>
2022-11-13 22:04:43 +0900

    feat: 소수점 둘째 자리 반올림 추가

--- src/Calculator.js
+++ src/Calculator.js
@@ -7,15 +7,15 @@ class LottoEarnings {
   }
 
   getEarnings() {
-    const earnings =
+    const earnings = (
       ((Match.lottoWinnerNumber[0] * LOTTO_WINNING_AMOUNT.FIRST_PRIZE +
         Match.lottoWinnerNumber[1] * LOTTO_WINNING_AMOUNT.SECOND_PRIZE +
         Match.lottoWinnerNumber[2] * LOTTO_WINNING_AMOUNT.THIRD_PRIZE +
         Match.lottoWinnerNumber[3] * LOTTO_WINNING_AMOUNT.FORTH_PRIZE +
         Match.lottoWinnerNumber[4] * LOTTO_WINNING_AMOUNT.FIFTH_PRIZE) /
         this.money) *
-      100;
-
+      100
+    ).toFixed(1);
     return earnings;
   }
 }
