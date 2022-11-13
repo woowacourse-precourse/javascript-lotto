f45542e4407959ff224aa9a5e5e7202e905334ca
HelloHailie <alsltjsals@gmail.com>
2022-11-14 01:17:18 +0900

    feat:로또 당첨번호,보너스번  입력받기

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,18 +11,47 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (
+      numbers[0] === numbers[1] ||
+      numbers[0] === numbers[2] ||
+      numbers[0] === numbers[3] ||
+      numbers[0] === numbers[4] ||
+      numbers[0] === numbers[5] ||
+      numbers[1] === numbers[2] ||
+      numbers[1] === numbers[3] ||
+      numbers[1] === numbers[4] ||
+      numbers[1] === numbers[5] ||
+      numbers[2] === numbers[3] ||
+      numbers[2] === numbers[4] ||
+      numbers[2] === numbers[5] ||
+      numbers[3] === numbers[4] ||
+      numbers[3] === numbers[5]
+    ) {
+      throw new Error("[ERROR] 로또 번호는 중복되서는 안됩니다.");
+    }
+    if (isNaN(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+    }
   }
 
   //당첨 로또 번호
   winLottoNum() {
-    const winLottoNum = [];
-    while (winLottoNum.length < 6) {
-      const lottoNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-      if (!winLottoNum.includes(lottoNum)) {
-        winLottoNum.push(lottoNum);
+    MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.", (userInput) => {
+      validate(userInput);
+    });
+    MissionUtils.Console.print(userInput);
+    return userInput;
+    this.winBonusLottoNum();
+  }
+
+  //당첨 보너스 로또 번호
+  winBonusLottoNum() {
+    MissionUtils.Console.readLine(
+      "보너스 번호를 입력해 주세요.",
+      (userInput) => {
+        MissionUtils.Console.print(userInput);
       }
-    }
-    return winLottoNum;
+    );
   }
 
   userPaid() {
@@ -31,8 +60,23 @@ class Lotto {
         throw new Error();
       }
       MissionUtils.Console.print(`${userInput / 1000}개를 구매했습니다.`);
+      const lottoNum = userInput / 1000;
       this.userLottoNum();
     });
+    return lottoNum;
+  }
+
+  userLottoNum() {
+    const userLottoNum = new Array(lottoNum);
+    while (userLottoNum.length < 6) {
+      const lottoNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      if (!userLottoNum.includes(lottoNum)) {
+        userLottoNum.push(lottoNum);
+      }
+    }
+    MissionUtils.Console.print(userLottoNum);
+    this.winLottoNum();
+    return userLottoNum;
   }
 }
 
