7d91077a45bbeeeadbe0432b955a3ed9606b76e5
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-13 22:42:43 +0900

    feat(enterAmount): 사용자의 구입 금액 입력 함수 추가
    Add
      userEnterAmount : 사용자의 구입 금액을 저장하는 private field 추가
      enterAmount(): userEnterAmount에 입력받은 금액을 저장
      getUserEnterAmount(): class외부에서 userEnterAmount에 접근하기 위한 get함수

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
+
+  #userEnterAmount;
+
+  enterAmount() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (userEnterAmount) => {
+      this.#userEnterAmount = userEnterAmount;
+    });
+  }
+
+  getUserEnterAmount() {
+    return this.#userEnterAmount;
+  }
+
   play() {
-    console.log("정상 작동");
+    // 기능 1 : 사용자의 구입 금액 입력
+    this.enterAmount();
   }
 }
 
