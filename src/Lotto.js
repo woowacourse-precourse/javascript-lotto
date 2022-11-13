5c138d863077ad3f619899ca4de1c27ea7ca45c1
evencoding <evencoding@gmail.com>
2022-11-14 01:08:15 +0900

    refactor(Lotto): 불필요하게 GameTools 클래스로 분리했던 메소드들을 Lotto 클래스에 합침

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,7 +1,6 @@
 const Validator = require('./Validator');
-const GameTools = require('./GameTools');
 const Render = require('./Render');
-const { RANK, LOTTO } = require('./constants');
+const { RANK, LOTTO, PRIZE_MONEY } = require('./constants');
 
 class Lotto {
   #numbers;
@@ -17,9 +16,9 @@ class Lotto {
 
   informStateOfWinning(userLottos, bonusNumber) {
     const winningState = userLottos.reduce((state, lotto) => {
-      const matchingCount = GameTools.getMatchingNumCount(lotto, this.#numbers);
+      const matchingCount = this.getMatchingNumCount(lotto);
       const matchesBonusNum = lotto.includes(Number(bonusNumber));
-      const rank = GameTools.getWinningRanking(matchingCount, matchesBonusNum);
+      const rank = this.getWinningRanking(matchingCount, matchesBonusNum);
       if (rank !== RANK.FAIL) state[rank] += 1;
 
       return state;
@@ -28,13 +27,32 @@ class Lotto {
     this.calcTotalPrize(winningState, userLottos.length);
   }
 
+  getMatchingNumCount(lottoNumbers) {
+    return lottoNumbers.filter((number) => this.#numbers.includes(number))
+      .length;
+  }
+
+  getWinningRanking(matchingCount, matchesBonusNum) {
+    if (matchingCount === LOTTO.LENGTH) return RANK.ONE;
+    if (matchingCount + matchesBonusNum === LOTTO.LENGTH) return RANK.TWO;
+    if (matchingCount === LOTTO.LENGTH - 1) return RANK.THREE;
+    if (matchingCount === LOTTO.LENGTH - 2) return RANK.FOUR;
+    if (matchingCount === LOTTO.LENGTH - 3) return RANK.FIVE;
+
+    return RANK.FAIL;
+  }
+
   calcTotalPrize(winningState, countOfLottos) {
-    const totalPrize = GameTools.calcTotalPrize(winningState);
+    let totalPrize = 0;
+    for (let i = 0; i < LOTTO.NUM_OF_PRIZE; i++) {
+      totalPrize += winningState[i] * PRIZE_MONEY[i];
+    }
+
     this.calcRateOfReturn(winningState, totalPrize, countOfLottos);
   }
 
   calcRateOfReturn(winningState, totalPrize, countOfLottos) {
-    const rateOfReturn = GameTools.calcRateOfReturn(totalPrize, countOfLottos);
+    const rateOfReturn = (totalPrize / (countOfLottos * LOTTO.PRICE)) * 100;
     this.renderGameResult(winningState, rateOfReturn);
   }
 
