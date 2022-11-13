e9794cb36270665095950501922ffe22755e766e
pakxe <pigkill40@naver.com>
2022-11-14 00:14:57 +0900

    chore(lottoTest): Lotto 클래스 생성자 제거에 의해 테스트 코드 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -3,31 +3,39 @@ const { ERROR } = require('../src/utiles/Constant');
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
+    const lotto = new Lotto();
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+      lotto.setWinningNumbers([1, 2, 3, 4, 5, 6, 7]);
     }).toThrow(`${ERROR.PREFIX} ${ERROR.COUNT}`);
   });
 
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
+    const lotto = new Lotto();
+
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
+      lotto.setWinningNumbers([1, 2, 3, 4, 5, 5]);
     }).toThrow(`${ERROR.PREFIX} ${ERROR.DUPLICATION}`);
   });
 
   test('로또 번호에 숫자 외의 요소가 있으면 예외가 발생한다.', () => {
+    const lotto = new Lotto();
+
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 'a']);
+      lotto.setWinningNumbers([1, 2, 3, 4, 5, 'a']);
     }).toThrow(`${ERROR.PREFIX} ${ERROR.NUMBER_ONLY}`);
   });
 
   test('로또 번호의 범위가 주어진 범위를 벗어난다면 예외가 발생한다.', () => {
+    const lotto = new Lotto();
+
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 46]);
+      lotto.setWinningNumbers([1, 2, 3, 4, 5, 46]);
     }).toThrow(`${ERROR.PREFIX} ${ERROR.RANGE}`);
   });
 
   test('로또 번호가 당첨 번호와 몇 개 일치하는지 확인', () => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const lotto = new Lotto();
+    lotto.setWinningNumbers([1, 2, 3, 4, 5, 6]);
     lotto.addBonusNumber(7);
 
     expect(lotto.getMatchCount([1, 2, 3, 4, 8, 10])).toEqual(4);
