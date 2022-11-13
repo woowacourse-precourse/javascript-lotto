b3647417043faa3610c3961b5cdc10c68433473d
금가경 <gageum0@gmail.com>
2022-11-13 21:40:43 +0900

    test(lotto) : 보너스 번호 테스트 코드 수정

--- __tests__/BonusTest.js
+++ __tests__/BonusTest.js
@@ -3,8 +3,7 @@ const Bonus = require('../src/Bonus');
 describe('보너스 클래스 테스트', () => {
   test('숫자 범위가 1~45가 아닙니다.', () => {
     expect(() => {
-      const bonus = new Bonus();
-      bonus.validate('55');
+      new Bonus('55');
     }).toThrow('[ERROR]');
   });
 });
