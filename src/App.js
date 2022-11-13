1f0af234ef8b323d826f895e408eae869cf57280
s0ojin <onepst@hanyang.ac.kr>
2022-11-13 18:07:55 +0900

    feat(App.js) : 로또 구입 금액을 입력받는 getAmount 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const Console = MissionUtils.Console;
+const Lotto = require('./Lotto');
+
 class App {
-  play() {}
+  #amount;
+
+  play() {
+    this.Lotto = new Lotto();
+    this.getAmount();
+  }
+
+  getAmount() {
+    Console.readLine('구입금액을 입력해 주세요.', answer => {
+      if (Lotto.isValidAmount(answer)) {
+        this.#amount = answer;
+      }
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
