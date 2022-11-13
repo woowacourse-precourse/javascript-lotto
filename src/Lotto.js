a3ac0776e6ede02e1c9a068183f6ef6e32047f0b
freemoon99 <soloist132@gmail.com>
2022-11-13 15:50:12 +0900

    feat: 중복확인기능
    로또 번호에 중복된 숫자가 있는지 확인하는 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,15 +4,22 @@ class Lotto {
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
+    this.isDuplicate(numbers);
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  isDuplicate(numbers) {
+    const setArr = new Set(numbers);
+    if (setArr.size < numbers.length) {
+      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.')
+    }
+  }
 }
 
 module.exports = Lotto;
