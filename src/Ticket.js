8fc6f9b3843e0acc3220fe4dbf65e4bf8486de3e
dev-redo <mis05041@naver.com>
2022-11-14 02:18:55 +0900

    feat(Ticket): 예산의 유효성을 검사하고 구매할 수 있는 로또의 개수를 얻는 Ticket 클래스 구현
    - 예산의 예외 처리를 위해 ErrorBoundary 클래스를 상속받는다.
    - validate(budget): 예산(budget)을 인자로 받아 예산의 유효성을 검사한다.
    - getErrorMessage: 예산이 유효성을 통과하지 못할시 띄워줄 에러메세지를 return한다.
    - buyTicket: 구매할 수 있는 로또의 개수를 구한다.

---
+++ src/Ticket.js
@@ -0,0 +1,36 @@
+const ErrorBoundary = require('./error/ErrorBoundary');
+const { TICKET_ERROR_MESSAGE, TICKET_PRICE } = require('./constants/lotto');
+
+const Ticket = class extends ErrorBoundary {
+  #budget;
+
+  constructor(budget) {
+    super();
+    this.#budget = budget;
+    this.setup(this.#budget);
+  }
+
+  validate(budget) {
+    const isBudgetDivided = budget % TICKET_PRICE === 0;
+
+    if (isBudgetDivided === true) {
+      return { status: true };
+    }
+
+    const budgetErrorMessage = this.getErrorMessage(isBudgetDivided);
+    return { status: false, message: budgetErrorMessage };
+  }
+
+  static getErrorMessage(isBudgetDivided) {
+    const { DIVIDED, DEFAULT } = TICKET_ERROR_MESSAGE;
+
+    if (isBudgetDivided === false) return DIVIDED;
+    return DEFAULT;
+  }
+
+  buyTicket() {
+    return this.#budget / TICKET_PRICE;
+  }
+};
+
+module.exports = Ticket;
