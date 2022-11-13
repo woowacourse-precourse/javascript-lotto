d0ad7606a5c19a50ae782ccd70a0b301e9b4b50c
JangHoonSeok <orirori512@naver.com>
2022-11-13 22:27:46 +0900

    ✨ feat(CheckError): 구입 금액 검증하는 함수 구현

---
+++ src/CheckError.js
@@ -0,0 +1,12 @@
+const { LOTTO_PRICE } = require("./constants/gameCondition.js");
+const { ERROR_MESSAGE } = require("./constants/messages.js");
+class checkError {
+  checkPurchaseAmount(purchaseAmount) {
+    // 구입 금액 검증하는 함수
+    if (!isNaN(purchaseAmount)) throw new Error(ERROR_MESSAGE.NOT_NUMBER);
+    if (purchaseAmount % LOTTO_PRICE !== 0)
+      throw new Error(ERROR_MESSAGE.CANT_DIVIDE);
+  }
+}
+
+module.exports = checkError;
