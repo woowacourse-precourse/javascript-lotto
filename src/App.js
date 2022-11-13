eecd21bb52146bbed65e41043c76009987343ff9
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-14 00:57:49 +0900

    feat : 생성자

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { Random } = require("@woowacourse/mission-utils");
+
 class App {
+  constructor() {
+    this.lottos = [];
+    this.answer = [];
+    this.item = 0;
+    this.count = 0;
+    this.bonusNum;
+    this.match = [
+      { collect: 3, reward: "5000" },
+      { collect: 4, reward: "50,000" },
+      { collect: 5, reward: "1,500,000" },
+      { collect: 5, reward: "30,000,000", Bonus: true },
+      { collect: 6, reward: "2,000,000,000" },
+    ];
+  }
+
   play() {}
 }
 
