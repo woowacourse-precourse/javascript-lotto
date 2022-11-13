5f92cc4c345d9e6cf40f81203aaec6e2cba26a92
mjmj0217 <mjmj0217@naver.com>
2022-11-13 22:58:37 +0900

    feat(Lotto): change and store winning numbers

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,14 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class Lotto {
-  #numbers;
+  #numbers; // 당첨번호
 
   constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
+    this.validateNumbers(numbers);
+    this.#numbers = numbers.map((x) => Number(x));
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
-
-  // TODO: 추가 기능 구현
+  validateNumbers(numbers) {}
 }
 
 module.exports = Lotto;
