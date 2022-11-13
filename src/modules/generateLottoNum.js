c821b09c5a9d7d37bc8d54baf309ef7f0fd7cb72
Hyun Su <tlsgustn93@naver.com>
2022-11-13 13:46:50 +0900

    refactor: 불필요한 코드 제거

--- src/modules/generateLottoNum.js
+++ src/modules/generateLottoNum.js
@@ -2,15 +2,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class generateLottoNum {
   createLottoNum() {
-    const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-    return numbers.sort((a, b) => a - b)
+    const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+    return numbers;
   }
 
-  lottoDetail(lottoCost) {
+   lottoDetail(lottoCost) {
     const lottoArr = []
     const lottoCount = lottoCost / 1000
     for(let i=0; i<lottoCount;i++) lottoArr.push(this.createLottoNum())
-
     return [lottoCount, lottoArr]
   }
 }
