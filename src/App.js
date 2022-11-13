b0460f8c0bcefd7f87a40abb965983eb0896c4a8
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-13 20:05:51 +0900

    feat: 구입금액 예외처리

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ class App {
 
   salesLotto() {
     Console.readLine('구입금액을 입력해 주세요.\n', (purchaseAmount) => {
-      this.purchaseAmount = purchaseAmount;
+      this.isValidatePurchaseAmount(purchaseAmount);
     });
+  }
+
+  isValidatePurchaseAmount(purchaseAmount) {
+    const numberRegex = /^[0-9]+$/g;
+
+    if (!purchaseAmount.match(numberRegex)) throw new Error('[ERROR] 숫자가 아닌 값은 입력할 수 없습니다.');
+    if (purchaseAmount % 1000 !== 0) throw new Error('[ERROR] 구입금액은 1000 단위 입니다.');
+    if (purchaseAmount === '0') throw new Error('[ERROR] 최소금액은 1000원입니다.');
+    this.purchaseAmount = purchaseAmount;
   }
 }
 
