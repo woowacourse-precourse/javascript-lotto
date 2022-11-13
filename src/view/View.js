37af50dfddfab03c9b3d4530a0ce82d43c940538
sh981013s <sh981013s@gmail.com>
2022-11-13 11:43:07 +0900

    feat(View): 로또 구입금액 input 받는 메서드 구현

--- src/view/View.js
+++ src/view/View.js
@@ -1,6 +1,15 @@
+const { Console } = require("@woowacourse/mission-utils");
+const messages = require("../constants/messages");
+
 class View {
   constructor(controller) {
     this.controller = controller;
+  }
+
+  getPurchasingAmountFromUser() {
+    Console.readLine(messages.PURCHASING_AMOUNT_MESSAGE, (userInput) => {
+      this.controller.lotto.setLottoNumbers(userInput);
+    });
   }
 }
 
