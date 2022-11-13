d2d03194e9d408251e92c9a610a13229e83cd4ab
bassyu <yrkdgus@gmail.com>
2022-11-13 14:48:22 +0900

    feat(Validator): 로또 숫자 검증 기능 추가

---
+++ src/Validator.js
@@ -0,0 +1,19 @@
+class Validator {
+  static checkNumber(number) {
+    if (isNaN(number)) throw new Error('[ERROR] 숫자를 입력해야 합니다.');
+    if (Number.isInteger(number) === false) throw new Error('[ERROR] 정수를 입력해야 합니다.');
+    if (number < 1 || number > 45) throw new Error('[ERROR] 1~45 범위의 숫자를 입력해야 합니다.');
+  }
+
+  static checkNumbers(numbers) {
+    if (numbers.length !== 6) throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    const set = new Set(numbers);
+    if (set.size !== 6) throw new Error('[ERROR] 로또 번호는 중복이 없어야 합니다.');
+
+    numbers.forEach((number) => {
+      this.checkNumber(number);
+    });
+  }
+}
+
+module.exports = Validator;
