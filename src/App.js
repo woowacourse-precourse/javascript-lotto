7ff86a201c2ddc75f46de628334dc4d39a590663
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-13 23:43:40 +0900

    ✨ [feat]: 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  constructor() {
+    this.purchaseAmout = 0;
+  }
+
   play() {}
+  askPurchaseAmount() {
+    MissionUtils.Console.readLine(
+      "구입금액을 입력해 주세요.",
+      (purchaseAmount) => {
+        this.purchaseAmout = purchaseAmount;
+      }
+    );
+  }
 }
 
 module.exports = App;
