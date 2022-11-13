42dedd4e1abade8120e968cabfdba3e783ff91d6
Yebin Kim <kyb4312@gmail.com>
2022-11-14 02:35:47 +0900

    feat(App): print issued lottoes

--- src/App.js
+++ src/App.js
@@ -16,12 +16,20 @@ class App {
   issueLottoes(money) {
     const lottoIssuer = new LottoIssuer();
     const issuedLottoes = lottoIssuer.issue(money);
+    this.printLottoes(issuedLottoes);
   }
 
   checkMoneyValidity(money) {
     if (money % 1000 !== 0) {
       throw new Error('[ERROR] 구입 금액은 1,000원 단위여야 합니다.');
     }
+  }
+
+  printLottoes(issuedLottoes) {
+    Console.print(`\n${issuedLottoes.length}개를 구매했습니다.`);
+    issuedLottoes.forEach((issuedLotto) => {
+      Console.print(issuedLotto);
+    });
   }
 }
 
