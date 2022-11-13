cd70b2e9fbc6da5cb8776ad18773f16f815ef27f
AnJaeSeong <dktmzh6@gmail.com>
2022-11-14 04:05:41 +0900

    test: test files update

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,39 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+  test("로또 번호의 개수가 6개보다 적으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+  });
+
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("로또 번호가 1~45 범위 밖의 수로 입력되면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 333, 4, 5, 6]);
+    }).toThrow("[ERROR]");
+    expect(() => {
+      new Lotto([-1, 0, 3, 4, 5, 6]);
+    }).toThrow("[ERROR]");
+  });
+});
+
+describe("로또 클래스 테스트 - 보너스 번호" ,() => {
+  test("보너스 번호가 1~45 범위 밖의 수로 입력되면 예외가 발생한다.", () => {
+    expect(() => {
+      Lotto.prototype.setBonusNum(-1);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.", () => {
+    expect(() => {
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      lotto.setBonusNum(1);
+    }).toThrow("[ERROR]");
+  })
 });
