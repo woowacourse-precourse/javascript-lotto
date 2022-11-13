28ba0ed660cb09c2a953e25e09683835392f921b
sujungYu <su_moment@naver.com>
2022-11-13 17:30:40 +0900

    feat(Lotto) : 로또 발행 예외 처리 및 오름차순 정렬

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,25 @@
+// const MissionUtils = require('@woowacourse/mission-utils');
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = this.lottoAscendingOrder(numbers);
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+    return numbers;
   }
 
-  // TODO: 추가 기능 구현
+  lottoAscendingOrder(numbers) {
+    return numbers.sort((a, b) => a - b);
+  }
+  getSortLotto() {
+    return this.#numbers;
+  }
 }
 
 module.exports = Lotto;
