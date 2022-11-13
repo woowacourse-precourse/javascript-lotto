3eca44c817160a27ecef75424593bb3aaad02d4d
jiyujin <yujin1679@naver.com>
2022-11-13 17:32:14 +0900

    feat: 로또 구입금액 입력받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    Console.print('구입금액을 입력해 주세요.');
+    Console.readLine('', (money) => {
+      console.log(money);
+    });
+  }
 }
 
+
+const app = new App();
+app.play();
 module.exports = App;
