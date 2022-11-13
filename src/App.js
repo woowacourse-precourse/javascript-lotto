78b137f40a2f25fe487e3ad39947546273de3fb8
cham-min <hymn9613@gmail.com>
2022-11-13 16:24:22 +0900

    feat: print lottery number

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 const { Console } = require("@woowacourse/mission-utils");
 const CheckValue = require("./CheckValue");
 const Message = require("./Message");
+const NumberGenerator = require("./NumberGenerator");
 
 class App {
+  constructor() {
+    this.checkValue = new CheckValue();
+    this.numberGenerator = new NumberGenerator();
+    this.lottoCount = 0;
+  }
+
   play() {
     this.requestPay();
   }
 
   requestPay() {
     Console.readLine(Message.GAME_START, (pay) => {
-      this.CheckValue.isValidPay(pay);
+      this.checkValue.isValidPay(pay);
       this.countLotto(pay);
+      this.printLotto(this.lottoCount);
     });
   }
 
   countLotto(pay) {
     this.lottoCount = pay / 1000;
     Console.print(`${this.lottoCount}개를 구매했습니다.`);
+  }
+
+  printLotto() {
+    const lottoArr = this.numberGenerator.drawLottery(this.lottoCount);
+    lottoArr.map((lotto) => {
+      Console.print(lotto);
+    });
   }
 }
 
--- src/NumberGenerator.js
+++ src/NumberGenerator.js
@@ -5,6 +5,15 @@ class NumberGenerator {
     const number = Random.pickUniqueNumbersInRange(1, 45, 6);
     return number.sort((a, b) => a - b);
   }
+
+  drawLottery(quantity) {
+    const lottoArr = [];
+    for (let i = 0; i < quantity; i++) {
+      lottoArr.push(this.createNumber());
+    }
+
+    return lottoArr;
+  }
 }
 
 module.exports = NumberGenerator;
