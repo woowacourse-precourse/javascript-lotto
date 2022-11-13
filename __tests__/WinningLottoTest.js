b32da42db74502ca56b03b284bd5d70866cf57c9
2je0 <bulljy@naver.com>
2022-11-13 21:09:12 +0900

    🧪 test: winning lotto 클래스 당첨번호 일치개수 테스트 추가
    winning lotto와 일반 lotto가 있을 때 winningLotto.getNumOfMatchedAndBonus 함수를 통해 일치하는 번호의 개수와 보너스 일치여부를
    반환하는 테스트 작성

--- __tests__/WinningLottoTest.js
+++ __tests__/WinningLottoTest.js
@@ -1,3 +1,4 @@
+const Lotto = require('../src/Lotto');
 const WinningLotto = require('../src/WinningLotto');
 
 describe('당첨로또 클래스 테스트', () => {
@@ -21,5 +22,20 @@ describe('당첨로또 클래스 테스트', () => {
     expect(() => {
       new WinningLotto([1, 2, 3, 4, 5, 6], 2);
     }).toThrow('[ERROR]');
+  });
+  test('로또 번호 일치 개수 테스트1', () => {
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
+    const myLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(winningLotto.getNumOfMatchedAndBonus(myLotto)).toEqual([6, 0]);
+  });
+  test('로또 번호 일치 개수 테스트2', () => {
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
+    const myLotto = new Lotto([7, 8, 9, 10, 11, 12]);
+    expect(winningLotto.getNumOfMatchedAndBonus(myLotto)).toEqual([0, 1]);
+  });
+  test('로또 번호 일치 개수 테스트3', () => {
+    const winningLotto = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
+    const myLotto = new Lotto([1, 2, 3, 5, 6, 7]);
+    expect(winningLotto.getNumOfMatchedAndBonus(myLotto)).toEqual([5, 1]);
   });
 });
