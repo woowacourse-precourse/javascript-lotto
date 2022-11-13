b4e302e8bdf7ef716560e61f6b674fcd3cbdfa66
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-11-13 18:39:58 +0900

    Test(feature No.5): add code to check calculate results
    using toEqual compare inputs, outputs of calculate methods

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -31,7 +31,7 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR] 금액은 1000의 배수인 숫자이여야 합니다.");
   });
 
-  test.only("입력받은 금액/로또가격 만큼의 배열이 반환된다", () => {
+  test("입력받은 금액/로또가격 만큼의 배열이 반환된다", () => {
     // input
     const money = 3000;
     const price = 1000;
@@ -70,6 +70,7 @@ describe("로또 클래스 테스트", () => {
     const winNums = [1, 2, 3, 4, 5, 6];
     const bonusNum = 7;
     const lottoNumaArr = [
+      [1, 2, 3, 4, 5, 7],
       [1, 2, 4, 5, 7, 8],
       [5, 6, 7, 8, 9, 10],
     ];
@@ -77,32 +78,96 @@ describe("로또 클래스 테스트", () => {
     const output = new Lotto(winNums, bonusNum, lottoNumaArr).filterMatchNum();
 
     expect(output).toEqual([
+      [1, 2, 3, 4, 5, 7],
       [1, 2, 4, 5, 7],
       [5, 6, 7],
     ]);
   });
 
-  test("보너스 번호가 당첨번호와 중복하면 예외가 발생한다", () => {
-    expect(() => {
-      new Lotto.checkBonusNum(1, [1, 2, 3, 4, 5, 6]);
-    }).toThrow(
-      "[ERROR] 보너스번호는 1~45사이의 당첨번호와 중복되지않는 숫자이어야합니다"
-    );
+  test.only("2등 번호를 제외한 번호들에게서 보너스번호를 삭제한다", () => {
+    // input
+    const winNums = [1, 2, 3, 4, 5, 6];
+    const bonusNum = 7;
+    const lottoNumaArr = [
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 4, 5, 7, 8],
+      [5, 6, 7, 8, 9, 10],
+    ];
+    const bonusAndWinMatchNum = [
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 4, 5, 7],
+      [5, 6, 7],
+    ];
+
+    //output
+    const output = new Lotto(
+      winNums,
+      bonusNum,
+      lottoNumaArr
+    ).deleteBonusNumExceptSecondLottery(bonusAndWinMatchNum);
+
+    expect(output).toEqual([
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 4, 5],
+      [5, 6],
+    ]);
   });
 
-  test("보너스 번호가 당첨번호와 중복하면 예외가 발생한다", () => {
-    expect(() => {
-      new Lotto.checkBonusNum(1, [1, 2, 3, 4, 5, 6]);
-    }).toThrow(
-      "[ERROR] 보너스번호는 1~45사이의 당첨번호와 중복되지않는 숫자이어야합니다"
+  test.only("정해진 길이의 로또 번호들만 출력", () => {
+    // input
+    const winNums = [1, 2, 3, 4, 5, 6];
+    const bonusNum = 7;
+    const lottoNumaArr = [
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 4, 5, 7, 8],
+      [5, 6, 7, 8, 9, 10],
+    ];
+    const onlyMatchNum = [
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 4, 5],
+      [5, 6],
+    ];
+    const matchCount = "FOURTH_LOTTERY";
+
+    //output
+    const output = new Lotto(winNums, bonusNum, lottoNumaArr).classifyLottery(
+      onlyMatchNum,
+      matchCount
     );
+
+    expect(output).toEqual([[1, 2, 4, 5]]);
   });
 
-  test("보너스 번호가 당첨번호와 중복하면 예외가 발생한다", () => {
-    expect(() => {
-      new Lotto.checkBonusNum(1, [1, 2, 3, 4, 5, 6]);
-    }).toThrow(
-      "[ERROR] 보너스번호는 1~45사이의 당첨번호와 중복되지않는 숫자이어야합니다"
-    );
+  test.only("6개 길이의 로또들에서 보너스 번호 포함 여부를 구분하여 객체로 출력", () => {
+    // input
+    const winNums = [1, 2, 3, 4, 5, 6];
+    const bonusNum = 7;
+    const lottoNumaArr = [
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 4, 5, 7, 8],
+      [5, 6, 7, 8, 9, 10],
+      [1, 2, 3, 4, 5, 6],
+    ];
+    const resultLottery = {
+      FIFTH_LOTTERY: [],
+      FOURTH_LOTTERY: [[1, 2, 4, 5]],
+      THIRD_LOTTERY: [],
+      FIRST_OR_SECOND_LOTTERY: [
+        [1, 2, 3, 4, 5, 6],
+        [1, 2, 3, 4, 5, 7],
+      ],
+    };
+
+    //output
+    const output = new Lotto(
+      winNums,
+      bonusNum,
+      lottoNumaArr
+    ).splitFirstandSecondLottery(resultLottery);
+
+    expect(output).toEqual({
+      SECOND_LOTTERY: [[1, 2, 3, 4, 5, 7]],
+      FIRST_LOTTERY: [[1, 2, 3, 4, 5, 6]],
+    });
   });
 });
