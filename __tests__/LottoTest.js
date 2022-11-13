78c49c1fa4c1efff351b13a3200a175bc61fc17a
chee9835 <huisu121@gmail.com>
2022-11-14 00:08:27 +0900

    test: 입력값이 1-45 사이의 숫자인지

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,23 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또 번호에 1부터 45 사이가 아닌 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([0, 2, 3, 4, 5, 6]);
+    }).toThrow("[ERROR]");
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호에 1부터 45 사이가 아닌 숫자가 있으면 예외가 발생한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(() => {
+      lotto.setBonusNumber(0);
+    }).toThrow("[ERROR]");
+    expect(() => {
+      lotto.setBonusNumber(46);
+    }).toThrow("[ERROR]");
+  });
 });
