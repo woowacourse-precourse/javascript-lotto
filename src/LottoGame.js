6ac18a6c6faeae2ba69539db4a4a8d7b3dd895ac
HBSPS <wiyeong79@naver.com>
2022-11-13 21:55:55 +0900

    feat: 로또 번호와 당첨번호를 비교하여 일치 갯수를 반환하는 기능 추가
    LottoGame class
    - 로또 게임과 관련된 메소드들을 담는다

---
+++ src/LottoGame.js
@@ -0,0 +1,21 @@
+class LottoGame {
+  constructor(lottoNumber, winningNumber, bonusNumber) {
+    this.lottoNumber = lottoNumber;
+    this.winningNumber = winningNumber;
+    this.bonusNumber = bonusNumber;
+  }
+
+  matchWinningNumber() {
+    const winningNumberSet = new Set(this.winningNumber);
+    let count = 0;
+
+    for (let i = 0; i < this.lottoNumber.length; i++) {
+      if (winningNumberSet.has(this.lottoNumber[i])) {
+        count += 1;
+      }
+    }
+    return count;
+  }
+}
+
+module.exports = LottoGame;
