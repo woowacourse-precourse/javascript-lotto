2d261296f7ca200707e6bfe0cf43cd9e5ea46782
Doyoung.Kim <dyflower@nate.com>
2022-11-13 12:31:43 +0900

    Feat : 구입 금액 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const Lotto = require('../src/Lotto');
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
+  constructor() {
+    this.cost = 0;
+  }
   play() {}
+  inputMoney() {
+    MissionUtils.Console.readLine('구입금액을 입력해주세요.\n', (cost) => {
+      if (cost % 1000 !== 0) {
+        throw new Error('[ERROR] 1,000원 단위 금액을 입력해주세요.');
+      }
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
