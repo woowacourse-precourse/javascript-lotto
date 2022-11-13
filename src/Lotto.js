0da563382c07ab6e10e78aa466abd2cbc2066b38
h2002y <h2002y@naver.com>
2022-11-13 22:40:25 +0900

    feat: Add checkLottoIsInt func for input lotto int exception in Lotto.js

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,7 +1,8 @@
 const ERROR_LOTTO_SIZE_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
+const ERROR_LOTTO_SEPARATE_SIZE_MESSAGE = "[ERROR] 십진수가 아닌 숫자입니다.";
+const ERROR_LOTTO_IS_INT_MESSAGE = "[ERROR] 정수가 아닌 숫자입니다.";
 const ERROR_LOTTO_OVERLAP_MESSAGE = "[ERROR] 로또 번호는 중복되는 숫자가 없어야 합니다.";
 const ERROR_LOTTO_VALUE_MESSAGE = "[ERROR] 로또 번호는 1 ~ 45 사이의 숫자입니다.";
-const ERROR_LOTTO_SEPARATE_SIZE_MESSAGE = "[ERROR] 십진수가 아닌 숫자입니다.";
 
 class Lotto {
   #numbers;
@@ -35,6 +36,14 @@ class Lotto {
     for(let i = 0; i < numbers.length; i++) {
       if(numbers[i].length !== 1) {
         throw new Error(ERROR_LOTTO_SEPARATE_SIZE_MESSAGE);
+      }
+    }
+  }
+
+  checkLottoIsInt(numbers) {
+    for(let i = 0; i < numbers.length; i++) {
+      if(Number(numbers[i]) % 1 !== 0) {
+        throw new Error(ERROR_LOTTO_IS_INT_MESSAGE);
       }
     }
   }
