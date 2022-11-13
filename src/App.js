ec23d4c3a2eb0b0b5a1838a18ecd1c0fc3de4a51
unsnruu <unsnruu@gmail.com>
2022-11-13 15:20:58 +0900

    feat : 입력한 금액에 대한 수만큼 로또 번호를 생성하는 메서드 구현

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const { Console, Random } = MissionUtils;
 
 class App {
+  unit;
+  lottos;
+
+  constructor() {
+    this.unit = 1000;
+    this.lottos = [];
+  }
+  createLottos(money) {
+    const lottoNum = money / this.unit;
+
+    for (let idx = 0; idx < lottoNum; idx++) {
+      const newLottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      const newLotto = newLotto(newLottoNumbers);
+      this.lottos(newLotto);
+    }
+  }
   validateMoney(money) {
-    const UNIT = 1000;
-    const remains = money % UNIT;
+    const remains = money % this.unit;
     if (remains !== 0) {
       throw new Error(`[ERROR] 1000원 단위로 금액을 입력해주세요.`);
     }
@@ -12,6 +27,7 @@ class App {
   buyLottos() {
     Console.readLine("구입금액을 입력해 주세요.", (money) => {
       this.validateMoney(money);
+      this.createLottos(money);
     });
   }
   play() {}
