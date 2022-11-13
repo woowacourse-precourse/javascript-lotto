190ef308b67425fd6301c7376e8ef9233db60681
JangHoonSeok <orirori512@naver.com>
2022-11-13 23:45:38 +0900

    ✨ feat(constants): Print할 메세지 constant 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -8,4 +8,7 @@ const ERROR_MESSAGE = Object.freeze({
   NOT_NUMBER: `[ERROR] 입력값이 숫자여야 합니다.`,
   UNDER_BASIC_PRICE: `[ERROR] 구입 금액이 1000 이상이어야 합니다.`,
 });
-module.exports = { INPUT_MESSAGE, ERROR_MESSAGE };
+const PRINT_MESSAGE = Object.freeze({
+  PURCHASE_MESSAGE: (num) => `${num}개를 구매했습니다.`,
+});
+module.exports = { INPUT_MESSAGE, ERROR_MESSAGE, PRINT_MESSAGE };
