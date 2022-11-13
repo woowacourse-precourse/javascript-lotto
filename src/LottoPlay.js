141a42b7f930193b1868607aa71d19c0ce8dc28c
JangHoonSeok <orirori512@naver.com>
2022-11-13 23:06:35 +0900

    ✨ feat: 구입금액을 통해 로또 개수를 구하는 기능 구현.

--- src/LottoPlay.js
+++ src/LottoPlay.js
@@ -1,5 +1,16 @@
+const UserInterface = require("./src/UserInterface.js");
+const CheckError = require("./src/CheckError.js");
 class LottoPlay {
-  play() {}
+  // Lotto 게임 기능 구현하는 클래스.
+  play() {
+    const purchaseAmount = UserInterface.purchaseRequest(); // 구입 금액 입력.
+    CheckError.checkPurchaseAmount(purchaseAmount); //
+    this.purchaseLotto(purchaseAmount);
+  }
+
+  purchaseLotto(purchaseAmount) {
+    return purchaseAmount / 1000;
+  }
 }
 
 module.exports = LottoPlay;
