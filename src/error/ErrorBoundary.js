125b62d261d84b4b43079173e1ca56d442785c8c
dev-redo <mis05041@naver.com>
2022-11-14 01:26:11 +0900

    feat(ErrorBoundary): ErrorBoundary 추상 클래스 메소드 정의
    - setup(value): Errorboundary를 상속받은 클래스에서 유효성 검사 콜백함수를 통해 유효성을 통과하지 못할 시 예외 처리하는 메소드
    - validate(value): Errorboundary를 상속받은 클래스의 유효성 검사 메소드
    - getErrorMessage(): 유효성을 통과하지 못한 경우 띄워줄 에러 메세지를 return하는 메소드

--- src/error/ErrorBoundary.js
+++ src/error/ErrorBoundary.js
@@ -4,6 +4,15 @@ const ErrorBoundary = class {
   constructor() {
     this.errorLogger = new ErrorLogger();
   }
+
+  setup(value) {
+    const validateValueCallback = () => this.validate(value);
+    this.errorLogger.onCallback(validateValueCallback);
+  }
+
+  validate(value) {}
+
+  static getErrorMessage() {}
 };
 
 module.exports = ErrorBoundary;
