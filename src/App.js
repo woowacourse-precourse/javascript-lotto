bfc0be8529c47e10440292cf4a7b26b1f79563b8
wlthdud <thdudzi@naver.com>
2022-11-14 02:59:30 +0900

    feat: 로또수량 계산하기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   constructor(){ //변수선언
-    this.lottoMoney;
+    this.lottoMoney; //로또 구입금액
+    this.lottoAmount; //로또 구입수량
   }
 
   play() { //게임시작
-
+    this.getMoney();
+    this.getLottoAmount();
   }
 
   getMoney(){ //구매금액 입력받기
@@ -14,8 +17,9 @@ class App {
     });
   }
 
-  getLottoAmount(){ //로또수량 계산하기
-    
+  getLottoAmount(){ //로또수량 계산하기 
+    this.lottoAmount = parseInt(this.lottoMoney/1000);
+    MissionUtils.Console.print(this.lottoAmount + '개를 구매했습니다.\n');
   }
 }
 
