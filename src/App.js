a077e506839f4e119b37a4f7b569ad52a0b67d2c
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-14 00:13:57 +0900

    refactor(util/exception): 예외처리 함수들을 묶어 파일 분리
    Add
      exception.js: 사용자의 잘못된 입력에 대한 예외처리 함수들을 다른 파일로 분리
    
    Change
      App.js파일에서 예외처리 함수 코드 삭제

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const {isInDivisible, isNotNumber} = require("./utils/exception");
 class App {
 
   #userEnterAmount;
 
-  isNotNumber(userEnterAmount) {
-    if(isNaN(userEnterAmount) || /\s/g.test(userEnterAmount)) {
-      throw new Error("[ERROR] 로또 구입 금액은 공백이 포함되지 않은 숫자 형태로 입력해야 합니다.")
-    }
-  }
-
-  isInDivisible(userEnterAmount) {
-    if(Number(userEnterAmount) % 1000 !== 0) {
-      throw new Error('[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다.')
-    }
-  }
-
   userEnterException(userEnterAmount) {
-    this.isInDivisible(userEnterAmount);
-    this.isNotNumber(userEnterAmount);
+    isInDivisible(userEnterAmount);
+    isNotNumber(userEnterAmount);
   }
 
   enterAmount() {
---
+++ src/utils/exception.js
@@ -0,0 +1,13 @@
+const isNotNumber = (userEnterAmount) => {
+  if(isNaN(userEnterAmount) || /\s/g.test(userEnterAmount)) {
+    throw new Error("[ERROR] 로또 구입 금액은 공백이 포함되지 않은 숫자 형태로 입력해야 합니다.")
+  }
+}
+
+const isInDivisible = (userEnterAmount) => {
+  if(Number(userEnterAmount) % 1000 !== 0) {
+    throw new Error('[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다.')
+  }
+}
+
+module.exports = {isNotNumber, isInDivisible};
