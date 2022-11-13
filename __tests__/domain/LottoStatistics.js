95d14d3e9ee9057ff275f5e9369a578ca99089a5
yong <tkddbs1644@gmail.com>
2022-11-13 22:00:33 +0900

    test(LottoStatistics): 등수를 알려주는 기능 테스트 코드

--- __tests__/domain/LottoStatistics.js
+++ __tests__/domain/LottoStatistics.js
@@ -38,4 +38,40 @@ describe("로또 통계 클래스 테스트", () => {
       expect(lottoStatistics.match(numbers)).toEqual(result);
     });
   });
+
+  test("등수를 알려준다.", () => {
+    const winningLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    winningLotto.addBonusNumber(7);
+    const lottoStatistics = new LottoStatistics(winningLotto);
+    const TEST_CASE = [
+      {
+        numbers: [1, 2, 3, 4, 5, 6],
+        result: 1,
+      },
+      {
+        numbers: [1, 3, 4, 5, 6, 7],
+        result: 2,
+      },
+      {
+        numbers: [1, 3, 4, 5, 6, 8],
+        result: 3,
+      },
+      {
+        numbers: [1, 2, 3, 15, 16, 18],
+        result: 5,
+      },
+      {
+        numbers: [1, 2, 13, 14, 15, 16],
+        result: -1,
+      },
+      {
+        numbers: [11, 12, 13, 14, 15, 16],
+        result: -1,
+      },
+    ];
+
+    TEST_CASE.forEach(({ numbers, result }) => {
+      expect(lottoStatistics.judgeRank(numbers)).toEqual(result);
+    });
+  });
 });
