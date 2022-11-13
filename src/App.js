e9479703bd59f8ff38d7024536aefea421524394
cruelladevil <dev.timetravel@gmail.com>
2022-11-13 19:00:13 +0900

    feat(App): player buy lottos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 const { Console } = require('@woowacourse/mission-utils');
+const Player = require('./Player');
 
 class App {
+  constructor() {
+    this.player = new Player();
+  }
+
   play() {
     this.askForPayment();
   }
 
   askForPayment() {
     Console.print('구입금액을 입력해 주세요.');
-    Console.readLine('', (input) => {});
+    Console.readLine('', (input) => {
+      const money = Number(input);
+      this.player.buyLottos(money);
+    });
   }
 }
 
