40a045df428d4216547ea64949c393b53df479ee
ShimSungBo <shimdb96@gmail.com>
2022-11-14 00:42:46 +0900

    feat: 로또의 순위를 리턴하는 함수 생성 1.(numberToString) 프라이빗필드 문자열로 변경함수 생성 2.(winningCount) 당첨번호를 받아 랜덤번호와 일치하는 숫자의 개수를 리턴하는 함수 3.(rank) 위에서 만들어진 개수에 따라 순위를 리턴 하는 함수

--- src/Lotto.js
+++ src/Lotto.js
@@ -14,7 +14,37 @@ class Lotto {
     }
 
     if (new Set(numbers).size !== 6) {
-      throw new Error("[ERROR 로또 번호는 중복되지 않아야 합니다.]");
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
+
+    if (!/\d/.test(numbers)) {
+      throw new Error("[ERROR] 숫자만 입력해주시기 바랍니다.");
+    }
+  }
+
+  numberToString() {
+    return `[${this.#numbers.join(", ")}]`;
+  }
+
+  winningCount(winNumber) {
+    let winCount = this.#numbers.filter((count) => {
+      return winNumber.includes(count);
+    });
+
+    return winCount.length;
+  }
+
+  rank(winNumber, bonusNumber) {
+    const winCount = this.winningCount(winNumber);
+    switch (winCount) {
+      case 3:
+        return 5;
+      case 4:
+        return 4;
+      case 5:
+        return this.#numbers.includes(bonusNumber) ? 2 : 3;
+      case 6:
+        return 1;
     }
   }
 
