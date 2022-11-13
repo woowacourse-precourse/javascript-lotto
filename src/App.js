c820ebdc1c089e458c1b0664d49646b6de7a6eb2
h2002y <h2002y@naver.com>
2022-11-13 15:47:03 +0900

    feat: Add validateMoney func for user input money exception handle in App.js

--- src/App.js
+++ src/App.js
@@ -12,8 +12,23 @@ class App {
   getMoney() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (userInput) => {
       this.validateMoney(userInput);
-      this.#money = userInput;
+      this.#money = Number(userInput);
     });
+  }
+
+  validateMoney(money) {
+    if(isNaN(money) || money === "" || money === " "){
+      throw new Error("[ERROR] 숫자를 입력해주세요.");
+    }
+    if(Number(money) % 1 !== 0){
+      throw new Error("[ERROR] 실수가 아닌 금액을 입력해주세요.")
+    }
+    if(Number(money) % 1000 !== 0){
+      throw new Error("[ERROR] 천원 단위의 금액으로 입력해주세요.");
+    }
+    if(Number(money) < 1000){
+      throw new Error("[ERROR] 천원 이상의 금액을 입력해주세요.");
+    }
   }
 }
 
