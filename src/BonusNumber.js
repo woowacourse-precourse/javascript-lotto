96d0356e7d4fa94da12da4a5b8ee4caafb3768f6
Rki0 <pky00823@gmail.com>
2022-11-13 23:56:17 +0900

    feat(BonusNumber): add exception handling function
    Throw an Error if the input starts with 0.

--- src/BonusNumber.js
+++ src/BonusNumber.js
@@ -1,3 +1,5 @@
+const ERROR_DONT_START_ZERO =
+  "[ERROR] 0으로 시작하는 숫자는 입력할 수 없습니다.";
 const ERROR_NOT_ONLY_NUMBER = "[ERROR] 숫자만이 입력 가능합니다.";
 const ERROR_OUT_OF_RANGE =
   "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
@@ -7,11 +9,18 @@ const ERROR_DUPLICATE_EXIST =
 class BonusNumber {
   constructor(lotto, bonus) {
     this.userLotto = lotto;
+    this.checkBonusNumberStartZero(bonus);
     this.checkOnlyNumber(bonus);
 
     const bonusTypeofNumber = Number(bonus);
     this.checkNumberRanges(bonusTypeofNumber);
     this.checkDuplicateWithWinningNumber(bonusTypeofNumber);
+  }
+
+  checkBonusNumberStartZero(bonus) {
+    if (bonus[0] === "0") {
+      throw new Error(ERROR_DONT_START_ZERO);
+    }
   }
 
   checkOnlyNumber(bonus) {
