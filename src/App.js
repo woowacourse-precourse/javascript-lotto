eeee716347e7e2e871bae215b55eed95f79f3f41
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-13 23:14:09 +0900

    feat(isInDivisible): 사용자의 구입 금액이 1000원으로 나누어 떨어지지 않는 경우 예외처리 구현
    Add
      isInDivisible(userEnterAmount): 로또 구입금액의 예외 처리 중 1000원으로 나누어 떨어지지 않을 경우 예외 처리
      userEnterException(userEnterAmount): 로또 구입 금액의 전체적인 예외처리를 진행하는 함수

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ class App {
 
   #userEnterAmount;
 
+  isInDivisible(userEnterAmount) {
+    if(Number(userEnterAmount) % 1000 !== 0) {
+      throw new Error('[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다.')
+    }
+  }
+
+  userEnterException(userEnterAmount) {
+    this.isInDivisible(userEnterAmount);
+  }
+
   enterAmount() {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (userEnterAmount) => {
+      this.userEnterException(userEnterAmount);
       this.#userEnterAmount = userEnterAmount;
     });
   }
