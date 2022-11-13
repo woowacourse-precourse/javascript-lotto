c454927f97dedb51ab760c265ba70b25c9ae7a6c
dhkham <khamdong@gmail.com>
2022-11-13 15:53:24 +0900

    feat(Lotto.js): add method that checks if lottery numbers are in between 1-45

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,6 +4,7 @@ class Lotto {
   constructor(numbers) {
     this.validateLength(numbers);
     this.validateNoDuplicates(numbers);
+    this.validateNumBetween1To45(numbers);
     this.#numbers = numbers;
   }
 
@@ -17,6 +18,14 @@ class Lotto {
     if (new Set(numbers).size !== numbers.length) {
       throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
     }
+  }
+
+  validateNumBetween1To45(numbers) {
+    numbers.forEach((num) => {
+      if (num < 1 || num > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    });
   }
 
 
