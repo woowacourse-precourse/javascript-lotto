b3a9d73a62fed17f34dcdf70b8e6b8e1b3f90066
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-13 22:09:11 +0900

    feat: 로또 게임 상수 저장

---
+++ src/constant/index.js
@@ -0,0 +1,16 @@
+const Constants = Object.freeze({
+  MAX: 45,
+  MIN: 1,
+  NUMBERS_LENGTH: 6,
+  BONUS_LENGTH: 1,
+  PRICE: 1000,
+  RANK_REWARD: {
+    [6]: 2000000000,
+    Bonus: 30000000,
+    [5]: 1500000,
+    [4]: 50000,
+    [3]: 5000,
+  },
+});
+
+module.exports = Constants;
