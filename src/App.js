e86ec16c96bc829cb0ca4c8da9cb4ec7943b685f
wonseok <rhdqngkrh77@khu.ac.kr>
2022-11-13 17:00:16 +0900

    fix: 예외 수정

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ class App {
     this.buyCost = 0;
     this.eachLotto = 0;
     this.lottoList = [];
-    this.winningLotto = [];
+    this.winningLotto;
     this.bonusLottoNum = 0;
     this.countEachWinningCost = [0, 0, 0, 0, 0]; // 3개, 4개, 5개, 5개+보너스, 6개순
   }
 
   inputCost() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (cost) => {
+      var tempCost = parseInt(cost);
+      if (String(tempCost) !== String(cost)) {
+        throw new Error("[ERROR] 로또 구입금액에 문자열이 포함되면 안됩니다.");
+      }
       if (cost / 1000 < 1) {
         throw new Error("[ERROR] 로또 구입금액이 부족합니다.");
       }
@@ -28,14 +32,22 @@ class App {
     for (var i = 0; i < this.eachLotto; i++) {
       var selectLotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
       var selectedLotto = new Lotto(selectLotto);
-      this.lottoList.push(selectedLotto);
+      this.lottoList.push(selectedLotto.getNumbers());
     }
   }
 
   printLottoInfo() {
-    MissionUtils.Console.print(`\n${this.eachLotto}개를 구입했습니다.`);
-    for (var lotto in this.lottoList) {
-      MissionUtils.Console.print(lotto);
+    MissionUtils.Console.print(`${this.eachLotto}개를 구매했습니다.`);
+    for (var i = 0; i < this.lottoList.length; i++) {
+      var lottoText = "[";
+      var lottoText2 = "";
+      for (var j = 0; j < this.lottoList[i].length; j++) {
+        lottoText += this.lottoList[i][j];
+        lottoText += ", ";
+      }
+      lottoText2 += lottoText.slice(0, lottoText.length - 2);
+      lottoText2 += "]";
+      MissionUtils.Console.print(lottoText2);
     }
   }
 
@@ -43,9 +55,9 @@ class App {
     MissionUtils.Console.readLine(
       "\n당첨 번호를 입력해 주세요.",
       (winLotto) => {
-        var tempWinLotto = winLotto.split(",");
+        var tempWinLotto = winLotto.split(",").map((i) => Number(i));
         var winLottoArr = new Lotto(tempWinLotto);
-        this.winningLotto.push(...winLottoArr);
+        this.winningLotto = winLottoArr.getNumbers();
       }
     );
   }
@@ -54,7 +66,7 @@ class App {
     MissionUtils.Console.readLine(
       "\n보너스 번호를 입력해 주세요.",
       (bonusNum) => {
-        if (bonusNum < 1 || bonusNum > 45)
+        if (bonusNum < "1" && bonusNum > "45")
           throw new Error(
             "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
           );
@@ -66,18 +78,18 @@ class App {
   }
 
   matchWinningLotto() {
-    for (var lotto in this.lottoList) {
+    for (var i = 0; i < this.lottoList.length; i++) {
       this.countWinningCost(
-        this.countMatchLotto(this.winningLotto, lotto),
-        lotto
+        this.countMatchLotto(this.winningLotto, this.lottoList[i]),
+        this.lottoList[i]
       );
     }
   }
 
   countMatchLotto(winLotto, anyLotto) {
     var combineLotto = anyLotto.concat(winLotto);
-    var lottoSet = set(combineLotto);
-    var countMatch = combineLotto.length - lottoSet.length;
+    var lottoSet = new Set(combineLotto);
+    var countMatch = combineLotto.length - lottoSet.size;
     return countMatch;
   }
 
@@ -105,7 +117,8 @@ class App {
   }
 
   printLottoResult() {
-    MissionUtils.Console.print("\n당첨 통계\n---");
+    MissionUtils.Console.print("당첨 통계");
+    MissionUtils.Console.print("---");
     MissionUtils.Console.print(
       `3개 일치 (5,000원) - ${this.countEachWinningCost[0]}개`
     );
@@ -136,7 +149,7 @@ class App {
 
   printRevenue() {
     var revenue = this.calculateRevenue();
-    MissionUtils.Console.print(`총 수익률은 ${revenue}% 입니다.`);
+    MissionUtils.Console.print(`총 수익률은 ${revenue}%입니다.`);
     MissionUtils.Console.close();
   }
 
