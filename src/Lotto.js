73070724ae48305c7962c02abd4ae94c3ae4c4ed
ghenmaru <rbgks1937@naver.com>
2022-11-13 16:14:18 +0900

    feat: 로또 수량만큼 로또 번호 만들기

--- src/Lotto.js
+++ src/Lotto.js
@@ -15,4 +15,16 @@ class Lotto {
   // TODO: 추가 기능 구현
 }
 
+const getLottoNumber = (count) => {
+  const numbers = [];
+  for(let i = 0; i < count; i++) {
+    numbers.push(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6));
+  }
+  // MissionUtils.Console.print(numbers);   
+  return numbers;
+}
+
+const MissionUtils = require("@woowacourse/mission-utils");
+getLottoNumber(3);
+// const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
 module.exports = Lotto;
