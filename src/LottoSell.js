899c43403525ff319098809a070987fdfcbb82b2
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-13 23:52:06 +0900

    refactor(LottoSell): Lotto 구매 기능 분리

---
+++ src/LottoSell.js
@@ -0,0 +1,29 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Error = require("./ErrorMessage.js") 
+const Notice = require("./NoticeMessage.js")
+const Lotto = require("./Lotto.js")
+class LottoSell{
+    constructor(){
+        this.Lotto = new Lotto()
+        this.Lottobuynumber = [];
+    }
+    Inputmoney(money){
+        this.validateMoney(money)
+        this.makeRandomnumber(money)
+    }
+    validateMoney(money){
+        if (money % 1000 != 0) {
+            throw Error.UNIT_ERROR;
+        }
+    }
+    makeRandomnumber(money){
+        const LottoCount = money / 1000;
+        MissionUtils.Console.print(LottoCount + Notice.BUY_LOTTO)
+        for (let i = 0; i<LottoCount; i++){
+          let RandomNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+          MissionUtils.Console.print(`[${RandomNumber.join(', ')}]`);
+          this.Lotto.validate(RandomNumber);
+          this.Lottobuynumber.push(RandomNumber)
+        }
+      }
+}
