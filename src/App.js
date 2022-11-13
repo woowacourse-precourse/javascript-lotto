fcb799c7f8b2194ec37993850fc95ed562be4b31
한수지 <fatholic5@gmail.com>
2022-11-13 10:53:49 +0900

    :sparkles: feat: 구입금액에 따른 로또 수량 생성과 출력 구현 사용자의 구입금액을 입력받는다. 입력조건이 잘못될 경우 예외처리한다. 구입금액에 맞게 로또번호를 생성한다. 생성된 로또번호를 출력한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
-class App {
-  play() {}
-}
+const{ Random, Console } = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
 
+const LOTTO_PRICE = 1000;
+
+class App {
+
+  constructor() {
+    this.purchaseAmount = 0;
+    this.lottoAmount = 0;
+    this.lottoList = {};
+  }
+
+  play() {
+  return Console.readLine('구입금액을 입력해 주세요.\n', (answer) => {
+      this.validate(answer);
+      this.purchaseAmount = answer;
+      this.countBuyLotto();
+    })
+  }
+
+  validate(answer) {
+    if(answer % LOTTO_PRICE !== 0) throw new Error(`[ERROR] ${LOTTO_PRICE}원으로 나누어 떨어져야 합니다.`);
+  }
+
+  countBuyLotto() {
+    this.lottoAmount = this.purchaseAmount / LOTTO_PRICE;
+    Console.print(`${this.lottoAmount}개를 구매했습니다.`);
+    this.printOutBuyLotto();
+  }
+
+  printOutBuyLotto() {
+    this.lottoList  = Lotto.getLottoNumberes(this.lottoAmount);
+    for(let idex = 0; idex < this.lottoAmount; idex++){
+      Console.print(`[${this.lottoList[idex].join(", ")}]`);
+    }
+  }
+}
+let app = new App();
+app.play();
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,9 @@
+const{ Random, Console } = require("@woowacourse/mission-utils");
+
+const START_LOTTO_NUMBER = 1;
+const END_LOTTO_NUMBER = 45;
+const LOTTO_LENGTH = 6;
+
 class Lotto {
   #numbers;
 
@@ -7,12 +13,20 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    this.numbers = Random.pickUniqueNumbersInRange(START_LOTTO_NUMBER, END_LOTTO_NUMBER, LOTTO_LENGTH);
+    if (numbers.length !== LOTTO_LENGTH) throw new Error(`[ERROR] 로또 번호는 ${LOTTO_LENGTH}개여야 합니다.`);
+    if(new Set(numbers).size !== LOTTO_LENGTH) throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
   }
 
   // TODO: 추가 기능 구현
+  static getLottoNumberes(purchaseAmount) {
+    let lottoList = [];
+    for(let idex = 0; idex < purchaseAmount; idex++){
+      let lotto = Random.pickUniqueNumbersInRange(START_LOTTO_NUMBER, END_LOTTO_NUMBER, LOTTO_LENGTH);
+      lottoList.push(lotto.sort((elementOne, elementTwo) => elementOne - elementTwo));
+    }
+    return lottoList;
+  }
 }
 
 module.exports = Lotto;
