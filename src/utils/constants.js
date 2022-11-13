aa355364d390373d1b8dfebcc99125ec334a71d1
nalsae <nalsae0207@naver.com>
2022-11-14 02:55:22 +0900

    feat: add a constant about error messages
    에러 메시지 관련 상수 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,8 +7,15 @@ const RANGE_OF_LOTTO_NUMBER = {
 
 const TOTAL_COUNTS = 6;
 
+const ERROR_MESSAGES = {
+  PRICE: '[ERROR] 구입 금액은 1000원 단위의 숫자로 입력해야 합니다.',
+  WINNING_NUMS: `[ERROR] 당첨 번호는 중복되지 않는 1 ~ 45 사이의 숫자 6개를 ','로 구분하여 입력해야 합니다.`,
+  BOUNS_NUM: `[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 1 ~ 45 사이의 숫자 1개를 입력해야 합니다.`,
+};
+
 module.exports = {
   LOTTO_PRICE,
   RANGE_OF_LOTTO_NUMBER,
   TOTAL_COUNTS,
+  ERROR_MESSAGES,
 };
