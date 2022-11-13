b29e0fa0b4b1ea844a544fefc727296316be2251
wonseok <rhdqngkrh77@khu.ac.kr>
2022-11-13 10:45:03 +0900

    feat: 구입금액을 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  constructor() {
+    this.buyCost = 0;
+  }
+
+  getCost() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (cost) => {
+      if (cost / 1000 < 1) {
+        throw new Error("[ERROR] 로또 구입금액이 부족합니다.");
+      }
+      this.buyCost = cost;
+    });
+  }
+
   play() {}
 }
 
