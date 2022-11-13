39524c03ef20eb87e466c20f0b7f892c9b02e1f9
ashleysyheo <ashleysyheo@gmail.com>
2022-11-13 19:29:47 +0900

    test: check winning number input length exception

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -40,4 +40,13 @@ describe('사용자 입력 값에 대한 예외 클래스 테스트', () => {
       }).toThrow('[ERROR]');
     });
   });
+
+  test('로또 당첨 번호 입력시 6개의 숫자가 아닌 경우 예외가 발생한다', () => {
+    const exception = new Exception();
+    const WINNING_NUMBER = [1, 2, 3, 4, 5];
+
+    expect(() => {
+      exception.validateWinningNumber(WINNING_NUMBER);
+    }).toThrow('[ERROR]');
+  });
 });
