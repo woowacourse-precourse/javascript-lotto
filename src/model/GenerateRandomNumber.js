e3959ac1f99ce3420f0f2dca8520e93be346875e
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-14 03:24:55 +0900

    ♻️ [refactor]: 배열 오름차순 기능 수정

--- src/model/GenerateRandomNumber.js
+++ src/model/GenerateRandomNumber.js
@@ -3,7 +3,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 function generate() {
   const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
 
-  return numbers.sort();
+  return numbers.sort((a, b) => a - b);
 }
 
 module.exports = generate;
