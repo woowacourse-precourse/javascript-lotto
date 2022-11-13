3679fb6192ac81c016c5be77e0cebef5e0f3e0eb
Hyungeol94 <55428818+Hyungeol94@users.noreply.github.com>
2022-11-13 22:44:26 +0900

    feat: 로또 구매하기

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ const Lotto = require("./Lotto");
 
 class App {
   play() {
-  //로또 구매 입력받기
-  //당첨 번호 입력받기
-  
+  //로또 구매 금액 입력받기
   const amount = this.get_amount();
+
+  //로또 구매하기
+  const lotteries = this.buy_lotteries(amount);
+
   
+ 
+
+  }
+
+  buy_lotteries(amount){
+    const MissionUtils = require("@woowacourse/mission-utils");
+    const lotteries = [];
+
+    const n = amount % 1000;
+    sentence = n.toString() + "개를 구매했습니다."
+    MissionUtils.Console.print(sentence);
+
+    for(var i =0;i<n;i++){
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      var lottery = new Lotto(numbers);
+      lotteries.push(lottery);
+      MissionUtils.Console.print(numbers);
+    }
+    
+    
   }
 
   get_amount(){
@@ -15,9 +37,8 @@ class App {
     const amount = answer;
     if (this.not_thousands(amount)) {throw 'not_thousands'};
     if (this.not_positive(amount)) {throw 'not a positive number'}
-  })
-
     return amount;
+  })
   }
 
   //1000단위가 아닐 경우 예외처리
