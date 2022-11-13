1a991b926b6bc35e4a282c38b27c05f8d0285ed9
장경은 <jang_kyung_eun@gyeong-eun-ui-MacBookAir.local>
2022-11-13 14:53:15 +0900

    feat: 당첨 번호 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,32 +2,41 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Console = MissionUtils.Console;
 const Random = MissionUtils.Random;
 let numOfLotto = 0;
-let lottoNumObj = {};
 
 class App {
   lottoMoney() {
-    Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+    Console.readLine('구입금액을 입력해 주세요.', (money) => {
       numOfLotto = money/1000;
-      Console.print(`\n${numOfLotto}개를 구매했습니다.`);
-      this.createNumber();
+      Console.print(`${numOfLotto}개를 구매했습니다.`);
     });
+    return numOfLotto;
   }
 
   createNumber() {
+    let lottoNumObj = {};
     for (let i = 1; i <= numOfLotto; i++) {
       lottoNumObj[`lotto${i}`] = Random.pickUniqueNumbersInRange(1, 45, 6).sort(function(a, b)  {
         return a - b;
       });
       Console.print(lottoNumObj[`lotto${i}`]);
     }
+    return lottoNumObj;
+  }
+
+  winNumber() {
+    let winNumber = [];
+    Console.readLine('당첨 번호를 입력해 주세요.', (number) => {
+      winNumber = number.toString().split(',').map(Number);
+    });
+    return winNumber;
   }
 
   play() {
-    this.lottoMoney();
+    let numOfLotto = this.lottoMoney();
+    let lottoNumObj = this.createNumber();
+    let winNumber = this.winNumber();
+    Console.close();
   }
 }
-
-const app = new App();
-app.play();
 
 module.exports = App;
