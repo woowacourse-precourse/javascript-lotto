dd93cfd0043a81c726e77153c37c20f94ffc27e9
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-13 23:29:28 +0900

    test(App): App 클래스 테스트 - createLottoResult()
    사용자 입력 시나리오 테스트

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -23,4 +23,51 @@ describe('App 클래스 테스트', () => {
       expect(App.convertToNumberArray('1,2,3,4,5,6')).toEqual([1, 2, 3, 4, 5, 6]);
     });
   });
+
+  describe('createLottoResult()', () => {
+    test('인수를 전달하면 객체 { winningAmount: [1, 0, 0, 0, 0], total: 5000 } 기대한다.', () => {
+      const userLottos = [[8, 21, 23, 26, 41, 43]];
+      const winningNumbers = [1, 2, 3, 8, 21, 23];
+      const bonus = 44;
+      const expected = { winningAmount: [1, 0, 0, 0, 0], total: 5000 };
+
+      expect(App.createLottoResult(userLottos, winningNumbers, bonus)).toStrictEqual(expected);
+    });
+
+    test('인수를 전달하면 객체 { winningAmount: [0, 1, 0, 0, 0], total: 50000 } 기대한다.', () => {
+      const userLottos = [[8, 21, 23, 26, 41, 43]];
+      const winningNumbers = [1, 2, 8, 21, 23, 26];
+      const bonus = 44;
+      const expected = { winningAmount: [0, 1, 0, 0, 0], total: 50000 };
+
+      expect(App.createLottoResult(userLottos, winningNumbers, bonus)).toStrictEqual(expected);
+    });
+
+    test('인수를 전달하면 객체 { winningAmount: [0, 0, 1, 0, 0], total: 1500000 } 기대한다.', () => {
+      const userLottos = [[8, 21, 23, 26, 41, 43]];
+      const winningNumbers = [1, 8, 21, 23, 26, 41];
+      const bonus = 44;
+      const expected = { winningAmount: [0, 0, 1, 0, 0], total: 1500000 };
+
+      expect(App.createLottoResult(userLottos, winningNumbers, bonus)).toStrictEqual(expected);
+    });
+
+    test('인수를 전달하면 객체 { winningAmount: [0, 0, 0, 1, 0], total: 30000000 } 기대한다.', () => {
+      const userLottos = [[8, 21, 23, 26, 41, 44]];
+      const winningNumbers = [8, 21, 23, 26, 41, 43];
+      const bonus = 44;
+      const expected = { winningAmount: [0, 0, 0, 1, 0], total: 30000000 };
+
+      expect(App.createLottoResult(userLottos, winningNumbers, bonus)).toStrictEqual(expected);
+    });
+
+    test('인수를 전달하면 객체 { winningAmount: [0, 0, 0, 0, 1], total: 2000000000 } 기대한다.', () => {
+      const userLottos = [[8, 21, 23, 26, 41, 43]];
+      const winningNumbers = [8, 21, 23, 26, 41, 43];
+      const bonus = 44;
+      const expected = { winningAmount: [0, 0, 0, 0, 1], total: 2000000000 };
+
+      expect(App.createLottoResult(userLottos, winningNumbers, bonus)).toStrictEqual(expected);
+    });
+  });
 });
