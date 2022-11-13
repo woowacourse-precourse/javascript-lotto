76617b484aea76d86ea479e2b010e57fa5283b02
안호진 <asdv94@naver.com>
2022-11-14 02:16:00 +0900

    feat(lotto): 입출력 유효성 검사 기능 구현

--- src/IO.js
+++ src/IO.js
@@ -1,3 +1,10 @@
+const { ERROR_MSG } = require('./Constant');
+
 class IO {
-  static validate() {}
+  static validate(input) {
+    if (/^\D|[^\d{1,2}]|,{2,}|\D[,]|,$/g.test(input.trim()))
+      throw new Error(ERROR_MSG.prefix + ERROR_MSG.notNumber);
+  }
 }
+
+module.exports = IO;
