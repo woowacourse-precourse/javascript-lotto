8d7e2e1bb738391b3fd2ed6cb889a0a7e4efbb99
d0422 <rlfehd2013@naver.com>
2022-11-13 17:06:58 +0900

    Refactor: validate 내부함수 분리 body: validate를 독립적으로 구현하기 위해 내부 함수를 분리하여 호출함

--- src/Model/Money.js
+++ src/Model/Money.js
@@ -5,6 +5,9 @@ class Money {
     this.money = inputMoney;
   }
   validate(money) {
+    this.checkMoneyDivide(money);
+  }
+  checkMoneyDivide(money) {
     if (money % 1000 != 0) {
       throw new Error('[ERROR] 돈은 1000원단위여야 합니다!');
     }
