2f20139d8b2a13647a7ed71c9e53e0c193b23944
saetakki <lime.lemonade03@gmail.com>
2022-11-14 03:20:27 +0900

    feat. Add got payment function

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,46 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const Console = MissionUtils.Console;
+const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
+    ``;
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
+  static validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+
+  static inputHowMuchToPay() {
+    Console.readLine('구입금액을 입력해 주세요.', pay => {
+      if (this.isValidPayAmount(pay)) {
+        Console.print(`${parseInt(pay / 1000)} 개를 구매했습니다.`);
+      }
+    });
+  }
+
+  static isNumber(target) {
+    if (typeof Number(target) === 'number') {
+      return true;
+    }
+    throw new Error('[ERROR] 숫자를 입력해주세요.');
+  }
+
+  static isValidPayAmount(cost) {
+    if (this.isNumber(cost) && cost >= 1000) {
+      return true;
+    }
+    throw new Error('[ERROR] 1000원 이상의 금액을 입력해주세요.');
+  }
 }
 
 module.exports = Lotto;
