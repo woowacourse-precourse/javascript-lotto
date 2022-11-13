7d8f29cf5d8193debd58067e68b280a8e303e655
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-14 00:09:32 +0900

    feat(method): add getBonusWinningNumber
    Add getBonusWinningNumber method to UI class
    - Try to get input of bonus number and return bonus number
    - Catch error if it fails

--- src/UI.js
+++ src/UI.js
@@ -3,11 +3,13 @@ const ErrorCase = require("./ErrorCase");
 
 const CASH_INPUT_MESSAGE = "구입금액을 입력해 주세요.\n";
 const WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.\n";
+const BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.\n";
 
 const PURCHASE_AMOUNT_NOTICE_MESSAGE = "개를 구매했습니다.";
 
 const CASH_INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 금액입니다. 금액은 숫자로 입력해주세요.";
 const WINNING_NUMBERS_ERROR_MESSAGE = "[ERROR] 1부터 45까지의 정수 중 중복되지 않는 수 6개를 쉼표로 분리해 입력해주세요.";
+const BONUS_NUMBER_ERROR_MESSAGE = "[ERROR] 1부터 45까지의 정수 중 당첨 번호와 중복되지 않는 수 하나를 입력해주세요.";
 
 const getUserInput = (message, errorHandler) => {
   return new Promise((resolve, reject) =>
@@ -62,6 +64,16 @@ class UI {
       return winningNumbers;
     } catch {
       throw new Error(WINNING_NUMBERS_ERROR_MESSAGE);
+    }
+  }
+
+  static async getBonusWinningNumber() {
+    try {
+      const bonusNumber = await getUserInput(BONUS_NUMBER_INPUT_MESSAGE, ErrorCase.isWrongBonusNumberInput);
+
+      return bonusNumber;
+    } catch {
+      throw new Error(BONUS_NUMBER_ERROR_MESSAGE);
     }
   }
 }
