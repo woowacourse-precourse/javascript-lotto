4194de6624efe7824b9a836b9cc592658ab03913
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-13 19:57:18 +0900

    refactor: isNaN() -> Number.isNaN() 코드수정

--- src/utils/isValidLottery.js
+++ src/utils/isValidLottery.js
@@ -9,7 +9,7 @@ const isValidLottery = (lottery) => {
   lottery.forEach((separateNumber) => {
     if (!Number.isInteger(separateNumber))
       result = EXCEPTION_REASON.INPUT_DECIMAL;
-    if (isNaN(separateNumber)) result = EXCEPTION_REASON.INPUT_ERROR;
+    if (Number.isNaN(separateNumber)) result = EXCEPTION_REASON.INPUT_ERROR;
     if (separateNumber < 1 || separateNumber > 45)
       result = EXCEPTION_REASON.INPUT_ERROR;
   });
