9f663abc5a937f86ceaab99e060149f6adcb84e1
minjaeKim <smallkdb@gmail.com>
2022-11-13 09:32:01 +0900

    refactor(LottoGame): 생성자 삭제 후 private 필드로 대체
    생성자를 삭제하고 LottoGame클래스에서 활용하는 모든 클래스를 private 필드로 선언하였습니다.

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,48 +1,49 @@
 const { Console } = require('@woowacourse/mission-utils');
 const Lotto = require('./Lotto');
 const Payment = require('./Payment');
-const LottoBonus = require('./LottoBonus');
 const LottoIssuer = require('./LottoIssuer');
 const LottoResult = require('./LottoResult');
 
 class LottoGame {
-  constructor() {
-    this.money = 0;
-    this.lottoIssuer = new LottoIssuer();
-    this.lottoResult = new LottoResult();
-    this.winningNumber = {
-      main: [],
-      bonus: 0,
-    };
-  }
+  #Payment;
+
+  #LottoIssuer;
+
+  #Lotto;
+
+  #LottoResult;
 
   run() {
     Console.readLine('구입금액을 입력해 주세요.\n', (input) => this.inputMoney(input));
   }
 
   inputMoney(input) {
-    this.money = new Payment(input).getMoney();
-    this.purchase(this.money / 1000);
+    this.#Payment = new Payment(input);
+    this.purchase(this.#Payment.getMoney() / 1000);
   }
 
   purchase(number) {
-    Console.print(`\n${number}개를 구매했습니다.`);
-    this.lottoIssuer.issue(number);
-    this.lottoIssuer.print();
+    this.#LottoIssuer = new LottoIssuer(number);
+    this.#LottoIssuer.issue();
+    this.#LottoIssuer.print();
     this.drawWinningNumbers();
   }
 
   drawWinningNumbers() {
-    Console.readLine('\n당첨 번호를 입력해 주세요.\n', (input) => {
-      this.winningNumber.main = new Lotto(input.split(',')).getNumbers();
-      this.drawBonusNumber();
+    Console.readLine('\n당첨 번호를 입력해 주세요.\n', (winningNumber) => {
+      this.drawBonusNumber(winningNumber.split(','));
     });
   }
 
-  drawBonusNumber() {
-    Console.readLine('\n보너스 번호를 입력해 주세요.\n', (input) => {
-      this.winningNumber.bonus = new LottoBonus(input, this.winningNumber.main).getNumber();
-      this.lottoResult.print(this.winningNumber, this.lottoIssuer.getLotteries(), this.money);
+  drawBonusNumber(winningNumber) {
+    Console.readLine('\n보너스 번호를 입력해 주세요.\n', (bonusNumber) => {
+      this.#Lotto = new Lotto(winningNumber, bonusNumber);
+      this.#LottoResult = new LottoResult();
+      this.#LottoResult.print(
+        this.#Lotto.getNumbers(),
+        this.#LottoIssuer.getLotteries(),
+        this.#Payment.getMoney()
+      );
     });
   }
 }
