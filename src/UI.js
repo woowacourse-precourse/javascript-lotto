31155a0c8def727269a48aec8afd56b0b5e8f84f
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-13 23:20:09 +0900

    refactor(function): modify getCashInput
    Refactor getCashInput funciton of UI class
    - Change name: getCashInput -> getUserInput
    - Receive arguments: message to user, errorHandling function
    
    Now the function can be used on various purpose

--- src/UI.js
+++ src/UI.js
@@ -6,13 +6,13 @@ const PURCHASE_AMOUNT_NOTICE_MESSAGE = "개를 구매했습니다.";
 
 const CASH_INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 금액입니다. 금액은 숫자로 입력해주세요.";
 
-const getCashInput = () => {
+const getUserInput = (message, errorHandler) => {
   return new Promise((resolve, reject) =>
-    MissionUtils.Console.readLine(CASH_INPUT_MESSAGE, (cashInput) => {
-      const isWrongInput = ErrorCase.isWrongCashInput(cashInput);
+    MissionUtils.Console.readLine(message, (userInput) => {
+      const isWrongInput = errorHandler(userInput);
       if (isWrongInput) reject();
 
-      resolve(cashInput);
+      resolve(userInput);
     })
   );
 };
@@ -24,7 +24,7 @@ class UI {
 
   static async getCash() {
     try {
-      const cash = await getCashInput();
+      const cash = await getUserInput(CASH_INPUT_MESSAGE, ErrorCase.isWrongCashInput);
 
       UI.makeSpaceAfterSection();
 
