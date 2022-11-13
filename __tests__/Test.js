b4e460cd71249784fb611920de010e96774bb875
durumi99 <kms990615@naver.com>
2022-11-13 20:07:14 +0900

    test: 당첨 내역을 출력하는 기능 테스트

--- __tests__/Test.js
+++ __tests__/Test.js
@@ -1,4 +1,5 @@
 const { makeString } = require('../src/Print');
+const { getStatisticsMessage } = require('../src/constant/Constant');
 
 describe("기능 테스트", () => {
   test("발행 로또 출력 스트링을 만드는 함수 테스트", () => {
@@ -6,6 +7,13 @@ describe("기능 테스트", () => {
     const result = makeString(input);
 
     expect(result).toEqual('[8, 21, 23, 41, 42, 43]');
+  });
+
+  test("당첨 통계 출력 메시지", () => {
+    const input = { three:1, four:0, five:0, bonus:0, six:0 };
+    const result = getStatisticsMessage(input);
+
+    expect(result).toEqual('3개 일치 (5,000원) - 1개\n4개 일치 (50,000원) - 0개\n5개 일치 (1,500,000원) - 0개\n5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n6개 일치 (2,000,000,000원) - 0개\n');
   });
 
 });
