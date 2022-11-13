d1aea644b3bed7ccda4f0bc1c360b50b47e44fe0
2je0 <bulljy@naver.com>
2022-11-13 21:44:00 +0900

    ✨ feat: 로또 성적이 주어졌을 때 수익을 계산하는 기능 추가

--- src/Money.js
+++ src/Money.js
@@ -1,4 +1,5 @@
 const { MESSAGE } = require('./Constants');
+const { PRIZE_MONEY } = require('./Constants');
 
 class Money {
   constructor(amount) {
@@ -21,6 +22,19 @@ class Money {
    */
   getNumOfLotto() {
     return this.numOfLotto;
+  }
+
+  /**
+   * 로또 성적이 주어지면 당첨금 총액을 반환합니다.
+   * @param {Array<number>} matchedLotto - [3개 일치, 4개 일치, 5개 일치, 5개+보너스 일치, 6개 일치] 개수
+   * @returns {number} earning - 로또 당첨을 통해 얻은 금액
+   */
+  getEarning(matchedLotto) {
+    let earning = 0;
+    matchedLotto.forEach((num, idx) => {
+      earning += num * PRIZE_MONEY[idx];
+    });
+    return earning;
   }
 }
 module.exports = Money;
