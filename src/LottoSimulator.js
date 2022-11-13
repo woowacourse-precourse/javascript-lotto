6f3778a1391172b7f3be10cc2f7c1051d5b449e1
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-13 22:35:08 +0900

    feat(LottoSimulator): 수익률을 계산하는 기능 구현

--- src/LottoSimulator.js
+++ src/LottoSimulator.js
@@ -9,6 +9,16 @@ class LottoSimulator {
     if (grade === 'none') return;
     this.gradeCount.set(grade, this.gradeCount.get(grade) + 1);
   }
+
+  calcReturnRate(money) {
+    const profit = this.gradeCount.get('first') * 2000000000 + this.gradeCount.get('second') * 30000000 + this.gradeCount.get('third') * 1500000 + this.gradeCount.get('fourth') * 50000 + this.gradeCount.get('fifth') * 5000;
+    const returnRate = profit / money * 100;
+    return this.roundToOne(returnRate).toFixed(1);
+  }
+
+  roundToOne(num) {
+    return +(Math.round(num + "e+1") + "e-1");
+  }
 }
 
 module.exports = LottoSimulator;
