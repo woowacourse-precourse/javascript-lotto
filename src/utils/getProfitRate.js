958e49a3d804db568a0578f93483a2b973aee83c
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-13 17:56:55 +0900

    refactor: 수익률 계산 함수 반환값 수정

--- src/utils/getProfitRate.js
+++ src/utils/getProfitRate.js
@@ -1,8 +1,7 @@
 const getProfitRate = (startMoney, earnMoney) => {
   let rate = ((earnMoney / startMoney) * 100).toFixed(1);
-  if (rate[rate.length - 1] === '0') rate = Number(rate);
 
-  return rate + '%';
+  return Number(rate);
 };
 
 module.exports = getProfitRate;
