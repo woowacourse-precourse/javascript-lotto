0cded85e0a540ccd4baf4794b95dd93e8345a22f
khj0426 <706shin1728@naver.com>
2022-11-14 02:25:32 +0900

    refactor:Random,Console 디스트럭쳐링

--- src/App.js
+++ src/App.js
@@ -1,5 +1,80 @@
+const { Random, Console } = require('@woowacourse/mission-utils');
+const MESSAGE = require('./MESSAGE');
+const input = require('./UserInput');
+const UserLotto = require('./UsetLotto');
+const Lotto = require('./Lotto');
+const winWon = require('./NumberWonMap');
+const numberObj = require('./NumberEqaulCount');
+const winWonCount = require('./UserWinNumCount');
+const {
+  THOUSAND,
+  LOTTO_INIT_STR,
+  LOTTO_LENGTH,
+  USER_NUMBER_LOTTOS,
+} = require('./MESSAGE');
+
 class App {
-  play() {}
+  #winnumbers;
+  #numoflottos;
+  #numoflottosWon;
+  #bonusnumber;
+  #userlottos;
+  constructor() {
+    this.#winnumbers = '';
+    this.#numoflottos = 0;
+    this.#numoflottosWon = 0;
+    this.#bonusnumber = '';
+  }
+
+  inputWon() {
+    input.getInput();
+    const inputResult = input.isValidInput();
+    if (inputResult === MESSAGE.INPUT_ERROR_MESSAGE) {
+      Console.close();
+      throw inputResult;
+    }
+  }
+
+  buylotto() {
+    this.#numoflottos = input.numofLotts(input.userInput);
+    this.#numoflottosWon = this.#numoflottos * THOUSAND;
+    Console.print(this.#numoflottos + USER_NUMBER_LOTTOS);
+  }
+
+  getrandomlotto() {
+    this.#userlottos = new UserLotto(LOTTO_LENGTH);
+    this.#userlottos.getUserLottos(this.#numoflottos);
+    this.#userlottos.printLottos();
+  }
+
+  getwinbonuslotto() {
+    const winlottos = new Lotto(LOTTO_INIT_STR);
+    this.#winnumbers = winlottos.getWinLotto();
+    this.#bonusnumber = winlottos.getBounsLotto();
+    Console.print(this.#bonusnumber);
+  }
+
+  play() {
+    this.inputWon();
+    this.buylotto();
+    this.getrandomlotto();
+    this.getwinbonuslotto();
+    this.#userlottos.lottoArr.forEach((eachArr) => {
+      const result = this.#userlottos.compareLotto(eachArr, this.#winnumbers);
+      if (result >= MESSAGE.Three) {
+        numberObj[result] += 1;
+      }
+      if (
+        result === MESSAGE.Five &&
+        this.#userlottos.compareBonusNumber(eachArr, this.#bonusnumber)
+      ) {
+        numberObj[MESSAGE.FiveBonus] += 1;
+      }
+    });
+    const totalyield = winWonCount() / this.#numoflottosWon;
+    Console.print(`총 수익률은 ${(totalyield * 100).toFixed(1)}${'%'}입니다.`);
+    Console.close();
+  }
 }
 
 module.exports = App;
