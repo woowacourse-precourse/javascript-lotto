6ebbcd47aed7d2ca1a599b9d1cc8370dcf3d738f
wjs5025 <wjs_5025@naver.com>
2022-11-13 23:35:33 +0900

    feat : 구입 금액 입력 받기 메소드 구현 (setInputMoney)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGES } = require("./constraints");
+const Lotto = require("./Lotto");
 class App {
-  play() {}
+  constructor() {
+    this.purchaseAmount;
+  }
+
+  play() {
+    this.setInputMoney();
+  }
+
+  setInputMoney() {
+    Console.readLine("구입 금액을 입력해주세요.", (money) => {
+      // 구입 금액이 숫자인지 검사
+      if (isNaN(+money)) throw new Error(MESSAGES.BUYING.TYPE_EXCEPTION);
+
+      // 구입 금액이 1,000원 단위인지 검사
+      if (+money % 1000 !== 0) throw new Error(MESSAGES.BUYING.UNIT_EXCEPTION);
+
+      // 모두 충족한다면 구입 금액 저장 후 출력
+      this.purchaseAmount = +money;
+      Console.print(this.purchaseAmount);
+      Console.close();
+    });
+  }
 }
 
-module.exports = App;
+const app = new App();
+app.play();
+
+// module.exports = App;
