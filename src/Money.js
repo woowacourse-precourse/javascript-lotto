3f3ecb2272ff72448edc925c164b710c06afdbc6
2je0 <bulljy@naver.com>
2022-11-13 20:01:35 +0900

    ✨ feat: 구입 금액에 따른 로또 개수반환 하는 기능 추가

--- src/Money.js
+++ src/Money.js
@@ -14,5 +14,13 @@ class Money {
   validate(amount) {
     if (amount % 1000 !== 0) throw new Error(MESSAGE.ERROR_NO_THOUSAND_WON);
   }
+
+  /**
+   * 구입 금액에 따른 로또 개수를 반환합니다.
+   * @return {number} numOfLotto - 로또 개수
+   */
+  getNumOfLotto() {
+    return this.numOfLotto;
+  }
 }
 module.exports = Money;
