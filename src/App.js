ed2885c424816a5f1635178ec9e99236bc3ea673
woong <inwoong100@gmail.com>
2022-11-13 12:20:18 +0900

    feat(app): issueLottos 메소드 구현
    lottoNumbers를 받아 값만큼 Lotto를 발행할 수 있도록 구현
    
    발행하는 과정에서, 값을 임의로 정하는 과정을 따로 분리 필요

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-const { Console } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 const { validateInputMoney, getLottoNumber } = require('./Function');
 const { MESSAGE } = require('./Constant');
+const Lotto = require('./Lotto');
 
 class App {
   constructor() {
@@ -11,7 +12,21 @@ class App {
     Console.readLine(MESSAGE.SETINPUT, inputMoney => {
       validateInputMoney(inputMoney);
       const lottoNumber = getLottoNumber(inputMoney);
+      this.issueLottos(lottoNumber);
+      console.log(this.lottos);
     });
+  }
+
+  issueLottos(lottoNumber) {
+    for (let i = 0; i < lottoNumber; i += 1) {
+      const numbers = [];
+      while (numbers.length !== 6) {
+        const number = Random.pickUniqueNumbersInRange(1, 45);
+        if (!numbers.includes(number)) numbers.push(number);
+      }
+      const lotto = new Lotto(numbers);
+      this.lottos.push(lotto);
+    }
   }
 }
 
