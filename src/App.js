e1f37b9d83c558c66afda6cf8915ab38ac2cf0d1
박승훈 <tmdgns1126@naver.com>
2022-11-13 22:54:48 +0900

    feat: 내부적으로 사용할 상수 모듈 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+// const { ERROR_MSG_THOUSAND_UNIT } = require("./constants/error-message");
+// const { WINNINGS } = require("./constants/winnings");
+
 class App {
   play() {}
 }
 
-module.exports = App;
+// module.exports = App;
+const app = new App();
+app.play();
