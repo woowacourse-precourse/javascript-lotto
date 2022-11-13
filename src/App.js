58c247dd8cad4fb1bac85b0225e64d4c202cb212
greeneeya <suzythetravelerr@gmail.com>
2022-11-13 21:25:20 +0900

    사용자 구매 금액에 따른 로또 수량 발행 및 출력 및 예외처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.readLine('구입금액을 입력해주세요.', (answer) => {});
+  }
 }
+
+const test = new App();
+test.play();
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class Lotto {
   #numbers;
 
@@ -8,11 +10,29 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  ticketing(money) {
+    if (money % 1000 !== 0) {
+      throw new Error('[ERROR] 구입 금액은 1,000원 단위여야 합니다.');
+    }
+    const numberOfTickets = money / 1000;
+    const tickets = [];
+    for (let i = 0; i < numberOfTickets; i++) {
+      tickets.push(this.oneTicketNumbers());
+      MissionUtils.Console.print(tickets[i]);
+    }
+  }
+
+  oneTicketNumbers() {
+    let oneTicket = [];
+    for (let i = 0; i < 6; i++) {
+      oneTicket.push(MissionUtils.Random.pickNumberInRange(1, 45));
+    }
+    return oneTicket;
+  }
 }
 
 module.exports = Lotto;
