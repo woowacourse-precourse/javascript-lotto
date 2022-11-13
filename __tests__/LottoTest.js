4e429af3f25be4ff6dfc575f4ddf74056d61e426
chex <chex417@naver.com>
2022-11-14 01:17:04 +0900

    test: Lotto클래스 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,27 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또 번호의 개수가 6개가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호의 범위가 1~45가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 50]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("당첨번호와 일치하는 번호 개수를 리턴한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 7]);
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    expect(lotto.getMatchCount(winningNumbers)).toEqual(5);
+  });
+
+  test("보너스 번호를 가지고 있는지 여부를 리턴한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.hasBonusNumber(6)).toEqual(true);
+    expect(lotto.hasBonusNumber(7)).toEqual(false);
+  });
 });
