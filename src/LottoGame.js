56891faac9f7359dfdaa673d1502dbab5c7d3854
sungjihyun <jhsung23@gmail.com>
2022-11-13 17:02:17 +0900

    feat(LottoGame): 몇 게임이 당첨됐는지 구하는 기능 추가

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -8,6 +8,27 @@ class LottoGame {
     this.winningNumbers = winningNumbers;
     this.bonusNumber = bonusNumber;
   }
+
+  getGameResult() {
+    const matchCount = this.user.lottos.map(lotto =>
+      this.getMatchCount(lotto.numbers, this.winningNumbers.numbers, this.bonusNumber)
+    );
+
+    const fifth = matchCount.filter(([count]) => count === 3).length;
+    const fourth = matchCount.filter(([count]) => count === 4).length;
+    const third = matchCount.filter(([count]) => count === 5).length;
+    const second = matchCount.filter(([count, bonusMatch]) => count === 5 && bonusMatch).length;
+    const first = matchCount.filter(([count]) => count === 6).length;
+
+    return { fifth, fourth, third, second, first };
+  }
+
+  getMatchCount(numbers, winningNumbers, bonusNumber) {
+    const matchCount = numbers.filter(number => winningNumbers.includes(number) || number === bonusNumber).length;
+    const bonusMatch = numbers.includes(bonusNumber);
+
+    return [matchCount, bonusMatch];
+  }
 }
 
 module.exports = LottoGame;
