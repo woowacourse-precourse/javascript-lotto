4b4fefd7ecee1f44c7df843e877515edeb8fc0c7
d0422 <rlfehd2013@naver.com>
2022-11-13 16:55:09 +0900

    Test:보너스 번호가 문자인 경우 테스트코드 작성

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -7,4 +7,9 @@ describe('보너스 숫자 클래스 테스트', () => {
       new BonusNumber(new Lotto([1, 2, 3, 4, 5, 6]), 6);
     }).toThrow('[ERROR]');
   });
+  test('보너스 번호가 문자면 오류 발생', () => {
+    expect(() => {
+      new BonusNumber(new Lotto([1, 2, 3, 4, 5, 6]), '가');
+    }).toThrow('[ERROR]');
+  });
 });
