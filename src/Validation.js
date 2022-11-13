fbb5b2d64f34c5df04b5a56143f382b501a0bc51
Siby1lA <psb4644@gmail.com>
2022-11-13 09:03:11 +0900

    test: 로또 구매시 문자열이 포함된 경우

--- src/Validation.js
+++ src/Validation.js
@@ -1,5 +1,7 @@
 class Validation {
   static purchaseInput(purchase) {
+    if (isNaN([...purchase]))
+      throw new Error("[ERROR] 문자열이 포함되었습니다.");
     const result = parseInt(purchase, 10) % 1000;
     if (result !== 0) throw new Error("[ERROR] 1000원 단위로 입력해주세요");
   }
