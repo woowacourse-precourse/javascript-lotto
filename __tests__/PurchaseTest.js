66c6ff1c6be4d5a69d6c993f9d2422529568e3aa
금가경 <gageum0@gmail.com>
2022-11-13 18:58:13 +0900

    fix(lotto) : 변수명 변경에 맞게 테스트 코드와 실행 코드 수정

--- __tests__/PurchaseTest.js
+++ __tests__/PurchaseTest.js
@@ -1,22 +1,22 @@
 const Purchase = require('../src/Purchase');
-const Messages = '../src/Constants/Messages';
+const PurchaseError = '../src/Constants/Messages';
 
 describe('구매 클래스 테스트', () => {
   test('구매 값은 숫자만으로 이루어져 있어야 합니다.', () => {
     expect(() => {
       new Purchase('1234j');
-    }).toThrow(Messages.NOT_NUMBER);
+    }).toThrow(PurchaseError.NOT_NUMBER);
   });
 
   test('ERROR] 0보다 큰 수를 입력해 주세요.', () => {
     expect(() => {
       new Purchase('-3000');
-    }).toThrow(Messages.MINUS);
+    }).toThrow(PurchaseError.MINUS);
   });
 
   test('구매 값은 1000원 단위입니다', () => {
     expect(() => {
       new Purchase('3800');
-    }).toThrow(Messages.NOT_DIVIDED_BY_THOUSAND);
+    }).toThrow(PurchaseError.NOT_DIVIDED_BY_THOUSAND);
   });
 });
--- src/Purchase.js
+++ src/Purchase.js
@@ -1,4 +1,4 @@
-const error = require('./Constants/Messages');
+const PurchaseError = require('./Constants/Messages');
 
 class Purchase {
   constructor(number) {
@@ -7,13 +7,13 @@ class Purchase {
 
   validate(number) {
     if (isNaN(number)) {
-      throw new Error(error.NOT_NUMBER);
+      throw new Error(PurchaseError.NOT_NUMBER);
     }
     if (number < 0) {
-      throw new Error(error.MINUS);
+      throw new Error(PurchaseError.MINUS);
     }
     if (number % 1000 !== 0) {
-      throw new Error(error.NOT_DIVIDED_BY_THOUSAND);
+      throw new Error(PurchaseError.NOT_DIVIDED_BY_THOUSAND);
     }
   }
 }
