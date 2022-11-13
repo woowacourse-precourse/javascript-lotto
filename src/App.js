f8b753866991799eee61b47ab72df0787f78f1aa
froggy1014 <clcl6084@gmail.com>
2022-11-13 13:09:41 +0900

    refactor: 변수 이름 및 상수 객체 불변성 적용
    1. 가독성을 위해 변수 이름 수정
    2. 상수 객체들을 Object.freeze를 통해 불변성 적용

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ const Player = require('./Player');
 const Lotto = require('./Lotto');
 const Machine = require('./Machine');
 const { Console, Statistics } = require('./util');
-const { MESSAGES, PRIZE, RESULTS } = require('./constants');
+const { MESSAGES, RESULTS } = require('./constants');
 
 class App {
   play() {
@@ -10,40 +10,40 @@ class App {
   }
 
   #payMoney() {
-    Console.readLine(`${MESSAGES.START}\n`, (money) => {
+    Console.readLine(`${MESSAGES.PAYMONEY}\n`, (money) => {
       this.machine = new Machine(money);
-      this.#issueLottery();
+      this.#issueLottery(money);
     });
   }
 
-  #issueLottery() {
-    const tickets = this.machine.insertMoney(this.machine.insertedMoney);
+  #issueLottery(money) {
+    const tickets = this.machine.insertMoney(money);
     this.player = new Player(tickets);
     this.#askWinningNum();
   }
 
   #askWinningNum() {
-    Console.readLine(`\n${MESSAGES.GET_LOTTOS}\n`, (numbers) => {
+    Console.readLine(`\n${MESSAGES.ASK_WINNING}\n`, (numbers) => {
       this.lotto = new Lotto(numbers.split(',').map(Number));
       this.#askBonusNum();
     });
   }
 
   #askBonusNum() {
-    Console.readLine(`\n${MESSAGES.GET_BONUS}\n`, (number) => {
+    Console.readLine(`\n${MESSAGES.ASK_BONUS}\n`, (number) => {
       this.lotto.winningNums = number;
-      this.#showResult();
+      this.#showResult(this.player.pocket, this.lotto.winningNums);
     });
   }
 
-  #showResult() {
-    const [tickets, luckyNum] = [this.player.pocket, this.lotto.winningNums];
+  #showResult(tickets, luckyNum) {
     const count = Statistics.countWinning(tickets, luckyNum);
+    const { PRIZE, TEXT } = RESULTS
     let profit = 0;
     Console.print('\n당첨 통계\n---');
     for (let i = 0; i < count.length; i++) {
-      Console.print(`${RESULTS[i]}${count[i]}개`);
-      profit += Number(PRIZE[i]) * Number(count[i]);
+      Console.print(TEXT[i] + count[i] + '개');
+      profit += PRIZE[i] * count[i];
     }
     const ROR = Statistics.rateOfReturn(profit, this.machine.insertedMoney);
     Console.print(`총 수익률은 ${ROR}%입니다.`);
@@ -51,7 +51,7 @@ class App {
   }
 }
 
-const app = new App();
-app.play();
+const lottery = new App();
+lottery.play();
 
 module.exports = App;
