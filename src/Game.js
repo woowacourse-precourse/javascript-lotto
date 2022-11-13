176c556eaac5e9abbb3b63a60751a57a91df1e53
안호진 <asdv94@naver.com>
2022-11-14 02:59:11 +0900

    feat(lotto): 입력 금액 유효성 검사 기능 구현

--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,10 @@
+const { ERROR_MSG } = require('./Constant');
+
 class Game {
-  static validate() {}
+  static validate(money) {
+    if (money % 1000 > 0)
+      throw new Error(ERROR_MSG.prefix + ERROR_MSG.only1000WonUnits);
+  }
 }
+
+module.exports = Game;
