b9ba29ac0a99ef057edeef976d28caea565af090
d0422 <rlfehd2013@naver.com>
2022-11-13 17:00:24 +0900

    Feat:보너스 숫자가 숫자인지 체크하는 기능 추가

--- src/Model/BonusNumber.js
+++ src/Model/BonusNumber.js
@@ -5,10 +5,17 @@ class BonusNumber {
   }
   validate(winLotto, inputNumber) {
     this.checkDuplicate(winLotto, inputNumber);
+    this.checkNumber(inputNumber);
   }
   checkDuplicate(winLotto, inputNumber) {
     if (winLotto.lottoNumbers.includes(Number(inputNumber))) {
       throw new Error('[ERROR] 보너스 숫자와 정답 로또 숫자가 중복되었습니다.');
+    }
+  }
+  checkNumber(inputNumber) {
+    const NumberReg = /[0-9]/g;
+    if (!NumberReg.test(inputNumber)) {
+      throw new Error('[ERROR] 보너스 숫자는 숫자여야합니다.');
     }
   }
 }
