fb81a63ebb9b79217bae771fbf1c3ceca5532bd9
안호진 <asdv94@naver.com>
2022-11-14 01:52:23 +0900

    style(lotto): newline string 수정

--- src/Constant.js
+++ src/Constant.js
@@ -1,21 +1,19 @@
 class Constant {
   static NEW_LINE = '\n';
   static GAME_MSG = Object.freeze({
-    pleaseEnterMoney: '구입금액을 입력해주세요.' + NEW_LINE,
-    bought: '개를 구매했습니다.' + NEW_LINE,
-    pleaseEnterWinningNumbers:
-      NEW_LINE + '당첨 번호를 입력해 주세요.' + NEW_LINE,
-    pleaseEnterBonusNumbers:
-      NEW_LINE + '보너스 번호를 입력해 주세요.' + NEW_LINE,
+    pleaseEnterMoney: '구입금액을 입력해주세요.\n',
+    bought: '개를 구매했습니다.\n',
+    pleaseEnterWinningNumbers: '\n당첨 번호를 입력해 주세요.\n',
+    pleaseEnterBonusNumbers: '\n보너스 번호를 입력해 주세요.\n',
   });
   static RESULT_MSG = Object.freeze({
-    stats: NEW_LINE + '당첨 통계' + NEW_LINE + '---' + NEW_LINE,
+    stats: '\n당첨 통계\n---\n',
     match3: '3개 일치 (5,000원) - ',
     match4: '4개 일치 (50,000원) - ',
     match5: '5개 일치 (1,500,000원) - ',
     matchBonus: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
     match6: '6개 일치 (2,000,000,000원) - ',
-    ea: '개' + NEW_LINE,
+    ea: '개\n',
     totalProfit: '총 수익률은 ',
     isNPercent: '%입니다.',
   });
