bd6735d824c6b5b290268ae71eceec545da3d8d8
sunny <huiseon37@gmail.com>
2022-11-14 01:19:35 +0900

    feat: LottoMachine 클래스 구매금액 입력받는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+class LottoManage{
+  inputPurchaseMoney(){
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n",(inputMoney)=>{
+    })
+  }
+}
+
 class App {
-  play() {}
+  constructor(){
+    this.lottomanage = new LottoManage();
+  }
+  play() {
+    this.lottomanage.inputPurchaseMoney();
+  }
 }
 
 module.exports = App;
