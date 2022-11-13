e95232b4e810c6a6449a060dab486da3886fab0d
d0422 <rlfehd2013@naver.com>
2022-11-13 17:02:33 +0900

    Test:보너스 번호가 로또범위를 넘어가는 경우 추가

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -7,9 +7,16 @@ describe('보너스 숫자 클래스 테스트', () => {
       new BonusNumber(new Lotto([1, 2, 3, 4, 5, 6]), 6);
     }).toThrow('[ERROR]');
   });
+
   test('보너스 번호가 문자면 오류 발생', () => {
     expect(() => {
       new BonusNumber(new Lotto([1, 2, 3, 4, 5, 6]), '가');
+    }).toThrow('[ERROR]');
+  });
+
+  test('보너스 번호가 로또 범위를 넘어가면 오류 발생', () => {
+    expect(() => {
+      new BonusNumber(new Lotto([1, 2, 3, 4, 5, 6]), -1);
     }).toThrow('[ERROR]');
   });
 });
