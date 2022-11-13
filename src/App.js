3877e3173d25383863077e42248c2e11923d3b8b
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-11-14 02:15:58 +0900

    refactor : 코드 리팩토링

--- src/App.js
+++ src/App.js
@@ -2,43 +2,39 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 
 class App {
-  constructor() {}
-
   play() {}
 
   // 금액 입력
-  getMoney(answer) {
+  getMoney() {
     Console.readLine("구입금액을 입력해 주세요.", (money) => {
       if (isNaN(money)) {
         throw new Error("[ERROR] 숫자를 입력하지 않았습니다.");
-      } else {
-        checkMoney(money);
       }
+      return this.checkMoney(money);
     });
   }
 
   // 금액 로또로 변환
   checkMoney(money) {
-    let moneyChange = money / 1000;
+    const moneyChange = money / 1000;
     if (!Number.isInteger(moneyChange)) {
       throw new Error("[ERROR] 알맞은 값을 입력하지 않았습니다.");
-    } else {
-      console.print(moneyChange + "개를 구매했습니다.");
     }
+    return moneyChange;
   }
 
   // 구입 금액에 해당하는 만큼 로또 발행, 출력
   lottosNumbers = [];
 
   creatLotto() {
-    for (let i = 0; i < moneyChange; i++) {
+    for (let i = 0; i < this.getMoney; i++) {
       const lottoNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
-      const lottoNumberSort = lottoNumber.sort(function (a, b) {
+      lottoNumber.sort(function (a, b) {
         return a - b;
       });
-      this.lottosNumbers.push(lottoNumberSort);
+      this.lottosNumbers.push(lottoNumber);
     }
-    console.print(lottosNumbers);
+    Console.print(lottosNumbers);
   }
 }
 
