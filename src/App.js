152eadb89a3eb8b0868da54b803fb08abf222f06
seoyeon <wjstjdus96@naver.com>
2022-11-13 16:29:20 +0900

    feat: 상수 선언

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+const ENTER_PRICE = "구입 금액을 입력해주세요";
+const ERROR_MARK = "[ERROR]";
+const WINNING_REWARDS = [2000000000, 30000000, 1500000, 50000, 5000];
+const WINNING_RANK = [
+  "6개 일치",
+  "5개 일치, 보너스 볼 일치",
+  "5개 일치",
+  "4개 일치",
+  "3개 일치",
+];
+
 class App {
   play() {}
 }
