bd4e5eab5391f0ec3e5e8267d1282972906e463f
2je0 <bulljy@naver.com>
2022-11-13 19:37:19 +0900

    ✨ feat: lotto 에러 검증 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,9 +7,15 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    if (numbers.length !== 6) throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+
+    const numSet = new Set(numbers);
+    if (numSet.size !== 6) throw new Error('[ERROR] 중복된 번호가 없어야 합니다.');
+
+    numbers.forEach((num) => {
+      if (!Number.isInteger(num)) throw new Error('[ERROR] 정수를 입력해야 합니다.');
+      if (num < 1 || num > 45) throw new Error('[ERROR] 1~45 사이 숫자여야 합니다.');
+    });
   }
 
   // TODO: 추가 기능 구현
