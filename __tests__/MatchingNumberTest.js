f9ed2383030ca884d399873e2e3b6b61138d731f
chiman <rlaclghks123@naver.com>
2022-11-13 13:50:40 +0900

    test: 테스트 수정
    불필요한 테스트 삭제 및 보너스 번호 포함시 테스트 추가

--- __tests__/MatchingNumberTest.js
+++ __tests__/MatchingNumberTest.js
@@ -8,38 +8,26 @@ const totalLottoNumber = [
 
 const winnerNumber = ["1", "5", "6", "10", "11", "12"];
 
-const numbers = [1, 5, 6, 10, 11, 13];
+let numbers = [1, 5, 6, 10, 11, 13];
 
 describe("로또 당첨 통계 부분 Test", () => {
   afterEach(() => {
     Console.close();
   });
 
-  test("5개 일치할때 보너스번호가 포함되어 있지 않을경우 결과값은 4인지 테스트", () => {
-    const bonusNumber = "12";
-    const matchingNumber = new MatchingNumber(totalLottoNumber, winnerNumber, bonusNumber);
-    const result = matchingNumber.getNumberOfNumbersCompareWtihBonusNumber(numbers);
-    expect(result).toBe(4);
-  });
+  describe("5개 일치할때 보너스 번호 포함 테스트", () => {
+    test("보너스번호가 포함되어 있지 않을경우 테스트", () => {
+      const bonusNumber = "12";
+      const matchingNumber = new MatchingNumber(totalLottoNumber, winnerNumber, bonusNumber);
+      const result = matchingNumber.isContainBonusNumber(numbers);
+      expect(result).toBeFalsy();
+    });
 
-  test("5개 일치할때 보너스번호가 포함되어 있는경우 결과값은 5인지 테스트", () => {
-    const bonusNumber = "13";
-    const matchingNumber = new MatchingNumber(totalLottoNumber, winnerNumber, bonusNumber);
-    const result = matchingNumber.getNumberOfNumbersCompareWtihBonusNumber(numbers);
-    expect(result).toBe(5);
-  });
-
-  test("순위를 기준으로 나타낸 일치개수가 보너스 번호가 포함된 경우 5인지 테스트", () => {
-    const bonusNumber = "13";
-    const matchingNumber = new MatchingNumber(totalLottoNumber, winnerNumber, bonusNumber);
-    const result = matchingNumber.getNumberOfMatchingNumber(numbers);
-    expect(result).toBe(5);
-  });
-
-  test("순위를 기준으로 나타낸 일치개수가 3개 일치할때 2인지 테스트", () => {
-    const bonusNumber = "42";
-    const matchingNumber = new MatchingNumber(totalLottoNumber, winnerNumber, bonusNumber);
-    const result = matchingNumber.getNumberOfMatchingNumber([1, 5, 6, 15, 16, 17]);
-    expect(result).toBe(2);
+    test("보너스번호가 포함되어 있는 경우 테스트", () => {
+      const bonusNumber = "13";
+      const matchingNumber = new MatchingNumber(totalLottoNumber, winnerNumber, bonusNumber);
+      const result = matchingNumber.isContainBonusNumber(numbers);
+      expect(result).toBeTruthy();
+    });
   });
 });
