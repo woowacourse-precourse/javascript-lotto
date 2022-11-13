a7725e4e8555d98b3dfe52f4cc5fbf5dcc943235
ashleysyheo <ashleysyheo@gmail.com>
2022-11-13 19:44:41 +0900

    test: check number in range exception

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -8,6 +8,15 @@ describe('사용자 입력 값에 대한 예외 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
+  test('1~45 사이의 숫자가 입력되지 않은 경우 에외가 발생한다', () => {
+    const exception = new Exception();
+    const NUMBER = 56; 
+
+    expect(() => {
+      exception.checkIsNumberInRange(NUMBER);
+    }).toThrow('[ERROR]');
+  });
+
   test('로또 구매 금액이 1000 단위가 아니면 예외가 발생한다', () => {
     const exception = new Exception();
     const PURCHASE_AMOUNTS = [123, 123456, 1, 100, 200, 3432];
@@ -44,6 +53,15 @@ describe('사용자 입력 값에 대한 예외 클래스 테스트', () => {
   test('로또 당첨 번호 입력시 6개의 숫자가 아닌 경우 예외가 발생한다', () => {
     const exception = new Exception();
     const WINNING_NUMBER = [1, 2, 3, 4, 5];
+
+    expect(() => {
+      exception.validateWinningNumber(WINNING_NUMBER);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 당첨 번호 입력시 1~45 이외의 숫자가 포함된 경우 예외가 발생한다', () => {
+    const exception = new Exception();
+    const WINNING_NUMBER = [1, 2, 7, 19, 34, 56]; 
 
     expect(() => {
       exception.validateWinningNumber(WINNING_NUMBER);
