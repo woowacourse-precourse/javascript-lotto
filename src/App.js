4434d4b3fa0317b6e19e1c674383dc9dbd835a35
강인영 <kiy@gang-in-yeong-ui-MacBookAir.local>
2022-11-13 22:44:54 +0900

    feat: Calculate game result(rank)

--- src/App.js
+++ src/App.js
@@ -27,7 +27,31 @@ class App {
     Console.print(`총 수익률은 ${profit}입니다.`);
   }
 */
-  
+  calculateRank(answerCnt, bonusOrNot) {
+    if(answerCnt == 6) return 1;
+    if(answerCnt == 5 && bonusOrNot) return 2;
+    if(answerCnt == 5) return 3;
+    if(answerCnt == 4) return 4;
+    if(answerCnt == 3) return 5;
+    return 0;
+  }
+  calculateGameResult(allLines, winningNums, bonusNum) {
+    Console.print('당첨 통계\n---\n');
+    const rankList = [];
+    this.bonusOrNot = false;
+
+    for(var i=0;i<allLines.length;i++){
+      const oneLine = allLines[i];
+      const answerCnt = oneLine.filter(x => winningNums.includes(x)).length;
+      if(answerCnt == 5) {
+        if(oneLine.includes(bonusNum)) this.bonusOrNot = true;
+      }
+      const rank = this.calculateRank(answerCnt, this.bonusOrNot);
+      rankList.push(rank);
+    }
+    //console.log(rankList);
+    return rankList;
+  }
   //6. 보너스 번호 입력받기
   enterBonus(allLines, winningNums){
     Console.readLine('\n보너스 번호를 입력해 주세요.\n',(bonus) =>{
