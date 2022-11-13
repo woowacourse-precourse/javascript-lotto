04ef14debb0bf4447e06808363182f26e24e2e7f
dhkham <khamdong@gmail.com>
2022-11-13 15:54:58 +0900

    feat(Lotto.js): add method that checks if lottery numbers are integers

--- src/Lotto.js
+++ src/Lotto.js
@@ -5,6 +5,7 @@ class Lotto {
     this.validateLength(numbers);
     this.validateNoDuplicates(numbers);
     this.validateNumBetween1To45(numbers);
+    this.validateInteger(numbers);
     this.#numbers = numbers;
   }
 
@@ -27,6 +28,15 @@ class Lotto {
       }
     });
   }
+
+  validateInteger(numbers) {
+    numbers.forEach((num) => {
+      if (!Number.isInteger(num)) {
+        throw new Error("[ERROR] 로또 번호는 정수여야 합니다.");
+      }
+    });
+  }
+
 
 
   // TODO: 추가 기능 구현
