22bc5f3480873552697c0d3dab9a764442f21246
2yunseong <dbsdltjd3701@naver.com>
2022-11-13 17:32:42 +0900

    feat(app): 안내 문구를 출력하는 기능 - 구입 금액을 입력해주세요

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 const OutputUI = require('./OutputUI');
 const InputUI = require('./InputUI');
+const message = require('./util/message');
 
 class App {
   constructor() {
     this.input = new InputUI();
     this.output = new OutputUI();
   }
+
   play() {}
+
+  renderBuyAmountView() {
+    this.output.print(message.INPUT_AMOUNT);
+    this.input.inputLine().then((data) => {});
+  }
 }
 
 module.exports = App;
