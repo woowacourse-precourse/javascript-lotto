72aa45c441b8a613adac65c3853529bd5f2292d9
vinivin153 <vinivin153@naver.com>
2022-11-13 22:33:46 +0900

    feat: 로또 구매 테스트 추가

---
+++ __tests__/LottoShopTest.js
@@ -0,0 +1,18 @@
+const LottoShop = require('../src/LottoShop');
+
+describe('구매 테스트', () => {
+  test('몇장의 로또를 구입할 수 있는지 계산하는 테스트.', () => {
+    const lottoShop = new LottoShop(8000);
+    const result = lottoShop.countLottoAmount();
+
+    expect(result).toEqual(8);
+  });
+
+  test('로또 생성 테스트.', () => {
+    const lottoShop = new LottoShop(8000);
+    const lottoAmount = lottoShop.countLottoAmount();
+    const result = lottoShop.createLottoNumbers(8).length;
+
+    expect(result).toEqual(8);
+  });
+});
