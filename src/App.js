e44b49bdfcf86535037cb85cd998d21ff7a52bb5
wlthdud <thdudzi@naver.com>
2022-11-14 02:49:52 +0900

    feat: constructor 변수 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {
+  constructor(){ //변수선언
+    this.lottoMoney;
+  }
+
+  play() { //게임시작
 
   }
 
-  getMoney(){ //구매 금액 입력받기
+  getMoney(){ //구매금액 입력받기
     MissionUtils.Console.readline('구입금액을 입력해 주세요.\n', (money) => {
-      
+        this.lottoMoney = money;
     });
+  }
+
+  getLottoAmount(){ //로또수량 계산하기
+    
   }
 }
 
