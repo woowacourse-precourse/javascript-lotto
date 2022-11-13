8a64b41444e3fc5420f0bb80128c44b7b362cc4b
jw <always1997@kakao.com>
2022-11-13 18:04:32 +0900

    feat: User클래스 로또목록조회 기능 구현

--- src/User.js
+++ src/User.js
@@ -1,3 +1,5 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class User {
   #myLottos;
   #amount;
@@ -7,11 +9,20 @@ class User {
     this.#amount = amount;
   }
 
+  addLotto(lotto) {
+    this.#myLottos.push(lotto);
+  }
+
   validate(amount) {
     if (amount % 1000 !== 0) {
       throw new Error("[ERROR] 구입 금액은 천원단위여야 합니다.");
     }
   }
 
-  printMyLottos() {}
+  printMyLottos() {
+    Console.print(this.#myLottos.length + "개를 구매했습니다.");
+    this.#myLottos.map((lotto) => {
+      Console.print(lotto);
+    });
+  }
 }
