d81059b205653edd3438eaebb98a744f09f4b138
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-14 04:08:10 +0900

    feat(method): add checking form of winning numbers input
    Add to ErrorCase
    - isWrongWinningNumbersInput method: check form of winning number input and return true if it is inappropriate
    - processWinningNumbersInput function: process winning numbers input to required form

--- src/ErrorCase.js
+++ src/ErrorCase.js
@@ -1,3 +1,14 @@
+const processWinningNumbersInput = (input) => {
+  return input
+    .split(" ")
+    .join("")
+    .split(",")
+    .map((number) => parseInt(number))
+    .filter((number) => !isNaN(number))
+    .filter((number) => number >= 1 && number <= 45)
+    .filter((number) => number === Math.floor(number));
+};
+
 class ErrorCase {
   static isWrongCashInput(cashInput) {
     if (isNaN(cashInput)) return true;
@@ -8,6 +19,13 @@ class ErrorCase {
   static duplicatedNumbers(numbers) {
     const erasedDuplicated = [...new Set(numbers)];
     if (erasedDuplicated.length !== numbers.length) return true;
+
+    return false;
+  }
+
+  static isWrongWinningNumbersInput(winningNumbersInput) {
+    const processed = processWinningNumbersInput(winningNumbersInput);
+    if (processed.length !== 6) return true;
 
     return false;
   }
