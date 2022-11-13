a677bf45422655d67e561e3e28bdab1cb820140c
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-13 23:06:03 +0900

    ✨ [feat]: 당첨 번호 입력에 대한 예외처리 - 로또 번호의 숫자 범위

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,14 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-  }
 
-  // TODO: 추가 기능 구현
+    if (
+      numbers.some(function (number) {
+        return number < 1 || number > 45;
+      })
+    )
+      throw new Error("[ERROR] 1부터 45까지의 로또 번호를 입력해 주세요.");
+  }
 }
 
 module.exports = Lotto;
