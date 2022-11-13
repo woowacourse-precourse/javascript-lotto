cc41f9272c0605428ea53066ad55d25f8d1825a0
khkh0109 <ist200120@gmail.com>
2022-11-13 15:45:52 +0900

    feat(UserError): 숫자 외 다른 문자가 입력된 경우 예외 처리 하는 기능 추가

--- src/UserError.js
+++ src/UserError.js
@@ -1,7 +1,13 @@
 class UserError {
+  includingCharactersError(money) {
+    if (/\D/g.test(money)) {
+      throw new Error("[ERROR] 숫자를 입력해 주세요.");
+    }
+  }
+
   validateMoney(money) {
     if (money % 1000 !== 0) {
-      throw new Error("[ERROR] 1,000원 단위로 입력해주세요.");
+      throw new Error("[ERROR] 1,000원 단위로 입력해 주세요.");
     }
   }
 }
