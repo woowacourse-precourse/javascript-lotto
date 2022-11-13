622b5118c1d00b33d9b56f0544b5ba1be99afba8
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-13 21:40:06 +0900

    feat(App): 금액만큼 로또를 발행하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 const { Console } = require("@woowacourse/mission-utils");
+const NumberGenerator = require("./NumberGenerator");
+
 class App {
+  lottos;
+
+  constructor() {
+    this.lottos = [];
+  }
+
   play() {
     Console.readLine('구입금액을 입력해 주세요.\n', this.issueLotto.bind(this));
   }
 
-  issueLotto(input) {
-    
+  issueLotto() {
+    const numberGenerator = new NumberGenerator();
+    for (let purchaseCount = 0; purchaseCount < purchaseMoney / 1000; purchaseCount++){
+      this.lottos.push(new Lotto(numberGenerator.createRandomNumber()));
+    }
   }
 }
 
