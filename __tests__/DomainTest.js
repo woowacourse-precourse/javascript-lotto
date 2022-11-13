8c7a6544f745e81f6deb3b61b758b8a79560becc
ilgon0110 <moses011010@gmail.com>
2022-11-14 00:42:46 +0900

    test: 당첨번호와 보너스번호가 적절히 입력되었는지 확인하는 테스트 완료 (#test4)

--- __tests__/DomainTest.js
+++ __tests__/DomainTest.js
@@ -1,6 +1,7 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const User = require('../src/User');
 const Lotto = require('../src/Lotto');
+const UserInput = require('../src/UserInput');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -74,5 +75,53 @@ describe('기능 2번. 로또 구입 금액 만큼의 로또 번호 생성', () 
   test('로또 번호에 중복된 숫자가 있는 경우', () => {
     mockRandoms([[8, 21, 45, 41, 41, 43]]);
     expect(() => new Lotto()).toThrow('[ERROR] 중복된 숫자가 있습니다.');
+  });
+});
+
+describe('기능 3-1번. 당첨 번호 : 중복되지 않는 숫자 6개', () => {
+  test('숫자 이외의 type을 입력한 경우', () => {
+    const userInput = new UserInput();
+    mockQuestions(['1,2,3,4,5,plz']);
+    expect(() => userInput.getUserLottos()).toThrow(
+      '[ERROR] 로또 번호는 숫자만 가능합니다. 쉼표로 구분해주세요.',
+    );
+  });
+  test('1부터 45의 범위를 벗어난 숫자를 입력한 경우', () => {
+    const userInput = new UserInput();
+    mockQuestions(['1,2,3,4,5,51']);
+    expect(() => userInput.getUserLottos()).toThrow(
+      '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+    );
+  });
+  test('숫자가 6개가 아닌 경우', () => {
+    const userInput = new UserInput();
+    mockQuestions(['1,2,3,4,5']);
+    expect(() => userInput.getUserLottos()).toThrow(
+      '[ERROR] 1부터 45까지의 서로 다른 6개의 숫자를 입력해주세요.',
+    );
+  });
+  test('중복되는 숫자가 있는 경우', () => {
+    const userInput = new UserInput();
+    mockQuestions(['1,2,3,4,5,5']);
+    expect(() => userInput.getUserLottos()).toThrow(
+      '[ERROR] 로또 번호는 중복이 불가능합니다.',
+    );
+  });
+});
+
+describe('기능 3-2번. 보너스 번호 : 숫자 1개', () => {
+  test('숫자 이외의 type을 입력한 경우', () => {
+    const userInput = new UserInput();
+    mockQuestions(['bonus']);
+    expect(() => userInput.getUserBonusNumber()).toThrow(
+      '[ERROR] 보너스 번호는 숫자만 가능합니다.',
+    );
+  });
+  test('숫자 이외의 type을 입력한 경우', () => {
+    const userInput = new UserInput();
+    mockQuestions(['50']);
+    expect(() => userInput.getUserBonusNumber()).toThrow(
+      '[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.',
+    );
   });
 });
