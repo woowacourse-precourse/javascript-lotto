501280c1b265f6240cba1cf789e78efc359cc138
bassyu <yrkdgus@gmail.com>
2022-11-13 15:38:42 +0900

    feat(Calculator): 검증 기능 적용

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,16 +1,15 @@
 const { LOTTO_PRICE, WINNIG_PRIZE } = require('./constants/price');
+const Validator = require('./Validator');
 
 class Calculator {
   #pay;
   #totalPrize;
 
   constructor(pay) {
-    this.validate(pay);
+    Validator.checkPay(pay);
     this.#pay = pay;
     this.#totalPrize = 0;
   }
-
-  validate(pay) {}
 
   calcBuyCount() {
     return Math.ceil(this.#pay / LOTTO_PRICE);
