b177aee6489ac0000889e3d9a34905ea21917d76
bae-sh <hyeon1293@gmail.com>
2022-11-13 21:47:11 +0900

    feat : buyLotto 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const { print, readLine, makeLottoNumber } = require('./util');
 class App {
+  #lottoCount;
+
+  buyLotto() {
+    readLine('구입금액을 입력해 주세요.', money => {
+      this.#lottoCount = Math.floor(Number(money) / 1000);
+    });
+  }
   play() {}
 }
 
+new App().buyLotto();
 module.exports = App;
