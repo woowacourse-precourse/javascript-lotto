d4c883de739b9847c7b422adcc92ba85d75a8598
sujungYu <su_moment@naver.com>
2022-11-13 17:39:33 +0900

    feat(App) : 로또 구입 금액, 당첨 번호, 보너스 번호 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const LottoSet = require('./LottoSet');
+const Lotto = require('./Lotto');
+const Bonus = require('./Bonus');
 class App {
-  play() {}
+  #winning;
+  #bonus;
+  #lottoSet;
+  #money;
+
+  constructor() {
+    this.#lottoSet = [];
+  }
+
+  getMoney() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+      this.#money = Number(money);
+      MissionUtils.Console.print('');
+      const lottoSet = new LottoSet(this.#money);
+      this.#lottoSet = lottoSet.play();
+      this.getWinnings();
+    });
+  }
+
+  getWinnings() {
+    MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.\n', (winning) => {
+      this.#winning = winning.split(',').map(Number);
+      new Lotto(this.#winning);
+      MissionUtils.Console.print('');
+      this.getBonus();
+    });
+  }
+
+  getBonus() {
+    MissionUtils.Console.readLine('보너스 번호를 입력해 주세요.\n', (bonus) => {
+      this.#bonus = Number(bonus);
+      new Bonus(this.#winning, this.#bonus);
+      MissionUtils.Console.print('');
+      this.compareLotto();
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
