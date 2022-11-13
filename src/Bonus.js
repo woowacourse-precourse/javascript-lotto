3bf01b9379f6480225ffef5b6d57c9f201f38aa3
dev-redo <mis05041@naver.com>
2022-11-14 01:33:30 +0900

    feat(Bonus): 보너스 번호의 유효성을 검사하는 Bonus 클래스 구현
    - 보너스 번호의 예외 처리를 위해 ErrorBoundary 클래스를 상속받는다.
    - validate(numbers, bonus): 로또 번호(numbers)와 보너스 번호(bonus)를 인자로 받아 보너스 번호의 유효성을 검사한다.
    - getErrorMessage: 보너스 번호가 유효성을 통과하지 못할시 띄워줄 에러메세지를 return한다.

---
+++ src/Bonus.js
@@ -0,0 +1,43 @@
+const ErrorBoundary = require('./error/ErrorBoundary');
+const { BONUS_ERROR_MESSAGE } = require('./constants/lotto');
+
+const Bonus = class extends ErrorBoundary {
+  #numbers;
+  #bonus;
+
+  constructor(numbers, bonus) {
+    super();
+    this.#numbers = numbers;
+    this.#bonus = bonus;
+    this.setup(this.#numbers, this.#bonus);
+  }
+
+  validate(numbers, bonus) {
+    const isBonusNotDuplicated = numbers.includes(bonus);
+    const isBonusRangeValid = bonus >= 1 && bonus <= 45;
+
+    const isBonusValid = isBonusNotDuplicated && isBonusRangeValid;
+
+    if (isBonusValid === true) {
+      return { status: true };
+    }
+
+    const bonusErrorMessage = this.getBonusErrorMessage({
+      isBonusNotDuplicated,
+      isBonusRangeValid,
+    });
+
+    return { status: false, message: bonusErrorMessage };
+  }
+
+  static getErrorMessage({ isBonusNotDuplicated, isBonusRangeValid }) {
+    const { DUPLICATED, RANGE, DEFAULT } = BONUS_ERROR_MESSAGE;
+
+    if (isBonusNotDuplicated) return DUPLICATED;
+    if (isBonusRangeValid) return RANGE;
+
+    return DEFAULT;
+  }
+};
+
+module.exports = Bonus;
