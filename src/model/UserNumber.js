71b3f34e731e01bf05585dd3f881804d18fa7550
sh981013s <sh981013s@gmail.com>
2022-11-13 13:55:47 +0900

    feat(UserNumber): 유저 구입금액 member variable 추가 및 getter/setter 메서드 추가

--- src/model/UserNumber.js
+++ src/model/UserNumber.js
@@ -1,5 +1,15 @@
 class UserNumber {
-  constructor() {}
+  constructor() {
+    this.purchasingAmount = null;
+  }
+
+  getPurchasingAmount() {
+    return this.getPurchasingAmount;
+  }
+
+  setPurchasingAmount(userPurchasingAmountInput) {
+    this.purchasingAmount = userPurchasingAmountInput;
+  }
 }
 
 module.exports = UserNumber;
