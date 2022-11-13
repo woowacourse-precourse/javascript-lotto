67451ecc720553ceb961c0247b5c1e5494847a33
woo-jk <201702042@o.cnu.ac.kr>
2022-11-14 02:32:19 +0900

    refactor: App 클래스 변수들 private 처리

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ const RankCalculator = require("./RankCalculator");
 const WinningLotto = require("./WinningLotto");
 
 class App {
+  #playerLottos;
+  #winningLotto;
+
   play() {
     this.purchaseProcess();
   }
@@ -16,9 +19,9 @@ class App {
       const lottoGenerator = new LottoGenerator();
 
       const purchaseCount = lottoSeller.getPurchaseCount(moneyInput);
-      this.playerLottos = lottoGenerator.getLottos(purchaseCount);
+      this.#playerLottos = lottoGenerator.getLottos(purchaseCount);
 
-      Utils.printPurchasedLotto(this.playerLottos);
+      Utils.printPurchasedLotto(this.#playerLottos);
 
       this.winningNumberProcess();
     });
@@ -34,7 +37,7 @@ class App {
     Utils.readLine("\n보너스 번호를 입력해 주세요.\n", (bonusNumberInput) => {
       const winningNumber = Utils.separateByComma(winningNumberInput);
       const bonusNumber = Number(bonusNumberInput);
-      this.winningLotto = new WinningLotto(winningNumber, bonusNumber);
+      this.#winningLotto = new WinningLotto(winningNumber, bonusNumber);
 
       this.resultProcess();
     });
@@ -44,9 +47,9 @@ class App {
     const rankCalculator = new RankCalculator();
     const prizeCalculator = new PrizeCalculator();
 
-    const rankCount = rankCalculator.getRankCount(this.playerLottos, this.winningLotto);
+    const rankCount = rankCalculator.getRankCount(this.#playerLottos, this.#winningLotto);
 
-    const playerMoney = this.playerLottos.length * 1000;
+    const playerMoney = this.#playerLottos.length * 1000;
     const prizeMoney = prizeCalculator.getPrizeMoney(rankCount);
     const rateOfReturn = prizeCalculator.getRateOfReturn(playerMoney, prizeMoney);
 
