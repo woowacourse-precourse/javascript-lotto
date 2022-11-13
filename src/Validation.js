3efe8d5f6b56b14d541a3a1b468949cc23d55d07
imhynni <omelas329@gmail.com>
2022-11-13 14:40:20 +0900

    fix: NaN 판단 시 숫자 외 모든 문자 허용 안되도록 변경

--- src/Validation.js
+++ src/Validation.js
@@ -5,17 +5,18 @@ const {
   RANGE_ERROR,
 } = require("./Constants");
 
-const checkNaN = (amount) => {
-  if (isNaN(Number(amount))) return true;
+const checkNaN = (number) => {
+  const check = /^[0-9]+$/;
+  if (!check.test(number)) return true;
 };
 
-const checkRemainder = (amount) => {
-  if (parseInt(amount) % 1000 !== 0) return true;
+const checkRemainder = (number) => {
+  if (parseInt(number) % 1000 !== 0) return true;
 };
 
-const checkPurchaseAmount = (amount) => {
-  if (checkNaN(amount)) throw NAN_ERROR;
-  if (checkRemainder(amount)) throw REMAINDER_ERROR;
+const checkPurchaseAmount = (number) => {
+  if (checkNaN(number)) throw NAN_ERROR;
+  if (checkRemainder(number)) throw REMAINDER_ERROR;
 };
 
 const checkSeparator = (numbers) => {};
