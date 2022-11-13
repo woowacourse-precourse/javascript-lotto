51e9f3f158bd4b95da34943f8b22d4cef631a172
tkdrb12 <tkdrb15@gmail.com>
2022-11-13 22:17:58 +0900

    feat : 당첨 번호를 입력받는 기능 예외처리 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,26 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const set = new Set(numbers);
+    const not_overlap = [...set];
+    if(not_overlap.length != numbers.length)
+      throw new Error("[ERROR] 로또 번호는 서로 중복되지 않아야 합니다.");
+
+    for(const number of numbers){
+      this.validate_EachNumber(number);
+    }
+  }
+
+  validate_EachNumber(number){
+    if(isNaN(number))
+      throw new Error("[ERROR] 숫자를 입력해 주세요.");
+    if(number < 0)
+      throw new Error("[ERROR] 양수를 입력해 주세요.");
+    if(!(number % 1 === 0))
+      throw new Error("[ERROR] 정수를 입력해 주세요.");    
+    if(number<1 || number>45)
+      throw new Error("[ERROR] 1부터 45까지 범위의 숫자를 입력해 주세요.");   
   }
   
   getNumbers(){
