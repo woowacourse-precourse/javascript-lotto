45c3ba55e8fca4b8ceba7b4a83125cf9ab13d3ec
thewronghand <penfreak77@gmail.com>
2022-11-14 04:04:07 +0900

    feat: 외부 모듈 및 라이브러리 호출 구문 추가
    LottoController에서 사용할 외부 라이브러리 및 모듈을 호출하는 구문을 추가한다.

--- src/LottoController.js
+++ src/LottoController.js
@@ -1,4 +1,12 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const LottoPublisher = require('./LottoPublisher.js');
+const LottoStore = require('./LottoStore.js');
+const LottoViewer = require('./LottoViewer.js');
+const Customer = require('./Customer.js');
+
 class LottoController {
   constructor() {}
   init() {}
 }
+
+module.exports = LottoController;
