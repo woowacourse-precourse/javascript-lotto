95e2c593baf9e07c6d08e66ce9fea50fad3484b7
BaeDongWoo <ehddn5410@naver.com>
2022-11-13 22:37:45 +0900

    구매 금액에 맞는 로또 생성

--- src/App.js
+++ src/App.js
@@ -11,10 +11,22 @@ class App {
   }
   costValidate(money){
     if(money<1000){
-      throw new Error("[ERROR] 최소 구입금액은 1000원 이상 이여야합니다");
+      throw new Error("[ERROR] 최소 구입금액은 1000원 이상입니다");
     }
-    if(money%1000!=0){
-      throw new Error("[ERROR] 구입금액은 1000원 단위 이여야합니다");
+    if(parseInt(money%1000)!=0){
+      throw new Error("[ERROR] 구입금액은 1000원 단위입니다");
+    }
+    this.numberofpurchases(money);
+  }
+  numberOfPurchases(money){
+    const number=parseInt(money/1000);
+    this.purchaseLottoNumbers(number);
+  }
+  purchaseLottoNumbers(number){
+    const numbersList=[];
+    for(let i=0;i<number;i++){
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      numbersList.push(numbers);
     }
   }
 }
