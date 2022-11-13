fe05af8e29ec30935392cf6b470cf1c2607a8077
한수지 <fatholic5@gmail.com>
2022-11-13 22:42:06 +0900

    :sparkles: feat: 사용자의 당첨 로또 번호 입력받기 구현 사용자가 잘못된 값을 입력시 예외상황 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ const{ Random, Console } = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 
 const LOTTO_PRICE = 1000;
+const START_LOTTO_NUMBER = 1;
+const END_LOTTO_NUMBER = 45;
+const LOTTO_LENGTH = 6;
 
 class App {
 
@@ -9,31 +12,55 @@ class App {
     this.purchaseAmount = 0;
     this.lottoAmount = 0;
     this.lottoList = {};
+    this.luckyNumbers = 0;
   }
 
   play() {
   return Console.readLine('구입금액을 입력해 주세요.\n', (answer) => {
-      this.validate(answer);
+      this.validatePrice(answer);
       this.purchaseAmount = answer;
       this.countBuyLotto();
     })
   }
 
-  validate(answer) {
+  validatePrice(answer) {
     if(answer % LOTTO_PRICE !== 0) throw new Error(`[ERROR] ${LOTTO_PRICE}원으로 나누어 떨어져야 합니다.`);
   }
 
   countBuyLotto() {
     this.lottoAmount = this.purchaseAmount / LOTTO_PRICE;
     Console.print(`${this.lottoAmount}개를 구매했습니다.`);
+    this.getLottoNumberes();
     this.printOutBuyLotto();
   }
 
+  getLottoNumberes() {
+    let lottoList = [];
+    for(let idex = 0; idex < this.lottoAmount; idex++){
+      let makeLotto = Random.pickUniqueNumbersInRange(START_LOTTO_NUMBER, END_LOTTO_NUMBER, LOTTO_LENGTH);
+      lottoList.push(makeLotto.sort((elementOne, elementTwo) => elementOne - elementTwo));
+    }
+    return this.lottoList = lottoList;
+  }
+
   printOutBuyLotto() {
-    this.lottoList  = Lotto.getLottoNumberes(this.lottoAmount);
     for(let idex = 0; idex < this.lottoAmount; idex++){
       Console.print(`[${this.lottoList[idex].join(", ")}]`);
     }
+    this.getLuckyNumbers();
+  }
+
+  getLuckyNumbers() {
+    Console.readLine('당첨번호를 입력해 주세요.\n', (answer)=>{
+      this.validateLuckyNumbers(answer);
+      this.luckyNumbers = answer;
+    })
+  }
+
+  validateLuckyNumbers(answer) {
+    let checkAnswer = answer.split(',');
+    let lotto = new Lotto(checkAnswer);
+    lotto.validate(checkAnswer);
   }
 }
 let app = new App();
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,5 @@
 const{ Random, Console } = require("@woowacourse/mission-utils");
+const App = require("./App");
 
 const START_LOTTO_NUMBER = 1;
 const END_LOTTO_NUMBER = 45;
@@ -13,19 +14,19 @@ class Lotto {
   }
 
   validate(numbers) {
-    this.numbers = Random.pickUniqueNumbersInRange(START_LOTTO_NUMBER, END_LOTTO_NUMBER, LOTTO_LENGTH);
     if (numbers.length !== LOTTO_LENGTH) throw new Error(`[ERROR] 로또 번호는 ${LOTTO_LENGTH}개여야 합니다.`);
-    if(new Set(numbers).size !== LOTTO_LENGTH) throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    this.validatLuckyNumbers(numbers);
   }
 
   // TODO: 추가 기능 구현
-  static getLottoNumberes(purchaseAmount) {
-    let lottoList = [];
-    for(let idex = 0; idex < purchaseAmount; idex++){
-      let lotto = Random.pickUniqueNumbersInRange(START_LOTTO_NUMBER, END_LOTTO_NUMBER, LOTTO_LENGTH);
-      lottoList.push(lotto.sort((elementOne, elementTwo) => elementOne - elementTwo));
+  validatLuckyNumbers(numbers) {
+    for(let idex = 0; idex < numbers.length; idex++){
+      if(isNaN(numbers[idex]) || numbers.join("").charCodeAt(numbers[idex]) == 32) throw new Error ("[ERROR] 숫자만 입력해주세요.");
     }
-    return lottoList;
+    for(let idex of numbers) {
+      if(idex > 45 || idex < 1) throw new Error(`[ERROR] 로또 번호는 ${START_LOTTO_NUMBER}부터 ${END_LOTTO_NUMBER}까지 입니다.`);
+    }
+    if(new Set(numbers).size !== LOTTO_LENGTH) throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
   }
 }
 
