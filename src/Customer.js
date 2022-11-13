6991572322044254c9f9a0bfd3109ac441ba1efd
thewronghand <penfreak77@gmail.com>
2022-11-14 03:00:53 +0900

    feat: printLottoPurchaseResult 메서드 생성
    구매 결과를 출력하기 위해 customer의 로또 구매 매수와  현재 #lottos의 내용물을 보여주는 메서드를 생성한다.

--- src/Customer.js
+++ src/Customer.js
@@ -12,6 +12,13 @@ class Customer {
   setNewLotto(lotto) {
     this.#lottos = this.#lottos.concat(lotto);
   }
+
+  printLottoPurchaseResult() {
+    MissionUtils.Console.print(`\n${this.#lottos.length}개를 구매했습니다.`);
+    this.#lottos.forEach((element) => {
+      MissionUtils.Console.print(`[${element.numbers}]`);
+    });
+  }
 }
 
 module.exports = Customer;
