7238fd363c7ea3d05dcee91836d5057cc2a3027e
cyberjam <jamsince1994@naver.com>
2022-11-13 19:02:24 +0900

    chore: prettier와 eslint 설정

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true, // 작은따옴표 사용
+  semi: true, // 세미콜론 사용 여부
+  tabWidth: 2, // 탭 너비
+  trailingComma: 'all', // 여러 줄을 사용할 때, 후행 콤마 사용 방식
+  printWidth: 80, // 줄 바꿈할 줄 길이
+  arrowParens: 'always', // 화살표 함수 괄호 사용 방식
+};
