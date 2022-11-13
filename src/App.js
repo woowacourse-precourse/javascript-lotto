4c7575ef8bce3f94a5a124623e32beb772a43798
bae-sh <hyeon1293@gmail.com>
2022-11-13 22:03:33 +0900

    feat : Lotto 숫자 입력

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
+const Lotto = require('./Lotto');
 const { print, readLine, makeLottoNumber } = require('./util');
 class App {
-  #lottos;
-  buyLotto() {
+  #lotto;
+  #myLottos;
+
+  printBoughtLottos({ lottoCount }) {
+    print(`${lottoCount}개를 구매했습니다.`);
+    this.#myLottos.forEach(lotto => print(lotto));
+  }
+
+  inputLottoNumber() {
+    readLine('당첨 번호를 입력해 주세요.\n', numbers => {
+      const lottoNumbers = numbers.split(',');
+
+      this.#lotto = new Lotto(lottoNumbers);
+    });
+  }
+  play() {
     readLine('구입금액을 입력해 주세요.\n', money => {
       const lottoCount = Math.floor(Number(money) / 1000);
 
-      console.log(lottoCount);
-      this.#lottos = makeLottoNumber(lottoCount);
+      this.#myLottos = makeLottoNumber(lottoCount);
       this.printBoughtLottos({ lottoCount });
     });
-  }
-  printBoughtLottos({ lottoCount }) {
-    print(`${lottoCount}개를 구매했습니다.`);
-    this.#lottos.forEach(lotto => print(lotto));
-  }
-  play() {
-    this.buyLotto();
   }
 }
 
