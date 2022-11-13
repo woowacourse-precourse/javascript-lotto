7224331e8ea6469b714c60df50d2d423362b6e1f
seongminn <berry9901@gmail.com>
2022-11-13 21:55:14 +0900

    feat(LottoGameValue): 발행한 로또 번호 출력하는 기능 구현

--- src/model/LottoGameModel.js
+++ src/model/LottoGameModel.js
@@ -13,21 +13,32 @@ class LottoGameModel {
     this.money = money;
 
     this.payMoney();
-    this.generateLottos();
+    this.printLottos(this.lottos);
   }
 
   payMoney() {
     this.lottoCount = this.money / 1000;
 
     LottoGameView.printLottoCount(this.lottoCount);
+
+    this.lottos = this.generateLottos();
   }
 
   generateLottos() {
+    const result = [];
+
     for (let i = 0; i < this.lottoCount; i++) {
       const lottoArr = pickUniqueNumbersInRange(1, 45, 6);
 
-      this.lottos.push(new Lotto(lottoArr));
+      new Lotto(lottoArr);
+      result.push(lottoArr);
     }
+
+    return result;
+  }
+
+  printLottos(lottos) {
+    LottoGameView.printLottoNumbers(lottos);
   }
 }
 
--- src/view/LottoGameView.js
+++ src/view/LottoGameView.js
@@ -4,6 +4,12 @@ class LottoGameView {
   static printLottoCount(count) {
     print(`${count}개를 구매했습니다.`);
   }
+
+  static printLottoNumbers(lottos) {
+    lottos.forEach((lotto) => {
+      print(`[${lotto.join(', ')}]`);
+    });
+  }
 }
 
 module.exports = LottoGameView;
