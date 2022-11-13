eaa99617c9240fdd3687fc7b33bcd810dfb7c03b
Conchip <86344121+Conchip@users.noreply.github.com>
2022-11-14 06:37:05 +0900

    [기능 구현] 구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+const {Random, Console} = require("@woowacourse/mission-utils");
+const {START_MASSAGE, ERROR_MASSAGE} = require("./message.js");
+const isValidUserNumberInput = require("./ValidationCheck.js");
 class App {
-  play() {}
+  constructor() {
+    this.winningNumber = this.generateWinningNumber();
+  }
+  play() {
+    Console.readLine(START_MASSAGE, (userInput) => {
+      const { isValid, errorType } = isValidUserNumberInput(userInput);
+      if(!isValid) {
+        this.handleInputError(errorType);
+      }
+      if(isValid) {
+        Console.print("성공적 입력");
+        this.quitLotto();
+      }
+    })
+  }
+
+  purchaseInput() {
+
+  }
+
+  generateWinningNumber() {
+
+  }
+
+  handleInputError(errorType) {
+    this.quitLotto();
+    throw new Error(ERROR_MASSAGE[errorType]);
+  }
+
+  quitLotto() {
+    Console.close();
+  }
 }
 
 module.exports = App;
