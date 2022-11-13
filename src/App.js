5a7483373540b3aec460bb720609d27e07b37dd9
wonseok <rhdqngkrh77@khu.ac.kr>
2022-11-13 11:41:18 +0900

    fix: 구입금액 입력 함수명 변경

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App {
     this.lottoList = [];
   }
 
-  getCost() {
+  inputCost() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (cost) => {
       if (cost / 1000 < 1) {
         throw new Error("[ERROR] 로또 구입금액이 부족합니다.");
@@ -35,6 +35,7 @@ class App {
       MissionUtils.Console.print(lotto);
     }
   }
+
   play() {}
 }
 
