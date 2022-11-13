711f55c727cd55fe5f2141e071ea8a1413062ec1
d0422 <rlfehd2013@naver.com>
2022-11-13 16:22:36 +0900

    Test: BonusNumber 클래스에 대한 단위테스트 코드 작성 body : 보너스 번호가 정답 로또 번호를 포함하면 예외 발생

---
+++ __tests__/BonusNumberTest.js
@@ -0,0 +1,10 @@
+const BonusNumber = require('../src/Model/BonusNumber');
+const Lotto = require('../src/Model/Lotto');
+
+describe('보너스 숫자 클래스 테스트', () => {
+  test('보너스 번호가 정답로또 번호를 포함하면 오류발생', () => {
+    expect(() => {
+      new BonusNumber(new Lotto([1, 2, 3, 4, 5, 6], 6));
+    }).toThrow('[ERROR]');
+  });
+});
