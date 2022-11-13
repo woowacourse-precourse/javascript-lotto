6bd998de7a375364fa210706c622ecf6515a743e
Doyoung.Kim <dyflower@nate.com>
2022-11-13 12:53:08 +0900

    Feat : 구입 갯수 계산 및 출력 기능

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ const Lotto = require('../src/Lotto');
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
-  constructor() {
-    this.cost = 0;
+  play() {
+    this.inputMoney();
   }
-  play() {}
   inputMoney() {
     MissionUtils.Console.readLine('구입금액을 입력해주세요.\n', (cost) => {
       if (cost % 1000 !== 0) {
         throw new Error('[ERROR] 1,000원 단위 금액을 입력해주세요.');
       }
+      this.countLotto(cost);
     });
+  }
+  countLotto(input) {
+    const cntLotto = input / 1000;
+    MissionUtils.Console.print(`\n${cntLotto}개를 구매했습니다.`);
   }
 }
 
