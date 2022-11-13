d8d0543ea545070a96ce3d28b013534b01683f47
Sim <zh025700@naver.com>
2022-11-13 13:25:59 +0900

    feat(src/LottoGame): 로또 결과 추첨 함수 추가

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -9,10 +9,12 @@ class LottoGame {
   gameCnt;
   userPickLotto;
   userEarn;
+  prizeList;
   constructor() {
     this.money = 0;
     this.gameCnt = 0;
     this.userEarn = 0;
+    this.prizeList = [0, 0, 0, 0, 0, 0, 0, 0];
   }
 
   buyLotto = () => {
@@ -42,30 +44,68 @@ class LottoGame {
 
   inputLottoNum = () => {
     MissionUtils.Console.readLine(STATIC.MESSAGE.LUCKY, (number) => {
-      const lotto = new Lotto(number.split(","));
+      const lotto = new Lotto(number.split(",")).getNumber();
       this.inputBonusNum(lotto);
     });
   };
 
   inputBonusNum = (lotto) => {
     MissionUtils.Console.readLine(STATIC.MESSAGE.BONUS, (number) => {
-      const bonusNumber = new BonusNum(number, lotto.getNumber()).bonusNum;
-      startCheckResult(lotto, this.userPickLotto, bonusNumber);
+      const bonusNumber = new BonusNum(number, lotto).bonusNum;
+      startCheckResult(lotto, this.userPickLotto, bonusNumber, this.prizeList);
     });
   };
 }
 
-const startCheckResult = (lotto, userPickLotto, bonusNumber) => {
-  userPickLotto.map((e) => {
-    checkResult(lotto, e, bonusNumber);
+const startCheckResult = (lotto, userPickLottos, bonusNumber, prizeList) => {
+  userPickLottos.map((e) => {
+    resultExceptBonus = checkResult(lotto, e, prizeList);
+    prizeList.splice(resultExceptBonus, 1, prizeList[resultExceptBonus] + 1);
+    checkBonusNum(e, bonusNumber, prizeList, resultExceptBonus);
   });
-  announceResult();
+
+  announceResult(prizeList);
 };
 
-const checkResult = (lotto, userPickLotto, bonusNumber) => {};
+const checkResult = (lotto, userPickLottoNum, prizeList) => {
+  let correctCnt = 0;
+  lotto.map((number) => {
+    correctCnt += correctLottoNum(userPickLottoNum, number);
+  });
+  return correctCnt;
+};
 
-const announceResult = () => {
+const checkBonusNum = (
+  userPickLotto,
+  bonusNumber,
+  prizeList,
+  resultExceptBonus
+) => {
+  if (resultExceptBonus == 5) {
+    isInBonousNum(userPickLotto, bonusNumber, prizeList);
+  }
+};
+
+const isInBonousNum = (userPickLotto, bonusNumber, prizeList) => {
+  if (userPickLotto.includes(parseInt(bonusNumber))) {
+    prizeList.splice(5, 1, prizeList[5] - 1);
+    prizeList.splice(6, 1, prizeList[6] + 1);
+  }
+};
+
+const correctLottoNum = (userPickLotto, number) => {
+  if (userPickLotto.includes(parseInt(number))) {
+    return 1;
+  }
+  return 0;
+};
+
+const announceResult = (prizeList) => {
   MissionUtils.Console.print(STATIC.MESSAGE.STATISTIC + "\n" + "---");
+  console.log(prizeList);
 };
 
 module.exports = LottoGame;
+
+// 리스트에서 5인걸 봐
+// 보너스가 맞으면 그거 빼고 보너스에 넣자
