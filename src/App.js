80df6eee641278ae91e67567cb1fe96b78ef2fb6
cofla <cofla159@naver.com>
2022-11-14 06:48:17 +0900

    feat: 구매 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Lotto from "./Lotto.js";
+import MissionUtils from "@woowacourse/mission-utils";
+
 class App {
-  play() {}
+  play() {
+    this.buy();
+  }
+  buy() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요. ", (input) => {
+      console.log(input);
+    });
+  }
 }
 
-module.exports = App;
+const app = new App();
+app.play();
+
+export default App;
