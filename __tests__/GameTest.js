9fa4ed8a41b4a3364cba32104f89260a508fc133
안호진 <asdv94@naver.com>
2022-11-14 02:53:24 +0900

    test(lotto): 게임 입력 금액 유효성 검사 테스트 코드

---
+++ __tests__/GameTest.js
@@ -0,0 +1,15 @@
+const Game = require('../src/Game');
+const { ERROR_MSG } = require('../src/Constant');
+
+describe('게임 클래스 테스트', () => {
+  test('입력 금액이 1,000원 단위가 아니면 예외가 발생한다.', () => {
+    const validMoney = 5000;
+    const invalidMoney = 5555;
+    expect(() => {
+      Game.validate(invalidMoney);
+    }).toThrow(ERROR_MSG.only1000WonUnits);
+    expect(() => {
+      Game.validate(validMoney);
+    }).not.toThrow(ERROR_MSG.only1000WonUnits);
+  });
+});
