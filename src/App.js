80a7c77863bcef21a665ace4daf2664176b3dacd
BBIYAC <kiy7833@gmail.com>
2022-11-13 21:40:45 +0900

    feat(App): get purchase amount
    - 구입 금액 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const { print, close } = require("@woowacourse/mission-utils").Console;
+
 class App {
-  play() {}
+  play() {
+    print('구입금액을 입력해 주세요.');
+    readLine('', (purchaseAmount) =>  {
+      close();
+    });
+  }
 }
 
 module.exports = App;
