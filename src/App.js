51e200f1203b643aa4d2d2ae05b63476b2430893
aram <snsdl0905@naver.com>
2022-11-13 22:12:52 +0900

    feat: 로또 구입 금액을 입력받는다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {}
+
+  InputMoney(){
+    MissionUtils.Console.readLine('구입 금액을 입력해주세요 : ', (money) => {
+      this.money = parseInt(money);
+      let amount = money/1000;
+    });
+  }
 }
 
 module.exports = App;
