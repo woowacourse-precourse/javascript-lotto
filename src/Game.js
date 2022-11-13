39788c775d560f0607d7293aee67e06ef9e2b226
oseunghyun <ohsh1477@gmail.com>
2022-11-13 20:58:37 +0900

    feat(Game): implement the lotto game

--- src/Game.js
+++ src/Game.js
@@ -1,36 +1,77 @@
 const { Console } = require('@woowacourse/mission-utils');
-const Lotto = require('./Lotto');
 const LottoGenerator = require('./domain/LottoGenerator');
+const LottoCalculator = require('./domain/LottoCalculator');
+const WinningLotto = require('./Lotto');
 
 class Game {
   constructor() {
-    this.lottos;
-    this.bonusNum;
+    this.winningLotto = new WinningLotto();
+    this.lottoGenerator = new LottoGenerator();
   }
+
   play() {
-    Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
-      this.getGeneratedLotto(money);
-      this.getLuckyLotto();
+    this.buyLotto().then(() => {
+      this.generateLuckyLotto().then(() => {
+        this.getResultOfLotto(
+          this.lottoGenerator.lottos,
+          this.winningLotto.lotto,
+          this.winningLotto.bonusNum
+        );
+      });
     });
   }
 
-  getGeneratedLotto(money) {
-    const lottoGenerator = new LottoGenerator(parseInt(money));
-    Console.print(`\n${lottoGenerator.numOfLottos}개를 구매했습니다.`);
-    this.lottos = lottoGenerator.lottos;
-    return lottoGenerator.lottos.forEach((lotto) => Console.print(lotto));
-  }
-
-  getLuckyLotto() {
-    Console.readLine('\n당첨 번호를 입력해 주세요.\n', (winningLotto) => {
-      const lotto = new Lotto(winningLotto.split(','));
-      return this.getBonusNum();
+  buyLotto() {
+    return new Promise((resolve) => {
+      Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+        const generatedLottos = this.lottoGenerator.generateLottos(money);
+        resolve(generatedLottos.forEach((lotto) => Console.print(lotto)));
+      });
     });
   }
 
-  getBonusNum() {
-    Console.readLine('\n보너스 번호를 입력해 주세요.\n', (bonusNum) => {
-      this.bonusNum = bonusNum;
+  async generateLuckyLotto() {
+    return new Promise((resolve) => {
+      Console.readLine('\n당첨 번호를 입력해 주세요.\n', (lotto) => {
+        lotto = lotto.split(',').map((num) => {
+          return parseInt(num);
+        });
+        resolve(this.winningLotto.setLotto(lotto));
+      });
+    }).then(() => {
+      return new Promise((resolve) => {
+        Console.readLine('\n보너스 번호를 입력해 주세요.\n', (bonusNum) => {
+          resolve(this.winningLotto.setBonusNum(bonusNum));
+        });
+      });
+    });
+  }
+
+  getResultOfLotto(lottos, winningLotto, bonusNum) {
+    return new Promise((resolve) => {
+      const lottoCalculator = new LottoCalculator(
+        lottos,
+        winningLotto,
+        bonusNum
+      );
+
+      const YIELD = lottoCalculator.calculateYield();
+      resolve(
+        Console.print('\n당첨 통계\n---'),
+        Console.print(`3개 일치 (5,000원) - ${lottoCalculator.score['3']}개`),
+        Console.print(`4개 일치 (50,000원) - ${lottoCalculator.score['4']}개`),
+        Console.print(
+          `5개 일치 (1,500,000원) - ${lottoCalculator.score['5']}개`
+        ),
+        Console.print(
+          `5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoCalculator.score['bonus']}개`
+        ),
+        Console.print(
+          `6개 일치 (2,000,000,000원) - ${lottoCalculator.score['6']}개`
+        ),
+        Console.print(`총 수익률은 ${YIELD}%입니다.`),
+        Console.close()
+      );
     });
   }
 }
