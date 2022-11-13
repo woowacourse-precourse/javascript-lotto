7ab45a026f9d8eeb87318c60f93e644f72c6e093
freemoon99 <soloist132@gmail.com>
2022-11-14 00:07:27 +0900

    feat: 당첨 내역 문구 출력

--- src/App.js
+++ src/App.js
@@ -118,6 +118,16 @@ class App {
       this.winnig[4] += 1;
       this.reward += 2000000000;
     }
+    this.result();
+  }
+
+  result() {
+    MissionUtils.Console.print('당첨 통계\n---');
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${this.winnig[0]}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${this.winnig[1]}개`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${this.winnig[2]}개`);
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.winnig[3]}개`);
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${this.winnig[4]}개`);
   }
 }
 
