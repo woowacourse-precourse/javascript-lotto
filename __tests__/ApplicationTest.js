f931f8cfe115d709441b1610c3dfdd67f2f0caad
imb96 <kimminje7267@naver.com>
2022-11-13 15:30:47 +0900

    test : 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -60,9 +60,47 @@ describe("로또 테스트", () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
     });
   });
+  test("기능 테스트", () => {
+    mockRandoms([
+      [5, 12, 21, 23, 25, 34],
+      [1, 2, 6, 8, 34, 42],
+      [15, 18, 28, 41, 43, 44],
+      [1, 8, 19, 20, 24, 29],
+      [11, 17, 19, 31, 32, 45],
+    ]);
+    mockQuestions(["5000", "1,2,6,8,34,3", "42"]);
+    const logs = [
+      "5개를 구매했습니다.",
+      "[5, 12, 21, 23, 25, 34]",
+      "[1, 2, 6, 8, 34, 42]",
+      "[15, 18, 28, 41, 43, 44]",
+      "[1, 8, 19, 20, 24, 29]",
+      "[11, 17, 19, 31, 32, 45]",
+      "3개 일치 (5,000원) - 0개",
+      "4개 일치 (50,000원) - 0개",
+      "5개 일치 (1,500,000원) - 0개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+      "6개 일치 (2,000,000,000원) - 0개",
+      "총 수익률은 600000.0%입니다.",
+    ];
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
 
   test("예외 테스트", () => {
     mockQuestions(["1000j"]);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR]");
+  });
+
+  test("예외 테스트", () => {
+    mockQuestions(["1200"]);
     expect(() => {
       const app = new App();
       app.play();
