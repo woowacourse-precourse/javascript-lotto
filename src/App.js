2cea49da6a23723f89c1e1e0ed503b29b4e5e0cc
wjs5025 <wjs_5025@naver.com>
2022-11-14 00:59:55 +0900

    feat : 보너스 번호 입력 기능 추가(App.js의 setBonusNumber) 및 유효성 검증 기능 추가(validator.js의 validateBonusNumber)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ const { Console } = require("@woowacourse/mission-utils");
 const {
   validatePurchaseAmount,
   validateWiningNumber,
+  validateBonusNumber,
 } = require("./utils/validator");
 
 const Lotto = require("./Lotto");
 class App {
   constructor() {
-    this.purchaseAmount = 0;
-    this.winningNumbers = [];
+    this.purchaseAmount = 0; // 구입 금액
+    this.purchasesCount = 0; // 구매한 로또 개수
+    this.winningNumbers = []; // 당첨 번호
+    this.bonusNumber = 0; // 보너스 번호
   }
 
   play() {
-    this.setWinningNumbers();
+    this.setPurchaseAmount();
   }
 
   setPurchaseAmount() {
@@ -20,6 +23,7 @@ class App {
       if (validatePurchaseAmount(money)) {
         // 구입 금액 유효성 검증을 통과한다면, 구입 금액 저장 후 출력
         this.purchaseAmount = +money;
+        this.purchasesCount = +money / 1000;
         Console.print(this.purchaseAmount);
       }
       Console.close();
@@ -27,8 +31,15 @@ class App {
   }
 
   setWinningNumbers() {
-    Console.readLine("당첨 번호를 입력해 주세요.", (numbers) => {
+    Console.readLine("당첨 번호를 입력해주세요.", (numbers) => {
       this.winningNumbers = [...validateWiningNumber(numbers)];
+      Console.close();
+    });
+  }
+
+  setBonusNumber() {
+    Console.readLine("보너스 번호를 입력해주세요.", (number) => {
+      this.bonusNumber = validateBonusNumber(number);
       Console.close();
     });
   }
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -4,8 +4,10 @@ const { MESSAGES } = require("../constraints");
 구입 금액을 유효성 검증하고, 통과 시 true 반환
  */
 const validatePurchaseAmount = (money) => {
-  if (isNaN(+money)) throw new Error(MESSAGES.BUYING.TYPE_EXCEPTION);
-  if (+money % 1000 !== 0) throw new Error(MESSAGES.BUYING.UNIT_EXCEPTION);
+  if (isNaN(+money))
+    throw new Error(MESSAGES.EXCEPTIONS.PURCHASE.TYPE_EXCEPTION);
+  if (+money % 1000 !== 0)
+    throw new Error(MESSAGES.EXCEPTIONS.PURCHASE.UNIT_EXCEPTION);
 
   return true;
 };
@@ -16,18 +18,23 @@ const validateWiningNumber = (numbers) => {
   numbers = numbers.split(",").map((el) => +el);
 
   if (numbers.includes(NaN))
-    throw new Error(MESSAGES.WINNING_NUMBERS.TYPE_EXCEPTION);
+    throw new Error(MESSAGES.EXCEPTIONS.TYPE_EXCEPTION);
   if (numbers.length !== 6)
-    throw new Error(MESSAGES.WINNING_NUMBERS.COUNT_EXCEPTION);
+    throw new Error(MESSAGES.EXCEPTIONS.COUNT_EXCEPTION);
   if (numbers.filter((el) => +el < 1 || +el > 45).length > 0)
-    throw new Error(MESSAGES.WINNING_NUMBERS.RANGE_EXCEPTION);
+    throw new Error(MESSAGES.EXCEPTIONS.RANGE_EXCEPTION);
   if ([...new Set(numbers)].length !== 6)
-    throw new Error(MESSAGES.WINNING_NUMBERS.DUPLICATION_EXCEPTION);
+    throw new Error(MESSAGES.EXCEPTIONS.DUPLICATION_EXCEPTION);
 
   return numbers;
 };
 
-const validateBonusNumber = (number) => {};
+const validateBonusNumber = (number) => {
+  number = +number;
+  if (number === NaN) throw new Error(MESSAGES.EXCEPTIONS.TYPE_EXCEPTION);
+  if (number < 1 || number > 45)
+    throw new Error(MESSAGES.EXCEPTIONS.RANGE_EXCEPTION);
+};
 
 module.exports = {
   validatePurchaseAmount,
