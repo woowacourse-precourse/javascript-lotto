ecb061031366cb40e9f39ce034a06cef6675cd75
evencoding <evencoding@gmail.com>
2022-11-13 18:01:34 +0900

    test: 하나의 로또에 대한 당첨 등수를 반환하는 기능 테스트

--- __tests__/GameToolsTest.js
+++ __tests__/GameToolsTest.js
@@ -1,20 +1,19 @@
-const {
-  stringToSortedNumberArray,
-  calcTotalPrize,
-} = require('../src/GameTools');
+const { Console } = require('@woowacourse/mission-utils');
 const GameTools = require('../src/GameTools');
+
+afterAll(() => Console.close());
 
 describe('로또 발행 테스트', () => {});
 
 test('입력받은 당첨 번호를 ,로 구분된 숫자 배열로 반환', () => {
-  const winningString = ['2,3,4,5,6,1', '31,1,24,45,3,4'];
+  const winningStrings = ['2,3,4,5,6,1', '31,1,24,45,3,4'];
   const answers = [
     [1, 2, 3, 4, 5, 6],
     [1, 3, 4, 24, 31, 45],
   ];
 
-  winningString.forEach((string, i) => {
-    expect(stringToSortedNumberArray(string)).toEqual(answers[i]);
+  winningStrings.forEach((string, i) => {
+    expect(GameTools.stringToSortedNumberArray(string)).toEqual(answers[i]);
   });
 });
 
@@ -38,8 +37,31 @@ test('두 배열이 모두 가진 숫자 요소의 개수를 반환', () => {
   });
 });
 
+test('하나의 로또에 대한 당첨 등수를 반환', () => {
+  const matchingCounts = [1, 2, 3, 3, 4, 4, 5, 5, 6, 6];
+  const matchesBonusNums = [
+    false,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    true,
+  ];
+  const answers = [-1, -1, 4, 4, 3, 3, 2, 1, 0, 0];
+
+  matchingCounts.forEach((count, i) => {
+    expect(GameTools.getWinningRanking(count, matchesBonusNums[i])).toBe(
+      answers[i]
+    );
+  });
+});
+
 test('총 상금을 계산해 반환', () => {
-  const winningState = [
+  const winningStates = [
     [0, 0, 0, 0, 1],
     [0, 0, 0, 4, 6],
     [0, 0, 3, 0, 3],
@@ -48,17 +70,17 @@ test('총 상금을 계산해 반환', () => {
   ];
   const answers = [5000, 230000, 4515000, 73510000, 4037660000];
 
-  winningState.forEach((state, i) => {
-    expect(calcTotalPrize(state)).toBe(answers[i]);
+  winningStates.forEach((state, i) => {
+    expect(GameTools.calcTotalPrize(state)).toBe(answers[i]);
   });
 });
 
 test('수익율을 계산해 소수점 둘 째 자리에서 반올림한 값을 반환', () => {
-  const totalPrize = [150000, 5000, 30000, 30000000, 4000000000];
+  const totalPrizes = [150000, 5000, 30000, 30000000, 4000000000];
   const countOfLottos = [3, 4, 5, 10, 20];
   const answers = ['5000.0', '125.0', '600.0', '300000.0', '20000000.0'];
 
-  totalPrize.forEach((money, i) => {
+  totalPrizes.forEach((money, i) => {
     expect(GameTools.calcRateOfReturn(money, countOfLottos[i])).toBe(
       answers[i]
     );
