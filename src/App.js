5797afd447d24358e867aacf7218f9ff50c2396f
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-13 23:34:32 +0900

    feat(isNotNumber): 사용자의 구입 금액이 숫자가 아닌 경우 예외 처리
    Add
      isNotNumber(userEnterAmount): 공백이나 문자가 포함되어있으면 예외 출력

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ class App {
 
   #userEnterAmount;
 
+  isNotNumber(userEnterAmount) {
+    if(isNaN(userEnterAmount) || /\s/g.test(userEnterAmount)) {
+      throw new Error("[ERROR] 로또 구입 금액은 공백이 포함되지 않은 숫자 형태로 입력해야 합니다.")
+    }
+  }
+
   isInDivisible(userEnterAmount) {
     if(Number(userEnterAmount) % 1000 !== 0) {
       throw new Error('[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다.')
