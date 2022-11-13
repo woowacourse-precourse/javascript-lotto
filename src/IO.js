deee948e2a6c40cb1e6bc52957f16f23a9632e0f
HojinAn <asdv94@naver.com>
2022-11-14 08:29:18 +0900

    feat(lotto): UI 로직 관리 위한 IO 기능 구현

--- src/IO.js
+++ src/IO.js
@@ -1,9 +1,22 @@
 const { ERROR_MSG } = require('./Constant');
+const { Console } = require('@woowacourse/mission-utils');
 
 class IO {
   static validate(input) {
     if (/^\D|[^\d{1,2}]|,{2,}|\D[,]|,$/g.test(input.trim()))
       throw new Error(ERROR_MSG.prefix + ERROR_MSG.notNumber);
+  }
+  static print(msg) {
+    Console.print(msg);
+  }
+  static readLine(msg, callback) {
+    Console.readLine(msg, (input) => {
+      IO.validate(input);
+      callback(input);
+    });
+  }
+  static close() {
+    Console.close();
   }
 }
 
