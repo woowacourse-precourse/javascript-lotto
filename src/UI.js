8b493343277446a965d7dbb97254ff1e914638f3
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-13 23:30:02 +0900

    refactor(function): move function calling statement
    Move function calling statement in UI class
    - Remove calling statement of makeSpaceAfterSection method from getCash method
    - Add calling statement of makeSpaceAfterSection method in getUserInput function
    
    Now getUserInput can automatically make space after itself worked

--- src/UI.js
+++ src/UI.js
@@ -2,6 +2,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const ErrorCase = require("./ErrorCase");
 
 const CASH_INPUT_MESSAGE = "구입금액을 입력해 주세요.\n";
+
 const PURCHASE_AMOUNT_NOTICE_MESSAGE = "개를 구매했습니다.";
 
 const CASH_INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 금액입니다. 금액은 숫자로 입력해주세요.";
@@ -11,6 +12,8 @@ const getUserInput = (message, errorHandler) => {
     MissionUtils.Console.readLine(message, (userInput) => {
       const isWrongInput = errorHandler(userInput);
       if (isWrongInput) reject();
+
+      UI.makeSpaceAfterSection();
 
       resolve(userInput);
     })
@@ -25,8 +28,6 @@ class UI {
   static async getCash() {
     try {
       const cash = await getUserInput(CASH_INPUT_MESSAGE, ErrorCase.isWrongCashInput);
-
-      UI.makeSpaceAfterSection();
 
       return cash;
     } catch {
