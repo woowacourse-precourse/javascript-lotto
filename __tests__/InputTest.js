299088003300c75d64b25c3ebb32b54e4ade18fc
Sangjun-man <molung@icloud.com>
2022-11-13 23:12:14 +0900

    test : 테스트 케이스 추가
    값 입력이 통과했을 때의 케이스를 추가했습니다

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -13,59 +13,65 @@ const mockQuestions = (answers) => {
 
 const { inputType } = keys;
 
-const errorCase = [
+const testCase = [
   ["나 숫자아님", "-1", "", "23"],
-  ["0,0.,..", "1,2,3,4,5,6,7", "1,,2,3,4,5,6", "1,1,2,3,4,5", ",1,2,3,4,5,6"],
-  ["나 숫자아님", "56", 1],
+  [
+    "0,0.,..",
+    "1,2,3,4,5,6,7",
+    "1,,2,3,4,5,6",
+    "1,1,2,3,4,5",
+    ",1,2,3,4,5,6",
+    "10,2,43,1,5,7",
+  ],
+  ["나 숫자아님", "56", "1", "7"],
 ];
 
 describe("Input 클래스 테스트", () => {
-  mockQuestions(errorCase.flat());
+  mockQuestions(testCase.flat());
   describe("1 구입금액 입력 테스트", () => {
-    test("1-1 숫자가 아닌 값을 입력했을 때", () => {
+    test("1-1 숫자가 아닌 값을 입력했을 때 예외 발생", () => {
       expect(() =>
         Input.getValueWithType(inputType.purchase_money, (inputValue) => {})
       ).toThrow();
     });
-    test("1-2 음수의 값을 입력했을 때", () => {
+    test("1-2 음수의 값을 입력했을 때 예외 발생", () => {
       expect(() =>
         Input.getValueWithType(inputType.purchase_money, () => {})
       ).toThrow();
     });
-    test("1-3 값을 입력하지 않았을 때", () => {
+    test("1-3 값을 입력하지 않았을 때 예외 발생", () => {
       expect(() =>
         Input.getValueWithType(inputType.purchase_money, (InputValue) => {})
       ).toThrow();
     });
-    test("1-4 정확한 값을 입력했을 때", () => {
-      () =>
-        Input.getValueWithType(inputType.purchase_money, (InputValue) => {
-          expect(inputType).toEqual("23");
-        });
+    test("1-4 정확한 값을 입력했을 때 통과", () => {
+      Input.getValueWithType(inputType.purchase_money, (InputValue) => {
+        expect(InputValue).toEqual("23");
+      });
     });
   });
   describe("2. 당첨번호 입력 테스트", () => {
-    test("2-1 ,와 숫자 이외의 값을 받았을 때", () => {
+    test("2-1 ,와 숫자 이외의 값을 받았을 때 예외 발생", () => {
       expect(() =>
         Input.getValueWithType(inputType.winning_number, (InputValue) => {})
       ).toThrow();
     });
-    test("2-2 배열로 받은 숫자가 더 많을때", () => {
+    test("2-2 배열로 받은 숫자가 더 많을 때 예외 발생", () => {
       expect(() =>
         Input.getValueWithType(inputType.winning_number, (InputValue) => {})
       ).toThrow();
     });
-    test("2-3 ,가 두번이상 연속으로 입력되었을 때", () => {
+    test("2-3 ,가 두번이상 연속으로 입력되었을 때 예외 발생", () => {
       expect(() =>
         Input.getValueWithType(inputType.winning_number, (InputValue) => {})
       ).toThrow();
     });
-    test("2-4 중복되는 숫자를 입력받았을 때", () => {
+    test("2-4 중복되는 숫자를 입력받았을 때 예외 발생", () => {
       expect(() =>
         Input.getValueWithType(inputType.winning_number, (InputValue) => {})
       ).toThrow();
     });
-    test("2-5 ,가 먼저 입력되었을 때", () => {
+    test("2-5 ,가 먼저 입력되었을 때 예외 발생", () => {
       expect(() =>
         Input.getValueWithType(inputType.winning_number, (InputValue) => {
           // 입력된 배열로 값을 테스트하는 과정에서 에러 처리, -> 뒤의 console.log 함수를 실행시켜 주었을때 에러 발생
@@ -73,10 +79,16 @@ describe("Input 클래스 테스트", () => {
         })
       ).toThrow();
     });
+    test("2-6 정확한 값이 입력되었을 때 통과", () => {
+      Input.getValueWithType(inputType.winning_number, (InputValue) => {
+        // 입력된 배열로 값을 테스트하는 과정에서 에러 처리, -> 뒤의 console.log 함수를 실행시켜 주었을때 에러 발생
+        expect(InputValue).toEqual("10,2,43,1,5,7");
+      });
+    });
   });
   describe("3. 보너스번호 입력 테스트", () => {
     const mockWinningNumber = [1, 2, 3, 4, 5, 6];
-    test("3-1 숫자가 아닌 값을 입력했을 때", () => {
+    test("3-1 숫자가 아닌 값을 입력했을 때 예외 발생", () => {
       expect(() =>
         Input.getValueWithType(
           inputType.bonus_number,
@@ -85,7 +97,7 @@ describe("Input 클래스 테스트", () => {
         )
       ).toThrow();
     });
-    test("3-2 로또 숫자 범위가 아닌 값을 입력했을 때", () => {
+    test("3-2 로또 숫자 범위가 아닌 값을 입력했을 때 예외 발생", () => {
       expect(() =>
         Input.getValueWithType(
           inputType.bonus_number,
@@ -94,12 +106,24 @@ describe("Input 클래스 테스트", () => {
         )
       ).toThrow();
     });
-    test("3-3 당첨번호와 중복되는 값을 입력했을 때", () => {
+    test("3-3 당첨번호와 중복되는 값을 입력했을 때 예외 발생", () => {
       expect(() =>
-        Input.getValueWithType(inputType.bonus_number, (InputValue) => {
-          mockWinningNumber;
-        })
+        Input.getValueWithType(
+          inputType.bonus_number,
+          (InputValue) => {},
+          mockWinningNumber
+        )
       ).toThrow();
+    });
+
+    test("3-4 정확한 값을 입력했을 때 통과", () => {
+      Input.getValueWithType(
+        inputType.bonus_number,
+        (InputValue) => {
+          expect(InputValue).toEqual("7");
+        },
+        mockWinningNumber
+      );
     });
   });
 });
