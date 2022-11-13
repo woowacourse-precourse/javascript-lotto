388347b76fdd3fd62a54b32ca37f3cd92a1a5432
freemoon99 <soloist132@gmail.com>
2022-11-13 23:46:42 +0900

    feat: 1000원단위 입력 확인 기능
    1000원 단위로 입력 및 예외 처리

--- src/Money.js
+++ src/Money.js
@@ -3,12 +3,19 @@ class Money {
 
   constructor(money) {
     this.isNumber(money);
+    this.isDivide(money);
     this.#money = money;
   }
 
   isNumber(money) {
     if (!Number(money)) {
       throw new Error('[ERROR] 숫자를 입력해주세요.');
+    }
+  }
+
+  isDivide(money) {
+    if (money % 1000 !== 0) {
+      throw new Error('[ERROR] 1000단위로 입력해주세요.');
     }
   }
 }
