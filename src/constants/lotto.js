f0bfd021dd9bf9e1c8ea3e973caf5fd561141fcd
Leejin-Yang <ejin1996@gmail.com>
2022-11-13 21:40:23 +0900

    feat: 로또 랭크 상수화

--- src/constants/lotto.js
+++ src/constants/lotto.js
@@ -10,11 +10,37 @@ const GAME_MESSAGE = Object.freeze({
   LOTTO_NUMBER_INPUT: '당첨 번호를 입력해 주세요.\n',
   BONUS_NUMBER_INPUT: '보너스 번호를 입력해 주세요.\n',
   RESULT_TITLE: '당첨 통계\n---',
-  FIFTH_PLACE: '3개 일치 (5,000원) - ',
-  FOURTH_PLACE: '4개 일치 (50,000원) - ',
-  THIRD_PLACE: '5개 일치 (1,500,000원) - ',
-  SECOND_PLACE: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
-  FIRST_PLACE: '6개 일치 (2,000,000,000원) - ',
 });
 
-module.exports = { RULE, GAME_MESSAGE };
+const RANK = Object.freeze({
+  FIFTH: 'fifth',
+  FOURTH: 'fourth',
+  THIRD: 'third',
+  SECOND: 'second',
+  FIRST: 'first',
+});
+
+const RANK_LENGTH = Object.freeze({
+  FIFTH: 3,
+  FOURTH: 4,
+  THIRD: 5,
+  FIRST: 6,
+});
+
+const RANK_REWARDS = Object.freeze({
+  FIFTH: 5_000,
+  FOURTH: 50_000,
+  THIRD: 1_500_000,
+  SECOND: 30_000_000,
+  FIRST: 2_000_000_000,
+});
+
+const RANK_MESSAGE = Object.freeze({
+  FIFTH: `3개 일치 (${RANK_REWARDS.FIFTH.toLocaleString()}원) - `,
+  FOURTH: `4개 일치 (${RANK_REWARDS.FOURTH.toLocaleString()}원) - `,
+  THIRD: `5개 일치 (${RANK_REWARDS.THIRD.toLocaleString()}원) - `,
+  SECOND: `5개 일치, 보너스 볼 일치 (${RANK_REWARDS.SECOND.toLocaleString()}원) - `,
+  FIRST: `6개 일치 (${RANK_REWARDS.FIRST.toLocaleString()}원) - `,
+});
+
+module.exports = { RULE, GAME_MESSAGE, RANK, RANK_LENGTH, RANK_REWARDS, RANK_MESSAGE };
