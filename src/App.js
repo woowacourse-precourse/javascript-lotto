eacb23b2c0e64b70c0aad84258c9f4e40d0ad72c
hxezin <leehxejin@gmail.com>
2022-11-14 00:29:04 +0900

    feat: 구매 가능한 로또 개수 구함

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   constructor() {
     this.money = 0;
+    this.lottoCount = 0;
   }
 
   play() {
     MissionUtils.Console.readLine("구매금액을 입력해 주세요.\n", (answer) => {
       this.validateMoney(answer);
       this.money = parseInt(answer);
+      this.getLottoCount();
     });
   }
 
@@ -21,6 +23,11 @@ class App {
     if (money % 1000 !== 0) {
       throw new Error("[ERROR] 구매금액은 1,000원 단위여야 합니다.");
     }
+  }
+
+  getLottoCount() {
+    const count = this.money / 1000;
+    this.lottoCount = count;
   }
 }
 
