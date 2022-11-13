8ae8578c5d99b85f307b3037090df7af1b31fb6d
bae-sh <hyeon1293@gmail.com>
2022-11-13 21:25:10 +0900

    feat : 로또 생성 함수 정렬 기능 추가

--- src/util.js
+++ src/util.js
@@ -8,13 +8,17 @@ const readLine = (content, func) => {
   MissionUtils.Console.readLine(content, func);
 };
 
-const makeRandomLottoNumber = count => {
+const makeLottoNumber = count => {
   const lottoNumbers = [];
 
   for (let i = 0; i < count; i++) {
-    const lottoNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    const lottoNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+      (a, b) => a - b,
+    );
 
     lottoNumbers.push(lottoNumber);
   }
   return lottoNumbers;
 };
+
+module.exports = { print, readLine, makeLottoNumber };
