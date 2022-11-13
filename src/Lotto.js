51a3ea6cb4f547aef035ffd1522c2d2eda2cdba2
Jeong Hun Nam <njh3421@gmail.com>
2022-11-14 03:16:32 +0900

    fix: fix validate function undefined err

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,17 +4,22 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    // this.validate(numbers);
+    this.validate(numbers);
     this.#numbers = numbers;
   }
 
   validate(numbers) {
-    // if (numbers.length !== 6) {
-    //   throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    // }
+    if(numbers!==undefined){
+      let numArr=numbers.split(',')
+      if (numArr.length !== 6) {
+        throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      }
+    }
+
   }
 
   // TODO: 추가 기능 구현
+
   setComputerRandomNumber() {
     const computerNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1,45,6);
     return computerNumbers;
@@ -30,6 +35,8 @@ class Lotto {
     MissionUtils.Console.readLine(
       "당첨 번호를 입력해 주세요.\n",
       (userLottoNumber) => {
+        this.#numbers=userLottoNumber
+        this.validate(this.#numbers)
         this.getUserBonusNumber(computerNumberArray, userLottoNumber,amountOfMoney);
       }
     );
