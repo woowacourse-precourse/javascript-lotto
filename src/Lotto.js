45e0f97dd7b3a9e42fe9a887573f8bccf214a2f1
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-11-14 00:23:59 +0900

    Feat: 로또번호 뽑기 함수 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,7 @@
+const Random = require("@woowacourse/mission-utils").Random;
+const Console = require("@woowacourse/mission-utils").Console;
+const inputValidation = require("./inputValidation");
+
 class Lotto {
   #numbers;
 
@@ -7,12 +11,18 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    const checkSixNum = inputValidation.checkSixNum(numbers);
   }
 
-  // TODO: 추가 기능 구현
+  //로또 뽑기 logic
+  randomLottoNumber() {
+    const lottoNum = Random.pickUniqueNumbersInRange(1, 45, 6);
+    return lottoNum;
+  }
+
+  randomBonusNumber() {
+    
+  }
 }
 
 module.exports = Lotto;
---
+++ src/inputValidation.js
@@ -0,0 +1,9 @@
+const inputValidation = {
+  checkSixNum(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  },
+};
+
+module.exports = inputValidation;
