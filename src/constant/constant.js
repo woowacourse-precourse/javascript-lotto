e12070d1ab402fa11a3330005bbc33a71d54d83a
김아현 <dhflxhdxhd@gmail.com>
2022-11-13 23:34:50 +0900

    Feat : 메세지 상수 그룹으로 묶기

---
+++ src/constant/constant.js
@@ -0,0 +1,7 @@
+const START_MESSAGE = "구입금액을 입력해 주세요.";
+const WINNING_MESSAGE = "당첨 번호를 입력해 주세요.";
+const BONUS_MESSAGE = "보너스 번호를 입력해 주세요."
+
+const NUMBER_MESSAGE = "개를 구매했습니다.";
+
+const RESULT_MESSAGE = "당첨 통계\n---";
