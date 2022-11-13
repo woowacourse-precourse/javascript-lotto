3a2ba2f9f4a5d99815afbddccdb23a675dfeca36
han0224 <dbn0224@gmail.com>
2022-11-13 15:08:54 +0900

    Test: 로또 등수 확인 테스트 코드 추가
    1등부터 5등까지 등수를 제대로 반환하는지 테스트코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -14,5 +14,34 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  // 로또 등수 확인
+  test("로또 1등.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.compare([1, 2, 3, 4, 5, 6], 7)).toBe(1);
+  });
+
+  test("로또 2등.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 7]);
+    expect(lotto.compare([1, 2, 3, 4, 5, 6], 7)).toBe(2);
+  });
+
+  test("로또 3등.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 6, 8]);
+    expect(lotto.compare([1, 2, 3, 4, 5, 6], 7)).toBe(3);
+  });
+
+  test("로또 4등.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 7, 8]);
+    expect(lotto.compare([1, 2, 3, 4, 5, 6], 7)).toBe(4);
+  });
+
+  test("로또 5등.", () => {
+    const lotto = new Lotto([1, 2, 3, 7, 8, 9]);
+    expect(lotto.compare([1, 2, 3, 4, 5, 6], 7)).toBe(5);
+  });
+
+  test("당첨되지 못한 경우", () => {
+    const lotto = new Lotto([1, 2, 7, 8, 9, 10]);
+    expect(lotto.compare([1, 2, 3, 4, 5, 6], 7)).toBe(0);
+  });
 });
