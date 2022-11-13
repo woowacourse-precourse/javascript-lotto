63ebbd61b897f7731f19cdbf417d6229e90278ec
cofla <cofla159@naver.com>
2022-11-14 07:00:39 +0900

    feat: 구매 금액 에러 발생 후 종료

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ class App {
   validatePurchase(input) {
     if (isNaN(input)) {
       throw new Error("[ERROR] 구매 금액을 숫자로 입력해주세요.");
+      MissionUtils.Console.close();
     }
     if (Number(input) % 1000 !== 0) {
       throw new Error("[ERROR] 구매 금액을 1000원 단위로 입력해주세요.");
+      MissionUtils.Console.close();
     }
   }
 }
