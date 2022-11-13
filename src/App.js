91ff7d47b6af9c9a4555698185ce743aafb23608
nalsae <nalsae0207@naver.com>
2022-11-13 23:01:11 +0900

    feat: implement a method to get winning number
    로또 당첨 번호를 입력 받아 저장하는 메서드 구현 완료
    - Lotto 인스턴스를 생성할 때 사용자의 입력 값을 인수로 전달하여 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-const Lotto = require("./Lotto");
-const { Console, Random } = require("@woowacourse/mission-utils");
+const Lotto = require('./Lotto');
+const { Console, Random } = require('@woowacourse/mission-utils');
 
 class App {
   #lottos = [];
@@ -9,7 +9,7 @@ class App {
   }
 
   startGame() {
-    Console.readLine("구입 금액을 입력해주세요.\n", (price) => {
+    Console.readLine('구입 금액을 입력해주세요.\n', (price) => {
       this.findPriceError(price);
 
       this.issueLotto(price);
@@ -28,19 +28,31 @@ class App {
       this.#lottos = [...this.#lottos, lotto];
       Console.print(lotto);
     });
+
+    this.getWinningNumber();
+  }
+
+  getWinningNumber() {
+    Console.readLine(`\n당첨 번호를 입력해 주세요.`, (winningNums) => {
+      const winningNums = winningNums.split(',');
+
+      this.findWinningNumberError();
+    });
   }
 
   findPriceError(price) {
     if (
       Number.isNaN(+price) ||
-      typeof +price !== "number" ||
+      typeof +price !== 'number' ||
       price % 1000 !== 0
     ) {
       throw new Error(
-        "[ERROR] 구입 금액은 1000원 단위의 숫자로 입력해야 합니다."
+        '[ERROR] 구입 금액은 1000원 단위의 숫자로 입력해야 합니다.'
       );
     }
   }
+
+  findWinningNumberError(winningNums) {}
 }
 
 const app = new App();
