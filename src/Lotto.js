0001b5427650268954dcc63be15b93795b82510a
dhkham <khamdong@gmail.com>
2022-11-13 15:51:25 +0900

    feat(Lotto.js): add method that checks for duplicates in lottery numbers

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,15 +2,23 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.validateLength(numbers);
+    this.validateNoDuplicates(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
+  validateLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
+
+  validateNoDuplicates(numbers) {
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
+  }
+
 
   // TODO: 추가 기능 구현
 }
