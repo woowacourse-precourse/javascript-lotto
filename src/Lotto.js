a6b5c6f1167a62a73971bf386f777cb0e8e85b10
wish0ne <wish@khu.ac.kr>
2022-11-14 02:23:34 +0900

    feat: add validate options of Lotto
    Lotto number should be number type.
    Lotto number's range should be 1 to 45.

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,8 +8,15 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    numbers.forEach((number) => {
+      if (typeof number !== 'number')
+        throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+      if (number < 1 || number > 45)
+        throw new Error('[ERROR] 로또 번호의 범위는 1 ~ 45여야 합니다.');
+    });
   }
 
   // TODO: 추가 기능 구현
