ce678845809ea6318d8af78295ec5d60df206d62
2je0 <bulljy@naver.com>
2022-11-13 21:01:10 +0900

    ✨ feat: 랜덤로또를 입력받아 일치하는 개수와 보너스 일치여부를 알려주는 기능 추가

--- src/WinningLotto.js
+++ src/WinningLotto.js
@@ -22,6 +22,22 @@ class WinningLotto extends Lotto {
     if (!Number.isInteger(bonus)) throw new Error(MESSAGE.ERROR_NO_INTEGER);
     if (bonus < 1 || bonus > 45) throw new Error(MESSAGE.ERROR_OUT_OF_RANGE);
   }
-}
 
+  /**
+   * 랜덤 로또 하나를 인자로 받아서 [일치하는 번호개수, 보너스 일치 개수] 를 반환한다.
+   * @param {Lotto} lotto - 로또
+   * @return {Array<number>} [matched number 개수, bonus matched]
+   */
+  getNumOfMatchedAndBonus(lotto) {
+    let matched = 0;
+    let bonus = 0;
+    const winningNums = this.getNums();
+    const lottoNums = lotto.getNums();
+    lottoNums.forEach((Num) => {
+      if (winningNums.includes(Num)) matched += 1;
+      if (Num === this.bonus) bonus = 1;
+    });
+    return [matched, bonus];
+  }
+}
 module.exports = WinningLotto;
