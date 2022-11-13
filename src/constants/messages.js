7e992ff2de72840fa9adfaa613c916db11082b9c
JangHoonSeok <orirori512@naver.com>
2022-11-13 22:18:28 +0900

    ✨ feat: 에러 메세지 상수 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -3,5 +3,8 @@ const INPUT_MESSAGE = Object.freeze({
   WINNER_INPUT: `당첨 번호를 입력해 주세요.`,
   BONUS_INPUT: `보너스 번호를 입력해 주세요.`,
 });
-
-module.exports = { INPUT_MESSAGE };
+const ERROR_MESSAGE = Object.freeze({
+  CANT_DIVIDE: `[ERROR] 구입 금액이 1000원으로 나누어 떨어지지 않습니다.`,
+  NOT_NUMBER: `[ERROR] 입력값이 숫자여야 합니다.`,
+});
+module.exports = { INPUT_MESSAGE, ERROR_MESSAGE };
