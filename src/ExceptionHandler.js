6ab169e7f6e1702fe96dfdc3f3831c24124d07a1
Buyoung Chung <sunarol@naver.com>
2022-11-13 17:24:26 +0900

    refactor: remove winning number redundancy exception
    winning number length exception can cover this

--- src/ExceptionHandler.js
+++ src/ExceptionHandler.js
@@ -9,11 +9,6 @@ class ExceptionHandler {
     if (winningNumber.split(Constant.COMMA).length !== 6) throw new Error("[ERROR] 숫자를 6개 입력해주세요.");
   }
 
-  static winningNumberRedundancy(winningNumber) {
-    const checkSet = new Set(winningNumber.split(Constant.COMMA));
-    if ([...checkSet].length < 6) throw new Error("[ERROR] 당첨 번호를 중복없이 입력해주세요.");
-  }
-
   static bonusNumberRange(bonusNumber) {
     if (bonusNumber < 1 && bonusNumber > 45) {
       throw new Error("[ERROR] 1부터 45까지의 숫자를 입력해주세요.");
