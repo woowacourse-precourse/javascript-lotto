193da311fcd3acaa197b15031d48f72e2e8d4cb9
yeongi <cbg5255@gmail.com>
2022-11-13 20:44:28 +0900

    test : LottoCaculate 로또 번호 비교 테스트 작성

--- __tests__/LottoCalculatorTest.js
+++ __tests__/LottoCalculatorTest.js
@@ -7,40 +7,22 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-const lottoSetting = new LottoSetting();
-describe("로또 당첨 세팅 테스트", () => {
-  test("로또 당첨 입력 확인", () => {
-    const numbers = ["1,2,3,4,5,6", "3,4,5,6,7,8", "8,21,23,41,42,43"];
-    const result = [
+const LottoCalculate = new LottoSetting();
+describe("로또 당첨 집계 테스트", () => {
+  test("로또 번호 확인 테스트", () => {
+    const winLotto = [1, 2, 3, 4, 5, 6];
+    const bonusNum = 8;
+    const myLotto = [
       [1, 2, 3, 4, 5, 6],
-      [3, 4, 5, 6, 7, 8],
-      [8, 21, 23, 41, 42, 43],
+      [1, 2, 3, 4, 5, 8],
+      [1, 2, 3, 4, 6, 10],
+      [1, 2, 3, 4, 13, 16],
+      [1, 2, 3, 9, 10, 12],
     ];
-    const logSpy = getLogSpy();
-    numbers.forEach((number) => {
-      lottoSetting.winNumToLottoClass(number);
-    });
-    result.forEach((numbers) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.arrayContaining(numbers));
-    });
-  });
-
-  test("로또 입력 예외처리 테스트", () => {
-    const logs = ["1,2,3,4", "1,2,3,4,5,5,3,4,"];
-    logs.forEach((log) => {
-      expect(() => {
-        lottoSetting.winNumToLottoClass(log);
-      }).toThrow("[ERROR]");
-    });
-  });
-
-  test("보너스 번호 입력 예외 처리 테스트", () => {
-    lottoSetting.winNumToLottoClass("1,2,3,4,5,6");
-    const logs = ["1", "99", "1,2"];
-    logs.forEach((log) => {
-      expect(() => {
-        lottoSetting.isBonusNum(log);
-      }).toThrow("[ERROR]");
+    let Rank = 1;
+    myLotto.forEach((lotto) => {
+      expect(LottoCalculate.compareLotto(lotto, winLotto, bonusNum)).toBe(Rank);
+      Rank++;
     });
   });
 });
