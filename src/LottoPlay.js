757082c182f7aa6e6ead9de3cccef30a19b7deb2
JangHoonSeok <orirori512@naver.com>
2022-11-13 23:47:41 +0900

    ✨ feat: LottoPlay의 play함수 업데이트

--- src/LottoPlay.js
+++ src/LottoPlay.js
@@ -1,11 +1,12 @@
-const UserInterface = require("./src/UserInterface.js");
-const CheckError = require("./src/CheckError.js");
+const UserInterface = require("./UserInterface.js");
+const CheckError = require("./CheckError.js");
 class LottoPlay {
   // Lotto 게임 기능 구현하는 클래스.
+  constructor() {}
   play() {
     const purchaseAmount = UserInterface.purchaseRequest(); // 구입 금액 입력.
     CheckError.checkPurchaseAmount(purchaseAmount); //
-    this.purchaseLotto(purchaseAmount);
+    UserInterface.printLottoNumber(this.purchaseLotto(purchaseAmount));
   }
 
   purchaseLotto(purchaseAmount) {
