311426598d6021c62cc03db116dffd5940f3bd3e
woo-jk <201702042@o.cnu.ac.kr>
2022-11-13 10:07:02 +0900

    refactor: Lotto, WinningLotto 클래스 validate 구조 변경 및  private 접근자 적용

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,29 +4,34 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.#validate(numbers);
     this.#numbers = numbers;
   }
 
-  isIncorrectRange(numbers) {
-    const filteredNumbers = numbers.filter((number) => 1 <= number && number <= 45);
-    return filteredNumbers.length !== numbers.length;
-  }
-
-  isDuplicated(numbers) {
-    return numbers.length !== new Set(numbers).size;
-  }
-
-  validate(numbers) {
+  #validateNumberLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    if (this.isIncorrectRange(numbers)) {
-      throw new Error("[ERROR] 로또 번호의 범위는 1~45 사이여야 합니다.");
-    }
-    if (this.isDuplicated(numbers)) {
+  }
+
+  #validateNumberRange(numbers) {
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호의 범위는 1~45 사이여야 합니다.");
+      }
+    });
+  }
+
+  #validateNumberDuplicate(numbers) {
+    if (numbers.length !== new Set(numbers).size) {
       throw new Error("[ERROR] 로또 번호는 서로 중복 되지 않아야 합니다.");
     }
+  }
+
+  #validate(numbers) {
+    this.#validateNumberLength(numbers);
+    this.#validateNumberRange(numbers);
+    this.#validateNumberDuplicate(numbers);
   }
 
   get numbers() {
--- src/WinningLotto.js
+++ src/WinningLotto.js
@@ -5,11 +5,11 @@ class WinningLotto extends Lotto {
 
   constructor(numbers, bonusNumber) {
     super(numbers);
-    this.validateBonus(bonusNumber);
+    this.#validateBonus(bonusNumber);
     this.#bonusNumber = bonusNumber;
   }
 
-  validateBonus(bonusNumber) {
+  #validateBonus(bonusNumber) {
     if (bonusNumber < 1 || bonusNumber > 45) {
       throw new Error("[ERROR] 보너스 번호의 범위는 1~45 사이여야 합니다.");
     }
