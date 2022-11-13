418842125d4c70e623ec4cae98748a2b7bf0083b
강인영 <kiy@gang-in-yeong-ui-MacBookAir.local>
2022-11-13 18:24:19 +0900

    style: Add base error message [ERROR]

--- src/constant.js
+++ src/constant.js
@@ -1,9 +1,10 @@
+const BASE_MESSAGE = "[ERROR] ";
 const ERROR_MESSAGE = Object.freeze({
-    BLANK: "아무것도 입력되지 않았습니다.",
-    SIX_DIGIT: "로또 번호는 6개여야 합니다.",
-    DUPLICATE: "중복된 숫자가 있습니다.",
-    NOT_NUMBER: "숫자만 입력해 주세요.",
-    WRONG_INPUT: "잘못된 값을 입력하여 게임을 종료합니다."
+    BLANK: BASE_MESSAGE + "아무것도 입력되지 않았습니다.",
+    SIX_DIGIT: BASE_MESSAGE + "로또 번호는 6개여야 합니다.",
+    DUPLICATE: BASE_MESSAGE + "중복된 숫자가 있습니다.",
+    NOT_NUMBER: BASE_MESSAGE + "숫자만 입력해 주세요.",
+    WRONG_INPUT: BASE_MESSAGE + "잘못된 값을 입력하여 게임을 종료합니다."
 })
 
 /*
