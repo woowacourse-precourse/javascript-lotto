796b980c6fcee347a3543de7dab0eb7542c402bd
myung-j-p <jmpack97@gmail.com>
2022-11-13 21:18:03 +0900

    Feat: 당첨 내역 출력

--- src/Lotto.js
+++ src/Lotto.js
@@ -71,7 +71,15 @@ class Lotto {
   }
 
   printResult(correctCount, correctBonus) {
-
+    MissionUtils.Console.print(
+      '\n당첨 통계\n'+
+      '---\n'+
+      `3개 일치 (5,000원) - ${correctCount[0]['3개']}개\n`+
+      `4개 일치 (50,000원) - ${correctCount[1]['4개']}개\n`+
+      `5개 일치 (1,500,000원) - ${correctCount[2]['5개']}개\n`+
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${correctBonus}개\n`+
+      `6개 일치 (2,000,000,000원) - ${correctCount[3]['6개']}개`
+      )
   }
 
 }
