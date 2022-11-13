70e99e83ed770b0456f09d1d140aeda699be4c82
evencoding <evencoding@gmail.com>
2022-11-13 17:19:43 +0900

    test: 총 상금을 계산해 반환하는 기능 테스트

--- __tests__/GameToolsTest.js
+++ __tests__/GameToolsTest.js
@@ -1,9 +1,12 @@
-const { stringToSortedNumberArray } = require('../src/GameTools');
+const {
+  stringToSortedNumberArray,
+  calcTotalPrize,
+} = require('../src/GameTools');
 const GameTools = require('../src/GameTools');
 
 describe('로또 발행 테스트', () => {});
 
-test.only('입력받은 당첨 번호를 ,로 구분된 숫자 배열로 반환', () => {
+test('입력받은 당첨 번호를 ,로 구분된 숫자 배열로 반환', () => {
   const winningString = ['2,3,4,5,6,1', '31,1,24,45,3,4'];
   const answers = [
     [1, 2, 3, 4, 5, 6],
@@ -35,6 +38,19 @@ test('두 배열이 모두 가진 숫자 요소의 개수를 반환', () => {
   });
 });
 
-test('총 상금을 계산해 반환', () => {});
+test.only('총 상금을 계산해 반환', () => {
+  const winningState = [
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 4, 6],
+    [0, 0, 3, 0, 3],
+    [0, 2, 9, 0, 2],
+    [2, 1, 5, 3, 2],
+  ];
+  const answers = [5000, 230000, 4515000, 73510000, 4037660000];
+
+  winningState.forEach((state, i) => {
+    expect(calcTotalPrize(state)).toBe(answers[i]);
+  });
+});
 
 describe('수익율을 계산해 반환', () => {});
