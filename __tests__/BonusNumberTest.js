7c1e9f831f05a0f759b03b14c464aee70dfe067e
Subin Jo <sbjo1202@naver.com>
2022-11-13 22:07:30 +0900

    test: 보너스 번호 입력 예외 테스트 - 당첨 번호가 중복인 경우

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -28,7 +28,7 @@ describe("보너스 번호 입력 테스트", () => {
       manager.inputWinningNumbers();
     }).toThrow("[ERROR]");
   });
-  
+
   test("1부터 45사이의 값이 아닌 경우 예외가 발생한다. - 1미만", () => {
     mockQuestions(["1,2,3,4,5,6", 0]);
 
@@ -40,6 +40,15 @@ describe("보너스 번호 입력 테스트", () => {
 
   test("1부터 45사이의 값이 아닌 경우 예외가 발생한다. - 45초과", () => {
     mockQuestions(["1,2,3,4,5,6", 46]);
+
+    expect(() => {
+      const manager = new LottoManager();
+      manager.inputWinningNumbers();
+    }).toThrow("[ERROR]");
+  });
+
+  test("당첨 번호와 중복인 경우 예외가 발생한다.", () => {
+    mockQuestions(["1,2,3,4,5,6", 3]);
 
     expect(() => {
       const manager = new LottoManager();
