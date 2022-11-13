137440c1b1c08050b825c1bcfa88f09f2d1c65a0
Yebin Kim <kyb4312@gmail.com>
2022-11-14 02:26:59 +0900

    feat(LottoIssuer): implement function to generate lotto numbers

--- src/LottoIssuer.js
+++ src/LottoIssuer.js
@@ -1,6 +1,16 @@
+const { Random } = require('@woowacourse/mission-utils');
+
 class LottoIssuer {
   calculateNumberOfLottoes(money) {
     return money / 1000;
+  }
+
+  generateLottoNumbers() {
+    return Random.pickUniqueNumbersInRange(1, 45, 6).sort(this.compareNumbers);
+  }
+
+  compareNumbers(a, b) {
+    return a - b;
   }
 }
 
