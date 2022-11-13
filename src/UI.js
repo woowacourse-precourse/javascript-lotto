88729c3e850803ae75b762d5f894eda1dfced0ee
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-13 23:41:44 +0900

    feat(method): add getBasicWinningNumbers
    Add getBasicWinningNumbers method to UI class
    - Try to get input of winning numbers and return winning numbers
    - Catch error if it fails

--- src/UI.js
+++ src/UI.js
@@ -2,10 +2,12 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const ErrorCase = require("./ErrorCase");
 
 const CASH_INPUT_MESSAGE = "구입금액을 입력해 주세요.\n";
+const WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.\n";
 
 const PURCHASE_AMOUNT_NOTICE_MESSAGE = "개를 구매했습니다.";
 
 const CASH_INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 금액입니다. 금액은 숫자로 입력해주세요.";
+const WINNING_NUMBERS_ERROR_MESSAGE = "[ERROR] 1부터 45까지의 정수 중 중복되지 않는 수 6개를 쉼표로 분리해 입력해주세요.";
 
 const getUserInput = (message, errorHandler) => {
   return new Promise((resolve, reject) =>
@@ -51,6 +53,16 @@ class UI {
     UI.printPurchasedLottoList(purchased);
 
     UI.makeSpaceAfterSection();
+  }
+
+  static async getBasicWinningNumbers() {
+    try {
+      const winningNumbers = await getUserInput(WINNING_NUMBERS_INPUT_MESSAGE, ErrorCase.isWrongWinningNumbersInput);
+
+      return winningNumbers;
+    } catch {
+      throw new Error(WINNING_NUMBERS_ERROR_MESSAGE);
+    }
   }
 }
 
