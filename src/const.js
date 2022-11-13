2809daedbc2953a5241fb24c4140ad80cac6e0a6
bae-sh <hyeon1293@gmail.com>
2022-11-14 01:31:30 +0900

    feat : 상수 수정

---
+++ src/const.js
@@ -0,0 +1,10 @@
+const PRIZE_NAME = ['fail', 'first', 'second', 'third', 'forth', 'fifth'];
+
+const PRIZE_REWARD = Object.freeze({
+  first: 2000000000,
+  second: 30000000,
+  third: 1500000,
+  forth: 50000,
+  fifth: 5000,
+});
+module.exports = { PRIZE_NAME, PRIZE_REWARD };
