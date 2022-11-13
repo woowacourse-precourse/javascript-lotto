3785ad1c6f33070c8e2f3cd5afc7245d700cf0ea
Leejin-Yang <ejin1996@gmail.com>
2022-11-13 21:44:29 +0900

    refactor: 로또 비교자 상수 적용 및 로또 랭크 기능 조건문 -> 객체값

--- src/LottoComparer.js
+++ src/LottoComparer.js
@@ -1,6 +1,14 @@
+const { RANK, RANK_LENGTH } = require('./constants/lotto');
+
 class LottoComparer {
   static getBuyerLottoRank(buyerLottos, lotto, bonusNumber) {
-    const result = { first: 0, second: 0, third: 0, fourth: 0, fifth: 0 };
+    const result = {
+      [RANK.FIRST]: 0,
+      [RANK.SECOND]: 0,
+      [RANK.THIRD]: 0,
+      [RANK.FOURTH]: 0,
+      [RANK.FIFTH]: 0,
+    };
 
     buyerLottos.forEach((buyerLotto) => {
       const rank = LottoComparer.#getLottoRank(buyerLotto, lotto, bonusNumber);
@@ -18,27 +26,19 @@ class LottoComparer {
   static #getLottoRank(buyerLotto, lotto, bonusNumber) {
     const matchingLength = LottoComparer.#compareLotto(buyerLotto, lotto);
 
-    if (matchingLength === 3) {
-      return 'fifth';
+    if (matchingLength === RANK_LENGTH.THIRD && buyerLotto.includes(bonusNumber)) {
+      return RANK.SECOND;
     }
 
-    if (matchingLength === 4) {
-      return 'fourth';
-    }
+    const rank =
+      {
+        [RANK_LENGTH.FIFTH]: RANK.FIFTH,
+        [RANK_LENGTH.FOURTH]: RANK.FOURTH,
+        [RANK_LENGTH.THIRD]: RANK.THIRD,
+        [RANK_LENGTH.FIRST]: RANK.FIRST,
+      }[matchingLength] ?? undefined;
 
-    if (matchingLength === 5) {
-      if (buyerLotto.includes(bonusNumber)) {
-        return 'second';
-      }
-
-      return 'third';
-    }
-
-    if (matchingLength === 6) {
-      return 'first';
-    }
-
-    return undefined;
+    return rank;
   }
 
   static #compareLotto(buyerLotto, lotto) {
