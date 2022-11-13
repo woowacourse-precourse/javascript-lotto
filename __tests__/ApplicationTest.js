de4a199ed93a32822fad46fa2e71e5795fd61529
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-14 01:47:31 +0900

    test: 보너스 번호 입력 예외 테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -128,4 +128,24 @@ describe("로또 테스트", () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
     });
   });
+
+  test("보너스 번호가 당첨 번호와 중복되면 예외 발생", () => {
+    const ERROR_MESSAGE = "[ERROR] 당첨 번호와 중복되지 않는 번호를 입력해주세요.";
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+    ]);
+    mockQuestions(["3000", "11,12,13,14,15,16", "12"]);
+    const logs = [
+      "3개를 구매했습니다.",
+      "[8, 21, 23, 41, 42, 43]",
+      "[3, 5, 11, 16, 32, 38]",
+      "[7, 11, 16, 35, 36, 44]",
+    ];
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR]");
+  });
 });
