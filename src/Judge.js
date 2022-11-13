de3d14404d9a9aafaec93e1edd8d8ce9840aa89b
안호진 <asdv94@naver.com>
2022-11-13 22:58:20 +0900

    feat(lotto): 로또, 당첨 번호 일치 개수 세는 기능 구현

--- src/Judge.js
+++ src/Judge.js
@@ -1,5 +1,7 @@
 class Judge {
-  static countCorrect() {}
+  static countCorrect(lotto, winnings) {
+    return lotto.reduce((cnt, no) => (cnt += winnings.includes(no) ? 1 : 0), 0);
+  }
 }
 
 module.exports = Judge;
