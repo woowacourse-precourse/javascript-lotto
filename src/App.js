254c501d402f5e50f50c689e5be6de9f0b6bd438
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-14 01:58:56 +0900

    feat: 로또 당첨 번호를 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 const { Console } = require('@woowacourse/mission-utils');
 const UserBudget = require('./UserBudget');
 const LottoMachine = require('./LottoMachine');
+const Lotto = require('./Lotto');
 const INPUT_BUDGET = '구입금액을 입력해 주세요.\n';
 
 class App {
   budget;
   lottoTickets;
+  lottoWinNumbers;
 
   printSpaceLine() {
     Console.print('');
@@ -13,6 +15,7 @@ class App {
 
   play() {
     this.getUserBudget(INPUT_BUDGET);
+    this.getWinLottoNumbers();
   }
 
   getUserBudget(INPUT_BUDGET) {
@@ -25,6 +28,16 @@ class App {
   validateUserBudget(budget) {
     this.budget = new UserBudget(Number(budget));
     this.lottoTickets = new LottoMachine(budget);
+  }
+
+  getWinLottoNumbers() {
+    Console.readLine('당첨 번호를 입력해 주세요.\n', (input) => {
+      this.checkLottoNumbers(input);
+    });
+  }
+
+  checkLottoNumbers(number) {
+    this.lottoWinNumbers = new Lotto(number);
   }
 }
 
