cad28b96b6f398cabb39ba3f4a7054fd127b468e
somin <smhs12@naver.com>
2022-11-13 16:33:05 +0900

    test: UserLotto클래스 calculateMatchResult함수 테스트

--- __tests__/UserLottoTest.js
+++ __tests__/UserLottoTest.js
@@ -15,3 +15,19 @@ describe("UserLotto클래스 createUserLotto함수 테스트", () => {
     expect(lotto.userLottoList.length).toEqual(amount / 1000);
   });
 });
+
+describe("UserLotto클래스 calculateMatchResult함수 테스트", () => {
+  test("당첨 번호 일치 갯수 별로 나누는 기능 테스트", () => {
+    const userLotto = new UserLotto();
+    const countResult = [[3], [4], [1], [2], [5]];
+    const matchResult = userLotto.calculateMatchResult(countResult);
+    expect(matchResult).toEqual({ 3: 1, 4: 1, 5: 1, 6: 0, containBonus: 0 });
+  });
+
+  test("당첨 번호 일치 갯수 별로 나누는 기능 테스트", () => {
+    const userLotto = new UserLotto();
+    const countResult = [[1], [6], [3], [4], [5, 1], [3], [4], [2], [1], [5]];
+    const matchResult = userLotto.calculateMatchResult(countResult);
+    expect(matchResult).toEqual({ 3: 2, 4: 2, 5: 1, 6: 1, containBonus: 1 });
+  });
+});
