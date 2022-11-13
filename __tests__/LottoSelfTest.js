297a2145a4fdee5311a3d7994563112ea951c0bb
Yeom-JinHo <pwlsghq@naver.com>
2022-11-14 00:02:27 +0900

    test : 로또 당첨번호  예외

--- __tests__/LottoSelfTest.js
+++ __tests__/LottoSelfTest.js
@@ -1,5 +1,5 @@
 const Lotto = require('../src/Lotto');
-const { LOTTO_LENGTH } = require('../src/constants');
+const { LOTTO_LENGTH, ERR_MSG } = require('../src/constants');
 
 describe('셀프 로또 테스트', () => {
   test('로또 구입 금액 예외 테스트', () => {
@@ -13,5 +13,20 @@ describe('셀프 로또 테스트', () => {
       expect(lotto.length).toEqual([...new Set(lotto)].length);
     });
     expect(lottoList.length).toEqual(TEST_LEN);
+  });
+
+  test('로또 당첨 번호 예외 테스트', () => {
+    const winningNumbers = [
+      [1, 2, 3, 4, 5, 6, 7],
+      [1, 2, 'a', 'b', 1, 3],
+      [1, 1, 1, 1, 1, 1, 1],
+      ['', '', '', '', '', '']
+    ];
+
+    winningNumbers.forEach(numbers => {
+      expect(() => {
+        new Lotto(numbers);
+      }).toThrow('[ERROR]');
+    });
   });
 });
