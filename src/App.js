f4bf9f50842d080acaab10a8f8b28323ae2732fa
nara04040 <nara04040@naver.com>
2022-11-13 14:48:08 +0900

    feat : 구입금액을 입력받는 console을 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-class App {
-  play() {}
-}
+const MissionUtils = require("@woowacourse/mission-utils");
+const readline = require("readline");
 
+class App {
+  constructor() {}
+
+  paymentInput() {
+    MissionUtils.Console.readLine("구입금액을 입력해주세요.", (answer) => {
+      console.log(`구입금액: ${answer}`);
+    });
+  }
+  play() {
+    this.paymentInput();
+  }
+}
+const app = new App();
+app.play();
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -16,5 +16,4 @@ class Lotto {
 
   // TODO: 추가 기능 구현
 }
-
 module.exports = Lotto;
