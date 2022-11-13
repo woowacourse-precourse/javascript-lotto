3377a4b5240f265b4c9ffd7a7053d4e4d8d3eb77
hxezin <leehxejin@gmail.com>
2022-11-14 00:28:42 +0900

    feat: 입력받은 구매금액이 유효한지 확인

--- src/App.js
+++ src/App.js
@@ -7,8 +7,20 @@ class App {
 
   play() {
     MissionUtils.Console.readLine("구매금액을 입력해 주세요.\n", (answer) => {
-      this.money = answer;
+      this.validateMoney(answer);
+      this.money = parseInt(answer);
     });
+  }
+
+  validateMoney(value) {
+    const reg = /^\d+$/;
+    if (!reg.test(value)) {
+      throw new Error("[ERROR] 구매금액은 숫자여야 합니다.");
+    }
+    const money = parseInt(value);
+    if (money % 1000 !== 0) {
+      throw new Error("[ERROR] 구매금액은 1,000원 단위여야 합니다.");
+    }
   }
 }
 
