59296143d7e7b02deff1ee934fae7fea4f9245d7
seoyeon <wjstjdus96@naver.com>
2022-11-13 16:43:00 +0900

    feat: 당첨번호 비교

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,7 +12,25 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  // 당첨번호 비교
+  isWinning(answer, bonus) {
+    let rank;
+    const winningNum = this.#numbers.filter((item) =>
+      answer.includes(item)
+    ).length;
+
+    if (winningNum == 3) {
+      rank = 5;
+    } else if (winningNum == 4) {
+      rank = 4;
+    } else if (winningNum == 5) {
+      this.isBonus(bonus) ? (rank = 2) : (rank = 3);
+    } else if (winningNum == 6) {
+      rank = 1;
+    }
+
+    return rank;
+  }
 }
 
 module.exports = Lotto;
