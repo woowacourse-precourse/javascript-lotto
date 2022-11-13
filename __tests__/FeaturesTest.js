9fe92e1416f282863cd61985811f90349696a519
hailey-hy <lillyine.hy@gmail.com>
2022-11-14 08:07:34 +0900

    test(FeaturesTest): 보너스 번호 중복 여부를 예외 처리하는 테스트 추가

--- __tests__/FeaturesTest.js
+++ __tests__/FeaturesTest.js
@@ -7,4 +7,11 @@ describe('기능 테스트', () => {
             '[ERROR] 로또를 살 수 없는 금액입니다.'
         );
     });
+
+    test('보너스 번호 중복 여부 예외 처리', () => {
+        const app = new App();
+        expect(() => app.isBonusDuplicated(1, [1, 2, 3, 4, 5, 6])).toThrow(
+            '[ERROR] 중복된 번호입니다.'
+        );
+    });
 })
