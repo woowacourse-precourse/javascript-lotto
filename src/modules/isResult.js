ba9266ba69d6fd855c673e458c3a0c836c393876
Hyun Su <tlsgustn93@naver.com>
2022-11-13 15:09:57 +0900

    refactor: 함수 정리

--- src/modules/isResult.js
+++ src/modules/isResult.js
@@ -16,7 +16,9 @@ class IsResult {
 
     for (let key in result) {
       MissionUtils.Console.print(
-        `${result[key].match} (${parseInt(result[key].price).toLocaleString()}원) - ${result[key].count}개`
+        `${result[key].match} (${parseInt(
+          result[key].price
+        ).toLocaleString()}원) - ${result[key].count}개`
       );
     }
 
@@ -27,68 +29,32 @@ class IsResult {
 
   resultProcess() {
     const result = {
-      1: {
-        match: "6개 일치",
-        price: LOTTO_PRIZE[1],
-        count: 0,
-      },
-      2: {
-        match: "5개 일치, 보너스 볼 일치",
-        price: LOTTO_PRIZE[2],
-        count: 0,
-      },
-      3: {
-        match: "5개 일치",
-        price: LOTTO_PRIZE[3],
-        count: 0,
-      },
-      4: {
-        match: "4개 일치",
-        price: LOTTO_PRIZE[4],
-        count: 0,
-      },
-      5: {
-        match: "3개 일치",
-        price: LOTTO_PRIZE[5],
-        count: 0,
-      },
+      1: { match: "6개 일치", price: LOTTO_PRIZE[1], count: 0 },
+      2: { match: "5개 일치, 보너스 볼 일치", price: LOTTO_PRIZE[2], count: 0 },
+      3: { match: "5개 일치", price: LOTTO_PRIZE[3], count: 0 },
+      4: { match: "4개 일치", price: LOTTO_PRIZE[4], count: 0 },
+      5: { match: "3개 일치", price: LOTTO_PRIZE[5], count: 0 },
     };
+    this.prizeProcess(result)
+  }
 
+  prizeProcess(result) {
     let count = 0;
     let totalWin = 0;
     this.userLottoArray.forEach((lotto) => {
       lotto.forEach((test) => {
         if (this.winningNumArray.includes(test)) count++;
       });
-
-      switch (count) {
-        case 3:
-          totalWin += LOTTO_PRIZE[5];
-          result[5].count += 1;
-          break;
-        case 4:
-          totalWin += LOTTO_PRIZE[4];
-          result[4].count += 1;
-          break;
-        case 5:
-          if (lotto.includes(this.bonusNum)) {
-            totalWin += LOTTO_PRIZE[2];
-            result[2].count += 1;
-          } else {
-            totalWin += LOTTO_PRIZE[3];
-            result[3].count += 1;
-          }
-          break;
-        case 6:
-          totalWin += LOTTO_PRIZE[1];
-          result[1].count += 1;
-          break;
-      }
+      if(count === 3) result[5].count++
+      if(count === 4) result[4].count++
+      if(count === 5) lotto.includes(this.bonusNum) ? result[2].count++ : result[3].count++
+      if(count === 6) result[1].count++
       count = 0;
+
+      for(let i=1; i<6; i++) totalWin += LOTTO_PRIZE[i] * result[i].count
     });
     this.resultPrint(result, totalWin);
   }
-
 }
 
 module.exports = IsResult;
