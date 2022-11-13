6908eea7fae0934400229ba7baf761586aa2611c
sunub <bsc5672@gmail.com>
2022-11-14 01:49:22 +0900

    feat : 도메인 로직3에 대한 단위 테스트 추가
    마지막 도메일 로직에 해당하는 보너스 숫자를 입력한 경우에 대한 테스트
    케이스에 대해서 추가 하였다. 보너스 숫자가 이전의 로또 번호와 중첩이
    되는지에 대한 테스트를 추가 하였다.

--- __tests__/my.test.js
+++ __tests__/my.test.js
@@ -1,14 +1,16 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const App = require("../src/App.js");
 const Lotto = require("../src/Lotto.js");
+const Generator = require("../src/Generator.js");
 const Checker = require("../src/Checker.js");
 
+const generator = new Generator();
+const checker = new Checker();
+
 describe("입력한 금액 값", () => {
-  const app = new App();
   const answer = [8000, 10000, 2000000, 12000];
 
   test("이 있을 경우 6자리 숫자의 복권 숫자를 생성하는 지", () => {
-    const generatedLottoNumber = app.domain.generator.generateLottoNumbers(8000);
+    const generatedLottoNumber = generator.generateLottoNumbers(8000);
 
     expect(generatedLottoNumber[0]).toHaveLength(6);
   });
@@ -16,13 +18,12 @@ describe("입력한 금액 값", () => {
   test("에 맞는 복권의 수를 생성하는 지", () => {
     const result = [8, 10, 2000, 12];
     for (let i = 0; i < answer.length; i++) {
-      const generatedLottoNumber = app.domain.generator.generateLottoNumbers(answer[i]);
+      const generatedLottoNumber = generator.generateLottoNumbers(answer[i]);
       expect(generatedLottoNumber).toHaveLength(result[i]);
     }
   });
 
   test("1000원 단위가 아닐 경우 예외를 반환 하는지", () => {
-    const checker = new Checker();
     const incorrect = [1234, 100001, "2aeud"];
     for (let i = 0; i < incorrect.length; i++) {
       expect(() => {
@@ -33,7 +34,6 @@ describe("입력한 금액 값", () => {
 });
 
 describe("입력한 당첨 번호 값", () => {
-  const app = new App();
   const answer = [1, 2, 3, 4, 5, 6];
   const ticket = [
     [1, 2, 3, 4, 5, 6],
@@ -69,7 +69,25 @@ describe("입력한 당첨 번호 값", () => {
 
     expect(() => {
       const lotto = new Lotto(incorrect, ticket);
-      lotto.validate(incorrect);
+      lotto.isOverlapping(incorrect);
     }).toThrow();
+  });
+});
+
+describe("입력한 보너스 숫자 값이", () => {
+  const lottoNumber = [1, 2, 3, 4, 5, 6];
+  const bonusNumber = 4;
+
+  test("이 숫자로 입력이 되었는지", () => {
+    const incorrectBonusNumber = "aaa";
+    expect(() => {
+      checker.isNumber(incorrectBonusNumber);
+    }).toThrow("[ERROR]");
+  });
+
+  test("이 로또 당첨 숫자와 중복될 경우 예외가 발생하는지", () => {
+    expect(() => {
+      checker.isOverlappingBonusNumber(lottoNumber, bonusNumber);
+    }).toThrow("[ERROR]");
   });
 });
