53ced47ba2a60d99881379c793171be0dbd91ba2
2yunseong <dbsdltjd3701@naver.com>
2022-11-13 17:49:01 +0900

    feat(app): 로또 금액을 입력 받는 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 const OutputUI = require('./OutputUI');
 const InputUI = require('./InputUI');
 const message = require('./util/message');
+const User = require('./User');
+const Vaildator = require('./Vaildator');
 
 class App {
   constructor() {
     this.input = new InputUI();
     this.output = new OutputUI();
+    this.user = new User();
   }
 
-  play() {}
+  play() {
+    this.renderBuyAmountView();
+  }
 
-  renderBuyAmountView() {
+  async renderBuyAmountView() {
     this.output.print(message.INPUT_AMOUNT);
-    this.input.inputLine().then((data) => {});
+    let buyAmount = await this.input.inputLine();
+    if (!Vaildator.isRightAmount(buyAmount)) {
+      throw new Error('[ERROR] : 올바른 금액이 아닙니다.');
+      return;
+    }
+    this.user.amount = buyAmount;
   }
 }
 
