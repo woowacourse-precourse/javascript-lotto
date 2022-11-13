a65e2cdc94313f2d6665616c0f8e951d0de4b878
h2002y <h2002y@naver.com>
2022-11-13 19:47:26 +0900

    feat: Add sort inside buyLotto func in App.js

--- src/App.js
+++ src/App.js
@@ -5,31 +5,44 @@ class App {
   #money;
 
   constructor() {
-    let amountOfLotto = 0;
-    let userLotto = [];
+    this.amountOfLotto = 0;
+    this.userLotto = [];
   }
 
   play() {
     this.getMoney();
-    this.getAmountOfLotto();
-    this.buyLotto();
   }
 
   getMoney() {
-    MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (userInput) => {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (userInput) => {
       this.validateMoney(userInput);
       this.#money = Number(userInput);
+      this.getAmountOfLotto();
     });
   }
 
-  getAmountOfLotto(){
+  getAmountOfLotto() {
     this.amountOfLotto = this.#money / 1000;
+    this.buyLotto();
   }
 
-  buyLotto(){
+  buyLotto() {
     for(let i = 0; i < this.amountOfLotto; i++){
       this.userLotto.push(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6));
+      this.userLotto[i].sort(function compare(leftNum, rightNum){
+        return leftNum - rightNum;
+      });
     }
+    this.printBoughtLotto();
+  }
+
+  printBoughtLotto() {
+    MissionUtils.Console.print(`\n${this.amountOfLotto}개를 구매했습니다.`);
+    for(let i = 0; i < this.userLotto.length; i++) {
+      MissionUtils.Console.print(this.userLotto[i]);
+    }
+    MissionUtils.Console.print("\n");
+    MissionUtils.Console.close();
   }
 
   validateMoney(money) {
@@ -47,5 +60,8 @@ class App {
     }
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
