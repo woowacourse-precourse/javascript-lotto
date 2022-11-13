171ad6bc6944745eaeba659b5f5e4451cb9af91e
nara04040 <nara04040@naver.com>
2022-11-13 15:16:51 +0900

    feat : 구입금액에 맞는 로또티켓 갯수를 구하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const readline = require("readline");
 
 class App {
-  constructor() {}
+  constructor() {
+    this.paymentCostStr = "";
+  }
 
   paymentInput() {
     MissionUtils.Console.readLine("구입금액을 입력해주세요.", (answer) => {
       this.isCorrectRange(answer);
+      this.lottoTicketPages();
     });
   }
   isCorrectRange(number) {
@@ -14,7 +17,16 @@ class App {
       return MissionUtils.Console.print("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
     console.log(`구입금액: ${number}`);
+    this.paymentCostStr += number;
   }
+  lottoIssuance() {
+    return this.paymentCostStr / 1000;
+  }
+  lottoTicketPages() {
+    const number = this.lottoIssuance();
+    console.log(`${number}개를 구매했습니다.`);
+  }
+
   play() {
     this.paymentInput();
   }
