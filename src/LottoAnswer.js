6fa1f8ad12d9c4e41fcd53c52bb877aa7997e56c
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-14 01:19:56 +0900

    feat: 1자리 보너스 번호 예외처리

--- src/LottoAnswer.js
+++ src/LottoAnswer.js
@@ -6,6 +6,15 @@ class LottoAnswer extends Lotto {
     super(numbers);
     this.#bonus = null;
   }
+
+  validateBonus(bonus) {
+    if (isNaN(bonus)) throw new Error("[ERROR] 1개의 숫자를 입력하세요.");
+    if (this.numbers.includes(bonus))
+      throw new Error("[ERROR] 당첨 번호와 중복되지 않는 숫자를 입력하세요.");
+    if (bonus < 1 || bonus > 45)
+      throw new Error("[ERROR] 1부터 45까지 숫자 중 입력하세요.");
+    if (!Number.isInteger(bonus)) throw new Error("[ERROR] 정수를 입력하세요.");
+  }
 }
 
 module.exports = LottoAnswer;
