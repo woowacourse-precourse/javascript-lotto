bb8f754f56f5a9d89d45592ec7c047336fa4c3b4
안호진 <asdv94@naver.com>
2022-11-14 01:01:08 +0900

    refactor(lotto): 메세지 상수 static 추가

--- src/Constant.js
+++ src/Constant.js
@@ -1,6 +1,6 @@
 class Constant {
-  NEW_LINE = '\n';
-  GAME_MSG = Object.freeze({
+  static NEW_LINE = '\n';
+  static GAME_MSG = Object.freeze({
     pleaseEnterMoney: '구입금액을 입력해주세요.' + NEW_LINE,
     bought: '개를 구매했습니다.' + NEW_LINE,
     pleaseEnterWinningNumbers:
@@ -8,7 +8,7 @@ class Constant {
     pleaseEnterBonusNumbers:
       NEW_LINE + '보너스 번호를 입력해 주세요.' + NEW_LINE,
   });
-  RESULT_MSG = Object.freeze({
+  static RESULT_MSG = Object.freeze({
     stats: NEW_LINE + '당첨 통계' + NEW_LINE + '---' + NEW_LINE,
     match3: '3개 일치 (5,000원) - ',
     match4: '4개 일치 (50,000원) - ',
@@ -19,7 +19,7 @@ class Constant {
     totalProfit: '총 수익률은 ',
     isNPercent: '%입니다.',
   });
-  ERROR_MSG = Object.freeze({
+  static ERROR_MSG = Object.freeze({
     prefix: '[ERROR] ',
     outOfRange: '로또 번호는 1부터 45 사이의 숫자여야 합니다.',
     notNumber: '숫자여야 합니다.',
