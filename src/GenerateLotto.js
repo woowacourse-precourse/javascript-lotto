6201ff97004f02b66955c222337f9ea5badbc069
jiyujin <yujin1679@naver.com>
2022-11-13 19:26:50 +0900

    feat: 구입한 개수만큼 로또를 랜덤으로 발행하는 setRandomNumber 함수 작성

--- src/GenerateLotto.js
+++ src/GenerateLotto.js
@@ -4,12 +4,23 @@ const LOTTO_PRICE = 1000;
 class GenerateLotto {
   constructor(money) {
     this.count = money / LOTTO_PRICE;
+    this.lottos = [];
     this.play();
   }
+
   play() {
     Console.print(`${this.count}개를 구매했습니다.`);
+    this.setRandomNumber(this.count);
   }
 
+  setRandomNumber() {
+    for (let count = 0; count < this.count; count++) {
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      this.lottos.push(numbers);
+      this.lottos[count].sort((a, b) => a - b);
+    }
+    console.log(this.lottos);
+  }
 
 }
 module.exports = GenerateLotto;
