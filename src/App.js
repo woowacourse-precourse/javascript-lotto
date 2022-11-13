77fb5a359d247d66ca559b8001f6a244da2d0230
cruelladevil <dev.timetravel@gmail.com>
2022-11-13 18:41:46 +0900

    feat(App): ask for payment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    this.askForPayment();
+  }
+
+  askForPayment() {
+    Console.print('구입금액을 입력해 주세요.');
+    Console.readLine('', (input) => {});
+  }
 }
 
 module.exports = App;
