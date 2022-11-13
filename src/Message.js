50fe56fddcc4ac92034ef5c55a701e981dfa4fbc
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-13 11:57:56 +0900

    feat(UI): 보너스 입력 안내 문구

--- src/Message.js
+++ src/Message.js
@@ -25,6 +25,10 @@ class Message {
   static enterWinningNumber() {
     return '당첨 번호를 입력해 주세요. \n';
   }
+
+  static enterBonusNumber() {
+    return '보너스 번호를 입력해 주세요. \n';
+  }
 }
 
 module.exports = Message;
