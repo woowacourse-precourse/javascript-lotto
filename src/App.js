cfe11ede5be210b09b3d6e62b81ef2ef9e60b45a
nalsae <nalsae0207@naver.com>
2022-11-13 23:26:29 +0900

    style: change names of methods
    메서드 이름 변경
    - findPriceError > validatePrice
    - validate > validateWinningNumber

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App {
 
   startGame() {
     Console.readLine('구입 금액을 입력해주세요.\n', (price) => {
-      this.findPriceError(price);
+      this.validatePrice(price);
 
       this.issueLotto(price);
     });
@@ -45,7 +45,7 @@ class App {
 
   getBonusNumber() {
     Console.readLine(`\n보너스 번호를 입력해 주세요.\n`, (userInput) => {
-      this.validateBonusNumber(userInput);
+      this.#Lotto.validateBonusNumber(userInput);
 
       const bonusNumber = +userInput;
 
@@ -53,7 +53,7 @@ class App {
     });
   }
 
-  findPriceError(price) {
+  validatePrice(price) {
     if (
       Number.isNaN(+price) ||
       typeof +price !== 'number' ||
--- src/Lotto.js
+++ src/Lotto.js
@@ -2,11 +2,11 @@ class Lotto {
   #winnigNums;
 
   constructor(winnigNums) {
-    this.validate(winnigNums);
+    this.validateWinningNubmer(winnigNums);
     this.#winnigNums = winnigNums.map((num) => +num);
   }
 
-  validate(winnigNums) {
+  validateWinningNubmer(winnigNums) {
     if (winnigNums.length !== 6) {
       throw new Error(
         `[ERROR] 당첨 번호는 중복되지 않는 1 ~ 45 사이의 숫자 6개를 ','로 구분하여 입력해야 합니다.`
