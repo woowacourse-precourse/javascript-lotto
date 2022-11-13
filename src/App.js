009d4e8c9f403120b260e408ea1003ee4184f1f4
unknown <sjjoseph106@gmail.com>
2022-11-13 15:35:39 +0900

    feat: 로또 구입 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("./constants/index");
+
 class App {
-  play() {}
+  constructor() {
+    this.lotto;
+  }
+
+  play() {
+    Console.print(MESSAGE.MONEY_INPUT);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+const MESSAGE = Object.freeze({
+  MONEY_INPUT: "구입금액을 입력해 주세요.",
+});
+
+module.exports = {
+  MESSAGE,
+};
