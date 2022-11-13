73b9a16c94bda582928098e723aaef3f56fd3fda
JangHoonSeok <orirori512@naver.com>
2022-11-14 01:03:51 +0900

    ✨ feat(CheckError): 로또 번호 유효성 검사 함수 구현

--- src/CheckError.js
+++ src/CheckError.js
@@ -9,6 +9,20 @@ class CheckError {
       throw new Error(ERROR_MESSAGE.CANT_DIVIDE);
     if (purchaseAmount < 1000) throw new Error(ERROR_MESSAGE.UNDER_BASIC_PRICE);
   }
+
+  static checkLottoNumbers(lottoNumbers) {
+    const lottoNumberSet = new Set(lottoNumbers);
+    const arrayToString = lottoNumbers.join();
+    if (lottoNumbers.length !== 6)
+      // 로또 길이가 6이 아닐 때
+      throw new Error(ERROR_MESSAGE.NOT_SIX_LENGTH);
+    else if (lottoNumberSet.size !== 6)
+      // 로또가 중복된 숫자를 가질 때.
+      throw new Error(ERROR_MESSAGE.IS_OVERLAPPED);
+    else if (isNaN(arrayToString))
+      // 로또에 숫자 이외의 문자가 있을 때.
+      throw new Error(ERROR_MESSAGE.NOT_NUMBER_IN_LOTTO);
+  }
 }
 
 module.exports = CheckError;
