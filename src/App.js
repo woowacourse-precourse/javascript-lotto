3ee9f62628f9828853aef4ebc7c8736a87e0fe5f
freemoon99 <soloist132@gmail.com>
2022-11-13 23:41:55 +0900

    feat: 게임 시작 기능
    게임을 시작하고 금액을 입력받아 Money.js와 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const Money = require('./Money');
+
 class App {
-  play() {}
+  #Money;
+
+  constructor() {
+    this.money = 0;
+  }
+
+  play() {
+    this.startGame();
+  }
+
+  startGame() {
+    MissionUtils.Console.readLine('구입금액을 입력해주세요.', (money) => {
+      this.#Money = new Money(money);
+      this.money = money;
+    });
+  }
 }
 
 module.exports = App;
