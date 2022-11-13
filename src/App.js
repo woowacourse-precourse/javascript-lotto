2e4b83e190255906af58fd305adf220b633c8588
박승훈 <tmdgns1126@naver.com>
2022-11-13 23:22:21 +0900

    feat: 범위 내 임의의 6개의 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+const { Random } = require("@woowacourse/mission-utils");
 // const { ERROR_MSG_THOUSAND_UNIT } = require("./constants/error-message");
 // const { WINNINGS } = require("./constants/winnings");
 
 class App {
+  constructor() {
+    this.lottos = [];
+  }
+
   play() {}
+
+  createLottoNums() {
+    const lottoNums = Random.pickUniqueNumbersInRange(1, 45, 6);
+    return lottoNums;
+  }
 }
 
 // module.exports = App;
