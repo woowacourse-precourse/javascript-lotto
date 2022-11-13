efcfc296ec4eee64f5cb4d6e59e09af1fc311b33
mjmj0217 <mjmj0217@naver.com>
2022-11-13 23:18:50 +0900

    feat: add printing winning statistics

--- src/Lotto.js
+++ src/Lotto.js
@@ -6,6 +6,13 @@ const LOTTO_ERROR = {
   length: "[ERROR] 로또 번호는 6개여야 합니다.",
   duplicate: "[ERROR] 로또 번호는 중복되지 않아야 합니다.",
 };
+const RESULT_INFO = {
+  rank1: "6개 일치 (2,000,000,000원)",
+  rank2: "5개 일치, 보너스 볼 일치 (30,000,000원)",
+  rank3: "5개 일치 (1,500,000원)",
+  rank4: "4개 일치 (50,000원)",
+  rank5: "3개 일치 (5,000원)",
+};
 
 class Lotto {
   #numbers; // 당첨번호
@@ -13,6 +20,52 @@ class Lotto {
   constructor(numbers) {
     this.validateNumbers(numbers);
     this.#numbers = numbers.map((x) => Number(x));
+  }
+
+  printResult(numbersArray, bonusNum) {
+    const winningCnt = [0, 0, 0, 0, 0];
+    for (let arr of numbersArray) {
+      this.checkLotto(winningCnt, arr, bonusNum);
+    }
+    this.printWinningStatistics(winningCnt, numbersArray, bonusNum);
+  }
+
+  checkLotto(winningCnt, yourNum, bonusNum) {
+    let cnt = 0;
+    for (let num of yourNum) {
+      if (this.#numbers.includes(num)) {
+        cnt += 1;
+      }
+    }
+    if (cnt < 3) return;
+    this.plusCnt(winningCnt, cnt, yourNum, bonusNum);
+  }
+
+  printWinningStatistics(winningCnt) {
+    Console.print(`${RESULT_INFO.rank5} - ${winningCnt[0]}개`);
+    Console.print(`${RESULT_INFO.rank4} - ${winningCnt[1]}개`);
+    Console.print(`${RESULT_INFO.rank3} - ${winningCnt[2]}개`);
+    Console.print(`${RESULT_INFO.rank2} - ${winningCnt[3]}개`);
+    Console.print(`${RESULT_INFO.rank1} - ${winningCnt[4]}개`);
+  }
+
+  plusCnt(winningCnt, cnt, yourNum, bonusNum) {
+    switch (cnt) {
+      case 3:
+      case 4:
+        winningCnt[cnt - 3] += 1;
+        break;
+      case 5:
+        if (yourNum.includes(bonusNum)) {
+          winningCnt[3] += 1;
+        } else {
+          winningCnt[2] += 1;
+        }
+        break;
+      case 6:
+        winningCnt[4] += 1;
+        break;
+    }
   }
 
   validateNumbers(numbers) {
