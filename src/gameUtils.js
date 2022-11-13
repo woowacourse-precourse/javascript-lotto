0c3ae34f5ad1093e17c6d5df3b24299999ba78d7
pinkishincoloragain <smb1103@gmail.com>
2022-11-14 03:12:59 +0900

    refactor: gameUtils ticket -> lotto 문구 변경

--- src/gameUtils.js
+++ src/gameUtils.js
@@ -1,11 +1,11 @@
 const { Random } = require('@woowacourse/mission-utils');
 const { GAME_RANGE } = require('./config');
 
-const generateTicket = () =>
+const generateLotto = () =>
   Random.pickUniqueNumbersInRange(
     GAME_RANGE.RANGE_MIN,
     GAME_RANGE.RANGE_MAX,
     GAME_RANGE.NUM_LENGTH,
   ).sort((a, b) => a - b);
 
-module.exports = { generateTicket };
+module.exports = { generateLotto };
