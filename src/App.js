3cc0ce212827dff48bb62aa272d564221e0d36f5
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-13 18:01:16 +0900

    feat: 사용자의 희망 구매금액을 입력받는다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    this.salesLotto();
+  }
+
+  salesLotto() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (purchaseAmount) => {
+      this.purchaseAmount = purchaseAmount;
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
