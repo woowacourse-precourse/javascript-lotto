84d647b16a0057ed5588a5ae7d5163125b581d41
SeungMiL <tmskqj@naver.com>
2022-11-14 02:10:37 +0900

    feat: 기능5, 기능7 입력 문구 출력 완료

--- src/controller/lotto.controller.js
+++ src/controller/lotto.controller.js
@@ -8,13 +8,26 @@ class LottoController {
 
   start() {
     this.getPayInput();
+    this.winningNumber();
+    this.bonusNumber();
   }
 
   getPayInput() {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (input) => {
       MissionUtils.Console.print(this.model.inputValidCheck(input));
       this.view.amountBuyLotto(input);
+    });
+  }
 
+  winningNumber() {
+    MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.', (input) => {
+        console.log('당첨번호 =>', input)
+    });
+  }
+
+  bonusNumber() {
+    MissionUtils.Console.readLine('보너스 번호를 입력해 주세요.', (input) => {
+        console.log('보너스번호 =>', input)
     });
   }
 }
