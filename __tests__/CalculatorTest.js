9a6b117ee8ca4c952232901327750f17e3297b05
HojinAn <asdv94@naver.com>
2022-11-14 08:23:52 +0900

    test(lotto): 구입 금액 따른 로또 개수 테스트 코드

--- __tests__/CalculatorTest.js
+++ __tests__/CalculatorTest.js
@@ -1,12 +1,16 @@
 const Calculator = require('../src/Calculator');
 
 describe('계산기 클래스 테스트', () => {
-  const cost1 = 8000;
-  const cost2 = 7000;
-  const income1 = 5000;
-  const income2 = 1500000;
   test('비용 대비 소득, 수익률을 계산한다.', () => {
+    const cost1 = 8000;
+    const cost2 = 7000;
+    const income1 = 5000;
+    const income2 = 1500000;
     expect(Calculator.calcProfit(cost1, income1)).toEqual(62.5);
     expect(Calculator.calcProfit(cost2, income2)).toEqual(21428.6);
+  });
+  test('입력한 금액을 1,000으로 나눈 몫을 계산한다.', () => {
+    const numerator = 8000;
+    expect(Calculator.calcQuotient(numerator)).toEqual(8);
   });
 });
--- src/Calculator.js
+++ src/Calculator.js
@@ -2,6 +2,7 @@ class Calculator {
   static calcProfit(cost, income) {
     return +((income / cost) * 100).toFixed(1);
   }
+  static calcQuotient() {}
 }
 
 module.exports = Calculator;
