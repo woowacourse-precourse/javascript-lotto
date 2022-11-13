a3203bc0ef94873b247a933d456bc1fd97fa15c3
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-13 22:14:08 +0900

    feat: 사용자로부터 로또 구입 금액을 입력받는 기능 추가

---
+++ src/LottoManager.js
@@ -0,0 +1,9 @@
+const MESSAGE = require("./constant/message");
+
+class LottoManager {
+  constructor() {}
+
+  receivePurchaseCost() {
+    Console.readLine(MESSAGE.START, (purchaseString) => {});
+  }
+}
