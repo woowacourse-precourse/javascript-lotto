249cd1da3b0a0d8d4c83066a7df4a25761395339
yeopto <gunyeop530@gmail.com>
2022-11-13 12:29:12 +0900

    feat: 보너스 번호가 숫자가 아닌 경우 예외 처리한다

--- src/BonusNum.js
+++ src/BonusNum.js
@@ -2,6 +2,7 @@ class BonusNum {
   constructor(bonusNum) {
     this.isInRange(bonusNum);
     this.isInteger(bonusNum);
+    this.isNum(bonusNum);
   }
 
   isInRange(bonusNum) {
@@ -14,6 +15,13 @@ class BonusNum {
     if (+bonusNum % 1 !== 0) {
       throw new Error('[ERROR] 정수를 입력해주세요.');
     }
+  }
+
+  isNum(bonusNum) {
+    const splitBonusNum = bonusNum.split('');
+    splitBonusNum.map((el) => {
+      if (isNaN(el) || el === ' ') throw new Error('[ERROR] 숫자만 입력해주세요.');
+    })
   }
 }
 
