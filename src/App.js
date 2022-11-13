fabfb3567531e5faf0fcd80cfb2352c4fb3b7a10
Choejeonghun <saa00123@naver.com>
2022-11-13 17:23:49 +0900

    feat: 1~45 사이의 랜던 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 class App {
-  play() {}
+  play() {
+    this.computer = this.randomNum();
+  }
+
+  randomNum() {
+    let randomNum = [];
+    while (randomNum.length != 7) {
+      let pickNm = MissionUtils.Random.pickNumberInRange(1, 45);
+      if (!randomNum.includes(pickNum)) {
+        randomNum.push(pickNum);
+      }
+    }
+  }
 }
 
 module.exports = App;
