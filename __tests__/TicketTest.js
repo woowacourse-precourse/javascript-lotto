976a708e456dfcd8ad5e15bd91d804ece8a5e9cd
dev-redo <mis05041@naver.com>
2022-11-14 02:13:13 +0900

    test(TicketTest): Ticket 컴포넌트 단위테스트

--- __tests__/TicketTest.js
+++ __tests__/TicketTest.js
@@ -1,15 +1,14 @@
-const Bonus = require('../src/Bonus');
+const Ticket = require('../src/Ticket');
 
-describe('로또 클래스 테스트', () => {
-  test('보너스 번호는 로또 번호와 중복되는 숫자가 있으면 예외가 발생한다.', () => {
+describe('티켓 클래스 테스트', () => {
+  test('로또 구입 금액이 1000원 단위가 아니면 예외가 발생합니다.', () => {
     expect(() => {
-      new Bonus([1, 2, 3, 4, 5, 6], 5);
+      new Ticket(800);
     }).toThrow('[ERROR]');
   });
 
-  test('로또 번호의 숫자 범위가 1~45 사이가 아니면 예외가 발생한다.', () => {
-    expect(() => {
-      new Bonus([1, 2, 3, 4, 5, 6], 50);
-    }).toThrow('[ERROR]');
+  test('로또 구입 금액이 1000원 단위가 아니면 예외가 발생합니다.', () => {
+    const ticket = new Ticket(8000);
+    expect(ticket.buyTicket()).toEqual(8);
   });
 });
