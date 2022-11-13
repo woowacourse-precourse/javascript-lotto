83413aeaa1cefccc5ddd9d661c8c74136355e623
Junseo <oponize@naver.com>
2022-11-14 00:02:32 +0900

    chore: 로또 순위 상수 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,7 +9,25 @@ const LOTTO_NUMBER = Object.freeze({
   VALID_WIN_NUMBER_LENGTH: 11,
 });
 
+const LOTTO_RANK = Object.freeze({
+  NAME: {
+    RANK_ONE: "RANK_ONE",
+    RANK_TWO: "RANK_TWO",
+    RANK_TREE: "RANK_TREE",
+    RANK_FOUR: "RANK_FOUR",
+    RANK_FIVE: "RANK_FIVE",
+  },
+  CASE: {
+    RANK_ONE: 6,
+    RANK_TWO: 5,
+    RANK_TREE: 5,
+    RANK_FOUR: 4,
+    RANK_FIVE: 3,
+  },
+});
+
 module.exports = {
   LOTTO_AMOUNT,
   LOTTO_NUMBER,
+  LOTTO_RANK,
 };
