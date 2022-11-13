6ad38ec18688c3810a527e513be58424b4fc2ded
sh981013s <sh981013s@gmail.com>
2022-11-13 14:46:08 +0900

    feat(UserNumber): 로또 발행 및 접근 로직 추가

--- src/model/UserNumber.js
+++ src/model/UserNumber.js
@@ -1,7 +1,10 @@
+const { Random } = require("@woowacourse/mission-utils");
+
 class UserNumber {
   constructor(controller) {
     this.controller = controller;
     this.purchasingAmount = null;
+    this.userLottoArray = [];
   }
 
   validatePurchasingAmount(userPurchasingAmountInput) {
@@ -17,6 +20,25 @@ class UserNumber {
   setPurchasingAmount(userPurchasingAmountInput) {
     this.validatePurchasingAmount(userPurchasingAmountInput);
     this.purchasingAmount = userPurchasingAmountInput;
+    this.setUserLottoArrayWithPurchasingAmount();
+  }
+
+  setUserLottoArrayWithPurchasingAmount() {
+    for (let idx = 0; idx < this.purchasingAmount / 1000; idx++) {
+      const singleLottoCombinationArray = Random.pickUniqueNumbersInRange(
+        1,
+        45,
+        6,
+      );
+
+      this.userLottoArray.push(
+        singleLottoCombinationArray.sort((first, second) => first - second),
+      );
+    }
+  }
+
+  getUserLottoArray() {
+    return this.userLottoArray;
   }
 }
 
