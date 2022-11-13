25d20259a130868cb166e12946bdfab65a55d94f
hxezin <leehxejin@gmail.com>
2022-11-14 00:04:54 +0900

    feat: 사용자에게 로또 구매금액 입력 받음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.money = 0;
+  }
+
+  play() {
+    MissionUtils.Console.readLine("구매금액을 입력해 주세요.\n", (answer) => {
+      this.money = answer;
+    });
+  }
 }
 
 module.exports = App;
