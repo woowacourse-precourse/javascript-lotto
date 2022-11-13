22780c56e35fefe2006e4bb8e17545dc32d9e55d
Yebin Kim <kyb4312@gmail.com>
2022-11-13 23:42:00 +0900

    feat(App): receive money and check its validity

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    const money = this.receiveMoney();
+  }
+
+  receiveMoney() {
+    Console.readLine('구입금액을 입력해 주세요.', (money) => {
+      this.checkMoneyValidity(money);
+    });
+  }
+
+  checkMoneyValidity(money) {
+    if (money % 1000 !== 0) {
+      throw new Error('[ERROR] 구입 금액은 1,000원 단위여야 합니다.');
+    }
+  }
 }
 
 module.exports = App;
