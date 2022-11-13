36368b28f5544df1acb264bb4542d16bb4afe9be
Hyungeol94 <55428818+Hyungeol94@users.noreply.github.com>
2022-11-13 22:16:59 +0900

    feat: 로또 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+const Lotto = require("./Lotto");
+
 class App {
-  play() {}
+  play() {
+  //로또 구매 입력받기
+  //당첨 번호 입력받기
+  
+  const amount = this.get_amount();
+  
+  }
+
+  get_amount(){
+    const MissionUtils = require("@woowacourse/mission-utils");
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (answer) => {
+    const amount = answer;
+    if (this.not_thousands(amount)) {throw 'not_thousands'};
+    if (this.not_positive(amount)) {throw 'not a positive number'}
+  })
+
+    return amount;
+  }
+
+  //1000단위가 아닐 경우 예외처리
+  not_thousands(amount){
+    if ((amount % 1000)!= 0) return true;
+  }
+
+  not_positive(amount){
+    if(amount<=0) return true;
+  }
+
+
 }
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -2,17 +2,30 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
+    //인스턴스 생성 및 초기화
     this.validate(numbers);
     this.#numbers = numbers;
   }
 
+  //프로토타입 메서드
+  //당첨 번호
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  //중복되지 않는 숫자
+  duplicatecheck(numbers){
+
+  }
+
+  //1~45까지의 숫자
+  rangecheck(numbers){
+    
+  }
+
+  
 }
 
 module.exports = Lotto;
