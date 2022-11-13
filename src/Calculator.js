b6f3c90e6e6f8c7612b577876c86fa3d8ba2c297
oio337a <oio337a@gmail.com>
2022-11-13 19:58:58 +0900

    feat : save how many matching numbers

--- src/Calculator.js
+++ src/Calculator.js
@@ -10,6 +10,16 @@ class Calculator {
     this.#winningNumbers = winningNumbers;
     this.#bonus = bonus;
     this.printWinningStats();
+    this.checkMatchingNumbers();
+  }
+
+  checkMatchingNumbers() {
+    const matchingCount = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, B: 0 };
+    const count = this.#myLottos.map(
+      (lotto) => [...new Set([...lotto, ...this.#winningNumbers])].length - 6
+    );
+    count.map((item) => (matchingCount[item] += 1));
+    Console.print(matchingCount);
   }
 
   printWinningStats() {
