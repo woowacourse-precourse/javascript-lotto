5b38495b05918d32ad6ddfc4c9cb3a0f044eee87
khkh0109 <ist200120@gmail.com>
2022-11-13 16:45:20 +0900

    feat(Money): 구입한 로또 개수 계산하는 기능 추가

--- src/Money.js
+++ src/Money.js
@@ -1,10 +1,20 @@
 const UserError = require("./UserError");
 
 class Money {
-  validate(money) {
+  constructor(money) {
+    this.money = money;
+    this.validate(this.money);
+  }
+
+  validate() {
     const error = new UserError();
-    error.includingCharactersError(money);
-    error.validateMoney(money);
+    error.includingCharactersError(this.money);
+    error.validateMoney(this.money);
+  }
+
+  divideMoney(money) {
+    const count = +money / 1000;
+    return count;
   }
 }
 
