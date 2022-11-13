910a3147552acfca01cdba299d42d71dd231b443
unsnruu <unsnruu@gmail.com>
2022-11-13 15:08:43 +0900

    feat : 사용자에게 얼마만큼의 구매를 할지 묻는 buyLottos 메서드 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const { Console, Random } = MissionUtils;
 
 class App {
+  buyLottos() {
+    Console.readLine("구입금액을 입력해 주세요.", (inputs) => {});
+  }
   play() {}
 }
 
