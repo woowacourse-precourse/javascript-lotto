302e056ed0f3870437ae0b9d3e170b8a6e14ba23
JangHoonSeok <orirori512@naver.com>
2022-11-13 23:46:39 +0900

    ✨ feat: 몇개의 로또를 구입했는지 print하는 함수 구현

--- src/UserInterface.js
+++ src/UserInterface.js
@@ -1,27 +1,26 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { INPUT_MESSAGE } = require("./constants/messages.js");
+const { INPUT_MESSAGE, PRINT_MESSAGE } = require("./constants/messages.js");
 class UserInterface {
   // 로또 게임을 진행할 때 입출력 기능을 담당하는 클래스.
 
   // 구입 금액을 입력 받는 기능을 하는 함수.
-  purchaseRequest() {
+  static purchaseRequest() {
     let purchaseAmount;
     Console.readLine(INPUT_MESSAGE.PURCHASE_INPUT, (purchase) => {
-      purchaseAmount = puchase;
+      purchaseAmount = purchase;
     });
     return purchaseAmount;
   }
 
+  static printLottoNumber(lottoNumber) {
+    Console.print(PRINT_MESSAGE.PURCHASE_MESSAGE(lottoNumber));
+  }
   winnerNumberRequest() {
     // TODO: 당첨 번호 입력 기능
   }
 
   bonusNumberRequest() {
     // TODO: 보너스 번호 입력 기능
-  }
-
-  printLottoNumber() {
-    // TODO: 몇개를 구매했는지 출력 하는 기능
   }
 
   printLottoArray() {
