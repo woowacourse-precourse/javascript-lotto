4d8b5319d8bfe54359b5cd505c26455807800ba1
freemoon99 <soloist132@gmail.com>
2022-11-13 23:45:19 +0900

    feat: 숫자가 맞는 확인하는 기능
    숫자가 맞는지 확인하고 예외 처리

--- src/Money.js
+++ src/Money.js
@@ -2,8 +2,14 @@ class Money {
   #money;
 
   constructor(money) {
-
+    this.isNumber(money);
     this.#money = money;
+  }
+
+  isNumber(money) {
+    if (!Number(money)) {
+      throw new Error('[ERROR] 숫자를 입력해주세요.');
+    }
   }
 }
 
