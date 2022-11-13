db557eea7a50e930b74f7aab8f4e1db2050d6ef3
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-13 23:26:47 +0900

    feat(constant): 로또 당첨 정보 상수화

---
+++ src/constant.js
@@ -0,0 +1,29 @@
+const GRADE = {
+  FIRST: {
+    DUPLICATE_COUNT: 6,
+    EXTRA_TEXT: '',
+    PRIZE_MONEY: '2,000,000,000',
+  },
+  SECOND: {
+    DUPLICATE_COUNT: 5,
+    EXTRA_TEXT: ', 보너스 볼 일치',
+    PRIZE_MONEY: '30,000,000',
+  },
+  THIRD: {
+    DUPLICATE_COUNT: 5,
+    EXTRA_TEXT: '',
+    PRIZE_MONEY: '1,500,000',
+  },
+  FOURTH: {
+    DUPLICATE_COUNT: 4,
+    EXTRA_TEXT: '',
+    PRIZE_MONEY: '50,000',
+  },
+  FIFTH: {
+    DUPLICATE_COUNT: 3,
+    EXTRA_TEXT: '',
+    PRIZE_MONEY: '5,000',
+  },
+};
+
+module.exports = { GRADE };
