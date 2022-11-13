6293c85243de13c73468d3299f33e9578c75e3d3
thewronghand <penfreak77@gmail.com>
2022-11-14 04:45:55 +0900

    fix: printLottoPurchaseResult 메서드 내 MissionUtils.Console.print 구문 내용 수정
    로또 구매 결과 출력 시에 배열의 요소마다 띄어쓰기가 적용되어야 하는 것으로 확인되어 element.numbers의 내용물을 0~5번째까지 나열하는 방식으로 수정한다.

--- src/Customer.js
+++ src/Customer.js
@@ -18,7 +18,9 @@ class Customer {
   printLottoPurchaseResult() {
     MissionUtils.Console.print(`\n${this.#lottos.length}개를 구매했습니다.`);
     this.#lottos.forEach((element) => {
-      MissionUtils.Console.print(`[${element.numbers}]`);
+      MissionUtils.Console.print(
+        `[${element.numbers[0]}, ${element.numbers[1]}, ${element.numbers[2]}, ${element.numbers[3]}, ${element.numbers[4]}, ${element.numbers[5]}]`
+      );
     });
   }
 }
