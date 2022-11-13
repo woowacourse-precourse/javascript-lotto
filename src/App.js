8035df979f7324a4e1b4b868123ca9742e494bf8
wlthdud <thdudzi@naver.com>
2022-11-14 02:44:31 +0900

    feat: 구매금액 입력받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+
+  }
+
+  getMoney(){ //구매 금액 입력받기
+    MissionUtils.Console.readline('구입금액을 입력해 주세요.\n', (money) => {
+      
+    });
+  }
 }
 
 module.exports = App;
