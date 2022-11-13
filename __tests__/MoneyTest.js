06d88b74030e6cb729a2bd7bb8f9fa1830900d37
2je0 <bulljy@naver.com>
2022-11-13 22:08:37 +0900

    🧪 test: 수익률 계산 테스트 추가

--- __tests__/MoneyTest.js
+++ __tests__/MoneyTest.js
@@ -1,5 +1,12 @@
+const MissionUtils = require('@woowacourse/mission-utils');
 const Money = require('../src/Money');
 const { CONSTANT, PRIZE_MONEY } = require('../src/Constants');
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe('Money 클래스 테스트', () => {
   test('천원단위의 입력이 아니면 예외가 발생한다.', () => {
@@ -44,5 +51,30 @@ describe('Money 클래스 테스트', () => {
     const prize = [1, 1, 1, 1, 1];
     const result = PRIZE_MONEY.reduce((acc, cur) => acc += cur, 0);
     expect(money.getEarning(prize)).toBe(result);
+  });
+
+  test('수익률 계산 테스트1', () => {
+    const logSpy = getLogSpy();
+    const money = new Money(5000);
+    const prize = [1, 0, 0, 0, 0];
+    const log = '100.0%';
+    money.printEarningRatio(prize);
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+  });
+  test('수익률 계산 테스트2', () => {
+    const logSpy = getLogSpy();
+    const money = new Money(10000);
+    const prize = [1, 0, 0, 0, 0];
+    const log = '50.0%';
+    money.printEarningRatio(prize);
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+  });
+  test('수익률 계산 테스트3 (소수점)', () => {
+    const logSpy = getLogSpy();
+    const money = new Money(7000);
+    const prize = [0, 1, 0, 0, 0];
+    const log = '714.3%';
+    money.printEarningRatio(prize);
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
   });
 });
