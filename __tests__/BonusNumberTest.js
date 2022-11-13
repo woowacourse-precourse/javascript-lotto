3cdcb85e56f863efecc5734ea8f9be4f6d2dd037
d0422 <rlfehd2013@naver.com>
2022-11-13 16:28:26 +0900

    Test:보너스번호 단위 테스트  오타 수정

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -4,7 +4,7 @@ const Lotto = require('../src/Model/Lotto');
 describe('보너스 숫자 클래스 테스트', () => {
   test('보너스 번호가 정답로또 번호를 포함하면 오류발생', () => {
     expect(() => {
-      new BonusNumber(new Lotto([1, 2, 3, 4, 5, 6], 6));
+      new BonusNumber(new Lotto([1, 2, 3, 4, 5, 6]), 6);
     }).toThrow('[ERROR]');
   });
 });
