a9db372512801d6edaa613ce5f4df324dcd1eea1
DongWooKim97 <nanaukim@naver.com>
2022-11-13 23:15:21 +0900

    refactor: 보너스 번호 관리 클래스에 따른 입력수단 추가 및 해당 클래스 연결

--- src/play/Clerk.js
+++ src/play/Clerk.js
@@ -2,12 +2,14 @@ const Mission = require("@woowacourse/mission-utils");
 const { GAME_MESSAGES } = require("../utils/Constants");
 const QuickPick = require("../input/QuickPick");
 const Lotto = require("../input/Lotto");
+const Bonus = require("../input/Bonus");
 
 class Clerk {
   #payment;
   #myLottoArray;
   #lottoAmount;
   #winningNumbers;
+  #bonusNumber;
 
   inputLottoAmount() {
     Mission.Console.readLine(GAME_MESSAGES.ASK_TO_PAY, (payment) => {
@@ -28,6 +30,15 @@ class Clerk {
         .sort((a, b) => a - b);
       const lotto = new Lotto(winningNum);
       this.#winningNumbers = lotto.output();
+      this.inputBonusNumber();
+    });
+    return;
+  }
+
+  inputBonusNumber() {
+    Mission.Console.readLine(GAME_MESSAGES.ASK_TO_BONUS_NUMBER, (bonusNumber) => {
+      const bonus = new Bonus(bonusNumber, this.#winningNumbers);
+      this.#bonusNumber = bonus.output();
     });
     return;
   }
