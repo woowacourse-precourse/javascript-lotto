8363bb9b9ebde43078e732778857c3de340757fa
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-14 23:32:16 +0900

    feat: 상수 선언

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+const LOTTO = {
+  PRICE: 1000,
+  MIN: 1,
+  MAX: 45,
+};
+
+const ERROR_MESSAGE = {
+  NAN: "[ERROR] 숫자만 입력해 주세요.",
+  MIN_PRICE: "[ERROR] 로또 가격은 개당 1000원 입니다. ",
+  PRICE_UNIT: "[ERROR] 로또 가격은 1000원으로 나누어 떨어져야 합니다.",
+  COMMA: "[ERROR] 콤마로 구분된 6자리 숫자로 입력해 주세요.",
+  RANGE: "[ERROR] 1 ~ 45 숫자를 입력해 주세요.",
+  DUPLICATE: "[ERROR] 중복이 있습니다.",
+};
+
+module.exports = {
+  LOTTO,
+  ERROR_MESSAGE,
+}
