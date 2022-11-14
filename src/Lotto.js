829525a0ac93b9d2edc90f5cdefc7e2dd3292671
inthhh <glass005533@gmail.com>
2022-11-14 19:49:47 +0900

    feat: 결과 출력 메소드 구현 (Lotto.printWinResult)

--- src/Lotto.js
+++ src/Lotto.js
@@ -81,6 +81,19 @@ class Lotto {
     let earningRatio = (100 * (earnPrice / buyPrice)).toFixed(1);
     return earningRatio;
   }
+
+  printWinResult(isMatch, bonusMatch) {
+    let winLottoCount = this.countWinLotto(isMatch, bonusMatch);
+    let priceEarn = this.priceEarningsRatio(isMatch);
+    Console.print("\n당첨 통계\n---");
+    Console.print("3개 일치 (5,000원) - " + winLottoCount[0] + "개");
+    Console.print("4개 일치 (50,000원) - " + winLottoCount[1] + "개");
+    Console.print("5개 일치 (1,500,000원) - " + winLottoCount[2] + "개");
+    Console.print("5개 일치, 보너스 볼 일치 (30,000,000원) - " + bonusMatch + "개");
+    Console.print("6개 일치 (2,000,000,000원) - " + winLottoCount[3] + "개");
+    Console.print("총 수익률은 " + priceEarn + "%입니다.");
+    return;
+  }
 }
 
 module.exports = Lotto;
