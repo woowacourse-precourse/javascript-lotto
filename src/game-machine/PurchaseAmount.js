812946436f673b603978541e9345eafaf568468a
SorinKwon <ksl020700@gmail.com>
2022-11-14 22:54:10 +0900

    feat: 구입 금액을 전달하는 기능 추가
    새로운 기능 추가:
    - 사용자가 입력한 구입 금액을 전달하는 기능
    - 다른 클래스의 함수로 전달함

--- src/game-machine/PurchaseAmount.js
+++ src/game-machine/PurchaseAmount.js
@@ -1,13 +1,14 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const  CalculateLotto= require("./CalculateLotto");
+
 class PurchaseAmount {
 
   constructor(){
     this.purchase_amount;
-    this.inputPurchaseAmount();
   }
 
   inputPurchaseAmount(){
-    MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (input_money) => {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (input_money) => {
       console.log(input_money);
       this.savePurchaseAmount(input_money);
     });
@@ -15,9 +16,16 @@ class PurchaseAmount {
   
   savePurchaseAmount(input_money){
     this.purchase_amount=input_money;
+    this.accessPurchaseAmount();
   }
+
+  accessPurchaseAmount(){
+    let purchase_amount = this.purchase_amount;
+    const cal_lotto = new CalculateLotto();
+    cal_lotto.getPurchaseAmount(purchase_amount);
+  }
+  
 }
 
 const amount = new PurchaseAmount();
-
 module.exports = PurchaseAmount;
