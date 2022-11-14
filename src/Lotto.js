8771236077a0076a632f90aae27509471c449db9
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-11-14 20:39:08 +0900

    Feat: private 필드 numbers get,set 변환

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,15 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  get inputNumbers() {
+    return this.#numbers;
+  }
+
+  set inputNumbers(value) {
+    const inputArr = value.split(",").map((value) => Number(value));
+    this.#numbers = inputArr;
+  }
+
   validate(numbers) {
     const checkSixNum = inputValidation.checkSixNum(numbers);
   }
@@ -26,21 +35,17 @@ class Lotto {
 
   //로또 입력logic
   inputMoney() {
-    Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+    Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
       //8개 구매했습니다. 8개 당첨로또 로직
-    })
+    });
   }
 
   inputLotto() {
-    Console.readLine('당첨 번호를 입력해 주세요.\n', (lotto) => {
-  
-    })
+    Console.readLine("당첨 번호를 입력해 주세요.\n", (lotto) => {});
   }
 
   inputBonus() {
-    Console.readLine('보너스 번호를 입력해 주세요.\n', (bonus) => {
-      
-    })
+    Console.readLine("보너스 번호를 입력해 주세요.\n", (bonus) => {});
   }
 }
 
--- src/inputValidation.js
+++ src/inputValidation.js
@@ -4,6 +4,12 @@ const inputValidation = {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   },
+
+  checkThousandNum(money) {
+    if (money % 1000 !== 0) {
+      throw new Error("[ERROR] 1000원 단위로 입력해주세요.")
+    }
+  }
 };
 
 module.exports = inputValidation;
