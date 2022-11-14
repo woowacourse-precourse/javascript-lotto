4d6660af3b2e1f6191f2efde205451831477e729
gyeongza <gyeongza@naver.com>
2022-11-15 02:06:34 +0900

    test : 보너스 번호 에러 테스트 완료

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -1,6 +1,15 @@
 const App = require("../src/App");
 const MissionUtils = require("@woowacourse/mission-utils");
 
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
 const mockRandoms = (numbers) => {
   MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
   numbers.reduce((acc, number) => {
