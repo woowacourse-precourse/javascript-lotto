fd004a4644f1c5958ec662eab0c4c0828bedbd7b
김혜원 <dev.blair06@gmail.com>
2022-11-14 21:56:58 +0900

    feat:#2 당첨번호 및 보너스번호 입력

--- src/Customer.js
+++ src/Customer.js
@@ -1,7 +1,10 @@
 const Constant = require("./utils/Constant");
 const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
 
 class Customer {
+  #lotto;
+
   constructor() {
     this.lottoList = [];
   }
@@ -11,25 +14,45 @@ class Customer {
       throw new Error(Constant.MESSAGE.ERROR.OUT_OF_RANGE);
     }
   }
-  purchaseLotto() {
-    MissionUtils.Console.readLine(
+  async purchaseLotto() {
+    await MissionUtils.Console.readLine(
       Constant.MESSAGE.GUIDE.ENTER_MONEY,
       (answer) => {
         //유효성검사
         this.publishLotto(answer);
-        console.log(this.lottoList);
+        this.getWinningNumber();
       }
     );
   }
   publishLotto(money) {
     const lottoNum = Math.floor(money / Constant.LOTTO.LOTTO_PRICE);
+    MissionUtils.Console.print(
+      Constant.MESSAGE.GUIDE.SUCCESS_TO_PURCHASE(lottoNum)
+    );
     for (let i = 0; i < lottoNum; i++) {
       const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
       this.lottoList.push(numbers);
+      MissionUtils.Console.print(`[${numbers}]`);
     }
-    return this.lottoList;
   }
 
+  getWinningNumber() {
+    MissionUtils.Console.readLine(
+      Constant.MESSAGE.GUIDE.ENTER_NUMBER,
+      (answer) => {
+        this.#lotto = new Lotto(answer);
+        this.getBonusNumber();
+      }
+    );
+  }
+  getBonusNumber() {
+    MissionUtils.Console.readLine(
+      Constant.MESSAGE.GUIDE.ENTER_NUMBER,
+      (answer) => {
+        this.#lotto.setBonusNum(answer);
+      }
+    );
+  }
   // TODO: 추가 기능 구현
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,16 +1,25 @@
 const Constant = require("./utils/Constant");
 class Lotto {
   #numbers;
+  #bonusNumber;
 
   constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = this.validate(numbers);
+    console.log(this.#numbers);
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
+    const numbersArr = numbers.split(",");
+    console.log(numbersArr);
+    if (numbersArr.length !== 6) {
       throw new Error(Constant.MESSAGE.ERROR.OUT_OF_RANGE);
     }
+    return numbersArr;
+  }
+
+  setBonusNum(number) {
+    //유효성 검사
+    this.#bonusNumber = number;
   }
 
   // TODO: 추가 기능 구현
