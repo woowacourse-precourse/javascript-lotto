4d0c7cebc5c16d0ba4d0b0c0dafe6f9859f086be
hwnim5324 <hwnim5324@naver.com>
2022-11-14 23:36:34 +0900

    feat: 로또 정답 관련 기능 구현 완료.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,16 @@
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
+  constructor(numbers, bonus) {
+    this.#numbers = {
+      numbers: numbers,
+      bonus: bonus
+    };
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  getNumbers(){
+    return this.#numbers;
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
