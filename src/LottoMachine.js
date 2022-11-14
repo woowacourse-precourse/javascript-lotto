a6214da857ba87854442122149429132685beec7
hjy0951 <hjy0951@gmail.com>
2022-11-15 00:10:43 +0900

    feat: LottoMachine에서 로또 번호 정렬하여 발급

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -5,7 +5,8 @@ class LottoMachine {
   buyLottos(money) {
     const lottos = [];
     while (money > 0) {
-      const numbers = this.createLottoNumbers();
+      let numbers = this.createLottoNumbers();
+      numbers = this.sortNumbers(numbers);
       lottos.push(new Lotto(numbers));
       money -= 1000;
     }
@@ -14,6 +15,15 @@ class LottoMachine {
 
   createLottoNumbers() {
     return Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
+
+  sortNumbers(numbers) {
+    numbers = numbers.sort(function (a, b) {
+      if (a > b) return 1;
+      if (a === b) return 0;
+      return -1;
+    });
+    return numbers;
   }
 }
 
