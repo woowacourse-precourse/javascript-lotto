2068ac407b02ee7ad824bae6decd6b60d41c3a79
deli-ght <minjju0660@gmail.com>
2022-11-15 01:57:03 +0900

    feat(Lotto.js): calculate win grades

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const { IS_ENOUGH, IS_RANGE } = require("./const/ErrorMessages");
 const { Console, Random } = MissionUtils;
 
 class Lotto {
@@ -10,16 +11,37 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    const NUMBER_LIST = [...new Set(numbers)];
+    if (NUMBER_LIST.length !== 6) throw Error(IS_ENOUGH);
+    NUMBER_LIST.forEach((number) => {
+      if (!Number.isInteger(+number) || number < 1 || number >= 46)
+        throw Error(IS_RANGE);
+    });
   }
 
   print() {
-    Console.print(this.#numbers);
+    Console.print(`[${this.#numbers.join(", ")}]`);
   }
 
-  // TODO: 추가 기능 구현
+  win(winnerNumbers, bonusNumber) {
+    const intersectionNum = [
+      ...new Set(
+        [...winnerNumbers].filter((number) =>
+          new Set(this.#numbers).has(number),
+        ),
+      ),
+    ];
+
+    if (intersectionNum.length === 5) {
+      const lastNum = [...new Set([...intersectionNum], this.#numbers)][0];
+      if (lastNum === bonusNumber) return 2;
+      return 3;
+    }
+
+    if (intersectionNum.length === 6) return 1;
+
+    return 6 - intersectionNum.length + 2;
+  }
 }
 
 module.exports = Lotto;
