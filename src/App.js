6d5d62ba01bdd0e1ad5a24158f9b1f418c612218
kyeongwxx <kyeongwxx@gmail.com>
2022-11-15 02:00:47 +0900

    feat(App): 보너스 번호 입력 문구 출력 및 사용자 입력 값을 받는 기능 구현
    "보너스 번호를 입력해 주세요." 문자열 출력 및 사용자 입력 값을 입력받음
    
    출력 및 입력은 [MissionUtils 라이브러리]에서 제공하는 Console.readLine() 메서드를 사용

--- src/App.js
+++ src/App.js
@@ -15,14 +15,19 @@ class App {
       this.game.setGame(input);
       this.game.quantityOfPurchase();
       this.game.printWinningNumberList();
-      this.inputLottoNumber();
+      this.inputWinningNumber();
     });
   }
 
-  inputLottoNumber() {
+  inputWinningNumber() {
     Console.readLine("\n당첨 번호를 입력해 주세요.\n", (input) => {
       this.game.setWinningNumber(input);
+      this.inputBonusNumber();
     });
+  }
+
+  inputBonusNumber() {
+    Console.readLine("\n보너스 번호를 입력해 주세요.\n", (input) => {});
   }
 }
 
