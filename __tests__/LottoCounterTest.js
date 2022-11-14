1fda246f59bbf85165a17d59c8fb8148ff16eb1d
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-14 15:27:31 +0900

    refactor: 테스트 설명 수정 및 코드 간결하게 수정

--- __tests__/LottoCounterTest.js
+++ __tests__/LottoCounterTest.js
@@ -1,32 +1,30 @@
 const LottoCounter = require('../src/LottoCounter');
 
 const getLottoQuantity = (cash) => {
-  const lottoCounter = new LottoCounter(Number(cash));
-  return lottoCounter.getLottosQuantity();
+  return new LottoCounter(Number(cash)).getLottosQuantity();
 };
 
-describe('LottoCounter 테스트', () => {
-  test('기능 테스트: 구매한 로또의 개수 구하기', () => {
+describe('LottoCounter 클래스 테스트', () => {
+  test('기능 테스트: 입력한 구입 금액에 해당하는 로또 개수를 구한다.', () => {
     let inputtedCash = '8000';
-    let result = getLottoQuantity(inputtedCash);
-    expect(result).toEqual(8);
+    expect(getLottoQuantity(inputtedCash)).toEqual(8);
   });
 
-  test('예외 테스트: 입력 받은 돈이 숫자가 아닌 경우', () => {
+  test('예외 테스트: 입력 받은 금액이 숫자가 아니면 예외가 발생한다.', () => {
     let inputtedCash = 'd134!';
     expect(() => {
       getLottoQuantity(inputtedCash);
     }).toThrow('[ERROR] 숫자만 입력해야 합니다.');
   });
 
-  test('예외 테스트: 입력 받은 돈이 1,000원 미만인 경우', () => {
+  test('예외 테스트: 입력 받은 금액이 1,000원 미만이면 예외가 발생한다.', () => {
     let inputtedCash = '200';
     expect(() => {
       getLottoQuantity(inputtedCash);
     }).toThrow('[ERROR] 1,000원 미만의 금액으로 로또를 구매할 수 없습니다.');
   });
 
-  test('예외 테스트: 입력 받은 돈이 1,000원 단위가 아닌 경우', () => {
+  test('예외 테스트: 입력 받은 금액이 1,000원 단위가 아니면 예외가 발생한다.', () => {
     let inputtedCash = '12345';
     expect(() => {
       getLottoQuantity(inputtedCash);
--- __tests__/NumberCreatorTest.js
+++ __tests__/NumberCreatorTest.js
@@ -1,12 +1,12 @@
 const NumberCreator = require('../src/NumberCreator');
 
-describe('NumberCreator 테스트', () => {
-  test('기능 테스트: 6개의 랜덤 숫자 생성하기', () => {
-    const numberCreator = new NumberCreator();
-    let numbers = numberCreator.getRandomSixNumbers();
+describe('NumberCreator 클래스 테스트', () => {
+  test('기능 테스트: 서로 중복되지 않는 6개의 랜덤 숫자를 생성한다.', () => {
+    let numbers = new NumberCreator().getRandomSixNumbers();
 
     expect(numbers).toHaveLength(6);
     expect(numbers.length).toEqual(new Set(numbers).size);
+
     numbers.every((number) => {
       expect(number).toBeGreaterThanOrEqual(1);
       expect(number).toBeLessThanOrEqual(45);
