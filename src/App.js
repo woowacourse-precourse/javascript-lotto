6ec92942ff8b3ebac66bee60b976c54ec16df46e
vjvl95 <vjvl95@naver.com>
2022-11-15 04:41:50 +0900

    feat : money에 대한 오류 검증 함수 추가

--- src/App.js
+++ src/App.js
@@ -7,9 +7,24 @@ class App {
 
   getMoney() {
     let money = 0;
-    MissionUtils.Console.readLine('구매금액을 입력해주세요.', (answer) => {
+    MissionUtils.Console.readLine('구매금액을 입력해주세요:', (answer) => {
       money = answer;
+      this.isValidMoney(money);
     });
+  }
+  isValidMoney(money) {
+    if (Number.isNaN(money)) {
+      throw `[ERROR] 입력 금액이 숫자형태가 아닙니다.`;
+    }
+    if (money % 1000 !== 0) {
+      throw `[ERROR] 천원 단위로 금액을 지불해주세요.`;
+    }
+    if (money > 1000000) {
+      throw `[ERROR] 한 번에 최대로 구입할 수 있는 금액은 100만원 입니다. 입력한 금액: ${money}`;
+    }
+    if (money < 1000) {
+      throw `[ERROR] 로또 한장의 가격은 1000원입니다. 입력한 금액: ${money}`;
+    }
   }
 }
 const app = new App();
