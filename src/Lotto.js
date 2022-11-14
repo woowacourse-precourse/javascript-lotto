27c3c850c1ccaac02d2b6bef7ee2fd62250a1d5f
Junseo <oponize@naver.com>
2022-11-14 17:53:24 +0900

    refactor: Lotto 클래스 검증 로직를 보기 편하게 변경

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,23 +10,11 @@ class Lotto {
   }
 
   validateLotto(numbers) {
-    Validator.isLength(numbers, LOTTO_NUMBER.VALID_NUMBER_LENGTH)
-      .isDuplicated(numbers)
-      .isEvery({
-        target: numbers,
-        callback: Validator.isNumber,
-        message: "[ERROR] 숫자가 아닌 번호가 존재합니다.",
-      })
-      .isEvery({
-        target: numbers,
-        callback: (number) =>
-          Validator.isRange({
-            target: number,
-            start: LOTTO_NUMBER.MIN_NUMBER,
-            end: LOTTO_NUMBER.MAX_NUMBER,
-          }),
-        message: "[ERROR] 로또 번호가 1~45 사이가 아닙니다.",
-      });
+    Validator.isLength(numbers, LOTTO_NUMBER.VALID_NUMBER_LENGTH).isDuplicated(numbers);
+    numbers.every(Validator.isNumber);
+    numbers.every((number) =>
+      Validator.isRange({ target: number, start: LOTTO_NUMBER.MIN_NUMBER, end: LOTTO_NUMBER.MAX_NUMBER })
+    );
     return true;
   }
 
