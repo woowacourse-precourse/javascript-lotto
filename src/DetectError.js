6b831b39c0cbde48fd2173cbc2c05720112f2e21
devMingu <rooster100@naver.com>
2022-11-14 17:49:21 +0900

    fix: 구입 금액 입력시 기능 수정

--- src/DetectError.js
+++ src/DetectError.js
@@ -6,6 +6,30 @@ class DetectError {
             throw new Error(`${Message.ERROR_MESSAGE.OVERLAP}`);
         }
     }
+
+    checkPrizNumber(przNum) {
+        if (przNum.length !== 6) {
+            throw new Error(`${Message.ERROR_MESSAGE.COMMA}`);
+        }
+
+        przNum.forEach(element => {
+            let prizeN = parseInt(element);
+            if ((prizeN < 1 || prizeN > 45) || !prizeN) {
+                throw new Error(`${Message.ERROR_MESSAGE.RANGE}`);
+            }
+        });
+    }
+
+    checkUserInput(userMoney) {
+        const userInput = [...userMoney];
+        userInput.forEach(element => {
+            if (!(element >= '0' && element <= '9')) {
+                throw new Error(`${Message.ERROR_MESSAGE.NUMBER}`);
+            }
+        });
+    }
+
+
 }
 
 
