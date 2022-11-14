ccc06dfbce4fc1ccbc11d287181857407e463b05
cocoa <jwv0323@kau.kr>
2022-11-14 11:13:46 +0900

    feat: 게임 진행 중에 출력할 문자열을 상수로 선언

---
+++ src/Constants.js
@@ -0,0 +1,29 @@
+const INPUT = {
+  PURCHASE_AMOUNT: '구입금액을 입력해 주세요.',
+  LOTTO_NUMBER: '당첨 번호를 입력해 주세요.',
+  BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+};
+
+const OUTPUT = {
+  PURCHASE_COUNT: '개를 구매했습니다.',
+  LOTTO_STATISTICS: '당첨 통계\n---',
+};
+
+const LOTTO = {
+  FIFTH_PLACE: '3개 일치 (5,000원) - ',
+  FOURTH_PLACE: '4개 일치 (50,000원) - ',
+  THIRD_PLACE: '5개 일치 (1,500,000원) - ',
+  SECOND_PLACE: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  FIRST_PLACE: '6개 일치 (2,000,000,000원) - ',
+
+  COUNT: '개',
+  RATE_RETURN: '총 수익률은 ',
+  PREDICATIVE_PARTICLE: '입니다.',
+}
+
+module.exports = {
+  INPUT,
+  OUTPUT,
+  LOTTO,
+};
+  
