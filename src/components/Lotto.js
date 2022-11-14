d6043e0f7ee8266c6737f03b4d6a835c0f46f38e
nalsae <nalsae0207@naver.com>
2022-11-14 10:57:04 +0900

    refactor: seperate methods to simplify
    로직을 분리하여 코드 전반의 단순화 완료
    - updateStatics 메서드 분리
    - printResults 메서드를 App 클래스로 이전

--- src/components/Lotto.js
+++ src/components/Lotto.js
@@ -5,51 +5,56 @@ const {
   ERROR_MESSAGES,
   ADD_COMMA_EXP,
 } = require("../utils/constants");
-const { Console } = require("@woowacourse/mission-utils");
 
 class Lotto {
   #winningNums;
 
   constructor(winningNums) {
     this.validateWinningNubmer(winningNums);
+
     this.#winningNums = winningNums.map((num) => +num);
+    this.statics = Object.assign({}, INITIAL_STATICS);
   }
 
   calculateStatics(issuedLottos, bonusNumber) {
-    const statics = Object.assign({}, INITIAL_STATICS);
-
     issuedLottos.forEach((lotto) => {
       let sameCount = 0;
 
       lotto.forEach((num, index) => {
-        this.#winningNums.includes(num) && sameCount++;
+        this.#winningNums.includes(num) && (sameCount += 1);
 
-        if (index + 1 === lotto.length) {
-          if (sameCount === 5 && lotto.includes(bonusNumber)) {
-            statics["5andBonus"] += 1;
-          } else {
-            sameCount in statics && (statics[sameCount] += 1);
-          }
-        }
+        if (index + 1 === lotto.length)
+          this.updateStatics(sameCount, lotto.includes(bonusNumber));
       });
     });
 
-    this.calculateEarningsRate(statics, issuedLottos.length);
+    const earningsRate = this.calculateEarningsRate(issuedLottos.length);
+
+    return { statics: this.statics, earningsRate };
   }
 
-  calculateEarningsRate(statics, purchase) {
-    const matchedCounts = Object.keys(statics);
+  updateStatics(sameCount, isMatchBonusNum) {
+    if (sameCount === 5 && isMatchBonusNum) {
+      this.statics["5andBonus"] += 1;
+    } else {
+      sameCount in this.statics && (this.statics[sameCount] += 1);
+    }
+  }
+
+  calculateEarningsRate(purchase) {
+    const matchedCounts = Object.keys(this.statics);
     let total = 0;
 
-    const addWinningPrice = (matchedCount) =>
-      statics[matchedCount] !== 0 &&
-      (total += WINNING_PRICES[matchedCount] * statics[matchedCount]);
+    const addWinningPrice = (matchedCount) => {
+      this.statics[matchedCount] !== 0 &&
+        (total += WINNING_PRICES[matchedCount] * this.statics[matchedCount]);
+    };
 
     matchedCounts.forEach((matchedCount) => addWinningPrice(matchedCount));
 
     const earnings = (total / (purchase * LOTTO_PRICE)) * 100;
 
-    this.printResults(statics, this.convertRate(earnings));
+    return this.convertRate(earnings);
   }
 
   convertRate(earnings) {
@@ -59,22 +64,6 @@ class Lotto {
         .replace(ADD_COMMA_EXP, ",") + "%";
 
     return earningsRate;
-  }
-
-  printResults(statics, earningsRate) {
-    const resultMessage = `
-당첨 통계
----
-3개 일치 (5,000원) - ${statics[3]}개
-4개 일치 (50,000원) - ${statics[4]}개
-5개 일치 (1,500,000원) - ${statics[5]}개
-5개 일치, 보너스 볼 일치 (30,000,000원) - ${statics["5andBonus"]}개
-6개 일치 (2,000,000,000원) - ${statics[6]}개
-총 수익률은 ${earningsRate}입니다.
-`;
-
-    Console.print(resultMessage);
-    Console.close();
   }
 
   validateWinningNubmer(winningNums) {
