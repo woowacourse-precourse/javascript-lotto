18d8960cba6cbf5241aad82926a26e6fefb4d4fe
YejinHwang-D <hyj3463@naver.com>
2022-11-14 21:41:37 +0900

    feat: 금액 입력받기 기능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 
 class App {
+  constructor() {
+    this.purchase;
+  }
+
   play() {
-    const lotto = new Lotto();
-    
+    this.purchase = this.input('구입금액을 입력해 주세요.\n');
+    if (this.purchase === undefined)
+      return 0;
+  }
+
+  input(text) {
+    let result;
+    MissionUtils.Console.readLine(text, (answer) => {
+      result = answer;
+    });
+    return result;
   }
 }
 
