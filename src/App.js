6a7ac58e433532d316b755649288567415261744
jiho1996 <jhp9270@naver.com>
2022-11-14 21:10:51 +0900

    refactor : controller분리에 따른 app모듈 코드 정리

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
-const Calculator = require("./Calculator");
-const MyNumberGenerator = require("./Generator");
-const Lotto = require("./Lotto");
-const { readLine } = require("./Missionutils");
-
+const LottoGameController = require("./LottoGameController");
 class App {
-  #winningNumbers;
-  #numberGenerator = new MyNumberGenerator();
-
   play() {
-    this.inputMoneyFromUser();
-  }
-
-  inputMoneyFromUser() {
-    readLine("구입 금액을 입력해주세요.", (moneyInput) => {
-      const myLottoNumbers = this.#numberGenerator.generateMyLottoNumber(moneyInput);
-      this.getWinningNumbersFromUser(myLottoNumbers);
-    });
-  }
-
-  getWinningNumbersFromUser(myLottoNumbers) {
-    readLine("당첨 번호를 입력해 주세요.", (winningNumber) => {
-      this.getBonusWinningNumberFromUser(myLottoNumbers, winningNumber);
-    });
-  }
-
-  getBonusWinningNumberFromUser(myLottoNumbers, winningNumber) {
-    readLine("보너스 번호를 입력해 주세요.", (bonusNumber) => {
-      const lotto = new Lotto({ winningNumber, bonus: +bonusNumber });
-      this.#winningNumbers = lotto.getConvertedLottoNumber();
-      this.getResult(myLottoNumbers);
-    });
-  }
-
-  getResult(myLottoNumbers) {
-    this.calculateWinningCount(myLottoNumbers);
-  }
-
-  calculateWinningCount(myLottoNumbers) {
-    new Calculator(myLottoNumbers, this.#winningNumbers);
+    new LottoGameController().start();
   }
 }
 const a = new App();
