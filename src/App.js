f111fd00434aaf33811d2746c3419ca3f911bd4c
murpin <qw04011@gmail.com>
2022-11-14 18:14:39 +0900

    feat(App): constructor 선언 추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ const { Console, Random } = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 
 class App {
-  play() {}
+  constructor(){
+    this.lottoCount = 0;
+    this.lottoInfo = [];
+    this.BonusNumber = '';
+    this.lottoResult = [0,0,0,0,0];
+  };
+
 }
 
 module.exports = App;
