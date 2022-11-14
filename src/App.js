ac174099f5812b19403ab0f1357fa3649f056b8e
KangYunJi <dbswl701@ajou.ac.kr>
2022-11-15 01:09:53 +0900

    feat: 금액 입력 시 로또 갯수 결정 및 에러 처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Console = MissionUtils.Console;
+const Lotto = require("./Lotto");
 
 class App {
 
+  isValidCacheInput(answer){
+    const lottoCount = answer/1000;
+    if(!Number.isInteger(lottoCount)) {
+      throw new Error("[ERROR] 이상한 입력");
+    }
+    return lottoCount;
+  }
+
+
   InputCache(){
     Console.readLine('구입금액을 입력해 주세요.', (answer)=>{
-      console.log(`금액: ${answer}`);
-      Console.print(`${answer}개를 구매했습니다.`);
+      // 맞는지 확인하는 함수
+      const lottoCount = this.isValidCacheInput(answer);
+      Console.print(`${lottoCount}개를 구매했습니다.`);
     })
     Console.close();
   }
