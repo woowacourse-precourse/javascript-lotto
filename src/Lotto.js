52cab8eabb6c525b0afab0c9c194d59fe904c706
saetakki <lime.lemonade03@gmail.com>
2022-11-15 04:22:18 +0900

    refactor. Change validate function

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,6 +1,6 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const { isNotDuplicated } = require('./Validator');
 const Console = MissionUtils.Console;
-const Validator = require('./Validator');
 const randomNums = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
 console.log(randomNums);
 
@@ -8,27 +8,22 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    Lotto.validate(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error();
-    }
-  }
+  static validate = (...func) => {
+    func.reduce((acc, cur) => {
+      if (acc) return cur;
+    });
+  };
+
+  static makeIntArray = input => {
+    return input.split(',').map(x => Number(x));
+  };
 
   // TODO: 추가 기능 구현
-
-  static inputHowMuchToPay() {
-    Console.readLine('구입금액을 입력해 주세요.', pay => {
-      if (this.isValidPayAmount(pay)) {
-        Console.print(`${parseInt(pay / 1000)} 개를 구매했습니다.`);
-      }
-    });
-  }
 }
 
 const lotto = new Lotto(randomNums);
-
 module.exports = Lotto;
