5a7a8b569ea605952a0d027eedaf44644103bb89
leeuihyun <meta212121@naver.com>
2022-11-15 02:34:59 +0900

    feat : 보너스 번호에 관한 로직 단위 테스트

---
+++ src/lib/utils/bonusUtils.js
@@ -0,0 +1,16 @@
+const { LOTTO_RANGE_REGEX } = require('../constants');
+
+const checkWinningIncludeBonus = (winningNumbers, bonus) => {
+  console.log(winningNumbers.includes(bonus));
+  if (winningNumbers.includes(bonus)) {
+    return true;
+  }
+  return false;
+};
+
+const checkBonusRange = (bonus) => {
+  if (!LOTTO_RANGE_REGEX.test(bonus)) return true;
+  return false;
+};
+
+module.exports = { checkWinningIncludeBonus, checkBonusRange };
