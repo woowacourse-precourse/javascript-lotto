4b43ac1f586a421a1f88f124946e88cc3968baed
solo5star <solo_5@naver.com>
2022-11-15 07:10:33 +0900

    feat(lotto-basic): 로또가 1에서 45 사이의 숫자를 가지도록 구현

--- src/domains/Lotto.js
+++ src/domains/Lotto.js
@@ -2,6 +2,12 @@ class Lotto {
   /** 로또가 가질 수 있는 숫자 갯수 */
   static NUMBER_COUNT = 6;
 
+  /** 로또가 가질 수 있는 숫자의 최소값 */
+  static NUMBER_MIN = 1;
+
+  /** 로또가 가질 수 있는 숫자의 최대값 */
+  static NUMBER_MAX = 45;
+
   /** @type {number[]} */
   #numbers;
 
@@ -13,9 +19,15 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  /**
+   * @param {number[]} numbers
+   */
   validate(numbers) {
     if (numbers.length !== Lotto.NUMBER_COUNT) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (numbers.every((number) => Lotto.NUMBER_MIN <= number && number <= Lotto.NUMBER_MAX)) {
+      throw new Error('[ERROR] 로또 번호는 1에서 45 사이여야 합니다.');
     }
   }
 }
