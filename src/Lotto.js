6e1c7f8418bf757520663e3c3b662228540f20dc
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-14 19:57:02 +0900

    feat: 로또 당첨 번호 중복 체크하는 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,19 @@ class Lotto {
 
   validateLottoNumbers(numbers) {
     this.validateLengthIsSix(numbers);
+    this.validateIsDuplication(numbers);
   }
 
   validateLengthIsSix(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+  }
+
+  validateIsDuplication(numbers) {
+    const duplicationCheck = new Set(numbers);
+    if (duplicationCheck.size !== numbers.length) {
+      throw new Error('[ERROR] 중복이 없는 서로 다른 숫자를 입력하세요.');
     }
   }
 }
