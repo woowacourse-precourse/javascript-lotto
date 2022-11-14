510a340b0ea22cd8e8de6e178f0834f13da8d672
MINJUN PARK <mj9457@naer,com>
2022-11-14 22:41:35 +0900

    feat. 6자리 랜덤 숫자 생성 메서드

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const { Console, Random } = MissionUtils;
 
 class App {
+  randomSixNumberArr = [];
+
   play() {
     this.inputOfLottoPurchaseAmount();
-    this.printPurchaseCountMessage();
   }
 
   inputOfLottoPurchaseAmount() {
+    let countLotto;
     Console.readLine("구입금액을 입력해 주세요.",(amountInput) => {
       amountInput = parseInt(amountInput);
       this.validationInputLottoPurchaseAmount(amountInput);
+      countLotto = amountInput % 1000;
+      this.printPurchaseCountMessage(countLotto);
+      this.createRandomSixNumber(countLotto);
     });
   }
 
   validationInputLottoPurchaseAmount(amountInput) {
-    const VALIDATE_AMOUNT_1000 = amountInput % 1000
+    const COUNT_LOTTO = amountInput % 1000
     if (amountInput < 0) {
       throw new Error(`[ERROR] 입력한 금액 ${amountInput}원은 음수가 될 수 없습니다.`);
     }
@@ -28,13 +33,20 @@ class App {
       throw new Error(`[ERROR] 입력한 금액 ${amountInput}원은 숫자 이외의 값이 될 수 없습니다.`)
     }
 
-    if (VALIDATE_AMOUNT_1000 != 0) {
+    if (COUNT_LOTTO != 0) {
       throw new Error(`[ERROR] 입력한 금액 ${amountInput}원은 1,000원 단위로 나누어 떨어지지 않습니다.`)
     }
   }
 
-  printPurchaseCountMessage(VALIDATE_AMOUNT_1000) {
-    Console.print(`${VALIDATE_AMOUNT_1000}개를 구매했습니다.`);
+  printPurchaseCountMessage(COUNT_LOTTO) {
+    Console.print(`${COUNT_LOTTO}개를 구매했습니다.`);
+  }
+
+  createRandomSixNumber(COUNT_LOTTO) {
+    for(let i=0; i<COUNT_LOTTO; i++) {
+      randomNumber = Random.pickUniqueNumbersInRange(1, 45, 6)
+      this.randomSixNumberArr.push(randomNumber);
+    }
   }
 }
 
