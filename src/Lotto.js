c4ee85d961b94f4027221c3fd52165ea2f453f43
kitez <duswl339@ajou.ac.kr>
2022-11-15 00:13:29 +0900

    feat: Lotto class 내부 method utils 함수로 전환
    + Lotto뿐만 아니라 당첨 번호랑 보너스 번호도 validate check가 필요하기 때문에 수정.

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,35 +4,23 @@ const Constant = require('./Constant');
 class Lotto {
   #numbers;
   #same;
+  #bonus;
 
   constructor(numbers) {
-    this.#validateLength(numbers);
-    this.#validateNumber(numbers);
+    utils.validateLotto(numbers);
     this.#numbers = numbers;
     this.#same = 0;
-  }
-
-  // TODO: 추가 기능 구현
-  /**
-   * 입력된 lotto 번호의 길이가 6이 아닌 경우 체크
-   */
-  #validateLength(numbers) {
-    if (numbers.length !== Constant.LOTTO_LENGTH) {
-      throw new Error("[ERROR] 로또 번호는 "+Constant.LOTTO_LENGTH+"개여야 합니다.");
-    }
-  }
-
-  #validateNumber(numbers) {
-      for(let i = 0 ; i < Constant.LOTTO_LENGTH ; i++){
-        if(numbers[i] <= 0 || numbers[i] >= 46){
-          throw new Error("[ERROR] 로또 번호는 1~45사이어야 합니다.");
-        }
-      }
+    this.#bonus = false;
   }
 
   // 당첨된 번호 갯수 반환하는 함수
   get WinningNumber(){
     return this.#same;
+  }
+
+  // 보너스 여부 반환 함수
+  get bonus(){
+    return this.#bonus;
   }
 
   // 당첨된 번호 갯수 계산하는 함수
@@ -45,6 +33,11 @@ class Lotto {
   }
 
   // 보너스 번호 일치하는지 확인하는 함수
+  matchBonus(bonus){
+    if(this.#numbers.includes(Number(bonus))){
+      this.#bonus = true;
+    }
+  }
 
   // 수익금
 }
