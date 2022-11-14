4f220424bda926cc25beaca250d6e9ec94d1f670
YeonsuBaek <soothbugger@gmail.com>
2022-11-14 22:41:27 +0900

    test(lotto): 구매금액이 1원보다 작은 경우 예외 처리 테스트 완료

--- __tests__/MoneyTest.js
+++ __tests__/MoneyTest.js
@@ -7,6 +7,18 @@ describe("Money 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
+  test("구입 금액이 1원보다 작은 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new Money(["0"]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("구입 금액이 1원보다 작은 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new Money(["-1000"]);
+    }).toThrow("[ERROR]");
+  });
+
   test("구입 금액이 천원 단위가 아닌 경우 예외가 발생한다.", () => {
     expect(() => {
       new Money([["4500"]]);
