ce6209c946f82efe8a4925c4ff2780436b8cf64e
동동이 <drdragon777@naver.com>
2022-11-15 01:42:45 +0900

    feat - add initial play scenario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 const Console = require("./Console");
+const Lotto = require("./Lotto");
+const Validator = require("./validator/Validator");
 const { THREE, FOUR, FIVE, FIVE_AND_BONUS, SIX } = require("./constants");
+const LottoAdmin = require("./LottoAdmin");
 
 class App {
   constructor() {
@@ -12,10 +15,17 @@ class App {
       [FIVE]: 0,
       [FIVE_AND_BONUS]: 0,
       [SIX]: 0,
-    }
+    };
   }
   play() {
+    Console.getUserInput(Console.REQUEST_LOTTO_PRICE, (price) => {
+      if (!Validator.isValidPrice(price)) return;
 
+      const lottoNum = Lotto.caculateLottoNumPerUnit(Number(price));
+      Console.print("\n" + lottoNum + Console.PURCHASED_LOTTO_COUNT_MSG);
+
+      const lottos = LottoAdmin.generateLottoAnswer(lottoNum);
+    });
   }
 }
 
