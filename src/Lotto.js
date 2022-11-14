5629525279b1e41ca4ae5ec2a2c3e578e899d8d8
kimeodml <kimeodml@gmail.com>
2022-11-15 05:15:25 +0900

    feat: 당첨 번호 유효성 검사하는 함수 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,6 +1,9 @@
+const { LOTTO_ERROR } = require("./constants/Constants.js");
+
+
 class Lotto {
   #numbers;
-
+  
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
@@ -8,10 +11,19 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(LOTTO_ERROR.LENGTH);
+    }
+    if (new Set(numbers).size !== 6) {
+      throw new Error(LOTTO_ERROR.DUPLICATE);
+    }
+    if (isNaN(numbers.join(''))) {
+      console.log(isNaN(numbers))
+      throw new Error(LOTTO_ERROR.NUMBER);
+    }
+    if (numbers.some(number => number < 1 || number > 45)) {
+      throw new Error(LOTTO_ERROR.RANGE);
     }
   }
-
   // TODO: 추가 기능 구현
 }
 
