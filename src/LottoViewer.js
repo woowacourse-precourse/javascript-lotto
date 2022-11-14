624b2ab087691048927416547fa0741b6acf7878
thewronghand <penfreak77@gmail.com>
2022-11-15 03:54:21 +0900

    refactor: 상수 변수 사용문 수정

--- src/LottoViewer.js
+++ src/LottoViewer.js
@@ -1,11 +1,6 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-const {
-  SIX_MATCH_REWARD,
-  BONUS_MATCH_REWARD,
-  FIVE_MATCH_REWARD,
-  FOUR_MATCH_REWARD,
-  THREE_MATCH_REWARD
-} = require('./constants/numbers.js');
+
+const { REWARD } = require('./constants/numbers');
 
 class LottoViewer {
   arrangeLottoWinningResult(ranks, money) {
@@ -14,11 +9,11 @@ class LottoViewer {
       result[element] = (result[element] || 0) + 1;
     }
     const totalIncome =
-      SIX_MATCH_REWARD * result[1] +
-      BONUS_MATCH_REWARD * result[2] +
-      FIVE_MATCH_REWARD * result[3] +
-      FOUR_MATCH_REWARD * result[4] +
-      THREE_MATCH_REWARD * result[5];
+      REWARD.SIX_MATCH_REWARD * result[1] +
+      REWARD.BONUS_MATCH_REWARD * result[2] +
+      REWARD.FIVE_MATCH_REWARD * result[3] +
+      REWARD.FOUR_MATCH_REWARD * result[4] +
+      REWARD.THREE_MATCH_REWARD * result[5];
     result['returnRate'] = ((totalIncome / money) * 100).toFixed(1);
     return result;
   }
