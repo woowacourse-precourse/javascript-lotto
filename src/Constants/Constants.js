0d823a825c3d35e9e5449c47ccb530c5e5b90ef4
osdoonhyun <skw1996@naver.com>
2022-11-14 17:28:45 +0900

    feat(Constants): MESSAGES,ERROR_MESSAGES,LOTTO 관련 상수 생성

---
+++ src/Constants/Constants.js
@@ -0,0 +1,14 @@
+const MESSAGES = {
+  ENTER_PURCHASE_AMOUNT: "구입 금액을 입력해 주세요.\n",
+};
+
+const ERROR_MESSAGES = {
+  INVALID_AMOUNT_UNIT: "[ERROR] 구입 금액은 1000원 단위여야 합니다.",
+};
+
+LOTTO = {
+  PRICE: 1000,
+  NUMBER: 6,
+};
+
+module.exports = { MESSAGES, ERROR_MESSAGES, LOTTO };
