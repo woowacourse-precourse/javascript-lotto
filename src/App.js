17a96c69187fbe3f0455c388731895f83413ab89
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-14 20:48:58 +0900

    feat: [구매 수량 출력]

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Money = require("./Money");
 
 class App {
-  play() {}
   constructor() {
     this.money = 0;
+    this.purchaseQuantity = 0;
   }
 
   play() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
       new Money(money);
-      MissionUtils.Console.print(money);
+      this.money = money;
+      this.printPurchaseQuantity(this.money);
     });
+  }
+
+  printPurchaseQuantity(money) {
+    const UNIT = 1000;
+    const convertIntoQuantity = money / UNIT;
+    this.purchaseQuantity = convertIntoQuantity;
+    MissionUtils.Console.print(`\n${this.purchaseQuantity}개를 구매했습니다.`);
   }
 }
 
