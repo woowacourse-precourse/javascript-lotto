bb25ec525401975736c66c4070992e512f465aad
chchaeun <kce3615@naver.com>
2022-11-15 03:27:49 +0900

    delete: Console에서 분리된 상수 삭제

--- src/Console.js
+++ src/Console.js
@@ -1,10 +1,6 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class Console {
-  static INPUT = {
-    PURCHASE: '구입금액을 입력해 주세요.',
-  };
-
   static readLine(message, callback) {
     return MissionUtils.Console.readLine(message, callback);
   }
