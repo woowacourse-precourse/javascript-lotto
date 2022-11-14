a93fc416c64ea7dce0ce8b2dd2ab581c5a7831e8
thewronghand <penfreak77@gmail.com>
2022-11-14 17:52:04 +0900

    test: 잘못된 금액 입력 시 에러 발생 테스트 구현
    validateMoney 메서드를 이용하여 금액 입력 시 잘못된 금액이 입력될 경우를 판별, 케이스에 맞는 에러를 반환하는 로직을 테스트한다.

--- __tests__/LottoStoreTest.js
+++ __tests__/LottoStoreTest.js
@@ -1,11 +1,35 @@
 const LottoStore = require('../src/LottoStore');
 
 describe('로또스토어 클래스 테스트', () => {
+  const lottoStore = new LottoStore();
   test('금액을 입력하면 해당 금액으로 구매할 수 있는 최대 로또 매수가 반환된다.', () => {
-    const lottoStore = new LottoStore();
     const testMoney = 5000;
     const expectedBuyCount = 5;
     const testCount = lottoStore.askBuyLottoCount(testMoney);
     expect(testCount).toBe(expectedBuyCount);
+  });
+
+  test('금액 입력 시 숫자 이외의 값을 입력하면 에러가 발생한다.', () => {
+    expect(() => {
+      lottoStore.validateMoney('삼천원');
+    }).toThrow('[ERROR]');
+  });
+
+  test('금액 입력 시 소수점 이하가 포함된 숫자를 입력하면 에러가 발생한다.', () => {
+    expect(() => {
+      lottoStore.validateMoney(2000.35);
+    }).toThrow('[ERROR]');
+  });
+
+  test('금액 입력 시 음수를 입력하면 에러가 발생한다.', () => {
+    expect(() => {
+      lottoStore.validateMoney(-2000);
+    }).toThrow('[ERROR]');
+  });
+
+  test('금액 입력 시 1000으로 나누어 떨어지지 않는 수를 입력하면 에러가 발생한다.', () => {
+    expect(() => {
+      lottoStore.validateMoney(1500);
+    }).toThrow('[ERROR]');
   });
 });
