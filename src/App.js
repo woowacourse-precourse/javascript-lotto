7a666a13f33a65efa007920ea64db1a8c89fb08c
gyeongza <gyeongza@naver.com>
2022-11-14 19:58:38 +0900

    feat : 로또 구매 금액 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.inputMoney = 0;
+  }
+
+  play() {
+    Console.readLine("구입금액을 입력해 주세요.\n", (inputMoney) => {
+      this.inputMoney = inputMoney;
+    });
+  }
 }
 
-module.exports = App;
+const app = new App();
+app.play();
+// module.exports = App;
