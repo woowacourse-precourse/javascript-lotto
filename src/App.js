a00626f7f3d7adc4ef2a1d9ea53c1a244cb84611
KangYunJi <dbswl701@ajou.ac.kr>
2022-11-14 23:54:01 +0900

    feat: 로또 구입 금액 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Console = MissionUtils.Console;
+
 class App {
-  play() {}
+
+  InputCache(){
+    Console.readLine('구입금액을 입력해 주세요.', (answer)=>{
+      console.log(`금액: ${answer}`);
+      Console.print(`${answer}개를 구매했습니다.`);
+    })
+    Console.close();
+  }
+
+
+
+  play() {
+    // 금액 입력
+    this.InputCache();
+      // 금액에 맞는 로또 수량 발행
+      // 해당 갯수에 맞는 로또 번호 생성
+      //CreateUserLotto();
+    // 당첨번호 입력
+    //InputWinLotto();
+    // 보너스 번호 입력
+    //InputBonusLotto();
+      // 당첨번호와 사용자 번호 비교
+     // CheckWin();
+    // 출력
+    //PrintLotto();
+  }
 }
 
 module.exports = App;
