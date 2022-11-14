454204a1a491bb160127d04ea6303d504c5e66e2
GunHoi <lgh509@kyonggi.ac.kr>
2022-11-14 17:05:27 +0900

    feat - 6 : 통계 출력

--- src/App.js
+++ src/App.js
@@ -116,6 +116,15 @@ class App {
       this.userWinningStatics[4] +=1;
     }
   }
+  printUserWinningStatics(winningStatic){
+    MissionUtils.Console.print("당첨 통계");
+    MissionUtils.Console.print("---");
+    MissionUtils.Console.print("3개 일치 (5,000원) - "+winningStatic[4]+"개");
+    MissionUtils.Console.print("4개 일치 (50,000원) - "+winningStatic[3]+"개");
+    MissionUtils.Console.print("5개 일치 (1,500,000원) - "+winningStatic[2]+"개");
+    MissionUtils.Console.print("5개 일치, 보너스 볼 일치 (30,000,000원) - "+winningStatic[1]+"개");
+    MissionUtils.Console.print("6개 일치 (2,000,000,000원) - "+winningStatic[0]+"개");
+  }
 }
 
 module.exports = App;
