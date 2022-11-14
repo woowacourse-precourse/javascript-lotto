57386c3c408cc19147714671a54bb3113fbdc33d
catca <kdg96g@gmail.com>
2022-11-14 23:54:02 +0900

    feat: 로또 구입 금액이 1,000원으로 나누어 떨어지지 않는 경우 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const { Console } = require("@woowacourse/mission-utils");
+const MESSAGES = require('./Constants');
+
 class App {
-  play() {}
+  constructor() {
+    this.payment = 0;
+  }
+  play() {
+    Console.readline(MESSAGES.PAYMENT, (payment) => {
+      this.payment = payment;
+    });
+    if(payment % 1000 === 0){
+      throw new Error(MESSAGES.ERROR.PAYMENT);
+    }
+  }
 }
+
+const app = App();
+app.play();
 
 module.exports = App;
