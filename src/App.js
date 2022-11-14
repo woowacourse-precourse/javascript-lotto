bb64b3b152930f74f7aee1c4553c33bfffac4268
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-14 18:16:38 +0900

    feat: App Class Method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { buyLottos, getStatisticsTemplate } = require("./Core/App.util");
 class App {
-  play() {}
-  inputBuyPrice() {}
-  buyLottos() {}
-  inputWinningNumber() {}
-  inputBonusNumber() {}
-  getStatistics() {}
-  getRateOfReturn() {}
+  play() {
+    this.inputBuyPrice((price) => {
+      if (price % 1000 !== 0) {
+        throw new Error("[ERROR] 복권 구매 금액 맞지 않음");
+      }
+      const lottoLength = price / 1000;
+      const lottos = buyLottos(lottoLength);
+      this.print(`${lottos}개를 구매했습니다.\n`);
+      lottos.forEach((lotto) => lotto.print());
+      this.inputWinningNumber((winningNumbers) => {
+        this.inputBonusNumber((bonusNumber) => {
+          const counts = lottos.map((lotto) =>
+            lotto.checkLottoResult(winningNumbers, bonusNumber)
+          );
+          const result = getStatistics(counts);
+          this.getStatistics(result);
+          this.getRateOfReturn(price, result);
+        });
+      });
+    });
+  }
+
+  inputBuyPrice(cb) {
+    Console.readLine("구입금액을 입력해 주세요. \n ", (n) => cb(n));
+  }
+
+  buyLottos(lottoLength) {
+    const lottos = buyLottos(lottoLength);
+  }
+
+  inputWinningNumber(cb) {
+    Console.readLine("\n당첨 번호를 입력해 주세요. \n ", (n) => cb(n));
+  }
+
+  inputBonusNumber(cb) {
+    Console.readLine("\n보너스 번호를 입력해 주세요. \n", (n) => cb(n));
+  }
+
+  getStatistics(statistics) {
+    this.print("당첨 통계 \n --- \n");
+    this.print(getStatisticsTemplate(statistics));
+  }
+
+  getRateOfReturn(price, statistics) {
+    this.print(
+      `총 수익률은 ${getRateOfReturn(price, getRevenue(statistics))}%입니다.`
+    );
+  }
+  print(message) {
+    Console.print(message);
+  }
 }
 
 module.exports = App;
