5da9c8ce0ff466c929b62ec11aa990d34b87f109
osdoonhyun <skw1996@naver.com>
2022-11-14 17:32:01 +0900

    feat(LottoGame): 기능 목록[1] 로또 구입 금액 1000원 단위로 입력 받기 기능 구현

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,5 +1,15 @@
+const {MESSAGES, } = require('./Constants/Constants')
+const Validation = require('./Validation');
 class LottoGame {
+
   play();
+
+  purchaseLotto() {
+    Console.readLine(MESSAGES.ENTER_PURCHASE_AMOUNT, (amount) => {
+      Validation.validatePurchaseAmount(amount);
+    });
+  }
+
 }
 
 module.exports = LottoGame;
