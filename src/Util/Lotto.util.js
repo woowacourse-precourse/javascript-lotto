bf40d10d87c987cf289181c206a8d9aa320382db
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-14 10:58:47 +0900

    feat: Lotto Util Func && Util Func Test Code

---
+++ src/Util/Lotto.util.js
@@ -0,0 +1,9 @@
+const checkDuplicatedLotto = (input) => {};
+
+const checkAscendingLotto = (input) => {};
+
+const checkLottoResult = (input, compareInput, bonus) => {};
+
+exports.checkDuplicatedLotto = checkDuplicatedLotto;
+exports.checkAscendingLotto = checkAscendingLotto;
+exports.checkLottoResult = checkLottoResult;
