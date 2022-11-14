98c5ab9ccded9376949d7b66204550b025b3e8c0
jihong <rlawyd9@kookmin.ac.kr>
2022-11-15 03:38:36 +0900

    feat: 로또 번호 추첨하기

--- src/App.js
+++ src/App.js
@@ -13,11 +13,19 @@ class App {
     MissionUtils.Console.print(`${lotto_cnt}개를 구매했습니다.`);
   }
 
+  draw_lotto(lotto_cnt){
+    for(let i=0; i<lotto_cnt; i++){
+      let drawed = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      MissionUtils.Console.print(drawed);
+    } 
+  }
+
   inputMoney(){
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', async(money) => {
       if(await this.checkInput(money)){
         const lotto_cnt = Math.floor(money/1000);
         this.print_lotto_cnt(lotto_cnt);
+        this.draw_lotto(lotto_cnt);
       }
       await MissionUtils.Console.close();
 
