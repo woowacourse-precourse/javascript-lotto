68d53a68fd24c3d6863a8bd78fc979389b9e4d48
cho seonghoon <chsida655@gmail.com>
2022-11-15 01:42:04 +0900

    feat: 로또 결과&수익률 출력하는 함수

--- src/Output.js
+++ src/Output.js
@@ -1,7 +1,20 @@
 const { Print } = require("./lib/MissionUtils.js");
-
+const { RESULT_MESSAGE } = require("./ResultMessage.js");
 const printUserPurchaseAmount = (amount) => {
-  return Print(`\n${amount / 1000}개를 구매했습니다.\n`);
+  return Print(`\n${amount}개를 구매했습니다.`);
 };
 
-module.exports = { printUserPurchaseAmount };
+const printLottoResult = (res) => {
+  Print("\n당첨통계");
+  Print("---");
+  res.forEach((el, i) => Print(RESULT_MESSAGE[i] + `${el}개`));
+};
+
+const printProfit = (profit, purchase) => {
+  let profitPercentage = isNaN((profit / purchase) * 100)
+    ? "0.0"
+    : ((profit / purchase) * 100).toFixed(1);
+  Print(`총 수익률은 ${profitPercentage}%입니다.`);
+};
+
+module.exports = { printUserPurchaseAmount, printLottoResult, printProfit };
