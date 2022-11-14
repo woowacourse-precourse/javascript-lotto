e1e792c3462d27ac6e330486229147501e77fe7a
sunny <huiseon37@gmail.com>
2022-11-15 01:34:18 +0900

    feat: message 스트링 상수 생성

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+const CONSOLE_MESSAGE = {
+  INPUT_PURCHASE_MONEY: "구입금액을 입력해 주세요.\n",
+  INPUT_WINNING_LOTTO: "당첨 번호를 입력해 주세요.\n",
+  SHOW_PURCHASED_MONEY: "개를 구매했습니다.",
+}
+
+module.exports = {CONSOLE_MESSAGE}
