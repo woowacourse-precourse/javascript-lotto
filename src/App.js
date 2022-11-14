6379069fcc035c351cc2b277206f8f9c6432644e
emma <jhj191058@gmail.com>
2022-11-15 03:45:56 +0900

    feat : 기능 1 - 1 로또 구매 금액 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+
 class App {
+  constructor() {}
+
   play() {}
+
+  lottoNum(answer) {
+    console.readLine('구입금액을 입력 해주세요/n', (answer) => {
+      console.log(answer);
+    });
+  }
 }
+
 
 module.exports = App;
