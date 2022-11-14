85cf321a7c73d5641e01e3c4719773ef6e9c4c3c
murpin <qw04011@gmail.com>
2022-11-14 18:36:33 +0900

    feat(Lotto/validate): 당첨번호 중복에 대한 예외처리 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -17,9 +17,11 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    const duplicateSet = new Set(numbers);
+    if (numbers.length !== duplicateSet) {
+      throw new Error("[ERROR] 중복된 수를 입력하면 안됩니다.")
+    }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
