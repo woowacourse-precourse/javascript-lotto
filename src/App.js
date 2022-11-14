d067372449ff48e613b536bd96610760a6792125
turtle601 <turtle601@naver.com>
2022-11-14 17:20:42 +0900

    refactor(App) print 객체 적용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 const Lotto = require('./Lotto');
-
 const Bonus = require('./domain/Bonus');
 const LottoStore = require('./domain/LottoStore');
-const LottoAdjustment = require('./domain/LottoAdjustment');
-const LottoDrawFactory = require('./domain/LottoDrawFactory');
-
-const LottoPaymentComponent = require('./ui/component/LottoPaymentComponent');
-const LottoWinCountComponet = require('./ui/component/LottoWinCountComponent');
-const LottoIncomComponent = require('./ui/component/LottoIncomComponent');
 
 const { LOTTO_QUESTION } = require('../utils/constants');
-const { validateLottoRange } = require('../utils/method');
+const { validateLottoRange, print } = require('../utils/method');
 
 class App {
   #lottoStore;
@@ -31,7 +24,7 @@ class App {
     Console.readLine(LOTTO_QUESTION.money, input => {
       this.#lottoStore = new LottoStore(input);
 
-      new LottoPaymentComponent({
+      print.lottoPaymentUI({
         count: this.#lottoStore.getCount(),
         lottos: this.#lottoStore.getLottos(),
       });
@@ -52,25 +45,15 @@ class App {
     });
   }
 
-  #printResult() {
-    const lottoPayment = new LottoAdjustment(
-      new LottoDrawFactory({
-        lotto: this.#lotto,
-        bonus: this.#bonus,
-        lottoStore: this.#lottoStore,
-      }),
-    );
-
-    new LottoWinCountComponet({ winScore: lottoPayment.getLottoCountScore() });
-
-    new LottoIncomComponent({ income: lottoPayment.getIncome() });
-  }
-
   #drawBonus() {
     Console.readLine(LOTTO_QUESTION.bonus, input => {
       this.#bonus = new Bonus(input);
 
-      this.#printResult();
+      print.lottoAdjustmentUI({
+        lotto: this.#lotto,
+        bonus: this.#bonus,
+        lottoStore: this.#lottoStore,
+      });
 
       Console.close();
     });
