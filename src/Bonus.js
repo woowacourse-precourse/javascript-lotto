039c9f140616e8679c29a88925e31a4595d2958c
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-14 22:42:21 +0900

    feat: [보너스 번호 입력 받기] - 입력값이 1~45 범위의 숫자가 아닌 경우 예외 처리

--- src/Bonus.js
+++ src/Bonus.js
@@ -2,7 +2,26 @@ class Bonus {
   #bonus;
 
   constructor(bonus) {
+    this.validate(bonus);
     this.#bonus = bonus;
+  }
+
+  validate(bonus) {
+    this.checkNumberAndInrange(bonus);
+  }
+
+  checkNumberAndInrange(bonus) {
+    const START_LOTTO_NUMBER = 1;
+    const END_LOTTO_NUMBER = 45;
+
+    if (
+      !(parseInt(bonus) >= START_LOTTO_NUMBER) ||
+      !(parseInt(bonus) <= END_LOTTO_NUMBER)
+    ) {
+      throw new Error(
+        `[ERROR] 보너스 번호는 ${START_LOTTO_NUMBER}부터 ${END_LOTTO_NUMBER}까지의 숫자여야 합니다.`
+      );
+    }
   }
 }
 
