ec2a3761e74b2f5db05b27a492c6369cf90e7c79
Chanwoong1 <jcw7468@gmail.com>
2022-11-14 16:28:54 +0900

    test: Lotto 클래스 수정에 따른 테스트 케이스 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -8,44 +8,44 @@ afterAll(() => {
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생합니다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+      new Lotto([1, 2, 3, 4, 5, 6, 7]).validate();
     }).toThrow("[ERROR]");
   });
 
   test("로또 번호의 개수가 6개 미만일 경우 예외가 발생합니다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5]);
+      new Lotto([1, 2, 3, 4, 5]).validate();
     }).toThrow("[ERROR]");
     expect(() => {
-      new Lotto([1]);
+      new Lotto([1]).validate();
     }).toThrow("[ERROR]");
     expect(() => {
-      new Lotto([]);
+      new Lotto([]).validate();
     }).toThrow("[ERROR]");
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생합니다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
+      new Lotto([1, 2, 3, 4, 5, 5]).validate();
     }).toThrow("[ERROR]");
   });
 
   test("로또 번호에 숫자가 아닌 문자가 있으면 예외가 발생합니다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 'a']);
+      new Lotto([1, 2, 3, 4, 5, 'a']).validate();
     }).toThrow("[ERROR]");
   });
 
   test("로또 번호에 적절하지 않은 숫자가 있으면 예외가 발생합니다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 46]);
+      new Lotto([1, 2, 3, 4, 5, 46]).validate();
     }).toThrow("[ERROR]");
     expect(() => {
-      new Lotto([1, 2, 0, 4, 5, 6]);
+      new Lotto([1, 2, 0, 4, 5, 6]).validate();
     }).toThrow("[ERROR]");
     expect(() => {
-      new Lotto([-15, 2, 3, 4, 5, 46]);
+      new Lotto([-15, 2, 3, 4, 5, 46]).validate();
     }).toThrow("[ERROR]");
   });
 });
