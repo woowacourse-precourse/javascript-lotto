69c267eda6273357b2195ceb737879a4f5631c8b
Junseo <oponize@naver.com>
2022-11-14 11:13:27 +0900

    chore: 상수 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,6 +24,13 @@ const LOTTO_RANK = Object.freeze({
     RANK_FOUR: 4,
     RANK_FIVE: 3,
   },
+  MESSAGE: {
+    RANK_ONE: "6개 일치 (2,000,000,000원) - ",
+    RANK_TWO: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+    RANK_TREE: "5개 일치 (1,500,000원) - ",
+    RANK_FOUR: "4개 일치 (50,000원) - ",
+    RANK_FIVE: "3개 일치 (5,000원) - ",
+  },
 });
 
 module.exports = {
