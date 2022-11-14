e08734f1bbc0a19a8bd9cf7a7b40b6776e1c6cb8
jiho1996 <jhp9270@naver.com>
2022-11-14 17:43:51 +0900

    feat : 1~5등 결과 출력 구현

---
+++ src/view.js
@@ -0,0 +1,14 @@
+const { print } = require("./Missionutils");
+class view {
+  #winngingResult;
+
+  printRankingResult(winngingResult) {
+    this.#winngingResult = winngingResult;
+    print(`3개 일치 (5,000원) - ${this.#winngingResult[5]}개`);
+    print(`4개 일치 (50,000원) - ${this.#winngingResult[4]}개`);
+    print(`5개 일치 (1,500,000원) - ${this.#winngingResult[3]}개`);
+    print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.#winngingResult[2]}개`);
+    print(`6개 일치 (2,000,000,000원) - ${this.#winngingResult[1]}개`);
+  }
+}
+module.exports = view;
