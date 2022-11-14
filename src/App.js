c71c17a4f549135c8892f03c21b8de3bdef82649
Serin-Kim <serin9864@gmail.com>
2022-11-15 01:21:08 +0900

    feat: getMoney 함수 구현
    로또 구입 금액을 입력받는 코드 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const { Random, Console } = require("@woowacourse/mission-utils");
+import { MESSAGE } from "./Constant";
 class App {
-  play() {}
+  #money;
+
+  constructor() {}
+
+  getMoney = () => {
+    Console.readLine(MESSAGE.GETMONEY, (money) => {
+      // TODO: 입력된 값 유효성 체크
+      this.#money = money;
+      this.getNumberOfLottery();
+    });
+  };
+
+  play() {
+    this.getMoney();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/Constant.js
@@ -0,0 +1,5 @@
+const MESSAGE = {
+  GETMONEY: "구입금액을 입력해 주세요.",
+};
+
+module.exports = { MESSAGE };
