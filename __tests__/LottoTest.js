11341ba15a75d53380428abe6742bb252d3207e7
moseung <tmdah900@naver.com>
2022-11-14 13:37:15 +0900

    ✅ test : 기능3 관련 테스트 -로또 넘버 Validation 테스트 -보너스 넘버 Validation 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -5,14 +5,14 @@ const { BUY_LOTTO, ERROR } = require("../src/constant/constant");
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+      new Lotto("1,2,3,4,5,6,7");
+    }).toThrow(ERROR.IS_LOTTO_INPUT);
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
+      new Lotto("1,2,3,4,5,5");
     }).toThrow("[ERROR]");
   });
 
@@ -25,6 +25,19 @@ describe("로또 클래스 테스트", () => {
   test("숫자가 아닌 값을 받으면 예외가 발생한다", () => {
     expect(() => {
       new BuyLotto("dadd");
+    }).toThrow(ERROR.NAN);
+  });
+
+  test("보너스넘버 1~45를 제외한 숫자를 받으면 예외가 발생한다.", () => {
+    expect(() => {
+      const lotto = new Lotto("1,2,3,4,5,6");
+      lotto.validateBonusNumber(47);
+    }).toThrow(ERROR.IS_LOTTO_NUMBER);
+  });
+
+  test("로또 넘버에서 구분자외에 숫자가 아닌값을 입력받으면 예외가 발생한다.", () => {
+    expect(() => {
+      const lotto = new Lotto("1,2,3,4,5,f");
     }).toThrow(ERROR.NAN);
   });
 
