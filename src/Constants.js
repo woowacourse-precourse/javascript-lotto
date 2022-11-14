49dd92966fe15a52e474a42fa9cdc59b6b5babc1
cocoa <jwv0323@kau.kr>
2022-11-14 20:30:05 +0900

    refactor: Lotto에 포함된 에러문구 상수로 수정

--- src/Constants.js
+++ src/Constants.js
@@ -25,10 +25,22 @@ const NUMBER = {
   LOTTO : 6,
 }
 
+const ERROR = {
+  LOTTO_COUNT: '[ERROR] 로또 번호는 6개여야 합니다.',
+  LOTTO_OVERLAP: '[ERROR] 로또 번호는 중복된 숫자가 없어야 합니다.',
+  LOTTO_NUMBER: '[ERROR] 로또 번호는 숫자로만 이루어져야 합니다.',
+  LOTTO_RANGE: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+
+  BONUS_NUMBER: '[ERROR] 보너스 번호는 숫자로만 이루어져야 합니다.',
+  BONUS_RANGE: '[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.',
+  BONUS_OVERLAP: '[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.',
+}
+
 module.exports = {
   INPUT,
   OUTPUT,
   LOTTO,
   NUMBER,
+  ERROR,
 };
   
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,4 @@
-const { NUMBER } = require('./Constants');
+const { NUMBER, ERROR } = require('./Constants');
 
 class Lotto {
   #numbers;
@@ -10,20 +10,20 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== NUMBER.LOTTO) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR.LOTTO_NUMBER);
     }
 
     if (new Set(numbers).size !== NUMBER.LOTTO){
-      throw new Error("[ERROR] 로또 번호는 중복된 숫자가 없어야 합니다.")
+      throw new Error(ERROR.LOTTO_OVERLAP);
     }
     
     numbers.map((number) => {
       if (/[^0-9]/g.test(number)){
-        throw new Error("[ERROR] 로또 번호는 숫자로만 이루어져야 합니다.")
+        throw new Error(ERROR.LOTTO_NUMBER);
       }
 
       if (number < 1 || number > 45) {
-        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+        throw new Error(ERROR.LOTTO_RANGE);
       }
     });
   }
@@ -35,15 +35,15 @@ class Lotto {
 
   validateBonusNumber(number) {
     if (/[^0-9]/g.test(number)){
-      throw new Error("[ERROR] 보너스 번호는 숫자로만 이루어져야 합니다.")
+      throw new Error(ERROR.BONUS_NUMBER);
     }
 
     if (number < 1 || number > 45) {
-      throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.")
+      throw new Error(ERROR.BONUS_RANGE);
     }
 
     if (this.#numbers.includes(number)){
-      throw new Error("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.")
+      throw new Error(ERROR.BONUS_OVERLAP)
     }
   }
   
