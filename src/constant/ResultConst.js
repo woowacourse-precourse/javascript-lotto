84ec82ba9ecb7c91c5fe4fc2ad97d2e5119a9efe
Rki0 <pky00823@gmail.com>
2022-11-14 10:58:10 +0900

    refactor(Result): modularize constants to another file
    Move constants managed in one file to a separate file.

---
+++ src/constant/ResultConst.js
@@ -0,0 +1,31 @@
+const FIFTH_PRIZE = 5000;
+const FOURTH_PRIZE = 50000;
+const THIRD_PRIZE = 1500000;
+const SECOND_PRIZE = 30000000;
+const FIRST_PRIZE = 2000000000;
+
+const CONDITION_OF_FIFTH_RANK = "3개 일치 (5,000원) - ";
+const CONDITION_OF_FOURTH_RANK = "4개 일치 (50,000원) - ";
+const CONDITION_OF_THIRD_RANK = "5개 일치 (1,500,000원) - ";
+const CONDITION_OF_SECOND_RANK = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+const CONDITION_OF_FIRST_RANK = "6개 일치 (2,000,000,000원) - ";
+
+const ResultConst = {
+  NOTIFY_WINNING_STATE: "\n당첨 통계\n---",
+  EACH_PRIZE: [
+    FIFTH_PRIZE,
+    FOURTH_PRIZE,
+    THIRD_PRIZE,
+    SECOND_PRIZE,
+    FIRST_PRIZE,
+  ],
+  CONDITION_OF_EACH_RANK: [
+    CONDITION_OF_FIFTH_RANK,
+    CONDITION_OF_FOURTH_RANK,
+    CONDITION_OF_THIRD_RANK,
+    CONDITION_OF_SECOND_RANK,
+    CONDITION_OF_FIRST_RANK,
+  ],
+};
+
+module.exports = ResultConst;
