e40aff26790773d328677496133340a33b0f20b0
ho-ji <wkdd0205@gmail.com>
2022-11-14 21:24:59 +0900

    feat: Add printNumberOfLottos()
    구입한 로또의 수량을 출력함

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ class App {
   #numberOfLottos;
   play() {
     this.inputMoneyToBuyLottos();
+    this.printNumberOfLottos();
   }
   inputMoneyToBuyLottos() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (moneyToBuyLottos) => {
@@ -11,6 +12,9 @@ class App {
         throw new Error("[ERROR] 구입 금액은 1000원 단위의 금액을 입력해주세요");
       this.#numberOfLottos = Number(moneyToBuyLottos)/1000;
     });
+  }
+  printNumberOfLottos() {
+    MissionUtils.Console.print(`${this.#numberOfLottos}개를 구매했습니다.`);
   }
 }
 
