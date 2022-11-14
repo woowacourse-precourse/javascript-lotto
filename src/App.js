b231a6cf55c2313fa76fccafbad55e909bf131f6
seolyeongbae <peixueying@gmail.com>
2022-11-15 01:13:06 +0900

    feat: input user's price, except check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  lottoPrice;
+
+  play() {
+    this.buyLotto();
+  }
+
+  buyLotto() {
+    MissionUtils.Console.readLine("구입 금액을 입력해주세요 :", (price) => {
+      const isValidInput = this.checkValidPrice(price);
+      if (isValidInput == true) {
+        this.lottoPrice = price;
+        MissionUtils.Console.print(`${price / 1000}개를 구매했습니다.`);
+      } else {
+        MissionUtils.Console.close();
+      }
+    });
+
+    return;
+  }
+
+  checkValidPrice(price) {
+    if (price % 1000 !== 0) {
+      throw "[ERROR] invalid price";
+    }
+    return true;
+  }
 }
 
 module.exports = App;
