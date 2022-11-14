7e2adc7220b36d51bf8b1eceb44db069ead70e44
unsnruu <unsnruu@gmail.com>
2022-11-15 00:04:11 +0900

    test : 입력한 당첨 번호에 대한 Validation 메서드 테스트 생성

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -13,4 +13,22 @@ describe("App 내부의 Validation 메서드에 대한 테스트", () => {
       app.validateMoney("1200");
     }).toThrow("[ERROR] 1000원 단위로 금액을 입력해주세요.");
   });
+  test("입력한 당첨 번호가 6자리가 아닐 경우 오류가 발생한다.", () => {
+    const app = new App();
+    expect(() => {
+      app.validateWinningNumbers("1, 2, 3, 4, 5, 6, 7");
+    }).toThrow("[ERROR] 당첨 로또 번호의 길이는 6개입니다.");
+  });
+  test("입력한 당첨 번호 중 1~45의 범위 외의 숫자가 포함되어 있으면 오류가 발생한다.", () => {
+    const app = new App();
+    expect(() => {
+      app.validateWinningNumbers("1,2,3,4,5,50");
+    }).toThrow("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+  });
+  test("입력한 당첨 번호 중 1~45의 범위 외의 숫자가 포함되어 있으면 오류가 발생한다.", () => {
+    const app = new App();
+    expect(() => {
+      app.validateWinningNumbers("1,1,2,3,4,5");
+    }).toThrow("[ERROR] 동일한 숫자가 포함되어 있습니다.");
+  });
 });
