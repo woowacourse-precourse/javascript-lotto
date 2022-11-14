f5c9f474431dec99a954307b791e11f423619fd7
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-14 23:12:13 +0900

    test: 수익률 값 출력을 확인하는 테스트와 게임 시작 후 사용자의 입력값에 대한 결과 확인하는 큰 단위의 테스트 추가

--- __tests__/RateOfReturnCalculatorTest.js
+++ __tests__/RateOfReturnCalculatorTest.js
@@ -4,12 +4,39 @@ describe('RateOfReturnCalculator 클래스 테스트', () => {
   test('기능 테스트: 올바른 수익률 값을 출력한다.', () => {
     let winningLottosQuantity = [null, 0, 1, 0, 0, 0];
     let lottosQuantity = 8;
+    let rateOfReturn = '375,000.0';
 
     expect(
       new RateOfReturnCalculator(
         winningLottosQuantity,
         lottosQuantity
       ).getRateOfReturn()
-    ).toEqual('375,000.0');
+    ).toEqual(rateOfReturn);
+  });
+
+  test('기능 테스트: 올바른 수익률 값을 출력한다.', () => {
+    let winningLottosQuantity = [null, 0, 0, 0, 0, 2];
+    let lottosQuantity = 15;
+    let rateOfReturn = '66.7';
+
+    expect(
+      new RateOfReturnCalculator(
+        winningLottosQuantity,
+        lottosQuantity
+      ).getRateOfReturn()
+    ).toEqual(rateOfReturn);
+  });
+
+  test('기능 테스트: 올바른 수익률 값을 출력한다.', () => {
+    let winningLottosQuantity = [null, 0, 1, 0, 0, 0];
+    let lottosQuantity = 3;
+    let rateOfReturn = '1,000,000.0';
+
+    expect(
+      new RateOfReturnCalculator(
+        winningLottosQuantity,
+        lottosQuantity
+      ).getRateOfReturn()
+    ).toEqual(rateOfReturn);
   });
 });
