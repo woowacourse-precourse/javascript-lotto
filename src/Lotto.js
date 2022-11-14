6b9bc374702d121d1d78f49b7b5cd8f79d1cd84a
JiSeungha <jish0210@naver.com>
2022-11-14 15:03:44 +0900

    Some Lotto Class test implemented

--- src/Lotto.js
+++ src/Lotto.js
@@ -14,6 +14,13 @@ class Lotto {
     if(numberSet.size < 6){
       throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
+    numbers.forEach(function(number){
+      if (isNaN(number) && isNaN(parseFloat(number))) 
+        throw new Error("[ERROR] 로또 번호는 숫자이어야 합니다.");
+      if(number < 1 || number > 45 || parseInt(number) !== number) 
+        throw new Error("[ERROR] 로또 번호는 1에서 45 범위의 정수이어야 합니다.");
+    })
+    
   }
 
   getNumbers(){
