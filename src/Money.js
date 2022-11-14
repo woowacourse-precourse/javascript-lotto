89a97750a5b45cc9c6b4d2b09e4c0ca1d01b09de
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-14 20:28:03 +0900

    feat: [로또 구입 금액 입력 받기] - 1,000원 단위가 아닌 경우 예외 처리

--- src/Money.js
+++ src/Money.js
@@ -8,11 +8,19 @@ class Money {
 
   validate(money) {
     this.checkNumber(money);
+    this.checktUnit(money);
   }
 
   checkNumber(money) {
     if (isNaN(money)) {
       throw new Error("[ERROR] 구입 금액은 숫자로 입력해야 합니다.");
+    }
+  }
+
+  checktUnit(money) {
+    const UNIT = 1000;
+    if (money % UNIT !== 0) {
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
     }
   }
 }
