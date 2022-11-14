305388e32f0c126a121e6285b246664247c76df7
jeonbyeongmin <qudals7613@gmail.com>
2022-11-15 02:52:51 +0900

    test: User 클래스 예외처리 테스트 케이스 추가

---
+++ __tests__/UserTest.js
@@ -0,0 +1,17 @@
+const User = require('../src/User');
+
+describe('유저 클래스 테스트', () => {
+  const user = new User();
+
+  test('로또 구매 금액이 정수가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      user.validateAmount('abc');
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 구매 금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다.', () => {
+    expect(() => {
+      user.validateAmount('8200');
+    }).toThrow('[ERROR]');
+  });
+});
