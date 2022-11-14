38fa335d61129be67718be24f90b5c9aeebce36c
안호진 <asdv94@naver.com>
2022-11-14 22:52:20 +0900

    feat(lotto): 구입금액 입력받는 기능 구현

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,23 @@
-const { ERROR_MSG } = require('./Constant');
+const { ERROR_MSG, GAME_MSG, NEW_LINE } = require('./Constant');
+const IO = require('./IO');
 
 class Game {
   constructor() {
     this.lottos = null;
     this.cost = 0;
   }
+
   static validate(money) {
     if (money % 1000 > 0)
       throw new Error(ERROR_MSG.prefix + ERROR_MSG.only1000WonUnits);
+  }
+
+  static enterMoney() {
+    IO.readLine(GAME_MSG.pleaseEnterMoney + NEW_LINE, (input) => {
+      const money = +input;
+      this.cost = money;
+      Game.validate(money);
+    });
   }
 }
 
