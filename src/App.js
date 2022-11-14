95e990b54753db81ee878a09d7687af838c1a84b
Raehyeon Jeong <raehy19@gmail.com>
2022-11-15 00:17:09 +0900

    feat(src/App.js) : App play 메소드 입력받은 금액 숫자인지 확인 예외처리

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
   play() {
-    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (money) => {});
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+      this.#inputMoneyExceptionCheck(money);
+    });
+  }
+
+  #inputMoneyExceptionCheck(money) {
+    if (this.#stringHasNaN(money))
+      throw new Error('[ERROR] 금액은 숫자로 입력해 주세요');
+  }
+
+  #stringHasNaN(money) {
+    for (let i = 0; i < money.length; ++i) {
+      if (
+        money.charCodeAt(i) < '0'.charCodeAt(0) ||
+        money.charCodeAt(i) > '9'.charCodeAt(0)
+      )
+        return true;
+    }
+    return false;
   }
 }
 
