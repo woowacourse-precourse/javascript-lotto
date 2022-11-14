6327e4e6fd937600fddd7de6427b956687ce0394
minh0518 <rubics@naver.com>
2022-11-14 19:37:06 +0900

    feat: 구입한 로또번호 예외처리 (중복된 값이 존재하는 경우)

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,6 +1,8 @@
 class Lotto {
   #numbers
 
+  //여기에 내가 구입한 로또번호들을 구현하면 되는 듯?
+
   constructor(numbers) {
     this.validate(numbers)
     this.#numbers = numbers
@@ -11,6 +13,11 @@ class Lotto {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.')
     }
 
+    //중복 에러
+    const set = new Set(numbers)
+    if (numbers.length !== set.size) {
+      throw new Error('[ERROR] 로또 번호는 중복된 값이 없어야 합니다.')
+    }
   }
 
   print() {
