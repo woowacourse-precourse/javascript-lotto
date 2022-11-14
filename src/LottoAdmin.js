32b62b1d444181c756be3292a7bb0b8d4f54d8e5
동동이 <drdragon777@naver.com>
2022-11-15 08:05:21 +0900

    chore - 접근제어자 표시

--- src/LottoAdmin.js
+++ src/LottoAdmin.js
@@ -4,10 +4,10 @@ const { LOTTO_RESULT } = require("./constants");
 
 class LottoAdmin {
   static generateLottoAnswer(lottoNum) {
-    return Array.from({ length: lottoNum }, () => this.generateLotto());
+    return Array.from({ length: lottoNum }, () => this.#generateLotto());
   }
 
-  static generateLotto() {
+  static #generateLotto() {
     return Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) =>
       a > b ? 1 : -1
     );
@@ -17,21 +17,21 @@ class LottoAdmin {
     return lotto.filter((num) => winNumbers.includes(num)).length;
   }
 
-  static getStatisticsStrArr(statistics) {
+  static #getStatisticsStrArr(statistics) {
     const ment = Object.values(LOTTO_RESULT);
     return statistics.map((value, idx) => ment[idx] + value + "개").join("\n");
   }
 
   static printWinStatistics(statistics) {
-    Console.print(this.getStatisticsStrArr(statistics));
+    Console.print(this.#getStatisticsStrArr(statistics));
   }
 
-  static getStatisticsPriceSum(price, ranks) {
+  static #getStatisticsPriceSum(price, ranks) {
     return ranks.reduce((acc, value, idx) => acc + value * price[idx], 0);
   }
 
   static getMargin(price, [lottos, ranks]) {
-    const margin = this.getStatisticsPriceSum(price, ranks);
+    const margin = this.#getStatisticsPriceSum(price, ranks);
     const totalLottoPrice = lottos.length * 1000;
     const middle = (margin / totalLottoPrice) * 100;
     return Math.round(middle * 100) / 100;
