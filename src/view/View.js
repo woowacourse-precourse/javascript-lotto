e8a69556550acb5c722397ba9b8ab19dcfb8ce9a
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-14 23:48:39 +0900

    feat: 구매 개수 출력 함수

--- src/view/View.js
+++ src/view/View.js
@@ -1,7 +1,13 @@
 const { Console } = require("@woowacourse/mission-utils");
 
 class View {
+  constructor() {
+    this.printLottoCount = this.printLottoCount.bind(this);
+  }
 
+  printLottoCount(lottoCount) {
+    Console.print(`\n${lottoCount}개를 구매했습니다.`);
+  }
 }
 
 module.exports = View;
