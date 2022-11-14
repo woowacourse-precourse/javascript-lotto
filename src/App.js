2e9871a6d0ae03219e080586e8e2061bea73f0c1
hwnim5324 <hwnim5324@naver.com>
2022-11-14 23:54:47 +0900

    feat: App 클래스 내 전체 기능 구성.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const User = require('./User');
+const Lotto = require('./Lotto');
+const Raffle = require('./Raffle');
 class App {
-  play() {}
+  play() {
+    const user = new User();
+    const raffle = new Raffle(user.setMoney());
+    const lotto = new Lotto(user.setNumbers(), user.setBonus());
+
+    raffle.compareLottoes(lotto.getNumbers());
+    raffle.printResult();
+  }
 }
 
 module.exports = App;
