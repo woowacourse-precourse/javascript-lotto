1a0054cfc80deb5cfba85b9be64c48424c7f339a
Suyoung <jsy8492@gmail.com>
2022-11-14 14:37:01 +0900

    refactor(lottoManagerTest): 로또 발행 기능을 Mock Function을 이용하여 테스트하도록 수정

--- __tests__/LottoManagerTest.js
+++ __tests__/LottoManagerTest.js
@@ -1,29 +1,44 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 const LottoManager = require('../src/LottoManager');
 
+const { ERROR } = require('../src/lib/constants/error');
 const { LOTTO } = require('../src/lib/constants/lotto');
 
-const mockCreateLottoNumbers = () => [1, 2, 3, 4, 5, 6];
+const mockRandoms = numbers => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
 
 describe('로또 관리자 클래스 테스트', () => {
   test(`로또 구입 금액이 ${LOTTO.UNIT_PRICE}으로 나누어 떨어지지 않으면 예외가 발생한다`, () => {
     expect(() => {
-      const lottoManager = new LottoManager();
-      lottoManager.initLottos('1001');
-    }).toThrow('[ERROR]');
+      new LottoManager().initLottos('1001');
+    }).toThrow(ERROR.PURCHASE_AMOUNT.CANNOT_BE_DIVIDED);
   });
 
   test('로또 구입 금액에 숫자가 아닌 문자가 존재하면 예외가 발생한다.', () => {
     expect(() => {
-      const lottoManager = new LottoManager();
-      lottoManager.initLottos('1000j');
-    }).toThrow('[ERROR]');
+      new LottoManager().initLottos('1000j');
+    }).toThrow(ERROR.PURCHASE_AMOUNT.NOT_NUMBER);
   });
 
-  test('유효한 로또 구입 금액인 경우 구입 금액에 해당하는 만큼의 로또를 발행한다.', () => {
+  test('올바른 구입 금액이 입력되면 구입 금액만큼의 로또를 발행한다.', () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
     const lottoManager = new LottoManager();
-    lottoManager.createLottoNumbers = jest.fn(mockCreateLottoNumbers);
     lottoManager.initLottos('8000');
-
-    expect(lottoManager.lottos).toHaveLength(8000 / 1000);
+    const result = lottoManager.lottos.length;
+    expect(result).toEqual(8000 / LOTTO.UNIT_PRICE);
   });
 });
