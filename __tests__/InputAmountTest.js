ac2f531cd390b2fdab44ff619946083276bc061e
2pandi <coder.2pandi@gmail.com>
2022-11-14 23:31:43 +0900

    test: add tests if input is numeric

--- __tests__/InputAmountTest.js
+++ __tests__/InputAmountTest.js
@@ -1,6 +1,12 @@
 const InputAmount = require("../src/InputAmount");
 
 describe("구입금액 클래스 테스트", () => {
+  test("구매금액이 숫자가 아닌 문자가 들어가면 예외가 발생한다.", () => {
+    expect(() => {
+      new InputAmount("a");
+    }).toThrow("[ERROR]");
+  });
+
   test("구매금액이 천 원 단위가 아니면 예외가 발생한다.", () => {
     expect(() => {
       new InputAmount(1111);
--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,12 @@
 const Lotto = require("../src/Lotto");
 
 describe("로또 클래스 테스트", () => {
+  test("로또 번호에 숫자가 아닌 문자가 들어가면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([NaN, 1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+  });
+
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
@@ -10,12 +16,6 @@ describe("로또 클래스 테스트", () => {
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
-  });
-
-  test("로또 번호에 숫자가 아닌 문자가 들어가면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([NaN, 1, 2, 3, 4, 5]);
     }).toThrow("[ERROR]");
   });
 
