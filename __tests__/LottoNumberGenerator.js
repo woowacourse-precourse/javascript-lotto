8e3720a6d89fcc3844dc16ef6746f599bff5d146
연 <mogoo22@naver.com>
2022-11-15 05:29:36 +0900

    test: 로또 번호, 당첨 번호 및 보너스 번호의 예외 처리 메세지를 테스트한다.

--- __tests__/LottoNumberGenerator.js
+++ __tests__/LottoNumberGenerator.js
@@ -1,5 +1,7 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const LottoNumberGenerator = require('../src/LottoNumberGenerator');
+const { ERROR_MESSAGE } = require('../src/constants');
+const { makeErrorMsg } = require('../src/utils');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -26,4 +28,74 @@ describe.only('로또 번호 추첨기 클래스 테스트', () => {
     ]);
   });
 
+  test('당첨 번호가 숫자가 아니면 예외가 발생한다.', () => {
+    const errorMsg = makeErrorMsg(ERROR_MESSAGE.NUMBER);
+    const answers = ['1,2,3,4,5,k', '7'];
+
+    mockQuestions(answers);
+
+    expect(() => {
+      const lottoNumberGenerator = new LottoNumberGenerator();
+      lottoNumberGenerator.drawLottery();
+    }).toThrow(errorMsg);
+  });
+
+  test('당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
+    const errorMsg = makeErrorMsg(ERROR_MESSAGE.WINNER_NUMBER_LENTH);
+    const answers = ['1,2,3,4,5,6,7', '7'];
+
+    mockQuestions(answers);
+
+    expect(() => {
+      const lottoNumberGenerator = new LottoNumberGenerator();
+      lottoNumberGenerator.drawLottery();
+    }).toThrow(errorMsg);
+  });
+
+  test('보너스 번호의 개수가 1개가 넘어가면 예외가 발생한다.', () => {
+    const errorMsg = makeErrorMsg(ERROR_MESSAGE.BONUS_NUMBER_LENTH);
+    const answers = ['1,2,3,4,5,6', '7,1'];
+
+    mockQuestions(answers);
+
+    expect(() => {
+      const lottoNumberGenerator = new LottoNumberGenerator();
+      lottoNumberGenerator.drawLottery();
+    }).toThrow(errorMsg);
+  });
+
+  test('당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
+    const errorMsg = makeErrorMsg(ERROR_MESSAGE.DUPLICATION);
+    const answers = ['1,2,3,4,6,6', '7'];
+
+    mockQuestions(answers);
+
+    expect(() => {
+      const lottoNumberGenerator = new LottoNumberGenerator();
+      lottoNumberGenerator.drawLottery();
+    }).toThrow(errorMsg);
+  });
+
+  test('당첨 번호의 숫자 범위가 1~45 사이가 아니면 예외가 발생한다.', () => {
+    const errorMsg = makeErrorMsg(ERROR_MESSAGE.RANGE);
+    const answers = ['0,2,3,4,5,6', '7'];
+
+    mockQuestions(answers);
+
+    expect(() => {
+      const lottoNumberGenerator = new LottoNumberGenerator();
+      lottoNumberGenerator.drawLottery();
+    }).toThrow(errorMsg);
+  });
+
+  test('보너스 번호의 숫자 범위가 1~45 사이가 아니면 예외가 발생한다.', () => {
+    const errorMsg = makeErrorMsg(ERROR_MESSAGE.RANGE);
+    const answers = ['1,2,3,4,5,6', '46'];
+
+    mockQuestions(answers);
+    expect(() => {
+      const lottoNumberGenerator = new LottoNumberGenerator();
+      lottoNumberGenerator.drawLottery();
+    }).toThrow(errorMsg);
+  });
 });
--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,22 +1,34 @@
 const Lotto = require('../src/Lotto');
+const { makeErrorMsg } = require('../src/utils');
+const { ERROR_MESSAGE } = require('../src/constants');
 
 describe.only('로또 클래스 테스트', () => {
   test('로또 번호가 숫자가 아니면 예외가 발생한다.', () => {
+    const errorMsg = makeErrorMsg(ERROR_MESSAGE.NUMBER);
     expect(() => {
       new Lotto(['1', '2', '3', '4', '5', '6']);
-    });
+    }).toThrow(errorMsg);
   });
 
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
+    const errorMsg = makeErrorMsg(ERROR_MESSAGE.LOTTO_LENGTH);
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow('[ERROR]');
+    }).toThrow(errorMsg);
   });
 
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
+    const errorMsg = makeErrorMsg(ERROR_MESSAGE.DUPLICATION);
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow('[ERROR]');
+    }).toThrow(errorMsg);
+  });
+
+  test('로또 번호의 숫자 범위가 1~45 사이가 아니면 예외가 발생한다', () => {
+    const errorMsg = makeErrorMsg(ERROR_MESSAGE.RANGE);
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow(errorMsg);
   });
 
   test('로또의 qrCode를 통해 로또 번호를 읽을 수 있다.', () => {
