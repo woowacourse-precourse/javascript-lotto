a792145bbe29706c2c10e5834d6e61ab9bce154c
jihong <rlawyd9@kookmin.ac.kr>
2022-11-15 03:54:20 +0900

    feat: 당첨 번호 입력 받기

--- src/App.js
+++ src/App.js
@@ -20,12 +20,19 @@ class App {
     } 
   }
 
+  inputWinningNum(){
+    MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.', async(winning_num) => {
+      console.log(winning_num);
+    });
+  }
+
   inputMoney(){
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', async(money) => {
       if(await this.checkInput(money)){
         const lotto_cnt = Math.floor(money/1000);
         this.print_lotto_cnt(lotto_cnt);
         this.draw_lotto(lotto_cnt);
+        this.inputWinningNum();
       }
       await MissionUtils.Console.close();
 
@@ -36,5 +43,8 @@ class App {
     this.inputMoney();
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
