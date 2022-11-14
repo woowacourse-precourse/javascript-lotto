17edfddf1cc75fed553cbe0c4adbeb95ff36f8ad
RooT_Kim <bdd3452@gmail.com>
2022-11-15 03:53:26 +0900

    추가: 구매 금액 클래스 생성 Pay 클래스 선언 구매 금액으로 전달되는 값이 유효한지 확인하는 validate 메소드 선언

---
+++ src/Objects/Pay.js
@@ -0,0 +1,21 @@
+const { Messages } = require('../Constants');
+
+class Pay {
+	constructor(pay) {
+		this.validate(pay);
+	}
+
+	validate(pay) {
+		if (typeof Number(pay) !== 'number') {
+			throw new Error(Messages.ERROR.BUY_PRICE.NOT_NUMBER);
+		}
+		if (pay < 1000) {
+			throw new Error(Messages.ERROR.BUY_PRICE.LESS_THAN_THOUSAND);
+		}
+		if (pay % 1000 !== 0) {
+			throw new Error(Messages.ERROR.BUY_PRICE.NOT_ZERO_REMAIN);
+		}
+	}
+}
+
+module.exports = Pay;
