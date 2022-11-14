6d96f5670fd90816053b312c40083fc194112199
DongWooKim97 <nanaukim@naver.com>
2022-11-14 22:08:46 +0900

    style: 상수에서 관리하는 구입금액을 묻는 출력문으로 변수명 변경

--- src/play/Clerk.js
+++ src/play/Clerk.js
@@ -1,8 +1,10 @@
 const Mission = require("@woowacourse/mission-utils");
+const { GAME_MESSAGES } = require("../utils/Constants");
+const QuickPick = require("../input/QuickPick");
 
 class Clerk {
   inputLottoAmount() {
-    Mission.Console.readLine("구입금액을 입력해 주세요.", (payment) => {
+    Mission.Console.readLine(GAME_MESSAGES.ASK_TO_PAY, (payment) => {
       Mission.Console.close();
     });
   }
