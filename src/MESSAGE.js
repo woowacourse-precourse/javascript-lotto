eeb1df4c741660b7cde61dd3a0dbba20921f1819
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-15 03:14:30 +0900

    fix(message): separate input & error message

--- src/MESSAGE.js
+++ src/MESSAGE.js
@@ -1,5 +1,14 @@
 const MESSAGE = {
-  ENTERPRICE: '구입금액을 입력해 주세요.\n',
+  INPUT: {
+    MONEY: '구입금액을 입력해 주세요.\n',
+  },
+  ERROR: {
+    MONEY: {
+      NUMBER: '[ERROR] 구입금액은 숫자여야 합니다.',
+      THOUSAND: '[ERROR] 구입금액은 1000단위여야 합니다.',
+      ZERO: '[ERROR] 구입금액은 0이상 이여야 합니다.',
+    },
+  },
 };
 
 module.exports = MESSAGE;
