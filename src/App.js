8995dfab0c77f3a1181ab9190fd5cb349b194cb8
kimeodml <kimeodml@gmail.com>
2022-11-14 22:28:11 +0900

    feat: 입력한 값이 올바른 값인지 확인하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { MESSAGES } = require("./constants/Constants.js");
-
+const { MESSAGES, PAY_ERROR } = require("./constants/Constants.js");
+const ONLY_NUMBER = /^[1-9]+$/;
+const UNIT = 1000;
 
 class App {
   #count = 0;
@@ -11,7 +12,14 @@ class App {
 
   start() {
     Console.readLine(MESSAGES.PAY_COST, (input) => {
+      this.checkPayValidation(input);
     });
+  }
+
+  checkPayValidation(purchase) {
+    if (purchase < UNIT) throw new Error(PAY_ERROR.UNDER);
+    if (!ONLY_NUMBER.test(purchase)) throw new Error(PAY_ERROR.ONLY_NUMBER);
+    if (purchase % UNIT !== 0) throw new Error(PAY_ERROR.UNIT);
   }
 
 }
