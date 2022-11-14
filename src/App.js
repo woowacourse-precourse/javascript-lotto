ab053c00b9c23aa8f631db7540fdd0b5a0d62ee3
wusi-hub <jws9595@naver.com>
2022-11-15 05:52:52 +0900

    feat: 돈을 입력 받는 창 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const PURCHASE_MONEY = '구입금액을 입력해 주세요.';
+
 class App {
-  play() {}
+  play() {
+    userWindow(PURCHASE_MONEY, lottoCount);
+  }
 }
+
+const userWindow = (window, callback) => {
+  MissionUtils.Console.readLine(`${window}\n`, (input) => {
+    callback(input);
+  });
+};
 
 module.exports = App;
