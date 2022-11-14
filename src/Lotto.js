1a17c4ffcd554586d4821054389755eaef6f5c98
yeonkyoung <keep.ykk@gmail.com>
2022-11-14 18:46:39 +0900

    feat: validate로 사용자 입력값 검증 메서드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,7 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const messages = require("./constants/messages.js");
+const terms = require("./constants/terms");
+
 class Lotto {
   #numbers;
 
@@ -7,12 +11,43 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    try {
+      this.checkNumbersLength(numbers);
+      this.checkSameNumber(numbers);
+      this.checkNotNumber(numbers);
+      this.checkNumberRange(numbers);
+    } catch (e) {
+      MissionUtils.Console.print(e);
+      MissionUtils.Console.close();
     }
   }
 
-  // TODO: 추가 기능 구현
+  checkNumbersLength(numbers) {
+    if (numbers.length !== messages.NUMBERS_LENGTH) {
+      throw new Error(messages.TOTAL_NUMBER_ERROR);
+    }
+  }
+  checkSameNumber(numbers) {
+    if (set(numbers).length !== messages.NUMBERS_LENGTH) {
+      throw new Error(messages.SAME_NUMBER_ERROR);
+    }
+  }
+  checkNotNumber(numbers) {
+    if (numbers.every((number) => !isNaN(number))) {
+      throw new Error(messages.NOT_A_NUMBER_ERROR);
+    }
+  }
+  checkNumberRange(numbers) {
+    if (
+      !numbers.every(
+        (number) =>
+          terms.MIN_NUMBER_RANGE <= number && number <= terms.MAX_NUMBER_RANGE
+      )
+    ) {
+      throw new Error(messages.NUMBER_RANGE_ERROR);
+    }
+  }
 }
 
+a = new Lotto([1, 2, 3, 4]);
 module.exports = Lotto;
