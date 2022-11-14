6464529c9b6aabcc8ab907b7572bf40a388f77c2
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-14 23:39:04 +0900

    refactor : 로또 결과값 출력 기능을 하는 LottoResult 클래스 분리

---
+++ src/LottoResult.js
@@ -0,0 +1,29 @@
+const {
+  LOTTO_RESURL_STATISTICS_MESSAGE,
+} = require('./lib/Constants');
+const { print } = require('./lib/Utils');
+
+class LottoResult {
+  constructor(result, rate) {
+    this.result = result;
+    this.rate = rate;
+  }
+
+  printResult() {
+    print(LOTTO_RESURL_STATISTICS_MESSAGE);
+
+    Object.keys(this.result).forEach((key) => {
+      const value = this.result[key];
+      return print(`${value.text}${value.count}개`);
+    });
+
+    return this;
+  }
+
+  printRate() {
+    print(`총 수익률은 ${this.rate || 0}%입니다.`);
+    return this;
+  }
+}
+
+module.exports = LottoResult;
