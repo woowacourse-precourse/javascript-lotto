99c3b08d9300ea9298c9a5826f971ebfd79c9f6a
GC-Park <kuyt3122@naver.com>
2022-11-14 13:45:26 +0900

    feat:상수 값을 담는 파일 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,3 @@
-Lotto
-
 class Lotto {
   #numbers;
 
---
+++ src/constant.js
@@ -0,0 +1,12 @@
+export const INPUT_LOTTO_NUMBER_ERROR ="[ERROR] 로또 번호는 6개여야 합니다.";
+
+export const INPUT_MONEY = '구입금액을 입력해 주세요.';
+export const INPUT_WINNING_NUMBER = '당첨 번호를 입력해 주세요.';
+export const INPUT_BONUS_NUMBER = '보너스 번호를 입력해 주세요.';
+export const PURCHASE_LOTTO = '개를 구매했습니다.';
+export const WINNING_STATICS= "당첨 통계\n---";
+export const THREE_MATCHED = "3개 일치 (5,000원) - ";
+export const FOUR_MATCHED = "4개 일치 (50,000원) - ";
+export const FIVE_MATCHED = "5개 일치 (1,500,000원) - ";
+export const FIVE_WITH_BONUS_MATCHED = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+export const SIX_MATCHED = "6개 일치 (2,000,000,000원) - ";
