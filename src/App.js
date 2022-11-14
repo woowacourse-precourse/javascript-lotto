801f3f08f43c290b0a39415fc3de62232edb80a5
ho-ji <wkdd0205@gmail.com>
2022-11-14 21:23:40 +0900

    feat: Add inputMoneyToBuyLottos()
    로또 구입 금액을 입력 받아서, 로또를 몇 개 구매할 것인지 구함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  #numberOfLottos;
+  play() {
+    this.inputMoneyToBuyLottos();
+  }
+  inputMoneyToBuyLottos() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (moneyToBuyLottos) => {
+      this.#numberOfLottos = Number(moneyToBuyLottos)/1000;
+    });
+  }
 }
 
 module.exports = App;
