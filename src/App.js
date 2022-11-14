472c3d00a4cfbf82a936724a70e2a6977d9dfc31
deli-ght <minjju0660@gmail.com>
2022-11-15 01:14:50 +0900

    feat(App.js): get bonus number

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const ERROR_MESSAGES = require("../src/const/ErrorMessages");
-const Lotto = require("./Lotto");
-const { Console, Random } = MissionUtils;
+const { Console, Random } = require("@woowacourse/mission-utils");
 const {
   IS_NUMBER,
   IS_OVER_MIN_COST,
@@ -9,20 +6,31 @@ const {
   IS_WINNER_NUMBER,
   IS_ENOUGH,
   IS_RANGE,
-} = ERROR_MESSAGES;
-
+  IS_NOT_IN_WINNER_NUMBER,
+} = require("../src/const/ErrorMessages");
+const Lotto = require("./Lotto");
 class App {
   #amount;
   #userLottos;
   #winnerNumbers;
+  #bonusNumber;
 
   constructor() {
     this.#amount = 0;
     this.#userLottos = [];
     this.#winnerNumbers = null;
+    this.#bonusNumber = 0;
   }
 
-  setUserLottoAmount() {
+  getWinnerNumbers() {
+    return this.#winnerNumbers;
+  }
+
+  setWinnerNumbers(numbers) {
+    this.#winnerNumbers = numbers;
+  }
+
+  inputUserLottoAmount() {
     Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
       this.validateMoney(money);
 
@@ -30,11 +38,11 @@ class App {
       this.#amount = AMOUNT;
       // Console.print("");
       Console.print(`${AMOUNT}개를 구매했습니다.`);
-      this.setLottos(AMOUNT);
+      this.inializeLottos(AMOUNT);
     });
   }
 
-  setLottos(amount) {
+  inializeLottos(amount) {
     for (let i = 0; i < amount; i++) {
       const newLottoNums = Random.pickUniqueNumbersInRange(1, 45, 6);
       newLottoNums.sort((a, b) => a - b);
@@ -44,15 +52,24 @@ class App {
       newLotto.print();
     }
     // Console.print("");
-    this.setWinnerNumbers();
+    this.inputWinnerNumbers();
   }
 
-  setWinnerNumbers() {
+  inputWinnerNumbers() {
     Console.readLine("당첨 번호를 입력해 주세요.\n", (numbers) => {
       this.validateWinnerNumbers(numbers);
 
       const NUMBER_LIST = [...new Set(numbers.split(","))];
       this.#winnerNumbers = NUMBER_LIST;
+
+      this.inputBonusNumbers();
+    });
+  }
+
+  inputBonusNumbers() {
+    Console.readLine("보너스 번호를 입력해 주세요.\n", (number) => {
+      this.validateBonusNumbers(number);
+      this.#bonusNumber = number;
     });
   }
 
@@ -74,8 +91,16 @@ class App {
     });
   }
 
+  validateBonusNumbers(number) {
+    if (!/^\d+$/.test(number)) throw Error(IS_NUMBER);
+    if (!Number.isInteger(+number) || number < 1 || number >= 46)
+      throw Error(IS_RANGE);
+    if (this.#winnerNumbers.includes(number))
+      throw Error(IS_NOT_IN_WINNER_NUMBER);
+  }
+
   play() {
-    this.setUserLottoAmount();
+    this.inputUserLottoAmount();
   }
 }
 
