a7a26b0b5a367fedb979e8623b0541204f350c52
unknown <sjjoseph106@gmail.com>
2022-11-14 12:59:25 +0900

    test: Validator 클래스 예외 처리 테스트 코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
 const Lotto = require("../src/Lotto");
+const Validator = require("../src/Validator");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -22,6 +23,36 @@ describe("로또 클래스 테스트", () => {
   test("로또 번호에 1~45 범위에 해당하지 않는 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR]");
+  });
+});
+
+describe("벨리데이터 클래스 테스트", () => {
+  test("구입 금액이 숫자가 아니라면 예외가 발생한다.", () => {
+    expect(() => {
+      const validator = new Validator();
+      validator.checkMoneyValid("돈");
+    }).toThrow("[ERROR]");
+  });
+
+  test("구입 금액이 0원이면 예외가 발생한다.", () => {
+    expect(() => {
+      const validator = new Validator();
+      validator.checkMoneyValid(0);
+    }).toThrow("[ERROR]");
+  });
+
+  test("구입 금액이 1,000원 단위가 아니라면 예외가 발생한다.", () => {
+    expect(() => {
+      const validator = new Validator();
+      validator.checkMoneyValid(1001);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 입력한 당첨 번호에 포함된다면 예외가 발생한다.", () => {
+    expect(() => {
+      const validator = new Validator();
+      validator.checkBonusNumberValid(6, [1, 2, 3, 4, 5, 6]);
     }).toThrow("[ERROR]");
   });
 });
