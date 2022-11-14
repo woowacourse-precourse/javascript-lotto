a1e8b63eb89d8885f99c965c4da61f2c11eebf5d
myung-j-p <jmpack97@gmail.com>
2022-11-14 23:10:59 +0900

    Refactor: Lotto.js 리팩토링
    - checkCorrect()

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,12 +12,10 @@ class Lotto {
     if (numbers.length !== limit) {
       throw new Error(`[ERROR] 입력 번호는 ${limit}개여야 합니다.`);
     }
-    
     numbers.forEach(number => {
       if (number > 45 || number < 1) {
         throw new Error("[ERROR] 번호의 범위는 1~45 입니다.");}
     })
-
     const orderedNumbers = new Set(numbers)
     if(Array.from(orderedNumbers).length !== limit){
       throw new Error("[ERROR] 중복된 숫자가 있습니다.")
@@ -33,73 +31,87 @@ class Lotto {
     });
   }
 
-  checkCorrect(winningAndBonusNum, makedLottos){
-    const winningNum = winningAndBonusNum[0];
-    const bonusNum = winningAndBonusNum[1];
-    const correctNums = [];
-    let correctBonus = 0;
-    this.#numbers = makedLottos.length;
+  checkCorrect(winAndBonusNum, makedLots){
+    const winningNumLs = winAndBonusNum[0];
+    const bonusNum = winAndBonusNum[1];
+    const correctLists = [];
+    let secondPrize = 0;
+    this.savePurchaseQuantity(makedLots);
 
-    for (let i = 0; i < makedLottos.length; i++) {
-      const correctNum = makedLottos[i].filter(number => winningNum.includes(String(number)));
-      const matchBonus = this.checkBonus(makedLottos[i], ...bonusNum)
-
-      if (correctNum.length == 5 && matchBonus) correctBonus += 1;
-      else correctNums.push(correctNum.length);
+    for (let i = 0; i < makedLots.length; i++) {
+      const winNumInLot = this.checkWinNumInlot(makedLots[i], winningNumLs);
+      const is2ndPrize = this.check2ndPrize(winNumInLot, makedLots[i], ...bonusNum);
+      is2ndPrize ? secondPrize += 1 : correctLists.push(winNumInLot.length);
     }
-    this.checkResult(correctNums, correctBonus);
-    return [correctNums, correctBonus]
+    this.checkResult(correctLists, secondPrize);
+    return [correctLists, secondPrize]
   }
 
-  checkBonus(correctFiveNumLottos, bonusNum) {
+  savePurchaseQuantity(makedLots) {
+    this.#numbers = makedLots.length;
+  }
+
+  checkWinNumInlot(makedLots, winningNumLs) {
+    return makedLots.filter(number => winningNumLs.includes(String(number)));
+  }
+
+  check2ndPrize(winNumInLot, makedLotto, bonusNum){
+    if (winNumInLot.length == 5) {
+      if (this.checkHasBonusNum(makedLotto, bonusNum)) return true
+      else return false
+    }
+    return false
+  }
+
+  checkHasBonusNum(correctFiveNumLottos, bonusNum) {
     return correctFiveNumLottos.includes(Number(bonusNum))
   }
 
-  checkResult(correctNums, correctBonus) {
-    const correctCount = [{'3개':0},{'4개':0},{'5개':0},{'6개':0}];
+  checkResult(correctLists, secondPrize) {
+    const winningList = [{'3개':0},{'4개':0},{'5개':0},{'6개':0}];
 
-    correctNums.forEach(correctLength => {
-      if(correctLength === 3) correctCount[0]['3개'] += 1;
-      if(correctLength === 4) correctCount[1]['4개'] += 1;
-      if(correctLength === 5) correctCount[2]['5개'] += 1;
-      if(correctLength === 6) correctCount[3]['6개'] += 1;
+    correctLists.forEach(correctLength => {
+      if(correctLength === 3) winningList[0]['3개'] += 1;
+      if(correctLength === 4) winningList[1]['4개'] += 1;
+      if(correctLength === 5) winningList[2]['5개'] += 1;
+      if(correctLength === 6) winningList[3]['6개'] += 1;
     });
-    const result = this.preparePrint(correctCount, correctBonus);
+    const totalResult = this.preparePrint(winningList, secondPrize);
 
-    this.printResult(result);
-    this.printEarningsRate(result);
-    return [correctCount, correctBonus]
+    this.printResult(totalResult);
+    this.printEarningsRate(totalResult);
+    return [winningList, secondPrize]
   }
 
-  preparePrint(correctCount, correctBonus) {
-    const result = [];
-    result.push(correctCount[0]['3개'])
-    result.push(correctCount[1]['4개'])
-    result.push(correctCount[2]['5개'])
-    result.push(correctBonus)
-    result.push(correctCount[3]['6개'])
-    return result
+  preparePrint(winningList, secondPrize) {
+    const totalResult = [];
+    totalResult.push(winningList[0]['3개'])
+    totalResult.push(winningList[1]['4개'])
+    totalResult.push(winningList[2]['5개'])
+    totalResult.push(secondPrize)
+    totalResult.push(winningList[3]['6개'])
+    return totalResult
   }
 
-  printResult(result) {
+  printResult(totalResult) {
     return MissionUtils.Console.print(
       '\n당첨 통계\n'+
       '---\n'+
-      `3개 일치 (5,000원) - ${result[0]}개\n`+
-      `4개 일치 (50,000원) - ${result[1]}개\n`+
-      `5개 일치 (1,500,000원) - ${result[2]}개\n`+
-      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[3]}개\n`+
-      `6개 일치 (2,000,000,000원) - ${result[4]}개`
+      `3개 일치 (5,000원) - ${totalResult[0]}개\n`+
+      `4개 일치 (50,000원) - ${totalResult[1]}개\n`+
+      `5개 일치 (1,500,000원) - ${totalResult[2]}개\n`+
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${totalResult[3]}개\n`+
+      `6개 일치 (2,000,000,000원) - ${totalResult[4]}개`
       )
   }
 
-  printEarningsRate(result) {
+  printEarningsRate(totalResult) {
     let earnings = 0;
-    earnings += 5000 * result[0];
-    earnings += 50000 * result[1];
-    earnings += 1500000 * result[2];
-    earnings += 30000000 * result[3];
-    earnings += 2000000000 * result[4];
+    earnings += 5000 * totalResult[0];
+    earnings += 50000 * totalResult[1];
+    earnings += 1500000 * totalResult[2];
+    earnings += 30000000 * totalResult[3];
+    earnings += 2000000000 * totalResult[4];
 
     const earningsRate = earnings / (this.#numbers*10);
 
