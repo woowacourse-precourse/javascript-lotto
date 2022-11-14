7a9eace302118ae676395e803a72a3054b407273
foundaway98 <foundaway98@naver.com>
2022-11-14 19:32:41 +0900

    <feat> : play() 메소드에서 hitLottoChecker 객체 생성
    -인자로 money와 구매한 로또를 저장한 배열 전달

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 const LottoBuyer = require("./LottoBuyer.js");
 const MissionUtils = require("@woowacourse/mission-utils");
+const HitLottoChecker = require("./HitLottoChecker.js");
 class App {
   lottoBuyer;
+  hitLottoChecker;
 
   play() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
       this.lottoBuyer = new LottoBuyer(money);
       this.lottoBuyer.buyLotto();
+      this.hitLottoChecker = new HitLottoChecker(
+        money,
+        this.lottoBuyer.getLottoArray()
+      );
     });
   }
 }
