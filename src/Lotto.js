dddbb68a2c54922206fe293ce9e2fa5629e8eb75
Suyeon <msjgr2801@gmail.com>
2022-11-14 15:00:14 +0900

    Refactor: [Lotto.js] 코드 리팩토링
    1. validate의 escape 상수를 클래스 밖 상수로 선언
    2. countOfCorrect -> correctCount 변경
    3. standardOfRank 함수 -> decideRank 로 변경
    4. getRankFromLotto 와 decideRank 에서 사용되는 매개변수의 이름을 통일 시킴

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,28 +1,29 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
+const ESCAPE = ["\n", "\t", "\a", "\v", "\b", "\f", "\\", "\'", "\""];
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
+    MissionUtils.Console.close();
   }
 
   validate(numbers) {
     const numSet = new Set(numbers);
-    const escape = ["\n", "\t", "\a", "\v", "\b", "\f", "\\", "\'", "\""];
 
     if (numbers.length !== 6) {
       throw `[ERROR] 로또 번호는 6개여야 합니다.`;
     }
-
     if (numSet.size !== 6) {
       throw `[ERROR] 로또 번호가 중복됩니다.`;
     }
-    
-    escape.forEach((element) => {
+
+    ESCAPE.forEach((element) => {
       if (numSet.has(element)) {
-        throw `[ERROR] 번호들은 콤마(,)로만 구분되며 이스케이프 문자를 사용하지 않습니다.`
+        throw `[ERROR] 번호들은 콤마(,)로만 구분되며 이스케이프 문자를 사용하지 않습니다.`;
       }
     })
 
@@ -30,7 +31,6 @@ class Lotto {
       if (isNaN(num)) {
         throw `[ERROR] 숫자가 아닌 값이 있습니다.`;
       }
-
       if (Number(num) < 1 || Number(num) > 45) {
         throw `[ERROR] 로또 번호는 1 ~ 45번까지만 존재합니다.`; 
       }
@@ -42,29 +42,28 @@ class Lotto {
     MissionUtils.Console.print(`[${this.#numbers.join(", ")}]`);
   }
 
-  calRank(winningNums) {
+  getRankFromLotto(winningNums) {
     const bonusNumber = winningNums.pop();
     const winningNumSet = new Set(winningNums);
-    let countOfCorrectNum = 0;
+    let correctCount = 0;
     let bonusFlag = false;
 
     for (let num of this.#numbers) {
       if (winningNumSet.has(num)) {
-        countOfCorrectNum++;
+        correctCount++;
       }
-
       if (num === bonusNumber){
         bonusFlag = true;
       }
     }
 
-    return this.standardOfRank(countOfCorrectNum, bonusFlag);
+    return this.decideRank(correctCount, bonusFlag);
   }
 
-  standardOfRank(count, flag) {
+  decideRank(correctCount, bonusFlag) {
     let rank = 0;
 
-    switch (count) {
+    switch (correctCount) {
       case 3:
         rank = 5;
         break;
@@ -85,7 +84,7 @@ class Lotto {
         break;
     }
 
-    if (rank === 3 && flag === true) {
+    if (rank === 3 && bonusFlag === true) {
       rank = 2;
     }
 
