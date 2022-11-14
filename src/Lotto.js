39327600dbf492199c027166e54955fd2c76c112
Chaeduk <kkj2551@naver.com>
2022-11-14 23:30:59 +0900

    feat: 로또 번호 수량&자동 발급 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,20 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error("[ERROR] 중복된 로또 번호가 존재합니다.");
+    }
+  }
+
+  setBonusNum(bonusNum) {
+    if (this.#numbers.includes(bonusNum)) {
+      throw new Error("[ERROR] 중복된 입력이 존재합니다.");
+    }
+    this.#numbers.push(bonusNum);
+  }
+
+  getLottoNum() {
+    return this.#numbers;
   }
 
   // TODO: 추가 기능 구현
