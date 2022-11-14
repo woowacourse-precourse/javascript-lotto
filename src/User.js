2382f61bc709d2b6c0c301a9ceee7fbe43a4fd44
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-15 04:23:44 +0900

    feat: 로또 구입금액 입력받는 기능의 예외 처리 - src/User.js : 입력 받은 구입금액이 유효한지 확인하는 isValid()를 구현하였고, isValid() 메소드의 반환값이 false라면 에러 메시지 출력 후 프로그램이 종료되는 기능을 추가했습니다. - docs/README.md : 구현 완료한 기능에 체크 표시

--- src/User.js
+++ src/User.js
@@ -2,11 +2,24 @@ class User {
   #money
 
   constructor(money) {
+    if (!this.isValid(money)) {
+      throw new Error("[ERROR] 구입 금액은 1000원 단위의 수여야 합니다.");
+    }
     this.#money = money;
   }
 
   getMoney() {
     return this.#money;
+  }
+
+  isValid(money) {
+    if (isNaN(money)) {
+      return false;
+    }
+    if (money % 1000 != 0) {
+      return false;
+    }
+    return true;
   }
 }
 
