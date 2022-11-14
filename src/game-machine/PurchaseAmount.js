43facea0de360c363bac05598af407bba070c373
SorinKwon <ksl020700@gmail.com>
2022-11-14 20:57:02 +0900

    feat: 구입 금액 입력 함수 호출

--- src/game-machine/PurchaseAmount.js
+++ src/game-machine/PurchaseAmount.js
@@ -2,14 +2,15 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class PurchaseAmount {
 
   constructor(){
-    this.purchase_amount=purchase_amount;
+    this.purchase_amount;
+    this.inputPurchaseAmount();
   }
 
   inputPurchaseAmount(){
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (input_money) => {
       console.log(input_money);
+      this.savePurchaseAmount(input_money);
     });
-    this.savePurchaseAmount(input_money);
   }
   
   savePurchaseAmount(input_money){
