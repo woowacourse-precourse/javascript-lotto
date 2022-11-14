89cbe55db1ad6868abfe76b45729299df9594b19
OrangeVinyl <hap_hazard@naver.com>
2022-11-14 23:20:38 +0900

    feat: 로또 구매 갯수 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,26 +1,15 @@
-const { Console } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 
 class Lotto {
-  // #numbers;
+  #countLotto;
 
-  // constructor(numbers) {
-  //   this.validate(numbers);
-  //   this.#numbers = numbers;
-  // }
-
-  inputMoney() {
-    Console.readLine('구입금액을 입력해 주세요.\n', (inputMoney) => {
-      this.validate(inputMoney);
-      Console.close();
-    });
+  constructor(numbers) {
+    // this.validate(numbers);
+    this.#countLotto = 0;
   }
 
-  play() {
-    this.inputMoney();
-  }
-
-  thorwError(c) {
-    throw new Error(c);
+  thorwError(message) {
+    throw new Error(message);
   }
 
   validate(inputMoney) {
@@ -29,21 +18,35 @@ class Lotto {
       !this.isNumber(inputMoney) ||
       !this.isThousandUnit(inputMoney)
     ) {
-      return this.thorwError('[ERROR] 일단은 통합한 오류.');
+      return this.thorwError(
+        '[ERROR] 일단은 통합한 오류 입력금액을 다시 적으세요.'
+      );
     }
     return true;
   }
 
-  isBlank(number) {
-    return number.length !== 6;
+  isBlank(input) {
+    return !input;
   }
 
-  isNumber(number) {
-    return !isNaN(number);
+  isNumber(input) {
+    return !isNaN(input);
   }
 
-  isThousandUnit(number) {
-    return number % 1000 === 0;
+  isThousandUnit(input) {
+    return input % 1000 === 0;
+  }
+
+  inputMoney() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (inputMoney) => {
+      this.validate(inputMoney);
+      this.#countLotto = Number(inputMoney) / 1000;
+      Console.close();
+    });
+  }
+
+  play() {
+    this.inputMoney();
   }
 }
 
