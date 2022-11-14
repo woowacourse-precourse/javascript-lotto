5e8def4e5a0b28c9b32f2be4bdfa1ae26e9d2fa9
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-11-14 15:49:26 +0900

    test: 보너스 번호 입력 기능 테스트 작성 - 보너스 번호와 당첨 번호가 중복될 때 오류가 발생한다.

--- __tests__/MyUnitTest.js
+++ __tests__/MyUnitTest.js
@@ -4,6 +4,7 @@ describe('My Unit test', () => {
   test('1000원 단위가 아닌 로또 구입 금액 입력시 오류가 난다.', () => {
     const app = new App();
     const amount = 4500;
+
     expect(() => {
       app.setAmount(amount);
     }).toThrow('[ERROR]');
@@ -12,6 +13,7 @@ describe('My Unit test', () => {
   test('로또 구입 금액 입력시 숫자를 제외한 입력을 하면 오류가 난다.', () => {
     const app = new App();
     const amount = '오천원';
+
     expect(() => {
       app.setAmount(amount);
     }).toThrow('[ERROR]');
@@ -23,6 +25,7 @@ describe('My Unit test', () => {
     app.setAmount(amount);
     app.issueLottos();
     const lottos = app.getLottos();
+
     expect(lottos).toHaveLength(amount / 1000);
   });
 
@@ -32,6 +35,7 @@ describe('My Unit test', () => {
     app.setAmount(amount);
     app.issueLottos();
     const lottos = app.getLottos();
+
     lottos.forEach((lotto) => {
       const sortedLotto = [...lotto];
       sortedLotto.sort();
@@ -42,6 +46,7 @@ describe('My Unit test', () => {
   test('중복된 당첨 번호가 있을 때 오류가 난다.', () => {
     const app = new App();
     const winNumbers = [1, 2, 3, 4, 5, 1];
+
     expect(() => {
       app.setWinNumbers(winNumbers);
     }).toThrow('[ERROR]');
@@ -50,8 +55,20 @@ describe('My Unit test', () => {
   test('당첨 번호가 6개가 아닐 때 오류가 난다.', () => {
     const app = new App();
     const winNumbers = [1, 2, 3, 4, 5];
+
     expect(() => {
       app.setWinNumbers(winNumbers);
+    }).toThrow('[ERROR]');
+  });
+
+  test('보너스 번호와 당첨 번호가 중복될 때 오류가 발생한다.', () => {
+    const app = new App();
+    const winNumbers = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 1;
+
+    expect(() => {
+      app.setWinNumbers(winNumbers);
+      app.setBonusNumber(bonusNumber);
     }).toThrow('[ERROR]');
   });
 });
