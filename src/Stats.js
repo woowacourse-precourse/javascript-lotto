119aac1ebb4a2ab42122f68cf17e6be7fd571860
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-15 00:33:22 +0900

    feat(method): add countCorrectNumbers
    Add to Stats class
    scoreMap object
    - Receive correct number counts and call functions for each
    
    score functions
    - Return score in string by scoreMap key
    
    countCorrecNumbers
    - Count how many mathcing numbers a Lotto instance has with winning numbers
    - Get score by count
    - Return score

--- src/Stats.js
+++ src/Stats.js
@@ -1,8 +1,33 @@
+const underThree = (bonus) => "underThree";
+const three = (bonus) => "three";
+const four = (bonus) => "four";
+const five = (bonus) => (bonus ? "five" : "fivePlusBonus");
+const six = (bonus) => "six";
+
+const scoreMap = {
+  0: underThree,
+  1: underThree,
+  2: underThree,
+  3: three,
+  4: four,
+  5: five,
+  6: six,
+};
+
 class Stats {
   constructor({ winningNumbers, bonusNumber, purchased }) {
     this.winningNumbers = winningNumbers;
     this.bonusNumber = bonusNumber;
     this.purchased = purchased;
+  }
+
+  countCorrectNumbers(lotto) {
+    const lottoNumbers = lotto.showNumbers();
+    const correctCount = this.winningNumbers.filter((number) => lottoNumbers.includes(number)).length;
+    const bonus = lottoNumbers.includes(this.bonusNumber);
+
+    const score = scoreMap[correctCount](bonus);
+    return score;
   }
 }
 
