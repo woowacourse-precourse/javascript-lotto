83c0b2382fb4fb01615917a5410b8163e8d43eb4
jhyns <tockiib@gmail.com>
2022-11-14 18:37:37 +0900

    feat: Add validate method of Lotto class
    Lotto 클래스의 validate method 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,10 @@
+const {
+  LOTTO_LENGTH_NOT_SIX_ERROR,
+  LOTTO_DUPLICATE_ERROR,
+  LOTTO_OUT_OF_RANGE_ERROR,
+  LOTTO_NAN_ERROR,
+} = require('./Constants');
+
 class Lotto {
   #numbers;
 
@@ -6,13 +13,35 @@ class Lotto {
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  checkDuplicates(numbers) {
+    const set = new Set(numbers);
+    return set.size !== numbers.length;
   }
 
-  // TODO: 추가 기능 구현
+  checkRange(numbers) {
+    const min = Math.min(...numbers);
+    const max = Math.max(...numbers);
+    return min < 1 || max > 45;
+  }
+
+  checkNumber(numbers) {
+    return numbers.some((num) => typeof num !== 'number');
+  }
+
+  validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error(LOTTO_LENGTH_NOT_SIX_ERROR);
+    }
+    if (this.checkDuplicates(numbers)) {
+      throw new Error(LOTTO_DUPLICATE_ERROR);
+    }
+    if (this.checkRange(numbers)) {
+      throw new Error(LOTTO_OUT_OF_RANGE_ERROR);
+    }
+    if (this.checkNumber(numbers)) {
+      throw new Error(LOTTO_NAN_ERROR);
+    }
+  }
 }
 
 module.exports = Lotto;
