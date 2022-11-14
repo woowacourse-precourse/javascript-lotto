db3fda2efa14c9d30f118dc18454db3bdb7f5dd0
su-lim <sujin.0xlim@gmail.com>
2022-11-15 01:47:01 +0900

    feat: 당첨 번호 입력 시 발생가능한 예외사항을 처리하는 메서드 구현

--- src/LotteryMachine.js
+++ src/LotteryMachine.js
@@ -1,5 +1,6 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const { QUESTION, ERR_MSG } = require("./constants/constants");
+const Lotto = require("./Lotto");
 
 class LotteryMachine {
   #winningNum;
@@ -16,20 +17,32 @@ class LotteryMachine {
       winningNum = this.changeNumArray(input);
     });
     MissionUtils.Console.close();
-    this.#winningNum = winningNum;
+    this.#winningNum = new Lotto(winningNum).getNumber();
   }
 
   setBonusNum() {
+    let bonusNum = 0;
     MissionUtils.Console.readLine(QUESTION.setBonusNum, (input) => {
-      isNumber(input);
-      this.#bonusNum = parseInt(input);
+      bonusNum = input;
     });
     MissionUtils.Console.close();
+    this.validNum(bonusNum);
+    this.validBonusNum(bonusNum);
+    this.#bonusNum = parseInt(bonusNum);
   }
 
-  isNumber(value) {
+  validNum(value) {
     if (isNaN(+value)) {
       throw new Error(ERR_MSG.notNumber);
+    }
+    if (!(value >= 1 && value <= 45)) {
+      throw new Error(ERR_MSG.notLottoRange);
+    }
+  }
+
+  validBonusNum(value) {
+    if (this.#winningNum.includes(parseInt(value))) {
+      throw new Error(ERR_MSG.notUniqueNumber);
     }
   }
 
@@ -37,7 +50,7 @@ class LotteryMachine {
     const array = string.split(",");
     const numberArray = [];
     array.map((number) => {
-      isNumber(number);
+      this.validNum(number);
       numberArray.push(parseInt(number));
     });
     return numberArray;
