1431d375cde56a0159381da02c41f54512b59bda
hyunjin <leehj0110@kakao.com>
2022-11-14 23:48:03 +0900

    refactor: isValidWinningNumber 에러처리 추가

--- src/Validation.js
+++ src/Validation.js
@@ -15,7 +15,30 @@ class Validation {
     return true;
   }
 
-  static isValidWinningNumber(winningNumbers) {}
+  static isValidWinningNumber(winningNumbers) {
+    const winningNumbersArray = winningNumbers.split(",").map(Number);
+
+    if (winningNumbersArray.length !== 6) {
+      throw new Error("[ERROR] 당첨 번호는 6개여야 합니다.");
+    }
+
+    if (new Set(winningNumbersArray).size !== 6) {
+      throw new Error("[ERROR] 당첨 번호는 중복되지 않아야 합니다.");
+    }
+
+    winningNumbersArray.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 당첨 번호는 1~45 사이의 숫자여야 합니다.");
+      }
+
+      if (isNaN(number)) {
+        throw new Error("[ERROR] 당첨 번호는 숫자여야 합니다.");
+      }
+    });
+
+    return true;
+  }
+
   static isValidLottoArray(lotto) {}
   static isValidBonusNumber(bonusNumber) {}
 }
