53c4737b30111a26335dfb0fc1881fc2177e33ae
Sallyvely <tpdus4159@naver.com>
2022-11-14 15:40:03 +0900

    refactor: 코드 리팩토링

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,30 +4,30 @@ class Lotto {
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
-    this.checkUniqueRange(numbers);
-    this.checkNumberRange(numbers);
+    //this.checkUniqueRange(numbers);
+    //this.checkNumberRange(numbers);
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    
   }
+
   // TODO: 추가 기능 구현
-  checkUniqueRange(winningArr){
-    const setWinningArr=new Set(winningArr);
-    if(setWinningArr.size==6){
-        return true;
+  checkUniqueRange (winningArr) {
+    const setWinningArr = new Set(winningArr);
+    if (setWinningArr.size === 6) {
+      return true;
     }
     throw new Error("[ERROR] 로또 번호는 중복되면 안됩니다.");
-}
-  checkNumberRange(winningArr){
-    for(let index=0;index<winningArr.length;index++){
-        if(!(winningArr[index]>=1&&winningArr[index]<=45)){
-            throw new Error("[ERROR] 로또 번호가 잘못 입력되었습니다.");
-        }
-        
+  }
+
+  checkNumberRange (winningArr) {
+    for (let index = 0; index < winningArr.length; index++) {
+      if (!(winningArr[index] >= 1 && winningArr[index] <= 45)) {
+        throw new Error("[ERROR] 로또 번호가 잘못 입력되었습니다.");
+      }
     }
   }
 }
