18e884951c9a18021ccdd5ee88ad189f5c3420a0
Hungeun2 <bepoohun@gmail.com>
2022-11-14 23:51:06 +0900

    Feat: 구매한 로또 오름차순 정렬 추가 AscendingSort.js

--- src/BuyLotto.js
+++ src/BuyLotto.js
@@ -1,10 +1,11 @@
 const { Random } = require("@woowacourse/mission-utils");
+const ascendingSort = require("./util/AscendingSort");
 
 function buyLotto(Count) {
   const boughtLotto = [];
   for (let i = 0; i < Count; i++) {
     const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
-    boughtLotto.push(numbers);
+    boughtLotto.push(ascendingSort(numbers));
   }
   return boughtLotto;
 }
