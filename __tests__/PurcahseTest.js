62d46ab0c4254c27298b52daeca598eab7c710c5
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-14 20:53:51 +0900

    test: check purchase lottery tickets

--- __tests__/PurcahseTest.js
+++ __tests__/PurcahseTest.js
@@ -1,4 +1,12 @@
 const PurChase = require('../src/Purchase');
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
 
 describe('금액 테스트 오류 확인', () => {
   test('금액이 숫자인지 확인', () => {
@@ -44,5 +52,49 @@ describe('금액 테스트 오류 확인', () => {
         new PurChase(answer).showLotteryTickets();
       }).toThrow('[ERROR]');
     });
+  });
+});
+
+describe('로또 티켓 출력 개수 확인', () => {
+  test('3000원 일 경우', () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+    ]);
+    const result = new PurChase('3000').showLotteryTickets();
+    expect(result).toEqual([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+    ]);
+  });
+
+  test('만원일 경우', () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+      [3, 4, 5, 6, 12, 23, 35],
+      [16, 17, 31, 34, 36, 40],
+    ]);
+    const result = new PurChase('10000').showLotteryTickets();
+    expect(result).toEqual([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+      [3, 4, 5, 6, 12, 23, 35],
+      [16, 17, 31, 34, 36, 40],
+    ]);
   });
 });
--- src/Profit.js
+++ src/Profit.js
@@ -1,12 +1,15 @@
 const { DECIMAL_PLACES } = require('./constant');
 
 class Profit {
+  #amount;
+  #matchInfo;
+
   constructor(purchaseInfo) {
-    [this.amount, this.matchInfo] = purchaseInfo;
+    [this.#amount, this.#matchInfo] = purchaseInfo;
   }
 
   calculateProfit() {
-    const profitRate = (this.calculateTotalAmount(this.matchInfo) / parseInt(this.amount)) * 100;
+    const profitRate = (this.calculateTotalAmount(this.#matchInfo) / parseInt(this.#amount)) * 100;
     return Math.round(profitRate * DECIMAL_PLACES) / DECIMAL_PLACES;
   }
 
