49e32e674ece842f0b8f4884f84b2f3df513713c
nailen1 <nietsnie1@gmail.com>
2022-11-14 23:22:28 +0900

    feat: 로또 객체를 생성 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,10 @@
 class Lotto {
   #numbers;
-
-  constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
+  constructor(numbersArr) {
+    this.#numbers = numbersArr;
+    this.matchingResult = {};
+    this.prizeResult = {};
   }
-
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
