48812a1e2669e2d3ad3e95bcaf3d2651653977d4
Bori-github <qhflrnfl4324@gmail.com>
2022-11-14 14:25:34 +0900

    feat(App): input Lotto purchase amount
    - validate player input
    - calculate the number of purchase
    - print the message

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { UNIT_OF_AMOUNT } = require('./constant/index');
 
 class App {
   play() {
     this.startGame();
   }
 
+  startGame() {
+    this.printStartMessage();
+    this.getAmountOfPurchase();
+  }
+
   printStartMessage() {
     Console.print('구입금액을 입력해 주세요.');
   }
 
-  startGame() {
-    this.printStartMessage();
+  getAmountOfPurchase() {
+    Console.readLine('', (answer) => {
+      const amountOfPurchase = this.convertToNumber(answer);
+
+      this.validatePurchaseAmount(amountOfPurchase);
+
+      Console.print(
+        `${this.numberOfPurchase(amountOfPurchase)}개를 구매했습니다.`
+      );
+    });
+  }
+
+  convertToNumber(stringNumber) {
+    return parseInt(stringNumber, 10);
+  }
+
+  validatePurchaseAmount(amount) {
+    if (amount % UNIT_OF_AMOUNT !== 0)
+      throw new Error('[ERROR] 로또 구입 금액은 1,000원 단위로 입력해주세요');
+  }
+
+  numberOfPurchase(amount) {
+    return Math.floor(amount / UNIT_OF_AMOUNT);
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/constant/index.js
@@ -0,0 +1,3 @@
+const UNIT_OF_AMOUNT = 1000;
+
+module.exports = { UNIT_OF_AMOUNT };
