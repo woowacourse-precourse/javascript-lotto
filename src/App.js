07056727bfee4f486c450301b6cd1bfa673a3e95
dustmddus <dustmddus5@gmail.com>
2022-11-14 13:04:44 +0900

    feat: 입력 금액 유효성 검사

--- src/App.js
+++ src/App.js
@@ -7,8 +7,18 @@ class App {
     this.printLottoNumber(lottoAmount);
   }
 
+  inputMoneyValidate(money) {
+    if (typeof money !== "number") {
+      throw new Error("금액은 숫자만 입력 가능합니다.");
+    }
+    if (money % 10 !== 0) {
+      throw new Error("금액은 10원 단위로 입력 가능합니다.");
+    }
+  }
+
   inputMoney() {
     Console.readLine("구매 금액을 입력해주세요.", (money) => {
+      this.inputMoneyValidate();
       this.setLottoAmount(money);
     });
   }
