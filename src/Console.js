13f656be6c9e68be27cf5c6e2c1c4f9b8c2989ea
동동이 <drdragon777@naver.com>
2022-11-14 22:09:49 +0900

    feat - add getUserInput, print, close static 메소드

--- src/Console.js
+++ src/Console.js
@@ -6,6 +6,18 @@ class Console {
   static REQUEST_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.\n";
   static REQUEST_BONUT_NUMBER = "보너스 번호를 입력해 주세요.";
   static REQUEST_BONUT_NUMBER = "당첨 통계\n---";
+
+  static getUserInput(question, callback) {
+    MissionUtils.Console.readLine(question, callback);
+  }
+
+  static print(message) {
+    MissionUtils.Console.print(message);
+  }
+
+  static close() {
+    MissionUtils.Console.close();
+  }
 }
 
 module.exports = Console;
