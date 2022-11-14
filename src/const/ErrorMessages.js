e4933249d1238ee88a87980bc3493ed7c0c25d75
deli-ght <minjju0660@gmail.com>
2022-11-14 18:53:37 +0900

    feat(ErrorMessages.js): initialize error messages to constant

---
+++ src/const/ErrorMessages.js
@@ -0,0 +1,10 @@
+const ERROR_MESSAGES = {
+  IS_NUMBER: "[ERROR] 숫자를 입력해주세요.",
+  IS_OVER_MIN_COST: "[ERROR] 최소 금액 1000원 이상 작성해야합니다.",
+  IS_NO_CHARGE: "[ERROR] 1000원 단위로 입력해주세요.",
+  IS_ENOUGH: "[ERROR] 로또 번호는 6개여야 합니다.",
+  IS_REPEATED_NUMBER: "[ERROR] 로또 번호는 중복될 수 없습니다.",
+  IS_RANGE: "[ERROR] 로또 번호는 1~45 사이의 숫자만 가능합니다.",
+};
+
+module.exports = ERROR_MESSAGES;
