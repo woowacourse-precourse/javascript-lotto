6b4be067c5259724212a0306cc70fab995d11e78
chsua <csj1919@naver.com>
2022-11-14 16:12:31 +0900

    feat: 2. 1) 랜덤번호 예외처리(Lotto.js)

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,11 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    numbers.forEach(x => {
+      if (x < 1 || x > 45) throw new Error("[ERROR] 로또 번호는 0보다 크고 46보다 작아야 합니다.");
+    })
+    let set = new Set(numbers);
+    if (set.size !== 6) throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.")
   }
 
   // TODO: 추가 기능 구현
