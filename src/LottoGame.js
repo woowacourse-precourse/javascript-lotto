2ca3b82c2bcd73a33a2db4f123380e89606d54f3
adultlee <seoungin1228@gmail.com>
2022-11-15 00:50:06 +0900

    feat: 구입 금액 입력 받는 기능 구현
    구입금액을 입력 받습니다.
    구입금액을  purchasePrice , lottoCount에 각각 저장합니다. (아직 예외처리를 포함하지 않습니다.)

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -6,7 +6,15 @@ class LottoGame {
     this.purchasePrice = 0;
   }
 
-  start() {}
+  start() {
+    MissionUtils.Console.readLine(
+      "구입금액을 입력해 주세요.\n",
+      (purchasePrice) => {
+        this.purchasePrice = purchasePrice;
+        this.lottoCount = purchasePrice / 1000;
+      }
+    );
+  }
 }
 
 module.exports = LottoGame;
