da049d8df270ea581d86dd7e47c887fee4e0bce5
jaehak26 <jack10829@naver.com>
2022-11-15 00:42:30 +0900

    로또 구입금액 입력, 로또 번호 만들고 출력
    로또 구입금액을 입력하고 예외가 있는지 확인시킨다.
    이후 로또 번호를 만들고 출력시키는 기능을 구현했다

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-const Lotto = require("./Lotto")
-const MissionUtils = require("@woowacourse/mission-utils");
+// run `node index.js` in the terminal
+//npm i @woowacourse/mission-utils
+
+const Lotto = require('./Lotto');
+const MissionUtils = require('@woowacourse/mission-utils');
+import { exceptLottoPrice, exceptBonusNumber } from './Exception';
 
 class App {
-  play() {}
-}
+  #lotteryTicketNumber; //로또 수량
+  #lotteryTickets
 
+  play() {
+    this.#inputLottoPrice();
+  }
+
+  #inputLottoPrice() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (lottoPrice) => {
+      exceptLottoPrice(Number(lottoPrice));
+      this.#lotteryTicketNumber = Number(lottoPrice) / 1000;
+
+      this.#makeLotteryTickets(this.#lotteryTicketNumber, this.#lotteryTickets);
+    });
+  }
+
+  #makeLotteryTickets(lotteryTicketNumber, lotteryTickets) {
+    lotteryTickets = new Array(lotteryTicketNumber);
+    lotteryTickets.map((lotteryTicket) => {
+      lotteryTicket = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      lotteryTicket.sort();
+    });
+
+    this.#printLotteryTickets(lotteryTicketNumber, lotteryTickets)
+  }
+
+  #printLotteryTickets(lotteryTicketNumber, lotteryTickets=[]){
+    MissionUtils.Console.print(`${lotteryTicketNumber}개를 구매했습니다.`)
+    lotteryTickets.map((lotteryTicket)=>{
+      MissionUtils.Console.print(`${lotteryTicket}`)
+    })
+  }
+
+  #inputWinningNumbers(){
+
+  }
+
+  #inputBonusNumbers(){
+
+  }
+
+  
+}
 
 const app = new App();
 app.play();
