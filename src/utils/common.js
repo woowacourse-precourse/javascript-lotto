5810f95d42947314c5976e3d50d417f845eb8b5d
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-14 19:57:44 +0900

    feat: 소수 둘째자리까지 반올림하여 반환하는 함수 구현

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,4 +1,13 @@
 /**
+ * 소수 둘째자리까지 반올림하여 반환하는 함수
+ * @param {number} number
+ * @returns {number} 반올림된 숫자
+ */
+const roundNumber = (number) => {
+  return Math.round(number * 100) / 100;
+};
+
+/**
  * 숫자 문자열을 입력받아 숫자로 변환하는 함수
  * @param {string} string 정수형으로 변환할 문자열
  * @returns {number}
@@ -17,6 +26,7 @@ const commaizeNumber = (number) => {
 };
 
 module.exports = {
+  roundNumber,
   stringToNumber,
   commaizeNumber,
 };
