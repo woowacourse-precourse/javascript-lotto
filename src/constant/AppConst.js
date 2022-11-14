17248a26389663b04d34013c606b5bf101b4dc58
Rki0 <pky00823@gmail.com>
2022-11-14 10:27:01 +0900

    refactor(App): modularize constants to another file
    Move constants managed in one file to a separate file.

---
+++ src/constant/AppConst.js
@@ -0,0 +1,11 @@
+const AppConst = {
+  ASK_AMOUNTS_MESSAGE: "구입금액을 입력해 주세요.\n",
+  ASK_WINNING_NUMBER_MESSAGE: "\n당첨 번호를 입력해 주세요.\n",
+  ASK_BONUS_NUMBER: "\n보너스 번호를 입력해 주세요.\n",
+  ERROR_DONT_START_ZERO: "[ERROR] 0으로 시작하는 숫자는 입력할 수 없습니다.",
+  ERROR_NOT_THOUSAND_UNIT: "[ERROR] 1,000원 단위로만 구매 가능합니다.",
+  ERROR_NOT_ONLY_NUMBER: "[ERROR] 숫자만 입력 가능합니다.",
+  ERROR_NOT_NUMBER_AND_COMMA: "[ERROR] 숫자와 ,(쉼표) 기호만을 입력해주세요.",
+};
+
+module.exports = AppConst;
