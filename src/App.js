cf5b204de8451af9e9f8f6ccf5fa4e805da245ec
Junghwan Kim <junghwki@c5r5s2.42seoul.kr>
2022-11-14 17:57:31 +0900

    feat: 구매 금액 입력 함수 및 검증 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  getPlayerInput() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (input) => {
+      console.log(this.validate(input));
+    });
+  }
+
+  validate(input) {
+    let element;
+
+    for (let index = 0; index < input.length; index++) {
+      element = Number(input[index]);
+      if (isNaN(element)) {
+        throw new Error("숫자만 입력해 주세요");
+      }
+    }
+    return this.countLotto(parseInt(input));
+  }
+
+  countLotto(cost) {
+    if (cost % 1000) {
+      throw new Error("1,000원 단위로 입력해야합니다");
+    }
+    return cost / 1000;
+  }
+
+  play() {
+    this.getPlayerInput();
+  }
 }
 
 module.exports = App;
