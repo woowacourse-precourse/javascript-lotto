6d073b04e6b802788309a9b2e63c86955e8f5653
Narcoker <jykim9868@gmail.com>
2022-11-15 02:47:46 +0900

    Feat: ENTER 메세지 문구 개행 추가

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,7 @@
 const MESSAGE = Object.freeze({
-    ENTER_CASH: "구입금액을 입력해 주세요.",
-    ENTER_BOUNS_NUMBER: "보너스 번호를 입력해 주세요.",
-    ENTER_WINNING_LOTTO: "당첨 번호를 입력해 주세요.",
+    ENTER_CASH: "구입금액을 입력해 주세요.\n",
+    ENTER_BOUNS_NUMBER: "보너스 번호를 입력해 주세요.\n",
+    ENTER_WINNING_LOTTO: "당첨 번호를 입력해 주세요.\n",
     BUY_LOTTOS_COUNT: "개를 구매했습니다.",
     RANK_TEXT: [
         "6개 일치 (2,000,000,000원)",
