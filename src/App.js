d922afdb66cc41d2e5a7730ab70d2efcea586849
cho seonghoon <chsida655@gmail.com>
2022-11-14 19:11:50 +0900

    feat: 유저의 금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const { getPurchaseAmount } = require("./UserInput.js");
+const { printUserPurchaseAmount } = require("./Output.js");
+const { Print } = require("./lib/MissionUtils.js");
 class App {
-  play() {}
+  $purchaseAmount;
+  $lottos = [];
+  $winNum = [];
+  async play() {
+    await this.savePurchaseAmount();
+  }
+  async savePurchaseAmount() {
+    let $purchaseAmount = await getPurchaseAmount().then((data) => data);
+    printUserPurchaseAmount($purchaseAmount);
+  }
 }
+
+let app = new App();
+app.play();
 
 module.exports = App;
