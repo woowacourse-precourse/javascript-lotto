2a48e1bd6c87ec0edecc513becbd14043980290d
jinhobyeong <jhb_0921@naver.com>
2022-11-14 23:51:43 +0900

    feat : 총 이익 액수 계산 함수 추가

--- src/LottoGenerator.js
+++ src/LottoGenerator.js
@@ -16,8 +16,18 @@ const calculateProfit = (profit, inputMoney) => {
   return ((profit / inputMoney) * 100).toFixed(1);
 };
 
+const estimateProfit = (scoreBoard, inputMoney) => {
+  let totalProfit = 0;
+  const RANK_MONEY = [2000000000, 30000000, 1500000, 50000, 5000];
+  scoreBoard.forEach((eachScore, index) => {
+    totalProfit += RANK_MONEY[index] * eachScore;
+  });
+  return calculateProfit(totalProfit, inputMoney);
+};
+
 module.exports = {
   getLottoNumber,
   makeLottoArray,
   calculateProfit,
+  estimateProfit,
 };
