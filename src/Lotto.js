278e50ba661fed79bcb271109a65dd68335cbbab
sujungYu <su_moment@naver.com>
2022-11-14 15:24:42 +0900

    feat(Lotto) : 로또 번호 예외 처리 항목 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,3 @@
-// const MissionUtils = require('@woowacourse/mission-utils');
 class Lotto {
   #numbers;
 
@@ -8,9 +7,13 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-    }
+    const testType = /[0-9]/;
+    if (numbers.length !== 6) throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    if (numbers.length !== new Set(numbers).size) throw new Error('[ERROR] 중복되는 숫자가 있습니다.');
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) throw new Error('[ERROR] 1~45 사이 숫자여야 합니다.');
+      if (!testType.test(numbers)) throw new Error('[ERROR] 숫자여야 합니다.');
+    });
     return numbers;
   }
 
