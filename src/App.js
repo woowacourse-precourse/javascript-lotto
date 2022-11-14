3b74d1dd9b33caa43b60046f81c6c79bb47c70ee
foundaway98 <foundaway98@naver.com>
2022-11-14 16:31:04 +0900

    <feat> : 로또 구매 기능 구현
    App.js 파일에서 로또 구매 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const LottoBuyer = require("./LottoBuyer.js");
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  lottoBuyer;
+
+  play() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
+      this.lottoBuyer = new LottoBuyer(money);
+      this.lottoBuyer.buyLotto();
+    });
+  }
 }
 
 module.exports = App;
