e28e1782d72c067185580485f2bf5ddde6938316
kyeongwxx <kyeongwxx@gmail.com>
2022-11-14 23:43:35 +0900

    feat(validation): 생성한 로또 번호 유효성 검사 기능 구현
    잘못된 로또 번호를 출력할 경우 throw문을 사용해 예외를 발생시키고,"[ERROR]"로 시작하는 에러 메시지를 출력 후 애플리케이션 종료
    
    1에서 45까지의 숫자
    중복되지 않는 서로 다른 숫자

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,8 @@
 const ERROR = {
   INVALID_MONEY_INPUT: "[ERROR] 구입금액은 1,000원 단위입니다.",
+  INVALID_NUMBERS_OUTPUT_LENGTH: "[ERROR] 로또 번호는 6개여야 합니다.",
+  INVALID_NUMBERS_OUTPUT_RANGE: "[ERROR] 로또 번호는 1에서 45까지의 숫자입니다.",
+  INVALID_NUMBERS_OUTPUT_UNIQUE: "[ERROR] 로또 번호는 중복되지 않은 숫자입니다."
 };
 
 exports.ERROR = ERROR;
--- src/validation/validation.js
+++ src/validation/validation.js
@@ -4,6 +4,14 @@ const validate = {
   moneyInput: (input) => {
     if (input % 1000 !== 0) throw new Error(ERROR.INVALID_MONEY_INPUT);
   },
+
+  numbersOutput: (numbers) => {
+    if (numbers.length !== 6) throw new Error(ERROR.INVALID_NUMBERS_OUTPUT_LENGTH);
+    if (numbers.map((el) => Number(el)).every((el) => el >= 1 && el <= 45))
+      throw new Error(ERROR.INVALID_NUMBERS_OUTPUT_RANGE);
+    if ([...new Set(numbers)].length !== 6)
+      throw new Error(ERROR.INVALID_NUMBERS_OUTPUT_UNIQUE);
+  },
 };
 
 module.exports = validate;
