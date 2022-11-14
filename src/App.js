799b3f7d132eb86c4a8966cd89d5b09ced94f51f
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-11-15 07:27:54 +0900

    feat: 사용자로부터 당첨 번호를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const LottoManager = require('./LottoManager');
 class App {
   constructor() {
     this.lottoManager = new LottoManager();
+    this.winningNumbers = [];
   }
 
   play() {
@@ -14,6 +15,7 @@ class App {
     Console.readLine('구입금액을 입력해 주세요.\n', (purchaseAmount) => {
       const lottoArray = this.lottoManager.issueLotto(purchaseAmount);
       this.printLotto(lottoArray);
+      this.inputWinningNumbers();
     });
   }
 
@@ -22,6 +24,45 @@ class App {
     lottoArray.forEach((lotto) =>
       Console.print(`[${lotto.getNumbers().join(', ')}]`)
     );
+  }
+
+  inputWinningNumbers() {
+    Console.readLine('당첨 번호를 입력해 주세요.\n', (numbers) => {
+      const splittedNumbers = numbers.split(',');
+      this.validateWinningNumbers(splittedNumbers);
+      this.winningNumbers = splittedNumbers;
+    });
+  }
+
+  validateWinningNumbers(numbers) {
+    if (!this.isLength(numbers)) {
+      throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
+    }
+    if (!this.isAllNumber(numbers)) {
+      throw new Error('[ERROR] 당첨 번호는 숫자여야 합니다.');
+    }
+    if (!this.isAllRange(numbers)) {
+      throw new Error('[ERROR] 당첨 번호는 1이상 45이하의 정수여야 합니다.');
+    }
+    if (!this.isUnique(numbers)) {
+      throw new Error('[ERROR] 당첨 번호는 중복된 숫자가 없어야 합니다.');
+    }
+  }
+
+  isLength(numbers) {
+    return numbers.length === 6;
+  }
+
+  isAllNumber(numbers) {
+    return numbers.every((number) => number.match(/^[0-9]+$/));
+  }
+
+  isAllRange(numbers) {
+    return numbers.every((number) => number >= 1 && number <= 45);
+  }
+
+  isUnique(numbers) {
+    return numbers.length === new Set(numbers).size;
   }
 }
 
