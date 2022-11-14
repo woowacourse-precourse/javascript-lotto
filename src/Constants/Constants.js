d3fbe1f54bafb1baf6b61f45774de318c91ce964
osdoonhyun <skw1996@naver.com>
2022-11-14 23:00:22 +0900

    feat(Constants): 당첨 번호 메세지 & 당첨 번호 예외 메세지 추가

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -5,6 +5,9 @@ const MESSAGES = {
 
 const ERROR_MESSAGES = {
   INVALID_AMOUNT_UNIT: "[ERROR] 구입 금액은 1000원 단위여야 합니다.",
+  INVALID_NUMBER: "[ERROR] 로또 번호는 6개여야 합니다.",
+  INVALID_RANGE: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+  DUPLICATION_NUMBERS: "[ERROR] 로또 번호는 중복이 없어야 합니다.",
 };
 
 const LOTTO = {
