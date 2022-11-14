14637244c416ce178e579edfb4341dca16b3465b
OrangeVinyl <hap_hazard@naver.com>
2022-11-15 03:50:33 +0900

    refactor: 함수 재정리 및 상수 통합

---
+++ src/Contants.js
@@ -0,0 +1,12 @@
+const CONSTANT = {
+  ERROR: {
+    NOT_THOUSAND_ERROR: `[ERROR] 결제 금액은 1000원 단위로 입력해주셔야 합니다.`,
+    NOT_NUMBER_ERROR: `[ERROR] 결제 금액은 숫자만 입력해주셔야 합니다.`,
+  },
+  MESSAGE: {
+    INPUT_MONEY: '구입금액을 입력해 주세요.',
+    PRINT_COUNTLOTTO: '개를 구매했습니다.',
+  },
+};
+
+module.exports = CONSTANT;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,53 +1,46 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
+const { ERROR, MESSAGE } = require('./Contants.js');
 
 class Lotto {
   #countLotto;
 
-  thorwError(message) {
-    throw new Error(message);
-  }
+  validate(price) {
+    this.isNumber(price);
+    this.isThousandUnit(price);
+  };
 
-  validate(inputMoney) {
-    if (
-      this.isBlank(inputMoney) ||
-      !this.isNumber(inputMoney) ||
-      !this.isThousandUnit(inputMoney)
-    ) {
-      return this.thorwError(
-        '[ERROR] 일단은 통합한 오류 입력금액을 다시 적으세요.'
-      );
+  isNumber(price) {
+    if(isNaN(price)) {
+        throw new Error(ERROR.NOT_NUMBER_ERROR)
     }
-    return true;
-  }
+  };
 
-  isBlank(input) {
-    return !input;
-  }
-
-  isNumber(input) {
-    return !isNaN(input); //
-  }
-
-  isThousandUnit(input) {
-    return input % 1000 === 0;
-  }
+  isThousandUnit(price) {
+    if(price % 1000 !== 0) {
+        throw new Error(ERROR.NOT_THOUSAND_ERROR)
+    }
+  };
 
   inputMoney() {
-    Console.readLine('구입금액을 입력해 주세요.\n', (inputMoney) => {
+    Console.readLine('구입금액을 입력해 주세요.\n', inputMoney => {
       this.validate(inputMoney);
-      this.#countLotto = Number(inputMoney) / 1000;
-      this.printCountLotto(this.#countLotto);
-      this.buyLotto();
-      Console.close();
+      this.buyLotto(inputMoney);
     });
   }
 
-  printCountLotto(inputCount) {
-    Console.print(`\n${inputCount}개를 구매했습니다.`);
+  buyLotto(inputMoney) {
+    this.#countLotto = Number(inputMoney) / 1000
+    this.printCountLotto(this.#countLotto);
+    this.makeLotto();
+    Console.close();
   }
 
-  buyLotto() {
-    for(let i = 0; i < this.#countLotto; i++) {
+  printCountLotto(inputCount) {
+    Console.print(`\n${inputCount}`+ MESSAGE.PRINT_COUNTLOTTO);
+  }
+
+  makeLotto() {
+    for (let i = 0; i < this.#countLotto; i++) {
       let lottoNumber = this.ascendingLottoArray(this.makeRandomLottoNumber());
       this.printRandomLottoNumber(lottoNumber);
     }
@@ -60,12 +53,10 @@ class Lotto {
   ascendingLottoArray(lottoNumber) {
     return lottoNumber.sort((a, b) => a - b);
   }
-  
-  printRandomLottoNumber(lottoNumber) {
-    return Console.print(`[${lottoNumber.join(', ')}]`)
-  }
 
-  
+  printRandomLottoNumber(lottoNumber) {
+    return Console.print(`[${lottoNumber.join(', ')}]`);
+  }
 
   play() {
     this.inputMoney();
