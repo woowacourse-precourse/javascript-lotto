11c954bea95eff4a110587c871357ac9329df57a
saetakki <lime.lemonade03@gmail.com>
2022-11-15 04:22:50 +0900

    refactor. Change all validation functions

--- src/Validator.js
+++ src/Validator.js
@@ -3,34 +3,26 @@ const Console = MissionUtils.Console;
 
 class Validator {
   static isNumber(target) {
-    const regExp = /^[0-9]+$/;
-    target.filter(x => {
-      if (!regExp.test(x)) throw new Error('[ERROR] 숫자를 입력해주세요.');
-    });
+    if (isNaN(target)) throw new Error('[ERROR] 숫자가 아닙니다.');
     return true;
   }
 
   static isValidPayAmount(cost) {
-    if (this.isNumber(cost) && cost >= 1000) {
-      return true;
-    }
+    if (cost >= 1000) return true;
     throw new Error('[ERROR] 1000원 이상의 금액을 입력해주세요.');
   }
 
-  static isNotDuplicated(list, len) {
+  static isNotDuplicated(list, len = 6) {
     const set = new Set(list);
     if (set.size !== len) {
       throw new Error('[ERROR] 중복된 숫자가 있습니다.');
     }
     return true;
   }
-
-  static isEnough(list, len) {
-    if (list.length !== len) {
-      throw new Error(
-        `[ERROR] ${len}개의 숫자가 필요합니다. 현재 입력된 갯수: ${list.length}`,
-      );
-    }
+  static isValidLottoNumber(number) {
+    if (number < 1 || number > 45)
+      throw new Error('[ERROR] 1~45 사이의 숫자를 입력하세요.');
     return true;
   }
 }
+module.exports = Validator;
