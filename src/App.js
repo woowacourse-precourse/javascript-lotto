abf322af782d8534028872bed959b1f593ac1c58
Sallyvely <tpdus4159@naver.com>
2022-11-14 14:48:03 +0900

    feat : 구매한 로또 갯수를 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Calculator = require('./components/Calculator.js');
+const Input = require('./components/Input.js');
+
 class App {
-  play() {}
+  play () {
+    const cal = new Calculator();
+    const input = new Input();
+
+    const lottocount = this.printLottoCount();
+    const lottoArr = cal.createLottoNumber(lottocount);
+
+    const winNumber = (input.inputWinNumber()).split(',').map(Number);
+    const bonusNumber = input.inputBonusNumber();
+
+    const lottoresult = cal.calWinning(lottoArr, winNumber);
+    const winningArr = cal.winningScore(lottoresult, lottoArr, bonusNumber);
+
+  }
+
+
+
+  printLottoCount () {
+    const input = new Input();
+    const lottocount = input.inputMoneyCount();
+    MissionUtils.Console.print(`${lottocount}개를 구매했습니다.`);
+    return lottocount;
+  }
 }
 
 module.exports = App;
