cb243074ba1e628b2e06afa348a9498141dfc955
HWI <dlwognl99@naver.com>
2022-11-15 01:29:58 +0900

    feat: 로또 번호에 입력된 보너스 번호가 포함되어 있는지 확인하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -83,10 +83,15 @@ class App {
     );
     const eachResult = singleLotto.map(this.compareNumber.bind(this));
     console.log(eachResult);
+    const bonusNumberStatus = singleLotto.map(this.hasBonusNumber.bind(this));
+    console.log(bonusNumberStatus);
   }
   compareNumber(eachLotto) {
     return eachLotto.filter((number) => this.winnerNumber.includes(number))
       .length;
+  }
+  hasBonusNumber(eachLotto) {
+    return eachLotto.includes(this.bonusNumber);
   }
 }
 
