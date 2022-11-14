646c2695b7af203d3147cbe1300011703eaa0f0f
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-15 01:56:14 +0900

    test: Lotto 클래스 테스트 코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,30 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또 번호에 1~45 이외의 수가 포함되어 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([0, 1, 2, 3, 4, 555]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("countMatch 메서드 테스트", () => {
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const lotto = new Lotto([10, 20, 30, 40, 5, 6]);
+
+    expect(lotto.countMatch(winningNumbers)).toBe(2);
+  });
+
+  test("hasBonusNumber 메서드 테스트", () => {
+    const bonusNumber = 7;
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.hasBonusNumber(bonusNumber)).toBe(false);
+  });
+
+  test("hasBonusNumber 메서드 테스트2", () => {
+    const bonusNumber = 1;
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.hasBonusNumber(bonusNumber)).toBe(true);
+  });
 });
