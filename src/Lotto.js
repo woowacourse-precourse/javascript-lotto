d5547ca81b9b62fd1cc77ab1340d74bcf18ab635
su-lim <sujin.0xlim@gmail.com>
2022-11-14 20:05:21 +0900

    feat(validate): 로또 번호 유효성 체크 메서드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,9 +7,23 @@ class Lotto {
     this.#numbers = this.sortNumber(numbers);
   }
   validate(numbers) {
+    const setNumbers = new Set(numbers);
+    const validReg = /[0-9]/;
+
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (numbers.length !== setNumbers.size) {
+      throw new Error("[ERROR] 중복된 번호가 있습니다.");
+    }
+    numbers.map((number) => {
+      if (!validReg.test(number)) {
+        throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+      }
+      if (!(number >= 1 && number <= 45)) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    });
   }
 
   sortNumber(numbers) {
