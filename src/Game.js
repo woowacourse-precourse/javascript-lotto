467b83f338239ca9a15b62b5e2ab5c8edf74d435
안호진 <asdv94@naver.com>
2022-11-14 22:47:09 +0900

    feat(lotto): Game 클래스 필요 상태 초기화

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,10 @@
 const { ERROR_MSG } = require('./Constant');
 
 class Game {
+  constructor() {
+    this.lottos = null;
+    this.cost = 0;
+  }
   static validate(money) {
     if (money % 1000 > 0)
       throw new Error(ERROR_MSG.prefix + ERROR_MSG.only1000WonUnits);
