69fa3263ed503b47cfa5b414a55b32cadf49647b
h2002y <h2002y@naver.com>
2022-11-14 18:18:04 +0900

    feat: Add printBuyLotto func for printing user lotto

--- src/App.js
+++ src/App.js
@@ -33,11 +33,16 @@ class App {
   buyLotto() {
     for(let i = 0; i < this.#numOfLotto; i++) {
       let tmp = Random.pickUniqueNumbersInRange(1, 45, 6);
-      Console.print(tmp);
       tmp = tmp.sort((a,b) => a - b);
       this.#userLotto.push(tmp);
     }
-    printBuyLotto();
+    this.printBuyLotto();
+  }
+
+  printBuyLotto() {
+    Console.print(`\n${this.#numOfLotto}개를 구매했습니다.`);
+    this.#userLotto.forEach((lotto) => Console.print(`[${lotto.join(", ")}]`));
+    getWinLotto();
   }
 }
 
