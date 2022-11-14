667cac4d30c0cd96347965fd3566f34a66d3188b
areumsheep <areumsheep@gmail.com>
2022-11-14 18:55:46 +0900

    add: 당첨 번호 6자리 입력 및 예외 처리 추가

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ const { MESSAGES, ERROR_MESSAGES } = require('./constants/index');
 const LOTTO_PRICE = 1000;
 class App {
   #buyLotto = [];
+  #winningLotto = [];
 
   play() {
     Console.readLine(MESSAGES.INPUT_MONEY, (number) => {
       if (number % LOTTO_PRICE !== 0)
         throw new Error(ERROR_MESSAGES.INVALID_MONEY_UNIT);
+
       this.buy(Number(number / LOTTO_PRICE));
     });
   }
@@ -22,12 +24,34 @@ class App {
       this.#buyLotto.push(randomLotto);
       Console.print(randomLotto);
     }
+    this.lottoInput();
   }
 
   randomLotto() {
     const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
     if (!randomNumbers) throw new Error(ERROR_MESSAGES.EMPTY_NUMBERS);
     return randomNumbers.sort((a, b) => a - b);
+  }
+
+  checkValidLotto(numbers = []) {
+    const numberSet = new Set(numbers);
+
+    if (numbers.length !== 6)
+      throw new Error(ERROR_MESSAGES.INVALID_LOTTO_LENGTH);
+    if (numbers.find((number) => number < 1 || number > 45))
+      throw new Error(ERROR_MESSAGES.INVALID_LOTTO_RANGE);
+    if (numberSet.size !== 6)
+      throw new Error(ERROR_MESSAGES.NOT_DUPLICATE_NUMBER);
+  }
+
+  lottoInput() {
+    Console.readLine(MESSAGES.INPUT_LOTTO_NUMBERS, (numbers) => {
+      const splittedNumbers = numbers.split(',');
+      this.checkValidLotto(splittedNumbers);
+
+      this.#winningLotto = splittedNumbers;
+      this.bonusInput();
+    });
   }
 }
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -12,9 +12,9 @@ const ERROR_MESSAGES = Object.freeze({
 
 const MESSAGES = Object.freeze({
   INPUT_MONEY: '구입금액을 입력해 주세요.\n',
-  BUY_COUNT: (count) => `${count}개를 구매했습니다.\n`,
-  INPUT_LOTTO_NUMBERS: '당첨 번호를 입력해 주세요.\n',
-  INPUT_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
+  BUY_COUNT: (count) => `\n${count}개를 구매했습니다.`,
+  INPUT_LOTTO_NUMBERS: '\n당첨 번호를 입력해 주세요.\n',
+  INPUT_BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
   WINNING_STATS: (
     [correct3, correct4, correct5, correct5Bonus, correct6],
     rate
