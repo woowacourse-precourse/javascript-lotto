a252a6de1bcfee970ab017e72e3d3f525e3bbf89
jihong <rlawyd9@kookmin.ac.kr>
2022-11-15 03:28:44 +0900

    feat: 구입한 로또 개수 출력하기

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ class App {
     return 1;
   }
 
+  print_lotto_cnt(lotto_cnt){
+    MissionUtils.Console.print(`${lotto_cnt}개를 구매했습니다.`);
+  }
 
   inputMoney(){
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', async(money) => {
       if(await this.checkInput(money)){
         const lotto_cnt = Math.floor(money/1000);
+        this.print_lotto_cnt(lotto_cnt);
       }
       await MissionUtils.Console.close();
 
