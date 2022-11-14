d52bddca5f95acdbc9cfdaba275d5cfbae561975
adultlee <seoungin1228@gmail.com>
2022-11-15 00:51:24 +0900

    feat: Lotto 생성
    입력받은 lottoCount 만큼 Lotto를 생성합니다.

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -4,6 +4,7 @@ class LottoGame {
   constructor() {
     this.lottoCount = 0;
     this.purchasePrice = 0;
+    this.lottos = [];
   }
 
   start() {
@@ -12,8 +13,19 @@ class LottoGame {
       (purchasePrice) => {
         this.purchasePrice = purchasePrice;
         this.lottoCount = purchasePrice / 1000;
+        this.purchaseLotto();
       }
     );
+  }
+
+  purchaseLotto() {
+    MissionUtils.Console.print(this.lottoCount + "개를 구매했습니다.");
+    for (let i = 0; i < this.lottoCount; i++) {
+      const Lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 20, 6);
+      this.lottos.push(Lotto);
+      MissionUtils.Console.print(Lotto);
+    }
+    MissionUtils.Console.print(this.lottos);
   }
 }
 
