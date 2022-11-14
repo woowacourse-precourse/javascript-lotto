ce76c334df5c51e63b7a16da217044b66d878a98
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-11-14 10:57:56 +0900

    Feat: 로또 입력 logic 구현
    1. 로또구입금액 입력
    2. 당첨번호 입력
    3. 보너스번호 입력

--- src/Lotto.js
+++ src/Lotto.js
@@ -21,7 +21,26 @@ class Lotto {
   }
 
   randomBonusNumber() {
-    
+    // const bonusNum = Random.
+  }
+
+  //로또 입력logic
+  inputMoney() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+      //8개 구매했습니다. 8개 당첨로또 로직
+    })
+  }
+
+  inputLotto() {
+    Console.readLine('당첨 번호를 입력해 주세요.\n', (lotto) => {
+  
+    })
+  }
+
+  inputBonus() {
+    Console.readLine('보너스 번호를 입력해 주세요.\n', (bonus) => {
+      
+    })
   }
 }
 
