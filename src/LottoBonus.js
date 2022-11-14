50945c70e794df8a17fcefd58313c2d611cae12b
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-15 01:08:25 +0900

    refactor: 입력한 로또 번호가 숫자인지 체크하는 중복 기능 제거

--- src/LottoBonus.js
+++ src/LottoBonus.js
@@ -8,21 +8,12 @@ class LottoBonus {
 
   validate(numbers) {
     this.validateNumberInRange(numbers);
-    this.validateNotNumber(numbers);
   }
 
   validateNumberInRange(numbers) {
     for (let i = 0; i < numbers.length; i++) {
       if (numbers[i] < 1 || numbers[i] > 45) {
         throw new Error('[ERROR] 보너스 번호는 숫자 1 ~ 45 범위 내에서 입력하세요.');
-      }
-    }
-  }
-
-  validateNotNumber(numbers) {
-    for (let i = 0; i < numbers.length; i++) {
-      if (isNaN(...winNumber[i])) {
-        throw new Error('[ERROR] 보너스 번호는 숫자만 입력해야 합니다.');
       }
     }
   }
