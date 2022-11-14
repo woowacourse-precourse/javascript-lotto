2a75f3e9d5e31075c7d9073e05ee116b50d4b392
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-14 23:49:12 +0900

    feat: 자동 번호 출력 함수

--- src/view/View.js
+++ src/view/View.js
@@ -3,10 +3,15 @@ const { Console } = require("@woowacourse/mission-utils");
 class View {
   constructor() {
     this.printLottoCount = this.printLottoCount.bind(this);
+    this.printQuickPick = this.printQuickPick.bind(this);
   }
 
   printLottoCount(lottoCount) {
     Console.print(`\n${lottoCount}개를 구매했습니다.`);
+  }
+
+  printQuickPick(quickPick) {
+    Console.print(this.serialize(quickPick));
   }
 }
 
