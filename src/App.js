80687d4a6ba0c242ef0cb366887652bb1d47c730
jihong <rlawyd9@kookmin.ac.kr>
2022-11-15 03:26:33 +0900

    feat: 로또 구입금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  
+  checkInput(money){
+    if(isNaN(money)){
+      throw "[ERROR] 구입금액은 숫자로만 이루어져야 합니다."
+    }
+    return 1;
+  }
+
+
+  inputMoney(){
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.', async(money) => {
+      if(await this.checkInput(money)){
+        const lotto_cnt = Math.floor(money/1000);
+      }
+      await MissionUtils.Console.close();
+
+    });
+  }
+  
+  play() {
+    this.inputMoney();
+  }
 }
 
 module.exports = App;
