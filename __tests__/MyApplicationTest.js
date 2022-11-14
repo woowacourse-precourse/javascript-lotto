c17318e8a14d8170a8c352dd9dc63adc2470f954
confidential-nt <youyoy689@gmail.com>
2022-11-15 03:51:39 +0900

    test($App): issueLotto 메소드 테스트
    사용자가 원하는 수 만큼의 로또를 발행하는지 테스트하는 코드를 작성하였다.

--- __tests__/MyApplicationTest.js
+++ __tests__/MyApplicationTest.js
@@ -4,7 +4,10 @@ describe("App 클래스 추가 테스트", () => {
   test("사용자가 입력한 구입금액에 숫자가 아닌 값이 있을 경우 예외가 발생한다.", () => {
     expect(() => {
       new App().isValidate("rsfa");
-      console.log("Zz");
     }).toThrow("[ERROR]");
+  });
+
+  test("사용자가 원하는 수 만큼의 로또를 발행해야한다.", () => {
+    expect(new App().issueLotto(3).length).toBe(3);
   });
 });
