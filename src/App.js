48c044c8a2a7607223032525ea9d2e6c169b19ed
shinhyunsu01 <makelabel@naver.com>
2022-11-15 03:08:49 +0900

    feat: 구매한 로또 랜덤 생성

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ class App {
 		this.state = {
 			money: 0,
 			buyLotto: 0,
+			publishLotto: [],
 		};
 	}
 	play() {
 		this.inputMoney();
+		this.publishLotto();
 	}
 
 	inputMoney() {
@@ -23,6 +25,23 @@ class App {
 			this.valid.inputMoney(this.state.money);
 			this.state.buyLotto = this.state.money / 1000;
 		});
+	}
+	publishLotto() {
+		Console.print(`${this.state.buyLotto + MESSAGE.PRINT_BUY}`);
+		this.state.publishLotto = this.makeLotto(this.state.buyLotto);
+
+		this.state.publishLotto.forEach((ele) => {
+			Console.print(`[${String(ele).replaceAll(",", ", ")}]`);
+		});
+	}
+	makeLotto(num) {
+		let publishLottoArr = [];
+		for (let i = 0; i < num; i++) {
+			let numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+			numbers = numbers.sort((a, b) => a - b);
+			publishLottoArr.push(numbers);
+		}
+		return publishLottoArr;
 	}
 }
 
