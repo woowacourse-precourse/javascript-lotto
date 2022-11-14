fa084cf61ecc84909fef9971a1556eb520a763ef
thewronghand <penfreak77@gmail.com>
2022-11-15 01:13:34 +0900

    test: 로또 도메인 로직 단위 테스트 - 당첨 번호 및 보너스 번호 입력 시 당첨 통계 출력 테스트 구현
    로또 도메인 로직 중 당첨 번호와 보너스 번호를 입력하면 구매한 모든 로또의 당첨 결과를 확인하여 통계를 출력하는 로직을 테스트한다.

--- __tests__/LottoIntegrationTest.js
+++ __tests__/LottoIntegrationTest.js
@@ -1,9 +1,5 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const App = require('../src/App');
-const LottoPublisher = require('../src/LottoPublisher.js');
-const LottoStore = require('../src/LottoStore.js');
-const LottoViewer = require('../src/LottoViewer.js');
-const Customer = require('../src/Customer.js');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -54,6 +50,45 @@ describe('로또 integration 테스트', () => {
       '[1, 7, 22, 24, 32, 44]',
       '[1, 7, 12, 16, 21, 43]',
       '[8, 14, 18, 20, 37, 42]'
+    ];
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test('사용자가 당첨 번호와 보너스 번호를 입력 시, 구매한 로또의 당첨 통계를 출력한다.', () => {
+    mockRandoms([
+      [3, 5, 13, 14, 37, 45],
+      [9, 11, 24, 29, 37, 45],
+      [4, 5, 6, 19, 26, 36],
+      [3, 15, 16, 20, 22, 44],
+      [14, 15, 22, 29, 35, 42],
+      [6, 10, 39, 43, 44, 45],
+      [1, 4, 26, 29, 34, 43],
+      [14, 22, 27, 35, 38, 44]
+    ]);
+    mockQuestions(['8000', '3,5,37,45,35,43', '44']);
+    const logs = [
+      '8개를 구매했습니다.',
+      '[3, 5, 13, 14, 37, 45]',
+      '[9, 11, 24, 29, 37, 45]',
+      '[4, 5, 6, 19, 26, 36]',
+      '[3, 15, 16, 20, 22, 44]',
+      '[14, 15, 22, 29, 35, 42]',
+      '[6, 10, 39, 43, 44, 45]',
+      '[1, 4, 26, 29, 34, 43]',
+      '[14, 22, 27, 35, 38, 44]',
+      '당첨 통계',
+      '---',
+      '3개 일치 (5,000원) - 0개',
+      '4개 일치 (50,000원) - 1개',
+      '5개 일치 (1,500,000원) - 0개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
+      '6개 일치 (2,000,000,000원) - 0개',
+      '총 수익률은 625.0%입니다.'
     ];
     const logSpy = getLogSpy();
     const app = new App();
