aba91008a86b4b04339f06490e8b378c2e7c33c1
JISUN HAN <my__memoria@naver.com>
2022-11-14 15:54:39 +0900

    feat: 메세지 및 규칙 상수화

---
+++ src/constants/index.js
@@ -0,0 +1,39 @@
+const RULES = Object.freeze({
+  START_RANGE: 1,
+  END_RANGE: 45,
+  LENGTH: 6,
+  PURCHASE_UNIT: 1000,
+});
+
+const PRIZE = Object.freeze({
+  MONEY: [2000000000, 30000000, 1500000, 50000, 5000],
+});
+
+const CONSOLE = Object.freeze({
+  PURCHASE_MONEY_INPUT: "구입금액을 입력해 주세요.",
+  WINNING_NUMBER_INPUT: "당첨 번호를 입력해 주세요.",
+  BONUS_NUMBER_INPUT: "보너스 번호를 입력해 주세요.",
+  RESULT: "당첨 통계\n---",
+});
+
+const RESULT = {
+  PURCHASE_CHECK: (num) => `${String(num)}개를 구매했습니다.`,
+  FIFTH_PLACE: (num) => `3개 일치 (5,000원) - ${String(num)}개`,
+  FOURTH_PLACE: (num) => `4개 일치 (50,000원) - ${String(num)}개`,
+  THIRD_PLACE: (num) => `5개 일치 (1,500,000원) - ${String(num)}개`,
+  SECOND_PLACE: (num) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${String(num)}개`,
+  FIRST_PLACE: (num) => `6개 일치 (2,000,000,000원) - ${String(num)}개`,
+  TOTAL_PER: (percent) => `총 수익률은 ${String(percent)}%입니다.`,
+};
+
+const ERROR = Object.freeze({
+  BUY_CHECK: "[ERROR] 구매 갯수는 정수로만 입력해주세요",
+  INPUT_UNIT_CHECK: "[ERROR] 구매 금액은 1,000원 단위로 입력해주세요.",
+  SEPARATOR_CHECK: "[ERROR] 당첨번호는 쉼표를 사용하여 구분해주세요.",
+  NUMBER_RANGE_CHECK: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+  INPUT_CNT_CHECK: "[ERROR] 숫자는 6개만 입력해주세요.",
+  BONUS_CHECK: "[ERROR] 1개의 숫자만 입력해주세요.",
+  DUPLICATE_CHECK: "[ERROR] 중복되지 않는 수들로 작성해주세요.",
+});
+
+module.exports = { RULES, PRIZE, CONSOLE, RESULT, ERROR };
