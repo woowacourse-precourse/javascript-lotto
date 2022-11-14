fbf9ff9c183c50844a4da75d014b440c079e0841
jincde <asdzxc@kakao.com>
2022-11-14 23:53:39 +0900

    feat : 로또 번호 입력 문구

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto.js")
+const Messages = require("./Messages.js")
 
 class App {
-
+  constructor() {
+    this.Lotto() = new Lotto()
+  }
   play() {
+    MissionUtils.Console.readLine(Messages.INPUT_MONEY, (money) => {
+      console.log(money)
+    });
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/Messages.js
@@ -0,0 +1,5 @@
+const MESSAGE = {
+  INPUT_MONEY: "구입금액을 입력하세요 : ",
+};
+
+module.exports = MESSAGE;
