a95a2a9d794efa60b741d11aeb34d1e5587ac659
Hungeun2 <bepoohun@gmail.com>
2022-11-14 15:07:11 +0900

    Feat: App.js Budget Class import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-class App {
-  play() {}
-}
+const { Console } = require("@woowacourse/mission-utils");
+const { LOTTO_MESSAGE } = require("./util/Constant");
 
+const Budget = require("./Budget");
+
+class App {
+  play() {
+    this.lottoMachineOn();
+  }
+  lottoMachineOn() {
+    Console.readLine(LOTTO_MESSAGE.BUDGET, (input) => {
+      this.setBudget(input);
+    });
+  }
+  setBudget(money) {
+    const budget = new Budget(money);
+  }
+}
 module.exports = App;
