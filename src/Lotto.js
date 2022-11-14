aee46416d8be14767f032b5938c8662d72782ef2
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-14 21:07:54 +0900

    feat(Lotto) : 로또의 랜덤한 수 유효성 판단
    - 개수가 6개인지 확인
    - 범위(1-45)사이의 숫자인지 확인

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const { RANGE } = require("./Constant");
+
 class Lotto {
   #numbers;
 
@@ -10,9 +12,18 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (!this.isNumInRange(numbers)) {
+      throw new Error("[ERROR] 로또 번호가 1-45 사이 숫자가 아닙니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  isNumInRange(randomNumArr) {
+    for (let i = 0; i < randomNumArr.length; i++) {
+      if (randomNumArr[i] < RANGE.MIN && randomNumArr[i] > RANGE.MAX)
+        return false;
+    }
+    return true;
+  }
 }
 
 module.exports = Lotto;
