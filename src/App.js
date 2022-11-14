ebea4e2b17bc99f3dd8c5496ed9794ebfa469a36
catca <kdg96g@gmail.com>
2022-11-15 00:28:41 +0900

    feat: 로또 구입 금액을 입력하면 구입 금액에 해당하는 만큼 로또 발행

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 const MESSAGES = require('./Constants');
 
 class App {
-  constructor() {
-    this.payment = 0;
-  }
   play() {
-    Console.readline(MESSAGES.PAYMENT, (payment) => {
-      this.payment = payment;
+    Console.readLine(MESSAGES.PAYMENT, (payment) => {
+      this.checkPayment(payment);
+      Console.print(`${payment / 1000}개를 구매했습니다.`)
+      const lottoNumbers = this.getLottoNumbers(payment / 1000)
     });
-    if(payment % 1000 === 0){
+  }
+  checkPayment = (payment) => {
+    if(payment % 1000 !== 0){
       throw new Error(MESSAGES.ERROR.PAYMENT);
     }
   }
+  getLottoNumbers = (purchaseNumber) => {
+    const lottoNumbers = [];
+    for(let i = 0; i < purchaseNumber; i++){
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+      lottoNumbers.push(numbers);
+      Console.print(numbers);
+    } 
+    return lottoNumbers;
+  }
 }
 
-const app = App();
+const app = new App();
 app.play();
 
 module.exports = App;
