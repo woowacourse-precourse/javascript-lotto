5642f28deef7360623c08e6ecb6667db820b2bb4
soomin <qkrtnals970804@gmail.com>
2022-11-14 23:39:36 +0900

    feat: 구입 금액 1000원 단위로 나누어지는지 예외처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 const { print, readLine } = require('./utils');
-const LOTTO_PRICE = require('./var');
+const { LOTTO_PRICE } = require('./var');
 
 class App {
   play() {
@@ -8,9 +8,21 @@ class App {
 
   inputPrice(){
     readLine('구입금액을 입력해주세요.\n', (money) => {
+      this.checkInputPrice(money);
+    })
+  }
+
+  checkInputPrice(money){
+    if (isNaN(money)) {
+      throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
+    }
+    if (money < 1000) {
+      throw new Error('[ERROR] 1000원 이상의 단위를 입력해주세요.');
+    }
+    if (!isNaN(money) && money >= 1000) {
       const countLotto = Math.floor(Number(money) / LOTTO_PRICE);
       print(`${countLotto}개를 구매했습니다.\n`);
-    })
+    }
   }
 
 }
