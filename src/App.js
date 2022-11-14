bc191955d6affd56ac704b31b61b6fc330c1d802
hafnium1923 <hafnium1923@gmail.com>
2022-11-15 00:15:13 +0900

    style 요구사항 충족

--- src/App.js
+++ src/App.js
@@ -6,19 +6,15 @@ class App {
   }
   buyLotto() {
     MU.Console.readLine('구입금액을 입력해 주세요.\n',(money) => {
-      if(isNaN(money)) {
+      if(isNaN(money))
         throw new Error("[ERROR] 구입금액은 숫자여야 합니다.");
-      }
-      if(parseInt(money/1000) === 0) {
+      if(parseInt(money/1000) === 0)
         throw new Error("[ERROR] 최소 구입금액은 1000원입니다.");
-      }
-      if(parseInt(money%1000) != 0) {
+      if(parseInt(money%1000) != 0)
         throw new Error("[ERROR] 1000원 단위로 입력해주세요.");
-      }
       let purchaseNum = parseInt(money/1000);
       MU.Console.print(`${purchaseNum}개를 구매했습니다.`);
       let lottos = this.makeLottoNum(purchaseNum);
-      
       this.getWinNum(lottos);
     });
   }
@@ -32,51 +28,48 @@ class App {
   getBonusNum(lottos,win) {
     MU.Console.readLine('\n보너스 번호를 입력해 주세요.\n', (number) =>{
       let bonus = parseInt(number);
-      if(isNaN(number)) {
+      if(isNaN(number))
         throw new Error("[ERROR] 보너스 번호는 숫자여야 합니다.");
-      }
-      if(bonus < 1 || 45 < bonus) {
+      if(bonus < 1 || 45 < bonus)
         throw new Error("[ERROR] 로또 범위 안의 숫자를 입력해주세요.");
-      }
-      if(win.getNumber().includes(bonus)) {
+      if(win.getNumber().includes(bonus))
         throw new Error("[ERROR] 보너스 번호는 당첨번호와 중복될 수 없습니다.");
-      }
       let winNum = win.getNumber();
       this.winLotto(winNum,bonus,lottos);
       return MU.Console.close();
     });
   }
   winLotto(win,bonus,lottos) {
-    let rslt = Array.from({length: 6}, () => 0);
+    let result = Array.from({length: 6}, () => 0);
     for(let i = 0; i < lottos.length; i++) {
-      rslt[this.isSameNum(win,bonus,lottos[i])]++;
+      result[this.isSameNum(win,bonus,lottos[i])]++;
     }
-    this.printLottoResult(rslt);
-    this.calcRateOfReturn(rslt, lottos.length);
+    this.printLottoResult(result);
+    this.calcRateOfReturn(result, lottos.length);
   }
-  calcRateOfReturn(rslt, quantity){
+  calcRateOfReturn(result, quantity) {
     let money = 0;
-    money = rslt[1] * 2000000000 + 
-            rslt[2] * 30000000 +
-            rslt[3] * 1500000 +
-            rslt[4] * 50000 +
-            rslt[5] * 5000;
+    money = result[1] * 2000000000 + 
+            result[2] * 30000000 +
+            result[3] * 1500000 +
+            result[4] * 50000 +
+            result[5] * 5000;
     let rateOfReturn = Math.round(money / (quantity * 1000) * 1000)/10;
     MU.Console.print(`총 수익률은 ${rateOfReturn}%입니다.`);
   }
-  printLottoResult(rslt) {
-    MU.Console.print(`\n\n당첨 통계\n---\n3개 일치 (5,000원) - ${rslt[5]}개`);
-    MU.Console.print(`4개 일치 (50,000원) - ${rslt[4]}개`);
-    MU.Console.print(`5개 일치 (1,500,000원) - ${rslt[3]}개`);
-    MU.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${rslt[2]}개`);
-    MU.Console.print(`6개 일치 (2,000,000,000원) - ${rslt[1]}개`);   
+  printLottoResult(result) {
+    MU.Console.print(`\n\n당첨 통계\n---\n3개 일치 (5,000원) - ${result[5]}개`);
+    MU.Console.print(`4개 일치 (50,000원) - ${result[4]}개`);
+    MU.Console.print(`5개 일치 (1,500,000원) - ${result[3]}개`);
+    MU.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[2]}개`);
+    MU.Console.print(`6개 일치 (2,000,000,000원) - ${result[1]}개`);   
   }
   isSameNum(win, bonus, lotto) {
     let count = win.filter(x => lotto.includes(x)).length;
     if(count === 6) return 1;
     if(count === 5 && lotto.includes(bonus)) return 2;
     if(count === 5) return 3;
-    if(count === 4 || count === 3) return 8 -count;
+    if(count === 4 || count === 3) return 8 - count;
     return 0;
   }
   makeLottoNum(purchaseNum) {
@@ -99,6 +92,4 @@ class App {
   }
   
 }
-// const app = new App();
-// app.play();
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -7,21 +7,17 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
+    if (numbers.length !== 6)
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
     for(let i = 0; i < numbers.length; i++) {
-      if (isNaN(numbers[i])) {
+      if (isNaN(numbers[i]))
         throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
-      }
     }
-    if((new Set(numbers)).size != 6) {
+    if((new Set(numbers)).size != 6)
       throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
-    }
     for(let i = 0; i < numbers.length; i++) {
-      if(numbers[i] < 1 || 45 < numbers[i]) {
+      if(numbers[i] < 1 || 45 < numbers[i])
         throw new Error("[ERROR] 범위 안의 숫자가 아닙니다.");
-      }
     }
   }
 
