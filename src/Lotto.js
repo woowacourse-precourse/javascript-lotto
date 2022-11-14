fc7e49127344206b24855449464b3925956e8280
pione3r <jhyong0626@gmail.com>
2022-11-14 20:00:08 +0900

    feat: add compareLottos function and checkNumber function
    compareLottos function
    - compare numbers and return winning result
    
    checkNumber function
    - if lotto includes winning number increase count

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,11 +1,14 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-import {User} from "./User";
+import { User } from "./User";
 
 class Lotto {
   #numbers;
 
   constructor() {
     this.setWinningNumbers();
+    const bonusNumber = this.setBonusNumber();
+
+    this.compareLottos(bonusNumber);
   }
 
   validate(numbers) {
@@ -17,19 +20,52 @@ class Lotto {
   // TODO: 추가 기능 구현
   setWinningNumbers() {
     MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.\n", (answer) => {
-      const numbers = answer.split(",");
-      this.validate(numbers)
+      const numbers = answer.split(",").map((item) => Number(item));
+      this.validate(numbers);
       this.#numbers = numbers;
-    })
+    });
   }
 
   setBonusNumber() {
     let bonusNumber = 0;
-    MissionUtils.Console.readLine("보너스 번호를 입력해 주세요.\n", (answer) => {
-      bonusNumber = Number(answer);
-    })
+    MissionUtils.Console.readLine(
+      "보너스 번호를 입력해 주세요.\n",
+      (answer) => {
+        bonusNumber = Number(answer);
+      }
+    );
 
     return bonusNumber;
+  }
+
+  compareLottos(bonusNumber) {
+    const user = new User();
+
+    let winningNumbersObj = {
+      winningNumbersCount: 0,
+      winningBonusNumberCount: 0,
+    };
+
+    let winningResult = [];
+
+    for (let lotto of user.lottos) {
+      winningNumbersObj.winningNumbersCount = checkWinningNumber(lotto);
+      if (lotto.includes(bonusNumber))
+        winningNumbersObj.winningBonusNumberCount = 1;
+      winningResult.push(winningNumbersObj);
+    }
+
+    return winningResult;
+  }
+
+  checkWinningNumber(lotto) {
+    let count = 0;
+
+    for (let number of this.#numbers) {
+      if (lotto.includes(number)) count++;
+    }
+
+    return count;
   }
 }
 
