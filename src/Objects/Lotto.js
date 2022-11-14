98c090a83d4b358abab18f49174b30264f63ad9f
RooT_Kim <bdd3452@gmail.com>
2022-11-15 03:54:31 +0900

    수정: 당첨 번호 클래스 수정 Lotto 클래스 수정 당첨 번호로 전달되는 값이 유효한지 확인하는 validate 메소드 선언

---
+++ src/Objects/Lotto.js
@@ -0,0 +1,46 @@
+const { Messages } = require('../Constants');
+
+class Lotto {
+	#numbers;
+
+	constructor(numbers) {
+		this.validate(numbers);
+		this.#numbers = numbers;
+	}
+
+	validate(numbers) {
+		if (numbers.split(',').every(this.checkNumber)) {
+			throw new Error(Messages.ERROR.WINNING_NUMBERS.NOT_NUMBER);
+		}
+		if (numbers.split(',').every(this.checkBelongTo)) {
+			throw new Error(Messages.ERROR.WINNING_NUMBERS.NOT_BELONG_NUMBER);
+		}
+		if (this.checkDuplicate(numbers.split(','))) {
+			throw new Error(Messages.ERROR.WINNING_NUMBERS.DUPLICATED_NUMBER);
+		}
+		if (this.checkComma(numbers)) {
+			throw new Error(Messages.ERROR.WINNING_NUMBERS.NOT_INSERT_COMMA);
+		}
+		if (numbers.length !== 6) {
+			throw new Error(Messages.ERROR.WINNING_NUMBERS.DIFFERENT_LENGTH);
+		}
+	}
+
+	checkNumber(number) {
+		return typeof Number(number) === 'number';
+	}
+
+	checkBelongTo(number) {
+		return number >= 1 && number <= 45;
+	}
+
+	checkDuplicate(numbers) {
+		return [...new Set(numbers)].length === 6;
+	}
+
+	checkComma(numbers) {
+		return numbers.match(/,/g).length === 5;
+	}
+}
+
+module.exports = Lotto;
