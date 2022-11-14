1429ace2275d24b69b612f30388a2d7b26923855
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-14 22:43:56 +0900

    ✅ [test]: Lottos 클래스의 사용자가 구입한 로또를 생성하는 경우

--- __tests__/LottosTest.js
+++ __tests__/LottosTest.js
@@ -1,5 +1,5 @@
 const Lottos = require("../src/model/Lottos");
-const { ERROR } = require("../src/utils/constants");
+const { ERROR, UNIT } = require("../src/utils/constants");
 
 describe("Lottos 클래스 테스트", () => {
   test("구입 금액이 1,000원으로 나누어 떨어지지 않는 경우 예외가 발생한다.", () => {
@@ -12,5 +12,11 @@ describe("Lottos 클래스 테스트", () => {
     expect(() => {
       new Lottos("만원");
     }).toThrow(ERROR.ISNAN);
+  });
+
+  test("사용자가 구입한 장 수 만큼 로또가 생성되는 경우", () => {
+    const purchaseAmount = 5000;
+    const lottos = new Lottos(purchaseAmount);
+    expect(lottos.lottos.length).toEqual(purchaseAmount / UNIT.DIVIDE);
   });
 });
