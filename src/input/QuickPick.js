b5e774e9719ed1c574de9ef46314fbde76f94da2
DongWooKim97 <nanaukim@naver.com>
2022-11-14 21:58:40 +0900

    feat: 입력한 payment(구입금액)에 대한 예외처리 추가

--- src/input/QuickPick.js
+++ src/input/QuickPick.js
@@ -1,3 +1,5 @@
+const { ERROR_MESSAGES } = require("../utils/Constants");
+
 class QuickPick {
   #amount;
   #myLottoArray;
@@ -6,6 +8,20 @@ class QuickPick {
     this.validate(payment);
     this.countAmount(payment);
     this.pickRandomNumbers();
+  }
+
+  validate(payment) {
+    if (!Number(payment)) {
+      throw new Error(ERROR_MESSAGES.INVALID_COST_TYPE);
+    }
+
+    if (payment % 1000 !== 0) {
+      throw new Error(ERROR_MESSAGES.INVALID_COST_UNIT);
+    }
+
+    if (payment < 1000) {
+      throw new Error(ERROR_MESSAGES.INVALID_COST_MIN);
+    }
   }
 }
 
