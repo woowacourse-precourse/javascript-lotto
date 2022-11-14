4368f2ce27b159cb535d58099026eff3d80ac1b8
Rki0 <pky00823@gmail.com>
2022-11-14 13:43:17 +0900

    refactor(Lotto): add constants for static functions
    Since the functions in other classes were imported into Lotto,
    bring the constants used in other class.

--- src/constant/LottoConst.js
+++ src/constant/LottoConst.js
@@ -1,7 +1,9 @@
-const LottoConst = {
+const lottoConst = {
   ERROR_NOT_LENGTH_SIX: "[ERROR] 로또 번호는 6개여야 합니다.",
   ERROR_OUT_OF_RANGE: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
   ERROR_SAME_NUMBER_EXIST: "[ERROR] 로또 번호는 모두 다른 숫자여야 합니다.",
+  ERROR_NOT_NUMBER_AND_COMMA: "[ERROR] 숫자와 ,(쉼표) 기호만을 입력해주세요.",
+  ERROR_DONT_START_ZERO: "[ERROR] 0으로 시작하는 숫자는 입력할 수 없습니다.",
 };
 
-module.exports = LottoConst;
+module.exports = lottoConst;
