666cd3f9c2d360677bbf5308bff589a52fb306a7
jaehak26 <jack10829@naver.com>
2022-11-15 02:38:50 +0900

    당첨 내역 확인, 출력, 수익률 출력
    각각 로또 티켓 별로 당첨 내역을 확인하고 몇 등을 몇 번 했는지로 변환하여 출력함
    이후 수익률도 출력

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ const Lotto = require('./Lotto');
 const MissionUtils = require('@woowacourse/mission-utils');
 import { exceptLottoPrice, exceptBonusNumber } from './Exception';
 
+const winningPrice = [0, 2000000000, 30000000, 1500000, 50000, 5000];
+
 class App {
   #lotteryTicketNumber; //로또 수량
-  #lotteryTickets;
+  #lotteryTickets = [[]];
   #winningNumbers = [];
   #bonusNumber = 0;
+  #winningCount = [];
 
   play() {
     this.#inputLottoPrice();
@@ -18,7 +21,7 @@ class App {
   #inputLottoPrice() {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (lottoPrice) => {
       exceptLottoPrice(Number(lottoPrice));
-      this.#lotteryTicketNumber = Number(lottoPrice) / 1000;
+      this.#lotteryTicketNumber = parseInt(Number(lottoPrice) / 1000);
 
       this.#lotteryTickets = this.#makeLotteryTickets(
         this.#lotteryTicketNumber
@@ -63,21 +66,107 @@ class App {
         exceptBonusNumber(this.#winningNumbers, bonusNum);
         this.#bonusNumber = bonusNum;
 
-        this.#makeWinningHistorys(this.#lotteryTicketNumber);
+        this.#winningCount = this.#makeWinningCount(
+          this.#lotteryTicketNumber
+        );
+
+        this.#printWinningCounts(this.#winningCount)
+        this.#printYield(this.#winningCount, this.#lotteryTicketNumber)
+
+        //프로그램 종료
+        MissionUtils.Console.close();
       }
     );
   }
 
-  #makeWinningHistorys(lotteryTicketNumber) {
+  #printWinningCounts(winningCount){
+    MissionUtils.Console.print('당첨 통계')
+    MissionUtils.Console.print('---')
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${winningCount[5]}개`)
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${winningCount[4]}개`)
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${winningCount[3]}개`)
+    MissionUtils.Console.print(`5개, 보너스 볼 일치 (30,000,000원) - ${winningCount[2]}개`)
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${winningCount[1]}개`)
+  }
+
+  #printYield(winningCount, lotteryTicketNumber){
+    let amount = 0;
+    winningCount.map((count,countIdx)=>{
+      amount += count * winningPrice[countIdx]
+    })
+    let yield = amount / lotteryTicketNumber * 10
+
+    MissionUtils.Console.print(`총 수익률은 ${yield}입니다.`)
+
+  }
+
+  #makeWinningCount(lotteryTicketNumber) {
     let winningHistorys = new Array(lotteryTicketNumber);
-    winningHistorys.map((winningHistory)=>{
-      
+    winningHistorys.map((winningHistory, historysIdx) => {
+      winningHistory = this.#confirmLottery(
+        this.#lotteryTickets[historysIdx],
+        this.#winningNumbers,
+        this.#bonusNumber
+      );
+    });
+
+    return this.#convertWinningCount(winningHistorys)
+  }
+
+  //각각 로또마다 몇등을 했는지에서 몇 등을 몇 번 했는지로 변환한다
+  #convertWinningCount(winningHistorys){
+    let winningCount = new Array(6).fill(0)
+    winningCount.map((count,countIdx)=>{
+      winningHistorys.map((winningHistory)=>{
+        if(countIdx===winningHistory){count++}
+      })
     })
   }
 
-  #confrimLottery(){
-    
+  #confirmLottery(lotteryTicket = [], winningNumbers = [], bonusNumber = 0) {
+    let winning = []
+    let countSameNumber = 0;
+    let countBonus = 0;
+
+    lotteryTicket.map((lottoNumber) => {
+      if (winningNumbers.includes(lottoNumber) === true) {
+        countSameNumber++;
+      }
+    });
+
+    if (countSameNumber === 5) {
+      if (lotteryTicket.includes(bonusNumber) === true) {
+        countBonus++;
+      }
+    }
+
+    winning = this.#convertToWinningHistory(countSameNumber, countBonus);
   }
+
+  //각각 몇등 했는지로 변환
+  #convertToWinningHistory(countSameNumber, countBonus) {
+    if (countBonus === 1) {
+      return 2;
+    }
+
+    switch (countSameNumber) {
+      case 6:
+        return 1;
+      case 5:
+        return 3;
+      case 4:
+        return 4;
+      case 3:
+        return 5;
+      default:
+        return 0;
+    }
+  }
+
+  #convertToWinningHistory(winning){
+
+  }
+  
 }
 
 const app = new App();
