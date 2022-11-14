7874c535ea9160ce65d8ccf00c5e3dfac7b0b38b
김아현 <dhflxhdxhd@gmail.com>
2022-11-15 00:56:02 +0900

    Feat : 당첨 번호와 로또 번호 비교하여 일치하는 개ㅐ수 세기

--- src/Lotto.js
+++ src/Lotto.js
@@ -58,6 +58,14 @@ class Lotto {
     this.calcWinningResult();
   }
 
+  calcWinningResult(){
+    this.mylotto.forEach((mylottoArray) => {
+      console.log(mylottoArray);
+      this.compareNumbers(mylottoArray);
+    })
+  }
+
+
   // validate(numbers) {
   //   if (numbers.length !== 6) {
   //     throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
