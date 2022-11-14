ae615f5527787c3d046ca4f5fff2fe0d40a8d1cd
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-15 00:30:29 +0900

    refactor: 상수 객체의 수정이 불가능 하도록 object.freeze 적용

--- src/constants/condition.js
+++ src/constants/condition.js
@@ -2,21 +2,21 @@ const LOTTO_PRICE = 1000;
 const LOTTO_NUM_MIN_RANGE = 1;
 const LOTTO_NUM_MAX_RANGE = 45;
 const LOTTO_DIGITS = 6;
-const PRIZE_MATCH_NUMBER_COUNT = {
+const PRIZE_MATCH_NUMBER_COUNT = Object.freeze({
   firstPrize: 6,
   secondPrize: 5,
   thirdPrize: 5,
   fourthPrize: 4,
   fifthPrize: 3,
-};
-const LOTTO_PRIZE_MONEY = {
+});
+const LOTTO_PRIZE_MONEY = Object.freeze({
   firstPrize: 2000000000,
   secondPrize: 30000000,
   thirdPrize: 1500000,
   fourthPrize: 50000,
   fifthPrize: 5000,
   fail: 0,
-};
+});
 
 module.exports = {
   LOTTO_PRICE,
--- src/constants/message.js
+++ src/constants/message.js
@@ -7,11 +7,11 @@ const WINNING_NUMBERS = `당첨 번호를 입력해 주세요.
 const BONUS_NUMBER = `보너스 번호를 입력해 주세요.
 `;
 
-const REQUEST_MESSAGE = {
+const REQUEST_MESSAGE = Object.freeze({
   PURCHASE_AMOUNT,
   WINNING_NUMBERS,
   BONUS_NUMBER,
-};
+});
 
 const INVALID_INPUT_TYPE = "[ERROR] 숫자 외의 문자를 입력할 수 없습니다. 게임 종료!";
 const START_WITH_ZERO = "[ERROR] 입력은 0으로 시작할 수 없습니다. 게임 종료!";
@@ -22,7 +22,7 @@ const DUPLICATE_LOTTO_NUMBER = "[ERROR] 로또 번호에 중복된 숫자가 있
 const INVALID_LOTTO_NUMBER_RANGE = `[ERROR] 로또 번호의 범위는 ${LOTTO_NUM_MIN_RANGE} ~ ${LOTTO_NUM_MAX_RANGE} 이여야 합니다.`;
 const EMPTY_INPUT = "[ERROR] 입력값이 없습니다.";
 
-const ERROR_MESSAGE = {
+const ERROR_MESSAGE = Object.freeze({
   INVALID_INPUT_TYPE,
   START_WITH_ZERO,
   INDIVISIBLE_BY_LOTTO_PRICE,
@@ -30,6 +30,6 @@ const ERROR_MESSAGE = {
   DUPLICATE_LOTTO_NUMBER,
   INVALID_LOTTO_NUMBER_RANGE,
   EMPTY_INPUT,
-};
+});
 
 module.exports = { REQUEST_MESSAGE, ERROR_MESSAGE };
