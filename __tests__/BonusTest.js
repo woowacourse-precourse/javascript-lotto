ea49722388406e047a850e1ac97896b01491c9db
ryuna <anottrx@gmail.com>
2022-11-14 21:36:45 +0900

    test: 보너스에 문자가 입력되는 경우 테스트케이스 작성

--- __tests__/BonusTest.js
+++ __tests__/BonusTest.js
@@ -2,18 +2,15 @@ const Bonus = require('../src/Bonus');
 const { ERROR_MESSAGE } = require('../src/constant/constant');
 
 describe('보너스 클래스 테스트', () => {
+  test('숫자 아닌 값 입력', () => {
+    expect(() => {
+      new Bonus('1j');
+    }).toThrow(ERROR_MESSAGE.NOT_NUMBER);
+  });
+
   test(ERROR_MESSAGE.LOTTO.NUMBER_RANGE, () => {
     expect(() => {
       new Bonus(55);
     }).toThrow(ERROR_MESSAGE.LOTTO.NUMBER_RANGE);
   });
-
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  // test('로또 번호와 중복된 숫자가 있으면 예외가 발생한다.', () => {
-  //   expect(() => {
-  //     new Lotto([1, 2, 3, 4, 5, 5]);
-  //   }).toThrow(ERROR_MESSAGE.LOTTO.NUMBER_DUPLICATED);
-  // });
-
-  // 아래에 추가 테스트 작성 가능
 });
