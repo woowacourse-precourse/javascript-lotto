4f73cd7e180a46197156e8de1b8932bf7a07b6dd
nuheajiohc <jaehun341012@gmail.com>
2022-11-14 17:35:38 +0900

    test: 보너스 번호 입력 예외 테스트 추가

--- __tests__/myTest.js
+++ __tests__/myTest.js
@@ -1,4 +1,5 @@
 const buyingAmountValidator = require("../src/buyingAmountValidator");
+const bonusNumberValidator = require("../src/bonusNumberValidator");
 
 describe("입력값 예외처리 테스트", () => {
   test("입력값에 공백이 포함되어 있으면 에러 문구를 출력한다.", () => {
@@ -27,5 +28,41 @@ describe("입력값 예외처리 테스트", () => {
     expect(() => {
       buyingAmountValidator(input);
     }).toThrow("[ERROR]");
+  });
+});
+
+describe("보너스 번호 예외처리 테스트", () => {
+  const winningNumbers = ["1", "2", "3", "4", "5", "6"];
+  test("보너스 번호에 문자를 입력하면 에러 문구를 출력한다.", () => {
+    const inputList = ["a", "1a", "a1"];
+    inputList.forEach((input) => {
+      expect(() => bonusNumberValidator(winningNumbers, input)).toThrow(
+        "[ERROR]"
+      );
+    });
+  });
+  test("보너스 번호가 1~45 사이의 값이 아닐 경우 에러 문구를 출력한다.", () => {
+    const inputList = ["0", "-1", "46", "100"];
+    inputList.forEach((input) => {
+      expect(() => bonusNumberValidator(winningNumbers, input)).toThrow(
+        "[ERROR]"
+      );
+    });
+  });
+  test("보너스 번호가 당첨 번호에 포함되면 에러 문구를 출력한다.", () => {
+    const inputList = ["1", "2", "3", "4", "5", "6"];
+    inputList.forEach((input) => {
+      expect(() => bonusNumberValidator(winningNumbers, input)).toThrow(
+        "[ERROR]"
+      );
+    });
+  });
+  test("보너스 번호에 공백을 입력할 시 에러 문구를 출력한다.", () => {
+    const inputList = [" ", "3 ", "10 ", " 9"];
+    inputList.forEach((input) => {
+      expect(() => bonusNumberValidator(winningNumbers, input)).toThrow(
+        "[ERROR]"
+      );
+    });
   });
 });
