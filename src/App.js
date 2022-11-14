9097a82f5989573e46152664226494fa409290ac
BullWooStar <ccatal66@naver.com>
2022-11-14 21:54:05 +0900

    feat: 당첨 번호와 보너스 번호 입력

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
 const { Random, Console } = require("@woowacourse/mission-utils");
-// const Lotto = require("./Lotto");
-// const LottoFunction = new Lotto();
+const Lotto = require("./Lotto");
 
 class App {
   constructor() {
     this.lottoAmount = "";
     this.purchasedLottoNumber = [];
     this.purchasedLottoNumberArray = [];
-  }
-
-  playLottoGame() {
-    this.putMoney();
-  }
-
-  putMoney() {
-    Console.readLine("구입금액을 입력해 주세요.", (answer) => {
-      this.moneyValidate(answer);
-      this.lottoAmount = answer / 1000;
-      for (let i = 0; i < this.lottoAmount; i++) {
-        this.makePurchasedLottoNumber();
-      }
-      Console.print(`${this.lottoAmount}개를 구매했습니다.`);
-      this.purchasedLottoNumberArray.map((array) => {
-        Console.print(array);
-      });
-    });
+    this.lottoNumber = [];
   }
 
   moneyValidate(money) {
     if (money % 1000 !== 0) {
       throw "[ERROR] 1000원 단위로만 구매 가능합니다";
     }
+    this.lottoAmount = money / 1000;
+  }
+
+  makeAndPrintPurchasedLottoNumber() {
+    for (let i = 0; i < this.lottoAmount; i++) {
+      this.makePurchasedLottoNumber();
+    }
+    Console.print(`${this.lottoAmount}개를 구매했습니다.`);
+    this.purchasedLottoNumberArray.map((array) => {
+      Console.print(array);
+    });
   }
 
   makePurchasedLottoNumber() {
@@ -49,8 +42,35 @@ class App {
     this.purchasedLottoNumber = [];
   }
 
+  inputLottoNumbers() {
+    Console.readLine("당첨 번호를 입력해 주세요.", (number) => {
+      this.lottoNumber = new Lotto(number.split(",").map(Number)).returnValue();
+      this.inputBonusNumber();
+    });
+  }
+
+  inputBonusNumber() {
+    Console.readLine("보너스 번호를 입력해 주세요.", (number) => {
+      this.validateBonusNumber(number);
+      this.bonusNumber = number;
+    });
+  }
+
+  validateBonusNumber(number) {
+    if (this.lottoNumber.includes(number)) {
+      throw "[ERROR] 당첨번호와 다른 번호를 입력하세요!";
+    }
+    if (number > 45 || number === 0) {
+      throw "[ERROR] 범위에 맞는 수를 입력하세요";
+    }
+  }
+
   play() {
-    this.playLottoGame();
+    Console.readLine("구입금액을 입력해 주세요.", (answer) => {
+      this.moneyValidate(answer);
+      this.makeAndPrintPurchasedLottoNumber();
+      this.inputLottoNumbers();
+    });
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,21 +1,35 @@
-const { Random, Console } = require("@woowacourse/mission-utils");
-
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
+    this.rangeBoolean = false;
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size < numbers.length) {
+      throw "[ERROR] 서로다른 수를 입력하세요!";
+    }
+    this.validateRange(numbers);
+    if (this.rangeBoolean) {
+      throw "[ERROR] 범위에 맞는 수를 입력하세요";
+    }
   }
 
-  inputLottoNumbers() {
-    Console.readLine("구입금액을 입력해 주세요.", (answer) => {});
+  validateRange(numbers) {
+    numbers.map((number) => {
+      if (number > 45 || number === 0) {
+        this.rangeBoolean = true;
+      }
+    });
+  }
+
+  returnValue() {
+    return this.#numbers;
   }
 }
 
