ba646616f8d8df84f52283029cb1425f8fc29a2a
osdoonhyun <skw1996@naver.com>
2022-11-14 17:41:03 +0900

    fix(LottoGame): 사용자가 입력한 구입 금액 type 변환

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,15 +1,19 @@
-const {MESSAGES, } = require('./Constants/Constants')
-const Validation = require('./Validation');
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGES } = require("./Constants/Constants");
+const Validation = require("./Validation");
 class LottoGame {
+  #purchaseAmount;
 
-  play();
-
-  purchaseLotto() {
-    Console.readLine(MESSAGES.ENTER_PURCHASE_AMOUNT, (amount) => {
-      Validation.validatePurchaseAmount(amount);
-    });
+  play() {
+    this.purchaseLotto();
   }
 
+  purchaseLotto() {
+    Console.readLine(MESSAGES.ENTER_PURCHASE_AMOUNT, (input) => {
+      this.#purchaseAmount = Number(input);
+      Validation.validatePurchaseAmount(this.#purchaseAmount);
+    });
+  }
 }
 
 module.exports = LottoGame;
