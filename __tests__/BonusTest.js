f710dbf3282a4fba6e4b5d1a847ad497740c91b7
dhkham <khamdong@gmail.com>
2022-11-15 00:44:04 +0900

    test(BonusTest.js): add test that throws error when bonus number is not in between 1-45

--- __tests__/BonusTest.js
+++ __tests__/BonusTest.js
@@ -6,4 +6,10 @@ describe("보너스 클래스 테스트", () => {
       new Bonus([1, 2]);
     }).toThrow("[ERROR]");
   });
+
+  test("보너스 번호에 1-45 범위 밖의 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Bonus(46);
+    }).toThrow("[ERROR]");
+  });
 });
