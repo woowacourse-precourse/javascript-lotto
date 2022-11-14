5e4293b1f6bd730ddaca84096632131019120293
wonkyung <mwk0725@gmail.com>
2022-11-14 13:30:21 +0900

    refactor: 당첨 통계 결과 상수화

--- src/Result.js
+++ src/Result.js
@@ -1,3 +1,4 @@
+const { RESULT_TXT } = require('./constant');
 const MissionUtils = require('@woowacourse/mission-utils');
 const Match = require('./Matcher');
 const Console = MissionUtils.Console;
@@ -6,9 +7,16 @@ class Result {
   printResult(earnings) {
     const earningsString = String(earnings) + '%';
 
+    Console.print(RESULT_TXT.TITLE);
+    Console.print(RESULT_TXT.LINE);
+    Console.print(RESULT_TXT.MATCH_THREE + `${Match.lottoWinnerNumber[4]}개`);
+    Console.print(RESULT_TXT.MATCH_FOUR + `${Match.lottoWinnerNumber[3]}개`);
+    Console.print(RESULT_TXT.MATCH_FIVE + `${Match.lottoWinnerNumber[2]}개`);
     Console.print(
-      `\n당첨 통계\n---\n3개 일치 (5,000원) - ${Match.lottoWinnerNumber[4]}개\n4개 일치 (50,000원) - ${Match.lottoWinnerNumber[3]}개\n5개 일치 (1,500,000원) - ${Match.lottoWinnerNumber[2]}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${Match.lottoWinnerNumber[1]}개\n6개 일치 (2,000,000,000원) - ${Match.lottoWinnerNumber[0]}개\n총 수익률은 ${earningsString}입니다.`,
+      RESULT_TXT.MATCH_FIVE_BONUS + `${Match.lottoWinnerNumber[1]}개`,
     );
+    Console.print(RESULT_TXT.MATCH_SIX + `${Match.lottoWinnerNumber[0]}개`);
+    Console.print(`\n총 수익률은 ${earningsString}입니다.`);
     Console.close();
   }
 }
--- src/constant.js
+++ src/constant.js
@@ -6,4 +6,14 @@ const LOTTO_WINNING_AMOUNT = {
   FIFTH_PRIZE: 5000,
 };
 
-module.exports = { LOTTO_WINNING_AMOUNT };
+const RESULT_TXT = {
+  TITLE: '\n당첨 통계',
+  LINE: '---',
+  MATCH_THREE: '3개 일치 (5,000원) - ',
+  MATCH_FOUR: '4개 일치 (50,000원) - ',
+  MATCH_FIVE: '5개 일치 (1,500,000원) - ',
+  MATCH_FIVE_BONUS: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  MATCH_SIX: '6개 일치 (2,000,000,000원) - ',
+};
+
+module.exports = { LOTTO_WINNING_AMOUNT, RESULT_TXT };
