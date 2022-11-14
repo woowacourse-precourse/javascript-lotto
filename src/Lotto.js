e4f5d143e8d3855f37d06d3953ec804c72cd2e93
inthhh <glass005533@gmail.com>
2022-11-14 19:39:33 +0900

    feat: 당첨번호 예외처리 (Lotto.validate)

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,7 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Console = MissionUtils.Console;
+const Random = MissionUtils.Random;
+
 class Lotto {
   #numbers;
 
@@ -7,12 +11,16 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    let isNumCheck = numbers.join("");
+    if (isNaN(isNumCheck)) throw new Error("[ERROR] 문자를 입력하실 수 없습니다.");
+    for (let i = 0; i < 6; ++i) {
+      if (numbers[i] < 0 || numbers[i] > 45)
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
+    if (numbers.length !== 6) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    let numSet = [...new Set(numbers)];
+    if (numbers.length != numSet.length) throw new Error("[ERROR] 중복된 숫자가 존재합니다.");
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
