6386808223fbbb3dd1eba968e2f698efbae52fe8
h2002y <h2002y@naver.com>
2022-11-14 21:03:53 +0900

    feat: Add printLottoMatchResult func in App.js

--- src/App.js
+++ src/App.js
@@ -105,7 +105,18 @@ class App {
         this.lottoPrizeResult[fifthPrize]++;
       }
     }
-    printLottoMatchResult();
+    this.printLottoMatchResult();
+  }
+
+  printLottoMatchResult() {
+    MissionUtils.Console.print("당첨 통계");
+    MissionUtils.Console.print("---");
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${this.lottoResult[fifthPlace]}`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${this.lottoResult[fourthPlace]}`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${this.lottoResult[thirdPlace]}`);
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.lottoResult[secondPlace]}`);
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${this.lottoResult[firstPlace]}`);
+    this.getProfit();
   }
 }
 
