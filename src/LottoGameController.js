b0d2bfafec17bc8e9a0e0e61e27cfbf9fbd1b780
y00eunji <aine0227@naver.com>
2022-11-14 22:34:20 +0900

    feat(LottoGameController): Buy lotto and show lotto numbers

---
+++ src/LottoGameController.js
@@ -0,0 +1,20 @@
+const Lottos = require("./domain/Lottos");
+const OutputView = require("./view/OutputView");
+const LottoGameService = require("./LottoGameService");
+
+class LottoGameController {
+  #outputView;
+  #gameService;
+
+  constructor() {
+    this.#outputView = new OutputView();
+    this.#gameService = new LottoGameService();
+  }
+
+  start(money) {
+    const lottos = this.#gameService.buyLotto(money);
+    this.#outputView.showLottos(lottos);
+  }
+}
+
+module.exports = LottoGameController;
