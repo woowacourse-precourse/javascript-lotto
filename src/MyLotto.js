e48494c3db2728a37b34e8aff4da970dbf3c3562
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-15 08:58:42 +0900

    feat : 구입 금액에 따라 발행할 로또 수 계산 & 로또 수에 맞춰 로또 번호 생성

--- src/MyLotto.js
+++ src/MyLotto.js
@@ -4,6 +4,7 @@ class MyLotto {
     lottoList = [];
     constructor(money) {
         this.validate(money);
+        this.createLottoList(this.calculateNumberOfLotto(money))
     }
 
     validate(money) {
@@ -11,6 +12,18 @@ class MyLotto {
             throw new Error("[ERROR] 로또 구입 금액은 1000원 단위로 입력 가능합니다.");
         }
     }
+
+    calculateNumberOfLotto(money) {
+        return Math.floor(money / 1000)
+    }
+
+    createLottoList(numbers) {
+        for (let i = 0; i < numbers; i++) {
+            let lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+            this.lottoList.push(lotto)
+        }
+    }
+
 }
 
 module.exports = MyLotto;
