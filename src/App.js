14fc9157e993e5696451df5412c8fba24b0cd850
jiwon903 <50747295+jiwon903@users.noreply.github.com>
2022-11-15 00:16:03 +0900

    구입 개수만큼 로또번호 생성하여 배열에 저장한 후 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,16 +4,33 @@ const MissionUtils =require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    this.getMoney()
-    
+    this.getMoney();
     
   }
+
   getMoney() {
     MissionUtils.Console.readLine('구입금액을 입력해주세요\n', this.saveMoney.bind(this));
   }
   saveMoney(input) {
     const money = input;
-    MissionUtils.Console.print('\n'+ money/1000 + ' 개를 구매했습니다.')
+    const lottoNum = money/1000;
+    MissionUtils.Console.print('\n'+ lottoNum + ' 개를 구매했습니다.');
+    this.MakeLottoNum(lottoNum);
+  }
+
+  MakeLottoNum(lottoNum) {
+    var lottoList=[];
+    for(var i=0; i<lottoNum; i++){
+      lottoList.push(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6));
+    }
+    for(var i=0; i<lottoNum; i++){
+      MissionUtils.Console.print(lottoList[i]);
+    }
+    
+  }
+
+  check() {
+ 
   }
 
 }
