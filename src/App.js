36f0bde52b73837c13fbfc0d77fdc42b3f6d44d2
NaveOWO <pianowell96@gmail.com>
2022-11-14 21:41:47 +0900

    [ feat ] 로또의 총 개수를 게산하는 기능구현

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ class App {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요\n", (answer) => {
       let lottoNum = 0;
       this.buyMoney = answer;
+      lottoNum = this.divideMoneyByThousand();
     });
+  }
+
+  divideMoneyByThousand() {
+    const lottoNum = parseInt(this.buyMoney / 1000);
+    MissionUtils.Console.print(`${lottoNum}개를 구매했습니다.`);
+    return lottoNum;
   }
 }
 
