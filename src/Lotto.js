c67ca059e8a38850669475d55571ec0071b29bfa
foundaway98 <foundaway98@naver.com>
2022-11-14 19:38:26 +0900

    <feat> : 오류 제어 기능 추가(입력된 로또 번호에 중복된 값이 있는 경우)
    입력된 로또 번호에 중복된 값이 있을 경우 에러 문구 출력 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,6 +11,13 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    let numbersSet = new Set(numbers);
+    numbersSet = [...numbersSet];
+    if (numbersSet.length !== numbers.length) {
+      throw new Error(
+        "[ERROR] 로또 번호는 중복되지 않은 1부터 45 사이의 숫자여야 합니다."
+      );
+    }
   }
 
   getLottoNumbers() {
