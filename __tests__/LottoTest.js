942972dcd8ee06d5affe8dfc785b063da0ff2779
Suyoung <jsy8492@gmail.com>
2022-11-14 10:48:54 +0900

    refactor(lottoTest): 로또, 당첨 번호와 관련된 상수를 관리하는 파일에서 불러와 사용하도록 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,31 +1,34 @@
 const Lotto = require('../src/Lotto');
 
+const { ERROR } = require('../src/lib/constants/error');
+const { LOTTO } = require('../src/lib/constants/lotto');
+
 describe('로또 클래스 테스트', () => {
-  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
+  test(`로또 번호의 개수가 ${LOTTO.NUMBER_COUNT}개가 넘어가면 예외가 발생한다.`, () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow('[ERROR]');
+    }).toThrow(ERROR.LOTTO.NOT_LOTTO_LENGTH);
   });
 
-  test('로또 번호의 개수가 6개 미만이면 예외가 발생한다.', () => {
+  test(`로또 번호의 개수가 ${LOTTO.NUMBER_COUNT}개 미만이면 예외가 발생한다.`, () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5]);
-    }).toThrow('[ERROR]');
+    }).toThrow(ERROR.LOTTO.NOT_LOTTO_LENGTH);
   });
 
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow('[ERROR]');
+    }).toThrow(ERROR.LOTTO.DUPLICATE);
   });
 
-  test('로또 번호에 1 이상 45 이하가 아닌 숫자가 있으면 예외가 발생한다.', () => {
+  test(`로또 번호에 ${LOTTO.MIN_NUMBER_LIMIT} 이상 ${LOTTO.MAX_NUMBER_LIMIT} 이하가 아닌 숫자가 있으면 예외가 발생한다.`, () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 46]);
-    }).toThrow('[ERROR]');
+    }).toThrow(ERROR.LOTTO.OUT_OF_BOUND);
   });
 
-  test('로또 번호가 1 이상 45 이하의 중복 없는 6개의 숫자면 로또를 생성한다.', () => {
+  test('올바른 로또 번호가 입력되면 해당 번호를 갖는 로또를 생성한다.', () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
 
     expect(lotto.numbers).toHaveLength(6);
