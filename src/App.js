697bbec0b36d2fc91c773c22e36d08fbebfd4d4c
yeongi <cbg5255@gmail.com>
2022-11-14 12:36:03 +0900

    docs : readme.md app 기능 목록 작성

--- src/App.js
+++ src/App.js
@@ -2,24 +2,12 @@ const LottoBuy = require("./LottoBuy");
 const LottoSetting = require("./LottoSetting");
 const MissionUtils = require("@woowacourse/mission-utils");
 const { INPUT } = require("./lib/library");
-const LottoCalculate = require("./LottoCalculator");
 
 class App {
-  async play() {
-    const myLotto = new LottoBuy();
-    const lottoSetting = new LottoSetting();
-    const caculator = new LottoCalculate();
-    MissionUtils.Console.readLine(INPUT.BUY, (price) => {
-      myLotto.inputPurchasePrice(price);
-      lottoSetting.inputWinLottoNum();
-      const result = caculator.resultCaculator(
-        myLotto.getMyLottos(),
-        lottoSetting.getWinInfo()
-      );
-      caculator.printWinResult(result);
-      caculator.printGainPercent(result);
-    });
-  }
+  play() {}
+  lottoBuyPrint() {}
+  lottoWinSetting() {}
+  lottoResultCalculator() {}
 }
 
 const app = new App();
