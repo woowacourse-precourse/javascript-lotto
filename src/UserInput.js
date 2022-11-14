eaf549bd3605a74a34bf81a3fe8fb2717a62bded
cho seonghoon <chsida655@gmail.com>
2022-11-14 19:12:47 +0900

    feat: readLine을 이용하는 함수들 분리 및 비동기 처리

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,8 +1,22 @@
-const { Console } = require("./lib/MissionUtils.js");
-const { Random } = require("./lib/MissionUtils.js");
+const { ReadLine } = require("./lib/MissionUtils.js");
 
 const getPurchaseAmount = () => {
   return new Promise((resolve) => {
-    Console.readLine("구입금액을 입력해 주세요.\n", resolve);
-  });
+    ReadLine("구입금액을 입력해 주세요.\n", resolve);
+  }); // 숫자, 1000원 단위 체크
 };
+
+const getWinningNumber = () => {
+  return new Promise((resolve) => {
+    ReadLine("당첨 번호를 입력해 주세요.\n", resolve);
+  }); //쉼표 단위로 구분 안되어 있을 시 에러처리 + 숫자 체크
+};
+
+const getBonusNumber = () => {
+  return new Promise((resolve) => {
+    ReadLine("보너스 번호를 입력해 주세요.\n", resolve);
+  }); //숫자 + 1개인 지 체크
+};
+//로또 번호는 범위 체크!
+
+module.exports = { getPurchaseAmount, getWinningNumber, getBonusNumber };
