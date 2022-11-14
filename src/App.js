197739f44cd59c1416d2e00919047f42e463fdaa
SoobinJ <soobin08011812@gmail.com>
2022-11-14 23:30:39 +0900

    feat : 로또 구입 금액 입력/ 입력된 구입 금액이 유효한지 검사 구현

--- src/App.js
+++ src/App.js
@@ -12,7 +12,18 @@ class App {
   }
 
   inputAmount() {
-    Console.readLine("구입금액을 입력해 주세요.", (purchaseAmount) => {});
+    Console.readLine("구입금액을 입력해 주세요.", (purchaseAmount) => {
+      this.isValidInput(purchaseAmount);
+      this.#purchaseAmount = purchaseAmount;
+    });
+  }
+
+  isValidInput(purchaseAmount) {
+    if (isNaN(purchaseAmount)) {
+      throw "[EROR] 로또 구입금액은 숫자여야 합니다.";
+    } else if (purchaseAmount % LOTTOPRICE !== 0) {
+      throw "[EROR] 로또 구입금액은 1,000원 단위여야 합니다.";
+    }
   }
 }
 
