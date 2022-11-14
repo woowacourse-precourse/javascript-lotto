86f5d631487a2e3838073da3a2211392e37c5ec9
HeoJiye <heoze1031@gmail.com>
2022-11-15 01:53:01 +0900

    fix: 문법 오류 수정

--- src/LottoStore.js
+++ src/LottoStore.js
@@ -1,15 +1,26 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const Lotto = require("Lotto");
+const Lotto = require("./Lotto");
 
 class LottoStore { 
   static LOTTO_PRICE = 1000;
 
   buy(money) {
-    lottos = [];
+    this.vaildate(money);
+
+    const lottos = [];
 
     while(money > 0) {
       money -= 1000;
-      lottos.push(new Lotto());
+      lottos.push(new Lotto([ 1, 2, 3, 4, 5, 6 ]));
+    }
+  }
+
+  vaildate(money) {
+    if(isNaN(money)) {
+      throw new Error("[ERROR] 금액은 숫자로 입력해야 합니다.");
+    }
+    if(money % this.LOTTO_PRICE != 0) {
+      throw new Error("[ERROR] 금액은 "+this.LOTTO_PRICE+"로 나누어 떨어져야 합니다."); 
     }
   }
 }
