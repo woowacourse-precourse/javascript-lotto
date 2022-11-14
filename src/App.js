659d8fbfda2b6ebb26f6931fe896a2f2bfdb3777
nuheajiohc <jaehun341012@gmail.com>
2022-11-14 11:21:43 +0900

    feat: 구입 금액을 입력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("./constants.js");
+
 class App {
-  play() {}
+  play() {
+    this.inputBuyingAmount();
+  }
+
+  inputBuyingAmount() {
+    Console.readLine(MESSAGE.INPUT_BUYING_AMOUNT, (amount) => {
+      console.log(amount);
+    });
+  }
 }
+
+const a = new App();
+a.play();
 
 module.exports = App;
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const MESSAGE = {
+  INPUT_BUYING_AMOUNT: "구입금액을 입력해 주세요\n",
+};
+
+module.exports = { MESSAGE };
