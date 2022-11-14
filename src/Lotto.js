75a6bc8eba1a74f43f8e68de00bb43ee3ca2134c
areumsheep <areumsheep@gmail.com>
2022-11-15 01:12:41 +0900

    refactor: Lotto 번호 입력 포맷 검증 메소드 및 랜덤 번호 생성 메소드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+const { Random } = require('@woowacourse/mission-utils');
 const { MESSAGES, ERROR_MESSAGES } = require('./constants/index');
 
 const MAX_COUNT = 6;
@@ -6,27 +7,45 @@ const RANGE = {
   END: 45,
 };
 class Lotto {
-  #numbers;
+  #numbers = [];
+  #bonus = 1;
 
   constructor(numbers) {
-    this.validate(numbers);
     this.#numbers = numbers;
+    this.#validate(numbers);
   }
 
-  validate(numbers = []) {
+  #validate(numbers = []) {
     if (numbers.length !== MAX_COUNT) {
       throw new Error(ERROR_MESSAGES.INVALID_LOTTO_LENGTH);
     }
+
     if (numbers.find((number) => number < RANGE.START || number > RANGE.END)) {
       throw new Error(ERROR_MESSAGES.INVALID_LOTTO_RANGE);
     }
+
     const numberSet = new Set(numbers);
     if (numberSet.size !== MAX_COUNT) {
       throw new Error(ERROR_MESSAGES.NOT_DUPLICATE_NUMBER);
     }
   }
 
-  // TODO: 추가 기능 구현
+  #validateBonus(bonus) {
+    //TODO 보너스 번호 1자리만 입력한 것을 판단하는 방법
+    // throw new Error(ERROR_MESSAGES.INVALID_BONUS_LENGTH);
+    if (this.#numbers.includes(bonus))
+      throw new Error(ERROR_MESSAGES.NOT_DUPLICATE_NUMBER);
+  }
+
+  static createRandomNumbers() {
+    const numbers = Random.pickUniqueNumbersInRange(
+      RANGE.START,
+      RANGE.END,
+      MAX_COUNT
+    );
+    if (!numbers) throw new Error(ERROR_MESSAGES.EMPTY_NUMBERS);
+    return numbers.sort((a, b) => a - b);
+  }
 }
 
 module.exports = Lotto;
