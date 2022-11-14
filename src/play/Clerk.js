df523bbb70355092170dac524e89dcefb8d3387b
DongWooKim97 <nanaukim@naver.com>
2022-11-14 22:09:30 +0900

    feat: 로또 번호를 생성하는 클래스와 연결하여 프라이빗 필드에서 해당 배열 저장

--- src/play/Clerk.js
+++ src/play/Clerk.js
@@ -3,8 +3,16 @@ const { GAME_MESSAGES } = require("../utils/Constants");
 const QuickPick = require("../input/QuickPick");
 
 class Clerk {
+  #payment;
+  #myLottoArray;
+  #lottoAmount;
+
   inputLottoAmount() {
     Mission.Console.readLine(GAME_MESSAGES.ASK_TO_PAY, (payment) => {
+      const quickPick = new QuickPick(payment);
+      this.#payment = payment;
+      this.#myLottoArray = quickPick.arrayOutput();
+      this.#lottoAmount = quickPick.amountOutput();
       Mission.Console.close();
     });
   }
