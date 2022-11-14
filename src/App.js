095c71eb39350b8d7ac41cf97519f5fbaa6a02b5
Jiwon <jiwony933@naver.com>
2022-11-14 12:03:26 +0900

    feat : printing될 message 상수데이터 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+const {
+  GET_MONEY,
+  SHOW_AMOUNT,
+  GET_NUMBERS,
+  GET_BONUS_NUMBER,
+  WINNING_STASTICS,
+  MATCHING_3,
+  MATCHING_4,
+  MATCHING_5,
+  MATCHING_5_BONUS,
+  MATCHING_6,
+  COUNT,
+  RETURN_RATE,
+  RETURN_RATE_ENDING_WORD,
+} = require('./Messages');
+
 class App {
   play() {}
+
+  printMessage(message) {
+    Console.print(message);
+  }
+
+  startLotto() {}
 }
 
 module.exports = App;
