9b9c9d45a34e392e2f92cdb7ff24b548a703f7ff
wusi-hub <jws9595@naver.com>
2022-11-15 08:25:28 +0900

    feat(Lotto.js): 맞는 로또 번호 범위 확인

--- src/Lotto.js
+++ src/Lotto.js
@@ -14,6 +14,9 @@ class Lotto {
     if (new Set(numbers).size !== 6) {
       throw new Error("[ERROR] 로또 번호는 중복되지 않는 숫자여야 합니다.");
     }
+    numbers.map((number) => {
+      if (!(number >= 1 && number <= 45)) throw new Error("[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다.");
+    });
   }
 
   // TODO: 추가 기능 구현
