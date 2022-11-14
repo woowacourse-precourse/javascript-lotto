a7bbc910da2a0efc7e30290492d0caf937d912fa
cho seonghoon <chsida655@gmail.com>
2022-11-14 23:25:18 +0900

    feat: 당첨 번호 입력 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-const { getPurchaseAmount } = require("./UserInput.js");
+const { getPurchaseAmount, getWinningNumber } = require("./UserInput.js");
 const { printUserPurchaseAmount } = require("./Output.js");
 const { Print } = require("./lib/MissionUtils.js");
+const { createLotto } = require("./Random.js");
 class App {
   $purchaseAmount;
-  $lottos = [];
-  $winNum = [];
+  $lottos;
+  $winNum;
   async play() {
     await this.savePurchaseAmount();
+    this.$lottos = createLotto(this.$purchaseAmount);
+    await this.saveWinningNumber();
   }
   async savePurchaseAmount() {
-    let $purchaseAmount = await getPurchaseAmount().then((data) => data);
-    printUserPurchaseAmount($purchaseAmount);
+    this.$purchaseAmount = await getPurchaseAmount().then(
+      (data) => Number(data) / 1000
+    );
+    printUserPurchaseAmount(this.$purchaseAmount);
   }
+  async saveWinningNumber() {
+    this.$winNum = await getWinningNumber().then((data) =>
+      data.split(",").map((v) => +v)
+    );
+  } //나눌 때 예외처리, 숫자,6개인지
 }
 
 let app = new App();
