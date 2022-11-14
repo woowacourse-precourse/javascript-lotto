6f2da55c921a2949faee6dae9db16786559f2f52
minseok1109 <park485201@naver.com>
2022-11-14 17:07:05 +0900

    test: [green] 테스트 코드가 정상적으로 작동하는지 확인

--- src/Lotto.js
+++ src/Lotto.js
@@ -50,6 +50,19 @@ class Lotto {
   static #printCountLotto() {
     Console.print(`${this.countOfLotto}개를 구매했습니다.`);
   }
+
+  static makeRandomArray() {
+    return [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ];
+  }
 }
 
 module.exports = Lotto;
