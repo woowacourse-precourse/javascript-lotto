1e8ea57e3cc3604d722e7b091f6b23b6c91ab319
ReturnReason <reasonz@daum.net>
2022-11-14 23:14:22 +0900

    test: 사용자가 입력한 금액을 반환하는 메서드 테스트

---
+++ __tests__/UserTest.js
@@ -0,0 +1,9 @@
+const User = require('../src/User');
+
+describe('유저 클래스 테스트', () => {
+  test('✨ 사용자가 입력한 금액을 반환한다.', () => {
+    const user = new User();
+    user.setMoney(8000);
+    expect(user.getMoney()).toBe(8000);
+  });
+});
