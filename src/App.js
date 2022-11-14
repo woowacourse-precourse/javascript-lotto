ca1784a1dd57bcdf5e901103091b9cb8e05d0383
ho-ji <wkdd0205@gmail.com>
2022-11-14 21:24:33 +0900

    feat: Add error when money to buy lottos is wrong input
    입력 받은 금액이 1000원 단위의 금액이 아닐 때 에러를 처리함

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ class App {
   }
   inputMoneyToBuyLottos() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (moneyToBuyLottos) => {
+      const regex = /^[1-9]+[0-9]*[0]{3}$/;
+      if (!regex.test(moneyToBuyLottos))
+        throw new Error("[ERROR] 구입 금액은 1000원 단위의 금액을 입력해주세요");
       this.#numberOfLottos = Number(moneyToBuyLottos)/1000;
     });
   }
