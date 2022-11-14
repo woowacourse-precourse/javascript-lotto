88a8654e562435c5edd02638883a3c8ae737db26
Narcoker <jykim9868@gmail.com>
2022-11-15 03:49:55 +0900

    Test: 당첨 통계 출력 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -286,6 +286,24 @@ describe("로또 클래스 테스트", () => {
     expect(SYSTEM.compare(lotto.getNumber(), new Set(winningLotto), bonusNumber)).toBe(7);
   });
 
+  test("기능테스트: 당첨 통계 출력", () => {
+    const results = [1, 2, 3, 4, 5];
+    const logs = [
+      "3개 일치 (5,000원) - 5개",
+      "4개 일치 (50,000원) - 4개",
+      "5개 일치 (1,500,000원) - 3개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 2개",
+      "6개 일치 (2,000,000,000원) - 1개",
+    ];
+    const logSpy = getLogSpy();
+
+    SYSTEM.printWinningHistory(results);
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+
 
 
 
