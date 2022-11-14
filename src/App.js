8a5efd0980644a69550ed32d2ab38bcee226acfb
kaori-killer <sksbthwjd@daum.net>
2022-11-14 20:27:55 +0900

    style: 공백 라인 수정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ class App {
     this.userLottos = [];
     this.winningLotto = [];
     this.winningBonusNumber = 0;
-    this.winningResult = [[0, 5000], [0, 50000], [0, 1500000], [0, 30000000], [0, 2000000000]];  
+    this.winningResult = [
+      [0, 5000],
+      [0, 50000], 
+      [0, 1500000], 
+      [0, 30000000], 
+      [0, 2000000000]];  
   }
 
   play() {
