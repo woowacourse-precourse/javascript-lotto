76f157d739a68c140e4ad40bb9a20cbce8a0bde4
DongWooKim97 <nanaukim@naver.com>
2022-11-14 23:39:28 +0900

    feat: 보너스 숫자를 이용하여 보너스 인스턴스 생성 및 번호를 저장하는 메소드 추가

--- src/play/Clerk.js
+++ src/play/Clerk.js
@@ -2,12 +2,14 @@ const Mission = require("@woowacourse/mission-utils");
 const { GAME_MESSAGES } = require("../utils/Constants");
 const QuickPick = require("../input/QuickPick");
 const Lotto = require("../Lotto");
+const Bonus = require("../input/Bonus");
 
 class Clerk {
   #payment;
   #myLottoArray;
   #lottoAmount;
   #winningNumbers;
+  #bonusNumber;
 
   inputLottoAmount() {
     Mission.Console.readLine(GAME_MESSAGES.ASK_TO_PAY, (payment) => {
@@ -27,7 +29,16 @@ class Clerk {
         .sort((a, b) => a - b);
       const lotto = new Lotto(winningNum);
       this.#winningNumbers = lotto.output();
+      this.inputBonusNumber();
     });
+  }
+
+  inputBonusNumber() {
+    Mission.Console.readLine(GAME_MESSAGES.ASK_TO_BONUS_NUMBER, (bonusNumber) => {
+      const bonus = new Bonus(bonusNumber, this.#winningNumbers);
+      this.#bonusNumber = bonus.output();
+    });
+    return;
   }
 }
 
