11478bdc3e98db3b2c14e70ec6f07f46ab9eba48
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-15 05:05:05 +0900

    feat : 당첨 내역을 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ const Lotto = require('./Lotto');
 
 const ONE_THOUSAND_WON = 1000;
 
+const ERROR_MESSAGE = {
+  NOT_THOUSAND_WON : '[ERROR] 구입 금액은 1,000원 단위 입니다'
+}
+
 class App {
   constructor() {
     this.winningLotto = [];
     this.randomNumberArr = [];
     this.bonusLotto = 0;
+    this.winningRanking = { First: 0, Second: 0, Third: 0, Fourth: 0, Fifth: 0 };
     }
 
   play() {
     this.priceInput();
-    this.userRandomNumber();
-    this.userBonusNumber();
   };
   
   priceInput() {
@@ -26,7 +29,7 @@ class App {
   };
 
   priceExceptionHandling(price) {
-    if(price % 1000 !== 0 ) throw '[ERROR] 구입 금액은 1,000원 단위 입니다';
+    if(price % ONE_THOUSAND_WON !== 0 ) throw new Error (ERROR_MESSAGE.NOT_THOUSAND_WON)
   };
 
   totalLottoCounts(price) {
@@ -40,6 +43,7 @@ class App {
       this.randomNumberArr.push(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b));
       MissionUtils.Console.print(this.randomNumberArr[i]);
     };
+    this.userRandomNumber();
   };
 
   userRandomNumber() {
@@ -47,14 +51,15 @@ class App {
       this.winningLotto = number.split(",").map(Number);
       new Lotto(this.winningLotto);
       console.log(`당첨번호을 입력해 주세요.\n${this.winningLotto}`);
+      this.userBonusNumber();
     });
   };
 
   userBonusNumber() {
     MissionUtils.Console.readLine('보너스 번호를 입력해 주세요.', (bonusNumber) => {
       this.bonusLotto = parseInt(bonusNumber);
-      this.bonusCheck(this.bonusLotto);
       console.log(`보너스 번호를 입력해 주세요.\n${this.bonusLotto}`);
+      this.bonusCheck(this.bonusLotto);
     });
   };
  
@@ -64,7 +69,50 @@ class App {
     if (this.randomNumberArr.includes(bonusLotto)) throw new Error("당첨 번호와 중복되지 않은 숫자여야 합니다.");
 
     if (isNaN(bonusLotto) === true) throw new Error("보너스 번호에 올바른 값이 들어가지 않았습니다.");
+
+    this.randomNumberArr.forEach((randomNumberArr) => this.countLotto(randomNumberArr, this.winningLotto));
+    
+    this.printResult(this.winningRanking);
   };
+
+  countLotto(randomNumberArr, winningLotto) {
+    let count = 0;
+
+    randomNumberArr.forEach((randomNumberArr) => {
+      if (winningLotto.includes(randomNumberArr)) {
+        count++;
+      };
+    });
+
+    this.checkRanking(count, this.bonusLotto, winningLotto);
+  }
+
+  checkRanking(count, bonusLotto, myLotto) {
+    switch (count) {
+      case 3:
+        this.winningRanking.Fifth++;
+        break;
+      case 4:
+        this.winningRanking.Fourth++;
+        break;
+      case 5:
+        myLotto.includes(bonusLotto) ? this.winningRanking.Second++ : this.winningRanking.Third++;
+        break;
+      case 6:
+        this.winningRanking.First++;
+        break;
+    }
+  }
+
+  printResult(winningRanking) {
+    MissionUtils.Console.print("\n당첨 통계");
+    MissionUtils.Console.print("---");
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${winningRanking.Fifth}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${winningRanking.Fourth}개`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${winningRanking.Third}개`);
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningRanking.Second}개`);
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${winningRanking.First}개`);
+  }
 };
 
 module.exports = App;
