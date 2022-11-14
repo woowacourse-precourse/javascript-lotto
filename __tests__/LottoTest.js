79a4360450cd5b668b5d39de0ee1cea53ebc2a0c
thewronghand <penfreak77@gmail.com>
2022-11-14 14:51:08 +0900

    test: Lotto 클래스 numbers getter 작동 여부 테스트 코드 추가
    Lotto 클래스 내의 getter인 numbers를 사용하여 프라이빗 필드 #numbers의 값을 반환받는 기능이 제대로 작동하는지 테스트한다.

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,7 +7,6 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
@@ -42,5 +41,11 @@ describe('로또 클래스 테스트', () => {
     expect(() => {
       new Lotto([1, 2, 3, 50, 5, 6]);
     }).toThrow('[ERROR]');
+  });
+
+  test('getter를 사용해 lotto 인스턴스의 #numbers 필드 값을 확인할 수 있다', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const numberGetter = lotto.numbers;
+    expect(numberGetter).toEqual([1, 2, 3, 4, 5, 6]);
   });
 });
