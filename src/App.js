4186c60be04e8cbdd8c50382500ae183d9cb2e2b
jetty8013 <jetty8013@gmail.com>
2022-11-14 23:11:36 +0900

    feat: 로또 구입 금액 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.inputMoeny();
+    
+  }
+
+  buyingLotto() {
+    
+  }
+
+  inputMoeny() {
+    let userInput = 0;
+    MissionUtils.Console.readLine('구입 금액을 입력해 주세요.\n', (answer) => { 
+      userInput = answer;
+    });
+    if(Number(userInput)%1 === 0) return userInput;
+    throw new Error("[ERROR] 1,000원 단위로 구매할 수 있습니다.");
+  }
+
 }
 
 module.exports = App;
