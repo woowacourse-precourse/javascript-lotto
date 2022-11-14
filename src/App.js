49e877a40a6af65ff37fa67e71717d52f74fb9d5
pp449 <mma7710@naver.com>
2022-11-14 19:24:45 +0900

    feat: 당첨 통계 출력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App {
   #lottoPurchase() {
     const lottoNumber = parseInt(this.amount / 1000);
 
-    this.print(`${lottoNumber}개를 구매했습니다.`);
+    this.print(`\n${lottoNumber}개를 구매했습니다.`);
     for (let i = 0; i < lottoNumber; i++) {
       const lotto = new Lotto(RandomNumbers.generate());
       this.lottoList.push(lotto);
@@ -65,10 +65,22 @@ class App {
         this.numberOfWins[numberOfSuccess]++;
       }
     });
+
+    this.print_rank();
   }
 
   print(message) {
     Console.print(message);
+  }
+
+  print_rank() {
+    this.print(MESSAGE.PRINT_RANK_TITLE);
+    this.print('---');
+    this.print(MESSAGE.THREE_MATCHES + `${this.numberOfWins[4]}개`);
+    this.print(MESSAGE.FOUR_MATCHES + `${this.numberOfWins[3]}개`);
+    this.print(MESSAGE.FIVE_MATCHES + `${this.numberOfWins[2]}개`);
+    this.print(MESSAGE.FIVE_BONUS_MATCHES + `${this.numberOfWins[1]}개`);
+    this.print(MESSAGE.SIX_MATCHES + `${this.numberOfWins[0]}개`);
   }
 }
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,13 @@
 const MESSAGE = Object.freeze({
-  PURCHASE_AMOUT: '구입금액을 입력해 주세요.',
-  WINNING_NUMBER: '당첨 번호를 입력해 주세요.',
-  BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+  PURCHASE_AMOUT: '구입금액을 입력해 주세요.\n',
+  WINNING_NUMBER: '\n당첨 번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
+  PRINT_RANK_TITLE: '\n당첨 통계',
+  THREE_MATCHES: '3개 일치 (5,000원) - ',
+  FOUR_MATCHES: '4개 일치 (50,000원) - ',
+  FIVE_MATCHES: '5개 일치 (1,500,000원) - ',
+  FIVE_BONUS_MATCHES: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  SIX_MATCHES: '6개 일치 (2,000,000,000원) - ',
 });
 
 module.exports = MESSAGE;
