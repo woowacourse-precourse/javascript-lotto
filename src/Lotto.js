4a65e8134635fe444c872c68affdb96d245ead69
daal2quf <daal2quf@gmail.com>
2022-11-15 03:30:55 +0900

    feat(lotto): 유저가 구매한 로또와 당첨 숫자 몇개씩 맞았는지 확인 기능 추가
    add:
    sameNumberCount(): 유저가 구매한 각각의 로또 배열을 돌면서 당첨 숫자와 같은 만큼의 개수를 배열로 반환

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,6 @@
-const { Random, Console } = require("@woowacourse/mission-utils");
+const { Random, Console } = require('@woowacourse/mission-utils');
+const Calculator = require('./Calculator');
+const Utils = require('./Utils');
 
 class Lotto {
   #numbers;
@@ -7,29 +9,37 @@ class Lotto {
     this.validate(userInputWinNumbers);
     this.#numbers = userInputWinNumbers;
     this.bonusNumber;
-    this.lottoBundle;
+    this.bundle;
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
     if (numbers.length !== [...new Set(numbers)].length) {
-      throw new Error("[ERROR] 중복된 숫자가 없어야 합니다.");
+      throw new Error('[ERROR] 중복된 숫자가 없어야 합니다.');
     }
   }
 
   makeSixNumbers() {
-    return Random.pickUniqueNumbersInRange(1, 45, 6).sort((num1, num2) => num1 - num2);
+    return Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+      (num1, num2) => num1 - num2,
+    );
   }
 
   bundleCreate(lottoCount) {
-    this.lottoBundle = Array.from({ length: lottoCount }, this.makeSixNumbers);
-    return this.lottoBundle;
+    this.bundle = Array.from({ length: lottoCount }, this.makeSixNumbers);
+    return this.bundle;
   }
 
   getBonusNumber(UserInputBonusNumber) {
     this.bonusNumber = UserInputBonusNumber;
+  }
+
+  sameNumberCount(winNumbers, userPurchaselottos) {
+    const calculator = new Calculator();
+    const countArray = userPurchaselottos.map((lotto) => calculator.matchNumberCount(winNumbers, lotto));
+    return countArray;
   }
 }
 
