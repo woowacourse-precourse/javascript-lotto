005d58297c7cdf7801e4c52556f0c2ec1095ff7b
chsua <csj1919@naver.com>
2022-11-14 16:09:14 +0900

    feat: 2. 랜덤숫자 발행하기(App.js)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 
 class App {
+  #lottos // Array[Lotto]
+  constructor() {
+    this.#lottos = []
+  }
   
   play() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.", money => {
@@ -12,6 +16,16 @@ class App {
       this.makeWinningLotto();       // 뒤에 당첨번호 생성
     });
   }
+
+  makeUserLotto() {
+    for (let i = 0; i < this.money / 1000; i++) {
+      let randomArr = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      let lotto = new Lotto(randomArr.sort((a, b) => a - b));
+      MissionUtils.Console.print(`[${lotto.numbers.join(", ")}]`);
+      this.#lottos.push(lotto)
+    }
+  }
+
 }
 
 module.exports = App;
