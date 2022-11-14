a0e10b24f8f351386f880dd358787ee5da370dae
nerdchanii <nerdchanii@gmail.com>
2022-11-14 14:08:37 +0900

    feat: 로도클래스의 예외처리 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,11 +13,14 @@ class Lotto {
     if (numbers.length !== LOTTO.NUMBER_COUNT) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
-    if (!numbers.every((number) => number >= LOTTO.MIN_NUMBER && number <= LOTTO.MAX_NUMBER)) {
+    if (!numbers.every(isInRange(LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER))) {
       throw new Error('[ERROR] 로또 번호는 1부터 45까지입니다.');
     }
-    if (new Set(numbers).size !== LOTTO.NUMBER_COUNT) {
+    if (!isUnique(numbers)) {
       throw new Error('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
+    }
+    if (!numbers.every(isNumber)) {
+      throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
     }
   }
 
