ec1bb8785c119d9cd6973cce65e35e46028ab796
semnil5202 <semnil5202@gmail.com>
2022-11-15 07:18:48 +0900

    fix(Yield): 클래스, 메서드, 변수명 수정
    yield 라는 단어가 자바스크립트 제너레이터 예약어여서 가독성을 해치므로 earningRate로 수정한다.

--- src/components/Yield.js
+++ src/components/Yield.js
@@ -1,15 +1,15 @@
-class Yield {
-  static calculateYield(winPlace, purchaseAmount) {
+class EarningRate {
+  static getEarningRate(winPlace, purchaseAmount) {
     const prizes = [2000000000, 30000000, 1500000, 50000, 5000];
+    let earningRate = 0;
     let index = 0;
-    let yield = 0;
     for (let key in winPlace) {
-      yield += winPlace[key] * prizes[index];
+      earningRate += winPlace[key] * prizes[index];
       index += 1;
     }
-    yield = (yield / purchaseAmount) * 100;
-    return yield;
+    earningRate = (earningRate / purchaseAmount) * 100;
+    return earningRate;
   }
 }
 
-module.exports = Yield;
+module.exports = EarningRate;
