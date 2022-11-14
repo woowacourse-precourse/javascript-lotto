224ca9c6020398bccc9f938c3b2c7f05ce4baf05
nailen1 <nietsnie1@gmail.com>
2022-11-14 23:33:13 +0900

    feat: 모든 로또들의 당첨 여부와 상금 목록 저장 기능

--- src/Toolbox.js
+++ src/Toolbox.js
@@ -1,5 +1,10 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
 
+function showMylottos(storeClass) {
+    printMessage(`\n${storeClass.amount}개를 구매했습니다.`)
+    storeClass.generatedLottos.forEach(numbersArr => printMessage(`[${numbersArr.join(', ')}]`));
+}
+
 function printMessage(sentence) {
     Console.print(sentence);
 }
@@ -9,13 +14,31 @@ function generateRandomNumbers(min, max, numbers) {
     return randoms.map(x => Number(x)).sort((i, j) => i - j);
 }
 
-function showMylottos(storeClass) {
-    printMessage(`\n${storeClass.amount}개를 구매했습니다.`)
-    storeClass.generatedLottos.forEach(numbersArr => printMessage(`[${numbersArr.join(', ')}]`));
+function getMatchingResult(storeClass, lottoClasses) {
+    lottoClasses.forEach(lottoClass => lottoClass.matching(storeClass.winningNumbers, storeClass.bonusNumber));
 }
+
+function getPrizeResult(lottoClasses) {
+    lottoClasses.forEach(lottoClass => lottoClass.prize());
+}
+
+function numberOfPrizes(prizeMoney, lottoClasses) {
+    return lottoClasses.filter(lottoClass => lottoClass.prizeResult.money === prizeMoney).length;
+}
+
+function getRecord(storeClass, lottoClasses) {
+    storeClass.record.forEach(prize => {
+        prize.numbers = numberOfPrizes(prize.money, lottoClasses);
+    });
+}
+
 
 module.exports = {
     showMylottos,
     printMessage,
     generateRandomNumbers,
+    getMatchingResult,
+    getPrizeResult,
+    numberOfPrizes,
+    getRecord,
 }
