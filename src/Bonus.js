5424207fb497ad184afc02c690e3d1c60b87907a
dhkham <khamdong@gmail.com>
2022-11-14 22:13:19 +0900

    feat(Bonus.js): add method that checks if bonus number is in between 1-45

--- src/Bonus.js
+++ src/Bonus.js
@@ -15,6 +15,11 @@ class Bonus {
       }
     }
   
+    validateBonusNumBetween1To45(numbers) {
+        if (numbers < 1 || numbers > 45) {
+          throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+      }
     
   }
   
