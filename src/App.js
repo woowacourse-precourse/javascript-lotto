9a3b35c2afc1f524349fa17de08aa8f2b83cc1a3
kaori-killer <sksbthwjd@daum.net>
2022-11-14 17:14:45 +0900

    feat: 수익 금액 확인 및 계산한다.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ const Lotto = require('./Lotto');
 
 class App {
   constructor() {
+    this.userAmount = 0;
     this.userLottos = [];
     this.winningLotto = [];
     this.winningBonusNumber = 0;
-    this.winningResult = [0, 0, 0, 0, 0]  
+    this.winningResult = [[0, 5000], [0, 50000], [0, 1500000], [0, 30000000], [0, 2000000000]];  
   }
 
   play() {
     Utils.readLine('구입금액을 입력해 주세요.' , (input) => {
-      const amount = this.validateAmount(Number(input));
+      this.userAmount = Number(input);
+      const amount = this.validateAmount(this.userAmount);
       const quantity = this.countLottoQuantity(amount);
       Utils.print(`${quantity}개를 구매했습니다.`);
       this.userLottos = this.createLotto(quantity);
@@ -28,6 +30,7 @@ class App {
               this.matchRank(matchCount);
             }
             Utils.print(this.setMatchResult());
+            Utils.print(`총 수익률은 ${this.setRevenue()}%입니다.`);
             Utils.close();
         });
       });
@@ -67,7 +70,6 @@ class App {
         result += `${number}, `;
       }
     }
-
     result += `]`;
 
     Utils.print(result);
@@ -92,26 +94,36 @@ class App {
   }
   matchRank(matchCount){
   if(matchCount == 5 && userLotto.includes(this.winningBonusNumber)){
-    this.winningResult[3] += 1;
+    this.winningResult[3][0] += 1;
     return
   }
   if(matchCount == 6){
-    this.winningResult[4] += 1
+    this.winningResult[4][0] += 1
     return
   }
   if(2 < matchCount && matchCount < 6) {
-    this.winningResult[matchCount-3] += 1;
+    this.winningResult[matchCount-3][0] += 1;
   }
 }
   setMatchResult(){
     const result = `
-    3개 일치 (5,000원) - ${this.winningResult[0]}개
-    4개 일치 (50,000원) - ${this.winningResult[1]}개
-    5개 일치 (1,500,000원) - ${this.winningResult[2]}개
-    5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.winningResult[3]}개
-    6개 일치 (2,000,000,000원) - ${this.winningResult[4]}개
+    3개 일치 (5,000원) - ${this.winningResult[0][0]}개
+    4개 일치 (50,000원) - ${this.winningResult[1][0]}개
+    5개 일치 (1,500,000원) - ${this.winningResult[2][0]}개
+    5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.winningResult[3][0]}개
+    6개 일치 (2,000,000,000원) - ${this.winningResult[4][0]}개
     `
     return result;
+  }
+
+  setRevenue(){
+    // 수익률 = (현재 가치 - 초기 가치) / 초기 가치 X 100 
+    let revenue = 0;
+    for(let index = 0; index < this.winningResult.length; index++){
+      revenue += this.winningResult[index][0] * this.winningResult[index][1];
+    }
+    const totalRevenue = 100 - ((this.userAmount - revenue) / this.userAmount * 100);
+    return totalRevenue;
   }
 }
 
