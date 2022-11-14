7583b3df75c19cc5e0e52b02f0c10885a72e7fca
Monte <monte6198@gmail.com>
2022-11-15 07:50:39 +0900

    feat: 보너스 번호 입력 기능 추가

--- src/LottoGenerator.js
+++ src/LottoGenerator.js
@@ -1,18 +1,26 @@
 const { Console } = require("@woowacourse/mission-utils");
 const Lotto = require("../src/Lotto");
+const LottoValidation = require("./Validation/LottoValidation");
 
 class LottoGenerator {
   winningNumber;
+  bonusNumber;
   constructor() {}
   getWinningNumbers() {
     Console.readLine("\n당첨 번호를 입력해 주세요.\n", (userInput) => {
       const lottoNumber = this.changeToNumbers(userInput);
       this.winningNumber = new Lotto(lottoNumber); // 로또 번호 생성기를 통해 로또 당첨 번호 생성
+      this.getBonusNumber();
     });
   }
   changeToNumbers(numberString) {
     const numbers = numberString.split(",");
     return numbers;
+  }
+  getBonusNumber() {
+    Console.readLine("\n보너스 번호를 입력해 주세요.\n", (userInput) => {
+      //   this.bonusNumber = Number(userInput);
+    });
   }
 }
 
