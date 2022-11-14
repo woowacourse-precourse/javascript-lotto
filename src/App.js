b26164cb90ff706a5f3adb1856dd14018e3de400
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-14 19:58:46 +0900

    feat(App.js, Lotto.js) : 구입금액에 맞는 로또 발행
    - (App.js-makeLottoAuto)Random유틸을 사용하여 6자리 숫자생성 및
    오름차순으로 정렬하는 기능 구현
    - (App.js-publishLotto)유저에게 금액을 입력받아 금액에 맞게
    로또를 발행하고, 상태값으로 이를 가지고 있음
    - (Lotto.js-publishLotto)Console유틸을 사용하여 로또를 발행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+const { GAME, MESSAGE } = require('./modules/Constant');
+const Lotto = require('./Lotto');
+
 class App {
-  play() {}
+  constructor() {
+    this.publishedLottos = [];
+    this.amount = 0;
+  }
+
+  play() {
+    this.publishLotto();
+  }
+
+  publishLotto() {
+    Console.readLine(MESSAGE.BUY, (amount) => {
+      // TODO: 입력 금액의 유효성검사
+      // 인풋은 숫자를 입력해도 문자열로 들어온다
+      // 문자열 => 숫자 했는데 숫자가아니면 무효!
+      // 1000의 배수가 아니면 무효
+      this.amount = Number(amount);
+
+      const countOfLotto = this.amount / GAME.PRICE; // 1000 상수처리
+
+      for (let i = 0; i < countOfLotto; i++) {
+        const lotto = new Lotto(this.makeLottoAuto());
+        lotto.publishLotto();
+        this.publishedLottos.push(lotto);
+      }
+
+      Console.print(`${MESSAGE.CONFIRM_BUY(countOfLotto)}`);
+
+      // TODO: 당첨번호 입력받기
+    });
+  }
+
+  makeLottoAuto() {
+    const lotto = Random.pickUniqueNumbersInRange(GAME.START, GAME.END, GAME.COUNT);
+    return lotto.sort((prev, next) => prev - next);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
