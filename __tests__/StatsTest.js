fec846c378dc6771531dcd660f0bdf570573f962
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-15 02:46:04 +0900

    test: gather method
    Add test of gather method of Stats class
    Modified arguments of Stats instance in first test of StatsTest.js, because of addition of method to the class

--- __tests__/StatsTest.js
+++ __tests__/StatsTest.js
@@ -6,7 +6,7 @@ describe("통계 기능 검증", () => {
   test("맞춘 번호 개수 세기", () => {
     const winningNumbers = [1, 2, 3, 4, 5, 6];
     const bonusNumber = 7;
-    const stats = new Stats({ winningNumbers, bonusNumber, purchased: [] });
+    const stats = new Stats({ winningNumbers, bonusNumber, purchased: { lottoArray: [] } });
     const lottoArray = [
       new Lotto([41, 42, 43, 44, 32, 33]),
       new Lotto([11, 3, 32, 44, 15, 26]),
@@ -23,6 +23,33 @@ describe("통계 기능 검증", () => {
     lottoArray.forEach((lotto, index) => {
       const score = stats.getScore(lotto);
       expect(score).toBe(scoreArray[index]);
+    });
+  });
+
+  test("통계 내기", () => {
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 7;
+    const purchased = {
+      lottoArray: [
+        new Lotto([41, 42, 43, 44, 32, 33]),
+        new Lotto([11, 3, 32, 44, 15, 26]),
+        new Lotto([11, 3, 6, 14, 25, 22]),
+        new Lotto([1, 2, 3, 27, 35, 26]),
+        new Lotto([4, 3, 1, 27, 35, 2]),
+        new Lotto([5, 4, 6, 27, 2, 1]),
+        new Lotto([5, 3, 1, 7, 2, 6]),
+        new Lotto([6, 4, 3, 2, 5, 1]),
+      ],
+    };
+    const stats = new Stats({ winningNumbers, bonusNumber, purchased });
+
+    expect(stats.data).toEqual({
+      underThree: 3,
+      three: 1,
+      four: 1,
+      five: 1,
+      fivePlusBonus: 1,
+      six: 1,
     });
   });
 });
