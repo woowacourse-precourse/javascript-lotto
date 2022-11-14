35a72ef8138d8ec67ec35ee94b9d81ecc5e64176
saram <yujun1018@naver.com>
2022-11-14 17:43:28 +0900

    refactor: 로또 번호 생성 함수 변경

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,25 +1,32 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
 
 const { PHRASE, ERROR, LOTTO } = require('./constants');
+const DrawMachine = require('./DrawMachine');
 const Lotto = require('./Lotto');
 
 class VendingMachine {
-  #lottoNumber = [];
+  #lottoNumber;
+
+  constructor() {
+    this.drawMachine = new DrawMachine();
+  }
 
   askPurchaseAmount() {
     Console.readLine(PHRASE.PURCHASE_AMOUNT, (input) => {
       this.validate(input);
       this.makeLotto(input);
+      this.drawMachine.darwWinningNumber();
     });
   }
 
   validate(input) {
     if (isNaN(input)) throw new Error(ERROR.PURCHASE_AMOUNT_TYPE);
-    if (input % 1000 !== 0 || input == 0) throw new Error(ERROR.PURCHASE_AMOUNT_UNIT);
+    if (input % LOTTO.PRICE !== 0 || input == 0)
+      throw new Error(ERROR.PURCHASE_AMOUNT_UNIT);
   }
 
   makeLotto(input) {
-    const count = input / 1000;
+    const count = input / LOTTO.PRICE;
     this.printLottoCount(count);
 
     for (let i = 0; i < count; i++) {
@@ -30,12 +37,12 @@ class VendingMachine {
   }
 
   generateLottoNumber() {
-    this.#lottoNumber.splice(0);
-
-    while (this.#lottoNumber.length < LOTTO.NUMBERS_COUNT) {
-      const number = Random.pickNumberInRange(LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER);
-      if (!this.#lottoNumber.includes(number)) this.#lottoNumber.push(number);
-    }
+    const number = Random.pickUniqueNumbersInRange(
+      LOTTO.MIN_NUMBER,
+      LOTTO.MAX_NUMBER,
+      LOTTO.NUMBERS_COUNT
+    );
+    this.#lottoNumber = number;
   }
 
   printLottoCount(count) {
