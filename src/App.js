5ceae8bd52a757ec715bcb10ab50df9506f08622
YejinHwang-D <hyj3463@naver.com>
2022-11-14 21:46:12 +0900

    feat: 로또 구매 수량 계산 및 예외처리

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ class App {
   }
 
   play() {
+    let amount;
+    
     this.purchase = this.input('구입금액을 입력해 주세요.\n');
     if (this.purchase === undefined)
       return 0;
     this.input_exception();
+
+    amount = this.get_quantity();
   }
 
   input(text) {
@@ -32,6 +36,15 @@ class App {
       MissionUtils.Console.close();
       throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
+  }
+
+  get_quantity() {
+    let amount = Number(this.purchase);
+    if (amount % 1000 !== 0) {
+      throw new Error("[ERROR] 1,000원 단위가 아닙니다.");
+    }
+    let result = amount / 1000;
+    return result;
   }
 }
 
