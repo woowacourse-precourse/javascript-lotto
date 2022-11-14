c22a59610065e10caa503aec69ec4a7a33812958
yeonkyoung <keep.ykk@gmail.com>
2022-11-15 02:05:02 +0900

    feat: 입력받은 당첨 번호 확인 메서드 구현

--- src/WinningLotto.js
+++ src/WinningLotto.js
@@ -4,6 +4,17 @@ const terms = require("./constants/terms");
 
 class WinningLotto {
   constructor(numbers) {}
+
+  checkWinningNumber(lottoNumber) {
+    const lottoArray = lottoNumber.split(",");
+    if (!this.checkLottoLength(lottoArray)) {
+      return { result: false, errorMessage: messages.TOTAL_NUMBER_ERROR };
+    }
+    if (!this.checkLottoIsNumber(lottoArray)) {
+      return { result: false, errorMessage: messages.NOT_A_NUMBER_ERROR };
+    }
+    return { result: true };
+  }
 }
 
 module.exports = WinningLotto;
