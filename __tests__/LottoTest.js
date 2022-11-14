be520a30d4c532cd064618944426a30cd5076e90
ryuna <anottrx@gmail.com>
2022-11-14 21:37:00 +0900

    test: 로또 번호가 아닌 문자가 입력될 경우 테스트케이스 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,24 +7,28 @@ describe('로또 클래스 테스트', () => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
     }).toThrow('[ERROR]');
   });
+
   test('로또 번호의 개수가 6개가 안되면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3]);
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow(ERROR_MESSAGE.LOTTO.NUMBER_DUPLICATED);
   });
 
-  test(ERROR_MESSAGE.LOTTO.NUMBER_RANGE, () => {
+  test('로또 번호는 1부터 45 사이의 숫자가 아니면 예외가 발생한다', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 55]);
     }).toThrow(ERROR_MESSAGE.LOTTO.NUMBER_RANGE);
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또 번호에 숫자가 아닌 문자가 있으면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 'a']);
+    }).toThrow(ERROR_MESSAGE.LOTTO.NOT_NUMBER);
+  });
 });
