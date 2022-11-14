796d92bcfcfe6cd55e39cb8a2f6736f1913771d6
saetakki <lime.lemonade03@gmail.com>
2022-11-15 07:12:37 +0900

    feat. Add lottoGenerator function

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,14 +1,17 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-const { isNotDuplicated } = require('./Validator');
+const { isNotDuplicated, isNotExceedAmount } = require('./Validator');
 const Console = MissionUtils.Console;
 const randomNums = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-console.log(randomNums);
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    Lotto.validate(numbers, isNotDuplicated(numbers));
+    Lotto.validate(
+      numbers,
+      isNotExceedAmount(numbers),
+      isNotDuplicated(numbers),
+    );
     this.#numbers = numbers;
   }
 
@@ -22,11 +25,13 @@ class Lotto {
     return input.split(',').map(x => Number(x));
   };
 
-  // TODO: 추가 기능 구현
-  static LottoGenerator = () => {
-    return new Lotto(randomNums);
+  static lottoGenerator = () => {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+      (a, b) => a - b,
+    );
   };
+
+  // TODO: 추가 기능 구현
 }
 
-const lotto = new Lotto(randomNums);
 module.exports = Lotto;
