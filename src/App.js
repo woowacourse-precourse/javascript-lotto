93ee1394dcc7f3cc97bc7ae449cd3487045d0a52
kimeodml <kimeodml@gmail.com>
2022-11-14 22:01:22 +0900

    feat: 로또 구입 금액 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGES } = require("./constants/Constants.js");
+
+
 class App {
-  play() {}
+  #count = 0;
+
+  play() {
+    this.start();
+  } 
+
+  start() {
+    Console.readLine(MESSAGES.PAY_COST, (input) => {
+    });
+  }
+
 }
 
+const app = new App();
+app.play();
 module.exports = App;
