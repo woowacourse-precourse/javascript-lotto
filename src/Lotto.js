f6206eea8a07ba6c84bad2ca70f7505c2665a46b
Sangjun-man <molung@icloud.com>
2022-11-15 00:40:05 +0900

    feat : Lotto 클래스 구현
    로또 생성하기, 로또번호 유효성 확인, 로또 당첨 확인 메서드가 있는 Lotto 클래스를 구현했습니다

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,120 @@
+const { Random } = require("@woowacourse/mission-utils");
+const keys = require("./utils/key");
+const CustomError = require("./CustomError");
+const ExceptionCheck = require("./ExceptionCheck");
+const ErrorMessage = require("./utils/const/error");
 class Lotto {
-  #numbers;
+  #numbers = null;
 
-  constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
-  }
+  constructor() {
+    const lottoNumber = this.constructor.createLotteryNumber();
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (this.constructor.validate(lottoNumber)) {
+      this.#numbers = lottoNumber;
     }
   }
 
-  // TODO: 추가 기능 구현
+  static createLotteryNumber() {
+    const lottoNumber = [];
+    while (lottoNumber.length < 6) {
+      const randomNumber = Random.pickNumberInRange(1, 45);
+      if (lottoNumber.includes(randomNumber)) continue;
+      lottoNumber.push(Number(randomNumber));
+    }
+    return lottoNumber.sort((a, b) => a - b);
+  }
+
+  static validate(lottoNumbers) {
+    return new LottoExceptionCheck().check(
+      keys.lotto.lotteryNumber,
+      lottoNumbers
+    );
+  }
+
+  winningConfrim(winninglottoNumber) {
+    if (!this.#numbers)
+      throw new CustomError(ErrorMessage.lottoNumberNonexistence);
+
+    const { lottery: winningLottery = [], bonus: winningBonus = 0 } =
+      winninglottoNumber;
+
+    if (
+      !new LottoExceptionCheck().check(
+        keys.lotto.lottoNumber,
+        winninglottoNumber
+      )
+    )
+      throw new CustomError(ErrorMessage.notLottoNumber);
+
+    const correct = this.#lotteryCheck(winningLottery);
+    const isBonusCorrect = this.#bonusCheck(winningBonus);
+    const winningConfrimResult = { correct, isBonusCorrect };
+    return winningConfrimResult;
+  }
+
+  #lotteryCheck(winningLottery) {
+    let count = 0;
+    for (let winning of winningLottery) {
+      for (let number of this.#numbers) {
+        if (winning === number) count++;
+      }
+    }
+    return count;
+  }
+  #bonusCheck(winningBonus) {
+    for (let number of this.#numbers) {
+      if (number === winningBonus) return true;
+    }
+  }
+}
+
+class LottoExceptionCheck extends ExceptionCheck {
+  lotteryNumber(lottery) {
+    const lottoNumReg = /(^[1-9]$)|(^[1-3]{1}[0-9]{1}$)|(^4{1}[0-5]{1}$)/;
+    const overLap = {};
+    if (!Array.isArray(lottery)) {
+      throw new CustomError(ErrorMessage.notNumberArray);
+    }
+    if (lottery.length !== 6)
+      throw new CustomError(ErrorMessage.numberListLengthMustSix);
+    lottery.forEach((num, i, arr) => {
+      overLap[num] ? (overLap[num] += 1) : (overLap[num] = 1);
+      if (!lottoNumReg.test(num))
+        throw new CustomError(ErrorMessage.notNumberInRange);
+      if (typeof num != "number") throw new CustomError(ErrorMessage.notNumber);
+      if (overLap[num] > 1) throw new CustomError(ErrorMessage.overLapNumber);
+      if (i == 0) return;
+      if (arr[i - 1] > arr[i]) {
+        throw new CustomError(ErrorMessage.notSortedNumberList);
+      }
+    });
+  }
+
+  lottoNumber(lottoNumber) {
+    const { lottery = [], bonus = 0 } = lottoNumber;
+
+    if (!(bonus || typeof bonus !== "number"))
+      throw new CustomError(ErrorMessage.notBonusNumber);
+
+    if (lottery.length !== 6)
+      throw new CustomError(ErrorMessage.numberListLengthMustSix);
+
+    if (!this.lotteryNumber(lottery)) {
+      throw new CustomError(ErrorMessage.notLotteryNumber);
+    }
+
+    if (!this.bonusNumber(bonus)) {
+      throw new CustomError(ErrorMessage.notBonusNumber);
+    }
+    return true;
+  }
+  bonusNumber(bonusNumber, lottery) {
+    if (!(bonus || typeof bonus !== "number"))
+      throw new CustomError(ErrorMessage.notBonusNumber);
+
+    if (lottery.includes(bonusNumber))
+      throw new CustomError(ErrorMessage.notBonusNumber);
+  }
 }
 
 module.exports = Lotto;
--- src/utils/key.js
+++ src/utils/key.js
@@ -1,12 +1,17 @@
 const KEY_PURCHASE_MONEY = "purchaseMoney";
 const KEY_LOTTERY_NUMBER = "lotteryNumber";
 const KEY_BONUS_NUMBER = "bonusNumber";
+const KEY_LOTTO_NUMBER = "lotteryNumber";
 
 const keys = {
   inputType: {
     purchaseMoney: KEY_PURCHASE_MONEY,
     lotteryNumber: KEY_LOTTERY_NUMBER,
     bonusNumber: KEY_BONUS_NUMBER,
+  },
+  lotto: {
+    lotteryNumber: KEY_LOTTERY_NUMBER,
+    lotteryNumber: KEY_LOTTO_NUMBER,
   },
 };
 
