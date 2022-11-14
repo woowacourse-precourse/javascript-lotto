4eaa982bfff22958d2d09bbdd8199e7dd4f18be1
hjy0951 <hjy0951@gmail.com>
2022-11-14 23:48:22 +0900

    fix: buyLottos 오타 수정 및 util 모듈 추가

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -1,17 +1,19 @@
+const { Random } = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 
 class LottoMachine {
-  buyLotto(money) {
+  buyLottos(money) {
     const lottos = [];
     while (money > 0) {
       const numbers = this.createLottoNumbers();
       lottos.push(new Lotto(numbers));
+      money -= 1000;
     }
     return lottos;
   }
 
   createLottoNumbers() {
-    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    return Random.pickUniqueNumbersInRange(1, 45, 6);
   }
 }
 
