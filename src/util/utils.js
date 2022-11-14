bf64bd4a299115d718953fdcaacb11f28bb81dd6
thewronghand <penfreak77@gmail.com>
2022-11-15 03:22:53 +0900

    refactor: utils.js 문서 함수 내용 간소화
    입력값의 적합성을 판별하는 함수에서 축약 가능한 내용을 하나로 합쳐 15 라인 이내로 축약한다.

--- src/util/utils.js
+++ src/util/utils.js
@@ -26,14 +26,8 @@ const isValidLottoNumbers = (numbers) => {
   if (hasNonNumber(numbers)) {
     throw new Error('[ERROR] 로또 번호는 숫자로만 구성되어야 합니다.');
   }
-  if (hasNonInteger(numbers)) {
-    throw new Error('[ERROR] 로또 번호에는 소수점 이하가 허용되지 않습니다.');
-  }
-  if (hasNegativeNumber(numbers)) {
-    throw new Error('[ERROR] 로또 번호에는 음수가 포함될 수 없습니다.');
-  }
-  if (hasInvalidSizeNumber(numbers)) {
-    throw new Error('[ERROR] 로또 번호는 1 이상 45 이하여야 합니다.');
+  if (hasNonInteger(numbers) || hasInvalidSizeNumber(numbers)) {
+    throw new Error('[ERROR] 로또 번호는 1 이상 45 이하의 자연수여야 합니다.');
   }
   if (hasDuplicateNumbers(numbers)) {
     throw new Error('[ERROR] 로또 번호에 중복된 숫자가 포함되어 있습니다.');
@@ -44,16 +38,10 @@ const isValidLottoBonusNumber = (number, winningNumbers) => {
   if (isNaN(number) || number === undefined) {
     throw new Error('[ERROR] 보너스 번호는 하나의 숫자만 허용됩니다.');
   }
-  if (!Number.isInteger(number)) {
+  if (!Number.isInteger(number) || isInvalidSize(number)) {
     throw new Error(
-      '[ERROR] 보너스 번호 입력 시 소수점 이하는 허용되지 않습니다.'
+      '[ERROR] 보너스 번호는 1 이상 45 이하의 자연수여야 합니다.'
     );
-  }
-  if (isNegativeNumber(number)) {
-    throw new Error('[ERROR] 보너스 번호 입력 시 음수는 허용되지 않습니다.');
-  }
-  if (isInvalidSize(number)) {
-    throw new Error('[ERROR] 보너스 번호는 1 이상 45 이하여야 합니다.');
   }
   if (winningNumbers.includes(number)) {
     throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
