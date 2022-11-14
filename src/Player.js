8b0fbb954edbf7f5b31d43bfaa65f69bca37a92d
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-14 15:57:24 +0900

    feat: add validation for input value

--- src/Player.js
+++ src/Player.js
@@ -1,3 +1,20 @@
+const { Random } = require("@woowacourse/mission-utils");
+const { REG_EXP } = require("./RegEx");
+const { ERROR } = require("./Error");
 class Player {
   #lottos;
+  constructor() {
+    this.#lottos = [];
+  }
+  validateMoney = (money) => {
+    if (!REG_EXP.onlyNumber.test(money)) {
+      throw new Error(ERROR.notNumber);
+    }
+    if (Number(money) % 1000 !== 0) {
+      throw new Error(ERROR.cannotDivide);
+    }
+    return true;
+  };
 }
+
+module.exports = { Player };
