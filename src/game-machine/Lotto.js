540e3c25a505bf738a6c01387dc1ec31477370db
SorinKwon <ksl020700@gmail.com>
2022-11-15 01:16:11 +0900

    feat: 로또 수량을 받아와서 저장하는 기능 추가

--- src/game-machine/Lotto.js
+++ src/game-machine/Lotto.js
@@ -1,16 +1,24 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const CalculateLotto = require("./CalculateLotto");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
+    this.lotto_quantity;
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    
+  }
+
+  getLottoQuantity(lotto_quantity){
+    this.lotto_quantity = lotto_quantity;
   }
 }
 module.exports = Lotto;
