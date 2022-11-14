013334e7efc1bfaba1feec89bebdd1e1f0c1ba33
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-14 20:40:27 +0900

    refactor: 예외 케이스 및 순서 바꾸기
    - 음수인 경우는 -가 붙어서 정규표현식 검사시 예외처리에 포함된다
    - 1000원으로 나누어 떨어지는지 검사는 정규표현식으로 값이 숫자인지 확인 후 하는 게 맞아서 바꿈 처리

--- src/Purchase.js
+++ src/Purchase.js
@@ -25,16 +25,13 @@ class Purchase {
   }
 
   validateCashInput(value) {
-    if (value % MONEY_UNIT !== 0) {
-      throw new Error(ERROR_MESSAGE.NOT_DIVIDE_BY_THOUSAND_ERROR);
-    }
-    if (value <= 0) {
-      throw new Error(ERROR_MESSAGE.NOT_POSITIVE_NUMBER_ERROR);
-    }
     const regExp = /[0-9]/g;
     const matchArr = value.match(regExp);
     if (matchArr.length !== value.length) {
       throw new Error(ERROR_MESSAGE.NOT_NUMBER_ERROR);
+    }
+    if (value % MONEY_UNIT !== 0) {
+      throw new Error(ERROR_MESSAGE.NOT_DIVIDE_BY_THOUSAND_ERROR);
     }
   }
 }
