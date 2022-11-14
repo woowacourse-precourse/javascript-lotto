f5be1d9aeb6384b784b94e5e6741a27b1095f65b
useon <yoosk4986@naver.com>
2022-11-15 06:11:17 +0900

    feat(Lotto): 당첨 통계를 구한다
    발행 목록 중 당첨 번호와 3개, 4개, 5개, 5개+보너스, 6개 일치하는 각 개수를 구한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const Lotto = require('./Lotto');
+const Issue = require('./Issue');
+const Draw = require('./Draw');
+
 class App {
-  play() {}
+  constructor() {
+    this.issue = new Issue();
+    this.draw = new Draw();
+  }
+
+  play() {
+    const issueList = this.issue.purchase();
+    const totalNumbers = this.draw.winning();
+    new Lotto(totalNumbers, issueList);
+  }
 }
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,37 @@
 class Lotto {
   #numbers;
-
-  constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
+  constructor(totalNumbers, issueList) {
+    this.#numbers = totalNumbers;
+    this.issueList = issueList;
+    this.winningNumber = this.#numbers.slice(0, 6);
+    this.bonusNumber = this.#numbers.slice(6);
+    this.purchaseAmount = issueList.size * 1000;
+    this.winningContentArr = ['3개 일치', '4개 일치', '5개 일치', '5개 일치, 보너스 볼 일치', '6개 일치'];
+    this.winningAmountArr = ['5,000', '50,000', '1,500,000', '30,000,000', '2,000,000,000'];
+    this.winningCounterArr = new Array(5).fill(0);
+    this.statistics();
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  statistics() {
+    this.issueList.forEach((issueNumber) => {
+      const matchCount = issueNumber.filter((number) => this.winningNumber.includes(number)).length;
+      if (matchCount === 3) {
+        this.winningCounterArr[0] += 1;
+      }
+      if (matchCount === 4) {
+        this.winningCounterArr[1] += 1;
+      }
+      if (matchCount === 5 && issue.includes(this.bonusNumber) !== false) {
+        this.winningCounterArr[2] += 1;
+      }
+      if (matchCount === 5 && issue.includes(this.bonusNumber)) {
+        this.winningCounterArr[3] += 1;
+      }
+      if (matchCount === 6) {
+        this.winningCounterArr[4] += 1;
+      }
+    });
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
