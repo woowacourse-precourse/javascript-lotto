e272b0f90df6b948466c98239d1515825f41b49c
y00eunji <aine0227@naver.com>
2022-11-15 03:55:03 +0900

    feat(InputView): Input Win numbers

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,5 +1,6 @@
 const INPUT_MESSAGE = Object.freeze({
   BUY: "구입금액을 입력해 주세요.\n",
+  WIN_NUMBER: "\n당첨번호를 입력해주세요.\n",
 });
 
 const OUTPUT_MESSAGE = Object.freeze({
@@ -10,7 +11,6 @@ const ERROR_MESSAGE = Object.freeze({
   NOT_NUMBER: "[ERROR] 로또 구입금액은 숫자여야 합니다.",
   IS_EMPTY: "[ERROR] 로또 구입금액을 입력해주세요.",
   CANT_DIVIDE: "[ERROR] 로또 구입금액을 1000단위로 입력해주세요.",
-  OVER_LENGTH: "[ERROR] 로또 번호는 6개여야 합니다.",
   MAX_MONEY: "[ERROR] 로또 구입금액이 너무 큽니다.",
   WORNG_LENGTH: "[ERROR] 로또 번호는 6개여야 합니다.",
   IS_DUPLICATE: "[ERROR] 로또 번호가 중복되었습니다.",
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { INPUT_MESSAGE } = require("../constants/message");
+const { INPUT_MESSAGE, OUTPUT_MESSAGE } = require("../constants/message");
 const LottoGameController = require("../LottoGameController");
 
 class InputView {
@@ -10,9 +10,22 @@ class InputView {
   }
 
   inputMoney() {
-    Console.readLine(INPUT_MESSAGE.BUY, (money) =>
-      this.#lottoGameController.start(money)
-    );
+    Console.readLine(INPUT_MESSAGE.BUY, (money) => {
+      const lottos = this.#lottoGameController.start(money);
+      this.#showLottos(lottos);
+    });
+  }
+
+  #showLottos(lottos) {
+    Console.print(OUTPUT_MESSAGE.BUY(lottos.size()));
+    Console.print(lottos.toString());
+    this.#inputWinNumber();
+  }
+
+  #inputWinNumber() {
+    Console.readLine(INPUT_MESSAGE.WIN_NUMBER, (numbers) => {
+      console.log(numbers);
+    });
   }
 }
 
