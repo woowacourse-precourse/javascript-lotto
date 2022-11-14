aceb551097e1689fef18f2610609909d0ca213d6
ElAsJay <tjdwnwkd12371@gmail.com>
2022-11-15 01:56:43 +0900

    feat($App): inputMoney method

---
+++ docs/README.md
@@ -0,0 +1,10 @@
+## JavaScript-LOTTO Commit List
+
+feat($App): inputMoney method
+
+Added new method to $App:
+- get input from user
+- print the error message and stop the process if input from user is wrong
+- print the counts of lotto
+
+---
--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("../src/Lotto");
+
 class App {
-  play() {}
+
+  async play() {
+    const LottoCount = await this.inputMoney();
+  }
+
+  inputMoney(){
+    let count;
+    MissionUtils.Console.readLine("구입 금액을 입력해주세요.\n",(money)=>{
+      if(money % 1000 != 0)
+        throw new Error("[ERROR] 1000원 이상의 단위만 입력받을 수 있습니다.");
+      count = money / 1000;
+      MissionUtils.Console.print("\n" + count + "개를 구매했습니다.");
+    });
+    return count;
+  }
+
 }
 
 module.exports = App;
