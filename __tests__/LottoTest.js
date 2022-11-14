dce5432895672da37ad03cec0cfc668f5833acdc
kimeuns <dmstn2006@gmail.com>
2022-11-15 02:33:29 +0900

    test: 로또 클래스 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -32,21 +32,26 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  // test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-  //   expect(() => {
-  //     new Lotto([1, 2, 3, 4, 5, 5]);
-  //   }).toThrow("[ERROR]");
-  // });
-
-  test("1000원 단위로 떨어지지 않으면 로또를 구매할 수 없다", () => {
-    const payment = 1400;
-    const buyer = new Buyer();
+  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
-      buyer.isValidpayment(payment);
-    }).toThrow();
+      new Lotto([1, 2, 3, 4, 5, 5]);
+    }).toThrow("[ERROR]");
   });
 
+  test("로또 번호에 1보다 작거나 45보다 큰 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 문자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, "h"]);
+    }).toThrow("[ERROR]");
+  });
+});
+
+describe("로또 클래스 테스트", () => {
   test("금액은 숫자만 입력이 가능하다.", () => {
     const payment = "testPayment";
     const buyer = new Buyer();
