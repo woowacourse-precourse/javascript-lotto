96dfb0a848be658ae558b385595785ebc82a81f9
nerdchanii <nerdchanii@gmail.com>
2022-11-14 13:58:10 +0900

    feat: 당첨번호 뽑기를 추가

--- src/constants.js
+++ src/constants.js
@@ -9,5 +9,19 @@ const CASHER = {
   ASK_MONEY: '구입금액을 입력해 주세요.',
   NOTICE_PURCHASE_QUANTITY: '개를 구매했습니다.',
 };
+const LOTTO_PICKER = {
+  ASK_WINNING_NUMBER: '당첨 번호를 입력해 주세요.\n',
+  ASK_BONUS_NUMBER: '보너스 볼을 입력해 주세요.\n',
+  PICKER_ERROR: {
+    WINNING_NUMBERS_LENGTH: '[ERROR] 당첨 번호는 6개여야 합니다.',
+    WINNIG_NUMBERS_IS_NOT_NUMBER: '[ERROR] 당첨 번호는 숫자여야 합니다.',
+    WINNING_NUMBERS_IS_NOT_IN_RANGE: '[ERROR] 당첨 번호는 1~45 사이의 숫자여야 합니다.',
+    BONUS_NUMBER_IS_DUPLICATED: '[ERROR] 보너스 볼은 당첨 번호와 중복될 수 없습니다.',
+    BONUS_NUMBER_IS_NOT_NUMBER: '[ERROR] 보너스 볼은 숫자여야 합니다.',
+    BONUS_NUMBER_IS_NOT_IN_RANGE: '[ERROR] 보너스 볼은 1~45 사이의 숫자여야 합니다.',
+  },
+};
+
+};
 
 module.exports = { LOTTO, CASHER };
---
+++ src/utils/validators/InputValidator.js
@@ -0,0 +1,45 @@
+const {
+  LOTTO,
+  CASHER: { ERROR_MESSAGE: CASHER_ERROR },
+  LOTTO_PICKER: { PICKER_ERROR: PICKER_ERROR },
+} = require('../../constants');
+const { isNumber, isInRange } = require('./numbers');
+
+const validateMoney = (userInput) => {
+  if (!isNumber(userInput)) throw new Error(CASHER_ERROR.MONEY_IS_NOT_NUMBER);
+  if (userInput < LOTTO.TICKET_PRICE) throw new Error(CASHER_ERROR.MONEY_IS_NOT_ENOUGH);
+  if (userInput % LOTTO.TICKET_PRICE !== 0) {
+    throw new Error(CASHER_ERROR.MONEY_HAS_CHAGE_LEFT);
+  }
+
+  return true;
+};
+
+const validateWinningNumbers = (winningNumbers) => {
+  const numbers = winningNumbers.split(',').map((num) => Number(num));
+  if (numbers.length !== LOTTO.NUMBER_COUNT) {
+    throw new Error(PICKER_ERROR.WINNING_NUMBERS_LENGTH);
+  };
+  if (numbers.some((num) => !isNumber(num))) {
+    throw new Error(PICKER_ERROR.WINNIG_NUMBERS_IS_NOT_NUMBER);
+  }
+  if (numbers.some((num) => !isInRange(num))) {
+    throw new Error(PICKER_ERROR.WINNING_NUMBERS_IS_NOT_IN_RANGE);
+  }
+
+  return true;
+};
+
+const validateBonuesNumber = (bonusNumber, winningNumbers) => {
+  if (!isNumber(bonusNumber)) {
+    throw new Error(PICKER_ERROR.BONUS_NUMBER_IS_NOT_NUMBER);
+  }
+  if (winningNumbers.includes(Number(bonusNumber))) {
+    throw new Error(PICKER_ERROR.BONUS_NUMBER_IS_DUPLICATED);
+  }
+  if (!isInRange(LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER, bonusNumber)) {
+    throw new Error(PICKER_ERROR.BONUS_NUMBER_IS_NOT_IN_RANGE);
+  }
+};
+
+module.exports = { validateMoney, validateWinningNumbers, validateBonuesNumber };
