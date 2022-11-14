f6192a3b3072552beba48e0175898eba99107704
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-14 21:48:20 +0900

    feat: 구입 금액 입력 기능 예외처리

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   play() {
     this.priceInput();
-  }
+  };
   
   priceInput() {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (price) => {
+        this.priceExceptionHandling(price)
         console.log(`구입금액을 입력해 주세요.\n ${price}`);
       }
     );
-  }
+  };
+
+  priceExceptionHandling(price) {
+    if(price % 1000 !== 0 ) throw '[ERROR] 구입 금액은 1,000원 단위 입니다';
+  };
 }
 
 module.exports = App;
