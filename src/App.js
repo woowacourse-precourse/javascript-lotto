0164868893a8c04c6a9e709ca5cb3c78d2eeca91
MINJUN PARK <mj9457@naer,com>
2022-11-14 21:40:02 +0900

    refactor. Lotto.js의 구매 입력 메서드를 App.js로 이동

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 class App {
   play() {}
+
+  inputOfLottoPurchaseAmount() {
+    Console.readLine("구입금액을 입력해 주세요.",(amountInput) => {
+      amountInput = parseInt(amountInput);
+    });
+  }
 }
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -4,7 +4,7 @@ const { Console, Random } = MissionUtils;
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
+  constructor(numbers) { // 당첨된 로또 번호 6자리 숫자
     this.validate(numbers);
     this.#numbers = numbers;
   }
@@ -13,12 +13,6 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-  }
-
-  inputOfLottoPurchaseAmount() {
-    Console.readLine("구입금액을 입력해 주세요.",(amountInput) => {
-      amountInput = parseInt(amountInput);
-    })
   }
 }
 
