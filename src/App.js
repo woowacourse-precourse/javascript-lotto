35815791dc865328c68efd5f9413ce1c7ad96d94
semnil5202 <semnil5202@gmail.com>
2022-11-15 08:11:41 +0900

    refactor: 컴포넌트 리팩토링으로 인한 전반적인 코드 수정

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-const IssueLottery = require('./components/IssueLottery');
+const IssueLotto = require('./components/IssueLotto');
 const { Console } = require('@woowacourse/mission-utils');
 
 class App {
-  constructor() {}
   play() {}
 
   buyLotto() {
     Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
-      const [lottos, count] = IssueLottery.setLotteryNumber(input);
+      const purchase = Number(input);
+      const [lottos, count] = IssueLotto.setLotteryNumber(purchase);
       Console.print(`${count}개를 구매했습니다.`);
       lottos.forEach((lotto) => {
         Console.print(lotto.getNumbers());
       });
-      this.typedWinNumber();
+      this.typedWinNumber({ lottos, purchase });
     });
   }
 
-  typedWinNumber() {
+  typedWinNumber({ lottos, purchase }) {
     Console.readLine('\n당첨 번호를 입력해 주세요.\n', (input) => {
-      const winNumbers = input.split(',').map((number) => Number(number));
-      this.typedBonusNumber();
+      const wins = input.split(',').map((number) => Number(number));
+      this.typedBonusNumber({ lottos, wins, purchase });
     });
   }
 
-  typedBonusNumber() {
+  typedBonusNumber({ lottos, wins, purchase }) {
     Console.readLine('\n보너스 번호를 입력해 주세요.\n', (input) => {
-      const bonusNumber = Number(input);
+      const bonus = Number(input);
     });
   }
 }
