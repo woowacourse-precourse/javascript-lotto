c4605ab43884b31fdca0db2bc171a041abae0090
Deemou <4106243@naver.com>
2022-11-14 21:43:48 +0900

    feat(Validation): 로또 번호(당첨 번호) 유효검사 기능 추가

--- src/Validation.js
+++ src/Validation.js
@@ -3,8 +3,26 @@ const { ERROR_MESSAGE } = require('./constant/Error');
 const InvalidInputError = require('./error/InvalidInputError');
 
 class Validation {
+  static validateLottoNumber(numbers) {
+    if (!this.isNumber(numbers)) this.throwException(InvalidInputError, ERROR_MESSAGE.NOT_A_NUMBER);
+    if (numbers.length !== LOTTO.LENGTH)
+      this.throwException(InvalidInputError, ERROR_MESSAGE.NOT_SIX_NUMBERS);
+    if (!this.isInRange(numbers, LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER))
+      this.throwException(InvalidInputError, ERROR_MESSAGE.OUT_OF_RANGE);
+    if (!this.isUnique(numbers))
+      this.throwException(InvalidInputError, ERROR_MESSAGE.NOT_UNIQUE_NUMBER);
+  }
   static throwException(error, message) {
     throw new error(message);
+  }
+  static isNumber(list) {
+    return REGEX.NUMBER.test(list.join(''));
+  }
+  static isInRange(list, min, max) {
+    return list.every((num) => Number(num) >= min && Number(num) <= max);
+  }
+  static isUnique(list) {
+    return new Set(list).size === list.length;
   }
 }
 
