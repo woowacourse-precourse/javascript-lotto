c2d87be71825de66bcf04e17cd464e34c0387ff4
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-14 13:11:09 +0900

    refactor(App,CalculateLotto): 로또 당첨 계산 기능 분리 구현

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ const Notice = require("./NoticeMessage.js")
 const Error = require("./ErrorMessage.js")
 const LottoSell = require("./LottoSell.js")
 const Lotto = require("./Lotto.js")
+const CaculateLotto = require("./CaclulateLotto.js")
 class App {
   constructor(){
     this.Lotto = new Lotto()
     this.LottoSell = new LottoSell()
+    this.CaculateLotto = new CaculateLotto()
     this.bonusnumber = 0;
     this.Winnumber = ''
-    this.correctList =[0,0,0,0,0]
-    this.getMoney = [5000,50000,1500000,30000000,2000000000]
-    this.ratio = 0
   }
   play() {
     MissionUtils.Console.readLine(Notice.INPUT_MONEY,(money) => {
@@ -28,7 +27,7 @@ class App {
     MissionUtils.Console.readLine(Notice.INPUT_BONUS,(numbers) => {
       this.Lotto.validateBonusnumber(numbers)
       this.bonusnumber = numbers
-      this.checkNumber()
+      this.CaculateLotto.checkNumber(this.LottoSell.Lottobuynumber,this.Winnumber,this.bonusnumber)
     });
   }
   setWinNumber(numbers){
@@ -45,48 +44,6 @@ class App {
     const max = Math.max(...numbers)
     const min = Math.min(...numbers)
     this.Lotto.validateInputRange(max,min)
-  }
-  checkNumber(){
-    for (let i=0; i<this.LottoSell.Lottobuynumber.length; i++){
-      let correctNumber = this.LottoSell.Lottobuynumber[i].filter(x=> this.Winnumber.includes(x))
-      let bonusNumberStatus = this.LottoSell.Lottobuynumber[i].includes(Number(this.bonusnumber))
-      this.makeCorrectList(correctNumber,bonusNumberStatus)
-    }
-    this.calculateReturn()
-    this.printResult()
-  }
-  makeCorrectList(list,status){
-    if (list.length ==3){
-      this.correctList[0] += 1;
-    }
-    else if (list.length == 4){
-      this.correctList[1] += 1;
-    }
-    else if (list.length == 5 && status == true){
-      this.correctList[3] += 1;
-    }
-    else if (list.length == 5) {
-      this.correctList[2] += 1;
-    }
-    else if (list.length == 6){
-      this.correctList[4] += 1;
-    } 
-  }
-  printResult(){
-    for (let i = 0; i<5; i++){
-      MissionUtils.Console.print(Notice.RESULT_MESSAGE[i] + this.correctList[i] + "개")
-    }
-    MissionUtils.Console.print(`총 수익률은 ${this.ratio}%입니다.`)
-    MissionUtils.Console.close()
-  }
-  calculateReturn(){
-    let earn = 0
-    const buy = 1000*(this.LottoSell.Lottobuynumber.length)
-    for (let i = 0; i<5; i++){
-      earn += this.correctList[i] * this.getMoney[i]
-    }
-    const earningratio = ((earn/buy)*100).toFixed(1)
-    this.ratio = earningratio
   }
 }
 
