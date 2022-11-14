c1a42451865473b563d1cef8b928d9b75b05195d
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-11-14 21:37:49 +0900

    temp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,115 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+
 class App {
-  play() {}
+  #lotto 
+  cntlotto
+  constructor() {
+    this.#lotto = [];
+    this.cntlotto=0;
+  }
+  
+
+  play() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.", money => {
+      if (money % 1000 != 0) throw new Error("[ERROR] 천 원 단위로 넣어주세요.")
+      this.money = money;
+      this.cntLotto = money / 1000;
+      MissionUtils.Console.print(`${ this.cntLotto }개를 구매했습니다.`);
+      
+      this.showMeTheUserLotto(); 
+
+      this.showMeWinLotto(); 
+    });
+  }
+
+  showMeTheUserLotto() {
+    for (let i = 0; i < this.cntLotto; i++) {
+
+      let randomNumLotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      randomNumLotto = randomNumLotto.sort(function compare(a, b) {
+        return a - b;
+      });
+
+      let lotto = new Lotto(randomNumLotto);
+
+      MissionUtils.Console.print(`[${lotto.numbers.join(", ")}]`);
+
+      this.#lotto.push(lotto)
+    }
+  }
+
+  showMeWinLotto() {
+    MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.", winningLotto => {
+      winningLotto = winningLotto.split(",").map(Number);
+      this.winningLottoArr = [this.checkWinLotto(winningLotto)]; 
+      MissionUtils.Console.readLine("보너스 번호를 입력해 주세요.", winningLottoBonus => {
+        winningLottoBonus = +winningLottoBonus;
+        this.winningLottoArr.push(this.checkWinBonusLotto(winningLottoBonus))
+        this.printRank(this.rankCount());
+        MissionUtils.Console.close();
+      });
+    });
+  }
+
+  checkWinLotto(lottoNum) {
+    if (lottoNum.length != 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    let lottoSet = new Set(lottoNum);
+    if (lottoSet.size != 6) throw new Error("[ERROR] 로또 번호가 중복 됩니다")
+
+    for(let i=0; i<lottoNum.length; i++){
+      if(lottoNum[i]>45 || lottoNum[i]<1 ){
+        throw new Error("[ERROR] 로또 번호 사이는 1에서 45 사이 입니다");
+      }
+    }
+
+   
+    return lottoNum
+  }
+
+  checkWinBonusLotto(num) {
+    if (this.winningLottoArr[0].indexOf(num) !== -1) {
+      throw new Error("[ERROR] 보너스 로또 번호가 다른 로또 번호와 중복 됩니다")
+    }
+
+    if ( num > 45 || num < 1 ) {
+      throw new Error("[ERROR] 보너스 로또 번호 사이는 1에서 45 사이 입니다");
+    }
+    
+    return num
+  }
+
+  rankCount() {
+    let rank = [0, 0, 0, 0, 0]; //각 순서대로 3개 일치 ~ 6개 일치
+    this.#lotto.forEach(x => {
+      rank[x.userLottoWinningLottoCompare(this.winningLottoArr)]++
+    })
+    rank.push(Math.round(((this.yieldCal(rank) / this.money) * 1000)) / 10);  
+    return rank;
+  }
+
+  yieldCal(rank) {
+    let yieldLotto = rank[0] * 5000;
+    yieldLotto += rank[1] * 50000;
+    yieldLotto += rank[2] * 1500000;
+    yieldLotto += rank[3] * 30000000;
+    yieldLotto += rank[4] * 2000000000;
+    return yieldLotto;
+  }
+
+  printRank(rank) {
+    MissionUtils.Console.print("당첨 통계\n")
+    MissionUtils.Console.print("---")
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${rank[0]}개`)
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${rank[1]}개`)
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${rank[2]}개`)
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank[3]}개`)
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${rank[4]}개`)
+    MissionUtils.Console.print(`총 수익률은 ${rank[5]}%입니다.`)
+  }
+
 }
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -2,17 +2,46 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.checkLotto(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  get numbers() {
+    return this.#numbers;
+  }
+
+  checkLotto(numbers) {
+    if (numbers.length != 6) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    
+    for(let i=0; i<numbers.length; i++){
+      if(numbers[i]>45 || numbers[i]<1 ){
+        throw new Error("[ERROR] 로또 번호 사이는 1에서 45 사이 입니다");
+      }
+    }
+    let set = new Set(numbers);
+    if (set.size !== 6) throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.")
+  }
+
+  userLottoWinningLottoCompare(winningLottoArr) {
+    let set = new Set(this.#numbers);
+    winningLottoArr[0].forEach(x => set.add(x))
+    return this.setSizeCount(set, winningLottoArr[1])
+  }
+
+  setSizeCount(set, standardBonus) {  //App.js의 rank(등수 Array) index 반환하기
+    switch (set.size) {
+      case 9:   //3개 일치 = 중복제외 합 9개
+        return 0;
+      case 8:    //4개 일치 = 중복제외 합 8개
+        return 1;
+      case 6:    //6개 일치 = 중복제외 합 6개
+        return 4;
+      case 7:    //5개 일치 = 중복제외 합 7개
+        if (set.has(standardBonus)) return 3;  //보너스 일치
+        else return 2; //보너스 불일치
     }
   }
 
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
