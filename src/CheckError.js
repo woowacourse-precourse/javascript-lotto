47bc1eea9702f3d88bb1d1a3d96185d64439ad76
pp449 <mma7710@naver.com>
2022-11-14 10:06:55 +0900

    feat: 입력받은 로또 번호가 1~45 사이가 아닌 경우 에러발생

--- src/CheckError.js
+++ src/CheckError.js
@@ -6,6 +6,9 @@ class CheckError {
     numbers.map((number) => {
       if (isNaN(number))
         throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+
+      if (number < 1 || number > 45)
+        throw new Error('[ERROR] 로또 번호는 1~45 사이여야 합니다.');
     });
   }
 }
