bd8fab78b5cc43eb369dd9ad489cba8ed88bc1e7
kitez <duswl339@ajou.ac.kr>
2022-11-15 00:16:50 +0900

    feat: 로또 게임 핵심 로직 추가
    + 1) 구입 금액 입력
    + 2) 로또 구입 갯수만큼 로또 발급
    + 3) 당첨 번호 입력
    + 4) 보너스 번호 입력
    + 5) 보너스 번호 validation check

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ class App {
   #price; // 로또 구입금액
   #lottoNumber; // 구입 로또 갯수
   #Lotto; // 발급 받은 로또 배열
+  #winnerNumber; // 당첨 번호
+  #bonus; // 보너스 번호
 
   constructor() {
     this.#price = 0;
     this.#lottoNumber = 0;
     this.#Lotto = [];
+    this.#bonus = 0;
+    this.#winnerNumber = '';
   }
 
   play() {
@@ -23,16 +27,32 @@ class App {
     }
 
     utils.print(this.#lottoNumber+"개를 구매했습니다.");
+    // 로또 발급
     this.#Lotto = this.#issueLotto();
+
+    // 당첨 번호 입력 받는 로직
+    this.#winnerNumber = utils.scan(Constant.MESSAGE.LOTTO_NUMBER_INPUT).split(',');
+    utils.validateLotto(this.#winnerNumber);
+
+    // 보너스 번호 입력
+    this.#bonus = utils.scan(Constant.MESSAGE.BONUS_NUMBER_INPUT);
+    this.#validateBonus();
+
+    for(let i = 0 ; i < this.#lottoNumber ; i++){
+      this.#Lotto[i].calcWinning(this.#winnerNumber);
+      this.#Lotto[i].match(this.#bonus);
+    }
   }
 
   /***
    * lotto 발급하는 함수
    */
   #issueLotto(){
+    // 로또 번호는 오름차순으로 정렬
     const result = [];
     for(let i = 0 ; i < this.#lottoNumber ; i++){
-      const tmp = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      let tmp = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, Constant.LOTTO_LENGTH);
+      tmp = tmp.sort((a,b) => utils.ascSort(a, b));
       utils.print(tmp);
       result.push(new Lotto(tmp));
     }
@@ -49,6 +69,10 @@ class App {
       return this.#price / 1000;
     }
     return -1;
+  }
+
+  #validateBonus(){
+    if(this.#lottoNumber.includes(this.#bonus)) throw new Error("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.");
   }
 }
 
