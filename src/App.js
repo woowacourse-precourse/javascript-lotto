efdb456b79c03dfe5f2796fdb2696d04caeafd74
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-15 01:26:46 +0900

    Feat: 게임 시작 문구 출력
    constant를 이용해 게임 시작 문구 정의

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const { GAME_START_MESSAGE } = require("./constants/constant");
+const MessageOutput = require("./domain/MessageOutput");
+
 class App {
-  play() {}
+  play() {
+    const messageOutput = new MessageOutput(GAME_START_MESSAGE);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/constants/constant.js
@@ -0,0 +1,1 @@
+export const GAME_START_MESSAGE = "구입금액을 입력해 주세요.";
