4cb40ead90757c414728abd4b58be2a469abe6e9
foundaway98 <foundaway98@naver.com>
2022-11-14 19:40:00 +0900

    <feat> : 오류 제어 기능 추가(입력한 값이 로또 번호의 범위를 넘어가는 경우)
    입력한 값이 로또 번호의 범위를 넘어가는 경우 에러 문구 출력 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -18,6 +18,11 @@ class Lotto {
         "[ERROR] 로또 번호는 중복되지 않은 1부터 45 사이의 숫자여야 합니다."
       );
     }
+    for (const number of numbers) {
+      if (number > 45 || number < 1) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    }
   }
 
   getLottoNumbers() {
