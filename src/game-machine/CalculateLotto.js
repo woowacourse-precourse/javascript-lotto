5ccfa4d466b9293bf7c5334ec141cf140bdd37fa
SorinKwon <ksl020700@gmail.com>
2022-11-14 23:43:13 +0900

    feat: 발행할 랜덤 로또 번호 계산 기능 추가

--- src/game-machine/CalculateLotto.js
+++ src/game-machine/CalculateLotto.js
@@ -1,4 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
 class CalculateLotto{
   constructor(){
     this.purchase_amount;
@@ -14,6 +15,12 @@ class CalculateLotto{
     lotto_quantity=this.purchase_amount/1000;
     this.lotto_quantity=lotto_quantity;
     return lotto_quantity;
+  }
+
+  calculateLottoNumbers(){
+    let random_lotto_numbers;
+    random_lotto_numbers=MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6); 
+    return random_lotto_numbers;
   }
 
 }
