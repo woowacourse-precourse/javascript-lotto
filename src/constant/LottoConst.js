b6878eff483c24bd964d3a753366099780191d33
Rki0 <pky00823@gmail.com>
2022-11-14 10:33:29 +0900

    refactor(Lotto): modularize constants to another file
    Move constants managed in one file to a separate file.

---
+++ src/constant/LottoConst.js
@@ -0,0 +1,7 @@
+const LottoConst = {
+  ERROR_NOT_LENGTH_SIX: "[ERROR] 로또 번호는 6개여야 합니다.",
+  ERROR_OUT_OF_RANGE: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+  ERROR_SAME_NUMBER_EXIST: "[ERROR] 로또 번호는 모두 다른 숫자여야 합니다.",
+};
+
+module.exports = LottoConst;
