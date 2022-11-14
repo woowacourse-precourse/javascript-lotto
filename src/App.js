1ac5353033e546cb04e430aa8428ad12bb27a78d
nerdchanii <nerdchanii@gmail.com>
2022-11-14 14:20:43 +0900

    feat: 구매를 위한 루틴 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const TicketPublisher = require('./domain/TicketPublisher');
+const Casher = require('./domain/Casher');
+const { CASHER } = require('./constants');
 class App {
-  play() {}
+  tickets;
+
+  constructor() {
+    this.tickets = [];
+  }
+
+  play() {
+  }
+
+  purchaseRoutine(purchaseAmount) {
+    const ticketQuantity = Casher.getPurchasableQuantity(purchaseAmount);
+    Casher.noticePurchasedQuantity(ticketQuantity);
+    this.tickets = TicketPublisher.publishTickets(ticketQuantity);
+    TicketPublisher.showTickets(this.tickets);
+
+  }
 }
 
 module.exports = App;
