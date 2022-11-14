688c17875c8193d21c1e60377bd3861876389b9b
pp449 <mma7710@naver.com>
2022-11-14 10:04:53 +0900

    feat: 입력받은 로또 번호가 숫자가 아닌 경우 에러발생

--- src/CheckError.js
+++ src/CheckError.js
@@ -2,6 +2,11 @@ class CheckError {
   static check(numbers) {
     if (numbers.length !== 6)
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+
+    numbers.map((number) => {
+      if (isNaN(number))
+        throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+    });
   }
 }
 
