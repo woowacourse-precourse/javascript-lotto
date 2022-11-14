416c70917e995a4c2179c1aa5a8b9fa533d52395
murpin <qw04011@gmail.com>
2022-11-14 18:18:00 +0900

    feat(App/printLottoJackpotResult): 복권 당첨결과 출력 기능

--- src/App.js
+++ src/App.js
@@ -15,4 +15,35 @@ class App {
     Console.print(`총 수익률은 ${RevenuePercent * 100}%입니다.`)
   }
 
+  printLottoJackpotResult() {
+    let countAmount = 0;
+    Console.print('당첨 통계\n---')
+    this.lottoResult.forEach((count, index) => {
+      switch(true) {
+        case index === 0:
+          Console.print(`3개 일치 (5,000원) - ${count}개`);
+          countAmount += count * 5000;
+          break;
+        case index === 1:
+          Console.print(`4개 일치 (50,000원) - ${count}개`);
+          countAmount += count * 50000;
+          break;
+        case index === 2:
+          Console.print(`5개 일치 (1,500,000원) - ${count}개`);
+          countAmount += count * 1500000;
+          break;
+        case index === 3:
+          Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${count}개`);
+          countAmount += count * 30000000;
+          break;
+        case index === 4:
+          Console.print(`6개 일치 (2,000,000,000원) - ${count}개`);
+          countAmount += count * 2000000000;
+          break;
+      }
+    })
+    this.printLottoRevenuePercent(countAmount);
+  }
+
+}
 module.exports = App;
