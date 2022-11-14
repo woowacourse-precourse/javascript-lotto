0e280ab4931e47a2bb8f7d521a2546a2ec121763
confidential-nt <youyoy689@gmail.com>
2022-11-14 14:19:08 +0900

    feat(요구사항): 요구사항6. 사용자에게 당첨통계 보여주기
    당첨여부와 수익률을 계산하여 사용자에게 보여주는 기능을 구현아였다.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ const { Console, Random } = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 
 class App {
-  LENGTH_OF_LOTTO_NUMBER = 6;
+  static LENGTH_OF_LOTTO_NUMBER = 6;
+  static PRIZE = [5000, 50000, 1500000, 30000000, 2000000000];
 
   play() {
     this.readLine("구입금액을 입력해주세요.", this.runLotto.bind(this));
@@ -17,25 +18,9 @@ class App {
   }
 
   runLotto(value) {
-    const numberOfLotto = this.makePayment(value);
-    const lottos = this.issueLotto(numberOfLotto);
-    this.getWinner(lottos);
-  }
-
-  getWinner(lottos) {
-    this.readLine("당첨번호를 입력해주세요.", this.getWinnerNumber.bind(this));
-  }
-
-  getWinnerNumber(value) {
-    const winnerNumber = value.split(",");
-    this.readLine(
-      "보너스 번호를 입력해주세요.",
-      this.getBonusNumber.bind(this)
-    );
-  }
-
-  getBonusNumber(value) {
-    const bonusNumber = value;
+    const amountOfPaid = this.makePayment(value);
+    const lottos = this.issueLotto(amountOfPaid / 1000);
+    this.getWinner(lottos, amountOfPaid);
   }
 
   makePayment(value) {
@@ -51,7 +36,7 @@ class App {
 
     this.print(`${numberOfLotto}개를 구매했습니다.`);
 
-    return numberOfLotto;
+    return money;
   }
 
   issueLotto(numberOfLotto) {
@@ -71,7 +56,7 @@ class App {
     const numbers = Random.pickUniqueNumbersInRange(
       1,
       45,
-      this.LENGTH_OF_LOTTO_NUMBER
+      App.LENGTH_OF_LOTTO_NUMBER
     );
 
     return numbers.sort();
@@ -81,6 +66,106 @@ class App {
     for (let i = 0; i < lottos.length; i++) {
       lottos[i].printNumbers();
     }
+  }
+
+  getWinner(lottos, amountOfPaid) {
+    this.readLine("당첨번호를 입력해주세요.", (value) => {
+      this.getWinnerNumber(value, lottos, amountOfPaid);
+    });
+  }
+
+  getWinnerNumber(value, lottos, amountOfPaid) {
+    const winnerNumber = value.split(",").map((string) => parseInt(string, 10));
+
+    if (winnerNumber.length !== App.LENGTH_OF_LOTTO_NUMBER) {
+      this.print("6개의 번호를 입력해주세요.");
+      this.getWinner(lottos, amountOfPaid);
+      return;
+    }
+
+    this.readLine("보너스 번호를 입력해주세요.", (value) => {
+      const bonusNumber = value;
+      const winningDetails = this.getWinningDetails(
+        lottos,
+        winnerNumber,
+        bonusNumber,
+        amountOfPaid
+      );
+      this.showFinalResult(winningDetails, amountOfPaid);
+    });
+  }
+
+  getWinningDetails(lottos, winnerNumber, bonusNumber) {
+    const winningDetails = {
+      threeMatches: 0,
+      fourMatches: 0,
+      fiveMatches: 0,
+      fiveMatchesWithBonus: 0,
+      sixMatches: 0,
+    };
+
+    for (let i = 0; i < lottos.length; i++) {
+      const result = lottos[i].getComparisonResult(winnerNumber, bonusNumber);
+      this.switchComparisonResult(result, winningDetails);
+    }
+
+    return winningDetails;
+  }
+
+  switchComparisonResult(comparisonResult, winningDetails) {
+    if (comparisonResult.winnerCount === 3) winningDetails.threeMatches += 1;
+    if (comparisonResult.winnerCount === 4) winningDetails.fourMatches += 1;
+    if (comparisonResult.winnerCount === 5) winningDetails.fiveMatches += 1;
+    if (comparisonResult.winnerCount === 5 && comparisonResult.bonusFlag)
+      winningDetails.fiveMatchesWithBonus += 1;
+
+    if (comparisonResult.winnerCount === 6) winningDetails.sixMatches += 1;
+  }
+
+  showFinalResult(winningDetails, amountOfPaid) {
+    this.showWinningDetails(winningDetails);
+    this.showEarningsRate(winningDetails, amountOfPaid);
+    Console.close();
+  }
+
+  showWinningDetails({
+    threeMatches,
+    fourMatches,
+    fiveMatches,
+    fiveMatchesWithBonus,
+    sixMatches,
+  }) {
+    this.print("당첨통계\n");
+    this.print("---\n");
+    this.print(`3개 일치 (5,000원) - ${threeMatches}개\n`);
+    this.print(`4개 일치 (50,000원) - ${fourMatches}개\n`);
+    this.print(`5개 일치 (1,500,000원) - ${fiveMatches}개\n`);
+    this.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${fiveMatchesWithBonus}개\n`
+    );
+    this.print(`6개 일치 (2,000,000,000원) - ${sixMatches}개\n`);
+  }
+
+  showEarningsRate(winningDetails, amountOfPaid) {
+    const earningsRate = this.caculateEarningsRate(
+      winningDetails,
+      amountOfPaid
+    );
+
+    this.print(`총 수익률은 ${earningsRate}%입니다.`);
+  }
+
+  caculateEarningsRate(winningDetails, amountOfPaid) {
+    const totalEarnings = Object.values(winningDetails).reduce(
+      (pre, cur, i) => {
+        return pre + cur * App.PRIZE[i];
+      },
+      0
+    );
+
+    const earningsRate = (totalEarnings / amountOfPaid).toFixed(1);
+
+    return earningsRate;
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -19,6 +19,20 @@ class Lotto {
     const joinedNumbers = this.#numbers.join(", ");
     Console.print(`[${joinedNumbers}]\n`);
   }
+
+  getComparisonResult(winnerNumber, bonusNumber) {
+    const result = {
+      winnerCount: 0,
+      bonusFlag: false,
+    };
+
+    for (let i = 0; i < this.#numbers.length; i++) {
+      if (this.#numbers.includes(winnerNumber[i])) result.winnerCount++;
+      if (this.#numbers.includes(bonusNumber)) result.bonusFlag = true;
+    }
+
+    return result;
+  }
 }
 
 module.exports = Lotto;
