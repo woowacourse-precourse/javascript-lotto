83ffd491cbcceccdf45597362530773d23d8700d
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-15 08:45:14 +0900

    feat: 입력된 금액이 1000원 단위가 아닐 경우 예외 처리

--- src/MyLotto.js
+++ src/MyLotto.js
@@ -2,6 +2,15 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class MyLotto {
     lottoList = [];
+    constructor(money) {
+        this.validate(money);
+    }
+
+    validate(money) {
+        if (money % 1000 === 0) {
+            throw new Error("[ERROR] 로또 구입 금액은 1000원 단위로 입력 가능합니다.");
+        }
+    }
 }
 
 module.exports = MyLotto;
