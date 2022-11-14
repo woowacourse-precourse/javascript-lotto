17b0094a698c832ee0b02ea6d5edcab0c13e73e6
unknown <chn0202@gmail.com>
2022-11-14 23:37:36 +0900

    feat(BuyLottery): 로또구입 최정본

--- src/BuyLottery.js
+++ src/BuyLottery.js
@@ -3,25 +3,26 @@ class BuyLottery {
   buy(money) {
     this.checkAmout(money);
     const quentity = this.numberOfpurchases(money);
-
     const lottos = this.createRendomLotto(quentity);
     return { quentity, lottos };
   }
+
   checkAmout(money) {
     if (money % 1000 !== 0)
       throw new Error("[ERROR] 금액은 1000원 단위로 나누어 떨어져야 합니다.");
   }
-  numberOfpurchases(money) {
-    let numberOfPurchases = money / 1000;
 
-    return numberOfPurchases;
+  numberOfpurchases(money) {
+    return money / 1000;
   }
+
   createRendomLotto(lotteryNumber) {
     let totalLottoNumbers = [];
     while (lotteryNumber !== totalLottoNumbers.length) {
       const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-      totalLottoNumbers.push(numbers);
+      totalLottoNumbers.push(numbers.sort((a, b) => a - b));
     }
+
     return totalLottoNumbers;
   }
 }
