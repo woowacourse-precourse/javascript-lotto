58f9d52ba099c53d8bf55a543187e1e87be7df6d
semnil5202 <semnil5202@gmail.com>
2022-11-15 03:16:37 +0900

    feat: 사용자에게 로또 구입 금액을 입력 받고 출력하는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const IssueLottery = require('./components/IssueLottery');
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
+  constructor() {}
   play() {}
+
+  buyLotto() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
+      const [lottos, count] = IssueLottery.setLotteryNumber(input);
+      Console.print(`${count}개를 구매했습니다.`);
+      lottos.forEach((lotto) => {
+        Console.print(lotto.getNumbers());
+      });
+    });
+  }
 }
 
 module.exports = App;
