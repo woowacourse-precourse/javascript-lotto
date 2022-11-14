db85073265f2755c89e0fa49c474c89483b19993
River <swimmingr@gmail.com>
2022-11-14 22:34:15 +0900

    금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+
+function insertCoin(inputCoin){
+ let Coin = parseInt(inputCoin/1000);
+  return Coin;
+} // user 입력 금액
+
+function getWinningNum() {
+  let inputNum = prompt("input number");
+  let numList = inputNum.split(",");
+  console.log(numList);
+} // user 입력 당첨 번호
+
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.readLine("heelo",(answer)=>{
+      insertCoin(answer);
+    })
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
