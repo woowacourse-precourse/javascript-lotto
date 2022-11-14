63ffe4ccbbff7acca39f4d23d8f1ba309e10ae13
저니녁 <wjs_5025@naver.com>
2022-11-14 17:45:25 +0900

    fix : 메시지 수정

--- src/constraints.js
+++ src/constraints.js
@@ -12,9 +12,9 @@ const MESSAGES = {
   EXCEPTIONS: {
     PURCHASE: {
       UNIT_EXCEPTION: "[ERROR] 구입 금액을 1,000원 단위로 입력해주세요.",
-      TYPE_EXCEPTION: "[ERROR] 구입 금액은 반드시 숫자여야 합니다.",
+      TYPE_EXCEPTION: "[ERROR] 구입 금액은 숫자여야 합니다.",
     },
-    TYPE_EXCEPTION: "[ERROR] 로또 번호는 정수여야 합니다.",
+    TYPE_EXCEPTION: "[ERROR] 로또 번호는 숫자여야 합니다.",
     COUNT_EXCEPTION: "[ERROR] 로또 번호는 6자리여야 합니다.",
     RANGE_EXCEPTION: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
     DUPLICATION_EXCEPTION: "[ERROR] 로또 번호는 중복되지 않아야 합니다.",
