0f800c92844e7e2fe9467ea136f2b39c89f46a59
pp449 <mma7710@naver.com>
2022-11-14 20:01:25 +0900

    feat: 로또 클래스의 로또번호에도 예외처리를 하게 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const CheckError = require('./CheckError');
+
 class Lotto {
   #numbers;
 
@@ -8,9 +10,7 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-    }
+    CheckError.check(numbers);
   }
 
   #printLottoNumbers() {
