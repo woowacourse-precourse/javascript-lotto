ac7c0c6eec67da3ebf858a358b7ff909e2a4b230
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-11-14 13:22:32 +0900

    refactor: 상수 처리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const Lotto = require('./Lotto');
-const { RANK } = require('./utils/CONSTANT');
+const { RANK, MESSAGE } = require('./utils/CONSTANT');
 const { countTickets, generateRandomNumbers, profitRate } = require('./utils/lottery');
 const { convertNumberToComma } = require('./utils/string');
 const { validateNumbers } = require('./utils/validation');
@@ -21,7 +21,7 @@ class App {
   }
 
   inputMoney() {
-    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (answer) => {
+    MissionUtils.Console.readLine(`${MESSAGE.inputMoney}\n`, (answer) => {
       const money = Number(answer);
       this.#paid = money;
       this.generateTickets();
@@ -34,13 +34,13 @@ class App {
     for (let i = 0; i < ticketsCount; i += 1) {
       this.#tickets.push(new Lotto(generateRandomNumbers()));
     }
-    MissionUtils.Console.print(`\n${this.#tickets.length}개를 구매했습니다.`);
+    MissionUtils.Console.print(`\n${this.#tickets.length}${MESSAGE.paid}`);
     this.#tickets.map((ticket) => MissionUtils.Console.print(`[${ticket.getTicketNumbers().join(', ')}]`));
     this.setLottoNumbers();
   }
 
   setLottoNumbers() {
-    MissionUtils.Console.readLine('\n당첨 번호를 입력해 주세요.\n', (answer) => {
+    MissionUtils.Console.readLine(`\n${MESSAGE.inputNumbers}\n`, (answer) => {
       const numbers = answer.split(',').map((i) => Number(i));
       validateNumbers(numbers);
       this.#answerNumbers = numbers;
@@ -49,27 +49,27 @@ class App {
   }
 
   setBonusNumber() {
-    MissionUtils.Console.readLine('\n보너스 번호를 입력해 주세요.\n', (answer) => {
+    MissionUtils.Console.readLine(`\n${MESSAGE.inputBonusNumber}\n`, (answer) => {
       this.#bonusNumber = Number(answer);
       this.calculateTickets();
     });
   }
 
   calculateTickets() {
-    MissionUtils.Console.print('\n당첨 통계\n---');
+    MissionUtils.Console.print(`\n${MESSAGE.calculateTickets}\n---`);
     this.#totalResults = this.#tickets
       .map((ticket) => ticket.calculateNumbers(this.#answerNumbers, this.#bonusNumber));
     let rewards = 0;
     RANK.forEach((rank) => {
       const count = this.#totalResults.filter((result) => result === rank.rank).length;
-      MissionUtils.Console.print(`${rank.hit}개 일치${rank.bonus ? ', 보너스 볼 일치' : ''} (${convertNumberToComma(rank.money)}원) - ${count}개`);
+      MissionUtils.Console.print(`${rank.hit}${MESSAGE.hit}${rank.bonus ? MESSAGE.bonus : ''} (${convertNumberToComma(rank.money)}원) - ${count}개`);
       rewards += rank.money * count;
     });
     this.calculateProfits(rewards);
   }
 
   calculateProfits(rewards) {
-    MissionUtils.Console.print(`총 수익률은 ${convertNumberToComma(profitRate(rewards, this.#paid))}%입니다.`);
+    MissionUtils.Console.print(`${MESSAGE.profitPrefix}${convertNumberToComma(profitRate(rewards, this.#paid))}${MESSAGE.profitPostfix}`);
     MissionUtils.Console.close();
   }
 }
--- src/utils/CONSTANT.js
+++ src/utils/CONSTANT.js
@@ -1,5 +1,16 @@
 // good
 const TICKET_PRICE = 1000;
+const MESSAGE = {
+  inputMoney: '구입금액을 입력해 주세요.',
+  paid: '개를 구매했습니다.',
+  inputNumbers: '당첨 번호를 입력해 주세요.',
+  inputBonusNumber: '보너스 번호를 입력해 주세요.',
+  calculateTickets: '당첨 통계',
+  hit: '개 일치',
+  bonus: ', 보너스 볼 일치',
+  profitPrefix: '총 수익률은 ',
+  profitPostfix: '%입니다.',
+};
 const RANK = [
   {
     rank: 'FIFTH',
@@ -33,4 +44,4 @@ const RANK = [
   },
 
 ];
-module.exports = { TICKET_PRICE, RANK };
+module.exports = { TICKET_PRICE, RANK, MESSAGE };
