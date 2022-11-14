ed9d71c9d046a8bd29eef139042a26a5853ce710
shinhyunsu01 <makelabel@naver.com>
2022-11-15 03:21:41 +0900

    feat: 유효 데이터 확인

--- src/Validation.js
+++ src/Validation.js
@@ -6,6 +6,31 @@ class Validation {
 		this.sizeCheck(input);
 		this.zeroCheck(input);
 	}
+	inputWinLotto(input) {
+		this.typeArrCheck(input);
+		this.lenCheck(6, input);
+		this.dupliCheck(input);
+		this.insideNumCheck(input);
+	}
+
+	typeArrCheck(numbers) {
+		numbers.forEach((num) => {
+			if (isNaN(num)) this.error(ERROR.LEN_TYPE);
+		});
+	}
+
+	lenCheck(len, data) {
+		if (data.length > len) this.error(ERROR.NUM_TYPE);
+	}
+	dupliCheck(input) {
+		const arr = new Set(input);
+		if (input.length !== [...arr].length) this.error(ERROR.DUPLI_TYPE);
+	}
+	insideNumCheck(numbers) {
+		numbers.forEach((num) => {
+			if (num < 1 || num > 45) this.error(ERROR.RANGE_TYPE);
+		});
+	}
 
 	typeCheck(money) {
 		if (isNaN(money)) this.error(ERROR.NUM_TYPE);
