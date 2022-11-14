d8c7c432622545372ece945643c63b67775106b8
JaeyeonHong <39540473+HongJaeyeon@users.noreply.github.com>
2022-11-15 02:39:34 +0900

    feat: 당첨 번호 입력 기능 및 입력값 유효 판단 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,8 +7,36 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  setNumbers(userInput){
+    this.#numbers = userInput;
+  }
+
   start(){
     this.enterQuantity();
+  }
+
+  isEachUniqueNumer(numbers){
+    return new Set(numbers).size === 6 ? true : false;
+  }
+
+  isValidNumber(number){
+    return (number > 0 && number < 46) ? true : false;
+  }
+
+  isValidLength(numbers){
+    return numbers.length === 6 ? true : false;
+  }
+
+  validate(numbers) {
+    if (!this.isValidLength(numbers)) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+
+    if (!this.isEachUniqueNumer(numbers)) throw new Error("[ERROR] 로또 번호는 중복되면 안됩니다.");
+    
+    numbers.forEach(number => {
+      if (!this.isValidNumber(number)) throw new Error("[ERROR] 로또 번호는 1에서 45사이여야 합니다.");
+    })
+
+    return true;
   }
 
   enterQuantity(){
@@ -20,14 +48,22 @@ class Lotto {
     });
   }
 
-  printQuantity(quantity){
-    MissionUtils.Console.print(`${quantity}개를 구매했습니다.`);
+  enterNumbers(){
+    MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.", (input) => {
+      let userInput = input.split(',').map(item => parseInt(item));
+      if (this.validate(userInput)) this.setNumbers(userInput);
+    });
   }
 
   makeLotto(quantity){
     let uniqueNumbersList = [];
     for (let i=0; i<quantity; i++) uniqueNumbersList.push(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6));
     this.printLotto(uniqueNumbersList);
+    this.enterNumbers();
+  }
+
+  printQuantity(quantity){
+    MissionUtils.Console.print(`${quantity}개를 구매했습니다.`);
   }
 
   printLotto = (uniqueNumbersList) => {
