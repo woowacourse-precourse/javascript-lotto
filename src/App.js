cd084726423244cbcd6281b0d74fc28e77d20d90
kitez <duswl339@ajou.ac.kr>
2022-11-14 16:27:03 +0900

    feat: 구매 로또 갯수 구하는 함수 추가
    + App class의 private field로 입력받을 prcie(구입금액)과 그에따른(lottoNumber) 로또의 갯수 선언
    + App class constructor에서 private field 초기화
    + App.js의 play() 함수 내부에 사용자가 로또 구입 하는 금액 입력하는 로직 추가
    + 사용자가 입력한 로또 구입 금액이 1000원 단위인지 체크하고 1000원 단위라면 로또의 갯수를 return, 아니라면 -1을 return하는 getLottoNumber() 함수를 App class의 method로 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+const utils = require("./utils");
+const Constant = require("./Constant");
+
 class App {
-  play() {}
+  #price; // 로또 구입금액
+  #lottoNumber; // 구입 로또 갯수
+
+  constructor() {
+    this.#price = 0;
+    this.#lottoNumber = 0;
+  }
+
+  play() {
+    this.#price = utils.scan(Constant.MESSAGE.GAME_START_MSG);
+    this.#lottoNumber = this.#getLottoNumber();
+    if(this.#lottoNumber === -1){
+      throw Error();
+    }
+
+    utils.print(this.#lottoNumber+"개를 구매했습니다.");
+
+  }
+
+  /***
+   * @returns {number}
+   * 사용자가 입력한 구입금액이 1000원 단위라면 price/1000을 return
+   * 아니라면 -1을 return하는 함수
+   */
+  #getLottoNumber(){
+    if(this.#price % 1000 === 0){
+      return this.#price / 1000;
+    }
+    return -1;
+  }
+
+
 }
 
 module.exports = App;
