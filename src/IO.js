f3352f01d315a21bf22c25564d4e45104cd825d9
안호진 <asdv94@naver.com>
2022-11-15 00:12:54 +0900

    refactor(lotto): 입력 유효성 검사 IO 클래스로 분리

--- src/IO.js
+++ src/IO.js
@@ -1,20 +1,32 @@
 const { ERROR_MSG } = require('./Constant');
 const { Console } = require('@woowacourse/mission-utils');
+const { duplicateNumbers, only6Numbers, outOfRange, prefix, notNumber } =
+  ERROR_MSG;
 
 class IO {
   static validate(input) {
     if (/^\D|[^\d{1,2}]|,{2,}|\D[,]|,$/g.test(input.trim()))
-      throw new Error(ERROR_MSG.prefix + ERROR_MSG.notNumber);
+      throw new Error(prefix + notNumber);
   }
+
+  static validateNumbers(numbers) {
+    if (numbers.length !== 6) throw new Error(prefix + only6Numbers);
+    if (new Set(numbers).size < 6) throw new Error(prefix + duplicateNumbers);
+    if (numbers.reduce((isOut, no) => isOut || no < 1 || 45 < no, false))
+      throw new Error(prefix + outOfRange);
+  }
+
   static print(msg) {
     Console.print(msg);
   }
+
   static readLine(msg, callback) {
     Console.readLine(msg, (input) => {
       IO.validate(input);
       callback(input);
     });
   }
+
   static close() {
     Console.close();
   }
--- src/Lotto.js
+++ src/Lotto.js
@@ -11,11 +11,7 @@ class Lotto {
   }
 
   validate(numbers) {
-    const { duplicateNumbers, only6Numbers, outOfRange, prefix } = ERROR_MSG;
-    if (numbers.length !== 6) throw new Error(prefix + only6Numbers);
-    if (new Set(numbers).size < 6) throw new Error(prefix + duplicateNumbers);
-    if (numbers.reduce((isOut, no) => isOut || no < 1 || 45 < no, false))
-      throw new Error(prefix + outOfRange);
+    IO.validateNumbers(numbers);
   }
 
   get numbers() {
