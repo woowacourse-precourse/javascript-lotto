2b9b54dc9d816c1e068420d3fd0ddc7ab864ae29
yeonkyoung <keep.ykk@gmail.com>
2022-11-15 02:03:56 +0900

    feat: 당첨 로또 번호 생성 클래스 구현

---
+++ src/WinningLotto.js
@@ -0,0 +1,9 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const messages = require("./constants/messages.js");
+const terms = require("./constants/terms");
+
+class WinningLotto {
+  constructor(numbers) {}
+}
+
+module.exports = WinningLotto;
