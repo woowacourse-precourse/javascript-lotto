9721bda207a7874471cc089782dfbac86c031484
Sangmin Lee <gink.ss00@gmail.com>
2022-11-14 22:25:16 +0900

    feat: 로또 구입 금액 입력하는 메소드 구현
    로또 구입 금액 입력할 수 있는 메소드 구현하였다.
    - 숫자만 입력할 수 있다.
    - 0으로 시작할 수 없다.
    - 최소 금액은 1000원이다.
    - 1000원 단위로 입력해야 한다.
    - 위 상황이 위배되면 예외를 발생시키고 에러 메시지를 출력 후 종료한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+const { Random, Console } = require('@woowacourse/mission-utils');
+const { REGEXP, MESSAGE, ERROR_MESSAGE } = require('./constant/constant');
+
 class App {
-  play() {}
+  play() {
+    this.inputPurchasingPrice();
+  }
+
+  inputPurchasingPrice() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (price) => {
+      this.validate(price);
+    });
+  }
+
+  validate(price) {
+    if (!REGEXP.CHECK_NUMBER.test(price)) {
+      throw new Error(ERROR_MESSAGE.ONLY_INPUT_NUMBER);
+    }
+
+    if (REGEXP.CHECK_START_NUMBER.test(price)) {
+      throw new Error(ERROR_MESSAGE.START_NUMBER_ZERO);
+    }
+
+    if (parseInt(price, 10) < 1000) {
+      throw new Error(ERROR_MESSAGE.MIN_PRICE);
+    }
+
+    if (parseInt(price, 10) % 1000 !== 0) {
+      throw new Error(ERROR_MESSAGE.INVALID_UNIT);
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
