d159e1fda271fd3e6a84fe266ae7b3a5053bea28
Hannah Ahn <ahnlook90@gmail.com>
2022-11-14 13:30:10 +0900

    feat: random lotto numbers generate

---
+++ src/BoughtGames.js
@@ -0,0 +1,24 @@
+const MissionUtils = require('@woowacourse/mission-utils')
+const { Console, Random } = MissionUtils
+
+class BoughtGames {
+	#purchasePrice
+	#games
+
+	constructor (purchasePrice) {
+		this.#purchasePrice = purchasePrice
+		this.generateGaems()
+	}
+
+	generateGaems() {
+    this.#games = []
+    for(let i = 0; i < this.#purchasePrice/1000; i++) {
+      this.#games.push(
+        Random.pickUniqueNumbersInRange(1, 45, 6)
+        .sort((a, b)=> a - b)
+      ) // ex. [ 39, 6, 14, 2, 36, 15 ]
+    }
+	}
+}
+
+module.exports = BoughtGames
