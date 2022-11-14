fc59fa19ba64217ee116fff30221d7ed7e9365f3
DongWooKim97 <nanaukim@naver.com>
2022-11-14 21:38:18 +0900

    feat: 로또의 시작인 구입금액 입력받는 메소드 작성

--- src/play/Clerk.js
+++ src/play/Clerk.js
@@ -1,3 +1,11 @@
-class Clerk {}
+const Mission = require("@woowacourse/mission-utils");
+
+class Clerk {
+  inputLottoAmount() {
+    Mission.Console.readLine("구입금액을 입력해 주세요.", (payment) => {
+      Mission.Console.close();
+    });
+  }
+}
 
 module.exports = Clerk;
