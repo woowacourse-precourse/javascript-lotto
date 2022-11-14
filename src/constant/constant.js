bc1e02e9e102a347164ba80112f7531299eac515
Sangmin Lee <gink.ss00@gmail.com>
2022-11-15 04:03:06 +0900

    refactor: 상수 값 변경
    양식에 맞게 줄 띄움 및 에러 메시지를 보고 이해하기 쉽게 정보를 추가하였다.

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -6,19 +6,19 @@ const REGEXP = {
 const MESSAGE = {
   INPUT_PURCHASING_PRICE: '구입금액을 입력해 주세요.\n',
   NUMBER_OF_LOTTOS_AMOUNT: '개를 구매했습니다.',
-  INPUT_WINNING_NUMBERS: '당첨 번호를 입력해 주세요.',
-  INPUT_BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
-  WINNING_STATISTICS: '당첨 통계\n---',
-  FIFTH: '3개 일치 (5,000원) - ',
-  FOURTH: '4개 일치 (50,000원 - ',
-  THIRD: '5개 일치 (1,500,000원) - ',
-  SECOND: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
-  FIRST: '6개 일치 (2,000,000,000원) - ',
+  INPUT_WINNING_NUMBERS: '\n당첨 번호를 입력해 주세요.\n',
+  INPUT_BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
+  WINNING_STATISTICS: '\n당첨 통계\n---',
+  FIFTH: '\n3개 일치 (5,000원) - ',
+  FOURTH: '\n4개 일치 (50,000원 - ',
+  THIRD: '\n5개 일치 (1,500,000원) - ',
+  SECOND: '\n5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  FIRST: '\n6개 일치 (2,000,000,000원) - ',
   AMOUNT: '개',
 };
 
 const ERROR_MESSAGE = {
-  ONLY_INPUT_NUMBER: '[ERROR] 숫자만 입력 가능합니다.',
+  ONLY_INPUT_NUMBER: '[ERROR] 최소 구입 금액은 숫자만 입력 가능합니다.',
   MIN_PRICE: '[ERROR] 최소 구입 금액은 1000원 입니다.',
   START_NUMBER_ZERO: '[ERROR] 구입 금액은 0으로 시작할 수 없습니다.',
   INVALID_UNIT: '[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.',
