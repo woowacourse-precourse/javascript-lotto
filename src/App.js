fc8778c8d1de3f6aee4932a6fa264a23a36c0376
eunsung <esk114477@gmail.com>
2022-11-15 01:24:25 +0900

    feat : lotto class 중복 처리 및 숫자 이외 예외처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const { Random, Console } = require("@woowacourse/mission-utils");
+const Lotto = require('./Lotto.js');
 
-let lottos = [];
+class App {
 
-const buyLotto = () => {
+buyLotto() {
   let countLotto;
   let throwInput;
   let input;
@@ -13,12 +14,14 @@ const buyLotto = () => {
     countLotto = Number(num) / 1000;
     throwInput = Number(num) % 1000;
     Console.print(`${countLotto}개를 구매했습니다.`);
-    showLottoNumber(countLotto, throwInput);
+    this.showLottoNumber(countLotto, throwInput);
   });
 }
 
-const setLottoNumbers = () => {
+setLottoNumbers(){
   const lottoNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  const checkLotto = new Lotto(lottoNumber);
+  checkLotto.checkNum();
   lottoNumber.sort(function (a, b) {
     if (a > b) return 1;
     if (a === b) return 0;
@@ -27,49 +30,53 @@ const setLottoNumbers = () => {
   return lottoNumber;
 }
 
-const showLottoNumber = (countLotto, throwInput) => {
+showLottoNumber(countLotto, throwInput){
+  let lottos = [];
   if (throwInput > 0) throw '[ERROR] 금액은 1,000원 단위로 입력해주세요';
   for (let i = 0; i < countLotto; i++) {
-    lottos[i] = setLottoNumbers()
+    lottos[i] = this.setLottoNumbers();
     Console.print(lottos[i]);
   }
-  inputLottoNumber(lottos);
+  this.inputLottoNumber(lottos);
 }
 
-const inputLottoNumber = (lottos) => {
+inputLottoNumber(lottos){
   let inputSixNumber;
   Console.readLine('당첨 번호를 입력해 주세요.\n', (num) => {
     inputSixNumber = num.toString().split(",").map((str) => Number(str));
     console.log(inputSixNumber);
-    const lengthThrow = new Set(inputSixNumber);
-    const uniqueNumber = [...lengthThrow];
-    if(uniqueNumber.length != inputSixNumber.length) throw '[ERROR] 중복된 로또 번호가 있습니다.';
-    console.log(uniqueNumber);
-    inputBonusLotto(inputSixNumber, lottos);
-  })
+    const lottoCheck = new Lotto(inputSixNumber);
+    lottoCheck.checkNum();
+    lottoCheck.checkOther(inputSixNumber);
+    // const lengthThrow = new Set(inputSixNumber);
+    // const uniqueNumber = [...lengthThrow];
+    // if(uniqueNumber.length != inputSixNumber.length) throw '[ERROR] 중복된 로또 번호가 있습니다.';
+    // console.log(uniqueNumber);
+    this.inputBonusLotto(inputSixNumber, lottos);
+  });
 }
 
-const inputBonusLotto = (inputSixNumber, lottos) => {
+inputBonusLotto(inputSixNumber, lottos){
   let inputBonusNumber;
   Console.readLine('보너스 번호를 입력해 주세요.\n', (num) => {
     inputBonusNumber = Number(num);
     inputSixNumber.push(inputBonusNumber);
     console.log(inputSixNumber);
-    compareLotto(inputSixNumber, lottos);
-  })
+    this.compareLotto(inputSixNumber, lottos);
+  });
 }
 
-const compareLotto = (inputSixNumber, lottos) => {
+compareLotto(inputSixNumber, lottos){
   let checkLength = [];
   let last = inputSixNumber.pop();
   console.log(last);
   for (let i = 0; i < lottos.length; i++) {
     checkLength[i] = inputSixNumber.filter(num => lottos[i].includes(num)).length;
   }
-  countScore(checkLength,lottos,last);
+  this.countScore(checkLength,lottos,last);
 }
 
-const countScore = (checkLength,lottos,last) => {
+countScore(checkLength,lottos,last){
   let second = 0;
   for(let i = 0; i < lottos.length; i++){
     if (lottos[i].includes(last) && checkLength[i] === 4) {
@@ -80,10 +87,10 @@ const countScore = (checkLength,lottos,last) => {
   let third = checkLength.filter(num => 5 === num).length;
   let fourth = checkLength.filter(num => 4 === num).length;
   let fiveth = checkLength.filter(num => 3 === num).length;
-  showCount(first,second,third,fourth,fiveth,lottos);
+  this.showCount(first,second,third,fourth,fiveth,lottos);
 }
 
-const showCount = (first, second, third, fourth, fiveth,lottos) => {
+showCount(first, second, third, fourth, fiveth,lottos){
   Console.print("당첨 통계\n---");
   Console.print(`3개 일치 (5,000원) - ${fiveth}개`);
   Console.print(`4개 일치 (50,000원) - ${fourth}개`);
@@ -95,23 +102,20 @@ const showCount = (first, second, third, fourth, fiveth,lottos) => {
   let thirdMoney = third * 1500000;
   let secondMoney = second * 30000000;
   let firstMoney = first * 2000000000;
-  plusRate(fivethMoney,fourthMoney,thirdMoney,secondMoney,firstMoney,lottos);
+  this.plusRate(fivethMoney,fourthMoney,thirdMoney,secondMoney,firstMoney,lottos);
 }
 
-const plusRate = (first, second, third, fourth, fiveth,lottos) => {
+plusRate(first, second, third, fourth, fiveth,lottos){
   let allPlus = first + second + third + fourth + fiveth;
   let lottoLength = lottos.length;
-  let returnRate = (allPlus/lottoLength)/100;
+  let returnRate = (allPlus/lottoLength)/10;
   Console.print(`총 수익률은 ${returnRate.toFixed(1)}%입니다.`);
 }
 
-class App {
-  play() {
-    buyLotto();
+
+play() {
+    this.buyLotto();
   }
 }
-
-const app = new App();
-app.play()
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,28 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    this.checkSix(numbers)
   }
 
   // TODO: 추가 기능 구현
+  checkNum(){
+    const check = new Set(this.#numbers);
+    if(check.size !== 6){
+      throw "[ERROR] 중복이 확인되었습니다."
+    }
+  }
+
+  checkSix(numbers){
+    const check = new Set(numbers);
+    if(check.size !== 6){
+      throw "[ERROR] 중복이 확인되었습니다."
+    }
+  }
+
+  checkOther(number){
+    const check = number.toString().split("").map(str => Number(str));
+    if(check.includes("NaN")) throw "[ERROR] 숫자를 입력해주세요."
+  }
 }
 
 module.exports = Lotto;
