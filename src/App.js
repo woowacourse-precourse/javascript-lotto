06cefd971740ddde649c13da77dfc92cf3c43c9a
하지훈 <jnh03336@gmail.com>
2022-11-14 19:23:47 +0900

    feat(lottoApp): 로직 순차 실행하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+/* eslint-disable prettier/prettier */
+const { Console } = require("@woowacourse/mission-utils");
+const LottoCompany = require("./LottoCompany");
+const LottoManager = require("./LottoManager");
+const LogicChain = require("./LogicChain");
+
+const lottoPrice = 1000;
+const winningMoney = [2_000_000_000, 30_000_000, 1_500_000, 50_000, 5_000];
 class App {
-  play() {}
+  #lottoCompany;
+
+  #lottoManager;
+
+  #logicChain;
+
+  constructor() {
+    this.#lottoCompany = new LottoCompany(lottoPrice, winningMoney);
+    this.#lottoManager = new LottoManager();
+    this.#logicChain = new LogicChain();
+    this.makeLogicOrders();
+  }
+
+  makeLogicOrders() {
+    [
+      () => this.#lottoManager.buyLottos(this.#lottoCompany, () => this.#logicChain.executeNext()),
+      () => { this.#lottoManager.printLottosStatus(); this.#logicChain.executeNext();},
+      () => this.#lottoCompany.makeWinningNumbers(() => this.#logicChain.executeNext()),
+      () => this.#lottoCompany.makeBonusNumber(() => this.#logicChain.executeNext()),
+      () => { this.#lottoManager.checkResults(this.#lottoCompany); this.#logicChain.executeNext(); },
+      () => { 
+        this.#lottoCompany.printReportByRanks(this.#lottoManager.getLottoResults()); 
+        this.#logicChain.executeNext();
+      },
+      () => { this.#lottoManager.printYield(this.#lottoCompany); this.#logicChain.executeNext(); },
+      () => { Console.close(); },
+    ].forEach((logic) => this.#logicChain.addNextLogic(logic));
+  }
+
+  play() {
+    this.#logicChain.executeNext();
+  }
 }
 
 module.exports = App;
