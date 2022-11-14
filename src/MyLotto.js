85ac280d8094c89715634d27952be38b19a3892f
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-15 08:57:35 +0900

    fix : 1000원 단위가 아닐 경우 예외처리 에러 수정

--- src/MyLotto.js
+++ src/MyLotto.js
@@ -7,7 +7,7 @@ class MyLotto {
     }
 
     validate(money) {
-        if (money % 1000 === 0) {
+        if (money % 1000 !== 0) {
             throw new Error("[ERROR] 로또 구입 금액은 1000원 단위로 입력 가능합니다.");
         }
     }
