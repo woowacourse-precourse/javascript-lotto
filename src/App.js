9fc33f66bc7ec8ec3725c0b20a241868dd5acac1
nerdchanii <nerdchanii@gmail.com>
2022-11-14 14:22:32 +0900

    feat: 당첨조회를 위한 루틴 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 const TicketPublisher = require('./domain/TicketPublisher');
 const Casher = require('./domain/Casher');
 const { CASHER } = require('./constants');
+const LottoPicker = require('./domain/LottoPicker');
+const RankDeterminator = require('./domain/RankDeterminator');
 class App {
   tickets;
+  static rankDeterminator;
 
   constructor() {
     this.tickets = [];
@@ -16,7 +19,14 @@ class App {
     Casher.noticePurchasedQuantity(ticketQuantity);
     this.tickets = TicketPublisher.publishTickets(ticketQuantity);
     TicketPublisher.showTickets(this.tickets);
+  }
 
+  markRoutine({ winningNumbers, bonusNumber }) {
+    App.rankDeterminator = new RankDeterminator({ winningNumbers, bonusNumber });
+    const ranks = this.tickets.map((ticket) => ticket.getRank(App.rankDeterminator));
+    Casher.setResults(ranks);
+    Casher.noticeResult();
+    Casher.noticeProfit();
   }
 }
 
