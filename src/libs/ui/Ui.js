9b5efe983ba00a2df79c7f57705153cb7b1c4e5a
ksg2388 <ksg2388@naver.com>
2022-11-14 21:59:49 +0900

    refactor(UI): 코딩 컨벤션에 맞춰 리팩토링

--- src/libs/ui/Ui.js
+++ src/libs/ui/Ui.js
@@ -2,18 +2,6 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const { lottoStatistics } = require('../lottoFunction/lottoResult');
 const Lotto = require('../../Lotto');
 
-const accountInput = () => {
-  MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
-    checkAccountValidation(input);
-    lottoPrint(input);
-    generateNumber(input);
-  });
-};
-
-const lottoPrint = (account) => {
-  MissionUtils.Console.print(`\n${account / 1000}개를 구매했습니다.`);
-};
-
 const checkAccountValidation = (input) => {
   const account = Number(input);
 
@@ -28,30 +16,34 @@ const checkAccountValidation = (input) => {
   }
 };
 
-const generateNumber = (count) => {
-  const lottos = [];
-  const lottoCount = count / 1000;
-
-  for (let i = 0; i < lottoCount; i++) {
-    const lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
-      (a, b) => a - b,
-    );
-    lottos.push(lotto);
-  }
-
-  printLottoNumber(lottos);
+const lottoPrint = (account) => {
+  MissionUtils.Console.print(`\n${account / 1000}개를 구매했습니다.`);
 };
 
-const printLottoNumber = (tickets) => {
-  for (const ticket of tickets) {
-    let output = '';
-    for (const num of ticket) {
-    }
+const checkBonusValidation = (bonusNumber, winningNumber) => {
+  const temp = Number(bonusNumber);
+  if (Number.isNaN(temp)) {
+    throw new Error('[ERROR] 보너스 번호는 숫자만 입력 가능합니다.');
   }
-  tickets.map((element) =>
-    MissionUtils.Console.print(`[${element.join(', ')}]`),
+  if (temp < 1 || temp > 45) {
+    throw new Error('[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.');
+  }
+  winningNumber.forEach((num) => {
+    if (num === temp) {
+      throw new Error('[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.');
+    }
+  });
+};
+
+const bonusNumberInput = (winningNumber, tickets) => {
+  const winningNumberList = winningNumber.split(',').map((num) => Number(num));
+  MissionUtils.Console.readLine(
+    '\n보너스 번호를 입력해 주세요.\n',
+    (bonusNumber) => {
+      checkBonusValidation(bonusNumber, winningNumberList);
+      lottoStatistics(tickets, winningNumberList, bonusNumber);
+    }
   );
-  winningNumberInput(tickets);
 };
 
 const winningNumberInput = (tickets) => {
@@ -59,34 +51,37 @@ const winningNumberInput = (tickets) => {
     '\n당첨 번호를 입력해 주세요.\n',
     (winningNumber) => {
       const lotto = new Lotto(winningNumber);
-      bonusNumberInput(winningNumber, tickets);
-    },
-  );
-};
-
-const bonusNumberInput = (winningNumber, tickets) => {
-  winningNumber = winningNumber.split(',').map((num) => Number(num));
-  MissionUtils.Console.readLine(
-    '\n보너스 번호를 입력해 주세요.\n',
-    (bonusNumber) => {
-      checkBonusValidation(bonusNumber, winningNumber);
-      lottoStatistics(tickets, winningNumber, bonusNumber);
-    },
-  );
-};
-
-const checkBonusValidation = (bonusNumber, winningNumber) => {
-  bonusNumber = Number(bonusNumber);
-  if (isNaN(bonusNumber)) {
-    throw new Error('[ERROR] 보너스 번호는 숫자만 입력 가능합니다.');
-  }
-  if (bonusNumber < 1 || bonusNumber > 45) {
-    throw new Error('[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.');
-  }
-  winningNumber.map((num) => {
-    if (num === bonusNumber) {
-      throw new Error('[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.');
+      bonusNumberInput(lotto.getNumber(), tickets);
     }
+  );
+};
+
+const printLottoNumber = (tickets) => {
+  tickets.map((element) =>
+    MissionUtils.Console.print(`[${element.join(', ')}]`)
+  );
+  winningNumberInput(tickets);
+};
+
+const generateNumber = (count) => {
+  const lottos = [];
+  const lottoCount = count / 1000;
+
+  for (let i = 0; i < lottoCount; i++) {
+    const lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+      (a, b) => a - b
+    );
+    lottos.push(lotto);
+  }
+
+  printLottoNumber(lottos);
+};
+
+const accountInput = () => {
+  MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
+    checkAccountValidation(input);
+    lottoPrint(input);
+    generateNumber(input);
   });
 };
 
