0affb83a5afa81b233358d375aee2fb4b4b1b7ae
useon <yoosk4986@naver.com>
2022-11-15 05:49:05 +0900

    feat(Validation): 당첨 번호 입력값을 검사한다

--- src/Draw.js
+++ src/Draw.js
@@ -1,8 +1,10 @@
 const { Console } = require('@woowacourse/mission-utils');
+const Validation = require('./Validation');
 
 class Draw {
   winning() {
     Console.readLine('당첨번호를 입력해 주세요.', (inputValue) => {
+      new Validation().winningInputValue(inputValue);
       this.winningNumbers = inputValue.split(',').map((item) => Number(item));
       Console.close();
     });
--- src/Validation.js
+++ src/Validation.js
@@ -8,6 +8,29 @@ class Validation {
       throw new Error('[ERROR] 1000원 단위로 입력해주세요.');
     }
   }
+
+  winningInputValue(inputValue) {
+    let winningNumbers = inputValue.split(',');
+    if (inputValue.includes(',') === false) {
+      throw new Error('[ERROR] 각 번호를 콤마로 구분하여 입력해주세요.');
+    }
+    if (winningNumbers.length !== 6) {
+      throw new Error('[ERROR] 당첨 번호 6개를 입력해주세요.');
+    }
+    winningNumbers.forEach((item) => {
+      const numberItem = Number(item);
+      if (Number.isNaN(numberItem)) {
+        throw new Error('[ERROR] 당첨 번호는 숫자만 입력해주세요.');
+      }
+      if (numberItem < 1 || numberItem > 45) {
+        throw new Error('[ERROR] 당첨 번호는 1 ~ 45 범위의 숫자로 입력해주세요.');
+      }
+    });
+    winningNumbers = new Set(winningNumbers);
+    if (winningNumbers.size !== 6) {
+      throw new Error('[ERROR] 당첨 번호를 중복 없이 입력해주세요.');
+    }
+  }
 }
 
 module.exports = Validation;
