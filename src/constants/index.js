24e5a181bffe8be4b3c906aa43527f02ad000ef8
areumsheep <areumsheep@gmail.com>
2022-11-14 18:33:44 +0900

    add: 상수 주석 및 예외 에러 메세지 부분 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,13 @@
 const ERROR_MESSAGES = Object.freeze({
+  // 사용자 입력 예외 에러 메세지
   INVALID_MONEY_UNIT: '[ERROR] 1,000원 단위로 입력해 주세요.',
   INVALID_LOTTO_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
   INVALID_LOTTO_RANGE: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
   INVALID_BONUS_LENGTH: '[ERROR] 보너스 번호 1자리를 입력해 주세요.',
   NOT_DUPLICATE_NUMBER: '[ERROR] 로또 번호는 중복될 수 없습니다.',
+
+  // 예외 에러 메세지
+  EMPTY_NUMBERS: '[ERROR] 랜덤 로또 번호가 생성되지 않았습니다.',
 });
 
 const MESSAGES = Object.freeze({
