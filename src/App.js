929add01a4ecd02f88f9469ea62144f9b9054141
afds4567 <afds4567@gmail.com>
2022-11-14 23:14:17 +0900

    feat: 로또 구입 금액 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
-class App {
-  play() {}
-}
+const MissionUtils = require('@woowacourse/mission-utils');
+const { Message } = require('./Message');
 
+class App {
+  inputMoney;
+
+  constructor() {
+    this.inputMoney = 0;
+  }
+  getMoney() {
+    let money;
+    MissionUtils.Console.readLine(`${Message.COST_MESSAGE}`, input => {
+      money = input;
+      this.validate(money);
+      this.inputMoney = money;
+    });
+  }
+  checkUnit(money) {
+    if (money % 1000 === 0) return true;
+    return false;
+  }
+  checkType(money) {
+    if (Number.isInteger(money)) return true;
+    return false;
+  }
+  checkRange(money) {
+    if (money >= 1000) return true;
+    return false;
+  }
+  validate(money) {
+    if (!this.checkRange(money)) throw new Error(Message.ERROR_COST_RANGE);
+    if (!this.checkUnit(money)) throw new Error(Message.ERROR_COST_UNIT);
+    if (!this.checkType(money)) throw new Error(Message.ERROR_COST_TYPE);
+  }
+  play() {
+    this.getMoney();
+  }
+}
+const app = new App();
+app.play();
 module.exports = App;
--- src/Message.js
+++ src/Message.js
@@ -1,4 +1,4 @@
-const Message = {
+const Message = Object.freeze({
   COST_MESSAGE: '구입금액을 입력해 주세요.\n',
   LOTTO_MESSAGE: '당첨 번호를 입력해 주세요.\n',
   BONUS_MESSAGE: '보너스 번호를 입력해 주세요.\n',
@@ -12,5 +12,6 @@ const Message = {
   ERROR_LOTTO_TYPE: '[ERROR] 로또 번호는 숫자로만 이루어져야 합니다.',
 
   ERROR_LOTTO_BONUS_DUPLICATE: '[ERROR] 보너스 번호와 당첨 번호가 중복됩니다.',
-};
-export default Message;
+});
+
+module.exports = { Message };
