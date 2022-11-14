6c715240d8608a5fd5389e65ef57594808e47e6b
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-15 06:21:14 +0900

    Feat: 맞춘 개수 체크 기능

--- src/Prize.js
+++ src/Prize.js
@@ -1,10 +1,13 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const Lotto = require("./Lotto");
+const Print = require("./Print");
 
 class Prize {
 	constructor() {
 		this.prizeMoney = 0;
 		this.winningNums = [];
 		this.winningBonusNum = 0;
+		this.winningCount = [];
 	}
 
 	validate(nums, bonusNum) {
@@ -20,7 +23,7 @@ class Prize {
 		this.winningBonusNum = bonusNum;
 	}
 
-	isNum(numbers) {
+	static isNum(numbers) {
     const validNums = /[^0-9]/;
 
     numbers.forEach((num) => {
@@ -30,13 +33,13 @@ class Prize {
     });
   }
 
-  isSevenNumbers(numbers) {
+  static isSevenNumbers(numbers) {
     if (numbers.length !== 7) {
       throw new Error('[ERROR] 보너스 번호 포함 당첨 번호는 7개여야 합니다.');
     }
   }
 
-  isNumsInRange(numbers) {
+  static isNumsInRange(numbers) {
     numbers.forEach((num) => {
       if (parseInt(num) < 1 || parseInt(num) > 45) {
         throw new Error('[ERROR] 보너스 번호 포함 당첨 번호는 (1 ~ 45) 범위 내의 숫자여야 합니다.');
@@ -44,20 +47,55 @@ class Prize {
     })
   }
 
-  isDuplicated(numbers) {
+  static isDuplicated(numbers) {
     const duplicateCheck = new Set(numbers);
     if (duplicateCheck.size !== 7) {
       throw new Error('[ERROR] 보너스 번호 포함 당첨 번호는 중복되지 않아야 합니다.');
     }
   }
 
+	static isFirstPlace(count) {
+		if (count === 6) {
+			return true;
+		}
+		return false;
+	}
+	
+	static isSecondPlace(count) {
+		if (count === 5 && lotto.indexOf(this.winningBonusNum) !== -1) {
+			return true;
+		}
+		return false;
+	}
+
+	howManyCorrect(lotto) {
+		let count = 0;
+		for (let i = 0; i < 6; i++) {
+			if (this.winningNums.indexOf(lotto[i]) !== -1) {
+				count += 1;
+			}
+		}
+		if (isFirstPlace(count) || isSecondPlace(count)) {
+			count += 1;
+		}
+		return count;
+	}
+
+	countingPrize(lottoBundle) {
+		let winningCount = 0;
+		for (let i = 0; i < lottoBundle.length(); i++) {
+			winningCount = this.howManyCorrect(lottoBundle[i]);
+			this.winningCount[winningCount] += 1;
+		}
+	}
 	winningCheck(lottoBundle) {
 		Console.readLine('당첨 번호를 입력해 주세요.\n', (nums) => {
 			Console.readLine('보너스 번호를 입력해 주세요.\n', (bonusNum) => {
 				this.validate(nums, bonusNum);
-				this.countingPrize();
-				// print
-				// Console.close();
+				this.countingPrize(lottoBundle);
+
+				const print = new Print();
+				print.winningResult(this.winningCount);
 			})
 		})
 	}
