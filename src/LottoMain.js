8ffcebe60b8a7f5f008abc341adfe96d33c77f72
BuildTools <unconfigured@null.spigotmc.org>
2022-11-15 01:02:31 +0900

    feat : 결과 출력부 프로토타입

--- src/LottoMain.js
+++ src/LottoMain.js
@@ -11,26 +11,57 @@ class LottoMain {
 
 function printResult(lottoList, prize, bonus) {
   ConsoleWork.print('당첨 통계\n---\n');
+  lottoLoop(lottoList, prize, bonus);
 }
 
 function lottoLoop(lottoList, prize, bonus) {
+  const prizeArray = stringToArray(prize);
+  console.log(prizeArray);
   for (let i = 0; i < lottoList.length; i++) {
-    const countArray = countCorrectNumber(lottoList[i], prize, bonus);
+    const countArray = countCorrectNumber(lottoList[i], prizeArray, bonus);
     const prizeCount = countArray[0];
     const bonusCount = countArray[1];
+    const result = prizeResult(prizeCount, bonusCount);
+    ConsoleWork.print(result);
   }
 }
 
+function stringToArray(str) {
+  const array = str.split(',').map(Number);
+  return array;
+}
+
 function countCorrectNumber(lotto, prize, bonus) {
-  const prizeCount = 0;
-  const bonusCount = 0;
-  if (lotto.includes(prize)) {
-    prizeCount++;
+  let prizeCount = 0;
+  let bonusCount = 0;
+  for (let j = 0; j < prize.length; j++) {
+    if (lotto.includes(prize[j])) {
+      prizeCount++;
+    }
   }
   if (lotto.includes(bonus)) {
     bonusCount++;
   }
   return [prizeCount, bonusCount];
+}
+
+function prizeResult(prizeCount, bonusCount) {
+  if (prizeCount == 3) {
+    return 'CORRECT3';
+  }
+  if (prizeCount == 4) {
+    return 'CORRECT4';
+  }
+  if (prizeCount == 5) {
+    if (bonusCount == 1) {
+      return 'CORRECT5_BONUS';
+    }
+    return 'CORRECT5';
+  }
+  if (prizeCount == 6) {
+    return 'CORRECT6';
+  }
+  return 'NOTHING';
 }
 
 module.exports = LottoMain;
