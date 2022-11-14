c018bae5fd1abbffbdf21f0181a000846d909c9d
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-14 15:40:03 +0900

    ✅  [test]: lottos 클래스의 로또 구입 금액에 대한 test

--- src/model/Lottos.js
+++ src/model/Lottos.js
@@ -4,12 +4,23 @@ const Lotto = require("./Lotto");
 
 class Lottos {
   constructor(purchaseAmount) {
+    this.validate(purchaseAmount);
     this.generateLottoNumbers = new GenerateLottoNumbers();
     this.count = purchaseAmount / 1000;
     this.lottos = [];
     this.ranks = [];
 
     this.createLottos();
+  }
+
+  validate(money) {
+    if (isNaN(Number(money))) {
+      throw new Error("[ERROR] 숫자를 입력해주세요.");
+    }
+
+    if (money % 1000 !== 0) {
+      throw new Error("[ERROR] 1000원 단위로 입력해주세요.");
+    }
   }
 
   createLotto() {
@@ -48,6 +59,25 @@ class Lottos {
       accu.set(curr, (accu.get(curr) || 0) + 1);
       return accu;
     }, new Map());
+
+    MissionUtils.Console.print("당첨 통계\n---");
+    MissionUtils.Console.print(
+      `3개 일치 (5,000원) - ${map.has(5) ? map.get(5) : 0}개`
+    );
+    MissionUtils.Console.print(
+      `4개 일치 (50,000원) - ${map.has(4) ? map.get(4) : 0}개`
+    );
+    MissionUtils.Console.print(
+      `5개 일치 (1,500,000원) - ${map.has(3) ? map.get(3) : 0}개`
+    );
+    MissionUtils.Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${
+        map.has(2) ? map.get(2) : 0
+      }개`
+    );
+    MissionUtils.Console.print(
+      `6개 일치 (2,000,000,000원) - ${map.has(1) ? map.get(1) : 0}개`
+    );
   }
 }
 
