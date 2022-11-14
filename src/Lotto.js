dbac1ae05528759f8db8e448466282a28ef7cf49
연 <mogoo22@naver.com>
2022-11-14 20:20:35 +0900

    feat: 로또 번호가 숫자가 아니면 예외가 발생한다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,6 +7,10 @@ class Lotto {
   }
 
   static #validate(numbers) {
+    if (numbers.filter((number) => Number.isNaN(number)).length > 0) {
+      throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+    }
+
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
