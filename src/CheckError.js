600ff33c2395413839cdb56c34d34929e0b2d1d6
JangHoonSeok <orirori512@naver.com>
2022-11-14 22:25:51 +0900

    🐛 fix: 오름차순인지 확인하는 함수 return 오류 해결

--- src/CheckError.js
+++ src/CheckError.js
@@ -45,10 +45,11 @@ class CheckError {
   }
 
   static isAscendingArray(arr) {
+    let isAscending = true;
     for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) return false;
+      if (arr[i] > arr[i + 1]) isAscending = false;
     }
-    return true;
+    return isAscending;
   }
 
   static checkWinnerNumber(winningNumber) {
