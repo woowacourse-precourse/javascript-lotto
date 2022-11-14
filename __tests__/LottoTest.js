d272e0976765a4cd7295159916fb43d1860f8641
연 <mogoo22@naver.com>
2022-11-14 21:12:51 +0900

    style: airbnb 코드 포맷팅으로 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,21 +1,22 @@
-const Lotto = require("../src/Lotto");
+const Lotto = require('../src/Lotto');
 
+describe.only('로또 클래스 테스트', () => {
   test('로또 번호가 숫자가 아니면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto(['1', '2', '3', '4', '5', '6']);
     });
   });
 
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   test('로또의 qrCode를 통해 로또 번호를 읽을 수 있다.', () => {
