d9aca6c89d29ac4d3d6c2d5509e8e5327a971332
Subin Jo <sbjo1202@naver.com>
2022-11-15 00:23:54 +0900

    refactor: 변수명 변경
    사용자의 당첨 기록을 담는 배열 usersTotalRank와 각각 당첨이 몇 번 됐는지 당첨 횟수를 담는 배열 checkRank의 이름을
    각각 usersWinningRecord와 countWinningRecord로 변경하였음

--- src/LottoCalculator.js
+++ src/LottoCalculator.js
@@ -7,9 +7,9 @@ class LottoCalculator {
   bonusNumber;
   usersLottos;
   usersMoney;
-  usersTotalRank = [];
+  usersWinningRecord = [];
   rankReward = [REWARD_MONEY.BANG, REWARD_MONEY.FIRST, REWARD_MONEY.SECOND, REWARD_MONEY.THIRD, REWARD_MONEY.FOURTH, REWARD_MONEY.FIFTH];
-  checkRank = new Array(LOTTO.SIX_NUMBERS).fill(0);
+  countWinningRecord = new Array(LOTTO.SIX_NUMBERS).fill(0);
   rewardMoney = MONEY.ZERO;
   yield;
 
@@ -21,7 +21,7 @@ class LottoCalculator {
   calculate() {
     this.usersLottos.forEach((usersLotto) => {
       const rank = this.compareNumber(usersLotto);
-      this.usersTotalRank.push(rank);
+      this.usersWinningRecord.push(rank);
     });
 
     this.calculateResult();
@@ -50,25 +50,24 @@ class LottoCalculator {
   }
 
   calculateResult() {
-    this.usersTotalRank.forEach((rank) => {
+    this.usersWinningRecord.forEach((rank) => {
       this.rewardMoney += this.rankReward[rank];
-      this.checkRank[rank] += 1;
+      this.countWinningRecord[rank] += 1;
     });    
     this.yield = ((this.rewardMoney / this.usersMoney) * CALCULATE.MAKE_PERCENTAGE).toFixed(CALCULATE.ROUND_OFF_FROM_SECOND_PLACE);
   }
 
   printResult() {
     MissionUtils.Console.print(RESULT_MESSAGE.OPENING_MESSAGE);
-    MissionUtils.Console.print(RESULT_MESSAGE.FIFTH_PLACE + `${this.checkRank[RANK.FIFTH_PLACE]}` + RESULT_MESSAGE.UNIT);
-    MissionUtils.Console.print(RESULT_MESSAGE.FOURTH_PLACE + `${this.checkRank[RANK.FOURTH_PLACE]}` + RESULT_MESSAGE.UNIT);
-    MissionUtils.Console.print(RESULT_MESSAGE.THIRD_PLACE + `${this.checkRank[RANK.THIRD_PLACE]}` + RESULT_MESSAGE.UNIT);
-    MissionUtils.Console.print(RESULT_MESSAGE.SECOND_PLACE + `${this.checkRank[RANK.SECOND_PLACE]}` + RESULT_MESSAGE.UNIT);
-    MissionUtils.Console.print(RESULT_MESSAGE.FIRST_PLACE + `${this.checkRank[RANK.FIRST_PLACE]}` + RESULT_MESSAGE.UNIT);
+    MissionUtils.Console.print(RESULT_MESSAGE.FIFTH_PLACE + `${this.countWinningRecord[RANK.FIFTH_PLACE]}` + RESULT_MESSAGE.UNIT);
+    MissionUtils.Console.print(RESULT_MESSAGE.FOURTH_PLACE + `${this.countWinningRecord[RANK.FOURTH_PLACE]}` + RESULT_MESSAGE.UNIT);
+    MissionUtils.Console.print(RESULT_MESSAGE.THIRD_PLACE + `${this.countWinningRecord[RANK.THIRD_PLACE]}` + RESULT_MESSAGE.UNIT);
+    MissionUtils.Console.print(RESULT_MESSAGE.SECOND_PLACE + `${this.countWinningRecord[RANK.SECOND_PLACE]}` + RESULT_MESSAGE.UNIT);
+    MissionUtils.Console.print(RESULT_MESSAGE.FIRST_PLACE + `${this.countWinningRecord[RANK.FIRST_PLACE]}` + RESULT_MESSAGE.UNIT);
     MissionUtils.Console.print(RESULT_MESSAGE.YIELD_FRONT_MESSAGEL + `${this.yield}` + RESULT_MESSAGE.YIELD_BACK_MESSAGEL);
 
     MissionUtils.Console.close();
-  }
-  
+  } 
 }
 
 module.exports = LottoCalculator;
