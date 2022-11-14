f379045726dbe3d0f263123136f11fe96187c88b
jhyns <tockiib@gmail.com>
2022-11-14 21:49:01 +0900

    feat: Make play method get user input
    play 메소드에서 구매 금액을 입력받고 값을 다른 메소드로 넘기는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
+const { INPUT_PRICE_MSG } = require('./Constants.js');
+
 class App {
-  play() {}
+  inputPriceCallback(input) {}
+
+  play() {
+    Console.readLine(INPUT_PRICE_MSG, (input) =>
+      this.inputPriceCallback(input)
+    );
+  }
 }
 
 module.exports = App;
