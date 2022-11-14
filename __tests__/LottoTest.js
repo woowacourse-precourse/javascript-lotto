c79f3e9e9b2d4e2be6b9a32345ebcdc78fbfb8c4
jhyns <tockiib@gmail.com>
2022-11-14 22:48:28 +0900

    test: Add test for Lotto class
    Lotto 클래스의 메소드에 대한 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -37,4 +37,31 @@ describe('로또 클래스 테스트', () => {
       new Lotto([45, 44, 43, 42, 41, 40]);
     }).not.toThrow('[ERROR]');
   });
+
+  test('toString 메소드가 정상적으로 동작한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.toString()).toBe('[1, 2, 3, 4, 5, 6]');
+  });
+
+  test('getRank 메소드가 정상적으로 동작한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.getRank(6, false)).toBe(1);
+    expect(lotto.getRank(5, true)).toBe(2);
+    expect(lotto.getRank(5, false)).toBe(3);
+    expect(lotto.getRank(4, false)).toBe(4);
+    expect(lotto.getRank(3, false)).toBe(5);
+    expect(lotto.getRank(2, false)).toBe(0);
+    expect(lotto.getRank(1, false)).toBe(0);
+    expect(lotto.getRank(0, false)).toBe(0);
+  });
+
+  test('checkWin 메소드가 정상적으로 동작한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.checkWin([1, 2, 3, 4, 5, 6], 7)).toBe(1);
+    expect(lotto.checkWin([1, 2, 3, 4, 5, 7], 6)).toBe(2);
+    expect(lotto.checkWin([1, 2, 3, 4, 5, 7], 16)).toBe(3);
+  });
 });
