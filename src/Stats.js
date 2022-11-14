d637449bcd78cb2e44f9bdb53995964281cae6a7
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-14 23:46:23 +0900

    feat(class): add Stats
    Add Stats class

---
+++ src/Stats.js
@@ -0,0 +1,9 @@
+class Stats {
+  constructor({ winningNumbers, bonusNumber, purchased }) {
+    this.winningNumbers = winningNumbers;
+    this.bonusNumber = bonusNumber;
+    this.purchased = purchased;
+  }
+}
+
+module.exports = Stats;
