2fcb0f927a4c1aea8b0ad1ff1ba041b877d19bf7
SorinKwon <ksl020700@gmail.com>
2022-11-14 20:46:45 +0900

    feat: 구입 금액을 저장하는 기능 추가
    새로운 기능 추가:
    - 객체를 생성하고 구입 금액을 속성으로 저장함.
    - 다른 클래스에서 접근하기 위해 객체를 이용함.

--- src/game-machine/PurchaseAmount.js
+++ src/game-machine/PurchaseAmount.js
@@ -1,13 +1,22 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class PurchaseAmount {
 
+  constructor(){
+    this.purchase_amount=purchase_amount;
+  }
 
   inputPurchaseAmount(){
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (input_money) => {
       console.log(input_money);
     });
+    this.savePurchaseAmount(input_money);
+  }
+  
+  savePurchaseAmount(input_money){
+    this.purchase_amount=input_money;
   }
 }
 
+const amount = new PurchaseAmount();
 
 module.exports = PurchaseAmount;
