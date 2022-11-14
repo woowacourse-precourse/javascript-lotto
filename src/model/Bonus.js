f9a01b332c4ec51fa744f8fc1b149e3735306ddb
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-14 23:34:14 +0900

    feat: 보너스 숫자 검증 로직

--- src/model/Bonus.js
+++ src/model/Bonus.js
@@ -1,3 +1,5 @@
+const { LOTTO, ERROR_MESSAGE } = require("../constants");
+
 class Bonus {
   #bonus;
 
@@ -7,7 +9,15 @@ class Bonus {
   }
 
   validate(bonus) {
+    const str2num = Number(bonus);
 
+    if (!Number.isInteger(str2num)) {
+      throw new Error(ERROR_MESSAGE.NAN);
+    }
+    if (str2num < LOTTO.MIN || str2num > LOTTO.MAX) {
+      throw new Error(ERROR_MESSAGE.RANGE);
+    }
+    return true;
   }
 }
 
