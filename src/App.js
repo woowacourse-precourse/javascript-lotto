29bfad187beefc92a61e7186db9047ca62a3f271
Rki0 <pky00823@gmail.com>
2022-11-14 13:22:15 +0900

    refactor(App): separate the core logic
    Previously, UI logic and core logic were all mixed together.
    Separate those two logics for clean code, and leave only UI logic in the 'App'.
    
    BREAKING CHANGE: Separate the core logic, leave only UI logic.
    
    Before:
    
    Core logic + UI logic
    
    After:
    
    Only UI logic leave in 'App'

--- src/App.js
+++ src/App.js
@@ -1,71 +1,34 @@
 const Console = require("@woowacourse/mission-utils").Console;
-const Random = require("@woowacourse/mission-utils").Random;
+const appConst = require("./constant/AppConst");
+const Purchase = require("./Purchase");
 const Lotto = require("./Lotto");
 const BonusNumber = require("./BonusNumber");
 const Result = require("./Result");
-const AppConst = require("./constant/AppConst");
 
 class App {
+  constructor() {
+    this.bundleOfLotto = null;
+    this.userLotto = null;
+  }
+
   play() {
     this.getPurchaseAmount();
   }
 
   getPurchaseAmount() {
-    Console.readLine(AppConst.ASK_AMOUNTS_MESSAGE, (amount) => {
-      this.checkAmountStartZero(amount);
-      this.checkOnlyNumber(amount);
-      const amountTypeofNumber = Number(amount);
-      this.checkPurchaseAmount(amountTypeofNumber);
+    Console.readLine(appConst.ASK_AMOUNTS_MESSAGE, (amount) => {
+      const purchase = new Purchase(amount);
+      this.bundleOfLotto = Purchase.createRandomLotto(purchase.numberOfLotto);
 
-      const numberOfLotto = amountTypeofNumber / 1000;
-      this.showNumberOfPurchasedLotto(numberOfLotto);
-      this.createRandomLotto(numberOfLotto);
+      this.showNumberOfPurchasedLotto(purchase.numberOfLotto);
       this.showEveryLotto();
 
       this.getWinningNumber();
     });
   }
 
-  checkAmountStartZero(amount) {
-    if (amount[0] === "0") {
-      throw new Error(AppConst.ERROR_DONT_START_ZERO);
-    }
-  }
-
-  checkOnlyNumber(amount) {
-    const regex = /^\d+$/;
-
-    if (!regex.test(amount)) {
-      throw new Error(AppConst.ERROR_NOT_ONLY_NUMBER);
-    }
-  }
-
-  checkPurchaseAmount(amount) {
-    if (amount % 1000 !== 0) {
-      throw new Error(AppConst.ERROR_NOT_THOUSAND_UNIT);
-    }
-  }
-
   showNumberOfPurchasedLotto(amount) {
     Console.print(`\n${amount}개를 구매했습니다.`);
-  }
-
-  createRandomLotto(amount) {
-    this.bundleOfLotto = [];
-
-    while (this.bundleOfLotto.length < amount) {
-      const randomLotto = Random.pickUniqueNumbersInRange(1, 45, 6);
-
-      const sortedLotto = this.getSortedLotto(randomLotto);
-
-      this.bundleOfLotto.push(sortedLotto);
-    }
-  }
-
-  getSortedLotto(lotto) {
-    lotto.sort((a, b) => a - b);
-
-    return lotto;
   }
 
   showEveryLotto() {
@@ -75,65 +38,42 @@ class App {
   }
 
   getWinningNumber() {
-    Console.readLine(AppConst.ASK_WINNING_NUMBER_MESSAGE, (userInput) => {
-      const arrayedUserInput = this.getArrayedUserInput(userInput);
-      this.checkUesrInputHaveOnlyNumberAndComma(arrayedUserInput);
+    Console.readLine(appConst.ASK_WINNING_NUMBER_MESSAGE, (userInput) => {
+      const arrayedUserInput = Lotto.getArrayedUserInput(userInput);
+      Lotto.checkUesrInputHaveOnlyNumberAndComma(arrayedUserInput);
 
-      const splitedInput = this.getSplitedUserInput(userInput);
-      this.checkWinningNumberStartZero(splitedInput);
-
-      this.userLotto = this.getUserLotto(splitedInput);
-
+      const splitedInput = Lotto.getSplitedUserInput(userInput);
+      Lotto.checkWinningNumberStartZero(splitedInput);
+      this.userLotto = Lotto.getUserLotto(splitedInput);
       new Lotto(this.userLotto);
 
       this.getBonusNumber();
     });
   }
 
-  getArrayedUserInput(userInput) {
-    const arrayedUserInput = userInput.split("");
-
-    return arrayedUserInput;
-  }
-
-  checkUesrInputHaveOnlyNumberAndComma(arrayedUserInput) {
-    arrayedUserInput.forEach((item) => {
-      const ASCII = item.charCodeAt();
-
-      if ((ASCII !== 44 && ASCII < 48) || ASCII > 57) {
-        throw new Error(AppConst.ERROR_NOT_NUMBER_AND_COMMA);
-      }
-    });
-  }
-
-  getSplitedUserInput(userInput) {
-    const splitedInput = userInput.split(",");
-
-    return splitedInput;
-  }
-
-  checkWinningNumberStartZero(userInput) {
-    userInput.forEach((item) => {
-      if (item[0] === "0") {
-        throw new Error(AppConst.ERROR_DONT_START_ZERO);
-      }
-    });
-  }
-
-  getUserLotto(userInput) {
-    const userLotto = userInput.map((item) => Number(item));
-
-    return userLotto;
-  }
-
   getBonusNumber() {
-    Console.readLine(AppConst.ASK_BONUS_NUMBER, (bonus) => {
+    Console.readLine(appConst.ASK_BONUS_NUMBER, (bonus) => {
       new BonusNumber(this.userLotto, bonus);
 
       const result = new Result(this.bundleOfLotto, this.userLotto, bonus);
-
       result.calculateEachLotto();
+
+      this.showResult(result.rank);
+      this.showEarningsRate(result.earningsRate);
     });
+  }
+
+  showResult(rank) {
+    Console.print(appConst.NOTIFY_WINNING_STATE);
+
+    rank.forEach((item, index) => {
+      Console.print(`${appConst.CONDITION_OF_EACH_RANK[index]}` + `${item}개`);
+    });
+  }
+
+  showEarningsRate(earningsRate) {
+    Console.print(`총 수익률은 ${earningsRate}%입니다.`);
+    Console.close();
   }
 }
 
