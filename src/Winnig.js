6572d082c6ba957bfdbf6da0001c8282b4f8149c
JSH99 <tjgus9966@naver.com>
2022-11-14 22:21:02 +0900

    feat(Winning): 당첨 안내 출력 기능 구현

---
+++ src/Winnig.js
@@ -0,0 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+class Winning {
+    constructor() {
+        this.printWinningAnnouncement();
+    }
+
+    printWinningAnnouncement() {
+        MissionUtils.Console.print('당첨 통계\n---');
+    }
+
+    calculateWinningCount() {
+
+    }
+
+    printWinningHistory() {
+
+    }
+
+}
+
+module.exports = Winning;
