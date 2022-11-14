0a116c7d5f9daa4f91669c27938c151092a1036b
JHdesktop <thsehgml37@korea.ac.kr>
2022-11-14 19:47:41 +0900

    feat: 로또 구입 금액 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto.js");
+const PurchaseLotto = require("./PurchaseLotto.js");
+
 class App {
-  play() {}
+  constructor() {
+    this.wonLotto = null;
+    this.userLotto = null;
+  }
+  play() {
+    this.userPurchase();
+  }
+
+  userPurchase() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (payment) => {
+      this.purchaseLotto = new PurchaseLotto(payment);
+    });
+  }
 }
 
 module.exports = App;
---
+++ src/PurchaseLotto.js
@@ -0,0 +1,16 @@
+class PurchaseLotto {
+  #payment;
+
+  constructor(payment) {
+    console.log(typeof payment);
+    this.isNumber(payment);
+  }
+
+  isNumber(payment) {
+    if (payment.match(/^[0-9]+$/) == false) {
+      throw new Error("[ERROR] 로또 구입 금액은 숫자만 입력해주세요.");
+    }
+  }
+}
+
+module.exports = PurchaseLotto;
