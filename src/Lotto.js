e85bf995397f8ea2fa8cfe90bf939cc346c04a2d
Rki0 <pky00823@gmail.com>
2022-11-14 13:39:47 +0900

    refactor(Lotto): migration the logic for the number entered by the user
    Brings the core logic to transform the input into the appropriate form to satisfy the mission condition.
    Since the value input from the app needs to be processed, use 'static' to facilitate its use.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,4 @@
-const LottoConst = require("./constant/LottoConst");
+const lottoConst = require("./constant/LottoConst");
 
 class Lotto {
   #numbers;
@@ -12,14 +12,14 @@ class Lotto {
 
   checkLottoLength(numbers) {
     if (numbers.length !== 6) {
-      throw new Error(LottoConst.ERROR_NOT_LENGTH_SIX);
+      throw new Error(lottoConst.ERROR_NOT_LENGTH_SIX);
     }
   }
 
   checkNumberRanges(numbers) {
     numbers.forEach((item) => {
       if (item < 1 || item > 45) {
-        throw new Error(LottoConst.ERROR_OUT_OF_RANGE);
+        throw new Error(lottoConst.ERROR_OUT_OF_RANGE);
       }
     });
   }
@@ -28,8 +28,44 @@ class Lotto {
     const arrayRemovedDuplicate = new Set(numbers);
 
     if (arrayRemovedDuplicate.size !== 6) {
-      throw new Error(LottoConst.ERROR_SAME_NUMBER_EXIST);
+      throw new Error(lottoConst.ERROR_SAME_NUMBER_EXIST);
     }
+  }
+
+  static getArrayedUserInput(userInput) {
+    const arrayedUserInput = userInput.split("");
+
+    return arrayedUserInput;
+  }
+
+  static checkUesrInputHaveOnlyNumberAndComma(arrayedUserInput) {
+    arrayedUserInput.forEach((item) => {
+      const ASCII = item.charCodeAt();
+
+      if ((ASCII !== 44 && ASCII < 48) || ASCII > 57) {
+        throw new Error(lottoConst.ERROR_NOT_NUMBER_AND_COMMA);
+      }
+    });
+  }
+
+  static getSplitedUserInput(userInput) {
+    const splitedInput = userInput.split(",");
+
+    return splitedInput;
+  }
+
+  static checkWinningNumberStartZero(userInput) {
+    userInput.forEach((item) => {
+      if (item[0] === "0") {
+        throw new Error(lottoConst.ERROR_DONT_START_ZERO);
+      }
+    });
+  }
+
+  static getUserLotto(userInput) {
+    const userLotto = userInput.map((item) => Number(item));
+
+    return userLotto;
   }
 }
 
