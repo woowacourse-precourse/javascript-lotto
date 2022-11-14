da5485b366c78f335326faeb0a1abc0c0b2b9982
Yesong <to06109@naver.com>
2022-11-14 14:42:05 +0900

    feat(getUserInput): 로또 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const COMMAND = require('../util/Message');
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    this.getUserInput(COMMAND.MONEY);
+  }
+
+  getUserInput(message) {
+    Console.readLine(message, (answer) => {
+      console.log(answer);
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ util/Message.js
@@ -0,0 +1,5 @@
+const COMMAND = Object.freeze({
+  MONEY: '구입금액을 입력해 주세요.\n',
+});
+
+module.exports = COMMAND;
