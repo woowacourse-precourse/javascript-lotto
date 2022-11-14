3114e413626229f9f5deb42bdd11ae86b28fd502
wusi-hub <jws9595@naver.com>
2022-11-15 07:21:02 +0900

    refactor: MissionUtils 삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-
-const MissionUtils = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 
 const PURCHASE_MONEY = '구입금액을 입력해 주세요.';
 const PRICE_LOTTO = 1000;
@@ -15,20 +13,20 @@ class App {
   }
 
   userWindow(window) {
-   MissionUtils.Console.readLine(`${window}\n`, (input) => {
+   Console.readLine(`${window}\n`, (input) => {
     this.lottoCount(input);
    });
   }
 
   lottoCount(money) {
     const lottoAmount = money / PRICE_LOTTO;
-    MissionUtils.Console.print(`\n${lottoAmount}개를 구매했습니다.`);
+    Console.print(`\n${lottoAmount}개를 구매했습니다.`);
     this.lottoCreate(lottoAmount);
   }
 
   lottoCreate(amount) {
     for (let i = 0; i < amount; i++) {
-      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
       this.lottoArray.push(numbers);
     }
   }
