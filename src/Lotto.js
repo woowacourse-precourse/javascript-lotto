97e8af4bc81901ced9fbbff4d8b6de6ca18166c6
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-15 00:59:50 +0900

    feat : 당첨 번호 입력 받기 예외처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -6,13 +6,35 @@ class Lotto {
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
-
   // TODO: 추가 기능 구현
+  validate(numbers) {
+    this.noInput(numbers);
+    this.lengthNumber(numbers);
+    this.duplicateNumber(numbers);
+    this.specifiedNumber(numbers);
+    this.notNumber(numbers);
+  };
+
+  lengthNumber(numbers) {
+    if (numbers.length !== 6) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  };
+
+  duplicateNumber(numbers) {
+    const set = new Set(numbers);
+    if (set.size !== 6) throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.")
+  };
+
+  noInput(numbers) {
+    if (numbers === undefined) throw new Error("[ERROR] 로또 번호를 입력해야 합니다.");
+  };
+
+  specifiedNumber(numbers) {
+    if (!(numbers < 0 || numbers > 46)) throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+  };
+  
+  notNumber(numbers) {
+    if (!(isNaN(numbers))) throw new Error("[ERROR] 로또 번호는 문자가 들어 갈 수 없습니다.");
+  };
 }
 
 module.exports = Lotto;
