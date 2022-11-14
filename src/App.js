7f7d7f62dc6573f998d73a30018a6f70fc200a1d
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-14 20:50:11 +0900

    feat: [로또 발행 기능]

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App {
   constructor() {
     this.money = 0;
     this.purchaseQuantity = 0;
+    this.lottoList = [];
   }
 
   play() {
@@ -20,6 +21,24 @@ class App {
     const convertIntoQuantity = money / UNIT;
     this.purchaseQuantity = convertIntoQuantity;
     MissionUtils.Console.print(`\n${this.purchaseQuantity}개를 구매했습니다.`);
+
+    this.issueLotto(this.purchaseQuantity);
+  }
+
+  issueLotto(quantity) {
+    const START_LOTTO_NUMBER = 1;
+    const END_LOTTO_NUMBER = 45;
+    const TOTAL_LOTTO_COUNT = 6;
+
+    for (let line = 0; line < quantity; line++) {
+      const lotto = MissionUtils.Random.pickUniqueNumbersInRange(
+        START_LOTTO_NUMBER,
+        END_LOTTO_NUMBER,
+        TOTAL_LOTTO_COUNT
+      );
+      MissionUtils.Console.print(lotto);
+      this.lottoList.push(lotto);
+    }
   }
 }
 
