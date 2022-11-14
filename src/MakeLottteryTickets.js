88158dc7f4faca881c351e58e4d86e85da76e456
feb-dain <feb.dain@gmail.com>
2022-11-14 15:55:02 +0900

    feat: print lottery tickets

--- src/MakeLottteryTickets.js
+++ src/MakeLottteryTickets.js
@@ -1,4 +1,4 @@
-const { RANDOM_UTIL } = require("./Constants");
+const { RANDOM_UTIL, CONSOLE_UTIL } = require("./Constants");
 
 class MakeLotteryTickets {
   constructor(userMoney, numberOfTickets) {
@@ -12,7 +12,20 @@ class MakeLotteryTickets {
     while (numberOfRepetition--) {
       const randomNumbers = RANDOM_UTIL.pickUniqueNumbersInRange(1, 45, 6);
       this.lotteryTickets.push(randomNumbers);
+      this.sortLotteryNumbers(randomNumbers);
     }
+  }
+
+  sortLotteryNumbers(randomNumbers) {
+    randomNumbers = randomNumbers.sort(
+      (firstEl, secondEl) => firstEl - secondEl
+    );
+    this.printTickets(randomNumbers);
+  }
+
+  printTickets(randomNumbers) {
+    const convertToString = `[${String(randomNumbers).replaceAll(",", ", ")}]`;
+    CONSOLE_UTIL.print(convertToString);
   }
 }
 
