b45f6b50e2b79770f99ca7e129990d33413e74c9
daal2quf <daal2quf@gmail.com>
2022-11-15 02:28:28 +0900

    feat(app): 로또 구매 메세지 출력 및 구매한 로또 번호 출력 기능 추가
    add:
    printResult(): 몇장 구매했는지 구매한 로또의 번호는 무엇인지 출력

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-const { Console } = require("@woowacourse/mission-utils");
-const Calculator = require("./Calculator");
-const ExceptionCheck = require("./ExceptionCheck");
-const Lotto = require("./Lotto");
-const Utils = require("./Utils");
+const { Console } = require('@woowacourse/mission-utils');
+const Calculator = require('./Calculator');
+const ExceptionCheck = require('./ExceptionCheck');
+const Lotto = require('./Lotto');
+const Printer = require('./Printer');
+const Utils = require('./Utils');
 
 class App {
   constructor() {
-    this.exeptionCheck = new ExceptionCheck;
+    this.exeptionCheck = new ExceptionCheck();
     this.calculator = new Calculator();
-    this.utils = new Utils;
+    this.utils = new Utils();
+    this.bonusNumber;
     this.winNumbers;
     this.LottoCount;
+    this.lotto = null;
   }
 
   play() {
@@ -21,12 +24,14 @@ class App {
     Console.readLine('구매 금액을 1,000단위로 입력해주세요.\n', moneyValue => {
       this.exeptionCheck.userInputMoneyValue(moneyValue);
       this.lottoCount = this.calculator.ofPurchaseLottoCount(moneyValue);
+      this.requestWinNumbers();
     });
   }
 
   requestWinNumbers() {
     Console.readLine('당첨번호를 입력해주세요.\n', userInput => {
       this.winNumbers = this.utils.transeStringToNumber(userInput);
+      this.lotto = new Lotto(this.winNumbers);
       this.requestBonusnumber();
     });
   }
@@ -34,13 +39,20 @@ class App {
   requestBonusnumber() {
     Console.readLine('보너스 숫자를 입력해주세요\n', bonusNumber => {
       this.bonusNumber = this.utils.transeStringToNumber(bonusNumber);
+      this.lotto.getBonusNumber(this.bonusNumber);
+      this.printResult();
     });
   }
 
-
+  printResult() {
+    Console.print(`${this.lottoCount}개를 구매했습니다.`);
+    this.lotto
+      .bundleCreate(this.lottoCount)
+      .forEach(lottoNums => Console.print(`[${lottoNums.join(', ')}]`));
+  }
 }
 
 const app = new App();
 app.play();
 
-module.exports = App; 
+module.exports = App;
