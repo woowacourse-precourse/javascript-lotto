4f8fe46dca2fa690d3fa9626ceab5e79fdad142f
jhyns <tockiib@gmail.com>
2022-11-14 18:43:39 +0900

    feat: Add constants for Lotto class
    Lotto 클래스에서 쓰이는 상수 추가

---
+++ src/Constants.js
@@ -0,0 +1,12 @@
+const LOTTO_LENGTH_NOT_SIX_ERROR = '[ERROR] 로또 번호는 6개의 숫자여야 합니다.';
+const LOTTO_DUPLICATE_ERROR = '[ERROR] 로또 번호는 중복되면 안됩니다.';
+const LOTTO_OUT_OF_RANGE_ERROR =
+  '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.';
+const LOTTO_NAN_ERROR = '[ERROR] 로또 번호는 숫자여야 합니다.';
+
+module.exports = {
+  LOTTO_LENGTH_NOT_SIX_ERROR,
+  LOTTO_DUPLICATE_ERROR,
+  LOTTO_OUT_OF_RANGE_ERROR,
+  LOTTO_NAN_ERROR,
+};
