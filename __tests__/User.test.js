0462af53a98a659c0fd580bfb213b89dec5e0298
sungjihyun <jhsung23@gmail.com>
2022-11-15 01:24:54 +0900

    test(User): User 클래스에 대한 테스트 작성

---
+++ __tests__/User.test.js
@@ -0,0 +1,15 @@
+const User = require('../src/User');
+const { Console } = require('@woowacourse/mission-utils');
+
+describe('입력받은 구입 금액으로 구매할 수 있는 장 수를 게산한다.', () => {
+  test('User 생성자의 매개변수를 나누기 1000한 값을 반환한다.', () => {
+    expect(new User(10000).buyLotto().quantity).toEqual(10);
+    expect(new User(3000).buyLotto().quantity).toEqual(3);
+    expect(new User(234000).buyLotto().quantity).toEqual(234);
+    expect(new User(5210000).buyLotto().quantity).toEqual(5210);
+    expect(new User('10000').buyLotto().quantity).toEqual(10);
+    expect(new User('3000').buyLotto().quantity).toEqual(3);
+    expect(new User('234000').buyLotto().quantity).toEqual(234);
+    expect(new User('5210000').buyLotto().quantity).toEqual(5210);
+  });
+});
