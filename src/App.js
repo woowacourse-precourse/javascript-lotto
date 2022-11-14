075c8ee30e9921803c91b2a35102ee9831f66750
emma <jhj191058@gmail.com>
2022-11-15 04:20:08 +0900

    feat : 기능 1 - 2 구매한 로또 개수만큼 로또 번호 발행

--- src/App.js
+++ src/App.js
@@ -11,6 +11,21 @@ class App {
       console.log(answer);
     });
   }
+  //제출 금액 만큼 lotto 구매하기
+  changeLotto(answer) {
+    let changeMoney = answer/1000;
+    Console.print(`${changeLotto}개를 구매했습니다.`);
+  }
+  //구매한 lotto 갯수만큼 로또 번호 발행
+  buyLottoNum = new Array();
+
+  buyLottoNum() {
+    for ( let i = 0; i < changeMoney.length; i++){
+      let jackPot = Random.pickUniqueNumberInRange(1, 45, 6);
+    };
+    this.buyLottoNum.push(jackpot); 
+    Console.print(buyLottoNum);
+  }
 }
 
 
