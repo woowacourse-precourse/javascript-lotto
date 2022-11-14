0e9b986fe97cbd297d3772f242a880e346c48674
su-lim <sujin.0xlim@gmail.com>
2022-11-14 19:41:19 +0900

    feat(sortNumber): 또 번호 오름차순 정렬 메서드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,12 +4,16 @@ class Lotto {
 
   constructor(numbers) {
     this.validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = this.sortNumber(numbers);
   }
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
+
+  sortNumber(numbers) {
+    return numbers.sort((a, b) => a - b);
   }
 }
 
