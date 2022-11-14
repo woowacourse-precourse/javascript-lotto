367a6f033f7c2b6f493f7b1c060bc6e6468b026b
murpin <qw04011@gmail.com>
2022-11-14 18:16:29 +0900

    feat(App/printLottoRevenuePercent): 총 수익률 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ class App {
     this.lottoResult = [0,0,0,0,0];
   };
 
-}
+  printLottoRevenuePercent(lottoRevenue) {
+    let investMoney = 1000 * this.lottoCount;
+    let RevenuePercent = lottoRevenue / investMoney;
+    Console.print(`총 수익률은 ${RevenuePercent * 100}%입니다.`)
+  }
 
 module.exports = App;
