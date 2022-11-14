7c399ee20be606156b4a43b41de29e6b4c29cfa8
jhyns <tockiib@gmail.com>
2022-11-14 21:54:11 +0900

    feat: Add constants for validate input price
    구매 금액에 관련된 에러 메세지 추가

--- src/Constants.js
+++ src/Constants.js
@@ -3,10 +3,17 @@ const LOTTO_DUPLICATE_ERROR = '[ERROR] 로또 번호는 중복되면 안됩니�
 const LOTTO_OUT_OF_RANGE_ERROR =
   '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.';
 const LOTTO_NAN_ERROR = '[ERROR] 로또 번호는 숫자여야 합니다.';
+const PRICE_NAN_ERROR = '[ERROR] 구입 금액은 숫자여야 합니다.';
+const PRICE_TOO_LOW_ERROR = '[ERROR] 최소 구입금액은 1000원입니다.';
+const PRICE_NOT_MULTIPLE_ERROR =
+  '[ERROR] 구입금액은 1000원 단위로 입력해주세요.';
 
 module.exports = {
   LOTTO_LENGTH_NOT_SIX_ERROR,
   LOTTO_DUPLICATE_ERROR,
   LOTTO_OUT_OF_RANGE_ERROR,
   LOTTO_NAN_ERROR,
+  PRICE_NAN_ERROR,
+  PRICE_TOO_LOW_ERROR,
+  PRICE_NOT_MULTIPLE_ERROR,
 };
