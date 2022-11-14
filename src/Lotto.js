8f217b14fe07a73ab801fee4c5692f0a99841d4b
장경은 <jang_kyung_eun@gyeong-eun-ui-MacBookAir.local>
2022-11-15 01:37:17 +0900

    feat: 예외 상황 3가지 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,18 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const set = new Set(numbers);
+    const newNumbers = [...set];
+    if (numbers.length != newNumbers.length) {
+      throw new Error("[ERROR] 로또 번호에 중복되는 값이 있습니다.");
+    }
+
+    for (let i = 0; i < 6; i++) {
+      if (numbers[i] < 1 || numbers[i] > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자만 가능합니다.");
+      }
+    }
   }
 
   // TODO: 추가 기능 구현
