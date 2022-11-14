3268512ff26bfc3e9f6105176236e64763a84a5d
Rki0 <pky00823@gmail.com>
2022-11-14 14:24:12 +0900

    test(Lotto): migration core logic test from 'App'
    As the core logic is transferred from the 'App',
    migrate the unit test code of the function from 'App'.

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -20,4 +20,62 @@ describe("로또 클래스 테스트", () => {
       new Lotto([12, 24, 31, 34, 51, 6]);
     }).toThrow("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
   });
+
+  test("string 타입 입력값이 배열로 변환되는지 테스트", () => {
+    expect(Lotto.getArrayedUserInput("1,2,3,4,5,6")).toStrictEqual([
+      "1",
+      ",",
+      "2",
+      ",",
+      "3",
+      ",",
+      "4",
+      ",",
+      "5",
+      ",",
+      "6",
+    ]);
+  });
+
+  test("숫자와 , 기호 이 외의 문자는 에러 처리", () => {
+    expect(() => {
+      Lotto.checkUesrInputHaveOnlyNumberAndComma([
+        "1",
+        ",",
+        "2",
+        ",",
+        "3",
+        ",",
+        "4",
+        ",",
+        "5",
+        ",",
+        " ",
+        "6",
+      ]);
+    }).toThrow("[ERROR] 숫자와 ,(쉼표) 기호만을 입력해주세요.");
+  });
+
+  test("입력된 당첨 번호를 쉼표를 기준으로 나눈다.", () => {
+    expect(Lotto.getSplitedUserInput("1,2,3,4,5,6")).toStrictEqual([
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+    ]);
+  });
+
+  test("당첨 번호는 0으로 시작하면 안된다.", () => {
+    expect(() => {
+      Lotto.checkWinningNumberStartZero(["1", "2", "3", "04", "5", "6"]);
+    }).toThrow("[ERROR] 0으로 시작하는 숫자는 입력할 수 없습니다.");
+  });
+
+  test("배열 원소가 number 타입으로 변환되어야한다.", () => {
+    expect(Lotto.getUserLotto(["1", "2", "3", "4", "5", "6"])).toStrictEqual([
+      1, 2, 3, 4, 5, 6,
+    ]);
+  });
 });
