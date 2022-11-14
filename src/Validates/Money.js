2af36716105b568d894bc6b17552c7eb80e35c2d
h2002y <h2002y@naver.com>
2022-11-14 17:32:43 +0900

    feat: Add validateMoney func in Validates/Money.js

---
+++ src/Validates/Money.js
@@ -0,0 +1,16 @@
+const validateMoney = (money) => {
+    if(isNaN(money) || money === "" || money === " "){
+        throw new Error("[ERROR] 숫자를 입력해주세요.");
+    }
+    if(Number(money) % 1 !== 0){
+      throw new Error("[ERROR] 실수가 아닌 금액을 입력해주세요.")
+    }
+    if(Number(money) < 1000){
+      throw new Error("[ERROR] 천원 이상의 금액을 입력해주세요.");
+    }
+    if(Number(money) % 1000 !== 0){
+      throw new Error("[ERROR] 천원 단위의 금액으로 입력해주세요.");
+    }
+};
+
+module.exports = { validateMoney };
