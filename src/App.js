beadf377db78a5ef85428097f45e81d6a96bcd29
Minju Park <project.mizzu@gmail.com>
2022-11-14 23:35:59 +0900

    Feat : 로또번호 오름차순 출력
    - 1 ~ 45 사이의 로또 번호 6개를 quantity만큼 뽑기
    - 로또번호 6개는 오름차순으로 정렬하여 출력

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const { Console } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 
 class App {
   constructor(text) {
@@ -7,8 +7,20 @@ class App {
 
   play() {
     Console.readLine(this.text, (cost) => {
-      const quantity = cost / 1000;
+      let quantity = cost / 1000;
       Console.print(`${'\n'}${quantity}개를 구매했습니다.`);
+      let computer = [];
+      while (quantity >= 1) {
+        while (computer.length < 6) {
+          const number = Random.pickNumberInRange(1, 45);
+          if (!computer.includes(number)) {
+            computer.push(number);
+          }
+        }
+        Console.print(computer.sort((a, b) => a - b));
+        computer = [];
+        quantity -= 1;
+      }
     });
   }
 }
