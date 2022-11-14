5132e5bb5adfc0f60322209f63fa4eae50d42aa4
murpin <qw04011@gmail.com>
2022-11-14 18:35:32 +0900

    feat(Lotto/getLottoNumber): 당첨 번호를 가져오는 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,6 +8,11 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  getLottoNumber() {
+    let JackpotNumber = this.#numbers.split(',');
+    return JackpotNumber;
+  }
+
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
