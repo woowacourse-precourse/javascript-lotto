fd6853bb6c72f437d29ddb497771b65fde9ac4b7
thewronghand <penfreak77@gmail.com>
2022-11-14 16:03:22 +0900

    test: printLottoPurchaseResult 메서드 동작 테스트 추가
    printLottoPurchaseResult 메서드 실행 시, 구매 매수와 현재 보유하고 있는 lotto의 내용이 콘솔에 출력되는지 확인한다.

--- __tests__/CustomerTest.js
+++ __tests__/CustomerTest.js
@@ -1,5 +1,13 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 const Customer = require('../src/Customer');
 const Lotto = require('../src/Lotto');
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe('커스터머 클래스 테스트', () => {
   test('purchaseLotto 메서드를 이용해 #lottos 필드에 새 로또를 추가할 수 있다', () => {
@@ -9,5 +17,17 @@ describe('커스터머 클래스 테스트', () => {
     customer.purchaseLotto(lotto1);
     customer.purchaseLotto(lotto2);
     expect(customer.list()).toEqual([lotto1, lotto2]);
+  });
+
+  test('printLottoPurchaseResult 메서드를 이용해 로또 구매 결과를 출력할 수 있다.', () => {
+    const logs = ['1개를 구매했습니다.', '[1, 2, 3, 4, 5, 6]'];
+    const logSpy = getLogSpy();
+    const customer = new Customer();
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    customer.purchaseLotto(lotto);
+    customer.printLottoPurchaseResult();
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
   });
 });
