cdcae5cb3d9e0321c19ae2af92ac526c14f0bfaf
Raehyeon Jeong <raehy19@gmail.com>
2022-11-15 00:09:42 +0900

    feat(src/App.js) : App play 메소드 금액 입력 받는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (money) => {});
+  }
 }
 
 module.exports = App;
