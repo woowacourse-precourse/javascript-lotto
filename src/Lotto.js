3841ed0a2b3541e06674f0a108a92aa8cab1613d
mochang2 <qjsrodksro@gmail.com>
2022-11-14 14:09:04 +0900

    feat: Lotto 클래스 생성자 유효성 검증 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,112 @@
+const {
+  validationError,
+  WINNING_NUMBER_COUNT,
+  range,
+} = require('./constants/lotto.js')
+
+/**
+ * @typedef {Object} lotto
+ * @property {number[]} wins - 당첨 번호
+ * @property {number} bonus - 보너스 번호
+ */
+
 class Lotto {
   #numbers
 
+  /**
+   * @param {lotto} numbers
+   */
   constructor(numbers) {
-    this.validate(numbers)
+    this.#validate(numbers)
+
     this.#numbers = numbers
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.')
+  /**
+   * @param {lotto} numbers
+   */
+  #validate(numbers) {
+    if (this.#isNotValidType(numbers)) {
+      throw new Error(validationError.TYPE)
+    }
+
+    if (this.#isNotValidLength(numbers)) {
+      throw new Error(validationError.WINNING_NUMBER_COUNT)
+    }
+
+    if (this.#isNotValidRange(numbers)) {
+      throw new Error(validationError.RANGE)
+    }
+
+    if (this.#isNotValidDuplication(numbers)) {
+      throw new Error(validationError.DUPLICATION)
     }
   }
 
-  // TODO: 추가 기능 구현
+  /**
+   * @param {lotto} numbers
+   * @returns {boolean}
+   */
+  #isNotValidType(numbers) {
+    if (!Array.isArray(numbers.wins)) {
+      return true
+    }
+
+    const isNotAllNumber =
+      numbers.wins.some((win) => !Number.isInteger(win)) ||
+      !Number.isInteger(numbers.bonus)
+
+    if (isNotAllNumber) {
+      return true
+    }
+
+    return false
+  }
+
+  /**
+   * @param {lotto} numbers
+   * @returns {booelan}
+   */
+  #isNotValidLength(numbers) {
+    if (numbers.wins.length !== WINNING_NUMBER_COUNT) {
+      return true
+    }
+
+    return false
+  }
+
+  /**
+   * @param {lotto} numbers
+   * @returns {booelan}
+   */
+  #isNotValidRange(numbers) {
+    const isNotInRange =
+      numbers.wins.some((win) => win < range.MIN || range.MAX < win) ||
+      numbers.bonus < range.MIN ||
+      range.MAX < numbers.bonus
+
+    if (isNotInRange) {
+      return true
+    }
+
+    return false
+  }
+
+  /**
+   * @param {lotto} numbers
+   * @returns {booelan}
+   */
+  #isNotValidDuplication(numbers) {
+    const lottoNumbers = [...numbers.wins, numbers.bonus]
+    const hasDuplication =
+      new Set(lottoNumbers).size !== WINNING_NUMBER_COUNT + 1
+
+    if (hasDuplication) {
+      return true
+    }
+
+    return false
+  }
 }
 
 module.exports = Lotto
