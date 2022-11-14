ecb9372db9449be6080f783e178b101c445f694e
yeonkyoung <keep.ykk@gmail.com>
2022-11-14 19:29:39 +0900

    feat: 입력한 금액이 천원으로 나누어 떨어지는지 확인하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const messages = require("./constants/messages.js");
+const terms = require("./constants/terms");
+const Lotto = require("./Lotto.js");
+
 class App {
+  constructor() {}
   play() {}
+
+  checkInputUnits(money) {
+    if (money % 1000 > 0) {
+      return { isPurchaseable: false, error: messages.MONEY_UNIT_ERROR };
+    }
+    return true;
+  }
 }
 
+a = new App();
 module.exports = App;
