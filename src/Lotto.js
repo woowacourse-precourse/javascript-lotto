be84ab00199652b25439b45eb840fe0b1f3cec1c
jetty8013 <jetty8013@gmail.com>
2022-11-14 22:44:00 +0900

    feat: 로또 번호 유효성(중복) 검사 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,6 +3,7 @@ class Lotto {
 
   constructor(numbers) {
     this.validate(numbers);
+    this.duplicateCheck(numbers)
     this.#numbers = numbers;
   }
 
@@ -13,6 +14,13 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  duplicateCheck(numbers) {
+    const checkNumbers = new Set(numbers);
+    if(checkNumbers.size !== numbers.length){
+      throw new Error("[ERROR] 중복 된 번호가 있습니다.");
+    }
+  }
+  
 }
 
 module.exports = Lotto;
