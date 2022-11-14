061ac0a10ac7f379a8723df8fa3adbb1b22ddf83
sunny <huiseon37@gmail.com>
2022-11-15 04:36:35 +0900

    test: 로또머신 클래스 테스트 코드 수정

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -1,15 +1,14 @@
 const LottoMachine = require("../src/LottoMachine");
 
 describe("로또머신 클래스 테스트", () => {
-  test("6개의 로또를 발행한다", () => {
-    const lottoMachine = new LottoMachine();
+  test("6개의 로또를 발행한다.", () => {
+    const lottoMachine = new LottoMachine(7700);
     const result = lottoMachine.makeLotto();
     expect(result).toHaveLength(6);
   });
 
   test("구매금액 만큼 로또를 발행한다", () => {
-    const lottoMachine = new LottoMachine();
-    lottoMachine.purchaseLotto(7700);
+    const lottoMachine = new LottoMachine(7700);
     const result = lottoMachine.getLottoList();
     expect(result).toHaveLength(7);
   });
