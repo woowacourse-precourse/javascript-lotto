edcc553dad05854339c5efeb41a9cab2090ade8d
RooT_Kim <bdd3452@gmail.com>
2022-11-14 22:54:59 +0900

    기능: 로또 구매 메소드 생성 ReadLine 클래스 내 구매 금액을 입력하여 callback 함수를 실행하는 payout 메소드 구현

--- src/MissionUtils/ReadLine.js
+++ src/MissionUtils/ReadLine.js
@@ -1,2 +1,10 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { Messages } = require('../Constants');
+
+class ReadLine {
+	payout(callback) {
+		Console.readline(Messages.START_BUY, (pay) => callback(pay));
+	}
+}
+
+module.exports = ReadLine;
