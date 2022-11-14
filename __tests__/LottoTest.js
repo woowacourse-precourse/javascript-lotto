61c7c40038c8cf5e1ca8ec14fcaa766dffc87eac
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-14 12:29:07 +0900

    test: 입력한 로또 각 자리가 범위 내에 존재확인 테스트 코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,24 @@
-const Lotto = require("../src/Lotto");
+/* eslint-disable */
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+const Lotto = require('../src/Lotto');
+const { ERROR_MESSAGE } = require('../src/constants');
+
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrowError(ERROR_MESSAGE.WINNING_NUMBER_LENGTH_ERROR);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrowError(ERROR_MESSAGE.WINNING_NUMBER_NOT_DIFFERENT_NUMBER_ERROR);
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('각 로또 번호 중 1-45 사이에 존재하지 않는 숫자가 있으면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 46, 0]);
+    }).toThrowError(ERROR_MESSAGE.NUM_IN_RANGE_ERROR);
+  });
 });
