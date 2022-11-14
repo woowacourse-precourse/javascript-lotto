1b015fced81829e19a8437c795bac79fd783b5fe
dev-redo <mis05041@naver.com>
2022-11-14 17:31:17 +0900

    feat(App): Lotto 게임 MVC 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-const App = class {
-  constructor() {}
+const LottoView = require('./view/LottoView');
+const LottoModel = require('./model/LottoModel');
+const LottoCtrl = require('./controller/LottoCtrl');
 
-  play() {}
+const App = class {
+  constructor() {
+    this.lottoView = new LottoView();
+    this.lottoModel = new LottoModel();
+    this.lottoCtrl = new LottoCtrl(this.lottoView, this.lottoModel);
+  }
+
+  play() {
+    this.lottoCtrl.start();
+  }
 };
+
+const app = new App();
+app.play();
 
 module.exports = App;
