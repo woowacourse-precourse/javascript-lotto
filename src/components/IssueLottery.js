7d6e50b9b81cf371a6e4c3d5e27a59e824aa9c56
semnil5202 <semnil5202@gmail.com>
2022-11-15 03:18:16 +0900

    refactor: 로또 구입 개수 출력을 위한 변수 설정

--- src/components/IssueLottery.js
+++ src/components/IssueLottery.js
@@ -5,11 +5,12 @@ class IssueLottery {
   static setLotteryNumber(purchaseNumber) {
     const results = [];
     let draw = purchaseNumber / 1000;
+    let count = draw;
     while (draw !== 0) {
       results.push(new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6)));
       draw -= 1;
     }
-    return results;
+    return [results, count];
   }
 }
 
