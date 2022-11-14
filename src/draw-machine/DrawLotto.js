d952e1c4e49932d45f4be3b6421e83f1ae6e3346
SorinKwon <ksl020700@gmail.com>
2022-11-15 02:39:53 +0900

    feat: 보너스 번호 입력 기능 추가

--- src/draw-machine/DrawLotto.js
+++ src/draw-machine/DrawLotto.js
@@ -11,6 +11,13 @@ class DrawLotto {
         this.draw_number = draw_number;
     });
     }
+    inputBounsNumbers(){
+      MissionUtils.Console.readLine("보너스 번호를 입력해 주세요.", (bonus_number) => {
+        console.log(bonus_number);
+        this.bonus_number=bonus_number;
+    });
+    }
+
 
   }
   
