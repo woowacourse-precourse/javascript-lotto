0840eff930abbbf05dbf5b9c55c8426d371c5fb5
violetwhenisawu <anashape710@gmail.com>
2022-11-14 20:48:25 +0900

    refactor: 당첨 내역, 이익금을 증가시키는 조건문 간결하게 수정

--- src/model/LottoResult.js
+++ src/model/LottoResult.js
@@ -16,28 +16,18 @@ class LottoResult {
   getWinStatus() {
     this.filteredArr.forEach((nums) => {
       let arrLength = nums.length;
-      if (arrLength === 3) {
-        this.totalWinStatus.countWinning[0] += 1;
-        this.totalWinStatus.totalReturn += 5000;
-      }
-      if (arrLength === 4) {
-        this.totalWinStatus.countWinning[1] += 1;
-        this.totalWinStatus.totalReturn += 50000;
-      }
-      if (arrLength === 5 && !this.sameBonusNum) {
-        this.totalWinStatus.countWinning[2] += 1;
-        this.totalWinStatus.totalReturn += 1500000;
-      }
-      if (arrLength === 5 && this.sameBonusNum) {
-        this.totalWinStatus.countWinning[3] += 1;
-        this.totalWinStatus.totalReturn += 30000000;
-      }
-      if (arrLength === 6) {
-        this.totalWinStatus.countWinning[4] += 1;
-        this.totalWinStatus.totalReturn += 2000000000;
-      }
+      if (arrLength === 3) this.statusChange(0, 5000);
+      if (arrLength === 4) this.statusChange(1, 50000);
+      if (arrLength === 5) this.statusChange(2, 1500000);
+      if (arrLength === 5 && !this.sameBonusNum) this.statusChange(3, 30000000);
+      if (arrLength === 6) this.statusChange(4, 2000000000);
     });
     return this.totalWinStatus;
+  }
+
+  statusChange(index, money) {
+    this.totalWinStatus.countWinning[index] += 1;
+    this.totalWinStatus.totalReturn += money;
   }
 }
 
