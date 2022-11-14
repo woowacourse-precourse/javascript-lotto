000cf7b9bb9a9ee8013994e6539698c832286ff1
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-11-14 12:09:01 +0900

    test: figureLotteryRank() 전수 조사

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
 const Lotto = require('../src/Lotto');
+const { figureLotteryRank } = require('../src/utils/lottery');
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -18,6 +19,48 @@ describe('로또 클래스 테스트', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 50]);
     }).toThrow('[ERROR]');
+  });
+
+  test('로또 판별기 1 : FIRST', () => {
+    const hit = 6;
+    const bonus = false;
+    const result = figureLotteryRank(hit, bonus);
+    expect(result).toEqual('FIRST');
+  });
+
+  test('로또 판별기 2 : SECOND', () => {
+    const hit = 5;
+    const bonus = true;
+    const result = figureLotteryRank(hit, bonus);
+    expect(result).toEqual('SECOND');
+  });
+
+  test('로또 판별기 3 : THIRD', () => {
+    const hit = 5;
+    const bonus = false;
+    const result = figureLotteryRank(hit, bonus);
+    expect(result).toEqual('THIRD');
+  });
+
+  test('로또 판별기 4 : FOURTH', () => {
+    const hit = 4;
+    const bonus = false;
+    const result = figureLotteryRank(hit, bonus);
+    expect(result).toEqual('FOURTH');
+  });
+
+  test('로또 판별기 5 : FIFTH', () => {
+    const hit = 3;
+    const bonus = false;
+    const result = figureLotteryRank(hit, bonus);
+    expect(result).toEqual('FIFTH');
+  });
+
+  test('로또 판별기 6 : 꽝', () => {
+    const hit = 2;
+    const bonus = false;
+    const result = figureLotteryRank(hit, bonus);
+    expect(result).toEqual(null);
   });
 
   // 아래에 추가 테스트 작성 가능
