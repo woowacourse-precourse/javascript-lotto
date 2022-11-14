6f27fbf71315cb0f423c0695dd82dc0882a495ec
banma1234 <banma1234@gmail.com>
2022-11-14 23:08:45 +0900

    feature: 로또게임 실행함수 / refactor: 출력관련 함수 모듈화

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-const { MissionUtils } = require('@woowacourse/mission-utils');
+const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require('./Lotto');
-const Money = require('./UserMoney');
+const InputMoney = require('./InputMoney');
+const LottoNumbers = require('./LottoNumbers');
+const MakeRandomValue = require('./MakeRandomValue');
 
 class App{
-  play() {
-    let MONEY = new Money().getUserMoney();
+  constructor() {
+    this.inputMoney = new InputMoney();
+    this.lottoNumbers = new LottoNumbers();
+  }
 
+  play() {
+    let MONEY = this.inputMoney.getMoney();
+    let NUMBERS = this.lottoNumbers.getNumbers();
+    let BONUS_NUMBER = this.lottoNumbers.getBonusNumber();
+
+    let lotteryTicket = new MakeRandomValue().makeRandomValue(MONEY);
+    this.printTickets(lotteryTicket, MONEY);
+
+    const lotto = new Lotto(NUMBERS);
+    lotto.matchLotto(lotteryTicket, BONUS_NUMBER, MONEY);
+  }
+
+  printTickets(lotteryTicket, MONEY) {
+    MissionUtils.Console.print(`${MONEY/1000}개를 구매했습니다.`);
+
+    lotteryTicket.forEach((item) => {
+      let temp = '[' + String(item.join(', ')) + ']';
+      MissionUtils.Console.print(temp);
+    });
   }
 }
-
-const app = new App();
-app.play();
 
 module.exports = App;
