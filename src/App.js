8ae6b9f5bd4b34b22687383b1088f24729d1f068
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-14 14:18:33 +0900

    feat(Application): 로또 구매 함수
    사용자 입력에 따라서 로또를 구매한다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 const Application = require('./Application');
+const Lotto = require('./Lotto');
 
 class App {
   static purchaseCount(userInputNumber) {
     const UNIT_AMOUNT = 1000;
 
     return Application.purchaseCount(userInputNumber, UNIT_AMOUNT);
+  }
+
+  static buyLotto(userInputNumber) {
+    return Lotto.buyLotto(userInputNumber);
   }
 }
 
