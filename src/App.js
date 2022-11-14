af7fc36004c3ab32ea650f664ba505aec0a2f738
vjvl95 <vjvl95@naver.com>
2022-11-15 04:21:11 +0900

    feat: 사용자의 돈 입력받는 기능 완성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 const Lotto = require('./Lotto');
-
+const MissionUtils = require('@woowacourse/mission-utils');
 class App {
-  play() {}
+  play() {
+    const money = this.getMoney();
+  }
+
+  getMoney() {
+    let money = 0;
+    MissionUtils.Console.readLine('구매금액을 입력해주세요.', (answer) => {
+      money = answer;
+    });
+  }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
