f94d517f7c60155af7963897a347fc34db33377a
wlthdud <thdudzi@naver.com>
2022-11-15 00:02:12 +0900

    feat: Lotto 테스트케이스 작성

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,10 +9,18 @@ class Lotto {
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    } 
+    if((new Set(numbers)).size != 6){
+      throw new Error("[ERROR] 중복된 숫자가 있습니다.");
+    }
+    if(numbers[0] < 1 || numbers[5] > 45){
+      throw new Error("[ERROR] 로또 번호는 1과 45사이의 수여야 합니다.");
     }
   }
 
   // TODO: 추가 기능 구현
+  
+
 }
 
 module.exports = Lotto;
