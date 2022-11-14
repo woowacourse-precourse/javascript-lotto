62cd159e7e7de869353689813284b0ab77bdeb7c
BuildTools <unconfigured@null.spigotmc.org>
2022-11-14 23:16:27 +0900

    feat : 각 로또의 당첨 번호가 몇개인지 세는 함수

--- src/LottoMain.js
+++ src/LottoMain.js
@@ -13,4 +13,21 @@ function printResult(lottoList, prize, bonus) {
   ConsoleWork.print('당첨 통계\n---\n');
 }
 
+function lottoLoop(lottoList, prize, bonus) {
+  for (let i = 0; i < lottoList.length; i++) {
+    checkLottoResult(lottoList[i], prize, bonus);
+  }
+}
+
+function checkLottoResult(lotto, prize, bonus) {
+  const prizeCount = 0;
+  if (lotto.includes(prize)) {
+    prizeCount++;
+  }
+  if (lotto.includes(bonus)) {
+    prizeCount++;
+  }
+  return prizeCount;
+}
+
 module.exports = LottoMain;
