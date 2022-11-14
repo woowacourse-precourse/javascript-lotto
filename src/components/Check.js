0f076aa9257275575a6049c40cf663a7599d0971
Sallyvely <tpdus4159@naver.com>
2022-11-14 14:49:31 +0900

    refactor: 코드 리팩토링

--- src/components/Check.js
+++ src/components/Check.js
@@ -1,25 +1,19 @@
-const Lotto=require("../Lotto.js");
-class Check{
-
-    checkDivideMoney(money){
-        if(!(money%1000==0)){
-            throw new Error("[ERROR]");
-        }
-        return true;
+const Lotto = require('../Lotto.js');
+class Check {
+  checkDivideMoney (money) {
+    if (!(money % 1000 === 0)) {
+      throw new Error('[ERROR]');
     }
+    return true
+  }
 
-    checkWinNumVaildation(winningstr){; 
-        let winningArr=winningstr.split(",").map(Number);
-        let lotto=new Lotto(winningArr);
-        // if(!lotto.checkNumberRange(winningArr)){
-        //     throw new Error("[ERROR]");
-        // }
-        lotto.checkNumberRange(winningArr);
-        lotto.validate(winningArr);
-        lotto.checkUniqueRange(winningArr);
-        return true;
-    }
-
-
+  checkWinNumVaildation (winningstr) {
+    const winningArr = winningstr.split(',').map(Number);
+    const lotto = new Lotto(winningArr);
+    lotto.checkNumberRange(winningArr);
+    lotto.validate(winningArr);
+    lotto.checkUniqueRange(winningArr);
+    return true;
+  }
 }
 module.exports = Check;
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,43 +1,42 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const Check=require("./Check.js");
-const Lotto= require("../Lotto.js");
+const Check = require('./Check.js');
 
-class Input{
+class Input {
+  inputAmountMoney () {
+    const check = new Check();
+    let lottomoney;
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.', money => {
+      if (check.checkDivideMoney(money)) {
+        lottomoney = money;
+      }
+    });
+    return lottomoney;
+  }
 
-    inputAmountMoney(){
-        let check= new Check();
-        let lottomoney;
-        MissionUtils.Console.readLine('구입금액을 입력해 주세요.',money=>{
-            if(check.checkDivideMoney(money)){
-                lottomoney=money;
-            }
-        });
-        return lottomoney;
-    } 
-    inputMoneyCount(){
-        let count=this.inputAmountMoney()/1000;
-        return count;
-    }
-    inputWinNumber(){
-        let check= new Check();
-        let winNumber;
-        MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.", number=>{
-            if(check.checkWinNumVaildation(number)==true){
-                winNumber=number;
-            }
-        })
-        return winNumber;
-    }
-    inputBonusNumber(){
-        let bonusNumber;
-        MissionUtils.Console.readLine("보너스 번호를 입력해 주세요.", number=>{
-            bonusNumber=number;
+  inputMoneyCount () {
+    const count = this.inputAmountMoney() / 1000;
+    return count;
+  }
 
-        })
-        MissionUtils.Console.close();
-        return bonusNumber;
-    }
+  inputWinNumber () {
+    const check = new Check();
+    let winNumber;
+    MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.', number => {
+      if (check.checkWinNumVaildation(number)) {
+        winNumber = number;
+      }
+    })
+    return winNumber;
+  }
 
+  inputBonusNumber () {
+    let bonusNumber;
+    MissionUtils.Console.readLine('보너스 번호를 입력해 주세요.', number => {
+      bonusNumber = number;
+    })
+    MissionUtils.Console.close();
+    return bonusNumber;
+  }
 }
 
 module.exports = Input;
