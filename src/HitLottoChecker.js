228c6d533a06c25c37fd6d3e6150e4157f49ce87
foundaway98 <foundaway98@naver.com>
2022-11-14 16:35:01 +0900

    <feat> : HitLottoChecker 클래스 constructor 추가
    HitLottoChecker 클래스에 constructor 추가
    - 인자로 로또 구매 금액과 구매 로또 배열을 받으면 멤버변수에 저장한다

--- src/HitLottoChecker.js
+++ src/HitLottoChecker.js
@@ -1,4 +1,21 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
-class HitLottoChecker {}
+class HitLottoChecker {
+  lottoPurchaseFee;
+  boughtLottoArray;
+  hitLotto;
+  bonusNumber;
+  matchLottoNumbersTable = {
+    3: 0,
+    4: 0,
+    5: 0,
+    "5B": 0,
+    6: 0,
+  };
+
+  constructor(lottoPurchaseFee, lottoArray) {
+    this.lottoPurchaseFee = lottoPurchaseFee;
+    this.boughtLottoArray = lottoArray;
+  }
+}
 module.exports = HitLottoChecker;
