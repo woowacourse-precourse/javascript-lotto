96c10c1cf64ceb7191a37a5113665339fe735795
ho-ji <wkdd0205@gmail.com>
2022-11-15 01:12:05 +0900

    feat: Add getCountMatchNumber() in Lotto class
    Lotto 클래스 안에 비교할 숫자들을 인자로 받아 number와 일치하는 개수를 반환하는 함수를 구현함

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,6 +13,14 @@ class Lotto {
     if (new Set(numbers).size !== 6)
       throw new Error("[ERROR] 로또 번호는 서로 다른 6개의 숫자여야 합니다.");
   }
+  getCountOfMatchNumber(numbersToCompare){
+    let countOfMatchNumber = 0;
+    for(let numberToCompare of numbersToCompare){
+      if(this.#numbers.includes(numberToCompare))
+       countOfMatchNumber++;
+    }
+    return  countOfMatchNumber;
+  }
 }
 
 module.exports = Lotto;
