7c5f702e101f2e629e1baa1b1f2dd2efb605ab8a
pione3r <jhyong0626@gmail.com>
2022-11-14 22:06:52 +0900

    feat: add errorHandling function
    add errorHandling function when purchase lotto

--- src/User.js
+++ src/User.js
@@ -1,8 +1,8 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class User {
-  purchaseAmount;
-  lottos;
+  purchaseAmount = 0;
+  lottos = [];
 
   constructor() {
     this.getPurchaseAmount();
@@ -11,25 +11,44 @@ class User {
 
   getPurchaseAmount() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (answer) => {
-      this.validatePurchaseAmount(Number(answer));
-      this.purchaseAmount = Number(answer);
+      let purchaseAmount = answer;
+      this.validatePurchaseAmount(purchaseAmount);
+      this.purchaseAmount = Number(purchaseAmount);
     });
   }
 
   validatePurchaseAmount(amount) {
-    if (amount % 1000 !== 0) {
+    if (Number(amount) % 1000 !== 0) {
       throw new Error("[ERROR] 입력 금액은 1000원 단위여야 합니다.");
+    }
+
+    if (isNaN(Number(amount))) {
+      throw new Error("[ERROR] 입력 금액은 숫자여야 합니다.")
     }
   }
 
   purchaseLottos() {
-    const lottosAmount = this.purchaseAmount % 1000;
+    let lottosAmount = this.purchaseAmount / 1000;
 
-    MissionUtils.Console.print(`${lottosAmount}개를 구매했습니다.`);
-    
+    MissionUtils.Console.print(`${lottosAmount}개를 구매했습니다.\n`);
+
     for (let i = 0; i < lottosAmount; i++) {
       let lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-      MissionUtils.Console.print(lotto);
+      MissionUtils.Console.print(
+        "[" +
+          lotto[0] +
+          ", " +
+          lotto[1] +
+          ", " +
+          lotto[2] +
+          ", " +
+          lotto[3] +
+          ", " +
+          lotto[4] +
+          ", " +
+          lotto[5] +
+          "]"
+      );
       this.lottos.push(lotto);
     }
   }
