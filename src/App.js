6290c1b9d73750017fe5fb3c6f17747048181443
hyukzz <ri_chard@naver.com>
2022-11-14 18:52:41 +0900

    feat: 구매한 로또 갯수 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const UserInput = require("./UserInput.js");
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+	lottoCnt;
+
+	userInputLotto(price) {
+		let userInput = new UserInput(price);
+		this.lottoCnt = userInput.lottoCnt;
+	}
+
+	printLottoCnt(lottoCnt) {
+		Console.print(`${lottoCnt}개를 구매했습니다.`);
+	}
+	play() {
+		Console.print("구입금액을 입력해 주세요.");
+	}
 }
 
 module.exports = App;
