834ba7bcf40ea0869538866939d7c819fed8e697
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-15 00:01:25 +0900

    feat: 로또 번호 범위 예외 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -15,6 +15,10 @@ class Lotto {
     if (new Set(numbers).size !== numbers.length) {
       throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
+
+    if (numbers.some((number) => number < 1 || number > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 수여야 합니다.");
+    }
   }
 
   get numbers() {
