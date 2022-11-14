a38f75bace939c5ae4f052a9faded7e294d69baf
kknyapple <kknyapple@gmail.com>
2022-11-14 14:46:30 +0900

    feat: 로또 당첨 내역 출력하기

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -52,11 +52,25 @@ class LottoGame {
     });
   }
 
-  printResult(lottos) {
+  printWinningResult(result) {
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${result[3] || 0}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${result[4] || 0}개`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${result[5] || 0}개`);
+    MissionUtils.Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${result["5+1"] || 0}개`
+    );
+    MissionUtils.Console.print(
+      `6개 일치 (2,000,000,000원) - ${result[6] || 0}개`
+    );
+  }
+
+  printResult(lottos, money) {
     const result = {};
     lottos.matchingNumber.forEach((x) => {
       result[x] = (result[x] || 0) + 1;
     });
+
+    this.printWinningResult(result);
 
     MissionUtils.Console.close();
   }
