b5f8d8e5061dde8c4f307d036912f5f7b58ba3f3
금가경 <gageum0@gmail.com>
2022-11-14 16:57:21 +0900

    fix(lotto) : 로또 당첨 번호 유효성 오류 개선

--- src/Constants/ErrorMessages.js
+++ src/Constants/ErrorMessages.js
@@ -5,7 +5,6 @@ const purchaseError = {
 };
 
 const lottoNumberError = {
-  NOT_DIVIDED_BY_COMMA: '[ERROR] 숫자는 콤마로 구분해 주세요.',
   NOT_VALID_NUMBER_SCOPE: '[ERROR] 숫자 범위가 1~45가 아닙니다',
   NOT_SIX: '[ERROR] 로또 번호는 6개여야 합니다.',
   NUMBER_DUPLICATION: '[ERROR] 중복된 번호를 입력할 수 없습니다',
--- src/Lotto.js
+++ src/Lotto.js
@@ -11,8 +11,8 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error(lottoNumberError.NOT_SIX);
     }
-    if (numbers < 1 || numbers > 45) {
-      throw new Error(lottoNumberError.NOT_DIVIDED_BY_COMMA);
+    if (numbers === [...new Set(numbers)]) {
+      throw new Error(lottoNumberError.NUMBER_DUPLICATION);
     }
     if (numbers !== numbers.filter((number) => number >= 1 && number <= 45)) {
       throw new Error(lottoNumberError.NOT_VALID_NUMBER_SCOPE);
