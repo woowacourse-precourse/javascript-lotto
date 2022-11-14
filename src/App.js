cb387db0a08fa105d212b1a4e2c16891ce373af1
GunHoi <lgh509@kyonggi.ac.kr>
2022-11-14 18:02:46 +0900

    feat - 7 : 수익률 (소수점 2자리까지 )계산

--- src/App.js
+++ src/App.js
@@ -131,6 +131,10 @@ class App {
   }
   calcRateOfReturn(inputMoney, earnMoney){
     const RateOfReturn = (earnMoney/inputMoney)*100;
+    this.calcDecimalPointTwo(RateOfReturn);
+  }
+  calcDecimalPointTwo(RateOfReturn){
+    Math.round(RateOfReturn*100)/100;
   }
   calcEarnMoney(winningStatic){
     let winningMoney = winningStatic[4]*5000 +  winningStatic[3]*50000 +  winningStatic[2]*1500000 +  winningStatic[1]*30000000 +  winningStatic[0]*2000000000;
