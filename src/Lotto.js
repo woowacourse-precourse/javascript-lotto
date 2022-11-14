b28035211c14815010d5b6c0449a992060721460
yeongseokim <1903020@ewhain.net>
2022-11-14 13:03:10 +0900

    feat(Lotto): Lotto에 대한 get 접근자 프로퍼티 생성

--- src/Lotto.js
+++ src/Lotto.js
@@ -6,13 +6,15 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  get numbers() {
+    return this.#numbers;
+  }
+
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
