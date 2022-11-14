e9b663cc4ef2baa05f9e23aea48475717acc13c1
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-15 02:57:07 +0900

    feat: 당첨 번호, 보너스 번호 예외 처리 기능 추가 (Winning.js)

--- src/controller/Winning.js
+++ src/controller/Winning.js
@@ -11,29 +11,45 @@ class Winning{
   start(){
     this.view.inputWin();
   }
-  validWin(win_numbers){
-    const lotto = new Lotto(win_numbers);
-    // 로또 중복 테스트 and RETURN
-    return lotto.winTest();
+  validateWin(input){
+    const check = /^[0-9]+$/;
+    input.split(',').forEach((target)=>{
+      if(!check.test(target)){
+        throw new Error("[ERROR] 로또 번호는 숫자로 이루어져야 합니다.")
+      }
+    })
   }
-  validBonus(bonus_numbers){
-    const win_numbers = this.model.getWin();
+  validateBonus(input){
+    const check = /^[0-9]+$/;
+    if(!check.test(input)){
+      throw new Error("[ERROR] 보너스 번호는 숫자로 이루어져야 합니다.")
+    }
+  }
+  checkWinNumbers(win_numbers){
     const lotto = new Lotto(win_numbers);
-    // 보너스 넘버 유효 테스트 and RETURN
-    return lotto.bonusTest(bonus_numbers);
+  }
+  checkBonusNumber(bonus_number){
+    if(this.model.getWin().includes(bonus_number)){
+      throw new Error("[ERROR] 보너스 번호는 당첨 번호에 포함되면 안됩니다")
+    }
+    if(bonus_number<1 || bonus_number>45){
+      throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.")
+    }
   }
   controllWin(input){
+    this.validateWin(input);
     const win_numbers = input.split(',').map((number)=>parseInt(number));
-    if(this.validWin(win_numbers)){
-      this.model.setWin(win_numbers);
-    }
+    this.checkWinNumbers(win_numbers);
+
+    this.model.setWin(win_numbers);
     this.view.inputBonus();
   }
   controllBonus(input){
+    this.validateBonus(input);
     const bonus_number = parseInt(input);
-    if(this.validBonus(bonus_number)){
-      this.model.setBonus(bonus_number);
-    }
+    this.checkBonusNumber(bonus_number);
+
+    this.model.setBonus(bonus_number);
     this.next();
   }
   next(){
