91e182e0bcb4d2c7d6829a81d9f1ab5bcbbe2e05
하지훈 <jnh03336@gmail.com>
2022-11-14 17:57:03 +0900

    feat(lotto): 로또 번호가 유효한지 확인한다

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,6 +11,14 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (!numbers.every((elem, idx) => numbers.indexOf(elem) === idx)) {
+      throw new Error("[ERROR] 로또 번호는 모두 서로 달라야 합니다.");
+    }
+    if (
+      !numbers.every((elem) => Number.isInteger(elem) && elem > 0 && elem < 46)
+    ) {
+      throw new Error("[ERROR] 로또 번호는 모두 1이상 45이하 정수여야 합니다.");
+    }
   }
 
   getNumbers() {
