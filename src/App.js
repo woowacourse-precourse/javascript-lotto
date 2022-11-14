78c2454fb800b6461c1a517fc063c45656369cc7
jiwon903 <50747295+jiwon903@users.noreply.github.com>
2022-11-14 20:56:00 +0900

    기능구현 목록 작성 및 구입금액 입력받기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const MissionUtils =require("@woowacourse/mission-utils");
+
+
+
 class App {
-  play() {}
+  play() {
+    this.getMoney()
+    
+    
+  }
+  getMoney() {
+    MissionUtils.Console.readLine('구입금액을 입력해주세요\n', this.saveMoney.bind(this));
+  }
+  saveMoney(input) {
+    const money = input;
+    MissionUtils.Console.print('\n'+ money/1000 + ' 개를 구매했습니다.')
+  }
+
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class Lotto {
   #numbers;
 
@@ -11,8 +12,27 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
+  getMoney() {
+    MissionUtils.Console.readLine('구입금액을 입력해주세요\n', this.saveMoney.bind(this));
+  }
+  saveMoney(input) {
+    const money = input;
+    MissionUtils.Console.print(money + ' 개를 구매했습니다.')
+  }
+
+  
+
 
   // TODO: 추가 기능 구현
 }
+
+
+
+
+
+
+
+
+
 
 module.exports = Lotto;
