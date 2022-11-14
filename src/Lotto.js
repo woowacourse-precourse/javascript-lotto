570688240d7dca31995d3656d529c125a9d0a4bc
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-14 22:36:07 +0900

    feat: 입력된 당첨 번호, 보너스 번호 검사 (Lotto.js)

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,6 +13,19 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  winTest(){
+    const setNumbers = new Set(this.#numbers);
+    if (this.#numbers.length !== [...setNumbers].length){
+      throw new Error("[ERROR] 로또 번호는 중복되면 안됩니다.")
+    }
+    return true;
+  }
+  bonusTest(bonus_number){
+    if (this.#numbers.includes(bonus_number)){
+      throw new Error("[ERROR] 보너스 번호는 로또 번호에 포함되면 안됩니다.")
+    }
+    return true;
+  }
 }
 
 module.exports = Lotto;
