cf1f119a871bf456f87b378e6536f1bbf909b642
Deemou <4106243@naver.com>
2022-11-14 21:57:39 +0900

    chore(constant): 로또 상수 수정 및 추가

--- src/constant/Lotto.js
+++ src/constant/Lotto.js
@@ -1,23 +1,34 @@
-const NUMBER = Object.freeze({
-  START: 1,
-  END: 45,
-  LIMIT: 6,
-  MONEY: 1000
+const LOTTO = Object.freeze({
+  PRICE: 1000,
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 45,
+  LENGTH: 6,
+  TOTAL_RANK: 5,
+  MATCH: [3, 4, 5, 5, 6],
+  PRIZE: [5000, 50000, 1500000, 30000000, 2000000000],
+  RANK: {
+    FIRST: 4,
+    SECOND: 3,
+    THIRD: 2,
+    FOURTH: 1,
+    FIFTH: 0
+  }
 });
 const MESSAGE = Object.freeze({
-  INPUT_MONEY: '구입금액을 입력해 주세요.',
-  PURCHASE_RESULT: (number) => `${number}개를 구매했습니다.`,
-  INPUT_NUMBER: '당첨 번호를 입력해 주세요.',
-  INPUT_BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
-  RESULT: '당첨 통계\n---',
-  WIN_FIRST: (number) => `6개 일치 (2,000,000,000원) - ${number}개`,
-  WIN_SECOND: (number) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${number}개`,
-  WIN_THIRD: (number) => `5개 일치 (1,500,000원) - ${number}개`,
-  WIN_FOURTH: (number) => `4개 일치 (50,000원) - ${number}개`,
-  WIN_FIFTH: (number) => `3개 일치 (5,000원) - ${number}개`,
+  INPUT_MONEY: '구입금액을 입력해 주세요.\n',
+  PURCHASE_RESULT: (number) => `\n${number}개를 구매했습니다.`,
+  INPUT_NUMBER: '\n당첨 번호를 입력해 주세요.\n',
+  INPUT_BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
+  RESULT: '\n당첨 통계\n---',
+  WIN: (match, bonus = '', prize, count) => `${match}개 일치${bonus} (${prize}원) - ${count}개`,
   RATES_OF_RETURN: (number) => `총 수익률은 ${number}%입니다.`
 });
+const REGEX = Object.freeze({
+  NUMBER: /^\d+$/
+});
+
 module.exports = {
-  NUMBER,
-  MESSAGE
+  LOTTO,
+  MESSAGE,
+  REGEX
 };
