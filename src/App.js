62c1a872bed5f3641f665ac14d0319be9b6e06fe
mjmj0217 <mjmj0217@naver.com>
2022-11-14 13:25:32 +0900

    feat(App): make Lotto

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+const Lotto = require("./Lotto");
 const { Console, Random } = require("@woowacourse/mission-utils");
 
 const INPUT_MESSAGE = {
@@ -5,22 +6,34 @@ const INPUT_MESSAGE = {
   bonus: "보너스 번호를 입력하세요.",
   winning: "당첨 번호를 입력해 주세요.",
 };
-
 const MONEY_ERROR = {
   number: "[ERROR] 숫자를 입력해 주세요",
   amount: "[ERROR] 1000원 단위로 입력해 주세요",
 };
 
 class App {
+  prizeNumbersArray;
+  userNumbersArray;
+  lotto;
+
   play() {
     Console.print(INPUT_MESSAGE.money);
     Console.readLine("", (money) => {
       this.validateMoney(money);
-    });
 
-    this.userNumbersArray = this.getRandomNumbers(numberOfLottery);
-    this.printRandomNumbers(this.userNumbersArray);
+      this.userNumbersArray = this.getRandomNumbers(numberOfLottery);
+      this.printRandomNumbers(this.userNumbersArray);
+
+      this.MakeLotto(this.userNumbersArray);
+    });
   }
+
+  printRandomNumbers(userNumbersArray) {
+    for (let arr of userNumbersArray) {
+      Console.print("[" + arr.join(", ") + "]");
+    }
+  }
+
   getRandomNumbers(num) {
     const userNumbersArray = [];
 
@@ -29,11 +42,18 @@ class App {
         Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b)
       );
     }
-  printRandomNumbers(userNumbersArray) {
-    for (let arr of userNumbersArray) {
-      Console.print("[" + arr.join(", ") + "]");
-    }
+
+    return userNumbersArray;
   }
+
+  MakeLotto() {
+    Console.print(INPUT_MESSAGE.winning);
+    Console.readLine("", (prizeNums) => {
+      this.prizeNumbersArray = prizeNums.split(",");
+      this.lotto = new Lotto(this.prizeNumbersArray);
+    });
+  }
+
   validateMoney(money) {
     if (isNaN(money)) {
       throw new Error(MONEY_ERROR.number);
