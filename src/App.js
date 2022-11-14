55d323c492c3d6057c6a0b270c47aedf021d7009
dustmddus <dustmddus5@gmail.com>
2022-11-14 12:56:37 +0900

    feat: 구입 금액 입력받고 로또 수량 구하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const { Random, Console } = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+
 class App {
-  play() {}
+  setLottoAmount(money) {
+    const lottoAmount = Number(money / 1000);
+    this.printLottoNumber(lottoAmount);
+  }
+
+  inputMoney() {
+    Console.readLine("구매 금액을 입력해주세요.", (money) => {
+      this.setLottoAmount(money);
+    });
+  }
+
+  play() {
+    this.inputMoney();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
