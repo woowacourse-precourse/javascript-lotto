e10ee6913666f1740808d08b8cbd492c38f934ed
violetwhenisawu <anashape710@gmail.com>
2022-11-15 02:56:17 +0900

    test: 로또 번호에 공백이 있는지 확인하는 테스트코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -31,4 +31,10 @@ describe('로또 클래스 테스트', () => {
       new Lotto([0, 2, 3, 4, 5, 45]);
     }).toThrow('[ERROR]');
   });
+
+  test('로또 번호에 공백이 있으면 예외 발생.', () => {
+    expect(() => {
+      new Lotto([ , ' ', 3, 4, 5, 6 ]);
+    }).toThrow('[ERROR]');
+  });
 });
