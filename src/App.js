d3818eb1e2f3288613ecd424b2b93f56421d9d86
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-14 23:33:04 +0900

    feat : 입력한 금액의 로또 개수와 랜덤숫자 출력

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (price) => {
         this.priceExceptionHandling(price)
         console.log(`구입금액을 입력해 주세요.\n ${price}`);
+        this.totalLottoCounts(price);
       }
     );
   };
@@ -16,6 +17,19 @@ class App {
   priceExceptionHandling(price) {
     if(price % 1000 !== 0 ) throw '[ERROR] 구입 금액은 1,000원 단위 입니다';
   };
+
+  totalLottoCounts(price) {
+    const totalCounts = price / 1000;
+    MissionUtils.Console.print(`${totalCounts}개를 구매했습니다.`);
+    this.printRandomLotto(totalCounts)
+  };
+
+  printRandomLotto(totalCounts) {
+    for (let i = 0; i < totalCounts; i++) {
+      const LottoArr = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+      MissionUtils.Console.print(LottoArr);
+    }
+  }
 }
 
 module.exports = App;
