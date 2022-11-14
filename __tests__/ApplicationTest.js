a91720660714875d5a4f26ba1ffa07d5a749ec3f
Narcoker <jykim9868@gmail.com>
2022-11-15 01:05:58 +0900

    Test: 보너스 숫자 입력 기능 및 예외 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,6 +1,7 @@
 const App = require("../src/App");
 const MissionUtils = require("@woowacourse/mission-utils");
 const Person = require("../src/Person");
+const Lotto = require("../src/Lotto");
 const { ERROR } = require('../src/Constants');
 const { SYSTEM } = require("../src/System");
 
@@ -72,6 +73,31 @@ describe("로또 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
+  // 테스트 추가
+
+  // 로또 자동 작성 테스트
+  test("기능 테스트: 금액에 맞는 개수 만큼 로또 생성", () => {
+    const logs = [
+      "3개를 구매했습니다.",
+      "[1, 2, 3, 5, 42, 43]",
+      "[2, 4, 8, 16, 38, 44]",
+      "[5, 7, 8, 10, 11, 12]",
+    ];
+    mockRandoms([
+      [1, 2, 3, 5, 42, 43],
+      [2, 4, 8, 16, 38, 44],
+      [5, 7, 8, 10, 11, 12],
+    ]);
+    const logSpy = getLogSpy();
+
+    const cash = '3000';
+    SYSTEM.publishLotto(cash);
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
   test("예외 테스트: 현금이 1,000원으로 나누어 떨어지지 않는 경우 1", () => {
     expect(() => {
       const person = new Person();
@@ -100,27 +126,64 @@ describe("로또 테스트", () => {
     }).toThrow(ERROR.CASH_IS_NOT_NATURAL_NUMBER);
   });
 
-  test("기능 테스트: 금액에 맞는 개수 만큼 로또 생성", () => {
-    const logs = [
-      "3개를 구매했습니다.",
-      "[1, 2, 3, 5, 42, 43]",
-      "[2, 4, 8, 16, 38, 44]",
-      "[5, 7, 8, 10, 11, 12]",
-    ];
+
+  // 보너스 숫자 입력 기능 테스트
+  test("기능 테스트: 보너스 숫자 입력", () => {
     mockRandoms([
-      [1, 2, 3, 5, 42, 43],
-      [2, 4, 8, 16, 38, 44],
-      [5, 7, 8, 10, 11, 12],
+      [1, 2, 3, 4, 5, 6],
     ]);
-    const logSpy = getLogSpy();
-
-    const cash = '3000';
-    SYSTEM.publishLotto(cash);
-
-    logs.forEach((log) => {
-
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
+    expect(() => {
+      const bonusNumber = 1;
+      const winningLotto = SYSTEM.makeLotto();
+      SYSTEM.isCorrectBonusNumber(bonusNumber, winningLotto.getNumber());
+    }).not.toThrow(ERROR.INVAID_NUMBER);
   });
+
+  test("예외 테스트: 보너스 숫자가 1~45 사이의 숫자가 아닌 경우 1", () => {
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+    ]);
+    expect(() => {
+      const bonusNumber = Number('0');
+      const winningLotto = SYSTEM.makeLotto();
+      SYSTEM.isCorrectBonusNumber(bonusNumber, winningLotto.getNumber());
+    }).toThrow(ERROR.INVAID_NUMBER);
+  });
+
+  test("예외 테스트: 보너스 숫자가 1~45 사이의 숫자가 아닌 경우 2", () => {
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+    ]);
+    expect(() => {
+      const bonusNumber = Number('46');
+      const winningLotto = SYSTEM.makeLotto();
+      SYSTEM.isCorrectBonusNumber(bonusNumber, winningLotto.getNumber());
+    }).toThrow(ERROR.INVAID_NUMBER);
+  });
+
+  test("예외 테스트: 보너스 숫자가 당첨 숫자와 중복될 경우", () => {
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+    ]);
+
+    expect(() => {
+      const bonusNumber = Number('1');
+      const winningLotto = SYSTEM.makeLotto();
+      SYSTEM.isCorrectBonusNumber(bonusNumber, winningLotto.getNumber());
+    }).toThrow(ERROR.HAS_NUMBER);
+  });
+
+  test("예외 테스트: 보너스 숫자 입력값이 숫자가 아닌 경우", () => {
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+    ]);
+
+    expect(() => {
+      const bonusNumber = Number('zero');
+      const winningLotto = SYSTEM.makeLotto();
+      SYSTEM.isCorrectBonusNumber(bonusNumber, winningLotto.getNumber());
+    }).toThrow(ERROR.NOT_NUMBER);
+  });
+
 
 });
