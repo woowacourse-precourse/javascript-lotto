511c0f66b12992af4b4e2373caaec5d36614ed60
ilgon0110 <moses011010@gmail.com>
2022-11-14 20:50:11 +0900

    test: 리팩토링에 따른 테스트 코드 수정

--- __tests__/DomainTest.js
+++ __tests__/DomainTest.js
@@ -1,10 +1,12 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-const User = require('../src/User');
-const Lotto = require('../src/Lotto');
+const Model = require('../src/Model/Model');
+const Lotto = require('../src/Model/Lotto');
 const UserInput = require('../src/UserInput');
+const Controller = require('../src/Controller/Controller');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
+  // eslint-disable-next-line arrow-body-style
   answers.reduce((acc, input) => {
     return acc.mockImplementationOnce((question, callback) => {
       callback(input);
@@ -14,6 +16,7 @@ const mockQuestions = (answers) => {
 
 const mockRandoms = (numbers) => {
   MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  // eslint-disable-next-line arrow-body-style
   numbers.reduce((acc, number) => {
     return acc.mockReturnValueOnce(number);
   }, MissionUtils.Random.pickUniqueNumbersInRange);
@@ -27,40 +30,43 @@ const getLogSpy = () => {
 
 describe('기능 1번. 로또 구입 금액 입력 받기', () => {
   test('숫자 이외의 타입을 입력한 경우', () => {
-    const user = new User();
+    const model = new Model();
+    const controller = new Controller(model);
     mockQuestions(['1000d ']);
-    expect(() => user.getUserMoney()).toThrow('[ERROR]');
+    expect(() => controller.getUserMoneyAndLottos()).toThrow('[ERROR]');
   });
   test('1000원 미만의 값을 입력한 경우', () => {
-    const user = new User();
+    const controller = new Controller();
     mockQuestions(['900']);
-    expect(() => user.getUserMoney()).toThrow('[ERROR]');
+    expect(() => controller.getUserMoneyAndLottos()).toThrow('[ERROR]');
   });
   test('NUMBER.MAX_SAFE_INTEGER 이상 값을 입력한 경우', () => {
-    const user = new User();
+    const controller = new Controller();
     mockQuestions([`${Number.MAX_SAFE_INTEGER}`]);
-    expect(() => user.getUserMoney()).toThrow('[ERROR]');
+    expect(() => controller.getUserMoneyAndLottos()).toThrow('[ERROR]');
   });
   test('1000원으로 나누어 떨어지지 않는 경우', () => {
-    const user = new User();
+    const controller = new Controller();
     mockQuestions(['1350']);
-    expect(() => user.getUserMoney()).toThrow('[ERROR]');
+    expect(() => controller.getUserMoneyAndLottos()).toThrow('[ERROR]');
   });
   test('값을 제대로 입력한 경우', () => {
     const logSpy = getLogSpy();
-    const user = new User();
+    const model = new Model();
+    const controller = new Controller(model);
     mockQuestions(['8 00 0']);
-    user.getUserMoney();
-    expect(logSpy).toHaveBeenCalledWith('유효한 입력입니다.');
+    controller.getUserMoneyAndLottos();
+    expect(logSpy).toBeCalledTimes(10);
   });
 });
 
 describe('기능 2번. 로또 구입 금액 만큼의 로또 번호 생성', () => {
   test('입력받은 금액 만큼의 로또 발행', () => {
-    const user = new User();
+    const model = new Model();
+    const controller = new Controller(model);
     mockQuestions(['8000']);
-    user.getUserMoney();
-    expect(user.lottoList.length).toEqual(8);
+    controller.getUserMoneyAndLottos();
+    expect(model.lottoLists.length).toEqual(8);
   });
   test('로또 번호가 숫자 이외의 type인 경우', () => {
     mockRandoms([[8, 21, 45, 41, 42, 'plz']]);
