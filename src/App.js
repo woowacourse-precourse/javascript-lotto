b8cb6aa2ca62aacd047f3de5fbda2a800d5d9df0
Miniling <bsm3925@naver.com>
2022-11-15 04:35:32 +0900

    Add: func_result

--- src/App.js
+++ src/App.js
@@ -15,6 +15,13 @@ class App {
       third: 0,
       fourth: 0,
       fifth: 0,
+    }];
+    this.reward = [{
+      first: 2000000000,
+      second: 30000000,
+      third: 1500000,
+      fourth: 50000,
+      fifth: 5000,
     }]
   }
 
@@ -93,11 +100,23 @@ class App {
     }
   }
 
+  result() {
+    MissionUtils.Console.print("당첨 통계\n");
+    MissionUtils.Console.print("---\n");
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${this.rank['fifth']}개\n`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${this.rank['fourth']}개\n`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${this.rank['third']}개\n`);
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.rank['second']}개\n`);
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${this.rank['first']}개\n`);
+    MissionUtils.Console.print(`총 수익률은 ${}%입니다.\n`);
+  }
+
   play() {
     this.purchaseLotto();
     this.makeLotto(this.countLotto);
     this.winningNum();
     this.checkWinner();
+    this.result();
   }
 }
 
