6928f200f105d308ed2b5756c11fa1e9616fc789
cho seonghoon <chsida655@gmail.com>
2022-11-15 01:42:27 +0900

    feat: 로또 당첨 금액 배열 선언

--- src/ResultMessage.js
+++ src/ResultMessage.js
@@ -5,5 +5,6 @@ const RESULT_MESSAGE = [
   "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
   "6개 일치 (2,000,000,000원) - ",
 ];
+const RESULT_WINNINGS = [5000, 50000, 1500000, 30000000, 2000000000];
 
-module.exports = { RESULT_MESSAGE };
+module.exports = { RESULT_MESSAGE, RESULT_WINNINGS };
