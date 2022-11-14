1660da957d4d7f792681485fc3da990f2cee8e7c
areumsheep <areumsheep@gmail.com>
2022-11-14 18:33:15 +0900

    add: 구매한 뒤 랜덤 번호 생성된 배열 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,31 @@ const { MESSAGES, ERROR_MESSAGES } = require('./constants/index');
 
 const LOTTO_PRICE = 1000;
 class App {
+  #buyLotto = [];
+
   play() {
     Console.readLine(MESSAGES.INPUT_MONEY, (number) => {
       if (number % LOTTO_PRICE !== 0)
         throw new Error(ERROR_MESSAGES.INVALID_MONEY_UNIT);
+      this.buy(Number(number / LOTTO_PRICE));
     });
+  }
+
+  buy(count) {
+    Console.print(MESSAGES.BUY_COUNT(count));
+
+    for (let index = 1; index <= count; index++) {
+      const randomLotto = this.randomLotto();
+
+      this.#buyLotto.push(randomLotto);
+      Console.print(randomLotto);
+    }
+  }
+
+  randomLotto() {
+    const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    if (!randomNumbers) throw new Error(ERROR_MESSAGES.EMPTY_NUMBERS);
+    return randomNumbers.sort((a, b) => a - b);
   }
 }
 
