deb1fefdd2341ca54a5a6599f38b6944c03b4123
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-14 20:27:21 +0900

    feat: [로또 구입 금액 입력 받기] - 숫자가 아닌 경우 예외 처리

--- src/Money.js
+++ src/Money.js
@@ -2,7 +2,18 @@ class Money {
   #money;
 
   constructor(money) {
+    this.validate(money);
     this.#money = money;
+  }
+
+  validate(money) {
+    this.checkNumber(money);
+  }
+
+  checkNumber(money) {
+    if (isNaN(money)) {
+      throw new Error("[ERROR] 구입 금액은 숫자로 입력해야 합니다.");
+    }
   }
 }
 
