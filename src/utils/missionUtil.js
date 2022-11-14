8bedf3a45a59f350fd58e8e5ae53cdc4f23504ce
ReturnReason <reasonz@daum.net>
2022-11-14 14:46:52 +0900

    style: 코드 사이 공백 추가

--- src/utils/missionUtil.js
+++ src/utils/missionUtil.js
@@ -3,6 +3,7 @@ const { Random, Console } = require('@woowacourse/mission-utils');
 const print = (message) => {
   return Console.print(message);
 };
+
 const readLine = (query, callback) => {
   return Console.readLine(query, callback);
 };
