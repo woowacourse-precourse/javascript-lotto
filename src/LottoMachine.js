2993db9fa590f0ce3759d7c2753869d5befc7133
yeongseokim <1903020@ewhain.net>
2022-11-14 14:06:56 +0900

    refact(LottoMachine): 오름차순 정렬 로직 sortNumbersAscending 메서드로 분리

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -3,12 +3,17 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class LottoMachine {
     static createLotto() {
         const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-        lottoNumbers.sort(function (a, b) {
+        this.sortNumbersAscending(lottoNumbers);
+        return lottoNumbers;
+    }
+
+    static sortNumbersAscending(numberArray) {
+        numberArray.sort(function (a, b) {
             if (a > b) return 1;
             if (a === b) return 0;
             if (a < b) return -1;
         });
-        return lottoNumbers;
+        return numberArray;
     }
 }
 
