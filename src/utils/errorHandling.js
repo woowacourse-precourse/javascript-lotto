2d30808a0247c01c6fbd8a28fa61a603747f5423
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-11-15 00:43:26 +0900

    Refactor: switch문 Airbnb 코드 컨벤션으로 수정

--- src/utils/errorHandling.js
+++ src/utils/errorHandling.js
@@ -8,27 +8,35 @@ const {
 class UserInputValidationError {
   static priceErrorMessage(type) {
     switch (type) {
-      case PRICE_TYPE.NOT_NUMBER:
+      case PRICE_TYPE.NOT_NUMBER: {
         throw new Error(PRICE_MESSAGE.NOT_INPUT_NUMBER);
-      case PRICE_TYPE.NOT_THOUSAND:
+      }
+      case PRICE_TYPE.NOT_THOUSAND: {
         throw new Error(PRICE_MESSAGE.ONLY_INPUT_THOUSAND);
-      case PRICE_TYPE.NOT_BUY_LOTTO:
+      }
+      case PRICE_TYPE.NOT_BUY_LOTTO: {
         throw new Error(PRICE_MESSAGE.BELOW_THOUSAND);
+      }
     }
   }
 
   static lottoErrorMessage(type) {
     switch (type) {
-      case LOTTO_TYPE.NOT_SIX_NUMBERS:
+      case LOTTO_TYPE.NOT_SIX_NUMBERS: {
         throw new Error(LOTTO_MESSAGE.NOT_SIX_NUMBERS);
-      case LOTTO_TYPE.NOT_RANGE:
+      }
+      case LOTTO_TYPE.NOT_RANGE: {
         throw new Error(LOTTO_MESSAGE.CHECK_RANGE);
-      case LOTTO_TYPE.IS_DUPLICATION:
+      }
+      case LOTTO_TYPE.IS_DUPLICATION: {
         throw new Error(LOTTO_MESSAGE.CHECK_DUPLICATION);
-      case LOTTO_TYPE.IS_VALIDATION:
+      }
+      case LOTTO_TYPE.IS_VALIDATION: {
         throw new Error(LOTTO_MESSAGE.CHECK_VALIDATION_INPUT);
-      case LOTTO_TYPE.IS_BONUS_IN_WIN:
+      }
+      case LOTTO_TYPE.IS_BONUS_IN_WIN: {
         throw new Error(LOTTO_MESSAGE.CHECK_BONUS_IN_WIN);
+      }
     }
   }
 }
