3bf65a78ac0921388b0d8371dd86812e16e1749e
Rki0 <pky00823@gmail.com>
2022-11-14 11:23:02 +0900

    refactor(Result): modify use of constants and calculation structure
    Since constants are modularized into other files, modify how they are used appropriately.
    
    By changing the structure that uses Console.print, modify the way the prize is calculated in a more suitable form.
    
    BREAKING CHANGE: Modify calculation structure.
    
    Before:
    
    rank array - [1st, 2nd, 3rd, 4th, 5th]
    
    After:
    
    rank array - [5th, 4th, 3rd, 2nd, 1st]

--- src/Result.js
+++ src/Result.js
@@ -1,18 +1,5 @@
 const Console = require("@woowacourse/mission-utils").Console;
-
-const NOTIFY_WINNING_STATE = "\n당첨 통계\n---";
-const FIRST_PRIZE = 2000000000;
-const SECOND_PRIZE = 30000000;
-const THIRD_PRIZE = 1500000;
-const FOURTH_PRIZE = 50000;
-const FIFTH_PRIZE = 5000;
-const EACH_PRIZE = [
-  FIRST_PRIZE,
-  SECOND_PRIZE,
-  THIRD_PRIZE,
-  FOURTH_PRIZE,
-  FIFTH_PRIZE,
-];
+const ResultConst = require("./constant/ResultConst");
 
 class Result {
   constructor(bundleOfLotto, userLotto, bonus) {
@@ -73,13 +60,13 @@ class Result {
 
   getFifthRank() {
     if (this.score === 3) {
-      this.rank[4] += 1;
+      this.rank[0] += 1;
     }
   }
 
   getFourthRank() {
     if (this.score === 4) {
-      this.rank[3] += 1;
+      this.rank[1] += 1;
     }
   }
 
@@ -91,32 +78,31 @@ class Result {
 
   getSecondRank() {
     if (this.score === 5 && this.matchBonus) {
-      this.rank[1] += 1;
+      this.rank[3] += 1;
     }
   }
 
   getFirstRank() {
     if (this.score === 6) {
-      this.rank[0] += 1;
+      this.rank[4] += 1;
     }
   }
 
   showResult() {
-    Console.print(NOTIFY_WINNING_STATE);
-    Console.print(`3개 일치 (5,000원) - ${this.rank[4]}개`);
-    Console.print(`4개 일치 (50,000원) - ${this.rank[3]}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${this.rank[2]}개`);
-    Console.print(
-      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.rank[1]}개`
-    );
-    Console.print(`6개 일치 (2,000,000,000원) - ${this.rank[0]}개`);
+    Console.print(ResultConst.NOTIFY_WINNING_STATE);
+
+    this.rank.forEach((item, index) => {
+      Console.print(
+        `${ResultConst.CONDITION_OF_EACH_RANK[index]}` + `${item}개`
+      );
+    });
   }
 
   getPrize() {
     let finalPrize = 0;
 
     this.rank.forEach((item, index) => {
-      finalPrize += item * EACH_PRIZE[index];
+      finalPrize += item * ResultConst.EACH_PRIZE[index];
     });
 
     return finalPrize;
