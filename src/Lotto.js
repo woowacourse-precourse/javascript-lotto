6838a5cf0e01cf1f510f56e4d7b58bf7f694b247
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-15 02:56:33 +0900

    feat: 당첨 번호 예외 처리 모듈 작성 (Lotto.js)

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,30 +2,35 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.checkLength(numbers);
+    this.checkRedundancy(numbers);
+    this.checkNumberRange(numbers);
+
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
+  checkLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
   // TODO: 추가 기능 구현
-  winTest(){
-    const setNumbers = new Set(this.#numbers);
-    if (this.#numbers.length !== [...setNumbers].length){
+  checkRedundancy(numbers){//중복성
+    const setNumbers = new Set(numbers);
+    if (numbers.length !== [...setNumbers].length){
       throw new Error("[ERROR] 로또 번호는 중복되면 안됩니다.")
     }
-    return true;
   }
-  bonusTest(bonus_number){
-    if (this.#numbers.includes(bonus_number)){
-      throw new Error("[ERROR] 보너스 번호는 로또 번호에 포함되면 안됩니다.")
-    }
-    return true;
+
+  checkNumberRange(numbers){
+    numbers.forEach((number)=>{
+      if(number<1 || number>45){
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+      }
+    })
   }
+
 }
 
 module.exports = Lotto;
