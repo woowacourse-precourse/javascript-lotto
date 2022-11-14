b3a8d99f2211662ce2f493bc44b20a89fd2e68d3
BB-choi <choibobae0809@gmail.com>
2022-11-15 08:05:40 +0900

    test(Lotto): 로또 클래스 예외 처리 테스트
    로또 번호 입력, 보너스 번호 입력 예외 처리 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,33 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow('[ERROR]');
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('배열에 숫자가 아닌 입력이 들어오면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, NaN, 3, 4, 5, 6]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호가 1~45 범위를 넘어가면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([0, 1, 2, 3, 4, 5]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('보너스 번호가 숫자가 아닌 경우 예외가 발생한다', () => {
+    expect(() => {
+      Lotto.isValidBonusNumber(NaN, [1, 2, 3, 4, 5, 6]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호가 보너스 번호와 같은 경우 예외가 발생한다', () => {
+    expect(() => {
+      Lotto.isValidBonusNumber(1, [1, 2, 3, 4, 5, 6]);
+    }).toThrow('[ERROR]');
+  });
 });
