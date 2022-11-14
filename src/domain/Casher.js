49e87d1d7947972ba861ef8e1a773f5a01c006be
nerdchanii <nerdchanii@gmail.com>
2022-11-14 14:13:47 +0900

    feat: 당첨된 등수를 저장하고 수익률을 구하는 기능 추가

--- src/domain/Casher.js
+++ src/domain/Casher.js
@@ -1,8 +1,10 @@
-const { LOTTO, CASHER } = require('../constants');
+const { LOTTO, CASHER, LOTTO_RESULT } = require('../constants');
 const Io = require('../infrastructure/io/io');
 const InputValidator = require('../utils/InputValidator');
 
 class Casher {
+  static ranks = new Map();
+  static #profit = null;
   static #purchaseMoney = 0;
 
   static getMoney(message, afterGetMoney) {
@@ -20,6 +22,19 @@ class Casher {
 
   static noticePurchasedQuantity(quantity) {
     return Io.output(`${quantity}${CASHER.NOTICE_PURCHASE_QUANTITY}`);
+  }
+
+  static setResults(ranks) {
+    ranks.map((rank) => Casher.ranks.set(rank, (Casher.ranks.get(rank) || 0) + 1));
+    this.#setProfit(ranks);
+  }
+
+  static #setProfit(ranks) {
+    const wholePrize = ranks.reduce((acc, rank) => acc + LOTTO_RESULT.RANK_PRIZE[rank], 0);
+    const profit = (wholePrize / this.#purchaseMoney) * 100;
+    // 최대 소수점 2자리까지 표현
+    // 마지막 소숮점이 0이면 소수점을 제거
+    this.#profit = profit.toFixed(2).replace(/\.?0+$/, '');
   }
 }
 
