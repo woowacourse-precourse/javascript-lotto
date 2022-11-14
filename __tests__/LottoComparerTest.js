acc20e2cf3d99fe60e3c92faf7feee31b00a96fa
Leejin-Yang <ejin1996@gmail.com>
2022-11-14 14:24:38 +0900

    test: 로또 비교자 클래스 테스트 수정

--- __tests__/LottoComparerTest.js
+++ __tests__/LottoComparerTest.js
@@ -1,5 +1,16 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+
+const Lotto = require('../src/Lotto');
+const LottoBuyer = require('../src/LottoBuyer');
 const LottoComparer = require('../src/LottoComparer');
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickUniqueNumbersInRange,
+  );
+};
 
 describe('로또 비교 클래스 테스트', () => {
   afterEach(() => {
@@ -7,19 +18,23 @@ describe('로또 비교 클래스 테스트', () => {
   });
 
   test('로또와 구매자 로또를 비교해 로또 등수를 구한다.', () => {
-    const buyerLottos = [
+    const randoms = [
       [8, 22, 23, 42, 43, 45],
       [1, 2, 3, 4, 5, 6],
       [8, 22, 23, 42, 43, 44],
       [8, 22, 23, 40, 41, 42],
       [8, 21, 23, 40, 41, 42],
     ];
-    const lotto = [8, 22, 23, 42, 43, 45];
     const bonusNumber = 44;
-    const rank = { first: 1, second: 1, third: 0, fourth: 1, fifth: 1 };
+    const ranking = { first: 1, second: 1, third: 0, fourth: 1, fifth: 1 };
 
-    const result = LottoComparer.getBuyerLottoRank(buyerLottos, lotto, bonusNumber);
+    mockRandoms(randoms);
 
-    expect(result).toStrictEqual(rank);
+    const comparer = new LottoComparer(new LottoBuyer(5000), new Lotto([8, 22, 23, 42, 43, 45]));
+    comparer.buyer.buyLotto();
+    comparer.lotto.setBonusNumber(bonusNumber);
+    comparer.setBuyerLottoRanking();
+
+    expect(comparer.ranking).toStrictEqual(ranking);
   });
 });
