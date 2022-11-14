17176b4ca7e15ac7fdc3b3eb7540d13e6d114825
xownsdl0 <xownsdl0@naver.com>
2022-11-15 01:27:02 +0900

    feat(lotto): 입력 받은 데이터 저장을 위한 객체 생성

---
+++ src/model/InputDataObject.js
@@ -0,0 +1,18 @@
+const inputDataObject = {
+  principal: 0,
+  inputMoney: 0,
+  raffleNumbers: [],
+  bonusNumber: 0,
+  publishLotto: [],
+  correctNumber: [],
+  averageReturn: {
+    3: [0, 5000],
+    4: [0, 50000],
+    '5개': [0, 1500000],
+    '5B': [0, 30000000],
+    6: [0, 2000000000],
+  },
+  profit: 0,
+};
+
+module.exports = inputDataObject
