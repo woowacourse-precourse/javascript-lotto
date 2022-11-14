3c0436dfcea4291ca7a455b1c6c89cb925683b36
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-15 05:17:12 +0900

    refactor: 함수 병합 및 이름 변경, 클래스로 통합으로 파일 삭제

--- src/getLottoNum.js
+++
@@ -1,7 +0,0 @@
-const { Random } = require('@woowacourse/mission-utils');
-
-function getLottoNum() {
-  return Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
-}
-
-module.exports = getLottoNum;
