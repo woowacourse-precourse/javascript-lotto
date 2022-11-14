9963c5437263ecc5aa8a414aaa1b2d40f2c1de48
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-14 14:15:40 +0900

    feat(Application): 구매 건수 계산 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const Application = require('./Application');
+
 class App {
-  play() {}
+  static purchaseCount(userInputNumber) {
+    const UNIT_AMOUNT = 1000;
+
+    return Application.purchaseCount(userInputNumber, UNIT_AMOUNT);
+  }
 }
 
 module.exports = App;
