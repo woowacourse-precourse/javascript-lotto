5643bd5b375af321cdaddc1804f936e8f3e37f81
sunny <huiseon37@gmail.com>
2022-11-15 05:43:30 +0900

    feat: 로또 랭크 반환 함수 구현

--- src/LottoResult.js
+++ src/LottoResult.js
@@ -2,26 +2,43 @@ class LottoResult {
   #number;
   #bonus;
   #lottoMachine;
+  #rank;
 
   constructor(winningLotto, bonusNumber, LottoMachine) {
     this.#lottoMachine = LottoMachine;
     this.#number = winningLotto;
     this.#bonus = bonusNumber;
+    this.#rank = Array(5).fill(0);
+  }
+
+  checkRank() {
+    this.#lottoMachine.getLottoList().forEach((Lotto)=>{
+      const matchCnt = this.checkWinning(Lotto.getNumbers());
+      if(matchCnt === 3 || matchCnt === 4){
+        this.#rank[matchCnt-3] += 1;
+      }
+      if(matchCnt === 5){
+        this.checkBonus(Lotto.getNumbers()) ? this.#rank[matchCnt-2] += 1 : this.#rank[matchCnt-3] += 1;
+      }
+      if(matchCnt === 6){
+        this.#rank[matchCnt-2] += 1; 
+      }
+    })
   }
 
   checkWinning(lotto) {
-    let equalCnt = 0;
+    let matchCnt = 0;
     this.#number.forEach((number)=>{
       if(lotto.includes(number)){
-        equalCnt = equalCnt + 1;
+        matchCnt = matchCnt + 1;
       }
     });
-    return equalCnt;
+    return matchCnt;
   }
 
   checkBonus(lotto) {
     if(lotto.includes(this.#bonus)){
-        return true
+      return true
     }
     return false;
   }
