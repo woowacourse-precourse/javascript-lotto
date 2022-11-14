16e2e88247f3d1ba25f8ea3a8d66105101b99ee7
동동이 <drdragon777@naver.com>
2022-11-14 22:09:02 +0900

    feat - add Console클래스 및 콘솔에서 출력할 메시지 추가

---
+++ src/Console.js
@@ -0,0 +1,11 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+class Console {
+  static REQUEST_LOTTO_PRICE = "구입금액을 입력해 주세요.\n";
+  static PURCHASED_LOTTO_COUNT_MSG = "개를 구매했습니다.\n";
+  static REQUEST_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.\n";
+  static REQUEST_BONUT_NUMBER = "보너스 번호를 입력해 주세요.";
+  static REQUEST_BONUT_NUMBER = "당첨 통계\n---";
+}
+
+module.exports = Console;
