6c1292ce1690ecfa85f2638b521d6bf8e29c34f0
gyeongza <gyeongza@naver.com>
2022-11-14 20:35:37 +0900

    feat : 로또 구매 개수 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -12,9 +12,16 @@ class App {
 
   inputMoney() {
     Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
-      this.money = new InputMoney(money);
+      this.money = new InputMoney(money).validate();
+
+      this.printLotto(money);
     });
   }
+  
+  printLotto(money) {
+    Console.print(`${money / 1000}개를 구매했습니다.`);
+  }
+
 }
 
 module.exports = App;
