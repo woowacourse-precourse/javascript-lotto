e0587a20fceb42ea1cc9e9aab360bd438dfd1b93
동동이 <drdragon777@naver.com>
2022-11-15 00:18:44 +0900

    feat - add validator constants

---
+++ src/validator/validator.constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+  BASIC: "[ERROR] ",
+  INVALID_INPUT: {
+    RANGE_ERROR: "로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+    LOTTO_DUPLICATED: "로또 번호는 중복되지 않는 6개의 숫자여야 합니다.",
+    LOTTO_NOT_NUMBER: "로또 번호는 숫자만 입력할 수 있습니다.",
+    PRICE_UNIT: "구입 금액은 1,000원 단위로 입력해주세요.",
+    PRICE_NOT_NUMBER: "구입 금액은 숫자(1,000원 단위)로 입력해주세요.",
+  },
+};
