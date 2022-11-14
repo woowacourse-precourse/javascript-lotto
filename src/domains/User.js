d0cd6af81c3a0c3a2c844f4d591078a6a8af89b7
solo5star <solo_5@naver.com>
2022-11-15 08:07:08 +0900

    feat(lotto-buy): 최소 로또 1개를 구매할 수 있는 금액을 넣게 하도록 구현

--- src/domains/User.js
+++ src/domains/User.js
@@ -8,6 +8,9 @@ class User {
    * @param {number} money
    */
   buyLottos(money) {
+    if (money < Lotto.PRICE) {
+      throw new Error('[ERROR] 최소 로또를 1개 이상 구매할 수 있는 금액을 입력해야 합니다.');
+    }
     if (money % Lotto.PRICE !== 0) {
       throw new Error('[ERROR] 로또를 구입한 후 남는 금액이 없어야 합니다.');
     }
