6378d2f402f2b4a4b40757a7e90e94cadb3c6f22
윤동주 <yoondj98@naver.com>
2022-11-15 00:55:29 +0900

    feat: 로또 구입 금액 입력받는 기능 및 예외 처리 구현 - buyLotto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.usedMoney = 0;
+    this.lottoAmount = 0;
+  }
+
+  play() {
+    this.buyLotto();
+  }
+
+  buyLotto() {
+    MissionUtils.Console.readLine(
+      "로또를 구매할 금액을 입력해주세요.ㅜ",
+      (input) => {
+        if (input % 1000 != 0)
+          throw new Error(
+            "[ERROR] 구입 금액은 1000원 단위로만 구매 가능합니다.\n"
+          );
+        this.usedMoney = input;
+        this.lottoAmount = input / 1000;
+      }
+    );
+  }
 }
 
 module.exports = App;
