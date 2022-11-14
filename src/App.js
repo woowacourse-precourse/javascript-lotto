4b94ca0ab2c5f6f487d7c246fcf117f0f055f28f
Hoon <si932174@gmail.com>
2022-11-14 10:54:20 +0900

    refactor : 메소드명 변경 priceInput->lottoPurchaser

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
 
 class App {
-  static priceInput() {
+  static lottoPurchaser() {
     Console.readLine('구입금액을 입력해 주세요.\n', (answer) => {
       Console.print(answer);
     });
