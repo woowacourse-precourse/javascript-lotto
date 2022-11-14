7faeb5883ebc0ff2eb7a0ab1915614081825f922
kimeodml <kimeodml@gmail.com>
2022-11-15 07:41:00 +0900

    feat: 당첨 통계와 관련된 순위와 고정 문구 Constants에 추가

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -24,4 +24,21 @@ const PAY_ERROR = {
   UNDER: '[ERROR] 1000원 이하로 입력하셨습니다.',
 };
 
-module.exports = { MESSAGES, LOTTO_ERROR, BONUS_ERROR, PAY_ERROR };
+const RANK = {
+  three: 0,
+  four: 0,
+  five: 0,
+  bfive: 0,
+  six: 0
+}
+
+const WINNING_STATISTICS = {
+  THREE: (num) => `3개 일치 (5,000원) - ${num}개`,
+  FOUR: (num) => `4개 일치 (50,000원) - ${num}개`,
+  FIVE: (num) => `5개 일치 (1,500,000원) - ${num}개`,
+  FIVE_BONUS: (num) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${num}개`,
+  SIX: (num) => `6개 일치 (2,000,000,000원) - ${num}개`,
+  YIELD: (num) => `총 수익률은 ${num}%입니다.`
+}
+
+module.exports = { MESSAGES, LOTTO_ERROR, BONUS_ERROR, PAY_ERROR, RANK, WINNING_STATISTICS };
