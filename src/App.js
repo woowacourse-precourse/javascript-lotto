913859c4906cbcf876ae68a866a763fb243870ca
pitang <wpeffect@gmail.com>
2022-11-14 14:08:36 +0900

    feat: 로또 구입금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 class App {
   play() {}
+  userBuyLotto() {
+    MissionUtils.Console.readLine(
+      "구입금액을 입력해 주세요. \n",
+      (buyPrice) => {
+        if (buyPrice % 1000 !== 0) {
+          throw new Error("알맞는 금액을 입력해 주세요.");
+        }
+        return this.userLottoNumber(buyPrice / 1000);
+      }
+    );
+  }
 }
 
 module.exports = App;
