2bec1a7e0ec08218b88348e1c7c6a24dd8c66d63
JSH99 <tjgus9966@naver.com>
2022-11-14 22:32:09 +0900

    feat(Winning): 일치하는 번호 개수 세는 기능 구현

--- src/Winnig.js
+++ src/Winnig.js
@@ -8,8 +8,24 @@ class Winning {
     printWinningAnnouncement() {
         MissionUtils.Console.print('당첨 통계\n---');
     }
+        
+    // 3개 일치 (5,000원) - 1개
+    // 4개 일치 (50,000원) - 0개
+    // 5개 일치 (1,500,000원) - 0개
+    // 5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
+    // 6개 일치 (2,000,000,000원) - 0개
+        
+    calculateWinningCount(randomNumbers, winningNumbers, bonusNumber) {
+        let count = 0;
 
-    calculateWinningCount() {
+        for(let i = 0; i < winningNumbers.length; i++) {
+            if (randomNumbers.includes(winningNumbers[i]))
+                count++;
+        }
+    }
+
+
+    checkBonus() {
 
     }
 
