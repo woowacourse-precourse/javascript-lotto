cdd32ac340e270adddcce103fbe850ecc2e34700
y00eunji <aine0227@naver.com>
2022-11-14 16:41:11 +0900

    feat(LottoNumber): Generate lotto numbers

---
+++ src/LottoNumber.js
@@ -0,0 +1,20 @@
+const { Random } = require("@woowacourse/mission-utils");
+const { LOTTO_NUMBER } = require("./utils/Constant");
+
+class LottoNumber {
+  static generate() {
+    const randomNumbers = Random.pickUniqueNumbersInRange(
+      LOTTO_NUMBER.START,
+      LOTTO_NUMBER.END,
+      LOTTO_NUMBER.COUNT
+    );
+
+    return LottoNumber.sort(randomNumbers);
+  }
+
+  static sort(lottoNumbers) {
+    return lottoNumbers.sort((lottoNum1, lottoNum2) => lottoNum1 - lottoNum2);
+  }
+}
+
+module.exports = LottoNumber;
