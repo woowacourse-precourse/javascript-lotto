d3ca6959941fd4b8e0bf9b025e2065a66b009349
foundaway98 <foundaway98@naver.com>
2022-11-14 19:49:22 +0900

    <feat> : 오류 제어 기능 추가(당첨 금액이 천원으로 나누어 떨어지지 않는 경우)
    당첨 금액이 천원으로 나누어 떨어지지 않는 경우 에러 문구 출력 기능 추가

--- src/LottoBuyer.js
+++ src/LottoBuyer.js
@@ -6,7 +6,11 @@ class LottoBuyer {
   lottoArray = new Array();
   constructor(money) {
     money = parseInt(money);
-
+    if (money % 1000 !== 0) {
+      throw new Error(
+        "[ERROR] 로또 구매 금액 단위는 1,000원 단위로 이루어져야 합니다."
+      );
+    }
     this.money = money;
     this.lottoAmount = money / 1000;
   }
