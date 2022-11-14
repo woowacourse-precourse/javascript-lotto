ca02b8715355ed7dc1bf59f51bd448d56dfca8ae
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-15 05:34:20 +0900

    Feat: 당첨 번호  유효성 검사 기능

--- src/Prize.js
+++ src/Prize.js
@@ -1,3 +1,5 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class Prize {
 	constructor() {
 		this.prizeMoney = 0;
@@ -5,14 +7,17 @@ class Prize {
 		this.winningBonusNum = 0;
 	}
 
-	validate() {
-		let numbers = [...this.winningNums];
-		numbers.init(this.bonusNum);
+	validate(nums, bonusNum) {
+		let numsForValidCheck = nums.split(',');
+		numsForValidCheck.push(bonusNum);
 
-		this.isNum(numbers);
-		this.isSevenNumbers(numbers);
-		this.isNumsInRange(numbers);
-		this.isDuplicated(numbers);
+		this.isNum(numsForValidCheck);
+		this.isSevenNumbers(numsForValidCheck);
+		this.isNumsInRange(numsForValidCheck);
+		this.isDuplicated(numsForValidCheck);
+
+		this.winningNums = nums.split(',');
+		this.winningBonusNum = bonusNum;
 	}
 
 	isNum(numbers) {
@@ -20,21 +25,21 @@ class Prize {
 
     numbers.forEach((num) => {
       if (validNums.test(num)) {
-        throw new Error("[ERROR] 보너스 번호 포함 당첨 번호는 숫자여야 합니다.");
+        throw new Error('[ERROR] 보너스 번호 포함 당첨 번호는 숫자여야 합니다.');
       }
-    })
+    });
   }
 
   isSevenNumbers(numbers) {
     if (numbers.length !== 7) {
-      throw new Error("[ERROR] 보너스 번호 포함 당첨 번호는 7개여야 합니다.");
+      throw new Error('[ERROR] 보너스 번호 포함 당첨 번호는 7개여야 합니다.');
     }
   }
 
   isNumsInRange(numbers) {
     numbers.forEach((num) => {
       if (parseInt(num) < 1 || parseInt(num) > 45) {
-        throw new Error("[ERROR] 보너스 번호 포함 당첨 번호는 (1 ~ 45) 범위 내의 숫자여야 합니다.");
+        throw new Error('[ERROR] 보너스 번호 포함 당첨 번호는 (1 ~ 45) 범위 내의 숫자여야 합니다.');
       }
     })
   }
@@ -42,16 +47,14 @@ class Prize {
   isDuplicated(numbers) {
     const duplicateCheck = new Set(numbers);
     if (duplicateCheck.size !== 7) {
-      throw new Error("[ERROR] 보너스 번호 포함 당첨 번호는 중복되지 않아야 합니다.");
+      throw new Error('[ERROR] 보너스 번호 포함 당첨 번호는 중복되지 않아야 합니다.');
     }
   }
 
 	winningCheck(lottoBundle) {
 		Console.readLine('당첨 번호를 입력해 주세요.\n', (nums) => {
 			Console.readLine('보너스 번호를 입력해 주세요.\n', (bonusNum) => {
-				this.winningNums = nums.split(',');
-				this.winningBonusNum = bonusNum;
-				this.validate();
+				this.validate(nums, bonusNum);
 				this.countingPrize();
 				// print
 				// Console.close();
@@ -59,3 +62,5 @@ class Prize {
 		})
 	}
 }
+
+module.exports = Prize;
