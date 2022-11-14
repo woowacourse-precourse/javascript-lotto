acb864506355d33ee2c224481c65b1c55ed1edea
sumin <csumin008255@gmail.com>
2022-11-15 00:27:57 +0900

    Fix: [기능6-2] 오류 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,12 +9,13 @@ class Lotto {
   }
 
   checkWinNumber(winNumberArr) {
+    MissionUtils.Console.print(new Set(winNumberArr).size);
     if (new Set(winNumberArr).size != 6)
-      throw new Error("[ERROR]로또 번호 입력 오류");
+      throw new Error("[ERROR]로또 번호 입력 오류1");
     winNumberArr.forEach((number) => {
-      if (!isNaN(number)) throw new Error("[ERROR]로또 번호 입력 오류");
+      if (!Number(number)) throw new Error("[ERROR]로또 번호 입력 오류2");
       if (number > 45 || number < 1)
-        throw new Error("[ERROR]로또 번호 입력 오류");
+        throw new Error("[ERROR]로또 번호 입력 오류3");
     });
   }
 }
