c771375a5fa5c24cae65ccc24c031b0b70328cc2
RooT_Kim <bdd3452@gmail.com>
2022-11-14 20:15:12 +0900

    추가: 상수 Constants.js 파일 생성 : 출력메시지와 에러메시지를 포함한 Messages 상수 선언

---
+++ src/Constants.js
@@ -0,0 +1,27 @@
+const Messages = Object.freeze({
+	START_BUY: '구입금액을 입력해 주세요.',
+	INSERT_NUMBERS: '당첨 번호를 입력해 주세요.',
+	INSERT_BONUS: '보너스 번호를 입력해 주세요.',
+
+	ERROR: {
+		BUY_PRICE: {
+			LESS_THAN_THOUSAND: '[ERROR] 금액은 1000원 이상이어야 합니다.',
+			NOT_ZERO_REMAIN: '[ERROR] 금액은 1000원 단위여야 합니다.',
+			NOT_NUMBER: '[ERROR] 금액은 숫자만 입력해야 합니다.',
+		},
+		WINNING_NUMBERS: {
+			NOT_BELONG_NUMBER: '[ERROR] 딩첨 번호는 1부터 45사이여야 합니다.',
+			DUPLICATED_NUMBER: '[ERROR] 당첨 번호는 서로 다른 숫자여야 합니다.',
+			NOT_INSERT_COMMA: '[ERROR] 당첨 번호를 쉼표로 구분하여 입력하십시오.',
+			DIFFERENT_LENGTH: '[ERROR] 당첨 번호는 6개여야 합니다.',
+		},
+		BONUS_NUMBER: {
+			NOT_BELONG_NUMBER: '[ERROR] 보너스 번호는 1부터 45사이여야 합니다.',
+			NOT_ONE_NUMBER: '[ERROR] 보너스 번호는 1개여야 합니다.',
+		},
+	},
+});
+
+module.exports = {
+	Messages,
+};
