136e77e3f06650b99d5c601348c4467e8b09307b
useon <yoosk4986@naver.com>
2022-11-15 04:56:36 +0900

    feat(Issue): 구매 개수만큼 발행 목록을 생성한다

--- src/Issue.js
+++ src/Issue.js
@@ -1,4 +1,4 @@
-const { Console } = require('@woowacourse/mission-utils')
+const { Console, Random } = require('@woowacourse/mission-utils')
 const Validation = require('./Validation');
 
 class Issue {
@@ -7,7 +7,17 @@ class Issue {
       new Validation().purchaseValue(inputValue);
       this.purchaseCount = Math.floor(Number(inputValue) / 1000);
       Console.close();
+      this.list();
     });
+    return this.issueList;
+  }
+
+  list() {
+    const issueSet = new Set();
+    while (issueSet.size < this.purchaseCount) {
+      issueSet.add(Random.pickUniqueNumbersInRange(1, 45, 6));
+    }
+    this.issueList = issueSet;
   }
 }
 
