f614478f5ad92230cb1c09bd3c5f4572a8f869f0
2je0 <bulljy@naver.com>
2022-11-15 00:47:36 +0900

    ✨ feat: 구입 금액을 입력받아 로또 개수만큼 로또를 발행하고 출력하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const Money = require('./Money');
+const RandomLottos = require('./RandomLottos');
+const { MESSAGE } = require('./Constants');
+
+const { Console } = MissionUtils;
+
 class App {
-  play() {}
+  constructor() {
+    this.money = {};
+    this.randomLottos = {};
+    this.winningLotto = {};
+    this.matchedLotto = [];
+  }
+
+  play() {
+    this.enterPurchaseAmountMode();
+  }
+
+  enterPurchaseAmountMode() {
+    Console.readLine(MESSAGE.ASK_AMOUNT, (amount) => {
+      Console.print('');
+      this.money = new Money(+amount);
+      const numOfLotto = this.money.getNumOfLotto();
+      this.randomLottos = new RandomLottos(numOfLotto);
+      this.randomLottos.printRandomLottos();
+    });
+  }
 }
 
 module.exports = App;
