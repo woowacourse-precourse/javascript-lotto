e05fa1ae1ef6d3ad0f7846ff3f61df1024d81698
Sim <zh025700@naver.com>
2022-11-14 15:52:09 +0900

    refactor(src/LottoGame): 함수를 메소드로 이동 - 파일 내 함수들을 LottoGame의 메소드로 이전

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -39,7 +39,6 @@ class LottoGame {
     return [...Array(this.money / 1000).keys()].map(() => {
       const randLotto = new UserLotto().number;
       let str = "[";
-      //   MissionUtils.Console.print(randLotto.toString());
       [...randLotto].map((e) => {
         str = str + e + ", ";
       });
@@ -59,109 +58,95 @@ class LottoGame {
   inputBonusNum = (lotto) => {
     MissionUtils.Console.readLine(STATIC.MESSAGE.BONUS, (number) => {
       const bonusNumber = new BonusNum(number, lotto).bonusNum;
-      startCheckResult(
-        lotto,
-        this.userPickLotto,
-        bonusNumber,
-        this.prizeList,
-        this.userPrizeValue,
-        this.money
-      );
+      this.startCheckResult(lotto, bonusNumber);
     });
   };
+
+  startCheckResult = (lotto, bonusNumber) => {
+    this.userPickLotto.map((e) => {
+      const resultExceptBonus = this.checkResult(lotto, e);
+      this.prizeList.splice(
+        resultExceptBonus,
+        1,
+        this.prizeList[resultExceptBonus] + 1
+      );
+      this.checkBonusNum(e, bonusNumber, resultExceptBonus);
+    });
+    this.announceResult();
+    this.userPrizeValue = this.calculateResult();
+    this.announceYield();
+    MissionUtils.Console.close();
+  };
+
+  checkResult = (lotto, userPickLottoNum) => {
+    let correctCnt = 0;
+    lotto.map((number) => {
+      correctCnt += this.correctLottoNum(userPickLottoNum, number);
+    });
+    return correctCnt;
+  };
+
+  correctLottoNum = (userPickLotto, number) => {
+    if (userPickLotto.includes(parseInt(number))) {
+      return 1;
+    }
+    return 0;
+  };
+
+  checkBonusNum = (userPickLotto, bonusNumber, resultExceptBonus) => {
+    if (resultExceptBonus == 5) {
+      this.isInBonousNum(userPickLotto, bonusNumber);
+    }
+  };
+
+  isInBonousNum = (userPickLotto, bonusNumber) => {
+    if (userPickLotto.includes(parseInt(bonusNumber))) {
+      this.prizeList.splice(5, 1, this.prizeList[5] - 1);
+      this.prizeList.splice(6, 1, this.prizeList[6] + 1);
+    }
+  };
+
+  announceResult = () => {
+    MissionUtils.Console.print(STATIC.MESSAGE.STATISTIC + "\n" + "---");
+    MissionUtils.Console.print(
+      `3${STATIC.MESSAGE.CORRECT} (${STATIC.LOTTERY_AMOUNT.THREE}) - ${this.prizeList[3]}개`
+    );
+    MissionUtils.Console.print(
+      `4${STATIC.MESSAGE.CORRECT} (${STATIC.LOTTERY_AMOUNT.FOUR}) - ${this.prizeList[4]}개`
+    );
+    MissionUtils.Console.print(
+      `5${STATIC.MESSAGE.CORRECT} (${STATIC.LOTTERY_AMOUNT.FIVE}) - ${this.prizeList[5]}개`
+    );
+    MissionUtils.Console.print(
+      `5${STATIC.MESSAGE.CORRECT_BONUS}(${STATIC.LOTTERY_AMOUNT.FIVE_BONUS}) - ${this.prizeList[7]}개`
+    );
+    MissionUtils.Console.print(
+      `6${STATIC.MESSAGE.CORRECT} (${STATIC.LOTTERY_AMOUNT.SIX}) - ${this.prizeList[6]}개`
+    );
+  };
+
+  calculateResult = () => {
+    return (
+      this.calculatePrize(STATIC.LOTTERY_PRIZE.THREE, this.prizeList[3]) +
+      this.calculatePrize(STATIC.LOTTERY_PRIZE.FOUR, this.prizeList[4]) +
+      this.calculatePrize(STATIC.LOTTERY_PRIZE.FIVE, this.prizeList[5]) +
+      this.calculatePrize(STATIC.LOTTERY_PRIZE.FIVE_BONUS, this.prizeList[7]) +
+      this.calculatePrize(STATIC.LOTTERY_PRIZE.SIX, this.prizeList[6])
+    );
+  };
+
+  calculatePrize = (value, cnt) => {
+    return value * cnt;
+  };
+
+  announceYield = () => {
+    MissionUtils.Console.print(
+      `${STATIC.MESSAGE.YIELD}${(
+        (this.userPrizeValue / this.money) *
+        100
+      ).toFixed(1)}%입니다.`
+    );
+  };
 }
-
-const startCheckResult = (
-  lotto,
-  userPickLottos,
-  bonusNumber,
-  prizeList,
-  userPrizeValue,
-  money
-) => {
-  userPickLottos.map((e) => {
-    resultExceptBonus = checkResult(lotto, e, prizeList);
-    prizeList.splice(resultExceptBonus, 1, prizeList[resultExceptBonus] + 1);
-    checkBonusNum(e, bonusNumber, prizeList, resultExceptBonus);
-  });
-  announceResult(prizeList, userPrizeValue);
-  userPrizeValue = calculateResult(prizeList);
-  announceYield(money, userPrizeValue);
-  MissionUtils.Console.close();
-};
-
-const checkResult = (lotto, userPickLottoNum) => {
-  let correctCnt = 0;
-  lotto.map((number) => {
-    correctCnt += correctLottoNum(userPickLottoNum, number);
-  });
-  return correctCnt;
-};
-
-const correctLottoNum = (userPickLotto, number) => {
-  if (userPickLotto.includes(parseInt(number))) {
-    return 1;
-  }
-  return 0;
-};
-
-const checkBonusNum = (
-  userPickLotto,
-  bonusNumber,
-  prizeList,
-  resultExceptBonus
-) => {
-  if (resultExceptBonus == 5) {
-    isInBonousNum(userPickLotto, bonusNumber, prizeList);
-  }
-};
-
-const isInBonousNum = (userPickLotto, bonusNumber, prizeList) => {
-  if (userPickLotto.includes(parseInt(bonusNumber))) {
-    prizeList.splice(5, 1, prizeList[5] - 1);
-    prizeList.splice(6, 1, prizeList[6] + 1);
-  }
-};
-
-const announceResult = (prizeList) => {
-  MissionUtils.Console.print(STATIC.MESSAGE.STATISTIC + "\n" + "---");
-  MissionUtils.Console.print(
-    `3${STATIC.MESSAGE.CORRECT} (${STATIC.LOTTERY_AMOUNT.THREE}) - ${prizeList[3]}개`
-  );
-  MissionUtils.Console.print(
-    `4${STATIC.MESSAGE.CORRECT} (${STATIC.LOTTERY_AMOUNT.FOUR}) - ${prizeList[4]}개`
-  );
-  MissionUtils.Console.print(
-    `5${STATIC.MESSAGE.CORRECT} (${STATIC.LOTTERY_AMOUNT.FIVE}) - ${prizeList[5]}개`
-  );
-  MissionUtils.Console.print(
-    `5${STATIC.MESSAGE.CORRECT_BONUS}(${STATIC.LOTTERY_AMOUNT.FIVE_BONUS}) - ${prizeList[7]}개`
-  );
-  MissionUtils.Console.print(
-    `6${STATIC.MESSAGE.CORRECT} (${STATIC.LOTTERY_AMOUNT.SIX}) - ${prizeList[6]}개`
-  );
-};
-
-const calculateResult = (prizeList) => {
-  return (
-    calculatePrize(STATIC.LOTTERY_PRIZE.THREE, prizeList[3]) +
-    calculatePrize(STATIC.LOTTERY_PRIZE.FOUR, prizeList[4]) +
-    calculatePrize(STATIC.LOTTERY_PRIZE.FIVE, prizeList[5]) +
-    calculatePrize(STATIC.LOTTERY_PRIZE.FIVE_BONUS, prizeList[7]) +
-    calculatePrize(STATIC.LOTTERY_PRIZE.SIX, prizeList[6])
-  );
-};
-
-const calculatePrize = (value, cnt) => {
-  return value * cnt;
-};
-
-const announceYield = (money, userPrizeValue) => {
-  MissionUtils.Console.print(
-    `${STATIC.MESSAGE.YIELD}${((userPrizeValue / money) * 100).toFixed(
-      1
-    )}%입니다.`
-  );
-};
 
 module.exports = LottoGame;
