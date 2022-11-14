b25599386e8c6b03a918e6179cedeeabd8ba1766
MAGHC <windowsapple95@gmail.com>
2022-11-14 20:44:15 +0900

    refactor: 주석 제거

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,7 +10,6 @@ class Lotto {
     this.#numbers = numbersArray;
   }
 
-  // TODO: 추가 기능 구현
   ascendingSortLottoArray() {
     return this.#numbers.sort((a, b) => a - b);
   }
@@ -35,7 +34,5 @@ class Lotto {
     return lottoNumbers;
   }
 }
-
-// const lotto = new Lotto([1, 3, 2, 7, 4, 5]);
 
 module.exports = Lotto;
