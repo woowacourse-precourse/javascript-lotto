34c1a2aeae6248cad991fb388b4b7a480fcd4b2c
Monte <monte6198@gmail.com>
2022-11-15 07:30:00 +0900

    feat: 로또 당첨 번호 입력 예회처리 추가

---
+++ src/Validation/LottoValidation.js
@@ -0,0 +1,29 @@
+class LottoValidation {
+  static hasSixNumbers(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다. 종료합니다.");
+    }
+  }
+  static isNotNumber(numbers) {
+    numbers.map((number) => {
+      if (isNaN(number)) {
+        throw new Error("[ERROR] 로또 번호는 숫자이어야 합니다. 종료합니다.");
+      }
+    });
+  }
+  static checkRange(numbers) {
+    numbers.map((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 정수이어야 합니다. 종료합니다.");
+      }
+    });
+  }
+  static isUniqueNumber(numbers) {
+    let lottoSet = new Set([...numbers]);
+    if (lottoSet.size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복 숫자가 존재해서는 안 됩니다. 종료합니다.");
+    }
+  }
+}
+
+module.exports = LottoValidation;
