55048d0e572597177c1640d1d046e2743dddf21f
gyeongza <gyeongza@naver.com>
2022-11-14 21:39:13 +0900

    test : 구매한 로또 번호 출력 테스트 완료

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -1,6 +1,13 @@
 const App = require("../src/App");
 const MissionUtils = require("@woowacourse/mission-utils");
 
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
 const getLogSpy = () => {
   const logSpy = jest.spyOn(MissionUtils.Console, "print");
   logSpy.mockClear();
@@ -14,8 +21,40 @@ describe("함수별 기능 테스트", () => {
     const money = 8000;
 
     const app = new App();
-    app.printLotto(money);
+    app.printLottoQuantity(money);
 
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+  });
+
+  test("발행 로또 번호 출력", () => {
+    const logSpy = getLogSpy();
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+    const logs = [
+      "[8, 21, 23, 41, 42, 43]",
+      "[3, 5, 11, 16, 32, 38]",
+      "[7, 11, 16, 35, 36, 44]",
+      "[1, 8, 11, 31, 41, 42]",
+      "[13, 14, 16, 38, 42, 45]",
+      "[7, 11, 30, 40, 42, 43]",
+      "[2, 13, 22, 32, 38, 45]",
+      "[1, 3, 5, 14, 22, 45]",
+    ];
+    const lottoQuantity = 8;
+
+    const app = new App();
+    app.printLotto(lottoQuantity);
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
   });
 });
