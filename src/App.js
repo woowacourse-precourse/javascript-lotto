589f85b002b436f0a48664b987dc378c4c99ae72
nailen1 <nietsnie1@gmail.com>
2022-11-14 23:12:27 +0900

    feat: 입력값을 요구하는 메세지를 출력, 입력값을 받아 읽는 기능

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-
 const { Console } = require('@woowacourse/mission-utils');
 
 class App {
   play() {
+  }
+
+  readPurchase() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+      this.isNull(money);
+    });
+  }
+
+  readWinningNumbers() {
+    Console.readLine('\n당첨 번호를 입력해 주세요.\n', (commaNumbers) => {
+      this.isNull(commaNumbers);
+    })
+  }
+
+  readBonusNumber() {
+    Console.readLine('\n보너스 번호를 입력해 주세요.\n', (number) => {
+      this.isNull(number);
+    });
+  }
+
+  isNull(input) {
+    if (input === '') throw new Error("[ERROR] 입력값이 존재하지 않습니다.")
+    return true;
   }
 
   end() {
