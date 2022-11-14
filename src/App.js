c5b453a52c5e379bae8a4d8a5d49453b7d0df84b
mochang2 <qjsrodksro@gmail.com>
2022-11-14 16:13:29 +0900

    feat: App 클래스 구입 금액 입력, 유효성 검증 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,57 @@
+const { Random, Console } = require('@woowacourse/mission-utils')
+const { LOTTO_PRICE, query, validationError } = require('./constants/app.js')
+
 class App {
-  play() {}
+  #perchaseAmount
+
+  play() {
+    this.#perchaseAmount = 0
+
+    this.#getPurchaseAmount()
+  }
+
+  #getPurchaseAmount() {
+    Console.readLine(query.PURCHASE, (price) => {
+      this.#perchaseAmount = Number(price)
+      this.#checkValidation()
+
+      // TODO: 로또 개수 입력받고 개수대로 로또 만들어서 출력 + property에 추가\
+
+      // TODO: 당첨 통계를 계산한다.
+    })
+  }
+
+  #checkValidation() {
+    if (this.#isNotValidType()) {
+      this.#close()
+
+      throw new Error(validationError.TYPE)
+    }
+
+    if (this.#isNotValidUnit()) {
+      this.#close()
+
+      throw new Error(validationError.UNIT)
+    }
+  }
+
+  /**
+   * @returns {booelan}
+   */
+  #isNotValidType() {
+    return !Number.isInteger(this.#perchaseAmount)
+  }
+
+  /**
+   * @returns {booelan}
+   */
+  #isNotValidUnit() {
+    return this.#perchaseAmount % LOTTO_PRICE !== 0
+  }
+
+  #close() {
+    Console.close()
+  }
 }
 
 module.exports = App
