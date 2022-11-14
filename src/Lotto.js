c1e9cb8e558af5699b243791b11d9744890149e4
cho seonghoon <chsida655@gmail.com>
2022-11-15 00:44:05 +0900

    feat: 개별 로또 결과 판별하는 메소드

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,6 +1,7 @@
+const { Print } = require("./lib/MissionUtils.js");
+
 class Lotto {
   #numbers;
-
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
@@ -12,6 +13,35 @@ class Lotto {
     }
   }
 
+  checkCorrespond(winNum, bonus) {
+    let correspondObj = { num: 0, bonus: 0 };
+    this.#numbers.forEach((el) => {
+      if (winNum.includes(el)) correspondObj.num += 1;
+      if (el === bonus) correspondObj.bonus += 1;
+    });
+    return this.checkResult(correspondObj);
+  }
+  checkResult(correspondObj) {
+    let result;
+    switch (correspondObj.num) {
+      case 6:
+        result = 1;
+        break;
+      case 5:
+        result = 3;
+        break;
+      case 4:
+        result = 4;
+        break;
+      case 3:
+        result = 5;
+        break;
+      default:
+        result = 0;
+    }
+    if (result === 2 && correspondObj.bonus === 1) result = 2;
+    return result;
+  }
   // TODO: 추가 기능 구현
 }
 
