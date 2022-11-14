66f6919163a4a0f4c85a10428442abf3dca8f9cf
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-14 15:10:40 +0900

    test: 로또 발행 테스트 케이스 수정

--- __tests__/CreateLottoTest.js
+++ __tests__/CreateLottoTest.js
@@ -1,26 +1,26 @@
-const LottoGame = require("../src/LottoGame.js");
+const LottoMachine = require("../src/LottoMachine.js");
 const Lotto = require("../src/Lotto.js");
 const { Console } = require("@woowacourse/mission-utils");
 
-describe("LottoGame.createLottos", () => {
+describe("LottoGame.purchaseLottos", () => {
   afterEach(() => Console.close());
 
-  test("input 숫자로 생성된 로또의 개수는 input과 같아야 한다.", () => {
+  test("금액이 투입되면 투입금 만큼의 로또가 발행되어야 한다.", () => {
     // given
-    const input = 8;
+    const lottoMachine = new LottoMachine();
+    const input = "5000";
+    lottoMachine.insertMoney(input);
     // when
-    const lottoGame = new LottoGame();
-    lottoGame.createLottos(input);
-    const result = lottoGame.lottos;
+    const result = lottoMachine.purchaseLottos();
     // then
-    expect(result).toHaveLength(input);
+    expect(result).toHaveLength(5);
   });
 
-  test("생성된 로또는 모두 Lotto클래스의 instance여야 한다.", () => {
-    const lottoGame = new LottoGame();
-    lottoGame.createLottos(5);
+  test("발행된 로또는 모두 Lotto클래스의 instance여야 한다.", () => {
+    const lottoMachine = new LottoMachine();
+    const result = lottoMachine.purchaseLottos();
 
-    lottoGame.lottos.forEach((lotto) => {
+    result.forEach((lotto) => {
       expect(lotto).toBeInstanceOf(Lotto);
     });
   });
