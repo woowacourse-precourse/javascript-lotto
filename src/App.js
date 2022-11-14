dd4c49a33f2508ec8f18df54236a5a2129358ff0
hafnium1923 <hafnium1923@gmail.com>
2022-11-14 19:52:01 +0900

    feat winLotto, isSameNum 구현
    로또 결과 계산 함수, 로또 숫자 비요 함수 구현

--- src/App.js
+++ src/App.js
@@ -2,59 +2,81 @@ const Lotto = require("./Lotto");
 const MU = require("@woowacourse/mission-utils");
 class App {
   play() {
-    this.getWinNum();
-    //this.buyLotto();
+    this.buyLotto();
   }
-  buyLotto(){
+  buyLotto() {
     MU.Console.readLine('구입금액을 입력해 주세요.\n',(money) => {
-      if(isNaN(money)){
+      if(isNaN(money)) {
         throw new Error("[ERROR] 구입금액은 숫자여야 합니다.");
       }
-      if(parseInt(money/1000) === 0){
+      if(parseInt(money/1000) === 0) {
         throw new Error("[ERROR] 최소 구입금액은 1000원입니다.");
       }
-      if(parseInt(money%1000) != 0){
+      if(parseInt(money%1000) != 0) {
         throw new Error("[ERROR] 1000원 단위로 입력해주세요.");
       }
       let purchaseNum = parseInt(money/1000);
-      this.makeLottoNum(purchaseNum);
-      this.getWinNum();
+      let lottos = this.makeLottoNum(purchaseNum);
+      this.getWinNum(lottos);
       
     });
   }
-  getWinNum(){
-    MU.Console.readLine('\n당첨 번호를 입력해 주세요.', (number) =>{
+  getWinNum(lottos) {
+    MU.Console.readLine('\n당첨 번호를 입력해 주세요.\n', (number) =>{
       let stringToNum =  number.split(',').map(a => parseInt(a));
-      console.log(stringToNum); // 테스트코드
       let win = new Lotto(stringToNum);
-      console.log(win.getNumber()); // 테스트코드
-      return MU.Console.close();;
+      this.getBonusNum(lottos,win);
     });
   }
-  getBonusNum(){
-    MU.Console.readLine('보너스 번호를 입력해 주세요.', (number) =>{
-      if(isNoN(number)){
+  getBonusNum(lottos,win) {
+    MU.Console.readLine('\n보너스 번호를 입력해 주세요.\n', (number) =>{
+      let bonus = parseInt(number);
+      if(isNaN(number)) {
         throw new Error("[ERROR] 보너스 번호는 숫자여야 합니다.");
       }
-      if(parseInt(number) < 1 || 45 < parseInt(number)){
+      if(bonus < 1 || 45 < bonus) {
         throw new Error("[ERROR] 로또 범위 안의 숫자를 입력해주세요.");
       }
+      if(win.getNumber().includes(bonus)) {
+        throw new Error("[ERROR] 보너스 번호는 당첨번호와 중복될 수 없습니다.");
+      }
+      let winNum = win.getNumber();
+      this.winLotto(winNum,bonus,lottos);
+      return MU.Console.close();
     });
   }
-  makeLottoNum(purchaseNum){
-    let Lottos = [purchaseNum];
-    for(let i = 0; i < purchaseNum; i++){
-      Lottos[i] = MU.Random.pickUniqueNumbersInRange(1,45,6);
+  winLotto(win,bonus,lottos) {
+    let rslt = Array.from({length: 6}, () => 0);
+    for(let i = 0; i < lottos.length; i++) {
+      rslt[this.isSameNum(win,bonus,lottos[i])]++;
     }
-    this.printBuyLotto(Lottos);
+    console.log(rslt);
   }
-  printBuyLotto(Lottos){
-    MU.Console.print(`\n${Lottos.length}개를 구매했습니다.`);
-    for(let i = 0; i < Lottos.length; i++){
-      Lottos[i].sort((a,b) => {
+  isSameNum(win, bonus, lotto) {
+    let count = win.filter(x => lotto.includes(x)).length;
+    if(count === 6) return 1;
+    if(count === 5 && lotto.includes(bonus)) return 2;
+    if(count === 5) return 3;
+    if(count === 4 || count === 3) return 8 -count;
+    return 0;
+  }
+  makeLottoNum(purchaseNum) {
+    let lottos = [purchaseNum];
+    for(let i = 0; i < purchaseNum; i++) {
+      lottos[i] = MU.Random.pickUniqueNumbersInRange(1,45,6);
+    }
+    for(let i = 0; i < lottos.length; i++){
+      lottos[i].sort((a,b) => {
         return a-b;
       });
-      MU.Console.print(Lottos[i]);
+    }
+    this.printBuyLotto(lottos);
+    return lottos;
+  }
+  printBuyLotto(lottos) {
+    MU.Console.print(`\n${lottos.length}개를 구매했습니다.`);
+    for(let i = 0; i < lottos.length; i++){
+      MU.Console.print(lottos[i]);
     }
   }
   
