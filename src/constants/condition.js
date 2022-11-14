738542fafc72b44d38a41582dc03eacb93b86d48
Dohyeon Lee_Laptop <dlehgus1110@naver.com>
2022-11-14 09:42:00 +0900

    feat: 로또 수상 조건 상수 추가

--- src/constants/condition.js
+++ src/constants/condition.js
@@ -2,10 +2,17 @@ const LOTTO_PRICE = 1000;
 const LOTTO_NUM_MIN_RANGE = 1;
 const LOTTO_NUM_MAX_RANGE = 45;
 const LOTTO_DIGITS = 6;
+const LOTTO_PRIZE_MATCH_COUNT = {
+  firstPlace: 6,
+  thirdPlace: 5,
+  fourthPlace: 4,
+  fifthPlace: 3,
+};
 
 module.exports = {
   LOTTO_PRICE,
   LOTTO_NUM_MIN_RANGE,
   LOTTO_NUM_MAX_RANGE,
   LOTTO_DIGITS,
+  LOTTO_PRIZE_MATCH_COUNT,
 };
