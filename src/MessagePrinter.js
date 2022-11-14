c40a4abe6514fc9e8fcc698e22889a0b4bb94e84
yeongseokim <1903020@ewhain.net>
2022-11-14 18:22:19 +0900

    feat(MessagePrinter): 발행한 로또 번호 리스트를 출력 기능 구현

--- src/MessagePrinter.js
+++ src/MessagePrinter.js
@@ -4,6 +4,10 @@ class MessagePrinter {
     static printPurchaseAmount(amount) {
         MissionUtils.Console.print(`${amount}개를 구매했습니다.`);
     }
+
+    static printAllLottos(lottoWallet) {
+        lottoWallet.forEach(lotto => MissionUtils.Console.print(lotto.numbers));
+    }
 }
 
 module.exports = MessagePrinter;
