90f963d2bb50234c0898a81518e4d06c50e3f190
Yebin Kim <kyb4312@gmail.com>
2022-11-14 11:35:21 +0900

    feat(Lotto): implement function to validate bonus number

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,6 +4,7 @@ class Lotto {
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
+    this.bonusNumber;
   }
 
   validate(numbers) {
@@ -19,6 +20,22 @@ class Lotto {
     if (numbers.length !== new Set(numbers).size) {
       throw new Error('[ERROR] 로또 번호는 중복된 숫자를 포함할 수 없습니다.');
     }
+  }
+
+  validateBonusNumber(number) {
+    if (!number) {
+      throw new Error('[ERROR] 보너스 번호는 한자리 숫자여야 합니다.');
+    }
+    if (!Number.isInteger(number)) {
+      throw new Error('[ERROR] 로또 번호는 정수여야 합니다.');
+    }
+    if (!(number >= 1 && number <= 45)) {
+      throw new Error('[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.');
+    }
+    if (this.#numbers.includes(number)) {
+      throw new Error('[ERROR] 로또 번호는 중복된 숫자를 포함할 수 없습니다.');
+    }
+    this.bonusNumber = number;
   }
 }
 
