2ba10b068218ba40bb354b4282b84666d53616ec
GunHoi <lgh509@kyonggi.ac.kr>
2022-11-14 15:43:37 +0900

    feat - 6 : 맞춘 개수 별로 당첨통계에 저장한다.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const Bonus = require("./Bonus.js");
 class App {
   insertedMoney;
   userLottoNumberLists = [];
+  userWinningStatics = [0,0,0,0,0];
   async play() {
     await this.insertMoney();
     const LOTTO_COUNT = this.insertedMoney / 1000;
@@ -86,13 +87,27 @@ class App {
     });
     state = checkBonusNumber(bonusNumber, userNumbers);
     count += state[0];
-    const isBonus = state[1]; 
+    const isBonus = state[1];
+    addWinningStatics(count,isBonus);
   }
   checkBonusNumber(bonusNumber, userNumbers){
     if(userNumbers.includes(...bonusNumber)){
       return [1,true];
     }else {
       return [0,false];
+    }
+  }
+  addWinningStatics(count, isBonus){
+    if(count===6){
+      this.userWinningStatics[0] +=1;
+    }else if(count === 5 && isBonus){
+      this.userWinningStatics[1] +=1;
+    }else if(count === 5){
+      this.userWinningStatics[2] +=1;
+    }else if(count === 4){
+      this.userWinningStatics[3] +=1;
+    }else if(count === 3){
+      this.userWinningStatics[4] +=1;
     }
   }
 }
