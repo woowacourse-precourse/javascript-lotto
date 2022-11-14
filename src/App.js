6c2fd0fe256d221ecc0e5fcd413593c1edd096d5
binllionaire <46593078+binllionaire@users.noreply.github.com>
2022-11-14 15:48:22 +0900

    feat : 구입금액이 1000원 단위가 아닐시 예외처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const PRICE = 1000
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요 : ', (input) => {
+      this.verificateMoney(input)
+    });
+  }
+  verificateMoney(money){
+    if(money % PRICE != 0) throw new Error('[ERROR] 구입금액은 1000원 단위여야 합니다')
+  }
 }
-
+const app = new App();
+      app.play();
 module.exports = App;
