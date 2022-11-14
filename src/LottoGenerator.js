a456ab9701e7e5006a718488224097a5d7f8628a
hyeongdong <khd941018@gmail.com>
2022-11-15 02:24:20 +0900

    feat(LottoGenerator) : 구입 금액만큼 로또를 생성하는 기능 구현

--- src/LottoGenerator.js
+++ src/LottoGenerator.js
@@ -1,21 +1,38 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
 
 class LottoGenerator {
   LOTTO_PRICE = 1000;
+  MIN_NUMBER = 1;
+  MAX_NUMBER = 45;
+  PICK_COUNT = 6;
 
   constructor(payment) {
     this.validate(payment);
-    this.numberOfLotto = Number(payment) / LOTTO_PRICE;
+    this.totalNumberOfLotto = Number(payment) / this.LOTTO_PRICE;
   }
 
   validate(payment) {
     if (isNaN(payment)) {
-      throw new Error("[ERROR]");
+      throw new Error("[ERROR] 구매 금액은 숫자만 입력하셔야 합니다.");
     }
 
-    if (payment % 1000 !== 0) {
-      throw new Error("[ERROR]");
+    if (payment % this.LOTTO_PRICE !== 0) {
+      throw new Error("[ERROR] 구매 금액은 천원 단위로 입력하셔야 합니다.");
     }
+  }
+
+  createLottos() {
+    const lottos = [];
+    let numberOfCreatedLotto = 0;
+
+    while (numberOfCreatedLotto < this.totalNumberOfLotto) {
+      const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(this.MIN_NUMBER, this.MAX_NUMBER, this.PICK_COUNT);
+      lottos.push(new Lotto(randomNumbers));
+      numberOfCreatedLotto++;
+    }
+
+    return lottos;
   }
 }
 
