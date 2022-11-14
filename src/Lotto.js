5d9121cd4c3849511c556be08d2618694b09c29c
wusi-hub <jws9595@naver.com>
2022-11-15 08:36:21 +0900

    feat(Lotto.js): 숫자가 아닌 문자를 확인할 수 있는 예외 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,28 +1,27 @@
 class Lotto {
   #numbers;
-
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
   }
-
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-
     if (new Set(numbers).size !== 6) {
       throw new Error("[ERROR] 로또 번호는 중복되지 않는 숫자여야 합니다.");
     }
+
     numbers.map((number) => {
-      if (!(parseInt(number) >= 1 && parseInt(number) <= 45)) throw new Error("[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다.");
+      if (!(Number(number) >= 1 && Number(number) <= 45)) throw new Error("[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다.");
+      if (!(this.filterNumber(number) >= 1 && this.filterNumber(number) <= 45)) throw new Error("[ERROR] 로또 번호는 1부터 45까지의 숫자여야 하고 다른 문자를 포함할 수 없습니다.");
     });
   }
 
-  // TODO: 추가 기능 구현
+  filterNumber(number) {
+    if (/[1-45]/.test(number)) return Number(number);
+    else return NaN;
+  }
 }
-
-let lotto = new Lotto([1, 2, 3, 4, 5, '49']);
-lotto.validate;
 
 module.exports = Lotto;
