b15cd14095abfd80162a88c02a0e4f9f3ee06263
yeongseokim <1903020@ewhain.net>
2022-11-14 14:10:07 +0900

    test(LottoMachine): 정렬된 배열 반환 테스트 코드 추가

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -4,4 +4,9 @@ describe("로또 기계 클래스 테스트", () => {
     test("6개 수를 가진 로또가 생성된다.", () => {
         expect(LottoMachine.createLotto().length).toEqual(6);
     });
+
+    test("정렬된 배열을 반환한다.", () => {
+        const unsortedNumbers = [45, 35, 26, 10, 33, 2]
+        expect(LottoMachine.sortNumbersAscending(unsortedNumbers)).toEqual([2, 10, 26, 33, 35, 45]);
+    });
 })
