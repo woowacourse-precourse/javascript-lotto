7259b48d77d82ef6ba55e4a1c705a88d7c24acfd
jetty8013 <jetty8013@gmail.com>
2022-11-14 23:40:38 +0900

    feat: 로또 구매 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     const ticket = this.inputMoeny();
+    buyingLotto(ticket)
   }
 
   createLotto(numbers) {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, numbers)
     .sort((a,b) => a - b)
   }
-
 
   inputMoeny() {
     let userInput = 0;
@@ -18,6 +18,18 @@ class App {
     });
     if(userInput%1000 === 0 && userInput > 0) return userInput/1000;
     throw new Error("[ERROR] 1,000원 단위로 구매할 수 있습니다.");
+  }
+
+  buyingLotto(ticket) {
+    const ticketArr = [];
+    MissionUtils.Console.print(`${ticket}개를 구매했습니다.`)
+    while(0 < ticket) {
+      const getCreateLotto = this.createLotto(6)
+      ticketArr.push(getCreateLotto);
+      MissionUtils.Console.print(`[${getCreateLotto.join(', ')}]`)
+      ticket-=1;
+    }
+    return ticketArr;
   }
 
 }
