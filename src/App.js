41077dc523ad5f754ded614c583ab17bc820bd8d
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-15 07:04:44 +0900

    Feat: app play 함수 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const User = require("./User");
+const Prize = require("./Prize");
+
 class App {
-  play() {}
+  play() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+      const user = new User(money);
+      user.buyLottos(parseInt(money / 1000));
+
+      const prize = new Prize();
+      prize.winningCheck(user);
+		})
+  }
 }
 
 module.exports = App;
