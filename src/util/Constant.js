7b40e66c72f01a31a807276be2a786f08293eb56
Hungeun2 <bepoohun@gmail.com>
2022-11-14 15:05:04 +0900

    Feat: Constant 추가 로또 가격 로또 관련 메시지 예외 처리 관련 메시지

---
+++ src/util/Constant.js
@@ -0,0 +1,14 @@
+const LOTTO_PRICE = 1000;
+
+const LOTTO_MESSAGE = {
+  BUDGET: "구입금액을 입력해 주세요.\n",
+  BUYING: "개를 구매했습니다.",
+  WIN_NUM: "당첨 번호를 입력해 주세요.",
+  BONUS_NUM: "보너스 번호를 입력해 주세요.",
+};
+const BUDGET_ERROR_MESSAGE = {
+  NON_NUMBER: "[ERROR] 구매 금액은 숫자만 입력해 주세요.",
+  INDIVISIBLE: "[ERROR] 1000원 단위로 입력해 주세요.",
+  SHORT_OF_MONEY: "[ERROR] 최소 금액은 1000원 입니다.",
+};
+module.exports = { LOTTO_MESSAGE, BUDGET_ERROR_MESSAGE, LOTTO_PRICE };
