d693b27c9409bcad78fbb89962a983624270b1f5
banma1234 <banma1234@gmail.com>
2022-11-14 23:07:35 +0900

    refactor: 제한사항에 맞게 Lotto 필드 수정 및 모듈화

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,97 +3,41 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class Lotto {
   #numbers;
 
-  constructor(MONEY, numbers, BONUS_NUMBER) {
+  constructor(numbers) {
     this.validate(numbers);
-    this.errorHandler(MONEY, numbers, BONUS_NUMBER);
-
     this.#numbers = numbers;
-    this.BONUS_NUMBER = BONUS_NUMBER;
-    this.MONEY = MONEY;
-  }
-
-  sliceNumber(numbers) {
-    return numbers.split(',').map((item) => {
-      return parseInt(item);
-    })
   }
 
   validate(numbers) {
-    let numberArray = this.sliceNumber(numbers);
-    const lottoSet = new Set(numberArray);
-    if (lottoSet.size != numberArray.length) {
-      throw new Error("[ERROR] 중복된 숫자를 입력할 수 없습니다.");
-    }
-
-    if (lottoSet.size !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-
-    for (let item of lottoSet.values()) {
-      if (item < 1 | item > 45) {
-        throw new Error("[ERROR] 1 ~ 45까지의 숫자만 입력할 수 있습니다.")
-      }
-    }
+    // if (numbers.length !== 6) {
+    //   throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    // }
   }
-  
-  errorHandler(MONEY, numbers, BONUS_NUMBER) {
-    if (BONUS_NUMBER < 1 | BONUS_NUMBER > 45) {
-      throw new Error("[ERROR] 1 ~ 45까지의 숫자만 입력할 수 있습니다.");
-    }
 
-    if (MONEY%1000 != 0) {
-      throw new Error("[ERROR] 복권의 1장당 가격은 1000원입니다.");
-    }
-
-    if (numbers.includes(BONUS_NUMBER)) {
+  errorHandler(BONUS_NUMBER) {
+    if (this.#numbers.includes(BONUS_NUMBER)) {
       throw new Error("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
     }
   }
 
-  makeRandomValue(MONEY) {
-    let lotteryTicket = [];
+  matchLotto(lotteryTicket, BONUS_NUMBER, MONEY) {
+    this.errorHandler(BONUS_NUMBER);
 
-    for(let i=0; i<MONEY/1000; i++) {
-      let temp = [];
-      temp = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-      lotteryTicket.push(temp.sort((a, b) => {
-        return a-b;
-      }));
-    }
-
-    this.printTicket(lotteryTicket, MONEY/1000);
-    
-    return lotteryTicket
-  };
-
-  printTicket(lotteryTicket, MONEY) {
-    MissionUtils.Console.print(`${MONEY}개를 구매했습니다.`);
-
-    lotteryTicket.forEach((item) => {
-      let temp = '[' + String(item.join(', ')) + ']';
-      MissionUtils.Console.print(temp);
-    });
-  }
-
-  matchLotto(lotteryTicket) {
     let prize = [];
     lotteryTicket.forEach((ticket) => {
-      prize.push(this.judgeNumber(ticket));
+      prize.push(this.judgeNumber(ticket, BONUS_NUMBER));
     })
 
-    this.printResult(this.makeResult(prize));
+    this.printResult(this.makeResult(prize), MONEY);
   }
 
-  judgeNumber(ticket) {
+  judgeNumber(ticket, BONUS_NUMBER) {
     let count = [0, 0];
-    let numberArray = this.#numbers.split(',').map((e) => {
-      return parseInt(e);
-    })
     ticket.forEach((item) => {
-      if (numberArray.includes(item)) {
+      if (this.#numbers.includes(item)) {
         count[0] += 1;
       }
-      else if (item == this.BONUS_NUMBER) {
+      else if (item == BONUS_NUMBER) {
         count[1] += 1;
       }
     });
@@ -101,9 +45,9 @@ class Lotto {
     return count;
   }
 
-  printResult(result) {
+  printResult(result, MONEY) {
     let winnigPrize = this.caculatePrize(result);
-    let returnRate = ((winnigPrize/this.MONEY) * 10 / 10) * 100;
+    let returnRate = ((winnigPrize/MONEY) * 10 / 10) * 100;
 
     MissionUtils.Console.print(`3개 일치 (5,000원) - ${result[0]}개`)
     MissionUtils.Console.print(`4개 일치 (50,000원) - ${result[1]}개`)
