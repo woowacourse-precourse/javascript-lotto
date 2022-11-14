fd698169dc4a4101bfd89825d372741271bf16a8
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-14 23:21:18 +0900

    refactor: App Class Play Method split user input method merge

--- src/App.js
+++ src/App.js
@@ -9,17 +9,13 @@ const {
 class App {
   play() {
     this.inputBuyPrice((price) => {
+      if (price % 1000 !== 0) {
+        throw new Error("[ERROR] 복권 구매 금액 오류");
+      }
       const lottos = this.sellLottos(price);
-      this.inputWinningNumber((winningNumbers) => {
-        this.inputBonusNumber((bonusNumber) => {
-          const counts = lottos.map((lotto) =>
-            lotto.checkLottoResult(winningNumbers, bonusNumber)
-          );
-          const statistics = getStatistics(counts);
-          this.getStatistics(statistics);
-          this.getRateOfReturn(price, statistics);
-        });
-      });
+      this.inputAnswerLottoNumber((winningNumbers, bonusNumber) =>
+        this.compareLottos(price, lottos, winningNumbers, bonusNumber)
+      );
     });
   }
 
@@ -28,20 +24,27 @@ class App {
   }
 
   sellLottos(price) {
-    if (price % 1000 !== 0) {
-      throw new Error("[ERROR] 복권 구매 금액 오류");
-    }
     const lottoLength = price / 1000;
     this.print(`${lottoLength}개를 구매했습니다.`);
     const lottos = buyLottos(lottoLength);
     return lottos;
   }
-  inputWinningNumber(cb) {
-    Console.readLine("\n당첨 번호를 입력해 주세요. \n ", (n) => cb(n));
+
+  compareLottos(price, lottos, winningNumbers, bonusNumber) {
+    const counts = lottos.map((lotto) =>
+      lotto.checkLottoResult(winningNumbers, bonusNumber)
+    );
+    const statistics = getStatistics(counts);
+    this.getStatistics(statistics);
+    this.getRateOfReturn(price, statistics);
   }
 
-  inputBonusNumber(cb) {
-    Console.readLine("\n보너스 번호를 입력해 주세요. \n", (n) => cb(n));
+  inputAnswerLottoNumber(cb) {
+    Console.readLine("\n당첨 번호를 입력해 주세요. \n ", (winningNumbers) => {
+      Console.readLine("\n보너스 번호를 입력해 주세요. \n", (bonusNumber) =>
+        cb(winningNumbers, bonusNumber)
+      );
+    });
   }
 
   getStatistics(statistics) {
