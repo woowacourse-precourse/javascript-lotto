5d3818caac290bccb9df7e273601e9f92a7dee20
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-11-15 02:29:10 +0900

    input, output 상수

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const INPUTS = {
+  INPUT_MONEY: "구입금액을 입력해 주세요.\n",
+  INPUT_NUMBERS: "당첨 번호를 입력해 주세요.\n",
+  INPUT_BONUS: "보너스 번호를 입력해 주세요.\n",
+};
+
+const OUTPUTS = {
+  CORRECT_THREE: "3개 일치 (5,000원) - ",
+  CORRECT_FOUR: "4개 일치 (50,000원) - ",
+  CORRECT_ONLY_FIVE: "5개 일치 (1,500,000원) - ",
+  CORRECT_FIVE_BONUS: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+};
+
+module.exports = { INPUTS, OUTPUT };
