5521e7695ebe4f62d0a2b60bc8ce0279b46228e7
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-14 21:35:50 +0900

    fix: change object keys because of  having repeated keys
    matchlist의 키 값이 보너스 숫자를 가진 경우 5, 5로 같아서 오류를 발생할 수 있다는 것을 발견하고 수정했다.

--- src/Statistics.js
+++ src/Statistics.js
@@ -10,11 +10,8 @@ class Statistics {
     const matchInfoList = this.matchInfo();
     return Object.keys(RANKING).map((rank) => {
       const rankInfo = RANKING[rank];
-      const rankMatch = RANKING[rank]['MATCH'];
-      const matchCount =
-        matchInfoList[rankMatch] && matchInfoList[rankMatch]['bonus'] === rankInfo['BONUS']
-          ? matchInfoList[rankMatch]['count']
-          : 0;
+      const name = `match${rankInfo['MATCH']}${rankInfo['BONUS']}`;
+      const matchCount = matchInfoList[name] ? matchInfoList[name]['count'] : 0;
       return { matchCount, matchMoney: rankInfo['JACKPOT'], rankMessage: rankInfo['MESSAGE'] };
     });
   }
@@ -27,7 +24,8 @@ class Statistics {
       return [matchNumber, bonusMatch];
     });
     return matchList.reduce((matchSum, [match, bonus]) => {
-      matchSum[match] = { bonus, count: (matchSum[match] ? matchSum[match]['count'] : 0) + 1 };
+      const name = `match${match}${bonus}`;
+      matchSum[name] = { bonus, count: (matchSum[name] ? matchSum[name]['count'] : 0) + 1 };
       return matchSum;
     }, {});
   }
