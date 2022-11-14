9063ef98eb2f5d2049759d502b559ed15be50a43
minh0518 <rubics@naver.com>
2022-11-14 19:45:09 +0900

    refactor : Lotto.js 예외처리들을 함수로 각각 분리

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,20 +9,26 @@ class Lotto {
   }
 
   validate(numbers) {
+
+    this.validateForNumLength(numbers)
+
+    this.validateForDuplication(numbers)
+  }
+
+  validateForNumLength(numbers){
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.')
     }
+  }
+  
 
-    //중복 에러
+  validateForDuplication(numbers){
     const set = new Set(numbers)
     if (numbers.length !== set.size) {
-      throw new Error('[ERROR] 로또 번호는 중복된 값이 없어야 합니다.')
+      throw new Error('[ERROR] 로또 번호는 중복된 숫자가 없어야 합니다.')
     }
   }
-
-  print() {
-    console.log(this.#numbers)
-  }
+  
 
   // TODO: 추가 기능 구현
 }
