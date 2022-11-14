ae64cc6b60a98693b5a02ef0101d5fbb4fcd8d59
minh0518 <rubics@naver.com>
2022-11-14 20:05:19 +0900

    feat : 구입한 로또 예외처리(숫자가 아닌 값이 들어가 있는 경우)

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,40 +7,47 @@ class Lotto {
   }
 
   validate(numbers) {
-
     this.validateForNumLength(numbers)
 
     this.validateForDuplication(numbers)
-    
+
     this.validateForNumRange(numbers)
 
-    
+    this.validateForNotNumber(numbers)
   }
 
-  validateForNumLength(numbers){
+  validateForNumLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.')
     }
   }
-  
 
-  validateForDuplication(numbers){
+  validateForDuplication(numbers) {
+    this.validateForNotNumber(numbers) 
+    //숫자가 아닌 값이 들어오면 set로직이 이상해지므로 여기서 미리 체크
+
     const set = new Set(numbers)
     if (numbers.length !== set.size) {
       throw new Error('[ERROR] 로또 번호는 중복된 숫자가 없어야 합니다.')
     }
   }
 
-  validateForNumRange(numbers){
-    let min=Math.min(...numbers)
-    let max=Math.max(...numbers)
+  validateForNumRange(numbers) {
+    let min = Math.min(...numbers)
+    let max = Math.max(...numbers)
 
-    if(min<=1 || max>45){
+    if (min < 1 || max > 45) {
       throw new Error('[ERROR] 로또 번호는 1에서 45사이의 숫자여야 합니다')
     }
-
   }
-  
+
+  validateForNotNumber(numbers) {
+    numbers.map((i) => {
+      if (isNaN(i)) {
+        throw new Error('[ERROR] 로또 번호는 숫자로 이뤄진 값이어야 합니다.')
+      }
+    })
+  }
 
   // TODO: 추가 기능 구현
 }
