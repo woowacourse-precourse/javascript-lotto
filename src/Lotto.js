5003950c5f3fa103f635bed9135d29b7217eb44f
gyeongza <gyeongza@naver.com>
2022-11-14 22:05:35 +0900

    feat : 당첨번호 입력 에러 처리 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,12 +7,24 @@ class Lotto {
   }
 
   validate(numbers) {
+    // 개수가 올바르지 않은 경우 에러 처리
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    // 중복된 숫자가 있는 경우 에러 처리
+    if ([...new Set(numbers)].length < 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
+    // 숫자가 아닌 경우 에러 처리
+    if (isNaN(numbers.join(""))) {
+      throw new Error("[ERROR] 로또 번호는 숫자만 입력되어야 합니다.");
+    }
+    // 숫자가 범위를 벗어난 경우 에러 처리
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45까지의 숫자입니다.");
+      }
+    });
   }
-
-  // TODO: 추가 기능 구현
 }
-
 module.exports = Lotto;
