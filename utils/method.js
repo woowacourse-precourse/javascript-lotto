fdbe57c88342535156288f22506b901827781198
turtle601 <turtle601@naver.com>
2022-11-15 06:04:41 +0900

    refactor(util/print) Component.render 관련 로직 제거

--- utils/method.js
+++ utils/method.js
@@ -1,16 +1,16 @@
-const LottoPaymentUI = require('../src/ui/component/LottoPayment');
-const LottoWinCountUI = require('../src/ui/component/LottoWinCount');
-const LottoIncomeUI = require('../src/ui/component/LottoIncome');
-const Component = require('../src/ui/core/Component');
+const LottoPaymentUI = require("../src/ui/component/LottoPayment");
+const LottoWinCountUI = require("../src/ui/component/LottoWinCount");
+const LottoIncomeUI = require("../src/ui/component/LottoIncome");
+const Component = require("../src/ui/core/Component");
 
-const LottoDrawFactory = require('../src/domain/LottoDrawFactory');
+const LottoDrawFactory = require("../src/domain/LottoDrawFactory");
 
-const LottoIncome = require('../src/domain/LottoIncome');
-const LottoWinCount = require('../src/domain/LottoWinCount');
+const LottoIncome = require("../src/domain/LottoIncome");
+const LottoWinCount = require("../src/domain/LottoWinCount");
 
-const { VARIABLE_LOTTO, LOTTO_ERROR_MESSAGE } = require('./constants');
+const { VARIABLE_LOTTO, LOTTO_ERROR_MESSAGE } = require("./constants");
 
-const validateLottoRange = number => {
+const validateLottoRange = (number) => {
   if (!VARIABLE_LOTTO.regex.test(number)) {
     throw new Error(LOTTO_ERROR_MESSAGE.range);
   }
@@ -20,18 +20,18 @@ const validateLottoRange = number => {
 
 const print = {
   lottoPaymentUI: ({ count, lottos }) => {
-    const ui = new Component(new LottoPaymentUI({ count, lottos }));
-    ui.render();
+    const ui = new LottoPaymentUI({ count, lottos });
+    ui.print();
   },
 
   lottoWinCountUI: ({ winScore }) => {
-    const ui = new Component(new LottoWinCountUI({ winScore }));
-    ui.render();
+    const ui = new LottoWinCountUI({ winScore });
+    ui.print();
   },
 
   lottoIncomeUI: ({ income }) => {
-    const ui = new Component(new LottoIncomeUI({ income }));
-    ui.render();
+    const ui = new LottoIncomeUI({ income });
+    ui.print();
   },
 
   lottoAdjustmentUI: ({ lotto, bonus, lottoStore }) => {
