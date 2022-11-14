c64b9f283ff55fe6e204a343fef83ebe194d9055
seongminn <berry9901@gmail.com>
2022-11-15 01:02:12 +0900

    feat(CalculatorModel): 등수를 계산하는 기능 구현

--- src/model/CalculatorModel.js
+++ src/model/CalculatorModel.js
@@ -1,16 +1,53 @@
+const initRank = {
+  FIFTH: 0,
+  FOURTH: 0,
+  THIRD: 0,
+  SECOND: 0,
+  FIRST: 0,
+};
+
 class CalculatorModel {
+  constructor() {
+    this.rank = initRank;
+  }
+
   draw(lottos, winning) {
-    const winningList = [];
     const [winningLotto, bonus] = winning;
+    const winningList = [];
 
     lottos.forEach((lotto) => {
       let comparedLotto = lotto.compare(winningLotto);
       let comparedBonus = lotto.isContain(bonus);
 
-      winningList.push({ comparedLotto, comparedBonus });
+      const result = [
+        comparedBonus ? comparedLotto + 1 : comparedLotto,
+        comparedBonus,
+      ];
+
+      winningList.push(result);
     });
 
-    return winningList;
+    return this.getRank(winningList);
+  }
+
+  getRank(winningList) {
+    winningList.forEach((list) => {
+      const [count, isBonusCorrected] = list;
+
+      switch (count) {
+        case 3:
+          this.rank.FIFTH += 1;
+          break;
+        case 4:
+          this.rank.FOURTH += 1;
+          break;
+        case 5:
+          isBonusCorrected ? (this.rank.THIRD += 1) : (this.rank.SECOND += 1);
+          break;
+        case 6:
+          this.rank.FIRST += 1;
+      }
+    });
   }
 }
 
