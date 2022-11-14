696818ae6c1a588414416c6303724c13bd8b7e80
MINJUN PARK <mj9457@naer,com>
2022-11-15 00:17:57 +0900

    feat. 당첨 번호 입력 메서드

--- src/App.js
+++ src/App.js
@@ -53,6 +53,13 @@ class App {
   printRandomSixNumber() {
     Console.print(this.randomSixNumberArr);
   }
+
+  inputOfWinningNumber() {
+    let winningNumbers;
+    Console.readLine("당첨 번호를 입력해 주세요.", (winNumber) => {
+      winningNumbers = winNumber.split(",").map(Number);
+    });
+  }
   
 }
 
