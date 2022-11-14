517fe51d8a14c16373f0e75087c716477d8b0782
pangkyu <baesense@naver.com>
2022-11-14 21:45:16 +0900

    Feat : constant 조건 추가

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -4,11 +4,13 @@ const MESSAGE = {
   INPUT_PRIZE: "당첨 번호를 입력해 주세요.",
   INPUT_BONUS: "보너스 번호를 입력해 주세요.",
   PRIZE_RESULT: "당첨 통계",
-  ERR_LOTTO_NUM_LENGHT: "[ERROR] 로또 번호는 6개여야 합니다.",
 };
 
 const CONDITION = {
   BASE_PRICE: 1000,
 };
 
-module.exports = { MESSAGE, CONDITION };
+const ERR_MESSAGE = {
+  ERR_LOTTO_NUM_LENGHT: "[ERROR] 로또 번호는 6개여야 합니다.",
+};
+module.exports = { MESSAGE, CONDITION, ERR_MESSAGE };
