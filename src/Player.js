84b52bc593cb25aca43be183d9638f29be6ed17b
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-14 16:17:50 +0900

    feat: add function to generate a lottery number as much as the input

--- src/Player.js
+++ src/Player.js
@@ -3,9 +3,17 @@ const { REG_EXP } = require("./RegEx");
 const { ERROR } = require("./Error");
 class Player {
   #lottos;
-  constructor() {
-    this.#lottos = [];
+  constructor(money) {
+    this.#lottos = this.makeLottoNumbers(money);
   }
+  makeLottoNumbers = (quantity) => {
+    let lottos = [];
+    this.validateMoney(quantity);
+    for (let line = 0; line < Number(quantity) / 1000; line++) {
+      lottos.push(Random.pickUniqueNumbersInRange(1, 45, 6));
+    }
+    return lottos;
+  };
   validateMoney = (money) => {
     if (!REG_EXP.onlyNumber.test(money)) {
       throw new Error(ERROR.notNumber);
