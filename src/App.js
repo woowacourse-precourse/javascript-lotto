fdfff2a61ae5e6bcf74c4c031d8fa333f22bea47
dustmddus <dustmddus5@gmail.com>
2022-11-14 14:12:07 +0900

    feat: 사용자 로또 번호 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
 const { Random, Console } = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
+const Validation = require("./InputValidate");
 const { LOTTERY_AMOUNT } = require("./constant");
 
 class App {
-  // lottoNumberValidate(lottoNumber) {
-  //   const lotto = new Lotto(lottoNumber);
-  // }
+  constructor() {
+    this.validation = new Validation();
+  }
 
-  // makeLottoNumber() {
-  //   const lottoNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
-  //   this.lottoNumberValidate(lottoNumber);
-  //   console.log(lottoNumber);
-  // }
+  lottoNumberValidate(lottoNumber) {
+    const lotto = new Lotto(lottoNumber);
+  }
 
-  printLottoNumber(lottoAmount) {
+  makeLottoNumber() {
+    const lottoNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
+    this.lottoNumberValidate(lottoNumber);
+    console.log(lottoNumber);
+  }
+
+  printUserNumber(lottoAmount) {
     Console.print(`${lottoAmount}개를 구매했습니다.`);
     for (let time = 0; time < lottoAmount; time += 1) {
       const userNumber = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
         (a, b) => a - b
       );
+      this.validation.userNumberValidate(userNumber);
       Console.print(userNumber);
     }
   }
 
   setLottoAmount(money) {
     const lottoAmount = Number(money / LOTTERY_AMOUNT);
-    this.printLottoNumber(lottoAmount);
-  }
-
-  inputMoneyValidate(money) {
-    if (typeof money !== "number") {
-      throw new Error("[ERROR]금액은 숫자만 입력 가능합니다.");
-    }
-    if (money % 10 !== 0) {
-      throw new Error("[ERROR]금액은 10원 단위로 입력 가능합니다.");
-    }
-    if (money < 1000) {
-      throw new Error(`[ERROR]${LOTTERY_AMOUNT}원 이상부터 구매가 가능합니다.`);
-    }
+    this.printUserNumber(lottoAmount);
   }
 
   inputMoney() {
     Console.readLine("구매 금액을 입력해주세요.", (money) => {
-      this.inputMoneyValidate(Number(money));
+      this.validation.inputMoneyValidate(Number(money));
       this.setLottoAmount(money);
     });
   }
---
+++ src/InputValidate.js
@@ -0,0 +1,29 @@
+class InputValidation {
+  inputMoneyValidate(money) {
+    if (Number.isNaN(money)) {
+      throw new Error("[ERROR] 금액은 숫자만 입력 가능합니다.");
+    }
+    if (money % 10 !== 0) {
+      throw new Error("[ERROR] 금액은 10원 단위로 입력 가능합니다.");
+    }
+    if (money < 1000) {
+      throw new Error(
+        `[ERROR] ${LOTTERY_AMOUNT}원 이상부터 구매가 가능합니다.`
+      );
+    }
+  }
+
+  userNumberValidate(userNumber) {
+    if (userNumber.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    userNumber.forEach((number) => {
+      if (typeof number !== "number")
+        throw new Error("[ERROR] 로또 번호는 숫자로만 구성해야 합니다.");
+      if (number < 1 || number > 45)
+        throw new Error("[ERROR] 로또 번호는 1~45 사이의 정수 입니다.");
+    });
+  }
+}
+
+module.exports = InputValidation;
