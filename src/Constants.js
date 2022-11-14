a993a06dc86012f3d9da44bdcc9818bf87701b7a
daal2quf <daal2quf@gmail.com>
2022-11-15 07:35:17 +0900

    feat(constants): 객체의 key 값으로 value를 카운트하는 과정에서 헷갈리기에 상수를 미리 포장
    add:
    PRIZE_MATCH : 해당 객체의 key와 value값을 이용해 this.resultMap 멤버변수에 접근

---
+++ src/Constants.js
@@ -0,0 +1,14 @@
+const PRIZE_MATCH = {
+  0: 'loseMoney',
+  1: 'loseMoney',
+  2: 'loseMoney',
+  3: 'fifthGrade',
+  4: 'forthGrade',
+  5: 'thirdGrade',
+  6: 'firstGrade',
+  fivePlusBonus: 'secondGrade',
+};
+
+module.exports = {
+  PRIZE_MATCH,
+};
