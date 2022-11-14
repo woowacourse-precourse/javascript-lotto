f2c377fbd8ff911f5c1f96fb2dbd8df558b8b098
khj0426 <706shin1728@naver.com>
2022-11-14 22:18:44 +0900

    refactor:당첨번호별 객체(3개당첨,,4개당첨..) 배열로 변경

--- src/NumberEqaulCount.js
+++ src/NumberEqaulCount.js
@@ -1,12 +1,2 @@
-const MESSAGE = require('./MESSAGE');
-
-const [Three, Four, Five, FiveBonus, Six] = MESSAGE;
-
-const numbereqaulObj = {
-  Three: 0,
-  Four: 0,
-  Five: 0,
-  FiveBonus: 0,
-  Six: 0,
-};
-module.exports = numbereqaulObj;
+const numbereqaulArr = [0, 0, 0, 0, 0, 0, 0, 0];
+module.exports = numbereqaulArr;
