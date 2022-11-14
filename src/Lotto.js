8d0f3846ccf7e96b219d4fd395152271e8860fb9
unqeye <salyuan@naver.com>
2022-11-14 15:24:35 +0900

    feat: 로또 발행 시 숫자 검증

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,12 @@
 const { Console } = require('@woowacourse/mission-utils');
 
+const ERROR_MESSAGE = {
+  NOT_6_NUMBERS: '[ERROR] 로또 번호는 6개여야 합니다.',
+  NOT_A_NUMBER: '[ERROR] 로또 번호는 숫자여야 합니다.',
+  OUT_OF_RANGE: '[ERROR] 로또 번호는 1부터 45사이여야 합니다.',
+  DUPLICATED: '[ERROR] 로또 번호는 중복되지 않아야 합니다.',
+};
+
 class Lotto {
   #numbers;
 
@@ -12,7 +19,13 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+      throw new Error(ERROR_MESSAGE.NOT_6_NUMBERS);
+    } else if (numbers.some((number) => Number.isNaN(number))) {
+      throw new Error(ERROR_MESSAGE.NOT_A_NUMBER);
+    } else if (numbers.some((number) => number < 1 || number > 45)) {
+      throw new Error(ERROR_MESSAGE.OUT_OF_RANGE);
+    } else if (new Set(numbers).size !== 6) {
+      throw new Error(ERROR_MESSAGE.DUPLICATED);
     }
   }
 
