360e14d18a2b7e775a27d227f46bc8e1b79f5d3f
ShimSungBo <shimdb96@gmail.com>
2022-11-14 23:42:52 +0900

    refactor: 로또 상금 관련 정보 상수 추가

--- src/Console.js
+++ src/Console.js
@@ -5,6 +5,7 @@ class Console {
   static NUMBER_OF_LOTTO_PURCHASED = "개를 구매했습니다.";
   static ASK_WIN_NUMBER = "당첨 번호를 입력해주세요.\n";
   static ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.\n";
+  static WINNING_STATUS = "당첨 통계\n---";
 
   static askUserInput(question, callback) {
     MissionUtils.Console.readLine(question, callback);
