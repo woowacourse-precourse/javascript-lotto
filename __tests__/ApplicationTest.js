eefd716f8981f308cc2ebc7d54569c5360da7530
h2002y <h2002y@naver.com>
2022-11-14 23:45:41 +0900

    test: Add lotto prize 3 test in ApplicationTest.js

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -107,6 +107,29 @@ describe("로또 테스트", () => {
     });
   });
 
+  test("기능 테스트 - 로또 3등", () => {
+    mockRandoms([
+      [1, 2, 3, 4, 5, 8],
+    ]);
+    mockQuestions(["1000", "1,2,3,4,5,6", "7"]);
+    const logs = [
+      "1개를 구매했습니다.",
+      "[1, 2, 3, 4, 5, 8]",
+      "3개 일치 (5,000원) - 0개",
+      "4개 일치 (50,000원) - 0개",
+      "5개 일치 (1,500,000원) - 1개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+      "6개 일치 (2,000,000,000원) - 0개",
+      "총 수익률은 150000%입니다.",
+    ];
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
   test("예외 테스트", () => {
     mockQuestions(["1000j"]);
     expect(() => {
