abcc6022019357da59f221cdb18416f80d4699d0
Rki0 <pky00823@gmail.com>
2022-11-14 10:39:13 +0900

    refactor(BonusNumber): modify use of constants
    Since constants are modularized into other files,
    modify how they are used appropriately.

--- src/BonusNumber.js
+++ src/BonusNumber.js
@@ -1,10 +1,4 @@
-const ERROR_DONT_START_ZERO =
-  "[ERROR] 0으로 시작하는 숫자는 입력할 수 없습니다.";
-const ERROR_NOT_ONLY_NUMBER = "[ERROR] 숫자만이 입력 가능합니다.";
-const ERROR_OUT_OF_RANGE =
-  "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
-const ERROR_DUPLICATE_EXIST =
-  "[ERROR] 6개의 당첨 번호로 입력한 숫자는 보너스 번호로 사용 할 수 없습니다.";
+const BonusNumberConst = require("./constant/BonusNumberConst");
 
 class BonusNumber {
   constructor(lotto, bonus) {
@@ -19,7 +13,7 @@ class BonusNumber {
 
   checkBonusNumberStartZero(bonus) {
     if (bonus[0] === "0") {
-      throw new Error(ERROR_DONT_START_ZERO);
+      throw new Error(BonusNumberConst.ERROR_DONT_START_ZERO);
     }
   }
 
@@ -27,19 +21,19 @@ class BonusNumber {
     const regex = /^\d+$/;
 
     if (!regex.test(bonus)) {
-      throw new Error(ERROR_NOT_ONLY_NUMBER);
+      throw new Error(BonusNumberConst.ERROR_NOT_ONLY_NUMBER);
     }
   }
 
   checkNumberRanges(bonus) {
     if (bonus < 1 || bonus > 45) {
-      throw new Error(ERROR_OUT_OF_RANGE);
+      throw new Error(BonusNumberConst.ERROR_OUT_OF_RANGE);
     }
   }
 
   checkDuplicateWithWinningNumber(bonus) {
     if (this.userLotto.includes(bonus)) {
-      throw new Error(ERROR_DUPLICATE_EXIST);
+      throw new Error(BonusNumberConst.ERROR_DUPLICATE_EXIST);
     }
   }
 }
