b30ccd56cab967926b5cbf589326135d2d145763
pp449 <mma7710@naver.com>
2022-11-14 10:12:42 +0900

    feat: 입력받은 로또 번호가 중복된 경우 에러발생

--- src/CheckError.js
+++ src/CheckError.js
@@ -1,7 +1,12 @@
 class CheckError {
   static check(numbers) {
+    const setNumbers = new Set(numbers);
+
     if (numbers.length !== 6)
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+
+    if (setNumbers.size !== 6)
+      throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다.');
 
     numbers.map((number) => {
       if (isNaN(number))
