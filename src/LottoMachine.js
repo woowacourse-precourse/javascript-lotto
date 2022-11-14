1d533de2538975e1a732b7a65c3da4f74bf2c9da
yeongseokim <1903020@ewhain.net>
2022-11-14 18:06:13 +0900

    feat(LottoMachine): 필요한 개수만큼 로또를 생성하여 반환 구현

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -1,5 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-
+const Lotto = require("./Lotto");
 class LottoMachine {
     static createLotto() {
         const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
@@ -14,6 +14,16 @@ class LottoMachine {
             if (a < b) return -1;
         });
         return numberArray;
+    }
+
+    static createLottos(purchaseAmount) {
+        let lottoWallet = [];
+        for (let count = 0; count < purchaseAmount; count++) {
+            const lottoNumbers = this.createLotto();
+            const lotto = new Lotto(lottoNumbers);
+            lottoWallet.push(lotto);
+        }
+        return lottoWallet;
     }
 }
 
