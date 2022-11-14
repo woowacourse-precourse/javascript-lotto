6c7a2a562dacddfda64695b4b4cd79d20960a2ef
thewronghand <penfreak77@gmail.com>
2022-11-14 15:40:31 +0900

    test: setNewLotto 테스트 => purchaseLotto 테스트 명칭 변경
    setNewLotto 메서드는 실제로 사용 시에는 purchaseLotto 메서드 내부에서 동작하므로, purchaseLotto 테스트로 명칭을 변경하고 내용을 수정한다.

--- __tests__/CustomerTest.js
+++ __tests__/CustomerTest.js
@@ -2,12 +2,12 @@ const Customer = require('../src/Customer');
 const Lotto = require('../src/Lotto');
 
 describe('커스터머 클래스 테스트', () => {
-  test('setNewLotto 메서드를 이용해 #lottos 필드에 새 로또를 추가할 수 있다', () => {
+  test('purchaseLotto 메서드를 이용해 #lottos 필드에 새 로또를 추가할 수 있다', () => {
     const customer = new Customer();
     const lotto1 = new Lotto([1, 2, 3, 4, 5, 6]);
     const lotto2 = new Lotto([7, 8, 9, 10, 11, 12]);
-    customer.setNewLotto(lotto1);
-    customer.setNewLotto(lotto2);
+    customer.purchaseLotto(lotto1);
+    customer.purchaseLotto(lotto2);
     expect(customer.list()).toEqual([lotto1, lotto2]);
   });
 });
