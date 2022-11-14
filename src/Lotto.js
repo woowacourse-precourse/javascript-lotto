1ef5f80b5c21f23ce76a0bbaca25d05a490e5fa3
sumin <csumin008255@gmail.com>
2022-11-15 00:54:09 +0900

    Feat: [기능7] 보너스 번호 입력

--- src/Lotto.js
+++ src/Lotto.js
@@ -6,17 +6,26 @@ class Lotto {
   constructor(numbers) {
     this.checkWinNumber(numbers);
     this.#numbers = numbers;
+    this.inputBonusNumber();
   }
 
   checkWinNumber(winNumberArr) {
-    MissionUtils.Console.print(new Set(winNumberArr).size);
     if (new Set(winNumberArr).size != 6)
-      throw new Error("[ERROR]로또 번호 입력 오류1");
+      throw new Error("[ERROR]로또 번호 입력 오류");
     winNumberArr.forEach((number) => {
-      if (!Number(number)) throw new Error("[ERROR]로또 번호 입력 오류2");
+      if (!Number(number)) throw new Error("[ERROR]로또 번호 입력 오류");
       if (number > 45 || number < 1)
-        throw new Error("[ERROR]로또 번호 입력 오류3");
+        throw new Error("[ERROR]로또 번호 입력 오류");
     });
+  }
+
+  inputBonusNumber() {
+    MissionUtils.Console.readLine(
+      "\n보너스 번호를 입력해 주세요.\n",
+      (bonusNumber) => {
+        MissionUtils.Console.print(bonusNumber);
+      }
+    );
   }
 }
 
