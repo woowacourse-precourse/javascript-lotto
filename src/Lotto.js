758763c312e27deaef4c0cd963bbd4b301cfb53a
JaeyeonHong <39540473+HongJaeyeon@users.noreply.github.com>
2022-11-15 02:32:15 +0900

    feat: 사용자의 로또 구입 개수 출력 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -15,8 +15,14 @@ class Lotto {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (input) => {
       if ((input % 1000) !== 0) throw new Error("[ERROR] 올바르지 않은 구입금액입니다.");
       const quantity = input / 1000;
+      this.printQuantity(quantity);
     });
   }
+
+  printQuantity(quantity){
+    MissionUtils.Console.print(`${quantity}개를 구매했습니다.`);
+  }
+
 }
 
 module.exports = Lotto;
