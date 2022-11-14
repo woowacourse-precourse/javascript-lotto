43d83e4b5edbfaa6970a58588bfcba528a352470
ShimSungBo <shimdb96@gmail.com>
2022-11-14 20:58:26 +0900

    test: 문자열로 변환됐는지, 정답 개수가 올바른지 체크하는 테스크 코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,15 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또를 문자열로 변환한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.numberToString()).toEqual("[1, 2, 3, 4, 5, 6]");
+  });
+
+  test("로또 중 몇개가 정답인지 확인한다.", () => {
+    const lotto1 = new Lotto([1, 2, 3, 4, 5, 6]);
+    const lotto2 = new Lotto([7, 8, 9, 10, 11, 12]);
+    expect(lotto1.winningCount([1, 2, 3, 7, 8, 9])).toEqual(3);
+    expect(lotto2.winningCount([7, 8, 9, 10, 11, 12])).toEqual(6);
+  });
 });
