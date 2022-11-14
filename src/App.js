1ccc04c47c9c2bfc1c739834b765a9858e2a0384
vjvl95 <vjvl95@naver.com>
2022-11-15 07:47:44 +0900

    feat : 당첨번호 함수 완성

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 const Lotto = require('./Lotto');
+const BuyLotto = require('./BuyLotto');
+const LottoGenerator = require('./LottoGenerator');
 const MissionUtils = require('@woowacourse/mission-utils');
 class App {
-  play() {
-    const money = this.getMoney();
+  #Lotto;
+  constructor() {
+    this.winningLotto = [];
+    this.myLotto = [];
+    this.lottoCount = 0;
   }
-
-  getMoney() {
-    let money = 0;
-    MissionUtils.Console.readLine('구매금액을 입력해주세요:', (answer) => {
-      money = answer;
+  getBuyLottoMoney() {
+    let buyMoney = 0;
+    const moneyInput = (money) => {
       this.isValidMoney(money);
+      buyMoney = money;
+      this.lottoCount = buyMoney / 1000;
+      const lottoGenerator = new LottoGenerator(buyMoney / 1000);
+      this.myLotto = lottoGenerator.getLottoNumber();
+      this.winningNum();
+    };
+    MissionUtils.Console.readLine(
+      '구매금액을 입력해주세요(1000원 단위로 입력):',
+      moneyInput
+    );
+  }
+  winningNum() {
+    MissionUtils.Console.print(`${this.lottoCount}개를 구매했습니다.`);
+    MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.\n', (winning) => {
+      this.winningLotto = winning.split(',').map(Number);
+      this.#Lotto = new Lotto(this.winningLotto);
+      MissionUtils.Console.print(`${this.winningLotto}입니다.`);
     });
   }
   isValidMoney(money) {
@@ -20,14 +40,16 @@ class App {
       throw `[ERROR] 천원 단위로 금액을 지불해주세요.`;
     }
     if (money > 1000000) {
-      throw `[ERROR] 한 번에 최대로 구입할 수 있는 금액은 100만원 입니다. 입력한 금액: ${money}`;
+      throw `[ERROR] 최대 구입 가능 금액은 100만원 입니다. 다시 시작하세요. 입력한 금액: ${money}`;
     }
     if (money < 1000) {
-      throw `[ERROR] 로또 한장의 가격은 1000원입니다. 입력한 금액: ${money}`;
+      throw `[ERROR] 로또 한장의 가격은 1000원입니다. 1000원 보다 높은 금액을 입력하세요. 입력한 금액: ${money}`;
     }
+  }
+  play() {
+    this.getBuyLottoMoney();
   }
 }
 const app = new App();
 app.play();
-
 module.exports = App;
