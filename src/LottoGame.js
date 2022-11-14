1e9a557ccbb81dc3546ffd5fee0992df3f280b61
adultlee <seoungin1228@gmail.com>
2022-11-15 00:46:32 +0900

    feat: LottoGame 선언
    해당 게임에서는 LottoGame에서 필요한 로직들이 추가될 예정입니다.

---
+++ src/LottoGame.js
@@ -0,0 +1,12 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+class LottoGame {
+  constructor() {
+    this.lottoCount = 0;
+    this.purchasePrice = 0;
+  }
+
+  start() {}
+}
+
+module.exports = LottoGame;
