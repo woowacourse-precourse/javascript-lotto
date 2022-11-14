74c41013b217c43f7445c2b96b7444d325d8e69d
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-14 18:40:01 +0900

    test: 로또 각 등수 당첨 테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -148,4 +148,114 @@ describe("로또 테스트", () => {
       app.play();
     }).toThrow(ERROR_MESSAGE);
   });
+
+  test("로또 1등 당첨 테스트", () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+    ]);
+    mockQuestions(["1000", "8,21,23,41,42,43", "7"]);
+    const logs = [
+      "1개를 구매했습니다.",
+      "[8, 21, 23, 41, 42, 43]",
+      "3개 일치 (5,000원) - 0개",
+      "4개 일치 (50,000원) - 0개",
+      "5개 일치 (1,500,000원) - 0개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+      "6개 일치 (2,000,000,000원) - 1개",
+    ];
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test("로또 2등 당첨 테스트", () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+    ]);
+    mockQuestions(["1000", "7,8,21,23,41,42", "43"]);
+    const logs = [
+      "1개를 구매했습니다.",
+      "[8, 21, 23, 41, 42, 43]",
+      "3개 일치 (5,000원) - 0개",
+      "4개 일치 (50,000원) - 0개",
+      "5개 일치 (1,500,000원) - 0개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+      "6개 일치 (2,000,000,000원) - 0개",
+    ];
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test("로또 3등 당첨 테스트", () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+    ]);
+    mockQuestions(["1000", "7,8,21,23,41,42", "39"]);
+    const logs = [
+      "1개를 구매했습니다.",
+      "[8, 21, 23, 41, 42, 43]",
+      "3개 일치 (5,000원) - 0개",
+      "4개 일치 (50,000원) - 0개",
+      "5개 일치 (1,500,000원) - 1개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+      "6개 일치 (2,000,000,000원) - 0개",
+    ];
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test("로또 4등 당첨 테스트", () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+    ]);
+    mockQuestions(["1000", "1,7,8,21,23,41", "42"]);
+    const logs = [
+      "1개를 구매했습니다.",
+      "[8, 21, 23, 41, 42, 43]",
+      "3개 일치 (5,000원) - 0개",
+      "4개 일치 (50,000원) - 1개",
+      "5개 일치 (1,500,000원) - 0개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+      "6개 일치 (2,000,000,000원) - 0개",
+    ];
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test("로또 5등 당첨 테스트", () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+    ]);
+    mockQuestions(["1000", "1,7,8,20,23,41", "42"]);
+    const logs = [
+      "1개를 구매했습니다.",
+      "[8, 21, 23, 41, 42, 43",
+      "3개 일치 (5,000원) - 1개",
+      "4개 일치 (50,000원) - 0개",
+      "5개 일치 (1,500,000원) - 0개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+      "6개 일치 (2,000,000,000원) - 0개",
+    ];
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
 });
