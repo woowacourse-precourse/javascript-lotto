99e14f639912fbcab5af417d51223bb5497c6180
박승훈 <tmdgns1126@naver.com>
2022-11-14 22:44:07 +0900

    feat: 로또 번호가 숫자형의 나열이 아닌 경우 에러 처리

--- src/Checker.js
+++ src/Checker.js
@@ -3,13 +3,22 @@ const { ERROR_MSG_MAIN_NUMS_COUNT } = require("./constants/error-message");
 class Checker {
   validateMainNums(input) {
     const mainNumStrs = input.split(",");
-    checkSix(mainNumStrs);
+    this.checkSix(mainNumStrs);
+    this.checkAllIsNumber(mainNumStrs);
   }
 
   checkSix(mainNumStrs) {
-    if (mainNumStrs.length !== 6) {
-      throw new Error(ERROR_MSG_MAIN_NUMS_COUNT);
-    }
+    if (mainNumStrs.length === 6) return;
+    throw new Error(ERROR_MSG_MAIN_NUMS_COUNT);
+  }
+
+  checkAllIsNumber(mainNumStrs) {
+    mainNumStrs.forEach((str) => this.checkIsNumber(str));
+  }
+
+  checkIsNumber(str) {
+    if (!isNaN(str)) return;
+    throw new Error(ERROR_MSG_MAIN_NUMS_COUNT);
   }
 }
 
--- src/constants/error-message.js
+++ src/constants/error-message.js
@@ -1,19 +1,22 @@
 const ERROR_MSG_PREFIX = "[ERROR] ";
+const ERROR_MSG_MAIN_NUMS_COUNT =
+  ERROR_MSG_PREFIX + "로또 번호의 개수는 6개여야 합니다.";
+const ERROR_MSG_NUMS_NAN =
+  ERROR_MSG_PREFIX + "로또 번호는 숫자형이어야 합니다.";
 const ERROR_MSG_NUM_RANGE =
   ERROR_MSG_PREFIX + "로또 번호는 1 이상 45 이하의 숫자여야 합니다.";
 const ERROR_MSG_NUMS_DUPLICATED =
   ERROR_MSG_PREFIX + "로또 번호의 숫자가 중복되지 않아야 합니다.";
-const ERROR_MSG_MAIN_NUMS_COUNT =
-  ERROR_MSG_PREFIX + "로또 번호의 개수는 6개여야 합니다.";
 const ERROR_MSG_BONUS_NUM_COUNT =
   ERROR_MSG_PREFIX + "보너스 번호의 개수는 1개여야 합니다.";
 const ERROR_MSG_THOUSAND_UNIT =
   ERROR_MSG_PREFIX + "로또 구입 금액은 1,000원으로 나누어 떨어져야 합니다.";
 
 module.exports = {
+  ERROR_MSG_MAIN_NUMS_COUNT,
+  ERROR_MSG_NUMS_NAN,
   ERROR_MSG_NUM_RANGE,
   ERROR_MSG_NUMS_DUPLICATED,
-  ERROR_MSG_MAIN_NUMS_COUNT,
   ERROR_MSG_BONUS_NUM_COUNT,
   ERROR_MSG_THOUSAND_UNIT,
 };
