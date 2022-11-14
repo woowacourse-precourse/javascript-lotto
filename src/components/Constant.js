c8bdd53d422d3cba7baaf3e65ef2b80a35421698
Inae Choi <inaechoi89@gmail.com>
2022-11-14 10:27:24 +0900

    feat: add some constants

--- src/components/Constant.js
+++ src/components/Constant.js
@@ -1,5 +1,9 @@
 const Const = {
   ORDER_MESSAGE: "구입금액을 입력해 주세요.",
+  UNIT_ERROR: "[ERROR] 1000원 단위로 입력해야 합니다.",
+  QUANTITY_MESSAGE: "개를 구매했습니다.",
+  WINNING_NUMBER_MESSAGE: "당첨 번호를 입력해 주세요.",
+  BONUS_NUMBER_MESSAGE: "보너스 번호를 입력해 주세요.",
 };
 
 module.exports = Const;
