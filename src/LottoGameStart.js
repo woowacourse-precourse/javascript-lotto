fa205af9e49c48101e7813464d1920e02db50db5
jinhobyeong <jhb_0921@naver.com>
2022-11-14 23:48:16 +0900

    feat : 당첨 통계 출력 함수 추가

--- src/LottoGameStart.js
+++ src/LottoGameStart.js
@@ -47,6 +47,22 @@ class LottoGameStart {
       });
     });
   }
+
+  printResultStatic(scoreBoard) {
+    Console.print("당첨 통계");
+    Console.print("---");
+    Console.print(`3개 일치 (5,000원) - ${scoreBoard[4]}개`);
+    Console.print(`4개 일치 (50,000원) - ${scoreBoard[3]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${scoreBoard[2]}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${scoreBoard[1]}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${scoreBoard[0]}개`);
+    Console.print(
+      `총 수익률은 ${estimateProfit(scoreBoard, this.#inputMoney)}%입니다.`
+    );
+    Console.close();
+  }
 }
 
 module.exports = LottoGameStart;
