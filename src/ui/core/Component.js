e6eec2395e0bd34ebc12b364b1023a147325b5a5
turtle601 <turtle601@naver.com>
2022-11-14 17:02:14 +0900

    refactor(Component) render함수 추가
    Explained:
    - 하위 클래스의 print()함수를 실행

--- src/ui/core/Component.js
+++ src/ui/core/Component.js
@@ -1,6 +1,16 @@
 class Component {
   #ERRORMESSAGE = '[ERROR] YOU SHOULD DECLARE OVERIDING';
 
+  #componentInstance;
+
+  constructor(componentInstance) {
+    this.#componentInstance = componentInstance;
+  }
+
+  render() {
+    this.#componentInstance.print();
+  }
+
   print() {
     throw new Error(this.#ERRORMESSAGE);
   }
