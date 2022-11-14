c82e0ed87a914b7db4134698ab7ebe8855f6e3ce
solo5star <solo_5@naver.com>
2022-11-15 08:04:45 +0900

    feat(lotto-buy): 금액에 맞게 로또를 구매하여 저장하도록 구현

--- src/domains/User.js
+++ src/domains/User.js
@@ -11,6 +11,12 @@ class User {
     if (money % Lotto.PRICE !== 0) {
       throw new Error('[ERROR] 로또를 구입한 후 남는 금액이 없어야 합니다.');
     }
+    const amount = money / Lotto.PRICE;
+    this.#lottos.concat(
+      Array(amount)
+        .fill()
+        .map(() => Lotto.fromRandom()),
+    );
   }
 }
 
