fef8064e44ba5fa6ae4e2117262cc34c2defdaf8
pinkishincoloragain <smb1103@gmail.com>
2022-11-15 01:21:12 +0900

    feat: App 클래스에 Input 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+const { getLottoBudget, getTargetNumber, getBonusNumber } = require('./inputUtils');
 class App {
-  play() {}
+  play() {
+    this.getLottoBudget();
+  }
+
+  getLottoBudget() {
+    getLottoBudget((budget) => {
+      this.budget = budget;
+      this.getTargetNumber();
+    });
+  }
+
+  getTargetNumber() {
+    getTargetNumber((target) => {
+      this.target = target;
+      this.getBonusNumber();
+    });
+  }
+
+  getBonusNumber() {
+    getBonusNumber((bonus) => {
+      this.bonus = bonus;
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
