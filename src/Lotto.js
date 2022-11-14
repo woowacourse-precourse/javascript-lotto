f329678531d28550e319a17ed5ef094caa3c8b08
김아현 <dhflxhdxhd@gmail.com>
2022-11-15 01:00:48 +0900

    Feat : 순위 계산 함수 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -65,7 +65,19 @@ class Lotto {
     })
   }
 
+  compareNumbers(mylottoArray){
+    let count = 0;
+    mylottoArray.forEach((number) => {
+      if(this.#numbers.includes(number)){
+        count += 1;
+      }
+    }) 
+    this.rankScore(count,mylottoArray);
+  }
 
+  rankScore(){
+    
+  }
   // validate(numbers) {
   //   if (numbers.length !== 6) {
   //     throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
