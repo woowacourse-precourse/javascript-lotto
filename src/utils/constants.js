97671236bfc73738c120cc1d6c4b95dd73e53f02
nalsae <nalsae0207@naver.com>
2022-11-14 10:03:53 +0900

    feat: add a constant about regular expression
    쉼표를 추가하는 정규 표현식 상수화

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,7 +8,7 @@ const RANGE_OF_LOTTO_NUMBER = {
 const TOTAL_COUNTS = 6;
 
 const ERROR_MESSAGES = {
-  PRICE: '[ERROR] 구입 금액은 1000원 단위의 숫자로 입력해야 합니다.',
+  PRICE: "[ERROR] 구입 금액은 1000원 단위의 숫자로 입력해야 합니다.",
   WINNING_NUMS: `[ERROR] 당첨 번호는 중복되지 않는 1 ~ 45 사이의 숫자 6개를 ','로 구분하여 입력해야 합니다.`,
   BOUNS_NUM: `[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 1 ~ 45 사이의 숫자 1개를 입력해야 합니다.`,
 };
@@ -18,7 +18,7 @@ const INITIAL_STATICS = {
   4: 0,
   5: 0,
   6: 0,
-  '5andBonus': 0,
+  "5andBonus": 0,
 };
 
 const WINNING_PRICES = {
@@ -26,8 +26,10 @@ const WINNING_PRICES = {
   4: 50000,
   5: 1500000,
   6: 2000000000,
-  '5andBonus': 30000000,
+  "5andBonus": 30000000,
 };
+
+const ADD_COMMA_EXP = /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g;
 
 module.exports = {
   LOTTO_PRICE,
@@ -36,4 +38,5 @@ module.exports = {
   ERROR_MESSAGES,
   INITIAL_STATICS,
   WINNING_PRICES,
+  ADD_COMMA_EXP,
 };
