8a1ad750f362775a149d3b6741246b75f724fa83
ho-ji <wkdd0205@gmail.com>
2022-11-15 00:44:45 +0900

    feat: Add createIssuedLottoList()
    구매한 수량에 맞춰 로또를 발행하고, 로또 번호를 오름차순으로 정렬하여 출력함

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+const Lotto = require("../src/Lotto");
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   #numberOfLottos;
+  #issuedLottosList= new Array();
   play() {
     this.inputMoneyToBuyLottos();
     this.printNumberOfLottos();
+    this.createIssuedLottoList();
   }
   inputMoneyToBuyLottos() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (moneyToBuyLottos) => {
@@ -15,6 +18,15 @@ class App {
   }
   printNumberOfLottos() {
     MissionUtils.Console.print(`${this.#numberOfLottos}개를 구매했습니다.`);
+  }
+  createIssuedLottoList(){
+    for(let i=0; i<this.#numberOfLottos; i++){
+      const tempRandomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      tempRandomNumbers.sort((a,b)=>a-b);
+      const issuedLottoNumbers = new Lotto(tempRandomNumbers);
+      MissionUtils.Console.print(`[${tempRandomNumbers.join(', ')}]`);
+      this.#issuedLottosList.push(issuedLottoNumbers);
+    }
   }
 }
 
