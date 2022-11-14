625af5c84e016871e419c7b3c2a74c8b797efefc
random-olive <104327907+random-olive@users.noreply.github.com>
2022-11-14 18:31:58 +0900

    feat(validation): add error messages for numbers

--- src/Validation.js
+++ src/Validation.js
@@ -1,3 +1,5 @@
+const { ERROR_INPUT_MESSAGE } = require("./constants");
+
 class Validation {
   amountType(value) {
     if (typeof value === "number") {
@@ -15,50 +17,47 @@ class Validation {
 
   type(numbers) {
     if (numbers.filter((el) => typeof el !== "number").length !== 0) {
-      throw new Error();
+      throw new Error(ERROR_INPUT_MESSAGE.TYPE);
     }
   }
 
   range(numbers) {
     if (numbers.filter((el) => el < 1 || el > 45).length !== 0) {
-      throw new Error();
+      throw new Error(ERROR_INPUT_MESSAGE.RANGE);
     }
   }
 
   lengthOne(numbers) {
     if (numbers.length !== 1) {
-      throw new Error();
+      throw new Error(ERROR_INPUT_MESSAGE.LENGTH_ONE);
     }
   }
 
   lengthSix(numbers) {
     if (numbers.length !== 6) {
-      throw new Error();
+      throw new Error(ERROR_INPUT_MESSAGE.LENGTH_SIX);
     }
   }
 
   duplication(numbers) {
     if (new Set(numbers) !== 6) {
-      throw new Error();
+      throw new Error(ERROR_INPUT_MESSAGE.DUPLICATION);
     }
   }
 
   formatArray(numbers) {
     if (!Array.isArray(numbers)) {
-      throw new Error();
+      throw new Error(ERROR_INPUT_MESSAGE.FORMAT_ARRAY);
     }
   }
 
   //string : '1,2,3,4,5,6'
 
   formatString(string) {
-    if (typeof string !== "string") {
-      throw new Error();
-    }
     if (typeof string === "string") {
       const resolved = string.split(",").map((el) => Number(el));
       // if (resolved 검증 실패){
-      //   throw new Error()
+      //   throw new Error(ERROR_INPUT_MESSAGE.FORMAT_STRING)
       // }
     }
   }
