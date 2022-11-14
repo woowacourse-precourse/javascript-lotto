1be89c7796852a9d910686d6d677fe00a189025f
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-14 12:43:51 +0900

    fix: 보너스번호 유효성 검사 메서드 수정
    isUniqueBonusNumber 메서드의 자료형 버그 수정

--- __tests__/UserInputValidationTest.js
+++ __tests__/UserInputValidationTest.js
@@ -126,7 +126,7 @@ describe("Validation.isUniqueBonusNumber", () => {
   test("보너스번호가 당첨번호와 중복되면, false를 반환해야 한다.", () => {
     // given
     const bonusNumber = "6";
-    const winningNumbers = ["1", "2", "3", "4", "5", "6"];
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
     // when
     const result = Validation.isUniqueBonusNumber(bonusNumber, winningNumbers);
     // then
