eb6ef2d3b6eecceae43eac2329c98adb85937d97
yuna Kim <yuna000804@gmail.com>
2022-11-14 17:03:20 +0900

    feat : 당첨 번호 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const Lotto = require('./Lotto');
+const { Random, Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    Console.readLine("당첨 번호를 입력해 주세요.\n", this.letWinningNumbers);
+  }
+
+  letWinningNumbers = (winningNumbers) => {
+    let winningList = winningNumbers.split(',').map(num => parseInt(num));
+    console.log(winningList);
+    const startLotto = new Lotto(winningList);
+    startLotto;
+  }
 }
+const a = new App;
+a.play();
 
 module.exports = App;
