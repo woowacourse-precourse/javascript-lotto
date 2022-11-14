ef60d1a0b8b2d8f2129ea330794a62a10267ef5a
sunny <huiseon37@gmail.com>
2022-11-14 22:55:54 +0900

    feat: 로또 한개 발행 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-class LottoManage{
-  inputPurchaseMoney(){
+class LottoMachine {
+  #lottoList;
+
+  inputPurchaseMoney() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n",(inputMoney)=>{
     })
+  }
+
+  makeLotto() {
+    const lotto = new Set();
+    while(lotto.size < 6){
+      lotto.add(MissionUtils.Random.pickNumberInRange(1, 45));
+    }
+    return [...lotto]
   }
 }
 
 class App {
-  constructor(){
+  constructor() {
     this.lottomanage = new LottoManage();
   }
+
   play() {
     this.lottomanage.inputPurchaseMoney();
   }
