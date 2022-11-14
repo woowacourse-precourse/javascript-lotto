e8912b87ef5e1fa038ccbfa0ab48d911b761c3ba
동동이 <drdragon777@naver.com>
2022-11-15 01:03:32 +0900

    feat - add isValidBounusRange validator method

--- src/validator/Validator.js
+++ src/validator/Validator.js
@@ -2,14 +2,19 @@ const InValidInputError = require("../error/InValidInputError");
 const { BASIC, INVALID_INPUT } = require("./validator.constants");
 
 class Validator {
-  static isInvalidLottoType(string) {
+  static isValidLottoType(string) {
     if (/^[0-9]+$/.test(string)) return true;
     throw new InValidInputError(BASIC + INVALID_INPUT.LOTTO_NOT_NUMBER);
   }
 
-  static isRange(numberArr, [min, max]) {
+  static isValidBonusRange(num, [min = 1, max = 45]) {
+    if (num >= min && num <= max) return true;
+    throw new InValidInputError(BASIC + INVALID_INPUT.BOUNUS_RANGE_ERROR);
+  }
+
+  static isValidLottoRange(numberArr, [min = 1, max = 45]) {
     if (numberArr.every((num) => num >= min && num <= max)) return true;
-    throw new InValidInputError(BASIC + INVALID_INPUT.RANGE_ERROR);
+    throw new InValidInputError(BASIC + INVALID_INPUT.LOTTO_RANGE_ERROR);
   }
 
   static isDuplicated(string) {
--- src/validator/validator.constants.js
+++ src/validator/validator.constants.js
@@ -1,7 +1,8 @@
 module.exports = {
   BASIC: "[ERROR] ",
   INVALID_INPUT: {
-    RANGE_ERROR: "로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+    LOTTO_RANGE_ERROR: "로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+    BOUNUS_RANGE_ERROR: "보너스 번호는 1부터 45 사이의 숫자여야 합니다.",
     LOTTO_DUPLICATED: "로또 번호는 중복되지 않는 6개의 숫자여야 합니다.",
     LOTTO_NOT_NUMBER: "로또 번호는 숫자만 입력할 수 있습니다.",
     PRICE_UNIT: "구입 금액은 1,000원 단위로 입력해주세요.",
