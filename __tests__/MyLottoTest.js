5dbd7bef0d62b06e6de36787d711e99be3db4dc5
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-15 08:56:21 +0900

    style : 테스트명 수정

--- __tests__/MyLottoTest.js
+++ __tests__/MyLottoTest.js
@@ -1,7 +1,7 @@
 const MyLotto = require("../src/MyLotto");
 
 describe("마이 로또 클래스 테스트", () => {
-    test("로또 구입 금액이 1000원 단위가 아니면 예외가 발생한다.", () => {
+    test("로또 구입 금액이 1000원 단위가 아닌 경우 예외 발생", () => {
         expect(() => {
             new MyLotto(1100);
         }).toThrow("[ERROR] 로또 구입 금액은 1000원 단위로 입력 가능합니다.");
