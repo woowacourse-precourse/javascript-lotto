58594d1c228ede7616d5a201734d5f7e2fcb3a53
hwnim5324 <hwnim5324@naver.com>
2022-11-14 23:52:46 +0900

    refactor: 사용자 입력 관련 기능 개선. * 필요없는 필드 및 생성자, getter 제거. * set과 동시에 값 반환을 통해 코드 개선.

--- src/User.js
+++ src/User.js
@@ -1,59 +1,61 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class User {
-    #money;
-    #numbers;
-    #bonus;
-
     constructor() { }
 
     isValidMoney(answer){
         if(answer<0 || answer%1000!==0){
             throw new Error("[ERROR] 구입금액은 1,000원 단위로만 입력해야 합니다.");
         }
-        return true;
     }
 
     isValidNumber(answer){
         if(answer<1 || answer>45) {
             throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
-        return true;
     }
 
     isValidNumbers(answer){
         answer.forEach((number)=>{
             this.isValidNumber(Number(number));
         });
-        return true;
     }
 
     isValidLength(answer){
         if(answer.length!==6) {
             throw new Error("[ERROR] 로또 번호는 6자리로 쉼표(,)로 구분해 입력해야 합니다.");
         }
-        return true;
     }
 
-    getMoney(){
+    setMoney(){
+        let money;
         MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (answer) => {
             const MONEY = Number(answer);
-            if(this.isValidMoney(MONEY)) this.#money=MONEY;
+            this.isValidMoney(MONEY);
+            money=MONEY;
         });
+        return money;
     }
 
-    getNumbers(){
+    setNumbers(){
+        let numbers;
         MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.\n", (answer) => {
             const NUMBERS = answer.split(',').map((number)=>Number(number));
-            if(this.isValidLength(NUMBERS) && this.isValidMoney(NUMBERS)) this.#numbers=NUMBERS;
+            this.isValidLength(NUMBERS);
+            this.isValidNumbers(NUMBERS);
+            numbers=NUMBERS;
         });
+        return numbers;
     }
 
-    getBonus(){
+    setBonus(){
+        let bonus;
         MissionUtils.Console.readLine("보너스 번호를 입력해 주세요.\n", (answer) => {
             const BONUS = Number(answer);
-            if(this.isValidNumber(BONUS)) this.#bonus=BONUS;
+            this.isValidNumber(BONUS)
+            bonus=BONUS;
         });
+        return bonus;
     }
 }
 
