213859080b2452ef4d10acd97592371a5448ce77
kitez <duswl339@ajou.ac.kr>
2022-11-14 18:02:13 +0900

    feat: lotto 번호 길이&구성 번호 validation check
    + 로또 구성 번호
    + 로또 길이
    + validation check

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,34 @@
+const utils = require('./utils');
+const Constant = require('./Constant');
+
 class Lotto {
   #numbers;
+  #same;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.#validateLength(numbers);
+    this.#validateNumber(numbers);
     this.#numbers = numbers;
-  }
-
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    this.#same = 0;
   }
 
   // TODO: 추가 기능 구현
+  /**
+   * 입력된 lotto 번호의 길이가 6이 아닌 경우 체크
+   */
+  #validateLength(numbers) {
+    if (numbers.length !== Constant.LOTTO_LENGTH) {
+      throw new Error("[ERROR] 로또 번호는 "+Constant.LOTTO_LENGTH+"개여야 합니다.");
+    }
+  }
+
+  #validateNumber(numbers) {
+      for(let i = 0 ; i < Constant.LOTTO_LENGTH ; i++){
+        if(numbers[i] <= 0 || numbers[i] >= 46){
+          throw new Error("[ERROR] 로또 번호는 1~45사이어야 합니다.");
+        }
+      }
+  }
 }
 
 module.exports = Lotto;
