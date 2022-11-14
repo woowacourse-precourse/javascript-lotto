95d85d0c534fa58aeb612d1462c66653f9a0ef2c
lee-il-woo <empty001430@gmail.com>
2022-11-14 23:23:40 +0900

    Test: 예외사항 테스트코드 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,14 @@
-const Lotto = require("../src/Lotto");
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -8,9 +18,67 @@ describe("로또 클래스 테스트", () => {
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test("금액 숫자 입력 테스트", () => {
+    mockQuestions(["1000j"]);
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR]");
+  });
+
+  test("금액 단위 테스트", () => {
+    mockQuestions(["14030"]);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR]");
+  });
+
+  test("입력값 개수 테스트", () => {
+    mockQuestions(["8000","1,2,3,4,5,6,7"]);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR]");
+  });
+
+  test("입력값 개수 테스트", () => {
+    mockQuestions(["8000","1,2,3,4,5,6,7"]);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR]");
+  });
+
+  test("입력값 개수 테스트", () => {
+    mockQuestions(["8000","1,2,3,4,5,6,7"]);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR]");
+  });
+
+  test("입력값 중복 테스트", () => {
+    mockQuestions(["8000","1,2,3,5,5,6"]);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR]");
+  });
+
+  test("입력값 범위 테스트", () => {
+    mockQuestions(["8000","1,2,3,4,5,46"]);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR]");
+  });
+
+  test("당첨번호와 보너스번호 중복 테스트", () => {
+    mockQuestions(["8000","1,2,3,4,5,6","6"]);
+    expect(() => {
+      const app = new App();
+      app.play();
     }).toThrow("[ERROR]");
   });
 
