064db668c037ffe8e1af4f1f1c8b979ccd00003f
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-14 23:23:04 +0900

    test: 구입 금액 예외처리

--- __tests__/StoreTest.js
+++ __tests__/StoreTest.js
@@ -13,4 +13,37 @@ describe("Store 클래스 테스트", () => {
       store.candidates.map((candidate) => candidate.numbers)
     ).toContainEqual([1, 2, 3, 4, 5, 6]);
   });
+
+  test("1개의 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      store.validatePrice("");
+    }).toThrow("[ERROR] 숫자를 입력해 주세요.");
+    expect(() => {
+      store.validatePrice("1,2");
+    }).toThrow("[ERROR] 숫자를 입력해 주세요.");
+    expect(() => {
+      store.validatePrice("%");
+    }).toThrow("[ERROR] 숫자를 입력해 주세요.");
+  });
+
+  test("1000원 단위로 입력되지 않으면 예외가 발생한다.", () => {
+    expect(() => {
+      store.validatePrice(1500);
+    }).toThrow("[ERROR] 1000원 단위로 입력해 주세요.");
+    expect(() => {
+      store.validatePrice(1);
+    }).toThrow("[ERROR] 1000원 단위로 입력해 주세요.");
+    expect(() => {
+      store.validatePrice(1000.5);
+    }).toThrow("[ERROR] 1000원 단위로 입력해 주세요.");
+  });
+
+  test("0 또는 음수가 입력되면 예외가 발생한다.", () => {
+    expect(() => {
+      store.validatePrice(-1000);
+    }).toThrow("[ERROR] 양의 정수를 입력해 주세요.");
+    expect(() => {
+      store.validatePrice(0);
+    }).toThrow("[ERROR] 양의 정수를 입력해 주세요.");
+  });
 });
