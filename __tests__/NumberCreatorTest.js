c7d1bafb9ed8251c71041d9a63fb7d999aecd972
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-14 12:16:58 +0900

    test: 6개의 랜덤 숫자 생성 확인하는 테스트 코드 추가

---
+++ __tests__/NumberCreatorTest.js
@@ -0,0 +1,15 @@
+const NumberCreator = require('../src/NumberCreator');
+
+describe('NumberCreator 테스트', () => {
+  test('기능 테스트: 6개의 랜덤 숫자 생성하기', () => {
+    const numberCreator = new NumberCreator();
+    let numbers = numberCreator.getRandomSixNumbers();
+
+    expect(numbers).toHaveLength(6);
+    numbers.every((number) => {
+      expect(number).toBeGreaterThanOrEqual(1);
+      expect(number).toBeLessThanOrEqual(45);
+      expect(number).not.toBeNaN();
+    });
+  });
+});
