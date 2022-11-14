4c9aab7f9cd7ea4596970d06b2ebc62fa41f28b3
Wang HoEun <hoeunwang@gmail.com>
2022-11-14 11:19:43 +0900

    feat: 로또 번호 예외 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+const { Console } = require("@woowacourse/mission-utils");
 class Lotto {
   #numbers;
 
@@ -10,6 +11,25 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (!this.checkType(numbers))
+      throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+    if ([...new Set(numbers)].length !== 6)
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    if (!this.checkRange(numbers))
+      throw new Error("[ERROR] 로또 번호의 범위는 1~45이어야 합니다.");
+  }
+
+  checkType(numbers) {
+    return numbers.every((number) => !isNaN(number));
+  }
+  checkRange(numbers) {
+    return numbers.every((number) => number <= 45 && number >= 1);
+  }
+
+  printNumbers() {
+    this.sortNumbers();
+
+    Console.print(`[${this.#numbers.join(", ")}]`);
   }
 
   // TODO: 추가 기능 구현
