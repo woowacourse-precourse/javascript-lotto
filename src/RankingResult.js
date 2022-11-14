746d905c93f75cd2c60647afb7e16d2b1e9b80dc
kimeuns <dmstn2006@gmail.com>
2022-11-14 22:35:00 +0900

    refactor: 당첨 순위 관련 객체 생성

---
+++ src/RankingResult.js
@@ -0,0 +1,63 @@
+const {RANKING} = require("./Constants");
+const { getMatchedinWinningNumberCount, hasBounsNumber } = require("./utils");
+
+class RankingResult {
+  rankingResult;
+
+  constructor() {
+    this.rankingResult = [
+      {
+        ranking: "FIFTH",
+        reward: RANKING.REWARD_FIFTH,
+        mathcedCount: RANKING.MATCHEDCOUNT_FIFTH,
+        hasBounsNumber: false,
+        amount: 0,
+      },
+      {
+        ranking: "FOURTH",
+        reward: RANKING.REWARD_FOURTH,
+        mathcedCount: RANKING.MATCHEDCOUNT_FOURTH,
+        hasBounsNumber: false,
+        amount: 0,
+      },
+      {
+        ranking: "THIRD",
+        reward: RANKING.REWARD_THIRD,
+        mathcedCount: RANKING.MATCHEDCOUNT_THRID,
+        hasBounsNumber: false,
+        amount: 0,
+      },
+      {
+        ranking: "SECOND",
+        reward: RANKING.REWARD_SECOND,
+        mathcedCount: RANKING.MATCHEDCOUNT_SECOND,
+        hasBounsNumber: true,
+        amount: 0,
+      },
+      {
+        ranking: "FIRST",
+        reward: RANKING.REWARD_FIRST,
+        mathcedCount: RANKING.MATCHEDCOUNT_FIRST,
+        hasBounsNumber: false,
+        amount: 0,
+      },
+    ];
+  }
+
+  setRankingResult(issuedLotto, winniglotto, bonusLotto) {
+    issuedLotto.forEach((lotto) => {
+      const matchCount = getMatchedinWinningNumberCount(lotto, winniglotto);
+      const ranking = this.rankingResult.find(
+        (rank) =>
+          rank.mathcedCount == matchCount &&
+          rank.hasBounsNumber == hasBounsNumber(lotto, bonusLotto)
+      );
+      if (ranking) {
+        ranking.amount += 1;
+      }
+    });
+    return this.rankingResult
+  }
+}
+
+module.exports = RankingResult;
