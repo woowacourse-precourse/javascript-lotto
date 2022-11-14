1e9e7026190bf5639f6ad7559b840024070a651d
2yunseong <dbsdltjd3701@naver.com>
2022-11-14 15:55:38 +0900

    feat(user): 모든 로또가 몇 등인지 카운트 하는 로직 작성

--- src/User.js
+++ src/User.js
@@ -1,3 +1,4 @@
+const rank = require('./util/rank');
 const util = require('./util/util');
 
 class User {
@@ -5,10 +6,58 @@ class User {
     this.amount = 0;
     this.lottos = [];
     this.lottoCount = 0;
+    this.prize = 0;
+    this.hitRanks = [0, 0, 0, 0, 0, 0];
+    this.returnOfInvestment = 0;
   }
 
   calculateLottoCount() {
     this.lottoCount = this.amount / util.AMOUNT_PER_GAME;
+  }
+
+  calculateHitLottoCount(hitLotto, bonusNumber) {
+    const hitLottoNumbers = hitLotto.getNumbers();
+    this.lottos.forEach((lotto) => {
+      const lottoNumbers = lotto.getNumbers();
+      const count = calcHitNumberCount(lottoNumbers, hitLottoNumbers);
+      const isBonus = hitLottoNumbers.includes(bonusNumber);
+      const rank = this.dicideRank(count, isBonus);
+      this.hitRanks[rank] += 1;
+    });
+    return;
+  }
+
+  calcHitNumberCount(lottoNumbers, hitLottoNumbers) {
+    let count = 0;
+    lottoNumbers.forEach((lottoNumber) => {
+      if (hitLottoNumbers.includes(lottoNumber)) {
+        count++;
+      }
+    });
+    return count;
+  }
+
+  dicideRank(count, isBonus) {
+    if (count === 6) {
+      return rank.FIRST;
+    }
+    if (count === 5 && isBonus) {
+      return rank.SECOND;
+    }
+    if (count === 5) {
+      return rank.THIRD;
+    }
+    if (count === 4) {
+      return rank.FOURTH;
+    }
+    if (count == 3) {
+      return rank.FIFTH;
+    }
+    return rank.LOST;
+  }
+
+  calculateStat() {
+    this.calculateLottoCount();
   }
 }
 
