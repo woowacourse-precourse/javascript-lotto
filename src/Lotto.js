d48fa6cd232753caaae587c400e8aa2b62a325b1
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-14 15:03:46 +0900

    Feat: Lotto 예외사항, 중복되지 않는 6개의 숫자인지 확인한다. 로또 번호의 숫자 범위를 확인한다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,12 +7,39 @@ class Lotto {
   }
 
   validate(numbers) {
+    isNum(numbers);
+    isSixNumbers(numbers);
+    isNumbersInRange(numbers);
+    isDuplicated(numbers);
+  }
+
+  isNum(numbers) {
+    const validNums = /[^0-9]/;
+    if (validNums.test(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+    }
+  }
+
+  isSixNumbers(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  isNumbersInRange(numbers) {
+    numbers.foreach((num) => {
+      if (num < 1 || num > 45) {
+        throw new Error("[ERROR] 로또 번호 6개는 (1 ~ 45) 범위 내의 숫자여야 합니다.");
+      }
+    })
+  }
+
+  isDuplicated(numbers) {
+    const duplicateCheck = new Set(numbers);
+    if (duplicateCheck.size !== 6) {
+      throw new Error("[ERROR] 로또 번호 6개는 중복되지 않아야 합니다.");
+    }
+  }
 }
 
 module.exports = Lotto;
