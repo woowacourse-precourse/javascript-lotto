b2eefe3ee52f514d7577657396813aa5ba5d8759
NaveOWO <pianowell96@gmail.com>
2022-11-15 05:18:25 +0900

    [ feat ] 당첨번호 입력 예외사항 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,9 +9,34 @@ class Lotto {
   }
 
   validate(numbers) {
+    this.validateLength(numbers);
+    this.validateIsNumber(numbers);
+    this.validateRange(numbers);
+    this.validateRepeat(numbers);
+  }
+
+  validateLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
+
+  validateIsNumber(numbers) {
+    numbers.forEach((number) => {
+      if (isNaN(number)) throw new Error("[ERROR] 숫자를 입력해주세요.");
+    });
+  }
+
+  validateRange(numbers) {
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45)
+        throw new Error("[ERROR] 1부터 45 사이의 숫자를 입력해주세요.");
+    });
+  }
+
+  validateRepeat(numbers) {
+    if (numbers.length !== new Set(numbers).size)
+      throw new Error("[ERROR] 서로 다른 숫자를 입력해주세요.");
   }
 
   getLottoNumber() {
