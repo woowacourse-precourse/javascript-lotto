9a67bd58faed5e253cea45835073daeb10e9564f
Sunghyun627 <kksscc4@gmail.com>
2022-11-14 15:29:50 +0900

    feat: 로또 구입 금액 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+const messages = require('./constants/constants.js');
 class App {
-  play() {}
+  play() {
+    this.getPurchaseAmount();
+  }
+  getPurchaseAmount() {
+    Console.readLine(messages.GUIDE.ENTER_LOTTO_PURCHASE_AMOUNT, (purchaseAmountInput) => {
+      if (this.isValidPurchaseAmount(purchaseAmountInput))
+        this.purchaseAmount = Number(purchaseAmountInput);
+    });
+  }
+
+  isValidPurchaseAmount(purchaseAmountInput) {
+    return (
+      this.isDigit(purchaseAmountInput) &&
+      this.isFirstDigitNotZero(purchaseAmountInput) &&
+      this.isDivisibleByThousand(purchaseAmountInput)
+    );
+  }
+
+  isDigit(purchaseAmountInput) {
+    const possibleNumbers = Random.pickUniqueNumbersInRange(0, 9, 10).map((number) =>
+      String(number)
+    );
+    if (purchaseAmountInput.split('').every((digit) => possibleNumbers.includes(digit)))
+      return true;
+    throw new Error(messages.ERROR.INCLUDE_NOT_NUMBER_ERROR);
+  }
+
+  isFirstDigitNotZero(purchaseAmountInput) {
+    if (purchaseAmountInput[0] !== '0') return true;
+    throw new Error(messages.ERROR.START_WITH_ZERO_ERROR);
+  }
+
+  isDivisibleByThousand(purchaseAmountInput) {
+    if (Number(purchaseAmountInput) % 1000 === 0) return true;
+    throw new Error(messages.ERROR.NOT_DIVISIBLE_BY_THOUSAND_ERROR);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/constants/constants.js
@@ -0,0 +1,9 @@
+const ENTER_LOTTO_PURCHASE_AMOUNT = '구입금액을 입력해 주세요.\n';
+const INCLUDE_NOT_NUMBER_ERROR = '[ERROR] 구입금액은 숫자여야 합니다.\n';
+const START_WITH_ZERO_ERROR = '[ERROR] 구입금액의 첫 자리는 1이상 9이하의 숫자이여야 합니다.';
+const NOT_DIVISIBLE_BY_THOUSAND_ERROR = '[ERROR] 구입금액은 1000으로 나누어 떨어져야 합니다.';
+
+module.exports = {
+  GUIDE: { ENTER_LOTTO_PURCHASE_AMOUNT },
+  ERROR: { INCLUDE_NOT_NUMBER_ERROR, START_WITH_ZERO_ERROR, NOT_DIVISIBLE_BY_THOUSAND_ERROR },
+};
