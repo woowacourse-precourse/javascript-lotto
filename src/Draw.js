30d0d19b9284ead21f2d1255b5649c3151bde736
useon <yoosk4986@naver.com>
2022-11-15 06:17:42 +0900

    style(Draw): 변수명 오타를 수정한다
    winningValue -> winningInputValue

--- src/Draw.js
+++ src/Draw.js
@@ -4,7 +4,7 @@ const Validation = require('./Validation');
 class Draw {
   winning() {
     Console.readLine('당첨번호를 입력해 주세요.', (inputValue) => {
-      new Validation().winningValue(inputValue);
+      new Validation().winningInputValue(inputValue);
       this.winningNumbers = inputValue.split(',').map((item) => Number(item));
       Console.close();
       this.bonus();
