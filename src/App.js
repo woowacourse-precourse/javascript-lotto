08087306c02300fa31ad86dfb3217e99bd56898d
dianestar <dianestar@naver.com>
2022-11-14 15:59:34 +0900

    feat: 로또 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.money = 0;
+  }
+
+  setMoney() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (input) => {
+      this.money = Number(input);
+      // console.log(this.money);
+    }); 
+  }
+
+  play() {
+    this.setMoney();
+  }
 }
+
+/* 나중에 지울 코드 */
+const app = new App();
+app.play();
+/*******************/
 
 module.exports = App;
