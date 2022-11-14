014c68dd1b3ee244575769215e212a28f4fd1c08
DongWooKim97 <nanaukim@naver.com>
2022-11-14 23:35:25 +0900

    feat: 입력받은 보너스 번호(문자열)에 대해 예외처리 하는 메소드 추가 작성

--- src/input/Bonus.js
+++ src/input/Bonus.js
@@ -1,10 +1,31 @@
+const { ERROR_MESSAGES } = require("../utils/constants");
+
 class Bonus {
   #bonusNumber;
   #winningNumbers;
 
   constructor(bonusNumber, winningNumbers) {
     this.#winningNumbers = winningNumbers;
+    this.validate(bonusNumber);
     this.#bonusNumber = Number(bonusNumber);
+  }
+
+  validate(bonusNumber) {
+    if (!Number(bonusNumber)) {
+      throw new Error(ERROR_MESSAGES.INVALID_BONUS_TYPE);
+    }
+
+    if (String(bonusNumber).split("").length > 2) {
+      throw new Error(ERROR_MESSAGES.INVALID_BONUS_RANGE);
+    }
+
+    if (this.#winningNumbers.includes(Number(bonusNumber))) {
+      throw new Error(ERROR_MESSAGES.DUPLICATED_BONUS_NUM);
+    }
+  }
+
+  output() {
+    return this.#bonusNumber;
   }
 }
 
