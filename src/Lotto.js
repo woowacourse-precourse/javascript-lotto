0bac06392780efd14917552d3eb0d7a62567e5fc
banma70 <banma1234@gmail.com>
2022-11-14 16:31:01 +0900

    feature: 출력형식에 맞게 결과를 출력하는 함수 작성

--- src/Lotto.js
+++ src/Lotto.js
@@ -80,6 +80,8 @@ class Lotto {
     lotteryTicket.forEach((ticket) => {
       prize.push(this.judgeNumber(ticket));
     })
+
+    this.printResult(this.makeResult(prize));
   }
 
   judgeNumber(ticket) {
@@ -97,6 +99,32 @@ class Lotto {
     });
 
     return count;
+  }
+
+  printResult(result) {
+    let winnigPrize = this.caculatePrize(result);
+    let returnRate = ((winnigPrize/this.MONEY) * 10 / 10) * 100;
+
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${result[0]}개`)
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${result[1]}개`)
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${result[2]}개`)
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[3]}개`)
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${result[4]}개`)
+    MissionUtils.Console.print(`총 수익률은 ${returnRate}%입니다.`)
+    MissionUtils.Console.close();
+  }
+
+  caculatePrize(result) {
+    let winnigPrize = 0;
+    const prizeBoard = [5000, 50000, 1500000, 30000000, 2000000000]
+
+    result.forEach((item, index) => {
+      if (item != 0) {
+        winnigPrize = prizeBoard[index];
+      }
+    })
+
+    return winnigPrize;
   }
 
   makeResult(prize) {
