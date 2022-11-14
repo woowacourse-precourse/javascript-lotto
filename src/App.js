c5ad1f926f2f2a55bc40ae29033420a32ae149f8
JHdesktop <thsehgml37@korea.ac.kr>
2022-11-15 08:56:52 +0900

    feat: 당첨 번호 입력 기능

--- src/App.js
+++ src/App.js
@@ -11,12 +11,13 @@ class App {
     this.totalLotto = 0;
   }
   play() {
-    this.userPurchase();
+    this.inputPurchase();
     this.userLotto = new UserLotto(this.totalLotto);
     this.userLottoNumber = this.userLotto.getUserLotto();
+    this.inputWonLotto();
   }
 
-  userPurchase() {
+  inputPurchase() {
     MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (payment) => {
       this.isNumber(payment);
       this.purchaseLotto = new PurchaseLotto(parseInt(payment));
@@ -28,6 +29,16 @@ class App {
     if (isNaN(payment)) {
       throw new Error("[ERROR] 로또 구입 금액은 숫자만 입력해주세요.");
     }
+  }
+
+  inputWonLotto() {
+    MissionUtils.Console.readLine(
+      "\n당첨 번호를 입력해 주세요.\n",
+      (lottoNumber) => {
+        const winLotto = lottoNumber.split(",");
+        this.lotto = new Lotto(winLotto);
+      }
+    );
   }
 }
 
