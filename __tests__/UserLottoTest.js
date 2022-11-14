6148bac79be7ac540643939de5e215c3a3af5b01
somin <smhs12@naver.com>
2022-11-14 13:35:04 +0900

    test: 수익률 계산 기능 테스트

--- __tests__/UserLottoTest.js
+++ __tests__/UserLottoTest.js
@@ -31,3 +31,22 @@ describe("UserLotto클래스 calculateMatchResult함수 테스트", () => {
     expect(matchResult).toEqual({ 3: 2, 4: 2, 5: 1, 6: 1, containBonus: 1 });
   });
 });
+
+describe("UserLotto클래스 calculateBenefit함수 테스트", () => {
+  test("로또 6개 구매 4개 당첨 수익률 계산", () => {
+    const userLotto = new UserLotto();
+    userLotto.lottoCount = 6;
+    const benefit = userLotto.calculateBenefit({
+      3: 2,
+      4: 1,
+      5: 0,
+      6: 0,
+      containBonus: 1,
+    });
+    expect(benefit).toEqual(501000);
+  });
+
+  test("보너스 번호 포함하지 않는 경우 수익률 계산", () => {
+    const userLotto = new UserLotto();
+  });
+});
