62bc64e26a0d007bf9e1a05c8c1f5e38336a2ab7
2je0 <bulljy@naver.com>
2022-11-15 00:54:06 +0900

    ✨ feat: 당첨 로또번호와 보너스 번호를 입력받아 당첨로또를 생성하는 함수

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const Money = require('./Money');
 const RandomLottos = require('./RandomLottos');
+const WinningLotto = require('./WinningLotto');
 const { MESSAGE } = require('./Constants');
 
 const { Console } = MissionUtils;
@@ -24,6 +25,18 @@ class App {
       const numOfLotto = this.money.getNumOfLotto();
       this.randomLottos = new RandomLottos(numOfLotto);
       this.randomLottos.printRandomLottos();
+      this.enterWinningNumberMode();
+    });
+  }
+
+  enterWinningNumberMode() {
+    Console.readLine(MESSAGE.ASK_WINNING_NUMBERS, (winNums) => {
+      Console.print('');
+      Console.readLine(MESSAGE.ASK_BONUS_NUMBER, (bonus) => {
+        Console.print('');
+        const correct = winNums.split(',').map((e) => +e);
+        this.winningLotto = new WinningLotto(correct, +bonus);
+      });
     });
   }
 }
