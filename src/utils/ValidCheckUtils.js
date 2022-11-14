783763a923e586870fb44e4b9003301c1f29582c
kyw0716 <kyw0716@naver.com>
2022-11-15 01:24:18 +0900

    refactor: 함수 하나가 15줄 이상이 되지 않도록 수정

--- src/utils/ValidCheckUtils.js
+++ src/utils/ValidCheckUtils.js
@@ -5,10 +5,8 @@ class ValidCheckUtils {
 
   static checkPay(pay) {
     if (pay.length === 0) throw new Error(ErrorMsg.INVALID_PAY_EMPTY_STRING);
-
     if (pay.replace(/[0-9]/g, "").length > 0)
       throw new Error(ErrorMsg.INVALID_PAY_NOT_NUM);
-
     if (Number(pay) % 1000 > 0) throw new Error(ErrorMsg.INVALID_PAY_UNIT);
   }
 
@@ -17,19 +15,15 @@ class ValidCheckUtils {
 
     if (winningNum.length === 0)
       throw new Error(ErrorMsg.INVALID_WINNING_EMPTY_STRING);
-
     if (new Set(winningNumberArray).size !== winningNumberArray.length)
       throw new Error(ErrorMsg.INVALID_WINNING_DUPLICATE);
-
     if (
       winningNumberArray.filter((num) => Number(num) < 1 || Number(num) > 45)
         .length > 0
     )
       throw new Error(ErrorMsg.INVALID_WINNING_RANGE);
-
     if (winningNumberArray.length !== 6)
       throw new Error(ErrorMsg.INVALID_WINNING_COUNT);
-
     if (winningNum.replace(/[0-9]|\,/g, "").length > 0)
       throw new Error(ErrorMsg.INVALID_WINNING_NOT_NUM);
   }
@@ -37,13 +31,10 @@ class ValidCheckUtils {
   static checkBonusNumber(bonusNum, winningNumberArray) {
     if (bonusNum.length === 0)
       throw new Error(ErrorMsg.INVALID_BONUS_EMPTY_STRING);
-
     if (winningNumberArray.includes(bonusNum))
       throw new Error(ErrorMsg.INVALID_BONUS_DUPLICATE);
-
     if (Number(bonusNum) < 1 || Number(bonusNum) > 45)
       throw new Error(ErrorMsg.INVALID_BONUS_RANGE);
-
     if (String(bonusNum).replace(/[0-9]/g, "").length > 0 || bonusNum === "")
       throw new Error(ErrorMsg.INVALID_BONUS_NOT_NUM);
   }
