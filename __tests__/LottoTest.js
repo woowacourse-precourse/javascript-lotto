fd46496100ea1a5c067bb7a6ffc160a539f3b737
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-14 09:07:04 +0900

    💫test: 로또 클래스 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -4,15 +4,39 @@ describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow("[ERROR] 로또 번호의 개수는 6개입니다.");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow("[ERROR] 로또 번호는 중복될 수 없습니다.");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("금액이 1000원 미만이면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6]).validateMoney(900);
+    }).toThrow("[ERROR] 금액이 부족합니다.");
+  });
+
+  test("금액이 1000원 단위가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6]).validateMoney(1100);
+    }).toThrow("[ERROR] 1000원 단위의 금액을 입력해 주세요.");
+  });
+
+  test("금액을 입력하지 않으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6]).validateMoney();
+    }).toThrow("[ERROR] 금액을 입력해주세요.");
+  });
+
+  test("알맞은 개수의 로또를 생성한다.", () => {
+    const result = new Lotto([1, 2, 3, 4, 5, 6]).createLottos(3);
+    result.forEach((lotto) => {
+      new Lotto(lotto);
+    });
+
+    expect(result.length).toEqual(3);
+  });
 });
