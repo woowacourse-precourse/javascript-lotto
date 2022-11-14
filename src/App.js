52335f2cc5aada4c1e356cbb9e3cdb761c571d94
hozzijeong <ghwls0905@naver.com>
2022-11-14 16:16:14 +0900

    Feat: 로또 구입 금액 입력 및 로또 개수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+const Lotto = require("./Lotto.js");
+const { Console, Random } = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  randomLottoNums = [];
+  correctLottoNums = {};
+
+  play() {
+    Console.readLine("구입금액을 입력해주세요.\n", (input) => {
+      const inputChange = Number(input);
+      if (isNaN(inputChange)) throw new Error("[ERROR] 숫자를 입력해주세요.");
+      if (input % 1000 !== 0)
+        throw new Error("[ERROR] 금액은 1000원 단위로 입력해야 합니다.");
+      const lottoCount = input / 1000;
+      Console.print(`${lottoCount}개를 구매했습니다.`);
+
+      this.correctLottoNums = Array(lottoCount)
+        .fill([])
+        .map((lotto) => {
+          const lottoNum = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+            (a, b) => a - b,
+          );
+          Console.print(lottoNum);
+          return lottoNum;
+        });
+    });
+    Console.readLine("당첨 번호를 입력해 주세요.", (input) => {
+      console.log(input); // 여기서 보너스 번호까지 받음.
+    });
+  }
 }
 
-module.exports = App;
+const app = new App();
+app.play();
+
+// module.exports = App;
