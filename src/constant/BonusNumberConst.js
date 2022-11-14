056798c448907bd25f8b415c687a7c8878cdf01a
Rki0 <pky00823@gmail.com>
2022-11-14 10:38:33 +0900

    refactor(BonusNumber): modularize constants to another file
    Move constants managed in one file to a separate file.

---
+++ src/constant/BonusNumberConst.js
@@ -0,0 +1,9 @@
+const BonusNumberConst = {
+  ERROR_DONT_START_ZERO: "[ERROR] 0으로 시작하는 숫자는 입력할 수 없습니다.",
+  ERROR_NOT_ONLY_NUMBER: "[ERROR] 숫자만이 입력 가능합니다.",
+  ERROR_OUT_OF_RANGE: "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.",
+  ERROR_DUPLICATE_EXIST:
+    "[ERROR] 6개의 당첨 번호로 입력한 숫자는 보너스 번호로 사용 할 수 없습니다.",
+};
+
+module.exports = BonusNumberConst;
