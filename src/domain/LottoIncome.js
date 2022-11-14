fab3b25121057ed4aab022b1548117f3db51748a
turtle601 <turtle601@naver.com>
2022-11-14 19:25:51 +0900

    refactor(LottoIncome) LottoIncome 구현
    Explained:
    - 수익률 관련 데이터 만드는 객체

---
+++ src/domain/LottoIncome.js
@@ -0,0 +1,51 @@
+const LottoCalculator = require('./LottoCalculator');
+
+const { LOTTO_AMOUNT } = require('../../utils/constants');
+
+class LottoIncomeDomain extends LottoCalculator {
+  #income;
+
+  #payment;
+
+  constructor(inputs) {
+    super(inputs);
+
+    this.#income = '';
+    this.#calculateIncome();
+  }
+
+  getResult() {
+    return this.#income;
+  }
+
+  #roundUpFor() {
+    this.#income = Math.round(
+      (LOTTO_AMOUNT.reduce((acc, moneyUnit, index) => {
+        return acc + moneyUnit * this.getLottoCountScore()[index];
+      }, 0) /
+        this.payment.getMoney()) *
+        1000,
+    );
+
+    return this;
+  }
+
+  #makeDecimalFirst() {
+    if (this.#income === 0) {
+      this.#income = '0.0';
+      return this;
+    }
+
+    this.#income = String(this.#income).split('');
+    this.#income.splice(this.#income.length - 1, 0, '.');
+    this.#income = this.#income.join('');
+
+    return this;
+  }
+
+  #calculateIncome() {
+    this.#roundUpFor().#makeDecimalFirst();
+  }
+}
+
+module.exports = LottoIncomeDomain;
