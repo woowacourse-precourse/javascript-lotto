db93810aa8ecd75f3452968063678df5ce0d0e59
pinkishincoloragain <smb1103@gmail.com>
2022-11-14 20:11:59 +0900

    feat: 로또 수익률 통계 생성: generateLottoStat()

--- src/Game.js
+++ src/Game.js
@@ -1,34 +1,31 @@
 const { Random } = require('@woowacourse/mission-utils');
-const { GAME_RANGE, PRIZES, STAT_PHRASE, PRIZE_CONFIG } = require('./config');
+const { GAME_RANGE, PRIZES } = require('./config');
 
 class Game {
   #lottos = [];
   #winningNumbers = [];
   #bonusNUmber = 0;
 
-  constructor() {
-    this.result = {};
-  }
-
-  set lottos(lottos) {
-    this.#lottos = lottos;
+  constructor(lottoNum, winningNumbers, bonusNumber) {
+    this.#lottos = Array.from({ length: lottoNum }, () => this.generateOneLotto());
+    this.#winningNumbers = winningNumbers;
+    this.#bonusNUmber = bonusNumber;
   }
 
   generateOneLotto() {
-    const oneLotto = Random.pickUniqueNumbersInRange(
+    return Random.pickUniqueNumbersInRange(
       GAME_RANGE.RANGE_MIN,
       GAME_RANGE.RANGE_MAX,
       GAME_RANGE.NUM_LENGTH,
     ).sort((a, b) => a - b);
-    return oneLotto;
+  }
+
+  generateLottoStat(budget, ranks) {
+    const totalValue = ranks.reduce((acc, rank) => acc + PRIZES[rank].VALUE, 0);
+    const profitRate = ((totalValue / budget) * 100).toFixed(2);
+
+    return profitRate;
   }
 }
-
-const generateLottoStat = (budget, ranks) => {
-  const totalValue = ranks.reduce((acc, rank) => acc + PRIZES[rank].VALUE, 0);
-  const profitRate = ((totalValue / budget) * 100).toFixed(2);
-
-  return profitRate;
-};
 
 module.exports = Game;
