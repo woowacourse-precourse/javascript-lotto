ae5bbe59a5625dc658928f307395f2a412d400a9
jungsmins1 <jungsmins00@gmail.com>
2022-11-14 12:29:28 +0900

    feat: 구입한 금액 만큼 랜덤한 번호를 발행하는 기능 테스트 추가

--- __tests__/LottoPurchaseTest.js
+++ __tests__/LottoPurchaseTest.js
@@ -1,9 +1,15 @@
-const LottoPurchase = require("../src/LottoPurchaseTest");
+const LottoPurchase = require("../src/LottoPurchase");
 
 describe("LottoPurchase 클래스 테스트", () => {
+
   test("로또 구입 금액이 1,000원 단위가 아니면 예외가 발생한다.", () => {
     expect(() => {
       new LottoPurchase(1100);
     }).toThrow("[ERROR]");
   });
+
+  test("구입한 금액 만큼 랜덤한 번호를 발행한다.", () => {
+    expect(new LottoPurchase(2000).lottoPublish()
+    ).toHaveLength(2)
+  })
 });
