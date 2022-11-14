f14033d932815c34f9f264151737c7b82c4109b9
Raehyeon Jeong <raehy19@gmail.com>
2022-11-14 23:34:41 +0900

    feat(src/Lotto.js) : 로또 클래스 constructor 숫자 validate 메소드 추가 => 배열의 원소들이 숫자인지 확인, 숫자가 중복이 아닌지 확인

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,22 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (this.#hasNaN(numbers)) {
+      throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+    }
+    if ([...new Set(numbers)].length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  #hasNaN(numbers) {
+    for (let i = 0; i < numbers.length; ++i) {
+      if (typeof numbers[i] !== 'number') return true;
+    }
+    return false;
+  }
 }
 
 module.exports = Lotto;
