50fb8976d1175d6a7abfbd2fc9ae0906e2d71266
areumsheep <areumsheep@gmail.com>
2022-11-14 18:12:11 +0900

    add: 구입 금액 입력 처리 및 1,000원 단위가 아닐 경우 예외 처리 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+const { MESSAGES, ERROR_MESSAGES } = require('./constants/index');
+
+const LOTTO_PRICE = 1000;
 class App {
-  play() {}
+  play() {
+    Console.readLine(MESSAGES.INPUT_MONEY, (number) => {
+      if (number % LOTTO_PRICE !== 0)
+        throw new Error(ERROR_MESSAGES.INVALID_MONEY_UNIT);
+    });
+  }
 }
 
 module.exports = App;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const ERROR_MESSAGES = Object.freeze({
+const ERROR_MESSAGES = Object.freeze({
   INVALID_MONEY_UNIT: '[ERROR] 1,000원 단위로 입력해 주세요.',
   INVALID_LOTTO_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
   INVALID_LOTTO_RANGE: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
@@ -6,7 +6,7 @@ export const ERROR_MESSAGES = Object.freeze({
   NOT_DUPLICATE_NUMBER: '[ERROR] 로또 번호는 중복될 수 없습니다.',
 });
 
-export const MESSAGES = Object.freeze({
+const MESSAGES = Object.freeze({
   INPUT_MONEY: '구입금액을 입력해 주세요.',
   BUY_COUNT: (count) => `${count}개를 구매했습니다.`,
   INPUT_LOTTO_NUMBERS: '당첨 번호를 입력해 주세요.',
@@ -25,3 +25,5 @@ export const MESSAGES = Object.freeze({
     총 수익률은 ${rate}%입니다.
   `,
 });
+
+module.exports = { ERROR_MESSAGES, MESSAGES };
