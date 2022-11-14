d71964049d72447f5599f9308edcbf5a51a64af5
동동이 <drdragon777@naver.com>
2022-11-14 23:49:23 +0900

    feat - add 멤버변수 초기화

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const Console = require("./Console");
+const { THREE, FOUR, FIVE, FIVE_AND_BONUS, SIX } = require("./constants");
+
 class App {
-  play() {}
+  constructor() {
+    this.lottos = [];
+    this.winNumbers = [];
+    this.bonusNumber = null;
+    this.winStatics = {
+      [THREE]: 0,
+      [FOUR]: 0,
+      [FIVE]: 0,
+      [FIVE_AND_BONUS]: 0,
+      [SIX]: 0,
+    }
+  }
+  play() {
+
+  }
 }
 
 module.exports = App;
