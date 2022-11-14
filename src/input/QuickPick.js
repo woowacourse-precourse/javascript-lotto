7d99e76821a586182cf584c38789b228f1b4e83b
DongWooKim97 <nanaukim@naver.com>
2022-11-14 22:06:25 +0900

    feat: 구매수량에 맞게 로또 번호(배열)들을 만들고, 출력하는 메소드 추가

--- src/input/QuickPick.js
+++ src/input/QuickPick.js
@@ -1,4 +1,5 @@
-const { ERROR_MESSAGES } = require("../utils/Constants");
+const Mission = require("@woowacourse/mission-utils");
+const { LOTTO_INFO, GAME_MESSAGES, NUMBERS, ERROR_MESSAGES } = require("../utils/Constants");
 
 class QuickPick {
   #amount;
@@ -26,6 +27,27 @@ class QuickPick {
 
   countAmount(payment) {
     this.#amount = payment / LOTTO_INFO.LOTTO_PRICE;
+  }
+
+  pickRandomNumbers() {
+    this.#myLottoArray = Array(this.#amount)
+      .fill(null)
+      .map((item) =>
+        Mission.Random.pickUniqueNumbersInRange(
+          NUMBERS.MIN_LOTTO_NUMBER,
+          NUMBERS.MAX_LOTTO_NUMBER,
+          NUMBERS.CORRECT_LOTTO_LENGTH
+        ).sort((a, b) => a - b)
+      );
+    this.printNumbersArray();
+  }
+
+  printNumbersArray() {
+    Mission.Console.print(`\n${GAME_MESSAGES.RETURN_PURCHASED_AMOUNT(this.#amount)}`);
+    this.#myLottoArray.forEach((item) => {
+      let eachItems = String(item).replace(/,/gi, ", ");
+      Mission.Console.print(`[${eachItems}]`);
+    });
   }
 }
 
