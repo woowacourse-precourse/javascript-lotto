66963ea32ddc1ae63f625ec7c2af6dbe492582f4
solo5star <solo_5@naver.com>
2022-11-15 08:01:00 +0900

    feat(lotto-buy): 로또를 랜덤으로 생성하는 정적 팩토리 메서드 추가

--- src/domains/Lotto.js
+++ src/domains/Lotto.js
@@ -1,3 +1,5 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class Lotto {
   /** 로또가 가질 수 있는 숫자 갯수 */
   static NUMBER_COUNT = 6;
@@ -20,6 +22,15 @@ class Lotto {
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers.sort();
+  }
+
+  static fromRandom() {
+    const numbers = MissionUtils.Random.pickUniqueNumbersInRange(
+      Lotto.NUMBER_MIN,
+      Lotto.NUMBER_MAX,
+      Lotto.NUMBER_COUNT,
+    );
+    return new Lotto(numbers);
   }
 
   /**
