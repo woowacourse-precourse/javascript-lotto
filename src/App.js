841f9af693d80392775419764604e404d373c45c
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-11-15 03:15:15 +0900

    Feat: class 나눠놓은 것 철회, App.js 안에 넣기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-// 최종 반환되는 class
+const Lotto = require("./");
+const Console = require("@woowacourse/mission-utils").Console;
+const Random = require("@woowacourse/mission-utils").Random;
+const inputValidation = require("./inputValidation");
 
 class App {
+  #countLottos;
   play() {
-    const startLotto = new //클래스이름();
-    startLotto.play();
+    this.inputMoney();
+    this.outputCountLottos();
+    Console.close();
+  }
+
+  inputMoney() {
+    Console.readLine(INPUTS.INPUT_MONEY, (money) => {
+      inputValidation.checkThousandNum(money);
+      this.#countLottos = Number(money) / 1000;
+    });
+  }
+
+  outputCountLottos() {
+    Console.print(`${this.#countLottos}개를 구매했습니다.`);
+  }
+
+  makeLottosList() {
+    
   }
 }
 
 module.exports = App;
+
+//  //당첨번호 입력받기
+// inputNumbers() {
+//   Console.readLine(INPUTS.INPUT_NUMBERS, (numbers) => {
+//     this.#numbers = numbers;
+//   });
+// };
+
+//   //로또 뽑기 logic
+//   randomLottoNumber() {
+//     const lottoNum = Random.pickUniqueNumbersInRange(1, 45, 6);
+//     return lottoNum;
+//   }
+
+//   randomBonusNumber() {
+//     // const bonusNum = Random.
+//   }
--- src/Controller/LottoController.js
+++
@@ -1,7 +0,0 @@
-const Random = require("@woowacourse/mission-utils").Random;
-const Console = require("@woowacourse/mission-utils").Console;
-
-
-class LottoController {}
-
-module.exports = LottoController;
