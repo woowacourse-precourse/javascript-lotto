0e1cbc10fa341a5f1c68542cf25dc2d742dbdcc2
catca <kdg96g@gmail.com>
2022-11-15 01:58:16 +0900

    feat: 당첨 번호 입렵 받기 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
 const MESSAGES = require('./Constants');
+const Lotto = require("./Lotto");
 
 class App {
   play() {
     Console.readLine(MESSAGES.PAYMENT, (payment) => {
       this.checkPayment(payment);
-      Console.print(`${payment / 1000}개를 구매했습니다.`)
-      const lottoNumbers = this.getLottoNumbers(payment / 1000)
+      Console.print(`${payment / 1000}개를 구매했습니다.`);
+      const lottoNumbers = this.getLottoNumbers(payment / 1000);
+      this.setLottoNumber();
     });
   }
   checkPayment = (payment) => {
@@ -22,6 +24,11 @@ class App {
       Console.print(numbers);
     } 
     return lottoNumbers;
+  }
+  setLottoNumber = () => {
+    Console.readLine(MESSAGES.SET_LOTTO, (answer) => {
+      Console.print(answer);
+    })
   }
 }
 
--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,7 @@
 const MESSAGES = {
   PAYMENT: '구입금액을 입력해 주세요.\n',
+  SET_LOTTO: '당첨 번호를 입력해 주세요.\n',
+  SET_BONUS: '보너스 번호를 입력해 주세요.\n',
   ERROR: {
     PAYMENT: '[ERROR] 구입금액이 1,000원 단위가 아닙니다.',
     DUPICATION: '[ERROR] 로또 번호는 중복되면 안됩니다.',
