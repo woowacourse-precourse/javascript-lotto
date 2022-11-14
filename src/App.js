8faf0f20e66c7365f00d2715eef36530fdb16591
김나현 <nahyeon.dev@gmail.com>
2022-11-15 03:31:11 +0900

    feat: constructor() 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+
 class App {
+  constructor(){
+    this.purchase = 0;
+    this.lottoNumber = [];
+    this.winningNumber = [];
+    this.bonusNumber = 0;
+  }
+  
   play() {}
 }
 
