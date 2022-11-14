cea47f68b5feb10167f23a6854276c9cac41ad92
seongminn <berry9901@gmail.com>
2022-11-15 04:18:50 +0900

    test(MoneyValidatorTest): 오타 수정

--- __tests__/MoneyValidatorTest.js
+++ __tests__/MoneyValidatorTest.js
@@ -1,31 +1,31 @@
 const MoneyValidator = require('../src/validator/MoneyValidator');
 
-describe('로또 클래스 테스트', () => {
+describe('로또 구입 금액 유효성 검사기 클래스 테스트', () => {
   test('로또 구입 금액이 숫자가 아닐 경우 예외가 발생한다.', () => {
     expect(() => {
       MoneyValidator.validate('1e9');
     }).toThrow('[ERROR]');
   });
 
-  test('로또 입력 금액이 1,000원 단위가 아닐 경우 예외가 발생한다.', () => {
+  test('로또 구입 금액이 1,000원 단위가 아닐 경우 예외가 발생한다.', () => {
     expect(() => {
       MoneyValidator.validate('30001');
     }).toThrow('[ERROR]');
   });
 
-  test('로또 입력 금액이 0원일 경우 예외가 발생한다.', () => {
+  test('로또 구입 금액이 0원일 경우 예외가 발생한다.', () => {
     expect(() => {
       MoneyValidator.validate('0');
     }).toThrow('[ERROR]');
   });
 
-  test('로또 입력 금액이 null일 경우 예외가 발생한다.', () => {
+  test('로또 구입 금액이 null일 경우 예외가 발생한다.', () => {
     expect(() => {
       MoneyValidator.validate(null);
     }).toThrow('[ERROR]');
   });
 
-  test('로또 입력 금액이 undefined일 경우 예외가 발생한다.', () => {
+  test('로또 구입 금액이 undefined일 경우 예외가 발생한다.', () => {
     expect(() => {
       MoneyValidator.validate(undefined);
     }).toThrow('[ERROR]');
