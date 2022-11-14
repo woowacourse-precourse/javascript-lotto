4516a31f6c572cf49fc8ec51dceb10532e5954ae
pitang <wpeffect@gmail.com>
2022-11-14 16:51:37 +0900

    refactor: throw와 기능 분리

--- src/App.js
+++ src/App.js
@@ -23,11 +23,9 @@ class App {
   userBuyLotto() {
     MissionUtils.Console.readLine(
       "구입금액을 입력해 주세요. \n",
-      (buyPrice) => {
-        if (buyPrice % 1000 !== 0) {
-          throw new Error("알맞는 금액을 입력해 주세요.");
-        }
-        return this.userLottoNumber(buyPrice / 1000);
+      (buyLotto) => {
+        this.buyLotto = buyLotto;
+        this.userBuyLottoCheck(this.buyLotto);
       }
     );
   }
