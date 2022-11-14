c83df3955cd19b2be46c5777b8a7de02bdc89ef1
guridaek <iue16@naver.com>
2022-11-14 17:17:44 +0900

    test: App 기능 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -14,10 +14,7 @@ const mockQuestions = answers => {
 
 const mockRandoms = numbers => {
   MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
-  numbers.reduce(
-    (acc, number) => acc.mockReturnValueOnce(number),
-    MissionUtils.Random.pickUniqueNumbersInRange,
-  );
+  numbers.reduce((acc, number) => acc.mockReturnValueOnce(number), MissionUtils.Random.pickUniqueNumbersInRange);
 };
 
 const getLogSpy = () => {
@@ -27,42 +24,75 @@ const getLogSpy = () => {
 };
 
 describe('로또 테스트', () => {
-  // test('기능 테스트', () => {
-  //   mockRandoms([
-  //     [8, 21, 23, 41, 42, 43],
-  //     [3, 5, 11, 16, 32, 38],
-  //     [7, 11, 16, 35, 36, 44],
-  //     [1, 8, 11, 31, 41, 42],
-  //     [13, 14, 16, 38, 42, 45],
-  //     [7, 11, 30, 40, 42, 43],
-  //     [2, 13, 22, 32, 38, 45],
-  //     [1, 3, 5, 14, 22, 45],
-  //   ]);
-  //   mockQuestions(['8000', '1,2,3,4,5,6', '7']);
-  //   const logs = [
-  //     '8개를 구매했습니다.',
-  //     '[8, 21, 23, 41, 42, 43]',
-  //     '[3, 5, 11, 16, 32, 38]',
-  //     '[7, 11, 16, 35, 36, 44]',
-  //     '[1, 8, 11, 31, 41, 42]',
-  //     '[13, 14, 16, 38, 42, 45]',
-  //     '[7, 11, 30, 40, 42, 43]',
-  //     '[2, 13, 22, 32, 38, 45]',
-  //     '[1, 3, 5, 14, 22, 45]',
-  //     '3개 일치 (5,000원) - 1개',
-  //     '4개 일치 (50,000원) - 0개',
-  //     '5개 일치 (1,500,000원) - 0개',
-  //     '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
-  //     '6개 일치 (2,000,000,000원) - 0개',
-  //     '총 수익률은 62.5%입니다.',
-  //   ];
-  //   const logSpy = getLogSpy();
-  //   const app = new App();
-  //   app.play();
-  //   logs.forEach(log => {
-  //     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-  //   });
-  // });
+  test('기능 테스트', () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+    mockQuestions(['8000', '1,2,3,4,5,6', '7']);
+    const logs = [
+      '8개를 구매했습니다.',
+      '[8, 21, 23, 41, 42, 43]',
+      '[3, 5, 11, 16, 32, 38]',
+      '[7, 11, 16, 35, 36, 44]',
+      '[1, 8, 11, 31, 41, 42]',
+      '[13, 14, 16, 38, 42, 45]',
+      '[7, 11, 30, 40, 42, 43]',
+      '[2, 13, 22, 32, 38, 45]',
+      '[1, 3, 5, 14, 22, 45]',
+      '3개 일치 (5,000원) - 1개',
+      '4개 일치 (50,000원) - 0개',
+      '5개 일치 (1,500,000원) - 0개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
+      '6개 일치 (2,000,000,000원) - 0개',
+      '총 수익률은 62.5%입니다.',
+    ];
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    logs.forEach(log => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test('기능 테스트2', () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [20, 25, 24, 23, 22, 26],
+      [21, 22, 23, 1, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [43, 11, 30, 42, 40, 7],
+    ]);
+    mockQuestions(['6000', '20,21,22,23,24,25', '26']);
+    const logs = [
+      '6개를 구매했습니다.',
+      '[8, 21, 23, 41, 42, 43]',
+      '[3, 5, 11, 16, 32, 38]',
+      '[20, 22, 23, 24, 25, 26]',
+      '[1, 21, 22, 23, 41, 42]',
+      '[13, 14, 16, 38, 42, 45]',
+      '[7, 11, 30, 40, 42, 43]',
+      '3개 일치 (5,000원) - 1개',
+      '4개 일치 (50,000원) - 0개',
+      '5개 일치 (1,500,000원) - 0개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 1개',
+      '6개 일치 (2,000,000,000원) - 0개',
+      '총 수익률은 500083.3%입니다.',
+    ];
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    logs.forEach(log => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
 
   test('올바르지 않은 금액이면 예외 발생.', () => {
     mockQuestions(['1000j']);
@@ -79,48 +109,4 @@ describe('로또 테스트', () => {
       app.play();
     }).toThrow('[ERROR]');
   });
-
-  test('구매 및 출력 테스트', () => {
-    mockRandoms([
-      [8, 21, 23, 41, 42, 43],
-      [3, 5, 11, 16, 32, 38],
-      [7, 11, 16, 35, 36, 44],
-      [1, 8, 11, 31, 41, 42],
-    ]);
-    mockQuestions(['4000']);
-    const logs = [
-      '4개를 구매했습니다.',
-      '[8, 21, 23, 41, 42, 43]',
-      '[3, 5, 11, 16, 32, 38]',
-      '[7, 11, 16, 35, 36, 44]',
-      '[1, 8, 11, 31, 41, 42]',
-    ];
-    const logSpy = getLogSpy();
-    const app = new App();
-    app.inputMoney();
-    app.buyingLotto();
-    app.printLottos();
-    logs.forEach(log => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
-  });
-
-  test('당첨 번호 예외 발생.', () => {
-    mockQuestions(['1,2,3,4,5,5']);
-    expect(() => {
-      const app = new App();
-      app.inputWinningNumbers();
-    }).toThrow('[ERROR]');
-  });
-
-  test('보너스 번호가 중복 번호일 경우 예외 발생.', () => {
-    mockQuestions(['1,2,3,4,5,6', '3']);
-    expect(() => {
-      const app = new App();
-      app.inputWinningNumbers();
-      app.inputBonusNumber();
-    }).toThrow('[ERROR]');
-  });
-
-  MissionUtils.Console.close();
 });
