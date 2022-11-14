ff0895d02de1d884efa85c9ae9ef16266969322e
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-14 19:46:42 +0900

    feat : 상수 선언 파일 생성
    - 상수로 쓰일만한 요소들을 상수화
    - 추후 추가 및 네이밍의 변경이 있을 수 있음

---
+++ src/modules/Constant.js
@@ -0,0 +1,25 @@
+const GAME = {
+  START: 1,
+  END: 45,
+  COUNT: 6,
+  PRICE: 1000,
+};
+
+const MESSAGE = {
+  BUY: '구입금액을 입력해 주세요.',
+  CONFIRM_BUY: (count) => `${count}개를 구매했습니다.`,
+  CONFIRM_WIN: '당첨 번호를 입력해 주세요.',
+  CONFIRM_BONUS: '보너스 번호를 입력해 주세요.',
+  first: (count) => `6개 일치 (2,000,000,000원) - ${count}개`,
+  second: (count) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${count}개`,
+  third: (count) => `5개 일치 (1,500,000원) - ${count}개`,
+  fourth: (count) => `4개 일치 (50,000원) - ${count}개`,
+  fifth: (count) => `3개 일치 (5,000원) - ${count}개`,
+  profit: (percentage) => `총 수익률은 ${percentage}%입니다.`,
+};
+
+const ERROR = {
+  COMMON: '[ERROR]',
+};
+
+module.exports = { GAME, MESSAGE, ERROR };
