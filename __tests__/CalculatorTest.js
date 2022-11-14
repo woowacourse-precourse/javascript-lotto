b3442ded50a46835208ba414c2d5b41a28719bbb
daal2quf <daal2quf@gmail.com>
2022-11-15 03:14:10 +0900

    test(calculator): matchNumberCount() 기능 테스트
    add:
    test1: 같은 숫자가 3개일때 count가 3이 반환되는지 확인

--- __tests__/CalculatorTest.js
+++ __tests__/CalculatorTest.js
@@ -1,5 +1,6 @@
 const App = require("../src/App");
 const { Console } = require("@woowacourse/mission-utils");
+const Calculator = require("../src/Calculator");
 
 
 afterEach(() => {
@@ -12,5 +13,15 @@ describe("계산 기능 테스트", () => {
     const input = 4000;
     const lottoCount = app.calculatorLottoCount(input);
     expect(lottoCount).toEqual(4);
+  });
+});
+
+describe("Utils 클래스 matchNumberCount() 테스트", () => {
+  test("당첨 숫자와 유저 구매 숫자의 같은 값 개수 확인", () => {
+    const calculator = new Calculator();
+    const winNumbers = [1, 2, 3, 4, 5, 6, 7];
+    const userlottoNumbers = [1, 3, 5, 9, 40, 41];
+    const count = calculator.matchNumberCount(winNumbers, userlottoNumbers);
+    expect(count).toEqual(3);
   });
 });
