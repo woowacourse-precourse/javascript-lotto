19e363e11d9a2b8ea4e2517a532dcc961b55e083
thewronghand <penfreak77@gmail.com>
2022-11-14 18:32:48 +0900

    test: 당첨 번호 적합성 판단 테스트 구현
    당첨 번호 적합성 판단 메서드가 정상적으로 작동하는지 테스트한다.

---
+++ __tests__/LottoPublisherTest.js
@@ -0,0 +1,46 @@
+const LottoPublisher = require('../src/LottoPublisher');
+
+describe('로또스토어 클래스 테스트', () => {
+  const lottoPublisher = new LottoPublisher();
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
+    expect(() => {
+      lottoPublisher.validateWinningNumbers([1, 2, 3, 4, 5, 6, 7]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
+    expect(() => {
+      lottoPublisher.validateWinningNumbers([1, 2, 3, 4, 5, 5]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호에 숫자가 아닌 값이 포함되면 예외가 발생한다', () => {
+    expect(() => {
+      lottoPublisher.validateWinningNumbers([1, 2, 3, NaN, 5, 6]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호에 정수가 아닌 유리수가 포함되면 예외가 발생한다', () => {
+    expect(() => {
+      lottoPublisher.validateWinningNumbers([1, 2, 3, 3.014, 5, 6]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호에 음수가 포함되면 예외가 발생한다', () => {
+    expect(() => {
+      lottoPublisher.validateWinningNumbers([1, 2, 3, -4, 5, 6]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호에 46 이상의 수가 포함되면 예외가 발생한다', () => {
+    expect(() => {
+      lottoPublisher.validateWinningNumbers([1, 2, 3, 50, 5, 6]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호에 1 미만의 수가 포함되면 예외가 발생한다', () => {
+    expect(() => {
+      lottoPublisher.validateWinningNumbers([1, 2, 3, 0, 5, 6]);
+    }).toThrow('[ERROR]');
+  });
+});
