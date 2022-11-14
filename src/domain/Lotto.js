1f98868724c4afb9bce962334d3308de3aa2ca02
고동현 <dhjk35@naver.com>
2022-11-14 14:04:02 +0900

    feat(lotto): 로또 번호에 중복되는 수가 있다면 예외 처리하는 기능 구현

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -14,10 +14,18 @@ class Lotto {
     if (!this.#hasValidLength(numbers)) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    if (!this.#hasUniqueValues(numbers)) {
+      throw new Error('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
+    }
   }
 
   #hasValidLength(numbers) {
     return numbers.length === 6;
+  }
+
+  #hasUniqueValues(numbers) {
+    return new Set(numbers).size === 6;
   }
 }
 
