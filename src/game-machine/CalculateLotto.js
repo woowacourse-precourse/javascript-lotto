8b9ba6ae19b6ae87b802304d1eb9b5f044ecbd77
SorinKwon <ksl020700@gmail.com>
2022-11-14 23:19:05 +0900

    feat: 발행할 로또 수량 계산 기능 추가
    새로운 기능 추가:
    - 구입 금액을 이용해 발행할 로또 수량을 계산하는 기능을 추가함
    - 계산한 로또 수량은 객체 속성에 저장됨

--- src/game-machine/CalculateLotto.js
+++ src/game-machine/CalculateLotto.js
@@ -2,10 +2,18 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class CalculateLotto{
   constructor(){
     this.purchase_amount;
+    this.lotto_quantity;  
   }
 
   getPurchaseAmount(purchase_amount){
     this.purchase_amount = purchase_amount;
+  }
+
+  calculateLottoQuantity(){
+    let lotto_quantity;
+    lotto_quantity=this.purchase_amount/1000;
+    this.lotto_quantity=lotto_quantity;
+    return lotto_quantity;
   }
 
 }
