b8fb5d7415a297c2f8e794cd905271ada291b49d
Yebin Kim <kyb4312@gmail.com>
2022-11-14 09:46:29 +0900

    feat(Lotto): validate numbers

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,18 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (numbers.some((number) => !Number.isInteger(number))) {
+      throw new Error('[ERROR] 로또 번호는 정수여야 합니다.');
+    }
+    if (numbers.some((number) => !(number >= 1 && number <= 45))) {
+      throw new Error('[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.');
+    }
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error('[ERROR] 로또 번호는 중복된 숫자를 포함할 수 없습니다.');
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
