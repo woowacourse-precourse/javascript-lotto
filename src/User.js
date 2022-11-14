1a7ddc1b742607c5abcd1bf04c17ba02cd0a1e45
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-14 19:43:06 +0900

    Fix: 구입 금액 유효성 검사 부분 수정

--- src/User.js
+++ src/User.js
@@ -2,32 +2,33 @@ class User {
 	#seedMoney
 
 	constructor(money) {
-		this.validate(money);		
+		this.validate(money);
 		this.#seedMoney = money;
+		throw ('is Valid');
 	}
 
 	validate(money) {
-		isNum(money)
-		isMoneyEnough(money);
-		isThousands(money);
+		this.isNum(money)
+		this.isMoneyEnough(money);
+		this.isThousands(money);
 	}
 
 	isNum(money) {
     const validNums = /[^0-9]/;
 		if (validNums.test(money)) {
-			throw new Error("[ERROR] 구입 금액은 숫자여야 합니다.");
+			throw new Error('[ERROR] 구입 금액은 숫자여야 합니다.');
 		}
   }
 
 	isMoneyEnough(money) {
 		if (money < 1000) {
-			throw new Error("[ERROR] 구입 금액이 충분하지 않습니다.");
+			throw new Error('[ERROR] 구입 금액이 충분하지 않습니다.');
 		}
 	}
 
 	isThousands(money) {
 		if ((money % 1000) !== 0) {
-			throw new Error("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+			throw new Error('[ERROR] 구입 금액은 1,000원 단위여야 합니다.');
 		}
 	}
 }
