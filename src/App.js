06548c5673bbcb20533ba5b008cf2b1b109e5a82
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-11-14 20:54:15 +0900

    feat: 구입금액 검증 및 몇장 뽑을지 구하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const { Lotto } = require("./Lotto");
+const { Console } = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+    insertMoney();
+  }
 }
-
+let result = [];
+function insertMoney() {
+  let lottoMount = "";
+  Console.readLine("구입금액을 입력해 주세요.", (answer) => {
+    result.push(answer);
+    validateMoney(answer);
+    //구입금액이 1000원 단위로 떨어지는지..
+    function validateMoney(money) {
+      if (money % 1000 !== 0) {
+        throw "[ERROR]천원 단위 돈만 입력 가능함";
+      } else lottoMount = money / 1000;
+      console.log(lottoMount);
+    }
+  });
+}
+let playApp = new App();
+playApp.play();
 module.exports = App;
