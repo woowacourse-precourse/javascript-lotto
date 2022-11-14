c7ec55467ff19412cf7fe0b060015c832020e5dc
김아현 <dhflxhdxhd@gmail.com>
2022-11-14 23:46:33 +0900

    Feat : 로또 개수만큼 로또 번호 발행 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,27 +1,38 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 const { MESSAGE } = require("./constant/constant.js");
 
 class Lotto {
   #numbers;
   
   constructor(numbers) {
-    this.validate(numbers);
+    // this.validate(numbers);
     this.#numbers = numbers;
+    this.mylotto = [];
   }
   
   askLottoCost(){
     Console.readLine(`${MESSAGE.START}\n`,(cost) => {
       this.cost = Number(cost);
       this.lottoCount = parseInt(this.cost / 1000);
-      console.log(this.lottoCount);
+      this.makeLottoNumber(this.lottoCount);
     })
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+
+
+  makeLottoNumber(lottoCount){
+    for(let i = 0; i<lottoCount; i++){
+      let lotto = Random.pickUniqueNumbersInRange(1,45,6);
+      this.mylotto.push(lotto);
     }
+    console.log(this.mylotto)
   }
+
+  // validate(numbers) {
+  //   if (numbers.length !== 6) {
+  //     throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  //   }
+  // }
 }
 
 module.exports = Lotto;
