c6b8822af10f365d4d68a658fcfeff67ff287c6b
mjmj0217 <mjmj0217@naver.com>
2022-11-14 13:14:12 +0900

    feat: add get money from user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
+const INPUT_MESSAGE = {
+  money: "구입금액을 입력해 주세요.",
+  bonus: "보너스 번호를 입력하세요.",
+  winning: "당첨 번호를 입력해 주세요.",
+};
+
 class App {
-  play() {}
+  play() {
+    Console.print(INPUT_MESSAGE.money);
+    Console.readLine("", (money) => {});
+  }
 }
 
 module.exports = App;
