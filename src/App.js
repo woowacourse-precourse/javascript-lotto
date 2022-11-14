6cad4f9d7e053a5be68cb4de485509c5259378d7
wisdomin121@naver.com <wisdomin121@naver.com>
2022-11-14 21:08:31 +0900

    ✨ [FEAT] : 구입 금액 입력받기
    로또 수량을 계산하고, 로또 번호를 생성하기 위해
    구입 금액을 먼저 입력 받았다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const { Console } = require("@woowacourse/mission-utils");
+const Machine = require('./Machine.js');
+const machine = new Machine();
+
 class App {
-  play() {}
+  constructor() {
+    
+  }
+
+  inputCost() {
+    machine.getCost();
+    
+    Console.readLine('', (cost) => {
+      machine.calculateCount(cost);
+    })
+  }
+
+  play() {
+    this.inputCost();
+  }
 }
+
+const app = new App();
+app.play();
+
 
 module.exports = App;
--- src/Machine.js
+++ src/Machine.js
@@ -1,5 +1,19 @@
-class Machine {
+const { Console } = require("@woowacourse/mission-utils");
 
+const GET_COST_SENTENCE = '구입금액을 입력해 주세요.';
+
+class Machine {
+  constructor() {
+    this.count;
+  }
+
+  getCost() {
+    Console.print(GET_COST_SENTENCE);
+  }
+
+  calculateCount(cost) {
+    Console.print(cost);
+  }
 }
 
 module.exports = Machine;
