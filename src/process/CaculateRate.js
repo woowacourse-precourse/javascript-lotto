23df2f1719c032426ae6e1c4b08d929154d5139a
yesolyo <ahnyelim77@gmail.com>
2022-11-14 20:44:38 +0900

    refactor: 변수명 수정 및 계산식 변경

--- src/process/CaculateRate.js
+++ src/process/CaculateRate.js
@@ -1,17 +1,21 @@
+const Result = require("../result/Result");
+
 class CaculateRate {
-  constructor(result) {
-    this.result = result;
+  constructor(rate) {
+    this.rate = rate;
+    this.result = new Result();
   }
-  caculateNumbers(purchaseAmout, GRADE) {
+  caculateNumbers(computerNumbers, GRADE) {
     const RESULT =
       ((GRADE[4] * 5000 +
         GRADE[3] * 50000 +
         GRADE[2] * 1500000 +
         GRADE[1] * 30000000 +
         GRADE[0] * 2000000000) /
-        purchaseAmout) *
-      0.1;
-    this.result = RESULT;
+        (computerNumbers.length * 1000)) *
+      100;
+    this.rate = RESULT.toFixed(1);
+    this.result.showResult(GRADE, this.rate);
   }
 }
 
