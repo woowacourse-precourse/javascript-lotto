0e917a1e0b71f4a18856cc6f571f891103891c54
안호진 <asdv94@naver.com>
2022-11-14 22:19:02 +0900

    style(lotto): 메세지 상수 수정

--- src/Constant.js
+++ src/Constant.js
@@ -1,19 +1,20 @@
 class Constant {
   static NEW_LINE = '\n';
   static GAME_MSG = Object.freeze({
-    pleaseEnterMoney: '구입금액을 입력해주세요.\n',
-    bought: '개를 구매했습니다.\n',
-    pleaseEnterWinningNumbers: '\n당첨 번호를 입력해 주세요.\n',
-    pleaseEnterBonusNumbers: '\n보너스 번호를 입력해 주세요.\n',
+    pleaseEnterMoney: '구입금액을 입력해주세요.',
+    bought: '개를 구매했습니다.',
+    pleaseEnterWinningNumbers: '당첨 번호를 입력해 주세요.',
+    pleaseEnterBonusNumber: '보너스 번호를 입력해 주세요.',
   });
   static RESULT_MSG = Object.freeze({
-    stats: '\n당첨 통계\n---\n',
+    stats: '당첨 통계',
+    hrLine: '---',
     match3: '3개 일치 (5,000원) - ',
     match4: '4개 일치 (50,000원) - ',
     match5: '5개 일치 (1,500,000원) - ',
     matchBonus: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
     match6: '6개 일치 (2,000,000,000원) - ',
-    ea: '개\n',
+    ea: '개',
     totalProfit: '총 수익률은 ',
     isNPercent: '%입니다.',
   });
@@ -25,6 +26,7 @@ class Constant {
     only6Numbers: '로또 번호는 6개여야 합니다.',
     duplicateNumbers: '로또 번호는 중복되지 않아야 합니다.',
   });
+  static INCOMES = Object.freeze([2000000000, 30000000, 1500000, 50000, 5000]);
 }
 
 module.exports = Constant;
