1d93ea3a5017330eacfa13473ac77838a3b72882
GC-Park <kuyt3122@naver.com>
2022-11-14 14:57:07 +0900

    feat:예외 처리 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const { INPUT_LOTTO_NUMBER_ERROR } = require("./constants");
+
 class Lotto {
   #numbers;
 
@@ -8,8 +10,9 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(INPUT_LOTTO_NUMBER_ERROR);
     }
+    
   }
 
   lottoNumberSort(numbers) {
