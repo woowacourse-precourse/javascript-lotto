ccdbb63e8c643b4be30b4941273a2194c8dc080a
Hungeun2 <bepoohun@gmail.com>
2022-11-14 17:05:14 +0900

    Feat: Constant LOTTO_MESSAGE 수정 String 에서 function으로 변경

--- src/util/Constant.js
+++ src/util/Constant.js
@@ -2,7 +2,9 @@ const LOTTO_PRICE = 1000;
 
 const LOTTO_MESSAGE = {
   BUDGET: "구입금액을 입력해 주세요.\n",
-  BUYING: "개를 구매했습니다.",
+  BUYING: function (count) {
+    return count + "개를 구매했습니다.";
+  },
   WIN_NUM: "당첨 번호를 입력해 주세요.",
   BONUS_NUM: "보너스 번호를 입력해 주세요.",
 };
