2d815ad9e2a8f7a61c7fa79fe646bc081b1df1bd
kwh97 <zzzang0512@naver.com>
2022-11-15 06:16:18 +0900

    fix: Lotto 클래스 내 코드 재구성

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,25 +2,15 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
-    this.duplicate(numbers);
     this.#numbers = numbers;
+    this.validate(numbers);
   }
 
   validate(numbers) {
-    console.log(numbers);
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
-
-  duplicate(numbers) {
-    if (new Set(numbers).size < 6) {
-      throw new Error("[ERROR] 로또 번호 중에서 중복괸 번호가 있습니다.");
-    }
+    this.checkLength(numbers);
+    this.checkDuplicate(numbers);
+    this.checkRange(numbers);
   }
 }
-
-  // TODO: 추가 기능 구현
 
 module.exports = Lotto;
