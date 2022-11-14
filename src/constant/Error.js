606afa898326835e994ad5cca25fcf23b1e5e5e6
Deemou <4106243@naver.com>
2022-11-14 21:33:39 +0900

    chore(constant): 에러 메시지 변수 프로퍼티 추가

--- src/constant/Error.js
+++ src/constant/Error.js
@@ -1,4 +1,5 @@
 const ERROR_MESSAGE = Object.freeze({
+  NOT_A_NUMBER: '[ERROR] 숫자가 아닙니다.',
   NOT_ENOUGH_MONEY: '[ERROR] 구입 금액은 1,000원 이상이어야 합니다.',
   NOT_DIVIDED_BY_THOUSNAD: '[ERROR] 구입 금액은 1,000원으로 나누어 떨어져야 합니다.',
   NOT_SIX_NUMBERS: '[ERROR] 로또 번호는 6개의 숫자로 이루어져야 합니다.',
@@ -10,6 +11,7 @@ const INVALID_INPUT_ERROR = Object.freeze({
   TITLE: 'Invalid Input',
   MESSAGE: '[ERROR] 유효하지 않은 입력입니다.'
 });
+
 module.exports = {
   ERROR_MESSAGE,
   INVALID_INPUT_ERROR
