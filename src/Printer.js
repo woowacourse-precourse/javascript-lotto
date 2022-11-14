2a82bcdce0ab286ac5311e367b801f30865ae454
MAGHC <windowsapple95@gmail.com>
2022-11-14 19:31:06 +0900

    feat: 결과 출력 기능 생성

--- src/Printer.js
+++ src/Printer.js
@@ -13,6 +13,16 @@ let Printer = class {
   static askBonusNumber() {
     Uitls.printMessage(Constant.GAME_PROGRESS_MESSAGES.ENTER_BONUS_NUMBER);
   }
+
+  static result(obj, statistics) {
+    Uitls.printMessage(`
+    3개 일치 (5,000원) - ${obj[5]}개
+    4개 일치 (50,000원) - ${obj[4]}개
+    5개 일치 (1,500,000원) - ${obj[3]}개
+    5개 일치, 보너스 볼 일치 (30,000,000원) - ${obj[2]}개
+    6개 일치 (2,000,000,000원) - ${obj[1]}개
+    총수익률은 ${statistics}입니다.`);
+  }
 };
 
 module.exports = Printer;
