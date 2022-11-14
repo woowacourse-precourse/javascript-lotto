d54e6370ec8d1d7636975b44b43f5d189581229a
pinkishincoloragain <smb1103@gmail.com>
2022-11-15 04:51:00 +0900

    feat: 로또 티켓 정상 생성 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -13,4 +13,16 @@ describe('로또 클래스 테스트', () => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow('[ERROR]');
   });
+
+  test('로또 번호에 범위 밖 숫자가 있으면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호는 6개의 숫자로 이루어진다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow('[ERROR]');
+  });
 });
