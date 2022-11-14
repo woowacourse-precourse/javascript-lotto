6b37e25dbd773b2302d2eb4aab081b1eac956988
loopy-lim <loopy@jnu.ac.kr>
2022-11-15 04:03:29 +0900

    refector(app): 코드 길이를 15줄 이하로 줄인다

--- src/App.js
+++ src/App.js
@@ -11,22 +11,20 @@ class App {
   #lottoInputDto;
   #lottoPrizeDto;
 
-   play() {
-    const money =  InputConsole.getMoney();
-    const purchaseNumber = LottoValidator.getLottoPuchaseNumber(money);
-    OutputConsole.lottoPurchaseNumber(purchaseNumber);
-
-    this.#lottoPurchaseDtos = Array.from(
-      { length: purchaseNumber },
-      () => new LottoPurchaseDto(),
-    );
-    OutputConsole.lottoNumbers(this.#lottoPurchaseDtos);
-
-    const lottoNumbers =  InputConsole.getLotto();
-    const lottoAdditinalNumber =  InputConsole.getLottoAdditional(lottoNumbers);
-    this.#lottoInputDto = new LottoInputDto(lottoNumbers, lottoAdditinalNumber);
-
+  constructor() {
     this.#lottoPrizeDto = new LottoPrizeDto();
+  }
+
+  play() {
+    const { purchaseNumber, money } = this.#getMoney();
+
+    this.#makeLottoPurchaseDtos(purchaseNumber);
+    this.#makeLottoInputDto();
+
+    this.#getResult(money);
+  }
+
+  #getResult(money) {
     this.#lottoPurchaseDtos.forEach((lottoPurchaseDto) => {
       LottoValidator.checkLottoWin(
         this.#lottoInputDto,
@@ -36,6 +34,27 @@ class App {
     });
     OutputConsole.result(this.#lottoPrizeDto);
     OutputConsole.sumMoney(this.#lottoPrizeDto, money);
+  }
+
+  #makeLottoInputDto() {
+    const lottoNumbers = InputConsole.getLotto();
+    const lottoAdditinalNumber = InputConsole.getLottoAdditional(lottoNumbers);
+    this.#lottoInputDto = new LottoInputDto(lottoNumbers, lottoAdditinalNumber);
+  }
+
+  #makeLottoPurchaseDtos(purchaseNumber) {
+    this.#lottoPurchaseDtos = Array.from(
+      { length: purchaseNumber },
+      () => new LottoPurchaseDto(),
+    );
+    OutputConsole.lottoNumbers(this.#lottoPurchaseDtos);
+  }
+
+  #getMoney() {
+    const money = InputConsole.getMoney();
+    const purchaseNumber = LottoValidator.getLottoPuchaseNumber(money);
+    OutputConsole.lottoPurchaseNumber(purchaseNumber);
+    return { purchaseNumber, money };
   }
 }
 
