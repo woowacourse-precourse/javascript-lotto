a338edb92bd075cc6ae86fadcc6a4e3029fd4bcc
kitez <duswl339@ajou.ac.kr>
2022-11-14 18:03:46 +0900

    test: lotto class method test code
    + lotto 당첨 번호 갯수 계산하는 함수
    + lotto 당첨 번호 갯수 반환하는 get 함수에 대한 테스트 코드

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,66 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또 번호는 1~45 사이의 값이어야 한다. (1) 46이상 값 포함", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호는 1~45 사이의 값이어야 한다. (1) 1미만 값 포함", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 0]);
+    }).toThrow("[ERROR]");
+  });
+});
+
+describe("로또 당첨 테스트 :: calcWinning", () => {
+  test("로또 0개 당첨", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.calcWinning(["1","9","10","12","13","14"]);
+    console.log(lotto.WinningNumber);
+    expect(lotto.WinningNumber).toEqual(0);
+  });
+
+  test("로또 1개 당첨", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.calcWinning(["1","9","10","12","13","14"]);
+    console.log(lotto.WinningNumber);
+    expect(lotto.WinningNumber).toEqual(1);
+  });
+
+  test("로또 2개 당첨", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.calcWinning(["1","2","10","12","13","14"]);
+    console.log(lotto.WinningNumber);
+    expect(lotto.WinningNumber).toEqual(2);
+  });
+
+  test("로또 3개 당첨", () => {
+    const lotto = new Lotto([1, 2, 3, 5, 6, 7]);
+    lotto.calcWinning(["1","2","4","6","13","14"]);
+    console.log(lotto.WinningNumber);
+    expect(lotto.WinningNumber).toEqual(3);
+  });
+
+  test("로또 4개 당첨", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.calcWinning(["1","2","4","6","13","14"]);
+    console.log(lotto.WinningNumber);
+    expect(lotto.WinningNumber).toEqual(4);
+  });
+
+  test("로또 5개 당첨", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.calcWinning(["1","2","3","4","5","14"]);
+    console.log(lotto.WinningNumber);
+    expect(lotto.WinningNumber).toEqual(5);
+  });
+
+  test("로또 6개 당첨", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.calcWinning(["1","2","3","4","5","6"]);
+    console.log(lotto.WinningNumber);
+    expect(lotto.WinningNumber).toEqual(6);
+  });
 });
