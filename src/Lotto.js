591e9bf55684d2c2a58544154df22b1deae9b0e4
ksg2388 <ksg2388@naver.com>
2022-11-14 21:59:19 +0900

    feat(Lotto): number를 반환하는 메소드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,20 +7,23 @@ class Lotto {
   }
 
   validate(numbers) {
-    const winningNumber = numbers.split(",").map((num) => Number(num));
+    const winningNumber = numbers.split(',').map((num) => Number(num));
 
     if (winningNumber.length !== 6) {
-      throw new Error("[ERROR] 잘못된 입력입니다.");
+      throw new Error('[ERROR] 잘못된 입력입니다.');
     }
     if (winningNumber.filter((num) => num >= 1 && num <= 45).length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
     }
     if (new Set(winningNumber).size !== 6) {
-      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  getNumber() {
+    return this.#numbers;
+  }
 }
 
 module.exports = Lotto;
