fd0c3ce1aefdad4f6b2f12576b477c79ae061e99
nalsae <nalsae0207@naver.com>
2022-11-14 19:28:25 +0900

    test: add tests to confirm features
    구현 기능의 정상 동작 여부를 확인하기 위한 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,79 +1,79 @@
-const Lotto = require('../src/components/Lotto');
+const Lotto = require("../src/components/Lotto");
 
-describe('당첨 번호 예외 테스트', () => {
-  test('당첨 번호로 숫자가 아닌 값을 입력하는 경우 예외가 발생한다.', () => {
+describe("당첨 번호 예외 테스트", () => {
+  test("당첨 번호로 숫자가 아닌 값을 입력하는 경우 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto(['안녕하세요']);
-    }).toThrow('[ERROR]');
+      new Lotto(["안녕하세요"]);
+    }).toThrow("[ERROR]");
   });
 
-  test('당첨 번호로 숫자가 아닌 값을 입력하는 경우 예외가 발생한다.', () => {
+  test("당첨 번호로 숫자가 아닌 값을 입력하는 경우 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto(['#$@!@!$']);
-    }).toThrow('[ERROR]');
+      new Lotto(["#$@!@!$"]);
+    }).toThrow("[ERROR]");
   });
 
-  test('당첨 번호의 개수가 6개를 초과하면 예외가 발생한다.', () => {
+  test("당첨 번호의 개수가 6개를 초과하면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow('[ERROR]');
+    }).toThrow("[ERROR]");
   });
 
-  test('당첨 번호의 개수가 6개보다 적으면 예외가 발생한다.', () => {
+  test("당첨 번호의 개수가 6개보다 적으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5]);
-    }).toThrow('[ERROR]');
+    }).toThrow("[ERROR]");
   });
 
-  test('당첨 번호의 숫자가 1보다 작으면 예외가 발생한다.', () => {
+  test("당첨 번호의 숫자가 1보다 작으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 0]);
-    }).toThrow('[ERROR]');
+    }).toThrow("[ERROR]");
   });
 
-  test('당첨 번호의 숫자가 45보다 크면 예외가 발생한다.', () => {
+  test("당첨 번호의 숫자가 45보다 크면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 48, 8]);
-    }).toThrow('[ERROR]');
+    }).toThrow("[ERROR]");
   });
 
-  test('당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
+  test("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow('[ERROR]');
+    }).toThrow("[ERROR]");
   });
 });
 
-describe('보너스 번호 예외 테스트', () => {
+describe("보너스 번호 예외 테스트", () => {
   const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
 
-  test('보너스 번호로 숫자가 아닌 값을 입력하는 경우 예외가 발생한다.', () => {
+  test("보너스 번호로 숫자가 아닌 값을 입력하는 경우 예외가 발생한다.", () => {
     expect(() => {
-      lotto.validateBonusNumber('우테코');
-    }).toThrow('[ERROR]');
+      lotto.validateBonusNumber("우테코");
+    }).toThrow("[ERROR]");
   });
 
-  test('보너스 번호로 숫자가 아닌 값을 입력하는 경우 예외가 발생한다.', () => {
+  test("보너스 번호로 숫자가 아닌 값을 입력하는 경우 예외가 발생한다.", () => {
     expect(() => {
-      lotto.validateBonusNumber('*^^*');
-    }).toThrow('[ERROR]');
+      lotto.validateBonusNumber("*^^*");
+    }).toThrow("[ERROR]");
   });
 
-  test('보너스 번호의 숫자가 1보다 작으면 예외가 발생한다.', () => {
+  test("보너스 번호의 숫자가 1보다 작으면 예외가 발생한다.", () => {
     expect(() => {
       lotto.validateBonusNumber(0);
-    }).toThrow('[ERROR]');
+    }).toThrow("[ERROR]");
   });
 
-  test('보너스 번호의 숫자가 45보다 크면 예외가 발생한다.', () => {
+  test("보너스 번호의 숫자가 45보다 크면 예외가 발생한다.", () => {
     expect(() => {
       lotto.validateBonusNumber(50);
-    }).toThrow('[ERROR]');
+    }).toThrow("[ERROR]");
   });
 
-  test('보너스 번호의 숫자가 당첨 번호와 중복되는 경우 예외가 발생한다.', () => {
+  test("보너스 번호의 숫자가 당첨 번호와 중복되는 경우 예외가 발생한다.", () => {
     expect(() => {
       lotto.validateBonusNumber(6);
-    }).toThrow('[ERROR]');
+    }).toThrow("[ERROR]");
   });
 });
