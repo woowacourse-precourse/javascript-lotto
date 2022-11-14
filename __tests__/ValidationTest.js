d6efd0ce2e8b93ea7c3acf1f3e46f341bb0b8826
Monte <monte6198@gmail.com>
2022-11-14 09:01:32 +0900

    feat: 로또 구입 금액 정수 범위 벗어난 경우 예외사항 테스트

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,10 +1,20 @@
 const Validation = require("../src/Validation");
+const MAX_INTEGER = Number.MAX_SAFE_INTEGER;
 
 describe("Validation 단위 테스트", () => {
   test("1,000의 배수 여부 확인 테스트", () => {
     const validation = Validation;
     const result1 = validation.isDivisible(170000);
     const result2 = validation.isDivisible(170123);
+
+    expect(result1).toEqual(true);
+    expect(result2).toEqual(false);
+  });
+
+  test("정수 범위 벗어난 구입 금액 테스트", () => {
+    const validation = Validation;
+    const result1 = validation.isAvailablePurchase(MAX_INTEGER);
+    const result2 = validation.isAvailablePurchase(MAX_INTEGER + 1);
 
     expect(result1).toEqual(true);
     expect(result2).toEqual(false);
