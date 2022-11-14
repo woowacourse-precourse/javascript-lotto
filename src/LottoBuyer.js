41334e1606886f5dc868f8e4c03db871223652db
foundaway98 <foundaway98@naver.com>
2022-11-14 16:13:58 +0900

    <feat> : 로또 구매 기능 구현
    로또 구매 기능 구현
    - LottoBuyer 클래스에 buyLotto 메소드 생성

--- src/LottoBuyer.js
+++ src/LottoBuyer.js
@@ -2,9 +2,25 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto.js");
 class LottoBuyer {
   money;
+  lottoAmount;
   constructor(money) {
     money = parseInt(money);
+
     this.money = money;
+    this.lottoAmount = money / 1000;
+  }
+  buyLotto() {
+    MissionUtils.Console.print(`${this.lottoAmount}개를 구매했습니다.`);
+    for (
+      let lottoAmountCount = 0;
+      lottoAmountCount < this.lottoAmount;
+      lottoAmountCount++
+    ) {
+      let boughtLotto = new Lotto(
+        MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
+      );
+      this.lottoArray.push(boughtLotto);
+    }
   }
 }
 module.exports = LottoBuyer;
