b59189ef36ebd0cecb36677572b6c618b60c0720
cruelladevil <dev.timetravel@gmail.com>
2022-11-14 23:52:41 +0900

    feat(Player): add addPrizeCounts, addWinMoney

--- src/Player.js
+++ src/Player.js
@@ -1,15 +1,35 @@
 const Machine = require('./Machine');
 
+const { PRIZE } = require('./constants/prize');
+
 class Player {
   constructor() {
     this.spentMoney = 0;
     this.lottos = [];
+    this.winMoney = 0;
+    this.prizeCounts = new Map([
+      [PRIZE.FIFTH, 0],
+      [PRIZE.FOURTH, 0],
+      [PRIZE.THIRD, 0],
+      [PRIZE.SECOND, 0],
+      [PRIZE.FIRST, 0],
+    ]);
   }
 
   buyLottos(money) {
     const lottoCount = money / 1000;
     this.spentMoney = money;
     this.lottos = Array.from({ length: lottoCount }, () => Machine.publishLotto());
+  }
+
+  addPrizeCounts(prize) {
+    const prizeCount = this.prizeCounts.get(prize);
+
+    this.prizeCounts.set(prize, prizeCount + 1);
+  }
+
+  addWinMoney(winMoney) {
+    this.winMoney += winMoney;
   }
 }
 
