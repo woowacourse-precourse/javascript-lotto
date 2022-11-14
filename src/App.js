1b86f56ff6bab31fb1fc8ee4df7d6127cfce93ee
NaveOWO <pianowell96@gmail.com>
2022-11-14 21:39:57 +0900

    [ feat ] 로또 구입 금액 입력받는 기능구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.buyMoney = 0;
+  }
+
+  play() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요\n", (answer) => {
+      let lottoNum = 0;
+      this.buyMoney = answer;
+    });
+  }
 }
 
 module.exports = App;
