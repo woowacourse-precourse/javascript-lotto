6765e9ad57eef2516bcb92342d044d4d7a273d32
seohee0112 <ghdtj7@naver.com>
2022-11-14 20:25:14 +0900

    feat : 필요한 변수 선언

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { Random } = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+const {
+  isPurchasePriceToNumber,
+  getTotalProfit,
+} = require("./util/validate/purchase");
+const {
+  LOTTO_PURCHASE_PRICE,
+  INPUT_MESSAGE,
+  ERROR_MESSAGE,
+  RESULT_MESSAGE,
+  STATISTIC,
+  PRIZE_KEY,
+  PRIZE_REWARD,
+  PRIZE_RESULT,
+} = require("./util/constants");
 class App {
+  #lotto;
+
+  price;
+  winNumberList = [];
+  lottoList = [];
+  bonusNumber;
+
+  constructor() {}
   play() {}
 }
+
+const game = new App();
+game.play();
 
 module.exports = App;
