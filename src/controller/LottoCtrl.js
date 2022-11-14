0ffa22ba59650bcc11b2b8c29fc0f93a073d8d5a
dev-redo <mis05041@naver.com>
2022-11-14 22:42:33 +0900

    refactor(LottoCtrl): LottoCtrl을 MVC패턴에 맞게 리펙토링 및 당첨 번호 입력기능 구현
    - refactor: 로또를 구매하는데 필요한 데이터를 model에 저장하게끔 수정
    - 당첨번호 입력 기능 구현

--- src/controller/LottoCtrl.js
+++ src/controller/LottoCtrl.js
@@ -1,4 +1,9 @@
-const { BUDGET_MESSAGE, TICKET_MESSAGE } = require('../constants/lotto');
+const {
+  BUDGET_MESSAGE,
+  TICKET_MESSAGE,
+  WINNING_NUMBER_MESSAGE,
+  TICKET_NUMBER,
+} = require('../constants/lotto');
 const GameCtrl = require('./GameCtrl');
 const LottoView = require('../view/LottoView');
 const LottoModel = require('../model/LottoModel');
@@ -9,40 +14,55 @@ const LottoCtrl = class extends GameCtrl {
   }
 
   start() {
-    this.view.input(BUDGET_MESSAGE, this.gameProcess.bind(this));
+    this.gameProcess();
   }
 
-  // 1. 유저로부터 로또 구입 금액을 입력받는다. ✅
-  gameProcess(budget) {
-    this.budget = Number(budget);
-    this.model.setLottoBudget(this.budget);
-
-    this.buyLottoTicket();
+  gameProcess() {
+    this.inputLottoBudget();
   }
 
-  // 2. 컴퓨터에서 자동으로 로또 개수만큼 로또를 구입한다. ✅
+  inputLottoBudget() {
+    const onInputLottoBudget = budget => {
+      this.model.setLottoBudget(budget);
+      this.buyLottoTicket();
+    };
+
+    this.view.input(BUDGET_MESSAGE, onInputLottoBudget);
+  }
+
   buyLottoTicket() {
-    this.setLottoCount();
+    this.getLottoCount();
 
-    const lottoTickets = [...new Array(this.ticketCount)].reduce(tickets => {
-      const currTicket = this.model.pickLottoTickets(1, 45, 6);
+    const { ticketCount } = this.model.Budget;
+    const lottoTickets = [...new Array(ticketCount)].reduce(tickets => {
+      const currTicket = this.model.pickLottoTickets(TICKET_NUMBER);
       tickets.push(currTicket);
 
       return tickets;
     }, []);
 
+    this.model.lottoTickets = lottoTickets;
     this.view.output(lottoTickets);
-    this.lottoTickets = lottoTickets;
+
+    this.inputLottoWinningNumbers();
   }
 
-  setLottoCount() {
-    this.ticketCount = this.model.setLottoCount();
-    const ticketMessage = `${this.ticketCount}${TICKET_MESSAGE}`;
+  getLottoCount() {
+    const ticketCount = this.model.setLottoCount();
+    const ticketMessage = `${ticketCount}${TICKET_MESSAGE}`;
 
     this.view.output(ticketMessage);
   }
 
-  // 3. 유저로부터 로또 당첨 번호를 입력받는다.
+  inputLottoWinningNumbers() {
+    const onGetLottoWinningNumbers = winningNumbers => {
+      winningNumbers = winningNumbers.split(',');
+      this.model.winningNumbers = winningNumbers;
+    };
+
+    this.view.input(WINNING_NUMBER_MESSAGE, onGetLottoWinningNumbers);
+  }
+
   // 4. 유저로부터 보너스 번호를 입력받는다.
 
   // end method
