adcde4e261258ec876079a786ed452394e726b53
Minju Park <project.mizzu@gmail.com>
2022-11-14 23:24:24 +0900

    Feat : 구입금액에 해당하는 만큼 로또 구매수량 반환
    - 입력받은 금액을 1000원으로 나눈 구매수량 반환

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ class App {
   }
 
   play() {
-    Console.readLine(this.text, (answer) => Console.print(answer));
+    Console.readLine(this.text, (cost) => {
+      const quantity = cost / 1000;
+      Console.print(`${'\n'}${quantity}개를 구매했습니다.`);
+    });
   }
 }
 
-const test = new App('구입금액을 입력해 주세요.');
+const test = new App(`구입금액을 입력해 주세요.${'\n'}`);
 test.play();
 
 module.exports = App;
