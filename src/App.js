f5a1e721c7ed9d4a9b4778686ab23ba92cbe0ea3
wusi-hub <jws9595@naver.com>
2022-11-15 08:01:24 +0900

    feat: 당첨과 갯수를 구분하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ const { Console, Random } = require("@woowacourse/mission-utils");
 const PURCHASE_MONEY = '구입금액을 입력해 주세요.';
 const LOTTO_WINDOW = '당첨 번호를 입력해 주세요.'
 const BONUS_WINDOW = '보너스 번호를 입력해 주세요.'
+const ANSWER_STATS = '\n당첨 통계\n---';
+const SAME_THREE = '3개 일치 (5,000원)';
+const SAME_FOUR = '4개 일치 (50,000원)';
+const SAME_FIVE = '5개 일치 (1,500,000원)';
+const SAME_FIVE_BONUS = '5개 일치, 보너스 볼 일치 (30,000,000원)';
+const SAME_SIX = '6개 일치 (2,000,000,000원)';
 const PRICE_LOTTO = 1000;
 
 class App {
@@ -10,6 +16,13 @@ class App {
     this.lottoArray = [];
     this.winningNumber = 0;
     this.bonusNumber = 0;
+    this.answer = {
+      SAME_THREE: 0,
+      SAME_FOUR: 0,
+      SAME_FIVE: 0,
+      SAME_FIVE_BONUS: 0,
+      SAME_SIX: 0
+    };
   }
 
   play() {
@@ -58,8 +71,15 @@ class App {
       numbers.map((number) => {
         if(this.winningNumber.includes(number)) sameNumber += 1;
       });
-      console.log(sameNumber);
+      this.winSeparate(sameNumber);
     });
+    Console.print(this.answer);
+  }
+  winSeparate(count) {
+    if (count === 3) this.answer['SAME_THREE'] += 1;
+    if (count === 4) this.answer['SAME_FOUR'] += 1;
+    if (count === 5) this.answer['SAME_FIVE'] += 1;
+    if (count === 6) this.answer['SAME_SIX'] += 1;
   }
 }
 
