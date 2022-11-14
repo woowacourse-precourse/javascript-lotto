ed9805b9006195d2b88965c6d2ac13dca0010cff
useon <yoosk4986@naver.com>
2022-11-15 05:53:21 +0900

    feat(Draw): 문구를 출력하고, 보너스 번호를 입력받는다

--- src/Draw.js
+++ src/Draw.js
@@ -4,9 +4,19 @@ const Validation = require('./Validation');
 class Draw {
   winning() {
     Console.readLine('당첨번호를 입력해 주세요.', (inputValue) => {
-      new Validation().winningInputValue(inputValue);
+      new Validation().winningValue(inputValue);
       this.winningNumbers = inputValue.split(',').map((item) => Number(item));
       Console.close();
+      this.bonus();
+      this.totalNumbers = [...this.winningNumbers, this.bonusNumber];
+    });
+    return this.totalNumbers;
+  }
+
+  bonus() {
+    Console.readLine('보너스 번호를 입력해 주세요.', (inputValue) => {
+      Console.close();
+      this.bonusNumber = Number(inputValue);
     });
   }
 }
