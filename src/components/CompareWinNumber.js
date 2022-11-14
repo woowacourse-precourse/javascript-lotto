182b5302195a1b3c98e78d09cc501110faa0e606
semnil5202 <semnil5202@gmail.com>
2022-11-15 05:59:01 +0900

    refactor(CompareWinNumber): 접근제한자 및 정적 메서드 수정
    JudgeLotto 클래스는 발행한 로또의 당첨 번호와 보너스 번호를 판별하는 기능만 담당한다.
    따라서 특별히 인스턴스를 생성하여 사용하지 않을 것이므로 모든 메서드를 정적 메서드로 변경한다.
    또한 외부에서 접근할 compareLotto 메서드를 제외하고 모두 private 메서드로 변경한다.

--- src/components/CompareWinNumber.js
+++ src/components/CompareWinNumber.js
@@ -1,25 +1,25 @@
 class JudgeLotto {
-  compareLotto({ lottos, wins, bonus }) {
+  static compareLotto({ lottos, wins, bonus }) {
     const winAndBonus = [];
     lottos.forEach((lotto) => {
-      winAndBonus.push(this.checkWin({ lotto, wins, bonus }));
+      winAndBonus.push(JudgeLotto.#checkWin({ lotto, wins, bonus }));
     });
     return winAndBonus;
   }
 
-  checkWin({ lotto, wins, bonus }) {
+  static #checkWin({ lotto, wins, bonus }) {
     let winCount = 0;
     let isBonus = false;
     wins.forEach((number) => {
       if (lotto.getNumbers().includes(number)) winCount += 1;
     });
     if (winCount === 5) {
-      isBonus = this.checkBonus({ lotto, bonus, isBonus });
+      isBonus = JudgeLotto.#checkBonus({ lotto, bonus, isBonus });
     }
     return [winCount, isBonus];
   }
 
-  checkBonus({ lotto, bonus, isBonus }) {
+  static #checkBonus({ lotto, bonus, isBonus }) {
     lotto.getNumbers().forEach((number) => {
       if (number === bonus) isBonus = true;
     });
