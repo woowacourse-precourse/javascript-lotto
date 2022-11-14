a7655795b5eb3d14280ea6f92610947312ef4feb
ksg2388 <ksg2388@naver.com>
2022-11-14 20:10:05 +0900

    test(UI): 입력된 금액에 대한 유효성 검사 테스트 케이스 추가

--- src/libs/ui/Ui.js
+++ src/libs/ui/Ui.js
@@ -1,9 +1,9 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const { lottoStatistics } = require("../lottoFunction/lottoResult");
-const Lotto = require("../../Lotto");
+const MissionUtils = require('@woowacourse/mission-utils');
+const { lottoStatistics } = require('../lottoFunction/lottoResult');
+const Lotto = require('../../Lotto');
 
 const accountInput = () => {
-  MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (input) => {
+  MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
     checkAccountValidation(input);
     lottoPrint(input);
     generateNumber(input);
@@ -11,20 +11,20 @@ const accountInput = () => {
 };
 
 const lottoPrint = (account) => {
-  MissionUtils.Console.print(`${account / 1000}개를 구매했습니다.`);
+  MissionUtils.Console.print(`\n${account / 1000}개를 구매했습니다.`);
 };
 
 const checkAccountValidation = (input) => {
   const account = Number(input);
 
   if (input.length !== String(account).length) {
-    throw new Error("[ERROR] 잘못된 입력입니다.");
+    throw new Error('[ERROR] 잘못된 입력입니다.');
   }
   if (account < 1000) {
-    throw new Error("[ERROR] 1000원 이상 입력해주세요.");
+    throw new Error('[ERROR] 1000원 이상 입력해주세요.');
   }
   if (account % 1000 !== 0) {
-    throw new Error("[ERROR] 1000원 단위로 입력해주세요.");
+    throw new Error('[ERROR] 1000원 단위로 입력해주세요.');
   }
 };
 
@@ -34,7 +34,7 @@ const generateNumber = (count) => {
 
   for (let i = 0; i < lottoCount; i++) {
     const lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
-      (a, b) => a - b
+      (a, b) => a - b,
     );
     lottos.push(lotto);
   }
@@ -44,34 +44,34 @@ const generateNumber = (count) => {
 
 const printLottoNumber = (tickets) => {
   for (const ticket of tickets) {
-    let output = "";
+    let output = '';
     for (const num of ticket) {
     }
   }
   tickets.map((element) =>
-    MissionUtils.Console.print(`[${element.join(", ")}]`)
+    MissionUtils.Console.print(`[${element.join(', ')}]`),
   );
   winningNumberInput(tickets);
 };
 
 const winningNumberInput = (tickets) => {
   MissionUtils.Console.readLine(
-    "\n당첨 번호를 입력해 주세요.\n",
+    '\n당첨 번호를 입력해 주세요.\n',
     (winningNumber) => {
       const lotto = new Lotto(winningNumber);
       bonusNumberInput(winningNumber, tickets);
-    }
+    },
   );
 };
 
 const bonusNumberInput = (winningNumber, tickets) => {
-  winningNumber = winningNumber.split(",").map((num) => Number(num));
+  winningNumber = winningNumber.split(',').map((num) => Number(num));
   MissionUtils.Console.readLine(
-    "\n보너스 번호를 입력해 주세요.\n",
+    '\n보너스 번호를 입력해 주세요.\n',
     (bonusNumber) => {
       checkBonusValidation(bonusNumber, winningNumber);
       lottoStatistics(tickets, winningNumber, bonusNumber);
-    }
+    },
   );
 };
 
@@ -79,7 +79,7 @@ const checkBonusValidation = (bonusNumber, winningNumber) => {
   bonusNumber = Number(bonusNumber);
   winningNumber.map((num) => {
     if (num === bonusNumber) {
-      throw new Error("[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.");
+      throw new Error('[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.');
     }
   });
 };
@@ -88,4 +88,6 @@ module.exports = {
   accountInput,
   winningNumberInput,
   bonusNumberInput,
+  checkAccountValidation,
+  checkBonusValidation,
 };
