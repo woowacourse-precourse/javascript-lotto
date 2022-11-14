3cd9297f825b2afcc2762920b496b3f7d461d445
Inae Choi <inaechoi89@gmail.com>
2022-11-14 10:06:35 +0900

    feat: add a function to validate money

--- src/Purchase.js
+++ src/Purchase.js
@@ -5,6 +5,11 @@ class Purchase {
   static inputMoney() {
     MissionUtils.Console.readLine(Constant.ORDER_MESSAGE, Purchase.validate);
   }
+
+  static validate(money) {
+    const moneyValidation = new MoneyValidator(money);
+    MissionUtils.Console.print(moneyValidation.money);
+  }
 }
 
 module.exports = Purchase;
