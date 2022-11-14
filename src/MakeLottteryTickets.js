9160122667df2a72ea0df5183672b2e0d3478808
feb-dain <feb.dain@gmail.com>
2022-11-14 15:47:50 +0900

    feat: create lottery tickets according to money

--- src/MakeLottteryTickets.js
+++ src/MakeLottteryTickets.js
@@ -1,8 +1,18 @@
 const { RANDOM_UTIL } = require("./Constants");
 
 class MakeLotteryTickets {
+  constructor(userMoney, numberOfTickets) {
+    this.userMoney = userMoney;
+    this.numberOfTickets = numberOfTickets;
+    this.lotteryTickets = [];
+  }
+
   makeTickets() {
-    const randomNumbers = RANDOM_UTIL.pickUniqueNumbersInRange(1, 45, 6);
+    let numberOfRepetition = this.numberOfTickets + 1;
+    while (numberOfRepetition--) {
+      const randomNumbers = RANDOM_UTIL.pickUniqueNumbersInRange(1, 45, 6);
+      this.lotteryTickets.push(randomNumbers);
+    }
   }
 }
 
