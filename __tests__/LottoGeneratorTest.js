3a11c8699b28d8007c0948f14da1704fe0f3b401
oseunghyun <ohsh1477@gmail.com>
2022-11-14 17:04:44 +0900

    test(LottoGeneratorTest): modify test as class changed

--- __tests__/LottoGeneratorTest.js
+++ __tests__/LottoGeneratorTest.js
@@ -1,27 +1,28 @@
 const LottoGenerator = require('../src/domain/LottoGenerator');
+const lottoGenerator = new LottoGenerator();
 
-describe('로또 생성기 클래스 - 금액 입력 테스트', () => {
+describe('로또 생성기 클래스 - 구입 금액 유효성 테스트', () => {
   test('로또 구매 금액을 입력하지 않으면 예외가 발생한다.', () => {
     expect(() => {
-      new LottoGenerator();
+      lottoGenerator.isInputMoneyValid();
     }).toThrow('[ERROR] 구입 금액을 입력해야 합니다.');
   });
 
   test('로또 구매 금액에 숫자가 아닌 값을 입력하면 예외가 발생한다.', () => {
     expect(() => {
-      new LottoGenerator('외상 되나요?');
+      lottoGenerator.isInputMoneyValid('외상 되나요?');
     }).toThrow('[ERROR] 숫자만 입력할 수 있습니다.');
   });
 
   test('로또 구매 금액에 0 이하의 값을 입력하면 예외가 발생한다.', () => {
     expect(() => {
-      new LottoGenerator(-1000);
+      lottoGenerator.isInputMoneyValid(-1000);
     }).toThrow('[ERROR] 금액은 0보다 커야 합니다.');
   });
 
   test('로또 구매 금액을 1000원 단위로 입력하지 않으면 예외가 발생한다.', () => {
     expect(() => {
-      new LottoGenerator(34280);
+      lottoGenerator.isInputMoneyValid(34280);
     }).toThrow('[ERROR] 천 원 단위로 입력해야 합니다.');
   });
 });
