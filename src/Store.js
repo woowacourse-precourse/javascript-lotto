f0a30bd809391f3b8c9c44b026ee57c6fa802baf
nailen1 <nietsnie1@gmail.com>
2022-11-14 23:35:51 +0900

    feat: 구입한 로또들의 총 획득 상금, 수익률 계산 기능

--- src/Store.js
+++ src/Store.js
@@ -1,10 +1,23 @@
 const { generateRandomNumbers } = require('./Toolbox');
 
 class Store {
+    #prizeList = [
+        { text: '3개 미만 일치 (0원)', money: 0 },
+        { text: '3개 일치 (5,000원)', money: 5000 },
+        { text: '4개 일치 (50,000원)', money: 50000 },
+        { text: '5개 일치 (1,500,000원)', money: 1500000 },
+        { text: '5개 일치, 보너스 볼 일치 (30,000,000원)', money: 30000000 },
+        { text: '6개 일치 (2,000,000,000원)', money: 2000000000 },
+    ];
     constructor() {
         this.purchase;
         this.amount;
         this.generatedLottos = [];
+        this.winningNumbers;
+        this.bonusNumber;
+        this.record = this.#prizeList;
+        this.earning;
+        this.earningRatio;
     }
 
     setStoreVars(number) {
@@ -15,6 +28,23 @@ class Store {
             this.generatedLottos.push(randomSixNumbers);
         }
     }
+
+    setWinningNumbers(numbers) {
+        this.winningNumbers = numbers.split(',').map(x => Number(x));
+    }
+
+    setBonusNumber(number) {
+        this.bonusNumber = Number(number);
+    }
+
+    setEarning() {
+        this.earning = this.record.map(x => x.money * x.numbers).reduce((i, j) => i + j);
+    }
+
+    setEarningRatio() {
+        this.earningRatio = (100 * this.earning / this.purchase).toFixed(1);
+    }
+
 }
 
 module.exports = Store;
--- src/Toolbox.js
+++ src/Toolbox.js
@@ -32,6 +32,15 @@ function getRecord(storeClass, lottoClasses) {
     });
 }
 
+function showRecord(storeClass) {
+    printMessage('\n당첨통계');
+    printMessage('---');
+    storeClass.record.forEach(prize => printMessage(`${prize.text} - ${prize.numbers}개`));
+}
+
+function showEarningRatio(storeClass) {
+    printMessage(`총 수익률은 ${storeClass.earningRatio}%입니다.`)
+}
 
 module.exports = {
     showMylottos,
@@ -41,4 +50,6 @@ module.exports = {
     getPrizeResult,
     numberOfPrizes,
     getRecord,
+    showRecord,
+    showEarningRatio,
 }
