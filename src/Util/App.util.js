7b50cfdf200403b5d5af30331d11d06cc54c8a89
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-14 15:23:04 +0900

    feat: App Util buyLottos Func

--- src/Util/App.util.js
+++ src/Util/App.util.js
@@ -1,4 +1,13 @@
-const buyLottos = () => {};
+const { Random } = require("@woowacourse/mission-utils");
+const Lotto = require("../Lotto");
+
+const buyLottos = (lottoLength) =>
+  Array.from({ length: lottoLength }, (x) => {
+    const input = Random.pickUniqueNumbersInRange(1, 45, 6);
+    input.sort((a, b) => a - b);
+    return new Lotto(input);
+  });
+
 const getStatistics = () => {};
 const getRateOfReturn = () => {};
 
