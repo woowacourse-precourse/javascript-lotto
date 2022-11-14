ddc5f5a80d4f9d5a2447fa09d8a2a27ec54afdb2
vinivin153 <vinivin153@naver.com>
2022-11-15 00:00:56 +0900

    feat: 로또 등수 확인하는 테스트 추가

--- __tests__/LottoGameTest.js
+++ __tests__/LottoGameTest.js
@@ -8,4 +8,22 @@ describe('로또 번호 비교', () => {
 
     expect(result).toEqual(3);
   });
+
+  test('로또 등수 확인하는 테스트1.', () => {
+    const input = new Input();
+    input.winningNumbers = [1, 2, 3, 4, 5, 6];
+    input.bonusNumber = 7;
+    const result = input.getLottoRank(5);
+
+    expect(result).toEqual(3);
+  });
+
+  test('로또 등수 확인하는 테스트2.', () => {
+    const input = new Input();
+    input.winningNumbers = [1, 2, 3, 4, 5, 6];
+    input.bonusNumber = 3;
+    const result = input.getLottoRank(5);
+
+    expect(result).toEqual(2);
+  });
 });
