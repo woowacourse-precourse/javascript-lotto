8339d01e61becb564aad4c11f799953c53786f7f
kwh97 <zzzang0512@naver.com>
2022-11-15 03:43:03 +0900

    refactor: App.js 코드 수정 getPurchaseAmount() -> inputPurchaseAmount() 으로 함수명 변경 및 코드 수정 printPurchaseCount() -> 로또개수를 얻는 getPurchaseCount() 함수와 이를 출력하는 printPurchaseCount() 함수로 분리 getUserLottoNumbers -> inputWinNumber() 으로 함수명 변경 및 코드 수정 getUserBonusNumbers() -> inputBonusNumber() 으로 함수명 변경 및 코드 수정 getCountOfMatchingNumber() -> compare() 으로 함수명 변경 및 코드 수정 getLottoResult() -> getMatchingCount() 으로 함수명 변경 및 코드 수정 constructor 내에 변수 추가

--- src/App.js
+++ src/App.js
@@ -1,111 +1,110 @@
-const { Console } = require('@woowacourse/mission-utils');
-const { Random } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 
-const Lotto = require('./Lotto');
-const Bonus = require('./Bonus');
 const Error = require('./components/Error');
+const Bonus = require('./Bonus');
+const Lotto = require('./Lotto');
 
 class App {
-  #money;
-
-  #count;
+  constructor() {
+    this.money = 0;
+    this.count = 0;
+    this.winNumber = 0;
+    this.bonusNumber = 0;
+    this.lottoList = [];
+    this.matchingCount = {three: 0, four: 0, five: 0, fiveBonus: 0, six: 0};
+  }
 
   play() {
     this.error = new Error();
-    this.getPurchaseAmount();
+    this.inputPurchaseAmount();
   }
 
-  getPurchaseAmount() {
+  inputPurchaseAmount() {
     Console.readLine('구매금액을 입력해 주세요. \n', (money) => {
       if (this.error.isDividedByTen(money)) {
-        this.#money = money;
-        this.printPurchasemoney(money);
+        this.money = money;
+        this.getPurchaseCount();
         this.getLottoList();
       }
     });
   }
 
-  printPurchaseCount (money) {
-    const count = money / 1000;
-    Console.print(`${amount}개를 구매했습니다.`);
-    this.#count = count;
+  getPurchaseCount() {
+    const count = this.money / 1000;
+    this.count = count;
+    this.printPurchaseCount();
+  }
+
+  printPurchaseCount() {
+    Console.print(`${this.count}개를 구매했습니다.`);
   }
 
   getLottoList() {
-    const amount = this.#amount;
-    let lottoList = [];
     let countIndex = 0;
 
-    while (countIndex < amount) {
+    while (countIndex < this.count) {
       const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
       lottoNumbers.sort((a, b) => a - b);
-      lottoList.push(lottoNumbers);
+      this.lottoList.push(lottoNumbers);
       countIndex++;
     }
 
-    this.printLottoList(lottoList);
+    this.printLottoList();
   }
 
-  printLottoList(lottoList) {
-    lottoList.forEach((lottoNumbers) => {
+  printLottoList() {
+    this.lottoList.forEach((lottoNumbers) => {
       Console.print(lottoNumbers);
     });
 
-    this.inputWinningNumber(lottoList);
+    this.inputWinNumber();  
   }
 
-  inputWinningNumber(lottoList) {
+  inputWinNumber() {
     Console.readLine('당첨 번호를 입력해 주세요. \n', (numbers) => {
-      const  winningNumber = numbers.split(',');
-      new Lotto(winningNumber);
-      this.inputBonus(winningNumber, lottoList);
+      const  winNumber = numbers.split(',');
+      new Lotto(winNumber);
+      this.winNumber = winNumber;
+      this.inputBonusNumber();
     });
   }
 
-  inputBonus(winningNumber, lottoList) {
+  inputBonusNumber() {
     Console.readLine('보너스 번호를 입력해 주세요. \n', (bonus) => {
-      new Bonus(bonus, winningNumber);
-      this.getCountOfMatchingNumber(lottoList, winningNumber, bonus);
+      new Bonus(bonus, this.winNumber);
+      this.bonusNumber = bonus;
+      this.compare();
     });
   }
 
-  getCountOfMatchingNumber(lottoList, userLottoNumber, userBonusNumber) {
-    let result = {
-      three: 0,
-      four: 0,
-      five: 0,
-      fiveBonus: 0,
-      six: 0,
-    };
-
-    lottoList.forEach((list) => {
+  compare() {
+    this.lottoList.forEach((list) => {
       let count = { normal: 0, bonus: 0 };
-
       list.forEach((number) => {
-        if (userLottoNumber.map(Number).includes(number) === true) {
+        if (this.winNumber.map(Number).includes(number) === true) {
           count.normal++;
         }
 
-        if (userBonusNumber.includes(number) === true) {
+        if (this.bonusNumber.includes(number) === true) {
           count.bonus++;
         }
       });
-
-      this.getLottoResult(count.normal, count.bonus, result);
+      this.getMatchingCount(count.normal, count.bonus);
     });
+    console.log(this.matchingCount);
   }
 
-  getLottoResult(normalCount, bonusCount, result) {
+  getMatchingCount(normalCount, bonusCount) {
     if (normalCount === 3) {
-      result.three++;
+      this.matchingCount.three++;
     } else if (normalCount === 4) {
-      result.four++;
+      this.matchingCount.four++;
     } else if (normalCount === 5) {
-      result.five++;
+      this.matchingCount.five++;
     } else if (normalCount === 5 && bonusCount === 1) {
-      result.fiveBonus++;
+      this.matchingCount.fiveBonus++;
     } else if (normalCount === 6) {
-      result.six++;
+      this.matchingCount.six++;
     }
   }
 }
