3a2ef3acfc33108fc27c96fe6d552748b8fd9a6a
unsnruu <unsnruu@gmail.com>
2022-11-14 21:36:30 +0900

    feat : validate에 반복되는 수가 있는지 검사하는 메서드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -5,17 +5,24 @@ class Lotto {
     this.validate(numbers);
     this.#numbers = numbers;
   }
+  isReapted(arr) {
+    const lottoVisited = Array.from({ length: 46 }, (_) => false);
+
+    for (let idx = 0; idx < arr.length; idx++) {
+      const elem = arr[idx];
+      if (lottoVisited[elem]) return true;
+      lottoVisited[elem] = true;
+    }
+    return false;
+  }
 
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-  }
-  set numbers(numbers) {
-    this.#numbers = numbers;
-  }
-  get numbers() {
-    return this.#numbers;
+    if (this.isReapted(numbers)) {
+      throw new Error("[ERROR] 중복된 번호가 존재합니다.");
+    }
   }
 }
 
