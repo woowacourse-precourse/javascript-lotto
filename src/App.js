4747de2639b2845528dd70b09ea388112ef6d9ea
hae-on <solbi2004@naver.com>
2022-11-14 21:35:33 +0900

    feat: 로또 구매 app이랑 연결 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const { Console } = require('@woowacourse/mission-utils');
+const PurchaseLotto = require('./PurchaseLotto');
+const { PURCHASE_MESSAGE, DEFAULT_PRICE } = require('./Constants');
 class App {
-  play() {}
+  play() {
+    Console.readLine(PURCHASE_MESSAGE, (purchaseMoney) => {
+      this.purchaseMoney = Number(purchaseMoney);
+      this.purchaseCount = this.purchaseMoney / DEFAULT_PRICE;
+
+      const purchaseLotto = new PurchaseLotto(this.purchaseCount);
+      this.purchaseLotto = purchaseLotto;
+
+      this.purchaseLotto.printPurchaseCountMessage(this.purchaseCount);
+      this.getPurchaseLottoNumber();
+    });
+  }
+
+  getPurchaseLottoNumber() {
+    this.purchaseLotto.getLottoNumbers(this.purchaseCount);
+    this.purchaseLotto.printLottoNumber();
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
--- src/PurchaseLotto.js
+++ src/PurchaseLotto.js
@@ -1,46 +1,32 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const { Console } = MissionUtils;
 const { lottoNumber } = require('./LottoNumber');
-const {
-  PURCHASE_MESSAGE,
-  PURCHASE_COUNT_MESSAGE,
-  DEFAULT_PRICE,
-} = require('./Constants');
+const { PURCHASE_COUNT_MESSAGE } = require('./Constants');
 
 class PurchaseLotto {
-  constructor() {
+  constructor(purchaseCount) {
     this.lottoNumbers = [];
+    this.purchaseCount = purchaseCount;
   }
 
-  getPurchaseAmount() {
-    Console.readLine(PURCHASE_MESSAGE, (purchaseMoney) => {
-      this.purchaseMoney = Number(purchaseMoney);
-      return this.printPurchaseLotto();
-    });
+  printPurchaseCountMessage(purchaseCount) {
+    Console.print(PURCHASE_COUNT_MESSAGE(purchaseCount));
   }
 
-  printPurchaseCountMessage() {
-    this.purchaseCount = this.purchaseMoney / DEFAULT_PRICE;
-    Console.print(PURCHASE_COUNT_MESSAGE(this.purchaseCount));
-  }
-
-  getLottoNumbers() {
-    for (let i = 0; i < this.purchaseCount; i++) {
+  getLottoNumbers(purchaseCount) {
+    for (let i = 0; i < purchaseCount; i++) {
       const num = lottoNumber.createLottoNumber();
       this.lottoNumbers.push(num);
     }
-    return this.lottoNumbers;
+  }
+
+  getLottoTickets() {
+    return [...this.lottoNumbers];
   }
 
   printLottoNumber() {
-    return [...this.getLottoNumbers()].forEach((num) => Console.print(num));
-  }
-
-  printPurchaseLotto() {
-    this.printPurchaseCountMessage();
-    this.printLottoNumber();
+    return this.getLottoTickets().forEach((num) => Console.print(num));
   }
 }
 
-const purchaseLotto = new PurchaseLotto();
-exports.purchaseLotto = purchaseLotto;
+module.exports = PurchaseLotto;
