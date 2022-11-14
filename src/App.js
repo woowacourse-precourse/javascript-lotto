47ee56ef91774216cb4fbeb0daebf4d916967bb9
SoobinJ <soobin08011812@gmail.com>
2022-11-14 23:28:45 +0900

    feat : 로또 구입 금액 입력/ 구입 금액 입력받기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { Random, Console } = require("@woowacourse/mission-utils");
+const LOTTOPRICE = 1000;
+
 class App {
   play() {}
+  #purchaseAmount;
+
+  constructor() {}
+
+  play() {
+    this.inputAmount();
+  }
+
+  inputAmount() {
+    Console.readLine("구입금액을 입력해 주세요.", (purchaseAmount) => {});
+  }
 }
 
 module.exports = App;
