e411f5ff4fe32d953f6fc7926ec343e4ceb004f9
MAGHC <windowsapple95@gmail.com>
2022-11-14 20:40:02 +0900

    refacotr: 안쓰는 printer 제거 및 let 키워드 const 로 수정

--- src/Printer.js
+++ src/Printer.js
@@ -1,19 +1,6 @@
 const Uitls = require('./Utils');
-const Constant = require('./Constant');
 
-let Printer = class {
-  static startGame() {
-    Uitls.printMessage(Constant.GAME_PROGRESS_MESSAGES.ENTER_AMOUNT);
-  }
-
-  static askWinningNumber() {
-    Uitls.printMessage(Constant.GAME_PROGRESS_MESSAGES.ENTER_WINNING_NUMBER);
-  }
-
-  static askBonusNumber() {
-    Uitls.printMessage(Constant.GAME_PROGRESS_MESSAGES.ENTER_BONUS_NUMBER);
-  }
-
+const Printer = class {
   static lottos(arrays) {
     arrays.forEach((array) => Uitls.printMessage(`[${array.join(', ')}]`));
   }
