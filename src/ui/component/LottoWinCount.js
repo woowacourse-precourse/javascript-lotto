27e4a030d74ac470f263f39e10c8909a87af7860
turtle601 <turtle601@naver.com>
2022-11-14 19:39:59 +0900

    refactor(LottoWinCountUI) 데이터 변경으로 인한 코드 수정
    Changed:
    - 당첨 결과를 저장하는 자료구조를 배열에서 객체로 변경으로 인한 코드 수정

--- src/ui/component/LottoWinCount.js
+++ src/ui/component/LottoWinCount.js
@@ -15,11 +15,11 @@ class LottoWinCount extends Component {
     const { winScore } = this.state;
 
     return [
-      `3개 일치 (5,000원) - ${winScore[0]}개`,
-      `4개 일치 (50,000원) - ${winScore[1]}개`,
-      `5개 일치 (1,500,000원) - ${winScore[2]}개`,
-      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winScore[3]}개`,
-      `6개 일치 (2,000,000,000원) - ${winScore[4]}개`,
+      `3개 일치 (5,000원) - ${winScore.fifth}개`,
+      `4개 일치 (50,000원) - ${winScore.fourth}개`,
+      `5개 일치 (1,500,000원) - ${winScore.third}개`,
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winScore.second}개`,
+      `6개 일치 (2,000,000,000원) - ${winScore.first}개`,
     ];
   }
 
