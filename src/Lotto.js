27dfdc2d92767dbafedebf81ddc6a954ec64808b
wusi-hub <jws9595@naver.com>
2022-11-15 08:28:50 +0900

    feat(Lotto.js): 맞는 로또 번호 범위 확인 및 추가 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -15,11 +15,14 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호는 중복되지 않는 숫자여야 합니다.");
     }
     numbers.map((number) => {
-      if (!(number >= 1 && number <= 45)) throw new Error("[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다.");
+      if (!(parseInt(number) >= 1 && parseInt(number) <= 45)) throw new Error("[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다.");
     });
   }
 
   // TODO: 추가 기능 구현
 }
+
+let lotto = new Lotto([1, 2, 3, 4, 5, '49']);
+lotto.validate;
 
 module.exports = Lotto;
