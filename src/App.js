7ee7c8fe33a1f9e953c543f12034b8f6aeeacb5d
GC-Park <kuyt3122@naver.com>
2022-11-14 13:15:36 +0900

    feat:MVC구조의 controller로 view와 model로 옮긴 함수 삭제와 변경

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 const MissionUtils = require('@woowacourse/mission-utils')
+const Lotto = require('./Lotto')
+const View = require('./views/view.js')
 
 class App {
-    money=0
-    lottoCount = 0
-    userLottoNumbers = []
-    winningNumbers = []
+    money
+    lottoCount
+    userLottoNumbers
+    winningNumbers
     bonusNumber
-    rank = [0, 0, 0, 0, 0]
-    prizeMoney=[2000000000, 1500000, 50000, 5000, 30000000]
-    rateOfReturn=0;
+    rank
+    prizeMoney
+    rateOfReturn
 
-    constructor() {}
+    constructor() {
+        this.money = 0
+        this.lottoCount = 0
+        this.userLottoNumbers = []
+        this.winningNumbers = []
+        this.rank = [0, 0, 0, 0, 0]
+        this.prizeMoney = [2000000000, 1500000, 50000, 5000, 30000000]
+        this.rateOfReturn = 0
+        this.view=new View();
+    }
 
     play() {
         this.insertMoney()
@@ -18,9 +29,9 @@ class App {
 
     insertMoney() {
         MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (answer) => {
-            this.money=answer
+            this.money = answer
             this.lottoCount = this.lottoPurchaseCount(this.money)
-            this.lottoCountPrint(this.lottoCount)
+            this.view.lottoCountPrint(this.lottoCount)
             this.makeLottoNumber()
         })
     }
@@ -31,25 +42,11 @@ class App {
 
     makeLottoNumber() {
         for (let i = 0; i < this.lottoCount; i++) {
-            const numbers = this.lottoNumberSort(
-                MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
-            )
-            this.lottoNumberPrint(numbers)
-            this.userLottoNumbers.push(numbers)
+            const numbers = new Lotto(MissionUtils.Random.pickUniqueNumbersInRange(1,45,6));
+            this.view.lottoNumberPrint(numbers.getNumbers())
+            this.userLottoNumbers.push(numbers.getNumbers())
         }
         this.winningNumberInput()
-    }
-
-    lottoNumberPrint(numbers) {
-        MissionUtils.Console.print('[' + numbers.join(', ') + ']')
-    }
-
-    lottoNumberSort(numbers) {
-        function compareNumbers(a, b) {
-            return a - b
-        }
-
-        return numbers.sort(compareNumbers)
     }
 
     winningNumberInput() {
@@ -83,31 +80,21 @@ class App {
             if (
                 this.winningCount(i) === 5 &&
                 this.userLottoNumbers.includes(this.bonusNumber)
-            ) this.rank[4] += 1
+            )
+                this.rank[4] += 1
             if (this.winningCount(i) > 2)
-              this.rank[6-this.winningCount(i)]+=1
+                this.rank[6 - this.winningCount(i)] += 1
         }
-        this.calculationOfEarnings();
+        this.calculationOfEarnings()
     }
 
-    calculationOfEarnings(){
-      for(let i=0; i<this.rank.length; i++){
-        this.rateOfReturn += this.rank[i]*this.prizeMoney[i];
-      }
-      this.rateOfReturn=this.rateOfReturn/this.money*100;
-      this.winningStatistics();
+    calculationOfEarnings() {
+        for (let i = 0; i < this.rank.length; i++) {
+            this.rateOfReturn += this.rank[i] * this.prizeMoney[i]
+        }
+        this.rateOfReturn = (this.rateOfReturn / this.money) * 100
+        this.view.winningStatistics(this.rank, this.rateOfReturn);
     }
-
-    winningStatistics(){
-      MissionUtils.Console.print("당첨 통계\n---");
-      MissionUtils.Console.print("3개 일치 (5,000원) - " + this.rank[3]+"개");
-      MissionUtils.Console.print("4개 일치 (50,000원) - " + this.rank[2]+"개");
-      MissionUtils.Console.print("5개 일치 (1,500,000원) - " + this.rank[1]+"개");
-      MissionUtils.Console.print("5개 일치, 보너스 볼 일치 (30,000,000원) - "+ this.rank[4]+"개");
-      MissionUtils.Console.print("6개 일치 (2,000,000,000원) - "+ this.rank[0]+"개");
-      MissionUtils.Console.print("총 수익률은 "+this.rateOfReturn+"%입니다.")
-    }
-
 }
 
 const app = new App()
