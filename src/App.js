ec38d6c6150779050ce42c4a5ac9199215390c8d
MINJUN PARK <mj9457@naer,com>
2022-11-15 01:42:01 +0900

    refactor. 결과 출력 메서드로 묶기

--- src/App.js
+++ src/App.js
@@ -137,6 +137,14 @@ class App {
     }
   }
 
+  printCountAll() {
+    this.printCountOfThreeMatch();
+    this.printCountOfFourMatch();
+    this.printCountOfFiveMatch();
+    this.printCountOfFiveAndBounsMatch();
+    this.printCountOfSixMatch();
+  }
+
   printCountOfThreeMatch() {
     Console.print(`3개 일치 (5,000원) - ${this.countThreeMatch}개`);
   }
@@ -155,6 +163,12 @@ class App {
 
   printCountOfSixMatch() {
     Console.print(`6개 일치 (2,000,000,000원) - ${this.countSixMatch}개`);
+  }
+
+  printResult() {
+    Console.print("당첨 통계");
+    Console.print("---");
+    this.printCountAll();
   }
 }
 
