d9667da92e92e89524680c86181d26293185b7ff
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-14 11:37:46 +0900

    enhance: LottoTest method checkLotto

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -20,4 +20,15 @@ describe("로또 클래스 테스트", () => {
       new Lotto([6, 5, 4, 3, 2, 1]);
     }).toThrow("[ERROR]");
   });
+
+  test("로또는 몇 개 맞았는지 확인할 수 있다.", () => {
+    // given
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const compareInput = [1, 7, 8, 9, 10, 11];
+    const bonus = 12;
+    // when
+    const expectResult = lotto.checkLottoResult(compareInput, bonus);
+    //then
+    expect(expectResult).toBe(1);
+  });
 });
