8571b553629b3f0601ad9f8e9252930e35e07005
random-olive <104327907+random-olive@users.noreply.github.com>
2022-11-14 18:27:42 +0900

    feat(validation): check numbers(array) validation

--- src/Validation.js
+++ src/Validation.js
@@ -4,21 +4,64 @@ class Validation {
       return true;
     }
   }
+
   amountUnit(value) {
     if (value % 1000 === 0) {
       return true;
     }
   }
 
-  // numbers
+  // numbers : [1,2,3,4,5,6]
 
-  type() {}
-  range() {}
-  lengthOne() {}
-  lengthSix() {}
-  duplication() {}
-  formatArray() {}
-  formatString() {}
+  type(numbers) {
+    if (numbers.filter((el) => typeof el !== "number").length !== 0) {
+      throw new Error();
+    }
+  }
+
+  range(numbers) {
+    if (numbers.filter((el) => el < 1 || el > 45).length !== 0) {
+      throw new Error();
+    }
+  }
+
+  lengthOne(numbers) {
+    if (numbers.length !== 1) {
+      throw new Error();
+    }
+  }
+
+  lengthSix(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error();
+    }
+  }
+
+  duplication(numbers) {
+    if (new Set(numbers) !== 6) {
+      throw new Error();
+    }
+  }
+
+  formatArray(numbers) {
+    if (!Array.isArray(numbers)) {
+      throw new Error();
+    }
+  }
+
+  //string : '1,2,3,4,5,6'
+
+  formatString(string) {
+    if (typeof string !== "string") {
+      throw new Error();
+    }
+    if (typeof string === "string") {
+      const resolved = string.split(",").map((el) => Number(el));
+      // if (resolved 검증 실패){
+      //   throw new Error()
+      // }
+    }
+  }
 }
 
 module.exports = Validation;
