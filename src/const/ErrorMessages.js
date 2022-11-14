7d97d482b602a21c23451405332e09b53195e222
deli-ght <minjju0660@gmail.com>
2022-11-15 00:34:45 +0900

    feat(ErrorMessages.js): add error messages

--- src/const/ErrorMessages.js
+++ src/const/ErrorMessages.js
@@ -2,9 +2,11 @@ const ERROR_MESSAGES = {
   IS_NUMBER: "[ERROR] 숫자를 입력해주세요.",
   IS_OVER_MIN_COST: "[ERROR] 최소 금액 1000원 이상 작성해야합니다.",
   IS_NO_CHARGE: "[ERROR] 1000원 단위로 입력해주세요.",
-  IS_ENOUGH: "[ERROR] 로또 번호는 6개여야 합니다.",
-  IS_REPEATED_NUMBER: "[ERROR] 로또 번호는 중복될 수 없습니다.",
+  IS_ENOUGH: "[ERROR] 서로 중복되지 않은 6개의 숫자를 입력해주세요.",
   IS_RANGE: "[ERROR] 로또 번호는 1~45 사이의 숫자만 가능합니다.",
+  IS_UNEXPECTABLE: "[ERROR] 예기치 못한 에러 발생",
+  IS_WINNER_NUMBER:
+    "[ERROR] 숫자와 쉼표를 이용해 중복되지 않은 6개의 숫자를 입력해주세요.",
 };
 
 module.exports = ERROR_MESSAGES;
