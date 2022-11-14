716ef02a9d672956919f930a2552c9f63a2acdb2
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-14 21:27:58 +0900

    feat: 구입 금액 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    this.priceInput();
+  }
+  
+  priceInput() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (price) => {
+        console.log(`구입금액을 입력해 주세요.\n ${price}`);
+      }
+    );
+  }
 }
 
 module.exports = App;
