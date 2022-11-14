1e9efb19420da905a192fe9ab98a274855091a3e
daal2quf <daal2quf@gmail.com>
2022-11-14 23:03:26 +0900

    refactor(lottoTest): makeBonusNumber() 메서드 삭제로 인한 테스트 삭제
    delete:
    유저가 구매한 로또의 번호에 bonus Number를 추가해야되는걸로 잘못 이해해 추가한 테스트 삭제

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -11,7 +11,6 @@ describe("로또 클래스 테스트", () => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
     }).toThrow("[ERROR]");
   });
-
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
@@ -34,34 +33,20 @@ describe("Lotto 클래스의 makeSixNumbers() 기능 테스트", () => {
   });
 });
 
-describe("Lotto 클래스의 makeBonusNumber() 기능 테스트", () => {
-  test("보너스 숫자가 1~45 내의 숫자인지 테스트", () => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
-    const numberRangeCheck = (number) => {
-      return (number >= 1) && (number <= 45);
-    };
-    const bonusNumber = lotto.makeBonusNumber();
-    expect(numberRangeCheck(bonusNumber)).toBeTruthy();
-  });
 
-  describe("Lotto 클래스의 NumbersPurchaseByUser() 기능 테스트", () => {
-    test("유저가 구매한 로또 번호의 자릿수가 7자리인지 확인", () => {
-      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
-      const lottoNumbers = lotto.numbersPurchaseByUser();
-      const lottoNumbersLength = lottoNumbers.length;
-      expect(lottoNumbersLength).toBe(7);
-    });
-    test("유저가 구매한 로또 번호에 중복이 없는지 확인", () => {
-      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
-      const lottoNumbers = lotto.numbersPurchaseByUser();
-      const noDuplicateLottoNumbers = [...new Set(lottoNumbers)];
-      expect(noDuplicateLottoNumbers.length).toEqual(lottoNumbers.length);
-    });
-    test("유저가 구매한 로또 번호의 숫자들이 1~45 범위인지 확인", () => {
-      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
-      const lottoNumbers = lotto.numbersPurchaseByUser();
-      const correctRangeCheck = lottoNumbers.every((number) => (number >= 1) && (number <= 45));
-      expect(correctRangeCheck).toBeTruthy();
-    });
+
+describe("Lotto 클래스의 NumbersPurchaseByUser() 기능 테스트", () => {
+
+  test("유저가 구매한 로또 번호에 중복이 없는지 확인", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const lottoNumbers = lotto.makeSixNumbers();
+    const noDuplicateLottoNumbers = [...new Set(lottoNumbers)];
+    expect(noDuplicateLottoNumbers.length).toEqual(lottoNumbers.length);
+  });
+  test("유저가 구매한 로또 번호의 숫자들이 1~45 범위인지 확인", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const lottoNumbers = lotto.makeSixNumbers();
+    const correctRangeCheck = lottoNumbers.every((number) => (number >= 1) && (number <= 45));
+    expect(correctRangeCheck).toBeTruthy();
   });
 });
