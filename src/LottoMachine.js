713a1bd78010374d70561454daf0bc0570c264b4
yeongseokim <1903020@ewhain.net>
2022-11-14 13:59:14 +0900

    feat(LottoMachine): createLotto를 오름차순으로 정렬된 6개의 숫자 배열을  반환하는 함수로 변경

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -3,6 +3,11 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class LottoMachine {
     static createLotto() {
         const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+        lottoNumbers.sort(function (a, b) {
+            if (a > b) return 1;
+            if (a === b) return 0;
+            if (a < b) return -1;
+        });
         return lottoNumbers;
     }
 }
