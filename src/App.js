7c2fa83145f09e89695b0fb02f1120dbf0911e4d
hanbeulYou <hanbeul.you@gmail.com>
2022-11-14 16:02:29 +0900

    feat: 당첨 통계 계산 및 출력

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ const inputObjects = {
 }
 
 const results = [0, 0, 0, 0, 0, 0]
+const REWARDS = [0, 5000, 50000, 1500000, 30000000, 2000000000]
+const RANK_MESSAGE = ['', 
+  '3개 일치 (5,000원) - ',
+  '4개 일치 (50,000원) - ', 
+  '5개 일치 (1,500,000원) - ', 
+  '5개 일치, 보너스 볼 일치 (30,000,000원) - ', 
+  '6개 일치 (2,000,000,000원) - ',
+]
 
 function getBudget() {
   Console.readLine('', (answer) => {
@@ -92,11 +100,33 @@ function getResult(randomNumbers, bonus) {
 function getRandomNumberList() {
   for(let i=0; i<inputObjects['budget']/1000; i++){
     const randomNumbers = getRandomNumbers();
-    const result = getResult(randomNumbers, inputObjects['bonus'])
+    const result = getResult(randomNumbers, inputObjects['bonus']);
     Console.print(randomNumbers);
     results[result] += 1;
   }
-  console.log(results)
+  getResultStatics();
+}
+
+function getResultStatics() {
+  const reward = results.reduce((acc, element, index) => {
+    return acc + element * REWARDS[index];
+  }, 0)
+  printLottoResult(reward);
+}
+
+function printLottoResult(reward) {
+  results.forEach((element, index) => {
+    if(index === 0) return;
+    Console.print(RANK_MESSAGE[index]+element+'개');
+  })
+  console.log(reward)
+  const yield = (reward/inputObjects['budget']*100).toFixed(1);
+  Console.print(`총 수익률은 ${yield}%입니다.`);
+  finishLotto()
+}
+
+function finishLotto() {
+  Console.close();
 }
 
 class App {
