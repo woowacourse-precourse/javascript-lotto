8c9b8f409574d622545caa6aef5b3f540970d126
saram <yujun1018@naver.com>
2022-11-14 18:21:46 +0900

    feat: 각 기능을 연결하여 로또 게임 구현

--- src/DrawMachine.js
+++ src/DrawMachine.js
@@ -1,11 +1,21 @@
 const { Console } = require('@woowacourse/mission-utils');
-const { PHRASE, ERROR } = require('./constants');
+
+const { PHRASE, ERROR, LOTTO } = require('./constants');
 const Lotto = require('./Lotto');
+const ScoreMachine = require('./ScoreMachine');
 
 class DrawMachine {
+  #winningNumber;
+  #bonusNumber;
+
+  constructor() {
+    this.scoreMachine = new ScoreMachine();
+  }
+
   darwWinningNumber() {
     Console.readLine(PHRASE.WINNING_NUMBER, (input) => {
       this.validateWinningNumber(input);
+      this.#winningNumber = input.split(',').map((number) => Number(number));
       this.drawBonusNumber();
     });
   }
@@ -19,6 +29,8 @@ class DrawMachine {
   drawBonusNumber() {
     Console.readLine(PHRASE.BONUS_NUMBER, (input) => {
       this.validateBonusNumber(input);
+      this.#bonusNumber = Number(input);
+      this.scoreMachine.compare(this.#winningNumber, this.#bonusNumber);
     });
   }
 
