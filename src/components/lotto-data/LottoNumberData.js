aa3f5e846de796e159e317b0d8c6983bfed79948
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-14 09:38:35 +0900

    feat: 객체 분리

---
+++ src/components/lotto-data/LottoNumberData.js
@@ -0,0 +1,17 @@
+const LottoNumberData = {
+  AmountPaid: 0,
+  Issued: [],
+  Winning: [],
+  Bonus: 0,
+  RateOfReturn: 0,
+};
+
+const LottoRanking = {
+  first: 0,
+  second: 0,
+  third: 0,
+  fourth: 0,
+  fifth: 0,
+};
+
+module.exports = { LottoNumberData, LottoRanking };
