a078233a4ab31116a6d72d203a7fd555fedade92
ashleysyheo <ashleysyheo@gmail.com>
2022-11-14 15:31:19 +0900

    feat: get common digit count

---
+++ src/CalculateLotto.js
@@ -0,0 +1,17 @@
+const { PARAMETERS } = require('./utils/constants');
+
+class CalculateLotto {
+  constructor(winningNumber, bonusNumber, userLottoNumbers, purchaseCount) {
+    this.winningCount = new Array(5).fill(0);
+    this.winningNumber = winningNumber;
+    this.bonusNumber = bonusNumber;
+    this.userLottoNumbers = userLottoNumbers;
+    this.purchaseAmount = purchaseCount * PARAMETERS.purchaseAmountUnit;
+  }
+
+  getCommonNumberCount(userLottoNumber) {
+    return userLottoNumber.filter((number) => this.winningNumber.includes(number)).length;
+  }
+}
+
+module.exports = CalculateLotto;
