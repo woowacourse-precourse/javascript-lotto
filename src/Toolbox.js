8411f9db87e1e9f22c1fe8ae1464392f20dd6ab5
nailen1 <nietsnie1@gmail.com>
2022-11-14 23:08:32 +0900

    feat: 오름차순으로 정렬된 난수배열을 생성하는 기능

--- src/Toolbox.js
+++ src/Toolbox.js
@@ -1,5 +1,15 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
 
+function printMessage(sentence) {
+    Console.print(sentence);
+}
+
+function generateRandomNumbers(min, max, numbers) {
+    let randoms = Random.pickUniqueNumbersInRange(min, max, numbers);
+    return randoms.map(x => Number(x)).sort((i, j) => i - j);
+}
+
 module.exports = {
     printMessage,
+    generateRandomNumbers,
 }
