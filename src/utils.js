91bef456263433d222602d64bb358b4b4bdf4322
adultlee <seoungin1228@gmail.com>
2022-11-15 18:04:09 +0900

    feat: 입력받은 숫자의 예외 처리

--- src/utils.js
+++ src/utils.js
@@ -6,4 +6,24 @@ function checkValiPrice(number) {
   }
 }
 
+function checkValidWinningNumber(inputWinningNumbers) {
+  const winningNumbers = inputWinningNumbers.split(",");
+  if (winningNumbers.length !== 6) {
+    throw new Error("[ERROR] 입력받은 숫자가 6개가 아닙니다.");
+  }
+
+  winningNumbers.map((number) => {
+    if (isNaN(number))
+      throw new Error("[ERROR] 입력받은 숫자에 문자가 있습니다.");
+  });
+
+  winningNumbers.map((number) => {
+    if (number < 1 || 45 < number)
+      throw new Error("[ERROR] 입력받은 숫자의 범위를 초과합니다.");
+  });
+
+  if (winningNumbers.length !== new Set(winningNumbers).size)
+    throw new Error("[ERROR] 중복이 있습니다.");
+}
+
 module.exports.checkValiPrice = checkValiPrice;
