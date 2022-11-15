598bc72f847497ecf80a7c4479778132c9cb113c
gsong <rmsdlf1206@gmail.com>
2022-11-15 16:26:23 +0900

    feat: app 구입금액 입력 메서드 setPurchaseAmount 구현

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-const { MissionUtils } = require('@woowacourse/mission-utils');
+const { Console } = require('@woowacourse/mission-utils');
 
 class App {
   constructor() {}
-
   play() {}
 
+  setPurchaseAmount() {
+    this.print('구입금액을 입력해 주세요.');
+    this.readLine('', input => {});
+  }
+
   readLine(message, callback) {
-    return MissionUtils.Console.readLine(message, callback);
+    return Console.readLine(message, callback);
   }
 
   print(message) {
-    return MissionUtils.Console.print(message);
+    return Console.print(message);
   }
 }
 
-const app = new App();
-app.play();
+// const app = new App();
+// app.setPurchaseAmount();
 module.exports = App;
