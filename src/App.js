575486ee2773b236e785e81437d9275fb5336a06
rayark1 <max980203@snu.ac.kr>
2022-11-15 16:38:35 +0900

    fix(src/App.js): 에러 메세지 순서 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Ticket = require("./Ticket");
 const Constant = {
-  PURCHASE_AMOUNT_QUESTION_MESSAGE: "구입금액을 입력해 주세요.",
+  PURCHASE_AMOUNT_QUESTION_MESSAGE: "구입금액을 입력해 주세요.\n",
   PURCHASE_AMOUNT_ERROR_MESSAGE: "구입 금액은 숫자여야 합니다.",
   PURCHASE_AMOUNT_ERROR_MESSAGE2: "구입 금액은 0원 이상이어야 합니다.",
   PURCHASE_AMOUNT_ERROR_MESSAGE3: "구입 금액은 1000원 단위여야 합니다.",
 };
 class App {
-  play() {}
+  play() {
+    this.purchaseTicket();
+  }
 
   purchaseTicket() {
     this.getInputPurchaseAmount(this.manageInputPurchaseAmount.bind(this));
@@ -16,13 +18,11 @@ class App {
   manageInputPurchaseAmount(price) {
     const numberOfTickets = this.calculateNumberOfTickets(price);
     MissionUtils.Console.print(`${numberOfTickets}개를 구매했습니다.`);
+    this.manageTickets(numberOfTickets);
   }
 
   getInputPurchaseAmount(callback) {
-    MissionUtils.Console.readLine(
-      Constant.PURCHASE_AMOUT_QUESTION_MESSAGE,
-      callback
-    );
+    MissionUtils.Console.readLine(Constant.PURCHASE_AMOUNT_QUESTION_MESSAGE, callback);
   }
 
   calculateNumberOfTickets(price) {
@@ -33,22 +33,22 @@ class App {
 
   validateInputPurchase(price) {
     if (isNaN(price)) {
-      throw new Error(PURCHASE_AMOUNT_ERROR_MESSAGE);
+      throw new Error(Constant.PURCHASE_AMOUNT_ERROR_MESSAGE);
+    }
+    if (price <= 0) {
+      throw new Error(Constant.PURCHASE_AMOUNT_ERROR_MESSAGE2);
     }
     if (price % 1000 !== 0) {
-      throw new Error(PURCHASE_AMOUNT_ERROR_MESSAGE2);
-    }
-    if (price < 0) {
-      throw new Error(PURCHASE_AMOUNT_ERROR_MESSAGE3);
+      throw new Error(Constant.PURCHASE_AMOUNT_ERROR_MESSAGE3);
     }
   }
 
   manageTickets(numberOfTickets) {
     for (let i = 0; i < numberOfTickets; i++) {
-      const ticketNumbers = this.createTicketNumbers();
+      let ticketNumbers = this.createTicketNumbers();
       ticketNumbers = this.sortAscendingOrder(ticketNumbers);
       this.printTickets(ticketNumbers);
-      this.tickets.push(new Ticket(randomTicketNumbers));
+      // this.tickets.push(new Ticket(randomTicketNumbers));
     }
   }
 
@@ -63,6 +63,10 @@ class App {
   sortAscendingOrder(ticketNumbers) {
     return ticketNumbers.sort((a, b) => a - b);
   }
+
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
