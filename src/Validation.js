b5d4f2e8cd47f9ad3ac4c09cd0f32b203537bd21
cmun2 <cmun2@illinois.edu>
2022-11-15 21:56:27 +0900

    feat 추가 로또 숫자 예외사항 처리

--- src/Validation.js
+++ src/Validation.js
@@ -13,6 +13,29 @@ class Validation {
     this.isNumberString(bonus.toString());
   }
 
+  validUserPurchase(userPurchase) {
+    if ([...userPurchase].length === 0) {
+      throw new Error(`${Text.ERROR_TEXT.BLANK}`);
+    }
+  }
+
+  validLottoNumber(lotto) {
+    if (lotto.length !== 6) {
+      throw new Error(`${Text.ERROR_TEXT.COMMA}`);
+    }
+
+    lotto.forEach((lottos) => {
+      this.isNumberString(lottos);
+    });
+
+    lotto.forEach((lottos) => {
+      var lottoNum = +lottos;
+      if (lottoNum < 1 || lottoNum > 45 || !lottoNum) {
+        throw new Error(`${Text.ERROR_TEXT.LIMIT}`);
+      }
+    });
+  }
+
   isNumberString(lotto) {
     var idx = 0;
     [...lotto].forEach((lottos) => {
