40d43572dd6d6adee8afde7cafd04f963d47f99d
동동이 <drdragon777@naver.com>
2022-11-15 21:31:16 +0900

    test - Ranker 클래스, 테스트 케이스 추가

--- __tests__/RankerTest.js
+++ __tests__/RankerTest.js
@@ -11,9 +11,19 @@ describe("순위 계산 단위 테스트", () => {
     [2, 13, 22, 32, 38, 45],
     [1, 3, 5, 14, 22, 45],
   ];
-  const win = [8, 21, 23, 30, 40, 42];
-  const bonus = 43;
+  const win1 = [8, 21, 23, 30, 40, 42];
+  const bonus1 = 43;
   test("금액의 오름차순으로 금액 당 당첨 개수 계산", () => {
-    expect(Ranker.getPriceRank(lottos, [win, bonus])).toEqual([0, 1, 0, 1, 0]);
+    expect(Ranker.getPriceRank(lottos, [win1, bonus1])).toEqual([
+      0, 1, 0, 1, 0,
+    ]);
+  });
+
+  const win2 = [8, 21, 23, 30, 40, 45];
+  const bonus2 = 43;
+  test("금액의 오름차순으로 금액 당 당첨 개수 계산", () => {
+    expect(Ranker.getPriceRank(lottos, [win2, bonus2])).toEqual([
+      1, 1, 0, 0, 0,
+    ]);
   });
 });
