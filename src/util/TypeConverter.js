fe1ed1336f896b228c363534b9d8a332f4768299
2yunseong <dbsdltjd3701@naver.com>
2022-11-15 19:03:17 +0900

    fix(TypeConverter): 사용하지 않는 메서드 삭제

--- src/util/TypeConverter.js
+++ src/util/TypeConverter.js
@@ -6,10 +6,6 @@ class TypeConverter {
   static stringToArray(str, seperator) {
     return str.split(seperator);
   }
-
-  static just(param) {
-    return param;
-  }
 }
 
 module.exports = TypeConverter;
