1f1d3907a0c08dfb16828e9e36443366746de582
wzrabbit <curious.wzrabbit@gmail.com>
2022-11-15 22:40:30 +0900

    feat(scoreboard): 로또 숫자 일치 개수가 주어지면, 결과를 판단하여 저장하는 기능 구현하기

---
+++ src/LottoScoreBoard.js
@@ -0,0 +1,34 @@
+const { LOTTO, PRIZE_TEXT, PRIZE_AMOUNT } = require('./constant/Constant');
+
+class LottoScoreBoard {
+  #drawResult = {
+    threeSame: 0,
+    fourSame: 0,
+    fiveSame: 0,
+    fiveSameWithBonus: 0,
+    allSame: 0,
+    profitRate: 0.0,
+  };
+
+  #totalCost = 0;
+  #profit = 0;
+
+  addLottoDrawResult(sameCount, bonusSameCount) {
+    this.#totalCost += LOTTO.PRICE;
+    if (sameCount < LOTTO.PRIZE_REQUIRED_SCORE) {
+      return;
+    }
+    if (
+      sameCount === LOTTO.PRIZE_REQUIRED_WITH_BONUS &&
+      bonusSameCount === LOTTO.BONUS_REQUIRED_SCORE
+    ) {
+      this.#drawResult.fiveSameWithBonus += 1;
+      this.#profit += PRIZE_AMOUNT.BONUS;
+      return;
+    }
+    this.#drawResult[PRIZE_TEXT[sameCount]] += 1;
+    this.#profit += PRIZE_AMOUNT[sameCount];
+  }
+}
+
+module.exports = LottoScoreBoard;
