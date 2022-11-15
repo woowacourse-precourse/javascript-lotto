8ccf4bd4f3b859b254def3bf1cb933d89e45029c
박겸영 <yong_drg@naver.com>
2022-11-15 18:39:14 +0900

    feat(Lotto): 구입한 로또 수량만큼 로또 발행하기
    - MissionUtils 라이브러리 이용하여 1~45 사이 중복되지 않는 랜덤 숫자 6개 생성하여 로또 1장 발행
    - 구입한 로또 수량만큼 반복하여 구입한 로또 모두 발행

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,6 @@
+const { Random } = require('@woowacourse/mission-utils');
+const { MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_LENGTH } = require('./settings');
+
 class Lotto {
   #numbers;
 
@@ -8,11 +11,29 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  static #issueSingleLotto() {
+    const numbers = Random.pickUniqueNumbersInRange(
+      MIN_LOTTO_NUMBER,
+      MAX_LOTTO_NUMBER,
+      LOTTO_LENGTH,
+    );
+
+    return new Lotto(numbers).#numbers;
+  }
+
+  static issueAllLottos(count) {
+    return new Array(count).fill([]).map(() => Lotto.#issueSingleLotto());
+  }
+
+  static issue(count) {
+    const issuedLottos = Lotto.issueAllLottos(count);
+
+    return issuedLottos;
+  }
 }
 
 module.exports = Lotto;
