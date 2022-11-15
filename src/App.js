85d69ebf199d1d3221773244aea51c4e3dfc7d2a
MIN SIK CHOI <minstones34@gmail.com>
2022-11-15 21:43:24 +0900

    feat: 로또 번호 비교하는 함수

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ const Lotto = require("./Lotto");
 const THOUSAND_WON = 1000;
 class App {
   constructor() {
-    this.userMoney = 0;
     this.lottoCount = 0;
+    this.bonusNum = 0;
     this.winingLotteryArr = [];
-    // this.lotto = new Lotto(this.lottoCount);
+    this.lottoNumArr = [];
   }
 
   getUserInputNumber() {
@@ -31,7 +31,6 @@ class App {
       if (forValidate % THOUSAND_WON != 0) {
         throw new Error("[ERROR] 천 원 단위로 입력해주십시오.");
       }
-      this.userMoney = forValidate;
       this.lottoCount = forValidate / THOUSAND_WON;
       MissionUtils.Console.print(`${this.lottoCount}개를 구매했습니다.`);
       this.generateLottoNum(this.lottoCount);
@@ -40,7 +39,6 @@ class App {
   }
 
   generateLottoNum(lottoCount) {
-    const lottoNumArr = [];
     for (let i = 0; i < lottoCount; i++) {
       const lottoRandomNum = MissionUtils.Random.pickUniqueNumbersInRange(
         1,
@@ -58,47 +56,27 @@ class App {
       "당첨 번호를 입력해 주세요. \n",
       (input) => {
         this.winingLotteryArr = input.split(",").map((x) => parseInt(x));
-        this.winningLotteryValidate(winingLotteryArr);
 
         this.inputBonusNum();
       }
     );
   }
 
-  winningLotteryValidate(winingLotteryArr) {
-    const forValidateNum = winingLotteryArr;
-    if (forValidateNum.length != 6) {
-      throw new Error("[ERROR] 6개의 숫자를 입력해주세요.");
-    }
-
-    forValidateNum.forEach((number) => {
-      if (isNaN(number)) {
-        throw new Error("[ERROR] 숫자만 입력해주세요.");
+  inputBonusNum() {
+    MissionUtils.Console.readLine(
+      "보너스 번호를 입력해 주세요.\n",
+      (inputBonusNum) => {
+        this.bonusNum = parseInt(inputBonusNum);
+        const lotto = new Lotto(
+          this.winingLotteryArr,
+          this.inputBonusNum,
+          this.lottoCount,
+          this.lottoNumArr
+        );
+        lotto.playLotto();
       }
-
-      if (
-        number == "" ||
-        number == null ||
-        number == undefined ||
-        (number != null &&
-          typeof number == "object" &&
-          !Object.keys(number).length)
-      ) {
-        throw new Error("[ERROR] 숫자를 제대로 입력해주십시오.");
-      }
-
-      if (number < 1 || 45 < number) {
-        throw new Error("[ERROR] 입력받은 숫자의 범위를 초과합니다.");
-      }
-    });
-
-    const set = new Set(forValidateNum);
-    if (set.size != 6) {
-      throw new Error("[ERROR] 중복되지 않은 값만 입력해주세요.");
-    }
+    );
   }
-
-  inputBonusNum() {}
 
   play() {
     this.getUserInputNumber();
--- src/Lotto.js
+++ src/Lotto.js
@@ -3,19 +3,54 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class Lotto {
   #numbers;
 
-  constructor(numbers, count) {
+  constructor(numbers, bonus, count, lottoNumArr) {
     this.validate(numbers);
     this.#numbers = numbers;
     this.lottoCount = count;
+    this.bonusNum = bonus;
+    this.lottoNumArr = lottoNumArr;
+    this.currentScore = [0, 0, 0, 0, 0, 0];
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    const forValidateNum = numbers;
+    if (forValidateNum.length != 6) {
+      throw new Error("[ERROR] 6개의 숫자를 입력해주세요.");
+    }
+
+    forValidateNum.forEach((element) => {
+      if (isNaN(element)) {
+        throw new Error("[ERROR] 숫자만 입력해주세요.");
+      }
+
+      if (element < 1 || 45 < element) {
+        throw new Error("[ERROR] 입력받은 숫자의 범위를 초과합니다.");
+      }
+    });
+
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 중복된 값을 입력하셨습니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  playLotto() {
+    for (let i = 0; i < this.lottoNumArr.length; i++) {
+      this.scoreCalculation(
+        this.compareLotteryNum(this.#numbers, this.lottoNumArr[i]),
+        this.lottoNumArr[i]
+      );
+    }
+    this.showResult();
+    this.showYield();
+  }
+
+  compareLotteryNum(numbers, lottoNumArr) {
+    const sumNumAndLottoNum = [...numbers, ...lottoNumArr];
+    const sumNumAndLottoNumLength = sumNumAndLottoNum.length;
+    const removeDuplicateElements = new Set(sumNumAndLottoNum).size;
+
+    return sumNumAndLottoNumLength - removeDuplicateElements;
+  }
 }
 
 module.exports = Lotto;
