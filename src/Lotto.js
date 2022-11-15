fbd1582846815c9dffc33635dfeb0f5554f372a4
KangYunJi <dbswl701@ajou.ac.kr>
2022-11-15 22:25:43 +0900

    refactor: 코드 정리 및 완성

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,8 +11,9 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-
+    if(numbers.length !== 6) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    const filter = numbers.filter((num)=> 1<=num && num<=45);
+    if(numbers.length !== filter.length) throw new Error("[ERROR] 1에서 45 사이의 숫자만 입력하세요.");
     const set = new Set(numbers);
     if(numbers.length !== set.size) throw new Error("[ERROR] 로또 번호는 중복이 불가능합니다.");
   }
@@ -27,10 +28,8 @@ class Lotto {
 
   setLottoResult(winningNum){
     for(const num of this.#numbers){
-      console.log(winningNum ,num, this.#numbers);
       if(winningNum.includes(num)) {
         this.result.lotto++;
-        console.log(`숫자 당첨!! ${num}`);
       }
     }
   }
