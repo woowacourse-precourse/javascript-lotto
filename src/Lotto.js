6e3f0057331304a89e2892c010aa6d80a6470a9b
jincde <asdzxc@kakao.com>
2022-11-15 21:46:01 +0900

    feat : 중복된 입력 숫자 예외처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,12 +10,16 @@ class Lotto {
     if (numbers.length !== 6) {
       throw Error.LENGTH_ERROR;
     }
-  }
-  rangeIsValid(max, min) {
-    if (max > 45 || min < 1) {
-      throw Error.RANGE_ERROR
+    else if (numbers !== undefined && new Set(numbers).size != 6) {
+      throw Error.OVERLAP_ERROR
     }
   }
+}
+rangeIsValid(max, min) {
+  if (max > 45 || min < 1) {
+    throw Error.RANGE_ERROR
+  }
+}
 
   // TODO: 추가 기능 구현
 }
