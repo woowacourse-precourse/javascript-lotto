ec5d91548aebf0b60a893dbc66087a8cb2988f54
Sanggil Han <tkdrlf95@naver.com>
2022-11-15 14:15:41 +0900

    test(LottoMachine): Implement test codes for LottoMachine
    LottoMachine 인스턴스가 잘 동작하는지 다음 항목들을 테스트하는 코드를 추가하였음
    - 숫자를 6개 생성하는지
    - 해당 숫자가 1~45 크기의 숫자인지
    - 인스턴스의 generate 메서드를 호출할 때마다 다른 숫자를 갖는지

---
+++ __tests__/LottoMachineTest.js
@@ -0,0 +1,28 @@
+const LottoMachine = require("../src/LottoMachine");
+
+describe("로또머신 클래스 테스트", () => {
+  
+  test("제너레이터 테스트", () => {
+    const lottoMachine = new LottoMachine();
+    const numbers = lottoMachine.generate();
+    expect(numbers).toHaveLength(6);
+  });
+
+  test("번호 테스트", () => {
+    const lottoMachine = new LottoMachine();
+    const numbers = lottoMachine.generate();
+    numbers.forEach((number) => {
+      expect(number).toBeGreaterThanOrEqual(number, 1);
+      expect(number).toBeLessThanOrEqual(number, 45);
+    });
+  });
+
+  test("매번 다른 번호를 뽑아 내는지 확인", () => {
+    const lottoMachine = new LottoMachine();
+    const numbers1 = lottoMachine.generate();
+    const numbers2 = lottoMachine.generate();
+    numbers1.forEach((number, idx) => {
+      expect(number).not.toEqual(numbers2[2]);
+    })
+  })
+});
