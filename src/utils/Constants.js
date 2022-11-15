9d1c35a631469f9b3324680bdcaf5f0fb28ead60
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-15 15:18:46 +0900

    feat(Constants): add constant message & max length

---
+++ src/utils/Constants.js
@@ -0,0 +1,19 @@
+const MESSAGE = {
+  USER_ENTER_PURCHASE: "구입금액을 입력해 주세요.",
+  USER_BOUGHT: "구입금액을 입력해 주세요.",
+  LOTTO_WINNING_NUMBER: "당첨 번호를 입력해 주세요.",
+  LOTTO_BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
+  WINNING_STATISTICS: "당첨 통계\n---",
+  MATCH_THREE: "3개 일치 (5,000원) - ",
+  MATCH_FOUR: "4개 일치 (50,000원) - ",
+  MATCH_FIVE: "5개 일치 (1,500,000원) - ",
+  MATCH_BONUS: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+  MATCH_ALL: "6개 일치 (2,000,000,000원) - ",
+};
+
+const MAX_LENGTH = 6;
+
+module.exports = {
+  MESSAGE,
+  MAX_LENGTH,
+};
