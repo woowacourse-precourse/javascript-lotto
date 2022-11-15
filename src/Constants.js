ebe60aa8411412bc0d1beea4e65d62dc8ac4c630
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-15 12:36:51 +0900

    Style: 상수들 따로 분리해서 관리

---
+++ src/Constants.js
@@ -0,0 +1,21 @@
+export const TYPE = {
+	LOTTO: '로또',
+	WINNINGNUM: '당첨',
+	BONUS: '보너스',
+};
+
+export const PRIZE = {
+	FIRST: 2000000000,
+	SECOND: 30000000,
+	THIRD: 15000000,
+	FOURTH: 50000,
+	FIFTH: 5000,
+};
+
+export const PLACE = {
+	FIRST: 7,
+	SECOND: 6,
+	THIRD: 5,
+	FOURTH: 4,
+	FIFTH: 3,
+};
