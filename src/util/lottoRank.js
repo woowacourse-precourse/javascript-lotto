7544597f1d536a48f5c5fec880e57dce6caa5d4b
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-15 22:43:49 +0900

    feat: 순위 별 맟번 개수 반환

--- src/util/lottoRank.js
+++ src/util/lottoRank.js
@@ -2,26 +2,32 @@ const lottoRank = [
   {
     prize: 0,
     isBonus: false,
+    matchCount: -1,
   },
   {
     prize: 2_000_000_000,
     isBonus: false,
+    matchCount: 6,
   },
   {
     prize: 30_000_000,
     isBonus: true,
+    matchCount: 5,
   },
   {
     prize: 1_500_000,
     isBonus: false,
+    matchCount: 5,
   },
   {
     prize: 50_000,
     isBonus: false,
+    matchCount: 4,
   },
   {
     prize: 5_000,
     isBonus: false,
+    matchCount: 3,
   },
 ];
 
