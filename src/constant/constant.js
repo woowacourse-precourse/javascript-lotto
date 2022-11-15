126ada6a427931aa41bf203555657e4b27ed4210
Hchan2 <seongchan_@naver.com>
2022-11-15 20:44:36 +0900

    로또 구매 시 메세지 상수 수정

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,5 +1,7 @@
 const BUY_LOTTO = '구입금액을 입력해 주세요.';
-const BUY_MESSAGE = (buyAmount) => { `${buyAmount}개를 구매했습니다.` };
+const BUY_MESSAGE = (buyAmount) => {
+    return `${buyAmount}개를 구매했습니다.`
+};
 const WINNING_NUMBERS = '당첨 번호를 입력해주세요.';
 const BONUS_NUMBER = '보너스 번호를 입력해 주세요.';
 const THOUSAND = 1000;
