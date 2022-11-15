ca8077dbe8b73ad35d0495947211706d383d5be4
lineJeong <line.jeong32@gmail.com>
2022-11-15 19:18:24 +0900

    FEAT: 로또 구매수량 및 로또번호 출력

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ const { Random, Console } = require("@woowacourse/mission-utils");
 const { error_message } = require("./const");
 
 class App {
+  constructor() {
+    this.lottoQuantity = 0;
+    this.lottoArrays = [];
+  }
+
   play() {
     this.getPurchasePrice();
   }
@@ -9,13 +14,28 @@ class App {
   getPurchasePrice() {
     Console.readLine("구입금액을 입력해 주세요.\n", (price) => {
       this.checkPurchasePrice(price);
-      price;
+      this.printLottoQuantity(price);
+      this.printLottoArrays();
     });
   }
 
   checkPurchasePrice(price) {
     if (price % 1000 !== 0)
       throw new Error(error_message.not_thousand_won_unit);
+  }
+
+  printLottoQuantity(price) {
+    this.lottoQuantity = parseInt(price) / 1000;
+    Console.print(`\n${this.lottoQuantity}개를 구매했습니다.`);
+  }
+
+  printLottoArrays() {
+    for (let i = 0; i < this.lottoQuantity; i++) {
+      const lottoNums = Random.pickUniqueNumbersInRange(1, 45, 6);
+      const sortedLottoNums = lottoNums.sort((a, b) => a - b);
+      Console.print(sortedLottoNums);
+      this.lottoArrays.push(sortedLottoNums);
+    }
   }
 }
 
