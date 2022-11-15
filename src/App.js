42899faf0799892bbc64ffaf0edb738b00dd5ea2
subin <newdeas100@gmail.com>
2022-11-15 11:59:14 +0900

    feat(lotto) : 결과를 읽어 map만드는 함수 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,58 @@ const { validateWinNums, validateBounus, validatePrice } = require("./util.js");
 const Lotto = require("./Lotto.js");
 const [Console, Random] = [MissionUtils.Console, MissionUtils.Random];
 
+const mapKey = Object.freeze({
+  THREE: "3개 일치 (5000원)",
+  FOUR: "4개 일치 (50,000원)",
+  FIVE: "5개 일치 (1,500,000원)",
+  FIVE_BOUNS: "5개 일치, 보너스 볼 일치 (30,000,000원)",
+  SIX: "6개 일치 (2,000,000,000원)",
+});
+
+const priceMap = Object.freeze({
+  THREE: 5000,
+  FOUR: 50000,
+  FIVE: 1500000,
+  FIVE_BOUNS: 30000000,
+  SIX: 2000000000,
+})
+
+function initMap(map) {
+  map.set(mapKey.THREE, 0);
+  map.set(mapKey.FOUR, 0);
+  map.set(mapKey.FIVE, 0);
+  map.set(mapKey.FIVE_BOUNS, 0);
+  map.set(mapKey.SIX, 0);
+}
+
+function makeMap(map, result) {
+  for (let i = 0; i < result.length; i++) {
+    const [winCount, bonusCount] = result[i];
+    if (winCount + bonusCount < 3) continue;
+    if (winCount + bonusCount === 3) map.set(mapKey.THREE, map.get(mapKey.THREE) + 1);
+    if (winCount + bonusCount === 4) map.set(mapKey.FOUR, map.get(mapKey.FOUR) + 1);
+    if (winCount + bonusCount === 5) map.set(mapKey.FIVE, map.get(mapKey.FIVE) + 1);
+    if (winCount + bonusCount === 6 && bonusCount === 1) map.set(mapKey.FIVE_BOUNS, map.get(mapKey.FIVE_BOUNS) + 1);
+    if (winCount + bonusCount === 6 && bonusCount === 0) map.set(mapKey.SIX, map.get(mapKey.SIX) + 1);
+  }
+}
+
+function calProfit(map){
+
+  let profit = 0;
+
+  for (const [key,value] of map){
+    if(key === mapKey.THREE) profit += value*priceMap.THREE;
+    if(key === mapKey.FOUR) profit += value*priceMap.FOUR;
+    if(key === mapKey.FIVE) profit += value*priceMap.FIVE;
+    if(key === mapKey.FIVE_BOUNS) profit += value*priceMap.FIVE_BOUNS;
+    if(key === mapKey.SIX) profit += value*priceMap.SIX;
+  }
+
+  return profit;
+}
+
+
 class App {
   #totalMoney;
   #lottoArray;
@@ -89,19 +141,23 @@ class App {
       })
     })
   }
-  
-  makeResult(){
-    const [lottos, winNums, bonus] = [this.getLotto(), this.getWinNums(),this.getBonus()];
+
+  makeResult() {
+    const [lottos, winNums, bonus] = [this.getLotto(), this.getWinNums(), this.getBonus()];
     const result = [];
+    const map = new Map();
 
     lottos.forEach(lotto => {
       const winCount = lotto.filter(winNums).length;
-      const bonusCount = lotto.includes(bonus)? 1 : 0;
+      const bonusCount = lotto.includes(bonus) ? 1 : 0;
       result.push([winCount, bonusCount]);
-    })
+    });
 
-    Console.print(result);
-
+    initMap(map);
+    makeMap(map, result);
+    Console.print(map);
+    const profit = calProfit(map);
+    Console.print(profit);
   }
 
 
