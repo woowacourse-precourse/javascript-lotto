f87138c2100c91552824b9a6fb6ea71bafbf0a85
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-15 21:58:14 +0900

    Add: lotto 상수 추가

---
+++ src/util/lottoRank.js
@@ -0,0 +1,28 @@
+const lottoRank = [
+  {
+    prize: 0,
+    isBonus: false,
+  },
+  {
+    prize: 2_000_000_000,
+    isBonus: false,
+  },
+  {
+    prize: 30_000_000,
+    isBonus: true,
+  },
+  {
+    prize: 1_500_000,
+    isBonus: false,
+  },
+  {
+    prize: 50_000,
+    isBonus: false,
+  },
+  {
+    prize: 5_000,
+    isBonus: false,
+  },
+];
+
+module.exports = lottoRank;
