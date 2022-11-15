8258527d27da7f06a3ab99113dbef54ec99e9513
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-15 22:28:21 +0900

    refactor: 컨트롤러에 뷰 주입

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const Controller = require("./controller/Controller");
+const View = require("./view/View");
+
 class App {
-  play() {}
+  play() {
+    const view = new View;
+    const controller = new Controller(view);
+  }
 }
 
 module.exports = App;
