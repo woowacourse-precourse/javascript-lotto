d29393a7facb4588fbf4ebf73441439864292d9f
KangYunJi <dbswl701@ajou.ac.kr>
2022-11-15 21:54:31 +0900

    feat: 수익률 계산 및 출력

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ const Lotto = require("./Lotto");
 
 class App {
   constructor() {
+    this.cache=0;
     this.lottos=[];
     this.bonus=0;
-    this.result = {
-      lotto : 0,
-      bonus : false,
-    }
+    // this.result = {
+    //   lotto : 0,
+    //   bonus : false,
+    // }
     this.result2 = {
       3: 0,
       4: 0,
@@ -18,12 +19,42 @@ class App {
       '5bonus': 0,
       6: 0,
     }
+    this.reward = {
+      3: 5000,
+      4: 50000,
+      5: 1500000,
+      '5bonus': 30000000,
+      6: 2000000000,
+    };
     this.winLotto=0;
     this.lottoCount=0;
+    this.yieldSum = 0;
+    this.rate = 0;
   }
 
   // statistics(){
   // }
+  printYieldRate(yieldSum, cache){
+    this.rate = yieldSum/cache * 100;
+    this.rate = Math.round(this.rate * 10) / 10;
+    Console.print(`총 수익률은 ${this.rate}%입니다.`)
+  }
+
+  yieldRate() {
+    // 분자 : 총 수입
+    // 분모 : 결제 금액
+    for(const key in this.result2){
+      if(this.result2[key] !== 0){
+        this.yieldSum += this.reward[key];
+        console.log(this.yieldSum);
+      }
+      // console.log(yield);
+    }
+
+    console.log(this.cache);
+    // const rate = this.yieldSum/this.cache * 100;
+    this.printYieldRate(this.yieldSum, this.cache);
+  }
 
   PrintLotto(){
     Console.print(`3개 일치 (5,000원) - ${this.result2[3]}개`);
@@ -101,6 +132,7 @@ class App {
   InputCache(){
     Console.readLine('구입금액을 입력해 주세요.', (answer)=>{
       // 맞는지 확인하는 함수
+      this.cache = answer;
       this.lottoCount = this.isValidCacheInput(answer);
       Console.print(`${this.lottoCount}개를 구매했습니다.`);
     })
@@ -129,6 +161,9 @@ class App {
 
     // 당첨내역 출력
     this.PrintLotto();
+
+    // 수익률 계산 및 출력
+    this.yieldRate();
   }
 }
 
