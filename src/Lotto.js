7d80e5669618a98e31ff6bf99da807363b4b2628
yuna Kim <yuna000804@gmail.com>
2022-11-15 19:07:01 +0900

    feat : 보너스 번호 예외 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -21,5 +21,15 @@ class Lotto {
     this.numbers = numbers;
   }
   
+  validateBonus = (bonusNumber) => {
+    this.bonusNumber = Number(bonusNumber);
+    if(this.numbers.includes(this.bonusNumber)) {
+      throw new Error("[ERROR] 로또 번호와 중복되지 않아야 합니다.");
+    }
+    if (this.bonusNumber < 1 || this.bonusNumber > 45) {
+      throw new Error("[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.");
+    }
+  }
+
 }
 module.exports = Lotto;
