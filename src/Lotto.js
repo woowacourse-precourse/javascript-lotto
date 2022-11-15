cb45b3d5c6580e22d30d2b2aad9e3b095e5843f3
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2022-11-15 22:51:56 +0900

    style : 주석 추가 및 줄 정리

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,27 +12,32 @@ class Lotto {
   };
 
   constructor(winningNumbers) {
+    // 당첨 번호 생성자
     const winning = winningNumbers.map((number) => Number(number));
     Lotto.validate(winning);
     this.#numbers = { winning };
   }
 
   static validate(number) {
+    // 당첨 번호 유효성 검사
     checkLength(number);
     checkLottoRange(number);
     checkDuplicate(number);
   }
   static validate_Bonus(number, winningNumbers) {
+    // 당첨 번호 및 보너스 번호 유효성 검사
     checkNumberRange(number);
     checkBonusDuplicate(number, winningNumbers);
   }
   setBonusNumber(bonusNumber) {
+    // 보너스 번호 셋
     Lotto.validate_Bonus(bonusNumber, this.#numbers.winning);
 
     this.#numbers.bonus = bonusNumber;
   }
 
   getNumbers() {
+    // 당첨 번호 출력
     return this.#numbers;
   }
 }
