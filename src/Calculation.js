0705466581bfd631d890d041647307123d709204
Deemou <4106243@naver.com>
2022-11-15 23:03:22 +0900

    feat(Calculation): 숫자 문자열 천 단위마다 콤마 찍는 기능 추가

--- src/Calculation.js
+++ src/Calculation.js
@@ -1,10 +1,14 @@
 class Calculation {
   static getRatesOfReturn(a, b) {
     const num = (a / b) * 100;
-    return this.roundToTwo(num);
+    const ROR = this.roundToTwo(num);
+    return this.setCommas(ROR);
   }
   static roundToTwo(num) {
     return (+(Math.round(num + 'e+2') + 'e-2')).toFixed(1);
+  }
+  static setCommas(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   }
 }
 
