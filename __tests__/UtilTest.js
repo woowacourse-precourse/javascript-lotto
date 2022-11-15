6974ae15cd5e4791b697bf1df22d8e22b8e5cb3d
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-15 12:56:31 +0900

    fix : util 함수 테스트 수정

--- __tests__/UtilTest.js
+++ __tests__/UtilTest.js
@@ -2,8 +2,9 @@ const {sortAscending} = require('../src/Util');
 
 describe("유틸 클래스 테스트", () => {
     test("sortAscending 테스트", () => {
-        expect(() => {
-            sortAscending([6, 5, 4, 3, 2, 1])
-        }).toEqual([1, 2, 3, 4, 5, 6])
+        let array = [6, 5, 4, 3, 2, 1]
+        sortAscending(array)
+
+        expect(array).toEqual([1, 2, 3, 4, 5, 6])
     });
 });
