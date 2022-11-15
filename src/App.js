b913fee6ff9322d302c43ee0547643447d6f20db
Minju Park <project.mizzu@gmail.com>
2022-11-15 16:15:18 +0900

    Feat : 로또 구입 금액 에러 기능 추가
    - 입력한 구입금액이 1,000원으로 나누어 떨어지지 않는 경우
    - 입력한 구입금액이 0원인 경우
    - 입력한 구입금액이 음수인 경우

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
 
 class App {
+  lotto = new Lotto();
+
   constructor(text) {
     this.text = text;
   }
 
   play() {
-    Console.readLine(this.text, (cost) => {
+    this.lotto.getPurchaseAmount();
+    /* Console.readLine(this.text, (cost) => {
       let quantity = cost / 1000;
+      const result = [];
       Console.print(`${'\n'}${quantity}개를 구매했습니다.`);
       let computer = [];
       while (quantity >= 1) {
@@ -18,24 +23,41 @@ class App {
           }
         }
         Console.print(computer.sort((a, b) => a - b));
+        result.push(computer.sort((a, b) => a - b));
         computer = [];
         quantity -= 1;
       }
-      Console.readLine(`${'\n'}당첨 번호를 입력해 주세요.${'\n'}`, (number) =>
+      Console.readLine(`${'\n'}당첨 번호를 입력해 주세요.${'\n'}`, (number) => {
+        const userNumbers = [];
+        const userBonus = [];
+        userNumbers.push(...number.split(','));
+        const numUser = userNumbers.map(Number);
         Console.readLine(
           `${'\n'}보너스 번호를 입력해 주세요.${'\n'}`,
           (bonus) => {
-            Console.print(`number: ${number} bonus: ${bonus}`);
+            userBonus.push(Number(bonus));
             Console.print(`${'\n'}당첨 통계`);
             Console.print(`---`);
+            result.forEach((num) => {
+              num.forEach((el) => {
+                for (let i = 0; i < numUser.length; i += 1) {
+                  if (el === numUser[i]) {
+                    Console.print(el);
+                  }
+                }
+                if (el === userBonus[0]) {
+                  Console.print(el);
+                }
+              });
+            });
           }
-        )
-      );
-    });
+        );
+      });
+    }); */
   }
 }
 
-const test = new App(`구입금액을 입력해 주세요.${'\n'}`);
+const test = new App();
 test.play();
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,22 +1,52 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
-  }
+  // constructor(numbers) {
+  //   this.validate(numbers);
+  //   this.#numbers = numbers;
+  // }
 
-  get err() {
-    return this.validate();
-  }
+  // validate(numbers) {
+  //   if (numbers.length !== 6) {
+  //     throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+  //   }
+  // }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+  // TODO: 추가 기능 구현
+
+  checkPurchaseAmount(cost) {
+    if (cost === '0') {
+      throw new Error('[ERROR] 로또는 1장 이상 구매 가능합니다.');
+    }
+    if (cost % 1000 !== 0) {
+      throw new Error('[ERROR] 1,000원 단위로만 입력 가능합니다.');
+    }
+    if (cost < 0) {
+      throw new Error('[ERROR] 1,000원 이상의 올바른 금액을 입력해주세요.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  getPurchaseAmount() {
+    Console.readLine(`구입금액을 입력해 주세요.${'\n'}`, (cost) => {
+      this.checkPurchaseAmount(cost);
+    });
+  }
+
+  getRandomNumbers(quantity) {
+    const result = [];
+    for (let i = 0; i < quantity; i += 1) {
+      const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+        (a, b) => a - b
+      );
+      result.push(randomNumbers);
+    }
+    return result;
+  }
 }
+
+const test = new Lotto();
+console.log(test.getRandomNumbers());
 
 module.exports = Lotto;
