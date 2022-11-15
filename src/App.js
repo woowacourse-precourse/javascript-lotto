9417176be46e28f447e7ceab797cba8d9a3b4c0b
Choejeonghun <saa00123@naver.com>
2022-11-15 20:10:41 +0900

    feat: 최종 출력 확인

--- src/App.js
+++ src/App.js
@@ -29,6 +29,8 @@ class App {
 
     result = lotto.CompareResult(BonusNum, LottoList);
     YieldAmount = this.CalcYield(result);
+
+    this.FinalResult(result, YieldAmount);
   }
 
   Input(text) {
@@ -105,6 +107,20 @@ class App {
     YieldAmount = (YieldAmount / this.purchase) * 100;
     YieldAmount.toFixed(1);
     return YieldAmount;
+  }
+
+  FinalResult(result, YieldAmount) {
+    MissionUtils.Console.print(`당첨 통계`);
+    MissionUtils.Console.print(`---`);
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${result[0]}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${result[1]}개`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${result[2]}개`);
+    MissionUtils.Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[3]}개`
+    );
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${result[4]}개`);
+    MissionUtils.Console.print(`총 수익률은 ${YieldAmount}%입니다.`);
+    MissionUtils.Console.print(`총 수익률은 ${YieldAmount}%입니다.`);
   }
 }
 
