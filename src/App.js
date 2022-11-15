4881e2c8aac354c528dfe9992000811dcf4185d9
spsp8426 <spsp8426@naver.com>
2022-11-15 09:30:11 +0900

    feat : 구입금액 예외처리 추가

--- src/App.js
+++ src/App.js
@@ -7,13 +7,20 @@ class App {
     this.bonusNum = null; //보너스 번호 숫자
   }
   setMoney() {
-    Console.readline("구입금액을 입력해 주세요.\n",(num) => {
+    Console.readLine("구입금액을 입력해 주세요.\n",(num) => {
+      if (!/^\d+$/.test(num))
+        throw new Error("[ERROR] 구입금액에 문자가 포함되어 있습니다.");
       const money = parseInt(num);
+      if(money % 1000 !== 0)
+        throw new Error("[ERROR] 구입금액 오류");
+        this.money = money;
     })
   }
 
   play() {
+    this.setMoney();
   }
 }
-
+const app = new App();
+app.play();
 module.exports = App;
