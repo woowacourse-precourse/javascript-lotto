35e81126f063ae6484432c8a461f978e5632366f
deli-ght <minjju0660@gmail.com>
2022-11-15 23:08:21 +0900

    test(LottoTest.js): add winning grade test

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -20,5 +20,28 @@ describe("로또 클래스 테스트", () => {
     }).toThrow(IS_ENOUGH);
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("5등 당첨", () => {
+    const LOTTO = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(LOTTO.win([1, 2, 3, 7, 8, 9], 10)).toBe(5);
+  });
+
+  test("4등 당첨", () => {
+    const LOTTO = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(LOTTO.win([1, 2, 3, 4, 8, 9], 10)).toBe(4);
+  });
+
+  test("3등 당첨", () => {
+    const LOTTO = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(LOTTO.win([1, 2, 3, 4, 5, 9], 10)).toBe(3);
+  });
+
+  test("2등 당첨", () => {
+    const LOTTO = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(LOTTO.win([1, 2, 3, 4, 5, 9], 6)).toBe(2);
+  });
+
+  test("1등 당첨", () => {
+    const LOTTO = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(LOTTO.win([1, 2, 3, 4, 5, 6], 10)).toBe(1);
+  });
 });
