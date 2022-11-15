14e8d86b06e34b00d199245a39ada276e4ca1d03
runkey <csi9509@naver.com>
2022-11-15 20:15:32 +0900

    Feat: 출력 메세지

--- src/Message.js
+++ src/Message.js
@@ -5,3 +5,17 @@ const GAME = { // 게임 정보
   PRICE: 1000,
 };
 
+const MESSAGE = { // 출력 메세지
+  BUY: '구입금액을 입력해 주세요.\n',
+  COUNT: (count) => `\n${count}개를 구매했습니다.`,
+  WIN_NUM: '당첨 번호를 입력해 주세요.\n',
+  BONUS_NUM: '보너스 번호를 입력해 주세요.\n',
+  RESULT: '당첨 통계\n---',
+  first: (count) => `6개 일치 (2,000,000,000원) - ${count}개`,
+  second: (count) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${count}개`,
+  third: (count) => `5개 일치 (1,500,000원) - ${count}개`,
+  fourth: (count) => `4개 일치 (50,000원) - ${count}개`,
+  fifth: (count) => `3개 일치 (5,000원) - ${count}개`,
+  return: (percentage) => `총 수익률은 ${percentage}%입니다.`,
+};
+
