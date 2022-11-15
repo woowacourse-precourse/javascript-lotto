6e4acb56db907bfb098012803abb2e681a4d75d5
ksone02 <eksovus@gmail.com>
2022-11-15 20:22:41 +0900

    test(Lotto): Lotto 클래스 메소드 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
 const Lotto = require('../src/components/Lotto');
+const { ERROR } = require('../src/data/constants');
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -16,9 +17,24 @@ describe('로또 클래스 테스트', () => {
 
   // 아래에 추가 테스트 작성 가능
 
-  test('로또 번호에 1~45의 범위를 지키지 않은 수가 있다면 예외가 발생한다..', () => {
+  test('로또 번호에 1~45의 범위를 지키지 않은 수가 있다면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 0, 46]);
-    }).toThrow('[ERROR]');
+    }).toThrow(ERROR.LOTTO_RANGE);
+  });
+
+  test('로또 번호 반환', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.getLottoNum()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  test('로또 맞춰보기', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.checkWinning([1, 2, 3, 4, 5, 6], 7)).toEqual(6);
+  });
+
+  test('로또 맞춰보기(보너스)', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.checkWinning([1, 2, 3, 4, 5, 23], 6)).toEqual('bonus');
   });
 });
