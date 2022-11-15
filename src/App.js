c1ad553166759876acaa6e48c63db2bac196bb17
jiyun1006 <jiyun4251@gmail.com>
2022-11-15 19:03:46 +0900

    feat : 로또 개수에 따라 로또를 생성하는 함수
    Lotto 클래스에 로또 배열을 넘겨줘서 유효성 검사를 거치고, 출력

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const lottoPrice = () => {
   MissionUtils.Console.readLine('구입금액을 입력해 주세요. \n', (answer) => {
     lottoPriceValidate(+answer);
     const lottoCnt = answer / LOTTO;
+    createLotto(lottoCnt);
   })
 
 }
@@ -21,6 +22,20 @@ const lottoPriceValidate = (lottoprice) => {
     throw new Error("[ERROR] 올바른 금액을 입력 하세요.");
   }
 }
+
+
+const createLotto = (lottocnt) => {
+  MissionUtils.Console.print(`\n${lottocnt}개를 구매했습니다.`);
+  for (let i = 0; i < lottocnt; i++){
+    let numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    numbers.sort();
+    const lottoArr = new Lotto(numbers);
+    
+  }
+  
+
+}
+
 
 const app = new App();
 app.play();
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,8 +1,11 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
+    this.printlotto(numbers);
     this.#numbers = numbers;
   }
 
@@ -10,6 +13,10 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
+
+  printlotto(numbers) {
+    MissionUtils.Console.print(numbers);
   }
 
   // TODO: 추가 기능 구현
