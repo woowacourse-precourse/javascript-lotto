8a8c8bc4200534ac1a73ee06743194599ded6771
lineJeong <line.jeong32@gmail.com>
2022-11-15 18:39:27 +0900

    FEAT: 로또 구입금액 입력과 이에 따른 예외처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const { Random, Console } = require("@woowacourse/mission-utils");
+const { error_message } = require("./const");
+
 class App {
-  play() {}
+  play() {
+    this.getPurchasePrice();
+  }
+
+  getPurchasePrice() {
+    Console.readLine("구입금액을 입력해 주세요.\n", (price) => {
+      this.checkPurchasePrice(price);
+      price;
+    });
+  }
+
+  checkPurchasePrice(price) {
+    if (price % 1000 !== 0)
+      throw new Error(error_message.not_thousand_won_unit);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/const.js
@@ -0,0 +1,5 @@
+const error_message = {
+  not_thousand_won_unit: "[ERROR] 구입 금액은 1,000원 단위입니다",
+};
+
+module.exports = { error_message };
