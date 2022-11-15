33e76dc24dac42a160b84fe1a85ce9dc85eef2ba
adultlee <seoungin1228@gmail.com>
2022-11-15 16:01:06 +0900

    feat: Lotto 정렬
    Lotto의 생성과 동시에 정렬을 진행합니다.

--- src/Lotto/Lotto.js
+++ src/Lotto/Lotto.js
@@ -2,7 +2,9 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class Lotto {
   constructor() {
-    const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 46, 6);
+    const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 46, 6).sort(
+      (a, b) => a - b
+    );
     this.#numbers = numbers;
   }
 }
