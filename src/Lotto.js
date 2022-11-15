326771ebcdd6d8f1cefa93fe12e8ed3d447b97c8
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-15 16:41:49 +0900

    feat(Lotto): add bonus number duplicates validation function

--- src/Lotto.js
+++ src/Lotto.js
@@ -21,6 +21,12 @@ class Lotto {
         throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
     });
   }
+
+  validateBonus(bonus) {
+    if (this.#numbers.indexOf(bonus) !== -1) {
+      throw new Error("[ERROR] 보너스 번호가 중복됩니다.");
+    }
+  }
   // TODO: 추가 기능 구현
 }
 
