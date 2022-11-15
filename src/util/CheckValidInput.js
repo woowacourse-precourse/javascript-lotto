6a16cc903eea6e122e9841da69d95061a91c9865
leeharyung <lhr4884@naver.com>
2022-11-15 20:34:46 +0900

    feat: bonus number input 유효성 검사

--- src/util/CheckValidInput.js
+++ src/util/CheckValidInput.js
@@ -19,7 +19,19 @@ const checkValidWinningNumberInput = (winningNumber) => {
   }
 };
 
+const checkValidBonusNumberInput = (winningNumber, bonusNumber) => {
+  if (bonusNumber < 1 || bonusNumber > 45) {
+    throw new Error('[ERROR] 보너스 번호는 1~45 사이의 숫자여야 합니다.');
+  }
+  if (winningNumber.includes(bonusNumber)) {
+    throw new Error(
+      '[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.',
+    );
+  }
+};
+
 module.exports = {
   checkValidMoneyInput,
   checkValidWinningNumberInput,
+  checkValidBonusNumberInput,
 };
