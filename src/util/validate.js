3ac73945d8b283a6f90b37ce2dcc15d4a30c025d
Bori-github <qhflrnfl4324@gmail.com>
2022-11-15 23:47:56 +0900

    refactor: 유효성 검사 유틸 함수로 분리

---
+++ src/util/validate.js
@@ -0,0 +1,26 @@
+const { VALID_LENGTH, VALID_MAX_NUM, VALID_MIN_NUM } = require('../constant');
+
+function validateLength(numbers) {
+  if (numbers.length !== VALID_LENGTH) {
+    throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+  }
+  return true;
+}
+
+function validateDuplicate(numbers) {
+  if (new Set(numbers).size !== VALID_LENGTH) {
+    throw new Error('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
+  }
+  return true;
+}
+
+function validateNumberRange(numbers) {
+  for (const number of numbers) {
+    if (number < VALID_MIN_NUM || number > VALID_MAX_NUM) {
+      throw new Error('[ERROR] 로또 번호는 1~45 사이여야 합니다.');
+    }
+  }
+  return true;
+}
+
+module.exports = { validateLength, validateDuplicate, validateNumberRange };
