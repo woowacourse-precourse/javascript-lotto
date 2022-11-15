9096af4f72a518f5718033551394eec5a5f3d114
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-11-15 21:28:22 +0900

    test(lotto): CalculatorTest 수익률 테스트 코드 작성

--- __tests__/CalculatorTest.js
+++ __tests__/CalculatorTest.js
@@ -6,4 +6,10 @@ describe("계산기 클래스 테스트", () => {
     const result = calculator.calculateAmountOfLotto(9000);
     expect(result).toBe(9);
   });
+
+  test("수익률 테스트", () => {
+    const calculator = new Calculator();
+    const result = calculator.calculateRateOfReturn(8000, 5000);
+    expect(result).toBe(62.5);
+  });
 });
