d01040f9d6b57ebc530dcbdb73e8789689af2e5d
curomame <curomame@naver.com>
2022-11-15 21:37:49 +0900

    ➕ feat - <Lotto> [CONSTRUCTOR] add edit
    기존 valid 함수를 Helper의 메서드들로 변경하여 유효성 검사를 생성자에 적용

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,15 @@
+const { Helper } = require("./lib/Helper");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    Helper.checkValidLength(numbers);
+    Helper.checkDuplicatedNumber(numbers);
+    Helper.checkRangedNumber(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
