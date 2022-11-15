a72b3191aefc8f8c15d9a055cb66ef2ed8b96874
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-15 16:51:46 +0900

    refactor(CompareLotto): 원시타입 상수화

--- src/CompareLotto.js
+++ src/CompareLotto.js
@@ -1,4 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { COMPARE_VALUE, MATCH, LOTTO_WIN, PRIZE } = require('./setting/Constants');
 
 const LOTTO_WIN = [5000, 50000, 1500000, 30000000, 2000000000];
 
@@ -26,16 +27,16 @@ class CompareLotto {
   }
 
   static number(count) {
-    const howManyWin = Array.from({ length: 5 }, () => 0);
+    const howManyWin = Array.from({ length: COMPARE_VALUE.length }, () => COMPARE_VALUE.zero);
 
-    for (let i = 0; i < count.length; i += 1) {
+    for (let i = COMPARE_VALUE.zero; i < count.length; i += COMPARE_VALUE.one) {
       const { correctNumber, correctBonus } = count[i];
 
-      if (correctNumber === 3) howManyWin[0] += 1;
-      if (correctNumber === 4) howManyWin[1] += 1;
-      if (correctNumber === 5) howManyWin[2] += 1;
-      if (correctNumber === 5 && correctBonus) howManyWin[3] += 1;
-      if (correctNumber === 6) howManyWin[4] += 1;
+      if (correctNumber === MATCH.three) howManyWin[COMPARE_VALUE.zero] += PRIZE;
+      if (correctNumber === MATCH.four) howManyWin[COMPARE_VALUE.one] += PRIZE;
+      if (correctNumber === MATCH.five) howManyWin[COMPARE_VALUE.two] += PRIZE;
+      if (correctNumber === MATCH.five && correctBonus) howManyWin[COMPARE_VALUE.three] += PRIZE;
+      if (correctNumber === MATCH.six) howManyWin[COMPARE_VALUE.four] += PRIZE;
     }
 
     return howManyWin;
@@ -43,7 +44,7 @@ class CompareLotto {
 
   static totalMoney(howManyWin) {
     const total = [];
-    for (let i = 0; i < howManyWin.length; i += 1) {
+    for (let i = COMPARE_VALUE.zero; i < howManyWin.length; i += COMPARE_VALUE.one) {
       total.push(howManyWin[i] * LOTTO_WIN[i]);
     }
 
@@ -55,13 +56,11 @@ class CompareLotto {
   static printResult(howManyWin) {
     Console.print('\n당첨 통계');
     Console.print('---');
-    Console.print(`3개 일치 (5,000원) - ${howManyWin[0]}개`);
-    Console.print(`4개 일치 (50,000원) - ${howManyWin[1]}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${howManyWin[2]}개`);
-    Console.print(
-      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${howManyWin[3]}개`
-    );
-    Console.print(`6개 일치 (2,000,000,000원) - ${howManyWin[4]}개`);
+    Console.print(`3개 일치 (5,000원) - ${howManyWin[COMPARE_VALUE.zero]}개`);
+    Console.print(`4개 일치 (50,000원) - ${howManyWin[COMPARE_VALUE.one]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${howManyWin[COMPARE_VALUE.two]}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${howManyWin[COMPARE_VALUE.three]}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${howManyWin[COMPARE_VALUE.four]}개`);
   }
 }
 
