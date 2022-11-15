e6a5686dfd19da1ff17e1338e61eb87f213cebd1
Choejeonghun <saa00123@naver.com>
2022-11-15 20:05:17 +0900

    feat: 입력받은 금액만큼 로또 발행 및 예외 처리

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App {
     this.purchase = this.Input('구입금액을 입력해 주세요.\n');
     if (this.purchase === undefined) return 0;
     this.CheckError();
+
+    amount = this.getQuantity();
   }
 
   Input(text) {
@@ -32,6 +34,16 @@ class App {
       MissionUtils.Console.close();
       throw new Error('[ERROR] 숫자 범위를 확인해주세요');
     }
+  }
+
+  getQuantity() {
+    let amount = Number(this.purchase);
+    if (amount % 1000 !== 0) {
+      MissionUtils.Console.cose();
+      throw new Error('[ERROR] 구입 금액이 1000원 단위가 아닙니다.');
+    }
+    let result = amount / 1000;
+    return result;
   }
 }
 
