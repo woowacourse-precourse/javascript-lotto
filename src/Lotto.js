ac8467aee687b9c10888e373c9d0e2bf7b279bf0
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-15 18:58:17 +0900

    Revert "style: change function style"
    This reverts commit 788db0ba3f94e6a1ced6e79f4fcb7f8a33ac45bb.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,55 +1,56 @@
-const throwError = require('./components/throwError');
+const ThrowError = require('./components/ThrowError');
 const { LOTTO_ERROR, LOTTO_INFO } = require('./constant');
 
 class Lotto {
   #numbers;
 
-  constructor (numbers) {
+  constructor(numbers) {
     this.#numbers = numbers;
     this.showValidateResult(this.#numbers);
   }
 
-  showValidateResult (lottoNumbers) {
+  showValidateResult(lottoNumbers) {
     const resultMessage = this.validate(lottoNumbers);
-    return resultMessage ? throwError(LOTTO_ERROR[resultMessage]) : true;
+    return resultMessage ? ThrowError(LOTTO_ERROR[resultMessage]) : true;
   }
 
-  validate (numbersList) {
+  validate(numbersList) {
     return (
-      this.isNumber(numbersList)
-      || this.isSplit(numbersList)
-      || this.isNumberLength(numbersList)
-      || this.isNumberRepeated(numbersList)
-      || this.isNumberRange(numbersList)
-      || this.isIncludeZero(numbersList)
+      this.isNumber(numbersList) ||
+      this.isSplit(numbersList) ||
+      this.isNumberLength(numbersList) ||
+      this.isNumberRepeated(numbersList) ||
+      this.isNumberRange(numbersList) ||
+      this.isIncludeZero(numbersList)
     );
   }
 
-  isNumber (numbersList) {
+  isNumber(numbersList) {
     return /^\d+$/.test(numbersList.join('')) ? false : 'NUMBER';
   }
 
-  isSplit (numbersList) {
+  isSplit(numbersList) {
     return numbersList.length === 1 ? 'SPLIT' : false;
   }
 
-  isNumberLength (numbersList) {
+  isNumberLength(numbersList) {
     return numbersList.length === LOTTO_INFO.PICK ? false : 'LEGNTH';
   }
 
-  isNumberRepeated (numbersList) {
+  isNumberRepeated(numbersList) {
     return new Set(numbersList).size === LOTTO_INFO.PICK ? false : 'REPEAT';
   }
 
-  isNumberRange (numbersList) {
+  isNumberRange(numbersList) {
     return numbersList.filter(
-      (nums) => Number(nums) >= LOTTO_INFO.START_RANGE && Number(nums) <= LOTTO_INFO.LAST_RANGE,
+      (number) =>
+        Number(number) >= LOTTO_INFO.START_RANGE && Number(number) <= LOTTO_INFO.LAST_RANGE
     ).length === LOTTO_INFO.PICK
       ? false
       : 'RANGE';
   }
 
-  isIncludeZero (numberList) {
+  isIncludeZero(numberList) {
     return numberList.filter((number) => number.startsWith('0')).length ? 'ZERO' : false;
   }
 }
--- src/components/ThrowError.js
+++ src/components/ThrowError.js
@@ -1,9 +1,9 @@
 const { LIBRARY_URL } = require('../constant');
 const { Console } = require(LIBRARY_URL);
 
-const throwError = function (errorMessage) {
+function ThrowError(errorMessage) {
   Console.close();
   throw new Error(errorMessage);
-};
+}
 
-module.exports = throwError;
+module.exports = ThrowError;
