748cc79e9f875f1dad71f1c30f167a78d495db45
c0c0pang <coc1789@naver.com>
2022-11-15 22:08:42 +0900

    test: 로또 번호가 문자 인지 확인하는 테스트 기능 구현

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,6 @@
 const Lotto = require("../src/Lotto");
+const MoneyExceptionHandling = require("../src/MoneyExceptionHandling");
+const BonusExceptionHandling = require("../src/BonusExceptionHandling");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -21,6 +23,12 @@ describe("로또 클래스 테스트", () => {
   });
 
   test("로또 번호가 1~45 아닌 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 49]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호는 숫자만 입력해야 합니다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 49]);
     }).toThrow("[ERROR]");
