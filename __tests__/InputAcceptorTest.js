1a3bd94bd1498fb727a7ae0bb086a56dd3168248
yeongseokim <1903020@ewhain.net>
2022-11-15 09:26:02 +0900

    test(InputAcceptor): 숫자가 아닌 문자에 대한 에러 발생 테스트 코드 및 숫자로 이루어진 문자에 대한 true 반환 테스트

--- __tests__/InputAcceptorTest.js
+++ __tests__/InputAcceptorTest.js
@@ -9,4 +9,29 @@ describe("인풋 처리 테스트", () => {
         const stringArray = ["1", "2,", "3", "4", "5"];
         expect(InputAcceptor.stringToNumber(stringArray)).toEqual([1, 2, 3, 4, 5]);
     });
+
+    test("checkANumber: 숫자가 아닌 문자에 대해 에러 발생 테스트", () => {
+        const str = 'a';
+        expect(() => InputAcceptor.checkANumber(str)).toThrow("[ERROR] 숫자만 입력해주세요.");
+    });
+
+    test("checkANumber: 숫자 문자에 대해 true 반환 테스트", () => {
+        const str = '1';
+        expect(InputAcceptor.checkANumber(str)).toBeTruthy();
+    });
+
+    test("checkNumbers: 숫자가 아닌 문자가 포함된 배열 에러 발생 테스트1", () => {
+        const strArray = ['1', '2', 'a'];
+        expect(() => InputAcceptor.checkNumbers(strArray)).toThrow("[ERROR] 숫자만 입력해주세요.");
+    });
+
+    test("checkNumbers: 숫자가 아닌 문자가 포함된 배열 에러 발생 테스트2", () => {
+        const strArray = ['1', '', '3'];
+        expect(() => InputAcceptor.checkNumbers(strArray)).toThrow("[ERROR] 숫자만 입력해주세요.");
+    });
+
+    test("checkNumbers: 숫자 문자만 포함된 true 반환 테스트", () => {
+        const strArray = ['1', '2', '3'];
+        expect(InputAcceptor.checkNumbers(strArray)).toBeTruthy();
+    });
 })
