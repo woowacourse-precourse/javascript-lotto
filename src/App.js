8038670aeaf3679f82da68907049c8b2ae7660c5
Dongkeun <hongdongk@naver.com>
2022-11-15 19:18:43 +0900

    기능2(2): 구입갯수만큼 랜덤숫자생성하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-const { Console } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 
 class App {
 
   purchasemoney;
+  numberoflotto;
+  sixlotterynumbers = [];
   
   play() {
     this.purchaseLotto();
@@ -11,11 +13,11 @@ class App {
 
   purchaseLotto() {
     Console.readLine("구입금액을 입력해 주세요.", (money) => {
-      const numberoflotto = (money / 1000);
       this.purchasemoney = money;
-      
+      this.numberoflotto = (money / 1000);
       this.validatePrice();
-      this.countingLotto(numberoflotto);
+      this.countingLotto();
+      this.printingLotto();
     });
   }
 
@@ -23,9 +25,19 @@ class App {
     if (this.purchasemoney % 1000 !== 0) throw new Error("[ERROR] 1000단위의 수를 입력해주세요!");
   }
 
-  countingLotto(numberoflotto) {
-    Console.print(`${numberoflotto}개를 구매했습니다.`);
+  countingLotto() {
+    Console.print(`${this.numberoflotto}개를 구매했습니다.`);
   }
+
+  printingLotto(){
+    for (let count = 1; count <= this.numberoflotto; count++) {
+      let lotterynumber = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a,b)=> a-b)
+      Console.print(`[${lotterynumber.join(', ')}]`);
+      this.sixlotterynumbers.push(lotterynumber);
+    }
+  }
+
+
 
 
 }
