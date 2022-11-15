8ee4aba20b654e411199aeb3900eb871ebc65fc2
seungki <sysh9498@gmail.com>
2022-11-15 20:10:51 +0900

    feat(Lotto): 같은 번호 갯수를 통해 당첨된 등수의 갯수를 저장하는 기능

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ class App {
     this.lottos = [];
     this.hitLottoNumber = [];
     this.bonusNumber = null;
+    this.rewards = [];
+    this.winning = {
+      FIRST: 0,
+      SECOND: 0,
+      THIRD: 0,
+      FOURTH: 0,
+      FIFTH: 0,
+    };
   }
   play() {
     Console.readLine(Const.BUY_LOTTO_MESSAGE, price => {
@@ -24,6 +32,27 @@ class App {
 
   countSameNumber(lotto, hitLottoNumber) {
     return lotto.filter(number => hitLottoNumber.includes(number)).length;
+  }
+
+  checkRewards(lottos, bonusNumber) {
+    lottos.forEach(lotto => {
+      let hitCount = this.countSameNumber(lotto, this.hitLottoNumber);
+      this.rewards.push(hitCount);
+    });
+
+    this.rewards.forEach((value, index) => {
+      if (value === 6) {
+        this.winning.FIRST += 1;
+      } else if (value === 5 && lottos[index].includes(bonusNumber)) {
+        this.winning.SECOND += 1;
+      } else if (value === 5) {
+        this.winning.THIRD += 1;
+      } else if (value === 4) {
+        this.winning.FOURTH += 1;
+      } else if (value === 3) {
+        this.winning.FIFTH += 1;
+      }
+    });
   }
 }
 
