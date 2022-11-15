055d3b8e7891b16c222f3aa4c1f4ea4cff3be869
dudgp128 <20181048@sungshin.ac.kr>
2022-11-15 15:48:54 +0900

    TEST([입력]당첨 번호 잘못된 입력시) : 함수의 반환값이 변경되어 테스트 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -18,14 +18,11 @@ describe("로또 클래스 테스트", () => {
   test("1000으로 나누어 떨어지는 숫자가 아닌 경우, 예외가 발생한다.", () => {
     const app = new App();
     const number = ["1400", "50", "", "한글", "10005", "900", "나1000"];
-    const isValid = false;
 
-    number.forEach((output) =>
-      expect(app.isValidMoney(output)).toEqual(isValid)
-    );
+    number.forEach((output) => expect(app.isValidMoney(output)).toEqual(false));
   });
 
-  test("[입력]로또 번호에 잘못된 입력시", () => {
+  test("[입력]당첨 번호에 잘못된 입력시", () => {
     const app = new App();
     const throwInput = [
       "",
@@ -36,17 +33,16 @@ describe("로또 클래스 테스트", () => {
       "70,1,2,3,4,5",
       "-1,5,4,3,2,1",
       "44,,55,65,,,78",
+      "4,4,4,4,4,4",
     ];
     const trueInput = ["1,2,3,4,5,6", "5,4,9,12,6,45"];
 
     throwInput.forEach((output, idx) => {
-      let winNumbers = new Set(output.split(",").map(Number));
-      expect(app.isValidWinNumbers(winNumbers)).toEqual(false);
+      expect(app.isValidWinNumbers(output)).toEqual(false);
     });
 
     trueInput.forEach((output, idx) => {
-      let winNumbers = new Set(output.split(",").map(Number));
-      expect(app.isValidWinNumbers(winNumbers)).toEqual(true);
+      expect(app.isValidWinNumbers(output)).toEqual(true);
     });
   });
 
@@ -67,7 +63,7 @@ describe("로또 클래스 테스트", () => {
       new Lotto([2, 3, 4, 5, 6, 7]),
       new Lotto([3, 4, 5, 38, 39, 40]),
     ];
-    const winNumbers = new Set([1, 2, 3, 4, 5, 6]);
+    const winNumbers = [1, 2, 3, 4, 5, 6];
     const bonusNumber = 7;
     const result = [
       [3, 5_000, 2],
