461dcc68c8af4f9dd459e0cb029efd6d42896b29
Subin <80036437+hansb1023@users.noreply.github.com>
2022-11-15 20:41:29 +0900

    feature: 발행한 로또 수량 및 번호 출력 기능

--- src/App.js
+++ src/App.js
@@ -9,8 +9,22 @@ class App {
       if(purchaseInput % 1000 != 0) {
         throw new Error('예외 발생- 1,000원 단위로 입력해주세요.');
       }
-    };
+
+      //발행한 로또 수량 및 번호 출력
+      const count = purchase / 1000;
+      this.printer("%d개를 구매했습니다. \n", count);
+      const lotto = new Array(count); //빈 로또 배열 생성
+      for(let i = 0; i < count; i++){
+        lotto[i] = new Array(6); //배열 내에 숫자 6개를 담을 배열 생성
+        lotto[i] = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+        this.printer(lotto[i] + '\n');
+      }
+      this.printer('/n');
+
+
   
+    };
+
   }
 }
 
