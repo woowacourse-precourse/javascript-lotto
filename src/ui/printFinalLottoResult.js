af0d46de136a2331d18021b309e782165c5113e4
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-16 00:44:43 +0900

    refactor: printFinalLottoResult함수를 가독성 증가를 위해 분할함

--- src/ui/printFinalLottoResult.js
+++ src/ui/printFinalLottoResult.js
@@ -9,12 +9,24 @@ const {
 } = require('../components/lotto-data/LottoNumberData');
 
 function printFinalLottoResult() {
+  printResultStartMessage();
+  printWinningHistory();
+  printRateOfReturn();
+  Console.close();
+}
+
+function printResultStartMessage() {
   console.log(`${RESULT_START_MESSAGE}`);
+}
+
+function printWinningHistory() {
   for (let message in RESULT_MESSAGE) {
     Console.print(`${RESULT_MESSAGE[message]} - ${LottoRanking[message]}개`);
   }
+}
+
+function printRateOfReturn() {
   Console.print(`총 수익률은 ${LottoNumberData['RateOfReturn']}%입니다.`);
-  Console.close();
 }
 
 module.exports = printFinalLottoResult;
