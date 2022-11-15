8defb188fb6b8a60a2bfd053a021914507ee3ceb
ywoo12321 <ywoo12321@gmail.com>
2022-11-15 22:20:09 +0900

    fix : 예기치 못한 오류 수정 시도

--- src/Exception.js
+++ src/Exception.js
@@ -1,2 +1,26 @@
-class Exception {}
+class Exception {
+  isEmpty(money) {
+    if (money === "0" || money === "" || money === null) {
+      throw new Error("[ERROR] : 1000원 이상의 금액을 입력하세요.");
+    }
+  }
+  isMultipleOfThousand(money) {
+    if (money % 1000 !== 0) {
+      throw new Error("[ERROR] : 금액은 1000의 배수여야 합니다.");
+    }
+    return true;
+  }
+  isNumber(text) {
+    if (isNaN(text)) {
+      throw new Error("[ERROR] : 숫자만 입력 가능합니다.");
+    }
+    return true;
+  }
+  isInWinNumber(bonusNumber, winNumber) {
+    if (winNumber.includes(bonusNumber)) {
+      throw new Error("[ERROR] : 당첨 번호 리스트에 이미 존재하는 번호입니다.");
+    }
+    return true;
+  }
+}
 module.exports = Exception;
