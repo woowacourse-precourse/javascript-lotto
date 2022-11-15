857ce9d78d9271373975bcbe3cc0a5c221cc9ee7
curomame <curomame@naver.com>
2022-11-15 21:33:01 +0900

    ➕ feat - <lib/Helper> [checkRangedNumber] add
    리스트의 값이 유효한 범위 내의 값인지 확인하는 헬퍼 메서드 구현

--- src/lib/Helper.js
+++ src/lib/Helper.js
@@ -6,11 +6,22 @@ const Helper = {
       throw new Error(Message.ERROR.LOTTO_LENGTH);
     }
   },
-  
+
   checkDuplicatedNumber(numbers) {
     let newNumbers = [...new Set(numbers)];
     if (numbers.length !== newNumbers.length) {
       throw new Error(Message.ERROR.LOTTO_DUPLICATED);
+    }
+  },
+
+  checkRangedNumber(numbers) {
+    for (let number of numbers) {
+      if (!Number(number) === true) {
+        throw new Error(Message.ERROR.NUMBER_RANGE);
+      }
+      if (number > 45 || number < 0) {
+        throw new Error(Message.ERROR.NUMBER_RANGE);
+      }
     }
   },
 };
