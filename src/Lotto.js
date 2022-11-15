5eb683db63e76f3a9844a0ccf39041409c85edb8
kmin-jeong <desk535353@gmail.com>
2022-11-15 21:39:12 +0900

    feat:로또 출력화면 나머지 로직들 구현 완료& 서로비교&랭크 정렬&app.js에결과 출력 완료

--- src/Lotto.js
+++ src/Lotto.js
@@ -28,6 +28,49 @@ class Lotto {
       throw new Error(WINNINGCONDITION.DUPLICATE);
     }
   }
+
+  comparisonEachothers(lotto, bonusNumber) {
+    let cnt = 0;
+    let bonusCnt = 0;
+
+    this.#numbers.forEach((cur) => {
+      if (lotto.includes(cur)) {
+        cnt += 1;
+      }
+    });
+
+    if (lotto.includes(bonusNumber)) {
+      bonusCnt += 1;
+    }
+
+    return { cnt, bonusCnt };
+  }
+
+  sortRank(cnt, bonusCnt) {
+    if (cnt === 5 && bonusCnt === 1) {
+      return 7;
+    } else {
+      return cnt;
+    }
+  }
+
+  comparisonNumbers(publishedlottos, bonusNumber) {
+    let arr = [0, 0, 0, 0, 0, 0, 0, 0];
+
+    publishedlottos.forEach((lotto) => {
+      const { cnt, bonusCnt } = this.comparisonEachothers(lotto, bonusNumber);
+      const idx = this.sortRank(cnt, bonusCnt);
+      arr[idx] += 1;
+    });
+
+    return {
+      three: arr[3],
+      four: arr[4],
+      five: arr[5],
+      bonus: arr[7],
+      six: arr[6],
+    };
+  }
 }
 
 module.exports = Lotto;
--- src/LottoView.js
+++ src/LottoView.js
@@ -9,3 +9,28 @@ const printNumber = (purchased) => {
   MissionUtils.Console.print(`${purchased}${MESSAGES.PURCHASEDLOTTO}`);
 };
 
+const makeStrLotto = (lotto) => {
+  let str = "[";
+  lotto.forEach((cur, idx) => {
+    str += cur;
+    if (idx != lotto.length - 1) str += ", ";
+  });
+  str += "]";
+
+  return str;
+};
+
+const printLotto = (lotto) => {
+  const str = makeStrLotto(lotto);
+  MissionUtils.Console.print(str);
+};
+
+const printResult = ({ three, four, five, bonus, six }, profitRate) => {
+  MissionUtils.Console.print(
+    StatisticsMessage({ three, four, five, bonus, six })
+  );
+  MissionUtils.Console.print(ProfitMessage(profitRate));
+  MissionUtils.Console.close();
+};
+
+module.exports = { printNumber, printLotto, printResult, makeStrLotto };
