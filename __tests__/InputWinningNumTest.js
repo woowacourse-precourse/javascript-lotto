bda8c59b1b0d5014d8221f334ca9c61b0e4ebd01
jinseo <ghkdwlstj99@gmail.com>
2022-11-15 22:57:31 +0900

    test: 보너스 번호의 값이 1개가 아닌 경우에 대한 test 코드 추가

---
+++ __tests__/InputWinningNumTest.js
@@ -0,0 +1,21 @@
+const InputWinningNum = require("../src/InputWinningNum");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestions = (answers) => {
+    MissionUtils.Console.readLine = jest.fn();
+    answers.reduce((acc, input) => {
+      return acc.mockImplementationOnce((question, callback) => {
+        callback(input);
+      });
+    }, MissionUtils.Console.readLine);
+  };
+
+describe("보너스 번호 값 테스트", () => {
+    test("보너스 번호가 1개가 아닌 경우 예외 처리 ", () => {
+        mockQuestions(["1,2"]);
+        const inputWinningNum = new InputWinningNum();
+        expect(() => {
+            inputWinningNum.bonusNumber();
+            }).toThrow("[ERROR]");
+    });
+});
