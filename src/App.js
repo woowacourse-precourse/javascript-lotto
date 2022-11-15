c5766336d4c1d49ad6f14d85a667e635f66e88ee
Choejeonghun <saa00123@naver.com>
2022-11-15 20:03:33 +0900

    feat: 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
+
 class App {
-  play() {}
+  constructor() {
+    this.purchase;
+  }
+  play() {
+    let amount;
+
+    this.purchase = this.Input('구입금액을 입력해 주세요.\n');
+    if (this.purchase === undefined) return 0;
+  }
+
+  Input(text) {
+    let result;
+    MissionUtils.Console.readLine(text, (answer) => {
+      result = answer;
+    });
+    return result;
+  }
 }
 
 module.exports = App;
