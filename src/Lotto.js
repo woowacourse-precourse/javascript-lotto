2f7fef87dfd1546d0953c217e4d519ec2f6193a7
cyberjam <jamsince1994@naver.com>
2022-11-15 11:24:41 +0900

    feat: 당첨 번호 입력 기능에 숫자 중복 유효성 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,5 @@
 /* eslint-disable class-methods-use-this */
+// const TicketBox = require('./TicketBox');
 
 class Lotto {
   #numbers;
@@ -12,6 +13,13 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 숫자를 중복없이 입력하여 주십시오.');
+    }
+  }
+
+  get numbers() {
+    return this.#numbers;
   }
 
   // TODO: 추가 기능 구현
