9a86d2ed7f9d89ca71edb633bfd9cd956e78b5dc
haesa <xdhaesa@gmail.com>
2022-11-15 13:58:37 +0900

    refactor(Lotto.js): 에러 메세지 상수 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const { LOTTO_ERROR } = require('./constants/constants');
+
 class Lotto {
   #numbers;
 
@@ -8,11 +10,11 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+      throw new Error(LOTTO_ERROR.LENGTH);
     }
 
     if (numbers.some((number) => /[^0-9]/.test(number))) {
-      throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+      throw new Error(LOTTO_ERROR.NUMBER);
     }
 
     if (
@@ -20,11 +22,11 @@ class Lotto {
         (number) => numbers.indexOf(number) !== numbers.lastIndexOf(number)
       )
     ) {
-      throw new Error('[ERROR] 로또 번호는 중복을 허용하지 않습니다.');
+      throw new Error(LOTTO_ERROR.DUPLICATION);
     }
 
     if (numbers.some((number) => !(number >= 1 && number <= 45))) {
-      throw new Error('[ERROR] 로또 번호는 1과 45 사이의 숫자여야 합니다.');
+      throw new Error(LOTTO_ERROR.DOMAIN);
     }
   }
 }
