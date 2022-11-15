4370ec4c13ed245f6cefa8dcdd9f1732fec58972
HeoJiye <heoze1031@gmail.com>
2022-11-15 13:37:36 +0900

    test: 예외 처리 case 묶음

--- __tests__/LottoDrawTest.js
+++ __tests__/LottoDrawTest.js
@@ -1,5 +1,6 @@
-const LottoDraw = require("../src/LottoDraw");
 const MissionUtils = require("@woowacourse/mission-utils");
+const LottoDraw = require("../src/LottoDraw");
+const Lotto = require("../src/Lotto");
 
 const getLogSpy = () => {
     const logSpy = jest.spyOn(MissionUtils.Console, "print");
@@ -7,39 +8,36 @@ const getLogSpy = () => {
     return logSpy;
   };
 
-describe("당첨 번호를 입력받는 기능", () => {
-    test("[예외처리] 입력한 값의 갯수가 6개가 아닌 경우", () => {
+describe("예외 테스트", () => {
+    test("[예외처리] 당첨 번호로 입력한 값의 갯수가 6개가 아닌 경우", () => {
         const draw = new LottoDraw();
-        expect(() => draw.setWinningNums(['1', '2', '4'])).toThrow("[ERROR]");
+        expect(() => draw.setWinningNums(['1', '2', '4'].map(Number))).toThrow("[ERROR]");
     });
-    test("[예외처리] 입력한 값 중 숫자가 아닌 값이 포함된 경우", () => {
+    test("[예외처리] 당첨 번호로 입력한 값 중 숫자가 아닌 값이 포함된 경우", () => {
         const draw = new LottoDraw();
-        expect(() => draw.setWinningNums(['1', '2', '4', 'd', '5', '6'])).toThrow("[ERROR]");
+        expect(() => draw.setWinningNums(['1', '2', '4', 'd', '5', '6'].map(Number))).toThrow("[ERROR]");
     });
-    test("[예외처리] 입력한 값 중 중복되는 숫자가 있을 경우", () => {
+    test("[예외처리] 당첨 번호로 입력한 값 중 중복되는 숫자가 있을 경우", () => {
         const draw = new LottoDraw();
-        expect(() => draw.setWinningNums(['1', '2', '4', '4', '5', '6'])).toThrow("[ERROR]");
+        expect(() => draw.setWinningNums(['1', '2', '4', '4', '5', '6'].map(Number))).toThrow("[ERROR]");
     });
-    test("[예외처리] 입력한 값 중 범위에서 벗어난 숫자가 있을 경우", () => {
+    test("[예외처리] 당첨 번호로 입력한 값 중 범위에서 벗어난 숫자가 있을 경우", () => {
         const draw = new LottoDraw();
-        expect(() => draw.setWinningNums(['1', '2', '4', '48', '5', '6'])).toThrow("[ERROR]");
+        expect(() => draw.setWinningNums(['1', '2', '4', '48', '5', '6'].map(Number))).toThrow("[ERROR]");
     });
-});
-
-describe("보너스 번호를 입력받는 기능", () => {
-    test("[예외처리] 입력한 값 중 숫자가 아닐 경우", () => {
+    test("[예외처리] 보너스 번호로 입력한 값이 숫자가 아닐 경우", () => {
         const draw = new LottoDraw();
-        draw.setWinningNums(['1', '2', '4', '7', '5', '6']);
-        expect(() => draw.setBonusNum('dd')).toThrow("[ERROR]");
+        draw.setWinningNums(['1', '2', '4', '7', '5', '6'].map(Number));
+        expect(() => draw.setBonusNum(Number('dd'))).toThrow("[ERROR]");
     });
-    test("[예외처리] 당첨 번호 값 중 중복되는 숫자가 있을 경우", () => {
+    test("[예외처리] 보너스 번호로 입력한 값이 당첨 번호 값 중 중복되는 숫자가 있을 경우", () => {
         const draw = new LottoDraw();
-        draw.setWinningNums(['1', '2', '4', '7', '5', '6']);
-        expect(() => draw.setBonusNum(['1', '2', '4', '4', '5', '6'])).toThrow("[ERROR]");
+        draw.setWinningNums(['1', '2', '4', '7', '5', '6'].map(Number));
+        expect(() => draw.setBonusNum(Number('2'))).toThrow("[ERROR]");
     });
-    test("[예외처리] 입력한 숫자가 범위에서 벗어난 경우", () => {
+    test("[예외처리] 보너스 번호로 입력한 값이 범위에서 벗어난 경우", () => {
         const draw = new LottoDraw();
-        draw.setWinningNums(['1', '2', '4', '7', '5', '6']);
-        expect(() => draw.setBonusNum(['1', '2', '4', '48', '5', '6'])).toThrow("[ERROR]");
+        draw.setWinningNums(['1', '2', '4', '7', '5', '6'].map(Number));
+        expect(() => draw.setBonusNum(Number('46'))).toThrow("[ERROR]");
     });
 });
