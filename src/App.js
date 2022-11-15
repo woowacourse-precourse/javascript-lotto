552df61d04583ba3c81a2fc6efde37ec11d116d9
OrangeChick <retww@naver.com>
2022-11-15 20:24:25 +0900

    feat: Implement of a function that outputs the lottery quantity
    - 구입 금액만큼 발행할 로또 수량을 출력하는 기능을 구현하였습니다.
    - 기능 구현 목록을 조금 더 상세하게 수정하였습니다.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,15 @@ class App {
   printStartGame() {
     MissionUtils.Console.readLine("구입금액을 입력해주세요: ", (amount) => {
       this.userAmount = amount;
+      this.howManyBuyLotto(amount);
     });
+  }
+
+  howManyBuyLotto(amount) {
+    const count = parseInt(amount / 1000);
+    MissionUtils.Console.print(`${Number(count)}개를 구매했습니다.`);
+
+    this.printLottoList(count);
   }
 }
 
