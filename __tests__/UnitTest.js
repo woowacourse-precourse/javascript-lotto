ea2bc573b2ab2199c542675baa4c4811aa32ee1a
jun <ggprgrkjh@naver.com>
2022-11-15 19:02:56 +0900

    test: winningStats 결과 함수 테스트

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -66,11 +66,43 @@ describe("App 클래스 메소드 테스트 ", () => {
     expect(app.winningNumber).toEqual(testarray);
   });
 
-  test("로또 번호 비교 함수 테스트", () => {
+  test("일치하는 로또 번호 filter 함수 테스트", () => {
     const app = new App();
     app.lottoArray = [[1, 2, 3, 4, 5, 6]];
     app.winningNumber = [1, 2, 3];
     app.compareLottoNumber();
     expect(app.collectNumber).toHaveLength(3);
+  });
+
+  test("로또 번호 일치 갯수 증가 함수 테스트", () => {
+    const app = new App();
+    app.collectNumber = [1, 2, 3];
+    app.bonusNumber = 4;
+    const testarray = [1, 2, 3];
+    app.collectLottoNumber(testarray);
+    expect(app.matchNumber[0]).toBe(1);
+    expect(app.matchNumber[1]).toBe(0);
+    expect(app.matchNumber[2]).toBe(0);
+    expect(app.matchNumber[3]).toBe(0);
+    expect(app.matchNumber[4]).toBe(0);
+  });
+
+  test("winningStats 결과 함수 테스트", () => {
+    const app = new App();
+    const logs = [
+      "당첨 통계",
+      "3개 일치 (5,000원) - 0개",
+      "4개 일치 (50,000원) - 0개",
+      "5개 일치 (1,500,000원) - 0개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+      "6개 일치 (2,000,000,000원) - 3개",
+    ];
+    const logSpy = getLogSpy();
+    app.matchNumber[4] = 3;
+    app.winningStatsResult();
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
   });
 });
