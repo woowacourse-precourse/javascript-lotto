5d525b8d7abf96f8b805c39565b4f1963a8a2be3
dhkham <khamdong@gmail.com>
2022-11-15 12:42:24 +0900

    feat(Payment.js): add method that checks if payment is in integer

--- src/Game.js
+++ src/Game.js
@@ -4,8 +4,7 @@ const Bonus = require("./Bonus");
 
 class Game {
   constructor() {
-    // this.computer = computer;
-    // this.user = user;
+    
   }
 
   startLottery() {
@@ -141,86 +140,6 @@ class Game {
     MissionUtils.Console.print(`총 수익률은 ${earningRate}%입니다.`);
     MissionUtils.Console.close();
   }
-//   match(computerNum) {
-//     Console.readLine(MESSAGE.INPUT, userInput => {
-//       const isUserInputValid = this.user.validateInput(userInput);
-
-//       if (isUserInputValid === false) {
-//         return this.throwError();
-//       }
-
-//       const { ballCount, strikeCount } = this.getGameResult(computerNum, userInput);
-//       this.renderGameMessage(ballCount, strikeCount);
-
-//       if (strikeCount !== NUMBER_LIMIT) {
-//         return this.match(computerNum);
-//       }
-
-//       this.askUserToRestart();
-//     });
-//   }
-
-//   getGameResult(computerNum, userInput) {
-//     const splittedComputerNum = [...computerNum];
-//     const splittedUserInput = [...userInput];
-
-//     const ballCount = splittedComputerNum.reduce((ballCount, currNum, index) => {
-//       const currNumIndex = splittedUserInput.indexOf(currNum);
-//       const isBall = currNumIndex !== -1 && currNumIndex !== index;
-
-//       if (isBall) ballCount += 1;
-//       return ballCount;
-//     }, 0);
-
-//     const strikeCount = splittedComputerNum.reduce((strikeCount, currNum, index) => {
-//       const isStrike = currNum === splittedUserInput[index];
-
-//       if (isStrike) strikeCount += 1;
-//       return strikeCount;
-//     }, 0);
-
-//     return { ballCount, strikeCount };
-//   }
-
-//   renderGameMessage(ballCount, strikeCount) {
-//     if (ballCount === 0 && strikeCount === 0) {
-//       return Console.print(RESULT.NOTHING);
-//     }
-
-//     const ballMessage = ballCount !== 0 ? `${ballCount}${RESULT.BALL}` : '';
-//     const strikeMessage = strikeCount !== 0 ? `${strikeCount}${RESULT.STRIKE}` : '';
-//     const gameMessage = `${ballMessage} ${strikeMessage}`;
-
-//     Console.print(gameMessage);
-//   }
-
-//   askUserToRestart() {
-//     Console.print(MESSAGE.SUCCESS);
-
-//     Console.readLine(MESSAGE.END, userInput => {
-//       if (userInput === OPTION.RESTART) {
-//         return this.replay();
-//       }
-
-//       if (userInput === OPTION.EXIT) {
-//         return this.exit();
-//       }
-
-//       this.throwError();
-//     });
-//   }
-
-//   replay() {
-//     this.start();
-//   }
-
-//   exit() {
-//     Console.close();
-//   }
-
-//   throwError() {
-//     throw new Error(MESSAGE.ERROR);
-//   }
 }
 
 module.exports = Game;
---
+++ src/Payment.js
@@ -0,0 +1,20 @@
+class Payment {
+  #numbers;
+
+  constructor(numbers) {
+    this.#numbers = numbers;
+    this.validatePaymentInteger(numbers);
+    this.validatePaymentMultipleOfThousand(numbers);
+    this.validatePaymentNotNegative(numbers);
+  }
+
+  validatePaymentInteger(numbers) {
+    if (!Number.isInteger(paymentNum)) {
+      throw new Error("[ERROR] 구매금액은 정수여야 합니다.");
+    }
+  }
+
+  // TODO: 추가 기능 구현
+}
+
+module.exports = Payment;
