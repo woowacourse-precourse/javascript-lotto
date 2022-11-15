210240e84390d94a7339688a107ab66489583f97
leeseooo <leeseoooo@naver.com>
2022-11-15 13:20:43 +0900

    feat: 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  constructor() {
+    this.purchase = 0;
+  }
+  play() {
+    this.getUserInput();
+  }
+  getUserInput() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (input) => {
+      this.purchase = input;
+      
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
