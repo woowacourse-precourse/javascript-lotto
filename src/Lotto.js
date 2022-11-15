92021cfa630c8357f04520930d8e3c9f1a96cdfb
dmsthf2000 <dsmthf200099@gmail.com>
2022-11-15 20:29:10 +0900

    all code commit

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class Lotto {
   #numbers;
 
@@ -10,9 +12,32 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    function hasDuplicates(arr) {
+      return new Set(arr).size !== arr.length;
+    }
+    if (hasDuplicates(numbers)) throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+
   }
 
-  // TODO: 추가 기능 구현
+  compare(lottos, bonus) {
+    let result = [];
+    for (let lotto of lottos) {
+      // let correct = this.#numbers.filter(x => lotto.includes(x));
+      let correct = this.#numbers.filter(x => lotto.includes(x));
+      Console.print(this.#numbers);
+      if (correct.length === 5 && this.isBonusCorrect(lotto, bonus)) {
+        result.push(-1);
+        continue;
+      }
+      if (correct.length >= 3) result.push(correct.length);
+    }
+    Console.print(result)
+    return result;
+  }
+
+  isBonusCorrect(lotto, bonus) {
+    return lotto.includes(bonus);
+  }
 }
 
 module.exports = Lotto;
