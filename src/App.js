6501cbbd44d4cccdb05fcff25a473ce0fa032244
spsp8426 <spsp8426@naver.com>
2022-11-15 10:23:58 +0900

    feat : 당첨 통계 출력 구현

--- src/App.js
+++ src/App.js
@@ -79,6 +79,22 @@ class App {
   }
   return rankSystem;
  }
+ countRank(rankSystem, winScore) {
+  winScore.forEach((number) =>{
+    rankSystem[number]++;
+  });
+ }
+ result() {
+  const rankObject = this.Ranksystem();
+  const winScore = this.lotto.map((lotto) => lotto.rank(this.winNum, this.bonusNum));
+  this.countRank(rankObject,winScore);
+  Console.print("\n당첨 통계\n---");
+  Console.print(`3개 일치 (5,000원) - ${rankObject[5]}개`);
+  Console.print(`4개 일치 (50,000원) - ${rankObject[4]}개`);
+  Console.print(`5개 일치 (1,500,000원) - ${rankObject[3]}개`);
+  Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${rankObject[2]}개`);
+  Console.print(`6개 일치 (2,000,000,000원) - ${rankObject[1]}개`);
+ }
   play() {
     this.setMoney();
   }
