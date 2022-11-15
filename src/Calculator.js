38507a264ee87bccca4117107847bafccdceb083
dudgp128 <20181048@sungshin.ac.kr>
2022-11-15 20:46:49 +0900

    STYLE : 주석작성

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,9 @@
 class Calculator {
   conductRevenue(rewards, payMoney) {
+    if (!rewards.some((reward) => reward[2] != 0)) {
+      // 당첨 개수가 없을때, 계산 전에 0을 반환한다.
+      return 0;
+    }
     const sum = rewards.reduce((total, arg) => {
       if (arg[2] != 0) {
         return (total += arg[1] * arg[2]);
