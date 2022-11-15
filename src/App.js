e8fa70addb57ad6dd9a25e495841516e2ad70973
OrangeChick <retww@naver.com>
2022-11-15 20:42:41 +0900

    feat: Implement function to output the winning details
    - 당첨 내역을 출력하는 기능을 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -89,6 +89,26 @@ class App {
       }
     }
     MissionUtils.Console.print(this.matchCount);
+
+    this.printResult();
+  }
+
+  printResult() {
+    MissionUtils.Console.print(
+      `3개 일치 (5,000원) - ${this.matchCount.fifth}개`
+    );
+    MissionUtils.Console.print(
+      `4개 일치 (50,000원) - ${this.matchCount.fourth}개`
+    );
+    MissionUtils.Console.print(
+      `5개 일치 (1,500,000원) - ${this.matchCount.third}개`
+    );
+    MissionUtils.Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.matchCount.second}개`
+    );
+    MissionUtils.Console.print(
+      `6개 일치 (2,000,000,000원) - ${this.matchCount.first}개`
+    );
   }
 }
 
