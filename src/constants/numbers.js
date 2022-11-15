e1b98e571a8e34939179138d637f69ecc193136f
thewronghand <penfreak77@gmail.com>
2022-11-15 15:33:15 +0900

    feat: ERROR 상수 객체 생성
    모든 에러 메세지를 저장하는 상수 객체를 생성한다.

--- src/constants/numbers.js
+++ src/constants/numbers.js
@@ -13,7 +13,32 @@ const REWARD = {
   THREE_MATCH_REWARD: 5000
 };
 
+const ERROR = {
+  MONEY_INPUT_NON_NUMBER:
+    '[ERROR] 금액 입력 시 숫자 이외에는 입력할 수 없습니다.',
+  MONEY_INPUT_NON_INTEGER:
+    '[ERROR] 금액 입력 시 소수점 이하는 허용되지 않습니다.',
+  MONEY_INPUT_NEGATIVE: '[ERROR] 금액 입력 시 음수를 입력할 수 없습니다.',
+  MONEY_INPUT_NON_DIVISIBLE:
+    '[ERROR] 금액은 1,000 원 단위로만 입력 가능합니다.',
+  LOTTO_NUMBERS_LENGTH_INVALID:
+    '[ERROR] 로또 번호는 6자리 숫자로 구성되어야 합니다.',
+  LOTTO_NUMBERS_HAVE_NON_NUMBER:
+    '[ERROR] 로또 번호는 숫자로만 구성되어야 합니다.',
+  LOTTO_NUMBERS_HAVE_INVALID_SIZE_NUMBER:
+    '[ERROR] 로또 번호는 1 이상 45 이하의 자연수여야 합니다.',
+  LOTTO_NUMBERS_HAVE_DUPLICATE:
+    '[ERROR] 로또 번호에 중복된 숫자가 포함되어 있습니다.',
+  BONUS_NUMBER_INVALID_INPUT:
+    '[ERROR] 보너스 번호는 하나의 숫자만 입력 가능합니다.',
+  BONUS_NUMBER_SIZE_INVALID:
+    '[ERROR] 보너스 번호는 1 이상 45 이하의 자연수여야 합니다.',
+  BONUS_NUMBER_OVERLAPPED:
+    '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.'
+};
+
 module.exports = {
   VALUE,
-  REWARD
+  REWARD,
+  ERROR
 };
