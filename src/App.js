9e9b50dbf0287c8df2e1201fb2edd0128252d561
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-11-15 21:27:41 +0900

    feat(App): add 당첨 결과 및 수익율을 출력하는 기능

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ const { Console } = MissionUtils;
 const VendingMachine = require("./lotto/domain/VendingMachine");
 const NumberGenerator = require("./lotto/domain/NumberGenerator");
 const Lotto = require("./Lotto");
+const LottoChecker = require("./lotto/domain/LottoChecker");
 
 class App {
   constructor() {
     this.vendingMachine = new VendingMachine();
     this.numberGenerator = new NumberGenerator();
+    this.lottoChecker = new LottoChecker();
   }
-
   play() {
     Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
       this.vendingMachine.setMoney(money);
@@ -47,8 +48,38 @@ class App {
 
   inputBonusNumber(totalLotto, winningLotto) {
     Console.readLine("\n보너스 번호를 입력해 주세요.\n", (bonus) => {
-      return;
+      bonus = Number(bonus);
+
+      return this.printLottoResult(totalLotto, winningLotto, bonus);
     });
+  }
+
+  printLottoResult(totalLotto, winningLotto, bonus) {
+    const lottoResult = this.lottoChecker.compareLotto(
+      totalLotto,
+      winningLotto,
+      bonus
+    );
+
+    const money = this.vendingMachine.money;
+    const lottoProfitRate = this.lottoChecker.CalculateProfit(
+      lottoResult,
+      money
+    );
+
+    Console.print(lottoResult);
+    Console.print(money);
+    Console.print(lottoProfitRate);
+    Console.print("\n당첨 통계\n---");
+    Console.print(`3개 일치 (5,000원) - ${lottoResult[0]}개`);
+    Console.print(`4개 일치 (50,000원) - ${lottoResult[1]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${lottoResult[3]}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoResult[2]}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${lottoResult[4]}개`);
+    Console.print(`총 수익률은 ${lottoProfitRate}%입니다.`);
+    Console.close();
   }
 }
 
