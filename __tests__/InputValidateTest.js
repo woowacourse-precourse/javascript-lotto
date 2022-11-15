ff409ba5235c3639d68cbfcf2396e13027ffef1c
rovxxmjxx <miju3545@gmail.com>
2022-11-15 12:50:55 +0900

    test: 유효성 - 숫자 범위 테스트 추가
    `isOutOfRangeAndThrowError()`에 대한 테스트 코드 추가

--- __tests__/InputValidateTest.js
+++ __tests__/InputValidateTest.js
@@ -1,10 +1,16 @@
 const Display = require('../src/Display');
-const { isDuplicatedAndThrowError } = require('../src/utils/inputValidate');
+const { isDuplicatedAndThrowError, isOutOfRangeAndThrowError } = require('../src/utils/inputValidate');
 
 describe('사용자 입력값 에러 테스트', () => {
   test('중복 여부를 확인한다.', () => {
     expect(() => {
       isDuplicatedAndThrowError([1, 2, 3, 4, 5, 5, 7]);
     }).toThrow(Display.error('DUPLICATED'));
+  });
+
+  test('숫자 범위는 1-45까지이며 유효하지 않으면 예외가 발생한다', () => {
+    expect(() => {
+      isOutOfRangeAndThrowError([45, 1, 2, 3, 4, 56]);
+    }).toThrow(Display.error('OUT_OF_RANGE'));
   });
 });
