758861207d16800c8d85f657180001495d08eaac
Deemou <4106243@naver.com>
2022-11-15 18:05:17 +0900

    feat(Game): 구매 금액 입력 받는 기능 추가

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,14 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { MESSAGE } = require('./constant/Lotto');
 
 class Game {
+  initGame() {
+    this.askNumber(MESSAGE.INPUT_MONEY, this.proceedStepOne.bind(this));
+  }
   askNumber(message, callback) {
     Console.readLine(message, callback);
   }
+  proceedStepOne(input) {}
 }
 
 module.exports = Game;
