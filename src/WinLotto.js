e72305fb77338d8be6aff6ac3da52a78855c95f1
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-15 13:11:17 +0900

    fix(validateWin) : 당첨 및 보너스 번호 버그 수정
    - 기존의 super로 상위 클래스의 예외처리를 거치는 코드는
    클래스의 동작 원리의 이해의 부족으로 버그가 발생
    - 이를 해결하기위해, WinLotto에서는 별개의 메소드를
    생성하고 예외 처리 진행

--- src/WinLotto.js
+++ src/WinLotto.js
@@ -4,13 +4,13 @@ const { ERROR } = require('./modules/Constant');
 class WinLotto extends Lotto {
   constructor(numbers, bonus) {
     super(numbers);
-    this.validate(numbers, bonus);
+    this.validateWin(numbers, bonus);
     this.bonus = bonus;
   }
-  validate(numbers, bonus) {
-    super.validate(numbers);
+
+  validateWin(numbers, bonus) {
     const numberReg = /^[0-9]*$/;
-    if (!numberReg.test(bonus) || number < 1 || number > 45) {
+    if (!numberReg.test(bonus) || bonus < 1 || bonus > 45) {
       throw new Error(`${ERROR.COMMON} ${ERROR.MUST_IN_RANGE}`);
     }
     if (numbers.includes(bonus)) {
