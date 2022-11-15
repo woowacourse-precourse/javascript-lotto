a00a4aaec04adc52508415ff5bd0c2d1d78e8a06
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-15 13:21:39 +0900

    feat(ValidationTest): 당첨 번호 입력값이 6개인지 테스트하는 함수 구현

---
+++ __tests__/ValidationTest.js
@@ -0,0 +1,42 @@
+const Validation = require("../src/Validation");
+
+describe("Validation 클래스 테스트", () => {
+  test("당첨 번호 입력값이 6개인지 검사", () => {
+    const input = [1, 2, 3, 4, 5];
+
+    expect(Validation.isThatSix(input)).toBe(false);
+  });
+
+//   test("당첨 번호 입력값이 1 ~ 45 사이인지 검사", () => {
+//     const input = [1, 2, 3, 4, 5, 70];
+
+//     expect(Validation.numberNet(input)).toBe(true);
+//   });
+
+//   test("당첨 번호 입력값이 6개인지 검사", () => {
+//     const input = [1, 2, 3, 4, 5];
+
+//     expect(Validation.itThatNumber(input)).toBe(false);
+//   });
+
+//   test("배열 정렬 테스트", () => {
+//     const input = [5, 4, 3, 2, 1, 7];
+//     const result = lotto.sortLottoNumber(input);
+
+//     expect(result).toStrictEqual([1, 2, 3, 4, 5, 7]);
+//   });
+
+//   test("돈이 아닐 경우 에러 발생 테스트", () => {
+//     const input = "money";
+
+//     expect(() => lotto.NumberPackage(input)).toThrow();
+//     expect(() => lotto.NumberPackage(input)).toThrow('[ERROR] 돈이 아닙니다.');
+//   });
+
+//   test("천원 단위로 입력되지 않으면 에러 발생", () => {
+//     const input = 2999;
+
+//     expect(() => lotto.rightAmount(input)).toThrow();
+//     expect(() => lotto.rightAmount(input)).toThrow('[ERROR] 천원 단위로 입력해주세요.');
+//   });
+});
