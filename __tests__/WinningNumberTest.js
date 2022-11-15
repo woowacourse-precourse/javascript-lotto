cf0bb07657d7808a35f7cfdfa474d964c837893b
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-15 20:21:07 +0900

    test: 당첨 번호 예외사항 테스트 코드 에러메시지 추가

--- __tests__/WinningNumberTest.js
+++ __tests__/WinningNumberTest.js
@@ -1,4 +1,5 @@
 const LottoController = require("../src/LottoController");
+const { ERROR_LOTTO } = require("../src/constants/messages.js");
 
 describe("로또 당첨 번호 입력 유효성 테스트", () => {
   it("당첨 번호가 1~45 이외의 숫자로 입력될 경우 예외처리", () => {
@@ -6,7 +7,7 @@ describe("로또 당첨 번호 입력 유효성 테스트", () => {
     const inputNumber = "1,2,3,4,5,50";
     const result = () => lottoController.setWinNumbers(inputNumber);
 
-    expect(result).toThrow();
+    expect(result).toThrow(ERROR_LOTTO.RANGE);
   });
 
   it("당첨 번호가 6개 숫자가 아닌 경우 예외처리", () => {
@@ -14,7 +15,7 @@ describe("로또 당첨 번호 입력 유효성 테스트", () => {
     const inputNumber = "1,2,3,4,5,6,7,8";
     const result = () => lottoController.setWinNumbers(inputNumber);
 
-    expect(result).toThrow();
+    expect(result).toThrow(ERROR_LOTTO.LENGTH);
   });
 
   it("당첨 번호가 중복되는 숫자일 경우 예외처리", () => {
@@ -22,7 +23,7 @@ describe("로또 당첨 번호 입력 유효성 테스트", () => {
     const inputNumber = "20,20,21,22,23,24";
     const result = () => lottoController.setWinNumbers(inputNumber);
 
-    expect(result).toThrow();
+    expect(result).toThrow(ERROR_LOTTO.DUPLICATED);
   });
 
   it("당첨 번호가 숫자가 아닌 경우 예외처리", () => {
@@ -30,7 +31,7 @@ describe("로또 당첨 번호 입력 유효성 테스트", () => {
     const inputNumber = "a,b,c,d,e,f";
     const result = () => lottoController.setWinNumbers(inputNumber);
 
-    expect(result).toThrow();
+    expect(result).toThrow(ERROR_LOTTO.TYPE);
   });
 
   it("당첨 번호에 공백이 입력된 경우 예외처리", () => {
@@ -38,6 +39,6 @@ describe("로또 당첨 번호 입력 유효성 테스트", () => {
     const inputNumber = "";
     const result = () => lottoController.setWinNumbers(inputNumber);
 
-    expect(result).toThrow();
+    expect(result).toThrow(ERROR_LOTTO.LENGTH);
   });
 });
