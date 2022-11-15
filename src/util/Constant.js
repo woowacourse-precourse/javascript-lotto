8efcc47119527320f63b104f15d2f03cae0ad724
Hungeun2 <bepoohun@gmail.com>
2022-11-15 14:03:31 +0900

    Feat: Result 관련 Constant 추가

--- src/util/Constant.js
+++ src/util/Constant.js
@@ -24,6 +24,42 @@ const LOTTO_ERROR = {
   NUM: "[ERROR] 로또 번호는 숫자만 입력해 주세요.",
 };
 
+const RESULT_COUNT = {
+  THREE: 3,
+  THREE_PRIZE: 5000,
+  FOUR: 4,
+  FOUR_PRIZE: 50000,
+  FIVE: 5,
+  FIVE_PRIZE: 1500000,
+  FIVE_BONUS: 6,
+  FIVE_BONUS_PRIZE: 30000000,
+  SIX: 6,
+  SIX_PRIZE: 2000000000,
+};
+
+const RESULT_CORRECT = {
+  STAT: "당첨 통계\n---",
+  THREE: function (count) {
+    return "3개 일치 (5,000원) - " + count + "개";
+  },
+  FOUR: function (count) {
+    return "4개 일치 (50,000원) - " + count + "개";
+  },
+  FIVE: function (count) {
+    return "5개 일치 (1,500,000원) - " + count + "개";
+  },
+  FIVE_BONUS: function (count) {
+    return "5개 일치, 보너스 볼 일치 (30,000,000원) - " + count + "개";
+  },
+  SIX: function (count) {
+    return "6개 일치 (2,000,000,000원) - " + count + "개";
+  },
+
+  YIELD: function (percent) {
+    return "총 수익률은 " + percent + "%입니다.";
+  },
+};
+
 module.exports = {
   LOTTO_PRICE,
   LOTTO_COUNT,
@@ -32,4 +68,6 @@ module.exports = {
   LOTTO_MESSAGE,
   BUDGET_ERROR_MESSAGE,
   LOTTO_ERROR,
+  RESULT_COUNT,
+  RESULT_CORRECT,
 };
