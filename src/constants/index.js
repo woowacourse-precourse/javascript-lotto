c0c4426f7498a52bd1715ca199f85705d9875264
sunny <huiseon37@gmail.com>
2022-11-15 14:39:15 +0900

    chore: 문자열 상수 수정

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
 const CONSOLE_MESSAGE = {
   INPUT_PURCHASE_MONEY: "구입금액을 입력해 주세요.\n",
-  INPUT_WINNING_LOTTO: "\n당첨 번호를 입력해 주세요.\n",
+  INPUT_WINNING_LOTTO: "당첨 번호를 입력해 주세요.\n",
   SHOW_PURCHASED_MONEY: "개를 구매했습니다.",
-  INPUT_BONUS_LOTTO: "\n보너스 번호를 입력해 주세요.\n",
+  INPUT_BONUS_LOTTO: "보너스 번호를 입력해 주세요.\n",
 }
 
 const RANK_STRING = {
@@ -15,7 +15,18 @@ const RANK_STRING = {
 
 const PRINT_RESULT = {
   TITLE: "당첨 통계\n---\n",
-  PROFIT_RATE: "총 수익률은",
 }
 
-module.exports = {CONSOLE_MESSAGE, RANK_STRING, PRINT_RESULT}
+const WINNING_MONEY = {
+  RANK_5: 5000,
+  RANK_4: 50000,
+  RANK_3: 1500000,
+  RANK_2: 30000000,
+  RANK_1: 2000000000,
+}
+
+function profitRateString(profitRate) {
+  return `총 수익률은 ${profitRate}%입니다.`
+}
+
+module.exports = {CONSOLE_MESSAGE, RANK_STRING, PRINT_RESULT, WINNING_MONEY, profitRateString}
