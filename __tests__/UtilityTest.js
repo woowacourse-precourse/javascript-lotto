3093b9ad77aa1a1c35b84fc441426be66d966bb9
gilpop8663 <wolfye@naver.com>
2022-11-15 13:34:41 +0900

    test: 오름차순으로 정렬하는 함수가 올바르게 작동하는지 체크하는 테스트 진행

--- __tests__/UtilityTest.js
+++ __tests__/UtilityTest.js
@@ -2,6 +2,7 @@ const {
   splitComma,
   matchArrayNumbers,
   createRandomLotto,
+  sortAscent,
 } = require('../src/libs/Utils');
 
 describe('쉼표(,) 기준으로 번호를 구분하는 함수 테스트 ', () => {
@@ -36,5 +37,13 @@ describe('랜덤 배열을 반환하는 함수 테스트 ', () => {
       expect(randomArray[i]).toBeGreaterThanOrEqual(1);
       expect(randomArray[i]).toBeLessThanOrEqual(45);
     }
+  });
+
+  describe('오름차순으로 정렬하는 함수 테스트', () => {
+    test('기능 테스트', () => {
+      const array = [15, 27, 45, 1, 5, 3];
+      const result = [1, 3, 5, 15, 27, 45];
+      expect(sortAscent(array)).toEqual(result);
+    });
   });
 });
