1029606ff24527cceb9da2c84bfece52e5010471
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-15 19:56:39 +0900

    feat: 로또 길이 확인 기능

--- src/Validator.js
+++ src/Validator.js
@@ -1,6 +1,12 @@
+const config = require("./util/config");
+
 class Validator {
   static isRange(start, end, target) {
     return start <= target && target <= end;
+  }
+
+  static isLottoLength(target) {
+    return target === config.LOTTO_MAX_LENGTH;
   }
 }
 
