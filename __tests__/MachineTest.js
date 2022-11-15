dad34dbfff28686a1155b45fa03b51aa41e37e6a
rovxxmjxx <miju3545@gmail.com>
2022-11-15 13:41:37 +0900

    test: 로또 구매 수량 테스트

--- __tests__/MachineTest.js
+++ __tests__/MachineTest.js
@@ -8,4 +8,10 @@ describe('로또 머신 테스트', () => {
       new Machine(3001);
     }).toThrow(Display.error('UNACCEPTABLE_PAYMENT'));
   });
+
+  test('로또 구매 수량만큼 로또 발행', () => {
+    const machine = new Machine(2000);
+    machine.work();
+    expect(Machine.user.purchasedLotto.length).toEqual(2);
+  });
 });
