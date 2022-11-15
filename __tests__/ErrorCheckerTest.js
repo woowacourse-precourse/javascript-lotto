a84f3e40204a0e8f3e60c334d342db9b36d08388
yeongseokim <1903020@ewhain.net>
2022-11-15 09:53:02 +0900

    test(ErrorChekcer): 중복 검사, 6개 요소 검사 테스트

---
+++ __tests__/ErrorCheckerTest.js
@@ -0,0 +1,21 @@
+const ErrorChecker = require("../src/ErrorChecker");
+
+describe("에러 체커 클래스 테스트", () => {
+    test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+        expect(() => {
+            ErrorChecker.checkSixElementArray([1, 2, 3, 4, 5, 6, 7]);
+        }).toThrow("[ERROR] 로또 번호는 6개여야 합니다.");
+    });
+
+    test("로또 번호의 개수가 6개가 미만이면 예외가 발생한다.", () => {
+        expect(() => {
+            ErrorChecker.checkSixElementArray([1, 2, 3, 4, 5]);
+        }).toThrow("[ERROR] 로또 번호는 6개여야 합니다.");
+    });
+
+    test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+        expect(() => {
+            ErrorChecker.checkDuplicatedElement([1, 2, 3, 4, 5, 5]);
+        }).toThrow("[ERROR] 로또 번호는 중복되지 않는 6자리 숫자여야 합니다.");
+    });
+})
