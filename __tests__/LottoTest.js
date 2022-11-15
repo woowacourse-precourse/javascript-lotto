81c665891a42477fd75c0086dcf8391bf24ad09b
leeharyung <lhr4884@naver.com>
2022-11-15 21:27:19 +0900

    test: 로또 번호와 입력받은 번호의 같은 숫자 개수 반환하는 메소드 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,20 @@
-const Lotto = require("../src/Lotto");
+const Lotto = require('../src/Lotto');
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('checkSameNumber 메소드는 로또 번호와 입력받은 번호를 비교하여 같은 숫자의 개수를 반환한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.checkSameNumber([1, 2, 3, 4, 5, 7])).toBe(5);
+  });
 });
