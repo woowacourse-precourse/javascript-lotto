e0085c25a79837c9ab315ee3d2315c35e76c710b
JaeyeonHong <39540473+HongJaeyeon@users.noreply.github.com>
2022-11-15 16:36:05 +0900

    refactor: Message 클래스 추가하여 message 생성 기능 분리

--- src/Manager.js
+++ src/Manager.js
@@ -3,6 +3,7 @@ const Validation = require("./Validation.js");
 const CreatedLotto = require("./CreatedLotto.js")
 const Lotto = require("./Lotto.js");
 const Bonus = require("./Bonus.js");
+const Message = require("./Message.js");
 
 class Manager {
   quantity;
@@ -35,9 +36,9 @@ class Manager {
     const createLotto = new CreatedLotto(this.quantity);
     this.createdLottoList = createLotto.getCreatedLottoList();
     this.createdLottoList.map(lottos =>  {
-      lottoToString = "["
+      lottoToString = "[";
       lottos.map((item, index) => index !== (lottos.length-1)? lottoToString+=`${item}, ` : lottoToString+=`${item}`);
-      lottoToString += "]"
+      lottoToString += "]";
       MissionUtils.Console.print(lottoToString);
     })
     this.enterNumbers();
@@ -57,53 +58,15 @@ class Manager {
     MissionUtils.Console.readLine("보너스 번호를 입력해 주세요.", (input) => {
       const bonus = new Bonus(this.numbers, input);
       this.bonus = bonus.getBonus();
+
       this.printResult();
     });
   }
 
-  makeWinInfo(){
-    const winCount = this.createdLottoList.map(lotto => lotto.filter(number => this.numbers.includes(number)).length);
-    const hasBonus = this.createdLottoList.map(lotto => lotto.filter(number => number === parseInt(this.bonus)).length)[0];
-
-    let winInfo = Array.from({length: 5}, () => 0)
-
-    for (let i = 0; i < winCount.length; i++) {
-      if (winCount[i] === 3) winInfo[0] += 1;
-
-      if (winCount[i] === 4) winInfo[1] += 1;
-
-      if (winCount[i] === 5) { 
-        winInfo[2] += 1;
-        if (hasBonus === 1) winInfo[3] += 1; 
-      }
-
-      if (winCount[i] === 6) winInfo[4] += 1;
-    }
-    return winInfo;
-  }
-
-  calRateOfReturn(sum){
-    return ((100 / this.quantity) * (sum / 1000)).toFixed(1); 
-  }
-
-  makeMessage(){
-    const winInfo = this.makeWinInfo();
-    const MONEY_UNITS = ["5,000", "50,000", "1,500,000", "30,000,000", "2,000,000,000"];
-    let message = "당첨 통계 \n---\n";
-    let sum = 0;
-    for (let i = 0; i<5; i++){
-      message += (i > 2) ? `${i+2}개 일치` : `${i+3}개 일치`;
-      message += (i === 3) ? `, 보너스 볼 일치` : "";
-      message += ` (${MONEY_UNITS[i]}원) - ${winInfo[i]}개\n`;
-      sum += parseInt(MONEY_UNITS[i].replace(/,/g, "")) * winInfo[i];
-    }
-    message += `총 수익률은 ${this.calRateOfReturn(sum)}%입니다.`;
-    return message;
-  }
-
   printResult(){
-    const message = this.makeMessage();
-    MissionUtils.Console.print(message);
+    const message = new Message(this.quantity, this.createdLottoList, this.numbers, this.bonus);
+    const result = message.getMessage();
+    MissionUtils.Console.print(result);
     MissionUtils.Console.close();
   }
 }
---
+++ src/Message.js
@@ -0,0 +1,49 @@
+class Message{
+  #message
+
+  constructor(quantity, createdLottoList, numbers, bonus){
+    this.makeMessage(quantity, createdLottoList, numbers, bonus);
+  }
+
+  getMessage(){
+    return this.#message;
+  }
+
+  makeWinInfo(createdLottoList, numbers, bonus){
+    const winInfo = Array.from({length: 5}, () => 0);
+    const winCount = createdLottoList.map(lotto => lotto.filter(number => numbers.includes(number)).length);
+    const hasBonus = createdLottoList.map(lotto => lotto.filter(number => number === parseInt(bonus)).length)[0];
+
+    for (let i = 0; i < winCount.length; i++) {
+      if (winCount[i] === 3) winInfo[0] += 1;
+      if (winCount[i] === 4) winInfo[1] += 1;
+      if (winCount[i] === 5) { 
+        winInfo[2] += 1;
+        if (hasBonus === 1) winInfo[3] += 1; 
+      }
+      if (winCount[i] === 6) winInfo[4] += 1;
+    }
+    return winInfo;
+  }
+
+  calRateOfReturn(quantity, sum){
+    return ((100 / quantity) * (sum / 1000)).toFixed(1); 
+  }
+
+  makeMessage(quantity, createdLottoList, numbers, bonus){
+    const MONEY_UNITS = ["5,000", "50,000", "1,500,000", "30,000,000", "2,000,000,000"];
+    const winInfo = this.makeWinInfo(createdLottoList, numbers, bonus);
+    let message = "당첨 통계 \n---\n";
+    let sum = 0;
+    for (let i = 0; i<5; i++){
+      message += (i > 2) ? `${i+2}개 일치` : `${i+3}개 일치`;
+      message += (i === 3) ? `, 보너스 볼 일치` : "";
+      message += ` (${MONEY_UNITS[i]}원) - ${winInfo[i]}개\n`;
+      sum += parseInt(MONEY_UNITS[i].replace(/,/g, "")) * winInfo[i];
+    }
+    message += `총 수익률은 ${this.calRateOfReturn(quantity, sum)}%입니다.`;
+    this.#message = message;
+  }
+}
+
+module.exports = Message;
