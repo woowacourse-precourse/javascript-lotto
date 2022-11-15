b7afa3e59392112729842e47bdea7c254011f204
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-15 16:42:32 +0900

    feat: add play function to process lotto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+const { Player } = require("./Player");
+const { Lotto } = require("./Lotto");
+const { Statistic } = require("./Statistics");
+const { Console } = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+    processLottery();
+  }
 }
-
+const processLottery = () => {
+  Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
+    const player = new Player(money);
+    player.showLottoNumbers();
+    inputWinningNumber(player);
+  });
+};
+const inputWinningNumber = (player) => {
+  Console.readLine("\n당첨 번호를 입력해 주세요\n", (winningNumbers) => {
+    const winningLotto = new Lotto(winningNumbers.split(","));
+    inputBonusNumber(player, winningLotto);
+  });
+};
+const inputBonusNumber = (player, winningLotto) => {
+  Console.readLine("\n보너스 번호를 입력해 주세요\n", (bonusNumber) => {
+    winningLotto.setBonusNumber(bonusNumber);
+    noticeStatistics(player, winningLotto);
+  });
+};
+const noticeStatistics = (player, winningLotto) => {
+  const statistic = new Statistic();
+  statistic.countRank(player.getLottos(), winningLotto.getNumber());
+  statistic.calculateLotteryReturn(player.getLottos().length * 1000);
+  statistic.showStatistics();
+  Console.close();
+};
+const app = new App();
+app.play();
 module.exports = App;
