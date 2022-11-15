6ff9cbea86e73c96cdd7a0f9a6b78854c2c78a8e
Jongco <narira0922@gmail.com>
2022-11-15 20:57:39 +0900

    feat: 올바르지 않은 상황 에러 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,20 +1,19 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-
+const { Random, Console } = require("@woowacourse/mission-utils");
 class Lotto {
-  #numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-
+  #numbers;
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (numbers !== undefined) {
+      if (numbers.length !== 6) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      if (numbers.some((number) => number > 45)) throw new Error("[ERROR] 숫자는 45보다 작아야 합니다.");
+      if (numbers.some((number) => number < 1)) throw new Error("[ERROR] 숫자는 1보다 커야 합니다.");
+      if ([...new Set(numbers)].length !== numbers.length) throw new Error("[ERROR] 중복된 값이 존재합니다.");
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
