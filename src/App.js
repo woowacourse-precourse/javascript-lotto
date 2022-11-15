4cbb342a8efcd685c8a41f06879e7d53e4752385
jiyun1006 <jiyun4251@gmail.com>
2022-11-15 18:48:32 +0900

    feat : 로또 개수 확인 및 로또 구입 금액 입력 예외 처리
    금액에 상수(1000을 담은 값)을 나누어 개수를 구하고, 금액이 아닌 값들을 입력하는 예외 경우 처리

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 const Lotto = require("./Lotto");
 const MissionUtils = require("@woowacourse/mission-utils");
+const LOTTO = 1000;
 
 class App {
   play() {
-    lottoBuyNum();
+    lottoPrice();
   }
 }
 
-const lottoBuyNum = () => {
+const lottoPrice = () => {
   MissionUtils.Console.readLine('구입금액을 입력해 주세요. \n', (answer) => {
-    console.log(answer);
+    lottoPriceValidate(+answer);
+    const lottoCnt = answer / LOTTO;
   })
+
+}
+
+const lottoPriceValidate = (lottoprice) => {
+  if ((lottoprice % LOTTO) !== 0) {
+    throw new Error("[ERROR] 올바른 금액을 입력 하세요.");
+  }
 }
 
 const app = new App();
