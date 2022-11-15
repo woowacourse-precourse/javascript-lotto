9cb388f546d4247de0a7c3a5112a7acfa2645e01
jungsmins1 <jungsmins00@gmail.com>
2022-11-15 17:27:35 +0900

    feat: 로또 구입 금액을 입력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("../src/Lotto");
+const LottoPurchase = require("../src/LottoPurchase");
+
 class App {
+
+  constructor() {}
+
   play() {}
+
+  inputAmount() {
+    MissionUtils.Console.readLine("구입 금액을 입력해 주세요.", (amount) => {
+      amount = Number(amount);
+      const lottoPurchase = new LottoPurchase(amount);
+      this.lottoListPrint(lottoPurchase);
+    })
+  }
 }
+
+const app = new App;
+app.play();
 
 module.exports = App;
