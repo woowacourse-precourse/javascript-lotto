cb7953b182d8fcbf71e05fa5093d6157324d1ab2
Hoon <si932174@gmail.com>
2022-11-15 09:26:57 +0900

    feat : 예외 사항 추가<- 1000원 이하의 금액을 입력하는 경우

--- src/purchaseAmountValidate.js
+++ src/purchaseAmountValidate.js
@@ -8,6 +8,9 @@ class purchaseAmountValidator {
     if (/[^0-9]/g.test(amount)) {
       throw new Error('[ERROR] 구입 금액은 숫자만 입력 가능합니다.');
     }
+    if (Number(amount) < 1000) {
+      throw new Error('[ERROR] 구입 금액은 숫자만 입력 가능합니다.');
+    }
   }
   // TODO: 추가 기능 구현
 }
