0b429d81063a6b7af6f2a698027252378de01661
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-15 19:03:13 +0900

    style: move makeStatsBoard to Utils
    Move makeStatsBoard to Utils from Print
    - Change name: makeStatsBoard -> Create.statsBoard

--- src/Print.js
+++ src/Print.js
@@ -1,21 +1,10 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { NOTICE_MESSAGE } = require("./Constants");
-
-const makeStatsBoard = (data, performance) => {
-  return `당첨 통계
-  ---
-  3개 일치 (5,000원) - ${data.three}개
-  4개 일치 (50,000원) - ${data.four}개
-  5개 일치 (1,500,000원) - ${data.five}개
-  5개 일치, 보너스 볼 일치 (30,000,000원) - ${data.fivePlusBonus}개
-  6개 일치 (2,000,000,000원) - ${data.six}개
-  총 수익률은 ${performance}%입니다.
-    `;
-};
+const { Create } = require("./Utils");
 
 class Print {
   static purchasedLottoAmount(purchased) {
-    Console.print(purchased.amount + NOTICE_MESSAGE.PURCHASE_AMOUNT);
+    Console.print("\n" + purchased.amount + NOTICE_MESSAGE.PURCHASE_AMOUNT);
   }
 
   static purchasedLottoList(purchased) {
@@ -32,7 +21,7 @@ class Print {
 
   static totalStats(totalStats) {
     const { data, performance } = totalStats;
-    const statsBoard = makeStatsBoard(data, performance);
+    const statsBoard = Create.statsBoard(data, performance);
     Console.print(statsBoard);
   }
 }
--- src/Utils.js
+++ src/Utils.js
@@ -49,6 +49,19 @@ module.exports = {
     randomNumbers() {
       return Random.pickUniqueNumbersInRange(1, 45, 6);
     },
+
+    statsBoard(data, performance) {
+      return `
+당첨 통계
+---
+3개 일치 (5,000원) - ${data.three}개
+4개 일치 (50,000원) - ${data.four}개
+5개 일치 (1,500,000원) - ${data.five}개
+5개 일치, 보너스 볼 일치 (30,000,000원) - ${data.fivePlusBonus}개
+6개 일치 (2,000,000,000원) - ${data.six}개
+총 수익률은 ${performance}%입니다.
+      `;
+    },
   },
 
   ScoreMap: {
