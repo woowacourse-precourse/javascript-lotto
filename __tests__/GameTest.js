b20a5f206bc2d083d18420c7d73cb4f1431f3ad7
Deemou <4106243@naver.com>
2022-11-15 22:06:04 +0900

    test(Game): getRank() 테스트 케이스 추가

--- __tests__/GameTest.js
+++ __tests__/GameTest.js
@@ -1,4 +1,5 @@
 const Game = require('../src/Game');
+const { LOTTO } = require('../src/constant/Lotto');
 const MissionUtils = require('@woowacourse/mission-utils');
 
 const getLogSpy = () => {
@@ -43,7 +44,7 @@ describe('게임 클래스 테스트', () => {
     expect(output).toStrictEqual(expectedOutput);
   });
 
-  test('로또 번호와 당첨 번호가 몇 개 일치하는지 확인 ', () => {
+  test('로또 번호와 당첨 번호가 몇 개 일치하는지 확인한다.', () => {
     const lottoList = [
       ['1', '2', '3', '4', '5', '6'],
       ['1', '2', '3', '4', '5', '7'],
@@ -54,6 +55,22 @@ describe('게임 클래스 테스트', () => {
     const game = new Game();
     lottoList.forEach((lottoNumber, index) => {
       const output = game.countMatch(lottoNumber, winningNumber);
+      expect(output).toStrictEqual(expectedOutput[index]);
+    });
+  });
+
+  test('등수를 반환한다.', () => {
+    const lottoList = [
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 3, 4, 5, 8]
+    ];
+    const match = [6, 5, 5];
+    const bonusNumber = 7;
+    const expectedOutput = [LOTTO.RANK.FIRST, LOTTO.RANK.SECOND, LOTTO.RANK.THIRD];
+    const game = new Game();
+    lottoList.forEach((lottoNumber, index) => {
+      const output = game.getRank(lottoNumber, bonusNumber, match[index]);
       expect(output).toStrictEqual(expectedOutput[index]);
     });
   });
