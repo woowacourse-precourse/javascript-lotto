bc83ea8da1dc3bef3ab73e35eaf858842ae3e661
RosaDamascena <hsj990604@gmail.com>
2022-11-15 15:49:49 +0900

    feat : 로또 구입 금액 예외 처리 기능 구현

--- src/MyLotto.js
+++ src/MyLotto.js
@@ -11,8 +11,15 @@ class MyLotto {
     getInputAmount() {
     MissionUtils.Console.readLine (
         "구입금액을 입력해 주세요. \n", (money) => {
-            this.#money = parseInt(money);
+            this.validate(money);
+            this.#money;
         });
+    }
+
+    validate(money) {
+        if (money % 1000 !== 0) {
+            throw new Error("[ERROR] 금액은 1000원 단위로 입력해야 합니다.");
+        }
     }
 }
 
