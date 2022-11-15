666b4e94eb51d922ada037265c082377bb7da2d4
sh981013s <sh981013s@gmail.com>
2022-11-15 17:31:07 +0900

    test(UserNumberTest): 보너스 번호 관련 테스트 작성

--- __tests__/UserNumberTest.js
+++ __tests__/UserNumberTest.js
@@ -5,19 +5,45 @@ describe("UserNumber 유저 구입금액 관련 테스트", () => {
 
   test("유저 구입금액이 1000원으로 나누어 떨어지지 않으면 예외가 발생한다.", () => {
     expect(() => {
-      userNumber.validatePurchasingAmount(1200);
+      userNumber.setPurchasingAmount(1200);
     }).toThrow("[ERROR]");
   });
 
   test("유저 구입금액이 빈문자열이라면 예외가 발생한다.", () => {
     expect(() => {
-      userNumber.validatePurchasingAmount("");
+      userNumber.setPurchasingAmount("");
     }).toThrow("[ERROR]");
   });
 
   test("유저 구입금액이 정수가 아니라면 예외가 발생한다.", () => {
     expect(() => {
-      userNumber.validatePurchasingAmount("이승환");
+      userNumber.setPurchasingAmount("이승환");
+    }).toThrow("[ERROR]");
+  });
+});
+
+describe("UserNumber 보너스 숫자 관련 테스트", () => {
+  const userNumber = new UserNumber();
+
+  test("보너스 숫자가 빈문자열이라면 예외가 발생한다.", () => {
+    expect(() => {
+      userNumber.setBonusNumber("");
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 숫자가 정수가 아니라면 예외가 발생한다.", () => {
+    expect(() => {
+      userNumber.setBonusNumber("이승환");
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 숫자가 1~45 범주를 벗어난다면 예외가 발생한다.", () => {
+    expect(() => {
+      userNumber.setBonusNumber(-3);
+    }).toThrow("[ERROR]");
+
+    expect(() => {
+      userNumber.setBonusNumber(47);
     }).toThrow("[ERROR]");
   });
 });
