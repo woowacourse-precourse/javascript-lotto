19235d27dea617ae941641cba88ebe8fefd7c9f3
Deemou <4106243@naver.com>
2022-11-15 15:43:23 +0900

    test(Validation): throwException() 테스트 케이스 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,5 @@
 const Validation = require('../src/Validation');
+const InvalidInputError = require('../src/error/InvalidInputError');
 
 describe('유효성 검사 테스트', () => {
   test('주어진 배열이 숫자 또는 숫자 문자열로 이루어졌는지 확인', () => {
@@ -43,5 +44,12 @@ describe('유효성 검사 테스트', () => {
       const output = Validation.isUnique(input);
       expect(output).toBe(expectedOutput[index]);
     });
+  });
+
+  test('예외 발생', () => {
+    const message = '[ERROR] 유효하지 않은 입력';
+    expect(() => {
+      Validation.throwException(InvalidInputError, message);
+    }).toThrow('[ERROR]');
   });
 });
