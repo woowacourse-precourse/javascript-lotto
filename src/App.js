f0ac150951f8de965a359519adf2c6214f658538
leeseooo <leeseoooo@naver.com>
2022-11-15 13:26:04 +0900

    feat: 당첨 번호 입력받기

--- src/App.js
+++ src/App.js
@@ -14,11 +14,20 @@ class App {
       this.lotto = Lotto.purchase(input);
       MissionUtils.Console.print(`${this.lotto.length}개를 구매했습니다.`);
       this.printLottos();
+      this.getWinNumbers();
     });
   }
   printLottos() {
     this.lotto.forEach((lotto) => {
       MissionUtils.Console.print(`[${lotto.numbers.join(", ")}]`);
+    });
+  }
+  getWinNumbers() {
+    MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.\n", (input) => {
+      if (input.split(",").length !== 6) {
+        throw new Error("[ERROR] 당첨 번호는 6개를 입력해주세요.");
+      }
+      Lotto.prototype.winNumbers = input;
     });
   }
 }
