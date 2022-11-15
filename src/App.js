359f644027681f3faaadfef6eb57dbdba0069ba9
yuna Kim <yuna000804@gmail.com>
2022-11-15 19:12:54 +0900

    feat : 당첨 통계 문구 출력

--- src/App.js
+++ src/App.js
@@ -72,6 +72,17 @@ class App {
     this.printResult(list);
   }
 
+  printResult(list) {
+    this.list = list;
+    Console.print('\n당첨 통계\n---');
+    Console.print(`3개 일치 (5,000원) - ${list[3]}개`);
+    Console.print(`4개 일치 (50,000원) - ${list[4]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${list[5]}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${list[7]}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${list[6]}개`);
+    this.printStatistic(list);
+  }
+  
 
 }
 const a = new App;
