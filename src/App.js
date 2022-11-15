348140f99706bf018268b77b19ec8b9c746cf11f
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-15 22:29:31 +0900

    Feat: 구입금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+const IO = require('./utils/io');
+const Prompter = require('./Prompter');
+const Lotto = require('./Lotto');
+const Book = require('./Book');
+const Pick = require('./utils/pick');
+const Evaluator = require('./Evaluator');
+const Bonus = require('./Bonus');
 class App {
-  play() {}
+	book;
+	evaluator;
+	tickets;
+	lotto;
+	bonus;
+
+	constructor() {
+		this.book = new Book();
+		this.evaluator = new Evaluator();
+	}
+
+	play() {
+		this.getInvest();
+	}
+
+	getInvest() {
+		Prompter.promptInvest(this.handleInvest.bind(this));
+	}
+
+	handleInvest(invest) {
+		this.book.setInvest(Number(invest));
+	}
 }
 
 module.exports = App;
