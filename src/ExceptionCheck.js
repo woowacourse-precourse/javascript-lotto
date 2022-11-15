446818f75ace49dc7d5322ce4b49b688aafb9a55
daal2quf <daal2quf@gmail.com>
2022-11-15 19:30:24 +0900

    feat(exceptionCheck): 유저가 입력한 당첨 숫자가 6자리가 아닐때 예외처리
    add:
    userInputWinNumbers(): 길이가 6 인지 확인

--- src/ExceptionCheck.js
+++ src/ExceptionCheck.js
@@ -1,11 +1,17 @@
-const App = require("./App");
-const { Console } = require("@woowacourse/mission-utils");
+const Utils = require('./Utils');
 
 class ExceptionCheck {
-
+  constructor() {
+    this.utils = new Utils();
+  }
   userInputMoneyValue(moneyValue) {
     if (moneyValue % 1000 !== 0 || moneyValue === 0) {
-      throw new Error("[ERROR] 구매 금액은 1,000 단위로 입력해주세요");
+      throw new Error('[ERROR] 구매 금액은 1,000 단위로 입력해주세요');
+    }
+  }
+  userInputWinNumbers(winNumbers) {
+    if (winNumbers.lenght !== 6) {
+      throw new Error('[ERROR] 당첨 숫자는 6자리로 입력해주십시오');
     }
   }
 }
