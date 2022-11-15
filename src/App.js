0a98b2a942bfe552cd5057bd2a491e478ef422c0
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2022-11-16 02:59:00 +0900

    feat : 보너스 번호 입력

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ class App {
   play() {
     this.lottoPrice();
   }
+  BonusInput = () => {
+    MissionUtils.Console.readLine(
+      "보너스 번호를 입력해 주세요.",
+      (BonusNumber) => {
+        new Winning.BonusNumber(BonusNumber);
+        MissionUtils.Console.print(BonusNumber);
+      }
+    );
+  };
   lottoIssuance = (count) => {
     MissionUtils.Console.print(`${count}개를 구매했습니다.`);
     const lottoNumber = Array.from({ length: count }, () => {
@@ -20,6 +29,7 @@ class App {
     lottoNumber.forEach((num) => {
       data.push(num.getNumbers());
     });
+    this.winningInput();
     MissionUtils.Console.print(data);
   };
 
