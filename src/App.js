13d97824d4d425fad0d329bc97aea049c7b6e8eb
SeungWoo Kim <osabero@naver.com>
2022-11-15 20:15:52 +0900

    다시 처음부터 하나하나 기능구현 하고 커밋하려고 합니다.

--- src/App.js
+++ src/App.js
@@ -3,91 +3,91 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    purchaseInput()
+    purchase()
 
   }
 }
-function purchaseInput() {
-  MissionUtils.Console.readLine('금액을 입력해 주세요', (purchase_input)=>{
-    exceptiondollar(purchase_input)
+// function purchaseInput() {
+//   MissionUtils.Console.readLine('금액을 입력해 주세요', (purchase_input)=>{
+//     exceptiondollar(purchase_input)
     
-    lotto(purchase_input)
-  })
-}
+//     lotto(purchase_input)
+//   })
+// }
 
-function lotto(dollarInput) {
-  MissionUtils.Console.print(`${dollarInput/1000}개를 구매했습니다.`)
-  let lotto_number = []
-  for (let i = 0; i < dollarInput/1000; i++){
-    let numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a,b)=> {
-      if(a>b){return 1}
-      if(a<b){return -1}
-    })
-    MissionUtils.Console.print(`[${numbers[0]}, ${numbers[1]}, ${numbers[2]}, ${numbers[3]}, ${numbers[4]}, ${numbers[5]}]`)
-    lotto_number.push(numbers)
-  }
+// function lotto(dollarInput) {
+//   MissionUtils.Console.print(`${dollarInput/1000}개를 구매했습니다.`)
+//   let lotto_number = []
+//   for (let i = 0; i < dollarInput/1000; i++){
+//     let numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a,b)=> {
+//       if(a>b){return 1}
+//       if(a<b){return -1}
+//     })
+//     MissionUtils.Console.print(`[${numbers[0]}, ${numbers[1]}, ${numbers[2]}, ${numbers[3]}, ${numbers[4]}, ${numbers[5]}]`)
+//     lotto_number.push(numbers)
+//   }
   
-  return winningInput(lotto_number)
-}
+//   return winningInput(lotto_number)
+// }
 
-function winningInput (lotto_number) {
-  MissionUtils.Console.readLine('당첨 번호를 입력해 주세요', (winning_inputnumber)=>{
-    exceptionWinningInput(winning_inputnumber)
-    MissionUtils.Console.readLine('보너스 번호를 입력해 주세요', (bonus_inputnumber)=>{
-      if(winning_inputnumber.includes(bonus_inputnumber)){
-        throw '[ERROR] 당첨번호와 다른 번호를 입력해주세요.'
-      }
-      return winningNumber(lotto_number, winning_inputnumber, bonus_inputnumber)
-    })
-  })
-}
+// function winningInput (lotto_number) {
+//   MissionUtils.Console.readLine('당첨 번호를 입력해 주세요', (winning_inputnumber)=>{
+//     exceptionWinningInput(winning_inputnumber)
+//     MissionUtils.Console.readLine('보너스 번호를 입력해 주세요', (bonus_inputnumber)=>{
+//       if(winning_inputnumber.includes(bonus_inputnumber)){
+//         throw '[ERROR] 당첨번호와 다른 번호를 입력해주세요.'
+//       }
+//       return winningNumber(lotto_number, winning_inputnumber, bonus_inputnumber)
+//     })
+//   })
+// }
 
-function winningNumber (lotto_number, winning_inputnumber, bonus_inputnumber) {
-  let winningPoint = [0, 0, 0, 0, 0]
-  lotto_number.forEach((item)=>{
-    let count = 0
-    item.forEach((idx)=>{
-      if(winning_inputnumber.includes(idx)){count ++}
-    })
-    countcheck(count, bonus_inputnumber, winningPoint, lotto_number)
-  })
-  return winningresult(lotto_number, winningPoint)
-}
-function countcheck(count, bonus_inputnumber, winningPoint, lotto_number){
-  if(count==5 && lotto_number.includes(bonus_inputnumber)){
-    console.log('----')
-    count = 100
-  }
-  switch (count) {
-    case 3:
-      winningPoint[0]++
-      break;
-    case 4:
-      winningPoint[1]++
-      break;
-    case 5:
-      winningPoint[2]++
-      break;
-    case 6:
-      winningPoint[4]++
-      break;
-    case 100:
-      winningPoint[3]++
-  }
-}
-function winningresult(lotto_number, winningPoint) {
-  let value = 5000*winningPoint[0] + 50000*winningPoint[1] + 1500000*winningPoint[2] + 30000000*winningPoint[3] + 2000000000*winningPoint[4]
-  let dummy = lotto_number.length*1000
-  MissionUtils.Console.print(`당첨 통계`)
-  MissionUtils.Console.print('---')
-  MissionUtils.Console.print(`3개 일치 (5,000원) - ${winningPoint[0]}개`)
-  MissionUtils.Console.print(`4개 일치 (50,000원) - ${winningPoint[1]}개`)
-  MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${winningPoint[2]}개`)
-  MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningPoint[3]}개`)
-  MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${winningPoint[4]}개`)
-  MissionUtils.Console.print(`총 수익률은 ${value*100/dummy}%입니다.`)
-  MissionUtils.Console.close()
-}
+// function winningNumber (lotto_number, winning_inputnumber, bonus_inputnumber) {
+//   let winningPoint = [0, 0, 0, 0, 0]
+//   lotto_number.forEach((item)=>{
+//     let count = 0
+//     item.forEach((idx)=>{
+//       if(winning_inputnumber.includes(idx)){count ++}
+//     })
+//     countcheck(count, bonus_inputnumber, winningPoint, lotto_number)
+//   })
+//   return winningresult(lotto_number, winningPoint)
+// }
+// function countcheck(count, bonus_inputnumber, winningPoint, lotto_number){
+//   if(count==5 && lotto_number.includes(bonus_inputnumber)){
+//     console.log('----')
+//     count = 100
+//   }
+//   switch (count) {
+//     case 3:
+//       winningPoint[0]++
+//       break;
+//     case 4:
+//       winningPoint[1]++
+//       break;
+//     case 5:
+//       winningPoint[2]++
+//       break;
+//     case 6:
+//       winningPoint[4]++
+//       break;
+//     case 100:
+//       winningPoint[3]++
+//   }
+// }
+// function winningresult(lotto_number, winningPoint) {
+//   let value = 5000*winningPoint[0] + 50000*winningPoint[1] + 1500000*winningPoint[2] + 30000000*winningPoint[3] + 2000000000*winningPoint[4]
+//   let dummy = lotto_number.length*1000
+//   MissionUtils.Console.print(`당첨 통계`)
+//   MissionUtils.Console.print('---')
+//   MissionUtils.Console.print(`3개 일치 (5,000원) - ${winningPoint[0]}개`)
+//   MissionUtils.Console.print(`4개 일치 (50,000원) - ${winningPoint[1]}개`)
+//   MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${winningPoint[2]}개`)
+//   MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningPoint[3]}개`)
+//   MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${winningPoint[4]}개`)
+//   MissionUtils.Console.print(`총 수익률은 ${value*100/dummy}%입니다.`)
+//   MissionUtils.Console.close()
+// }
 function exceptiondollar(dollarInput) {
   let regExp = new RegExp('/\D/gm')
   if (regExp.test(dollarInput)){
@@ -99,12 +99,11 @@ function exceptiondollar(dollarInput) {
 }
 function exceptionWinningInput(winningInput){
   let winningInputArray = winningInput.split(',')
-  let regExp = new RegExp('/\D/gm')
   let winningSet = new Set(winningInputArray)
-  if (regExp.test(winningInput)){
+  if (isNaN(winningInput)){
     throw '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.'
   }
-  if (Math.max(winningInput)>45){
+  if (Math.max(...winningInnputArray)>45){
     throw '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.'
   }
   if (winningInputArray.length !== 6) {
