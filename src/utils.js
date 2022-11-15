5982e98de34db4d8563d2be50db7270b9ef442b3
연 <mogoo22@naver.com>
2022-11-15 13:45:10 +0900

    refactor: 로또 번호 및 당첨 번호 유효성 검증 유틸 함수 분리

--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,22 @@ const { ERROR_PREFIX, ERROR_MSG } = require('./constants');
 
 const makeErrorMsg = (errorType) => `${ERROR_PREFIX} ${ERROR_MSG[errorType]}`;
 
-module.exports = makeErrorMsg;
+const invalidNumber = (numbers) =>
+  numbers.filter((number) => typeof number !== 'number' || Number.isNaN(number))
+    .length > 0;
+
+const invalidInputNum = (numbers, num) => numbers.length !== num;
+
+const invalidDuplication = (numbers, num) => new Set(numbers).size !== num;
+
+const invalidRange = (numbers, [minNum, maxNum]) =>
+  numbers.filter((number) => !(number >= minNum && number <= maxNum)).length >
+  0;
+
+module.exports = {
+  makeErrorMsg,
+  invalidNumber,
+  invalidInputNum,
+  invalidDuplication,
+  invalidRange,
+};
