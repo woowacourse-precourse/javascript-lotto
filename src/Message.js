edf9b9d15c178a8bcc12df9ccac9b94dc7041302
woowoni <ewoowon.2@gmail.com>
2022-11-15 23:27:32 +0900

    메세지 내용 수정

--- src/Message.js
+++ src/Message.js
@@ -6,11 +6,13 @@ const REQUEST_MESSAGE = {
 
 const ERROR_MESSAGE = {
   GAME_UNIT: "[ERROR] 1000원 단위만 입력이 가능합니다.",
+  GAME_BLANK: "[ERROR] 구입금액이 입력되지 않았습니다.",
+  GAME_FORM:'[ERROR] 입력형식이 올바르지 않습니다.',
   NUMBER_RANGE: "[ERROR] 1부터 45의 숫자만 입력이 가능합니다.",
   NUMBERS_LENGTH: "[ERROR] 로또 번호는 6개여야 합니다.",
   SINGLE_NUMBER: "[ERROR] 하나의 숫자만 입력이 가능합니다.",
   ONLY_NUMBER: "[ERROR] 숫자만 입력이 가능합니다.",
-  DUPLICATION: "[ERROR] 중복된 숫자는 입력이 불가능합니다."
+  DUPLICATION: "[ERROR] 중복된 숫자는 입력이 불가능합니다.",
 }
 
 module.exports = {REQUEST_MESSAGE, ERROR_MESSAGE}
