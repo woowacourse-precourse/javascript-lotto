d5eac3e268d4dd484b69079f584e6f1617554487
knh6269 <knh6269@gmail.com>
2022-11-15 22:14:38 +0900

    feat(Lotto): 당첨 번호 유효성 검사 함수
    기능목록(Lotto-2)

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,6 +7,19 @@ class Lotto {
   }
 
   validate(numbers) {
+  validate (numbers) {
+    numbers.map((item) => {
+      if (item < 1 || item > 45) {
+        InputOutput.close();
+        throw new Error(Message.LOTTO_NUMBER_BIGGER_THAN_ONE_SMALLER_THAN_FOURTY_FIVE);
+      }
+
+      if (parseInt(item) !== item) {
+        InputOutput.close();
+        throw new Error(Message.NOT_NUMBER);
+      }
+    })
+
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
