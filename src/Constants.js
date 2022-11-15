478ebb1cff4203000ed03464ad1cf002bacc2511
feb-dain <feb.dain@gmail.com>
2022-11-15 13:48:25 +0900

    refactor: remove \n

--- src/Constants.js
+++ src/Constants.js
@@ -39,7 +39,7 @@ const INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.\n";
 const INPUT_NUMBERS_MESSAGE = "\n당첨 번호를 입력해 주세요.\n";
 const INPUT_BONUS_NUMBERS_MESSAGE = "\n보너스 번호를 입력해 주세요.\n";
 const NUMBER_OF_TICKETS_MESSAGE = "개를 구매했습니다.";
-const RESULT_MESSAGE = "\n당첨 통계\n---\n";
+const RESULT_MESSAGE = "\n당첨 통계\n---";
 
 module.exports = {
   CONSOLE_UTIL,
