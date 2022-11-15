bec4dfe7d8474143f90d4a92b5815edad7c97cc1
osdoonhyun <skw1996@naver.com>
2022-11-15 18:53:44 +0900

    feat(LottoGame): 기능목록[5-1] 사용자가 구매한 로또 번호와 당첨 번호 비교하는 기능 구현

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,16 +1,26 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { MESSAGES } = require("./Constants/Constants");
+const { MESSAGES, LOTTO_RANK, LOTTO_PRIZE } = require("./Constants/Constants");
 const Validation = require("./Validation");
 const LottoGameOperator = require("./LottoGameOperator");
+const LottoStats = require("./LottoStats");
 
 class LottoGame {
   #purchaseAmount;
   #LottoList;
   #winningNumbers;
   #bonusNumber;
+  #lottoRanking;
 
   constructor() {
     this.operator = new LottoGameOperator();
+    this.#lottoRanking = {
+      boom: 0,
+      firstPlace: 0,
+      secondPlace: 0,
+      thirdPlace: 0,
+      fourthPlace: 0,
+      fifthPlace: 0,
+    };
   }
 
   play() {
@@ -18,10 +28,9 @@ class LottoGame {
   }
 
   purchaseLotto() {
-    Console.readLine(MESSAGES.ENTER_PURCHASE_AMOUNT, (input) => {
-      const amount = Number(input);
+    Console.readLine(MESSAGES.ENTER_PURCHASE_AMOUNT, (amount) => {
       Validation.validatePurchaseAmount(amount);
-      this.#purchaseAmount = amount;
+      this.#purchaseAmount = Number(amount);
       const lottoQuantity = this.operator.countLottoTickets(
         this.#purchaseAmount
       );
@@ -43,11 +52,57 @@ class LottoGame {
   }
 
   makeBonusNumber() {
-    Console.print(MESSAGES.INPUT_BONUS_NUMBER, (input) => {
-      Validation.validateBonusNumber(Number(input), this.#winningNumbers);
+    Console.readLine(MESSAGES.INPUT_BONUS_NUMBER, (input) => {
+      const inputBonusNumber = Number(input);
 
-      this.#bonusNumber = Number(input);
+      Validation.validateBonusNumber(Number(input), this.#winningNumbers);
+      this.#bonusNumber = inputBonusNumber;
+
+      return this.showLottoStats();
     });
+  }
+
+  showLottoStats() {
+    Console.print(MESSAGES.LOTTO_STATS);
+
+    this.calculateTotalProfit(
+      this.#LottoList,
+      this.#winningNumbers,
+      this.#bonusNumber,
+      this.#purchaseAmount
+    );
+  }
+
+  calculateTotalProfit(lottoList, winningNumbers, bonusNumber, purchaseAmount) {
+    lottoList.map((lottoNumber) => {
+      const result = this.compareLottoNumbers(
+        lottoNumber.getLottoNumbers(),
+        winningNumbers,
+        bonusNumber
+      );
+
+      this.#lottoRanking[result] += 1;
+    });
+  }
+
+  compareLottoNumbers(lottoNumber, winningNumbers, bonusNumber) {
+    const ranking = lottoNumber.filter((number) =>
+      winningNumbers.includes(number)
+    );
+
+    switch (ranking.length) {
+      case LOTTO_RANK.firstPlace:
+        return "firstPlace";
+      case (LOTTO_RANK.secondPlace, LOTTO_RANK.thirdPlace):
+        if (lottoNumber.includes(bonusNumber)) return "secondPlace";
+        return "thirdPlace";
+      case LOTTO_RANK.fourthPlace:
+        return "fourthPlace";
+      case LOTTO_RANK.fifthPlace:
+        return "fifthPlace";
+      default:
+        return "boom";
+    }
   }
 }
 
