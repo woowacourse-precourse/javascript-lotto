fba1d1e17e1ec9fc563241a2d8d248b3da4a2a80
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-11-15 17:28:55 +0900

    refactor : 결과 메시지 상수화

--- src/App.js
+++ src/App.js
@@ -105,9 +105,8 @@ class App {
   }
 
   printResult() {
-    Console.print(
-      `3개 일치 (5,000원) - ${this.countCorrect[0]}개\n4개 일치 (50,000원) - ${this.countCorrect[1]}개\n5개 일치 (1,500,000원) - ${this.countCorrect[2]}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.countCorrect[3]}개\n6개 일치 (2,000,000,000원) - ${this.countCorrect[4]}개\n총 수익률은 ${this.yield}%입니다.`
-    );
+    const message = `당첨통계\n---\n3개 일치 (5,000원) - ${this.countCorrect[0]}개\n4개 일치 (50,000원) - ${this.countCorrect[1]}개\n5개 일치 (1,500,000원) - ${this.countCorrect[2]}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.countCorrect[3]}개\n6개 일치 (2,000,000,000원) - ${this.countCorrect[4]}개\n총 수익률은 ${this.yield}%입니다.`;
+    Console.print(message);
     Console.close();
   }
 }
