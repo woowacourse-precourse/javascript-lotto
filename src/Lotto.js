49624c31dff45a0135bd185c26e3e32aef206efc
cyberjam <jamsince1994@naver.com>
2022-11-15 12:03:38 +0900

    test(Lotto): 숫자 범위 유효성 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-shadow */
+/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable class-methods-use-this */
-// const TicketBox = require('./TicketBox');
+
+const { TICKET_NUMBER } = require('./Constant');
 
 class Lotto {
   #numbers;
@@ -10,11 +13,21 @@ class Lotto {
   }
 
   validate(numbers) {
+    const notInRange = (numbers) =>
+      numbers.filter(
+        (number) =>
+          number < TICKET_NUMBER.RANGE_START ||
+          number > TICKET_NUMBER.RANGE_END,
+      ).length;
+
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
     if (new Set(numbers).size !== 6) {
       throw new Error('[ERROR] 숫자를 중복없이 입력하여 주십시오.');
+    }
+    if (notInRange(numbers)) {
+      throw new Error('[ERROR] 1~45 사이 숫자를 입력하여 주십시오.');
     }
   }
 
