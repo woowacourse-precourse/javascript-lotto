c574e724591fd5b5de7b834772c7c0be4e8ed2a5
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-15 22:12:12 +0900

    feat: 로또 모델 검증 로직

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -1,18 +1,45 @@
+const { LOTTO, ERROR_MESSAGE } = require("../constants");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
+    this.getNumbers = this.getNumbers.bind(this);
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    console.log({ numbers });
+    if (numbers.length !== LOTTO.LENGTH) {
+      throw new Error(ERROR_MESSAGE.COMMA);
+    }
+    if (!numbers.every(this.isNumber)) {
+      throw new Error(ERROR_MESSAGE.NAN);
+    }
+    if (!numbers.every(this.isValidRange)) {
+      throw new Error(ERROR_MESSAGE.RANGE);
+    }
+    if (this.hasDuplicate(numbers)) {
+      throw new Error(ERROR_MESSAGE.DUPLICATE);
     }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
+
+  isNumber(number) {
+    return Number.isInteger(number);
+  }
+
+  isValidRange(number) {
+    return LOTTO.MIN <= number && number <= LOTTO.MAX;
+  }
+
+  hasDuplicate(numbers) {
+    return new Set(numbers).size !== numbers.length;
+  }
 }
 
 module.exports = Lotto;
