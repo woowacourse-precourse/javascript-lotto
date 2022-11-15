4d5af33320b97e3c117cba9a7e3bc06466b72de9
hyukzz <ri_chard@naver.com>
2022-11-15 14:00:38 +0900

    feat: 로또 번호 예외처리 적용

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,17 @@
+const Validation = require("./Validation");
+
 class Lotto {
-  #numbers;
+	#numbers;
 
-  constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
-  }
+	constructor(numbers) {
+		const validation = new Validation();
+		validation.validateLotto(numbers);
+		this.#numbers = numbers;
+	}
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
-
-  // TODO: 추가 기능 구현
+	getNumbers() {
+		return this.#numbers;
+	}
 }
 
 module.exports = Lotto;
