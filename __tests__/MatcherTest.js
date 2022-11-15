f91c0eee3b4f45e533986eb4daea1052d44c72f9
yeongseokim <1903020@ewhain.net>
2022-11-15 21:12:27 +0900

    test(Matcher): matchWinthWinningNumbers 테스트
    1. 1등 배열이 들어오면 1등 인덱스 반환
    2. 2등 배열이 들어오면 3등 인덱스 반환 (아직 보너스 번호와 맞는지
       검사하지 않았으므로)
    3. 3등 배열이 들어오면 3등 인덱스 반환
    4. 4등 배열이 들어오면 4등 인덱스 반환
    5. 5등 배열이 들어오면 5등 인덱스 반환
    6. 낙첨 배열이 들어오면 낙첨 인덱스 반환

---
+++ __tests__/MatcherTest.js
@@ -0,0 +1,32 @@
+const Matcher = require("../src/Matcher");
+const Lotto = require("../src/Lotto");
+const PRIZE_INDEX = require('../src/constants').PRIZE_INDEX;
+const winningNumbers = [1, 2, 3, 4, 5, 6];
+const bonusNumber = 7;
+const matcher = new Matcher(winningNumbers, bonusNumber);
+
+describe("Matcher 클래스 테스트", () => {
+    test("matchWithWinningNumbers: 1등 인덱스 반환 테스트", () => {
+        expect(matcher.matchWithWinningNumbers(winningNumbers)).toEqual(PRIZE_INDEX.FIRST);
+    });
+
+    test("matchWithWinningNumbers: 3등 인덱스 반환 테스트", () => {
+        expect(matcher.matchWithWinningNumbers([1, 2, 3, 4, 5, 7])).toEqual(PRIZE_INDEX.THIRD);
+    });
+
+    test("matchWithWinningNumbers: 3등 인덱스 반환 테스트", () => {
+        expect(matcher.matchWithWinningNumbers([1, 2, 3, 4, 5, 8])).toEqual(PRIZE_INDEX.THIRD);
+    });
+
+    test("matchWithWinningNumbers: 4등 인덱스 반환 테스트", () => {
+        expect(matcher.matchWithWinningNumbers([1, 2, 3, 4, 7, 8])).toEqual(PRIZE_INDEX.FOURTH);
+    });
+
+    test("matchWithWinningNumbers: 5등 인덱스 반환 테스트", () => {
+        expect(matcher.matchWithWinningNumbers([1, 2, 3, 9, 7, 8])).toEqual(PRIZE_INDEX.FIFTH);
+    });
+
+    test("matchWithWinningNumbers: 낙첨 인덱스 반환 테스트", () => {
+        expect(matcher.matchWithWinningNumbers([1, 2, 10, 9, 7, 8])).toEqual(PRIZE_INDEX.NOTHING);
+    });
+})
