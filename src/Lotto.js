0c1adb29dbd0047447049ae3b2b3af95e29111b0
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-11-15 10:39:11 +0900

    feat(Lotto): 로또 클래스 유효성 검사 구현
    로또 클래스를 생성하면 인수가 로또 번호로 유효한지 검사하는 기능 구현
    - 로또 번호의 개수가 6개인지 검사한다.
    - 로또 번호에 중복이 없는지 검사한다.
    - 로또 번호가 모두 숫자인지 검사한다.
    - 로또 번호가 1에서 45 사이에 있는지 검사한다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,6 @@
 class Lotto {
+  static LOTTO_LENGTH = 6;
+  static LOTTO_RANGE = { MIN: 1, MAX: 45 };
   #numbers;
 
   constructor(numbers) {
@@ -7,12 +9,28 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (numbers.length !== Lotto.LOTTO_LENGTH) {
+      throw new Error(
+        `[ERROR] 로또 번호는 ${Lotto.LOTTO_LENGTH}개여야 합니다.`
+      );
     }
+
+    if (new Set(numbers).size !== Lotto.LOTTO_LENGTH) {
+      throw new Error(`[ERROR] 로또 번호에는 중복이 존재할 수 없습니다.`);
+    }
+
+    numbers.forEach((number) => this.validateNumber(number));
   }
 
-  // TODO: 추가 기능 구현
+  validateNumber(number) {
+    if (Number.isNaN(+number))
+      throw new TypeError('[ERROR] 로또 번호는 숫자만 입력해야 합니다.');
+
+    if (+number < Lotto.LOTTO_RANGE.MIN || +number > Lotto.LOTTO_RANGE.MAX)
+      throw new RangeError(
+        `[ERROR] 로또 번호는 ${Lotto.LOTTO_RANGE.MIN}부터 ${Lotto.LOTTO_RANGE.MAX} 사이의 숫자여야 합니다.`
+      );
+  }
 }
 
 module.exports = Lotto;
