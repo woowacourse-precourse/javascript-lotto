dd754491f66712f3869bf1ef0fc6ca7a556dd6a7
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-11-15 14:37:47 +0900

    feat: 당첨 통계를 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -117,6 +117,7 @@ class App {
         this.checkRanking(correctCount, lotto.getNumbers());
       }
     });
+    this.printStatistics();
   }
 
   checkRanking(count, lotto) {
@@ -141,6 +142,21 @@ class App {
 
   calculateEarningsRate() {
     return (this.winningReward / this.purchaseAmount) * 100;
+  }
+
+  printStatistics() {
+    Console.print('당첨 통계');
+    Console.print('---');
+    Console.print(`3개 일치 (5,000원) - ${this.ranking.fifth}개`);
+    Console.print(`4개 일치 (50,000원) - ${this.ranking.fourth}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${this.ranking.third}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.ranking.second}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${this.ranking.first}개`);
+
+    const earningsRate = this.calculateEarningsRate();
+    Console.print(`총 수익률은 ${earningsRate.toFixed(1)}%입니다.`);
   }
 }
 
