20c1e271bfbf83fb87b078b8c8f561fcfdf0c54b
wish0ne <wish@khu.ac.kr>
2022-11-15 21:16:51 +0900

    test: add LottoGameTest

--- __tests__/LottoGameTest.js
+++ __tests__/LottoGameTest.js
@@ -1,10 +1,41 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const Lotto = require('../src/Lotto');
 const LottoGame = require('../src/LottoGame');
 
-// 테스트 이름 : when_given_then
-// when: 어떤 행동을 했는지
-// given : 어떤 조건에서 이 일이 일어나는지
-// then : given과 같은 조건에서 when을 했을때 어떤 일이 일어날 것인지
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
 describe('LottoGame class test', () => {
-  test('', () => {});
+  test('countWinningNumber', () => {
+    const game = new LottoGame();
+    expect(
+      game.countWinningNumber([1, 6, 3, 10, 21, 23], [1, 2, 3, 4, 5, 6]),
+    ).toBe(3);
+    game.exitGame();
+  });
+  test('hasBonusNumber_hasBonusNumber', () => {
+    const game = new LottoGame();
+    expect(game.hasBonusNumber([1, 6, 3, 10, 21, 7], 3)).toBe(true);
+  });
+  test('hasBonusNumber_hasNotBonusNumber', () => {
+    const game = new LottoGame();
+    expect(game.hasBonusNumber([1, 6, 3, 10, 21, 7], 8)).toBe(false);
+  });
+  test('calcRateOfReturn', () => {
+    const game = new LottoGame();
+    expect(game.calcRateOfReturn(1000, 10000)).toBe('1000.0');
+  });
 });
