2d15ddd03558904da7485d058d75cccb237049f0
hyukzz <ri_chard@naver.com>
2022-11-15 20:02:15 +0900

    feat: 앱 실행 및 테스트 확인

--- src/App.js
+++ src/App.js
@@ -1,91 +1,13 @@
-const UserInput = require("./UserInput.js");
-const { Console, Random } = require("@woowacourse/mission-utils");
+let PrintLotto = require("./PrintLotto");
 
 class App {
-	lottoCnt;
-	winLottoNums;
-	BonusLottoNums;
-
-	userInputLotto(price) {
-		let userInput = new UserInput(price);
-		this.lottoCnt = userInput.lottoCnt;
-	}
-
-	printLottoCnt(lottoCnt) {
-		Console.print(`${lottoCnt}개를 구매했습니다.`);
-	}
-
-	winLottoNum() {
-		return Random.pickUniqueNumbersInRange(1, 45, 6);
-	}
-
-	sortedLottoNum(lottoNum) {
-		return lottoNum.sort((a, b) => a - b);
-	}
-
-	printLottoNum(lottoNum) {
-		return Console.print(`[${lottoNum.join(", ")}]`);
-	}
-
-	printWinLotto(lottoCnt) {
-		for (let i = 0; i < lottoCnt; i++) {
-			let lottoNum = this.sortedLottoNum(this.winLottoNum());
-			this.printLottoNum(lottoNum);
-		}
-	}
-
-	useWinLottoNum() {
-		Console.print("당첨 번호를 입력해 주세요.");
-		Console.readline("", input => {
-			const inputNum = input.split(",").map(Number);
-
-			this.isWinValidation(inputNum);
-			this.winLottoNums = inputNum;
-		});
-	}
-
-	isWinValidation(input) {
-		const inputNum = input.split(",");
-
-		if ([...new Set(inputNum)].length !== 6) {
-			throw new Error("[ERROR] 로또 번호는 중복되지 않는 숫자여야 합니다.");
-		}
-
-		inputNum.forEach(num => {
-			if (isNaN(num)) {
-				throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
-			}
-			if (Number(num) > 1 && Number(num) < 45) {
-				throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-			}
-		});
-	}
-
-	useBonusLottoNum() {
-		Console.print("보너스 번호를 입력해 주세요.");
-		Console.readline("", input => {
-			this.isBonusValidation(input);
-			this.BonusLottoNums = Number(input);
-		});
-	}
-
-	isBonusValidation(input) {
-		if (isNaN(input)) {
-			throw new Error("[ERROR] 보너스 번호에는 숫자만 입력 가능합니다.");
-		}
-		if (this.winLottoNums.includes(+input)) {
-			throw new Error(
-				"[ERROR] 보너스 번호와 로또 번호는 중복되지 않아야 합니다."
-			);
-		}
-		if (input.length !== 1) {
-			throw new Error("[ERROR] 보너스 번호는 하나만 입력 가능합니다.");
-		}
-	}
-
 	play() {
-		Console.print("구입금액을 입력해 주세요.");
+		let printLotto = new PrintLotto();
+		printLotto.inputPayment();
 	}
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
