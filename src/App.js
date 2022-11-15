a048491c9b62fdfb6360ba0cf2b35d7bc68c9d19
HelloHailie <alsltjsals@gmail.com>
2022-11-15 11:34:40 +0900

    feat:파일별 기능 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
+  //당첨 로또 번호
+  winLottoNum() {
+    MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.", (userInput) => {
+      validate(userInput);
+    });
+    MissionUtils.Console.print(userInput);
+    return userInput;
+    this.winBonusLottoNum();
+  }
+
+  //당첨 보너스 로또 번호
+  winBonusLottoNum() {
+    MissionUtils.Console.readLine(
+      "보너스 번호를 입력해 주세요.",
+      (userInput) => {
+        MissionUtils.Console.print(userInput);
+      }
+    );
+  }
+
+  userPaid() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (userInput) => {
+      if (userInput % 1000) {
+        throw new Error();
+      }
+      MissionUtils.Console.print(`${userInput / 1000}개를 구매했습니다.`);
+      const lottoNum = userInput / 1000;
+      this.userLottoNum();
+    });
+    return lottoNum;
+  }
+
+  userLottoNum() {
+    const userLottoNum = new Array(lottoNum);
+    while (userLottoNum.length < 6) {
+      const lottoNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      if (!userLottoNum.includes(lottoNum)) {
+        userLottoNum.push(lottoNum);
+      }
+    }
+    MissionUtils.Console.print(userLottoNum);
+    this.winLottoNum();
+    return userLottoNum;
+  }
   play() {}
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,3 @@
-const MissionUtils = require("@woowacourse/mission-utils");
 class Lotto {
   #numbers;
 
@@ -32,51 +31,8 @@ class Lotto {
     if (isNaN(numbers)) {
       throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
     }
-  }
 
-  //당첨 로또 번호
-  winLottoNum() {
-    MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.", (userInput) => {
-      validate(userInput);
-    });
-    MissionUtils.Console.print(userInput);
-    return userInput;
-    this.winBonusLottoNum();
-  }
-
-  //당첨 보너스 로또 번호
-  winBonusLottoNum() {
-    MissionUtils.Console.readLine(
-      "보너스 번호를 입력해 주세요.",
-      (userInput) => {
-        MissionUtils.Console.print(userInput);
-      }
-    );
-  }
-
-  userPaid() {
-    MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (userInput) => {
-      if (userInput % 1000) {
-        throw new Error();
-      }
-      MissionUtils.Console.print(`${userInput / 1000}개를 구매했습니다.`);
-      const lottoNum = userInput / 1000;
-      this.userLottoNum();
-    });
-    return lottoNum;
-  }
-
-  userLottoNum() {
-    const userLottoNum = new Array(lottoNum);
-    while (userLottoNum.length < 6) {
-      const lottoNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-      if (!userLottoNum.includes(lottoNum)) {
-        userLottoNum.push(lottoNum);
-      }
-    }
-    MissionUtils.Console.print(userLottoNum);
-    this.winLottoNum();
-    return userLottoNum;
+    return;
   }
 }
 
