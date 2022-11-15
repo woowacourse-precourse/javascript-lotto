28db99ab40f3fa16be85a06821e4a410c3e63455
afds4567 <afds4567@gmail.com>
2022-11-15 22:36:29 +0900

    test: 보너스 번호 중복 예외처리 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-lines-per-function */
 /* eslint-disable prettier/prettier */
 const Lotto = require('../src/Lotto');
+const WinLotto = require('../src/WinLotto');
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -19,6 +20,13 @@ describe('로또 클래스 테스트', () => {
   test('로또 번호에 숫자 이외의 값이 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 'f', 'g', 5]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('보너스 번호가 당첨 번호 6개와 중복되는 경우 예외가 발생한다', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 11, 12, 5]);
+      new WinLotto(2);
     }).toThrow('[ERROR]');
   });
   // 아래에 추가 테스트 작성 가능
