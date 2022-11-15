c88f1d672098b680c198503e911e22721970b819
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-15 11:33:51 +0900

    Test: 유터 테스트 배치 변경

--- __tests__/UserTest.js
+++ __tests__/UserTest.js
@@ -1,6 +1,16 @@
 const User = require('../src/User');
 
 describe('구입 금액 입력 테스트', () => {
+  test('유효한 큰 숫자 테스트', () => {
+    let user = new User(1000000000);
+    expect(user.getSeedMoney().toString()).toBe('1000000000');
+  });
+
+  test('유효한 큰 숫자 테스트2', () => {
+    let user = new User(99999999000);
+    expect(user.getSeedMoney().toString()).toBe('99999999000');
+  });
+  
   test('구입 금액은 1,000원 단위여야 한다.', () => {
     expect(() => {
     new User(1100);
@@ -35,15 +45,5 @@ describe('구입 금액 입력 테스트', () => {
     expect(() => {
       new User('10O00');
     }).toThrow('[ERROR]');
-  });
-
-  test('유효한 큰 숫자 테스트', () => {
-    let user = new User(1000000000);
-    expect(user.getSeedMoney().toString()).toBe('1000000000');
-  });
-
-  test('유효한 큰 숫자 테스트2', () => {
-    let user = new User(99999999000);
-    expect(user.getSeedMoney().toString()).toBe('99999999000');
   });
 });
