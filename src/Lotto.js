b1ae4001c3440f95694567a1dea8a27cebaf0111
Inae Choi <inaechoi89@gmail.com>
2022-11-15 13:55:22 +0900

    feat: handle exception of lotto numbers

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const Constant = require("./components/Constant");
+
 class Lotto {
   #numbers;
 
@@ -10,9 +12,45 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if (this.hasDuplicate(numbers)) {
+      throw new Error(Constant.DUPLICATE_ERROR);
+    }
+
+    if (this.isWrongRange(numbers)) {
+      throw new Error(Constant.WRONG_NUMBER_ERROR);
+    }
+
+    if (this.isNotNumber(numbers)) {
+      throw new Error(Constant.WRONG_NUMBER_ERROR);
+    }
   }
 
-  // TODO: 추가 기능 구현
+  hasDuplicate(numbers) {
+    return [...new Set(numbers)].length !== 6;
+  }
+
+  isWrongRange(numbers) {
+    return numbers.map((number) => this.checkRange(number));
+  }
+
+  checkRange(number) {
+    if (Number(number) < 1 || Number(number) > 45) {
+      return true;
+    }
+    return false;
+  }
+
+  isNotNumber(numbers) {
+    return numbers.map((number) => this.checkNumber(number));
+  }
+
+  checkNumber(number) {
+    if (!Number(number)) {
+      return true;
+    }
+    return false;
+  }
 }
 
 module.exports = Lotto;
