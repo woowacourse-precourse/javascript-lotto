760ab4ecb004f1ab6aec21f77d022c44ddcca436
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-15 11:58:04 +0900

    feat(Validation): 빈 배열을 검사하는 함수 구현

--- src/Validation.js
+++ src/Validation.js
@@ -2,26 +2,27 @@ const { Console } = require('@woowacourse/mission-utils');
 
 class Validation {
   static validate(numbers) {
-    if (numbers !== undefined) {
-      if (!Validation.itThatRightFormat(numbers)) {
-        Console.close();
-        throw new Error('[ERROR] 올바른 형식으로 입력해주세요.');
-      }
-      if (!Validation.isThatSix(numbers)) {
-        Console.close();
-        throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-      }
-      if (Validation.numberNet(numbers)) {
-        Console.close();
-        throw new Error('[ERROR] 1과 45사이의 숫자를 입력해주세요.');
-      }
-
-      if (Validation.isThatDuplicate(numbers)) {
-        Console.close();
-        throw new Error('[ERROR] 중복되는 숫자가 존재합니다.');
-      }
+    if (Validation.isThatEmpty(numbers)) {
+      Console.log('[ERROR] 값을 입력해주세요.')
+    }
+    if (!Validation.itThatRightFormat(numbers)) {
+      Console.close();
+      throw new Error('[ERROR] 올바른 형식으로 입력해주세요.');
+    }
+    if (!Validation.isThatSix(numbers)) {
+      Console.close();
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (Validation.numberNet(numbers)) {
+      Console.close();
+      throw new Error('[ERROR] 1과 45사이의 숫자를 입력해주세요.');
+    }
+    if (Validation.isThatDuplicate(numbers)) {
+      Console.close();
+      throw new Error('[ERROR] 중복되는 숫자가 존재합니다.');
     }
   }
+  
 
   static bonusValidate(numbers, number) {
     if (!Validation.itThatNumber(number)) {
@@ -36,6 +37,10 @@ class Validation {
       Console.close();
       throw new Error('[ERROR] 당첨 번호와 값이 중복됩니다.');
     }
+  }
+
+  static isThatEmpty(numbers) {
+    return numbers.length === 0;
   }
 
   static isThatSix(numbers) {
