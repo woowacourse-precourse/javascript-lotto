3230cd53be186d00c6703289d8bc77f3ebe4d0a0
cocoa <jwv0323@kau.kr>
2022-11-15 16:44:52 +0900

    test: Lotto 클래스 테스트 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -41,45 +41,40 @@ describe("로또 클래스 테스트", () => {
 
   // setBonus
   test("로또 번호에 보너스 번호를 추가한다.", () => {
-    const result = new Lotto([1, 2, 3, 4, 5, 6]);
-    result.setBonus(7);
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.setBonus(7);
 
-    expect(result.getLotto()).toContain(7);
+    expect(lotto.getLotto()).toContain(7);
   });
 
   // validateBonus
   test("보너스 번호에 문자가 있으면 예외가 발생한다.", () => {
     expect(() => {
-      const result = new Lotto([1, 2, 3, 4, 5, 6]);
-      result.setBonus("a");
+      new Lotto([1, 2, 3, 4, 5, 6]).setBonus("a");
     }).toThrow("[ERROR]");
   });
 
   test("보너스 번호에 특수문자가 있으면 예외가 발생한다.", () => {
     expect(() => {
-      const result = new Lotto([1, 2, 3, 4, 5, 6]);
-      result.setBonus("@");
+      new Lotto([1, 2, 3, 4, 5, 6]).setBonus("@");
     }).toThrow("[ERROR]");
   });
 
   test("보너스 번호가 1부터 45까지의 범위가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
-      const result = new Lotto([1, 2, 3, 4, 5, 6]);
-      result.setBonus(50);
+      new Lotto([1, 2, 3, 4, 5, 6]).setBonus(50);
     }).toThrow("[ERROR]");
   });
 
   test("보너스 번호는 당첨 번호와 중복되면 예외가 발생한다.", () => {
     expect(() => {
-      const result = new Lotto([1, 2, 3, 4, 5, 6]);
-      result.setBonus(6);
+      new Lotto([1, 2, 3, 4, 5, 6]).setBonus(6);
     }).toThrow("[ERROR]");
   });
 
   // getLotto
   test("로또 번호를 반환한다.", () => {
     const result = new Lotto([1, 2, 3, 4, 5, 6]).getLotto();
-
     expect(result).toEqual([1, 2, 3, 4, 5, 6]);
   });
 
