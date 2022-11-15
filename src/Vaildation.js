3f51d839004f06d92c63dff702156e14c03a23cd
ming0830 <minsun0830@naver.com>
2022-11-15 20:27:42 +0900

    feat : 보너스 번호 유효성 검사

---
+++ src/Vaildation.js
@@ -0,0 +1,24 @@
+class Vaildation{
+  constructor(number) {
+    this.isVaildBounsNumber(number);
+    this.number= number;
+  }
+  isVaildBounsNumber(number){
+
+    const numberPattern = /[^0-9]/g;
+    if(numberPattern.test(number)){
+      throw new Error("[ERROR] 숫자만 입력하세요.")
+    }
+
+    if(number.length !== 1)
+    throw new Error("[ERROR] 보너스번호는 한 개만 입력하세요.")
+    
+
+    if(number < 1 || number > 45){
+      throw new Error("[ERROR] 1~45 사이에 숫자만 입력하세요.")
+    }
+
+  }
+}
+
+module.exports = Vaildation;
