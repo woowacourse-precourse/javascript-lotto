fd0b8e314f63aff19b65a469b2d847e549e7802f
semnil5202 <semnil5202@gmail.com>
2022-11-15 12:58:00 +0900

    feat(Validation): 당첨 번호 및 발행한 로또 예외 처리 기능

--- src/components/Validation.js
+++ src/components/Validation.js
@@ -9,6 +9,23 @@ class Validation {
       throw new Error('[ERROR] 구입 금액은 숫자만 입력할 수 있습니다.');
     }
   }
+
+  static validateLotto(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
+    }
+    numbers.forEach((number) => {
+      if (number > 45 || number < 1) {
+        throw new Error('[ERROR] 로또 번호는 1에서 45사이 값이어야 합니다.');
+      }
+      if (Number.isNaN(number)) {
+        throw new Error('[ERROR] 로또 번호는 숫자만 가능합니다.');
+      }
+    });
+  }
 }
 
 module.exports = Validation;
