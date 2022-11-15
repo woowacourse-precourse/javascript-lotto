80cde049c642e2f0ab4aa7d77c8251c36a14476a
daal2quf <daal2quf@gmail.com>
2022-11-15 16:27:26 +0900

    feat(calculator): 총 상금 구하는 기능 추가
    add:
    profit: 당첨내역 객체를 받아오고 entries로 key와 value로 이루어진 배열로 변환 후 상금 누적값 구하는 reducer 함수 실행

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,13 +1,20 @@
-class Calculator {
+const { LOTTO_PRIZE } = require('./Constants');
 
+class Calculator {
   ofPurchaseLottoCount(moneyValue) {
     return Number(moneyValue / 1000);
   }
 
   matchNumberCount(winNumbers, userPurchaselotto) {
-    const matchNumbers = winNumbers.filter((number) => userPurchaselotto.includes(number));
+    const matchNumbers = winNumbers.filter(number => userPurchaselotto.includes(number));
     const matchCount = matchNumbers.length;
     return matchCount;
+  }
+
+  profit(lottoResultMap) {
+    return Object.entries(lottoResultMap).reduce((accemulateProfit, [grade, count]) => {
+      return accemulateProfit + LOTTO_PRIZE[grade] * count;
+    }, 0);
   }
 }
 
