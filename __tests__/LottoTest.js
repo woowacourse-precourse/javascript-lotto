9184a4a20812e39e033026ac527df866f65b9cc1
nalsae <nalsae0207@naver.com>
2022-11-15 22:13:54 +0900

    test: add tests for domain logic
    도메인 로직 관련 테스트 추가
    - 당첨 통계 저장 기능
    - 상금 계산 기능
    - 수익률 변환 기능

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -77,3 +77,106 @@ describe("보너스 번호 예외 테스트", () => {
     }).toThrow("[ERROR]");
   });
 });
+
+describe("당첨 통계 저장 기능 테스트", () => {
+  test("당첨 통계를 적절하게 저장한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const expected = {
+      3: 1,
+      4: 0,
+      5: 0,
+      6: 0,
+      "5andBonus": 0,
+    };
+
+    lotto.updateStatics(3, true);
+
+    expect(lotto.statics).toEqual(expected);
+  });
+
+  test("당첨 통계를 적절하게 저장한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const expected = {
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 1,
+      "5andBonus": 0,
+    };
+
+    lotto.updateStatics(6, false);
+
+    expect(lotto.statics).toEqual(expected);
+  });
+
+  test("당첨 통계를 적절하게 저장한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const expected = {
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      "5andBonus": 1,
+    };
+
+    lotto.updateStatics(5, true);
+
+    expect(lotto.statics).toEqual(expected);
+  });
+});
+
+describe("상금 계산 기능 테스트", () => {
+  test("정확한 상금을 계산하여 저장한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.statics = {
+      3: 1,
+      4: 1,
+      5: 0,
+      6: 0,
+      "5andBonus": 1,
+    };
+    const expected = 30055000;
+
+    Object.keys(lotto.statics).forEach((matchedCount) =>
+      lotto.addWinningPrice(matchedCount)
+    );
+
+    expect(lotto.totalPrice).toBe(expected);
+  });
+
+  test("정확한 상금을 계산하여 저장한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.statics = {
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 1,
+      "5andBonus": 1,
+    };
+    const expected = 2030000000;
+
+    Object.keys(lotto.statics).forEach((matchedCount) =>
+      lotto.addWinningPrice(matchedCount)
+    );
+
+    expect(lotto.totalPrice).toBe(expected);
+  });
+});
+
+describe("수익률 변환 기능 테스트", () => {
+  test("정확한 수익률을 반환한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const earnings = 500.78;
+    const expected = "500.8%";
+
+    expect(lotto.convertRate(earnings)).toBe(expected);
+  });
+
+  test("정확한 수익률을 반환한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const earnings = 30000.46;
+    const expected = "30,000.5%";
+
+    expect(lotto.convertRate(earnings)).toBe(expected);
+  });
+});
