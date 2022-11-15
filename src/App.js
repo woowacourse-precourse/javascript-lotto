6cf48fbbcff6313d34df7f4836925ed99b872f43
seungki <sysh9498@gmail.com>
2022-11-15 19:47:54 +0900

    feat(App): 로또 구매 금액 입력받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const { Console, Random } = MissionUtils;
+const Validation = require('./Validation');
+const Const = require('./Const');
 class App {
-  play() {}
+  constructor() {}
+  play() {
+    Console.readLine(Const.BUY_LOTTO_MESSAGE, price => {
+      Validation.checkInputPrice(price);
+    });
+  }
 }
 
 module.exports = App;
