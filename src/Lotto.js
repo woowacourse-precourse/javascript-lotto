2cfb71a1ee14b0b660294c39ca4a90c4e1646366
vjvl95 <vjvl95@naver.com>
2022-11-15 11:47:05 +0900

    refactor : 로또 정렬해서 반환해주는 함수 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class Lotto {
   #numbers;
 
@@ -8,11 +10,14 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  printLottoNumbers() {
+    this.#numbers.sort((a, b) => a - b);
+    return this.#numbers;
+  }
 }
 
 module.exports = Lotto;
