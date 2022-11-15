b845888702d8200fc48ec2185a0b1aef517b512f
Saans <92422813+saans808@users.noreply.github.com>
2022-11-15 20:41:40 +0900

    feat: 당첨번호 추첨

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 class App {
   play() {}
+
+  computerNumber() {
+    const computerRandomNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
+    computerRandomNumber.sort((a, b) => a - b);
+    const computerBonusNumber = Random.pickNumberInRange(1, 45);
+  }
 }
 
 module.exports = App;
