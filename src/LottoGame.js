2f525c22d679b81b9bfd242fbee1222791177a81
JiYoon Lee <wldbseja@daum.net>
2022-11-15 23:19:24 +0900

    feat:사용자가 입력한 금액만큼 로또를 만들어서 저장하는 기능

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -16,6 +16,17 @@ class LottoGame {
   getInputMoney() {
     return this.#inputMoney;
   }
+
+  // [x]사용자가 입력한 금액만큼 로또를 만들어서 저장하는 기능
+  setLottoGames() {
+    const numberLottos = this.#inputMoney / AMOUNT_STANDARD.ONE_THOUSAND_WON;
+
+    this.#lottoArray = [];
+    for (let i = 0; i < numberLottos; i++) {
+      const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      this.#lottoArray.push(new Lotto(lottoNumbers));
+    }
+  }
 }
 
 module.exports = LottoGame;
