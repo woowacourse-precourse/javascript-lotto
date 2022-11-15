4b79a2ec8bdee4479333633c68e6a5816e0111c9
woowoni <ewoowon.2@gmail.com>
2022-11-15 23:34:10 +0900

    당첨 번호 받기

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+const { ERROR_MESSAGE } = require('./Message');
 class Lotto {
   #numbers;
 
@@ -8,11 +9,62 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR_MESSAGE.NUMBERS_LENGTH);
+    }
+    const numberSet = new Set(numbers);
+    if (numberSet.size != 6)
+      throw new Error(ERROR_MESSAGE.DUPLICATION);
+
+    if (numbers.some(number => {
+      return isNaN(number)
+    })) {
+      throw new Error(ERROR_MESSAGE.ONLY_NUMBER)
+    }
+
+    if (numbers.some(number => {
+      return number < 1 || number > 45 || number === 0
+    })) {
+      throw new Error(ERROR_MESSAGE.NUMBER_RANGE)
     }
   }
 
-  // TODO: 추가 기능 구현
+  toString() {
+    return `[${this.#numbers.join(", ")}]`;
+  }
+
+  generateNumObject() {
+    const numObject = {};
+    for (let i = 1; i <= 45; i++) {
+      numObject[i] = false;
+    }
+    return numObject;
+  }
+
+  countingWin(winNumbers) {
+    let winCount = 0;
+    const numObject = this.generateNumObject();
+    this.#numbers.forEach((number) => {
+      numObject[number] = true;
+    });
+    winNumbers.forEach((number) => {
+      if (numObject[number]) winCount++;
+    });
+    return winCount;
+  }
+
+  rank(winNumbers, bonusNumber) {
+    const winCount = this.countingWin(winNumbers);
+    switch (winCount) {
+      case 3:
+        return 5;
+      case 4:
+        return 4;
+      case 5:
+        return this.#numbers.includes(bonusNumber) ? 2 : 3;
+      case 6:
+        return 1;
+    }
+  }
 }
 
 module.exports = Lotto;
