88890f6ed890fbc9de69fa5ee1400eb5d5f15277
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-11-15 21:05:15 +0900

    feat(App): add 로또 구입 금액 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { Console } = MissionUtils;
+
+const VendingMachine = require("./lotto/domain/VendingMachine");
+
 class App {
-  play() {}
+  constructor() {
+    this.vendingMachine = new VendingMachine();
+  }
+
+  play() {
+    Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
+      this.vendingMachine.setMoney(money);
+
+      return;
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
