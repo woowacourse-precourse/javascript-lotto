dae238b6b4438eb237c0d55f6c18e2cdf9ccd70f
hvvany <kjunhwan1998@gmail.com>
2022-11-15 22:58:06 +0900

    feat: 입력받은 로또 번호 중에서 중복된 번호가 있는지 확인하는 기능 구현하기

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,23 @@ class Lotto {
 
   constructor(numbers) {
     this.validate(numbers);
+    this.duplicate(numbers);
     this.#numbers = numbers;
   }
 
   validate(numbers) {
+    console.log(numbers);
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  duplicate(numbers) {
+    if (new Set(numbers).size < 6) {
+      throw new Error("[ERROR] 로또 번호 중에서 중복괸 번호가 있습니다.");
+    }
+  }
 }
 
+// TODO 추가기능
 module.exports = Lotto;
