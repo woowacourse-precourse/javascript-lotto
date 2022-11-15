3141eb0a3bd891e5a57076219c137669f8bb0cbf
Hchan2 <seongchan_@naver.com>
2022-11-15 20:46:45 +0900

    lotto의 number 값 전달

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require('./Lotto');
 const User = require('./User');
-const { BUY_LOTTO, WINNING_NUMBERS, BONUS_NUMBER } = require('../constant/constant');
+const { BUY_LOTTO, WINNING_NUMBERS, BONUS_NUMBER } = require('./constant/constant');
 const errorBuyHandling = require('./error/userInputHandling');
 const bonusNumberCheck = require('./error/bonusNumber');
+const lottoGame = require('./game/lottoGame');
 
 class App {
+  constructor() {
+    this.user = new User();
+  }
+
   play() {
     this.userBuyLotto();
-    this.userInputLottoNumbers();
-    this.userInputBonusNumber();
   }
 
   userBuyLotto() {
     MissionUtils.Console.readLine(BUY_LOTTO, (buyInput) => {
       errorBuyHandling(buyInput);
-      this.user = new User(buyInput);
-      this.user.lottoBuy();
+      this.user.lottoBuy(buyInput);
+      this.userInputLottoNumbers();
     });
   }
 
   userInputLottoNumbers() {
     MissionUtils.Console.readLine(WINNING_NUMBERS, (lottoNumber) => {
       this.lotto = new Lotto(lottoNumber);
-      this.lotto.validate(lottoNumber);
+      this.userInputBonusNumber(this.lotto.getNumber());
     });
   }
 
-  userInputBonusNumber() {
+  userInputBonusNumber(lottoNumber) {
     MissionUtils.Console.readLine(BONUS_NUMBER, (bonusNumber) => {
-      bonusNumberCheck(bonusNumber);
+      bonusNumberCheck(bonusNumber, lottoNumber);
+      this.user.saveBonusNumber(bonusNumber);
+      this.lottoGameStart(lottoNumber);
     });
   }
 
-  
+  lottoGameStart(lottoNumber) {
+    const numbers = lottoNumber;
+    const { lottoArray, bonusNumber, userBuyMoney } = this.user;
+    lottoGame(numbers, lottoArray, bonusNumber, userBuyMoney);
+  }
 }
 
 module.exports = App;
