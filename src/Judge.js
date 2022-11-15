5e661da8b73f5c87425225bfe03fd216067c7681
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-15 22:00:34 +0900

    feat(Judge) : 심판이 추첨인이 입력한 값이 숫자로만 이루어져있는지 확인하는 함수 isLottoInputNaN 구현

--- src/Judge.js
+++ src/Judge.js
@@ -1,13 +1,30 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { RANGE } = require("./Constant");
+
 class Judge {
   constructor() {}
 
-  isUserInputValidate(userInput) {
+  isUserInputValid(userInput) {
     if (userInput % 1000 !== 0) {
+      MissionUtils.Console.print(
+        "[ERROR] 로또 한 장은 1000원 입니다. 1000의 배수로만 입력해주세요."
+      );
       throw new Error(
         "[ERROR] 로또 한 장은 1000원 입니다. 1000의 배수로만 입력해주세요."
       );
     }
     return true;
+  }
+
+  isLottoInputNaN(lottoInput) {
+    for (let i = 0; i < lottoInput.length; i++) {
+      if (isNaN(lottoInput[i])) {
+        MissionUtils.Console.print(
+          "[ERROR] 당첨 번호는 숫자로 입력해야합니다."
+        );
+        throw new Error("[ERROR] 당첨 번호는 숫자로 입력해야합니다.");
+      }
+    }
   }
 }
 
