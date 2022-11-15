7558a888dc578676cf63153299871b8a8b731586
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-15 16:15:19 +0900

    feat(App): add validation function & divide lotto count

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 const { Random, Console } = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto.js");
+const Validation = require("./utils/Validation");
 const { MAX_LENGTH, MESSAGE } = require("./utils/Constants");
+
+const validation = new Validation();
 class App {
   #amount;
   #count;
 
+  countLotto(money) {
+    this.#count = money / 1000;
+    Console.print(`${this.#count}` + MESSAGE.USER_BOUGHT);
+  }
+
   inputPurchase() {
-    Console.readLine(MESSAGE.USER_ENTER_PURCHASE, (input) => {
-      this.#amount = input;
-      if (this.#amount % 1000 === 0) this.#count = input / 1000;
+    Console.readLine(MESSAGE.USER_ENTER_PURCHASE, (money) => {
+      this.#amount = money;
+      if (validation.isValidateMoney(money)) this.countLotto(money);
     });
   }
-  play() {}
+  play() {
+    this.inputPurchase();
+  }
 }
 
 module.exports = App;
