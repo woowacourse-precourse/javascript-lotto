9c3786a82097d2f242d2ead2a98711bb0dddfa0d
RooT_Kim <bdd3452@gmail.com>
2022-11-15 18:15:09 +0900

    추가: Bonus 클래스 getter 메소드, 예외처리 #bonus의 값을 반환하는 getter 메소드 생성 당첨 번호와 중복된 번호인지 확인하는 예외처리 로직 추가

--- src/Objects/Bonus.js
+++ src/Objects/Bonus.js
@@ -1,11 +1,14 @@
 const { Messages } = require('../Constants');
 
 class Bonus {
-	constructor(bonus) {
-		this.validate(bonus);
+	#bonus;
+
+	constructor(bonus, winningNumbers) {
+		this.validate(bonus, winningNumbers);
+		this.#bonus = bonus;
 	}
 
-	validate(bonus) {
+	validate(bonus, winningNumbers) {
 		if (typeof Number(bonus) !== 'number') {
 			throw new Error(Messages.ERROR.BONUS_NUMBER.NOT_NUMBER);
 		}
@@ -15,10 +18,17 @@ class Bonus {
 		if (bonus.length !== 1) {
 			throw new Error(Messages.ERROR.BONUS_NUMBER.NOT_ONE_NUMBER);
 		}
+		if (winningNumbers.includes(bonus)) {
+			throw new Error(Messages.ERROR.BONUS_NUMBER.DUPLICATED_NUMBER);
+		}
 	}
 
 	checkBelongTo(bonus) {
-		return bonus >= 1 && bonus <= 45;
+		return bonus < 1 && bonus > 45;
+	}
+
+	getters() {
+		return this.#bonus;
 	}
 }
 
