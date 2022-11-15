ef5e59ea13123a2f28cb7a34243a75d928069ba2
spsp8426 <spsp8426@naver.com>
2022-11-15 09:38:07 +0900

    feat : 구매 갯수 출력

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
 class App {
   constructor() {
     this.money = null;
@@ -6,6 +7,7 @@ class App {
     this.winNum = [];
     this.bonusNum = null;
   }
+
   setMoney() {
     Console.readLine("구입금액을 입력해 주세요.\n",(num) => {
       if (!/^\d+$/.test(num))
@@ -14,9 +16,20 @@ class App {
       if(money % 1000 !== 0)
         throw new Error("[ERROR] 구입금액 오류");
         this.money = money;
-    })
+        this.buy();
+    });
   }
-
+  buy() {
+    let getMoney = this.money
+    let count = parseInt(this.money / 1000);
+    while (getMoney > 0) {
+      getMoney -= 1000;
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+      this.lotto.push(new Lotto(numbers));
+    }
+    Console.print("\n"+ count + "개를 구매했습니다.");
+  }
+ 
   play() {
     this.setMoney();
   }
