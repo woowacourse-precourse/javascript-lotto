e68cd2866a4f8d631bcdc3648bea14e4a7e790e6
1 <wrasf175@naver.com>
2022-11-15 19:03:13 +0900

    feat(App): 사용자가 잘못된 금액을 입력시 예외 처리

--- src/Budget.js
+++ src/Budget.js
@@ -2,9 +2,19 @@ class Budget {
   #budget;
 
   constructor(budget) {
+    this.validate(budget);
     this.#budget = budget;
   }
-  
+
+  validate(budget) {
+    if (!budget.isInteger) {
+      throw new Error("[ERROR] 금액은 정수 값이어야 합니다.");
+    }
+    if (budget%1000!==0) {
+      throw new Error("[ERROR] 금액은 1000원 단위야만 합니다.");
+    }
+  }
+
 }
 
 module.exports = Budget;
