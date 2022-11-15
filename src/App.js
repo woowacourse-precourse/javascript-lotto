efc84ef4aa3f5f4378a60ef50b1d29d59866ab3a
Choejeonghun <saa00123@naver.com>
2022-11-15 19:59:29 +0900

    refactor: 코드 수정을 위한 초기화

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-const { Console, Random } = require('@woowacourse/mission-utils');
-const Lotto = require('./Lotto');
-
 class App {
-  constructor() {
-    let PurchaseAmount = 0;
-  }
-
-  play() {
-    this.PurchaseLotto();
-  }
-
-  PurchaseLotto() {
-    Console.readLine('구입금액을 입력해 주세요.', (amount) => {
-      this.PurchaseAmount = amount;
-      this.ValidatePurchaseAmount();
-      Console.print(`${this.PurchaseAmount / 1000}개를 구매했습니다.`);
-    });
-  }
-
-  ValidatePurchaseAmount() {
-    if (this.PurchaseAmount % 1000 !== 0) {
-      throw '[ERROR] 로또 구입 금액이 1000원 단위가 아닙니다.';
-    }
-  }
+  play() {}
 }
 
 module.exports = App;
