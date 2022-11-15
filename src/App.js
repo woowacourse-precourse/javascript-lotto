c3aa4e6ccdef65ee9b7c50b98dc2e8c430009f29
rayark1 <max980203@snu.ac.kr>
2022-11-15 15:15:33 +0900

    feat(src/App.js): 로또 구입을 시작할 purchaseTicket 함수, callback 함수에 들어갈 manageInputPurchaseAmout 함수 구현

--- src/App.js
+++ src/App.js
@@ -3,28 +3,37 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {}
 
+  purchaseTicket() {
+    this.getInputPurchaseAmount(this.manageInputPurchaseAmount.bind(this));
+  }
+
+  manageInputPurchaseAmount(price) {
+    const numberOfTickets = this.calculateNumberOfTickets(price);
+    MissionUtils.Console.print(`${numberOfTickets}개를 구매했습니다.`);
+    // this.generateTickets(numberOfTickets);
+  }
+
   getInputPurchaseAmount(callback) {
     MissionUtils.Console.readLine("구매금액을 입력해 주세요.", callback);
   }
 
   calculateNumberOfTickets(price) {
     this.validateInputPurchase(price);
-    const numberOfTickets = Math.floor(price / 1000);
+    const numberOfTickets = price / 1000;
     return numberOfTickets;
   }
 
   validateInputPurchase(price) {
     if (isNaN(price)) {
-      throw new Error(PURCHASE_AMOUNT_ERROR_MESSAGE2);
+      throw new Error(PURCHASE_AMOUNT_ERROR_MESSAGE);
     }
     if (price % 1000 !== 0) {
-      throw new Error(PURCHASE_AMOUNT_ERROR_MESSAGE);
+      throw new Error(PURCHASE_AMOUNT_ERROR_MESSAGE2);
     }
     if (price < 0) {
       throw new Error(PURCHASE_AMOUNT_ERROR_MESSAGE3);
     }
   }
-  
 }
 
 module.exports = App;
