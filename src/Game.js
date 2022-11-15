31d81320ba95e4f35920a2273db28189b7b404c0
Yeonju Jo <yjj0287@gmail.com>
2022-11-15 22:35:39 +0900

    feat: 발행한 로또 수량 및 번호 출력 함수 추가

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,13 @@
 "use strict";
 
 const { Notice } = require("./Constants");
-const { validateUserMoney } = require("./Utils");
+const { validateUserMoney, sortAscending } = require("./Utils");
 const MissionUtils = require("@woowacourse/mission-utils");
-const [Console] = [MissionUtils.Console, MissionUtils.Random];
+const [Console, Random] = [MissionUtils.Console, MissionUtils.Random];
 
 class Game {
   #money;
+  #tickets;
 
   constructor() {
     this.inputMoney();
@@ -19,6 +20,19 @@ class Game {
       Console.print(`${userMoney / 1000}${Notice.amounts}`);
       return this.randomPurchase(userMoney / 1000);
     });
+  }
+
+  randomPurchase(amounts) {
+    const tickets = [];
+    for (let i = 0; i < amounts; i++) {
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      sortAscending(numbers);
+      Console.print("[" + numbers.join(", ") + "]");
+      tickets.push(numbers);
+    }
+    this.#tickets = tickets;
+
+    return;
   }
 }
 
