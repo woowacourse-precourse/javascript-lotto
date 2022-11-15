4a85c9078b689e4a20f4eb1193ebec635aebe7ec
김아현 <dhflxhdxhd@gmail.com>
2022-11-15 20:08:21 +0900

    Test : 입력한 로또 번호의 개수 테스트 코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,15 @@
+const App = require("../src/App");
 const Lotto = require("../src/Lotto");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -13,4 +24,12 @@ describe("로또 클래스 테스트", () => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR] 입력값에 중복이 있습니다.");
   });
+
+  test("입력한 로또 번호의 개수가 6개가 아니면 예외 발생",()=>{
+    mockQuestions(["3000","1,2,3,4,5"]);
+    expect(()=>{
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR] 로또 번호는 6개여야 합니다.");
+  })
 });
