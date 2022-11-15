0e760dc2eb673bef2ff1c4d24c3aae8228fa4259
Minju Park <project.mizzu@gmail.com>
2022-11-15 17:59:00 +0900

    Feat : 당첨 번호 입력 에러 기능 추가
    - 쉼표(,)로 구분된 문자열이 아닌 경우
    - str.split(",").length가 6이 아닌 경우
    - str.split(",")의 각 인덱스가 숫자가 아닌 경우
    - 입력한 숫자 6개 중 중복되는 숫자가 있는 경우
    - 입력한 숫자가 1 ~ 45 범위 외인 경우

--- src/App.js
+++ src/App.js
@@ -10,24 +10,12 @@ class App {
 
   play() {
     this.lotto.getPurchaseAmount();
-    /* Console.readLine(this.text, (cost) => {
-      let quantity = cost / 1000;
-      const result = [];
-      Console.print(`${'\n'}${quantity}개를 구매했습니다.`);
-      let computer = [];
-      while (quantity >= 1) {
-        while (computer.length < 6) {
-          const number = Random.pickNumberInRange(1, 45);
-          if (!computer.includes(number)) {
-            computer.push(number);
-          }
-        }
+    /* 
         Console.print(computer.sort((a, b) => a - b));
         result.push(computer.sort((a, b) => a - b));
         computer = [];
         quantity -= 1;
       }
-      Console.readLine(`${'\n'}당첨 번호를 입력해 주세요.${'\n'}`, (number) => {
         const userNumbers = [];
         const userBonus = [];
         userNumbers.push(...number.split(','));
--- src/Lotto.js
+++ src/Lotto.js
@@ -3,15 +3,19 @@ const { Console, Random } = require('@woowacourse/mission-utils');
 class Lotto {
   #numbers;
 
-  // constructor(numbers) {
-  //   this.validate(numbers);
-  //   this.#numbers = numbers;
-  // }
+  constructor(numbers) {
+    // this.validate(numbers);
+    this.#numbers = numbers;
+  }
 
   // validate(numbers) {
-  //   if (numbers.length !== 6) {
-  //     throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-  //   }
+  //   // // if (numbers.length !== 6) {
+  //   // //   throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+  //   // // }
+  //   // if (!numbers.includes(',')) {
+  //   //   throw new Error('[ERROR] 쉼표로 구분한 하나의 문자열을 입력해주세요');
+  //   // }
+  //   Console.print(numbers.length);
   // }
 
   // TODO: 추가 기능 구현
@@ -26,12 +30,20 @@ class Lotto {
     if (cost < 0) {
       throw new Error('[ERROR] 1,000원 이상의 올바른 금액을 입력해주세요.');
     }
+    this.getLottoQuantity(cost);
   }
 
   getPurchaseAmount() {
     Console.readLine(`구입금액을 입력해 주세요.${'\n'}`, (cost) => {
       this.checkPurchaseAmount(cost);
     });
+  }
+
+  getLottoQuantity(cost) {
+    const quantity = cost / 1000;
+    Console.print(`${'\n'}${quantity}개를 구매했습니다.`);
+    const randomNumbers = this.getRandomNumbers(quantity);
+    this.showRandomNumbers(randomNumbers);
   }
 
   getRandomNumbers(quantity) {
@@ -44,9 +56,42 @@ class Lotto {
     }
     return result;
   }
+
+  showRandomNumbers(randomNumbers) {
+    randomNumbers.forEach((arr) => Console.print(arr));
+    this.getUserNumber();
+  }
+
+  checkUserNumber(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (numbers.includes(NaN)) {
+      throw new Error('[ERROR] 숫자만 입력해주세요.');
+    }
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 중복된 숫자는 입력할 수 없습니다.');
+    }
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error('[ERROR] 1 ~ 45 사이의 숫자만 입력 가능합니다.');
+      }
+    });
+  }
+
+  getUserNumber() {
+    Console.readLine(`${'\n'}당첨 번호를 입력해 주세요.${'\n'}`, (numbers) => {
+      if (!numbers.includes(',')) {
+        throw new Error('[ERROR] 쉼표로 구분한 하나의 문자열을 입력해주세요.');
+      }
+      this.#numbers = numbers.split(',').map(Number);
+      Console.print(this.#numbers);
+      this.checkUserNumber(this.#numbers);
+    });
+  }
 }
 
 const test = new Lotto();
-console.log(test.getRandomNumbers());
+test.getPurchaseAmount();
 
 module.exports = Lotto;
