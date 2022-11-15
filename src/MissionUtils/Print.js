ffc0b638e827ceca0e2a8c5448de75773a905dbd
RooT_Kim <bdd3452@gmail.com>
2022-11-15 18:50:13 +0900

    기능: 출력 메소드 추가 구매한 로또의 갯수를 출력하는 printCount 함수 구현 로또 번호를 모두 출력하는 printLottos 함수 구현 결과 메시지를 출력하는 printResults 함수 구현 총 수익률을 출력하는 printProfitPercent 함수 구현

--- src/MissionUtils/Print.js
+++ src/MissionUtils/Print.js
@@ -1,2 +1,32 @@
 const { Console } = require('@woowacourse/mission-utils');
-const { Messages } = require('../Constants');
+
+const printCount = (count) => {
+	Console.print(`${count}개를 구매했습니다.`);
+};
+
+const printLottos = (lottos) => {
+	lottos.forEach((lotto) => {
+		Console.print(`[${lotto.join(', ')}]`);
+	});
+};
+
+const printResults = (result) => {
+	Console.print('당첨 통계\n---');
+	Console.print(`3개 일치 (5,000원) - ${result[3]}개
+4개 일치 (50,000원) - ${result[4]}개
+5개 일치 (1,500,000원) - ${result[5]}개
+5개 일치, 보너스 볼 일치 (30,000,000원) - ${result.bonus}개
+6개 일치 (2,000,000,000원) - ${result[6]}개
+	`);
+};
+
+const printProfitPercent = (profitPercent) => {
+	Console.print(`총 수익률은 ${profitPercent}%입니다.`);
+};
+
+module.exports = {
+	printCount,
+	printLottos,
+	printResults,
+	printProfitPercent,
+};
