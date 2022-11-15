fda3508c449f568ff49c275f696bbed1b97cca62
semnil5202 <semnil5202@gmail.com>
2022-11-15 22:44:54 +0900

    test(IssueLottoTest): 파일명 수정
    기존에 IssueLotto 파일명으로 잘못 생성하여 IssueLottoTest로 수정한다.

--- __tests__/IssueLottoTest.js
+++ __tests__/IssueLottoTest.js
@@ -3,18 +3,18 @@ const IssueLotto = require('../src/components/IssueLotto');
 describe('로또 발행 클래스 테스트', () => {
   test('구입 금액이 1000원 단위가 아닐 경우 예외가 발생한다.', () => {
     expect(() => {
-      IssueLotto.setLotteryNumber(8700);
+      IssueLotto.setLottoNumber(8700);
     }).toThrow('[ERROR]');
   });
 
   test('구입 금액이 숫자가 아닌 값으로 받을 경우 예외가 발생한다.', () => {
     expect(() => {
-      IssueLotto.setLotteryNumber('a800b');
+      IssueLotto.setLottoNumber('a800b');
     }).toThrow('[ERROR]');
   });
 
   test('구입한 금액만큼 로또를 발행하는지 확인한다.', () => {
-    const lottos = IssueLotto.setLotteryNumber(10000);
+    const lottos = IssueLotto.setLottoNumber(10000);
     expect(lottos[0].length).toBe(10);
   });
 });
