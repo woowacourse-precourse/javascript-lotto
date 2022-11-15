5de486a62cdc12e1324d277717031d9a494a2e13
runkey <csi9509@naver.com>
2022-11-15 21:56:56 +0900

    Feat: 초기 셋팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+const { GAME, MESSAGE, PRIZE, ERROR } = require('./Message');
+const Lotto = require('./Lotto');
+
 class App {
-  play() {}
+  constructor() {
+    this.printedLottos = []; // 내 로또 번호
+    this.amount = 0; // 구입 금액
+    this.winNumbers = []; // 당첨 로또 번호
+    this.bonusNumber = 0; // 보너스 번호
+    this.score = { // 등수
+      first: 0,
+      second: 0,
+      third: 0,
+      fourth: 0,
+      fifth: 0,
+    };
+    this.prize = 0; // 상금
+    this.return = 0; // 수익률
+  }
+
+  play() {
+    this.printLotto();
+  }
+
+  printLotto() {
+    Console.readLine(MESSAGE.BUY, (amount) => { // 로또 구입 금액 입력
+      this.validateAmount(amount);
+      this.amount = Number(amount);
+
+      const countingLotto = this.amount / GAME.PRICE; // 로또 갯수
+
+      for (let i = 0; i < countingLotto; i++) {
+        const lotto = new Lotto(this.makingLotto());
+        this.printedLottos.push(lotto);
+      }
+
+      this.printLotto(countingLotto);
+      this.getWinNumber();
+    });
+  }
+
+
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
