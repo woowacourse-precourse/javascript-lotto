68b57f4d442b9e4951f3202da573ffaa4806ad9c
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-11-15 20:10:21 +0900

    feat(lotto): add 로또결과 등급 산출 기능

--- src/lotto/domain/LottoChecker.js
+++ src/lotto/domain/LottoChecker.js
@@ -13,7 +13,24 @@ class LottoChecker {
       });
     });
 
-    return;
+    return this.countWinningLotto(correctLottoPlus);
+  }
+
+  countWinningLotto(correctLottoPlus) {
+    let countLottoReward = [0, 0, 0, 0, 0];
+
+    for (let i = 0; i < correctLottoPlus.length; i++) {
+      const { correctLotto, correctBonus } = correctLottoPlus[i];
+
+      if (correctLotto.length === 3) countLottoReward[0] += 1;
+      else if (correctLotto.length === 4) countLottoReward[1] += 1;
+      else if (correctLotto.length === 5 && correctBonus === true)
+        countLottoReward[2] += 1;
+      else if (correctLotto.length === 5) countLottoReward[3] += 1;
+      else if (correctLotto.length === 6) countLottoReward[4] += 1;
+    }
+
+    return countLottoReward;
   }
 }
 
