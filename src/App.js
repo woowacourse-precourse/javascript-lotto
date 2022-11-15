81bcee022a1bcd465be4bf119a98fd9fb3b76267
sujungYu <su_moment@naver.com>
2022-11-15 14:12:02 +0900

    refactor(App) : 변수명, 메서드명 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const Lotto = require('./Lotto');
 const CompareLotto = require('./CompareLotto');
 const Bonus = require('./Bonus');
 class App {
-  #winning;
+  #winningSet;
   #bonus;
   #lottoSet;
   #money;
@@ -13,24 +13,24 @@ class App {
     this.#lottoSet = [];
   }
 
-  getMoney() {
+  getPurchaseAmount() {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
       this.#money = Number(money);
       MissionUtils.Console.print('');
-      this.buyLotto(money);
+      this.getLottoSet(money);
     });
   }
 
-  buyLotto(money) {
+  getLottoSet(money) {
     const lottoSet = new LottoSet(money);
     this.#lottoSet = lottoSet.play();
-    this.getWinnings();
+    this.getWinningSet();
   }
 
-  getWinnings() {
+  getWinningSet() {
     MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.\n', (winning) => {
-      this.#winning = winning.split(',').map(Number);
-      new Lotto(this.#winning);
+      this.#winningSet = winning.split(',').map(Number);
+      new Lotto(this.#winningSet);
 
       MissionUtils.Console.print('');
       this.getBonus(winning);
@@ -49,11 +49,11 @@ class App {
 
   compareLotto() {
     const compareLotto = new CompareLotto();
-    compareLotto.play(this.#lottoSet, this.#money, this.#winning, this.#bonus);
+    compareLotto.play(this.#lottoSet, this.#money, this.#winningSet, this.#bonus);
   }
 
   play() {
-    this.getMoney();
+    this.getPurchaseAmount();
   }
 }
 
