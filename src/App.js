005824b12290bffd177b35e035f622366ef21791
hvvany <kjunhwan1998@gmail.com>
2022-11-15 22:50:45 +0900

    feat: 사용자가 1000원 단위로 금액을 입력했는지 확인하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ class App {
   getPurchaseAmount() {
     Console.print('구매금액을 입력해 주세요.');
     Console.readLine('', (userInput) => {
-      console.log(userInput);
+      this.checkPurchaseAmount(userInput);
     })
   }
-}
 
+  checkPurchaseAmount(userInput) {
+    if(userInput % 1000 !== 0) {
+      throw '[ERROR] 1000원 단위로 금액을 입력하지 않았습니다.'
+    }
+    console.log('통과');
+  }
+}
 const app = new App();
 app.play();
 
