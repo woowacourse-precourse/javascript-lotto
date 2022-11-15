b153946271fc021c23e820800644bf4ace813657
BaeDongWoo <ehddn5410@naver.com>
2022-11-15 13:24:43 +0900

    bonusNumber 삭제

--- src/Bonus.js
+++ src/Bonus.js
@@ -1,23 +1,15 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const Lotto=require("./Lotto");
+
 
 class Bonus{
-  constructor(){
-    this.winingNumbers=Lotto.createWiningNumber();
-  }
-  bonusNumber(){
-    MissionUtils.Console.readLine('보너스 번호를 입력해 주세요',(bonus)=>{
-      if(this.bonusNumberValidate(this.winingNumbers,bonus)) return bonus;
-    });
-  }
-  bonusNumberValidate(winingNumbers,bonus){
-    if(winingNumbers.includes(bonus)){
+  bonusNumberValidate(getWining,getbonus){
+    if(getWining.includes(getbonus)){
       throw new Error("[ERROR] 보너스 번호가 잘못입력 되었습니다.");
     }
-    if(bonus.length!==1){
+    if(isNaN(getbonus)){
       throw new Error("[ERROR] 보너스 번호가 잘못입력 되었습니다.");
     }
-    if(bonus<0 && bonus>45){
+    if(getbonus<0 && getbonus>45){
       throw new Error("[ERROR] 보너스 번호가 잘못입력 되었습니다.");
     }
     return true;
