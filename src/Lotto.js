56096b115bef994f6112ddaea3fd144f91ef6e9f
BaeDongWoo <ehddn5410@naver.com>
2022-11-15 13:24:13 +0900

    createWiningNumber삭제

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,20 +1,12 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-import App from "./App";
 class Lotto {
   #numbers;
-
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
   }
-  createWiningNumber(){
-    MissionUtils.Console.readLine('당첨번호를 입력해 주세요',(wining)=>{
-      const winingNumbers=wining.split(',');
-      if (this.validate(winingNumbers)) bonusNumber(winingNumbers);  
-    });
-  }
   validate(numbers) {
-    if (numbers.length !== 6) {
+    if (numbers.length!== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
     if(new Set(numbers).size!==6){
@@ -27,8 +19,6 @@ class Lotto {
     }
     return true;
   }
-  
-
   // TODO: 추가 기능 구현
 }
 
