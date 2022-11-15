ef646f605fa46bd050121a18464cabe8bc9530ab
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-15 22:48:15 +0900

    feat(Clerk) : 등수별 당첨 결과를 console에 출력하는 함수 showPlace 구현

--- src/Clerk.js
+++ src/Clerk.js
@@ -45,6 +45,14 @@ class Clerk {
       this.#place[4] * 2000000000
     );
   }
+
+  showPlace() {
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${this.#place[0]}개
+4개 일치 (50,000원) - ${this.#place[1]}개
+5개 일치 (1,500,000원) - ${this.#place[2]}개
+5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.#place[3]}개
+6개 일치 (5,000원) - ${this.#place[4]}개`);
+  }
 }
 
 module.exports = Clerk;
