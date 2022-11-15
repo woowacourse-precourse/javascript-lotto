83475778605427845f1c05d8a3cb5e62a65c29a9
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-15 20:55:39 +0900

    test(user): test for buy lotto & calculate revenue

--- __tests__/UserTest.js
+++ __tests__/UserTest.js
@@ -1,9 +1,28 @@
 const User = require('../src/User');
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
 
 describe('유저 테스트', () => {
   test('돈으로 로또사기 6000원', () => {
     const user = new User();
     const lottos = user.buyLottos(6000);
     expect(lottos.length).toBe(6);
+  });
+
+  test('수익률 계산하기', () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+    ]);
+    const user = new User();
+    user.buyLottos(2000);
+    user.checkLottos([8, 21, 23, 3, 5, 11], 44);
+    expect(user.checkRateOfReturn()).toBe('500.0');
   });
 });
