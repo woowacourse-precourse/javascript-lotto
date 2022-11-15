4eb53b7476578c979db0c95f0c3ab72c9c6e9b85
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-15 22:32:51 +0900

    feat: 로또 구입 함수

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,11 +1,25 @@
-const { QUERY } = require("../constants");
+const { Random } = require("@woowacourse/mission-utils");
+const { LOTTO, QUERY, WINNING_PRICE_MAP, ERROR_MESSAGE } = require("../constants");
+const Payment = require("../model/Payment");
+const QuickPick = require("../model/QuickPick");
 
 class Controller {
   view;
+  paymentModel;
 
   constructor(view) {
     this.view = view;
+    this.buyLotto();
+  }
+
+  buyLotto() {
+    this.view.readLine(QUERY.BUY, (payment) => {
+      const lottoCount = this.calcLottoCount(payment);
+      this.paymentModel = new Payment(payment);
+      this.view.printLottoCount(lottoCount);
+    });
   }
 }
 
 module.exports = Controller;
+
