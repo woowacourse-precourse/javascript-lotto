e9ced90abcdf9614c29595b188b2fe7bffdfc00c
daal2quf <daal2quf@gmail.com>
2022-11-15 21:31:18 +0900

    refactor(printer): 각 파일의 출력을 담당하는 메서드 하나로 모음
    add:
    유저와의 소통하는 입출력 부분을 App.js 라는 파일에서 유일하게 처리하기 위해 분리
    purchaseCount(): 로또 구매 개수 출력
    purchaseLottoBundle() : 구매 개수 만큼의 로또 번호 출력
    lottoResult(): 당첨 결과 출력

--- src/Printer.js
+++ src/Printer.js
@@ -1,9 +1,28 @@
-const { Console } = require("@woowacourse/mission-utils");
-const App = require("./App");
+const { Console } = require('@woowacourse/mission-utils');
+const { LOTTO_RESULT_MSG } = require('./Constants');
 
 class Printer {
-  theNumberOfLotto(purchaseLottoCount) {
-    Console.print(`${purchaseLottoCount}개 구매하셨습니다.`);
+  purchaseLottoBundle(bundleOfLotto) {
+    bundleOfLotto.forEach(lottoNums => Console.print(`[${lottoNums.join(', ')}]`));
+  }
+
+  purchaseCount(count) {
+    Console.print(count);
+  }
+
+  purchaseLottoBundle(lottoBundle) {
+    lottoBundle.forEach(lottoNums => Console.print(`[${lottoNums.join(', ')}]`));
+  }
+
+  lottoResult(resultMap) {
+    const { fifthGrade, forthGrade, thirdGrade, secondGrade, firstGrade, profitRate } = resultMap;
+    Console.print(LOTTO_RESULT_MSG.WINNING_STATISTICS);
+    Console.print(LOTTO_RESULT_MSG.FIFTH_GRADE(fifthGrade));
+    Console.print(LOTTO_RESULT_MSG.FORTH_GRADE(forthGrade));
+    Console.print(LOTTO_RESULT_MSG.THIRTH_GRADE(thirdGrade));
+    Console.print(LOTTO_RESULT_MSG.SECOND_GRADE(secondGrade));
+    Console.print(LOTTO_RESULT_MSG.FIRST_GRADE(firstGrade));
+    Console.print(LOTTO_RESULT_MSG.PROFIT_RATE(profitRate));
   }
 }
 
