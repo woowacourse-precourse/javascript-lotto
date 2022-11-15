202fb2a92d3a784c46290e785392a75fc1cdca1d
khkh0109 <ist200120@gmail.com>
2022-11-15 22:35:54 +0900

    style(LottoGenerator): 상수 추가

--- src/LottoGenerator.js
+++ src/LottoGenerator.js
@@ -4,7 +4,14 @@ class LottoGenerator {
   createLottoNumbers(lottoCount) {
     const lottoArr = [];
     while (lottoArr.length < lottoCount) {
-      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      const RANGE_START = 1;
+      const RANGE_END = 45;
+      const LOTTO_LENGTH = 6;
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(
+        RANGE_START,
+        RANGE_END,
+        LOTTO_LENGTH
+      );
       numbers.sort((a, b) => a - b);
       lottoArr.push(numbers);
     }
