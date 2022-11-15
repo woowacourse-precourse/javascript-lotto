27aea038415d710dc79720cd4d16e42468ddc6ad
s0ojin <onepst@hanyang.ac.kr>
2022-11-15 22:47:53 +0900

    feat(User.js) : 수익률을 계산하는 calculateReturnRate 메서드 작성

--- src/User.js
+++ src/User.js
@@ -2,8 +2,7 @@ const Validator = require('./utils/Validator');
 class User {
   #amount;
 
-  constructor(amount) {
-    this.#amount = amount;
+  constructor() {
     this.Validator = new Validator();
   }
 
@@ -12,8 +11,28 @@ class User {
       this.Validator.isNumber(amount) !== Error &&
       this.Validator.isUnitOfThousnds(amount) !== Error
     ) {
+      this.#amount = amount;
       return true;
     }
+  }
+
+  calculateReturnRate(result) {
+    const LotteryAmount = {
+      0: 5000,
+      1: 50000,
+      2: 1500000,
+      3: 30000000,
+      4: 2000000000,
+    };
+
+    const LotteryAmountSum = result.reduce(
+      (acc, cur, index) => acc + LotteryAmount[index] * cur,
+      0,
+    );
+
+    const returnRate = (LotteryAmountSum / this.#amount) * 100;
+
+    return returnRate.toFixed(1);
   }
 }
 
