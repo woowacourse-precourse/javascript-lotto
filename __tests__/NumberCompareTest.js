8b60b4ec871002b288c038bafc99f483e944885e
jinseo <ghkdwlstj99@gmail.com>
2022-11-15 16:08:47 +0900

    test: 번호 비교에 대한 Test코드 추가

---
+++ __tests__/NumberCompareTest.js
@@ -0,0 +1,37 @@
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+
+const mockQuestions = (answers) => {
+    MissionUtils.Console.readLine = jest.fn();
+    answers.reduce((acc, input) => {
+      return acc.mockImplementationOnce((question, callback) => {
+        callback(input);
+      });
+    }, MissionUtils.Console.readLine);
+  };
+
+const mockRandoms = (numbers) => {
+    MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+    numbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, MissionUtils.Random.pickUniqueNumbersInRange);
+  };
+
+  describe("로또비교 테스트", () => {
+  test("당첨번호와 보너스번호 예외 테스트", () => {
+    mockRandoms([
+        [8, 21, 23, 41, 42, 43],
+        [3, 5, 11, 16, 32, 38],
+        [7, 11, 16, 35, 36, 44],
+        [1, 8, 11, 31, 41, 42],
+        [13, 14, 16, 38, 42, 45],
+        [7, 11, 30, 40, 42, 43],
+    ])
+    mockQuestions(["6000" , "1,15,23,31,39,41" , "15"]);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR]");
+  });
+});
