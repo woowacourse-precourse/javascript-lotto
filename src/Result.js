ad168243546aff581b32480dc5553d24806ec270
imb96 <kimminje7267@naver.com>
2022-11-15 15:50:29 +0900

    refactor : 결과도출 기능을 계산 기능, 출력 기능으로  분리

--- src/Result.js
+++ src/Result.js
@@ -3,9 +3,9 @@ const { PRIZE, RANK } = require("./constant");
 class Result {
   constructor(myMoney) {
     this.myMoney = myMoney;
+    this.collectCount = [0, 0, 0, 0, 0];
   }
   statistics(bundle, numbers, bonus) {
-    const collectCount = [0, 0, 0, 0, 0];
     for (let i = 0; i < bundle.length; i++) {
       let count = 0;
       let bonusCount = 0;
@@ -17,41 +17,49 @@ class Result {
           count += 1;
         }
       }
-      if (count === 3) {
-        collectCount[0]++;
-      }
-      if (count === 4) {
-        collectCount[1]++;
-      }
-      if (count === 5 && bonusCount !== 1) {
-        collectCount[2]++;
-      }
-      if (count === 5 && bonusCount === 1) {
-        collectCount[3]++;
-      }
-      if (count === 6) {
-        collectCount[4]++;
-      }
+      this.setCount(count, bonusCount);
     }
-    this.printResult(collectCount);
+    this.calculationResult();
   }
 
-  printResult(collectCount) {
+  setCount(count, bonusCount) {
+    if (count === 3) {
+      this.collectCount[0]++;
+    }
+    if (count === 4) {
+      this.collectCount[1]++;
+    }
+    if (count === 5 && bonusCount !== 1) {
+      this.collectCount[2]++;
+    }
+    if (count === 5 && bonusCount === 1) {
+      this.collectCount[3]++;
+    }
+    if (count === 6) {
+      this.collectCount[4]++;
+    }
+  }
+
+  calculationResult() {
     const LottoMoney =
-      collectCount[0] * PRIZE.FIFTH +
-      collectCount[1] * PRIZE.FOURTH +
-      collectCount[2] * PRIZE.THIRD +
-      collectCount[3] * PRIZE.SECOND +
-      collectCount[4] * PRIZE.FIRST;
+      this.collectCount[0] * PRIZE.FIFTH +
+      this.collectCount[1] * PRIZE.FOURTH +
+      this.collectCount[2] * PRIZE.THIRD +
+      this.collectCount[3] * PRIZE.SECOND +
+      this.collectCount[4] * PRIZE.FIRST;
     const rate = ((LottoMoney / this.myMoney) * 100).toFixed(1);
+    this.printResult(rate);
+  }
+
+  printResult(rate) {
     MissionUtils.Console.print(`
 당첨 통계
 ---
-${RANK.FIFTH} - ${collectCount[0]}개
-${RANK.FOURTH} - ${collectCount[1]}개
-${RANK.THIRD} - ${collectCount[2]}개
-${RANK.SECOND} - ${collectCount[3]}개
-${RANK.FIRST} - ${collectCount[4]}개
+${RANK.FIFTH} - ${this.collectCount[0]}개
+${RANK.FOURTH} - ${this.collectCount[1]}개
+${RANK.THIRD} - ${this.collectCount[2]}개
+${RANK.SECOND} - ${this.collectCount[3]}개
+${RANK.FIRST} - ${this.collectCount[4]}개
 총 수익률은 ${rate}%입니다.
 `);
     this.close();
