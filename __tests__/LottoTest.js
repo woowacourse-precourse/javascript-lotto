05ec6033fd3ed3b765274408477acc830bab5f38
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-15 20:40:45 +0900

    test: Lotto 클래스의 calculateNumbers() 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -19,4 +19,40 @@ describe('로또 클래스 테스트', () => {
       new Lotto([1, 2, 3, 4, 5, 50]);
     }).toThrow('[ERROR]');
   });
+
+  test('로또 판별기 : FIRST', () => {
+    const ticket = new Lotto([1, 2, 3, 4, 5, 6]);
+    const result = ticket.calculateNumbers([1, 2, 3, 4, 5, 6], 7);
+    expect(result).toEqual('FIRST');
+  });
+
+  test('로또 판별기 : SECOND', () => {
+    const ticket = new Lotto([1, 2, 3, 4, 5, 6]);
+    const result = ticket.calculateNumbers([1, 2, 3, 4, 5, 8], 6);
+    expect(result).toEqual('SECOND');
+  });
+
+  test('로또 판별기 : THIRD', () => {
+    const ticket = new Lotto([1, 2, 3, 4, 5, 6]);
+    const result = ticket.calculateNumbers([1, 2, 3, 4, 5, 8], 7);
+    expect(result).toEqual('THIRD');
+  });
+
+  test('로또 판별기 : FOURTH', () => {
+    const ticket = new Lotto([1, 2, 3, 4, 5, 6]);
+    const result = ticket.calculateNumbers([1, 2, 3, 5, 8, 9], 7);
+    expect(result).toEqual('FOURTH');
+  });
+
+  test('로또 판별기 : FIFTH', () => {
+    const ticket = new Lotto([1, 2, 3, 4, 5, 6]);
+    const result = ticket.calculateNumbers([1, 2, 3, 8, 9, 10], 7);
+    expect(result).toEqual('FIFTH');
+  });
+
+  test('로또 판별기 : 꽝', () => {
+    const ticket = new Lotto([1, 2, 3, 4, 5, 6]);
+    const result = ticket.calculateNumbers([1, 2, 8, 9, 10, 11], 7);
+    expect(result).toBeNull();
+  });
 });
