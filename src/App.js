5a48a4d365867b16418ddee977f4289f42580dcd
soohyun_kim5 <93cherry@naver.com>
2022-11-15 21:37:32 +0900

    feat: 당첨 번호 입력

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ class App {
     lotteryNumbersArr = [];
     lotteryWinningNumbersArr = [];
 
+    lotteryWinningNumbers() {
+        MissionUtils.Console.print("\n당첨 번호를 입력해 주세요.");
+        MissionUtils.Console.readLine("", (inputNumbers) => {
+            let lotteryWinningNumbers = inputNumbers.split(",").map(Number)
+            this.lotteryWinningNumbersArr.push(new Lotto(lotteryWinningNumbers));
+        });
+    }
+
     showLotteryNumbers() {
         this.lotteryNumbersArr.forEach((lotto) => {
             MissionUtils.Console.print(`[${String(lotto.getNumber()).split(',').join(', ')}]`);
