3d6c93339c56110298465da0f6d2b9763ea6f933
rovxxmjxx <miju3545@gmail.com>
2022-11-15 12:55:42 +0900

    test: 유효성 - 숫자 개수 테스트 추가
    `isOutOfVolumeAndThrowError()` 에 대한 테스트 코드

--- __tests__/InputValidateTest.js
+++ __tests__/InputValidateTest.js
@@ -1,5 +1,10 @@
 const Display = require('../src/Display');
-const { isDuplicatedAndThrowError, isOutOfRangeAndThrowError } = require('../src/utils/inputValidate');
+
+const {
+  isDuplicatedAndThrowError,
+  isOutOfRangeAndThrowError,
+  isOutOfVolumeAndThrowError,
+} = require('../src/utils/inputValidate');
 
 describe('사용자 입력값 에러 테스트', () => {
   test('중복 여부를 확인한다.', () => {
@@ -12,5 +17,11 @@ describe('사용자 입력값 에러 테스트', () => {
     expect(() => {
       isOutOfRangeAndThrowError([45, 1, 2, 3, 4, 56]);
     }).toThrow(Display.error('OUT_OF_RANGE'));
+  });
+
+  test('주어진 숫자 입력값 길이와 동일하지 않으면 예외가 발생한다.', () => {
+    expect(() => {
+      isOutOfVolumeAndThrowError([1, 2, 3, 4, 5, 6], 5);
+    }).toThrow(Display.error('OUT_OF_VOLUME'));
   });
 });
