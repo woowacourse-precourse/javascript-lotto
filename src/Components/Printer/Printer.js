d7d6bd0892b579d562b4e316cc5e6fc04a8aa0f3
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-15 20:28:27 +0900

    feat: 출력문을 반환문으로 수정

--- src/Components/Printer/Printer.js
+++ src/Components/Printer/Printer.js
@@ -1,12 +1,8 @@
-const { Console } = require("@woowacourse/mission-utils");
-
-const AbstractPrinter = require("../AbstractPrinter/AbstractPrinter");
+const AbstractPrinter = require('../AbstractPrinter/AbstractPrinter');
 
 class Printer extends AbstractPrinter {
   print() {
-    Console.print(
-      `${this.matchedNumberCount}개 일치 (${this.winningMoney}원) - ${this.matchedLottoCount}개`
-    );
+    return `${this.matchedNumberCount}개 일치 (${this.winningMoney}원) - ${this.matchedLottoCount}개`;
   }
 }
 
--- src/Components/PrinterWithBonusBall/PrinterWithBonusBall.js
+++ src/Components/PrinterWithBonusBall/PrinterWithBonusBall.js
@@ -1,12 +1,8 @@
-const { Console } = require("@woowacourse/mission-utils");
-
-const AbstractPrinter = require("../AbstractPrinter/AbstractPrinter");
+const AbstractPrinter = require('../AbstractPrinter/AbstractPrinter');
 
 class PrinterWithBonusBall extends AbstractPrinter {
   print() {
-    Console.print(
-      `${this.matchedNumberCount}개 일치, 보너스 볼 일치 (${this.winningMoney}원) - ${this.matchedLottoCount}개`
-    );
+    return `${this.matchedNumberCount}개 일치, 보너스 볼 일치 (${this.winningMoney}원) - ${this.matchedLottoCount}개`;
   }
 }
 
