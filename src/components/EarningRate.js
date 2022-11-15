bfccd31f55ae93354d51bcc981fc88d30742bac4
semnil5202 <semnil5202@gmail.com>
2022-11-15 11:00:34 +0900

    refactor(EarningRate): 매개변수 객체화

--- src/components/EarningRate.js
+++ src/components/EarningRate.js
@@ -1,5 +1,5 @@
 class EarningRate {
-  static getEarningRate(winPlace, purchaseAmount) {
+  static getEarningRate({ winPlace, purchase }) {
     const prizes = [2000000000, 30000000, 1500000, 50000, 5000];
     let earningRate = 0;
     let index = 0;
@@ -7,7 +7,7 @@ class EarningRate {
       earningRate += winPlace[key] * prizes[index];
       index += 1;
     }
-    earningRate = (earningRate / purchaseAmount) * 100;
+    earningRate = (earningRate / purchase) * 100;
     return earningRate;
   }
 }
