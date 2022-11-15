1a4142e2674ff64bdd28a8cf04793d917539c3b2
고동현 <dhjk35@naver.com>
2022-11-15 17:20:48 +0900

    refactor(lotto): 로또 클래스 LottoValidator를 이용해 검증 로직 리팩토링

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,5 +1,4 @@
-const { ERROR_MESSAGE, LOTTO } = require('../constants');
-const { ValidationError } = require('../errors');
+const LottoValidator = require('../validators/LottoValidator');
 
 class Lotto {
   #numbers;
@@ -14,42 +13,7 @@ class Lotto {
   }
 
   #validate(numbers) {
-    const validations = {
-      LENGTH: this.#hasValidLength.bind(this),
-      UNIQUE: this.#hasUniqueValues.bind(this),
-      VALUE: this.#hasNumberValuesOnly.bind(this),
-      RANGE: this.#hasValidRangeValues.bind(this),
-    };
-
-    Object.entries(validations).forEach(([key, validateLotto]) => {
-      if (!validateLotto(numbers)) {
-        throw new ValidationError(ERROR_MESSAGE.LOTTO[key]);
-      }
-    });
-  }
-
-  #hasValidLength(numbers) {
-    return numbers.length === LOTTO.NUMBER_COUNT;
-  }
-
-  #hasUniqueValues(numbers) {
-    return new Set(numbers).size === LOTTO.NUMBER_COUNT;
-  }
-
-  #hasNumberValuesOnly(numbers) {
-    return numbers.every((number) => this.#isNumber(number));
-  }
-
-  #isNumber(value) {
-    return typeof value === 'number';
-  }
-
-  #hasValidRangeValues(numbers) {
-    return numbers.every((number) => this.#isInRange(number));
-  }
-
-  #isInRange(number) {
-    return number >= LOTTO.MIN_NUMBER && number <= LOTTO.MAX_NUMBER;
+    LottoValidator.validateLottoNumbers(numbers);
   }
 }
 
