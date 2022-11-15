0576364abb14f49ec2ced0da27de276aff7a29f5
hyejinny97 <yhj970102@gmail.com>
2022-11-15 21:34:32 +0900

    feat: Lotto 클래스 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 const PurchaseLotto = require("./PurchaseLotto.js");
+const Lotto = require("./Lotto.js");
 
 class App {
   play() {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (money) => {
       this.purchaseLotto = new PurchaseLotto(money)
+    });
+
+    MissionUtils.Console.readLine('당첨 번호를 입력해 주세요.', (numbers) => {
+      this.lotto = new Lotto(numbers)
     });
   }
 }
