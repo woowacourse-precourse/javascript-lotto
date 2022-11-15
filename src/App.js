851c9383a2b33d82a4f1398118a2e036c8ed7a87
Sanggil Han <tkdrlf95@naver.com>
2022-11-15 23:46:47 +0900

    feat(App): Combine every components
    모든 기능들을 합쳐 APP을 완성하였음

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 const LottoMachine = require("./LottoMachine");
 const LottoStore = require("./LottoStore");
+const Prize = require("./Prize");
 
 class App {
   #lottoMachine = new LottoMachine();
@@ -15,15 +16,31 @@ class App {
 
   play() {
     this.buyLottos();
+    this.takeMoney();
   }
 
   buyLottos() {
-    const numbersOfLottos = this.#lottoStore.sellLottos();
-    console.log(numbersOfLottos);
-    for (let i = 0; i < numbersOfLottos; i++) {
-      let lotto = new Lotto(this.#lottoMachine.generate());
+    const numberOfLottos = this.#lottoStore.sellLottos();
+    for (let i = 0; i < numberOfLottos; i++) {
+      let lotto = new Lotto(this.#lottoMachine.generateNumbers());
       this.lottos.push(lotto);
     }
+  }
+
+  takeMoney() {
+    const prize = new Prize(this.lottos);
+    prize.calcualteWins();
+    prize.announceResult();
+    prize.calcualteResult();
+    this.earningsRate(prize);
+  }
+
+  earningsRate(prize) {
+    const payment = this.#lottoStore.payment;
+    const prizeMoney = prize.calcualteResult();
+    const earningRate = (prizeMoney/payment*100);
+    this.#console.print(`총 수익률은 ${earningRate}%입니다.`);
+    this.#console.close();
   }
 }
 
