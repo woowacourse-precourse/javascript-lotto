b8d66efd1da800d18ebfffff8a4a78b8b9d1e5f2
myung-j-p <jmpack97@gmail.com>
2022-11-15 13:57:33 +0900

    Test: lotto 테스트케이스 추가
    - checkWinNumInlot()
    - check2ndPrize()
    - checkHasBonusNum()
    - preparePrint()

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -28,7 +28,31 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  test('구입한 로또와 당첨번호, 보너스번호를 비교하고 결과를 출력한다.', () => {
+ test('당첨번호와 로또번호 1세트를 비교하고 일치하는 숫자만 리스트로 반환한다.', () => {
+    const makedLot = [ 1, 2, 3, 4, 5, 6 ];
+    const winningNumLs = [ '1', '2', '3', '5', '6', '7' ];
+    expect(lotto.checkWinNumInlot(makedLot, winningNumLs)
+    ).toEqual([ 1, 2, 3, 5, 6])
+  });
+
+  test('5개 일치한 로또를 보너스번호와 비교하여 2등인지 확인하고 불린을 반환한다.', () => {
+    const winNumInLot = [ 1, 2, 3, 5, 6 ];
+    const makedLotto = [ 1, 2, 3, 5, 6, 7 ];
+    const bonusNum = 7;
+    expect(lotto.check2ndPrize(winNumInLot, makedLotto, bonusNum)
+    ).toEqual(true)
+  });
+
+  test('5개 일치한 로또번호에서 나머지 1개가 보너스번호와 일치하는지 확인하고 불린을 반환한다. ', () => {
+    const correctFiveNumLottos = [ 1, 2, 3, 5, 6, 7 ];
+    const bonusNum = 7;
+    expect(lotto.checkHasBonusNum(correctFiveNumLottos, bonusNum)
+    ).toEqual(true)
+  });
+  
+  
+
+  test('구입한 로또와 당첨번호, 보너스번호를 비교하고 결과값을 반환한다.', () => {
     const lottoAndBonusNum = [['1','2','3','4','5','6'],['7']];
     const makedLottos = [
       [ 1, 2, 3, 4, 5, 7 ],
@@ -38,8 +62,17 @@ describe("로또 클래스 테스트", () => {
     expect(lotto.checkCorrect(lottoAndBonusNum, makedLottos)
     ).toEqual([[ 6, 3, 2 ],1]);
   });
+
+  test('카운트된 결과를 바탕으로 당첨개수를 순서대로 리스트화해서 반환한다.', () => {
+    const winningList = [ { '3개': 1 }, { '4개': 1 }, { '5개': 0 }, { '6개': 1 } ];
+    const secondPrize = 2;
+
+    expect(lotto.preparePrint(winningList, secondPrize)
+    ).toEqual([1,1,0,2,1])
+  });
   
-  test('로또 결과 카운트 테스트', () => {
+  
+  test('로또 결과를 카운트해서 오브젝트 리스트로 반환하고 출력한다. ', () => {
     const correctNums = [3,4,6];
     const correctBonus = 3;
 
