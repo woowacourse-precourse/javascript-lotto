843b501cc96f2ee001516edc5a4eca0abd2c7112
mochang2 <qjsrodksro@gmail.com>
2022-11-15 10:01:16 +0900

    refactor: 상수 취급하는 객체 전부 Object.freeze로 감쌈

--- src/constants/app.js
+++ src/constants/app.js
@@ -2,12 +2,12 @@ const { substractComma } = require('../lib/utils.js')
 
 const LOTTO_PRICE = substractComma('1,000')
 
-const validationError = {
+const validationError = Object.freeze({
   TYPE: '[ERROR] 구입 금액은 정수여야 합니다.',
   UNIT: `[ERROR] 구입 금액은 ,없이 ${LOTTO_PRICE}원 단위여야 합니다.`,
-}
+})
 
-const prize = {
+const prize = Object.freeze({
   1: {
     PRICE: substractComma('2,000,000,000'),
     CRITERIA: '6개 일치',
@@ -28,13 +28,13 @@ const prize = {
     PRICE: substractComma('5,000'),
     CRITERIA: '3개 일치',
   },
-}
+})
 
-const query = {
+const query = Object.freeze({
   PURCHASE: '구입금액을 입력해 주세요.',
   WINNING_NUMBERS: '당첨 번호를 입력해 주세요.',
   BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
-}
+})
 
 module.exports = {
   LOTTO_PRICE,
--- src/constants/common.js
+++ src/constants/common.js
@@ -1,9 +1,9 @@
 const WINNING_NUMBER_COUNT = 6
 
-const range = {
+const range = Object.freeze({
   MIN: 1,
   MAX: 45,
-}
+})
 
 module.exports = {
   WINNING_NUMBER_COUNT,
