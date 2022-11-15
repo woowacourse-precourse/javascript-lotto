bfbe5c849606f881e9054a49c3668cf3632d273e
Yeonju Jo <yjj0287@gmail.com>
2022-11-15 22:43:53 +0900

    feat: 로또 클래스에 유효성 검사 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,18 @@
+"use strict";
+
+const { validateLottoNumber, validateUniqueNumbers } = require("./Utils");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
-    this.#numbers = numbers;
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    validateLottoNumber(numbers);
+    validateUniqueNumbers(numbers);
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
