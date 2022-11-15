108dc768c68dec3ef1116a49ecdaf502a56f5ce4
curomame <curomame@naver.com>
2022-11-15 21:44:27 +0900

    ➕ feat - <Lotto> [returnBonusNumberCount] add
    보너스 번호를 포함한 당첨 번호와 구매한 로또 번호가 일치하는지 개수를 반환하는 메서드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -20,6 +20,22 @@ class Lotto {
     return count;
   }
 
+  returnBonusNumberCount(purchasedLottoNumbersList, bounusNumber) {
+    let count = 0;
+    const totalNumbersList = [...purchasedLottoNumbersList, bounusNumber];
+    totalNumbersList.forEach((number) => {
+      if (purchasedLottoNumbersList.includes(number) === true) {
+        count += 1;
+      }
+    });
+    if (count === 5) {
+      return 5;
+    }
+    if (count === 6) {
+      return "Bonus";
+    }
+  }
+
 }
 
 module.exports = Lotto;
