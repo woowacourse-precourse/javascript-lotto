5ca75cfaed4235f1f3f51bbf4ef5161c1676dc26
Sanggil Han <tkdrlf95@naver.com>
2022-11-15 16:31:30 +0900

    feat(App): Implement feature of buying lottos
    로또를 구매하는 기능을 구현하였음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+const LottoMachine = require("./LottoMachine");
+const LottoStore = require("./LottoStore");
+
 class App {
-  play() {}
+  #lottoMachine = new LottoMachine();
+  #lottoStore = new LottoStore();
+  #console = MissionUtils.Console;
+  lottos = [];
+
+  constructor() {
+
+  }
+
+  play() {
+    this.buyLottos();
+  }
+
+  buyLottos() {
+    const numbersOfLottos = this.#lottoStore.sellLottos();
+    console.log(numbersOfLottos);
+    for (let i = 0; i < numbersOfLottos; i++) {
+      let lotto = new Lotto(this.#lottoMachine.generate());
+      this.lottos.push(lotto);
+    }
+  }
 }
 
 module.exports = App;
