942bfa6bc6fe98697c90812a98f8623bc4189816
jaesukpark77 <jaesukpark77@naver.com>
2022-11-15 18:00:02 +0900

    feat: 로또 당첨 금액

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ const Lotto = require("./Lotto");
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   boughtLottos = [];
-  
+
+  play() {
+    this.inputAmountMoney();
+  }
+
   inputAmountMoney() {
     const WORD_TO_PRINT = "구입금액을 입력해 주세요.\n";
     let numberOfLotto;
@@ -29,6 +33,38 @@ class App {
         const WRONG_INPUT = "[ERROR] 1000원 단위 금액이 아닙니다.";
         throw new Error(WRONG_INPUT);
     }
+  }
+
+  printBoughtNumberOfLotto(NUMBER_OF_LOTTO) {
+    const WORD_TO_PRINT = ` \n${NUMBER_OF_LOTTO}개를 구매했습니다.`;
+    MissionUtils.Console.print(WORD_TO_PRINT);
+  }
+
+  randomLottoNumber(NUMBER_OF_LOTTO) {
+    for (let i = 0; i < NUMBER_OF_LOTTO; i++) {
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      numbers.sort((a, b) => {
+        return a - b;
+      });
+      this.boughtLottos.push(numbers);
+    }
+    for (const boughtLotto of this.boughtLottos) {
+      MissionUtils.Console.print(`[${boughtLotto.join(", ")}]`);
+    }
+
+    this.inputWinningNumbers();
+  }
+
+  inputWinningNumbers() {
+    const WORD_TO_PRINT = "\n당첨 번호를 입력해주세요.\n";
+    let winngNumbers;
+    let bonusNumber;
+    MissionUtils.Console.readLine(WORD_TO_PRINT, (numbers) => {
+      winngNumbers = numbers.split(",").map(Number);
+
+      const lotto = new Lotto(winngNumbers);
+      bonusNumber = lotto.inputBonusNumbers(this.boughtLottos);
+    });
   }
 }
 
