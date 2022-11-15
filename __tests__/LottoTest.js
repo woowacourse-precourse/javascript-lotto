814786bb4bfba1d02ee8e4fbca4a113d7ec04ac7
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2022-11-15 22:42:10 +0900

    test : 유효성 검사 확인 함수 생성 및 확인

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -22,4 +22,58 @@ describe("로또 클래스 테스트", () => {
       new Lotto([1, 2, 3, 4, 5, 55]);
     }).toThrow("[ERROR]");
   });
+  //유효성 검사
+  test("입력이 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      LottoValidation.checkIsNumber("a");
+    }).toThrow("[ERROR]");
+    expect(() => {
+      LottoValidation.checkIsNumber("b");
+    }).toThrow("[ERROR]");
+    expect(() => {
+      LottoValidation.checkIsNumber("1a");
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호가 6개가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      LottoValidation.checkLength([1, 2, 3, 4, 5, 6, 7]);
+    }).toThrow("[ERROR]");
+    expect(() => {
+      LottoValidation.checkLength([1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      LottoValidation.checkDuplicate([1, 2, 3, 4, 5, 1]);
+    }).toThrow("[ERROR]");
+    expect(() => {
+      LottoValidation.checkDuplicate([6, 2, 3, 4, 5, 6]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("배열에 1~45 범위 밖 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      LottoValidation.checkLottoRange([100, 2, 3, 4, 5, 6]);
+    }).toThrow("[ERROR]");
+    expect(() => {
+      LottoValidation.checkLottoRange([1, 2, 3, 4, 5, 67]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.", () => {
+    expect(() => {
+      LottoValidation.checkBonusDuplicate(1, [1, 2, 3, 4, 5, 6]);
+    }).toThrow("[ERROR]");
+    expect(() => {
+      LottoValidation.checkBonusDuplicate(6, [1, 2, 3, 4, 5, 6]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("입력이 0이면 예외가 발생한다.", () => {
+    expect(() => {
+      LottoValidation.checkZero("0");
+    }).toThrow("[ERROR]");
+  });
 });
