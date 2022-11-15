1e2d5908a6eb0058feeb36444a21cb7c3979a149
gsong <rmsdlf1206@gmail.com>
2022-11-15 16:52:23 +0900

    feat: 구입 금액 input이 0또는 공백인 확인

--- src/checkValid/checkPriceValidation.js
+++ src/checkValid/checkPriceValidation.js
@@ -8,9 +8,15 @@ function checkPriceDataType(input) {
     throw new Error('[ERROR] 숫자를 입력해주세요.');
   }
 }
-function checkPriceValidation() {
-  checkPriceDataType();
-  checkPriceUnits();
+function checkPriceZero(input) {
+  if (input === undefined || input === null || input === '' || input === 0) {
+    throw new Error('[ERROR] 공백 및 0은 입력할 수 없습니다.');
+  }
+}
+function checkPriceValidation(input) {
+  checkPriceZero(input);
+  checkPriceDataType(input);
+  checkPriceUnits(input);
 }
 
 module.exports = checkPriceValidation;
