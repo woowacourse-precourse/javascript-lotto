9fbd7f44e613a9982ca18d7f1752102339aa62e4
윤동주 <yoondj98@naver.com>
2022-11-15 20:29:56 +0900

    refactor: consoleWinningResult 함수 내 일부 코드 checkwinningMoney 함수로 분리
    1함수 3indent를 방지하기 위해 함수 분리 리팩토링.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ const { Console, Random } = require("@woowacourse/mission-utils");
 const Lotto = require("../src/Lotto");
 
 class App {
+  winningMoney = [5000, 50000, 1500000, 30000000, 2000000000];
   constructor() {
     this.usedMoney = 0;
     this.lottoAmount = 0;
@@ -21,21 +22,18 @@ class App {
     this.inputWinningNumbers();
     this.inputBonusNumbers();
     this.consoleWinningResult();
-    this.calculateResultPercentage();
+    Console.close();
   }
 
   buyLotto() {
-    Console.readLine(
-      "로또를 구매할 금액을 입력해주세요.",
-      (input) => {
-        if (input % 1000 != 0)
-          throw new Error(
-            "[ERROR] 구입 금액은 1000원 단위로만 구매 가능합니다.\n"
-          );
-        this.usedMoney = input;
-        this.lottoAmount = input / 1000;
-      }
-    );
+    Console.readLine("로또를 구매할 금액을 입력해주세요.", (input) => {
+      if (input % 1000 != 0)
+        throw new Error(
+          "[ERROR] 구입 금액은 1000원 단위로만 구매 가능합니다.\n"
+        );
+      this.usedMoney = input;
+      this.lottoAmount = input / 1000;
+    });
   }
 
   consoleLottoAmount() {
@@ -71,51 +69,59 @@ class App {
   }
 
   consoleWinningResult() {
-    for (let i = 0; i < this.lottoAmount; i++) {
-      let winningCount = 0;
-      for (let j = 0; j < 6; j++) {
-        if (
-          this.lottoNumbers[i]
-            .getLottoNumbersArray()
-            .includes(this.winningNumbers.getLottoNumbersArray()[j])
-        )
-          winningCount++;
-      }
-      if (
-        this.lottoNumbers[i].getLottoNumbersArray().includes(this.bonusNumber) &&
-        winningCount == 5
-      ) {
-        this.countOfWinningways[3]++;
-      } 
-      else if (winningCount == 3) this.countOfWinningways[0]++;
-      else if (winningCount == 4) this.countOfWinningways[1]++;
-      else if (winningCount == 5) this.countOfWinningways[2]++;
-      else if (winningCount == 6) this.countOfWinningways[4]++;
+    for (let eachLotto of this.lottoNumbers) {
+      this.checkWinningMoney(eachLotto);
     }
-    Console.print("당첨 통계");
-    Console.print("---");
-    Console.print("3개 일치 (5,000원) - " + this.countOfWinningways[0] + "개");
-    Console.print("4개 일치 (50,000원) - " + this.countOfWinningways[1] + "개");
     Console.print(
-      "5개 일치 (1,500,000원) - " + this.countOfWinningways[2] + "개"
-    );
-    Console.print(
-      "5개 일치, 보너스 볼 일치 (30,000,000원) - " +
+      "당첨 통계\n" +
+        "---\n" +
+        "3개 일치 (5,000원) - " +
+        this.countOfWinningways[0] +
+        "개\n" +
+        "4개 일치 (50,000원) - " +
+        this.countOfWinningways[1] +
+        "개\n" +
+        "5개 일치 (1,500,000원) - " +
+        this.countOfWinningways[2] +
+        "개\n" +
+        "5개 일치, 보너스 볼 일치 (30,000,000원) - " +
         this.countOfWinningways[3] +
-        "개"
-    );
-    Console.print(
-      "6개 일치 (2,000,000,000원) - " + this.countOfWinningways[4] + "개"
+        "개\n" +
+        "6개 일치 (2,000,000,000원) - " +
+        this.countOfWinningways[4] +
+        "개\n"
     );
     this.calculateResultPercentage();
   }
 
-  calculateResultPercentage(){
-    for(let i=0; i<5; i++){
-      this.totalEarnedMoney += this.countOfWinningways[i] * this.winningMoney[i];
+  checkWinningMoney(eachLotto) {
+    let winningCount = 0;
+    for (let j = 0; j < 6; j++) {
+      if (
+        eachLotto
+          .getLottoNumbersArray()
+          .includes(this.winningNumbers.getLottoNumbersArray()[j])
+      )
+        winningCount++;
     }
-    this.ResultPercentage = this.totalEarnedMoney / this.usedMoney*100;
-    Console.print("총 수익률은 "+this.ResultPercentage+"%입니다.");
+    if (
+      eachLotto.getLottoNumbersArray().includes(this.bonusNumber) &&
+      winningCount == 5
+    ) {
+      this.countOfWinningways[3]++;
+    } else if (winningCount == 3) this.countOfWinningways[0]++;
+    else if (winningCount == 4) this.countOfWinningways[1]++;
+    else if (winningCount == 5) this.countOfWinningways[2]++;
+    else if (winningCount == 6) this.countOfWinningways[4]++;
+  }
+
+  calculateResultPercentage() {
+    for (let i = 0; i < 5; i++) {
+      this.totalEarnedMoney +=
+        this.countOfWinningways[i] * this.winningMoney[i];
+    }
+    this.ResultPercentage = (this.totalEarnedMoney / this.usedMoney) * 100;
+    Console.print("총 수익률은 " + this.ResultPercentage + "%입니다.");
   }
 }
 
