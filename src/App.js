ba0b8c345712f6047c3392e1bd0531df30f58fd7
soohyun_kim5 <93cherry@naver.com>
2022-11-15 21:32:44 +0900

    feat: 구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 
 class App {
+    purchaseAmountArr = [];
 
-  play() {
-    MissionUtils.Console.print("구입금액을 입력해 주세요.");
-  }
+    purchaseAmount() {
+        MissionUtils.Console.readLine("", (purchaseAmount) => {
+            let purchaseAmountString = purchaseAmount.toString();
+        });
+    }
+
+    play() {
+        MissionUtils.Console.print("구입금액을 입력해 주세요.");
+        this.purchaseAmount();
+    }
 }
 
-module.exports = App;
+module
+    .exports = App;
