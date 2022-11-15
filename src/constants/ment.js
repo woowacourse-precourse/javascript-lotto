6beb7b8a043f96f561a98b0fdb7e348656a00a6c
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-15 22:22:01 +0900

    Feat: UI 문자열을 상수로 분리

---
+++ src/constants/ment.js
@@ -0,0 +1,15 @@
+const Reward = require('./reward');
+
+class Ment {
+	static INPUT_INVEST = '구입금액을 입력해 주세요.';
+	static INPUT_ANSWER = '당첨 번호를 입력해 주세요.';
+	static INPUT_BONUS = '보너스 번호를 입력해 주세요.';
+	static BOUGHT_CNT_SUFFIX = '개를 구매했습니다.';
+	static FIRST = '6개 일치';
+	static SECOND = '5개 일치, 보너스 볼 일치';
+	static THIRD = '5개 일치';
+	static FOURTH = '4개 일치';
+	static FIFTH = '3개 일치';
+}
+
+module.exports = Ment;
