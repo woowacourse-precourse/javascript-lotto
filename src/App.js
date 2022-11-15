aab7afa86440f4e38cb41477eb4629a0404cfcf4
jiyun1006 <jiyun4251@gmail.com>
2022-11-15 18:16:22 +0900

    feat : 구입 금액을 입력받는 함수 생성
    개행을 이용해 깔끔하게 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const Lotto = require("./Lotto");
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    lottoBuyNum();
+  }
 }
 
+const lottoBuyNum = () => {
+  MissionUtils.Console.readLine('구입금액을 입력해 주세요. \n', (answer) => {
+    console.log(answer);
+  })
+}
+
+const app = new App();
+app.play();
+
+
 module.exports = App;
+
+
