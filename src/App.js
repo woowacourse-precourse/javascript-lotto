34f8f7f4cc6553fc1f5f7d6db89c7923da5a3923
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-15 15:35:58 +0900

    feat(App): add count variable by amount

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ const Lotto = require("./Lotto.js");
 const { MAX_LENGTH, MESSAGE } = require("./utils/Constants");
 class App {
   #amount;
+  #count;
 
   inputPurchase() {
     Console.readLine(MESSAGE.USER_ENTER_PURCHASE, (input) => {
       this.#amount = input;
+      if (this.#amount % 1000 === 0) this.#count = input / 1000;
     });
   }
   play() {}
