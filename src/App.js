da23203a62b8d47ed7c00dbbd0b7e5f3cad5ab9a
rovxxmjxx <miju3545@gmail.com>
2022-11-15 16:40:37 +0900

    feat: app 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const { Console } = require('@woowacourse/mission-utils');
+const Machine = require('./Machine');
+const Display = require('./Display');
+
 class App {
-  play() {}
+  play() {
+    Console.readLine(Display.guidance('PAYMENT'), (payment) => {
+      const machine = new Machine(payment);
+      machine.work();
+    });
+  }
 }
 
 module.exports = App;
