9863ca9cffc6a48448b75ce685aa80b6e5d5a538
haesa <xdhaesa@gmail.com>
2022-11-15 13:54:19 +0900

    refactor(Bonus.js): 생성자 수정 및 에러 메세지 상수 처리

--- src/Bonus.js
+++ src/Bonus.js
@@ -1,20 +1,22 @@
+const { BONUS_ERROR } = require('./constants/constants');
+
 class Bonus {
-  constructor(bonus, numbers) {
-    this.validate(bonus, numbers);
-    this.number = bonus;
+  constructor(number, numbers) {
+    this.validate(number, numbers);
+    this.number = Number(number);
   }
 
   validate(bonus, numbers) {
     if (/[^0-9]/.test(bonus)) {
-      throw new Error('[ERROR] 보너스 번호는 숫자여야 합니다.');
+      throw new Error(BONUS_ERROR.NUMBER);
     }
 
     if (!(bonus >= 1 && bonus <= 45)) {
-      throw new Error('[ERROR] 보너스 번호는 1과 45 사이의 숫자여야 합니다.');
+      throw new Error(BONUS_ERROR.DOMAIN);
     }
 
     if (numbers.includes(bonus)) {
-      throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복되지 않습니다.');
+      throw new Error(BONUS_ERROR.DUPLICATION);
     }
   }
 }
