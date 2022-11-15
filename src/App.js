d88f0c4429b7bdee240d6b145a3569fe4a4c07bc
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-15 20:53:06 +0900

    feat: print, input 선언

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+const print = require("./console/print");
+const input = require("./console/input");
+
 class App {
+  constructor() {
+    this.input = new input();
+    this.print = new print();
+  }
+
   play() {}
 }
 
