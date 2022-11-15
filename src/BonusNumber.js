c076ea435fda2b5c965b0c20bd1dc30b6d0cfe76
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-15 10:02:47 +0900

    feat : 보너스 번호는 로또 번호와 중복이 있는 경우 예외 처리

--- src/BonusNumber.js
+++ src/BonusNumber.js
@@ -4,6 +4,12 @@ class BonusNumber {
     constructor(number, lotto) {
         this.#number = number;
     }
+
+    validateDuplicatedLotto(lotto, number) {
+        if (!list.includes(number)) {
+            throw new Error("[ERROR] 보너스 번호는 로또 번호와 중복이 없어야 합니다.");
+        }
+    }
 }
 
 module.exports = BonusNumber;
