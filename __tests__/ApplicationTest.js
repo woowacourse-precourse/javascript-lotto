9b70de5797635a649cee9dc0bbfa20d9ed9ee000
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-15 21:52:42 +0900

    test(ApplicationTest): test win number is not number

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -106,6 +106,14 @@ describe("로또 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
+  test("당첨번호가 숫자가 아니라면 예외가 발생한다.", () => {
+    mockQuestions(["8000", "1, 3, 5, 14, 22, k", "10"]);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("[ERROR]");
+  });
+
   test("보너스 번호가 숫자가 아니라면 예외가 발생한다.", () => {
     mockQuestions(["8000", "1, 3, 5, 14, 22, 45", "k"]);
     expect(() => {
