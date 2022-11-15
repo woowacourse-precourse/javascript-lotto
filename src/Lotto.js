44d13e425932df7b4e642cf8fc0661b54ff0d5d4
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-15 23:10:53 +0900

    feat(Lotto) : 생성된 로또 번호가 중복되는지 확인하는 함수 isDuplicate 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,6 +12,9 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (this.isDuplicate(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
     if (!this.isNumInRange(numbers)) {
       throw new Error("[ERROR] 로또 번호가 1-45 사이 숫자가 아닙니다.");
     }
@@ -23,6 +26,14 @@ class Lotto {
         return false;
     }
     return true;
+  }
+
+  isDuplicate(numbers) {
+    const numbersSet = new Set(numbers);
+    if (numbers.length !== numbersSet.size) {
+      return true;
+    }
+    return false;
   }
 }
 
