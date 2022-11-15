c31f3d86d6be618d690be42fa1db2ea15d9ae935
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-15 14:21:43 +0900

    feat : 로또 결과 산출 하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const MyLotto = require("./MyLotto");
 const Lotto = require("./Lotto");
+const BonusNumber = require("./BonusNumber");
 
 class App {
   money;
   myLotto;
+  lotto;
+  bonusNumber;
+  result = {'1등': 0, '2등': 0, '3등': 0, '4등': 0, '5등': 0, 'none': 0}
+  reward = {'1등': 2000000000, '2등': 30000000, '3등': 1500000, '4등': 50000, '5등': 5000, 'none': 0}
 
   play() {
     this.inputBuyLottoMoney();
@@ -41,6 +46,7 @@ class App {
 
       this.bonusNumber = new BonusNumber(parseInt(number), this.lotto.getLottoNumbers());
 
+      this.checkAllLottoResult(this.myLotto.getMyLottoList(), this.lotto.getLottoNumbers(), this.bonusNumber.getBonusNumber());
     });
   }
 
@@ -55,6 +61,47 @@ class App {
     if (isNaN(money)) {
       throw new Error("[ERROR] 숫자만 입력 가능합니다.");
     }
+  }
+
+  checkAllLottoResult(myLottoNumbersList, lottoNumbers, bonusNumber) {
+    for (const myLottoNumbers of this.myLotto.getMyLottoList()) {
+      let [lottoCount, bonus] = this.checkLottoResult(myLottoNumbers, lottoNumbers, bonusNumber);
+      let rank = this.getRank(lottoCount, bonus)
+      this.result[rank] += 1
+      this.revenue += this.reward[rank]
+    }
+  }
+
+  checkLottoResult(myLottoNumbers, lottoNumbers, bonusNumber) {
+    let lottoCount = 0;
+    for (const number of myLottoNumbers) {
+      lottoCount += this.checkMatchingNumber(lottoNumbers, number)
+    }
+    let bonus = this.checkMatchingNumber(myLottoNumbers, bonusNumber)
+    return [lottoCount, bonus];
+  }
+
+  checkMatchingNumber(lottoNumbers, number) {
+    return lottoNumbers.includes(number);
+  }
+
+  getRank(lottoCount, bonus) {
+    if (lottoCount === 3) {
+      return '5등'
+    }
+    if (lottoCount === 4) {
+      return '4등'
+    }
+    if (lottoCount === 5 && !bonus) {
+      return '3등'
+    }
+    if (lottoCount === 5 && bonus) {
+      return '2등'
+    }
+    if (lottoCount === 6) {
+      return '1등'
+    }
+    return 'none'
   }
 }
 
