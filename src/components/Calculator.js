6c59378a57d1e03f5106064207f90a9c62408e4c
Inae Choi <inaechoi89@gmail.com>
2022-11-15 10:29:24 +0900

    feat: add a function to calculate yield

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,11 +5,14 @@ class Calculator {
     this.bonusNumber = Number(bonusNumber);
     this.money = money;
     this.statistic = [0, 0, 0, 0, 0];
+    this.prize = [5000, 50000, 1500000, 30000000, 2000000000];
+    this.yield = 0;
     this.calculateStatistics();
   }
 
   calculateStatistics() {
     this.lotteries.forEach((lotto) => this.compareLotteries(lotto));
+    this.yield = this.calculateYield(this.statistic) && "0.0";
   }
 
   compareLotteries(lotto) {
@@ -41,6 +44,24 @@ class Calculator {
       return 3;
     }
     return 2;
+  }
+
+  calculateYield(statistic) {
+    const prizeMoney = this.getPrizeMoney(statistic);
+    return ((prizeMoney / this.money) * 100).toFixed(1).toLocaleString();
+  }
+
+  getPrizeMoney(statistic) {
+    const prizeMoneyArray = this.getPrizeMoneyArray(statistic);
+    const prizeMoney = prizeMoneyArray.reduce(
+      (sum, currentValue) => sum + currentValue,
+      0
+    );
+    return prizeMoney;
+  }
+
+  getPrizeMoneyArray(statistic) {
+    return statistic.map((count, index) => count * this.prize[index]);
   }
 }
 
