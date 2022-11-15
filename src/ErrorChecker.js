9ece2b0c927895739f69f4dabb5ac58f05d83e24
yeongseokim <1903020@ewhain.net>
2022-11-15 21:38:03 +0900

    chore(ErrorChecker, ErrorCheckerTest): 에러 메시지 상수화

--- src/ErrorChecker.js
+++ src/ErrorChecker.js
@@ -1,11 +1,13 @@
+const ERROR_MESSAGE = require("./constants").ERROR_MESSAGE;
+
 class ErrorChecker {
     static checkSixElementArray(array) {
-        if (array.length !== 6) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (array.length !== 6) throw new Error(ERROR_MESSAGE.LOTTO_NUMBERS_NOT_SIX_DIGITS);
     }
 
-    static checkDuplicatedElement(array, numOfValidElement) {
+    static checkDuplicatedElement(array, numOfValidElement) { //그냥 메시지를 입력 받는 것으로 변경해야 함
         const set = new Set(array);
-        if (set.size !== numOfValidElement) throw new Error("[ERROR] 로또 번호는 중복되지 않는 6자리 숫자여야 합니다.");
+        if (set.size !== numOfValidElement) throw new Error(`[ERROR] 로또 번호는 중복되지 않는 ${numOfValidElement}자리 숫자여야 합니다.`);
     }
 }
 
--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ const PRIZE_MONEY = {
     THIRD: 1500000,
     FOURTH: 50000,
     FIFTH: 5000
-}
+};
 
 const PRIZE_INDEX = {
     FIRST: 4,
@@ -13,9 +13,15 @@ const PRIZE_INDEX = {
     FOURTH: 1,
     FIFTH: 0,
     NOTHING: -1,
-}
+};
+
+const ERROR_MESSAGE = {
+    LOTTO_NUMBERS_NOT_SIX_DIGITS: "[ERROR] 로또 번호는 6개여야 합니다.",
+    LOTTO_NUMBERS_DUPLICATE: "[ERROR] 로또 번호는 중복되지 않는 6자리 숫자여야 합니다.",
+};
 
 module.exports = {
     PRIZE_MONEY,
     PRIZE_INDEX,
+    ERROR_MESSAGE,
 };
