50ee88f554243937393eb61d50c12d153162ca48
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-15 14:25:05 +0900

    feat(CompareLottoTest): 보너스 번호가 당첨됐는지 검사하는 함수 구현

--- __tests__/CompareLottoTest.js
+++ __tests__/CompareLottoTest.js
@@ -8,6 +8,12 @@ describe("CompareLotto 클래스 테스트", () => {
     expect(CompareLotto.count(lotto, winNumber)).toBe(5);
   });
 
+  test("보너스 번호가 당첨됐는지 검사하는 함수 테스트", () => {
+    const winNumber = [1,2,3,4,5,7];
+    const bonus = 3;
+
+    expect(CompareLotto.countBonus(winNumber, bonus)).toBe(true);
+  });
 
 
 //   test("배열 정렬 테스트", () => {
