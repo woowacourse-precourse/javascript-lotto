5028645c979dfd7b161d5976b0397a8ad81f1add
khkh0109 <ist200120@gmail.com>
2022-11-15 22:32:59 +0900

    style(UserError): 상수 추가

--- src/UserError.js
+++ src/UserError.js
@@ -16,28 +16,33 @@ class UserError {
   }
 
   validateMoney(money) {
-    if (money % 1000 !== 0) {
+    const MONEY_MIN = 1000;
+    if (money % MONEY_MIN !== 0) {
       throw new Error("[ERROR] 1,000원 단위로 입력해 주세요.");
     }
   }
 
   validateWinningNumbers(numbers) {
-    if (numbers.length !== 6) {
+    const LOTTO_LENGTH = 6;
+    if (numbers.length !== LOTTO_LENGTH) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
   validateOverlapNumber(numbers) {
+    const LOTTO_LENGTH = 6;
     const set = new Set(numbers);
     numbers = [...set];
-    if (numbers.length !== 6) {
+    if (numbers.length !== LOTTO_LENGTH) {
       throw new Error("[ERROR] 중복되는 번호가 있습니다.");
     }
   }
 
   validateNumberRange(numbers) {
+    const RANGE_START = 1;
+    const RANGE_END = 45;
     for (let i = 0; i < numbers.length; i++) {
-      if (numbers[i] < 1 || numbers[i] > 45) {
+      if (numbers[i] < RANGE_START || numbers[i] > RANGE_END) {
         throw new Error("[ERROR] 1~45 사이의 번호를 입력해 주세요.");
       }
     }
