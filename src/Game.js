396a1597dcfce55df4d8a417115f14563c489e38
Deemou <4106243@naver.com>
2022-11-15 18:27:19 +0900

    feat(Game): 로또 구매 개수 출력 기능 추가

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
-const { MESSAGE } = require('./constant/Lotto');
+const { LOTTO, MESSAGE } = require('./constant/Lotto');
+const Validation = require('./Validation');
 
 class Game {
   initGame() {
@@ -8,7 +9,11 @@ class Game {
   askNumber(message, callback) {
     Console.readLine(message, callback);
   }
-  proceedStepOne(input) {}
+  proceedStepOne(input) {
+    Validation.validateMoney(input);
+    const numberOfLotto = Math.floor(input / LOTTO.PRICE);
+    Console.print(MESSAGE.PURCHASE_RESULT(numberOfLotto));
+  }
 }
 
 module.exports = Game;
