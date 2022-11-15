1a593cd67b5dc6347fa13987363cc45192192e96
SorinKwon <ksl020700@gmail.com>
2022-11-15 14:52:49 +0900

    feat: 당첨번호와 보너스 번호를 전달하는 기능 추가

--- src/draw-machine/DrawLotto.js
+++ src/draw-machine/DrawLotto.js
@@ -1,4 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const ResultStats = require("../game-machine/ResultStats");
 class DrawLotto {
   constructor(){
     this.draw_number;
@@ -9,24 +10,23 @@ class DrawLotto {
       MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.", (draw_number) => {
         console.log(draw_number);
         this.draw_number = draw_number;
+        this.inputBounsNumbers();
     });
     }
     inputBounsNumbers(){
       MissionUtils.Console.readLine("보너스 번호를 입력해 주세요.", (bonus_number) => {
         console.log(bonus_number);
         this.bonus_number=bonus_number;
+        this.sendDrawAndBonusNumber();
     });
     }
 
-    sendDrawNumber(){
-      return this.draw_number;
-    }
-
-    sendBonusNumber(){
-      return this.bonus_number;
+    sendDrawAndBonusNumber(){
+      const result_stats = new ResultStats();
+      result_stats.getDrawAndBonusNumber(this.draw_number,this.bonus_number);
     }
 
   }
-  
+  const draw_lotto = new DrawLotto();
   module.exports = DrawLotto;
   
