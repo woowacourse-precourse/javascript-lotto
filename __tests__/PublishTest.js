6269b06dc63892a7b6af944a5371937bf27233ee
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-15 23:00:48 +0900

    test[PublishTest]: 구매금액에 따른 개수만큼 로또 발행 테스트 완료

---
+++ __tests__/PublishTest.js
@@ -0,0 +1,50 @@
+const App = require('../src/App');
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('로또 발행 테스트', () => {
+  test('입력금액에 따른 개수를 파악해 로또 발행', () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+    ]);
+
+    mockQuestions(['2000']);
+
+    const logs = [
+      '2개를 구매했습니다.',
+      '[8, 21, 23, 41, 42, 43]',
+      '[3, 5, 11, 16, 32, 38]',
+    ];
+
+    const logSpy = getLogSpy();
+
+    const app = new App();
+    app.askPurchasePrice();
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+});
