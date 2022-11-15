6f161b88869c6f4ade2dc6aaf57d8b8d5b8db644
윤동주 <yoondj98@naver.com>
2022-11-15 20:50:17 +0900

    refactor: Lotto 클래스 내 validate 검사문 코드 일부 변경
    숫자가 아닌 문자를 입력받았는지 확인하는 검사문에서 typeof 대신 isNaN을 활용하여 입력값의 숫자/문자 여부 확인.

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,32 +10,31 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    
-    for(let number of numbers){
-      if(typeof(number)!=="number") {
+
+    for (let number of numbers) {
+      if (isNaN(number)) {
         throw new Error("[ERROR] 숫자가 아닌 문자를 입력했습니다.");
       }
     }
 
-    for(let number of numbers){
-      if(1>number||number>45) {
+    for (let number of numbers) {
+      if (1 > number || number > 45) {
         throw new Error("[ERROR] 1~45 중의 자연수를 입력하세요.");
       }
     }
 
-    if(new Set(numbers).size !== 6){
+    if (new Set(numbers).size !== 6) {
       throw new Error("[ERROR] 중복된 숫자가 존재합니다.");
     }
   }
 
-  getLottoNumbers(){
-    return `[${this.#numbers.join(', ')}]`;
+  getLottoNumbers() {
+    return `[${this.#numbers.join(", ")}]`;
   }
 
-  getLottoNumbersArray(){
+  getLottoNumbersArray() {
     return this.#numbers;
   }
-  
 }
 
 module.exports = Lotto;
