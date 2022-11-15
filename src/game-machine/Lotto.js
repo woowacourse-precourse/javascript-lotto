2ff38ac2123f163387de0dd37f1c0693e8086484
SorinKwon <ksl020700@gmail.com>
2022-11-15 21:41:35 +0900

    refactor: 중복되는 기능의 함수 제거

--- src/game-machine/Lotto.js
+++ src/game-machine/Lotto.js
@@ -1,5 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const DrawLotto = require("../draw-machine/DrawLotto");
 const CalculateLotto = require("./CalculateLotto");
+const ResultStats = require("./ResultStats");
 
 class Lotto {
   #numbers;
@@ -13,13 +15,12 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    this.createLottoNumbersList(numbers);
+    this.printLottoNumbersList(numbers);
+  }
+  
+  printLottoNumbersList(numbers){
+    MissionUtils.Console.print(numbers);
   }
 
-  createLottoNumbersList(numbers_arr){
-    let lotto_numbers_list = [];
-    lotto_numbers_list += numbers_arr;
-  }
 }
-
 module.exports = Lotto;
