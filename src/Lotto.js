eab3296e24ee33c96e9dd4755c2c4a9e5973f80c
pitang <wpeffect@gmail.com>
2022-11-15 09:00:51 +0900

    fix: 로또 번호 예외사항 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
 
@@ -10,9 +12,13 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (numbers.filter((num) => num >= 1 && num <= 45).length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않는 숫자여야 합니다.");
+    }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
