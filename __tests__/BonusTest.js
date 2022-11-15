1ee3a577b2f604a2713867a192f2c01572f9f462
DAIN KIM <dnkim419@gmail.com>
2022-11-15 16:51:02 +0900

    test(BonusTest): 잘못된 보너스 번호를 입력한 경우 에러처리

---
+++ __tests__/BonusTest.js
@@ -0,0 +1,28 @@
+const Bonus = require('../src/Bonus');
+const Lotto = require('../src/Lotto');
+
+describe('Bonus 클래스 테스트', () => {
+  test('잘못된 보너스 번호를 입력한 경우 에러처리', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const values = [
+      // 숫자가 1~45까지 범위를 초과할 경우 (ex: 0, 46 등 불가)
+      '0',
+      '46',
+      // 번호가 아닐 경우 (ex: 음수, 소수 등 불가)
+      '-1',
+      '0.1',
+      // 숫자가 아닐 경우 (ex: 알파벳, 한글, 특수문자 등 불가)
+      'a',
+      'ㄱ',
+      '!',
+      // 당첨 번호와 중복될 경우
+      '6',
+    ];
+
+    values.forEach((value) => {
+      expect(() => {
+        new Bonus(value, lotto);
+      }).toThrow('[ERROR]');
+    });
+  });
+});
