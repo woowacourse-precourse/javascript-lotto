0f13576f678a3605a20761060105f0cd18d9cda3
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-15 22:18:26 +0900

    refactor: view 안쓰는 함수 제거

--- src/view/View.js
+++ src/view/View.js
@@ -4,7 +4,6 @@ class View {
   constructor() {
     this.printLottoCount = this.printLottoCount.bind(this);
     this.printQuickPick = this.printQuickPick.bind(this);
-    this.serialize = this.serialize.bind(this);
   }
 
   readLine(query, callback) {
@@ -12,18 +11,10 @@ class View {
   }
 
   printLottoCount(lottoCount) {
-    Console.print(`\n${lottoCount}개를 구매했습니다.`);
+    Console.print(`${lottoCount}개를 구매했습니다.`);
   }
 
-  printQuickPick(quickPick) {
-    Console.print(this.serialize(quickPick));
-  }
-
-  serialize(quickPick) {
-    return quickPick.reduce(
-      (str, list) => (str += `${JSON.stringify(list)}\n`),
-      ""
-    );
+  printQuicPick(quickPick) {
   }
 }
 
