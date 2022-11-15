893ae790be3567676386a9825ece6d359eaafe9e
Minhye Kang <alsgp9898@naver.com>
2022-11-15 13:11:58 +0900

    feat: 입력받은 금액만큼 로또를 생성하는 기능 추가
    - 두 가지 기능을 하나로 합쳤으므로 README 파일 수정

--- src/LottoGenerator.js
+++ src/LottoGenerator.js
@@ -1,4 +1,4 @@
-const { print } = require('./util');
+const { print, random } = require('./util');
 const { checkLottoAmount } = require('./validation');
 
 class LottoGenerator {
@@ -6,6 +6,18 @@ class LottoGenerator {
     checkLottoAmount(money);
     this.lottoAmount = money / 1000;
     print(`${this.lottoAmount}개를 구매했습니다.`);
+    this.publishUserLotto();
+  }
+
+  publishUserLotto() {
+    const publishedUserLotto = [];
+
+    for (let index = 0; index < this.lottoAmount; index++) {
+      let eachUserLottoNumber = random();
+      publishedUserLotto.push(eachUserLottoNumber);
+      print(`[${eachUserLottoNumber.join(', ')}]`);
+    }
+    return publishedUserLotto;
   }
 }
 
