e8d16c31c01d135109726247963d94d392b29eef
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-15 20:23:48 +0900

    test: PrinterWithBonusBall 클래스가 조합한 문자열을 반환하는지 테스트

--- src/Components/PrinterWithBonusBall/PrinterWithBonusBall.test.js
+++ src/Components/PrinterWithBonusBall/PrinterWithBonusBall.test.js
@@ -1,16 +1,14 @@
 const PrinterWithBonusBall = require('./PrinterWithBonusBall');
-const { getLogSpy } = require('../../testFunction');
 
 describe('PrinPrinterWithBonusBallter 클래스 테스트', () => {
   test('일치하는 번호 개수, 보너스 볼 일치 여부, 상금, 당첨 개수를 출력한다.', () => {
-    const logSpy = getLogSpy();
     const printString = `5개 일치, 보너스 볼 일치 (30,000,000원) - 0개`;
-    new PrinterWithBonusBall()
+    const result = new PrinterWithBonusBall()
       .setLottoNumberCount('5')
       .setWinningMoney('30,000,000')
       .setMatchedLottoCount('0')
       .print();
 
-    expect(logSpy).toHaveBeenCalledWith(printString);
+    expect(printString).toBe(result);
   });
 });
