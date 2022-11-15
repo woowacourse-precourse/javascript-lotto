289ebbd5f5a0989574bd62e6d7c34dae273ab9f2
KUD <kimud6003@gmail.com>
2022-11-15 22:51:16 +0900

    ✨ Feature : User.js - buyLottos : 돈에 맞추어 로또 사기 - getLottoList : LottoList 출력

--- src/User/index.js
+++ src/User/index.js
@@ -1,39 +1,30 @@
+const { LOTTO_PRICE } = require('../Constants/number');
+const Lotto = require('../Lotto');
+const IO = require('../IOControl');
+const Utils = require('../Utils');
 /**
  * @Class User
  * @description: 로또를 구매하는 유저를 나타내는 클래스
  */
 class User {
-  #lottos = [];
-
+  #lottoList = [];
   constructor() {}
-
   /**
    * @param {Number Array} lotto
    * @description 로또번호를 받아서 유저의 로또목록에 추가한다
    */
-  addLotto(lotto) {
-    this.#lottos.push(lotto);
-  }
-
-  /**
-   * @param {Number Array} winning
-   * @param {Number} bonusNum
-   * @description 로또 당첨번호와 보너스번호를 받아서 유저의 로또목록에 당첨여부를 체크한다
-   */
-  getLottosStatic(winning, bonusNum) {
-    let prize = [0, 0, 0, 0, 0, 0];
-    for (let lotto of this.#lottos) {
-      let idx = lotto.isPrize(winning, bonusNum) - 1;
-      if (!isNaN(idx)) prize[idx]++;
+  buyLottos(money) {
+    let lottoCnt = money / LOTTO_PRICE;
+    IO.printLottoBuy(lottoCnt);
+    for (let i = 0; i < lottoCnt; i++) {
+      let lotto = new Lotto(Utils.genRndNums());
+      this.#lottoList.push(lotto);
+      IO.printLotto(lotto.getLotto());
     }
-    console.log(prize);
   }
 
-  /**
-   * @returns {Number Array} 로또목록을 반환한다
-   */
-  get showLottos() {
-    for (let lotto of this.#lottos) lotto.getLotto;
+  getLottoList() {
+    return this.#lottoList;
   }
 }
 
