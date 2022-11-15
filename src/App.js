e7eb95338c72f07cb6c1c15fce019b1247c26e18
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-15 13:43:46 +0900

    feat : 구입 금액 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  money;
+  play() {
+    this.inputBuyLottoMoney();
+  }
+
+  inputBuyLottoMoney() {
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+      this.money = parseInt(money);
+    });
+  }
+
 }
 
 module.exports = App;
