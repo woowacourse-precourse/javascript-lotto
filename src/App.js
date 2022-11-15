01c0288766196a7b7c885aead477d6c5826eca1b
Subin <80036437+hansb1023@users.noreply.github.com>
2022-11-15 14:18:00 +0900

    feature: 구입금액을 입력 받는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 class App {
-  play() {}
+  play() {
+    const MissionUtils = require("@woowacourse/mission-utils");
+
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요."), (purchase) => {
+      const purchaseInput = purchase;
+      console.log(purchaseInput);
+
+      if(purchaseInput % 1000 != 0) {
+        throw new Error('예외 발생- 1,000원 단위로 입력해주세요.');
+      }
+    };
+  
+  }
 }
 
 module.exports = App;
