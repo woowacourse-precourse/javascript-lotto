af6043d21918b1e2aed52821c9df0c297763e03e
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-15 23:00:35 +0900

    feat: add input function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const { Random, Console } = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+
 class App {
-  play() {}
+  play() {
+    const [amount, money] = createUserInput();
+
+  }
+}
+
+function createUserInput() {
+  let money = 0;
+  let amount = 0;
+  Console.readLine("구입금액을 입력해 주세요.", (num) => {
+    money = num;
+    amount = num / 1000;
+  });
+  return [amount, money];
 }
 
 module.exports = App;
