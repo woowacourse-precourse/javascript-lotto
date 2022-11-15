1c3f75631e9984f81dac614317e4e0a364d3d962
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-15 21:36:35 +0900

    test: 로또 모델 테스작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
-const Lotto = require("../src/Lotto");
+const Lotto = require("../src/model/Lotto");
+const { ERROR_MESSAGE } = require("../src/constants");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -15,4 +16,15 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또 번호가 1~45에 포함되지 않으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 56]);
+    }).toThrow(ERROR_MESSAGE.RANGE);
+  });
+
+  test("로또 번호가 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto(['a', 2, 3, 4, 5, 56]);
+    }).toThrow(ERROR_MESSAGE.NAN);
+  });
 });
