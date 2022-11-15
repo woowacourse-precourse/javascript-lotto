575268f1d4ed515187eda5d88a04b933ed369593
Raehyeon Jeong <raehy19@gmail.com>
2022-11-15 17:08:33 +0900

    feat(src/App.js) : 당첨번호 입력받고 예외처리 후 저장 메소드 추가

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ const Lotto = require('./Lotto');
 class App {
   #ticketCnt;
   #purchased;
+  #winNumbers;
 
   constructor() {
     this.#ticketCnt = 0;
     this.#purchased = [];
+    this.#winNumbers = [];
   }
 
   play() {
@@ -16,6 +18,7 @@ class App {
       this.#ticketCnt = money / 1000;
       this.#generateLotto();
       this.#printPurchasedLotto();
+      this.#getWinNum();
     });
   }
 
@@ -25,7 +28,6 @@ class App {
     if (money % 1000 !== 0)
       throw new Error('[ERROR] 금액은 1000원 단위로 입력해 주세요');
   }
-
   #stringHasNaN(money) {
     for (let i = 0; i < money.length; ++i) {
       if (
@@ -42,7 +44,6 @@ class App {
       this.#purchased.push(new Lotto(this.#numGenerator()));
     }
   }
-
   #numGenerator() {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
       (a, b) => a - b
@@ -52,6 +53,45 @@ class App {
   #printPurchasedLotto() {
     MissionUtils.Console.print(`\n${this.#ticketCnt}개를 구매했습니다.`);
     this.#purchased.map((lotto) => lotto.printNumbers());
+  }
+
+  #getWinNum() {
+    MissionUtils.Console.readLine(
+      '\n당첨 번호를 입력해 주세요.\n',
+      (winNum) => {
+        this.#inputWinNumExceptionCheck(winNum);
+      }
+    );
+  }
+
+  #inputWinNumExceptionCheck(winNum) {
+    winNum.split(',').forEach((num) => this.#winNumbers.push(parseInt(num)));
+    this.#validateInputWinNum();
+  }
+  #validateInputWinNum() {
+    if (this.#winNumbers.length !== 6) {
+      throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
+    }
+    if (this.#arrHasNaN(this.#winNumbers)) {
+      throw new Error('[ERROR] 당첨 번호는 숫자여야 합니다.');
+    }
+    if ([...new Set(this.#winNumbers)].length !== 6) {
+      throw new Error('[ERROR] 당첨 번호는 중복될 수 없습니다.');
+    }
+    if (this.#outOfRange(this.#winNumbers)) {
+      throw new Error('[ERROR] 로또 번호 1 이상 45 이하여야만 합니다.');
+    }
+  }
+  #arrHasNaN(numbers) {
+    for (let i = 0; i < numbers.length; ++i) {
+      if (typeof numbers[i] !== 'number') return true;
+    }
+    return false;
+  }
+  #outOfRange(numbers) {
+    for (let i = 0; i < numbers.length; ++i) {
+      if (numbers[i] < 1 || numbers[i] > 45) return true;
+    }
   }
 }
 
