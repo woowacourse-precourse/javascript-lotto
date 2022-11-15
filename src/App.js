70b3eb5aff9ca1f9c91fed151eb976e59eab7276
lineJeong <line.jeong32@gmail.com>
2022-11-15 22:08:42 +0900

    FEAT: 3개~6개 일치에 해당하는 당첨 내역 출력

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App {
     this.lottoArrays = [];
     this.winningNumbers = [];
     this.bonusNumber = 0;
+    this.winningRanks = { first: 0, second: 0, third: 0, fourth: 0, fifth: 0 };
   }
 
   play() {
@@ -57,7 +58,58 @@ class App {
     Console.readLine("\n보너스 번호를 입력해 주세요.\n", (number) => {
       this.bonusNumber = Number(number);
       new Bonus(this.bonusNumber, this.winningNumbers);
+
+      this.getWinningStatistics();
     });
+  }
+
+  getWinningStatistics() {
+    for (let lottoNumbers of this.lottoArrays) {
+      this.compareLottoNumbers(lottoNumbers);
+    }
+    this.printWinningStatistics();
+  }
+
+  compareLottoNumbers(lottoNumbers) {
+    let count = 0;
+    for (let winningNumber of this.winningNumbers) {
+      if (lottoNumbers.includes(winningNumber)) count++;
+    }
+    this.getWinningRanks(count, lottoNumbers);
+  }
+
+  getWinningRanks(count, lottoNumbers) {
+    switch (count) {
+      case 3:
+        this.winningRanks.fifth++;
+        break;
+
+      case 4:
+        this.winningRanks.fourth++;
+        break;
+
+      case 5:
+        lottoNumbers.includes(this.bonusNumber)
+          ? this.winningRanks.second++
+          : this.winningRanks.third++;
+        break;
+
+      case 6:
+        this.winningRanks.first++;
+        break;
+    }
+  }
+
+  printWinningStatistics() {
+    Console.print("\n당첨 통계");
+    Console.print("---");
+    Console.print(`3개 일치 (5,000원) - ${this.winningRanks.fifth}`);
+    Console.print(`4개 일치 (50,000원) - ${this.winningRanks.fourth}`);
+    Console.print(`5개 일치 (1,500,000원) - ${this.winningRanks.third}`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.winningRanks.second}`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${this.winningRanks.first}`);
   }
 }
 
