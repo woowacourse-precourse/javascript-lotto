196e429e123c906e3c674c925960c45a24723835
ywoo12321 <ywoo12321@gmail.com>
2022-11-15 21:14:53 +0900

    test: 유효성 검사에 대한 test 항목 추가 후 검사 완료

--- src/Lotto.js
+++ src/Lotto.js
@@ -16,14 +16,14 @@ class Lotto {
   }
   isLengthSix(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error("[ERROR] : 로또 번호는 6개여야 합니다.");
     }
     return true;
   }
   isNumber(numbers) {
     numbers.map((number) => {
       if (isNaN(number)) {
-        throw new Error("[ERROR] 숫자만 입력 가능합니다.");
+        throw new Error("[ERROR] : 숫자만 입력 가능합니다.");
       }
     });
     return true;
@@ -32,7 +32,7 @@ class Lotto {
     const numberList = [];
     numbers.map((number) => {
       if (numberList.includes(number)) {
-        throw new Error("[ERROR] 중복된 숫자가 있습니다.");
+        throw new Error("[ERROR] : 중복된 숫자가 있습니다.");
       }
       numberList.push(number);
     });
@@ -41,7 +41,7 @@ class Lotto {
   isInRange(numbers) {
     numbers.map((number) => {
       if (number < 1 || number > 45) {
-        throw new Error("[ERROR] 1~45 사이의 숫자만 입력 가능합니다.");
+        throw new Error("[ERROR] : 1~45 사이의 숫자만 입력 가능합니다.");
       }
     });
     return true;
