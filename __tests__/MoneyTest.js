fb63165372a0e7c1a3c61863c28f90b1799037e3
khkh0109 <ist200120@gmail.com>
2022-11-15 11:21:00 +0900

    test(MoneyTest): Money 테스트 코드 추가

--- __tests__/MoneyTest.js
+++ __tests__/MoneyTest.js
@@ -3,15 +3,13 @@ const Money = require("../src/Money");
 describe("Money 클래스 테스트", () => {
   test("구매금액이 1,000원으로 나누어 떨어지지 않는 경우 예외가 발생한다.", () => {
     expect(() => {
-      const money = new Money();
-      money.validate("1001");
+      new Money("1001");
     }).toThrow("[ERROR]");
   });
 
   test("구매금액이 숫자 외의 문자인 경우 예외가 발생한다.", () => {
     expect(() => {
-      const money = new Money();
-      money.validate("12ap8");
+      new Money("12ap8");
     }).toThrow("[ERROR]");
   });
 });
