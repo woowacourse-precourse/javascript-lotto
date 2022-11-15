6e1ea5562e1fb95696095fdf075e92659a965267
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-11-15 19:07:06 +0900

    fix : 입력값 오류처리 버그 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,3 @@
-// 당첨번호를 입력받고, 비교
 class Lotto {
   #numbers;
 
@@ -6,7 +5,7 @@ class Lotto {
     this.validate(numbers);
     this.#numbers = numbers;
   }
-
+  // 당첨번호를 입력받고, 오류처리
   validate(numbers) {
     if (!numbers.every(Number)) {
       throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
@@ -14,34 +13,13 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    if (numbers.every((number) => number >= 1 && number <= 45)) {
-      throw new Error("[ERROR] 로또 번호는 1~45범위 내의 숫자여야 합니다.");
+    if (!numbers.every((number) => 1 <= number && number <= 45)) {
+      throw new Error("[ERROR] 로또 번호는 1 ~ 45범위 내의 숫자여야 합니다.");
     }
-    const overlap = new Set(numbers);
-    if (overlap.size > 0) {
+    //const overlap
+    if (new Set(numbers).size !== 6) {
       throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
     }
-  }
-
-  // 일치 갯수 반환
-  matchLotto(lottosNumbers, winningNumber, bonusNumber) {
-    let matchingCountArr = [];
-    let bonusMatch = [];
-    for (let i = 0; i < lottosNumbers.length; i++) {
-      const lottoPiece = lottosNumbers[i];
-      const matchingNumber = winningNumber.filter((num) =>
-        lottoPiece.includes(num)
-      );
-      if (
-        matchingNumber.length === 5 &&
-        lottosNumbers[i].includes(bonusNumber)
-      ) {
-        bonusMatch.push(bonusNumber);
-      } else {
-        matchingCountArr.push(matchingNumber.length);
-      }
-    }
-    return [matchingCountArr, bonusMatch];
   }
 }
 
