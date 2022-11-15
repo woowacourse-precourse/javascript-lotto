ba02b19b9bdcded8efe71654e062ffde633c7e40
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-15 12:51:21 +0900

    feat(method): add getInformation
    Add getInformation method to App class
    - Get cash input from user
    - Purchase lottos with cash
    - Get winning numbers and a bonus number from user
    - Return information of winning numbers, bonus number, purchased lottos, and cash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const UI = require("./UI");
+const LottoArray = require("./LottoArray");
+
 class App {
   play() {}
+
+  async getInformation() {
+    const cash = await UI.getCash();
+
+    const purchased = new LottoArray(cash);
+
+    UI.printPurchasedLottoStatus(purchased);
+
+    const { winningNumbers, bonusNumber } = await UI.getWinningNumbers();
+
+    return {
+      winningNumbers,
+      bonusNumber,
+      purchased,
+      cash,
+    };
+  }
 }
 
 module.exports = App;
