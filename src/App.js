be125204e28c979c372d15c53269b9de10518f1e
spsp8426 <spsp8426@naver.com>
2022-11-15 09:31:54 +0900

    style : 주석제거

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
 class App {
   constructor() {
-    this.money = null; // 금액 입력 숫자
-    this.lotto = []; // 로또 배열로 받기
-    this.winNum = []; //당첨숫자 배열로 받기
-    this.bonusNum = null; //보너스 번호 숫자
+    this.money = null;
+    this.lotto = []; 
+    this.winNum = [];
+    this.bonusNum = null;
   }
   setMoney() {
     Console.readLine("구입금액을 입력해 주세요.\n",(num) => {
