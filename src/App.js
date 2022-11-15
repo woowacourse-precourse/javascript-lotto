45210579075d576a6ccdcb2d191318cbec32b392
jinyong <zhaqh146@naver.com>
2022-11-15 14:44:37 +0900

    feat: 로또 구입 금액만큼 로또를 구입하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 const { Console } = require('@woowacourse/mission-utils');
 const ValidatePay = require('./ValidatePay');
+const makeLotto = require('./makeLotto');
 
 class App {
+
   play() {
+    this.buyLottoArray = [];
     this.inputPay();
   };
 
   inputPay() {
     Console.readLine(`구입금액을 입력해 주세요.\n`, answer => {
-      const validatepay = new ValidatePay(answer);
+      const validatePay = new ValidatePay(answer);
+      this.buyLotto(validatePay.count());
     });
   };
 
+  buyLotto(count) {
+    Console.print(`\n${count}개를 구매했습니다.`);
+
+    for (let i = 0; i < count; i++) {
+      const buyLottoElement = makeLotto();
+      this.buyLottoArray.push(buyLottoElement);
+      Console.print(buyLottoElement);
+    }
+  };
 }
 
 const app = new App();
