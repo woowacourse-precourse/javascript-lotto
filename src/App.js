febd16ae27b5ba89513a3f73727a929fc9978c75
semnil5202 <semnil5202@gmail.com>
2022-11-15 10:53:28 +0900

    feat: 당첨 내역과 수익률을 출력하는 기능

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-const IssueLotto = require('./components/IssueLotto');
 const { Console } = require('@woowacourse/mission-utils');
+const IssueLotto = require('./components/IssueLotto');
+const JudgeLotto = require('./components/JudgeLotto');
+const WinStatistics = require('./components/WinStatistics');
+const EarningRate = require('./components/EarningRate');
 
 class App {
-  play() {}
+  play() {
+    this.buyLotto();
+  }
 
   buyLotto() {
     Console.readLine('구입금액을 입력해 주세요.\n', (input) => {
@@ -26,7 +31,29 @@ class App {
   typedBonusNumber({ lottos, wins, purchase }) {
     Console.readLine('\n보너스 번호를 입력해 주세요.\n', (input) => {
       const bonus = Number(input);
+      this.printWinStatistics({ lottos, wins, bonus, purchase });
     });
+  }
+
+  printWinStatistics({ lottos, wins, bonus, purchase }) {
+    const judgeLotto = JudgeLotto.compareLotto({ lottos, wins, bonus });
+    const winPlace = WinStatistics.getWinStatistics(judgeLotto);
+    Console.print('\n당첨 통계');
+    Console.print('---');
+    Console.print(`3개 일치 (5,000원) - ${winPlace.fifthPlace}개`);
+    Console.print(`4개 일치 (50,000원) - ${winPlace.fourthPlace}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${winPlace.thirdPlace}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winPlace.secondPlace}개`,
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${winPlace.firstPlace}개`);
+    this.printEarningRate({ winPlace, purchase });
+  }
+
+  printEarningRate({ winPlace, purchase }) {
+    const earningRate = EarningRate.getEarningRate({ winPlace, purchase });
+    Console.print(`총 수익률은 ${earningRate}%입니다.`);
+    Console.close();
   }
 }
 
