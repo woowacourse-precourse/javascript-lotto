1a48e72f74b50c2ec32da85ccdee01858eedd6fa
Hoon <si932174@gmail.com>
2022-11-15 15:28:35 +0900

    feat : 예외사항 추가<-보너스 번호가 1~45 범위를 벗어난 경우

--- src/BonusValidate.js
+++ src/BonusValidate.js
@@ -6,6 +6,8 @@ class BonusValidate {
   static validate(bonusNumber) {
     if (/[^0-9]/g.test(bonusNumber))
       throw new Error('[ERROR] 보너스 번호는 숫자여야 합니다.');
+    if (bonusNumber > 45 || bonusNumber < 1)
+      throw new Error('[ERROR] 보너스 번호는 1~45 범위여야 합니다.');
   }
 }
 
