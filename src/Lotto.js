45e26d299cc091a89444edade78f74034843a98b
xownsdl0 <xownsdl0@naver.com>
2022-11-15 17:40:43 +0900

    feat(lotto): 구매한 로또 중에서 맞은 개수가 3 ~ 6개 사이인 로또를 찾는 코드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,4 @@
-const inputDataObject = require('../src/model/InputDataObject')
+const inputDataObject = require('../src/model/InputDataObject');
 
 class Lotto {
   #numbers;
@@ -9,29 +9,30 @@ class Lotto {
   }
 
   validate(numbers) {
-    const regex = /^[0-9]+$/
-    
+    const regex = /^[0-9]+$/;
+
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
-    numbers.filter((size) => {
+    numbers.filter(size => {
       if (!(size >= 1 && size <= 45) || !regex.test(size)) {
-        throw new Error('[ERROR] 입력 범위를 초과합니다.')
+        throw new Error('[ERROR] 입력 범위를 초과합니다.');
       }
-    })
-    if(new Set(numbers).size < numbers.length) {
-      throw new Error('[ERROR] 중복되는 숫자가 존재합니다.')
+    });
+    if (new Set(numbers).size < numbers.length) {
+      throw new Error('[ERROR] 중복되는 숫자가 존재합니다.');
     }
-    return this.compareLotto()
+    return this.compareLotto();
   }
 
   compareLotto() {
-    inputDataObject.correctNumber = inputDataObject.raffle.map(index =>
-      inputDataObject.raffleNumbers.filter(x => index.includes(x)),
-    );
-    return null
+    inputDataObject.correctNumber = inputDataObject.raffle
+      .map(index =>
+        inputDataObject.raffleNumbers.filter(x => index.includes(x)),
+      )
+      .filter(index => index.length > 2)
+    return this.averageReturn();
   }
-
 }
 
 module.exports = Lotto;
