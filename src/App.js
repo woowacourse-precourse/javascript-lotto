d5a86232560225be9f79798f818586285ead9c40
Dongja <jdh981118@naver.com>
2022-11-15 22:35:07 +0900

    App.js: input function make

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ class App {
     Rotto();
     print();
   }
+
+  Input(){
+    let money;
+    Console.readLine('구입금액을 입력해 주세요.', (input) => {
+      money = parseInt(input);
+    });
+    if(money < 1000 || money % 1000 !== 0)
+      throw new error("잘못된 입력입니다.");
+    return ;
+  }
 }
 
 module.exports = App;
