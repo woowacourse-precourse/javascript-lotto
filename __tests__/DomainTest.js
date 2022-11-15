7e6e94c73158304ce2d01964e6be4eacf5d06dde
violetwhenisawu <anashape710@gmail.com>
2022-11-15 19:28:32 +0900

    test: 로또 수량대로 배열이 반환되는지 확인하는 테스트코드 작성

--- __tests__/DomainTest.js
+++ __tests__/DomainTest.js
@@ -7,4 +7,11 @@ describe('도메인 로직 테스트', () => {
 
     expect(GET_RANDOM_NUM.getQuantity(input)).toBe(14);
   });
+
+  test('로또를 구입한 수량대로 배열이 반환되는지 확인', () => {
+    const amount = 14000;
+    const result = GET_RANDOM_NUM.getLottoNum(amount);
+
+    expect(result.lottoNums).toHaveLength(14);
+  });
 });
