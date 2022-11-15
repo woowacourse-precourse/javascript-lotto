9a25e1b9cdff2478628ce45abe51e5ce507138fb
seungki <sysh9498@gmail.com>
2022-11-15 20:04:22 +0900

    feat(HitLottoNumber): 보너스 번호 입력 기능

--- src/HitLottoNumber.js
+++ src/HitLottoNumber.js
@@ -1,5 +1,7 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const { Console } = MissionUtils;
+const { INPUT_HIT_NUMBER_MESSAGE, INPUT_BONUS_NUMBER_MESSAGE } = require('./Const');
+const Validation = require('./Validation');
 
 class HitLottoNumber {
   constructor() {
@@ -9,12 +11,23 @@ class HitLottoNumber {
   static inputHitLottoNumber() {
     let hitLottoNumber = [];
     Console.readLine(INPUT_HIT_NUMBER_MESSAGE, input => {
-      // 정답 로또 번호에 대한 예외처리
+      Validation.checkInputHitLottoNumber(input);
       hitLottoNumber = input.split(',').map(Number);
       this.hitLottoNumber = input.split(',');
       Console.close();
     });
 
     return hitLottoNumber;
+  }
+
+  static inputBonusNumber() {
+    let bonusNumber = null;
+    Console.readLine(INPUT_BONUS_NUMBER_MESSAGE, input => {
+      // 보너스 번호 입력에 대한 예외처리
+      bonusNumber = Number(input);
+      Console.close();
+    });
+
+    return bonusNumber;
   }
 }
