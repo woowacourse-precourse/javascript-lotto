31c9eec9dbf50891c29083c9ccf4ebda3545ea53
Sanggil Han <tkdrlf95@naver.com>
2022-11-15 22:53:58 +0900

    test(LottoMachineTest): Refactor function name
    generator 함수를 generatorNumbers 함수로 이름을 변경하였음

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -4,13 +4,13 @@ describe("로또머신 클래스 테스트", () => {
   
   test("제너레이터 테스트", () => {
     const lottoMachine = new LottoMachine();
-    const numbers = lottoMachine.generate();
+    const numbers = lottoMachine.generateNumbers();
     expect(numbers).toHaveLength(6);
   });
 
   test("번호 테스트", () => {
     const lottoMachine = new LottoMachine();
-    const numbers = lottoMachine.generate();
+    const numbers = lottoMachine.generateNumbers();
     numbers.forEach((number) => {
       expect(number).toBeGreaterThanOrEqual(number, 1);
       expect(number).toBeLessThanOrEqual(number, 45);
@@ -19,8 +19,8 @@ describe("로또머신 클래스 테스트", () => {
 
   test("매번 다른 번호를 뽑아 내는지 확인", () => {
     const lottoMachine = new LottoMachine();
-    const numbers1 = lottoMachine.generate();
-    const numbers2 = lottoMachine.generate();
+    const numbers1 = lottoMachine.generateNumbers();
+    const numbers2 = lottoMachine.generateNumbers();
     const duplicatedNumber = numbers1.filter((number, idx) => number === numbers2[idx]);
     expect(duplicatedNumber).not.toHaveLength(6);
   });
