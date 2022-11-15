b5f40b03d0c1c8e898c280c291317a4a191d7b9b
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-15 22:33:01 +0900

    test: UserBudget class 기능 테스트 추가

--- __tests__/UserBudgetTest.js
+++ __tests__/UserBudgetTest.js
@@ -1,6 +1,6 @@
 const UserBudget = require('../src/UserBudget');
 
-describe.only('버짓 클래스 테스트', () => {
+describe.only('UserBudget class 테스트', () => {
   test('1000단위로 안 떨어지면 에러가 발생한다.', () => {
     expect(() => {
       new UserBudget(10002);
