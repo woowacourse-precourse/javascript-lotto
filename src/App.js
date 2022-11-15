6cc71f455c5cec59e36c43a0714832a2a961d3ba
chaeeun-seo <seochaeeun961126@gmail.com>
2022-11-15 12:05:30 +0900

    feat : get total money that was spent for lotto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  // 구입금액을 입력받음
+  getMoney() {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (answer) => {
+      return this.getLottoNumber(answer);
+    });
+  }
+
+  play() {
+    this.getMoney();
+  }
 }
 
 const app = new App();
