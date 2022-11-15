eac9c1f6e7961f886cb28aee648b8fc72188cf58
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-15 15:29:57 +0900

    Test: Lotto 스택 초과 문제인지 확인차

--- src/User.js
+++ src/User.js
@@ -42,8 +42,9 @@ class User {
     for (let i = 0; i < count; i++) {
 			let randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
 			randomNumbers.sort((a, b) => (a - b));
-			let newLotto = new Lotto(randomNumbers);
-			this.lottoBundle.push(newLotto.getLotto());
+			// let newLotto = new Lotto(randomNumbers);
+			let newLotto = randomNumbers;
+			this.lottoBundle.push(newLotto);
     }
 	}
 }	
