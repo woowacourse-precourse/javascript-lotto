1f9dbeab69836c4e54f7d363ddcf58cb28b691d2
Lim_JiSeon <dpf226@naver.com>
2022-11-15 23:04:34 +0900

    feat(App, Lotto.js) : update functions and fix functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+const Lotto = require("./Lotto");
+
 class App {
   inputValue = {
     winningNumbers : [],
-    bonusNumber : 0,
-    purchaseAmount : 0
+    bonusNumber : null,
+    purchaseAmount : null,
+    purchaseLotto : []
   }
 
   winningCount = {
@@ -12,28 +15,38 @@ class App {
     "5+" : 0,
     "6" : 0
   }
-  
+
   inputPurchaseAmount() {
     Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
-      this.inputValue[purchaseAmount] = money;
+      this.validateMoney(Number(money))
+      this.inputValue[purchaseAmount] = Number(money);
     });
+    Console.close();
+  }
+
+  validateMoney(money) {
+    if (money%1000 !== 0) {
+      throw new Error("[ERROR] 구입 금액을 1000원 단위로 입력해주세요.");
+    }
   }
 
   inputWinningNumbers() {
     Console.readLine('당첨 번호를 입력해 주세요.\n', (numbers) => {
-      this.inputValue[winningNumbers] = numbers.split(',');
+      changeNumber(numbers.split(','));
     });
+    Console.close();
   }
 
   inputBonusNumber() {
     Console.readLine('보너스 번호를 입력해 주세요.\n', (number) => {
-      this.inputValue[bonusNumber] = parseInt(number);
+      this.inputValue[bonusNumber] = Number(number);
     });
+    Console.close();
   }
 
-  changeNumber() {
+  changeNumber(numbers) {
     const toNumbers = arr => arr.map(Number);
-    this.inputValue[winningNumbers] = toNumbers(this.inputValue[winningNumbers]);
+    this.inputValue[winningNumbers] = toNumbers(numbers);
   }
 
   calculateWinningAmount() {
@@ -44,7 +57,54 @@ class App {
     return Number((this.calculateWinningAmount/money*100).toFixed(1));
   }
 
-  play() {}
+  printLottoList(frequency) {
+    Console.print(frequency + "개를 구매했습니다.");
+    Console.close();
+    
+    for (let index = 0; index<frequency; index++) {
+      Console.print(this.inputValue[purchaseLotto[index]]);
+    }
+    Console.close();
+  }
+
+  printWinningStatistics() {
+    //Console.print("당첨 통계");
+    //Console.print("---");
+    Console.print("3개 일치 (5,000원) - " + this.inputValue[this.winningCount["3"]] + "개");
+    Console.print("4개 일치 (50,000원) - " + this.inputValue[this.winningCount["4"]] + "개");
+    Console.print("5개 일치 (1,500,000원) - " + this.inputValue[this.winningCount["5"]] + "개");
+    Console.print("5개 일치, 보너스 볼 일치 (30,000,000원) - " + this.inputValue[this.winningCount["5+"]] + "개");
+    Console.print("6개 일치 (2,000,000,000원) - " + this.inputValue[this.winningCount["6"]] + "개");
+    Console.print("총 수익률은 " + this.calculateYield(this.inputValue[purchaseAmount]) + "%입니다.");
+    Console.close();
+    //Console.print("---");
+  }
+
+  checkBonusException() {
+    const tempNumbers = this.inputValue[winningNumbers];
+    tempNumbers.push(this.inputValue[bonusNumber]);
+    return tempNumbers;
+  }
+
+  play() {
+    this.inputPurchaseAmount();
+
+    const frequency = this.inputValue[purchaseAmount]/1000;
+    this.inputValue[purchaseLotto] = rotto.createLotto(frequency);
+    this.printLottoList(frequency);
+
+    this.inputWinningNumbers();
+    const rotto = new Lotto(this.inputValue[winningNumbers]);
+    rotto.validate(this.inputValue[winningNumbers]);
+    rotto.deduplication(this.inputValue[winningNumbers]);
+    rotto.outerRange(this.inputValue[winningNumbers]);
+
+    this.inputBonusNumber();
+    rotto.deduplication(this.checkBonusException());
+    rotto.outerRange(this.checkBonusException());
+
+    printWinningStatistics();
+  }
 
 
 }
--- src/Lotto.js
+++ src/Lotto.js
@@ -16,27 +16,25 @@ class Lotto {
 
   deduplication(numbers) {
     const uniq = array => [...new Set(array)];
-    if (uniq(numbers).length !== 6) {
+    if (uniq(numbers).length !== numbers.length) {
       throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
     }
   }
 
   outerRange(numbers) {
     numbers.forEach( number => {
-      if (number > 0 && number < 46) {
+      number = Number(number);
+      if ( number < 1 && number > 45) {
         throw new Error("[ERROR] 로또 번호의 숫자 범위는 1 ~ 45까지 입니다.");
       }
     });
   }
 
-  validateMoney(money) {
-    if (money%1000 !== 0) {
-      throw new Error("[ERROR] 구입 금액을 1000원 단위로 입력해주세요.");
-    }
-  }
-
   randomGeneration() {
     const randomLotto = Random.pickUniqueNumbersInRange(1, 45, 6);
+    this.validate(randomLotto);
+    this.deduplication(randomLotto);
+    this.outerRange(randomLotto);
     randomLotto.sort((a, b) => a - b);
     return randomLotto;
   }
