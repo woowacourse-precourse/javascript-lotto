3dd462909869a2e894b7cc8903faf6d6db5e015b
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-15 18:52:41 +0900

    feat: 당첨 번호를 입력 받는 기능의 예외처리   - src/App.js : 입력값이 쉼표로 구분된 6개의 수로 유효한지 확인하는 메소드 isValidWinNumberInput()을 구현하여 예외 처리를 추가했습니다.   - src/Lotto.js : validate() 메소드에 로또 번호가 1과 45 사이의 수가 아닌 경우의 예외 처리를 추가했습니다.   - __tests__/LottoTest.js : 로또 번호가 1과 45 사이의 수가 아닌 경우 예외가 발생하는지 확인하기 위한 테스트를 구현했습니다.   - docs/README.md : 구현 완료한 기능에 체크 표시

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 const { Console, Random } = require("@woowacourse/mission-utils/");
 const User = require("./User")
+const Lotto = require("./Lotto")
 
 class App {
   #User;
@@ -20,11 +21,17 @@ class App {
 
   getWinLottoNumber() {
     Console.readLine('\n당첨 번호를 입력해 주세요.', (answer) => {
+      if (!this.isValidWinNumberInput(answer)) {
+        throw new Error("[ERROR] 로또 번호는 쉼표로 구분된 6개의 수를 입력해야 합니다.");
+      }
       const numbers = answer.split(',').map(Number);
-      this.#Lotto = new this.#Lotto(numbers)
+      this.#Lotto = new Lotto(numbers);
     })
   }
 
+  isValidWinNumberInput(answer) {
+    return (/^(\d{1,2},){5}\d{1,2}$/).test(answer);
+  }
 }
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -7,9 +7,14 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않는 수여야 합니다.");
     }
+    numbers.forEach(number => {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1과 45 사이의 수여야 합니다.");
+      }
+    });
   }
 
   // TODO: 추가 기능 구현
