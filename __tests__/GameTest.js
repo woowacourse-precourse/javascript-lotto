1755389e740e5bf99b5d1e47f3b6f4ba38bd9bd7
Deemou <4106243@naver.com>
2022-11-15 20:01:13 +0900

    test(Game): printPurchaseResult() 테스트 케이스 추가

--- __tests__/GameTest.js
+++ __tests__/GameTest.js
@@ -1,4 +1,11 @@
 const Game = require('../src/Game');
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe('게임 클래스 테스트', () => {
   test('주어진 수만큼 로또를 발행한다.', () => {
@@ -6,5 +13,25 @@ describe('게임 클래스 테스트', () => {
     const num = 5;
     const lottoList = game.issueLotto(num);
     expect(lottoList.length).toBe(num);
+  });
+
+  test('발행한 로또 수량과 로또 번호를 출력한다.', () => {
+    const lottoList = [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44]
+    ];
+    const logs = [
+      '3개를 구매했습니다.',
+      '[8, 21, 23, 41, 42, 43]',
+      '[3, 5, 11, 16, 32, 38]',
+      '[7, 11, 16, 35, 36, 44]'
+    ];
+    const logSpy = getLogSpy();
+    const game = new Game();
+    game.printPurchaseResult(lottoList);
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
   });
 });
