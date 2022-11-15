0d85277f844d9d4b4fdf7a2107ac2a2358c31cd8
soomin <qkrtnals970804@gmail.com>
2022-11-15 22:24:38 +0900

    feat: 당첨 번호 예외처리하기

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+const { print } = require('./utils');
 class Lotto {
   #numbers;
 
@@ -9,6 +10,9 @@ class Lotto {
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    if (new Set(numbers).size !== 6) {
+      throw  new Error("[ERROR] 로또 번호가 중복됩니다.");
     }
   }
 
