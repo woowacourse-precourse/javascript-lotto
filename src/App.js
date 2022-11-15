24a450096d890ce314988fe13aeeb4569ee0e12c
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2022-11-16 02:37:29 +0900

    feat : 구입 금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const Mission = require("@woowacourse/mission-utils");
+const Validator = require("./Validator");
 class App {
-  play() {}
-}
+  play() {
+    this.lottoPrice();
+  }
 
+  lottoPrice = () => {
+    Mission.Console.readLine("구입금액을 입력해 주세요.", (input) => {
+      Validator.purchaseInput(input);
+      Mission.Console.print(`${input / 1000}개를 구매했습니다.`);
+    });
+  };
+}
 module.exports = App;
