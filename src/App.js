965bbe7ae9336d243d31b611a08bbc2fa9a93ac1
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-15 13:44:09 +0900

    feat : 숫자만 입력 받기

--- src/App.js
+++ src/App.js
@@ -7,10 +7,18 @@ class App {
 
   inputBuyLottoMoney() {
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+      this.validateIsNumber(money);
+
       this.money = parseInt(money);
     });
   }
 
+
+  validateIsNumber(money) {
+    if (isNaN(money)) {
+      throw new Error("[ERROR] 숫자만 입력 가능합니다.");
+    }
+  }
 }
 
 module.exports = App;
