6d0bc59719a5e3ee0eed05d3957008d38e8675f8
JiYoon Lee <wldbseja@daum.net>
2022-11-15 23:27:29 +0900

    feat:구매 금액 입력 문구 출력 및 사용자 입력 후 메서드 순서대로 호출 기능

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { APP_MESSAGE, PRINT_LOTTO_MATCH } = require("./constants");
+
+const LottoGame = require("./LottoGame");
+
+class App {
+  constructor() {
+    this.lottoGame = new LottoGame();
+  }
+
+  // [v][구매금액 입력 문구출력 및 사용자 입력 후에 메서드 순서대로 호출 기능]
+  readInputMoney() {
+    Console.readLine(APP_MESSAGE.INPUT_AMOUNT, (userInputString) => {
+      const inputMoney = Number(userInputString);
+      this.lottoGame.setInputMoney(inputMoney);
+
+      this.printBuyResult();
+    });
+  }
 class App {
   play() {}
 }
