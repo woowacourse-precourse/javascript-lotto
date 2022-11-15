5a19546fdfd8bdef27aacc26920dddecb24d19cd
Lim_JiSeon <dpf226@naver.com>
2022-11-15 16:36:25 +0900

    feat(App.js) : update function - inputPurchaseAmount - inputWinningNumbers - inputBonusNumber

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 class App {
+  inputValue = {
+    winningNumbers : [],
+    bonusNumber : 0,
+    purchaseAmount : 0
+  }
+
   winningCount = {
     "3" : 0,
     "4" : 0,
@@ -7,6 +13,24 @@ class App {
     "6" : 0
   }
   
+  inputPurchaseAmount() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
+      this.inputValue[purchaseAmount] = money;
+    });
+  }
+
+  inputWinningNumbers() {
+    Console.readLine('당첨 번호를 입력해 주세요.\n', (numbers) => {
+      this.inputValue[winningNumbers] = numbers.split(',');
+    });
+  }
+
+  inputBonusNumber() {
+    Console.readLine('보너스 번호를 입력해 주세요.\n', (number) => {
+      this.inputValue[bonusNumber] = number;
+    });
+  }
+
   calculateWinningAmount() {
     return 5000*this.winningCount["3"] + 50000*this.winningCount["4"] + 1500000*this.winningCount["5"] + 30000000*this.winningCount["5+"] + 2000000000*this.winningCount["6"];
   }
