aa282735032b93fceff2726026099b5e0d658663
yeongseokim <1903020@ewhain.net>
2022-11-15 21:41:22 +0900

    chore(LottoTest): 에러 메시지 상수화

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,22 +1,23 @@
 const Lotto = require("../src/Lotto");
+const ERROR_MESSAGE = require("../src/constants").ERROR_MESSAGE;
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR] 로또 번호는 6개여야 합니다.");
+    }).toThrow(ERROR_MESSAGE.LOTTO_NUMBERS_NOT_SIX_DIGITS);
   });
 
   test("로또 번호의 개수가 6개가 미만이면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5]);
-    }).toThrow("[ERROR] 로또 번호는 6개여야 합니다.");
+    }).toThrow(ERROR_MESSAGE.LOTTO_NUMBERS_NOT_SIX_DIGITS);
   });
 
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR] 로또 번호는 중복되지 않는 6자리 숫자여야 합니다.");
+    }).toThrow(ERROR_MESSAGE.LOTTO_NUMBERS_DUPLICATE);
   });
 
   test("로또 get 접근자 프로퍼티 확인", () => {
