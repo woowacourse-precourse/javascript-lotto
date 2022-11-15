8f0ad66fb32712f556585bd72af68167d92eba35
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2022-11-15 21:45:19 +0900

    feat : LottoResult 파일 생성 및 생성자 set

---
+++ src/LottoResult.js
@@ -0,0 +1,18 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { FIVE, LOTTO_MATCHES, LOTTO_PRIZE, LOTTO_PRICE, RESULT_MESSAGE } = require("./constants");
+
+class LottoResult {
+  constructor() {
+    this.profitRate = 0;
+    this.lottoMatchCounter = {
+      three: 0,
+      four: 0,
+      five: 0,
+      fiveWithBonus: 0,
+      six: 0,
+      out: 0,
+    };
+  }
+}
+
+module.exports = LottoResult;
