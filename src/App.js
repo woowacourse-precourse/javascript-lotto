297ab6bd612727c9865f20b9e4dad2dc7c241da6
KangYunJi <dbswl701@ajou.ac.kr>
2022-11-15 11:51:12 +0900

    feat: 사용자 로또 번호 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Console = MissionUtils.Console;
+const Random = MissionUtils.Random;
 const Lotto = require("./Lotto");
 
 class App {
+  constructor() {
+    this.lottos=[];
+    this.lottoCount=0;
+  }
+
+  CreateUserLotto(){
+    for(let idx=0; idx<this.lottoCount; idx++){
+      this.lottos.push(Random.pickUniqueNumbersInRange(1, 45, 6));
+      console.log(this.lottos[idx]);
+    }
+    return this.lottos;
+  }
 
   isValidCacheInput(answer){
-    const lottoCount = answer/1000;
-    if(!Number.isInteger(lottoCount)) {
+    const lottotemp = answer/1000;
+    if(!Number.isInteger(lottotemp)) {
       throw new Error("[ERROR] 이상한 입력");
     }
-    return lottoCount;
+    return lottotemp;
   }
 
 
   InputCache(){
     Console.readLine('구입금액을 입력해 주세요.', (answer)=>{
       // 맞는지 확인하는 함수
-      const lottoCount = this.isValidCacheInput(answer);
-      Console.print(`${lottoCount}개를 구매했습니다.`);
+      this.lottoCount = this.isValidCacheInput(answer);
+      Console.print(`${this.lottoCount}개를 구매했습니다.`);
     })
     Console.close();
   }
@@ -29,7 +42,7 @@ class App {
     this.InputCache();
       // 금액에 맞는 로또 수량 발행
       // 해당 갯수에 맞는 로또 번호 생성
-      //CreateUserLotto();
+    this.CreateUserLotto();
     // 당첨번호 입력
     //InputWinLotto();
     // 보너스 번호 입력
