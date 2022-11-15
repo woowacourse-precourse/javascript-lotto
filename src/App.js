bb61f8ae2674df649f3fa6c69bc467a8c7c8b551
sojinjang <jsojin6119@gmail.com>
2022-11-15 18:38:32 +0900

    :sparkles: feat(로또 구매): 구입금액 입력받기, 예외처리 및 로또 번호 생성 후 정렬해 출력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+const { Console } = require("@woowacourse/mission-utils");
+const InfoMessages = require("./constants/InfoMessages");
+const LottoShop = require("./LottoShop.js");
+const Lotto = require("./Lotto.js");
+const PrizeCalculator = require("./PrizeCalculator.js");
+
 class App {
-  play() {}
+  #lotto;
+  #prizeCalculator;
+
+  constructor() {
+    this.#lotto = new Lotto();
+  }
+
+  play() {
+    Console.readLine(InfoMessages.ENTER_PURCHASE_AMOUNT, (money) => {
+      const lottoShop = new LottoShop(money);
+      this.userLottos = lottoShop.getLottos();
+      this.lottosQuantity = lottoShop.getLottosQuantity();
+      this.showUserLottos();
+    });
+  }
+  showUserLottos() {
+    Console.print(this.lottosQuantity + InfoMessages.PURCHASE_QUANTITY);
+    this.userLottos.forEach((lotto) => {
+      Console.print(lotto);
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
