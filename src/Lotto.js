fcf80d98d94e71397b93d8aa3ee68c7b39ec932d
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-15 17:10:41 +0900

    fix: add validate according to new test code for lottery number

--- src/Lotto.js
+++ src/Lotto.js
@@ -14,6 +14,9 @@ class Lotto {
       if (numbers[i] === numbers[i + 1]) {
         throw new Error("[ERROR] 로또 번호는 중복되면 안됩니다.")
       }
+      if (numbers[i] < 1 || numbers[i] > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
     }
   }
 
@@ -21,7 +24,7 @@ class Lotto {
   getNumbers() {
     return this.#numbers
   }
-  
+
   getNumbersToString() {
     return `[${this.#numbers.join(', ')}]`
   }
