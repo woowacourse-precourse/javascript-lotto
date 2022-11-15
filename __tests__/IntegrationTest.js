9a733a9c9d4bcc6cd409f09a04237929f6e8e641
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-15 19:33:44 +0900

    test: 통합테스트3 추가

--- __tests__/IntegrationTest.js
+++ __tests__/IntegrationTest.js
@@ -79,4 +79,31 @@ describe('로또 테스트', () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
     });
   });
+
+  test('기능 테스트3', () => {
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 7, 8, 9],
+      [7, 8, 9, 10, 11, 12],
+    ]);
+    mockQuestions(['3000', '1,2,3,7,10,11', '12']);
+    const logs = [
+      '3개를 구매했습니다.',
+      '[1, 2, 3, 4, 5, 6]',
+      '[1, 2, 3, 7, 8, 9]',
+      '[7, 8, 9, 10, 11, 12]',
+      '3개 일치 (5,000원) - 2개',
+      '4개 일치 (50,000원) - 1개',
+      '5개 일치 (1,500,000원) - 0개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
+      '6개 일치 (2,000,000,000원) - 0개',
+      '총 수익률은 2,000.0%입니다.',
+    ];
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
 });
