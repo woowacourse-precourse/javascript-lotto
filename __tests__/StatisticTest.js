c4e338fb764e937565bc7b81766cdd5d8ac9f008
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-15 15:02:49 +0900

    test: add test function to check lottery rank

--- __tests__/StatisticTest.js
+++ __tests__/StatisticTest.js
@@ -27,5 +27,39 @@ describe("금액 validation 테스트", () => {
     expect(statistic.checkBonusNumber(bonusHit, winningNumber)).toBe(true);
   });
 
-  test("로또 등수를 계산한다", () => {});
+  test("로또 등수를 계산한다", () => {
+    const statistic = new Statistic();
+    const winningNumber = [1, 4, 24, 32, 40, 44, 7];
+    const oneHit = [1, 2, 3, 5, 6, 7];
+    const twoHit = [1, 2, 3, 4, 5, 6];
+    const threeHit = [1, 4, 24, 25, 26, 27];
+    const fourHit = [1, 4, 24, 32, 33, 34];
+    const fiveHit = [1, 4, 24, 32, 40, 41];
+    const fiveHitBonus = [1, 4, 7, 24, 32, 40];
+    const sixHit = [1, 4, 24, 32, 40, 44];
+    expect(statistic.checkRank(statistic.countHit(oneHit, winningNumber))).toBe(
+      null
+    );
+    expect(statistic.checkRank(statistic.countHit(twoHit, winningNumber))).toBe(
+      null
+    );
+    expect(
+      statistic.checkRank(statistic.countHit(threeHit, winningNumber))
+    ).toBe("3hit");
+    expect(
+      statistic.checkRank(statistic.countHit(fourHit, winningNumber))
+    ).toBe("4hit");
+    expect(
+      statistic.checkRank(statistic.countHit(fiveHit, winningNumber))
+    ).toBe("5hit");
+    expect(
+      statistic.checkRank(
+        statistic.countHit(fiveHitBonus, winningNumber),
+        statistic.checkBonusNumber(fiveHitBonus, winningNumber)
+      )
+    ).toBe("5hitBonus");
+    expect(statistic.checkRank(statistic.countHit(sixHit, winningNumber))).toBe(
+      "6hit"
+    );
+  });
 });
