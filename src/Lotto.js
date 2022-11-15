6d1dfb2eea0ba7310d4fe63afb5815f7d1b39603
Lim_JiSeon <dpf226@naver.com>
2022-11-15 15:40:15 +0900

    feat(Lotto.js) : update exception - validateMoney

--- src/Lotto.js
+++ src/Lotto.js
@@ -26,6 +26,12 @@ class Lotto {
       }
     });
   }
+
+  validateMoney(money) {
+    if (money%1000 !== 0) {
+      throw new Error("[ERROR] 구입 금액을 1000원 단위로 입력해주세요.");
+    }
+  }
 }
 
 module.exports = Lotto;
