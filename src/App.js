fde890da8763c48028f59e9f68bb0e0d62a737bd
KimJerry <k90966277@gmail.com>
2022-11-15 20:02:19 +0900

    docs: TODO 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const Lotto = require('./Lotto');
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+    play() {
+        console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
+    }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
