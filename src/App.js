19833f4395ec97705a47077962db3313d9df4d57
하송희 <99heesong@naver.com>
2022-11-15 19:49:27 +0900

    기능추가: 랜덤 숫자 6개 생성후 오름차순 정렬

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+const getRandomNum = () => {
+  const randomArr = Random.pickUniqueNumbersInRange(1, 45, 6);
+  getRandomNumAscendingSort(randomArr);
+  return randomArr;
+};
+
+const getRandomNumAscendingSort = (randomArr) => {
+  randomArr.sort(function (prev, next) {
+    return prev - next;
+  });
+};
+
 class App {
   play() {}
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
