4b4b0f5f3a58c924af0296096339054a796144ca
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-15 16:28:17 +0900

    feat(App): add input lotto win number & bonus number

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ const validation = new Validation();
 class App {
   #amount;
   #count;
+  #lotto;
+  #bonus;
+  #numbers = [];
 
   countLotto(money) {
     this.#count = money / 1000;
@@ -17,10 +20,26 @@ class App {
     Console.readLine(MESSAGE.USER_ENTER_PURCHASE, (money) => {
       this.#amount = money;
       if (validation.isValidateMoney(money)) this.countLotto(money);
+      // this.#count = money / 1000;
     });
   }
+
+  inputWinNumber() {
+    Console.readLine("당첨 번호를 입력해 주세요.", (input) => {
+      this.#lotto = new Lotto(input.split(",").map((item) => parseInt(item)));
+      this.inputBonusNumber();
+    });
+  }
+
+  inputBonusNumber() {
+    Console.readLine("보너스 번호를 입력해 주세요.", (bonus) => {
+      this.#bonus = bonus;
+    });
+  }
+
   play() {
     this.inputPurchase();
+    this.inputWinNumber();
   }
 }
 
