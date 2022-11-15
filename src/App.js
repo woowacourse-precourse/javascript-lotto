f88182b57c0e64c92e2ea21cf8ea48abdb6b6139
DongWooKim97 <nanaukim@naver.com>
2022-11-15 13:19:57 +0900

    refactor: 게임을 시작하기 위한 코드 재작성 및 변수명 변경

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ const Clerk = require("./play/Clerk");
 class App {
   play() {
     const clerk = new Clerk();
-    clerk.inputLottoAmount();
+    clerk.initLottoGame();
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
