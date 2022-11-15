eeaa2f65f5d9365365f501d4085782cd3f42fe32
SorinKwon <ksl020700@gmail.com>
2022-11-15 16:50:37 +0900

    feat: 구입 금액 저장 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-class App {
-  play() {}
-}
+const MissionUtils = require("@woowacourse/mission-utils");
+const PurchaseAmount = require("./game-machine/PurchaseAmount");
+const CalculateLotto = require("./game-machine/CalculateLotto");
+const DrawLotto = require("./draw-machine/DrawLotto");
 
+class App {
+
+  constructor(){
+    this.purchase_amount;
+  }
+  play() {
+    this.getPurchaseAmount();
+  }
+
+  getPurchaseAmount(){
+    const amount = new PurchaseAmount();
+    this.purchase_amount=amount.inputPurchaseAmount();
+  }
+
+}
+const app = new App();
+app.play();
 module.exports = App;
