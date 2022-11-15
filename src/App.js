bab8fa07c839196f6405e0510881fddd4833c46e
MIN SIK CHOI <minstones34@gmail.com>
2022-11-15 20:01:43 +0900

    feat: 당첨번호 입력받는 기능 추가
    예외 처리하는 매서드 함수 또한 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App {
   constructor() {
     this.userMoney = 0;
     this.lottoCount = 0;
+    this.winingLotteryArr = [];
     // this.lotto = new Lotto(this.lottoCount);
   }
 
@@ -33,11 +34,12 @@ class App {
       this.userMoney = forValidate;
       this.lottoCount = forValidate / THOUSAND_WON;
       MissionUtils.Console.print(`${this.lottoCount}개를 구매했습니다.`);
-      this.generateLottoNumb(this.lottoCount);
+      this.generateLottoNum(this.lottoCount);
+      this.inputWinningLotteryNum();
     });
   }
 
-  generateLottoNumb(lottoCount) {
+  generateLottoNum(lottoCount) {
     const lottoNumArr = [];
     for (let i = 0; i < lottoCount; i++) {
       const lottoRandomNum = MissionUtils.Random.pickUniqueNumbersInRange(
@@ -50,6 +52,53 @@ class App {
       lottoNumArr.push(lottoRandomNum);
     }
   }
+
+  inputWinningLotteryNum() {
+    MediaCapabilities.Console.readline(
+      "당첨 번호를 입력해 주세요. \n",
+      (input) => {
+        this.winingLotteryArr = input.split(",").map((x) => parseInt(x));
+        this.winningLotteryValidate(winingLotteryArr);
+
+        this.inputBonusNum();
+      }
+    );
+  }
+
+  winningLotteryValidate(winingLotteryArr) {
+    const forValidateNum = winingLotteryArr;
+    if (forValidateNum.length != 6) {
+      throw new Error("[ERROR] 6개의 숫자를 입력해주세요.");
+    }
+
+    forValidateNum.forEach((number) => {
+      if (isNaN(number)) {
+        throw new Error("[ERROR] 숫자만 입력해주세요.");
+      }
+
+      if (
+        number == "" ||
+        number == null ||
+        number == undefined ||
+        (number != null &&
+          typeof number == "object" &&
+          !Object.keys(number).length)
+      ) {
+        throw new Error("[ERROR] 숫자를 제대로 입력해주십시오.");
+      }
+
+      if (number < 1 || 45 < number) {
+        throw new Error("[ERROR] 입력받은 숫자의 범위를 초과합니다.");
+      }
+    });
+
+    const set = new Set(forValidateNum);
+    if (set.size != 6) {
+      throw new Error("[ERROR] 중복되지 않은 값만 입력해주세요.");
+    }
+  }
+
+  inputBonusNum() {}
 
   play() {
     this.getUserInputNumber();
