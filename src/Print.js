9f68eadae1d8311ade7b12e587d7ba5a8a32aa6c
vjvl95 <vjvl95@naver.com>
2022-11-15 16:48:38 +0900

    feat : countLotto 메소드 추가

--- src/Print.js
+++ src/Print.js
@@ -1,8 +1,11 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class Print {
-  static myLotto(myLotto) {
-    MissionUtils.Console.print(myLotto);
+  static myLotto(lotto) {
+    lotto.forEach((v) => MissionUtils.Console.print(v));
+  }
+  static countLotto(countLotto) {
+    MissionUtils.Console.print(`${countLotto}개를 구매하셨습니다.`);
   }
 }
 module.exports = Print;
