c54050f744c4cd71406243e19d095818bcef6d4f
DongWooKim97 <nanaukim@naver.com>
2022-11-15 22:23:08 +0900

    test: 각 테스트 예시 추가 및 Lotto 클래스 정상 작동 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -13,11 +13,19 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow(ERROR_MESSAGES.DUPLICATED_LOTTO_NUM);
+
+    expect(() => {
+      new Lotto([2, 11, 17, 34, 39, 2]);
+    }).toThrow(ERROR_MESSAGES.DUPLICATED_LOTTO_NUM);
   });
 
   test("로또 번호가 숫자로만 이루어지지 않은 경우 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, "X", "Y", "Z"]);
+    }).toThrow(ERROR_MESSAGES.INVALID_LOTTO_TYPE);
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, "-"]);
     }).toThrow(ERROR_MESSAGES.INVALID_LOTTO_TYPE);
   });
 
@@ -25,5 +33,19 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 55, 6]);
     }).toThrow(ERROR_MESSAGES.INVALID_LOTTO_RANGE);
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 0, 6]);
+    }).toThrow(ERROR_MESSAGES.INVALID_LOTTO_RANGE);
+  });
+
+  test("로또 클래스 정상 작동 테스트", () => {
+    const exampleOne = [1, 2, 3, 4, 5, 6];
+    const exampleTwo = [3, 5, 10, 37, 38, 42];
+    const normalOperationOne = new Lotto(exampleOne);
+    const normalOperationTwo = new Lotto(exampleTwo);
+
+    expect(normalOperationOne.getLottoWinningNumber()).toEqual(exampleOne);
+    expect(normalOperationTwo.getLottoWinningNumber()).toEqual(exampleTwo);
   });
 });
