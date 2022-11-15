0ffd4a60706d067420e07445600a4c3bbb20067d
rickyshu <hoinleekk@naver.com>
2022-11-15 21:45:42 +0900

    Fix: LOTTERY_INFO 상수 객체 추가

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,5 +1,4 @@
 const LOTTERY_PRICE = 1000;
-const LOTTERY_LENGTH = 6;
 const MESSAGE = {
   LOTTERY_BUDGET: '구입금액을 입력해 주세요.\n',
   LOTTERY_WIN_NUMBER: '당첨 번호를 입력해 주세요.\n',
@@ -14,6 +13,12 @@ const EXCEPTIONS = {
   NUMBER_OVERLAPPED: '[ERROR] 중복된 번호를 입력하셨습니다.',
   NOT_A_DECIMAL: '[ERROR] 자연수만 입력 가능합니다.',
   UNIT_ERROR: `[ERROR] 금액의 단위가 맞지 않습니다! ${LOTTERY_PRICE}원 단위로 입력해주세요!`,
+};
+
+const LOTTERY_INFO = {
+  LOTTERY_LENGTH: 6,
+  LOTTERY_MIN_NUMBER: 1,
+  LOTTERY_MAX_NUMBER: 45,
 };
 
 module.exports = {
