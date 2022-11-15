2bca4dcfc62e7268d9ebe36f151b3c6d85044491
piporica <49173614+piporica@users.noreply.github.com>
2022-11-15 17:40:03 +0900

    feat: 구입 금액 입력하기 / 게임 종료 구현
    구입 금액 입력하는 기능 추가
    게임 종료 함수 추가(console close)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
-const { MESSAGES } = require("./lib/constants.");
+const { MESSAGES } = require("./lib/constant");
 class App {
-  play() {}
+  play() {
+    this.getAmountPaid((input) => {
+      this.appClose();
+    });
+  }
+
+  getAmountPaid(callback) {
+    Console.readLine(MESSAGES.TAKE_MONEY, (input) => {
+      console.log(input);
+      callback(input);
+    });
+  }
+
+  appClose() {
+    Console.close();
+  }
 }
 
 module.exports = App;
+
+let app = new App();
+app.play();
---
+++ src/lib/constant.js
@@ -0,0 +1,7 @@
+const MESSAGES = {
+  TAKE_MONEY: "구입금액을 입력해 주세요.\n",
+};
+
+module.exports = {
+  MESSAGES,
+};
