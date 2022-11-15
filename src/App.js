9f1e0fbbb25fd6cade5f584769ec68cd3908714f
cmun2 <cmun2@illinois.edu>
2022-11-15 21:31:12 +0900

    feat: 파일 구조 형성 및 로또 입력 관련 인풋 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const Lotto = require("./Lotto");
+const MissionUtils = require("@woowacourse/mission-utils");
+const Validation = require("./Validation");
+const Text = require("./Text");
+
 class App {
-  play() {}
+  constructor() {}
+
+  play() {
+    this.userPurchase();
+  }
+
+  userPurchase() {
+    MissionUtils.Console.readline(
+      `${Text.INPUT_TEXT.PRICE}\n`,
+      (purchasePrice) => {}
+    );
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
