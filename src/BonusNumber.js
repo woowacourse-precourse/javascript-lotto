a33d9d5c3f94ebdb9dbf4c4e1bb6605c3dd59c33
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-15 10:08:52 +0900

    feat : 보너스 번호가 1부터 45 사이의 자연수가 아닌 경우 예외 처리

--- src/BonusNumber.js
+++ src/BonusNumber.js
@@ -10,6 +10,18 @@ class BonusNumber {
             throw new Error("[ERROR] 보너스 번호는 로또 번호와 중복이 없어야 합니다.");
         }
     }
+
+    validateRangeOfNumber(number) {
+        if (number < 1 && number > 45) {
+            throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 수여야 합니다.");
+        }
+    }
+
+    validateIsInteger(number) {
+        if (!Number.isInteger(number)) {
+            throw new Error("[ERROR] 보너스 번호는 자연수여야 합니다.");
+        }
+    }
 }
 
 module.exports = BonusNumber;
