c5b8afc923721bccb433c147ad69946b8e75a3f1
gilpop8663 <wolfye@naver.com>
2022-11-15 13:06:52 +0900

    test: 로또 번호와 당첨 번호를 비교하는 함수가 올바르게 작동하는지 체크하는 테스트

--- __tests__/UtilityTest.js
+++ __tests__/UtilityTest.js
@@ -1,4 +1,4 @@
-const { splitComma } = require('../src/libs/Utils');
+const { splitComma, matchArrayNumbers } = require('../src/libs/Utils');
 
 describe('쉼표(,) 기준으로 번호를 구분하는 함수 테스트 ', () => {
   test('기능 테스트', () => {
@@ -11,5 +11,15 @@ describe('쉼표(,) 기준으로 번호를 구분하는 함수 테스트 ', () =
       '5',
       '6',
     ]);
+  });
+});
+
+describe('발행된 로또 번호와 당첨 번호를 비교하는 함수 테스트 ', () => {
+  test('기능 테스트', () => {
+    const array1 = [1, 2, 3, 4, 5, 6];
+    const array2 = [4, 5, 6, 7, 8, 9];
+    const array3 = [5, 6, 7, 8, 9, 10];
+    expect(matchArrayNumbers(array1, array2)).toBe(3);
+    expect(matchArrayNumbers(array2, array3)).toBe(5);
   });
 });
