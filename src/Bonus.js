2ea4e61eb7690d087ecf35ce7bd54cad2171b5f6
kwh97 <zzzang0512@naver.com>
2022-11-15 21:04:12 +0900

    style: 불필요 코드(주석) 삭제

--- src/Bonus.js
+++ src/Bonus.js
@@ -31,35 +31,3 @@ class Bonus {
 }
 
 module.exports =  Bonus;
-
-/*
-class Bonus {
-  constructor(bonusNumber, winNumber) {
-    this.validate(bonusNumber, winNumber);
-  }
-
-  validate(bonusNumber, winNumber) {
-    this.checkIsNumber(bonusNumber);
-    this.checkRange(bonusNumber);
-    this.checkDuplicate(bonusNumber, winNumber);
-  }
-
-  checkIsNumber(number) {
-    if (isNaN(number) === true) {
-      throw new Error('[ERROR] 보너스 번호는 숫자여야 합니다.');
-    }
-  }
-
-  checkRange(number) {
-    if (number > 45 || number < 1) {
-      throw new Error('[ERROR] 보너스 번호는 1 ~ 45 사이여야 합니다.')
-    }
-  }
-
-  checkDuplicate(number, winNumber) {
-    if (winNumber.includes(number)) {
-      throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복되면 안 됩니다.');
-    }
-  }
-}
-*/
