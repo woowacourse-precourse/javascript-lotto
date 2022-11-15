9f7c5792f1ce7533b906ca532a5ee8f980176a43
rovxxmjxx <miju3545@gmail.com>
2022-11-15 16:16:23 +0900

    fix: 테스트 코드 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -5,20 +5,20 @@ describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow(Display.error('OUT_OF_VOLUME'));
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow(Display.error('DUPLICATED'));
+    }).toThrow('[ERROR]');
   });
 
   test('로또 번호의 범위가 1~45가 아닐 경우 예외가 발생한다', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 56]);
-    }).toThrow(Display.error('OUT_OF_RANGE'));
+    }).toThrow('[ERROR]');
   });
 
   test('로또 번호는 오름차순으로 정렬되어야 한다.', () => {
