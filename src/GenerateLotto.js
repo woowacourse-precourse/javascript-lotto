980f78b23ef1be9ca3314d287ba740522a176a6c
seungki <sysh9498@gmail.com>
2022-11-15 19:53:05 +0900

    feat(GenerateLotto): 구매한 로또 갯수 출력 기능

--- src/GenerateLotto.js
+++ src/GenerateLotto.js
@@ -1,10 +1,14 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-const { PRICE_PER_LOTTO } = require('./Const');
+const { PRICE_PER_LOTTO, LOTTO_COUNT_MESSAGE } = require('./Const');
 const { Console, Random } = MissionUtils;
 
 class GenerateLotto {
   static carculateLottoCount(price) {
     return price / PRICE_PER_LOTTO;
+  }
+
+  static printLottoCount(lottoCount) {
+    Console.print(lottoCount + LOTTO_COUNT_MESSAGE);
   }
 }
 
