6f6fb4c6532c018178b9a990278f70d3836f8a33
sunny <huiseon37@gmail.com>
2022-11-15 15:27:58 +0900

    test: 당첨 번호, 보너스 번호 예외 처리 테스트

--- __tests__/LottoResultTest.js
+++ __tests__/LottoResultTest.js
@@ -1,3 +1,4 @@
+const { ERROR_MESSAGE } = require("../src/constants");
 const LottoMachine = require("../src/LottoMachine");
 const LottoResult = require("../src/LottoResult");
 
@@ -18,5 +19,41 @@ describe("로또결과 클래스 테스트", () => {
     const lottoResult = new LottoResult([1, 12, 34, 2, 3, 4], 7, new LottoMachine(7000));
     const result = lottoResult.checkRank(6);
     expect(result).toEqual('RANK_1');
+  });
+
+  test("당첨 번호에 중복이 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new LottoResult([1, 12, 34, 2, 3, 3], 7, new LottoMachine(7000));
+    }).toThrow(ERROR_MESSAGE.WINNING_LOTTO_DUPLICATION_ERROR);
+  });
+
+  test("보너스 번호에 중복이 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new LottoResult([1, 12, 34, 2, 3, 4], 4, new LottoMachine(7000));
+    }).toThrow(ERROR_MESSAGE.BONUS_NUMBER_DUPLICATION_ERROR);
+  });
+  
+  test("당첨 번호가 1 ~ 45 사이의 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new LottoResult([1, 12, 34, 2, 3, 47], 4, new LottoMachine(7000));
+    }).toThrow(ERROR_MESSAGE.NUMBER_RANGE_ERROR);
+  });
+  
+  test("보너스 번호가 1 ~ 45 사이의 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new LottoResult([1, 12, 34, 2, 3, 4], 47, new LottoMachine(7000));
+    }).toThrow(ERROR_MESSAGE.NUMBER_RANGE_ERROR);
+  });
+
+  test("당첨 번호가 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new LottoResult([1, 12, 34, 2, 3, 'hello'], 4, new LottoMachine(7000));
+    }).toThrow(ERROR_MESSAGE.NUMBER_IS_NAN_ERROR);
+  });
+
+  test("보너스 번호가 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new LottoResult([1, 12, 34, 2, 3, 4], 'hello', new LottoMachine(7000));
+    }).toThrow(ERROR_MESSAGE.NUMBER_IS_NAN_ERROR);
   });
 });
--- src/constants/index.js
+++ src/constants/index.js
@@ -33,7 +33,7 @@ const ERROR_MESSAGE = {
   PURCHASE_MONEY_ERROR: "[ERROR] 구매 금액은 1000원 이상이어야 합니다.",
   PURCHASE_IS_NAN_ERROR: "[ERROR] 구매 금액은 숫자여야 합니다.",
   WINNING_LOTTO_DUPLICATION_ERROR: "[ERROR] 당첨 번호는 중복 없는 6개 숫자여야 합니다.",
-  BONUS_NUMBER_DUPLICATION_ERROR: "[ERROR] 당첨 번호는 중복 없는 6개 숫자여야 합니다.",
+  BONUS_NUMBER_DUPLICATION_ERROR: "[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.",
   NUMBER_RANGE_ERROR: "[ERROR] 로또 번호는 1 ~ 45 사이여야 합니다.",
   NUMBER_IS_NAN_ERROR: "[ERROR] 로또 번호는 숫자여야 합니다.",
 }
