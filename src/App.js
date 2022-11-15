f56c65de19e5514d3b14e7dc6f61bb3c8e98dfe5
jihun1233 <leejihun1233@gmail.com>
2022-11-15 22:45:37 +0900

    feat: 2.입력 검사 기능 구현
    - 금액, 당첨 번호, 보너스 번호를 입력받은 후 입력이 올바른지 검사하는 함수를 구현
    
    - 번호가 올바른지 검사하는 함수를 validateLottoNumberInput으로 두고 이를 matchNumbersInput, bonusNumbersInput을 검사하는데 동일하게 사용
    - validateLottoNumberInput은 실제 사용하진 않으므로 exports에 추가하지 않음

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 const MissionUtils = require('@woowacourse/mission-utils')
-const getInput = require('./getInput.js')
+const getInput = require('./getInput')
+const {validateBonusNumberInput, validateMatchNumbersInput, validatePriceInput} = require('./validateInput')
 
 class App {
   async play() {
-    const price = await getInput('구입금액을 입력해 주세요\n')
-    MissionUtils.Console.print(price)
+    const priceInput = await getInput('구입금액을 입력해 주세요\n')
+    validatePriceInput(priceInput)
+    const price = parseInt(priceInput, 10)
+    MissionUtils.Console.print(priceInput)
 
-    const matchNumbers = await getInput('당첨 번호를 입력해 주세요\n')
-    MissionUtils.Console.print(matchNumbers)
+    const matchNumbersInput = await getInput('당첨 번호를 입력해 주세요\n')
+    validateMatchNumbersInput(matchNumbersInput)
+    const matchNumbers = matchNumbersInput.split(',').map(n => parseInt(n, 10))
+    MissionUtils.Console.print(matchNumbersInput)
 
-    const bonusNumber = await getInput('보너스 번호를 입력해 주세요\n')
-    MissionUtils.Console.print(bonusNumber)
+    const bonusNumberInput = await getInput('보너스 번호를 입력해 주세요\n')
+    validateBonusNumberInput(matchNumbers, bonusNumberInput)
+    const bonusNumber = parseInt(bonusNumberInput, 10)
+    MissionUtils.Console.print(bonusNumberInput)
     
     MissionUtils.Console.close()
   }
