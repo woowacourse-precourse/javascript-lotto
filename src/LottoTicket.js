18070771510af9da4a388ec61b031ef7fccb80e5
nuheajiohc <jaehun341012@gmail.com>
2022-11-15 09:12:50 +0900

    refactor: 구매한 로또 출력 기능 리팩토링
    가독성을 생각한 리팩토링

--- src/LottoTicket.js
+++ src/LottoTicket.js
@@ -3,18 +3,14 @@ const { RULE } = require("./constants.js");
 
 module.exports = class LottoTicket {
   static generateNumber() {
-    return Random.pickUniqueNumbersInRange(
-      RULE.MIN_NUMBER,
-      RULE.MAX_NUMBER,
-      RULE.SELECT_NUMBER
-    ).sort((a, b) => a - b);
+    return Random.pickUniqueNumbersInRange(RULE.MIN_NUMBER, RULE.MAX_NUMBER, RULE.SELECT_NUMBER).sort((a, b) => a - b);
   }
 
   static publish(lottoTicketNumber) {
     const lottoTicketList = [];
     for (let number = 1; number <= lottoTicketNumber; number++) {
       const lottoTicket = LottoTicket.generateNumber();
-      Console.print('"[' + lottoTicket.join(", ") + ']"');
+      Console.print(`[${lottoTicket.join(", ")}]`);
       lottoTicketList.push(lottoTicket);
     }
     return lottoTicketList;
