402c89216bc994cab11072467e470bb648c4c0da
h2002y <h2002y@naver.com>
2022-11-15 20:14:09 +0900

    refactor: make error messages as const variables in Money.js

--- src/Money.js
+++ src/Money.js
@@ -1,4 +1,8 @@
 const ONE_LOTTO_COST = 1000;
+const ERROR_MONEY_IS_NUMBER_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
+const ERROR_MONEY_IS_INT_MESSAGE = "[ERROR] 실수가 아닌 금액을 입력해주세요.";
+const ERROR_MONEY_OVER_MESSAGE = "[ERROR] 천원 이상의 금액을 입력해주세요.";
+const ERROR_MONEY_DIVIDE_MESSAGE = "[ERROR] 천원 단위의 금액으로 입력해주세요.";
 
 class Money {
   #money;
@@ -24,16 +28,16 @@ class Money {
 
   validateMoney(money) {
     if(isNaN(money) || money === "" || money === " "){
-      throw new Error("[ERROR] 숫자를 입력해주세요.");
+      throw new Error(ERROR_MONEY_IS_NUMBER_MESSAGE);
     }
     if(Number(money) % 1 !== 0){
-      throw new Error("[ERROR] 실수가 아닌 금액을 입력해주세요.")
+      throw new Error(ERROR_MONEY_IS_INT_MESSAGE);
     }
-    if(Number(money) < 1000){
-         throw new Error("[ERROR] 천원 이상의 금액을 입력해주세요.");
+    if(Number(money) < ONE_LOTTO_COST){
+         throw new Error(ERROR_MONEY_OVER_MESSAGE);
     }
-    if(Number(money) % 1000 !== 0){
-      throw new Error("[ERROR] 천원 단위의 금액으로 입력해주세요.");
+    if(Number(money) % ONE_LOTTO_COST !== 0){
+      throw new Error(ERROR_MONEY_DIVIDE_MESSAGE);
     }
   }
 }
