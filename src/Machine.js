75598882a43a2fae3cca52cfc2048083b622ad8b
rovxxmjxx <miju3545@gmail.com>
2022-11-15 22:11:52 +0900

    chore: import 순서 변경

--- src/Machine.js
+++ src/Machine.js
@@ -1,8 +1,8 @@
 const Lotto = require('./Lotto');
 const User = require('./User');
 const Display = require('./Display');
-const { Console } = require('@woowacourse/mission-utils');
 const { validateWinningNumbersInput, validateBonusNumberInput } = require('./utils/inputValidate');
+const { Console } = require('@woowacourse/mission-utils');
 
 class Machine {
   static user = new User();
@@ -13,19 +13,18 @@ class Machine {
   constructor(payment) {
     Machine.user.payment = payment;
     Machine.user.quantity = payment / Display.info('PRICE');
-
     this.#winningNumbers = [];
     this.#bonusNumber = 0;
     this.rankingCountMap = new Map();
   }
 
   work() {
-    this.#issue();
-    this.#displayPurchasedLottoInfo();
-    this.#getWinningNumbersInput();
+    this.issue();
+    this.displayPurchasedLottoInfo();
+    this.startWithGettingWinningNumbersInputs();
   }
 
-  #issue() {
+  issue() {
     let cnt = 0;
     while (cnt < Machine.user.quantity) {
       const numbers = Display.randomNumbers();
@@ -35,7 +34,7 @@ class Machine {
     }
   }
 
-  #displayPurchasedLottoInfo() {
+  displayPurchasedLottoInfo() {
     Console.print(Display.statistics('QUANTITY', Machine.user.quantity));
 
     Machine.user.purchasedLotto.forEach((lotto) => {
@@ -43,43 +42,43 @@ class Machine {
     });
   }
 
-  #getWinningNumbersInput() {
+  startWithGettingWinningNumbersInputs() {
     Console.readLine(Display.guidance('WINNING_NUMBER_INPUT'), (winning) => {
       validateWinningNumbersInput(winning);
       this.#winningNumbers = winning.split(',').map((s) => +s);
 
-      this.#getBonusNumberInput();
+      this.getBonusNumberInput();
     });
   }
 
-  #getBonusNumberInput() {
+  getBonusNumberInput() {
     Console.readLine(Display.guidance('BONUS_NUMBER_INPUT'), (bonus) => {
       validateBonusNumberInput(bonus);
       this.#bonusNumber = +bonus;
 
-      this.#countTotalRankings();
+      this.countTotalRankings();
     });
   }
 
-  #countTotalRankings() {
+  countTotalRankings() {
     Machine.user.purchasedLotto.forEach((lotto) => {
       const winningBonusCounts = lotto.countWinningBonusNumbers({
         winning: this.#winningNumbers,
         bonus: this.#bonusNumber,
       });
-      this.#computeRankingCountMap(winningBonusCounts);
+      this.computeRankingCountMap(winningBonusCounts);
     });
 
-    this.#displayResult();
+    this.displayResult();
   }
 
-  #computeRankingCountMap(lottoCounts) {
-    const ranking = this.#getRanking(lottoCounts);
+  computeRankingCountMap(lottoCounts) {
+    const ranking = this.getRanking(lottoCounts);
     let count = this.rankingCountMap.get(ranking) || 0;
     this.rankingCountMap.set(ranking, (count += 1));
   }
 
-  #getRanking({ winningCount, bonusCount }) {
+  getRanking({ winningCount, bonusCount }) {
     switch (winningCount) {
       case 3:
         return 'FIFTH';
@@ -94,7 +93,7 @@ class Machine {
     }
   }
 
-  #displayResult() {
+  displayResult() {
     Console.print(Display.guidance('RESULT'));
 
     const rankings = ['FIFTH', 'FOURTH', 'THIRD', 'SECOND', 'FIRST'];
@@ -103,16 +102,16 @@ class Machine {
       Console.print(Display.rankingStatistics(ranking, this.rankingCountMap.get(ranking) || 0));
     });
 
-    this.#displayProfits();
+    this.displayProfits();
+  }
+
+  displayProfits() {
+    this.computeProfits();
+    Console.print(Display.statistics('PROFITS', Machine.user.profits));
     Console.close();
   }
 
-  #displayProfits() {
-    this.#computeProfits();
-    Console.print(Display.statistics('PROFITS', Machine.user.profits));
-  }
-
-  #computeProfits() {
+  computeProfits() {
     Machine.user.winningPrize = [...this.rankingCountMap].reduce((sum, [ranking, count]) => {
       let prize = 0;
       if (ranking) {
