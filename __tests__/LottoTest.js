6b1627bc6f5efd63c43a6a7a2e15be46308101b4
Hoon <si932174@gmail.com>
2022-11-15 15:07:35 +0900

    fix : 85ad7749052bab81559a845a0e5fc64afd6551a5 에러 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -13,6 +13,18 @@ describe('로또 클래스 테스트', () => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow('[ERROR]');
   });
-
   // 아래에 추가 테스트 작성 가능
+
+  test('로또 번호가 1~45 범위를 벗어난 경우 예외가 발생한다.', () => {
+    const wrongLottoNumbers = [
+      [1, 2, 3, 4, 5, 46],
+      [-1, 2, 3, 4, 5, 6],
+    ];
+
+    wrongLottoNumbers.map(lottoNumber => {
+      expect(() => {
+        new Lotto(lottoNumber);
+      }).toThrow('[ERROR]');
+    });
+  });
 });
