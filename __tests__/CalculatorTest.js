a3ebb6d4728923f54a5a1613ebc27674d2b023e6
daal2quf <daal2quf@gmail.com>
2022-11-15 17:36:05 +0900

    test(calculatorTest): profit() 테스트
    add:
    test1: 꽝: 5개, 5등 :1개, 4등 :1개 일때 상금이 정상적으로 나오는지 확인
    test2: 꽝: 5개, 1등 :1개, 2등 :1개, 3등: 1개 일때 상금이 정상적으로 나오는지 확인

--- __tests__/CalculatorTest.js
+++ __tests__/CalculatorTest.js
@@ -1,27 +1,48 @@
-const App = require("../src/App");
-const { Console } = require("@woowacourse/mission-utils");
-const Calculator = require("../src/Calculator");
-
+const App = require('../src/App');
+const { Console } = require('@woowacourse/mission-utils');
+const Calculator = require('../src/Calculator');
+const { LOTTO_PRIZE } = require('../src/Constants');
 
 afterEach(() => {
   Console.close();
 });
 
-describe("계산 기능 테스트", () => {
-  test("App클래스의 calculatorLottoCount() 기능 테스트", () => {
-    const app = new App();
-    const input = 4000;
-    const lottoCount = app.calculatorLottoCount(input);
-    expect(lottoCount).toEqual(4);
-  });
-});
-
-describe("Utils 클래스 matchNumberCount() 테스트", () => {
-  test("당첨 숫자와 유저 구매 숫자의 같은 값 개수 확인", () => {
+describe('Utils 클래스 matchNumberCount() 테스트', () => {
+  test('당첨 숫자와 유저 구매 숫자의 같은 값 개수 확인', () => {
     const calculator = new Calculator();
     const winNumbers = [1, 2, 3, 4, 5, 6, 7];
     const userlottoNumbers = [1, 3, 5, 9, 40, 41];
     const count = calculator.matchNumberCount(winNumbers, userlottoNumbers);
     expect(count).toEqual(3);
+  });
+});
+
+describe('Utils 클래스 profit() 테스트', () => {
+  test('당첨내역과 내역별 상금이 주어졌을때 누적 상금이 계산되는지 확인', () => {
+    const calculator = new Calculator();
+    const resultMap = {
+      fifthGrade: 1,
+      forthGrade: 1,
+      thirdGrade: 0,
+      secondGrade: 0,
+      firstGrade: 0,
+      loseMoney: 5,
+    };
+
+    const profit = calculator.profit(resultMap);
+    expect(profit).toEqual(55000);
+  });
+  test('당첨내역과 내역별 상금이 주어졌을때 누적 상금이 계산되는지 확인', () => {
+    const calculator = new Calculator();
+    const resultMap = {
+      fifthGrade: 0,
+      forthGrade: 0,
+      thirdGrade: 1,
+      secondGrade: 1,
+      firstGrade: 1,
+      loseMoney: 5,
+    };
+    const profit = calculator.profit(resultMap);
+    expect(profit).toEqual(2031500000);
   });
 });
