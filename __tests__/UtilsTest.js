16d9459696dd1140ea1837fcd140b6fedf3bc614
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-15 17:36:26 +0900

    test: 보너스 번호 입력

--- __tests__/UtilsTest.js
+++ __tests__/UtilsTest.js
@@ -1,7 +1,10 @@
-const { parseAnswerInput } = require("../src/Utils");
+const { parseAnswerInput, parseBonusInput } = require("../src/Utils");
 
 describe("utils 테스트", () => {
   test("문자열을 숫자배열로 변환한다.", () => {
     expect(parseAnswerInput("1,2,,d")).toStrictEqual([1, 2, 0, NaN]);
+  });
+  test("문자열을 숫자로 변환한다.", () => {
+    expect(parseBonusInput("1")).toBe(1);
   });
 });
