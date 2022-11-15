df37d6f5176c1e8020f5efb34e78be26c6267ff4
gilpop8663 <wolfye@naver.com>
2022-11-15 13:30:48 +0900

    test: 랜덤 배열을 반환하는 함수가 올바르게 작동하는지 체크하는 테스트 진행

--- __tests__/UtilityTest.js
+++ __tests__/UtilityTest.js
@@ -1,4 +1,8 @@
-const { splitComma, matchArrayNumbers } = require('../src/libs/Utils');
+const {
+  splitComma,
+  matchArrayNumbers,
+  createRandomLotto,
+} = require('../src/libs/Utils');
 
 describe('쉼표(,) 기준으로 번호를 구분하는 함수 테스트 ', () => {
   test('기능 테스트', () => {
@@ -21,5 +25,16 @@ describe('발행된 로또 번호와 당첨 번호를 비교하는 함수 테스
     const array3 = [5, 6, 7, 8, 9, 10];
     expect(matchArrayNumbers(array1, array2)).toBe(3);
     expect(matchArrayNumbers(array2, array3)).toBe(5);
+  });
+});
+
+describe('랜덤 배열을 반환하는 함수 테스트 ', () => {
+  test('기능 테스트', () => {
+    const randomArray = createRandomLotto();
+    expect(randomArray).toHaveLength(6);
+    for (let i = 0; i < 5; i += 1) {
+      expect(randomArray[i]).toBeGreaterThanOrEqual(1);
+      expect(randomArray[i]).toBeLessThanOrEqual(45);
+    }
   });
 });
