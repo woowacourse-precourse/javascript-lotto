686b721179105d48a13b392b02506837ad806917
xownsdl0 <xownsdl0@naver.com>
2022-11-15 20:16:02 +0900

    feat(lotto): App.js 시작 실행 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const inputMessage = require('./model/InputMessage');
+const buyLotto = require('./controller/BuyLotto');
+
 class App {
-  play() {}
+  play() {
+    this.message = inputMessage.INPUT_MONEY;
+    buyLotto.inputMoney(this.message)
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
