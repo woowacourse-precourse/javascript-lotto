84c41e44472c18766b4ecd91b33b219d1399ccd6
jaesukpark77 <jaesukpark77@naver.com>
2022-11-15 17:58:21 +0900

    feat: 로또 금액 입력

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ const Lotto = require("./Lotto");
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   boughtLottos = [];
+  
+  inputAmountMoney() {
+    const WORD_TO_PRINT = "구입금액을 입력해 주세요.\n";
+    let numberOfLotto;
+    MissionUtils.Console.readLine(WORD_TO_PRINT, (money) => {
+      money = Number(money);
+      numberOfLotto = money / 1000;
+      this.checkAmountOfMoneyToBuy(money);
+      this.printBoughtNumberOfLotto(numberOfLotto);
+      this.randomLottoNumber(numberOfLotto);
+    });
+  }
 
   checkAmountOfMoneyToBuy(money) {
     const CHECK_RESULT = money % 1000;
