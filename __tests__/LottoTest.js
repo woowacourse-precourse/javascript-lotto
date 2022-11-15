3511eca471d2a5080c38d3fb430e6a8fe199851b
Sanggil Han <tkdrlf95@naver.com>
2022-11-15 20:10:08 +0900

    test(Lotto): Implement test codes to check right places
    등수를 확인하는 다음 내용과 같은 테스트 코드를 작성하였음
    - 숫자 6개가 모두 맞으면 1등
    - 숫자 5개가 맞고 보너스 숫자를 맞추면 2등
    - 숫자 5개가 맞고 보너스 숫자를 틀리면 3등
    - 숫자 6개를 모두 틀리면 아무것도 아님

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -17,4 +17,32 @@ describe("로또 클래스 테스트", () => {
   test("예외 조건에 걸리지 않으면 정상 동작한다.", () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
   });
+  
+  test("숫자 6개를 모두 맞추면 1등이다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const place = lotto.winWhatPlace([1, 2, 3, 4, 5, 6], 9);
+    console.log(`등수는: ${place}등 입니다.`);
+    expect(place).toEqual(1);
+  });
+
+  test("숫자 5개와 보너스 숫자를 맞추면 2등이다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const place = lotto.winWhatPlace([1, 2, 3, 4, 5, 7], 6);
+    console.log(`등수는: ${place}등 입니다.`);
+    expect(place).toEqual(2);
+  });
+  
+  test("숫자 5개는 맞지만 보너스 숫자를 맞추지 못하면 3등이다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const place = lotto.winWhatPlace([1, 2, 3, 4, 5, 7], 8);
+    console.log(`등수는: ${place}등 입니다.`);
+    expect(place).toEqual(2);
+  });
+
+  test("숫자 6개를 모두 틀리면 아무것도 아니다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const place = lotto.winWhatPlace([7, 8, 9, 10, 11, 12], 6);
+    console.log(`등수는: ${place}등 입니다.`);
+    expect(place).toEqual(0);
+  });
 });
