cc6ac1c4aa597cb200c72aa342f3b95da6445b9d
Saans <92422813+saans808@users.noreply.github.com>
2022-11-15 20:44:10 +0900

    feat: 사용자 구입금액 입력 처리

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ class App {
     computerRandomNumber.sort((a, b) => a - b);
     const computerBonusNumber = Random.pickNumberInRange(1, 45);
   }
+
+  userMoneyInput() {
+    Console.readLine('구입금액을 입력해 주세요.', (answer) => {
+      this.inputExceptionNotANumber(answer);
+      const inputMoney = answer;
+      const lottoCount = answer/1000;
+      Console.print(`${answer/1000}개를 구매했습니다.`);
+      this.inputExceptionThousand(answer);
+    });
+  } 
 }
 
 module.exports = App;
