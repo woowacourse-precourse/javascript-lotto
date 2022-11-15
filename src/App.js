582863f8f33963cc533e042152121eac238e281a
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-15 15:30:24 +0900

    feat(App): add input use purchase amount function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+const { Random, Console } = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto.js");
+const { MAX_LENGTH, MESSAGE } = require("./utils/Constants");
 class App {
+  #amount;
+
+  inputPurchase() {
+    Console.readLine(MESSAGE.USER_ENTER_PURCHASE, (input) => {
+      this.#amount = input;
+    });
+  }
   play() {}
 }
 
