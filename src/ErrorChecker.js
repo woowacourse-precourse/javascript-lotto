0ebf24d32b6f0fdb3870ffe62d4cfb6d83bf99f6
yeongseokim <1903020@ewhain.net>
2022-11-15 09:51:05 +0900

    refact(ErrorChecker): 6개 숫자 검사, 중복 검사 메서드를 클래스로 분리
    1. Lotto 클래스에서 로또 생성할 때
    2. InputAcceptor에서 인풋 처리할 때
    중복 사용되는 기능이므로 클래스로 분리한다.

---
+++ src/ErrorChecker.js
@@ -0,0 +1,12 @@
+class ErrorChecker {
+    static checkSixElementArray(array) {
+        if (array.length !== 6) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+
+    static checkDuplicatedElement(array) {
+        const set = new Set(array);
+        if (set.size !== 6) throw new Error("[ERROR] 로또 번호는 중복되지 않는 6자리 숫자여야 합니다.");
+    }
+}
+
+module.exports = ErrorChecker;
