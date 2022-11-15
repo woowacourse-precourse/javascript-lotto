58197e1e826c740b8f286ac9748f2310d5f1f6fc
oseunghyun <ohsh1477@gmail.com>
2022-11-15 13:07:30 +0900

    refactor(validations): update validations with constants

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,12 +1,20 @@
-const { INPUT_MONEY_ERROR } = require('../constants/constants');
+const {
+  INPUT_MONEY_ERROR,
+  GENERATE_LOTTO_ERROR,
+} = require('../constants/constants');
 
 const validateInputMoney = (money) => {
+  const NUM = /^[0-9]+$/;
   if (!money) {
     throw new Error(INPUT_MONEY_ERROR.NOT_NULL_ALLOWED);
   }
-  if (money < 0) {
-    throw new Error(INPUT_MONEY_ERROR.NUM_OVER_ZERO_ALLOWED);
+  if (!NUM.test(money)) {
+    throw new Error(INPUT_MONEY_ERROR.ONLY_NUM_ALLOWED);
   }
+  // if (money < 0) {
+  //   throw new Error(INPUT_MONEY_ERROR.NUM_OVER_ZERO_ALLOWED);
+  // }
+
   if (money % 1000 !== 0) {
     throw new Error(INPUT_MONEY_ERROR.THOUSAND_UNIT_ALLOWED);
   }
@@ -14,13 +22,13 @@ const validateInputMoney = (money) => {
 
 const validateLotto = (numbers) => {
   if (numbers.length !== 6) {
-    throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    throw new Error(GENERATE_LOTTO_ERROR.SIX_DIGIT_ALLOWED);
   }
   if (new Set(numbers).size !== numbers.length) {
-    throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
+    throw new Error(GENERATE_LOTTO_ERROR.NOT_DUPLICATED_ALLOWED);
   }
   if (!numbers.filter((num) => 1 > num || num > 45)) {
-    throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+    throw new Error(GENERATE_LOTTO_ERROR.NUM_IN_RANGE_ALLOWED);
   }
 };
 
