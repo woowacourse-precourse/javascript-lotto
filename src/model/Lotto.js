ae00a5a03e41c3244e683f1d3cdc9275d195054b
sh981013s <sh981013s@gmail.com>
2022-11-15 13:10:46 +0900

    comment(Lotto): Lotto 파일 메서드 주석 작성

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -3,16 +3,25 @@ const { WINNING_NUMBER_MESSAGE } = require("../constants/messages");
 class Lotto {
   #numbers;
 
+  /**
+   * Lotto class constructor
+   * @param numbers {number[]} [당첨 번호]
+   */
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
   }
 
+  /**
+   * 번호가 6개인지, 정수로 이루어져 있는지 검증하는 메서드
+   * @param userSplitInput {number[]} [당첨 번호]
+   */
   isInputSplitByComma(userSplitInput) {
+    // 6개가 아니라면
     if (userSplitInput.length !== 6) {
       throw new Error(WINNING_NUMBER_MESSAGE.COMMA_NUMBER_ERROR);
     }
-
+    // 정수가 아니라면
     for (const singleElement of userSplitInput) {
       if (!/^\d+$/.test(singleElement.toString())) {
         throw new Error(WINNING_NUMBER_MESSAGE.COMMA_NUMBER_ERROR);
@@ -20,6 +29,10 @@ class Lotto {
     }
   }
 
+  /**
+   * 번호가 1~45 범위의 숫자인지 검증하는 메서드
+   * @param userSplitInput {number[]} [당첨 번호]
+   */
   isSingleLottoElementInRange(userSplitInput) {
     for (const singleElement of userSplitInput) {
       if (1 > singleElement || singleElement > 45) {
@@ -28,6 +41,10 @@ class Lotto {
     }
   }
 
+  /**
+   * 번호들이 겹치지 않는 6개의 수로 이루어져 있는지 검증하는 메서드
+   * @param userSplitInput {number[]} [당첨 번호]
+   */
   isLottoUnique(userSplitInput) {
     const numbersSet = new Set(userSplitInput);
     if (numbersSet.size !== 6) {
@@ -35,12 +52,20 @@ class Lotto {
     }
   }
 
+  /**
+   * 유저 인풋에 대한 전체적인 검증 메서드
+   * @param userSplitInput {number[]} [당첨 번호]
+   */
   validate(userSplitInput) {
     this.isInputSplitByComma(userSplitInput);
     this.isSingleLottoElementInRange(userSplitInput);
     this.isLottoUnique(userSplitInput);
   }
 
+  /**
+   * 당첨 번호 반환 메서드
+   * @return {number[]} [당첨 번호]
+   */
   getLottoNumbers() {
     return this.#numbers;
   }
