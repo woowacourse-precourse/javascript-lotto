fc5536c9fd35f5dd5c4aef6c4a2d4b22ad671219
choieastsea <choieastsea@gmail.com>
2022-11-15 23:10:05 +0900

    refactor: user test 리팩토링

--- __tests__/UserTest.js
+++ __tests__/UserTest.js
@@ -1,20 +1,19 @@
-const Lotto = require('../src/Lotto');
 const User = require('../src/User');
 
 describe('유저 클래스 테스트', () => {
   test('금액은 숫자만 가능하다', () => {
-    expect(()=>{
-        new User('1000b');
-    }).toThrow('[ERROR]')
+    expect(() => {
+      new User('1000b');
+    }).toThrow('[ERROR]');
   });
   test('금액은 1000원 단위여야한다', () => {
-    expect(()=>{
-        new User('120002');
-    }).toThrow('[ERROR]')
+    expect(() => {
+      new User('120002');
+    }).toThrow('[ERROR]');
   });
   test('금액은 1000원보다 커야 한다', () => {
-    expect(()=>{
-        new User('0');
-    }).toThrow('[ERROR]')
+    expect(() => {
+      new User('0');
+    }).toThrow('[ERROR]');
   });
 });
