b549d55c0d834540813233c5879db29b06a65ff6
SorinKwon <ksl020700@gmail.com>
2022-11-15 17:33:27 +0900

    feat: 로또 번호를 배열로 변환하는 기능 추가

--- src/game-machine/ResultStats.js
+++ src/game-machine/ResultStats.js
@@ -1,11 +1,22 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const DrawLotto = require("../draw-machine/DrawLotto");
+const Lotto = require("../game-machine/Lotto");
 class ResultStats {
+  #lotto_num=[];
+
+  constructor(numbers){
+    this.lotto_num=numbers;
+  }
   
   getDrawAndBonusNumber(draw_number,bonus_number){
-    console.log(draw_number,bonus_number);
+
   }
+
+  changeToArray(numbers){
+    let lotto_list = (numbers + "").split(",");
+    this.getLottoNumbers(lotto_list);
+  }
+
 }
 
-const result_stats = new ResultStats();
 module.exports = ResultStats;
