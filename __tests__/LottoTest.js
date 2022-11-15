750364989dba40ca82db52146cf46beae57e22c6
이석준 <sukjune96@gmail.com>
2022-11-15 22:20:28 +0900

    test: 구매금액 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -46,3 +46,22 @@ describe("보너스 클래스 테스트", () => {
     }).toThrow(ERROR_MESSAGE.NOT_RANGE);
   });
 });
+describe("구매금액 예외 테스트", () => {
+  test("구매금액이 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      validatePayment("money");
+    }).toThrow(ERROR_MESSAGE.NOT_NUMBER);
+  });
+
+  test("구매금액이 1,000원 미만이면 예외가 발생한다.", () => {
+    expect(() => {
+      validatePayment("900");
+    }).toThrow(ERROR_MESSAGE.LESS_MONEY);
+  });
+
+  test("구매금액이 1,000원 단위가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      validatePayment("1234");
+    }).toThrow(ERROR_MESSAGE.NOT_DIVIDED);
+  });
+});
