d892e90faa6366f6a050cf478f2abe312775a9bc
MAGHC <windowsapple95@gmail.com>
2022-11-15 16:16:15 +0900

    test: Lotto 클래스 기능 test 코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -8,12 +8,25 @@ describe('로또 클래스 테스트', () => {
     }).toThrow(LOTTO_ERROR_MESSAGES.LENGTH_ONLY_SIX);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow(LOTTO_ERROR_MESSAGES.UNIQUE);
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또를 전달한 갯수만큼 생성했는지 확인한다.', () => {
+    const lengths = [6, 5, 4, 3];
+
+    lengths.forEach((amount) => {
+      expect(Lotto.generateLottoArrays(amount)).toHaveLength(amount);
+    });
+  });
+
+  test('입력받은 문자열 숫자 배열로 만드는 기능 확인', () => {
+    const input = '1,2,3,4,5,6';
+    const result = Lotto.covertStringToNumberArray(input);
+    const answer = [1, 2, 3, 4, 5, 6];
+
+    expect(result).toEqual(answer);
+  });
 });
