0cd47dbab6fd97b8c4848af9a970ec2ae71f779c
Dongkeun <hongdongk@naver.com>
2022-11-15 19:11:12 +0900

    기능2(1): 구입갯수구하기

--- src/App.js
+++ src/App.js
@@ -11,14 +11,22 @@ class App {
 
   purchaseLotto() {
     Console.readLine("구입금액을 입력해 주세요.", (money) => {
+      const numberoflotto = (money / 1000);
       this.purchasemoney = money;
+      
       this.validatePrice();
+      this.countingLotto(numberoflotto);
     });
   }
 
   validatePrice() {
     if (this.purchasemoney % 1000 !== 0) throw new Error("[ERROR] 1000단위의 수를 입력해주세요!");
   }
+
+  countingLotto(numberoflotto) {
+    Console.print(`${numberoflotto}개를 구매했습니다.`);
+  }
+
 
 }
 
