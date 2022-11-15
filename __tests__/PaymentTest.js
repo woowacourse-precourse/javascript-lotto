56676f504a90e81ee23a52aa48a7ad9d9daed000
dhkham <khamdong@gmail.com>
2022-11-15 12:59:59 +0900

    test(Payment.js): add test that checks if payment amount is a multiple of 1000

--- __tests__/PaymentTest.js
+++ __tests__/PaymentTest.js
@@ -7,23 +7,17 @@ describe("페이먼트 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
+  test("구매금액이 1000원 단위가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Payment(1001);
+    }).toThrow("[ERROR]");
+  });
 
 
 
 
 });
 
-// validatePaymentInteger(numbers) {
-//     if (!Number.isInteger(numbers)) {
-//       throw new Error("[ERROR] 구매금액은 정수여야 합니다.");
-//     }
-//   }
-
-//   validatePaymentMultipleOfThousand(numbers) {
-//     if (numbers % 1000 !== 0) {
-//       throw new Error("[ERROR] 구매금액은 1000원 단위여야 합니다.");
-//     }
-//   }
 
 //   validatePaymentNotNegative(numbers) {
 //     if (numbers < 0) {
