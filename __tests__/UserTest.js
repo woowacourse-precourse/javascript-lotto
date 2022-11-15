bb4384ff3e1a4d67738d7d7e82d0b51474abba31
choieastsea <choieastsea@gmail.com>
2022-11-15 23:24:21 +0900

    style: test code 개행 수정

--- __tests__/UserTest.js
+++ __tests__/UserTest.js
@@ -6,11 +6,13 @@ describe('유저 클래스 테스트', () => {
       new User('1000b');
     }).toThrow('[ERROR]');
   });
+
   test('금액은 1000원 단위여야한다', () => {
     expect(() => {
       new User('120002');
     }).toThrow('[ERROR]');
   });
+
   test('금액은 1000원보다 커야 한다', () => {
     expect(() => {
       new User('0');
