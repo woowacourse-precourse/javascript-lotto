802fcdef42a668e17a7333e709faed18c8f25b41
minh0518 <rubics@naver.com>
2022-11-15 17:14:46 +0900

    feat : 로또 결과 출력 구현 docs : 출력 부분 문장 수정

--- src/LottoResult.js
+++ src/LottoResult.js
@@ -18,6 +18,8 @@ class LottoResult {
 
     let revenue = this.getPrice(countNormalWinner, countBonusWinner)
     let result = this.getRate(revenue)
+
+    this.printResult(countNormalWinner, countBonusWinner,result)
   }
 
   findCorrectValue(userLottoNum, winningNum, lottoBonusNum) {
@@ -37,7 +39,18 @@ class LottoResult {
     return ((revenue / 8000) * 100).toFixed(1)
   }
 
- 
+  printResult(countNormalWinner, countBonusWinner,result){
+    Console.print( `
+    당첨 통계
+    ---
+    3개 일치 (5,000원) - ${countNormalWinner[0]}개
+    4개 일치 (50,000원) - ${countNormalWinner[1]}개
+    5개 일치 (1,500,000원) - ${countNormalWinner[2]}개
+    5개 일치, 보너스 볼 일치 (30,000,000원) - ${countBonusWinner[0]}개
+    6개 일치 (2,000,000,000원) - ${countNormalWinner[3]}개
+    총 수익률은 ${result}%입니다.
+    `)
+  }
 }
 
 module.exports = LottoResult
