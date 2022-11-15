f8657a7e113a3edbb823666b9719a8def9805c6e
spsp8426 <spsp8426@naver.com>
2022-11-15 09:25:05 +0900

    feat : 구입금액 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  constructor() {
+    this.money = null; // 금액 입력 숫자
+    this.lotto = []; // 로또 배열로 받기
+    this.winNum = []; //당첨숫자 배열로 받기
+    this.bonusNum = null; //보너스 번호 숫자
+  }
+  setMoney() {
+    Console.readline("구입금액을 입력해 주세요.\n",(num) => {
+      const money = parseInt(num);
+    })
+  }
+
+  play() {
+  }
 }
 
 module.exports = App;
