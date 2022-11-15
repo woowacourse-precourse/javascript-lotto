7eac439cbd2e4b2138445fd22a7c99d4117edc24
Subin <80036437+hansb1023@users.noreply.github.com>
2022-11-15 14:31:42 +0900

    feature: 로또 번호 중복 예외처리 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,11 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const setNumbers = new Set(numbers);
+    if(setNumbers.length < numbers.length){
+      throw new Error("[ERROR] 로또 번호는 중복되면 안됩니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
