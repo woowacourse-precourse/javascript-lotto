6048c5c70d625a2f9af1ccb699708866141b37ec
Lim_JiSeon <dpf226@naver.com>
2022-11-15 15:34:08 +0900

    feat(Lotto.js) : update exception - deduplication function - outerRange function

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,7 +12,20 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  deduplication(numbers) {
+    const uniq = array => [...new Set(array)];
+    if (uniq(numbers).length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
+  }
+
+  outerRange(numbers) {
+    numbers.forEach( number => {
+      if (number > 0 && number < 46) {
+        throw new Error("[ERROR] 로또 번호의 숫자 범위는 1 ~ 45까지 입니다.");
+      }
+    });
+  }
 }
 
 module.exports = Lotto;
