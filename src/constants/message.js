6c74aa245cd81a4c974d1a0cefe3bbe22b13198b
jiho1996 <jhp9270@naver.com>
2022-11-15 11:55:58 +0900

    refactor : 하드코딩에 대한 상수 정의

--- src/constants/message.js
+++ src/constants/message.js
@@ -13,15 +13,20 @@ const MESSAGE_ACCORDING_ERROR = {
   NOT_INPUTTED: "[ERROR] 입력이 없습니다.",
   NOT_POSITIVE_NUMBER: "[ERROR] 양수만 입력해주세요.",
   TYPE: "[ERROR] 숫자만 입력 가능합니다.",
-  NOT_THOUSAND_UNIT: "[ERROR] 천원 단위 입력을 해주세요."
+  NOT_THOUSAND_UNIT: "[ERROR] 천원 단위 입력을 해주세요.",
+
+  LOTTO_NOT_LENGTH_SIX: "[ERROR] 로또 번호는 6개여야 합니다.",
+  LOTTO_NOT_NUMBER: "[ERROR] 로또 번호는 숫자여야 합니다.",
+  LOTTO_NOT_RANGE: "[ERROR] 로또 번호의 범위는 1~45 입니다.",
+  LOTTO_NOT_DUPLICATED: "[ERROR] 로또 번호는 중복되지 않습니다."
 };
 
 const MESSAGE_ACCORDING_PRIZE_MONEY = {
-  1: "(2,000,000,000원)",
-  2: "(30,000,000원)",
-  3: "(1,500,000원)",
-  4: "(50,000원)",
-  5: "(5,000원)"
+  1: "(2,000,000,000원) -",
+  2: "(30,000,000원) -",
+  3: "(1,500,000원) -",
+  4: "(50,000원) -",
+  5: "(5,000원) -"
 };
 
 const MESSAGE_ACCORDING_CORRECT_COUNT = {
@@ -31,10 +36,16 @@ const MESSAGE_ACCORDING_CORRECT_COUNT = {
   4: "4개 일치",
   5: "3개 일치"
 };
+
+const MESSAGE_ACCORDING_INPUT_ACTION = {
+  RETURN_EARNING_RATE: (percentage) => `총 수익률은 ${percentage}%입니다.`,
+  RETURN_PURCHASED_LENGTH: (size) => `\n${size}개를 구매했습니다.`
+};
 module.exports = {
   MESSAGE_ACCORDING_ASK,
   MESSAGE_ACCORDING_ACTION,
   MESSAGE_ACCORDING_ERROR,
   MESSAGE_ACCORDING_CORRECT_COUNT,
-  MESSAGE_ACCORDING_PRIZE_MONEY
+  MESSAGE_ACCORDING_PRIZE_MONEY,
+  MESSAGE_ACCORDING_INPUT_ACTION
 };
