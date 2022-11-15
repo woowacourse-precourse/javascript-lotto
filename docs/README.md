7404111181035a57994ef3a50fdef29b1590e15e
ElAsJay <tjdwnwkd12371@gmail.com>
2022-11-15 18:01:12 +0900

    feat($Lotto): statistics, check, checkBonus method

--- docs/README.md
+++ docs/README.md
@@ -16,6 +16,7 @@ Added new method to $App:
 
 Breaks LottoCount, which was removed from $App.play
 Changes $App.inputMoney, which calls $App.buyLotto
+
 ---
 feat($App): inputWinning method
 
@@ -24,6 +25,7 @@ Added new method to $App:
 - get input, which is BonusNumber from user
 
 Breaks $App.buyLotto, which calls $App.inputWinning
+
 ---
 feat($Lotto): validate method
 
@@ -32,9 +34,18 @@ Changed the validate method of $Lotto:
 - check if the number is not in range(1~45)
 
 Breaks $Lotto.validate, which the check conditions are added
+
 ---
 feat($LottoTest): test case addition
 
 Added new test case
 - print the error message and quit, if $Lotto contains number which is not in range
+
 ---
+feat($Lotto): statistics, check, checkBonus method
+
+Added new methods to $Lotto:
+- check the count of the winning lotto
+- check if user's lotto includes the winning Bonus
+
+Breaks, $Lotto.statistics, $Lotto.check, $Lotto.checkBonus
--- src/Lotto.js
+++ src/Lotto.js
@@ -14,15 +14,51 @@ class Lotto {
     }
 
     let tmp = []
-    
+  
     numbers.forEach((number)=>{
       if (tmp.includes(number))
         throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
       if(number < 1 || number > 45)
         throw new Error("[ERROR] 로또 번호는 1부터 45사이의 정수만 입력할 수 있습니다.");
+      tmp.push(number);
     });
   }
 
+  statistics(lottoNumbers, bonus){
+    let result = [];
+    result.length = 8;
+    result.fill(0);
+    
+    lottoNumbers.forEach((number)=>{
+      let count = this.check(number, bonus);
+      if(count != 0){
+        result[count] += 1;
+      }
+    });
+    return result;
+  }
+  
+  check(number, bonus){
+    let count = 0;
+    for(let i = 0;i<number.length;i++){
+      if(this.#numbers.includes(number[i]))
+        count += 1;
+    }
+    if(count > 2){
+      if(count == 5){
+        if(this.checkBonus(number, Number(bonus)))
+          return 7;
+        else return count;
+      }
+    }
+    return 0;
+  }
+
+  checkBonus(number, bonus){
+    if(number.includes(bonus))
+      return true;
+    return false;
+  }
 }
 
 module.exports = Lotto;
