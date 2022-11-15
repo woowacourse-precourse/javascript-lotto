eab1d719aff48a43bbbe0ca12436b7ca4542d385
hvvany <kjunhwan1998@gmail.com>
2022-11-15 22:52:32 +0900

    feat: 사용자가 구매한 로또가 몇개인지 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { Random }= require('@woowacourse/mission-utils');
 
 class App {
   play() {}
@@ -10,14 +11,19 @@ class App {
     Console.print('구매금액을 입력해 주세요.');
     Console.readLine('', (userInput) => {
       this.checkPurchaseAmount(userInput);
-    })
+    });
   }
 
   checkPurchaseAmount(userInput) {
-    if(userInput % 1000 !== 0) {
+    if (userInput % 1000 !== 0) {
       throw '[ERROR] 1000원 단위로 금액을 입력하지 않았습니다.'
     }
-    console.log('통과');
+    const numberOfPurchase = userInput / 1000;
+    this.printNumberOfPurchase(numberOfPurchase);
+  }
+
+  printNumberOfPurchase(number) {
+    Console.print(`${number}개를 구매했습니다.`);
   }
 }
 const app = new App();
