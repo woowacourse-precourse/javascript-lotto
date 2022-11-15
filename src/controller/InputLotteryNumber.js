4f5a4fad3e90e5123b6025183955766e05b6064f
xownsdl0 <xownsdl0@naver.com>
2022-11-15 14:00:04 +0900

    feat(lotto): 보너스 입력 번호 구현

--- src/controller/InputLotteryNumber.js
+++ src/controller/InputLotteryNumber.js
@@ -1,10 +1,18 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const inputDataObject = require('../model/InputDataObject')
+const inputMessage = require('../model/InputMessage')
 
 const lotteryNumbers = {
   inputLotteryNumber(message) {
     MissionUtils.Console.readLine(message, inputValue => {
       inputDataObject.raffleNumbers = inputValue.split(',').map(Number);
+      return this.inputBonusNumber(inputMessage.BONUS_NUMBER)
+    });
+  },
+
+  inputBonusNumber(message) {
+    MissionUtils.Console.readLine(message, inputValue => {
+      inputDataObject.bonusNumber = Number(inputValue);
       return null
     });
   },
