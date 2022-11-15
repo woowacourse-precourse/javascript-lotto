b51f4d77f774e00fc7d8a15c590fa4d41665a709
knh6269 <knh6269@gmail.com>
2022-11-15 22:28:51 +0900

    feat(Score): 생성자 함수
    기능 목록(Score-1)

--- src/Score.js
+++ src/Score.js
@@ -1,4 +1,13 @@
 class Score {
+    constructor () {
+        this.userLotto = null
+        this.correctNumber = null;
+        this.bonusNumber = 0;
+        this.score = new Array(5).fill(0);
+        this.money = 0;
+        this.profit = 0;
+    }
+
 }
 
 module.exports = Score;
