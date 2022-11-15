0f6fe41945ec1df5e6ac397c04a6c748d6890f3d
SALT0601 <thfxm0601@gmail.com>
2022-11-15 20:54:34 +0900

    feat: 구입한 개수의 로또 번호 출력
    8개 구매했다면 8개의 로또 번호 출력

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const { Console } = MissionUtils;
+const { Console, Random } = MissionUtils;
 const InpuVal = require('./InputVal');
 
 class App {
+  play() {
+    this.inputMoney();
+  }
   inputMoney(){
     Console.readLine("구입금액을 입력해 주세요.", (money) => {
       Console.print(money);
       let InpuValidation = new InpuVal(money);
+      let cnt = money / 1000;
+      Console.print(cnt + "개를 구매했습니다.");
+      this.buyLotto(cnt);
+    
     });
   }
 
-  play() {
-    this.inputMoney();
-  }
+  createLottoNum() {
+  let lotto = Random.pickUniqueNumbersInRange(1, 45, 6);
+  return lotto;
+ } 
+  buyLotto(cnt){
+    let lottoArr = [];
+    for(let i=0; i<cnt;i++){
+      let lotto = this.createLottoNum();
+      lottoArr.push(lotto);
+    }
+    for (const lotto of lottoArr) {
+      Console.print('[' + lotto + ']');
+    }
+ }
 
 }
 
