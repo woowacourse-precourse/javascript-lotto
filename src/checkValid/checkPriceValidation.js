69fc863d55a516e3a2edad73029f943d8cb0c62e
gsong <rmsdlf1206@gmail.com>
2022-11-15 16:48:47 +0900

    feat: 구입 금액 input 유효성 검사 숫자로만 이루어져 있는지 확인

--- src/checkValid/checkPriceValidation.js
+++ src/checkValid/checkPriceValidation.js
@@ -3,7 +3,13 @@ function checkPriceUnits(input) {
     throw new Error('[ERROR] 1000원단위로 입력해주세요.');
   }
 }
+function checkPriceDataType(input) {
+  if (/[^(0-9)]/gi.test(input)) {
+    throw new Error('[ERROR] 숫자를 입력해주세요.');
+  }
+}
 function checkPriceValidation() {
+  checkPriceDataType();
   checkPriceUnits();
 }
 
