c569318ac94b628a09202d0ff945d5c556229138
SALT0601 <thfxm0601@gmail.com>
2022-11-15 20:25:47 +0900

    feat: 금액 입력 유효성검사

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const { Console } = MissionUtils;
+const InpuVal = require('./InputVal');
 
 class App {
   inputMoney(){
     Console.readLine("구입금액을 입력해 주세요.", (money) => {
       Console.print(money);
+      let InpuValidation = new InpuVal(money);
     });
   }
 
