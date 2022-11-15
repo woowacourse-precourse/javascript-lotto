413920354bf280c1683f04d67815a46a59b6c7bb
recomplete <wantop@jbnu.ac.kr>
2022-11-15 21:46:48 +0900

    feat : 사용자 입력 및 출력 포맷 구현

--- src/Store.js
+++ src/Store.js
@@ -1,7 +1,19 @@
+const { generateRandomNumbers } = require('./Toolbox');
+
 class Store {
     constructor() {
         this.purchase;
         this.amount;
+        this.generatedLottos = [];
+    }
+
+    setStoreVars(number) {
+        this.purchase = Number(number);
+        this.amount = this.purchase / 1000;
+        for (let i = 0; i < this.amount; i++) {
+            let randomSixNumbers = generateRandomNumbers(1, 45, 6);
+            this.generatedLottos.push(randomSixNumbers);
+        }
     }
 }
 
