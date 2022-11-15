5a6d1a373add0ad4b9dc856a705d120164564622
seungki <sysh9498@gmail.com>
2022-11-15 20:02:42 +0900

    test(Validation): 정답 로또 번호 입력에 대한 예외처리 테스트 케이스 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -22,4 +22,38 @@ describe('Validation 클래스 테스트', () => {
   test('올바른 구입 금액 테스트 케이스', () => {
     expect(Validation.checkInputPrice(1000)).toBe(true);
   });
+
+  test('당첨 번호가 6개가 아니면 예외가 발생(5개)', () => {
+    expect(() => {
+      Validation.checkInputHitLottoNumber('1,2,3,4,5');
+    }).toThrow('[ERROR]');
+  });
+
+  test('당첨 번호가 6개가 아니면 예외가 발생(7개)', () => {
+    expect(() => {
+      Validation.checkInputHitLottoNumber('1,2,3,4,5,6,7');
+    }).toThrow('[ERROR]');
+  });
+
+  test('당첨 번호에 중복된 숫자가 있으면 예외가 발생.', () => {
+    expect(() => {
+      Validation.checkInputHitLottoNumber('1,2,3,4,5,5');
+    }).toThrow('[ERROR]');
+  });
+
+  test('당첨 번호에 1~45가 아닌 숫자가 있으면 예외가 발생', () => {
+    expect(() => {
+      Validation.checkInputHitLottoNumber('1,2,3,4,5,50');
+    }).toThrow('[ERROR]');
+  });
+
+  test('당첨 번호가 숫자가 아니면 예외가 발생', () => {
+    expect(() => {
+      Validation.checkInputHitLottoNumber('1,2,3,4,5,6q');
+    }).toThrow('[ERROR]');
+  });
+
+  test('올바른 당첨 번호 입력 테스트 케이스', () => {
+    expect(Validation.checkInputHitLottoNumber('1,2,3,4,5,6')).toBe(true);
+  });
 });
