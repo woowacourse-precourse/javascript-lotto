75fc14469125f8cf2161aa693580e89ecc7c8674
jihun1233 <leejihun1233@gmail.com>
2022-11-15 21:21:25 +0900

    feat: 기능1 입력받기 구현
    - getInput에 구현: MissionUtils의 Console.readLine을 활용해 입력을 받는 Promise를 반환하여 비동기적으로 처리할 수 있도록 함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const MissionUtils = require('@woowacourse/mission-utils')
+const getInput = require('./getInput.js')
+
 class App {
-  play() {}
+  async play() {
+    const price = await getInput('구입금액을 입력해 주세요\n')
+    MissionUtils.Console.print(price)
+
+    const matchNumbers = await getInput('당첨 번호를 입력해 주세요\n')
+    MissionUtils.Console.print(matchNumbers)
+
+    const bonusNumber = await getInput('보너스 번호를 입력해 주세요\n')
+    MissionUtils.Console.print(bonusNumber)
+    
+    MissionUtils.Console.close()
+  }
 }
+
+const app = new App()
+app.play()
 
 module.exports = App;
