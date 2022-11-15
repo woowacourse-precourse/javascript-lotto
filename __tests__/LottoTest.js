ba3bb729cce958db6e82582e55e88a9d732d3ac0
chee9835 <huisu121@gmail.com>
2022-11-15 21:52:42 +0900

    test: 로또 번호,  당첨 번호 비교하기

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -42,4 +42,38 @@ describe("로또 클래스 테스트", () => {
       lotto.setBonusNumber(1);
     }).toThrow("[ERROR]");
   });
+
+  test("로또 번호와 당첨 번호를 비교하기", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.setBonusNumber(7);
+
+    const tickets = [
+      [1, 2, 10, 11, 12, 13],
+      [1, 2, 3, 7, 8, 9],
+      [1, 2, 3, 4, 8, 9],
+      [1, 2, 3, 5, 6, 7],
+    ];
+
+    lotto.compareTickets(tickets);
+
+    expect(lotto.resultMap.get(3)).toEqual(1);
+    expect(lotto.resultMap.get(4)).toEqual(1);
+    expect(lotto.resultMap.get(5)).toEqual(0);
+    expect(lotto.resultMap.get("5B")).toEqual(1);
+    expect(lotto.resultMap.get(6)).toEqual(0);
+  });
+
+  test("번호 5개 일치 후 보너스 번호 비교하기", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.setBonusNumber(7);
+    const tickets = [
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 3, 4, 5, 8],
+    ];
+    const result = [true, false];
+
+    tickets.forEach((ticket, idx) => {
+      expect(lotto.compareBonusNumber(ticket)).toEqual(result[idx]);
+    });
+  });
 });
