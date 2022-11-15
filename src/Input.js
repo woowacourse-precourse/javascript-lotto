cbfcefd1b18f40820c368960d834452684924a55
Sangjun-man <molung@icloud.com>
2022-11-15 22:02:56 +0900

    fix(Lotto) : ""  입력 예외처리 구현
    구매금액 입력 시 ""을 입력하는 경우 예외처리했습니다.
    - 타입변환간 ""를 0원으로 입력하여, 로또를 사지 않아서 예외처리했습니다.

--- src/Input.js
+++ src/Input.js
@@ -1,17 +1,16 @@
-const { Console } = require("@woowacourse/mission-utils");
+const MissionUtils = require("@woowacourse/mission-utils");
 const question = require("./utils/const/question");
 const CustomError = require("./CustomError");
 const ErrorMessage = require("./utils/const/error");
 const ExceptionCheck = require("./ExceptionCheck");
 
-class Input extends Console {
-  constructor() {
-    super();
-  }
-
+class Input {
   static getValueWithType(type, callback, errorCheckparam) {
-    this.readLine(question[type], (string) => {
+    MissionUtils.Console.readLine(`${question[type]}\n`, (string) => {
       const trimmedString = string.trim();
+      if (!trimmedString)
+        throw new CustomError("[ERROR] : 값이 입력되지 않았습니다.");
+
       new InputExceptionCheck().check(type, trimmedString, errorCheckparam);
       callback(trimmedString);
     });
