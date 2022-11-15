f0066562d6ee6815e57505e43c475f325b20d5f5
chaeeun-seo <seochaeeun961126@gmail.com>
2022-11-15 12:07:21 +0900

    feat : get the number of lotto if remainder is zero

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ class App {
     });
   }
 
+  // 로또의 개수를 구함
+  getLottoNumber(money) {
+    if (money % 1000 === 0) {
+      return MissionUtils.Console.print(`${parseInt(money/1000)}개를 구매했습니다.`);
+    } 
+    throw new Error("[ERROR] 1,000원으로 나누어 떨어지지 않습니다.");
+  }
+
   play() {
     this.getMoney();
   }
