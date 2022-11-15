cdaf1f3ba0e14fb94836aea5d35e19ebf0b9ad76
DAIN KIM <dnkim419@gmail.com>
2022-11-15 15:28:31 +0900

    test(StoreTest): 1~45까지 중복되지 않는 6개 숫자 뽑기

--- __tests__/StoreTest.js
+++ __tests__/StoreTest.js
@@ -1,4 +1,5 @@
 const Store = require('../src/Store');
+const { REGEX } = require('../src/constants');
 
 describe('스토어 클래스 테스트', () => {
   test('잘못된 구입 금액을 입력한 경우 에러처리', () => {
@@ -18,5 +19,16 @@ describe('스토어 클래스 테스트', () => {
         store.validate(value);
       }).toThrow('[ERROR]');
     });
+  });
+
+  test('1~45까지 중복되지 않는 6개 숫자 뽑기', () => {
+    const store = new Store();
+    store.pickNumbers();
+    const [lottoArr] = store.lottos;
+    const lottoStr = lottoArr.join(',');
+    const lottoSet = new Set(lottoArr);
+
+    expect(REGEX.WINNING_NUMBER.test(lottoStr)).toBe(true);
+    expect(lottoSet.size).toEqual(6);
   });
 });
