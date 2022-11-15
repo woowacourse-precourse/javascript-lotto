5c5d70b75810cc13f46e2659e13a40efdd54ae69
jinyong <zhaqh146@naver.com>
2022-11-15 13:45:56 +0900

    feat: 구입 금액을 입력 받는 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+
 class App {
-  play() {}
+  play() {
+    this.inputPay();
+  }
+
+  inputPay() {
+    Console.readLine(`구입금액을 입력해 주세요.\n`, answer => {
+      Console.print(answer);
+    })
+  }
+
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
