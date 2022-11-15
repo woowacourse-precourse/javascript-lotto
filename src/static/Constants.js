f5fe64deebb1a7cceaa4468a30b27b462a1ba271
curomame <curomame@naver.com>
2022-11-15 21:18:53 +0900

    ➕ feat - <static/Constants> [LOTTO_REWARD] add
    로또의 상금 상수 정의

--- src/static/Constants.js
+++ src/static/Constants.js
@@ -1,3 +1,11 @@
 const LOTTO_PRICE = 1000;
 
-module.exports = { LOTTO_PRICE };
+const LOTTO_REWARD = {
+  FIRST: 2000000000,
+  SECOND: 30000000,
+  THIRD: 1500000,
+  FOURTH: 50000,
+  FIFTH: 5000,
+};
+
+module.exports = { LOTTO_PRICE, LOTTO_REWARD };
