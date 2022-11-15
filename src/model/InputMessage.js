3d902aaadf2120a340e7faa66fc0e8f8602cd282
xownsdl0 <xownsdl0@naver.com>
2022-11-15 19:41:04 +0900

    style: InputMessage.stats_messae key이름 띄워쓰기 수정

--- src/model/InputMessage.js
+++ src/model/InputMessage.js
@@ -3,11 +3,11 @@ const inputMessage = {
   RAFFLE_NUMBERS: '\n당첨 번호를 입력해 주세요.\n',
   BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
   STATS_MESSAGE: {
-    CORRECT_THREE: '3개 일치 (5,000원) - ',
-    CORRECT_FOUR: '4개 일치 (50,000원) - ',
-    CORRECT_FIVE: '5개 일치 (1,500,000원) - ',
-    CORRECT_BONUS: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
-    CORRECT_SIX: '6개 일치 (2,000,000,000원) - ',
+    CORRECT_THREE: '3개 일치 (5,000원) -',
+    CORRECT_FOUR: '4개 일치 (50,000원) -',
+    CORRECT_FIVE: '5개 일치 (1,500,000원) -',
+    CORRECT_BONUS: '5개 일치, 보너스 볼 일치 (30,000,000원) -',
+    CORRECT_SIX: '6개 일치 (2,000,000,000원) -',
   },
 };
 
