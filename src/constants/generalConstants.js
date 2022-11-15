303881661d81783045ffd4f3cfdb84ca8c780eb0
sh981013s <sh981013s@gmail.com>
2022-11-15 22:07:53 +0900

    comment(generalConstants): 주석 최신화

--- src/constants/generalConstants.js
+++ src/constants/generalConstants.js
@@ -1,6 +1,6 @@
 /**
  * 전체적으로 쓰이는 상수
- * @type {Readonly<{SINGLE_LOTTO_PRICE: number}>} [로또 1장 가격]
+ * @type {Readonly<{THIRD_PRIZE: number, SINGLE_LOTTO_PRICE: number, SECOND_PRIZE: number, FIFTH_PRIZE: number, FOURTH_PRIZE: number, FIRST_PRIZE: number}>}
  */
 const generalConstants = Object.freeze({
   SINGLE_LOTTO_PRICE: 1000,
