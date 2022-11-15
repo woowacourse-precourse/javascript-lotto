91182ebf349eb1b1d3cab40d7f9410b9c47a2bd2
knh6269 <knh6269@gmail.com>
2022-11-15 22:01:59 +0900

    feat(Game): 다음 입력 전까지 메인역할을 하는 함수
    기능목록(Game-4)

--- src/Game.js
+++ src/Game.js
@@ -13,6 +13,17 @@ class Game {
         InputOutput.input(Message.PLEASE_TYPING_MONEY, (money) => this.start(money));
     }
 
+    start (money) {
+        this.checkMoney(money);
+        this.score.setMoney(money); 
+        this.user.setUserLottoCount(money);
+        this.printUserLottoCount();
+        this.user.makeUserLotto();
+        this.score.setUserLotto(this.user.getUserLotto());
+        this.printUserLotto();
+        this.getAnotherInput();
+    }
+
 }
 
 module.exports = Game;
