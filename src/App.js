359a9033c089c1d18d471e343dfccada302b52a6
leeharyung <lhr4884@naver.com>
2022-11-15 20:44:36 +0900

    feat: 사용자에게 돈 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { INPUT, PRINT, LOTTO } = require('./constant/Constant');
+const {
+  checkValidMoneyInput,
+  checkValidWinningNumberInput,
+  checkValidBonusNumberInput,
+} = require('./util/CheckValidInput');
+const LottoStore = require('./LottoStore');
+const LottoRule = require('./LottoRule');
+const LottoChecker = require('./LottoChecker');
+
 class App {
-  play() {}
+  lottoRule = new LottoRule(LOTTO.TOTAL_COUNT, {
+    min: LOTTO.MIN,
+    max: LOTTO.MAX,
+  });
+  store;
+  winningNumber;
+  bonusNumber;
+  lottos;
+  money = 0;
+
+  constructor() {
+    this.store = new LottoStore(this.lottoRule);
+  }
+
+  play() {
+    this.inputMoney();
+  }
+
+  inputMoney() {
+    Console.readLine(INPUT.MONEY, (input) => {
+      checkValidMoneyInput(Number(input));
+      this.money = parseInt(input, 10);
+      this.lottos = this.store.sellLottos(this.money);
+      this.printLotto();
+    });
+  }
 }
 
 module.exports = App;
