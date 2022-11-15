5755ad1f02ffe808831857a0cc9e76abaac99eb1
s0ojin <onepst@hanyang.ac.kr>
2022-11-15 22:48:39 +0900

    feat(App.js) : 수익률을 출력하는 printReturnRate 메서드 작성

--- src/App.js
+++ src/App.js
@@ -64,6 +64,12 @@ class App {
     const result = this.Lotto.getResult(this.UsersLottos, this.winningNumber);
     Console.print(`당첨 통계\n---\n3개 일치 (5,000원) - ${result[0]}개\n4개 일치 (50,000원) - ${result[1]}개\n5개 일치 (1,500,000원) - ${result[2]}개\n5개 일치, 보너스 불 일치 (30,000,000원) - ${result[3]}개\n6개 일치 (2,000,000,000원) - ${result[4]}개
     `);
+    this.printReturnRate(result);
+  }
+
+  printReturnRate(result) {
+    const returnRate = this.User.calculateReturnRate(result);
+    Console.print(`총 수익률은 ${returnRate}% 입니다.`);
   }
 }
 
