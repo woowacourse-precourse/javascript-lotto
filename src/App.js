efbff3b0a66698196db71fff8dd2d698a8107915
Choejeonghun <saa00123@naver.com>
2022-11-15 11:12:19 +0900

    refactor: App.js 수정 및 purchase 클래스 생성

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 class App {
-  play() {
-    this.computer = this.randomNum();
-  }
-
-  randomNum() {
-    let randomNum = [];
-    while (randomNum.length != 7) {
-      let pickNum = MissionUtils.Random.pickNumberInRange(1, 45);
-      if (!randomNum.includes(pickNum)) {
-        randomNum.push(pickNum);
-      }
-    }
-  }
+  play() {}
 }
 
 module.exports = App;
