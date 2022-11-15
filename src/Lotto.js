127f2f1cced1abfff4673cb914a17fe886784660
spsp8426 <spsp8426@naver.com>
2022-11-15 10:24:21 +0900

    feat : 통계를 위한 class 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -14,6 +14,38 @@ class Lotto {
   toString() {
     return `[${this.#numbers.join(", ")}]`;
   }
+  
+  NumberObject() {
+    const numObject = {};
+    for (let i = 1; i <= 45; i++) {
+      numObject[i] = false;
+    }
+    return numObject;
+  }
+  countWinNum(winNumbers) {
+    let winCount = 0;
+    const numObject = this.NumberObject();
+    this.#numbers.forEach((number) => {
+      numObject[number] = true;
+    });
+    winNumbers.forEach((number) => {
+      if (numObject[number]) winCount++;
+    });
+    return winCount;
+  }
+  rank(winNum, bonusNum) {
+    const winCount = this.countWinNum(winNum);
+    switch (winCount) {
+      case 3:
+        return 5;
+      case 4:
+        return 4;
+      case 5:
+        return this.#numbers.includes(bonusNum) ? 2 : 3;
+      case 6:
+        return 1;
+    }
+  }
 }
 
 module.exports = Lotto;
