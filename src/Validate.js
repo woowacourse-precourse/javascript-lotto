6f9faf2157a37c6713be08084044f9e7cf565217
cofla <cofla159@naver.com>
2022-11-15 09:33:35 +0900

    refactor: 에러메세지 상수 분리

--- src/Validate.js
+++ src/Validate.js
@@ -1,27 +1,27 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const { PURCHASE_ERROR, BONUS_ERROR } = require("./constants/ErrorMessage");
 
 function validatePurchase(input) {
   if (isNaN(input)) {
-    throw new Error("[ERROR] 구매 금액을 숫자로 입력해주세요.");
+    throw new Error(PURCHASE_ERROR.notANumber);
   }
   input = Number(input);
   if (input % 1000 !== 0 || !Number.isInteger(input)) {
-    throw new Error("[ERROR] 구매 금액을 1000원 단위로 입력해주세요.");
+    throw new Error(PURCHASE_ERROR.notDividedExactly);
   }
   if (input <= 0) {
-    throw new Error("[ERROR] 유효한 구매 금액을 입력해주세요.");
+    throw new Error(PURCHASE_ERROR.isZeroOrLess);
   }
 }
 function validateBonusNumber(numbers, bonusNumber) {
   if (isNaN(bonusNumber)) {
-    throw new Error("[ERROR] 보너스 번호를 숫자로 입력해주세요.");
+    throw new Error(BONUS_ERROR.notANumber);
   }
   if (numbers.indexOf(bonusNumber) > 0) {
-    throw new Error("[ERROR] 당첨 번호를 제외한 숫자를 입력해주세요.");
+    throw new Error(BONUS_ERROR.duplicateWithWinning);
   }
   bonusNumber = Number(bonusNumber);
   if (bonusNumber > 45 || bonusNumber < 1) {
-    throw new Error("[ERROR] 1~45 범위의 숫자를 입력해주세요.");
+    throw new Error(BONUS_ERROR.outOfRange);
   }
 }
 module.exports = { validateBonusNumber, validatePurchase };
