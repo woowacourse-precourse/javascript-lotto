090625398b01e7f31d42da17925048f02757757e
KES <aprilsmaple@gmail.com>
2022-11-15 17:42:39 +0900

    docs: 기능 목록 완료

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -8,14 +8,12 @@ describe("로또 클래스 테스트", () => {
     }).toThrow(ERROR.incorrect_number_of_lotto_numbers);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow(ERROR.has_duplicate_number);
   });
 
-  // 아래에 추가 테스트 작성 가능
   test("로또 번호 중 범위를 넘어서는 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([0, 2, 3, 4, 5, 46]);
