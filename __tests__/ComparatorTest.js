ceab37b43227db436142d2325e3cfd2c5d81f5b6
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-11-15 21:42:29 +0900

    test(lotto): ComparatorTest 테스트 코드 추가 및 수정

--- __tests__/ComparatorTest.js
+++ __tests__/ComparatorTest.js
@@ -1,7 +1,7 @@
 const Comparator = require("../src/Comparator");
 
 describe("비교기 클래스 테스트", () => {
-  test("6개 일치 1개, 3개 일치 3개", () => {
+  test("6개 일치 1개, 3개 일치 3개, 상금은 총 2000015000", () => {
     const comparator = new Comparator();
     let listOfNumbers = [
       [1, 2, 3, 4, 5, 6],
@@ -18,15 +18,18 @@ describe("비교기 클래스 테스트", () => {
       bonusNumber
     );
     expect(result).toEqual({
-      three: 3,
-      four: 0,
-      five: 0,
-      fivePlusBonus: 0,
-      six: 1,
+      prizeMoney: 2000015000,
+      winningList: {
+        three: 3,
+        four: 0,
+        five: 0,
+        fivePlusBonus: 0,
+        six: 1,
+      },
     });
   });
 
-  test("6개 일치", () => {
+  test("6개 일치, 상금은 총 2000000000", () => {
     const comparator = new Comparator();
     let listOfNumbers = [[1, 2, 3, 4, 5, 6]];
     let winningNumbers = "1, 2, 3, 4, 5, 6";
@@ -37,15 +40,18 @@ describe("비교기 클래스 테스트", () => {
       bonusNumber
     );
     expect(result).toEqual({
-      three: 0,
-      four: 0,
-      five: 0,
-      fivePlusBonus: 0,
-      six: 1,
+      prizeMoney: 2000000000,
+      winningList: {
+        three: 0,
+        four: 0,
+        five: 0,
+        fivePlusBonus: 0,
+        six: 1,
+      },
     });
   });
 
-  test("5개 + 보너스 번호 일치", () => {
+  test("5개 + 보너스 번호 일치, 상금은 총 30000000", () => {
     const comparator = new Comparator();
     let listOfNumbers = [[1, 2, 3, 4, 5, 7]];
     let winningNumbers = "1, 2, 3, 4, 5, 6";
@@ -56,15 +62,18 @@ describe("비교기 클래스 테스트", () => {
       bonusNumber
     );
     expect(result).toEqual({
-      three: 0,
-      four: 0,
-      five: 0,
-      fivePlusBonus: 1,
-      six: 0,
+      prizeMoney: 30000000,
+      winningList: {
+        three: 0,
+        four: 0,
+        five: 0,
+        fivePlusBonus: 1,
+        six: 0,
+      },
     });
   });
 
-  test("5개 일치", () => {
+  test("5개 일치, 상금은 총 1500000", () => {
     const comparator = new Comparator();
     let listOfNumbers = [[1, 2, 3, 4, 5, 8]];
     let winningNumbers = "1, 2, 3, 4, 5, 6";
@@ -75,15 +84,18 @@ describe("비교기 클래스 테스트", () => {
       bonusNumber
     );
     expect(result).toEqual({
-      three: 0,
-      four: 0,
-      five: 1,
-      fivePlusBonus: 0,
-      six: 0,
+      prizeMoney: 1500000,
+      winningList: {
+        three: 0,
+        four: 0,
+        five: 1,
+        fivePlusBonus: 0,
+        six: 0,
+      },
     });
   });
 
-  test("4개 일치", () => {
+  test("4개 일치, 상금은 총 50000", () => {
     const comparator = new Comparator();
     let listOfNumbers = [[1, 2, 3, 4, 7, 8]];
     let winningNumbers = "1, 2, 3, 4, 5, 6";
@@ -94,15 +106,18 @@ describe("비교기 클래스 테스트", () => {
       bonusNumber
     );
     expect(result).toEqual({
-      three: 0,
-      four: 1,
-      five: 0,
-      fivePlusBonus: 0,
-      six: 0,
+      prizeMoney: 50000,
+      winningList: {
+        three: 0,
+        four: 1,
+        five: 0,
+        fivePlusBonus: 0,
+        six: 0,
+      },
     });
   });
 
-  test("3개 일치", () => {
+  test("3개 일치, 상금은 총 5000", () => {
     const comparator = new Comparator();
     let listOfNumbers = [[1, 2, 3, 7, 8, 9]];
     let winningNumbers = "1, 2, 3, 4, 5, 6";
@@ -113,11 +128,14 @@ describe("비교기 클래스 테스트", () => {
       bonusNumber
     );
     expect(result).toEqual({
-      three: 1,
-      four: 0,
-      five: 0,
-      fivePlusBonus: 0,
-      six: 0,
+      prizeMoney: 5000,
+      winningList: {
+        three: 1,
+        four: 0,
+        five: 0,
+        fivePlusBonus: 0,
+        six: 0,
+      },
     });
   });
 });
