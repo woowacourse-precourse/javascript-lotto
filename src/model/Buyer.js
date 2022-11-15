2ca8aa0a1dc852011dcd9daf6d0c4916dcc52c00
yeong bin <2976622@naver.com>
2022-11-15 19:02:48 +0900

    chore(Buyer.js): Buyer 숫자 체크 추가

--- src/model/Buyer.js
+++ src/model/Buyer.js
@@ -3,8 +3,10 @@ const Ticket = require("./Ticket");
 
 class Buyer {
 	static #validate(money) {
-		if (money % Ticket.price())
-			throw new Error("[Error] 1000원 단위로 입력해주세요.");
+		if (/[^0-9]/.test(money))
+			throw new Error("[ERROR] 숫자를 입력해주세요.");
+		else if (money % Ticket.price())
+			throw new Error("[ERROR] 1000원 단위로 입력해주세요.");
 	}
 	static #createRandomNumbers() {
 		return Random
@@ -13,7 +15,7 @@ class Buyer {
 	static buy(money) {
 		Buyer.#validate(money);
 		return Array
-			.from({ length: money / Ticket.price()})
+			.from({ length: parseInt(money) / Ticket.price()})
 			.map(() => new Ticket(Buyer.#createRandomNumbers()));
 	}
 }
