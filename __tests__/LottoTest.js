92fec08f2a18451d51d716c554b05cae0e731abd
DongWooKim97 <nanaukim@naver.com>
2022-11-15 21:14:00 +0900

    test: Lotto클래스에 대한 예외처리 단위 기능 테스트 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,19 +1,29 @@
 /* eslint-disable */
 const Lotto = require("../src/Lotto");
+const { ERROR_MESSAGES } = require("../src/Constants");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR_MESSAGES.INVALID_LOTTO_LENGTH);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR_MESSAGES.DUPLICATED_LOTTO_NUM);
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("로또 번호가 숫자로만 이루어지지 않은 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, "X", "Y", "Z"]);
+    }).toThrow(ERROR_MESSAGES.INVALID_LOTTO_TYPE);
+  });
+
+  test("로또의 각 자리의 번호가 1 ~ 45가 아닐 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 55, 6]);
+    }).toThrow(ERROR_MESSAGES.INVALID_LOTTO_RANGE);
+  });
 });
