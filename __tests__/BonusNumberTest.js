f2ba93688e1a4fcf8ba737da52604940dfd4aede
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-15 10:26:08 +0900

    style : 테스트명 변경

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -1,6 +1,6 @@
 const BonusNumber = require("../src/BonusNumber");
 
-describe("로또 클래스 테스트", () => {
+describe("보너스 번호 클래스 테스트", () => {
     test("보너스 번호가 로또 번호와 중복이 있는 경우 예외 처리", () => {
         expect(() => {
             new BonusNumber(3, [1, 2, 3, 4, 5, 6]);
