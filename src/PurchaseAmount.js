cddcc087cf43336a5d4f7413d94ee5fe68ee341e
hvvany <kjunhwan1998@gmail.com>
2022-11-15 23:35:05 +0900

    feat: 구매금액에 대해 예외 사항을 처리하는 기능 추가하기

---
+++ src/PurchaseAmount.js
@@ -0,0 +1,29 @@
+const { VALIDATE_NUMBER, ERROR_MESSAGE } = require('./utils/Constants');
+
+class PurchaseAmount {
+  checkError(input) {
+    this.isNumber(input);
+    this.isDividedByTen(input);
+    this.isEmpty(input);
+  }
+
+  isNumber(input) {
+    if (isNaN(input) === true) {
+      throw new Error(ERROR_MESSAGE.notMoney);
+    }
+  }
+
+  isDividedByTen(input) {
+    if (input % VALIDATE_NUMBER.moneyUnit !== 0) {
+      throw new Error(ERROR_MESSAGE.moneyUnit);
+    }
+  }
+
+  isEmpty(input) {
+    if (!input) {
+      throw new Error(ERROR_MESSAGE.empty);
+    }
+  }
+}
+
+module.exports = PurchaseAmount;
