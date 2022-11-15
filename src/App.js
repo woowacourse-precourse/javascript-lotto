a8bbb3e1654af7c566ed8622abd21d1e850fd4b9
MIN SIK CHOI <minstones34@gmail.com>
2022-11-15 23:13:11 +0900

    fix: received 값이 밀리던 버그 수정
    []를 포함하여 ,를 기준으로 join해주었음

--- src/App.js
+++ src/App.js
@@ -11,37 +11,34 @@ class App {
   }
 
   getUserInputNumber() {
-    MissionUtils.Console.readLine(
-      "구입금액을 입력해 주세요.\n",
-      (inputMoney) => {
-        const forValidate = parseInt(inputMoney);
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (inputMoney) => {
+      const forValidate = parseInt(inputMoney);
 
-        if (isNaN(inputMoney)) {
-          throw new Error("[ERROR] 숫자를 입력해주십시오.");
-        }
-
-        if (
-          forValidate == "" ||
-          forValidate == null ||
-          forValidate == undefined ||
-          (forValidate != null &&
-            typeof forValidate == "object" &&
-            !Object.keys(forValidate).length)
-        ) {
-          throw new Error("[ERROR] 금액을 제대로 입력해주십시오.");
-        }
-
-        if (forValidate % THOUSAND_WON != 0) {
-          throw new Error("[ERROR] 천 원 단위로 입력해주십시오.");
-        }
-
-        this.userMoney = forValidate;
-        this.lottoCount = forValidate / THOUSAND_WON;
-        this.printPuchaseAmount();
-        this.generateLottoNum();
-        this.inputWinningLotteryNum();
+      if (isNaN(inputMoney)) {
+        throw new Error("[ERROR] 숫자를 입력해주십시오.");
       }
-    );
+
+      if (
+        forValidate == "" ||
+        forValidate == null ||
+        forValidate == undefined ||
+        (forValidate != null &&
+          typeof forValidate == "object" &&
+          !Object.keys(forValidate).length)
+      ) {
+        throw new Error("[ERROR] 금액을 제대로 입력해주십시오.");
+      }
+
+      if (forValidate % THOUSAND_WON != 0) {
+        throw new Error("[ERROR] 천 원 단위로 입력해주십시오.");
+      }
+
+      this.userMoney = forValidate;
+      this.lottoCount = forValidate / THOUSAND_WON;
+      this.printPuchaseAmount();
+      this.generateLottoNum();
+      this.inputWinningLotteryNum();
+    });
   }
 
   printPuchaseAmount() {
@@ -50,15 +47,21 @@ class App {
 
   generateLottoNum() {
     for (let i = 0; i < this.lottoCount; i++) {
-      const lottoRandomNum = MissionUtils.Random.pickUniqueNumbersInRange(
+      let lottoRandomNum = MissionUtils.Random.pickUniqueNumbersInRange(
         1,
         45,
         6
       );
       lottoRandomNum.sort((a, b) => a - b);
-      MissionUtils.Console.print(lottoRandomNum);
       this.lottoNumArr.push(lottoRandomNum);
     }
+    this.printLottoNumArr(this.lottoNumArr);
+  }
+
+  printLottoNumArr(lottoNumArr) {
+    lottoNumArr.map((eachlottoNum) =>
+      MissionUtils.Console.print(`[${eachlottoNum.join(", ")}]`)
+    );
   }
 
   inputWinningLotteryNum() {
