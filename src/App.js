6d42ecdedd3fe826539873871717999b152e75a0
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-15 18:05:01 +0900

    feat(src/App.js): 당첨 번호를 입력 받는 기능 사용자로부터 당첨 번호를 입력받는 getWinLottoNumber() 메소드를 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ const User = require("./User")
 
 class App {
   #User;
+  #Lotto;
 
   play() {
     this.getPerchaseAmount();
     this.#User.createLottoList();
+    this.getWinLottoNumber();
   }
 
   getPerchaseAmount() {
     Console.readLine('구입금액을 입력해 주세요.\n', (money) => {
       this.#User = new User(money);
       Console.close();
+    })
+  }
+
+  getWinLottoNumber() {
+    Console.readLine('\n당첨 번호를 입력해 주세요.', (answer) => {
+      const numbers = answer.split(',').map(Number);
+      this.#Lotto = new this.#Lotto(numbers)
     })
   }
 
