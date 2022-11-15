5720b0344c620e6ca2194bde58fe2c61605a4120
이여진 <jinspace3710@gmail.com>
2022-11-15 23:18:18 +0900

    refactor: 함수 분리 및 상수화한 문자열,숫자 적용

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ const { Console, Random } = MissionUtils;
 const Bonus = require('./Bonus');
 const Cost = require('./Cost');
 const Lotto = require('./Lotto');
+const {
+  VALID_LOTTO,
+  INPUT_MESSAGE,
+  OUTPUT_MESSAGE,
+  LOTTO_RANK_MONEY,
+} = require('./constant');
 
 class App {
   constructor() {
@@ -12,29 +18,36 @@ class App {
     this.bonusNumber;
     this.lottoResult = [0, 0, 0, 0, 0, 0];
     this.rateOfResult;
+    this.lottoQuantity;
   }
 
   getCost() {
-    Console.readLine('구입금액을 입력해 주세요.', userInputCost => {
+    Console.readLine(INPUT_MESSAGE.COST, userInputCost => {
       this.cost = new Cost(userInputCost);
     });
   }
 
-  getLottoArray() {
-    let lottoQuantity = this.cost.getValue() / 1000;
-    Console.print(`${lottoQuantity}개를 구매했습니다.`);
+  printLottoQuantity() {
+    this.lottoQuantity = this.cost.getValue() / 1000;
+    Console.print(`${this.lottoQuantity}${OUTPUT_MESSAGE.LOTTO_QUANTITY}`);
+  }
 
-    for (let i = 0; i < lottoQuantity; i++) {
-      let lotto = Random.pickUniqueNumbersInRange(1, 45, 6);
+  createLottoArray() {
+    for (let i = 0; i < this.lottoQuantity; i++) {
+      let lotto = Random.pickUniqueNumbersInRange(
+        VALID_LOTTO.NUMBER_MIN,
+        VALID_LOTTO.NUMBER_MAX,
+        VALID_LOTTO.NUMBER_COUNT
+      );
       lotto.sort(function (one, two) {
         return one - two;
       });
-      this.getLottoPrint(lotto);
+      this.printLotto(lotto);
       this.lottoArray.push(lotto);
     }
   }
 
-  getLottoPrint(lotto) {
+  printLotto(lotto) {
     let lottoPrint = '[';
     for (let i = 0; i < lotto.length; i++) {
       if (i !== lotto.length - 1) lottoPrint += `${lotto[i]}, `;
@@ -44,7 +57,7 @@ class App {
   }
 
   getWinningLottoNumberArray() {
-    Console.readLine('당첨 번호를 입력해 주세요.', winningLottoNumberInput => {
+    Console.readLine(INPUT_MESSAGE.WINNING_NUMBER, winningLottoNumberInput => {
       let winningLottoNumberInputArray = winningLottoNumberInput
         .split(',')
         .map(number => Number(number));
@@ -53,7 +66,7 @@ class App {
   }
 
   getBonusNumber() {
-    Console.readLine('보너스 번호를 입력해 주세요.', bonumNumberInput => {
+    Console.readLine(INPUT_MESSAGE.BONUS_NUMBER, bonumNumberInput => {
       this.bonusNumber = new Bonus(
         bonumNumberInput,
         this.winningLottoNumberArray.getValue()
@@ -61,48 +74,62 @@ class App {
     });
   }
 
-  getLottoResult() {
+  checkReduplication() {
     let winningLottoArray = this.winningLottoNumberArray.getValue();
     for (let i = 0; i < this.lottoArray.length; i++) {
       let count = 0;
       let bonusBoolean = false;
       this.lottoArray[i].filter(number => {
         if (winningLottoArray.includes(number)) count++;
-        if (number === this.bonusNumber.getValue()) bonusBoolean = true;
+        if (number === Number(this.bonusNumber.getValue())) {
+          bonusBoolean = true;
+        }
       });
-      if (count === 3) this.lottoResult[1]++;
-      if (count === 4) this.lottoResult[2]++;
-      if (count === 5 && !bonusBoolean) this.lottoResult[3]++;
-      if (count === 5 && bonusBoolean) this.lottoResult[4]++;
-      if (count === 6) this.lottoResult[5]++;
+      this.addLottoResult(count, bonusBoolean);
     }
   }
 
-  getRateOfReturn() {
+  addLottoResult(count, bonusBoolean) {
+    if (count === 3) this.lottoResult[1]++;
+    if (count === 4) this.lottoResult[2]++;
+    if (count === 5 && !bonusBoolean) this.lottoResult[3]++;
+    if (count === 5 && bonusBoolean) this.lottoResult[4]++;
+    if (count === 6) this.lottoResult[5]++;
+  }
+
+  calculateRateOfReturn() {
+    let benefitSum = this.addAllbenefit();
+
+    this.rateOfResult = ((benefitSum / this.cost.getValue()) * 100).toFixed(1);
+  }
+
+  addAllbenefit() {
     let benefit = 0;
     for (let i = 0; i < this.lottoResult.length; i++) {
       if (this.lottoResult[i] !== 0) {
-        if (i === 1) benefit += 5000 * this.lottoResult[i];
-        if (i === 2) benefit += 50000 * this.lottoResult[i];
-        if (i === 3) benefit += 1500000 * this.lottoResult[i];
-        if (i === 4) benefit += 30000000 * this.lottoResult[i];
-        if (i === 5) benefit += 2000000000 * this.lottoResult[i];
+        if (i === 1) benefit += LOTTO_RANK_MONEY.FIFTH * this.lottoResult[i];
+        if (i === 2) benefit += LOTTO_RANK_MONEY.FOURTH * this.lottoResult[i];
+        if (i === 3) benefit += LOTTO_RANK_MONEY.THIRD * this.lottoResult[i];
+        if (i === 4) benefit += LOTTO_RANK_MONEY.SECOND * this.lottoResult[i];
+        if (i === 5) benefit += LOTTO_RANK_MONEY.FIRST * this.lottoResult[i];
       }
     }
-    this.rateOfResult = ((benefit / this.cost.getValue()) * 100).toFixed(1);
+    return benefit;
   }
 
-  resultPrint() {
-    Console.print('당첨 통계');
-    Console.print('---');
-    Console.print(`3개 일치 (5,000원) - ${this.lottoResult[1]}개`);
-    Console.print(`4개 일치 (50,000원) - ${this.lottoResult[2]}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${this.lottoResult[3]}개`);
+  printLottoResult() {
+    Console.print(OUTPUT_MESSAGE.WINNING_NOTICE);
+    for (let i = 1; i < OUTPUT_MESSAGE.WINNING_STATISTICS.length; i++) {
+      Console.print(
+        `${OUTPUT_MESSAGE.WINNING_STATISTICS[i]} ${this.lottoResult[i]}${OUTPUT_MESSAGE.COUNT_UNIT}`
+      );
+    }
+  }
+
+  printLottoRateOfResult() {
     Console.print(
-      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.lottoResult[4]}개`
+      `${OUTPUT_MESSAGE.WINNING_RATE_OF_RESULT_NOTICE_START} ${this.rateOfResult}${OUTPUT_MESSAGE.WINNING_RATE_OF_RESULT_NOTICE_END}`
     );
-    Console.print(`6개 일치 (2,000,000,000원) - ${this.lottoResult[5]}개`);
-    Console.print(`총 수익률은 ${this.rateOfResult}%입니다.`);
   }
 
   endLottoGame() {
@@ -111,12 +138,14 @@ class App {
 
   play() {
     this.getCost();
-    this.getLottoArray();
+    this.printLottoQuantity();
+    this.createLottoArray();
     this.getWinningLottoNumberArray();
     this.getBonusNumber();
-    this.getLottoResult();
-    this.getRateOfReturn();
-    this.resultPrint();
+    this.checkReduplication();
+    this.calculateRateOfReturn();
+    this.printLottoResult();
+    this.printLottoRateOfResult();
     this.endLottoGame();
   }
 }
