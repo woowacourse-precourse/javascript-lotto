3d302e6068255ef6dc3c28a2830cc8addf9f6e85
soohyun_kim5 <93cherry@naver.com>
2022-11-15 21:33:27 +0900

    feat: 구입 금액란 입력 에러

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ class App {
 
     purchaseAmount() {
         MissionUtils.Console.readLine("", (purchaseAmount) => {
+            if (!/^\d+$/.test(purchaseAmount))
+                throw new Error("[ERROR] 문자를 입력할 수 없습니다.");
+            const amount = parseInt(purchaseAmount);
+            if (amount === 0)
+                throw new Error("[ERROR] 구입금액이 입력되지 않았습니다.");
+            if (amount % 1000 !== 0)
+                throw new Error("[ERROR] 구입금액의 단위는 1000원입니다.");
             let purchaseAmountString = purchaseAmount.toString();
         });
     }
