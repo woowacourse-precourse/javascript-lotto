fd0c3856077a023df021a933a3d6a429eedba9c5
KimJerry <k90966277@gmail.com>
2022-11-15 20:56:52 +0900

    feat: 로또 번호 중복 체크 유효성 검사 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,27 @@
 class Lotto {
-  #numbers;
+    #numbers;
 
-  constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
-  }
-
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    constructor(numbers) {
+        this.validate(numbers);
+        this.#numbers = numbers;
     }
-  }
 
-  // TODO: 추가 기능 구현
+    validate(numbers) {
+        // 로또 번호의 개수가 6개가 맞는지 체크.
+        if (numbers.length !== 6) {
+            throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+        }
+        // 로또 번호에 중복이 있는지 체크.
+        const dict = {};
+        for (let num of numbers) {
+            if (dict[num] !== undefined) {
+                throw new Error('[ERROR] 중복된 로또 번호가 존재합니다.');
+            }
+            dict[num] = 1;
+        }
+    }
 }
+
+// TODO: 추가 기능 구현
 
 module.exports = Lotto;
