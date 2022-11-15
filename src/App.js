6f7b2a3c89dff6aecdf53a6483f0b01ef6b91a6b
고동현 <dhjk35@naver.com>
2022-11-15 13:57:23 +0900

    feat(lotto): 당첨 번호를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const { LOTTO } = require('./constants');
+const { LOTTO, ERROR_MESSAGE } = require('./constants');
 const { ConsoleAdapter } = require('./adapters');
 const { LottoGenerator } = require('./domain');
 const { ValidationError } = require('./errors');
@@ -17,6 +17,9 @@ class App {
     const lotteryTickets = this.#purchaseLotteryTickets(cost);
 
     this.#printPurchaseInformation(lotteryTickets);
+
+    const winningNumbers = await this.#queryWinningNumbers();
+    console.log(winningNumbers);
   }
 
   async #queryPurchaseCost() {
@@ -56,6 +59,54 @@ class App {
   #printPurchaseInformation(lotteryTickets) {
     this.#console.print(`\n${lotteryTickets.length}개를 구매했습니다.`);
     lotteryTickets.forEach((lotto) => this.#console.print(lotto.numbers));
+  }
+
+  async #queryWinningNumbers() {
+    const input = await this.#requestUserInput('\n당첨 번호를 입력해 주세요.');
+    const winningNumbers = input.split(',').map(Number);
+
+    this.#validateWinningNumbers(winningNumbers);
+
+    return winningNumbers;
+  }
+
+  #validateWinningNumbers(numbers) {
+    const validations = {
+      LENGTH: this.#hasValidLength.bind(this),
+      UNIQUE: this.#hasUniqueValues.bind(this),
+      VALUE: this.#hasNumberValuesOnly.bind(this),
+      RANGE: this.#hasValidRangeValues.bind(this),
+    };
+
+    Object.entries(validations).forEach(([key, validateLotto]) => {
+      if (!validateLotto(numbers)) {
+        throw new ValidationError(ERROR_MESSAGE.WINNING_NUMBERS[key]);
+      }
+    });
+  }
+
+  #hasValidLength(numbers) {
+    return numbers.length === LOTTO.NUMBER_COUNT;
+  }
+
+  #hasUniqueValues(numbers) {
+    return new Set(numbers).size === LOTTO.NUMBER_COUNT;
+  }
+
+  #hasNumberValuesOnly(numbers) {
+    return numbers.every((number) => this.#isNumber(number));
+  }
+
+  #isNumber(value) {
+    return typeof value === 'number';
+  }
+
+  #hasValidRangeValues(numbers) {
+    return numbers.every((number) => this.#isInRange(number));
+  }
+
+  #isInRange(number) {
+    return LOTTO.MIN_NUMBER <= number && number <= LOTTO.MAX_NUMBER;
   }
 }
 
--- src/constants/errors.js
+++ src/constants/errors.js
@@ -5,6 +5,12 @@ const ERROR_MESSAGE = {
     VALUE: '로또 번호는 숫자여야 합니다.',
     RANGE: '로또 번호는 1~45 사이의 수여야 합니다.',
   },
+  WINNING_NUMBERS: {
+    LENGTH: '당첨 번호는 쉼표를 기준으로 6자리의 수를 입력해야 합니다.',
+    UNIQUE: '당첨 번호는 중복되지 않아야 합니다.',
+    VALUE: '당첨 번호는 숫자 이외의 값을 입력할 수 없습니다.',
+    RANGE: '당첨 번호는 1~45 사이의 수여야 합니다.',
+  },
 };
 
 module.exports = ERROR_MESSAGE;
