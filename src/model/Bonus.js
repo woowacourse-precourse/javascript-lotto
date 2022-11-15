e4fcc4a9a04c128077a293544c36de73dbcf6f68
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-15 22:14:43 +0900

    feat: 보너스 모델 getter추가

--- src/model/Bonus.js
+++ src/model/Bonus.js
@@ -3,21 +3,29 @@ const { LOTTO, ERROR_MESSAGE } = require("../constants");
 class Bonus {
   #bonus;
 
-  constructor(bonus) {
-    this.validate(bonus);
+  constructor(bonus, winningNumber) {
+    this.validate(bonus, winningNumber);
     this.#bonus = bonus;
+    this.getBonus = this.getBonus.bind(this);
   }
 
-  validate(bonus) {
+  validate(bonus, winningNumber) {
     const str2num = Number(bonus);
 
     if (!Number.isInteger(str2num)) {
       throw new Error(ERROR_MESSAGE.NAN);
     }
+    if (winningNumber.includes(str2num)) {
+      throw new Error(ERROR_MESSAGE.DUPLICATE);
+    }
     if (str2num < LOTTO.MIN || str2num > LOTTO.MAX) {
       throw new Error(ERROR_MESSAGE.RANGE);
     }
     return true;
+  }
+
+  getBonus() {
+    return this.#bonus;
   }
 }
 
