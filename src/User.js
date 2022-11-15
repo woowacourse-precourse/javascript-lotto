376533803403fa506c4e4d4f909384bc8e486164
rovxxmjxx <miju3545@gmail.com>
2022-11-15 14:40:47 +0900

    feat: 필드 추가

--- src/User.js
+++ src/User.js
@@ -3,9 +3,17 @@ const App = require('./App');
 
 class User {
   #payment;
+  #quantity;
+  #purchasedLotto;
+  #profits;
+  #winningPrize;
 
   constructor() {
     this.#payment = 0;
+    this.#quantity = 0;
+    this.#purchasedLotto = [];
+    this.#profits = 0;
+    this.#winningPrize = 0;
   }
 
   get payment() {
@@ -15,6 +23,34 @@ class User {
   set payment(payment) {
     validatePayment(payment);
     this.#payment = payment;
+  }
+
+  get quantity() {
+    return this.#quantity;
+  }
+
+  set quantity(quantity) {
+    this.#quantity = quantity;
+  }
+
+  get purchasedLotto() {
+    return this.#purchasedLotto;
+  }
+
+  get profits() {
+    return this.#profits;
+  }
+
+  set profits(profits) {
+    this.#profits = profits;
+  }
+
+  get winningPrize() {
+    return this.#winningPrize;
+  }
+
+  set winningPrize(winningPrize) {
+    this.#winningPrize = winningPrize;
   }
 }
 
