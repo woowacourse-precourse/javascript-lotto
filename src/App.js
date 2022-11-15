4b40714a596ce509ca31cf2a156f30aef123690c
Dongkeun <hongdongk@naver.com>
2022-11-15 18:40:18 +0900

    예외1: 구입금액예외처리하기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
 
   purchasemoney;
@@ -10,7 +12,12 @@ class App {
   purchaseLotto() {
     Console.readLine("구입금액을 입력해 주세요.", (money) => {
       this.purchasemoney = money;
+      this.validatePrice();
     });
+  }
+
+  validatePrice() {
+    if (this.purchasemoney % 1000 !== 0) throw new Error("[ERROR] 1000단위의 수를 입력해주세요!");
   }
 
 }
