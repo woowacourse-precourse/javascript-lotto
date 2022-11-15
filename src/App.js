750f8ca0e7c43ba25dd3fffb0fb23688dc2f78a8
Parksolmi <7548ck@naver.com>
2022-11-15 20:45:11 +0900

    feat: 로또 구매 기능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import * as MissionUtils from "@woowacourse/mission-utils";
-
-// console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
 
 class App {
-  play() {}
+
+  constructor() {
+    this.pay = 0;
+    this.lotto = [];
+    this.matchingNumbers = [];
+  }
+  
+  play() {
+    this.getUserInput();
+  }
+
+  getUserInput() {
+    MissionUtils.Console.readLine("구매금액을 입력해 주세요.\n", (input) => {
+      this.pay = input;
+      this.lotto = Lotto.purchase(input);
+      MissionUtils.Console.print(input);
+      MissionUtils.Console.print(`${this.lotto.length}개를 구매했습니다.`);
+      this.printLottos();
+      // this.getWinNumbers()
+    });
+  }
+
+  printLottos() {
+    this.lotto.forEach((lotto) => {
+      MissionUtils.Console.print(`[${lotto.numbers.join(",")}]`);
+    });
+  }
+
+
+
 }
 
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,15 +1,40 @@
+const { MissionUtils } = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
-
+  
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
   }
-
+  
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
+  
+  static purchase(input) {
+
+    if(!isNan(input)) {
+      throw new Error("[Error] 구매금액은 숫자만 입력하세요.");
+    }
+    if(input<1000 || input%1000 != 0) {
+      throw new Error("[Error] 구매금액은 1,000원 단위로 입력하세요.");
+    }
+
+    const lotto = [];
+    const count = input/1000;
+
+    for(let i=0; i<count; i++) {
+      lotto.push(new Lotto(this.drawLottos()));
+    }
+
+    return lotto;
+  }
+
+  static drawLottos() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1,45,6).sort();
   }
 
   // TODO: 추가 기능 구현
