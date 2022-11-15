dc5979b021998045178a5b11e5aa58e41e25db6d
ming0830 <minsun0830@naver.com>
2022-11-15 13:59:32 +0900

    feat : 사용자의 구매금액을 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const LottoMachine = require('./LottoMachine')
+
 class App {
-  play() {}
+  constructor(){
+    this.lottomachine = new LottoMachine();
+    this.lottoQuantity;
+  }
+  play() {
+    this.buyLotto();
+  }
+
+  buyLotto(){
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n",(money) => {
+      const purchaseAmount = parseInt(money)
+      this.lottoQuantity = this.lottomachine.lottoQuantity(purchaseAmount);
+    });
+  }
+
 }
+
+
+
+
+const app = new App();
+app.play();
 
 module.exports = App;
