8ab8981a20a8ccbddbe01ca61e10b8ee36663d3a
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-15 21:10:46 +0900

    feat: 로또 구매 금액 입력 받는 로직

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 const print = require("./console/print");
 const input = require("./console/input");
 const message = require("./util/message");
+const User = require("./User");
+const Validator = require("./Validator");
 
 class App {
   constructor() {
     this.input = new input();
     this.print = new print();
+    this.user = new User();
   }
 
-  play() {}
+  play() {
+    this.buyMessagePrint();
+  }
 
-  buyMessagePrint() {
+  async buyMessagePrint() {
     this.print.print(message.INPUT_MESSAGE);
-    this.input.inputLine().then((data) => {});
+    let buyFee = await this.input.inputLine();
+
+    if (!Validator.isRightNumber(buyFee)) {
+      throw new Error("[ERROR] : 올바른 금액이 아닙니다.");
+
+      return;
+    }
+    this.user.fee = buyFee;
   }
 }
 
