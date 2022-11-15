1468d9dd17e016510a3b5bd5d571d3c73a772e49
jjo-mi <whdal2636@naver.com>
2022-11-15 22:29:44 +0900

    fix: Lotto.js의 생성된 로또번호 중복검사 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,10 +11,10 @@ class Lotto {
     if(numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-  }
-
-  print() {
-    console.log(this.#numbers);
+    var isOverlap = new Set(numbers).size != numbers.length;   
+    if(isOverlap) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
+    }
   }
 
   sortData(numbers) {
