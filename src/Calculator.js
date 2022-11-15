59dd6b47ec1c392e86a6bbd83bd61d70a13d3f38
Jiwon <jiwony933@naver.com>
2022-11-15 20:02:08 +0900

    feat : 전체 상금, 수익률 계산 함수 구현

--- src/Calculator.js
+++ src/Calculator.js
@@ -12,6 +12,21 @@ const Calculator = {
   bonusNum: function (bonusNum, myNums) {
     return myNums.indexOf(bonusNum) !== -1 ? 1 : 0;
   },
+
+  getTotalPrize(stateObject) {
+    let totalPrize =
+      stateObject.matching_3 * 5000 +
+      stateObject.matching_4 * 50000 +
+      stateObject.matching_5 * 1500000 +
+      stateObject.matching_5_bonus * 30000000 +
+      stateObject.matching_6 * 2000000000;
+
+    return totalPrize;
+  },
+
+  getWinningRate(totalPrize, howManyLottos) {
+    return ((totalPrize / (howManyLottos * 1000)) * 100).toFixed(1);
+  },
 };
 
 module.exports = Calculator;
