d18852777a347063c5be23ae20d2e7c826e85f77
yeongseokim <1903020@ewhain.net>
2022-11-15 20:44:35 +0900

    fix(ReturnCalculator): 소수점 계산 오류 수정

--- src/ReturnCalculator.js
+++ src/ReturnCalculator.js
@@ -2,13 +2,12 @@ const PRIZE_MONEY = require('./constants').PRIZE_MONEY;
 class ReturnCalculator {
     static getReturn(result, purchaseAmount) {
         const prizeMoneyList = [PRIZE_MONEY.FIFTH, PRIZE_MONEY.FOURTH, PRIZE_MONEY.THIRD, PRIZE_MONEY.SECOND, PRIZE_MONEY.FIRST];
-        console.log(PRIZE_MONEY);
         let totalPrize = 0;
         for (let i = 0; i < result.length; i++) {
             totalPrize += result[i] * prizeMoneyList[i];
         }
         const totalReturn = totalPrize / purchaseAmount;
-        return totalReturn.toFixed(1) * 100;
+        return (totalReturn * 100).toFixed(1);
     }
 }
 
