c0c043c3ba5f2f58b7b349d7e27b3b578d3c3f23
saram <yujun1018@naver.com>
2022-11-15 15:37:51 +0900

    refactor: DrawMachine 클래스 메서드 분리

--- src/DrawMachine.js
+++ src/DrawMachine.js
@@ -13,11 +13,13 @@ class DrawMachine {
   }
 
   darwWinningNumber() {
-    Console.readLine(PHRASE.WINNING_NUMBER, (input) => {
-      this.validateWinningNumber(input);
-      this.#winningNumber = input.split(',').map((number) => Number(number));
-      this.drawBonusNumber();
-    });
+    Console.readLine(PHRASE.WINNING_NUMBER, this.run.bind(this));
+  }
+
+  run(input) {
+    this.validateWinningNumber(input);
+    this.acceptWinningNumber(input);
+    this.drawBonusNumber();
   }
 
   validateWinningNumber(input) {
@@ -26,18 +28,28 @@ class DrawMachine {
     else throw new Error(ERROR.WINNING_NUMBER);
   }
 
+  acceptWinningNumber(input) {
+    this.#winningNumber = input.split(',').map((number) => Number(number));
+  }
+
   drawBonusNumber() {
-    Console.readLine(PHRASE.BONUS_NUMBER, (input) => {
-      this.validateBonusNumber(input);
-      this.#bonusNumber = Number(input);
-      this.scoreMachine.compare(this.#winningNumber, this.#bonusNumber);
-    });
+    Console.readLine(PHRASE.BONUS_NUMBER, this.run2.bind(this));
+  }
+
+  run2(input) {
+    this.validateBonusNumber(input);
+    this.acceptBonusNumber(input);
+    this.scoreMachine.compare(this.#winningNumber, this.#bonusNumber);
   }
 
   validateBonusNumber(input) {
     if (isNaN(input)) throw new Error(ERROR.BONUS_NUMBER);
     if (input < LOTTO.MIN_NUMBER || input > LOTTO.MAX_NUMBER)
       throw new Error(ERROR.LOTTO_NUMBER_RANGE);
+  }
+
+  acceptBonusNumber(input) {
+    this.#bonusNumber = Number(input);
   }
 }
 
