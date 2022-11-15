dfb0d0c78be7f3ff3aa9ce0f3b2f1e48b4d3257e
h2002y <h2002y@naver.com>
2022-11-15 20:45:38 +0900

    test: Add no luck lotto application test in ApplicationTest.js

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -176,6 +176,29 @@ describe("로또 테스트", () => {
     });
   });
 
+  test("기능 테스트 - 꽝", () => {
+    mockRandoms([
+      [10, 11, 12, 13, 14, 15],
+    ]);
+    mockQuestions(["1000", "1,2,3,4,5,6", "7"]);
+    const logs = [
+      "1개를 구매했습니다.",
+      "[10, 11, 12, 13, 14, 15]",
+      "3개 일치 (5,000원) - 0개",
+      "4개 일치 (50,000원) - 0개",
+      "5개 일치 (1,500,000원) - 0개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+      "6개 일치 (2,000,000,000원) - 0개",
+      "총 수익률은 0%입니다.",
+    ];
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
   test("예외 테스트", () => {
     mockQuestions(["1000j"]);
     expect(() => {
