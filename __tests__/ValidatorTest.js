23dda29d0b6bec09fe4b78cddaaaf988ec18c034
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-15 22:31:14 +0900

    test[Validator2]: 숫자가 아닌 값 입력 예외 테스트 완료

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -11,5 +11,11 @@ describe('입력 공통 예외 테스트', () => {
     }).toThrow('[ERROR] 필수 입력 값을 입력해주세요.');
   });
 
-  
+  test('숫자 입력 예외 테스트', () => {
+    const input = 'asd';
+
+    expect(() => {
+      Validator.isNumber(input);
+    }).toThrow('[ERROR] 숫자가 아닙니다.');
+  });
 });
