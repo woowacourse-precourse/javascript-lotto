5f47851b7b6baaefab026c54e497a14c9693dccb
1 <wrasf175@naver.com>
2022-11-15 22:26:22 +0900

    test(Budget): 테스트 예외 사항 명확하게 수정

--- __tests__/BudgetTest.js
+++ __tests__/BudgetTest.js
@@ -3,25 +3,22 @@ const Budget = require("../src/Budget");
 describe("입력한 금액 예외 처리 테스트", () => {
   test("입력한 금액이 숫자가 아니면 예외가 발생한다.", () => {
     expect(() => {
-      new Budget('abc');
+      new Budget('i000');
     }).toThrow("[ERROR]");
-  });
-
-  test("입력한 금액이 숫자가 아니면 예외가 발생한다.", () => {
     expect(() => {
-      new Budget('3000a');
+      new Budget('가나다');
     }).toThrow("[ERROR]");
   });
 
   test("입력한 금액이 1000원 단위가 아니면 예외가 발생한다.", () => {
     expect(() => {
-      new Budget(3700);
+      new Budget(1001);
     }).toThrow("[ERROR]");
   });
 
   test("입력한 금액이 소수일 경우 예외가 발생한다.", () => {
     expect(() => {
-      new Budget(370.0);
+      new Budget(1000.1);
     }).toThrow("[ERROR]");
   });
 
