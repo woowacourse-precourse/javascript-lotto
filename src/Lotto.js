7bb9133698ec101b11b973c599e24cd58b2c65ef
Hoon <si932174@gmail.com>
2022-11-15 10:57:00 +0900

    feat : 예외 사항 추가<-  로또 번호가 1~45 범위를 벗어난 경우

--- src/Lotto.js
+++ src/Lotto.js
@@ -14,6 +14,8 @@ class Lotto {
     numbers.map((number, index) => {
       if (numbers.indexOf(number) !== index)
         throw new Error('[ERROR] 로또 번호는 중복이 없어야 합니다.');
+      if (number > 45 || number < 1)
+        throw new Error('[ERROR] 로또 번호는 1~45 범위여야 합니다.');
     });
   }
   // TODO: 추가 기능 구현
