b736e81cf6ccbdca9bb68fec69165f8c81d318da
jungsmins1 <jungsmins00@gmail.com>
2022-11-15 18:06:55 +0900

    style: 세미콜론 누락 부분 추가

--- __tests__/LottoPurchaseTest.js
+++ __tests__/LottoPurchaseTest.js
@@ -10,11 +10,11 @@ describe("LottoPurchase 클래스 테스트", () => {
 
   test("구입한 금액 만큼 랜덤한 번호를 발행한다.", () => {
     expect(new LottoPurchase(2000).lottoPublish()
-    ).toHaveLength(2)
+    ).toHaveLength(2);
   })
 
   test("6자리 번호를 오름차순으로 정렬한다.", () => {
-    expect(new LottoPurchase(1000).lottoNumbersSort([[6,5,4,3,2,1]])
-    ).toEqual([[1,2,3,4,5,6]])
+    expect(new LottoPurchase(1000).lottoNumbersSort([[6, 5, 4, 3, 2, 1]])
+    ).toEqual([[1, 2, 3, 4, 5, 6]]);
   })
 });
