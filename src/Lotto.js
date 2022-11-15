17d5b3e3ed085c01a22f717bae533557034ded60
dongyeopca <ckehdduq95@gmail.com>
2022-11-15 13:50:03 +0900

    "feat: 로또 클래스 예외처리 추가" 1~45사이의 숫자가 아닐경우, 중복된 번호가 있을 경우 업데이트

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,24 @@
+const { ANNOUNCE, GRADE, ERROR } = require('./constants/constants');
+
 class Lotto {
   #numbers;
-
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
   }
 
   validate(numbers) {
+    const checkOverlap = () => new Set(numbers).size === 6;
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (numbers.filter((number) => 1 <= number && number <= 45).length != 6) {
+      throw new Error('[ERROR] 로또 번호는 1에서 45 사이의 숫자입니다.');
+    }
+    if (!checkOverlap()) {
+      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.');
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
