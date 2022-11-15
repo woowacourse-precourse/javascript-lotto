bee8494298bff11e1b0a6a3835408c090d158007
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-15 20:34:20 +0900

    test: 로또 클래스 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,60 @@
-const Lotto = require("../src/Lotto");
+const { Console } = require('@woowacourse/mission-utils');
+const { ERROR } = require('../src/Error');
+const Lotto = require('../src/Lotto');
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow(`${ERROR.PREFIX} ${ERROR.NUMBER_COUNT}`);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow(`${ERROR.PREFIX} ${ERROR.NUMBER_DUPLICATE}`);
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또 번호의 숫자 범위가 1~45를 벗어나면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([41, 42, 43, 44, 45, 46]);
+    }).toThrow(`${ERROR.PREFIX} ${ERROR.NUMBER_RANGE}`);
+  });
+
+  test('로또 번호가 숫자가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([5, 6, 7, 8, 9, 'a']);
+    }).toThrow(`${ERROR.PREFIX} ${ERROR.FORMAT}`);
+  });
+
+  test('로또 번호와 당첨 번호를 비교하여 4개의 번호가 일치하면 4등이다.', () => {
+    const lotto = new Lotto([3, 4, 5, 6, 7, 8]);
+    const winningNumbers = new Set([1, 2, 3, 4, 5, 6]);
+    const bonusNumber = 7;
+
+    lotto.match(winningNumbers, bonusNumber);
+    lotto.setRank();
+    expect(lotto.rank).toEqual(4);
+  });
+
+  test('로또 번호와 당첨 번호를 비교하여 5개의 번호가 일치하면 3등이다.', () => {
+    const lotto = new Lotto([2, 3, 4, 5, 6, 7]);
+    const winningNumbers = new Set([1, 2, 3, 4, 5, 6]);
+    const bonusNumber = 45;
+
+    lotto.match(winningNumbers, bonusNumber);
+    lotto.setRank();
+    expect(lotto.rank).toEqual(3);
+  });
+
+  test('로또 번호와 당첨 번호를 비교하여 5개의 번호와 보너스 번호가 일치하면 2등이다.', () => {
+    const lotto = new Lotto([2, 3, 4, 5, 6, 7]);
+    const winningNumbers = new Set([1, 2, 3, 4, 5, 6]);
+    const bonusNumber = 7;
+
+    lotto.match(winningNumbers, bonusNumber);
+    lotto.setRank();
+    expect(lotto.rank).toEqual(2);
+  });
+  Console.close();
 });
