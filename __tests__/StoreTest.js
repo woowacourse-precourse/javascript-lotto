debdb026539ebdd5bb6093b5c1013482917032ea
nailen1 <nietsnie1@gmail.com>
2022-11-15 15:26:12 +0900

    style: 공지가이드 주석 삭제

--- __tests__/StoreTest.js
+++ __tests__/StoreTest.js
@@ -8,14 +8,12 @@ describe("스토어 클래스 테스트", () => {
         }).toThrow("[ERROR]");
     });
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     test("입력된 당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
         expect(() => {
             store.isValidLotto([1, 2, 3, 4, 5, 5]);
         }).toThrow("[ERROR]");
     });
 
-    // 아래에 추가 테스트 작성 가능
     test("입력된 당첨 번호에 숫자 이외의 문자가 있으면 예외가 발생한다.", () => {
         expect(() => {
             store.isValidLotto([1, 'a', 3, 4, 5, 6]);
