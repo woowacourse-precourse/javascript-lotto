1e6a947b0fbcd52b5b72086e035e3c9e5244e661
이석준 <sukjune96@gmail.com>
2022-11-15 17:43:15 +0900

    style: 수익률 출력 변수 상단으로 선언 위치 조정

--- src/App.js
+++ src/App.js
@@ -59,13 +59,13 @@ class App {
     this.printResult(result);
   }
   printResult(result) {
+    const revenue = Calculator.calculateRevenue(result, this.userPurchase);
     Console.print(OUTPUT_MESSAGE.WINNING_NOTICE);
     Console.print(OUTPUT_MESSAGE.FIFTH(result.fifth));
     Console.print(OUTPUT_MESSAGE.FOURTH(result.fourth));
     Console.print(OUTPUT_MESSAGE.THIRD(result.third));
     Console.print(OUTPUT_MESSAGE.SECOND(result.second));
     Console.print(OUTPUT_MESSAGE.FIRST(result.first));
-    const revenue = Calculator.calculateRevenue(result, this.userPurchase);
     Console.print(OUTPUT_MESSAGE.RESULT(revenue));
   }
 }
