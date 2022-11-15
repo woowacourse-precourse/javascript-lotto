2a92d9cb23862aba0696a71966b5112c046e1fa3
Hungeun2 <bepoohun@gmail.com>
2022-11-15 14:03:00 +0900

    Refactor: Budget Class money 필드 # prefix 추가

--- src/Budget.js
+++ src/Budget.js
@@ -1,25 +1,26 @@
 const { BUDGET_ERROR_MESSAGE, LOTTO_PRICE } = require("./util/Constant");
 
 class Budget {
+  #money;
   constructor(input) {
-    this.money = input;
+    this.#money = input;
     this.lottoToBuy = 0;
     this.isValidMoney();
   }
   isValidMoney() {
-    if (isNaN(this.money)) {
+    if (isNaN(this.#money)) {
       throw new Error(BUDGET_ERROR_MESSAGE.NON_NUMBER);
     }
-    if (this.money < 1000) {
+    if (this.#money < 1000) {
       throw new Error(BUDGET_ERROR_MESSAGE.SHORT_OF_MONEY);
     }
-    if (this.money % 1000 !== 0) {
+    if (this.#money % 1000 !== 0) {
       throw new Error(BUDGET_ERROR_MESSAGE.INDIVISIBLE);
     }
     this.calLottoToBuy();
   }
   calLottoToBuy() {
-    this.lottoToBuy = this.money / LOTTO_PRICE;
+    this.lottoToBuy = this.#money / LOTTO_PRICE;
   }
 }
 
