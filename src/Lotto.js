b2ded2f90e48ffd15d2f9114a8507640a953469d
jeno8522 <68889383+jeno8522@users.noreply.github.com>
2022-11-15 21:41:20 +0900

    Constructor 설정
        myPay : 구매 금액
        myLottoNumbers : 구매 번호 Array
        winNumbers : 당첨 번호
        bonusNumber : 보너스 번호
        rewardLotto : 순위 저장

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,9 +1,23 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Data = require("./Data");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
+    this.myPay = 0;
+    this.myLottoNumbers = [];
+    this.winNumbers = 0;
+    this.bonusNumber = 0;
     this.validate(numbers);
     this.#numbers = numbers;
+    this.rewardLotto = {
+      first: 0,
+      second: 0,
+      third: 0,
+      fourth: 0,
+      fifth: 0,
+    };
   }
 
   validate(numbers) {
