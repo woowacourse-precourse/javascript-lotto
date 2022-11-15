8dad10003bf661a4e89715e7228598afb368e7b4
HeoJiye <heoze1031@gmail.com>
2022-11-15 13:38:28 +0900

    test: 당첨 내역을 출력하는 기능 테스트 추가

--- __tests__/LottoDrawTest.js
+++ __tests__/LottoDrawTest.js
@@ -41,3 +41,37 @@ describe("예외 테스트", () => {
         expect(() => draw.setBonusNum(Number('46'))).toThrow("[ERROR]");
     });
 });
+
+describe("기능 테스트", () => {
+    test("당첨 내역을 출력하는 기능", () => {
+        const lottos = [
+            new Lotto([8, 21, 23, 41, 42, 43]),
+            new Lotto([3, 5, 11, 16, 32, 38]),
+            new Lotto([7, 11, 16, 35, 36, 44]),
+            new Lotto([1, 8, 11, 31, 41, 42]),
+            new Lotto([13, 14, 16, 38, 42, 45]),
+            new Lotto([7, 11, 30, 40, 42, 43]),
+            new Lotto([2, 13, 22, 32, 38, 45]),
+            new Lotto([1, 3, 5, 14, 22, 45]),
+        ];
+
+        const logs = [
+            "3개 일치 (5,000원) - 1개",
+            "4개 일치 (50,000원) - 0개",
+            "5개 일치 (1,500,000원) - 0개",
+            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+            "6개 일치 (2,000,000,000원) - 0개",
+        ];
+
+        const logSpy = getLogSpy();
+        
+        const draw = new LottoDraw();
+        draw.setWinningNums(['1', '2', '3', '4', '5', '6'].map(Number));
+        draw.setBonusNum(Number('7'));
+        draw.checkResult(lottos);
+
+        logs.forEach((log) => {
+            expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+        });
+    });
+});
