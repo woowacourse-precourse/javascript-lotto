99ca4c0b8a3cf82bf2ac5191dc3a9534170fb8e1
xownsdl0 <xownsdl0@naver.com>
2022-11-15 11:12:28 +0900

    style: BuyLotto.js 안의 객체이름 변경

--- src/controller/BuyLotto.js
+++ src/controller/BuyLotto.js
@@ -2,16 +2,16 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const inputDataObject = require('../model/InputDataObject')
 const checkError = require('../controller/CheckError')
 
-const inputMoneyOfLotto = {
+const buyLotto = {
   inputMoney(message) {
     this.message = message;
     MissionUtils.Console.readLine(message, inputValue => {
       if (checkError(inputValue)) {
-        inputObject.principal = inputValue; 
+        inputDataObject.principal = inputValue; 
       }
       return inputDataObject.principal
     });
   },
 };
 
-module.exports = inputMoneyOfLotto;
+module.exports = buyLotto;
