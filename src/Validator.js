7bbc67117ef07ee8946018803789e986b132858d
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-15 22:29:59 +0900

    feat(Validator): 숫자가 아닌 값을 입력하는 경우 예외처리

--- src/Validator.js
+++ src/Validator.js
@@ -5,6 +5,13 @@ class Validator {
       }
       throw new Error('[ERROR] 필수 입력 값을 입력해주세요.');
     }
+
+    static isNumber (string) {
+        if (!isNaN(string)) {
+          return true;
+        }
+        throw new Error('[ERROR] 숫자가 아닙니다.');
+      }
   
 }
   
