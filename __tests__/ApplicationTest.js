29adc046ae8ab978d783265ad5e3a1f9d672b689
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-15 19:23:50 +0900

    refactor(test): separate unit test by domain
    - Validate input test
    - Application test
    - Lotto test

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,4 @@
 const App = require('../src/App');
-const Lotto = require('../src/Lotto');
 const MissionUtils = require('@woowacourse/mission-utils');
 
 const mockQuestions = (answers) => {
@@ -62,89 +61,11 @@ describe('로또 테스트', () => {
     });
   });
 
-  test('예외 테스트 - 구입금액[숫자]', () => {
+  test('예외 테스트', () => {
     mockQuestions(['1000j']);
     expect(() => {
       const app = new App();
       app.play();
-    }).toThrow('[ERROR] 구입금액은 숫자여야 합니다.');
-  });
-
-  test('예외 테스트 - 구입금액[1000단위]', () => {
-    mockQuestions(['2300']);
-    expect(() => {
-      const app = new App();
-      app.play();
-    }).toThrow('[ERROR] 구입금액은 1000단위여야 합니다.');
-  });
-
-  test('예외 테스트 - 구입금액[0]', () => {
-    mockQuestions(['0']);
-    expect(() => {
-      const app = new App();
-      app.play();
-    }).toThrow('[ERROR] 구입금액은 0이상 이여야 합니다.');
-  });
-
-  test('예외 테스트 - 로또[숫자]', () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 'ab']);
-    }).toThrow('[ERROR] 로또 번호는 숫자여야 합니다.');
-  });
-
-  test('예외 테스트 - 로또[범위]', () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 49]);
-    }).toThrow('[ERROR] 로또 번호는 1부터 45 사이여야 합니다.');
-  });
-
-  test('예외 테스트 - 당첨번호[길이]', () => {
-    mockRandoms([[8, 21, 23, 41, 42, 43]]);
-    mockQuestions(['1000', '1,2,3,4,5,6,7']);
-    expect(() => {
-      const app = new App();
-      app.play();
-    }).toThrow('[ERROR] 로또 번호는 6개여야 합니다.');
-  });
-
-  test('예외 테스트 - 보너스번호[중복]', () => {
-    mockRandoms([[8, 21, 23, 41, 42, 43]]);
-    mockQuestions(['1000', '1,2,3,4,5,6', '5']);
-    expect(() => {
-      const app = new App();
-      app.play();
-    }).toThrow('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
-  });
-
-  test('보너스 테스트 - 맞았을 때(2등)', () => {
-    mockRandoms([[8, 21, 23, 41, 42, 43]]);
-    mockQuestions(['1000', '8,21,23,41,42,6', '43']);
-    const logs = [
-      '1개를 구매했습니다.',
-      '[8, 21, 23, 41, 42, 43]',
-      '5개 일치, 보너스 볼 일치 (30,000,000원) - 1개',
-    ];
-    const logSpy = getLogSpy();
-    const app = new App();
-    app.play();
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
-  });
-
-  test('로또 결과 테스트 - (3등)', () => {
-    mockRandoms([[8, 21, 23, 41, 42, 43]]);
-    mockQuestions(['1000', '8,21,23,41,42,6', '40']);
-    const logs = [
-      '1개를 구매했습니다.',
-      '[8, 21, 23, 41, 42, 43]',
-      '4개 일치 (50,000원) - 1개',
-    ];
-    const logSpy = getLogSpy();
-    const app = new App();
-    app.play();
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
+    }).toThrow('[ERROR]');
   });
 });
