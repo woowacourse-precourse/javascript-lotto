aa600b856d1e3d3aaeb70d260c7a39a5ea94ef33
curomame <curomame@naver.com>
2022-11-15 22:00:39 +0900

    ➕ feat - <App> [printPurchasedLottoList, returnListToStringList] add
    구입한 로또 리스트를 출력하는 메서드, 리스트를 문자열로 변경하는 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   #inputMoney = 0;
   #purchaseLottoList = [];
@@ -6,6 +8,22 @@ class App {
   #matchingNumberCountObj = {};
 
   play() {}
+
+  printPurchasedLottoList() {
+    const purchaseLottoList = this.#purchaseLottoList;
+    for (let lotto of purchaseLottoList) {
+      MissionUtils.Console.print(this.returnListToStringList(lotto));
+    }
+    MissionUtils.Console.print("\n");
+  }
+
+  returnListToStringList(list) {
+    let strList = list.join(", ");
+    strList.trim();
+    let output = "[" + strList + "]";
+    return output;
+  }
+
 }
 
 module.exports = App;
