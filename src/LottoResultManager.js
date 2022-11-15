625f0a622a75fb28995be1f4466c9191d2164ce0
hyeongdong <khd941018@gmail.com>
2022-11-15 20:16:07 +0900

    feat(LottoResultManager) : 결과값(당첨내역,수익률)을 출력하는 기능 구현

--- src/LottoResultManager.js
+++ src/LottoResultManager.js
@@ -1,5 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const LottoCalculator = require("./LottoCalculator");
+const LottoCalculator = require("./");
 
 class LottoResultManager {
   constructor() {
@@ -21,6 +21,26 @@ class LottoResultManager {
         this.winCount[winRank] += 1;
       }
     });
+  }
+
+  makeWinHistoryMessage() {
+    this.WinHistoryMessage += `3개 일치 (5,000원) - ${this.winCount.rank5}개
+    4개 일치 (50,000원) - ${this.winCount.rank4}개
+    5개 일치 (1,500,000원) - ${this.winCount.rank3}개
+    5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.winCount.rank2}개
+    6개 일치 (2,000,000,000원) - ${this.winCount.rank1}개`;
+  }
+
+  calculateProfitRate(payment) {
+    const lottoCalculator = new LottoCalculator();
+    lottoCalculator.sumRewards(this.winCount);
+
+    this.profitRate = lottoCalculator.divideProfitBy(payment);
+  }
+
+  printResult() {
+    MissionUtils.Console.print(this.WinHistoryMessage);
+    MissionUtils.Console.print(`총 수익률은 ${this.profitRate}%입니다.`);
   }
 }
 
