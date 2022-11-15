3a80cde9253e35804ae62a10562e762ce09f6675
OrangeChick <retww@naver.com>
2022-11-15 20:19:52 +0900

    feat: Implement of a function that outputs the input value of purchase amount
    - 구매 금액의 입력값을 출력하는 기능을 구현하였습니다.
    - 기능 구현 목록을 조금 더 상세하게 수정하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  constructor() {
+    this.userAmount;
+  }
+
   play() {}
+
+  printStartGame() {
+    MissionUtils.Console.readLine("구입금액을 입력해주세요: ", (amount) => {
+      this.userAmount = amount;
+    });
+  }
 }
 
 module.exports = App;
