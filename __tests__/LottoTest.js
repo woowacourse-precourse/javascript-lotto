4e783983390819fc40f65d24bc58ef9e06fe881a
박준서 <oponize@naver.com>
2022-11-15 13:39:17 +0900

    test: Lotto Class

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,5 +1,4 @@
 const Lotto = require("../src/Lotto");
-const Validator = require("../src/utils/Validator");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -16,68 +15,9 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
-});
-
-describe("Validator Util Class", () => {
-  test("인풋 값이 숫자가 아니라면 예외가 발생한다.", () => {
-    const errors = ["", " ", "a", "!^&"];
-    errors.forEach((error) => {
-      expect(() => {
-        Validator.isNumber(error);
-      }).toThrow();
-    });
-  });
-
-  test("인풋 값의 길이가 일치하지 않는다면 예외가 발생한다.", () => {
-    const targets = ["hi", "hello", "", " "];
-    const lengths = [3, 1, 2, 3];
-
-    targets.forEach((target, index) => {
-      expect(() => {
-        Validator.isLength(target, lengths[index]);
-      }).toThrow();
-    });
-  });
-
-  test("인풋 값의 범위가 일치하지 않는다면 예외가 발생한다.", () => {
-    const numbers = [34, 21];
-    const starts = [1, 22];
-    const ends = [3, 24];
-
-    numbers.forEach((number, index) => {
-      expect(() => {
-        Validator.isRange({
-          targe: number,
-          start: starts[index],
-          end: ends[index],
-        });
-      }).toThrow();
-    });
-  });
-
-  test("중복이 존재한다면 예외가 발생한다.", () => {
-    const duplicated = [1, 1, 2, 3, 4, 5, 6];
-    expect(() => {
-      Validator.isDuplicated(duplicated);
-    }).toThrow();
-  });
-
-  test("나눠서 나머지가 존재한다면 예외를 발생한다.", () => {
-    const target = 1200;
-    const unit = 1000;
-    expect(() => {
-      Validator.isDivisible(target, unit);
-    }).toThrow();
-  });
-
-  test("크거나 같지 않다면 예외를 발생한다.", () => {
-    const targets = [-1000, 999];
-    const unit = 1000;
-
-    targets.forEach((target) => {
-      expect(() => {
-        Validator.isGreaterOrEqual(target, unit);
-      }).toThrow();
-    });
+  test("정렬된 로또 번호를 발급한다.", () => {
+    const numbers = [5, 6, 1, 8, 3, 2];
+    const lotto = new Lotto(numbers);
+    expect(lotto.getSortedLotto()).toEqual([1, 2, 3, 5, 6, 8]);
   });
 });
