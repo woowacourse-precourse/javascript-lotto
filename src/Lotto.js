96f70be8c1d4691c4854639fb9b1b0bbc0274577
윤동주 <yoondj98@naver.com>
2022-11-15 19:42:23 +0900

    feat: Lotto 인스턴스의 생성자로 입력된 매개변수가 정상적인 배열인지 검사하는 기능 구현 - validate
    1. 미리 구현되어 있던 로또 번호가 6개인지, 즉 입력된 배열이 길이가 6인 배열인지 확인하는 검사문.
    
    2. 입력받은 배열의 값들이 모두 타입이 Number 즉, 숫자인지 확인하는 검사문.
    
    3. 입력받은 배열의 숫자들이 모두 1~45 중의 자연수 값인지 확인하는 검사문.
    
    4. Set 객체의 인자로 입력받은 숫자 배열을 넣어 그 길이가 6인지 확인함. Set이 중복한 값은 생략하는 특성을 이용하여 해당 배열의 중복 여부를 확인하는 검사문.

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,22 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    
+    for(let number of numbers){
+      if(typeof(number)!=="number") {
+        throw new Error("[ERROR] 숫자가 아닌 문자를 입력했습니다.");
+      }
+    }
+
+    for(let number of numbers){
+      if(1>number||number>45) {
+        throw new Error("[ERROR] 1~45 중의 자연수를 입력하세요.");
+      }
+    }
+
+    if(new Set(numbers).size !== 6){
+      throw new Error("[ERROR] 중복된 숫자가 존재합니다.");
+    }
   }
 
   getLottoNumbers(){
