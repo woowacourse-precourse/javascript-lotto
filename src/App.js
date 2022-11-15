c4c2a23f73a1668c4652d4e5f4550210b764058a
SoobinJ <soobin08011812@gmail.com>
2022-11-15 20:53:48 +0900

    refactor : App -> Lotto 당첨 번호 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 const { Random, Console } = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
 const LOTTOPRICE = 1000;
 const LOTTONUMCOUNT = 6;
 const PRIZEMONEY = [5000, 50000, 1500000, 30000000, 2000000000];
 
 class App {
   play() {}
-  #purchaseAmount;
-  #lottoNum;
-  #winningNum = [];
+  #Lotto;
+  #purchaseAmount; //입력금액
+  #lottoNum; //구입한 로또 수
   #bonusNum;
 
   constructor() {
@@ -22,7 +23,7 @@ class App {
   }
 
   inputAmount() {
-    Console.readLine("구입금액을 입력해 주세요. \n", (purchaseAmount) => {
+    Console.readLine("구입금액을 입력해 주세요.", (purchaseAmount) => {
       this.isValidInput(purchaseAmount);
       this.#purchaseAmount = purchaseAmount;
       this.issueLotto();
@@ -31,9 +32,9 @@ class App {
 
   isValidInput(purchaseAmount) {
     if (isNaN(purchaseAmount)) {
-      throw "[EROR] 로또 구입금액은 숫자여야 합니다.";
+      throw new Error("[ERROR] 로또 구입금액은 숫자여야 합니다.");
     } else if (purchaseAmount % LOTTOPRICE !== 0) {
-      throw "[EROR] 로또 구입금액은 1,000원 단위여야 합니다.";
+      throw new Error("[ERROR] 로또 구입금액은 1,000원 단위여야 합니다.");
     }
   }
 
@@ -46,7 +47,7 @@ class App {
   }
 
   printIssuedLotto() {
-    Console.print(`\n${this.#lottoNum}개를 구매했습니다.`);
+    Console.print(`${this.#lottoNum}개를 구매했습니다.`);
     for (var count = 0; count < this.#lottoNum; count++) {
       Console.print(this.issuedLottoNum[count]);
     }
@@ -54,31 +55,16 @@ class App {
   }
 
   inputWinningNum() {
-    Console.readLine("\n당첨 번호를 입력해 주세요. \n", (winningNum) => {
-      const winningNumArr = winningNum.split(",");
-      this.isValidWinningNum(winningNumArr);
-      this.#winningNum = winningNumArr;
+    Console.readLine("당첨 번호를 입력해 주세요.", (winningNum) => {
+      this.#Lotto = new Lotto(
+        winningNum.split(",").map((number) => Number(number))
+      );
       this.inputBonusNum();
     });
   }
 
-  isValidWinningNum(winningNumArr) {
-    const winningNumSet = new Set(winningNumArr);
-    if (winningNumArr.length !== 6) {
-      throw "[EROR] 로또 번호는 6자리여야 합니다.";
-    }
-    winningNumArr.forEach((element) => {
-      if (element < 1 || element > 45) {
-        throw "[EROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-      }
-    });
-    if (winningNumArr.length !== winningNumSet.size) {
-      throw "[EROR] 로또 번호는 중복되지 않아야 합니다.";
-    }
-  }
-
   inputBonusNum() {
-    Console.readLine("\n보너스 번호를 입력해 주세요. \n", (bonusNum) => {
+    Console.readLine("보너스 번호를 입력해 주세요.", (bonusNum) => {
       this.isValidBonusNum(bonusNum);
       this.#bonusNum = bonusNum;
       this.compareLottoNum();
@@ -87,9 +73,9 @@ class App {
 
   isValidBonusNum(bonusNum) {
     if (isNaN(bonusNum)) {
-      throw "[EROR] 로또 구입금액은 숫자여야 합니다.";
+      throw new Error("[ERROR] 로또 구입금액은 숫자여야 합니다.");
     } else if (bonusNum < 1 || bonusNum > 45) {
-      throw "[EROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
   }
 
@@ -98,7 +84,7 @@ class App {
       let count = 0;
       for (var winningIndex = 0; winningIndex < LOTTONUMCOUNT; winningIndex++) {
         this.issuedLottoNum[index].includes(
-          parseInt(this.#winningNum[winningIndex])
+          this.#Lotto.getLottoNum()[winningIndex]
         )
           ? count++
           : "";
