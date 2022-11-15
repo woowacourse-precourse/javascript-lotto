52c9939dbfe01f5d24aa94e726ddbb1537df83b5
c0c0pang <coc1789@naver.com>
2022-11-15 22:18:25 +0900

    test: 보너스 숫자의 범위를 확인하는 테스트 기능 구현

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -45,6 +45,12 @@ describe("로또 클래스 테스트", () => {
       new BonusExceptionHandling(7,[1,2,3,4,5,7]);
     }).toThrow("[ERROR]");
   });
+
+  test("보너스 숫자의 범위가 1~45 사이인지 확인하고 맞다면 예외가 발생한다.", () => {
+    expect(() => {
+      new BonusExceptionHandling(46,[1,2,3,4,5,7]);
+    }).toThrow("[ERROR]");
+  });
   
   // 아래에 추가 테스트 작성 가능
 });
