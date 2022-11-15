28960e0ff47e1158c742277545109ce907c50b22
osdoonhyun <skw1996@naver.com>
2022-11-15 18:57:10 +0900

    test: ApplicationTest 통과

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -27,7 +27,15 @@ describe("로또 클래스 테스트", () => {
   });
 });
 
-describe.only("로또 구입 금액 테스트", () => {
+describe("보너스 번호 테스트", () => {
+  test("보너스 번호가 당첨번호와 중복되면 false 발생.", () => {
+    const result = Validation.hasUniqueBonusNumber(1, [1, 2, 3, 4, 5, 6]);
+
+    expect(result).toBe(false);
+  });
+});
+
+describe("로또 구입 금액 테스트", () => {
   test("input에 숫자외에 문자가 포함된 경우 false를 반환해야 한다.", () => {
     const input = "1000j";
     const purchaseAmountArr = input.split("");
--- src/LottoGameOperator.js
+++ src/LottoGameOperator.js
@@ -23,7 +23,7 @@ class LottoGameOperator {
     });
 
     this.showLottoNumbers(lottoArray);
-    return;
+    return lottoArray;
   }
 
   showLottoNumbers(lottoArray) {
