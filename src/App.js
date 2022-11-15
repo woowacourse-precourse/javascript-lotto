d58792b1f9b96e6c6350418b9d252bd78de6eca0
aram <snsdl0905@naver.com>
2022-11-15 11:30:27 +0900

    feat: 사용자로부터 당첨 번호와 보너스 번호를 입력받는 함수

--- src/App.js
+++ src/App.js
@@ -15,6 +15,41 @@ class App {
     MissionUtils.Console.readLine('구입 금액을 입력해주세요 : ', (money) => {
       this.money = parseInt(money);
       let amount = money/1000;
+      this.getUsersLottoArray(amount);
+    });
+  }
+
+  getUsersLottoArray(amount){
+    MissionUtils.Console.print(`\n${amount}개를 구매했습니다.`);
+    for (let i=0; i<amount; i++){
+      this.UsersLotto.push(his.createLottoNumber());
+    }
+    this.getWinningNumber();
+  }
+
+  createLottoNumber(){
+    let lottoNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    lottoNumber=this.sortNumber(lottoNumber);
+    return lottoNumber;
+  }
+
+  sortNumber(numberArray){
+    numberArray.sort((a, b) => a - b);
+    return numberArray;
+  }
+
+  getWinningNumber(){
+    MissionUtils.Console.readLine("당첨 번호를 입력해주세요 : ", (winningNumber) => {
+      let winningArray = winningNumber.split(",").map(Number);
+      this.WinningLotto=this.sortNumber(winningArray);
+      this.getBonusNumber();
+    });
+  }
+
+  getBonusNumber(){
+    MissionUtils.Console.readLine("보너스 번호를 입력해주세요 : ", (bonusInput) => {
+      this.bonusNumber = parseInt(bonusInput);
+      MissionUtils.Console.close();
     });
   }
 }
