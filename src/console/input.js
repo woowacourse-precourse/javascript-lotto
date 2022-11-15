fcde6d09aa9872236398508b3dc02ce4c57cb091
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-15 21:44:58 +0900

    refactor: input 수정

--- src/console/input.js
+++ src/console/input.js
@@ -1,9 +1,28 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const Validator = require("../Validator");
 const console = require("./console");
 
 class input extends console {
-  inputLine(callback, message = "") {
-    MissionUtils.Console.readLine(message, callback)
+  fee(message = "") {
+    return new Promise((resolve, reject) => {
+      MissionUtils.Console.readLine(message, (fee) => {
+        if (!Validator.isRightFee(+fee)) {
+          reject(new Error("[ERROR] 올바른 금액 입력이 아닙니다."));
+        }
+        resolve(+fee);
+      });
+    });
+  }
+
+  handleInput(predicate, errorMessage, message = "") {
+    return new Promise((resolve, reject) => {
+      MissionUtils.Console.readLine(message, (answer) => {
+        if (predicate(answer)) {
+          resolve(answer);
+        }
+        reject(errorMessage);
+      });
+    });
   }
 }
 
