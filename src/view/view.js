b1fe9ead84ab5246e0bb88142e2b5d3829938462
jiho1996 <jhp9270@naver.com>
2022-11-15 21:20:41 +0900

    refactor : 변수명 수정

--- src/view/view.js
+++ src/view/view.js
@@ -3,24 +3,24 @@ const {
   MESSAGE_ACCORDING_CORRECT_COUNT,
   MESSAGE_ACCORDING_PRIZE_MONEY,
   MESSAGE_ACCORDING_ACTION,
-  MESSAGE_ACCORDING_INPUT_ACTION
+  MESSAGE_ACCORDING_INPUT_ACTION,
 } = require("../constants/Message");
 const { LOTTO_INFO } = require("../constants/Value");
 
 class view {
   #winngingResult;
 
-  rankingResult(winngingResult) {
-    this.#winngingResult = winngingResult;
+  rankingResult(winningResults) {
+    this.#winngingResult = winningResults;
     print(MESSAGE_ACCORDING_ACTION.NOTIFY_WINNING);
     [...Array(LOTTO_INFO.LEAST_PLACE).keys()]
       .map((key) => key + 1)
       .reverse()
       .forEach((place) => {
         print(
-          `${MESSAGE_ACCORDING_CORRECT_COUNT[place]} ${MESSAGE_ACCORDING_PRIZE_MONEY[place]} ${
-            this.#winngingResult[place]
-          }개`
+          `${MESSAGE_ACCORDING_CORRECT_COUNT[place]} ${
+            MESSAGE_ACCORDING_PRIZE_MONEY[place]
+          } ${this.#winngingResult[place]}개`
         );
       });
   }
@@ -30,11 +30,17 @@ class view {
   }
 
   purchasedSize(purchaseNumbers) {
-    print(MESSAGE_ACCORDING_INPUT_ACTION.RETURN_PURCHASED_LENGTH(purchaseNumbers.length));
+    print(
+      MESSAGE_ACCORDING_INPUT_ACTION.RETURN_PURCHASED_LENGTH(
+        purchaseNumbers.length
+      )
+    );
   }
 
   purchasedList(purchaseNumbers) {
-    Array.from(purchaseNumbers).forEach((lotto) => print(`[${lotto.join(", ")}]`));
+    Array.from(purchaseNumbers).forEach((lotto) =>
+      print(`[${lotto.join(", ")}]`)
+    );
   }
 }
 module.exports = view;
