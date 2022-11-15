67b06eb5fe1bb4a9f0d34654c9eaa9656a9a7cfe
rayark1 <max980203@snu.ac.kr>
2022-11-15 22:42:05 +0900

    refactor(src/Lotto.js): 입력값 검증함수 분리에 따른 refactoring

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,9 +1,14 @@
-const WINNING_NUMBERS_ERROR_MESSAGE = "[ERROR] 당첨 번호는 6개의 숫자여야 합니다.";
-const WINNING_NUMBERS_ERROR_MESSAGE2 = "[ERROR] 당첨 번호는 1~45 사이의 숫자여야 합니다.";
-const WINNING_NUMBERS_ERROR_MESSAGE3 = "[ERROR] 당첨 번호는 중복되지 않아야 합니다.";
+const Validator = require("./Validator");
+
+const WINNING_NUMBERS_ERROR_MESSAGE =
+  "[ERROR] 당첨 번호는 6개의 숫자여야 합니다.";
+const WINNING_NUMBERS_ERROR_MESSAGE2 =
+  "[ERROR] 당첨 번호는 1~45 사이의 숫자여야 합니다.";
+const WINNING_NUMBERS_ERROR_MESSAGE3 =
+  "[ERROR] 당첨 번호는 중복되지 않아야 합니다.";
+
 class Lotto {
   #numbers;
-  
 
   constructor(numbers) {
     this.validate(numbers);
@@ -14,21 +19,7 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    if (
-      numbers
-        .map((number) => parseInt(number))
-        .some((number) => isNaN(number))
-    ) {
-      throw new Error(WINNING_NUMBERS_ERROR_MESSAGE);
-    }
-    if (numbers.some((number) => number < 1 || number > 45)) {
-      throw new Error(WINNING_NUMBERS_ERROR_MESSAGE2);
-    }
-
-    const set = new Set(numbers);
-    if (set.size !== numbers.length) {
-      throw new Error(WINNING_NUMBERS_ERROR_MESSAGE3);
-    }
+    Validator.validateInputWinningNumbers(numbers);
   }
 
   getNumbers() {
