fc630dd5aab5036f9796bf3de0a9f696f3984c40
Choejeonghun <saa00123@naver.com>
2022-11-15 11:24:33 +0900

    refactor: 클래스 삭제 및 로또 구매 기능

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-const MissionUtils = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
+
 class App {
-  play() {}
+  constructor() {
+    let PurchaseAmount = 0;
+  }
+
+  play() {
+    this.PurchaseLotto();
+  }
+
+  PurchaseLotto() {
+    Console.readLine('구입금액을 입력해 주세요.', (amount) => {
+      this.PurchaseAmount = amount;
+      this.ValidatePurchaseAmount();
+      Console.print(`${this.PurchaseAmount / 1000}개를 구매했습니다.`);
+    });
+  }
+
+  ValidatePurchaseAmount() {
+    if (this.PurchaseAmount % 1000 !== 0) {
+      throw '[ERROR] 로또 구입 금액이 1000원 단위가 아닙니다.';
+    }
+  }
 }
 
 module.exports = App;
--- src/Controller/Purchase.js
+++
@@ -1,9 +0,0 @@
-const { Random } = require('@woowacourse/mission-utils');
-
-class Purchase {
-  getLottoNumbers() {
-    return Random.pickUniqueNumbersInRange(1, 45, 6);
-  }
-}
-
-module.exports = Purchase;
