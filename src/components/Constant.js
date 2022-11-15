5809412d583ec22c23464c9ee7a8363ee932b1b1
Inae Choi <inaechoi89@gmail.com>
2022-11-15 13:55:44 +0900

    feat: add some constants

--- src/components/Constant.js
+++ src/components/Constant.js
@@ -1,6 +1,10 @@
 const Const = {
   ORDER_MESSAGE: "구입금액을 입력해 주세요.",
-  UNIT_ERROR: "[ERROR] 1000원 단위로 입력해야 합니다.",
+  UNIT_ERROR: "[ERROR] 1000원 단위의 양수를 입력해야 합니다.",
+  POSITIVE_ERROR: "[ERROR] 양수를 입력해야 합니다.",
+  TYPE_ERROR: "[ERROR] 숫자를 입력해야 합니다.",
+  DUPLICATE_ERROR: "[ERROR] 로또 번호는 중복되면 안됩니다.",
+  WRONG_NUMBER_ERROR: "[ERROR] 로또 번호는 1~45 사이의 정수여야 합니다.",
   QUANTITY_MESSAGE: "개를 구매했습니다.",
   WINNING_NUMBER_MESSAGE: "당첨 번호를 입력해 주세요.",
   BONUS_NUMBER_MESSAGE: "보너스 번호를 입력해 주세요.",
