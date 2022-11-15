9b19a179e675947025f7aefd52a3c6bef77fe55e
1 <wrasf175@naver.com>
2022-11-15 22:24:47 +0900

    test(Bonus): 테스트 예외 사항 명확하게 수정

--- __tests__/BonusTest.js
+++ __tests__/BonusTest.js
@@ -4,32 +4,29 @@ const Lotto = require("../src/Lotto");
 describe("보너스 번호 클래스 테스트", () => {
   test("번호가 숫자가 아니면 예외가 발생한다.", () => {
     expect(() => {
-      new Bonus('a');
+      new Bonus('가');
     }).toThrow("[ERROR]");
   });
   
-  test("번호가 숫자가 아니면 예외가 발생한다.", () => {
+  test("번호가 정수가 아니면 예외가 발생한다.", () => {
     expect(() => {
-      new Bonus('3.7');
+      new Bonus(44.9);
     }).toThrow("[ERROR]");
   });
 
   test("번호가 중복되면 예외가 발생한다.", () => {
     expect(() => {
-      new Bonus(6);
+      new Bonus(1);
       new Lotto([1, 2, 3, 4, 5, 6])
     }).toThrow("[ERROR]");
   });
 
   test("번호가 범위 밖이면 예외가 발생한다.", () => {
     expect(() => {
-      new Bonus(-1);
+      new Bonus(0);
     }).toThrow("[ERROR]");
-  });
-
-  test("번호가 범위 밖이면 예외가 발생한다.", () => {
     expect(() => {
-      new Bonus(100);
+      new Bonus(46);
     }).toThrow("[ERROR]");
   });
 });
