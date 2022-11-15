67e98e178dd1d62fc7331968374542bad593a1b7
soohyun_kim5 <93cherry@naver.com>
2022-11-15 21:30:10 +0900

    feat: 게임 시작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+
 class App {
-  play() {}
+
+  play() {
+    MissionUtils.Console.print("구입금액을 입력해 주세요.");
+  }
 }
 
 module.exports = App;
