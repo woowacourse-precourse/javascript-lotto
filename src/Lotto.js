6e1f855cef908281618ee6889256c3d5f7d737f5
HeoJiye <heoze1031@gmail.com>
2022-11-15 14:34:31 +0900

    feat: 로또 생성 시 예외 사항 처리 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,22 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    numbers.forEach(element => {
+        if (isNaN(element)) {
+            throw new Error("[ERROR] 숫자가 아닌 값이 포함되어 있습니다.");
+        }
+        if (element < 1 || element > 45) {
+            throw new Error("[ERROR] 범위에서 벗어난 값이 포함되어 있습니다.");
+        }
+    });
+
+    const sorted_nums = numbers.sort((a, b) => a - b);
+    for (let i = 1; i < 6; i++) {
+        if (sorted_nums[i-1] == sorted_nums[i]) {
+            throw new Error("[ERROR] 중복 값이 포함되어 있습니다.");
+        }
+    }
   }
 
   // TODO: 추가 기능 구현
