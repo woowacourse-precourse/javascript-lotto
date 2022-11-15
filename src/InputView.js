7091f5bf01b1aab6e579bdd219042508e71254c9
rayark1 <max980203@snu.ac.kr>
2022-11-15 22:38:39 +0900

    feat(src/InputView.js): readline을 관리하는 class

---
+++ src/InputView.js
@@ -0,0 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+class InputView {
+  static getInputPurchaseAmount(callback) {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", callback);
+  }
+
+  static getInputLottoNumbers(callback) {
+    MissionUtils.Console.readLine(
+      "\n당첨 번호를 입력해 주세요.\n",
+      callback
+    );
+  }
+
+  static getInputBonusNumber(callback) {
+    MissionUtils.Console.readLine("\n보너스 번호를 입력해 주세요.\n", callback);
+  }
+}
+
+module.exports = InputView;
