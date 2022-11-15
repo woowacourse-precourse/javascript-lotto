5d659e3bfedb873940f5a644ef0da16594f72c2f
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-15 22:04:42 +0900

    feat: 등수 카운트 로직

--- src/User.js
+++ src/User.js
@@ -1,14 +1,68 @@
 const config = require("./util/config");
+const rank = require("./util/rank");
 
 class User {
   constructor() {
     this.fee = 0;
     this.lottos = [];
     this.lottoCount = 0;
+    this.prize = 0;
+    this.hitRanks = [0, 0, 0, 0, 0, 0];
+    this.returnOfInvestment = 0;
   }
 
   calculateLottoCount() {
     this.lottoCount = this.fee / config.FEE_PER_GAME;
+  }
+
+  calculateHitLottoCount(hitLotto, bonusNumber) {
+    const hitLottoNumbers = hitLotto.getNumbers();
+
+    this.lottos.forEach((lotto) => {
+      const lottoNumbers = lotto.getNumbers();
+      const count = calcHitNumberCount(lottoNumbers, hitLottoNumbers);
+      const isBonus = hitLottoNumbers.includes(bonusNumber);
+      const rank = this.dicideRank(count, isBonus);
+
+      this.hitRanks[rank] += 1;
+    });
+
+    return;
+  }
+
+  calcHitNumberCount(lottoNumbers, hitLottoNumbers) {
+    let count = 0;
+
+    lottoNumbers.forEach((lottoNumber) => {
+      if (hitLottoNumbers.includes(lottoNumber)) {
+        count++;
+      }
+    });
+
+    return count;
+  }
+
+  dicideRank(count, isBonus) {
+    if (count === 6) {
+      return rank.FIRST;
+    }
+    if (count === 5 && isBonus) {
+      return rank.SECOND;
+    }
+    if (count === 5) {
+      return rank.THIRD;
+    }
+    if (count === 4) {
+      return rank.FOURTH;
+    }
+    if (count == 3) {
+      return rank.FIFTH;
+    }
+    return rank.LOST;
+  }
+
+  calculateStat() {
+    this.calculateLottoCount();
   }
 }
 
