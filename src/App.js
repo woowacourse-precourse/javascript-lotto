d6368f7cd3907aca761589194c4a54eb20dc9cde
SALT0601 <thfxm0601@gmail.com>
2022-11-15 19:25:23 +0900

    feat: 로또 구입 금액 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { Console } = MissionUtils;
+
 class App {
-  play() {}
+  inputMoney(){
+    Console.readLine("구입금액을 입력해 주세요.", (money) => {
+      Console.print(money);
+    });
+  }
+
+  play() {
+    this.inputMoney();
+  }
+
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
