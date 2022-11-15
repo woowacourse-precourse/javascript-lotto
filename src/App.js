a5ab6f2ca2d10f9d7ccfc7a2b4a78c1c59acb6e7
jun <ggprgrkjh@naver.com>
2022-11-15 17:10:23 +0900

    feat: 로또 번호 type이 NaN일 경우 에러 발생

--- src/App.js
+++ src/App.js
@@ -151,4 +151,7 @@ class App {
   }
 }
 
+const app = new App();
+app.play();
+
 module.exports = App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -8,22 +8,42 @@ class Lotto {
     this.#numbers = numbers;
   }
   validate(numbers) {
-    const lottoSet = new Set(numbers);
-    const uniqueArray = [...lottoSet];
+    this.sixValidate(numbers);
+    this.typeValidate(numbers);
+    this.uniqueValidate(numbers);
+    this.rangeValidate(numbers);
+  }
 
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-
-    if (uniqueArray.length !== numbers.length) {
-      throw new Error("[ERROR] 로또 번호는 중복이 되면 안됩니다.");
-    }
-
+  rangeValidate(numbers) {
     numbers.map((number) => {
       if (Number(number) > 45 || Number(number) === 0) {
         throw new Error("[ERROR] 당첨 번호가 1~45가 아닙니다.");
       }
     });
+  }
+
+  uniqueValidate(numbers) {
+    const lottoSet = new Set(numbers);
+    const uniqueArray = [...lottoSet];
+    if (uniqueArray.length !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복이 되면 안됩니다.");
+    }
+  }
+
+  typeValidate(numbers) {
+    numbers.map((data) => {
+      console.log(typeof data + " " + data);
+
+      if (isNaN(data)) {
+        throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+      }
+    });
+  }
+
+  sixValidate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
