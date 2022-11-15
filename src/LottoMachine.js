7592c2c90a43996fbbc20ab308c714675b87d838
Sanggil Han <tkdrlf95@naver.com>
2022-11-15 21:58:11 +0900

    feat(LottoMachine): Implement take winning and bonus numbers
    LottoMachine 클래스에 당첨 번호와 보너스 번호를 받는 기능을 구현하였음
    - 당첨 번호를 받음
    - 당첨 번호가 유효한지 확인함
    - 보너스 번호를 받음
    - 보너스 번호가 유효한지 확인함

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -2,15 +2,62 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class LottoMachine {
   #random = MissionUtils.Random;
+  #console = MissionUtils.Console;
   
   constructor() {
   }
 
-  generate() {
+  generateNumbers() {
     let numbers = this.#random.pickUniqueNumbersInRange(1, 45, 6);
     return numbers.sort((a, b) => {
       return a - b;
     });
+  }
+  
+  takeWinningNumbers() {
+    this.#console.readLine('당첨 번호를 입력해 주세요.\n', (rawWinningNumbers) => {
+      this.#validateRawWinningNumbers(rawWinningNumbers);
+      const winningNumbers = rawWinningNumbers.split(',');
+      this.#validateWinningNumbers(winningNumbers);
+      return winningNumbers;
+    });
+    this.#console.close();
+  }
+  
+  #validateRawWinningNumbers(rawWinningNumbers) {
+    if (rawWinningNumbers.match(/[ ]/g)) {
+      throw new Error('[ERROR] 당첨 번호에는 공백이 없어야 합니다.');
+    }
+    if (rawWinningNumbers.match(/[^0-9,]/g)) {
+      throw new Error('[ERROR] 유효하지 않은 값을 입력하였습니다.');
+    }
+  }
+
+  #validateWinningNumbers(winningNumbers) {
+    if (winningNumbers.length !== 6) {
+      throw new Error('[ERROR] 입력한 수가 6개가 아닙니다.')
+    }
+    winningNumbers.forEach((winningNumber) => {
+      if (winningNumber < 1 || winningNumber > 45) {
+        throw new Error('[ERROR] 1에서 45까지의 수가 아닌 수를 입력하였습니다.');
+      }
+    });
+  }
+
+  takeBonusNumber() {
+    this.#console.readLine('보너스 번호를 입력해주세요.\n', (bonusNumber) => {
+      this.#validateBonusNumber(bonusNumber);
+      return bonusNumber;
+    });
+  }
+
+  #validateBonusNumber(bonusNumber) {
+    if (bonusNumber.match(/[^0-9]/g)) {
+      throw new Error('[ERROR] 보너스 번호는 숫자만 입력해야 합니다.');
+    }
+    if (bonusNumber < 1 || bonusNumber > 45) {
+      throw new Error('[ERROR] 보너스 번호는 1에서 45사이의 숫자여야 합니다.')
+    }
   }
 }
 
