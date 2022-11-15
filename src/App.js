40c69b94d74ea3662e68d59df16375d1b78bd3a5
woowoni <ewoowon.2@gmail.com>
2022-11-15 23:33:29 +0900

    보너스 번호 받기

--- src/App.js
+++ src/App.js
@@ -16,6 +16,48 @@ class App {
     this.money = null;
     this.lottes = [];
     this.winNumbers = [];
+    this.bonusNumber = null;
+  }
+
+  generateRankObject() {
+    let rankObject = {};
+    for (let i = 0; i < 6; i++) {
+      rankObject[i] = 0;
+    }
+    return rankObject;
+  }
+
+  countingRank(rankObject, winArr) {
+    winArr.forEach((number) => {
+      rankObject[number]++;
+    });
+  }
+
+  earningRate(rankObject) {
+    let prizeMoney = 0;
+    for (let i = 1; i <= 5; i++) {
+      if (rankObject[i] != 0) prizeMoney += prizeObject[i] * rankObject[i];
+    }
+    MissionUtils.Console.print(
+      `총 수익률은 ${((prizeMoney / this.money) * 100).toFixed(1)}%입니다.`
+    );
+    MissionUtils.Console.close();
+  }
+
+  result() {
+    const rankObject = this.generateRankObject();
+    const winArr = this.lottes.map((lotte) =>
+      lotte.rank(this.winNumbers, this.bonusNumber)
+    );
+    this.countingRank(rankObject, winArr);
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${rankObject[5]}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${rankObject[4]}개`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${rankObject[3]}개`);
+    MissionUtils.Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${rankObject[2]}개`
+    );
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${rankObject[1]}개`);
+    this.earningRate(rankObject);
   }
 
   viewLottes() {
@@ -36,6 +78,23 @@ class App {
     }
     MissionUtils.Console.print(count + "개를 구매했습니다.");
     this.viewLottes();
+    this.result();
+  }
+
+  setBonusNumber() {
+    MissionUtils.Console.readLine(REQUEST_MESSAGE.LUCKY_NUMBER, (line) => {
+      if (!/^\d+$/.test(line))
+        throw new Error(ERROR_MESSAGE.GAME_FORM);
+      const bonusNumber = parseInt(line);
+      if (bonusNumber < 1 || 45 < bonusNumber)
+        throw new Error(ERROR_MESSAGE.NUMBER_RANGE);
+      if (this.winNumbers.includes(line))
+        throw new Error(
+          ERROR_MESSAGE.DUPLICATION
+        );
+      this.bonusNumber = bonusNumber;
+      this.buy();
+    });
   }
 
   setWinNumbers() {
@@ -53,6 +112,7 @@ class App {
       if (numberSet.size != 6)
         throw new Error(ERROR_MESSAGE.DUPLICATION);
       this.winNumbers = numbers;
+      this.setBonusNumber();
     });
   }
 
