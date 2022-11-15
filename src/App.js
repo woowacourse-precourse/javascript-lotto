ac7dc383d7e134f04065a6b4343f90ddd4d6766b
rayark1 <max980203@snu.ac.kr>
2022-11-15 10:25:20 +0900

    feat(src/App.js): 로또 구입 가격을 입력받는 getInputPurchaseAmout 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+
+  getInputPurchaseAmount(callback) {
+    MissionUtils.Console.readLine("구매금액을 입력해 주세요.", callback);
+  }
+  
 }
 
 module.exports = App;
