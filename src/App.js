5e6f6165bdbeb6a0f76675d61c274d100743f48d
Saans <92422813+saans808@users.noreply.github.com>
2022-11-15 20:54:02 +0900

    feat: 당첨 통계 출력

--- src/App.js
+++ src/App.js
@@ -108,6 +108,16 @@ class App {
       throw new Error("[ERROR] 1개의 보너스 번호를 입력해 주세요.");
     }
   }
+
+  printRank(rankList) {
+    Console.print(`당첨 통계
+    ---
+    3개 일치 (5,000원) - ${rankList[0]}개
+    4개 일치 (50,000원) - ${rankList[1]}개
+    5개 일치 (1,500,000원) - ${rankList[2]}개
+    5개 일치, 보너스 볼 일치 (30,000,000원) - ${rankList[3]}개
+    6개 일치 (2,000,000,000원) - ${rankList[4]}개/n`);
+  }
 }
 
 module.exports = App;
