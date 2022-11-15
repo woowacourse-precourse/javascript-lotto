c2d120613e6ef12caa3c963ee5e515e22b703a9c
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-15 22:10:36 +0900

    fix: 상수 수정

--- src/constants.js
+++ src/constants.js
@@ -2,12 +2,14 @@ const LOTTO = {
   PRICE: 1000,
   MIN: 1,
   MAX: 45,
+  LENGTH: 6,
 };
 
 const QUERY = {
   BUY: "구입금액을 입력해 주세요.\n",
   WINNING: "\n당첨 번호를 입력해 주세요.\n",
   BONUS: "\n보너스 번호를 입력해 주세요.\n",
+  STATISTICS: "\n당첨 통계\n---",
 }
 
 const ERROR_MESSAGE = {
@@ -19,8 +21,26 @@ const ERROR_MESSAGE = {
   DUPLICATE: "[ERROR] 중복이 있습니다.",
 };
 
+const STATISTICS_MESSAGE = {
+  "3": '3개 일치 (5,000원) - ',
+  "4": '4개 일치 (50,000원) - ',
+  "5": '5개 일치 (1,500,000원) - ',
+  "6": '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  "7": '6개 일치 (2,000,000,000원) - ',
+}
+
+const WINNING_PRICE_MAP = {
+  "3": 5000,
+  "4": 50000,
+  "5": 1500000,
+  "6": 30000000,
+  "7": 2000000000,
+}
+
 module.exports = {
   LOTTO,
   QUERY,
   ERROR_MESSAGE,
+  WINNING_PRICE_MAP,
+  STATISTICS_MESSAGE
 }
