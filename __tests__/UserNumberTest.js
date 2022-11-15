2b855a6eddee98b5c90d676e60694808c8cc2633
sh981013s <sh981013s@gmail.com>
2022-11-15 21:18:23 +0900

    test(UserNumberTest): 로또 발행 테스트 코드 작성

--- __tests__/UserNumberTest.js
+++ __tests__/UserNumberTest.js
@@ -1,4 +1,5 @@
 const UserNumber = require("../src/model/UserNumber");
+const Lotto = require("../src/model/Lotto");
 
 describe("UserNumber 유저 구입금액 관련 테스트", () => {
   const userNumber = new UserNumber();
@@ -19,5 +20,17 @@ describe("UserNumber 유저 구입금액 관련 테스트", () => {
     expect(() => {
       userNumber.setPurchasingAmount("이승환");
     }).toThrow("[ERROR]");
+  });
+
+  test("로또 발행 테스트", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(userNumber.getIssuedLotto(3000).length).toEqual(3);
+
+    expect(() => {
+      userNumber.getUserIssuedLotto(3000).forEach((singleLotto) => {
+        lotto.validate(singleLotto);
+      });
+    }).not.toThrow("[ERROR]");
   });
 });
