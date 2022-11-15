404e8907163941450bc6ed8545ee21fcd852c5bb
Dongkeun <hongdongk@naver.com>
2022-11-15 18:34:56 +0900

    기능1: 구입금액입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 class App {
-  play() {}
+
+  purchasemoney;
+  
+  play() {
+    this.purchaseLotto();
+
+  }
+
+  purchaseLotto() {
+    Console.readLine("구입금액을 입력해 주세요.", (money) => {
+      this.purchasemoney = money;
+    });
+  }
+
 }
 
 module.exports = App;
