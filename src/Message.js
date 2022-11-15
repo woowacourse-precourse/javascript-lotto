c2565cbab42927df57c65455228792ede14420de
woowoni <ewoowon.2@gmail.com>
2022-11-15 23:08:14 +0900

    오류, 입력 메세지 모아두기

---
+++ src/Message.js
@@ -0,0 +1,16 @@
+const REQUEST_MESSAGE = {
+  MONEY: "구입금액을 입력해 주세요.\n",
+  NUMBERS: "\n당첨 번호를 입력해 주세요.\n",
+  LUCKY_NUMBER: "\n보너스 번호를 입력해 주세요.\n"
+}
+
+const ERROR_MESSAGE = {
+  GAME_UNIT: "[ERROR] 1000원 단위만 입력이 가능합니다.",
+  NUMBER_RANGE: "[ERROR] 1부터 45의 숫자만 입력이 가능합니다.",
+  NUMBERS_LENGTH: "[ERROR] 로또 번호는 6개여야 합니다.",
+  SINGLE_NUMBER: "[ERROR] 하나의 숫자만 입력이 가능합니다.",
+  ONLY_NUMBER: "[ERROR] 숫자만 입력이 가능합니다.",
+  DUPLICATION: "[ERROR] 중복된 숫자는 입력이 불가능합니다."
+}
+
+module.exports = {REQUEST_MESSAGE, ERROR_MESSAGE}
