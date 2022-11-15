c2a7e70307856b27103f50cf49eafda02969e501
Hoon <si932174@gmail.com>
2022-11-15 10:00:04 +0900

    test : 입력 금액이 1000원 단위가 아니면 예외가 발생한다.

--- __tests__/purchaseAmountTest.js
+++ __tests__/purchaseAmountTest.js
@@ -20,4 +20,14 @@ describe('로또 입력 금액 테스트', () => {
       }).toThrow('[ERROR]');
     });
   });
+
+  test('입력 금액이 1000원 단위가 아니면 예외가 발생한다.', () => {
+    const wrongInputAmounts = ['1001', '2002', '3003', '4400'];
+
+    wrongInputAmounts.map(wrongInputAmount => {
+      expect(() => {
+        new purchaseAmountValidator(wrongInputAmount);
+      }).toThrow('[ERROR]');
+    });
+  });
 });
