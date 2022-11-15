bdf0c2b439dbe75d36d6ed321f94b35b9d327151
gogh21 <gogh.phy@gmail.com>
2022-11-15 21:09:08 +0900

    feat: initialize variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
-}
+  play() {
+    //필요한 변수 선언
+    let obj = {3:[0,0], 4:[0,0], 5:[0,0], '5+보너스':[0,0], 6:[0,0]};
+    let amount = 0;
+    let userLottoNumbers;
+    let payCash = '';
+    let lottoEa = 0;
+    let lottos = [];
+    let bonusNum = '';
+    const realLottoNumbers = [];
+  } //Play() 닫는 닫는 괄호
+}//class App 닫는괄호
 
 module.exports = App;
