026b22eca6b06a4ed1890d2eba57bfc2678cde5a
Sanggil Han <tkdrlf95@naver.com>
2022-11-15 19:08:08 +0900

    feat(Lotto): Implement printing and validating features
    발행한 번호를 출력하고, LottoTest를 통과하도록 validate()함수를 수정하였음

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,38 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
+    this.#validate(numbers);
     this.#numbers = numbers;
+    this.#showNumbers();
   }
-
-  validate(numbers) {
+  
+  #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (this.#hasDuplicate(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.")
+    }
   }
 
-  // TODO: 추가 기능 구현
+  #showNumbers() {
+    MissionUtils.Console.print(`[${this.#numbers.join(', ')}]`);
+    MissionUtils.Console.close();
+  }
+
+  #hasDuplicate(numbers) {
+    const eliminateDuplicate = numbers.reduce((acc, number) => {
+      if (!acc.includes(number)) {
+        acc.push(number);
+      }
+      return acc;
+    }, []);
+    return eliminateDuplicate.length !== 6;
+  }
+
 }
 
 module.exports = Lotto;
