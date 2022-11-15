922b7b0b3868985d1e190786e95e95f45a79864d
RooT_Kim <bdd3452@gmail.com>
2022-11-15 19:35:53 +0900

    기능: 구매금액 입력 후 로또 발행 구입 금액 입력 메소드 호출 시 매개변수로 전달할 callback함수 구현 printCount 함수 호출 - 구입 금액 메시지 출력 lottoPublish 함수 구현 - 로또 번호 발행 createRandomLottoNumbers 함수 구현 - 로또 번호 랜덤 생성 printLottos 함수 호출 - 발행된 로또 목록 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+const { getPayout, getWinningNumbers, getBonusNumber } = require('./MissionUtils/ReadLine');
+const { printCount, printLottos, printResults, printProfitPercent } = require('./MissionUtils/Print');
+const { Random } = require('@woowacourse/mission-utils');
+const { BENEFIT } = require('./Constants');
+const Pay = require('./Objects/Pay');
+const Lotto = require('./Objects/Lotto');
+const Bonus = require('./Objects/Bonus');
 class App {
-  play() {}
+	pay = 0;
+	lottos = [];
+	winningNumbers = '';
+	bonusNumber = '';
+	#result = {
+		3: 0,
+		4: 0,
+		5: 0,
+		bonus: 0,
+		6: 0,
+	};
+
+	play() {
+		getPayout((pay) => {
+			const payout = new Pay(pay);
+			this.pay = payout.getters();
+			const count = this.pay / 1000;
+
+			printCount(count);
+
+			this.lottoPublish(count);
+
+			printLottos(this.lottos);
+		});
+	}
+
+	lottoPublish(count) {
+		for (let number = 0; number < count; number++) {
+			this.lottos.push(this.createRandomLottoNumbers());
+		}
+	}
+
+	createRandomLottoNumbers() {
+		return Random.pickUniqueNumbersInRange(1, 45, 6);
+	}
 }
 
 module.exports = App;
