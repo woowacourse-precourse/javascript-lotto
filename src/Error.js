e8c29809124616fe616dba9f563856a51c7290aa
jincde <asdzxc@kakao.com>
2022-11-15 21:42:08 +0900

    feat : 추가 예외처리 메세지 생성

--- src/Error.js
+++ src/Error.js
@@ -1,6 +1,8 @@
 const ERROR = {
   UNIT_ERROR: "[ERROR] 1,000원 단위로 구매가능합니다.",
-  RANGE_ERROR: "[ERROR] 숫자는 1~45 사이의 숫자여야 합니다."
+  RANGE_ERROR: "[ERROR] 숫자는 1~45 사이의 숫자여야 합니다.",
+  LENGTH_ERROR: "[ERROR] 로또 번호는 6개여야 합니다.",
+  OVERLAP_ERROR: "[ERROR] 중복된 숫자가 입력됐습니다.",
 };
 
 module.exports = ERROR;
