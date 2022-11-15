94139f697ca721472ab21fa5ef3e7c24beba5969
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-15 23:13:18 +0900

    test[StaticLotto2]: 수익률 계산 테스트 완료

--- __tests__/StaticTest.js
+++ __tests__/StaticTest.js
@@ -33,4 +33,31 @@ describe('당첨 통계 테스트', () => {
     expect(rankTest.getRank()).toEqual(rank);
   });
 
+  test('총 수익률 계산', () => {
+    const rankTest = new StaticLotto();
+
+    rankTest.setLottoList([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 42],
+      [3, 8, 11, 32, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+
+    rankTest.setWinLotto(new Lotto([3, 5, 11, 16, 32, 42]));
+
+    rankTest.setBonusNumber(45);
+
+    rankTest.getSameNumberCount();
+
+    rankTest.getRank();
+
+    rankTest.setPurchasePrice(3000);
+
+    expect(rankTest.setTotalRevenue()).toEqual('51833.3');
+  });
+
 });
