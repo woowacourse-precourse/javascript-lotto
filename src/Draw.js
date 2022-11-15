a2a7f55fd006b9fb7303f5c6bf9def98cf36ed0f
박겸영 <yong_drg@naver.com>
2022-11-15 19:54:33 +0900

    feat(Draw): 당첨 번호에 대한 예외 처리
    다음 경우에 예외를 발생시키고 에러 메시지 출력 후 어플리케이션 종료
    - 당첨 번호 구분자가 쉼표(,)가 아닌 경우
    - 당첨 번호가 자연수가 아닌 경우
    - 당첨 번호가 1 이상 45 이하의 수가 아닌 경우
    - 당첨 번호가 6개가 아닌 경우
    - 당첨 번호 중 중복되는 숫자가 있는 경우

--- src/Draw.js
+++ src/Draw.js
@@ -1,6 +1,8 @@
 const { Console } = require('@woowacourse/mission-utils');
-const { SPLITTER } = require('./settings');
+const { LOTTO_LENGTH, SPLITTER } = require('./settings');
 const { WINNING, BONUS } = require('./Message');
+const Lotto = require('./Lotto');
+const ErrorHandling = require('./ErrorHandling');
 
 class Draw {
   static winning;
@@ -9,9 +11,10 @@ class Draw {
   static getWinningNumbers(callback) {
     Console.readLine(WINNING, (answer) => {
       answer = answer.replace(/ /g, '');
+      Draw.validateWinningInput(answer);
 
       const winning = answer.split(SPLITTER).map((number) => Number(number));
-      Draw.winning = winning;
+      Draw.winning = Draw.validateWinningNumbers(winning);
 
       Draw.#getBonusNumber(callback);
     });
@@ -24,6 +27,22 @@ class Draw {
 
       callback(Draw);
     });
+  }
+
+  static validateWinningInput(input) {
+    const regExp = new RegExp(`^(\\d+${SPLITTER}){${LOTTO_LENGTH - 1}}\\d+$`);
+    const isInvalidForm = !regExp.test(input);
+
+    ErrorHandling.handleException(
+      isInvalidForm,
+      `당첨 번호는 쉼표(,)를 기준으로 구분되는 숫자 ${LOTTO_LENGTH}개여야 합니다. (ex. 1,2,3,4,5,6)`,
+    );
+  }
+
+  static validateWinningNumbers(numbers) {
+    Lotto.validate(numbers, '당첨 번호는');
+
+    return numbers;
   }
 }
 
