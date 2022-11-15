f71c303fdb566993c5dd7ec430802a9e035e07d8
vjvl95 <vjvl95@naver.com>
2022-11-15 20:04:01 +0900

    feat : 최종 결과 출력함수 완성

--- src/Print.js
+++ src/Print.js
@@ -1,11 +1,27 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-
+const { PRIZES } = require('./common/constants');
 class Print {
   static myLotto(lotto) {
     lotto.forEach((v) => MissionUtils.Console.print(v));
   }
   static countLotto(countLotto) {
     MissionUtils.Console.print(`${countLotto}개를 구매하셨습니다.`);
+  }
+  static result(reward, rinking, money) {
+    MissionUtils.Console.print('당첨 통계');
+    MissionUtils.Console.print('-------');
+    let j = 3;
+    for (let i = 5; i >= 1; i--) {
+      MissionUtils.Console.print(
+        `${j}개 일치 (${PRIZES[String(i + '등')]}) - ${
+          rinking[String(i + '등')]
+        }개`
+      );
+      j++;
+    }
+    const yielRate = (reward / money) * 100;
+    MissionUtils.Console.print(`총 수익률은 ${yielRate.toFixed(2)}%입니다.`);
+    MissionUtils.Console.close();
   }
 }
 module.exports = Print;
