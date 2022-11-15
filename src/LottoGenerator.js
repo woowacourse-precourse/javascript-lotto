cbbe01c76fe5d93e1ba506f1362369a3abf88aec
hyeongdong <khd941018@gmail.com>
2022-11-15 15:04:45 +0900

    refactor(LottoGenerator) : 상수 클래스밖으로 분리 및 에러 메세지 구체화

--- src/LottoGenerator.js
+++ src/LottoGenerator.js
@@ -1,15 +1,15 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 
-class LottoGenerator {
-  LOTTO_PRICE = 1000;
-  MIN_NUMBER = 1;
-  MAX_NUMBER = 45;
-  PICK_COUNT = 6;
+const LOTTO_PRICE = 1000;
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 45;
+const PICK_COUNT = 6;
 
+class LottoGenerator {
   constructor(payment) {
     this.validate(payment);
-    this.totalNumberOfLotto = Number(payment) / this.LOTTO_PRICE;
+    this.totalNumberOfLotto = Number(payment) / LOTTO_PRICE;
   }
 
   validate(payment) {
@@ -17,7 +17,7 @@ class LottoGenerator {
       throw new Error("[ERROR] 구매 금액은 숫자만 입력하셔야 합니다.");
     }
 
-    if (payment % this.LOTTO_PRICE !== 0) {
+    if (payment % LOTTO_PRICE !== 0) {
       throw new Error("[ERROR] 구매 금액은 천원 단위로 입력하셔야 합니다.");
     }
   }
@@ -27,7 +27,7 @@ class LottoGenerator {
     let numberOfCreatedLotto = 0;
 
     while (numberOfCreatedLotto < this.totalNumberOfLotto) {
-      const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(this.MIN_NUMBER, this.MAX_NUMBER, this.PICK_COUNT);
+      const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, PICK_COUNT);
       lottos.push(new Lotto(randomNumbers));
       numberOfCreatedLotto++;
     }
