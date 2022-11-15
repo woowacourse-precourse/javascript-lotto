14d67018db53c48cf496e311623d0a2bc7669ac5
sujungYu <su_moment@naver.com>
2022-11-15 14:12:33 +0900

    refactor(Bonus) : 변수명, 메서드명 수정

--- src/Bonus.js
+++ src/Bonus.js
@@ -6,19 +6,20 @@ class Bonus {
   constructor(winning, bonus) {
     this.#convertBonusType = Number(bonus);
     this.#convertWinningType = winning.split(',').map(Number);
-    this.validate();
+    this.validate(bonus);
   }
 
-  validate() {
-    const testType = /[0-9]/g;
-    const testlength = /[0-9]{1}/;
+  validate(bonus) {
+    const testType = /[0-9]/;
+    const testLength = /[0-9]{1}/;
+
     if (this.#convertWinningType.includes(this.#convertBonusType))
       throw new Error('[ERROR] 당첨 번호와 보너스 번호가 중복될 수 없습니다.');
     if (this.#convertBonusType < 1 || this.#convertBonusType > 45)
       throw new Error('[ERROR] 1~45 사이 숫자여야 합니다.');
-    if (!testlength.test(this.#convertBonusType))
-      throw new Error('[ERROR] 보너스숫자 하나만 입력해야 합니다.');
-    if (!testType.test(this.#convertBonusType)) throw new Error('[ERROR] 숫자만 입력해야 합니다.');
+    if (!testType.test(bonus)) throw new Error('[ERROR] 숫자만 입력해야 합니다.');
+    if (!testLength.test(this.#convertBonusType))
+      throw new Error('[ERROR] 보너스 숫자 하나만 입력해야 합니다.');
     return this.#convertBonusType;
   }
 }
