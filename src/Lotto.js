cc5fb6662c17d5a081356be8833d149d74134408
inthhh <glass005533@gmail.com>
2022-11-15 18:58:50 +0900

    fix: 범위 에러검출 오류 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -14,8 +14,7 @@ class Lotto {
     let isNumCheck = numbers.join("");
     if (isNaN(isNumCheck)) throw new Error("[ERROR] 문자를 입력하실 수 없습니다.");
     for (let i = 0; i < 6; ++i) {
-      if (numbers[i] < 0 || numbers[i] > 45)
-        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      if (numbers[i] < 0 || numbers[i] > 45) throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
     if (numbers.length !== 6) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     let numSet = [...new Set(numbers)];
