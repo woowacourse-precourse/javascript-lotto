50de8b697feba72cec78d19b3cabf3c3c3f17693
연 <mogoo22@naver.com>
2022-11-15 19:57:15 +0900

    test: 보너스 번호 중복 예외처리 테스트 추가

--- __tests__/LottoNumberGenerator.js
+++ __tests__/LottoNumberGenerator.js
@@ -65,8 +65,20 @@ describe('로또 번호 추첨기 클래스 테스트', () => {
   });
 
   test('당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
-    const errorMsg = makeErrorMsg(ERROR_MESSAGE.DUPLICATION);
+    const errorMsg = makeErrorMsg(ERROR_MESSAGE.WINNER_DUPLICATION);
     const answers = ['1,2,3,4,6,6', '7'];
+
+    mockQuestions(answers);
+
+    expect(() => {
+      const lottoNumberGenerator = new LottoNumberGenerator();
+      lottoNumberGenerator.drawLottery();
+    }).toThrow(errorMsg);
+  });
+
+  test('보너스 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
+    const errorMsg = makeErrorMsg(ERROR_MESSAGE.BONUS_DUPLICATION);
+    const answers = ['1,2,3,4,5,6', '6'];
 
     mockQuestions(answers);
 
