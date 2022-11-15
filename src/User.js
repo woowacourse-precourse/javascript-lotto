3c13e916a26c556118c306a32b97c8e93c579971
saetakki <lime.lemonade03@gmail.com>
2022-11-15 20:45:17 +0900

    feat. Create calculate profit ration function

--- src/User.js
+++ src/User.js
@@ -1,3 +1,7 @@
+const { lottoGenerator } = require('./Lotto.js');
+const { print, reducer } = require('./Funcs.js');
+const Lotto = require('./Lotto.js');
+
 class User {
   constructor() {
     this.money = 0;
@@ -9,9 +13,29 @@ class User {
     this.quentity = parseInt(payment / 1000);
   }
   myLottos() {
-    while (this.quentity--) {
-      this.lottos.push(Lotto.lottoGenerator());
+    let count = this.quentity;
+    while (count > 0) {
+      const lotto = lottoGenerator();
+      this.lottos.push(lotto);
+      count--;
     }
+  }
+
+  calculateRank() {
+    const myLottos = this.lottos;
+    const calculator = this.lotto.calculateRank;
+    reducer(
+      myLottos,
+      myLottos.map(item => calculator(item, this.lotto.numbers, RECORD)),
+    );
+  }
+
+  calculateProfitRatio(record) {
+    const table = Object.entries(record).map(item => item[1]);
+    const total = table.reduce((acc, cur) => {
+      return acc + cur.count * cur.money;
+    }, 0);
+    return ((total / this.money) * 100).toFixed(1);
   }
 }
 
