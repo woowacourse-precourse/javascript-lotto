e58b38ba9d8d73b0538bd62f2d000d9116f253ed
gsong <rmsdlf1206@gmail.com>
2022-11-15 15:31:53 +0900

    feat: App readLine, print 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { MissionUtils } = require('@woowacourse/mission-utils');
+
 class App {
+  constructor() {}
+
   play() {}
+
+  readLine(message, callback) {
+    return MissionUtils.Console.readLine(message, callback);
+  }
+
+  print(message) {
+    return MissionUtils.Console.print(message);
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
