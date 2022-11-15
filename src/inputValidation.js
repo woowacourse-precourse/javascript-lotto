5504aa5f6e10b0c7a3b26a7630e5a572e0c1ce67
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-11-15 21:28:56 +0900

    Feat:input validation-중복값이 있는지, 1~45사이 숫자인지 체크하는 메서드 구현

--- src/inputValidation.js
+++ src/inputValidation.js
@@ -3,7 +3,27 @@ const inputValidation = {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    if (numbers.length === 6) return true
+    if (numbers.length === 6) return true;
+  },
+
+  checkNoOverlap(numbers) {
+    let numbersSet = new Set(numbers);
+    if(numbersSet.size !== numbers.length){
+      throw new Error("[ERROR] 중복된 숫자가 존재합니다.")
+    } else{
+      return true;
+    }
+  },
+
+  checkLottoRange(numbers){
+    for (let index=0; index<numbers.length; index++){
+      if(numbers[index] < 1 && numbers[index] > 45){
+        throw new Error("[ERROR] 1~45 사이의 숫자를 넣어주세요.")
+      }
+      if(1 <= numbers[index] <= 45) {
+        return true;
+      }
+    }
   },
 
   checkThousandNum(money) {
