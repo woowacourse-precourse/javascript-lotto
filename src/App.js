64b82e237549d85bb27d6c3f1c06aaa2247bfd2a
Dongja <jdh981118@naver.com>
2022-11-15 23:55:46 +0900

    finisih init

--- src/App.js
+++ src/App.js
@@ -7,15 +7,61 @@ class App {
     printLotto(Lotto_list);
     let WinningNumber = InputWinningNumber();
     let BonusNumber = InputBonusNumber();
-    let benefit = checkWinning(Lotto_list, WinningNumber, BonusNumber);
-    printRevenue(cntLotto, benefit);
+    checkWinning(cntLotto,Lotto_list, WinningNumber, BonusNumber);
+  }
+
+  checkWinning(cntLotto, Lotto_list, WinningNumber, BonusNumber)
+  {
+    let array = new Array(5);
+    for(let lotto of Lotto_list)
+    {
+      let cnt = 0;
+      let bonus = 0;
+      num = lotto.getNumbers();
+      for(let n of WinningNumber)
+        cnt += num.indexof(n) == -1 ? 0 : 1;
+      bonus = num.indexof(BonusNumber) == -1 ? 0: 1;
+      if(cnt >= 3)
+        array[checkStatus(cnt, BonusNumber)]++;
+    }
+    printStatus(cntLotto,array);
+  }
+
+  checkStatus(cnt, BonusNumber){
+    if(cnt== 3)
+      return 0;
+    if(cnt == 4)
+      return 1;
+    if(cnt == 5 && BonusNumber == 0)
+      return 2;
+    else if (cnt == 5 && BonusNumber)
+      return 3;
+    else
+      return 4;
+  }
+
+  printStatus(cntLotto,array){
+    MissionUtils.Console.print('당첨 통계');
+    MissionUtils.Console.print('---');
+    const msg =['3개 일치 (5,000원) - ','4개 일치 (50,000원) - ','5개 일치 (1,500,000원) - ','5개 일치, 보너스 볼 일치 (30,000,000원) - ','6개 일치 (2,000,000,000원) - '];
+    const award=[5000,50000,1500000,30000000,2000000000];
+    let money=0;
+    for(let i = 0; i < 5 ;i++)
+    {
+      MissionUtils.Console.print(msg[i]+array[i]+'개');
+      money+= award[i]*array[i];
+    }
+    money/=(cntLotto*1000);
+    money*=100;
+    money.toFixed(2);
+    MissionUtils.Console.print('총 수익률은 ' + money.toFixed(2) + '%입니다.');
   }
 
   InputWinningNumber(){
     let num=[];
     MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (input) => {
       winning = input.split(',');
-      for(n of winning)
+      for(let n of winning)
         num.push(parseInt(n));
     });
     return num;
@@ -55,11 +101,9 @@ class App {
   }
 
   printLotto(Lotto_list){
-    for(lotto of Lotto_list)
+    for(let lotto of Lotto_list)
     MissionUtils.Console.print(lotto);
   }
-
-
 
 
 }
