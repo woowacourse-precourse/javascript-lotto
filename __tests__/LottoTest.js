dedcc50ece6268d996597d7eef4b991b9f48c49c
kimeuns <dmstn2006@gmail.com>
2022-11-15 23:25:33 +0900

    test: 테스트 파일 분리

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -3,11 +3,6 @@ const LottoPayment = require("../src/models/LottoPayment");
 const LottoIsuued = require("../src/models/LottoIssued");
 const LottoWinning = require("../src/models/LottoWinning");
 const {ERROR} = require("../src/utils/Constants")
-const {
-  getMatchedinWinningNumberCount,
-  getEarningsRate,
-  hasBounsNumber,
-} = require("../src/utils/Utils");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -33,20 +28,7 @@ describe("로또 클래스 테스트", () => {
       new Lotto([1, 2, 3, 4, 5, "h"]);
     }).toThrow(ERROR.LOTTO_TYPE);
   });
-});
 
-describe("발행한 로또 클래스 테스트",()=>{
-  test("로또 구매 개수를 입력 받는다.",()=>{
-    const lottoCount = 3;
-    const output = 3;
-
-    const lottoIssued = new LottoIsuued(lottoCount)
-
-    return expect(lottoIssued.lottoIssued).toHaveLength(output)
-  })
-})
-
-describe("구매 금액 테스트", () => {
   test("구매 금액은 1000단위만 입력 가능하다.", () => {
     const input = 1500;
     expect(() => {
@@ -67,39 +49,16 @@ describe("구매 금액 테스트", () => {
 
     const buyer = new LottoPayment(input);
 
-    return expect(buyer.lottoCount).toEqual(output);
-  });
-});
-
-describe("당첨 로또 테스트", () => {
-  test("당첨 번호는 숫자만 입력 가능합니다.", () => {
-    const input = "1,2,3,4,5,k";
-
-    const winnigLotto = new LottoWinning();
-
-    expect(() => {
-      winnigLotto.setWinningLotto(input);
-    }).toThrow();
+    expect(buyer.lottoCount).toEqual(output);
   });
 
-  test("당첨 번호에 중복된 숫자는 입력할 수 없습니다.", () => {
-    const input = "1,2,3,4,5,5";
+  test("로또 구매 개수만큼 로또를 발행한다.", () => {
+    const lottoCount = 3;
+    const output = 3;
 
-    const winnigLotto = new LottoWinning();
+    const lottoIssued = new LottoIsuued(lottoCount);
 
-    expect(() => {
-      winnigLotto.setWinningLotto(input);
-    }).toThrow();
-  });
-
-  test("당첨 번호에 범위 외 숫자는 입력할 수 없습니다.", () => {
-    const input = "1,2,3,4,5,50";
-
-    const winnigLotto = new LottoWinning();
-
-    expect(() => {
-      winnigLotto.setWinningLotto(input);
-    }).toThrow();
+    expect(lottoIssued.lottoIssued).toHaveLength(output);
   });
 
   test("보너스 번호는 숫자여야 합니다.", () => {
@@ -122,72 +81,5 @@ describe("당첨 로또 테스트", () => {
     expect(() => {
       winnigLotto.setBonusNumber(input);
     }).toThrow();
-  });
-});
-
-describe("기능 테스트", () => {
-  test("당첨로또와 겹치는 개수 계산한다.", () => {
-    const lotto = [2, 3, 5, 6, 8, 9];
-    const winnigLotto = [1, 2, 3, 4, 5, 6];
-    const output = 4;
-
-    const count = getMatchedinWinningNumberCount(lotto, winnigLotto);
-
-    expect(count).toEqual(output);
-  });
-
-  test("로또가 보너스 번호를 가지고 있는지 확인 한다.", () => {
-    const lotto = [2, 3, 5, 6, 8, 9];
-    const bonusNumber = 6;
-
-    const output = hasBounsNumber(lotto, bonusNumber);
-
-    expect(output).toBeTruthy();
-  });
-
-  test("당첨함수의 수익률을 계산한다.", () => {
-    const payment = 8000;
-    const input = [
-      {
-        ranking: "FIFTH",
-        reward: 5000,
-        mathcedCount: 3,
-        hasBounsNumber: false,
-        amount: 1,
-      },
-      {
-        ranking: "FOURTH",
-        reward: 50000,
-        mathcedCount: 4,
-        hasBounsNumber: false,
-        amount: 0,
-      },
-      {
-        ranking: "THIRD",
-        reward: 1500000,
-        mathcedCount: 5,
-        hasBounsNumber: false,
-        amount: 0,
-      },
-      {
-        ranking: "SECOND",
-        reward: 30000000,
-        mathcedCount: 5,
-        hasBounsNumber: true,
-        amount: 0,
-      },
-      {
-        ranking: "FIRST",
-        reward: 2000000000,
-        mathcedCount: 6,
-        hasBounsNumber: false,
-        amount: 0,
-      },
-    ];
-    const output = "62.5";
-
-    const earningsRate = getEarningsRate(input, payment);
-
-    expect(earningsRate).toEqual(output);
   });
 });
--- __tests__/RankTest.js
+++ __tests__/RankTest.js
@@ -1,6 +1,6 @@
 const RankingResult = require("../src/RankingResult.js")
 
-describe("로또 테스트", () => {
+describe("당첨 로또 테스트", () => {
   test("1등 당첨자 수 구하기", () => {
     const issuedLotto = [[1, 2, 3, 4, 5, 6]];
     const winningLotto = [1, 2, 3, 4, 5, 6];
@@ -109,6 +109,53 @@ describe("로또 테스트", () => {
 
     return expect(ranking.rankingResult).toEqual(output);
   });
+
+  test("수익률 계산하는 함수 - 소수점 둘째자리에서 반올림", () => {
+    const issuedLotto = [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ];
+    const winningLotto = [1, 2, 3, 4, 5, 6];
+    const bonusLotto = 7;
+    const lottoPayment = 8000;
+    const output = "62.5";
+
+    const ranking = new RankingResult();
+    ranking.setRankingResult(issuedLotto, winningLotto, bonusLotto);
+    ranking.setEarningsRate(lottoPayment);
+
+    expect(ranking.earningsRate).toEqual(output);
+  });
+
+  test("수익률 계산하는 함수 - 1000자리 이상 , 추가",()=>{
+    const issuedLotto = [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42]
+    ];
+    const winningLotto = [1, 3, 8, 11, 31, 41];
+    const bonusLotto = 44;
+    const lottoPayment = 8000
+    const output = "18,750.0"
+
+    const ranking = new RankingResult();
+    ranking.setRankingResult(
+      issuedLotto,
+      winningLotto,
+      bonusLotto
+    );
+    ranking.setEarningsRate(lottoPayment);
+
+    expect(ranking.earningsRate).toEqual(output)
+
+  })
 });
 
 
