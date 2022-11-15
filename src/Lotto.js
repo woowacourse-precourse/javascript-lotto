d75f1b3912c3f3e3c28aa4c06999fb1d84c437ab
김나현 <nahyeon.dev@gmail.com>
2022-11-15 11:38:08 +0900

    fix: length undefined 오류, calculate 함수 당첨 계산 함수 정교화

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class Lotto {
   #numbers;
 
@@ -7,12 +8,12 @@ class Lotto {
   }
 
   validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
     const numSet = new Set(numbers);
     if(numSet.size != 6){
       throw new Error("[ERROR] 중복된 숫자가 포함되어 있습니다.");
+    }
+    if (Array.from(numSet).length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
     //1-45 사이의 문자인지 보기
     if(!(parseInt(numbers) >= 1 && parseInt(numbers) <= 45)){
@@ -21,19 +22,18 @@ class Lotto {
     return numbers;
   }
   printRandom(){
-    return `[${this.#numbers.join(", ")}]`;
+    return `[${this.#numbers.sort((a, b) => a - b).join(", ")}]`;
   }
 
   numberObj() {
     const num = {};
-    let i =1;
-    while(i <= 45){
+    for (let i = 1; i <= 45; i++) {
       num[i] = false;
     }
     return num;
   }
 
-  winCheck(winNum) {
+  winCheck(winNum, bonusNum) {
     let count = 0;
     const obj = this.numberObj();
     this.#numbers.forEach((number) => {
@@ -42,21 +42,19 @@ class Lotto {
     winNum.forEach((number) => {
       if (obj[number]) count++;
     });
+    let bonus = this.#numbers.includes(bonusNum) ? 2 : 0; //6당첨과 겹치지 않게 보너스는 2점으로 계산
+    count += bonus;
     return count;
   }
 
   calculate(win, bonus) {
-    const count = this.winCheck(win);
-    switch (count) {
-      case 3:
-        return 5;
-      case 4:
-        return 4;
-      case 5:
-        return this.#numbers.includes(bonus) ? 2 : 3;
-      case 6:
-        return 1;
-    }
+    const count = this.winCheck(win, bonus);
+
+    if(count === 3) return 1;
+    if(count === 4) return 2;
+    if(count === 5) return 3;
+    if(count === 6) return 4;
+    if(count === 7) return 5; //5개 일치, 보너스 일치
   }
 
 
