07d07f036dc955711a12a536d3f1c126573eae04
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-15 16:52:49 +0900

    feat(Constants): 원시타입 상수화 과정 구현

--- src/setting/Constants.js
+++ src/setting/Constants.js
@@ -16,10 +16,33 @@ const LOTTO_VALUE = Object.freeze({
     digit: 6,
 });
 
+const MATCH = Object.freeze({
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+});
+
+const COMPARE_VALUE = Object.freeze({
+    length: 5,
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 1,
+})
+
+const PRIZE = 1;
+
+const LOTTO_WIN = [5000, 50000, 1500000, 30000000, 2000000000];
 
 module.exports = {
     VALIDATION_VALUE,
     LOTTO_VALUE,
+    COMPARE_VALUE,
+    LOTTO_WIN,
+    MATCH,
+    PRIZE,
 }
 
 
