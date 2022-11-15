13d6eebada564737de19870bdbbbee6534c38822
hyukzz <ri_chard@naver.com>
2022-11-15 18:25:13 +0900

    remove: 새로 파일 생성해서 삭제

--- src/WinLottoNumber.js
+++
@@ -1,9 +0,0 @@
-const { Random } = require("@woowacourse/mission-utils");
-
-class WinLottoNumber {
-	pickUniqueNumbers() {
-		return Random.pickUniqueNumbersInRange(1, 45, 6);
-	}
-}
-
-module.exports = WinLottoNumber;
