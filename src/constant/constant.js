4d782c3e7fce153fbf68217cc8cd01b25b9c81bb
Hchan2 <seongchan_@naver.com>
2022-11-15 22:41:47 +0900

    로또 결과 Count, 보너스 숫자 True를 위한 객체 상수화

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -5,11 +5,19 @@ const BUY_MESSAGE = (buyAmount) => {
 const WINNING_NUMBERS = '당첨 번호를 입력해주세요.';
 const BONUS_NUMBER = '보너스 번호를 입력해 주세요.';
 const THOUSAND = 1000;
+const RESULT_CHECK = {
+    three: { count: 0, winnings: 5000 },
+    four: { count: 0, winnings: 50000 },
+    five: { count: 0, winnings: 1500000 },
+    fiveBonus: { count: 0, winnings: 30000000 },
+    six: { count: 0, winnings: 2000000000 }
+};
 
 module.exports = {
     BUY_LOTTO,
     BUY_MESSAGE,
     WINNING_NUMBERS,
     BONUS_NUMBER,
-    THOUSAND
+    THOUSAND,
+    RESULT_CHECK
 }
