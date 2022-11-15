ea860ce30fcde62a6098a08eb95fadb94b490273
Junghwan Kim <junghwki@c2r5s8.42seoul.kr>
2022-11-15 12:57:58 +0900

    feat: Lotto 클래스 예외처리 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,26 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    const temp = new Set(numbers);
+    if (temp.size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복되어서는 안됩니다.");
+    }
+    this.isValidNumber(numbers);
   }
 
-  // TODO: 추가 기능 구현
+  isValidNumber(numbers) {
+    const regExp = /\D/g;
+
+    for (let index = 0; index < numbers.length; index++) {
+      if (regExp.test(numbers[index])) {
+        throw new Error("[ERROR] 숫자만 입력할 수 있습니다.");
+      }
+      if (numbers[index] < 1 || numbers[index] > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.");
+      }
+    }
+  }
 }
 
+const lotto = new Lotto(["1", "2", "3", "4", "5", "45"]);
 module.exports = Lotto;
