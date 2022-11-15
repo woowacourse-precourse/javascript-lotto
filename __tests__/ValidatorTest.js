b61fc0b33cd0dc9d07ec7065d69c22281a7aba06
동동이 <drdragon777@naver.com>
2022-11-15 18:45:33 +0900

    test - add ValidatorTest 보너스 로또 유효성 테스트 코드 작성

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,6 +1,6 @@
 const Validator = require("../src/validator/Validator");
 
-describe("로또 관련 유효성 테스트", () => {
+describe("사용자 로또 입력 유효성 테스트", () => {
   test("유효한 로또 개수를 입력하지 않으면 예외를 발생시킨다.", () => {
     expect(() => {
       Validator.isValidLottoNum([1, 2, 3, 4, 5, 6, 7]);
@@ -19,6 +19,24 @@ describe("로또 관련 유효성 테스트", () => {
   test("로또 숫자가 중복된 숫자이면 예외를 발생시킨다.", () => {
     expect(() => {
       Validator.isDuplicatedLotto([1, 2, 3, 4, 5, 5]);
+    }).toThrow("[ERROR]");
+  });
+});
+
+describe("보너스 로또 번호 유효성 테스트", () => {
+  test("보너스 로또 숫자가 1 ~ 45 범위가 아니면 예외를 발생시킨다.", () => {
+    expect(() => {
+      Validator.isValidBonusRange(48, [1, 45]);
+    }).toThrow("[ERROR]");
+  });
+  test("보너스 로또 숫자가 1 ~ 45 범위가 아니면 예외를 발생시킨다.", () => {
+    expect(() => {
+      Validator.isValidBonus(48);
+    }).toThrow("[ERROR]");
+  });
+  test("보너스 로또 숫자가 숫자가 아니면 예외를 발생시킨다.", () => {
+    expect(() => {
+      Validator.isValidBonus("45a");
     }).toThrow("[ERROR]");
   });
 });
