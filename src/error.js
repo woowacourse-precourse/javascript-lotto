ea82ea4e32c2106d91666b8166d06c7092f1b7b8
김아현 <dhflxhdxhd@gmail.com>
2022-11-15 18:01:01 +0900

    Feat : error 체크 분리

---
+++ src/error.js
@@ -0,0 +1,24 @@
+const { ERROR } = require("./constant/constant.js");
+
+class Exception {
+  checkCount(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error(ERROR.COUNT);
+    }
+  }
+
+  checkDuplicate(numbers) {
+    let uniqueNumbers = [...new Set(numbers)];
+    if (uniqueNumbers.length !== numbers.length) {
+      throw new Error(ERROR.DUPLICATE);
+    }
+  }
+
+  checkString(number) {
+    if (number !== Number(number)) {
+      throw new Error(ERROR.NUMBER_ONLY);
+    }
+  }
+}
+
+module.exports = Exception;
