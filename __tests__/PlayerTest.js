c1ab4f22e6c71e5f6cfc2fa02c5f5bb592ff5f6d
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-15 10:14:31 +0900

    test: add testing function for player input money error exception

--- __tests__/PlayerTest.js
+++ __tests__/PlayerTest.js
@@ -1,20 +1,22 @@
 const { Player } = require("../src/Player");
 const { ERROR } = require("../src/Error");
 describe("금액 validation 테스트", () => {
-  test("금액이 한글일 경우 예외 발생", () => {
-    const player = new Player();
+  test("금액이 숫자가 아닐 경우 예외 발생", () => {
     expect(() => {
-      player.validateMoney("한글입니다");
+      const player = new Player("asdf");
     }).toThrow(ERROR.notNumber);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("숫자가 1000으로 나누어 떨어지지 않을 경우 예외 발생", () => {
     expect(() => {
-      const player = new Player();
-      player.validateMoney("100123");
-      player.validateMoney("210000");
+      const player = new Player("123123");
     }).toThrow(ERROR.cannotDivide);
   });
-  // 아래에 추가 테스트 작성 가능
+
+  test("입력한 금액만큼 로또번호 생성", () => {
+    expect(() => {
+      const player = new Player();
+      player.makeLottoNumbers("5000");
+    });
+  });
 });
