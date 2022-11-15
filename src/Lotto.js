7832fdb84c6b806c3b413063bffec07a1fb191ec
Lena <lny970116@gmail.com>
2022-11-15 22:53:26 +0900

    feat: 당첨 여부 확인하기

--- src/Lotto.js
+++ src/Lotto.js
@@ -18,6 +18,7 @@ class Lotto {
 
   constructor(numbers) {
     const coin = new Coin();
+    this.setJackpotNum();
     this.validate(numbers);
     this.#numbers = numbers;
   }
@@ -25,6 +26,57 @@ class Lotto {
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  }
+
+  setJackpotNum(){
+    MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.\n", (answer)=>{
+        this.#numbers = answer.split(",");
+        MissionUtils.Console.print(this.#numbers)
+    })
+}
+  setBonusNumber(){
+    MissionUtils.Console.readLine("보너스 번호를 입력해 주세요.\n", (answer)=>{
+      this.bonus = answer;
+    })
+  }
+  getUserNumber(){
+    let randomNumbetList = [];
+    
+    for(let i=0; i<coin.userMoney; i++){
+      this.userNumberList.push(randomNumbetList.push(MissionUtils.Console.pickUniqueNumbersInRange(1, 45, 6).sort((a, b)=>{return a-b;})));
+      MissionUtils.Console.print(this.userNumberList[i] + "\n");
+    }
+
+  }
+  Matching(){
+    for(let i=0; i<this.userNumberList.length; i++){
+      let point = this.userNumberList[i].fiter(i => this.#numbers.includes(i)).length;
+
+      if(point === 6){
+        this.prize.first++;
+        point = 0;
+      }
+
+      if (point === 5) {
+        if (this.userNumberList[i].includes(this.bonus)) {
+          this.prize.second++;
+          point = 0;
+        } else {
+          this.prize.third++;
+          point = 0;
+        }
+      }
+
+      if(point === 4){
+        this.prize.fourth++;
+        point = 0;
+      }
+
+      if(point === 3){
+        this.prize.fifth++;
+        point = 0;
+      }
     }
   }
 }
