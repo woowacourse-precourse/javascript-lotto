622e5460fbb5d4cc17230945f4d5f5d00f53d634
b-hyoung <youqlrqod@naver.com>
2022-11-15 13:40:49 +0900

    add buy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-class App {
-  play() {}
+const {Console , Random}= require("@woowacourse/mission-utils");
+class App{
+  constructor(){
+  
+  }
+  play() {
+    this.buy()
+  }
+
+  buy(){
+    let lottoNum = 0
+    Console.readLine("구입 금액을 입력해주세요.\n" , (num) => {
+      lottoNum = num/1000
+      this.computer(lottoNum)
+    })
+  }
+  
 }
+
+const app = new App()
+app.play()
 
 module.exports = App;
