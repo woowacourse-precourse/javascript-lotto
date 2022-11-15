8db6f5386bb618421215a682fc73b5cecc53c0c9
aram <snsdl0905@naver.com>
2022-11-15 11:45:54 +0900

    feat: 결과를 출력하는 함수

--- src/Lotto.js
+++ src/Lotto.js
@@ -65,6 +65,15 @@ class Lotto {
     return false;
   }
 
+  printResult(){
+    MissionUtils.Console.print(`\n당첨 통계\n---`) 
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${usersRank[5]}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${usersRank[4]}개`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${usersRank[3]}개`);
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${usersRank[2]}개`);
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${usersRank[1]}개`);
+  }
+
   // TODO: 추가 기능 구현
 }
 
