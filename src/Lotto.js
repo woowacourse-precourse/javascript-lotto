ebea8437a96dd1e74cfa16fb73e73c56e50e3b1f
soohyun_kim5 <93cherry@naver.com>
2022-11-15 21:51:38 +0900

    test: 로또 번호 문자 테스트

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,29 +1,38 @@
 class Lotto {
-  #numbers;
+    #numbers;
 
-  constructor(numbers) {
-    this.validate(numbers);
-    this.checkDuplicate(numbers);
-    this.#numbers = numbers;
-  }
-
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    constructor(numbers) {
+        this.validate(numbers);
+        this.checkDuplicate(numbers);
+        this.checkNumber(numbers);
+        this.#numbers = numbers;
     }
-  }
 
-  checkDuplicate(numbers){
-    const set = new Set(numbers);
-    const uniqueArr = [...set];
-    if (numbers.length !== uniqueArr.length) {
-      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.")
+    validate(numbers) {
+        if (numbers.length !== 6) {
+            throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
     }
-  }
 
-  getNumber(){
-    return this.#numbers;
-  }
+    checkDuplicate(numbers) {
+        const set = new Set(numbers);
+        const uniqueArr = [...set];
+        if (numbers.length !== uniqueArr.length) {
+            throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.")
+        }
+    }
+
+    checkNumber(numbers) {
+        for (let x of numbers) {
+            if (isNaN(x) === true) {
+                throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+            }
+        }
+    }
+
+    getNumber() {
+        return this.#numbers;
+    }
 
 }
 
