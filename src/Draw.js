06cb23ef37d6a04299f090a71c9317afa4137a24
박겸영 <yong_drg@naver.com>
2022-11-15 19:08:02 +0900

    feat(Draw): 사용자에게 보너스 번호 1개 입력 받기
    당첨 번호 입력 받은 후에 보너스 번호 입력 실행(비동기 처리)

--- src/Draw.js
+++ src/Draw.js
@@ -1,16 +1,28 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { SPLITTER } = require('./settings');
-const { WINNING } = require('./Message');
+const { WINNING, BONUS } = require('./Message');
 
 class Draw {
   static winning;
+  static bonus;
 
-  static getWinningNumbers() {
+  static getWinningNumbers(callback) {
     Console.readLine(WINNING, (answer) => {
       answer = answer.replace(/ /g, '');
 
       const winning = answer.split(SPLITTER).map((number) => Number(number));
       Draw.winning = winning;
+
+      Draw.#getBonusNumber(callback);
+    });
+  }
+
+  static #getBonusNumber(callback) {
+    Console.readLine(BONUS, (answer) => {
+      const bonus = Number(answer);
+      Draw.bonus = bonus;
+
+      callback(Draw);
     });
   }
 }
