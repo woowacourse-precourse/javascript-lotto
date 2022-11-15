4fcc8d33ce477ee11efb5af9ec330ccdb7dc29e9
DESKTOP-8B9G08E\Cho young jae <rnlwlswk@naver.com>
2022-11-15 23:31:21 +0900

    아무것도 하지 못함 좀 더 공부해야겠다

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,6 +4,7 @@ class Lotto {
   constructor(numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
+    
   }
 
   validate(numbers) {
@@ -13,6 +14,9 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  money() {
+    
+  }
 }
 
 module.exports = Lotto;
