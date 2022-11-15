06290b66d3ffa7be029a5a46f140a8e19cebdbb5
vjvl95 <vjvl95@naver.com>
2022-11-15 17:51:41 +0900

    fix : 상수 메시지 적용

--- src/CheckVaild.js
+++ src/CheckVaild.js
@@ -1,18 +1,24 @@
+const { ERROR_MESSAGES } = require('./common/message');
+
 class CheckVaild {
   static isValidMoney(money) {
     if (Number.isNaN(money)) {
-      throw `[ERROR] 입력 금액이 숫자형태가 아닙니다.`;
+      throw new Error(`${ERROR_MESSAGES.NOT_A_NUMBER}`);
+    }
+    if (money < 0) {
+      throw new Error(`${ERROR_MESSAGES.NEGATIVE_NUMBER}`);
     }
     if (money % 1000 !== 0) {
-      throw `[ERROR] 천원 단위로 금액을 지불해주세요.`;
+      throw new Error(`${ERROR_MESSAGES.NO_REST_MONEY}`);
     }
     if (money > 1000000) {
-      throw `[ERROR] 최대 구입 가능 금액은 100만원 입니다. 다시 시작하세요. 입력한 금액: ${money}`;
+      throw new Error(`${ERROR_MESSAGES.OVER_THE_MAX} 입력금액: ${money}`);
     }
     if (money < 1000) {
-      throw `[ERROR] 로또 한장의 가격은 1000원입니다. 1000원 보다 높은 금액을 입력하세요. 입력한 금액: ${money}`;
+      throw new Error(`${ERROR_MESSAGES.UNDER_THE_MIN} 입력금액: ${money}`);
     }
   }
+  static isVaildWinningNumber(winningNumber) {}
 }
 
 module.exports = CheckVaild;
