88c353a6229056c18b26693012f9b94f67c889ed
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2022-11-15 21:50:48 +0900

    feat : constants 내 잘못된 상수 변경

--- src/constants.js
+++ src/constants.js
@@ -1,25 +1,31 @@
 const PURCHASE = Object.freeze({
-  INPUT_PRICE: "구입 금액을 입력해 주세요",
-  INPUT_SUCCESS: "개를 구매했습니다.",
+  INPUT_PRICE: "구입 금액을 입력해 주세요\n",
 });
 const SET_WINNGNUMBER = Object.freeze({
-  INPUT_NUMBER: "당첨 번호를 입력해 주세요.",
-  INPUT_BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
+  INPUT_NUMBER: "\n당첨 번호를 입력해 주세요.\n",
+  INPUT_BONUS_NUMBER: "\n보너스 번호를 입력해 주세요.\n",
 });
-const MESSAGE = Object.freeze({});
 
-const LOTTO_NUMBER = Object.freeze({});
+const LOTTO_PRIZE = Object.freeze({
+  six: 2000000000,
+  fiveWithBonus: 30000000,
+  five: 1500000,
+  four: 50000,
+  three: 5000,
+  out: 0,
+});
 
 const LOTTO_PRICE = 1000;
 const FIVE = 5;
 
 const RESULT_MESSAGE = Object.freeze({
+  PURCHASE: (number_Purchase) => `\n${number_Purchase}개를 구매했습니다.`,
   START: "\n당첨 통계\n---",
   THREE: (number) => `3개 일치 (5,000원) - ${number}개`,
+  FOUR: (number) => `4개 일치 (50,000원) - ${number}개`,
   FIVE: (number) => `5개 일치 (1,500,000원) - ${number}개`,
   FIVE_BONUS: (number) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${number}개`,
   SIX: (number) => `6개 일치 (2,000,000,000원) - ${number}개`,
-  PURCHASE: (number_Purchase) => `\n${number_Purchase}개를 구매했습니다.`,
   PROFIT: (profit) => `총 수익률은 ${profit}%입니다.`,
 });
 
@@ -33,4 +39,24 @@ const ERROR_MESSAGE = Object.freeze({
   BONUS_DUPLICATE: "[ERROR] 보너스 번호가 당첨 번호에 중복되는 값입니다.",
 });
 
-module.exports = { PURCHASE, SET_WINNGNUMBER, LOTTO_PRICE, FIVE, RESULT_MESSAGE, ERROR_MESSAGE };
+const LOTTO_MATCHES = Object.freeze({
+  0: "out",
+  1: "out",
+  2: "out",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+  fiveWithBonus: "fiveWithBonus",
+});
+
+module.exports = {
+  PURCHASE,
+  LOTTO_PRIZE,
+  SET_WINNGNUMBER,
+  LOTTO_PRICE,
+  FIVE,
+  RESULT_MESSAGE,
+  ERROR_MESSAGE,
+  LOTTO_MATCHES,
+};
