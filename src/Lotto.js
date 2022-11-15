891df4d1808412b55c16dc57d047ac2b29bfafe9
KangYunJi <dbswl701@ajou.ac.kr>
2022-11-15 12:05:07 +0900

    feat: 로또 번호 중복 검사 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,12 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    // 중복 있는지 검사
+    const set = new Set(numbers);
+    if(numbers.length !== set.size) {
+      throw new Error("[ERROR] 로또 번호는 중복이 불가능합니다.");
+    }
+    console.log(`lotto size : ${numbers.length}, set size : ${set.size}`);
   }
 
   getNumbers(){
