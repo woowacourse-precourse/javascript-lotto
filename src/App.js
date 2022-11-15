dcc5163892c55c3dffbf8696275feb9ac61bd85b
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2022-11-16 03:08:34 +0900

    feat : 당첨번호 입력

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-const Mission = require("@woowacourse/mission-utils");
-const Validator = require("./Validator");
+const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
+const Validator = require("./Validator");
+const Winning = require("./Winning");
 class App {
   play() {
     this.lottoPrice();
   }
+  lottoDraw = () => {};
   BonusInput = () => {
     MissionUtils.Console.readLine(
       "보너스 번호를 입력해 주세요.",
-      (BonusNumber) => {
-        new Winning.BonusNumber(BonusNumber);
-        MissionUtils.Console.print(BonusNumber);
+      (BonusInput) => {
+        new Winning.BonusNumber(BonusInput);
+        MissionUtils.Console.print(BonusInput);
       }
     );
+    this.lottoDraw();
   };
+  winningNumber = () => {
+    MissionUtils.Console.readLine(
+      "당첨 번호를 입력해 주세요.",
+      (winningInput) => {
+        new Winning(winningInput);
+        MissionUtils.Console.print(winningInput);
+      }
+    );
+    this.bonusNumber();
+  };
+
   lottoIssuance = (count) => {
     MissionUtils.Console.print(`${count}개를 구매했습니다.`);
     const lottoNumber = Array.from({ length: count }, () => {
@@ -25,16 +39,15 @@ class App {
       const ascending = randomNumber.sort((a, b) => a - b);
       return new Lotto(ascending);
     });
-    let data = [];
     lottoNumber.forEach((num) => {
-      data.push(num.getNumbers());
+      MissionUtils.Console.print(num.getNumbers());
     });
-    this.winningInput();
-    MissionUtils.Console.print(data);
+    this.winningNumber();
   };
 
   lottoPrice = () => {
-    Mission.Console.readLine("구입금액을 입력해 주세요.", (input) => {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.", (input) => {
+      new input(input);
       this.lottoIssuance(parseInt(input, 10) / 1000);
     });
   };
