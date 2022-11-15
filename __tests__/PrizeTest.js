92ff359407b26ec0846fcbd6f313cf54286547d3
Sanggil Han <tkdrlf95@naver.com>
2022-11-15 23:27:11 +0900

    test(Prize): Implement test codes to test caculate function
    당첨 회수를 계산하는 기능을 테스트하는 코드를 구현하였음

---
+++ __tests__/PrizeTest.js
@@ -0,0 +1,40 @@
+const Prize = require("../src/Prize");
+const Lotto = require("../src/Lotto");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("로또 테스트", () => {
+  test("1등과 2등이 당첨 되었을 때 일치 여부 확인 테스트", () => {
+    mockQuestions(["1,2,3,4,5,6", "7"]);
+    const logs = [
+      "3개 일치 (5,000원) - 0개",
+      "4개 일치 (50,000원) - 0개",
+      "5개 일치 (1,500,000원) - 0개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+      "6개 일치 (2,000,000,000원) - 1개",
+    ];
+    const logSpy = getLogSpy();
+    const lotto1 = new Lotto([1, 2, 3, 4, 5, 6]);
+    const lotto2 = new Lotto([2, 3, 4, 5, 6, 7]);
+    const prize = new Prize([lotto1, lotto2]);
+    prize.calcualteWins();
+    prize.announceResult();
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+});
