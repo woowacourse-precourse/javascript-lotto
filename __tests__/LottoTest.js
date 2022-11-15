3b066ba1c3f6da84c4373d6d6b1601c2889a7f84
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-15 17:49:22 +0900

    test: test ascending order of lotto

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,23 @@
-const Lotto = require("../src/Lotto");
-
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+const Lotto = require('../src/Lotto');
+const message = require('../src/Message');
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또 번호는 오름차순으로 정렬된다.', () => {
+    expect(() => {
+      const lotto = new Lotto([10, 1, 45, 2, 3, 4]);
+      lotto.order_lotto().toBe([1, 2, 3, 4, 10, 45]);
+    });
+  });
 });
