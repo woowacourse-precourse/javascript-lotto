b73fcaa6021324d93e5559b2f98150ba58b9996e
Dongja <jdh981118@naver.com>
2022-11-15 22:54:59 +0900

    app.js: make makeLottos function

--- src/App.js
+++ src/App.js
@@ -2,19 +2,38 @@ const MissionUtils = require("@woowacourse/mission-utils");
 import Lotto from './Lotto';
 class App {
   play() {
-    Input();
-    Rotto();
-    print();
+    let cntLotto = InputMoney();
+    let Lotto_list = makeLottos(cntLotto);
+    printLotto(Lotto_list);
+    let WinningNumber = InputWinningNumber();
+    let BonusNumber = InputBonusNumber();
+    let benefit = checkWinning(Lotto_list, WinningNumber, BonusNumber);
+    printRevenue(cntLotto, benefit);
   }
 
-  Input(){
+  InputMoney(){
     let money;
-    Console.readLine('구입금액을 입력해 주세요.', (input) => {
+    let cntLotto;
+
+    MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (input) => {
       money = parseInt(input);
     });
     if(money < 1000 || money % 1000 !== 0)
       throw new error("잘못된 입력입니다.");
-    return ;
+    cntLotto = money/1000;
+    MissionUtils.Console.print(cntLotto +'개를 구매했습니다.');
+    return cntLotto;
+  }
+
+  makeLottos(cntLotto){
+    let Lotto_list = [];
+    for(let i = 0;i<cntLotto;i++)
+    {
+        let num = MissionUtils.Random.pickUniqueNumbersInRange(1, 10, 2);
+        let lotto = new Lotto(num);
+        Lotto_list.push(lotto);
+    }
+    return Lotto_list;
   }
 }
 
