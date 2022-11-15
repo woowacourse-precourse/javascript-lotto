734a9d6ccc8f314c81d2f9f94534bc5b8044d1b9
wzrabbit <curious.wzrabbit@gmail.com>
2022-11-15 22:50:14 +0900

    feat(app): 게임 시작 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const BetterConsole = require('./lib/BetterConsole');
+const Lotto = require('./Lotto');
+const BonusNumber = require('./BonusNumber');
+const Inventory = require('./Inventory');
+const Printer = require('./Printer');
+const Validator = require('./lib/Validator');
+const { MESSAGE } = require('./constant/Constant');
+
 class App {
-  play() {}
+  #lottos;
+  #bonusNumber;
+  #inventory;
+
+  play() {
+    this.#inventory = new Inventory();
+    this.#moneyInputPhase();
+  }
 }
 
 module.exports = App;
