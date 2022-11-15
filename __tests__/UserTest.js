99cd654c2da50b806b542a376a88d8b612e844c5
ksone02 <eksovus@gmail.com>
2022-11-15 20:15:23 +0900

    test(User): User 클래스 메소드 테스트

--- __tests__/UserTest.js
+++ __tests__/UserTest.js
@@ -1,25 +1,81 @@
-const User = require("../src/components/User");
-const { ERROR } = require("../src/data/constants");
+const User = require('../src/components/User');
+const { ERROR, NUMBER } = require('../src/data/constants');
+const MissionUtils = require('@woowacourse/mission-utils');
 
-describe("유저 입력 테스트", () => {
-  test("1000원으로 나누어 떨어지는지", () => {
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+const mockRandoms = numbers => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+describe('유저 입력 테스트', () => {
+  test('1000원으로 나누어 떨어지는지', () => {
     expect(() => {
-      new User("1234001");
+      new User('1234001');
     }).toThrow(ERROR.DIVIDE);
   });
-  test("잘못된 범위", () => {
+  test('잘못된 범위', () => {
     expect(() => {
-      new User("kangsangwon");
+      new User('kangsangwon');
     }).toThrow(ERROR.RANGE);
   });
-  test("잘못된 범위2", () => {
+  test('잘못된 범위2', () => {
     expect(() => {
-      new User("-1234001");
+      new User('-1234001');
     }).toThrow(ERROR.RANGE);
   });
-  test("잘못된 범위3", () => {
+  test('잘못된 범위3', () => {
     expect(() => {
-      new User("0");
+      new User('0');
     }).toThrow(ERROR.RANGE);
+  });
+});
+describe('유저 클래스 테스트', () => {
+  test('구매 제한?', () => {
+    const user = new User('12000');
+    expect(user.countBuyLimit()).toEqual(12);
+  });
+  test('로또번호 출력', () => {
+    const user = new User('2000');
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+      [7, 8, 9, 10, 11, 12],
+    ]);
+    const logs = ['[1, 2, 3, 4, 5, 6]', '[7, 8, 9, 10, 11, 12]'];
+    const logSpy = getLogSpy();
+
+    user.setLottos();
+    user.printMyLottos();
+
+    logs.forEach(log => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+  test('수익률 계산', () => {
+    const user = new User('12000');
+    const map = new Map();
+    map.set(6, 1);
+    expect(user.calculateYield(map)).toEqual(
+      (NUMBER.SIX_WINNING / 12000) * 100,
+    );
+  });
+  test('당첨개수 확인', () => {
+    const user = new User('2000');
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+      [7, 8, 9, 10, 11, 12],
+    ]);
+    user.setLottos();
+    const map = new Map();
+    map.set(6, 1);
+    map.set(0, 1);
+    expect(user.checkWinningCount([1, 2, 3, 4, 5, 6], 7)).toEqual(map);
   });
 });
