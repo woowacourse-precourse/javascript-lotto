1248a6f3d724d8f7a6ab80c01f73cdffac9730fc
adultlee <seoungin1228@gmail.com>
2022-11-15 16:02:31 +0900

    feat: 로또의 type을 확인
    로또의 모든 원소는 숫자인지 확인합니다.

--- src/Lotto/Lotto.js
+++ src/Lotto/Lotto.js
@@ -10,10 +10,18 @@ class Lotto {
 
   validate(numbers) {
     this.checkNumbersLength(numbers);
+    this.checkNumbersType(numbers);
   checkNumbersLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
+
+  checkNumbersType(numbers) {
+    numbers.map((number) => {
+      if (isNaN(number))
+        throw new Error("[ERROR] 로또는 모두 숫자여야 합니다.");
+    });
   }
 }
 
