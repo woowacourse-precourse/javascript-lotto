b21ebf6c66ed9d731603e16046523cff4a9339f9
2yunseong <dbsdltjd3701@naver.com>
2022-11-15 19:10:08 +0900

    refactor(InputUI): 필요없는 import 문 삭제

--- src/ui/InputUI.js
+++ src/ui/InputUI.js
@@ -1,6 +1,4 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-const TypeConverter = require('../util/TypeConverter');
-const Vaildator = require('../Vaildator');
 const UI = require('./UI');
 
 class InputUI extends UI {
