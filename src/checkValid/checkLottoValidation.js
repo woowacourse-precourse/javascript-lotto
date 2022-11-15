6b470155f8300548c7fe6c9cebd41f65b3c6c310
gsong <rmsdlf1206@gmail.com>
2022-11-15 15:24:58 +0900

    feat: 당첨번호 input 유효성 검사: checkNumberRange 구현

--- src/checkValid/checkLottoValidation.js
+++ src/checkValid/checkLottoValidation.js
@@ -4,8 +4,15 @@ function checkSixNumber(numbers) {
   }
 }
 
+function checkNumberRange(numbers) {
+  if (Math.min(numbers) < 1 || Math.max(numbers) > 45) {
+    throw new Error('[ERROR] 로또 번호는 1~45범위 안에 있어야 합니다.');
+  }
+}
+
 function checkLottoValidation(numbers) {
   checkSixNumber(numbers);
+  checkNumberRange(numbers);
 }
 
 module.exports = checkLottoValidation;
