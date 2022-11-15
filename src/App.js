f556eacdc52b1055d0a6bf8bc53c58fd54d120ab
yeongseokim <1903020@ewhain.net>
2022-11-15 22:20:14 +0900

    feat(App): 실행 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+const LottoMachine = require("./LottoMachine");
+const InputAcceptor = require("./InputAcceptor");
+const MessagePrinter = require("./MessagePrinter");
+const Matcher = require("./Matcher");
+const ReturnCalculator = require("./ReturnCalculator");
+const PURCHASE_UNIT = 1000;
 class App {
-  play() {}
+  constructor() {
+    this.lottoWallet = [];
+    this.purchaseAmount = 0;
+    this.numOfLotto = 0;
+  }
+
+  isValidNumOfLotto(purchaseAmount) {
+    this.numOfLotto = purchaseAmount / PURCHASE_UNIT;
+    if (!Number.isInteger(this.numOfLotto)) throw new Error('[ERROR] 구입 금액은 1000원 단위로 입력해주세요.');
+  }
+
+  async play() {
+    this.purchaseAmount = await InputAcceptor.askPurchaseAmountAsyncAwait();
+    this.isValidNumOfLotto(this.purchaseAmount);
+    MessagePrinter.printPurchaseAmount(this.numOfLotto);
+    this.lottoWallet = LottoMachine.createLottos(this.numOfLotto);
+    MessagePrinter.printAllLottos(this.lottoWallet);
+    const winningNumbers = await InputAcceptor.askWinningLottoNumbers();
+    const winningBonusNumber = await InputAcceptor.askWinningLottoBonusNumber(winningNumbers);
+    const matcher = new Matcher(winningNumbers, winningBonusNumber);
+    const result = matcher.getMatchResult(this.lottoWallet);
+    MessagePrinter.printResultStatistics(result);
+    const lottoReturn = ReturnCalculator.getReturn(result, this.purchaseAmount);
+    MessagePrinter.printReturn(lottoReturn);
+  }
 }
 
 module.exports = App;
