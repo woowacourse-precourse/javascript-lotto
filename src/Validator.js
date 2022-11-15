ff0ddac24892e372c2ccb8aa42963c38d461245c
HojinAn <asdv94@naver.com>
2022-11-15 17:02:28 +0900

    refactor(lotto): 숫자 범위 유효성 검사 위임으로 반복 감소

--- src/Validator.js
+++ src/Validator.js
@@ -16,8 +16,7 @@ class Validator {
   static validateNumbers(numbers) {
     if (numbers.length !== 6) throw new Error(prefix + only6Numbers);
     if (new Set(numbers).size < 6) throw new Error(prefix + duplicateNumbers);
-    if (numbers.reduce((isOut, no) => isOut || no < 1 || 45 < no, false))
-      throw new Error(prefix + outOfRange);
+    numbers.forEach(Validator.validateNumber);
   }
 
   static validateNumber(number) {
