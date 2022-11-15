e17490d25a6878ac5358ce89283e111b3f568e85
DongWooKim97 <nanaukim@naver.com>
2022-11-15 13:22:27 +0900

    feat: 기존의 QuickPick 클래스를 세분화한 클래스 PurChase클래스 추가

---
+++ src/input/PurChase.js
@@ -0,0 +1,29 @@
+const { LOTTO_INFO, ERROR_MESSAGES } = require("../utils/Constants");
+
+class PurChase {
+  #payment;
+
+  constructor(payment) {
+    this.validate(payment);
+    this.#payment = payment;
+  }
+
+  validate(payment) {
+    if (Number(payment) < LOTTO_INFO.LOTTO_PRICE) {
+      throw new Error(ERROR_MESSAGES.INVALID_COST_MIN);
+    }
+
+    if (Number.isNaN(+payment)) {
+      throw new Error(ERROR_MESSAGES.INVALID_COST_TYPE);
+    }
+
+    if (Number(payment) % LOTTO_INFO.LOTTO_PRICE !== 0) {
+      throw new Error(ERROR_MESSAGES.INVALID_COST_UNIT);
+    }
+  }
+
+  getPayment() {
+    return Number(this.#payment);
+  }
+}
+module.exports = PurChase;
