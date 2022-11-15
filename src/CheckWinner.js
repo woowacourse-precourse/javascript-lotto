d32969d50a1610bd6f0b767c7be10b5c16cf760c
vjvl95 <vjvl95@naver.com>
2022-11-15 20:03:35 +0900

    fix : ranking 객체 수정

--- src/CheckWinner.js
+++ src/CheckWinner.js
@@ -1,21 +1,19 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class CheckWinner {
-  #count;
   #bouns;
-  #winningLooto;
+  #winningLotto;
   #myLotto;
   constructor(winningLotto, bouns, myLotto) {
-    this.#winningLooto = winningLotto;
+    this.#winningLotto = winningLotto;
     this.#bouns = bouns;
     this.#myLotto = myLotto;
-
-    this.rank = {
-      first: 0,
-      second: 0,
-      third: 0,
-      fourth: 0,
-      fifth: 0,
+    this.ranking = {
+      '1등': 0,
+      '2등': 0,
+      '3등': 0,
+      '4등': 0,
+      '5등': 0,
     };
     this.reward = 0;
     this.checkPrice();
@@ -26,51 +24,50 @@ class CheckWinner {
       let count = 0;
       let bonus = 0;
       this.#myLotto[i].map((n) => {
-        if (this.#winningLooto.includes(n)) {
+        if (this.#winningLotto.includes(n)) {
           count++;
         }
-        if (n == this.#bouns) {
+        if (n === this.#bouns) {
           bonus++;
         }
       });
-      MissionUtils.Console.print(
-        '결과' + '카운드: ' + count + ' 보너스:' + bonus
-      );
+
       this.divideWinner(count, bonus);
     }
   }
 
   divideWinner(count, bonus) {
     if (count === 3) {
-      this.rank['fifth'] += 1;
+      this.ranking['5등'] += 1;
       this.reward += 5000;
       return;
     }
     if (count === 4) {
-      this.rank['fourth']++;
+      this.ranking['4등']++;
       this.reward += 50000;
       return;
     }
     if (count === 5) {
       if (bonus === 0) {
-        this.rank['third']++;
+        this.ranking['3등']++;
         this.reward += 1500000;
         return;
       }
-      this.rank['second']++;
+      this.ranking['2등']++;
       this.reward += 30000000;
       return;
     }
     if (count === 6) {
-      this.rank['first']++;
+      this.ranking['1등']++;
       this.reward += 2000000000;
       return;
     }
-
-    MissionUtils.Console.print('최종상금:' + this.reward);
   }
   getReward() {
     return this.reward;
+  }
+  getRanking() {
+    return this.ranking;
   }
 }
 
