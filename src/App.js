2be3d36d2e713334978c1a3415eb97d809b3687d
KangYunJi <dbswl701@ajou.ac.kr>
2022-11-15 22:12:38 +0900

    refactor: 주석 제거 및 하드코딩 수정

--- src/App.js
+++ src/App.js
@@ -8,11 +8,8 @@ class App {
     this.cache=0;
     this.lottos=[];
     this.bonus=0;
-    // this.result = {
-    //   lotto : 0,
-    //   bonus : false,
-    // }
-    this.result2 = {
+
+    this.result = {
       3: 0,
       4: 0,
       5: 0,
@@ -32,8 +29,6 @@ class App {
     this.rate = 0;
   }
 
-  // statistics(){
-  // }
   printYieldRate(yieldSum, cache){
     this.rate = yieldSum/cache * 100;
     this.rate = Math.round(this.rate * 10) / 10;
@@ -41,47 +36,28 @@ class App {
   }
 
   yieldRate() {
-    // 분자 : 총 수입
-    // 분모 : 결제 금액
-    for(const key in this.result2){
-      if(this.result2[key] !== 0){
-        this.yieldSum += this.reward[key];
-        console.log(this.yieldSum);
-      }
-      // console.log(yield);
+    for(const key in this.result){
+      if(this.result[key] !== 0) this.yieldSum += this.reward[key];
     }
-
-    console.log(this.cache);
-    // const rate = this.yieldSum/this.cache * 100;
     this.printYieldRate(this.yieldSum, this.cache);
   }
 
   PrintLotto(){
-    Console.print(`3개 일치 (5,000원) - ${this.result2[3]}개`);
-    Console.print(`4개 일치 (50,000원) - ${this.result2[4]}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${this.result2[5]}개`);
-    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.result2['5bonus']}개`);
-    Console.print(`6개 일치 (2,000,000,000원) - ${this.result2[6]}개`);
+    for(const list in this.result){
+      if(list === '5bonus') Console.print(`5개 일치, 보너스 볼 일치 (${this.reward[list].toLocaleString()}원) - ${this.result[list]}개`);
+      else Console.print(`${list}개 일치 (${this.reward[list].toLocaleString()}원) - ${this.result[list]}개`);
+    }
   }
 
-  statistics(){ // 두개 비교
-    // console.log(`통계: ${this.winLotto.getNumbers().includes(this.lottos)}`);
-
+  statistics(){
     for(const lottoNum of this.lottos){
       // 여러개의 사용자 로또 번호 중 하나의 로또에서 당첨 로또 번호 있는지 비교
-
       lottoNum.setLottoResult(this.winLotto.getNumbers());
       lottoNum.setBonusResult(this.bonus);
       const winningCount = lottoNum.getResult();
 
-      if(winningCount.lotto === 5 && winningCount.bonus == true){
-        this.result2['5bonus']++;
-      } else if(winningCount.lotto >= 3){
-        this.result2[winningCount.lotto]++;
-      }
-      console.log(this.result2);
-      console.log(`==============================`);
-
+      if(winningCount.lotto === 5 && winningCount.bonus == true) this.result['5bonus']++;
+      else if(winningCount.lotto >= 3) this.result[winningCount.lotto]++;
     }
   }
 
@@ -91,7 +67,6 @@ class App {
       this.winLotto = new Lotto(splitNum);
       Console.readLine('보너스 번호를 입력해 주세요.', (answer)=>{
         this.bonus=answer;
-        console.log(`당첨번호 : ${this.winLotto.getNumbers()}, bonus : ${this.bonus}`);
       })
     })
     Console.close();
@@ -99,10 +74,8 @@ class App {
 
   PrintUserLottoNum(){
     for(const lotto of this.lottos){
-      // Console.print(lotto.getNumbers());
       Console.print(lotto.print());
     }
-
   }
 
   CreateLottoNum(){
@@ -112,26 +85,19 @@ class App {
   CreateUserLottoNum(){
     for(let idx=0; idx<this.lottoCount; idx++){
       const lottoNum = this.CreateLottoNum();
-      // console.log(lottoNum);
-
       const lotto = new Lotto(lottoNum);
       this.lottos.push(lotto);
-      // console.log(lotto.getNumbers());
     }
   }
 
   isValidCacheInput(answer){
     const lottotemp = answer/1000;
-    if(!Number.isInteger(lottotemp)) {
-      throw new Error("[ERROR] 이상한 입력");
-    }
+    if(!Number.isInteger(lottotemp)) throw new Error("[ERROR] 정상적이지 않은 입력입니다.");
     return lottotemp;
   }
 
-
   InputCache(){
     Console.readLine('구입금액을 입력해 주세요.', (answer)=>{
-      // 맞는지 확인하는 함수
       this.cache = answer;
       this.lottoCount = this.isValidCacheInput(answer);
       Console.print(`${this.lottoCount}개를 구매했습니다.`);
@@ -139,31 +105,14 @@ class App {
     Console.close();
   }
 
-
-
   play() {
-    // 금액 입력
-    this.InputCache();
-      // 금액에 맞는 로또 수량 발행
-      // 해당 갯수에 맞는 로또 번호 생성
-    this.CreateUserLottoNum();
-    // 사용자 로또 번호 출력
-    this.PrintUserLottoNum();
-    // 당첨번호 입력
-    this.InputWinLotto();
-    // 보너스 번호 입력
-    //InputBonusLotto();
-      // 당첨번호와 사용자 번호 비교
-     // CheckWin();
-
-    // 통계
-    this.statistics();
-
-    // 당첨내역 출력
-    this.PrintLotto();
-
-    // 수익률 계산 및 출력
-    this.yieldRate();
+    this.InputCache(); // 금액 입력
+    this.CreateUserLottoNum(); // 금액에 맞는 로또 번호 생성    
+    this.PrintUserLottoNum(); // 사용자 로또 번호 출력    
+    this.InputWinLotto(); // 당첨번호 입력     
+    this.statistics(); // 통계   
+    this.PrintLotto(); // 당첨내역 출력   
+    this.yieldRate(); // 수익률 계산 및 출력
   }
 }
 
