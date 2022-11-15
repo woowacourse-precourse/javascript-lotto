e089eb1530906cdfaf09526fd28ed9de4a736ebb
jihun1233 <leejihun1233@gmail.com>
2022-11-15 23:55:42 +0900

    feat: 3.번호 생성 기능 추가
    - 랜덤 번호를 생성하여 이를 Lotto 인스턴스로 만들어 User인스턴스에 addLotto로 추가
    
    - 마지막에 당첨번호와 보너스번호를 인수로 당첨현황과 수익률 출력

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 const MissionUtils = require('@woowacourse/mission-utils')
 const getInput = require('./getInput')
+const Lotto = require('./Lotto')
+const User = require('./User')
 const {validateBonusNumberInput, validateMatchNumbersInput, validatePriceInput} = require('./validateInput')
 
 class App {
   async play() {
+    
     const priceInput = await getInput('구입금액을 입력해 주세요\n')
     validatePriceInput(priceInput)
     const price = parseInt(priceInput, 10)
-    MissionUtils.Console.print(priceInput)
+
+    const user = new User(price)
+
+    const lottoCount = price / 1000
+
+    for (let index = 0; index < lottoCount; index++) {
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
+      user.addLotto(new Lotto(numbers))
+    }
+
+    user.printLottos()
 
     const matchNumbersInput = await getInput('당첨 번호를 입력해 주세요\n')
     validateMatchNumbersInput(matchNumbersInput)
     const matchNumbers = matchNumbersInput.split(',').map(n => parseInt(n, 10))
-    MissionUtils.Console.print(matchNumbersInput)
 
     const bonusNumberInput = await getInput('보너스 번호를 입력해 주세요\n')
     validateBonusNumberInput(bonusNumberInput, matchNumbers)
     const bonusNumber = parseInt(bonusNumberInput, 10)
-    MissionUtils.Console.print(bonusNumberInput)
     
+    user.printMatches(matchNumbers, bonusNumber)
+    user.printRateOfReturn()
+
     MissionUtils.Console.close()
   }
 }
