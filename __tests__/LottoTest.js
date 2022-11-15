71085a4e28b3075dc053398032be17765608b21d
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-15 12:06:57 +0900

    test(결과 반환 테스트) : 당첨번호에 따른 결과 반환 테스트
    - 1등부터 5등까지의 케이스를 모두 테스트(통과완료)

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -30,4 +30,25 @@ describe('로또 클래스 테스트', () => {
       new Lotto([1, 2, 3, 4, 'a', 6]);
     }).toThrow('[ERROR]');
   });
+
+  test('결과 반환 테스트', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const first = [1, 2, 3, 4, 5, 6];
+    const second = [1, 2, 3, 4, 5, 9];
+    const third = [1, 2, 3, 4, 5, 9];
+    const fourth = [1, 2, 3, 4, 8, 9];
+    const fifth = [1, 2, 3, 7, 8, 9];
+
+    const result1 = lotto.calculateResult(first);
+    const result2 = lotto.calculateResult(second, 6);
+    const result3 = lotto.calculateResult(third);
+    const result4 = lotto.calculateResult(fourth);
+    const result5 = lotto.calculateResult(fifth);
+
+    expect(result1).toBe('first');
+    expect(result2).toBe('second');
+    expect(result3).toBe('third');
+    expect(result4).toBe('fourth');
+    expect(result5).toBe('fifth');
+  });
 });
