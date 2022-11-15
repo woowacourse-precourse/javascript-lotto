8edcb762f4cebb883407d75cb88bdea4c1184c5f
DESKTOP-R2R34G5\Administrator <ujin4719@naver.com>
2022-11-15 17:33:49 +0900

    COMMIT

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const Randoms = () => {
+      MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    };
+
+    const Lotto = () => {
+      MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 7);
+    };
+
+    const userInput = () => {
+      MissionUtils.Console.readLine("구입금액을 입력해 주세요.", input);
+    };
+    const userInputnumber = () => {
+      MissionUtils.Console.readLine("당첨 번호를 입력해 주세요.", (input) => {
+        const userNumbers = input.split(",");
+        if (UserNumber !== 45) {
+          throw new Error(
+            "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야합니다."
+          );
+        }
+      });
+    };
+  }
 }
 
 module.exports = App;
