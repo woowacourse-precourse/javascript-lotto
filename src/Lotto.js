37d493dacceebbbae9cd85567dceb724b7152c72
1 <wrasf175@naver.com>
2022-11-15 19:04:27 +0900

    fix(App): 숫자 범위 입력 예외처리 메서드 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,7 +10,7 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    if (numbers.every((e) => e >= 1 && e <= 45)) {
+    if (!numbers.every((e) => e >= 1 && e <= 45)) {
       throw new Error("[ERROR] 로또 번호는 1에서 45 사이의 숫자만 가능합니다.");
     }
     const setNumbers = new Set(numbers)
