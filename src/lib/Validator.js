0740096b59ce7fcf51a33c5b3e12ccbfd83336f8
wzrabbit <curious.wzrabbit@gmail.com>
2022-11-15 22:10:55 +0900

    feat(validator): 로또 기본 숫자 6개를 검증하는 기능 구현하기

---
+++ src/lib/Validator.js
@@ -0,0 +1,21 @@
+const { LOTTO, ERROR, REGEX } = require('../constant/Constant');
+
+class Validator {
+  static errorIfLottosInvalidFormat(lottos) {
+    if (lottos.length !== LOTTO.COUNT) {
+      throw Error(ERROR.LOTTO_COUNT_INCORRECT);
+    }
+
+    if (new Set(lottos).size !== LOTTO.COUNT) {
+      throw Error(ERROR.LOTTO_DUPLICATED);
+    }
+
+    lottos.forEach((currentNumber) => {
+      if (!REGEX.LOTTO_NUMBER_FORMAT.test(currentNumber)) {
+        throw Error(ERROR.LOTTO_INVALID_VALUE);
+      }
+    });
+  }
+}
+
+module.exports = Validator;
