fb5072629a1cad85dd69fb8a5ea54aed18093d80
loopy-lim <loopy@jnu.ac.kr>
2022-11-15 19:37:01 +0900

    feat(validater): splitLottNumbers는 6개의 서로 다른 로또번호여야 한다

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -41,4 +41,9 @@ describe('Validator 클래스 테스트', () => {
       LottoValidator.getLottoPuchaseNumber(1002);
     }).toThrow('[ERROR]');
   });
+  test('splitLottoNumbers는 6개의 서로 다른 로또번호가 아닐시 예외가 발생한다.', () => {
+    expect(() => {
+      LottoValidator.splitLottoNumbers("1,2,3,4,5,5");
+    }).toThrow('[ERROR]');
+  });
 });
