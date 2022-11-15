665f031ee6ff7d0fd9951be6234683e2294b4151
동동이 <drdragon777@naver.com>
2022-11-15 19:43:23 +0900

    test - add LottoAdminTest 로또 관리자 이익률 계산 테스트

--- __tests__/LottoAdminTest.js
+++ __tests__/LottoAdminTest.js
@@ -1,5 +1,6 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const LottoAdmin = require("../src/LottoAdmin");
+const { price } = require("../src/constants");
 
 const getLogSpy = () => {
   const logSpy = jest.spyOn(MissionUtils.Console, "print");
@@ -16,6 +17,7 @@ describe("로또 관리자 테스트", () => {
       )
     ).toEqual(4);
   });
+
   test("당첨 통계를 알려준다.", () => {
     const statistics = [1, 2, 1, 4, 1];
     const logs = [
@@ -30,5 +32,13 @@ describe("로또 관리자 테스트", () => {
     logs.forEach((log) => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
     });
+  });
+
+  test("이익률을 계산한다.", () => {
+    expect(LottoAdmin.getMargin(price, [4, [1, 1, 1, 1, 1]])).toEqual(50788875);
+    expect(LottoAdmin.getMargin(price, [4, [1, 1, 0, 0, 0]])).toEqual(1375);
+    expect(LottoAdmin.getMargin(price, [8, [3, 1, 0, 0, 0]])).toEqual(812.5);
+    expect(LottoAdmin.getMargin(price, [12, [3, 1, 0, 0, 0]])).toEqual(541.67);
+    expect(LottoAdmin.getMargin(price, [17, [3, 3, 0, 0, 0]])).toEqual(970.59);
   });
 });
