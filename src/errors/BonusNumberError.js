d410b5b714d89bcfa37b1669b277948a6ce25dd1
yesolyo <ahnyelim77@gmail.com>
2022-11-15 19:07:09 +0900

    feat: 보너스 번호 1~45 외 입력시 예외 발생

--- src/errors/BonusNumberError.js
+++ src/errors/BonusNumberError.js
@@ -14,6 +14,13 @@ class BonusNumberError {
         throw new Error("[ERROR] 당첨번호와 보너스번호는 달라야합니다.");
       }
     }
+    this.rangeBonusNumber(inputBonusNumber);
+  }
+
+  rangeBonusNumber(inputBonusNumber) {
+    if (!(inputBonusNumber >= 1 && inputBonusNumber <= 45)) {
+      throw new Error("[ERROR] 1~45 숫자만 입력해야 합니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
