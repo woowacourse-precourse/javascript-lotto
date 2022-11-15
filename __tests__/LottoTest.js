41a69f7e76a269ef7245847892a6b41bc83bea32
하송희 <99heesong@naver.com>
2022-11-15 22:15:17 +0900

    테스트: 보유금액과 보너스 번호 예외 테스트 코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,8 @@
 const Lotto = require("../src/Lotto");
+const {
+  validateInputMoney,
+  validateInputBonusNum,
+} = require("../src/validator");
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -23,6 +27,61 @@ describe("로또 클래스 테스트", () => {
   test("로또 번호에 46이상의 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow();
+  });
+
+  test("보유 금액이 숫자가 아니면 예외가 발생한다.", () => {
+    const money = ["a", "B", "하나"];
+
+    money.forEach((result) => {
+      expect(() => {
+        validateInputMoney(result);
+      }).toThrow();
+    });
+  });
+
+  test("보유 금액이 1000원 단위가 아니면 예외가 발생한다.", () => {
+    const money = [999, 1001, 100, 500, 0];
+
+    money.forEach((result) => {
+      expect(() => {
+        validateInputMoney(result);
+      }).toThrow();
+    });
+  });
+
+  test("보너스 번호가 숫자가 아니면 예외가 발생한다.", () => {
+    const bonusNumber = ["a", "ABC", "에이"];
+
+    bonusNumber.forEach((result) => {
+      expect(() => {
+        validateInputBonusNum(result);
+      }).toThrow();
+    });
+  });
+
+  test("보너스 번호가 0이하의 숫자면 예외가 발생한다.", () => {
+    const bonusNumber = 0;
+
+    expect(() => {
+      validateInputBonusNum(bonusNumber);
+    }).toThrow();
+  });
+
+  test("보너스 번호가 46이상의 숫자면 예외가 발생한다", () => {
+    const bonusNumber = 46;
+
+    expect(() => {
+      validateInputBonusNum(bonusNumber);
+    }).toThrow();
+  });
+
+  test("보너스 번호와 당첨 번호가 중복되면 예외가 발생한다.", () => {
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 1;
+
+    expect(() => {
+      validateInputBonusNum(winningNumbers, bonusNumber);
     }).toThrow();
   });
 });
