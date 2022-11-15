c2371f12b64a36ac545fdde77cfe39d4fb87c2fe
Jongco <narira0922@gmail.com>
2022-11-15 20:58:40 +0900

    feat: 구매 금액을 입력 받았을 때 알맞은 로또 개수를 생성

--- src/Lotto.js
+++ src/Lotto.js
@@ -14,6 +14,15 @@ class Lotto {
       if ([...new Set(numbers)].length !== numbers.length) throw new Error("[ERROR] 중복된 값이 존재합니다.");
     }
   }
+
+  getInputMoney() {
+    Console.readLine("구매금액을 입력해주세요.\n", (money) => {
+      if (money != Number(money)) throw new Error("[ERROR] 숫자만 넣어주세요.");
+      if (money < 1000) throw new Error("[ERROR] 1000원 이상만 구매할 수 있습니다.");
+      const buyLotto = Math.floor(money / 1000);
+      this.getRandomNumber(buyLotto);
+    });
+  }
 }
 
 module.exports = Lotto;
