61212b66dfac6a55559eb791ff4f926fe0ffd1d7
Minhye Kang <alsgp9898@naver.com>
2022-11-15 13:23:22 +0900

    refactor(LottoGenerator): 구입한 로또 수량을 private 변수로 변경

--- src/LottoGenerator.js
+++ src/LottoGenerator.js
@@ -2,17 +2,19 @@ const { print, random } = require('./util');
 const { checkLottoAmount } = require('./validation');
 
 class LottoGenerator {
+  #lottoAmount;
+
   constructor(money) {
     checkLottoAmount(money);
-    this.lottoAmount = money / 1000;
-    print(`${this.lottoAmount}개를 구매했습니다.`);
+    this.#lottoAmount = money / 1000;
+    print(`${this.#lottoAmount}개를 구매했습니다.`);
     this.publishUserLotto();
   }
 
   publishUserLotto() {
     const publishedUserLotto = [];
 
-    for (let index = 0; index < this.lottoAmount; index++) {
+    for (let index = 0; index < this.#lottoAmount; index++) {
       let eachUserLottoNumber = random();
       publishedUserLotto.push(eachUserLottoNumber);
       print(`[${eachUserLottoNumber.join(', ')}]`);
