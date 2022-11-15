993e4866e09c46c5fd149b00f8cf5bd5176e9589
rayark1 <max980203@snu.ac.kr>
2022-11-15 10:28:23 +0900

    feat(src/App.js): 로또 구입 가격에 해당되는 로또의 총 갯수를 계산하는 calculateNumberOfTickets 함수 구현

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ class App {
   getInputPurchaseAmount(callback) {
     MissionUtils.Console.readLine("구매금액을 입력해 주세요.", callback);
   }
-  
+
+  calculateNumberOfTickets(price) {
+    this.validateInputPurchase(price);
+    const numberOfTickets = Math.floor(price / 1000);
+    return numberOfTickets;
+  }
+
 }
 
 module.exports = App;
