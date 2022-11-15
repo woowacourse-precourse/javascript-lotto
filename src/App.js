bd08134ade64c0f2ec66f0b60a0a49b3d7be2832
김나현 <nahyeon.dev@gmail.com>
2022-11-15 11:40:35 +0900

    feat: constructor()내에 ranking,random 추가, setRandomNumberLotto() 함수내의 로또번호 따로 randomNumber()로 이동

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ const Lotto = require("./Lotto");
 class App {
   constructor(){
     this.purchase = 0;
+    this.ranking = {};
+    this.random = [];
     this.lottoNumber = [];
     this.winningNumber = [];
     this.bonusNumber = 0;
@@ -72,10 +74,13 @@ class App {
   setRandomNumberLotto(num){
     while (num > 0) {
       num -= 1;
-      const numbers = MissionUtils.Random
-        .pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b); //여기 오류!
-      this.lottoNumber.push(new Lotto(numbers));
+      this.random = this.randomNumber();
+      this.lottoNumber.push(new Lotto(this.random));
     }
+  }
+  randomNumber(){
+    const number = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    return number;
   }
   printLottoNumber(){
     this.lottoNumber.forEach((lotto) => {
@@ -84,38 +89,37 @@ class App {
     this.calculateLotto();
   }
   calculateLotto(){
-    let ranking = {};
     let i = 0;
     while(i < 6){
-      ranking[i] = 0;
+      this.ranking[i] = 0;
       i++;
     }
     const arr = this.lottoNumber.map((lotto) =>
       lotto.calculate(this.winningNumber, this.bonusNumber)
     );
     arr.forEach((num) => {
-      ranking[num]++;
+      this.ranking[num]++;
     });
-    this.resultLotto(ranking);
+    this.resultLotto(this.ranking);
   }
   resultLotto(rank){
     MissionUtils.Console.print("당첨 통계\n---");
-    MissionUtils.Console.print(`3개 일치 (5,000원) - ${rank[5]}개`);
-    MissionUtils.Console.print(`4개 일치 (50,000원) - ${rank[4]}개`);
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${rank[1]}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${rank[2]}개`);
     MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${rank[3]}개`);
-    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank[2]}개`);
-    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${rank[1]}개`);
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank[5]}개`);
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${rank[4]}개`);
     MissionUtils.Console.print(`총 수익률은 ${this.resultRate(rank)}%입니다.`);
 
     MissionUtils.Console.close();
   }
   resultRate(rank){
     let sum = 0;
-    sum += rank[5]*5000;
-    sum += rank[4]*50000;
+    sum += rank[1]*5000;
+    sum += rank[2]*50000;
     sum += rank[3]*1500000;
-    sum += rank[1]*2000000000;
-    sum += rank[2]*30000000;
+    sum += rank[4]*2000000000;
+    sum += rank[5]*30000000;
 
     let rate = (sum / this.purchase) * 100;
     return rate.toFixed(1);
