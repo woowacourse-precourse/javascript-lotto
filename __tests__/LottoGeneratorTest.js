347899b5da987c92ff3c8ecd3ee5ba95be46a750
2yunseong <dbsdltjd3701@naver.com>
2022-11-15 21:46:53 +0900

    test(LottoGenerator): 로또 게임을 주어진 개수만큼 랜덤으로 생성하는 테스트

--- __tests__/LottoGeneratorTest.js
+++ __tests__/LottoGeneratorTest.js
@@ -14,4 +14,22 @@ describe('LottoGenerator 클래스 테스트', () => {
     mockRandoms([[1, 2, 3, 4, 6, 5]]);
     expect(LottoGenerator.generate().getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
   });
+
+  test('로또 게임을 주어진 개수만큼 랜덤으로 생성하는 테스트', () => {
+    const answer = [
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 4, 6, 8, 10],
+      [4, 5, 6, 7, 8, 9],
+    ];
+    mockRandoms([
+      [1, 4, 3, 2, 5, 6],
+      [2, 6, 4, 8, 10, 1],
+      [9, 8, 7, 6, 5, 4],
+    ]);
+
+    const lottos = LottoGenerator.generatedByCount(3);
+    lottos.forEach((lotto, index) => {
+      expect(lotto.getNumbers()).toEqual(answer[index]);
+    });
+  });
 });
