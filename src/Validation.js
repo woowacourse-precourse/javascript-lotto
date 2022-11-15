972ceffabbf88b43af706678f2ea3524648b8915
useon <yoosk4986@naver.com>
2022-11-15 17:02:19 +0900

    refactor(Validation, Error): 입력 에러 메세지를 상수화한다

--- src/Validation.js
+++ src/Validation.js
@@ -1,47 +1,49 @@
+const { PURCHASE_ERROR_MESSAGE, WINNING_ERROR_MESSAGE, BONUS_ERROR_MESSAGE } = require('./constants/Error');
+
 class Validation {
   purchaseInputValue(inputValue) {
     const inputNumber = Number(inputValue);
     if (Number.isNaN(inputNumber)) {
-      throw new Error('[ERROR] 금액을 입력해주세요.');
+      throw new Error(PURCHASE_ERROR_MESSAGE.INPUT_ONLY_NUMBER);
     }
     if (inputNumber % 1000 !== 0) {
-      throw new Error('[ERROR] 1000원 단위로 입력해주세요.');
+      throw new Error(PURCHASE_ERROR_MESSAGE.INPUT_VALID_UNIT);
     }
   }
 
   winningInputValue(inputValue) {
     let winningNumbers = inputValue.split(',');
     if (inputValue.includes(',') === false) {
-      throw new Error('[ERROR] 각 번호를 콤마로 구분하여 입력해주세요.');
+      throw new Error(WINNING_ERROR_MESSAGE.INPUT_SEPARATION_NUMBER);
     }
     if (winningNumbers.length !== 6) {
-      throw new Error('[ERROR] 당첨 번호 6개를 입력해주세요.');
+      throw new Error(WINNING_ERROR_MESSAGE.INPUT_SIX_NUMBER);
     }
     winningNumbers.forEach((item) => {
       const numberItem = Number(item);
       if (Number.isNaN(numberItem)) {
-        throw new Error('[ERROR] 당첨 번호는 숫자만 입력해주세요.');
+        throw new Error(WINNING_ERROR_MESSAGE.INPUT_ONLY_NUMBER);
       }
       if (numberItem < 1 || numberItem > 45) {
-        throw new Error('[ERROR] 당첨 번호는 1 ~ 45 범위의 숫자로 입력해주세요.');
+        throw new Error(WINNING_ERROR_MESSAGE.INPUT_WITHIN_RANGE);
       }
     });
     winningNumbers = new Set(winningNumbers);
     if (winningNumbers.size !== 6) {
-      throw new Error('[ERROR] 당첨 번호를 중복 없이 입력해주세요.');
+      throw new Error(WINNING_ERROR_MESSAGE.INPUT_DIFFERENT_NUMBER);
     }
   }
 
   bonusInputValue(winningInput, bonusInput) {
     const bonusNumber = Number(bonusInput);
     if (Number.isNaN(bonusNumber)) {
-      throw new Error('[ERROR] 보너스 번호는 숫자를 입력해주세요.');
+      throw new Error(BONUS_ERROR_MESSAGE.INPUT_ONLY_NUMBER);
     }
     if (bonusNumber < 1 || bonusNumber > 45) {
-      throw new Error('[ERROR] 보너스 번호는 1 ~ 45 범위의 숫자로 입력해주세요.');
+      throw new Error(BONUS_ERROR_MESSAGE.INPUT_WITHIN_RANGE);
     }
     if (winningInput.includes(bonusNumber)) {
-      throw new Error('[ERROR] 보너스 번호는 당첨 번호에 없는 숫자를 입력해주세요.');
+      throw new Error(BONUS_ERROR_MESSAGE.INPUT_NON_WINNING_NUMBER);
     }
   }
 }
---
+++ src/constants/Error.js
@@ -0,0 +1,24 @@
+const PURCHASE_ERROR_MESSAGE = {
+  INPUT_ONLY_NUMBER: '[ERROR] 구매 금액은 숫자만 입력해주세요.',
+  INPUT_VALID_UNIT: '[ERROR] 1000원 단위로 입력해주세요.',
+}
+
+const WINNING_ERROR_MESSAGE = {
+  INPUT_SEPARATION_NUMBER: '[ERROR] 각 번호를 콤마로 구분하여 입력해주세요.',
+  INPUT_SIX_NUMBER: '[ERROR] 당첨 번호 6개를 입력해주세요.',
+  INPUT_ONLY_NUMBER: '[ERROR] 당첨 번호는 숫자만 입력해주세요.',
+  INPUT_WITHIN_RANGE: '[ERROR] 당첨 번호는 1 ~ 45 범위의 숫자로 입력해주세요.',
+  INPUT_DIFFERENT_NUMBER: '[ERROR] 당첨 번호를 중복 없이 입력해주세요.',
+}
+
+const BONUS_ERROR_MESSAGE = {
+  INPUT_ONLY_NUMBER: '[ERROR] 보너스 번호는 숫자를 입력해주세요.',
+  INPUT_WITHIN_RANGE: '[ERROR] 보너스 번호는 1 ~ 45 범위의 숫자로 입력해주세요.',
+  INPUT_NON_WINNING_NUMBER: '[ERROR] 보너스 번호는 당첨 번호에 없는 숫자를 입력해주세요.',
+}
+
+module.exports = {
+  PURCHASE_ERROR_MESSAGE,
+  WINNING_ERROR_MESSAGE,
+  BONUS_ERROR_MESSAGE,
+}
