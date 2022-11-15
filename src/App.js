cbcc5f90599c09508f2df38f7d316232b76369ae
jaesukpark77 <jaesukpark77@naver.com>
2022-11-15 17:57:43 +0900

    feat: 로또 금액 확인

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const Lotto = require("./Lotto");
+
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  boughtLottos = [];
+
+  checkAmountOfMoneyToBuy(money) {
+    const CHECK_RESULT = money % 1000;
+
+    switch (isNaN(CHECK_RESULT) || CHECK_RESULT) {
+      case 0:
+        break;
+      case true:
+        const WRONG_NUMBER = "[ERROR] 숫자가 아닙니다.";
+        throw new Error(WRONG_NUMBER);
+      default:
+        const WRONG_INPUT = "[ERROR] 1000원 단위 금액이 아닙니다.";
+        throw new Error(WRONG_INPUT);
+    }
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
