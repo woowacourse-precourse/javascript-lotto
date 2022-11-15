c891c23fce742e66f5b4dbcaf8eaae6300e075cb
nsyeon <lovepny@gmail.com>
2022-11-15 11:29:57 +0900

    feat(ComparisonOfWinningNumbers): 당첨 내역 출력

--- src/ComparisonOfWinningNumbers.js
+++ src/ComparisonOfWinningNumbers.js
@@ -1,10 +1,13 @@
+const { MissionUtils } = require("@woowacourse/mission-utils");
+
 const NUMBER_OF_MATCHES = 3;
 
 class ComparisonOfWinningNumbers {
 
-  constructor(lottoNumber, winningNumber) {
+  constructor(lottoNumber, winningNumber, bonusNumber) {
     this.lottoNumber = lottoNumber;
     this.winningNumber = winningNumber;
+    this.bonusNumber = bonusNumber;
   }
 
   Comparison() {
@@ -18,11 +21,21 @@ class ComparisonOfWinningNumbers {
 
   countAndSave(numberComparison) {
     if (numberComparison >= NUMBER_OF_MATCHES) {
+      if (numberComparison === 5 && this.winningNumber.includes(this.bonusNumber)) {
+        return 1;
+      }
       return numberComparison;
     }
     return 0;
   }
 
+  checkTheRank(rank) {
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${rank.filter(element => '3' === element).length}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${rank.filter(element => '4' === element).length}개`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${rank.filter(element => '5' === element).length}개`);
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank.filter(element => '1' === element).length}개`);
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${rank.filter(element => '6' === element).length}개`);
+  }
 
 }
 
