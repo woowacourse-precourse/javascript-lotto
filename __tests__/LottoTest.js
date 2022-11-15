25573abc9ee8ada94145f23966f406b70804e314
nalsae <nalsae0207@naver.com>
2022-11-15 20:03:10 +0900

    test: modify tests
    테스트 로직 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,79 @@
 const Lotto = require("../src/Lotto");
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe("당첨 번호 예외 테스트", () => {
+  test("당첨 번호로 숫자가 아닌 값을 입력하는 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto(["안녕하세요"]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("당첨 번호로 숫자가 아닌 값을 입력하는 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto(["#$@!@!$"]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("당첨 번호의 개수가 6개를 초과하면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test("당첨 번호의 개수가 6개보다 적으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("당첨 번호의 숫자가 1보다 작으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 0]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("당첨 번호의 숫자가 45보다 크면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 48, 8]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
+});
 
-  // 아래에 추가 테스트 작성 가능
+describe("보너스 번호 예외 테스트", () => {
+  const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+  test("보너스 번호로 숫자가 아닌 값을 입력하는 경우 예외가 발생한다.", () => {
+    expect(() => {
+      lotto.validateBonusNum("우테코");
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호로 숫자가 아닌 값을 입력하는 경우 예외가 발생한다.", () => {
+    expect(() => {
+      lotto.validateBonusNum("*^^*");
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호의 숫자가 1보다 작으면 예외가 발생한다.", () => {
+    expect(() => {
+      lotto.validateBonusNum(0);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호의 숫자가 45보다 크면 예외가 발생한다.", () => {
+    expect(() => {
+      lotto.validateBonusNum(50);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호의 숫자가 당첨 번호와 중복되는 경우 예외가 발생한다.", () => {
+    expect(() => {
+      lotto.validateBonusNum(6);
+    }).toThrow("[ERROR]");
+  });
 });
