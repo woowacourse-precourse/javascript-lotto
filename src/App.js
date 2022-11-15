75d7a2a3fddf6a1e8b486b6f8b21e5637173933c
jinyong <zhaqh146@naver.com>
2022-11-15 14:13:05 +0900

    feat: 구입 금액에 대한 예외처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 const { Console } = require('@woowacourse/mission-utils');
-
+const ValidatePay = require('./ValidatePay');
 
 class App {
   play() {
     this.inputPay();
-  }
+  };
 
   inputPay() {
     Console.readLine(`구입금액을 입력해 주세요.\n`, answer => {
-      Console.print(answer);
-    })
-  }
+      const validatepay = new ValidatePay(answer);
+    });
+  };
 
 }
 
