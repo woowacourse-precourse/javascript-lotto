b49a7fc809ae1062537b75d66a33619d63587d1f
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-15 10:53:09 +0900

    test: add test case about lotto statistics

---
+++ __tests__/StatisticTest.js
@@ -0,0 +1,8 @@
+const { Statistic } = require("../src/Player");
+describe("금액 validation 테스트", () => {
+  test("로또 번호가 맞은 갯수를 센다", () => {});
+
+  test("보너스 번호가 맞았는지를 판단한다", () => {});
+
+  test("로또 등수를 계산한다", () => {});
+});
