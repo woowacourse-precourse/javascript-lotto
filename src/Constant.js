aad0f260b24fe251f88bcb52ab6d02065e98d086
energy1-914 <dlawldnjs5@daum.net>
2022-11-15 23:36:56 +0900

    chore:상수 추가

--- src/Constant.js
+++ src/Constant.js
@@ -10,12 +10,15 @@ const WINNING_MESSAGES = {
   THREE_MATCHED: '3개 일치 (5,000원)',
   FOUR_MATCHED: '4개 일치 (50,000원)',
   FIVE_MATCHED: '5개 일치 (1,500,000원)',
-  FIVE_BONUS_MATCHED: '5개 일치, 보너스 볼 일치 (30,000,000원',
+  FIVE_BONUS_MATCHED: '5개 일치, 보너스 볼 일치 (30,000,000원)',
   SIX_MATCHED: '6개 일치 (2,000,000,000원)',
 };
+
+const COUNT_MATCH_NUMBER = [0, 0, 0, 0, 0];
 
 module.exports = {
   INPUT_MESSAGES, 
   PAYMENT_AMOUNT_MESSAGES,
   WINNING_MESSAGES,
+  COUNT_MATCH_NUMBER,
 }
