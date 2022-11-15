0b49dcbbbb5ae1f10bce0197d5fe792d735a1002
NaveOWO <pianowell96@gmail.com>
2022-11-15 20:55:40 +0900

    [ refactor ] 변수명 변경

--- __tests__/AppValidationTest.js
+++ __tests__/AppValidationTest.js
@@ -3,28 +3,28 @@ const App = require("../src/App");
 describe("구매 금액 입력 에외 테스트", () => {
   test("숫자가 아닌 입력값일 경우에 예외가 발생한다.", () => {
     const app = new App();
-    const inputBuyMoney = "800p";
+    const inputLottoMoney = "800p";
 
     expect(() => {
-      app.validateIsNumber(inputBuyMoney);
+      app.validateIsNumber(inputLottoMoney);
     }).toThrow("[ERROR]숫자를 입력해주세요");
   });
 
   test("1000원 이하의 금액을 입력하면 예외가 발생한다.", () => {
     const app = new App();
-    const inputBuyMoney = "800 ";
+    const inputLottoMoney = "800 ";
 
     expect(() => {
-      app.validateIsOverThousand(inputBuyMoney);
+      app.validateIsOverThousand(inputLottoMoney);
     }).toThrow("[ERROR]1000원 이상의 금액을 입력해주세요.");
   });
 
   test("1000원 단위가 아닌 숫자를 입력하면 예외가 발생한다.", () => {
     const app = new App();
-    const inputBuyMoney = "8800";
+    const inputLottoMoney = "8800";
 
     expect(() => {
-      app.validateIsDividedByThounsand(inputBuyMoney);
+      app.validateIsDividedByThounsand(inputLottoMoney);
     }).toThrow("[ERROR]1000원 단위의 숫자를 입력해주세요.");
   });
 });
