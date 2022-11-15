67f2963a29351acf8f8f51a05742878d12d57dff
하송희 <99heesong@naver.com>
2022-11-15 23:00:43 +0900

    리팩토링: view 클래스 메서드 수정

--- src/View.js
+++ src/View.js
@@ -15,15 +15,15 @@ class View {
     return numbers;
   }
 
-  showWinResult(equalScore) {
+  showWinResult(equalScores) {
     Console.print("\n당첨 통계\n---");
-    Console.print("3개 일치 (5,000원) - " + equalScore[4] + "개");
-    Console.print("4개 일치 (50,000원) - " + equalScore[3] + "개");
-    Console.print("5개 일치 (1,500,000원) - " + equalScore[2] + "개");
+    Console.print("3개 일치 (5,000원) - " + equalScores[4] + "개");
+    Console.print("4개 일치 (50,000원) - " + equalScores[3] + "개");
+    Console.print("5개 일치 (1,500,000원) - " + equalScores[2] + "개");
     Console.print(
-      "5개 일치, 보너스 볼 일치 (30,000,000원) - " + equalScore[1] + "개"
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - " + equalScores[1] + "개"
     );
-    Console.print("6개 일치 (2,000,000,000원) - " + equalScore[0] + "개");
+    Console.print("6개 일치 (2,000,000,000원) - " + equalScores[0] + "개");
   }
 
   showProfit(lottoYield) {
