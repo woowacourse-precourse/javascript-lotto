ea65733cf8862657c48b67ac2330882e464c6927
dhkham <khamdong@gmail.com>
2022-11-15 12:51:43 +0900

    refactor(Game.js): validate payment input using Payment class
    - replace validateUserPayment function

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,10 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
 const Bonus = require("./Bonus");
+const Payment = require("./Payment");
 
 class Game {
   constructor() {
-    
   }
 
   startLottery() {
@@ -13,23 +13,11 @@ class Game {
 
   getUserPayment() { 
     MissionUtils.Console.readLine("구입금액을 입력해주세요 : \n", (userInput) => {
-      this.validatePayment(userInput);
+      const payment = Number(userInput);
+      (() => new Payment(payment))()
       const numOfTickets = userInput / 1000;
       this.generateGuessNumbers(numOfTickets);
     });
-  }
-
-  validatePayment(payment) {
-    const paymentNum = Number(payment);
-    if (!Number.isInteger(paymentNum)) {
-      throw new Error("[ERROR] 입력값은 정수여야 합니다.");
-    } 
-    if (paymentNum % 1000 !== 0) {
-      throw new Error("[ERROR] 입력값은 1000원 단위여야 합니다.");
-    } 
-    if (paymentNum < 0) {
-      throw new Error("[ERROR] 입력값은 0 이상이어야 합니다.");
-    }
   }
 
   generateGuessNumbers(numOfTickets) {
--- src/Payment.js
+++ src/Payment.js
@@ -9,19 +9,19 @@ class Payment {
   }
 
   validatePaymentInteger(numbers) {
-    if (!Number.isInteger(paymentNum)) {
+    if (!Number.isInteger(numbers)) {
       throw new Error("[ERROR] 구매금액은 정수여야 합니다.");
     }
   }
 
   validatePaymentMultipleOfThousand(numbers) {
-    if (paymentNum % 1000 !== 0) {
+    if (numbers % 1000 !== 0) {
       throw new Error("[ERROR] 구매금액은 1000원 단위여야 합니다.");
     }
   }
 
   validatePaymentNotNegative(numbers) {
-    if (paymentNum < 0) {
+    if (numbers < 0) {
       throw new Error("[ERROR] 구매금액은 0 이상이어야 합니다.");
     }
   }
