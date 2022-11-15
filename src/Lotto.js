514d140a31b49d6e2f64c38f293230ddfcd98dd7
jihun1233 <leejihun1233@gmail.com>
2022-11-15 23:50:27 +0900

    feat: Lotto 클래스 구현
    - #numbers를 생성자에서 받을 때 오름차순으로 sort하여 보관
    
    - getMatch함수로 당첨번호, 보너스번호와 몇개 일치하는지를 찾아 객체로 반환
    - printLotto로 해당 numbers를 출력할 수 있도록 함

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,9 +1,11 @@
+const MissionUtils = require('@woowacourse/mission-utils')
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a,b)=>a-b);
   }
 
   validate(numbers) {
@@ -12,7 +14,18 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  getMatch(matchNumbers, bonusNumber){
+    const matches = this.#numbers.reduce((acc, number)=>{
+      if(matchNumbers.includes(number))acc++
+      return acc
+    },0)
+    const bonus = this.#numbers.includes(bonusNumber)
+    return {matches, bonus}
+  }
+
+  printLotto(){
+    MissionUtils.Console.print(this.#numbers)
+  }
 }
 
 module.exports = Lotto;
