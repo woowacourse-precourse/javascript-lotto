49330fdee9530b50abf1ed7f547df15333ae57a0
xownsdl0 <xownsdl0@naver.com>
2022-11-15 15:17:44 +0900

    feat(lotto): 입력된 당첨 번호 유효성 검사 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,8 +7,18 @@ class Lotto {
   }
 
   validate(numbers) {
+    const regex = /^[0-9]+$/
+    
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    numbers.filter((size) => {
+      if (!(size >= 1 && size <= 45) || !regex.test(size)) {
+        throw new Error('[ERROR] 입력 범위를 초과합니다.')
+      }
+    })
+    if(new Set(numbers).size < numbers.length) {
+      throw new Error('[ERROR] 중복되는 숫자가 존재합니다.')
     }
   }
 
