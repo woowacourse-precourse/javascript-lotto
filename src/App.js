ea7adf4f29b4a6b192a705acff89f48528fa6910
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-15 22:39:05 +0900

    feat: 로또 구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const {
+  INPUT_MESSAGE,
+  ERROR_MESSAGE,
+  RESULT_MESSAGE,
+  LOTTO,
+  LOTTO_PRIZE,
+} = require("../src/Constants");
+
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    let inputMessage = "";
+    MissionUtils.Console.readLine(INPUT_MESSAGE.MONEY, (input) => {
+      inputMessage = input;
+    });
+    this.inputMoney(inputMessage);
+  }
+
+  inputMoney(input) {
+    this.money = input;
+  }
 }
 
 module.exports = App;
