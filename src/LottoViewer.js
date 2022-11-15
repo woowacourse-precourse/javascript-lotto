a6c2432fa69b9f13e95d909fb8bff1b069d60d8c
thewronghand <penfreak77@gmail.com>
2022-11-15 23:02:05 +0900

    refactor: result 객체 내 returnRate 밸류 반환 시 dot-notation 사용
    코드 컨벤션에 따라 객체의 밸류 반환에 dot-notation을 활용한다.

--- src/LottoViewer.js
+++ src/LottoViewer.js
@@ -21,13 +21,13 @@ class LottoViewer {
       REWARD.FIVE_MATCH_REWARD * result[3] +
       REWARD.FOUR_MATCH_REWARD * result[4] +
       REWARD.THREE_MATCH_REWARD * result[5];
-    result['returnRate'] = ((totalIncome / money) * 100).toFixed(1);
+    result.returnRate = ((totalIncome / money) * 100).toFixed(1);
     return result;
   }
 
   printLottoWinningStats(result) {
     MissionUtils.Console.print(
-      `\n당첨 통계\n---\n3개 일치 (5,000원) - ${result[5]}개\n4개 일치 (50,000원) - ${result[4]}개\n5개 일치 (1,500,000원) - ${result[3]}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[2]}개\n6개 일치 (2,000,000,000원) - ${result[1]}개\n총 수익률은 ${result['returnRate']}%입니다.`
+      `\n당첨 통계\n---\n3개 일치 (5,000원) - ${result[5]}개\n4개 일치 (50,000원) - ${result[4]}개\n5개 일치 (1,500,000원) - ${result[3]}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[2]}개\n6개 일치 (2,000,000,000원) - ${result[1]}개\n총 수익률은 ${result.returnRate}%입니다.`
     );
   }
 }
