b6dfbd10fa32271fc029a937e5bbe38f624f87bb
2yunseong <dbsdltjd3701@naver.com>
2022-11-15 19:22:15 +0900

    test(user): 로또 당첨 여부를 계산하는 기능 테스트

--- __tests__/UserTest.js
+++ __tests__/UserTest.js
@@ -1,6 +1,20 @@
 const User = require('../src/User');
 
 describe('User 클래스 테스트', () => {
+  test('금액으로 로또 게임을 얼마나 살 수 있는지 계산하는 기능', () => {
+    const user = new User();
+    user.amount = 10000;
+    user.calculateLottoCount();
+    expect(user.lottoCount).toEqual(10);
+  });
+
+  test('로또 당첨 여부를 계산하는 기능', () => {
+    const user = new User();
+    expect(
+      user.calcHitNumberCount([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])
+    ).toEqual(6);
+  });
+
   test('로또 순위를 계산하는 기능:', () => {
     const user = new User();
     expect(user.dicideRank(6, false)).toEqual(1);
@@ -9,12 +23,5 @@ describe('User 클래스 테스트', () => {
     expect(user.dicideRank(4, false)).toEqual(4);
     expect(user.dicideRank(3, false)).toEqual(5);
     expect(user.dicideRank(2, false)).toEqual(0);
-  });
-
-  test('금액으로 로또 게임을 얼마나 살 수 있는지 계산하는 기능', () => {
-    const user = new User();
-    user.amount = 10000;
-    user.calculateLottoCount();
-    expect(user.lottoCount).toEqual(10);
   });
 });
