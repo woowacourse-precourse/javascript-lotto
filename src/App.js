3b8cb77335b63217649edf26cb8589a1f4eaa54e
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-11-15 21:12:32 +0900

    feat(App): add 발행한 로또를 출력하는 기능

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const { Console } = MissionUtils;
 
 const VendingMachine = require("./lotto/domain/VendingMachine");
+const NumberGenerator = require("./lotto/domain/NumberGenerator");
 
 class App {
   constructor() {
     this.vendingMachine = new VendingMachine();
+    this.numberGenerator = new NumberGenerator();
   }
 
   play() {
     Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
       this.vendingMachine.setMoney(money);
 
-      return;
+      return this.printLotto(money);
     });
+  }
+
+  printLotto(money) {
+    Console.print(`\n${money / 1000}개를 구매했습니다.`);
+
+    let totalLotto = [];
+
+    for (let changeMoney = money; changeMoney >= 1000; changeMoney -= 1000) {
+      const purchasedLotto = this.numberGenerator.createRandomNumbers();
+      const strPurchasedLotto = "[" + purchasedLotto.join(", ") + "]";
+
+      totalLotto.push(purchasedLotto);
+      Console.print(strPurchasedLotto);
+    }
+
+    return;
   }
 }
 
