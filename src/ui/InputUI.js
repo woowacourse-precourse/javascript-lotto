12088cb696b13d2679c342e35bbb89cffc7c5773
2yunseong <dbsdltjd3701@naver.com>
2022-11-15 13:59:24 +0900

    refactor(InputUI): 책임 분리 및 중복되는 로직 함축

--- src/ui/InputUI.js
+++ src/ui/InputUI.js
@@ -4,41 +4,9 @@ const Vaildator = require('../Vaildator');
 const UI = require('./UI');
 
 class InputUI extends UI {
-  amount(message = '') {
-    return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLine(message, (amount) => {
-        if (!Vaildator.isRightAmount(+amount)) {
-          reject(new Error('[ERROR] 올바른 금액입력이 아닙니다.'));
-        }
-        resolve(+amount);
-      });
-    });
-  }
-
-  hitNumber(message = '') {
-    return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLine(message, (answer) => {
-        answer = TypeConverter.stringToArray(answer, ',');
-        if (Vaildator.isRightLottoNumbers(answer)) {
-          resolve(answer);
-        }
-        reject('error');
-      });
-    });
-  }
-
-  bonus(hitNumbers, message = '') {
-    return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLine(message, (answer) => {
-        answer = TypeConverter.stringToNumber(answer);
-        if (
-          Vaildator.isRightLottoNumber(answer) &&
-          Vaildator.isDuplicateNumberInArray(hitNumbers, answer)
-        ) {
-          resolve(answer);
-        }
-        reject('error');
-      });
+  readLine(callback, message = '') {
+    MissionUtils.Console.readLine(message, (answer) => {
+      callback(answer);
     });
   }
 }
