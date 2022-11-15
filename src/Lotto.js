377a87ceb7c28527572f8fc9aedb48e26b9db99b
saetakki <lime.lemonade03@gmail.com>
2022-11-15 20:44:28 +0900

    feat. Create calculate rank function

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,37 +1,45 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-const { isNotDuplicated, isNotExceedAmount } = require('./Validator');
-const Console = MissionUtils.Console;
-const randomNums = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+const { lottoInputCheckHandler } = require('./Validator.js');
+const { print, reducer } = require('./Funcs');
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    Lotto.validate(
-      numbers,
-      isNotExceedAmount(numbers),
-      isNotDuplicated(numbers),
-    );
-    this.#numbers = numbers;
+    const winningNumbers = lottoInputCheckHandler(numbers);
+    this.#numbers = {
+      winningNumbers: winningNumbers,
+      bonusNumber: 0,
+    };
   }
 
-  static validate = (...func) => {
-    func.reduce((acc, cur) => {
-      if (acc) return cur;
-    });
-  };
-
-  static makeIntArray = input => {
-    return input.split(',').map(x => Number(x));
-  };
-
-  static lottoGenerator = () => {
-    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
-      (a, b) => a - b,
-    );
-  };
-
   // TODO: 추가 기능 구현
+
+  get numbers() {
+    return this.#numbers;
+  }
+  setBonusNumber(input) {
+    this.#numbers.bonusNumber = Number(input);
+  }
+
+  static lottoGenerator() {
+    const randomNums = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    return randomNums.sort((a, b) => a - b);
+  }
+
+  calculateRank(mylotto, winningLotto, record) {
+    const mine = new Set(mylotto);
+    const winning = new Set(winningLotto.winningNumbers);
+    const score = [...mine].filter(x => winning.has(x)).length;
+
+    if (score === 6) return (record.first.count += 1);
+    if (score === 5 && mine.has(this.winningLotto.bonusNumber))
+      return (record.second.count += 1);
+    if (score === 5) return (record.third.count += 1);
+    if (score === 4) return (record.fourth.count += 1);
+    if (score === 3) return (record.fifth.count += 1);
+  }
+
 }
 
 module.exports = Lotto;
