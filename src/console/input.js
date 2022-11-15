d16736db4ebc4936f202cc8defd67d1f292bda41
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-15 22:42:51 +0900

    refactor: input 수정

--- src/console/input.js
+++ src/console/input.js
@@ -1,46 +1,9 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const TypeConverter = require("../util/TypeConverter");
-const Validator = require("../Validator");
 const console = require("./console");
 
 class input extends console {
-  fee(message = "") {
-    return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLine(message, (fee) => {
-        if (!Validator.isRightFee(+fee)) {
-          reject(new Error("[ERROR] 올바른 금액 입력이 아닙니다."));
-        }
-        resolve(+fee);
-      });
-    });
-  }
-
-  hitNumber(message = "") {
-    return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLine(message, (answer) => {
-        answer = TypeConverter.stringToArray(answer, ",");
-
-        if (Validator.isRightLottoNumbers(answer)) {
-          resolve(answer);
-        }
-        reject("error");
-      });
-    });
-  }
-
-  bonus(hitNumbers, message = "") {
-    return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLine(message, (answer) => {
-        answer = TypeConverter.stringToNumber(answer);
-
-        if (
-          Validator.isRightLottoNumber(answer) &&
-          Validator.isNumberInArray(hitNumbers, answer)
-        ) {
-          resolve(answer);
-        }
-        reject("error");
-      });
+  readLine(callback, message = "") {
+    MissionUtils.Console.readLine(message, (answer) => {
+      callback(answer);
     });
   }
 }
