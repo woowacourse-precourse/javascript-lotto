492ac56cc3945acb283a2c39967d6f8e1eff795b
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-15 19:59:34 +0900

    feat: 양수 확인 기능

--- src/Validator.js
+++ src/Validator.js
@@ -8,6 +8,13 @@ class Validator {
   static isLottoLength(target) {
     return target === config.LOTTO_MAX_LENGTH;
   }
+
+  static isPositiveNumber(target) {
+    if (isNaN(target)) {
+      return false;
+    }
+    return Number.isInteger(+target) && +target > 0;
+  }
 }
 
 module.exports = Validator;
