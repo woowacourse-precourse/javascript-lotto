d9e680fe738cee80e26fe2caca765fa466fbf9cd
동동이 <drdragon777@naver.com>
2022-11-15 16:46:27 +0900

    test - add LottoTest 예외처리 테스트 코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -14,6 +14,18 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
+  test("로또 번호 개수가 6개가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 숫자가 아닌 값이 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, NaN]);
+    }).toThrow("[ERROR]");
+  });
+
   test("로또 구매 금액에 대해 몇 개의 로또를 구매할 수 있는지 알 수 있다.", () => {
     expect(Lotto.caculateLottoNumPerUnit(1000)).toBe(1);
     expect(Lotto.caculateLottoNumPerUnit(19000)).toBe(19);
