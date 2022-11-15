883904380df907958f6b69b673dc8ec61092552c
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-15 22:03:08 +0900

    refactor(Judge) : 구매 금액의 유효성을 판단하는 함수를 의도가 더 담기도록 User에서 Buyer로 수정
    - isUserInputValid -> isBuyerInputValid
    - userInput -> buyerInput

--- src/Judge.js
+++ src/Judge.js
@@ -4,8 +4,8 @@ const { RANGE } = require("./Constant");
 class Judge {
   constructor() {}
 
-  isUserInputValid(userInput) {
-    if (userInput % 1000 !== 0) {
+  isBuyerInputValid(buyerInput) {
+    if (buyerInput % 1000 !== 0) {
       MissionUtils.Console.print(
         "[ERROR] 로또 한 장은 1000원 입니다. 1000의 배수로만 입력해주세요."
       );
@@ -13,7 +13,7 @@ class Judge {
         "[ERROR] 로또 한 장은 1000원 입니다. 1000의 배수로만 입력해주세요."
       );
     }
-    return true;
+    return buyerInput;
   }
 
   isLottoInputNaN(lottoInput) {
