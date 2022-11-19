8d50ad1b7971d73757e4372746c55d828e033a6a
Chun-gu <chungu.yi92@gmail.com>
2022-11-19 22:43:48 +0900

    refactor: add getter, change variable name

--- src/WinningNumbers.js
+++ src/WinningNumbers.js
@@ -3,24 +3,16 @@ const { LOTTO, ERROR, REGEXP } = require('./constant');
 class WinningNumbers {
   #numbers;
 
-  constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
+  constructor(input) {
+    this.validate(input);
+    this.#numbers = input.split(',').map(Number);
   }
 
-  getNumbers() {
-    return this.#numbers;
-  }
+  validate(input) {
+    const isNumbersAndCommas = REGEXP.winningNumbers.test(input);
+    if (!isNumbersAndCommas) throw new Error(ERROR.winningNumbers);
 
-  validate(numbers) {
-    numbers.forEach((number) => {
-      const isNumbersAndCommas = REGEXP.number.test(String(number));
-      if (!isNumbersAndCommas) throw new Error(ERROR.winningNumbers);
-    });
-
-    if (numbers.length > 6) throw new Error(ERROR.winningNumbers);
-
-    // const numbers = number.split(',').map(Number);
+    const numbers = input.split(',').map(Number);
     const hasDuplicates = numbers.length !== new Set(numbers).size;
     if (hasDuplicates) throw new Error(ERROR.winningNumbers);
 
@@ -29,6 +21,10 @@ class WinningNumbers {
     const lastNumber = sortedNumbers[sortedNumbers.length - 1];
     const isInRange = LOTTO.rangeStart <= firstNumber && lastNumber <= LOTTO.rangeEnd;
     if (!isInRange) throw new Error(ERROR.winningNumbers);
+  }
+
+  get() {
+    return this.#numbers;
   }
 }
 
