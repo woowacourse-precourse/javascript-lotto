bf65e82ff5ba24d3e87461022a16e6571621576a
dim <dim@student.42seoul.kr>
2022-11-23 20:33:05 +0900

    Style: InputView 파일 분리

---
+++ src/InputView.js
@@ -0,0 +1,42 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const { checkIsNum, checkRightAmountMoney } = requrie('./util');
+
+const InputView = {
+  readInput(makeLotto) {
+    MissionUtils.Console.readLine(PRINT.ENTER_PURCHASE_AMOUNT_INPUT, (money) => {
+      this.validateMoney(money);
+      makeLotto(money);
+    });
+  },
+
+  validateMoney(money) {
+    checkIsNum(money);
+    checkRightAmountMoney(money);
+  },
+
+  readLottoNums() {
+    MissionUtils.Console.print('');
+    MissionUtils.Console.readLine(PRINT.ENTER_WINNING_NUMS, (numbers) => {
+      this.lotto = new Lotto(numbers.split(','));
+      readBonusNum();
+    });
+  },
+
+  readBonusNum() {
+    MissionUtils.Console.print('');
+    MissionUtils.Console.readLine(PRINT.ENTER_BONUS_NUM, (input) => {
+      this.validateBonusNum(input);
+      this.bonusNum = input;
+      const result = new Result(
+        this.randomNumUnits,
+        this.lotto.getNumbers(),
+        this.bonusNum
+      );
+      result.getResult();
+      MissionUtils.Console.close();
+    });
+  },
+
+};
+
+module.exports = InputView;
