c297a6612b9aa04404e42f74164b5b7128cd1f35
hwanfront <kim015jh@gmail.com>
2022-12-02 00:29:29 +0900

    refactor(view): view 영역 클래스에서 객체로 변경

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,29 +1,29 @@
 const { Console } = require('@woowacourse/mission-utils');
 
-class InputView {
-  static #MESSAGE = Object.freeze({
+const InputView = {
+  MESSAGE: Object.freeze({
     inputLottoAmount: '구입금액을 입력해 주세요.\n',
     inputWinningLottoNumbers: '당첨 번호를 입력해 주세요.\n',
     inputBonusNumber: '보너스 번호를 입력해 주세요.\n',
-  });
+  }),
 
-  static inputLottoAmount(callback) {
-    Console.readLine(InputView.#MESSAGE.inputLottoAmount, (answer) => {
+  readLottoAmount(callback) {
+    Console.readLine(InputView.MESSAGE.inputLottoAmount, (answer) => {
       callback(Number(answer));
     });
-  }
+  },
 
-  static inputWinningLottoNumbers(callback) {
-    Console.readLine(InputView.#MESSAGE.inputWinningLottoNumbers, (answer) => {
+  readWinningLottoNumbers(callback) {
+    Console.readLine(InputView.MESSAGE.inputWinningLottoNumbers, (answer) => {
       callback(answer.split(',').map((value) => parseInt(value, 10)));
     });
-  }
+  },
 
-  static inputBonusNumber(callback) {
-    Console.readLine(InputView.#MESSAGE.inputBonusNumber, (answer) => {
+  readBonusNumber(callback) {
+    Console.readLine(InputView.MESSAGE.inputBonusNumber, (answer) => {
       callback(Number(answer));
     });
-  }
-}
+  },
+};
 
 module.exports = InputView;
