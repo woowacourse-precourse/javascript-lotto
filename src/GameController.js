ad0efc9f2b74f616d9c68b39fecd1ed746e831ab
박겸영 <yong_drg@naver.com>
2022-12-04 19:01:11 +0900

    feat(OutputView): 당첨 내역 및 수익률 출력하는 기능 추가
    - 수익률은 소수점 둘째 자리에서 반올림하고 3자리마다 쉼표(,) 표시하기

--- src/GameController.js
+++ src/GameController.js
@@ -1,15 +1,16 @@
+const { Console } = require('@woowacourse/mission-utils');
 const Purchase = require('./Purchase');
 const Draw = require('./Draw');
 const Prize = require('./Prize');
 const { readPurchaseAmount, readWinningNumbers, readBonusNumber } = require('./InputView');
-const { printPurchase } = require('./OutputView');
+const { printPurchase, printStatistics } = require('./OutputView');
 
 class GameController {
   #purchase = new Purchase();
 
   #draw = new Draw();
 
-  #prize = new Prize();
+  #prize;
 
   readPurchase() {
     readPurchaseAmount(this.makePurchase.bind(this));
@@ -20,6 +21,8 @@ class GameController {
     this.#purchase.issueLottos(count, Purchase.getRandomNumbers);
 
     printPurchase(count, this.#purchase.getIssuedLottos());
+
+    this.readWinning();
   }
 
   readWinning() {
@@ -32,18 +35,22 @@ class GameController {
 
   drawWinningNumbers(winningNumbers) {
     this.#draw.setWinningNumbers(winningNumbers);
+    this.readBonus();
   }
 
   drawBonusNumber(bonusNumber) {
     this.#draw.setBonusNumber(bonusNumber);
+    this.printGameResult();
   }
 
   getPrizeResult() {
     const { winningNumbers, bonusNumber } = this.#draw.getWinningAndBonus();
 
-    const prize = new Prize();
-    const result = prize.getResult(this.#purchase.getIssuedLottos(), winningNumbers, bonusNumber);
-
+    const result = this.#prize.getResult(
+      this.#purchase.getIssuedLottos(),
+      winningNumbers,
+      bonusNumber,
+    );
     return result;
   }
 
@@ -51,6 +58,16 @@ class GameController {
     const expense = this.#purchase.getExpense();
 
     return this.#prize.getProfitRate(expense);
+  }
+
+  printGameResult() {
+    this.#prize = new Prize();
+    const prizeResult = this.getPrizeResult();
+    const profitRate = this.getProfitRate();
+
+    printStatistics(prizeResult, profitRate);
+
+    Console.close();
   }
 }
 
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,24 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 const OutputView = {
-  message: { COUNT: '개를 구매했습니다.' },
+  message: {
+    COUNT: '개를 구매했습니다.',
+
+    statistics: {
+      HEADING: '당첨 통계\n---',
+      CRITERIA: '개 일치',
+      COUNT: '개',
+      BONUS: ', 보너스 볼 일치',
+
+      getMoney(money) {
+        return `(${money.toLocaleString('en-US')}원)`;
+      },
+
+      getProfitRate(profitRate) {
+        return `총 수익률은 ${profitRate}%입니다.`;
+      },
+    },
+  },
 
   printPurchase(count, issueLottos) {
     const { COUNT } = OutputView.message;
@@ -9,6 +26,27 @@ const OutputView = {
     const issuedLottosMessage = issueLottos.map((lotto) => `[${lotto.join(', ')}]`).join('\n');
 
     Console.print(`\n${countMessage}\n${issuedLottosMessage}`);
+  },
+
+  getPrizeResultMessage(prizeResult) {
+    const { CRITERIA, COUNT, BONUS, getMoney } = OutputView.message.statistics;
+    return prizeResult
+      .reverse()
+      .map(
+        ({ criteria, money, hasBonus, count }) =>
+          `${criteria}${CRITERIA}${hasBonus ? BONUS : ''} ${getMoney(money)} - ${count}${COUNT}`,
+      )
+      .join('\n');
+  },
+
+  printStatistics(prizeResult, profitRate) {
+    const { HEADING, getProfitRate } = OutputView.message.statistics;
+    const prizeResultMessage = OutputView.getPrizeResultMessage(prizeResult);
+
+    const profitRateMessage = `${getProfitRate(profitRate)}`;
+    const statisticsMessage = `\n${HEADING}\n${prizeResultMessage}\n${profitRateMessage}`;
+
+    Console.print(statisticsMessage);
   },
 };
 
