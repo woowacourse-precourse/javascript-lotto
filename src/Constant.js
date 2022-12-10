17f719dd940cc42bf23302ce51ac4fd1d0ce8a77
ming0830 <minsun0830@naver.com>
2022-12-10 20:40:10 +0900

    [refactor] feat : 사용자로부터 입력값 받는 기능 구현 ( 구매 금액, 당첨 번호, 보너스 번호)

---
+++ src/Constant.js
@@ -0,0 +1,10 @@
+/** 상수를 관리하는 역할을 한다 */
+
+const INPUT_MESSAGE = {
+  buyLottoMessage : "구입금액을 입력해 주세요.\n",
+  winningNumbersMessage : "당첨 번호를 입력해 주세요.\n",
+  bonusNumberMessage : "보너스 번호를 입력해 주세요.\n",
+};
+
+
+module.exports = {INPUT_MESSAGE}
